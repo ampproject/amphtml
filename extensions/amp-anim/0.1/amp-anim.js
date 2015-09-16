3ff7b525bf1d1eb676933fd545ee29a13ce77387
Jordan Adler <jmadler@google.com>
2015-09-15 23:04:55 -0400

    Add skeleton

--- extensions/amp-anim/0.1/amp-anim.js
+++ extensions/amp-anim/0.1/amp-anim.js
@@ -21,6 +21,8 @@ import * as st from '../../../src/style';
 
 class AmpAnim extends AMP.BaseElement {
 
+  var supported_mimetypes = {}; // cache of supported mime types
+
   /** @override */
   isLayoutSupported(layout) {
     return isLayoutSizeDefined(layout);
@@ -35,6 +37,42 @@ class AmpAnim extends AMP.BaseElement {
   buildCallback() {
     /** @private @const {?Element} */
     this.placeholder_ = this.getPlaceholder();
+
+    // We want to be able to support <source> children that represent either
+    // images or videos, and select the most performant one amongst them.  
+    //
+    // This is because some web developers will create a gif, webm, and mp4
+    // file representing the same animated image, and dynamically display the
+    // most performant one amongst these supported by the User Agent.
+    // 
+    // We rely on the source tags to already be in preferred order, so all 
+    // we need to do is select the first one that's supported, create the 
+    // corresponding amp-image/amp-video tag, and move the source tag under
+    // that tag, and delete the other source tags.
+
+
+    this.getRealChildNodes().forEach(child => { // XXX will this preserve order?
+      if (child.tagName == 'SOURCE') {
+          // only supports <source> tag at the moment.  the notable exclusion
+          // is <track>
+          // XXX do I need to confirm attributes exists, and type is a valid call?
+          if (child.attributes.type != null) {
+              // we do want to pass any optional parameters in the typeval
+              // media type through to typeSupported.
+              var typeval = trim(child.attributes.type); // XXX confirm output of trim() supports this usage
+              var type = typeval.slice(0, typeval.indexOf('/'));
+              if (type == 'IMAGE' or type == 'VIDEO') {
+                  // only image and video types are supported
+                  if (mimetypeSupported(typeval, type)) {
+                      // create a corresponding amp-img/video tag
+                      // XXX IMAGE => img
+                      var media_tag = 'amp-' + type.toLowerCase();
+                      // XXX create tag, move source under it, escape, delete parent tag
+                  }
+              }
+          }
+      }
+    });
 
     /** @private @const {!Element} */
     this.img_ = new Image();
@@ -51,6 +89,16 @@ class AmpAnim extends AMP.BaseElement {
     /** @private @const {!Srcset} */
     this.srcset_ = parseSrcset(this.element.getAttribute('srcset') ||
         this.element.getAttribute('src'));
+
+    /** @private */
+    typeSupported(typeval, type) {
+      if (!typeval in this.supported_types) {
+        // we haven't seen this mimetype yet
+        this.supported_types[type] =
+          document.createElement('video').canPlayType(type);
+      }
+      return this.supported_mimetypes[mimetype];
+    }
 
     /** @private {?Promise} */
     this.loadPromise_ = null;
