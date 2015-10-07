4897160abebe476b35ed6b7aac152a40a8d74708
Kent Brewster <kent@pinterest.com>
2015-10-06 18:42:13 -1000

    use CORS to get pin count for Pin It buttons

--- extensions/amp-pinterest/0.1/amp-pinterest.js
+++ extensions/amp-pinterest/0.1/amp-pinterest.js
@@ -64,119 +64,231 @@ class AmpPinterest extends AMP.BaseElement {
     let width = this.element.getAttribute('width');
 
     var pinDo = AMP.assert(this.element.getAttribute('data-do'),
-        'The data-do attribute is required for <amp-pinterest> %s',
-        this.element);
+      'The data-do attribute is required for <amp-pinterest> %s',
+      this.element);
 
     var pinUrl = AMP.assert(this.element.getAttribute('data-url'),
-        'The data-url attribute is required for <amp-pinterest> %s',
-        this.element);
+      'The data-url attribute is required for <amp-pinterest> %s',
+      this.element);
 
-    // set by data-do -- buttonPin renders a Pin It button and requires media and description
-    if (pinDo === 'buttonPin') {
+    // load data from API via XHR
+    var get = function(url) {
+      return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+              resolve(xhr.responseText);
+            } else {
+              reject(new Error('API call failed: ' + url));
+            }
+          }
+        };
+        xhr.open("GET", url, true);
+        xhr.send();
+      });
+    }
+
+    // pinDo is set by data-do
+    switch (pinDo) {
+
+      case 'buttonPin':
+
+        // buttonPin renders a Pin It button and requires media and description
 
         var pinMedia = AMP.assert(this.element.getAttribute('data-media'),
-            'The data-media attribute is required when <amp-pinterest> makes a Pin It button %s',
-            this.element);
+          'The data-media attribute is required when <amp-pinterest> makes a Pin It button %s',
+          this.element);
 
         var pinDescription = AMP.assert(this.element.getAttribute('data-description'),
-            'The data-description attribute is required when <amp-pinterest> makes a Pin It button %s',
-            this.element);
+          'The data-description attribute is required when <amp-pinterest> makes a Pin It button %s',
+          this.element);
 
-        // we're a simple link
-        let a = document.createElement('A');
-
-        a.href = 'https://www.pinterest.com/pin/create/button/';
-        a.href = a.href + '?url=' + encodeURIComponent(pinUrl);
-        a.href = a.href + '&media=' + encodeURIComponent(pinMedia);
-        a.href = a.href + '&description=' + encodeURIComponent(pinDescription);
-
-        // any special requests?
+        // options
         let shape = this.element.getAttribute('data-shape');
         let color = this.element.getAttribute('data-color');
         let height = this.element.getAttribute('data-height');
         let lang = this.element.getAttribute('data-lang');
+        let config = this.element.getAttribute('data-config');
 
-        // start setting our class name
-        let className = '';
+        // start building a link
+        var a = document.createElement('A');
 
-        // first selector: set size and shape
+        // save our outside context so we can return after rendering
+        let that = this;
 
-        if (shape === 'round') {
-          // we're round
-          className = 'amp-pinterest-round';
-          if (height === '32') {
-            // we're tall
-            className = 'amp-pinterest-round-32';
+        // built it
+        var render = function (r) {
+
+          // shorten the pin count so it will fit in our bubble
+          let prettyPinCount = function (n) {
+            if (n > 999) {
+              if (n < 1000000) {
+                n = parseInt(n / 1000, 10) + 'K+';
+              } else {
+                if (n < 1000000000) {
+                  n = parseInt(n / 1000000, 10) + 'M+';
+                } else {
+                  n = '++';
+                }
+              }
+            }
+            return n;
+          };
+
+          // start setting our class name
+          let className = '';
+
+          // first selector: set size and shape
+          if (shape === 'round') {
+
+            // we're round
+            className = 'amp-pinterest-round';
+            if (height === '32') {
+              // we're tall
+              className = 'amp-pinterest-round-32';
+            }
+
+          } else {
+
+            // we're rectangular
+            className = 'amp-pinterest-rect';
+            if (height === '28') {
+              // we're tall
+              className = className + '-28';
+            }
+
+            // second selector: set background image
+            className = className + ' amp-pinterest';
+            if (lang !== 'ja') {
+              // we're not Japanese
+              lang = 'en';
+            }
+
+            className = className + '-' + lang;
+            if (color !== 'red' && color !== 'white') {
+              // if we're not red or white, we're gray
+              color = 'gray';
+            }
+            className = className + '-' + color;
+
+            // yes, we do this twice; once for container and once for background image
+            if (height === '28') {
+              // we're tall
+              className = className + '-28';
+            }
           }
-        } else {
-          // we're rectangular
-          className = 'amp-pinterest-rect';
-          if (height === '28') {
-            // we're tall
-            className = className + '-28';
+
+          if (r) {
+
+            // API reply comes wrapped in a default callback; need to remove if present
+            r = r.replace(/^receiveCount\(/, '');
+            r = r.replace(/\)$/, '');
+
+            // can we parse the data?
+            try {
+              let data = JSON.parse(r);
+              let countBubble = document.createElement('SPAN');
+
+              // position the count
+              if (config === 'above') {
+                className = className + ' amp-pinterest-count-pad-above';
+                countBubble.className = 'amp-pinterest-bubble-above';
+              } else {
+                className = className + ' amp-pinterest-count-pad-beside';
+                countBubble.className = 'amp-pinterest-bubble-beside';
+              }
+
+              // size the bubble
+              if (height === '28') {
+                className = className + '-28';
+                countBubble.className = countBubble.className + '-28';
+              } else {
+                className = className + '-20';
+                countBubble.className = countBubble.className + '-20';
+              }
+
+              // fill with text; zero
+              countBubble.innerHTML = prettyPinCount(data.count - 0) || '0';
+
+              // append
+              a.appendChild(countBubble);
+            } catch (err) {
+              // fail silently; what we thought was JSON could not be parsed
+            }
           }
 
-          // second selector: set background image
+          // set our className
+          a.className = className;
 
-          className = className + ' amp-pinterest';
-          if (lang !== 'ja') {
-            // we're not Japanese
-            lang = 'en';
-          }
+          // build our link
+          a.href = 'https://www.pinterest.com/pin/create/button/';
+          a.href = a.href + '?url=' + encodeURIComponent(pinUrl);
+          a.href = a.href + '&media=' + encodeURIComponent(pinMedia);
+          a.href = a.href + '&description=' + encodeURIComponent(pinDescription);
 
-          className = className + '-' + lang;
-          if (color !== 'red' && color !== 'white') {
-            // if we're not red or white, we're gray
-            color = 'gray';
-          }
-          className = className + '-' + color;
+          // listen for clicks
+          a.addEventListener('click', function (e) {
+            // pop the pin create window on Pinterest
+            window.open(this.href, '_pinit', 'status=no,resizable=yes,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=900,height=500,left=0,top=0');
+            e.preventDefault();
+          });
 
-          // yes, we do this twice; once for container and once for background image
-          if (height === '28') {
-            // we're tall
-            className = className + '-28';
-          }
+          // fill it
+          that.applyFillContent(a);
+          // append it
+          that.element.appendChild(a);
+          // done
+          return loadPromise(a);
         }
 
-        a.className = className;
+        // only ask for a count if the operator requires it
+        if (config === 'above' || config === 'beside') {
+          // call the API
+          let call = 'https://widgets.pinterest.com/v1/urls/count.json?url=' + encodeURIComponent(pinUrl);
+          return get(call).then((r) => {
+            render(r);
+            return loadPromise(a);
+          });
+        } else {
+          // you have everything you need to render with no waiting for API
+          render();
+          return loadPromise(a);
+        }
 
-        a.addEventListener('click', function (e) {
-          window.open(this.href, '_pinit', 'status=no,resizable=yes,scrollbars=yes,personalbar=no,directories=no,location=no,toolbar=no,menubar=no,width=900,height=500,left=0,top=0');
-          e.preventDefault();
-        });
+      break;
 
-        this.applyFillContent(a);
-        this.element.appendChild(a);
-        return loadPromise(a);
+      default:
 
-    } else {
+        // we're a more complex widget, currently rendered in an iframe
+        // looking into doing these inline now
 
-      // we're a more complex widget, currently rendered in an iframe
-      let iframe = document.createElement('iframe');
-      iframe.setAttribute('frameborder', '0');
-      iframe.setAttribute('allowtransparency', 'true');
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowtransparency', 'true');
 
-      // start setting the source of our iframe
-      let src = 'https://assets.pinterest.com/ext/iffy.html?' +
-          'act=' + encodeURIComponent(pinDo) +
-          '&url=' + encodeURIComponent(pinUrl);
+        // start setting the source of our iframe
+        let src = 'https://assets.pinterest.com/ext/iffy.html?' +
+            'act=' + encodeURIComponent(pinDo) +
+            '&url=' + encodeURIComponent(pinUrl);
 
-      for (let i = 0; i < VALID_PARAMS.length; i = i + 1) {
-          let v = this.element.getAttribute(VALID_PARAMS[i]);
-          if (v) {
-              // remove data- prefix from params
-              src = src + '&' + VALID_PARAMS[i].replace(/data-/, '') + '=' + encodeURIComponent(v);
-          }
-      }
+        for (let i = 0; i < VALID_PARAMS.length; i = i + 1) {
+            let v = this.element.getAttribute(VALID_PARAMS[i]);
+            if (v) {
+                // remove data- prefix from params
+                src = src + '&' + VALID_PARAMS[i].replace(/data-/, '') + '=' + encodeURIComponent(v);
+            }
+        }
 
-      iframe.src = src;
+        iframe.src = src;
 
-      this.applyFillContent(iframe);
-      iframe.width = width;
-      iframe.height = height;
-      this.element.appendChild(iframe);
-      return loadPromise(iframe);
+        this.applyFillContent(iframe);
+        iframe.width = width;
+        iframe.height = height;
+        this.element.appendChild(iframe);
+        return loadPromise(iframe);
 
+      break;
     }
   }
 };
