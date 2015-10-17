f3604d312e891dba732f2c5139a995064606a3cf
Malte Ubl <malte.ubl@gmail.com>
2015-10-17 14:32:08 -0700

    Line

--- src/amp.js
+++ src/amp.js
@@ -40,6 +40,7 @@ try {
   // Should happen first.
   installErrorReporting(window);  // Also calls makeBodyVisible on errors.
   installStyles(document, cssText, () => {
+
     try {
       historyFor(window);
       viewerFor(window);
