134b47fa141a9ce2dea8b643813b8afe432e5321
Erwin Mombay <erwinm@google.com>
2015-11-02 12:35:57 -0800

    Put back destructure presubmit check

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -54,6 +54,10 @@ var forbiddenTerms = {
   'Promise\\.race': es6polyfill,
   '\\.startsWith': es6polyfill,
   '\\.endsWith': es6polyfill,
+  // TODO: (erwinm) rewrite the destructure and spread warnings as
+  // eslint rules (takes more time than this quick regex fix).
+  // No destructuring allowed since we dont ship with Array polyfills.
+  '^\\s*(?:let|const|var) *(?:\\[[^\\]]+\\]|{[^}]+}) *=': es6polyfill,
   // No spread (eg. test(...args) allowed since we dont ship with Array
   // polyfills except `arguments` spread as babel does not polyfill
   // it since it can assume that it can `slice` w/o the use of helpers.
