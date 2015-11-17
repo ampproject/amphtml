1f75c90494a07a3898997bb6b8f9cd1cc4636f3d
Dima Voytenko <dvoytenko@google.com>
2015-11-17 10:17:36 -0800

    UrlReplacements refactoring for amp-pixel and amp-list

---
+++ test/functional/test-url-replacements.js
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {createIframePromise} from '../../testing/iframe';
+import {UrlReplacements} from '../../src/url-replacements';
+
+
+describe('UrlReplacements', () => {
+
+  function expand(url) {
+    return createIframePromise().then(iframe => {
+      iframe.doc.title = 'Pixel Test';
+      const link = iframe.doc.createElement('link');
+      link.setAttribute('href', 'https://pinterest.com/pin1');
+      link.setAttribute('rel', 'canonical');
+      iframe.doc.head.appendChild(link);
+      const replacements = new UrlReplacements(iframe.win);
+      return replacements.expand(url);
+    });
+  }
+
+  it('should replace RANDOM', () => {
+    return expand('ord=RANDOM?').then(res => {
+      expect(res).to.match(/ord=(\d\.\d+)\?$/);
+    });
+  });
+
+  it('should replace CANONICAL_URL', () => {
+    return expand('?href=CANONICAL_URL').then(res => {
+      expect(res).to.equal('?href=https%3A%2F%2Fpinterest.com%2Fpin1');
+    });
+  });
+
+  it('should replace CANONICAL_HOST', () => {
+    return expand('?host=CANONICAL_HOST').then(res => {
+      expect(res).to.equal('?host=pinterest.com');
+    });
+  });
+
+  it('should replace CANONICAL_PATH', () => {
+    return expand('?path=CANONICAL_PATH').then(res => {
+      expect(res).to.equal('?path=%2Fpin1');
+    });
+  });
+
+  it('should replace DOCUMENT_REFERRER', () => {
+    return expand('?ref=DOCUMENT_REFERRER').then(res => {
+      expect(res).to.equal('?ref=http%3A%2F%2Flocalhost%3A9876%2Fcontext.html');
+    });
+  });
+
+  it('should replace TITLE', () => {
+    return expand('?title=TITLE').then(res => {
+      expect(res).to.equal('?title=Pixel%20Test');
+    });
+  });
+
+  it('should replace AMPDOC_URL', () => {
+    return expand('?ref=AMPDOC_URL').then(res => {
+      expect(res).to.not.match(/AMPDOC_URL/);
+    });
+  });
+
+  it('should replace AMPDOC_HOST', () => {
+    return expand('?ref=AMPDOC_HOST').then(res => {
+      expect(res).to.not.match(/AMPDOC_HOST/);
+    });
+  });
+
+  it('should accept $expressions', () => {
+    return expand('?href=$CANONICAL_URL').then(res => {
+      expect(res).to.equal('?href=https%3A%2F%2Fpinterest.com%2Fpin1');
+    });
+  });
+
+  it('should ignore unknown substitutions', () => {
+    return expand('?a=UNKNOWN').then(res => {
+      expect(res).to.equal('?a=UNKNOWN');
+    });
+  });
+
+  it('should replace several substitutions', () => {
+    return expand('?a=UNKNOWN&href=CANONICAL_URL&title=TITLE').then(res => {
+      expect(res).to.equal('?a=UNKNOWN' +
+          '&href=https%3A%2F%2Fpinterest.com%2Fpin1' +
+          '&title=Pixel%20Test');
+    });
+  });
+
+  it('should replace new substitutions', () => {
+    const replacements = new UrlReplacements(window);
+    replacements.set('ONE', () => 'a');
+    expect(replacements.expand('?a=ONE')).to.equal('?a=a');
+
+    replacements.set('ONE', () => 'b');
+    expect(replacements.expand('?a=ONE')).to.equal('?a=b');
+
+    replacements.set('TWO', () => 'b');
+    expect(replacements.expand('?b=TWO')).to.equal('?b=b');
+  });
+});
