2b57460f5c4d329aa9ff833246f0260efdc2c863
RonanDrouglazet <ronan.drouglazet@teads.tv>
2015-12-04 14:59:42 +0100

    Implement Teads on Google AMP Ads network

---
+++ ads/teads.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {loadScript, checkData, validateDataExists} from '../src/3p';
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ */
+export function teads(global, data) {
+  /*eslint "google-camelcase/google-camelcase": 0*/
+  global._teads_amp = {
+    allowed_data: ['pid'],
+    data: data,
+  };
+
+  validateDataExists(data, global._teads_amp.allowed_data);
+  checkData(data, global._teads_amp.allowed_data);
+
+  loadScript(global, 'https://a.teads.tv/page/' + encodeURIComponent(data.pid) + '/tag');
+}
