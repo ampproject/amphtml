9c390bd71567fb2085b52305992271f40de88cf8
Malte Ubl <malte.ubl@gmail.com>
2016-01-15 17:53:44 -0800

    Do not redundantly build the repo for tests when running on travis.

--- build-system/tasks/test.js
+++ build-system/tasks/test.js
@@ -51,10 +51,17 @@ function getConfig() {
   return extend(obj, karmaConfig.default);
 }
 
+var prerequisites = ['build'];
+if (process.env.TRAVIS) {
+  // No need to do this because we are guaranteed to have done
+  // it.
+  prerequisites = [];
+}
+
 /**
  * Run tests.
  */
-gulp.task('test', 'Runs tests in chrome', ['build'], function(done) {
+gulp.task('test', 'Runs tests', prerequisites, function(done) {
   if (argv.saucelabs && process.env.MAIN_REPO &&
       // Sauce Labs does not work on Pull Requests directly.
       // The @ampsauce bot builds these.
