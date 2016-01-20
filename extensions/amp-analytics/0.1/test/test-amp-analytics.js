ef11aa8e4cab894ea2707e3dff57e43912a2459d
Avi Mehta <avimehta@google.com>
2016-01-20 10:28:09 -0800

    Removed experiment flag from amp-analytics. #1485
    Also fixed a link related to GA. The link is still not live.

--- extensions/amp-analytics/0.1/test/test-amp-analytics.js
+++ extensions/amp-analytics/0.1/test/test-amp-analytics.js
@@ -73,23 +73,10 @@ describe('amp-analytics', function() {
     }
     const analytics = new AmpAnalytics(el);
     sandbox.stub(analytics, 'getWin').returns(windowApi);
-    analytics.isExperimentOn_ = () => true;
     analytics.createdCallback();
     sendRequestSpy = sandbox.spy(analytics, 'sendRequest_');
     return analytics;
   }
-
-  it('is blocked when experiment is off', function() {
-    const analytics = getAnalyticsTag({
-      'requests': {'foo': 'https://example.com/bar'},
-      'triggers': [{'on': 'visible', 'request': 'foo'}]
-    });
-
-    analytics.isExperimentOn_ = () => false;
-    return analytics.layoutCallback().then(() => {
-      expect(sendRequestSpy.callCount).to.equal(0);
-    });
-  });
 
   it('sends a basic hit', function() {
     const analytics = getAnalyticsTag({
@@ -112,7 +99,6 @@ describe('amp-analytics', function() {
     el.textContent = config;
     const analytics = new AmpAnalytics(el);
     sandbox.stub(analytics, 'getWin').returns(windowApi);
-    analytics.isExperimentOn_ = () => true;
     analytics.createdCallback();
     sendRequestSpy = sandbox.spy(analytics, 'sendRequest_');
 
@@ -145,7 +131,6 @@ describe('amp-analytics', function() {
         el.appendChild(script);
         const analytics = new AmpAnalytics(el);
         sandbox.stub(analytics, 'getWin').returns(windowApi);
-        analytics.isExperimentOn_ = () => true;
         analytics.createdCallback();
         sendRequestSpy = sandbox.spy(analytics, 'sendRequest_');
 
