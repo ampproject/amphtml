c6031c6d373f905e6172ce003be7977b1bf74762
Malte Ubl <malte.ubl@gmail.com>
2016-02-09 17:05:15 -0800

    Throttle intersection observer posts to 10 per second.
    Also filters out changes that happen in the same milli seconds.
    Deletes the `amp-iframe` intersection tests because they are super bad and I already filed #1822 to rewrite them.

--- src/intersection-observer.js
+++ src/intersection-observer.js
@@ -19,6 +19,7 @@ import {assert} from './asserts';
 import {layoutRectLtwh, rectIntersection, moveLayoutRect} from './layout-rect';
 import {listen, postMessage} from './iframe-helper';
 import {parseUrl} from './url';
+import {timer} from './timer';
 
 /**
  * Produces a change entry for that should be compatible with
@@ -103,10 +104,17 @@ export class IntersectionObserver extends Observable {
     this.is3p_ = opt_is3p || false;
     /** @private {boolean} */
     this.shouldSendIntersectionChanges_ = false;
-    /** @private {Array<function>} */
-    this.unlisteners_ = [];
     /** @private {boolean} */
     this.inViewport_ = false;
+
+    /** @private {!Array<!IntersectionObserverEntry>} */
+    this.pendingChanges_ = [];
+
+    /** @private {number} */
+    this.flushTimeout_ = 0;
+
+    /** @private @const {function()} */
+    this.boundFlush_ = this.flush_.bind(this);
 
     this.init_();
   }
@@ -118,23 +126,14 @@ export class IntersectionObserver extends Observable {
     // The second time this is called, it doesn't do much but it
     // guarantees that the receiver gets an initial intersection change
     // record.
-    this.unlisteners_.push(listen(this.iframe_, 'send-intersections', () => {
+    listen(this.iframe_, 'send-intersections', () => {
       this.startSendingIntersectionChanges_();
-    }, this.is3p_));
+    }, this.is3p_);
 
-    this.unlisteners_.push(this.add(() => {
+    this.add(() => {
       this.sendElementIntersection_();
-    }));
-  }
-
-  dispose() {
-    //used only in tests.
-    this.unlisteners_.forEach(unlisten => {
-      unlisten();
     });
-    this.unlisteners_ = [];
   }
-
   /**
    * Called via postMessage from the child iframe when the ad/iframe starts
    * observing its position in the viewport.
@@ -195,13 +194,31 @@ export class IntersectionObserver extends Observable {
       return;
     }
     const change = this.baseElement_.element.getIntersectionChangeEntry();
+    if (this.pendingChanges_.length > 0 &&
+        this.pendingChanges_[this.pendingChanges_.length - 1].time
+        == change.time) {
+      return;
+    }
+    this.pendingChanges_.push(change);
+    if (!this.flushTimeout_) {
+      // Send a maximum of 10 postMessages per second.
+      this.flushTimeout_ = timer.delay(this.boundFlush_, 100);
+    }
+  }
+
+  flush_() {
+    this.flushTimeout_ = 0;
+    if (!this.pendingChanges_.length) {
+      return;
+    }
     const targetOrigin =
         this.iframe_.src ? parseUrl(this.iframe_.src).origin : '*';
     postMessage(
         this.iframe_,
         'intersection',
-        {changes: [change]},
+        {changes: this.pendingChanges_},
         targetOrigin,
         this.is3p_);
+    this.pendingChanges_.length = 0;
   }
 }
