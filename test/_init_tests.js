cbebca0cc595bd91ef11bbe614070bc931f16d26
Malte Ubl <malte.ubl@gmail.com>
2016-02-17 23:41:30 -0500

    Deleting code is hard.

--- test/_init_tests.js
+++ test/_init_tests.js
@@ -76,7 +76,7 @@ sinon.sandbox.create = function(config) {
 // Global cleanup of tags added during tests. Cool to add more
 // to selector.
 afterEach(() => {
-  const cleanup = document.querySelectorAll('link,meta,iframe');
+  const cleanup = document.querySelectorAll('link,meta');
   for (let i = 0; i < cleanup.length; i++) {
     try {
       const element = cleanup[i];
