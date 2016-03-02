4514c58f26d86da84d3cc9c1b29f431b7317a260
Predrag Milosevic <pedjoni@gmail.com>
2016-03-02 16:43:47 +0100

    amp-springboard-player component

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ function buildExtensions(options) {
   buildExtension('amp-mustache', '0.1', false, options);
   buildExtension('amp-pinterest', '0.1', true, options);
   buildExtension('amp-soundcloud', '0.1', false, options);
+  buildExtension('amp-springboard-player', '0.1', false, options);
   buildExtension('amp-install-serviceworker', '0.1', false, options);
   /**
    * @deprecated `amp-slides` is deprecated and will be deleted before 1.0.
@@ -343,6 +344,7 @@ function buildExamples(watch) {
   buildExample('social-share.amp.html');
   buildExample('twitter.amp.html');
   buildExample('soundcloud.amp.html');
+  buildExample('springboard-player.amp.html');
   buildExample('user-notification.amp.html');
   buildExample('vimeo.amp.html');
   buildExample('vine.amp.html');
