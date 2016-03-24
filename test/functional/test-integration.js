bae15c8e402906e254c51fcfbff8b479e3dda42a
Michael Rooke <michaelr@sortable.com>
2016-03-24 16:20:56 -0400

    sortable amp ad implementation

--- test/functional/test-integration.js
+++ test/functional/test-integration.js
@@ -47,6 +47,7 @@ describe('3p integration.js', () => {
     expect(registrations).to.include.key('openx');
     expect(registrations).to.include.key('twitter');
     expect(registrations).to.include.key('yieldmo');
+    expect(registrations).to.include.key('sortable');
     expect(registrations).to.include.key('triplelift');
     expect(registrations).to.include.key('_ping_');
     expect(registrations).to.include.key('imobile');
