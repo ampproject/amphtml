b1155ffa906cb0dc260799b00854c29d43b12589
Indrajit Mallik <adtech.appservices@gmail.com>
2016-03-29 11:42:27 +0530

    Change sin colombia js

--- ads/colombia.js
+++ ads/colombia.js
@@ -21,15 +21,13 @@ import {writeScript, checkData} from '../src/3p';
  * @param {!Object} data
  */
 export function colombia(global, data) {
-	
-	checkData(data, [
-    'clmb_slot', 'clmb_position', 'clmb_section',
-	'clmb_divid', 'loadingStrategy',  
-	]);
+  checkData(data, [
+  'clmb_slot', 'clmb_position', 'clmb_section',
+  'clmb_divid', 'loadingStrategy',  
+  ]);
   global.clmbSlot = data.clmbSlot;
   global.clmbPosition = data.clmbPosition;
   global.clmbSection = data.clmbSection;
   global.clmbDivid = data.clmbDivid;
   writeScript(global, 'https://static.clmbtech.com/ad/commons/js/colombia-amp.js');
 }
-
