c801a2180d16e31a1f9b82d21d652c0281ab1e5d
Indrajit Mallik <adtech.appservices@gmail.com>
2016-03-29 12:09:04 +0530

    Changes in colombia js

--- ads/colombia.js
+++ ads/colombia.js
@@ -22,9 +22,9 @@ import {writeScript, checkData} from '../src/3p';
  */
 export function colombia(global, data) {
   checkData(data, [
-  'clmb_slot', 'clmb_position', 'clmb_section',
+    'clmb_slot', 'clmb_position', 'clmb_section',
     'clmb_divid', 'loadingStrategy',
-    ]);
+  ]);
   global.clmbSlot = data.clmbSlot;
   global.clmbPosition = data.clmbPosition;
   global.clmbSection = data.clmbSection;
