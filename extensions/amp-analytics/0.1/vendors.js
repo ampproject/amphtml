f4fa3573e3ef9b8b89217459102dcf3b5058efb3
ColombiaOnline <adtech.appservices@gmail.com>
2016-03-31 18:46:31 +0530

    Added container denfinition for ColAnalytics- Tagged ColAnalytics pageview tracking type- colanalytics
    Changes done as per suggested. Added code for colanalytics in examples/analytics.amp.html, extensions/amp-analytics/0.1/test/vendor-requests.json, extensions/amp-analytics/0.1/vendors.js  and  extensions/amp-analytics/amp-analytics.md
    
    Removed the error from file extensions/amp-analytics/0.1/vendors.js
    
    The file style format fixed
    
    changes in amp-analytics.md  and analytics.amp.html
    
    Squashed or commits

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -198,6 +198,29 @@ export const ANALYTICS_CONFIG = {
     },
   },
 
+  'colanalytics': {
+    'vars': {
+        'id': '',
+        'colurl':'',
+    },
+    'requests': {
+        'host': 'https://${colurl}',
+        'base': '${host}/message',
+        'pageview': '${base}?cid=${id}&val_101=${canonicalPath}&val_101=${canonicalHost}&uuid=${uid}&val_101=${authors}&val_101=${zone}&val_101=${sponsorName}&val_101=${contentType}&val_101=${scrollTop}&val_101=${scrollHeight}&val_101=${decayTime}&val_101=${totalEngagedTime}&val_101=${documentReferrer}&val_101=${pageLoadTime}&val_108=${title}'
+    },
+    'triggers': {
+        'defaultPageview': {
+            'on': 'visible',
+            'request': 'pageview'
+        }
+    },
+    'transport': {
+        'beacon': !1,
+        'xhrpost': !1,
+        'image': !0
+    },
+  },
+    
   'comscore': {
     'vars': {
       'c2': '1000001',
@@ -316,6 +339,11 @@ export const ANALYTICS_CONFIG = {
         'on': 'visible',
         'request': 'pageview',
       },
+    },
+    'transport': {
+      'beacon': false,
+      'xhrpost': false,
+      'image': true,
     },
   },
 
@@ -539,4 +567,3 @@ export const ANALYTICS_CONFIG = {
 };
 ANALYTICS_CONFIG['infonline']['triggers']['pageview']['iframe' +
 /* TEMPORARY EXCEPTION */ 'Ping'] = true;
-
