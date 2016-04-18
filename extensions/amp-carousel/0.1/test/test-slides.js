6b999adf374f15cac06c9327b2d4887853a667e6
Mohammad Khatib <mkhatib727@gmail.com>
2016-04-18 14:22:31 -0700

    Actually run the expectations.

--- extensions/amp-carousel/0.1/test/test-slides.js
+++ extensions/amp-carousel/0.1/test/test-slides.js
@@ -17,7 +17,7 @@
 import * as sinon from 'sinon';
 import * as tr from '../../../../src/transition';
 import {AmpSlides} from '../slides';
-
+import {Animation} from '../../../../src/animation';
 
 describe('Slides functional', () => {
 
@@ -771,6 +771,12 @@ describe('Slides functional', () => {
       slides.updateInViewport = sandbox.spy();
       slides.schedulePause = sandbox.spy();
       slides.schedulePreload = sandbox.spy();
+
+      Animation.animate = () => {
+        return {
+          thenAlways: cb => cb(),
+        };
+      };
     });
     afterEach(() => {
       sandbox.restore();
@@ -837,22 +843,20 @@ describe('Slides functional', () => {
     it('should preload slides in direction of navigation', () => {
       expect(slide0.style.visibility).to.be.equal('visible');
       expect(slide1.style.visibility).to.be.equal('hidden');
-      expect(slides.scheduleLayout.calledWith(slide0));
-      expect(slides.schedulePreload.calledWith(slide1));
 
-      slides.goCallback(1, /*animate*/ false);
+      slides.goCallback(1, /*animate*/ true);
       expect(slide0.style.visibility).to.be.equal('hidden');
       expect(slide1.style.visibility).to.be.equal('visible');
       expect(slide2.style.visibility).to.be.equal('hidden');
-      expect(slides.scheduleLayout.calledWith(slide1));
-      expect(slides.schedulePreload.calledWith(slide2));
+      expect(slides.scheduleLayout.calledWith(slide1)).to.be.true;
+      expect(slides.schedulePreload.calledWith(slide2)).to.be.true;
 
       slides.goCallback(-1, /*animate*/ false);
       expect(slide0.style.visibility).to.be.equal('visible');
       expect(slide1.style.visibility).to.be.equal('hidden');
       expect(slide2.style.visibility).to.be.equal('hidden');
-      expect(slides.scheduleLayout.calledWith(slide0));
-      expect(slides.schedulePreload.calledWith(slide1));
+      expect(slides.scheduleLayout.calledWith(slide0)).to.be.true;
+      expect(slides.schedulePreload.calledWith(slide2)).to.be.true;
     });
   });
 });
