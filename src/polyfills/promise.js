4bc06b7af218b7d9b782cc831e2d60bafb6a371b
Malte Ubl <cramforce@users.noreply.github.com>
2016-04-22 17:45:17 -0700

    Turn on closure compiler collapse properties. (#2972)
    Boom.
    
    The only apparent problem was related to handling of the default export from PJs, which needed both some local changes and a version bump for PJS.

--- src/polyfills/promise.js
+++ src/polyfills/promise.js
@@ -15,7 +15,7 @@
  */
 
 
-import {Promise} from 'promise-pjs/promise';
+import * as Promise from 'promise-pjs/promise';
 
 /**
  * Sets the Promise polyfill if it does not exist.
@@ -24,5 +24,16 @@ import {Promise} from 'promise-pjs/promise';
 export function install(win) {
   if (!win.Promise) {
     win.Promise = Promise;
+    // In babel the * export is an Object with a default property.
+    // In closure compiler it is the Promise function itself.
+    if (Promise.default) {
+      win.Promise = Promise.default;
+    }
+    // We copy the individual static methods, because closure
+    // compiler flattens the polyfill namespace.
+    win.Promise.resolve = Promise.resolve;
+    win.Promise.reject = Promise.reject;
+    win.Promise.all = Promise.all;
+    win.Promise.race = Promise.race;
   }
 }
