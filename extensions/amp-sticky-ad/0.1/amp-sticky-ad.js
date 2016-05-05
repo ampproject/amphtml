9fc389d0f2105280c873d859fd3e30f0e964034f
zhouyx <zhouyx@google.com>
2016-05-05 16:39:29 -0700

    Add feature display after scrolling

--- extensions/amp-sticky-ad/0.1/amp-sticky-ad.js
+++ extensions/amp-sticky-ad/0.1/amp-sticky-ad.js
@@ -19,6 +19,8 @@ import {Layout} from '../../../src/layout';
 import {dev} from '../../../src/log';
 import {isExperimentOn} from '../../../src/experiments';
 import {isLayoutSizeDefined} from '../../../src/layout';
+import {setStyles} from '../../../src/style';
+import {vsyncFor} from '../../../src/vsync';
 
 /** @const */
 const TAG = 'amp-sticky-ad';
@@ -28,15 +30,72 @@ class AmpStickyAd extends AMP.BaseElement {
   isLayoutSupported(layout) {
     return layout == Layout.NODISPLAY;
   }
-
   /** @override */
   buildCallback() {
     /** @const @private {boolean} */
     this.isExperimentOn_ = isExperimentOn(this.getWin(), TAG);
+    this.viewport_ = this.getViewport();
     if (!this.isExperimentOn_) {
       dev.warn(TAG, `TAG ${TAG} disabled`);
       return;
     }
+    this.viewport_ = this.getViewport();
+    this.isDisplayed_ = false;
+    this.initialScrollTop_ = this.viewport_.getScrollTop();
+    this.scrollHeight_ = this.viewport_.getScrollHeight();
+    this.viewportHeight_ =  this.viewport_.getSize().height;
+    /** @const @private {!Vsync} */
+    this.vsync_ = vsyncFor(this.getWin());
+    this.viewport_.onScroll(() => {
+      console.log("onscroll");
+      if(!this.isDisplayed_) {
+        console.log("not displayed yet");
+        console.log('getScrollTop is ' ,this.viewport_.getScrollTop());
+        console.log('initialScrollTop')
+        const scrollDist = (this.viewport_.getScrollTop() - this.initialScrollTop_);
+        console.log('scrollDist is ', scrollDist);
+        if(this.viewportHeight_ < Math.abs(scrollDist)) {
+          console.log("should display!");
+          // scroll down
+          if (scrollDist < 0) {
+            console.log('remain space ',this.viewport_.getScrollTop());
+            console.log('viewportheight', this.viewportHeight_);
+            if(this.viewport_.getScrollTop() < this.viewportHeight_) {
+              console.log('not enough space');
+              this.initialScrollTop_ = this.viewport_.getScrollTop();
+              return;
+            }
+          } /* scroll up*/else {
+            const remainHeight = this.scrollHeight - this.viewport_.getScrollTop()
+              - this.viewportHeight;
+            if (remainHeight < this.viewportHeight_) {
+              this.initialScrollTop_ = this.viewport_.getScrollTop();
+              return;
+            }
+          }
+          this.isDisplayed_ = true;
+          this.element.style.display = 'block';
+          this.vsync_.mutate(() => {
+            setStyles(this.element, {
+              'display': 'block',
+            });
+            this.viewport_.addToFixedLayer(this.element);
+            this.scheduleLayout(this.getRealChildren());
+          });
+        }
+      }
+    });
+  }
+
+  layoutCallback() {
+    this.isExperimentOn_ = isExperimentOn(this.getWin(), EXPERIMENT);
+    if (!this.isExperimentOn_) {
+      dev.warn(TAG, `Experiment ${EXPERIMENT} disabled`);
+      return Promise.resolve();
+    }
+    this.scheduleLayout(this.element.firstElementChild);
+    this.updateInViewport(this.element.firstElementChild, true);
+    return Promise.resolve();
   }
 }
 
