86518108c016f269e662ff59dd54cea9ce10f6d2
Mohammad Khatib <mkhatib727@gmail.com>
2016-05-09 17:47:41 -0700

    Fix amp-lightbox scheduling bug due to viewport not being updated.

--- extensions/amp-lightbox/0.1/amp-lightbox.js
+++ extensions/amp-lightbox/0.1/amp-lightbox.js
@@ -80,17 +80,19 @@ class AmpLightbox extends AMP.BaseElement {
     this.requestFullOverlay();
     this.getViewport().resetTouchZoom();
     this.getViewport().hideFixedLayer();
-    this.element.style.display = '';
-    this.element.style.opacity = 0;
 
-    // TODO(dvoytenko): use new animations support instead.
-    this.element.style.transition = 'opacity 0.1s ease-in';
-    requestAnimationFrame(() => {
-      this.element.style.opacity = '';
+    this.mutateElement(() => {
+      this.element.style.display = '';
+      this.element.style.opacity = 0;
+      // TODO(dvoytenko): use new animations support instead.
+      this.element.style.transition = 'opacity 0.1s ease-in';
+      requestAnimationFrame(() => {
+        this.element.style.opacity = '';
+      });
+    }).then(() => {
+      this.updateInViewport(this.container_, true);
+      this.scheduleLayout(this.container_);
     });
-
-    this.scheduleLayout(this.container_);
-    this.updateInViewport(this.container_, true);
 
     this.getHistory_().push(this.close.bind(this)).then(historyId => {
       this.historyId_ = historyId;
