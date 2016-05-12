017b3cdc9e8046e385994de7490c113c07381d67
Aziz Khoury <bentael@gmail.com>
2016-05-12 07:43:38 -0400

    work-in-progress

--- extensions/amp-cxense/0.1/amp-cxense.js
+++ extensions/amp-cxense/0.1/amp-cxense.js
@@ -13,41 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import {writeScript} from '../../../3p/3p';
 import {CSS} from '../../../build/amp-cxense-0.1.css';
-import {getLengthNumeral, isLayoutSizeDefined} from '../../../src/layout';
+import {isLayoutSizeDefined} from '../../../src/layout';
+import {listenFor} from '../../../src/iframe-helper';
 import {loadPromise} from '../../../src/event-helper';
 import {setStyles} from '../../../src/style';
-import {timer} from '../../../src/timer';
 
-const DEFAULT_AMD_SRC = location.protocol + '//embed.widget.cx/amd.js';
-const DEFAULT_EMBED_APP = '/app/player/m4/dist/';
-const PRE_CONNECT_URLS = [DEFAULT_AMD_SRC].concat([
-    location.protocol + '//api.widget.cx',
-    location.protocol + '//api.ramp.com' // todo: rm when safe
-]);
-const DATA_ATTR_NAME_MIDFIX = '-ramp-'; // todo: change to -cx- when safe
-
-let skipTransfer = {
-    'data-isplayer': true,
-    'data-noui': true,
-    'data-placeholder': true,
-    'class': true,
-    'layout': true
-};
-
-let _getLongDataAttributeName = (name) => {
-    return name.replace(/^data-/i, 'data' + DATA_ATTR_NAME_MIDFIX);
+let cxDefaults = {
+    apiHost: 'https://api.widget.cx',
+    amdSrc: 'https://embed.widget.cx/amd.js',
+    embedApp: '/app/player/m4/dist/'
 };
 
 class AmpCxense extends AMP.BaseElement {
 
     /** @override */
     preconnectCallback(onLayout) {
-        let self = this;
-        PRE_CONNECT_URLS.forEach((url) => {
-            self.preconnect.url(url, onLayout);
-        });
+        this.preconnect.url(cxDefaults.apiHost, onLayout);
+        this.preconnect.prefetch(cxDefaults.amdSrc, 'script');
     }
 
     /** @override */
@@ -57,42 +41,29 @@ class AmpCxense extends AMP.BaseElement {
 
     /** @override */
     buildCallback() {
-        this.element.className += ' amp-cxense';
+        this.element.classList.add('amp-cxense');
 
-        let src = this.element.getAttribute('src');
+        let widgetId = this.getDataAttribute_('widget-id');
+        let embed = this.getDataAttribute_('embed');
+        let module = this.getDataAttribute_('module');
 
-        let _widgetId = this._getDataAttribute('widget-id');
-        let _embed = this._getDataAttribute('embed');
-        let _module = this._getDataAttribute('module');
-
-        if (!src && !_widgetId && !_embed && !_module) {
+        if (!widgetId && !embed && !module) {
             // if none of these is specified, the default behavior is to load the app widget.
-            this.element.setAttribute('data-embed', DEFAULT_EMBED_APP);
-            this.element.setAttribute('data-isplayer', true);
+            this.setDataAttribute_('embed', cxDefaults.embedApp);
+            this.setDataAttribute_('isplayer', true);
         }
 
-        /** @private @const {string} */
-        this._src =  src || DEFAULT_AMD_SRC;
-        // necessary, otherwise it would confuse AMP
-        this.element.removeAttribute('src');
-
-        /** @private @const {string} */
-        this._placeholder = this.element.getAttribute('data-placeholder') !== "false";
-
-        // todo: we need a way to figure if metaplayer, so we can wait for it to be ready before removing the placeholder
         /** @private @const {boolean} */
-        this._isPlayer = !! (this.element.getAttribute('data-isplayer') || true);
+        this.placeholder_ = !(this.getDataAttribute_('placeholder') === false);
 
+        // todo: we need a way to figure if metaplayer,
+        // so we can wait for it to be ready before removing the placeholder
+        // right now we just make the assumption that it is a video-player
         /** @private @const {boolean} */
-        this._noui = this.element.getAttribute('data-noui') === "true";
+        this.isPlayer_ = !(this.getDataAttribute_('isplayer') === false);
 
-        this._setCXV();
-        if (this._CXV) {
-            this._CXV.Widgets && this._CXV.Widgets.init && this._CXV.Widgets.init();
-        }
-
-        if (!this.getPlaceholder() && this._placeholder && !this._noui) {
-            this._buildWidgetPlaceholder();
+        if (!this.getPlaceholder() && this.placeholder_) {
+            this.buildWidgetPlaceholder_();
         }
     }
 
@@ -100,79 +71,79 @@ class AmpCxense extends AMP.BaseElement {
     layoutCallback() {
         let self = this;
 
-        if (this._loaded) {
+        if (this.loaded_) {
             return new Promise(resolve => {
                 resolve(self);
             });
         }
+        /** @private @const {object} */
+        this.attr_ = this.getDataAttributes_();
 
-        if (this._noui) {
-            setStyles(this.element, {
-                'display': 'none'
-            });
-        } else {
-            this._createChildTarget();
-            this._id = this._target.getAttribute('id');
-        }
+        this.createChildTarget_();
 
-        return this._injectEmbedScript().then(() => {
-            self._setCXV();
+        const iframe = getIframe(this.element.ownerDocument.defaultView, this.element, 'cxense');
+        listenFor(iframe, 'embed-size', data => {
+
+            console.log("iframe-loaded", data);
+        }, true);
+
+        this.element.appendChild(iframe);
+
+        return loadPromise(iframe).then(() => {
+            console.log("loadPromise-iframe-loaded", data);
 
             return new Promise((resolve) => {
                 let newResolve = function (arg) {
-                    self._loaded = true;
+                    self.loaded_ = true;
                     return resolve(arg);
                 };
 
-                if (self._target && self._CXV) {
-                    self._CXV.Widgets.get('#' + self._id, function (embed) {
-                        self._embed = embed;
+                var widgetDomId = self.getEmbedId_();
+                self.cxWidgets_('get', '#' + widgetDomId, (embed) => {
+                    self.embed_ = embed;
 
-                        if (self._isPlayer) {
-                            let selector = '#' + self._id + '.metaplayer';
-                            self._CXV.Widgets.get(selector, function (mpf) {
+                    if (self.isPlayer_) {
+                        self.cxWidgets_('#' + widgetDomId + '.metaplayer', function (mpf) {
+                            self.mpf_ = mpf;
 
-                                self._mpf = mpf;
-                                mpf.listen('ready', () => {
-                                    self.applyFillContent(self._target);
-                                    newResolve(self);
-                                });
+                            mpf.listen('ready', () => {
+                                self.applyFillContent(self.target_);
+                                newResolve(self);
                             });
-                        } else {
-                            self.applyFillContent(self._target);
-                            newResolve(self);
-                        }
-                    });
-                } else {
-                    newResolve(self);
-                }
+                        });
+                    } else {
+                        self.applyFillContent(self.target_);
+                        newResolve(self);
+                    }
+                });
+
+                this.cxWidgets_('create', this.target_, this.attr_, {});
             });
         });
     }
 
     /** @override */
     unlayoutCallback() {
-        this._embed && this._embed.destroy();
-        this._target && this._target.parentNode.removeChild(this._target);
-        this._target = null;
+        this.element.parentNode.removeChild(this.element);
         return true;
     }
 
     /** @override */
     pauseCallback() {
-        if (! this._pauseMpfSync()) {
-            this._pauseMpf();
+        if (! this.pausePlayerSync_()) {
+            this.pausePlayer_();
         }
     }
 
     /** @private */
-    _pauseMpf() {
+    pausePlayer_() {
         let self = this;
 
         return new Promise((resolve) => {
-            if (self._target && self._CXV) {
-                self._CXV.Widgets.get('#' + self._target.getAttribute('id') + '.metaplayer', (mpf) => {
-                    self._mpf = self._mpf || mpf;
+            if (self.isPlayer_) {
+                self.cxWidgets_('get', '#' + self.target_.getAttribute('id') + '.metaplayer', (mpf) => {
+                    self.mpf_ = self.mpf_ || mpf;
+
                     mpf.video.pause();
                     resolve();
                 });
@@ -182,20 +153,36 @@ class AmpCxense extends AMP.BaseElement {
         });
     }
 
+
     /** @private */
-    _pauseMpfSync() {
-        return !!(this._mpf && (this._mpf.video.pause() || this._mpf.video.paused));
+    getElementId_() {
+        return this.element.getAttribute('id');
     }
 
     /** @private */
-    _buildWidgetPlaceholder() {
-        const doc = this._getDoc();
+    getEmbedId_() {
+        return this.target_.getAttribute('id');
+    }
+
+    /** @private */
+    getWidgetId_() {
+        return this.getDataAttribute_('widget-id');
+    }
+
+    /** @private */
+    pausePlayerSync_() {
+        return !!(this.mpf_ && (this.mpf_.video.pause() || this.mpf_.video.paused));
+    }
+
+    /** @private */
+    buildWidgetPlaceholder_() {
+        const doc = this.getDoc_();
 
         let placeholder = doc.createElement('div');
         placeholder.className = 'amp-cxense-placeholder';
 
         let spinner = doc.createElement('div');
-        spinner.className = 'amp-mpf-loader';
+        spinner.className = 'amp-cxense-loader';
         spinner.appendChild(doc.createElement('div'));
         spinner.appendChild(doc.createElement('div'));
         spinner.appendChild(doc.createElement('div'));
@@ -207,70 +194,77 @@ class AmpCxense extends AMP.BaseElement {
     }
 
     /** @private */
-    _injectEmbedScript () {
-        return this._injectScript(this._src);
+    createChildTarget_ () {
+        const div = this.getDoc_().createElement('div');
+        div.className = 'amp-embed-target';
+        this.element.appendChild(div);
+        this.target_ = div;
+        return this.target_;
     }
 
-    /** @private */
-    _injectScript (src) {
-        const doc = this._getDoc();
-        const script = doc.createElement('script');
-        script.setAttribute("src", src);
-        doc.head.appendChild(script);
-        return loadPromise(script);
-    }
+    cxWidgets_ () {
+        this.CXV_ = this.getWin().RAMP || this.getWin().CXV;
 
-    /** @private */
-    _createChildTarget () {
-        const target = this._getDoc().createElement('div');
-        setStyles(target, {
-            'background': 'black'
-        });
-        let id;
-        let attr;
-        let attributes = Array.prototype.slice.call(this.element.attributes);
-        let shortRegExp = new RegExp('^data-', 'i');
-        let fullRegExp = new RegExp('^data-' + DATA_ATTR_NAME_MIDFIX.replace('^-', ''), 'i');
+        let args = Array.prototype.slice.call(arguments, 0);
+        let fn = args.shift();
 
-        while(attr = attributes.pop()) {
-            let name = attr.nodeName;
-            let value = attr.nodeValue;
-
-            let skip = false;
-            if (skipTransfer[name]) {
-                skip = true;
-            }
-            if (name == "id") {
-                value += "-widget";
-            }
-            if (shortRegExp.test(name)) {
-                this.element.removeAttribute(attr.nodeName);
-
-                if (!fullRegExp.test(name)) {
-                    name = _getLongDataAttributeName(name);
-                }
-            }
-            !skip && target.setAttribute(name, value);
+        if (this.CXV_ && this.CXV_.Widgets && typeof this.CXV_.Widgets[fn] == 'function') {
+            return this.CXV_.Widgets[fn].apply(this.CXV_.Widgets, args);
         }
-        this.element.appendChild(target);
-        this._target = target;
+    }
+
+    cxEmbedUtils_ () {
+        this.CXV_ = this.getWin().RAMP || this.getWin().CXV;
+
+        let args = Array.prototype.slice.call(arguments, 0);
+        let fn = args.shift();
+
+        if (this.CXV_ && this.CXV_.util && this.CXV_.util.embed && typeof this.CXV_.util.embed[fn] == 'function') {
+            return this.CXV_.util.embed[fn].apply(this.CXV_.util.embed, args);
+        }
     }
 
     /** @private */
-    _setCXV () {
-        this._CXV = this.getWin().RAMP;
-    }
-
-    /** @private */
-    _getDoc () {
+    getDoc_ () {
         return this.getWin().document;
     }
 
     /** @private */
-    _getDataAttribute (name) {
-        name = 'data-' + name.replace(/^data-/i, '');
-        return this.element.getAttribute(name) || this.element.getAttribute(_getLongDataAttributeName(name));
+    getDataAttributes_ () {
+        return this.attr_ || this.cxEmbedUtils_('attrHash', this.element, 'data') || {};
+    }
+
+    /** @private */
+    getDataAttribute_ (name) {
+        return this.attr_ ? this.attr_[name] : resolveType(this.element.getAttribute('data-' + name));
+    }
+
+    /** @private */
+    setDataAttribute_ (name, value) {
+        return this.element.setAttribute('data-' + name, value);
     }
 }
+
+let resolveType = function (token) {
+    // guesses and resolves type of a string
+    if( typeof token != "string")
+        return token;
+
+    if( token.length < 15 && token.match(/^(0|-?(0\.|[1-9]\d*\.?)\d*)$/ ) ){
+        // don't match long ints where we would lose precision
+        // don't match numeric strings with leading zeros that are not decimals or "0"
+        token = parseFloat(token);
+    }
+    else if( token.match(/^true|false$/i) ){
+        token = Boolean( token.match(/true/i) );
+    }
+    else if(token === "undefined" ){
+        token = undefined;
+    }
+    else if(token === "null" ){
+        token = null;
+    }
+    return token;
+};
 
 AMP.registerElement('amp-cxense', AmpCxense, CSS);
