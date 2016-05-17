1153daa874b1f061f79d2c14742487ba8e839e44
Sriram Krishnan <sriramkrish85@gmail.com>
2016-05-17 13:54:49 -0700

    Remove sidebar experiment.

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -61,13 +61,6 @@ const EXPERIMENTS = [
     spec: 'https://github.com/ampproject/amphtml/blob/master/' +
         'extensions/amp-social-share/amp-social-share.md',
   },
-  // Amp Sidebar
-  {
-    id: 'amp-sidebar',
-    name: 'AMP Sidebar',
-    spec: 'https://github.com/ampproject/amphtml/blob/master/extensions/' +
-        'amp-sidebar/amp-sidebar.md',
-  },
   {
     id: 'amp-analytics-viewability',
     name: 'Viewability APIs for amp-analytics',
