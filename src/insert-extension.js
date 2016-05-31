aecf3989cea68be84de0e9ae9e4fffbdb76c6082
Yuxi Chen <muxin@users.noreply.github.com>
2016-05-31 11:02:36 -0700

    Fix insert extension test for local dev testing mode (#3370)

--- src/insert-extension.js
+++ src/insert-extension.js
@@ -60,7 +60,7 @@ function createAmpExtensionScript(win, extension) {
   ampExtensionScript.setAttribute('data-script', extension);
   const pathStr = win.location.pathname;
   const scriptSrc = calculateExtensionScriptUrl(pathStr, extension,
-      win.AMP_TEST);
+      win.AMP_TEST, window.ampTestRuntimeConfig.useCompiledJs);
   ampExtensionScript.src = scriptSrc;
   return ampExtensionScript;
 };
@@ -93,13 +93,15 @@ function isAmpExtensionScriptRequired(win, element, extension) {
  * @visibleForTesting
  * @param {string} path Location path of the window
  * @param {string} extension
- * @param {bool=} isTest
+ * @param {boolean=} isTest
+ * @param {boolean=} isUsingCompiledJs
  * @return {string}
  */
-export function calculateExtensionScriptUrl(path, extension, isTest) {
+export function calculateExtensionScriptUrl(path, extension, isTest,
+  isUsingCompiledJs) {
   if (getMode().localDev) {
     if (isTest) {
-      if (window.ampTestRuntimeConfig.useCompiledJs) {
+      if (isUsingCompiledJs) {
         return `/base/dist/v0/${extension}-0.1.js`;
       }
       return `/base/dist/v0/${extension}-0.1.max.js`;
