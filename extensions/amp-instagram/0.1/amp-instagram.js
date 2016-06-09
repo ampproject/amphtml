334cbf30b65c83bcaf47248cd48465d145c6fda7
Patrick Hyatt <patrick.hyatt@gmail.com>
2016-06-09 14:38:07 -0700

    Correct line width exceeding stylistic preferences
    Per https://travis-ci.org/ampproject/amphtml/builds/136549351
    Lines 118

--- extensions/amp-instagram/0.1/amp-instagram.js
+++ extensions/amp-instagram/0.1/amp-instagram.js
@@ -115,7 +115,8 @@ class AmpInstagram extends AMP.BaseElement {
     iframe.setAttribute('frameborder', '0');
     iframe.setAttribute('allowtransparency', 'true');
     //Add title to the iframe for better accessibility.
-    iframe.setAttribute('title', 'Instagram: ' + this.element.getAttribute('alt'));
+    iframe.setAttribute('title', 'Instagram: ' + 
+        this.element.getAttribute('alt'));
     iframe.src = 'https://www.instagram.com/p/' +
         encodeURIComponent(this.shortcode_) + '/embed/?v=4';
     this.applyFillContent(iframe);
