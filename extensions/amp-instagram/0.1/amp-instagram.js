7b2c9b9be2eaa4eb4433f4e647d3f0d18d3735cc
Patrick Hyatt <patrick.hyatt@gmail.com>
2016-06-10 16:29:25 -0700

    Add alt attribute for instagram img container (#3520)

--- extensions/amp-instagram/0.1/amp-instagram.js
+++ extensions/amp-instagram/0.1/amp-instagram.js
@@ -24,6 +24,7 @@
  * <code>
  * <amp-instagram
  *   data-shortcode="fBwFP"
+ *   alt="Fastest page in the west."
  *   width="320"
  *   height="392"
  *   layout="responsive">
@@ -87,6 +88,9 @@ class AmpInstagram extends AMP.BaseElement {
     image.setAttribute('src', 'https://www.instagram.com/p/' +
         encodeURIComponent(this.shortcode_) + '/media/?size=l');
     image.setAttribute('layout', 'fill');
+
+    this.propagateAttributes(['alt'], image);
+
     // This makes the non-iframe image appear in the exact same spot
     // where it will be inside of the iframe.
     setStyles(image, {
@@ -110,6 +114,9 @@ class AmpInstagram extends AMP.BaseElement {
     this.iframe_ = iframe;
     iframe.setAttribute('frameborder', '0');
     iframe.setAttribute('allowtransparency', 'true');
+    //Add title to the iframe for better accessibility.
+    iframe.setAttribute('title', 'Instagram: ' +
+        this.element.getAttribute('alt'));
     iframe.src = 'https://www.instagram.com/p/' +
         encodeURIComponent(this.shortcode_) + '/embed/?v=4';
     this.applyFillContent(iframe);
