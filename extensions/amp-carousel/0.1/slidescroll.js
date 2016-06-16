a5fe31e080bedda098234aec870194eef1d43327
Sriram Krishnan <sriramkrish85@users.noreply.github.com>
2016-06-15 17:58:05 -0700

    Amp slidescroll experiment - make use of snap points and show a max of 3 slides at a time. (#3490)
    * Snap points implementation-Show slides plus controls - make the nav buttons work - add unit tests.

--- extensions/amp-carousel/0.1/slidescroll.js
+++ extensions/amp-carousel/0.1/slidescroll.js
@@ -15,6 +15,10 @@
  */
 import {BaseCarousel} from './base-carousel';
 import {Layout} from '../../../src/layout';
+import {getStyle} from '../../../src/style';
+
+/** @const {string} */
+const SHOWN_CSS_CLASS = '-amp-slide-item-show';
 
 export class AmpSlideScroll extends BaseCarousel {
   /** @override */
@@ -26,21 +30,49 @@ export class AmpSlideScroll extends BaseCarousel {
     /** @private @const {!Window} */
     this.win_ = this.getWin();
 
+    /** @private @const {!boolean} */
+    this.hasNativeSnapPoints_ = (
+        getStyle(this.element, 'scrollSnapType') != undefined);
     this.element.classList.add('-amp-slidescroll');
 
     /** @private {!Array<!Element>} */
     this.slides_ = this.getRealChildren();
 
+    /** @private {!Element} */
+    this.slidesContainer_ = this.win_.document.createElement('div');
+    this.slidesContainer_.classList.add('-amp-slides-container');
+
+    /** @private {!Array<!Element>} */
+    this.slideWrappers_ = [];
+
     this.slides_.forEach(slide => {
-      const wrapper = this.win_.document.createElement('div');
-      wrapper.appendChild(slide);
-      wrapper.classList.add('-amp-slide-item');
-      this.element.appendChild(wrapper);
+      this.setAsOwner(slide);
+      const slideWrapper = this.win_.document.createElement('div');
+      slideWrapper.appendChild(slide);
+      slideWrapper.classList.add('-amp-slide-item');
+      this.slidesContainer_.appendChild(slideWrapper);
+      this.slideWrappers_.push(slideWrapper);
     });
+    this.element.appendChild(this.slidesContainer_);
+
+    /** @private {number} */
+    this.noOfSlides_ = this.slides_.length;
+  }
+
+  /** @override */
+  onLayoutMeasure() {
+    /** @private {number} */
+    this.slideWidth_ = this.getLayoutWidth();
+
+    /** @private {number} */
+    this.previousScrollLeft_ = this.slidesContainer_./*OK*/scrollLeft;
   }
 
   /** @override */
   layoutCallback() {
+    if (this.slideIndex_ == null) {
+      this.showSlide_(0);
+    }
     return Promise.resolve();
   }
 
@@ -57,9 +89,84 @@ export class AmpSlideScroll extends BaseCarousel {
 
   /** @override */
   hasPrev() {
+    return this.slideIndex_ > 0;
   }
 
   /** @override */
   hasNext() {
+    return this.slideIndex_ < this.slides_.length - 1;
+  }
+
+  /** @override */
+  goCallback(dir, unusedAnimate) {
+    if (this.slideIndex_ != null) {
+      if ((dir == 1 && this.hasNext()) ||
+          (dir == -1 && this.hasPrev())) {
+        this.showSlide_(this.slideIndex_ + dir);
+      }
+    }
+  }
+
+  /**
+   * Makes the slide corresponding to the given index and the slides surrounding
+   *    it available for display.
+   * @param {number} newIndex Index of the slide to be displayed.
+   * @private
+   */
+  showSlide_(newIndex) {
+    const noOfSlides = this.noOfSlides_;
+    if (newIndex < 0 ||
+        newIndex >= this.noOfSlides_ ||
+        this.slideIndex_ == newIndex) {
+      return;
+    }
+    const showIndexArr = [];
+    if (newIndex == noOfSlides - 1) {
+      // Last slide.
+      showIndexArr.push(noOfSlides - 1, noOfSlides - 2);
+    } else if (newIndex == 0) {
+      // First slide.
+      showIndexArr.push(0, 1);
+    } else {
+      showIndexArr.push(newIndex - 1, newIndex, newIndex + 1);
+    }
+    if (this.slideIndex_ != null) {
+      this.updateInViewport(this.slides_[this.slideIndex_], false);
+    }
+    this.updateInViewport(this.slides_[newIndex], true);
+    showIndexArr.forEach(showIndex => {
+      this.slideWrappers_[showIndex].classList.add(SHOWN_CSS_CLASS);
+      if (showIndex == newIndex) {
+        this.scheduleLayout(this.slides_[showIndex]);
+      } else {
+        this.schedulePreload(this.slides_[showIndex]);
+      }
+    });
+    // A max of 3 slides are displayed at a time - we show the first slide
+    // (which is at scrollLeft 0) when slide 0 is requested - for all other
+    // instances we show the second slide (middle slide at
+    // scrollLeft = slide's width).
+    const newScrollLeft = (newIndex == 0) ? 0 : this.slideWidth_;
+    this.slidesContainer_./*OK*/scrollLeft = newScrollLeft;
+    this.slideIndex_ = newIndex;
+    this.hideRestOfTheSlides_(newIndex);
+    this.setControlsState();
+  }
+
+  /**
+   * Given an index, hides rest of the slides that are not needed.
+   * @param {number} index Index of the slide to be displayed.
+   * @private
+   */
+  hideRestOfTheSlides_(index) {
+    for (let i = 0; i < this.noOfSlides_; i++) {
+      if (i != index && i != index - 1 && i != index + 1 &&
+          this.slideWrappers_[i]) {
+        if (this.slideWrappers_[i].classList.contains(SHOWN_CSS_CLASS)) {
+          this.slideWrappers_[i].classList.remove(SHOWN_CSS_CLASS);
+          this.schedulePause(this.slides_[i]);
+        }
+      }
+    }
   }
 }
