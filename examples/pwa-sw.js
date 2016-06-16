c056f6faad650b4bdebcffb0315f5a98ef972ec5
Dima Voytenko <dvoytenko@google.com>
2016-06-16 13:13:46 -0700

    Shell SW overrides the URL space

--- examples/pwa-sw.js
+++ examples/pwa-sw.js
@@ -7,5 +7,11 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log('SW: fetch: ', event.request);
+  if (event.request.url.indexOf('amp.max.html') != -1) {
+    const isDirectFetch = event.request.headers.get('AMP-Direct-Fetch') == '1';
+    console.log('SW: respond with shell: ', !isDirectFetch);
+    if (!isDirectFetch) {
+      event.respondWith(fetch('/examples.build/pwa.html'));
+    }
+  }
 });
