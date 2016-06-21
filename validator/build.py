81253483aab66decf941dadf807e760b19ae6f5d
Johannes Henkel <johannes@google.com>
2016-06-21 10:57:25 -0700

    insert os.walk to try to see what is going on

--- validator/build.py
+++ validator/build.py
@@ -420,7 +420,10 @@ def RunTests(out_dir, nodejs_cmd):
 
 def CreateWebuiAppengineDist(out_dir):
   logging.info('entering ...')
-  os.system('find')
+  for dirName, subdirList, fileList in os.walk('.'):
+    print 'Found directory: %s' % dirName
+    for fname in fileList:
+      print '-> %s' % fname
   try:
     tempdir = tempfile.mkdtemp()
     shutil.copytree('webui', os.path.join(tempdir, 'webui'))
