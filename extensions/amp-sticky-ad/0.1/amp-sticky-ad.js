75e815bd7a324f9166fdb6a441b26d6fac94503b
zhouyx <zhouyx@google.com>
2016-06-24 00:01:04 -0700

    set background-color to amp-ad inside amp-sticky-ad, add transform to amp-sticky-ad

--- extensions/amp-sticky-ad/0.1/amp-sticky-ad.js
+++ extensions/amp-sticky-ad/0.1/amp-sticky-ad.js
@@ -21,6 +21,7 @@ import {isExperimentOn} from '../../../src/experiments';
 import {removeElement} from '../../../src/dom';
 import {setStyles} from '../../../src/style';
 import {timer} from '../../../src/timer';
+import {toggle} from '../../../src/style';
 
 /** @const */
 const TAG = 'amp-sticky-ad';
@@ -34,12 +35,12 @@ class AmpStickyAd extends AMP.BaseElement {
   /** @override */
   buildCallback() {
     /** @const @private {boolean} */
-    this.isExperimentOn_ = isExperimentOn(this.getWin(), TAG);
-    if (!this.isExperimentOn_) {
-      dev.warn(TAG, `TAG ${TAG} disabled`);
-      return;
-    }
-
+    // this.isExperimentOn_ = isExperimentOn(this.getWin(), TAG);
+    // if (!this.isExperimentOn_) {
+    //   dev.warn(TAG, `TAG ${TAG} disabled`);
+    //   return;
+    // }
+    // toggle(this.element, true);
     this.element.classList.add('-amp-sticky-ad-layout');
     const children = this.getRealChildren();
     user.assert((children.length == 1 && children[0].tagName == 'AMP-AD'),
@@ -68,11 +69,11 @@ class AmpStickyAd extends AMP.BaseElement {
 
   /** @override */
   layoutCallback() {
-    this.isExperimentOn_ = isExperimentOn(this.getWin(), TAG);
-    if (!this.isExperimentOn_) {
-      dev.warn(TAG, `TAG ${TAG} disabled`);
-      return Promise.resolve();
-    }
+    // this.isExperimentOn_ = isExperimentOn(this.getWin(), TAG);
+    // if (!this.isExperimentOn_) {
+    //   dev.warn(TAG, `TAG ${TAG} disabled`);
+    //   return Promise.resolve();
+    // }
     // Reschedule layout for ad if layout sticky-ad again.
     if (this.visible_) {
       this.updateInViewport(this.ad_, true);
@@ -122,9 +123,10 @@ class AmpStickyAd extends AMP.BaseElement {
       this.removeOnScrollListener_();
       this.deferMutate(() => {
         this.visible_ = true;
-        setStyles(this.element, {
-          'visibility': 'visible',
-        });
+        // setStyles(this.element, {
+        //   'visibility': 'visible',
+        // });
+        toggle(this.element, true);
         this.viewport_.addToFixedLayer(this.element);
         this.updateInViewport(this.ad_, true);
         this.scheduleLayout(this.ad_);
