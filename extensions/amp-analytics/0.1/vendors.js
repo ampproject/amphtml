77d6729860e63dbeef49e63cc2839f7751a6114f
Avi Mehta <avimehta@google.com>
2016-06-30 12:18:54 -0700

    Updates to colanalytics vendor config for amp-analytics.

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -249,6 +249,7 @@ export const ANALYTICS_CONFIG = {
       'host': 'https://ase.clmbtech.com',
       'base': '${host}/message',
       'pageview': '${base}?cid=${id}' +
+        '&val_101=${id}' +
         '&val_101=${canonicalPath}' +
         '&ch=${canonicalHost}' +
         '&uuid=${uid}' +
@@ -262,7 +263,8 @@ export const ANALYTICS_CONFIG = {
         '&tet=${totalEngagedTime}' +
         '&dr=${documentReferrer}' +
         '&plt=${pageLoadTime}' +
-        '&val_108=${title}',
+        '&val_108=${title}' +
+        '&val_120=3',
     },
     'triggers': {
       'defaultPageview': {
