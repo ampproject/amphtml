0c7a83bebc88ab8be4ee92db5e6f307d3367c5e4
Malte Ubl <malte.ubl@gmail.com>
2016-07-02 14:03:34 -0700

    Optimize functions that showed up in profile.
    - no longer dispatch custom events in prod that we only use for tests.
    - cache parsed experiment cookie.
    - optimize log class to short circuit more conditionals in prod.
    - do not allocate a promise per service unless required.
    - lazily initialize URL replacement functions.
    - never change the shape of the URL object.

--- src/service/url-replacements-impl.js
+++ src/service/url-replacements-impl.js
@@ -50,8 +50,17 @@ export class UrlReplacements {
     this.replacements_ = this.win_.Object.create(null);
 
     /** @private @const {function():!Promise<?AccessService>} */
-    this.getAccessService_ = accessServiceForOrNull.bind(null);
+    this.getAccessService_ = accessServiceForOrNull;
 
+    /** @private {boolean} */
+    this.initialized_ = false;
+  }
+
+  /**
+   * Lazily initialize the default replacements.
+   */
+  initialize_() {
+    this.initialized_ = true;
     // Returns a random value for cache busters.
     this.set_('RANDOM', () => {
       return Math.random();
@@ -158,7 +167,7 @@ export class UrlReplacements {
           return service.get(opt_userNotificationId);
         });
       }
-      return cidFor(win).then(cid => {
+      return cidFor(this.win_).then(cid => {
         return cid.get({
           scope,
           createCookieIfNotPresent: true,
@@ -449,6 +458,9 @@ export class UrlReplacements {
    * @private
    */
   expand_(url, opt_bindings, opt_collectVars) {
+    if (!this.initialized_) {
+      this.initialize_();
+    }
     const expr = this.getExpr_(opt_bindings);
     let replacementPromise;
     const encodeValue = val => {
@@ -520,6 +532,7 @@ export class UrlReplacements {
   }
 
   /**
+   * Method exists to assist stubbing in tests.
    * @param {string} name
    * @return {function(*):*}
    */
