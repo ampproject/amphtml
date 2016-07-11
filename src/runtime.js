f5bc9b65550e6bdc751ea1cee068395bcf5cf680
Justin Ridgewell <jridgewell@google.com>
2016-07-11 17:52:19 -0400

    Attempt to install the SW

--- src/runtime.js
+++ src/runtime.js
@@ -64,6 +64,16 @@ const TAG = 'runtime';
 /** @type {!Object} */
 const elementsForTesting = {};
 
+// TODO
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
 
 /**
  * Install runtime-level services.
