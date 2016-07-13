25e0310cbc5c40413161a0ef72eb93fac9fca5bb
janwinkler <jan.winkler@adspirit.de>
2016-07-13 20:38:19 +0200

    Update adspirit.js

--- ads/adspirit.js
+++ ads/adspirit.js
@@ -21,14 +21,14 @@ import {checkData} from '../3p/3p';
   * @param {!Object} data
   */
 export function adspirit(global, data) {
- checkData(data, ['asm-params', 'asm-host']);
- const i = document.createElement('ins');
- i.setAttribute('data-asm-params', data['asm-params']);
- i.setAttribute('data-asm-host', data['asm-host']);
- i.setAttribute('class', 'asm_async_creative');
- i.style.cssText = 'display:inline-block;text-align:left;';
- global.document.getElementById('c').appendChild(i);
- const s = document.createElement('script');
- s.src = 'https://' + data['asm-host'] + '/adasync.js';
- global.document.body.appendChild(s);
+  checkData(data, ['asm-params', 'asm-host']);
+  const i = document.createElement('ins');
+  i.setAttribute('data-asm-params', data['asm-params']);
+  i.setAttribute('data-asm-host', data['asm-host']);
+  i.setAttribute('class', 'asm_async_creative');
+  i.style.cssText = 'display:inline-block;text-align:left;';
+  global.document.getElementById('c').appendChild(i);
+  const s = document.createElement('script');
+  s.src = 'https://' + data['asm-host'] + '/adasync.js';
+  global.document.body.appendChild(s);
 }
