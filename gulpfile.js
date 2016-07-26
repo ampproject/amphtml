f5f9c660780ef8f3c00b664edde4d454987fa70a
lannka <hungfei.ding@gmail.com>
2016-07-26 15:06:06 -0700

    Replace console.log by util.log in gulpfile.js, so that we can better profile the gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -235,7 +235,7 @@ function buildExtension(name, version, hasCss, options) {
       return;
     }
   }
-  console.log('Bundling ' + name);
+  $$.util.log('Bundling ' + name);
   // Building extensions is a 2 step process because of the renaming
   // and CSS inlining. This watcher watches the original file, copies
   // it to the destination and adds the CSS.
@@ -425,7 +425,7 @@ function buildExamples(watch) {
  */
 function buildExample(name) {
   var input = 'examples/' + name;
-  console.log('Processing ' + name);
+  $$.util.log('Processing ' + name);
   var html = fs.readFileSync(input, 'utf8');
   var max = html;
   max = max.replace(/(https:\/\/cdn.ampproject.org\/.+?).js/g, '$1.max.js');
@@ -457,7 +457,7 @@ function thirdPartyBootstrap(watch, shouldMinify) {
       thirdPartyBootstrap(false);
     });
   }
-  console.log('Processing ' + input);
+  $$.util.log('Processing ' + input);
   var html = fs.readFileSync(input, 'utf8');
   var min = html;
   // By default we use an absolute URL, that is independent of the
@@ -552,16 +552,17 @@ function compileJs(srcDir, srcFilename, destDir, options) {
       .pipe($$.rename(options.toName || srcFilename))
       .pipe(lazywrite())
       .on('end', function() {
+        $$.util.log('Compiled ' + srcFilename);
         activeBundleOperationCount--;
         if (activeBundleOperationCount == 0) {
-          console.info($$.util.colors.green('All current JS updates done.'));
+          $$.util.log($$.util.colors.green('All current JS updates done.'));
         }
       });
   }
 
   if (options.watch) {
     bundler.on('update', function() {
-      console.log('-> bundling ' + srcDir + '...');
+      $$.util.log('-> bundling ' + srcDir + '...');
       rebundle();
       // Touch file in unit test set. This triggers rebundling of tests because
       // karma only considers changes to tests files themselves re-bundle
@@ -580,7 +581,7 @@ function compileJs(srcDir, srcFilename, destDir, options) {
  */
 function buildExperiments(options) {
   options = options || {};
-  console.log('Bundling experiments.html/js');
+  $$.util.log('Bundling experiments.html/js');
 
   function copyHandler(name, err) {
     if (err) {
@@ -610,7 +611,7 @@ function buildExperiments(options) {
   }
 
   // Build HTML.
-  console.log('Processing ' + htmlPath);
+  $$.util.log('Processing ' + htmlPath);
   var html = fs.readFileSync(htmlPath, 'utf8');
   var minHtml = html.replace('../../dist.tools/experiments/experiments.max.js',
       'https://cdn.ampproject.org/v0/experiments.js');
@@ -654,7 +655,7 @@ function buildLoginDone(options) {
  */
 function buildLoginDoneVersion(version, options) {
   options = options || {};
-  console.log('Bundling amp-login-done.html/js');
+  $$.util.log('Bundling amp-login-done.html/js');
 
   function copyHandler(name, err) {
     if (err) {
@@ -684,7 +685,7 @@ function buildLoginDoneVersion(version, options) {
   }
 
   // Build HTML.
-  console.log('Processing ' + htmlPath);
+  $$.util.log('Processing ' + htmlPath);
   var html = fs.readFileSync(htmlPath, 'utf8');
   var minHtml = html.replace(
       '../../../dist/v0/amp-login-done-' + version + '.max.js',
@@ -723,7 +724,7 @@ function buildLoginDoneVersion(version, options) {
  */
 function buildAlp(options) {
   options = options || {};
-  console.log('Bundling alp.js');
+  $$.util.log('Bundling alp.js');
 
   compileJs('./ads/alp/', 'install-alp.js', './dist/', {
     toName: 'alp.max.js',
@@ -743,8 +744,8 @@ function buildAlp(options) {
 function checkMinVersion() {
   var majorVersion = Number(process.version.replace(/v/, '').split('.')[0]);
   if (majorVersion < 4) {
-    console.log('Please run AMP with node.js version 4 or newer.');
-    console.log('Your version is', process.version);
+    $$.util.log('Please run AMP with node.js version 4 or newer.');
+    $$.util.log('Your version is', process.version);
     process.exit(1);
   }
 }
