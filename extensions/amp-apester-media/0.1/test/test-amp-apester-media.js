34c9fb0750c45cd002c5516788802d554eb0e1d2
Ori Avraham <oria@apester.com>
2016-08-01 13:52:45 +0300

    lint getWin() removed

--- extensions/amp-apester-media/0.1/test/test-amp-apester-media.js
+++ extensions/amp-apester-media/0.1/test/test-amp-apester-media.js
@@ -14,62 +14,65 @@
  * limitations under the License.
  */
 
-import {createIframePromise, doNotLoadExternalResourcesInTest} from "../../../../testing/iframe";
-import "../amp-apester-media";
-import {adopt} from "../../../../src/runtime";
+import {
+  createIframePromise,
+  doNotLoadExternalResourcesInTest,
+} from '../../../../testing/iframe';
+import '../amp-apester-media';
+import {adopt} from '../../../../src/runtime';
 
 adopt(window);
 
 describe('amp-apester-media', () => {
 
-    function getApester(mediaId, opt_responsive, opt_beforeLayoutCallback) {
-        return createIframePromise(true, opt_beforeLayoutCallback).then(iframe => {
-            doNotLoadExternalResourcesInTest(iframe.win);
-            const media = iframe.doc.createElement('amp-apester-media');
-            media.setAttribute('data-apester-media-id', mediaId);
-            media.setAttribute('width', '600');
-            media.setAttribute('height', '390');
-            media.setAttribute('alt', 'Testing');
-            if (opt_responsive) {
-                media.setAttribute('layout', 'responsive');
-            }
-            return iframe.addElement(media);
-        });
-    }
-
-
-    function testLoader(image) {
-        expect(image).to.not.be.null;
-        expect(image.getAttribute('src')).to.equal(
-            'http://images.apester.com/images%2Floader.gif');
-        expect(image.getAttribute('layout')).to.equal('fill');
-       // expect(image.getAttribute('alt')).to.equal('Testing');
-    }
-
-    function testIframe(iframe) {
-        expect(iframe).to.not.be.null;
-        expect(iframe.src).to.equal('http://renderer.qmerce.local.com/interaction/578b4d6d2d9fb72943ce465c');
-        expect(iframe.getAttribute('width')).to.equal('600');
-        expect(iframe.getAttribute('height')).to.equal('390');
-      //  expect(iframe.getAttribute('title')).to.equal('Apester: Testing');
-    }
-
-    it('renders', () => {
-        return getApester('578b4d6d2d9fb72943ce465c').then(ins => {
-            testIframe(ins.querySelector('iframe'));
-            testLoader(ins.querySelector('amp-img'));
-        });
+  function getApester(mediaId, opt_responsive, opt_beforeLayoutCallback) {
+    return createIframePromise(true, opt_beforeLayoutCallback).then(iframe => {
+      doNotLoadExternalResourcesInTest(iframe.win);
+      const media = iframe.doc.createElement('amp-apester-media');
+      media.setAttribute('data-apester-media-id', mediaId);
+      media.setAttribute('width', '600');
+      media.setAttribute('height', '390');
+      media.setAttribute('alt', 'Testing');
+      if (opt_responsive) {
+        media.setAttribute('layout', 'responsive');
+      }
+      return iframe.addElement(media);
     });
+  }
 
 
-    it('renders responsively', () => {
-        return getApester('media', true).then(ins => {
-            expect(ins.className).to.match(/amp-layout-responsive/);
-        });
+  function testLoader(image) {
+    expect(image).to.not.be.null;
+    expect(image.getAttribute('src')).to.equal(
+      'http://images.apester.com/images%2Floader.gif');
+    expect(image.getAttribute('layout')).to.equal('fill');
+    // expect(image.getAttribute('alt')).to.equal('Testing');
+  }
+
+  function testIframe(iframe) {
+    expect(iframe).to.not.be.null;
+    expect(iframe.src).to.equal('http://stage3-renderer.qmerce.com/interaction/578b4d6d2d9fb72943ce465c');
+    expect(iframe.getAttribute('width')).to.equal('600');
+    expect(iframe.getAttribute('height')).to.equal('390');
+    //  expect(iframe.getAttribute('title')).to.equal('Apester: Testing');
+  }
+
+  it('renders', () => {
+    return getApester('578b4d6d2d9fb72943ce465c').then(ins => {
+      testIframe(ins.querySelector('iframe'));
+      testLoader(ins.querySelector('amp-img'));
     });
+  });
 
-    it('requires  media-id', () => {
-        expect(getApester('')).to.be.rejectedWith(
-            /The media-id attribute is required for/);
+
+  it('renders responsively', () => {
+    return getApester('media', true).then(ins => {
+      expect(ins.className).to.match(/amp-layout-responsive/);
     });
+  });
+
+  it('requires  media-id', () => {
+    expect(getApester('')).to.be.rejectedWith(
+      /The media-id attribute is required for/);
+  });
 });
