ampproject/amphtml

commit fe6a8ed77fdd2ca7a4fc9b5db9258fcc13b87087

YIELD ONE amp-ad support (#4166)
* Adding YIELD ONE to amp-ads

* _config.js: add adPreconnect section

* fixed syntax error

* changed from yieldoneParam.pubid/yieldoneParam.pid to data-pubid/data-pid

* L179- deleted yieldone host

* changed data- prefix

* pull request fix #4166

* pull request fix #4166

* pull request fix #4166

ads/_config.js
@@ -67,6 +67,11 @@ export const adPrefetch = {
     'https://s.yimg.jp/images/listing/tool/yads/ydn/amp/amp.js',
     'https://yads.c.yimg.jp/js/yads.js',
   ],
+  nend: 'https://js1.nend.net/js/amp.js',
+  adgeneration: 'https://i.socdm.com/sdk/js/adg-script-loader.js',
+  adtech: 'http://s.aolcdn.com/os/ads/adsWrapper3.js',
+  genieessp: 'https://js.gsspcln.jp/l/amp.js',
+  yieldone: 'https://img.ak.impact-ad.jp/ic/pone/commonjs/yone-amp.js',
   yieldbot: [
     'https://cdn.yldbt.com/js/yieldbot.intent.amp.js',
     'https://msg.yldbt.com/js/ybmsg.html',
@@ -201,11 +206,16 @@ export const adPreconnect = {
     'https://s.yieldmo.com',
     'https://ads.yieldmo.com',
   ],
+  adtech: [
+    'https://mads.at.atwola.com',
+    'https://aka-cdn.adtechus.com',
+  ],
   zergnet: [
     'https://www.zergnet.com',
     'https://zergnet.com',
   ],
 };
+
 
 /**
  * The externalCidScope used to provide CIDs to ads of the given type.

3p/integration.js
@@ -85,6 +85,7 @@ import {genieessp} from '../ads/genieessp';
 import {kargo} from '../ads/kargo';
 import {pulsepoint} from '../ads/pulsepoint';
 import {zergnet} from '../ads/zergnet';
+import {yieldone} from '../ads/yieldone';
 
 /**
  * Whether the embed type may be used with amp-embed tag.
@@ -154,6 +155,7 @@ register('yahoojp', yahoojp);
 register('yieldbot', yieldbot);
 register('yieldmo', yieldmo);
 register('zergnet', zergnet);
+register('yieldone', yieldone);
 
 register('_ping_', function(win, data) {
   win.document.getElementById('c').textContent = data.ping;

ads/yieldone.js
@@ -1,0 +1,31 @@
+/**
+  * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS-IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+ import {writeScript, checkData, validateDataExists} from '../3p/3p';
+
+ const yieldoneFields = ['pubid', 'pid','width', 'height'];
+
+ /**
+  * @param {!Window} global
+  * @param {!Object} data
+  */
+ export function yieldone(global, data) {
+   checkData(data, yieldoneFields);
+   validateDataExists(data, yieldoneFields);
+
+   global.yieldoneParam = data;
+   writeScript(global, 'https://img.ak.impact-ad.jp/ic/pone/commonjs/yone-amp.js');
+ }

examples/ads.amp.html
@@ -625,5 +625,14 @@
       data-zergid="42658">
   </amp-embed>
 
+  <h2>YIELD ONE</h2>
+  <amp-ad
+   width="320"
+   height="50"
+   type="yieldone"
+   data-pubid="0001"
+   data-pid="032478_4">
+   </amp-ad>
+
 </body>
 </html>

test/functional/test-integration.js
@@ -66,6 +66,7 @@ describe('3p integration.js', () => {
     expect(registrations).to.include.key('nend');
     expect(registrations).to.include.key('adgeneration');
     expect(registrations).to.include.key('genieessp');
+    expect(registrations).to.include.key('yieldone');
     expect(registrations).to.include.key('kargo');
     expect(registrations).to.include.key('amoad');
     expect(registrations).to.include.key('zergnet');

commit ecf0958bd08e8900429a10318c7c54437739a801

Add unit test for ads/_config.js. Fix a bad merge caused by #4166.

ads/_config.js
@@ -68,16 +68,12 @@ export const adPrefetch = {
     'https://s.yimg.jp/images/listing/tool/yads/ydn/amp/amp.js',
     'https://yads.c.yimg.jp/js/yads.js',
   ],
-  nend: 'https://js1.nend.net/js/amp.js',
-  adgeneration: 'https://i.socdm.com/sdk/js/adg-script-loader.js',
-  adtech: 'http://s.aolcdn.com/os/ads/adsWrapper3.js',
-  genieessp: 'https://js.gsspcln.jp/l/amp.js',
-  yieldone: 'https://img.ak.impact-ad.jp/ic/pone/commonjs/yone-amp.js',
   yieldbot: [
     'https://cdn.yldbt.com/js/yieldbot.intent.amp.js',
     'https://msg.yldbt.com/js/ybmsg.html',
   ],
   yieldmo: 'https://static.yieldmo.com/ym.amp1.js',
+  yieldone: 'https://img.ak.impact-ad.jp/ic/pone/commonjs/yone-amp.js',
 };
 
 /**
@@ -93,8 +89,8 @@ export const adPreconnect = {
     'https://staticd.cdn.adblade.com',
     'https://static.adblade.com',
   ],
-  adition: 'https://imagesrv.adition.com',
   adform: 'https://track.adform.net',
+  adition: 'https://imagesrv.adition.com',
   adreactor: 'https://adserver.adreactor.com',
   adsense: 'https://googleads.g.doubleclick.net',
   adstir: 'https://ad.ad-stir.com',
@@ -102,7 +98,6 @@ export const adPreconnect = {
     'https://mads.at.atwola.com',
     'https://aka-cdn.adtechus.com',
   ],
-  aduptech: 'https://s.d.adup-tech.com',
   amoad: [
     'https://d.amoad.com',
     'https://i.amoad.com',
@@ -116,22 +111,11 @@ export const adPreconnect = {
   criteo: [
     'https://cas.criteo.com',
   ],
+  dotandads: 'https://bal.ad.dotandad.com',
   doubleclick: [
     'https://partner.googleadservices.com',
-    'https://securepubads.g.doubleclick.net',
     'https://tpc.googlesyndication.com',
   ],
-  'mantis-display': [
-    'https://mantodea.mantisadnetwork.com',
-    'https://res.cloudinary.com',
-    'https://resize.mantisadnetwork.com',
-  ],
-  'mantis-recommend': [
-    'https://mantodea.mantisadnetwork.com',
-    'https://resize.mantisadnetwork.com',
-  ],
-  dotandads: 'https://bal.ad.dotandad.com',
-  gmossp: 'https://cdn.gmossp-sp.jp',
   imobile: 'https://spad.i-mobile.co.jp',
   improvedigital: 'https://ad.360yield.com/',
   industrybrains: [
@@ -143,6 +127,15 @@ export const adPreconnect = {
     'https://pubads.g.doubleclick.net',
     'https://prg.kargo.com',
   ],
+  'mantis-display': [
+    'https://mantodea.mantisadnetwork.com',
+    'https://res.cloudinary.com',
+    'https://resize.mantisadnetwork.com',
+  ],
+  'mantis-recommend': [
+    'https://mantodea.mantisadnetwork.com',
+    'https://resize.mantisadnetwork.com',
+  ],
   microad: [
     'https://s-rtb.send.microad.jp',
     'https://s-rtb.send.microadinc.com',
@@ -151,7 +144,6 @@ export const adPreconnect = {
     'https://deb.send.microad.jp',
   ],
   nend: [
-    'https://js1.nend.net',
     'https://output.nend.net',
     'https://img1.nend.net',
   ],
@@ -182,7 +174,6 @@ export const adPreconnect = {
     'https://images.taboola.com',
   ],
   teads: [
-    'https://cdn.teads.tv',
     'https://cdn2.teads.tv',
     'https://a.teads.tv',
     'https://t.teads.tv',
@@ -198,25 +189,18 @@ export const adPreconnect = {
   ],
   widespace: 'https://engine.widespace.com',
   yahoojp: [
-    'https://s.yimg.jp',
     'https://yads.yahoo.co.jp',
   ],
   yieldbot: 'https://i.yldbt.com',
   yieldmo: [
-    'https://static.yieldmo.com',
     'https://s.yieldmo.com',
     'https://ads.yieldmo.com',
-  ],
-  adtech: [
-    'https://mads.at.atwola.com',
-    'https://aka-cdn.adtechus.com',
   ],
   zergnet: [
     'https://www.zergnet.com',
     'https://zergnet.com',
   ],
 };
-
 
 /**
  * The externalCidScope used to provide CIDs to ads of the given type.

test/functional/test-ads-config.js
@@ -1,0 +1,67 @@
+/**
+ * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {adPrefetch, adPreconnect} from '../../ads/_config';
+
+describe('test-ads-config', () => {
+
+  it('should sort adPrefetch in alphabetic order', () => {
+    const keys = Object.keys(adPrefetch);
+    for (let i = 0; i < keys.length - 1; i++) {
+      expect(keys[i]).to.be.below(keys[i + 1]);
+    }
+  });
+
+  it('should sort adPreconnect in alphabetic order', () => {
+    const keys = Object.keys(adPreconnect);
+    for (let i = 0; i < keys.length - 1; i++) {
+      expect(keys[i]).to.be.below(keys[i + 1]);
+    }
+  });
+
+  it('adPreconnect should have no duplicates with adPrefetch', () => {
+    for (const adNetwork in adPreconnect) {
+      if (!adPreconnect.hasOwnProperty(adNetwork)) {
+        continue;
+      }
+
+      const preconnects = adPreconnect[adNetwork];
+      const prefetches = adPrefetch[adNetwork];
+
+      if (prefetches) {
+        checkDuplicates(preconnects, prefetches, adNetwork);
+      }
+    }
+  });
+});
+
+function checkDuplicates(preconnects, prefetches, adNetwork) {
+  if (!Array.isArray(preconnects)) {
+    preconnects = [preconnects];
+  }
+
+  if (!Array.isArray(prefetches)) {
+    prefetches = [prefetches];
+  }
+
+  const errorMsg =
+      `[${adNetwork}] no need to preconnect if the URL is in adPrefetch`;
+  for (let i = 0; i < preconnects.length; i++) {
+    for (let j = 0; j < prefetches.length; j++) {
+      expect(prefetches[j], errorMsg).to.not.contain(preconnects[i]);
+    }
+  }
+}