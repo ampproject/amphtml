a5ca5808114f32cd8f43db209b2e0cf5adc003d6
Johannes Henkel <johannes@google.com>
2016-08-03 17:22:11 -0700

    2016

--- validator/webui/build.py
+++ validator/webui/build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 #
-# Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+# Copyright 2016 The AMP HTML Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
