4253324eb5258f04f3a4aba77183787f4380dbd7
Ali Ghassemi <aghassemi@google.com>
2016-08-10 08:46:09 -0700

    More Vega features

--- extensions/amp-viz-vega/0.1/amp-viz-vega.js
+++ extensions/amp-viz-vega/0.1/amp-viz-vega.js
@@ -14,26 +14,14 @@
  * limitations under the License.
  */
 
-import {isJsonScriptTag, childElementsByTag} from '../../../src/dom';
+import {CSS} from '../../../build/amp-viz-vega-0.1.css';
+import * as dom from '../../../src/dom';
 import {isExperimentOn} from '../../../src/experiments';
 import {tryParseJson} from '../../../src/json';
 import {isLayoutSizeDefined} from '../../../src/layout';
 import {dev, user} from '../../../src/log';
 import {vsyncFor} from '../../../src/vsync';
 import {xhrFor} from '../../../src/xhr';
-
-/*
- * We are using `require()` instead of `import` here for two reasons:
- *    1- vega expects d3 to be available on window at import time but babel
- *       re-orders imports so setting window.d3 happens after loading vega.
- *    2- d3 and vega are commonJS modules and behaviour of `import *` differs
- *       between babel and closure compiler (used for dist). In the babel case
- *       one needs to do x.default but not in the closure compiler case.
- */
-/* global require: false */
-window.d3 = require('../../../third_party/d3/d3');
-require('../../../third_party/d3-geo-projection/d3-geo-projection');
-const vega = require('../../../third_party/vega/vega');
 
 /** @const */
 const EXPERIMENT = 'amp-viz-vega';
@@ -59,6 +47,19 @@ export class AmpVizVega extends AMP.BaseElement {
     /** @private {?string} */
     this.src_ = this.element.getAttribute('src');
 
+    /**
+     * @private
+     * Global vg (and implicitly d3) are required and they are created by
+     * appending vega and d3 minified files during build process.
+     */
+    this.vega_ = window.vg;
+
+    /**
+     * @private
+     * Instance of Vega chart object. https://goo.gl/laszHL
+     */
+    this.chart_ = null;
+
     user().assert(this.inlineData_ || this.src_,
         '%s: neither `src` attribute nor a ' +
         'valid <script type="application/json"> child was found for Vega data.',
@@ -75,6 +76,18 @@ export class AmpVizVega extends AMP.BaseElement {
   layoutCallback() {
     this.initialize_();
     return this.loadData_().then(() => this.renderGraph_());
+  }
+
+  /** @override */
+  onLayoutMeasure() {
+    const box = this.element.getLayoutBox();
+    this.measuredWidth_ = box.width;
+    this.measuredHeight_ = box.height;
+    if (this.chart_) {
+      vsyncFor(this.win).mutate(() => {
+        this.updateGraph_();
+      });
+    }
   }
 
   /**
@@ -99,10 +112,16 @@ export class AmpVizVega extends AMP.BaseElement {
     dev().assert(!this.src_ != !this.inlineData_);
 
     if (this.inlineData_) {
+      let err;
       this.data_ = tryParseJson(this.inlineData_, err => {
+        err = err;
+      });
+      if (err) {
         user().assert(!err, 'data could not be ' +
             'parsed. Is it in a valid JSON format?: %s', err);
-      });
+        return Promise.reject();
+      }
+      return Promise.resolve();
     } else {
       // TODO(aghassemi): We may need to expose credentials and set
       // requireAmpResponseSourceOrigin to true as well. But for now Vega
@@ -110,12 +129,10 @@ export class AmpVizVega extends AMP.BaseElement {
       // point to other Vega specs) an they don't include credentials on those
       // calls. We may want to intercept all "urls" in spec and do the loading
       // and parsing ourselves.
-      return xhrFor(this.win).fetchJson(this.src_).then( data => {
+      return xhrFor(this.win).fetchJson(this.src_).then(data => {
         this.data_ = data;
       });
     }
-
-    return Promise.resolve();
   }
 
   /**
@@ -123,7 +140,7 @@ export class AmpVizVega extends AMP.BaseElement {
    * @private
    */
   getInlineData_() {
-    const scripts = childElementsByTag(this.element, 'SCRIPT');
+    const scripts = dom.childElementsByTag(this.element, 'SCRIPT');
     if (scripts.length == 0) {
       return;
     }
@@ -132,7 +149,7 @@ export class AmpVizVega extends AMP.BaseElement {
         '<script> tags found. Only one allowed.', this.getName_());
 
     const child = scripts[0];
-    user().assert(isJsonScriptTag(child), '%s: data should ' +
+    user().assert(dom.isJsonScriptTag(child), '%s: data should ' +
         'be put in a <script type="application/json"> tag.', this.getName_());
 
     return child.textContent;
@@ -144,17 +161,31 @@ export class AmpVizVega extends AMP.BaseElement {
    */
   renderGraph_() {
     return new Promise((resolve, reject) => {
-      vega.parse.spec(this.data_, (error, chart) => {
+      this.vega_.parse.spec(this.data_, (error, chart) => {
         if (error) {
           reject(error);
           return;
         }
         vsyncFor(this.win).mutate(() => {
-          chart({el: this.container_}).update();
+          dom.removeChildren(this.container_);
+          this.chart_ = chart({el: this.container_});
+          this.updateGraph_();
           resolve();
         });
       });
     });
+  }
+
+  /**
+   * @private
+   */
+  updateGraph_() {
+    if (!this.chart_) {
+      return;
+    }
+    const measuredViewport = [this.measuredWidth_, this.measuredHeight_];
+    this.chart_.viewport(measuredViewport);
+    this.chart_.update();
   }
 
   /**
@@ -168,4 +199,4 @@ export class AmpVizVega extends AMP.BaseElement {
   }
 }
 
-AMP.registerElement('amp-viz-vega', AmpVizVega);
+AMP.registerElement('amp-viz-vega', AmpVizVega, CSS);
