9de9ff956b6896dc240b53ac11f27d93d2469dd7
Mohammad Khatib <mkhatib727@gmail.com>
2016-08-11 10:25:29 -0700

    Revert "Fix build being called before element is ready to build."

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -336,27 +336,20 @@ export class Resources {
       element.id = 'AMP_' + resource.getId();
     }
     this.resources_.push(resource);
-    this.buildOrScheduleBuildForElement_(resource);
-    dev().fine(TAG_, 'element added:', resource.debugid);
-  }
 
-  /**
-   * Builds the element if ready to be built, otherwise adds it to pending resources.
-   * @param {!Resource} resource
-   * @private
-   */
-  buildOrScheduleBuildForElement_(resource) {
     if (this.isRuntimeOn_) {
       if (this.documentReady_) {
         // Build resource immediately, the document has already been parsed.
         resource.build();
         this.schedulePass();
-      } else if (!resource.element.isBuilt()) {
+      } else if (!element.isBuilt()) {
         // Otherwise add to pending resources and try to build any ready ones.
         this.pendingBuildResources_.push(resource);
         this.buildReadyResources_();
       }
     }
+
+    dev().fine(TAG_, 'element added:', resource.debugid);
   }
 
   /**
@@ -379,7 +372,6 @@ export class Resources {
 
   /** @private */
   buildReadyResourcesUnsafe_() {
-    let builtElementsCount = 0;
     // This will loop over all current pending resources and those that
     // get added by other resources build-cycle, this will make sure all
     // elements get a chance to be built.
@@ -390,13 +382,8 @@ export class Resources {
         // Remove resource before build to remove it from the pending list
         // in either case the build succeed or throws an error.
         this.pendingBuildResources_.splice(i--, 1);
-        builtElementsCount++;
         resource.build();
       }
-    }
-
-    if (builtElementsCount > 0) {
-      this.schedulePass();
     }
   }
 
@@ -428,7 +415,12 @@ export class Resources {
    */
   upgraded(element) {
     const resource = Resource.forElement(element);
-    this.buildOrScheduleBuildForElement_(resource);
+    if (this.isRuntimeOn_) {
+      resource.build();
+      this.schedulePass();
+    } else if (resource.onUpgraded_) {
+      resource.onUpgraded_();
+    }
     dev().fine(TAG_, 'element upgraded:', resource.debugid);
   }
 
@@ -940,7 +932,7 @@ export class Resources {
     for (let i = 0; i < this.resources_.length; i++) {
       const r = this.resources_[i];
       if (r.getState() == ResourceState.NOT_BUILT) {
-        continue;
+        r.build();
       }
       if (relayoutAll || r.getState() == ResourceState.NOT_LAID_OUT) {
         r.applySizesAndMediaQuery();
@@ -1009,7 +1001,7 @@ export class Resources {
     // Phase 3: Trigger "viewport enter/exit" events.
     for (let i = 0; i < this.resources_.length; i++) {
       const r = this.resources_[i];
-      if (r.getState() == ResourceState.NOT_BUILT || r.hasOwner()) {
+      if (r.hasOwner()) {
         continue;
       }
       // Note that when the document is not visible, neither are any of its
