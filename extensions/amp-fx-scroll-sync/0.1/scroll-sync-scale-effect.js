c06f5e2126160652c30e8e12edc70b48f3203aa2
Mohammad Khatib <mkhatib727@gmail.com>
2016-08-12 13:35:20 -0700

    Initial implementation of ScrollSync Service. (#4504)

--- extensions/amp-fx-scroll-sync/0.1/scroll-sync-scale-effect.js
+++ extensions/amp-fx-scroll-sync/0.1/scroll-sync-scale-effect.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {ScrollSyncEffect} from './scroll-sync-effect'
+import {ScrollSyncEffect} from './scroll-sync-effect';
 
 class ScrollSyncScaleEffect extends ScrollSyncEffect {
   constructor(element, config) {
