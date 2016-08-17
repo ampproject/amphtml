5941e73aca0ceddc2ce92dcbd91725ff570cedcd
yevheniiminin <yevhenii.minin@teamaol.com>
2016-08-17 10:42:20 +0300

    add data-bid and data-vid to validator, add tests for bid-only and vid-only cases

--- extensions/amp-o2-player/0.1/test/test-amp-o2-player.js
+++ extensions/amp-o2-player/0.1/test/test-amp-o2-player.js
@@ -69,6 +69,20 @@ describe('amp-o2-player', () => {
     });
   });
 
+  it('requires data-pid', () => {
+    return getO2player({
+      'data-bcid': '50d595ec0364e95588c77bd2',
+    }, true).should.eventually.be.rejectedWith(
+        /Data-pid and data-bcid attributes are required for/);
+  });
+
+  it('requires data-bcid', () => {
+    return getO2player({
+      'data-pid': '573acb47e4b0564ec2e10011',
+    }, true).should.eventually.be.rejectedWith(
+        /Data-pid and data-bcid attributes are required for/);
+  });
+
   it('requires data-pid && data-bcid', () => {
     return getO2player({}).should.eventually.be.rejectedWith(
         /Data-pid and data-bcid attributes are required for/);
