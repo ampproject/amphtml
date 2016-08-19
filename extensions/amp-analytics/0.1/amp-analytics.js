410d0a7397470522699c0cee84dc97e993e289fb
Avi Mehta <avimehta@google.com>
2016-08-19 16:26:07 -0700

    Added support for multiple requests to be sent with a single trigger.
    Now if an array of requests is detected, each of those requests will be
    sent out when the trigger fires.
    
    Fixes #3847

--- extensions/amp-analytics/0.1/amp-analytics.js
+++ extensions/amp-analytics/0.1/amp-analytics.js
@@ -363,7 +363,7 @@ export class AmpAnalytics extends AMP.BaseElement {
 
   /**
    * Callback for events that are registered by the config's triggers. This
-   * method generates the request and sends the request out.
+   * method generates requests and sends them out.
    *
    * @param {!JSONType} trigger JSON config block that resulted in this event.
    * @param {!Object} event Object with details about the event.
@@ -371,7 +371,27 @@ export class AmpAnalytics extends AMP.BaseElement {
    * @private
    */
   handleEvent_(trigger, event) {
-    let request = this.requests_[trigger['request']];
+    const requests = isArray(trigger['request'])
+        ? trigger['request'] : [trigger['request']];
+
+    const resultPromises = [];
+    for (let r = 0; r < requests.length; r++) {
+      const request = this.requests_[requests[r]];
+      resultPromises.push(this.handleRequestForEvent_(request, trigger, event));
+    }
+    return Promise.all(resultPromises);
+  }
+
+  /**
+   * Processes a request for an event callback and sends it out.
+   *
+   * @param {string} request The request to process.
+   * @param {!JSONType} trigger JSON config block that resulted in this event.
+   * @param {!Object} event Object with details about the event.
+   * @return {!Promise.<string|undefined>} The request that was sent out.
+   * @private
+   */
+  handleRequestForEvent_(request, trigger, event) {
     if (!request) {
       user().error(this.getName_(), 'Ignoring event. Request string ' +
           'not found: ', trigger['request']);
