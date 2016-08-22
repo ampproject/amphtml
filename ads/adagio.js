4ce2997ad2799344eac061f1320a81f84b83d4c1
salvo_nicotra <salvo.nicotra@neodatagroup.com>
2016-08-22 23:14:03 +0200

    fixes

--- ads/adagio.js
+++ ads/adagio.js
@@ -35,5 +35,5 @@ export function adagio(global, data) {
     $neodata._adagio.B.setSid(data.sid);
     $neodata._adagio.B.addBanner(data.loc, 'c');
   }]);
- // loadScript($neodata, 'https://js-ssl.neodatagroup.com/adagio_amp.js');
+  loadScript($neodata, 'https://js-ssl.neodatagroup.com/adagio_amp.js');
 }
