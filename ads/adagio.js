5d780e8e500bfa2ccc54a5aef44879001a3ee09e
salvo_nicotra <salvo.nicotra@neodatagroup.com>
2016-08-23 16:15:41 +0200

    Added comment m# Please enter the commit message for your changes. Lines starting

--- ads/adagio.js
+++ ads/adagio.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// adagio
+// adagio js
 import {loadScript, checkData} from '../3p/3p';
 
 /**
