4a3706b26d8e4df05c9648d3b8f96fff9b363069
Michael Burns <mlb7687@users.noreply.github.com>
2016-08-30 11:21:01 -0400

    Center Doubleclick Ads (#4735)
    * Fix fo bug #4693.
    This adds the layout to window.context so that we can use it in cross domain iframe.
    We will need this ability to support multi-size ads and pad as well.
    For DoubleClick, if we see a responsive ad, pad and size things appropriately.
    
    * Remove debugging comments.
    
    * And more...
    
    * Update ad centering to apply to all ads.

--- ads/google/doubleclick.js
+++ ads/google/doubleclick.js
@@ -79,6 +79,14 @@ function doubleClickWithGpt(global, data, gladeExperiment) {
     parseInt(data.overrideHeight || data.height, 10),
   ]];
 
+  // Center the ad in the container.
+  const container = global.document.querySelector('#c');
+  container.style.top = '50%';
+  container.style.left = '50%';
+  container.style.bottom = '';
+  container.style.right = '';
+  container.style.transform = 'translate(-50%, -50%)';
+
   loadScript(global, 'https://www.googletagservices.com/tag/js/gpt.js', () => {
     global.googletag.cmd.push(() => {
       const googletag = global.googletag;
@@ -189,15 +197,14 @@ function doubleClickWithGlade(global, data, gladeExperiment) {
   }
   slot.setAttribute('data-page-url', global.context.canonicalUrl);
 
-  // Size setup.
-  // The ad container should simply fill the amp-ad iframe, but we still
-  // need to request a specific size from the ad server.
-  // The ad container size will be relative to the amp-iframe, so if the
-  // latter changes the ad container will match it.
-  slot.setAttribute('width', 'fill');
-  slot.setAttribute('height', 'fill');
-  slot.setAttribute('data-request-height', requestHeight);
-  slot.setAttribute('data-request-width', requestWidth);
+  // Center the ad in the container.
+  slot.setAttribute('height', requestHeight);
+  slot.setAttribute('width', requestWidth);
+  slot.style.top = '50%';
+  slot.style.left = '50%';
+  slot.style.bottom = '';
+  slot.style.right = '';
+  slot.style.transform = 'translate(-50%, -50%)';
 
   slot.addEventListener('gladeAdFetched', event => {
     if (event.detail.empty) {
