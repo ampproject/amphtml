ampproject/amphtml

commit 92b359a1ec1bb36874309479300b70bda8818b52

Added variable expansion for extraUrlParams. Updated examples. (#4576)
- extraUrlParams were not being expanded earlier. Now they should be.
- Analytics examples are more consistent now.
- Added click trigger example.
- Fixed vendor configs that use max-timer-length instead of maxTimerLength.

extensions/amp-analytics/0.1/amp-analytics.js
@@ -380,9 +380,12 @@ export class AmpAnalytics extends AMP.BaseElement {
 
     // Add any given extraUrlParams as query string param
     if (this.config_['extraUrlParams'] || trigger['extraUrlParams']) {
-      const params = {};
+      const params = Object.create(null);
       Object.assign(params, this.config_['extraUrlParams'],
           trigger['extraUrlParams']);
+      for (const k in params) {
+        params[k] = this.expandTemplate_(params[k], trigger, event);
+      }
       if (request.indexOf('${extraUrlParams}') >= 0) {
         const extraUrlParams = addParamsToUrl('', params).substr(1);
         request = request.replace('${extraUrlParams}', extraUrlParams);

extensions/amp-analytics/0.1/test/test-amp-analytics.js
@@ -632,7 +632,7 @@ describe('amp-analytics', function() {
     beforeEach(() => {
       config = {
         vars: {host: 'example.com', path: 'helloworld'},
-        extraUrlParams: {'s.evar0': '0', 's.evar1': '1', 'foofoo': 'baz'},
+        extraUrlParams: {'s.evar0': '0', 's.evar1': '${path}', 'foofoo': 'baz'},
         requests: {foo: 'https://${host}/${path}?a=b'},
         triggers: {trig: {'on': 'visible', 'request': 'foo'}},
       };
@@ -640,23 +640,24 @@ describe('amp-analytics', function() {
 
     function verifyRequest() {
       expect(sendRequestSpy.args[0][0]).to.have.string('v0=0');
-      expect(sendRequestSpy.args[0][0]).to.have.string('v1=1');
+      expect(sendRequestSpy.args[0][0]).to.have.string('v1=helloworld');
       expect(sendRequestSpy.args[0][0]).to.not.have.string('s.evar1');
       expect(sendRequestSpy.args[0][0]).to.not.have.string('s.evar0');
       expect(sendRequestSpy.args[0][0]).to.have.string('foofoo=baz');
     }
 
     it('are sent', () => {
-      const analytics = getAnalyticsTag(config, {'config': 'config1'});
+      const analytics = getAnalyticsTag(config);
       return waitForSendRequest(analytics).then(() => {
         expect(sendRequestSpy.args[0][0]).to.equal(
-            'https://example.com/helloworld?a=b&s.evar0=0&s.evar1=1&foofoo=baz');
+            'https://example.com/helloworld?a=b&s.evar0=0&s.evar1=helloworld' +
+            '&foofoo=baz');
       });
     });
 
     it('are renamed by extraUrlParamsReplaceMap', () => {
       config.extraUrlParamsReplaceMap = {'s.evar': 'v'};
-      const analytics = getAnalyticsTag(config , {'config': 'config1'});
+      const analytics = getAnalyticsTag(config);
       return waitForSendRequest(analytics).then(() => {
         verifyRequest();
       });
@@ -665,7 +666,7 @@ describe('amp-analytics', function() {
     it('are supported at trigger level', () => {
       config.triggers.trig.extraUrlParams = {c: 'd', 's.evar': 'e'};
       config.extraUrlParamsReplaceMap = {'s.evar': 'v'};
-      const analytics = getAnalyticsTag(config, {'config': 'config1'});
+      const analytics = getAnalyticsTag(config);
       return waitForSendRequest(analytics).then(() => {
         verifyRequest();
         expect(sendRequestSpy.args[0][0]).to.have.string('c=d');
@@ -675,10 +676,11 @@ describe('amp-analytics', function() {
 
     it('are supported as a var in URL', () => {
       config.requests.foo = 'https://${host}/${path}?${extraUrlParams}&a=b';
-      const analytics = getAnalyticsTag(config, {'config': 'config1'});
+      const analytics = getAnalyticsTag(config);
       return waitForSendRequest(analytics).then(() => {
         expect(sendRequestSpy.args[0][0]).to.equal(
-            'https://example.com/helloworld?s.evar0=0&s.evar1=1&foofoo=baz&a=b');
+            'https://example.com/helloworld?s.evar0=0&s.evar1=helloworld' +
+            '&foofoo=baz&a=b');
       });
     });
   });

examples/analytics.amp.html
@@ -34,17 +34,15 @@ Container for analytics tags. Positioned far away from top to make sure that doe
   "transport": {"beacon": false, "xhrpost": false},
   "requests": {
     "endpoint": "https://raw.githubusercontent.com/ampproject/amphtml/master/examples/img/ampicon.png",
-    "test-ping": "${endpoint}?title=${title}&acct=${account}",
-    "base": "${endpoint}?path=${canonicalPath}&name=${eventName}",
-    "pageview": "${base}&type=${eventId}&screenSize=${screenWidth}x${screenHeight}",
+    "base": "${endpoint}?${type}&path=${canonicalPath}",
     "event": "${base}&scrollY=${scrollTop}&scrollX=${scrollLeft}&height=${availableScreenHeight}&width=${availableScreenWidth}&scrollBoundV=${verticalScrollBoundary}&scrollBoundH=${horizontalScrollBoundary}",
-    "visibility": "${endpoint}?visibility&a=${maxContinuousVisibleTime}&b=${totalVisibleTime}&c=${firstSeenTime}&d=${lastSeenTime}&e=${fistVisibleTime}&f=${lastVisibleTime}&g=${minVisiblePercentage}&h=${maxVisiblePercentage}&i=${elementX}&j=${elementY}&k=${elementWidth}&l=${elementHeight}&m=${totalTime}&n=${loadTimeVisibility}&o=${backgroundedAtStart}&p=${backgrounded}&subTitle=${subTitle}"
+    "visibility": "${base}&a=${maxContinuousVisibleTime}&b=${totalVisibleTime}&c=${firstSeenTime}&d=${lastSeenTime}&e=${fistVisibleTime}&f=${lastVisibleTime}&g=${minVisiblePercentage}&h=${maxVisiblePercentage}&i=${elementX}&j=${elementY}&k=${elementWidth}&l=${elementHeight}&m=${totalTime}&n=${loadTimeVisibility}&o=${backgroundedAtStart}&p=${backgrounded}&subTitle=${subTitle}"
   },
   "vars": {
     "title": "Example Request"
   },
   "extraUrlParams": {
-    "param1": "Interesting value",
+    "param1": "${random}",
     "platform": "AMP"
   },
   "extraUrlParamsReplaceMap": {
@@ -53,10 +51,9 @@ Container for analytics tags. Positioned far away from top to make sure that doe
   "triggers": {
     "defaultPageview": {
       "on": "visible",
-      "request": "pageview",
+      "request": "base",
       "vars": {
-        "eventName": "page-loaded",
-        "eventId": "42"
+        "type": "pageLoad"
       },
       "extraUrlParams": {
         "param1": "Another value"
@@ -73,7 +70,7 @@ Container for analytics tags. Positioned far away from top to make sure that doe
         "continuousTimeMin": 2000
       },
       "vars": {
-        "eventName": "visibility event"
+        "type": "visibility"
       }
     },
     "hidden": {
@@ -84,7 +81,9 @@ Container for analytics tags. Positioned far away from top to make sure that doe
         "visiblePercentageMin": 20,
         "continuousTimeMax": 5000
       },
-      "extraUrlParams": { "on": "hidden"}
+      "vars": {
+        "type": "hidden"
+      }
     },
     "scrollPings": {
       "on": "scroll",
@@ -94,16 +93,28 @@ Container for analytics tags. Positioned far away from top to make sure that doe
         "horizontalBoundaries": [100]
       },
       "vars": {
-        "eventName": "scroll"
+        "type": "scroll"
+      }
+    },
+    "clickPings": {
+      "on": "click",
+      "selector": "${img}",
+      "request": "event",
+      "vars": {
+        "type": "click",
+        "img": "#anim-id, #container"
       }
     },
     "timer": {
       "on": "timer",
       "timerSpec": {
         "interval": 5,
-        "max-timer-length": 300
+        "maxTimerLength": 20
       },
-      "request": "test-ping"
+      "request": "base",
+      "vars": {
+        "type": "timer"
+      }
     }
   }
 }

extensions/amp-analytics/0.1/vendors.js
@@ -162,7 +162,7 @@ export const ANALYTICS_CONFIG = {
         'on': 'timer',
         'timerSpec': {
           'interval': 15,
-          'max-timer-length': 1200,
+          'maxTimerLength': 1200,
         },
         'request': 'pageping',
       },
@@ -724,7 +724,7 @@ export const ANALYTICS_CONFIG = {
         'on': 'timer',
         'timerSpec': {
           'interval': 5,
-          'max-timer-length': 1200,
+          'maxTimerLength': 1200,
         },
         'request': 'timer',
       },

commit d7dfbbe5129e4fdd64d096f68c723f95b5ea87f6

Fix js error that occurs when a extraUrlParams var has array value.
This regression was introduced in #4576.

extensions/amp-analytics/0.1/amp-analytics.js
@@ -404,7 +404,9 @@ export class AmpAnalytics extends AMP.BaseElement {
       Object.assign(params, this.config_['extraUrlParams'],
           trigger['extraUrlParams']);
       for (const k in params) {
-        params[k] = this.expandTemplate_(params[k], trigger, event);
+        if (typeof params[k] == 'string') {
+          params[k] = this.expandTemplate_(params[k], trigger, event);
+        }
       }
       if (request.indexOf('${extraUrlParams}') >= 0) {
         const extraUrlParams = addParamsToUrl('', params).substr(1);

extensions/amp-analytics/0.1/test/test-amp-analytics.js
@@ -714,6 +714,15 @@ describe('amp-analytics', function() {
             '&foofoo=baz&a=b');
       });
     });
+
+    it('work when the value is an array', () => {
+      config.extraUrlParams = {'foo': ['0']};
+      const analytics = getAnalyticsTag(config);
+      return waitForSendRequest(analytics).then(() => {
+        expect(sendRequestSpy.args[0][0]).to.equal(
+            'https://example.com/helloworld?a=b&foo=0');
+      });
+    });
   });
 
   it('fetches and merges remote config', () => {