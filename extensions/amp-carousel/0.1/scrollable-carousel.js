c38d139d2065e202795c69f9d8f0518bb202e33f
Ali Ghassemi <aghassemi@google.com>
2016-09-07 14:11:54 -0700

    Apply a default object-fit:contain for replaced content in carousel and expose a new amp-carousel-slide class (#4839)

--- extensions/amp-carousel/0.1/scrollable-carousel.js
+++ extensions/amp-carousel/0.1/scrollable-carousel.js
@@ -54,6 +54,7 @@ export class AmpScrollableCarousel extends BaseCarousel {
 
     this.cells_.forEach(cell => {
       this.setAsOwner(cell);
+      cell.classList.add('amp-carousel-slide');
       cell.style.display = 'inline-block';
       if (cell != this.cells_[0]) {
         // TODO(dvoytenko): this has to be customizable
