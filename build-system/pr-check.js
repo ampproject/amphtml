2a63f1fcd424494dcf8839387a9627786c18bf44
Johannes Henkel <johannes@google.com>
2016-09-22 13:55:59 -0700

    one more try

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -23,6 +23,7 @@
  * This script attempts to introduce some granularity for our
  * presubmit checking, via the determineBuildTargets method.
  */
+const child_process = require('child_process');
 const path = require('path');
 
 /**
