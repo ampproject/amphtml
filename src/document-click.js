edf16a6c1d1bbef85e8caca58a0f96eafc0b3a01
Justin Ridgewell <jridgewell@users.noreply.github.com>
2016-09-23 14:19:26 -0400

    Rollback: anchor href expand on document click (#5197)
    This "rollsback" #5053, since it won't cleanly revert. To rollforward, revert only this commit.
    
    /cc @keithwrightbos

--- src/document-click.js
+++ src/document-click.js
@@ -34,7 +34,6 @@ const ORIGINAL_HREF_ATTRIBUTE = 'data-amp-orig-href';
  */
 export function installGlobalClickListenerForDoc(ampdoc) {
   fromClassForDoc(ampdoc, 'clickhandler', ClickHandler);
-  fromClassForDoc(ampdoc, 'CaptureClickHandler', CaptureClickHandler);
 }
 
 
