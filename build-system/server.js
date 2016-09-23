2387b2a78ebd480ec777d145551dfaecc45513bb
Mohammad Khatib <mkhatib@google.com>
2016-09-23 15:12:26 -0700

    fix heroku link

--- build-system/server.js
+++ build-system/server.js
@@ -88,7 +88,7 @@ app.use('/api/echo/post', function(req, res) {
  * @type {RegExp}
  */
 const ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
-    '^https?://.+\.herokuapp\.com:8000');
+    '^https?://.+\.herokuapp\.com');
 
 /**
  * In practice this would be the publishers origin.
@@ -97,7 +97,7 @@ const ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
  * @type {RegExp}
  */
 const SOURCE_ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
-    '^https?://.+\.herokuapp\.com:8000/');
+    '^https?://.+\.herokuapp\.com');
 
 app.use('/form/html/post', function(req, res) {
   assertCors(req, res, ['POST']);
