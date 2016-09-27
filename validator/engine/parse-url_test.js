850d74d524646d37d9f2aff8cfef3d866c7c09bc
Greg Grothaus <greggrothaus@google.com>
2016-09-27 16:27:54 -0700

    More work on a new URL parser for javascript. This change parses out everything in the authority section of the URL, except for the host. That leaves host, path, query, and fragment.

--- validator/engine/parse-url_test.js
+++ validator/engine/parse-url_test.js
@@ -86,4 +86,112 @@ describe('parse_url', () => {
     assertStrictEqual('i have no idea what this should contain',
         url.schemeSpecificPart);
   });
+
+  it ('parses basic login', () => {
+    let urlString = 'https://user:password@example.com/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:password', url.login);
+  });
+
+  it ('parses login with ipv6 host that has colons to confuse things', () => {
+    let urlString = 'https://user:password@[2001:0db8:85a3]/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:password', url.login);
+  });
+
+  it ('parses login with ipv6 host and [ character in password', () => {
+    let urlString = 'https://user:pas[word@[2001:0db8:85a3]/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:pas[word', url.login);
+  });
+
+  it ('parses login with port to confuse things', () => {
+    let urlString = 'https://user:password@example.com:8000/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:password', url.login);
+  });
+
+  it ('parses login with @ to confuse things', () => {
+    let urlString = 'https://user:p@ssword@example.com/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:p@ssword', url.login);
+  });
+
+  it ('parses login with : to confuse things', () => {
+    let urlString = 'https://user:passwo:d@example.com/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual('user:passwo:d', url.login);
+  });
+
+  it ('parses valid IPv6 hostname', () => {
+    let urlString = 'https://[2001:0db8:85a3]/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(true, url.isValid);
+  });
+
+  it ('fails on invalid IPv6 hostname', () => {
+    let urlString = 'https://[2001:0db8:85a7]/';  // 7 not valid hex character
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(true, url.isValid);
+
+    urlString = 'https://[2001:0db8]/';
+    url = new parse_url.URL(urlString);
+    assertStrictEqual(false, url.isValid);
+  });
+
+  it ('fails on invalid port characters', () => {
+    let urlString = 'https://example.com:123abc/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(false, url.isValid);
+  });
+
+  it ('fails on port with decimal', () => {
+    let urlString = 'https://example.com:80.0/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(false, url.isValid);
+  });
+
+  it ('fails on port with negative', () => {
+    let urlString = 'https://example.com:-80/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(false, url.isValid);
+  });
+
+  it ('allows high port values', () => {
+    let urlString = 'https://example.com:65535/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(true, url.isValid);
+    assertStrictEqual(65535, url.port);
+  });
+
+  it ('fails on invalid port values', () => {
+    let urlString = 'https://example.com:65536/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(false, url.isValid);
+  });
+
+  it ('parses empty port strings', () => {
+    let urlString = 'https://example.com:/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(true, url.isValid);
+    assertStrictEqual(443, url.port);
+  });
+
+  it ('replaces 0 port with default port', () => {
+    let urlString = 'http://example.com:0/';
+    let url = new parse_url.URL(urlString);
+    assertStrictEqual(true, url.isValid);
+    assertStrictEqual(80, url.port);
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
