c308d183de1bb68025a10696921acdedc2b9929f
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2016-09-29 09:57:32 -0700

    Lightbox 2.0: item description detailed view (#5211)

--- extensions/amp-lightbox-viewer/0.1/test/test-amp-lightbox-viewer.js
+++ extensions/amp-lightbox-viewer/0.1/test/test-amp-lightbox-viewer.js
@@ -59,7 +59,9 @@ describe('amp-lightbox-viewer', () => {
     it('should lightbox item on activate', () => {
       return getAmpLightboxViewer(autoLightbox).then(viewer => {
         const impl = viewer.implementation_;
-
+        impl.vsync_.mutate = function(callback) {
+          callback();
+        };
         assertLightboxed(item1, impl, false, /*closed*/ true);
         assertControls(viewer, /*hasPrevious*/ false, /*hasNext*/ false);
         return impl.activate({source: item1}).then(() => {
@@ -72,7 +74,9 @@ describe('amp-lightbox-viewer', () => {
     it('should unlightbox item on close', () => {
       return getAmpLightboxViewer(autoLightbox).then(viewer => {
         const impl = viewer.implementation_;
-
+        impl.vsync_.mutate = function(callback) {
+          callback();
+        };
         assertLightboxed(item1, impl, false, /*closed*/ true);
         assertControls(viewer, /*hasPrevious*/ false, /*hasNext*/ false);
         return impl.activate({source: item1}).then(() => {
@@ -90,7 +94,9 @@ describe('amp-lightbox-viewer', () => {
     it('should lightbox next/previous elements', () => {
       return getAmpLightboxViewer(autoLightbox).then(viewer => {
         const impl = viewer.implementation_;
-
+        impl.vsync_.mutate = function(callback) {
+          callback();
+        };
         assertLightboxed(item1, impl, false, /*closed*/ true);
         impl.activate({source: item1});
         assertLightboxed(item1, impl, true, /*closed*/ false);
@@ -158,15 +164,50 @@ describe('amp-lightbox-viewer', () => {
       });
     });
 
+    it('should show detailed description correctly', () => {
+      return getAmpLightboxViewer(autoLightbox).then(viewer => {
+        const impl = viewer.implementation_;
+        impl.vsync_.mutate = function(callback) {
+          callback();
+        };
+        return impl.activate({source: item1}).then(() => {
+          assertLightboxed(item1, impl, true, /*closed*/ false);
+          const container = viewer.querySelector('.-amp-lbv');
+          const descriptionBox = viewer.querySelector('.amp-lbv-desc-box');
+          const button = viewer.querySelector('.amp-lbv-button-next');
+          expect(container).to.not.be.null;
+          expect(descriptionBox).to.not.be.null;
+          expect(descriptionBox.textContent).to.equal('test-text');
+          expect(descriptionBox.classList.contains('hide')).to.be.false;
+          // test click button won't toggle description box
+          button.dispatchEvent(new Event('click'));
+          expect(descriptionBox.classList.contains('hide')).to.be.false;
+          // test click on screen will hide description
+          container.dispatchEvent(new Event('click'));
+          expect(descriptionBox.classList.contains('hide')).to.be.true;
+          // test no content will disable toggling
+          impl.updateViewer_(item2);
+          container.dispatchEvent(new Event('click'));
+          expect(descriptionBox.classList.contains('hide')).to.be.true;
+          // test switching items
+          impl.updateViewer_(item4);
+          expect(descriptionBox.textContent).to.equal('test-text2');
+          expect(descriptionBox.classList.contains('hide')).to.be.true;
+          container.dispatchEvent(new Event('click'));
+          expect(descriptionBox.classList.contains('hide')).to.be.false;
+        });
+      });
+    });
+
     it('should create gallery with thumbnails', () => {
       return getAmpLightboxViewer(autoLightbox).then(viewer => {
         const impl = viewer.implementation_;
+        impl.vsync_.mutate = function(callback) {
+          callback();
+        };
         return impl.activate({source: item1}).then(() => {
           expect(impl.activeElement_).to.equal(item1);
           assertLightboxed(item1, impl, true, /*closed*/ false);
-          impl.vsync_.mutate = function(callback) {
-            callback();
-          };
           impl.openGallery_();
           const container = viewer.querySelector('.-amp-lbv');
           expect(container.getAttribute('gallery-view')).to.equal('');
@@ -217,6 +258,7 @@ describe('amp-lightbox-viewer', () => {
     if (!autoLightbox) {
       item1.setAttribute('lightbox', '');
     }
+    item1.setAttribute('alt', 'test-text');
 
     item2 = doc.createElement('blockquote');
     item2.setAttribute('lightbox', '');
@@ -227,6 +269,7 @@ describe('amp-lightbox-viewer', () => {
     }
 
     item4 = createImage();
+    item4.setAttribute('alt', 'test-text2');
     if (!autoLightbox) {
       item4.setAttribute('lightbox', '');
     }
