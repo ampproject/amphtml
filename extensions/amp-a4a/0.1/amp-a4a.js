60780766b9463173a32d200f797f7e94ad412505
Gene Levitzky <levitzky@google.com>
2016-10-03 12:01:21 -0400

    Updated amp-ad-network-fake-impl import.

--- extensions/amp-a4a/0.1/amp-a4a.js
+++ extensions/amp-a4a/0.1/amp-a4a.js
@@ -36,6 +36,7 @@ import {
   importPublicKey,
   isCryptoAvailable,
   verifySignature,
+  PublicKeyInfoDef,
 } from './crypto-verifier';
 
 /**
@@ -695,7 +696,7 @@ export class AmpA4A extends AMP.BaseElement {
       // Set opt_defaultVisible to true as 3p draw code never executed causing
       // render-start event never to fire which will remove visiblity hidden.
       this.apiHandler_.startUp(
-          iframe, /* is3p */opt_isNonAmpCreative,
+          iframe, /* is3p */ !!opt_isNonAmpCreative,
           /* opt_defaultVisible */ true);
       this.rendered_ = true;
     });
