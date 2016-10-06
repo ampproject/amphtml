c749487b3a8f9391e8418e4bba4b8d12634ad791
Affiliate-B <system+githubdev@for-it.co.jp>
2016-10-06 09:34:56 +0900

    resolve conflict
    fetched from master

--- ads/_config.js
+++ ads/_config.js
@@ -20,83 +20,8 @@
  *
  * yourNetworkName: {  // This is the "type" attribute of <amp-ad>
  *
- * @const {!Object<string, (string|!Array<string>)>}
- */
-export const adPrefetch = {
-  // Keep the list in alphabetic order
-  a9: 'https://c.amazon-adsystem.com/aax2/assoc.js',
-  accesstrade: 'https://h.accesstrade.net/js/amp/amp.js',
-  adblade: 'https://web.adblade.com/js/ads/async/show.js',
-  adgeneration: 'https://i.socdm.com/sdk/js/adg-script-loader.js',
-  // TODO: Remove this once we switch over to the direct request version of adsense.js.
-  adsense: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-  adsnative: 'https://static.adsnative.com/static/js/render.v1.js',
-  adstir: 'https://js.ad-stir.com/js/adstir_async.js',
-  adtech: 'http://s.aolcdn.com/os/ads/adsWrapper3.js',
-  aduptech: 'https://s.d.adup-tech.com/jsapi',
-  affiliateb: 'https://track.affiliate-b.com/amp/a.js',
-  amoad: [
-    'https://j.amoad.com/js/a.js',
-    'https://j.amoad.com/js/n.js',
-  ],
-  appnexus: 'https://acdn.adnxs.com/ast/ast.js',
-  atomx: 'https://s.ato.mx/p.js',
-  caprofitx: [
-    'https://cdn.caprofitx.com/pfx.min.js',
-    'https://cdn.caprofitx.com/tags/amp/profitx_amp.js',
-  ],
-  colombia: 'https://static.clmbtech.com/ad/commons/js/colombia-amp.js',
-  criteo: 'https://static.criteo.net/js/ld/publishertag.js',
-  dotandads: 'https://amp.ad.dotandad.com/dotandadsAmp.js',
-  doubleclick: [
-    'https://www.googletagservices.com/tag/js/gpt.js',
-    'https://securepubads.g.doubleclick.net/static/glade.js',
-  ],
-  eplanning: 'https://us.img.e-planning.net/layers/epl-amp.js',
-  ezoic: [
-    'https://www.googletagservices.com/tag/js/gpt.js',
-    'https://g.ezoic.net/ezoic/ampad.js',
-  ],
-  genieessp: 'https://js.gsspcln.jp/l/amp.js',
-  gmossp: 'https://cdn.gmossp-sp.jp/ads/amp.js',
-  imobile: 'https://spamp.i-mobile.co.jp/script/amp.js',
-  industrybrains: 'https://web.industrybrains.com/js/ads/async/show.js',
-  mads: 'https://eu2.madsone.com/js/tags.js',
-  'mantis-display': 'https://assets.mantisadnetwork.com/mantodea.min.js',
-  'mantis-recommend': 'https://assets.mantisadnetwork.com/recommend.min.js',
-  mediaimpact: 'https://ec-ns.sascdn.com/diff/251/divscripte/amp.js',
-  microad: 'https://j.microad.net/js/camp.js',
-  nativo: 'https://s.ntv.io/serve/load.js',
-  nend: 'https://js1.nend.net/js/amp.js',
-  openx: 'https://www.googletagservices.com/tag/js/gpt.js',
-  pubmatic: 'https://ads.pubmatic.com/AdServer/js/amp.js',
-  pubmine: 'https://s.pubmine.com/showad.js',
-  pulsepoint: 'https://ads.contextweb.com/TagPublish/getjs.static.js',
-  revcontent: 'https://labs-cdn.revcontent.com/build/amphtml/revcontent.amp.min.js',
-  smartadserver: 'https://ec-ns.sascdn.com/diff/js/amp.v0.js',
-  sortable: 'https://www.googletagservices.com/tag/js/gpt.js',
-  sovrn: 'https://ap.lijit.com/www/sovrn_amp/sovrn_ads.js',
-  teads: 'https://cdn.teads.tv/media/format/v3/teads-format.min.js',
-  'weborama-display': [
-    'https://cstatic.weborama.fr/js/advertiserv2/adperf_launch_1.0.0_scrambled.js',
-    'https://cstatic.weborama.fr/js/advertiserv2/adperf_core_1.0.0_scrambled.js',
-  ],
-  yahoojp: [
-    'https://s.yimg.jp/images/listing/tool/yads/ydn/amp/amp.js',
-    'https://yads.c.yimg.jp/js/yads.js',
-  ],
-  yieldbot: [
-    'https://cdn.yldbt.com/js/yieldbot.intent.amp.js',
-    'https://msg.yldbt.com/js/ybmsg.html',
-  ],
-  yieldmo: 'https://static.yieldmo.com/ym.amp1.js',
-  yieldone: 'https://img.ak.impact-ad.jp/ic/pone/commonjs/yone-amp.js',
-};
-
-/**
- * URLs to connect to for a given ad type.
- * No need to add URLs that have same hosts as in the adPrefetch.
- * This MUST be kept in sync with actual implementation.
+ *   // List of URLs for prefetch
+ *   prefetch: string|array
  *
  *   // List of hosts for preconnect
  *   preconnect: string|array
@@ -177,7 +102,7 @@ export const adConfig = {
     prefetch: 'https://track.affiliate-b.com/amp/a.js',
     renderStartImplemented: true,
   },
-
+  
   amoad: {
     prefetch: [
       'https://j.amoad.com/js/a.js',
