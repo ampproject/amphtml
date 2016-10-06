43b6511b0c96112dbcfda965e09d3853525e315c
Dustin Preuss <dustin@ooyala.com>
2016-10-06 16:01:55 -0700

    [amp-ooyala] Update example filename and merge with upstream master

--- test/integration/test-example-validation.js
+++ test/integration/test-example-validation.js
@@ -55,7 +55,7 @@ describe.configure().retryOnSaucelabs().run('example', function() {
     'facebook.amp.html',
     'gfycat.amp.html',
     'instagram.amp.html',
-    'ooyala.amp.html',
+    'ooyalaplayer.amp.html',
     'released.amp.html',
     'soundcloud.amp.html',
     'springboard-player.amp.html',
