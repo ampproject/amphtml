847e648094fb6adf7151a97f5140359bd66399f9
pabi <pavel.bilik@ibillboard.com>
2016-10-13 09:25:36 +0200

    + ibillboard ad example + valid hosts - one URL per line

--- ads/ibillboard.js
+++ ads/ibillboard.js
@@ -16,7 +16,13 @@
 
 import {writeScript, validateDataExists, validateSrcPrefix} from '../3p/3p';
 
-const validHosts = ['https://go.eu.bbelements.com', 'https://go.idnes.bbelements.com', 'https://go.goldbachpoland.bbelements.com', 'https://go.pol.bbelements.com', 'https://go.idmnet.bbelements.com'];
+const validHosts = [
+  'https://go.eu.bbelements.com',
+  'https://go.idnes.bbelements.com',
+  'https://go.goldbachpoland.bbelements.com',
+  'https://go.pol.bbelements.com',
+  'https://go.idmnet.bbelements.com'
+];
 
 /**
  * @param {!Window} global
