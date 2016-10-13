b31f047170099e126a62d4c70d2bc05918ebac16
Vanessa <lith@amykron.com>
2016-10-13 20:16:11 +0200

    Webediads - render-start support (#5462)

--- ads/webediads.js
+++ ads/webediads.js
@@ -16,11 +16,15 @@
 
 import {loadScript, validateData} from '../3p/3p';
 
-/** Created by Webedia on 07/03/16 */
+/**
+ * Created by Webedia on 07/03/16 - last updated on 11/10/16
+ * @param {!Window} global
+ * @param {!Object} data
+ */
 export function webediads(global, data) {
   validateData(data, ['site', 'page', 'position'], ['query']);
   loadScript(global, 'https://eu1.wbdds.com/amp.min.js', () => {
-    global.wads.init({
+    global.wads.amp.init({
       'site': data.site,
       'page': data.page,
       'position': data.position,
