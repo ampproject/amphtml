17a729f8ec7cad91fbc44575147f4990cc06b6e5
Justin Ridgewell <jridgewell@google.com>
2016-10-17 16:01:36 -0400

    Fix tests

--- test/functional/test-extensions.js
+++ test/functional/test-extensions.js
@@ -520,6 +520,7 @@ describes.sandboxed('Extensions', {}, () => {
     });
 
     it('with remote mode', () => {
+      window.AMP_MODE = {version: '123'};
       const script = calculateExtensionScriptUrl({
         pathname: 'examples/ads.amp.min.html',
         host: 'localhost:8000',
