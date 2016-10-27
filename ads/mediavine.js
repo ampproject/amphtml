a80fbda2cfd999f7ea02c856ab9899ee227e6491
Eric Hochberger <eric@mediavine.com>
2016-10-26 20:01:04 -0400

    add sizes as an optional param to validateData

--- ads/mediavine.js
+++ ads/mediavine.js
@@ -21,7 +21,7 @@ import {loadScript, validateData} from '../3p/3p';
  * @param {!Object} data
  */
 export function mediavine(global, data) {
-  validateData(data, ['site']);
+  validateData(data, ['site'], ['sizes']);
   loadScript(global, 'https://scripts.mediavine.com/amp/'
     + encodeURIComponent(data.site) + '.js');
 }
