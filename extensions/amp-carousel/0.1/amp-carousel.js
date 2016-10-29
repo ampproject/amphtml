e57427a39931ea34fd1893bb2f2ac61f5bc07ad2
Sriram Krishnan <camelburrito@users.noreply.github.com>
2016-10-28 17:36:52 -0700

    Use upgrade callback to pick the correct carousel class. (#5899)

--- extensions/amp-carousel/0.1/amp-carousel.js
+++ extensions/amp-carousel/0.1/amp-carousel.js
@@ -20,18 +20,18 @@ import {AmpScrollableCarousel} from './scrollable-carousel';
 import {CSS} from '../../../build/amp-carousel-0.1.css';
 import {isExperimentOn} from '../../../src/experiments';
 
-class CarouselSelector {
-
-  constructor(element) {
-    if (element.getAttribute('type') == 'slides') {
-      return new AmpSlideScroll(element);
+class CarouselSelector extends AMP.BaseElement {
+  /** @override */
+  upgradeCallback() {
+    if (this.element.getAttribute('type') == 'slides') {
+      return new AmpSlideScroll(this.element);
     }
     const scrollableCarouselExpt = isExperimentOn(
-        element.ownerDocument.defaultView, 'amp-scrollable-carousel');
+        this.element.ownerDocument.defaultView, 'amp-scrollable-carousel');
     if (scrollableCarouselExpt) {
-      return new AmpScrollableCarousel(element);
+      return new AmpScrollableCarousel(this.element);
     }
-    return new AmpCarousel(element);
+    return new AmpCarousel(this.element);
   }
 }
 
