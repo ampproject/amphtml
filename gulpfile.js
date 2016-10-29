36881dfa4a019fdc9445af490000566d401d40a3
Dima Voytenko <dvoytenko@google.com>
2016-10-29 15:39:16 -0700

    Skeleton for an amp-animation component (#5891)

--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,7 @@ declareExtension('amp-ad-network-doubleclick-impl', 0.1, false);
 declareExtension('amp-ad-network-fake-impl', 0.1, false);
 declareExtension('amp-analytics', '0.1', false);
 declareExtension('amp-anim', '0.1', false);
+declareExtension('amp-animation', '0.1', false);
 declareExtension('amp-apester-media', '0.1', true, 'NO_TYPE_CHECK');
 declareExtension('amp-app-banner', '0.1', true);
 declareExtension('amp-audio', '0.1', false);
