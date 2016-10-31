9f09d6c23b50f3307eb1824f2b31a56ef80c9bf7
Malte Ubl <malte.ubl@gmail.com>
2016-10-31 07:48:50 -0700

    Reflect tracking nature of iframes in priority.
    I've seen them being positioned at `fixed 0,0` and AMP prioritizing the load over actual user content.

--- extensions/amp-iframe/0.1/amp-iframe.js
+++ extensions/amp-iframe/0.1/amp-iframe.js
@@ -151,6 +151,9 @@ export class AmpIframe extends AMP.BaseElement {
     this.isAdLike_ = false;
 
     /** @private {boolean} */
+    this.isTrackingFrame_ = false;
+
+    /** @private {boolean} */
     this.isDisallowedAsAd_ = false;
 
     /**
@@ -191,6 +194,7 @@ export class AmpIframe extends AMP.BaseElement {
     this.measureIframeLayoutBox_();
 
     this.isAdLike_ = isAdLike(this);
+    this.isTrackingFrame_ = this.looksLikeTrackingIframe_();
     this.isDisallowedAsAd_ = this.isAdLike_ &&
         !isAdPositionAllowed(this.element, this.win);
 
@@ -250,8 +254,7 @@ export class AmpIframe extends AMP.BaseElement {
       return Promise.resolve();
     }
 
-    const isTracking = this.looksLikeTrackingIframe_();
-    if (isTracking) {
+    if (this.isTrackingFrame_) {
       trackingIframeCount++;
       if (trackingIframeCount > 1) {
         console/*OK*/.error('Only 1 analytics/tracking iframe allowed per ' +
@@ -280,7 +283,7 @@ export class AmpIframe extends AMP.BaseElement {
     setSandbox(this.element, iframe, this.sandbox_);
     iframe.src = this.iframeSrc;
 
-    if (!isTracking) {
+    if (!this.isTrackingFrame_) {
       this.intersectionObserver_ = new IntersectionObserver(this, iframe);
     }
 
@@ -290,7 +293,7 @@ export class AmpIframe extends AMP.BaseElement {
 
       this.activateIframe_();
 
-      if (isTracking) {
+      if (this.isTrackingFrame_) {
         // Prevent this iframe from ever being recreated.
         this.iframeSrc = null;
 
@@ -366,6 +369,9 @@ export class AmpIframe extends AMP.BaseElement {
   getPriority() {
     if (this.isAdLike_) {
       return 2; // See AmpAd3PImpl.
+    }
+    if (this.isTrackingFrame_) {
+      return 1;
     }
     return super.getPriority();
   }
