3e048e432aef07e1e2b8ca8c5c1876fea46fa205
Hongfei Ding <lannka@users.noreply.github.com>
2016-11-01 14:02:45 -0700

    Clean up: Move viewport specific logic out from viewer. (#5887)

--- test/functional/test-viewport.js
+++ test/functional/test-viewport.js
@@ -16,6 +16,7 @@
 
 import {AmpDocSingle, installDocService} from '../../src/service/ampdoc-impl';
 import {
+  installViewportServiceForDoc,
   Viewport,
   ViewportBindingDef,
   ViewportBindingIosEmbedWrapper_,
@@ -1549,4 +1550,58 @@ describes.realWin('ViewportBindingIosEmbedWrapper', {ampCss: true}, env => {
       expect(binding.getScrollTop()).to.equal(11);
     });
   });
+});
+
+describe('createViewport', () => {
+
+  describes.fakeWin('in Android', {win: {navigator: {userAgent: 'Android'}}},
+      env => {
+        let win;
+
+        beforeEach(() => {
+          win = env.win;
+          installPlatformService(win);
+          installTimerService(win);
+        });
+
+        it('should bind to "natural" when not iframed', () => {
+          win.parent = win;
+          const ampDoc = installDocService(win, true).getAmpDoc();
+          const viewport = installViewportServiceForDoc(ampDoc);
+          expect(viewport.binding_).to.be.instanceof(ViewportBindingNatural_);
+        });
+
+        it('should bind to "naturual" when iframed', () => {
+          win.parent = {};
+          const ampDoc = installDocService(win, true).getAmpDoc();
+          const viewport = installViewportServiceForDoc(ampDoc);
+          expect(viewport.binding_).to.be.instanceof(ViewportBindingNatural_);
+        });
+      });
+
+  describes.fakeWin('in iOS', {win: {navigator: {userAgent: 'iPhone'}}},
+      env => {
+        let win;
+
+        beforeEach(() => {
+          win = env.win;
+          installPlatformService(win);
+          installTimerService(win);
+        });
+
+        it('should bind to "natural" when not iframed', () => {
+          win.parent = win;
+          const ampDoc = installDocService(win, true).getAmpDoc();
+          const viewport = installViewportServiceForDoc(ampDoc);
+          expect(viewport.binding_).to.be.instanceof(ViewportBindingNatural_);
+        });
+
+        it('should bind to "natural iOS embed" when iframed', () => {
+          win.parent = {};
+          const ampDoc = installDocService(win, true).getAmpDoc();
+          const viewport = installViewportServiceForDoc(ampDoc);
+          expect(viewport.binding_).to
+              .be.instanceof(ViewportBindingNaturalIosEmbed_);
+        });
+      });
 });
