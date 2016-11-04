096553316fea2bad2a92cd5228df2a3ab2de0c02
keithwrightbos <keithwrightbos@users.noreply.github.com>
2016-11-03 21:26:55 -0400

    Fix error: "Can't find variable: TextDecoder"  (#6011)
    * Update amp-ad-network-doubleclick-impl.md
    
    * Reverting doc changes
    
    The previous change was made in error (wrong branch).
    
    * Initial changes, need to update tests
    
    * Start fixing tests
    
    * Fix unit test coverage
    
    * revert a4a examples
    
    * Include self (window) when verifying TextDecoder/TextEncoder existence
    
    * fix upstream conflict
    
    * PR feedback
    
    * PR feedback

--- src/utils/bytes.js
+++ src/utils/bytes.js
@@ -23,7 +23,7 @@ import {dev} from '../log';
  * @return {!Promise<string>}
  */
 export function utf8Decode(bytes) {
-  if (TextDecoder) {
+  if (typeof TextDecoder !== 'undefined') {
     return Promise.resolve(new TextDecoder('utf-8').decode(bytes));
   }
   return new Promise((resolve, reject) => {
@@ -44,7 +44,7 @@ export function utf8Decode(bytes) {
  * @return {!Promise<!Uint8Array>}
  */
 export function utf8Encode(string) {
-  if (TextEncoder) {
+  if (typeof TextEncoder !== 'undefined') {
     return Promise.resolve(new TextEncoder('utf-8').encode(string));
   }
   return new Promise((resolve, reject) => {
