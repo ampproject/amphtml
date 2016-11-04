221a13bd8399b563cab30aa2c27c4f625f8bc2e5
Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
2016-11-04 13:35:57 +0200

    Final, ready to PR

--- ads/_config.js
+++ ads/_config.js
@@ -201,10 +201,9 @@ export const adConfig = {
   },
 
   holder: {
-    prefetch: 'https://dl.dropboxusercontent.com/u/17485301/holderamp.js',
+    prefetch: 'https://i.holder.com.ua/js2/holder/ajax/ampv1.js',
     preconnect: [
       'https://h.holder.com.ua',
-      'https://i.holder.com.ua',
     ],
     renderStartImplemented: true,
   },
