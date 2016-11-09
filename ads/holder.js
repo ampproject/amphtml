67a27d0851055b751792b1074a90ed78e087af55
Anton Ornatskyi <heilig.thesaint@gmail.com>
2016-11-09 23:47:29 +0200

    AMP integration with Holder (#6024)
    * Draft w/o lib
    
    Draft w/o lib
    
    * Fix
    
    * Trying to setup local dev
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Draft
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * preconnect
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Dropbox
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * config
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Worked simple version
    
    Lint checked
    Banner received
    
    * holderamp lib v1
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * First PR candidate
    
    * Final, ready to PR
    
    * Draft w/o lib
    
    Draft w/o lib
    
    * Fix
    
    * Trying to setup local dev
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Draft
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Dropbox
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * config
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * Worked simple version
    
    Lint checked
    Banner received
    
    * holderamp lib v1
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * First PR candidate
    
    * Final, ready to PR
    
    * Final, ready to PR
    
    * Draft w/o lib
    
    Final version, ready to PR
    
    * Final version, ready for PR
    
    * holderamp lib v1
    
    Signed-off-by: Anton Ornatskyi <a.ornatskyi@digital.umh.ua>
    
    * PR candidate
    
    * Final, ready to PR
    
    * Final ready to PR
    
    * Cookie request removed
    
    * var document - removed
    
    * Syntax fixes
    
    * ampSlotIndex removed

---
+++ ads/holder.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {writeScript, validateData} from '../3p/3p';
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ */
+export function holder(global, data) {
+  validateData(data, ['block'], []);
+  const wcl = global.context.location;
+  const n = navigator.userAgent;
+  let l = '&r' + Math.round((Math.random() * 10000000)) + '&h' + wcl.href;
+  if (!(n.indexOf('Safari') != -1 && n.indexOf('Chrome') == -1)) {
+    l += '&c1';
+  }
+  data.queue = l;
+  writeScript(global,'https://i.holder.com.ua/js2/holder/ajax/ampv1.js');
+}
