4dc91a4f8117cdc3cc99e1142e3dabb48f19ef11
Ali Ghassemi <aghassemi@google.com>
2016-11-14 10:15:54 -0800

    typo fix

--- extensions/amp-o2-player/0.1/test/test-amp-o2-player.js
+++ extensions/amp-o2-player/0.1/test/test-amp-o2-player.js
@@ -69,19 +69,19 @@ describe('amp-o2-player', () => {
     return getO2player({
       'data-bcid': '50d595ec0364e95588c77bd2',
     }).should.eventually.be.rejectedWith(
-        /Data-pid attribute is required for/);
+        /data-pid attribute is required for/);
   });
 
   it('requires data-bcid', () => {
     return getO2player({
       'data-pid': '573acb47e4b0564ec2e10011',
     }).should.eventually.be.rejectedWith(
-        /Data-bcid attribute is required for/);
+        /data-bcid attribute is required for/);
   });
 
   it('requires data-pid && data-bcid', () => {
     return getO2player({}).should.eventually.be.rejectedWith(
-        /Data-pid attribute is required for/);
+        /data-pid attribute is required for/);
   });
 
   it('renders with data-vid passed', () => {
