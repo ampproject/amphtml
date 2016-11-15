c6f640c6be8c7e25b9da5543be93cea00fd66e61
Dima Voytenko <dvoytenko@google.com>
2016-11-15 15:54:16 -0800

    Do not install ios-embed viewport for a unembedded iframe

--- src/service/viewport-impl.js
+++ src/service/viewport-impl.js
@@ -1736,7 +1736,9 @@ const ViewportType = {
 function getViewportType(win, viewer) {
   let viewportType = viewer.getParam('viewportType') || ViewportType.NATURAL;
   if (platformFor(win).isIos()
-      && ((viewportType == ViewportType.NATURAL && viewer.isIframed())
+      && ((viewportType == ViewportType.NATURAL
+              && viewer.isIframed()
+              && viewer.isEmbedded())
           // Enable iOS Embedded mode so that it's easy to test against a more
           // realistic iOS environment.
           || getMode(win).localDev
