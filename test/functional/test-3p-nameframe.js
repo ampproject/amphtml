1bcfc7dcc761bc9d4f23f4f76cea9ea087b923a5
Terran Lane <tdrl@google.com>
2016-11-16 21:11:07 -0500

    Fixes to tests.  Clear window.name in nameframe (b/c some browsers use it to identify the window internally, leading to many string copies and serious garbage allocation if the name field is large and kept around).

--- test/functional/test-3p-nameframe.js
+++ test/functional/test-3p-nameframe.js
@@ -14,12 +14,8 @@
  * limitations under the License.
  */
 
-import {createIframePromise} from '../../testing/iframe';
-
-// Is there a way to mark this .skip?  Neither describes.skip.sandboxed
-// nor describes.sandboxed.skip works.
 describes.sandboxed('alt nameframe', {}, () => {
-  describes.realWin('nameframe', {}, env => {
+  describes.realWin('nameframe', {allowExternalResources: true}, env => {
     let fixture;
     let win;
     let doc;
@@ -30,12 +26,9 @@ describes.sandboxed('alt nameframe', {}, () => {
       doc = win.document;
       iframe = doc.createElement('iframe');
       iframe.src = 'http://localhost:9876/dist.3p/current/nameframe.max.html';
-      // Fails here.  srcdoc is not empty, so this iframe doesn't behave like a
-      // cross-origin iframe.
-      expect(iframe.getAttribute('srcdoc')).not.to.be.ok;
     });
 
-    it('should load remote nameframe and succeed', done => {
+    it('should load remote nameframe and succeed w/ valid JSON', done => {
       iframe.name = JSON.stringify({
         creative: `<html>
                    <body>
@@ -52,60 +45,25 @@ describes.sandboxed('alt nameframe', {}, () => {
       });
       doc.body.appendChild(iframe);
     });
-  });
-});
 
-describes.sandboxed('nameframe', {}, () => {
-  let fixture;
-  let win;
-  let doc;
-  let iframe;
-  beforeEach(() => {
-    return createIframePromise(/* runtimeOff */ true).then(f => {
-      fixture = f;
-      win = fixture.win;
-      doc = fixture.doc;
-      iframe = doc.createElement('iframe');
-      iframe.src = 'http://localhost:9876/dist.3p/current/nameframe.max.html';
+    it('should fail if JSON is not paresable', done => {
+      iframe.name = 'not valid JSON';
+      expectNoContent(win, done);
+      doc.body.appendChild(iframe);
+    });
+
+    it('should fail if JSON is valid, but missing creative field', done => {
+      iframe.name = JSON.stringify({fnord: 'some meaningless data'});
+      expectNoContent(win, done);
+      doc.body.appendChild(iframe);
+    });
+
+    it('should fail if JSON is missing altogether', done => {
+      iframe.name = null;
+      expectNoContent(win, done);
+      doc.body.appendChild(iframe);
     });
   });
-
-  it('should load remote nameframe and succeed with valid JSON input', done => {
-    iframe.name = JSON.stringify({
-      creative: `<html>
-                 <body>
-                   <script>
-                     window.parent.postMessage(
-                             {type: 'creative rendered'}, '*');
-                   </script>
-                 </body>
-                 </html>`,
-    });
-    win.addEventListener('message', content => {
-      expect(content.data.type).to.equal('creative rendered');
-      done();
-    });
-    doc.body.appendChild(iframe);
-  });
-
-  it('should fail if JSON is not paresable', done => {
-    iframe.name = 'not valid JSON';
-    expectNoContent(win, done);
-    doc.body.appendChild(iframe);
-  });
-
-  it('should fail if JSON is valid, but missing creative field', done => {
-    iframe.name = JSON.stringify({fnord: 'some meaningless data'});
-    expectNoContent(win, done);
-    doc.body.appendChild(iframe);
-  });
-
-  it('should fail if JSON is missing altogether', done => {
-    iframe.name = null;
-    expectNoContent(win, done);
-    doc.body.appendChild(iframe);
-  });
-
 });
 
 function expectNoContent(win, done) {
