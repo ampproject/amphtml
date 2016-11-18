b4abc5b7b94a69c12c93a7f7a0fd79b670710c72
Brad Frizzell <bradleyfrizzell@gmail.com>
2016-11-18 13:05:15 -0500

    Changed how ampcontext-lib creates window.context
    (for dev: cherry pick on to frizz-ampcontext)

--- 3p/ampcontext-lib.js
+++ 3p/ampcontext-lib.js
@@ -1,15 +1,19 @@
 import './polyfills';
 import {AmpContext} from './ampcontext.js';
 
+
 /**
  *  If window.context does not exist, we must instantiate a replacement and
  *  assign it to window.context, to provide the creative with all the required
  *  functionality.
  */
-try{
-  window.context = window.context || new AmpContext(window);
+try {
+  console.log("Attempting to make AmpContext");
   const windowContextCreated = new Event('windowContextCreated');
+  windowContextCreated.lib = new AmpContext(window);
+  // Allows for pre-existence, consider validating correct window.context lib instance?
+  window.context = window.context || windowContextCreated.lib;
   window.dispatchEvent(windowContextCreated);
 } catch (err) {
-  window.context = undefined;
+  // do nothing with error
 }
