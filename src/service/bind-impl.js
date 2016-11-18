41fd24fb48f7aebe9c4a0d423a82498e40895684
William Chou <willchou@google.com>
2016-11-18 05:29:07 -0500

    use vsync for digest mutation, add class and attr toggle

--- src/service/bind-impl.js
+++ src/service/bind-impl.js
@@ -18,8 +18,18 @@ import {evaluateBindExpr} from '../../extensions/amp-bind/0.1/bind-expr';
 import {fromClassForDoc} from '../service';
 import {getMode} from '../mode';
 import {user} from '../log';
+import {vsyncFor} from '../vsync'
 
 const TAG_ = 'AMP-BIND';
+
+/**
+ * @typedef {{
+ *   property: !string,
+ *   expression: !string,
+ *   element: !Element
+ * }}
+ */
+let BindingDef;
 
 export class Bind {
   /**
@@ -29,11 +39,17 @@ export class Bind {
     /** @const {!./ampdoc-impl.AmpDoc} */
     this.ampdoc = ampdoc;
 
-    /** @const {!Array} */
+    /** @const {!Array<BindingDef>} */
     this.bindings_ = [];
 
     /** @const {!Object} */
     this.scope_ = Object.create(null);
+
+    /** @const {!./vsync-impl.Vsync} */
+    this.vsync_ = vsyncFor(ampdoc.win);
+
+    /** @const {!Array<string>} */
+    this.protocolWhitelist_ = ['http', 'https'];
 
     this.ampdoc.whenBodyAvailable().then(body => {
       this.scanForBindings_(body);
@@ -73,10 +89,12 @@ export class Bind {
   /**
    * @param attribute {!Attr}
    * @param element {!Element}
-   * @return {?Object}
+   * @return {?BindingDef}
    * @private
    */
   bindingForAttribute_(attribute, element) {
+    // TODO: Disallow binding to blacklisted attributes.
+
     if (attribute.name.length <= 2) {
       return null;
     }
@@ -97,34 +115,52 @@ export class Bind {
   digest_(opt_verifyOnly) {
     for (let i = 0; i < this.bindings_.length; i++) {
       const binding = this.bindings_[i];
-      const result = evaluateBindExpr(binding.expression, this.scope_);
-      if (opt_verifyOnly) {
-        this.verifyBinding_(binding, result);
-      } else {
-        this.applyBinding_(binding, result);
-      }
+
+      /** {!VsyncTaskSpecDef} */
+      const task = {
+        measure: (state) => {
+          state.result = evaluateBindExpr(binding.expression, this.scope_);
+        },
+        mutate: (state) => {
+          if (opt_verifyOnly) {
+            this.verifyBinding_(binding, state.result);
+          } else {
+            this.applyBinding_(binding, state.result);
+          }
+        },
+      };
+      this.vsync_.run(task, {});
     }
   }
 
   /**
-   * @param binding {!Object}
+   * @param binding {!BindingDef}
    * @param newValue {!(Object|string|number)}
    * @private
    */
   applyBinding_(binding, newValue) {
     const {property, expression, element} = binding;
 
+    // TODO: Support arrays for classes and objects for attributes?
+
     if (property === 'text') {
       element.textContent = newValue;
     } else if (property === 'class') {
-      throw new Error('Class binding not implemented yet.');
+      element.classList = newValue;
     } else {
-      element.setAttribute(attribute, newValue);
+      if (newValue === false) {
+        element.removeAttribute(property);
+      } else {
+        const sanitizedValue = this.sanitizeAttribute_(newValue);
+        element.setAttribute(property, sanitizedValue);
+      }
     }
+
+    // TODO: Update AMP component state and queue relayout if necessary.
   }
 
   /**
-   * @param binding {!Object}
+   * @param binding {!BindingDef}
    * @param expectedValue {!(Object|string|number)}
    * @private
    */
@@ -135,16 +171,37 @@ export class Bind {
     if (property === 'text') {
       initialValue = element.textContent;
     } else if (property === 'class') {
-      throw new Error('Class binding not implemented yet.');
+      initialValue = element.classList;
     } else {
-      initialValue = element.getAttribute(attribute);
+      const attribute = element.getAttribute(property);
+      // A value of `false` means the attribute should be missing.
+      if (expectedValue === false) {
+        initialValue = (attribute !== null);
+      } else {
+        initialValue = attribute;
+      }
     }
 
     if (initialValue != expectedValue) {
       user().error(TAG_,
-        `<${element.tagName}> element [${property}] binding's ` +
+        `<${element.tagName}> element [${property}] binding ` +
         `default value (${initialValue}) does not match first expression ` +
         `result (${expectedValue}).`);
     }
+  }
+
+  /**
+   * @param value
+   * @return {string}
+   * @private
+   */
+  sanitizeAttribute_(value) {
+    if (typeof value === 'string') {
+      const protocol = newValue.split(':')[0];
+      if (this.protocolWhitelist_.indexOf(protocol) < 0) {
+        return 'unsafe:' + newValue;
+      }
+    }
+    return value;
   }
 }
