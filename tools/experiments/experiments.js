3a833613e6489c737582b5c02b741b86143074d0
xgretsch <david.karlin@bachtrack.com>
2016-11-18 22:27:24 +0000

    First implementation of imagead (intent to implement #5541) (#5751)
    * First implementation of imagead (intent to implement #5541)
    
    This is fairly bare-bones: still needs converting the example ad server
    from one on pbl.bachtrack.com to one on the test server, and still
    needs some tests implemented.
    
    * Sample JSON for imagead now in examples directory
    
    Previously, there was a dependency on an external ad server for the
    example imagead to work. This is now gone.
    
    * Fix Travis errors, get rid of global
    
    Uses of global.xxx changed to win.this.xxx as requested by @lannka at
    code review.
    
    * Separate out the ad batch manager
    
    The batch manager is now in a separate class.
    
    * Moved getBatchManager into the batch manager file
    
    This ensures that all batch manager related stuff is nicely separated
    out.
    
    * Fix blatant errors in getBatchManager
    
    * Add support for mustache templates, cleaned up lint errors
    
    * Several edits suggested by @lannka at code review
    
    Scanning of the document now only done once. Redundant callback passing
    now removed. Batch manager is now more general. Experiment check now
    turned on. Example has placeholder and fallback. Stray yahoo config
    removed.
    
    * Fixed lint errors
    
    * Removed default template and accompanying data-target variable
    
    * Latest batch of code review requests from @lannka
    
    * First batch of changes for @jridgewell code review
    
    * Got rid of use of e.win
    
    * Don't pass the element to the batch manager constructor, only its URL is needed
    
    * Next batch of code review items, mainly from @jridgewell
    
    Still not done: investigate getting rid of data-slot, use of timer
    module, use of “deferred XHR”, investigate further use of promises
    
    * Code review changes: rewrite to use promises properly
    
    amp-ad-batch-manager.js is no longer needed and has been deleted.
    
    * Next batch of code review fixes from @jridgewell
    
    * Rename imagead to custom
    
    * Next batch of @lannka code review comments
    
    * Implemented special case for ad slot not specified
    
    * Eliminate stray console.log statement
    
    * Implemented a unit test for getFullUrl_()
    
    * Next batch of @lannka code review changes
    
    In particular, incorporate the new AmpAdUIHandler code, plus tidying up
    of JSON examples.
    
    * Added ad-type-custom to experiments.js
    
    * Add file amp-ad-ui.js
    
    * Add some types to the commenting
    
    This is an attempt to get rid of some of the travis errors.
    
    * Next Travis error: unlayoutCallback was missing a return statement
    
    * Fixed some errors from gulp presubmit
    
    * Minor fixes to custom.md and to template rendering

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -56,6 +56,11 @@ const EXPERIMENTS = [
         'README.md#amp-dev-channel',
   },
   {
+    id: 'ad-type-custom',
+    name: 'Activates support for custom (self-serve) advertisements',
+    spec: 'https://github.com/ampproject/amphtml/ads/custom.md',
+  },
+  {
     id: 'alp',
     name: 'Activates support for measuring incoming clicks.',
     spec: 'https://github.com/ampproject/amphtml/issues/2934',
