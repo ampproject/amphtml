546e445d3830ef6caf4bff0d26e7a511f5bfa0ec
Hongfei Ding <lannka@users.noreply.github.com>
2016-11-19 12:03:37 -0800

    Create demo for amp-inabox with GPT (#6239)
    * Create example page for amp-inabox in GPT
    
    * Fix presubmit errors.
    
    * Address comment.

--- build-system/config.js
+++ build-system/config.js
@@ -139,6 +139,7 @@ module.exports = {
     // Files in this testdata dir are machine-generated and are not part
     // of the AMP runtime, so shouldn't be checked.
     '!extensions/amp-a4a/*/test/testdata/*.js',
+    '!examples/*.js',
   ],
   changelogIgnoreFileTypes: /\.md|\.json|\.yaml|LICENSE|CONTRIBUTORS$/
 };
