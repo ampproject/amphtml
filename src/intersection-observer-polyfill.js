dadd76e29a911d76040076e2cce1b6d072c5c1ee
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2016-11-22 18:16:57 -0800

    Check for win.performance before using it (#6316)
    * git diff HEAD~1
    
    * check performance
    
    * also check for performance.now
    
    * nit

--- src/intersection-observer-polyfill.js
+++ src/intersection-observer-polyfill.js
@@ -38,6 +38,8 @@ export const DEFAULT_THRESHOLD =
     [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4,
     0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1];
 
+const INIT_TIME = Date.now();
+
 /**
  * A function to get the element's current IntersectionObserverEntry
  * regardless of the intersetion ratio. Only available when element is not
@@ -411,7 +413,8 @@ function calculateChangeEntry(
   }
 
   return /** @type {!IntersectionObserverEntry} */ ({
-    time: performance.now(),
+    time: (typeof performance !== 'undefined' && performance.now) ?
+        performance.now() : Date.now() - INIT_TIME,
     rootBounds: rootBounds && DomRectFromLayoutRect(rootBounds),
     boundingClientRect: DomRectFromLayoutRect(boundingClientRect),
     intersectionRect: DomRectFromLayoutRect(intersection),
