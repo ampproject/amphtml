a4678e1d6a6e1a4cf131a2ce8c94652b2ad6c808
TELEKI Sandor <steleki@adverticum.com>
2016-12-06 11:09:22 +0100

    adTypeAdverticum integration v2

---
+++ ads/adverticum.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {writeScript} from '../3p/3p';
+
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ */
+
+export function adverticum(global,data) {
+  if (data.goa3zone) {
+    const zoneid = 'zone' + data.goa3zone;
+    const d = global.document.createElement('div');
+
+    d.setAttribute('id', zoneid);
+    d.setAttribute('class', 'goAdverticum goa-embedded');
+
+    document.getElementById('c').appendChild(d);
+
+    if (data.costumetargetstring) {
+      const s = global.document.createTextNode(data.costumetargetstring);
+      const v = global.document.createElement('var');
+      v.setAttribute('id', 'cT');
+      v.setAttribute('class', 'customtarget');
+      v.setAttribute('style', 'display:none');
+      v.appendChild(s);
+      document.getElementById(zoneid).appendChild(v);
+    }
+    writeScript(global, 'http://ad.adverticum.net/g3.js');
+  }
+}
