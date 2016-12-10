8c021ed00e66667f64e6bae5f0a7ee56ba319214
Yuxi Chen <yuxichen@google.com>
2016-12-09 16:48:52 -0800

    Refactor methods in viewer into history-impl

--- extensions/amp-share-tracking/0.1/test/test-amp-share-tracking.js
+++ extensions/amp-share-tracking/0.1/test/test-amp-share-tracking.js
@@ -16,7 +16,7 @@
 
 import {createIframePromise} from '../../../../testing/iframe';
 import {AmpShareTracking} from '../amp-share-tracking';
-import {Viewer} from '../../../../src/service/viewer-impl';
+import {History} from '../../../../src/service/history-impl';
 import {Xhr} from '../../../../src/service/xhr-impl';
 import {shareTrackingForOrNull} from '../../../../src/share-tracking-service';
 import {toggleExperiment} from '../../../../src/experiments';
@@ -25,15 +25,16 @@ import * as bytes from '../../../../src/utils/bytes';
 
 describe('amp-share-tracking', () => {
   let sandbox;
-  let viewerGetFragmentStub;
-  let viewerUpdateFragmentStub;
+  let historyGetFragmentStub;
+  let historyUpdateFragmentStub;
   let xhrStub;
   let randomBytesStub;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    viewerGetFragmentStub = sandbox.stub(Viewer.prototype, 'getFragment');
-    viewerUpdateFragmentStub = sandbox.stub(Viewer.prototype, 'updateFragment');
+    historyGetFragmentStub = sandbox.stub(History.prototype, 'getFragment');
+    historyUpdateFragmentStub = sandbox.stub(History.prototype,
+        'updateFragment');
     xhrStub = sandbox.stub(Xhr.prototype, 'fetchJson');
     randomBytesStub = sandbox.stub(bytes, 'getCryptoRandomBytesArray');
   });
@@ -43,7 +44,7 @@ describe('amp-share-tracking', () => {
   });
 
   function getAmpShareTracking(optVendorUrl) {
-    return createIframePromise(/* runtimeOff */ true).then(iframe => {
+    return createIframePromise().then(iframe => {
       toggleExperiment(iframe.win, 'amp-share-tracking', true);
       const el = iframe.doc.createElement('amp-share-tracking');
       if (optVendorUrl) {
@@ -57,10 +58,10 @@ describe('amp-share-tracking', () => {
   }
 
   it('should get incoming fragment starting with dot', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve('.12345'));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve('.12345'));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.incomingFragment).to.equal('12345');
       });
     });
@@ -68,31 +69,31 @@ describe('amp-share-tracking', () => {
 
   it('should get incoming fragment starting with dot and ignore ' +
       'other parameters', () => {
-    viewerGetFragmentStub.onFirstCall()
+    historyGetFragmentStub.onFirstCall()
         .returns(Promise.resolve('.12345&key=value'));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.incomingFragment).to.equal('12345');
       });
     });
   });
 
   it('should ignore incoming fragment if it is empty', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.incomingFragment).to.equal('');
       });
     });
   });
 
   it('should ignore incoming fragment if it does not start with dot', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve('12345'));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve('12345'));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.incomingFragment).to.equal('');
       });
     });
@@ -101,14 +102,14 @@ describe('amp-share-tracking', () => {
   it('should get outgoing fragment randomly if no vendor url is provided ' +
       'and win.crypto is availble, update the url fragment correctly ' +
       'when original fragment is empty', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     randomBytesStub.onFirstCall().returns(new Uint8Array([1, 2, 3, 4, 5, 6]));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         // the base64url of byte array [1, 2, 3, 4, 5, 6]
         expect(fragments.outgoingFragment).to.equal('AQIDBAUG');
-        expect(viewerUpdateFragmentStub.withArgs('#.AQIDBAUG')).to.be
+        expect(historyUpdateFragmentStub.withArgs('#.AQIDBAUG')).to.be
             .calledOnce;
       });
     });
@@ -117,14 +118,14 @@ describe('amp-share-tracking', () => {
   it('should get outgoing fragment randomly if no vendor url is provided ' +
       'and win.crypto is availble, update the url fragment correctly ' +
       'when original fragment only contains share tracking fragment', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve('.12345'));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve('.12345'));
     randomBytesStub.onFirstCall().returns(new Uint8Array([1, 2, 3, 4, 5, 6]));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         // the base64url of byte array [1, 2, 3, 4, 5, 6]
         expect(fragments.outgoingFragment).to.equal('AQIDBAUG');
-        expect(viewerUpdateFragmentStub.withArgs('#.AQIDBAUG')).to.be
+        expect(historyUpdateFragmentStub.withArgs('#.AQIDBAUG')).to.be
             .calledOnce;
       });
     });
@@ -134,15 +135,15 @@ describe('amp-share-tracking', () => {
       'and win.crypto is availble, update the url fragment correctly ' +
       'when original fragment contains share tracking fragment and ' +
       'other fragments', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(
         '.12345&key=value'));
     randomBytesStub.onFirstCall().returns(new Uint8Array([1, 2, 3, 4, 5, 6]));
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         // the base64url of byte array [1, 2, 3, 4, 5, 6]
         expect(fragments.outgoingFragment).to.equal('AQIDBAUG');
-        expect(viewerUpdateFragmentStub.withArgs('#.AQIDBAUG&key=value'))
+        expect(historyUpdateFragmentStub.withArgs('#.AQIDBAUG&key=value'))
             .to.be.calledOnce;
       });
     });
@@ -150,14 +151,14 @@ describe('amp-share-tracking', () => {
 
   it('should get outgoing fragment randomly if no vendor url ' +
       'is provided and fallback to Math.random generation', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     sandbox.stub(Math, 'random').returns(0.123456789123456789);
     randomBytesStub.onFirstCall().returns(null);
     return getAmpShareTracking().then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.outgoingFragment).to.equal('H5rdN8Eh');
-        expect(viewerUpdateFragmentStub.withArgs('#.H5rdN8Eh')).to.be
+        expect(historyUpdateFragmentStub.withArgs('#.H5rdN8Eh')).to.be
             .calledOnce;
       });
     });
@@ -165,26 +166,26 @@ describe('amp-share-tracking', () => {
 
   it('should get outgoing fragment from vendor if vendor url is provided ' +
       'and the response format is correct', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     const mockJsonResponse = {fragment: '54321'};
     xhrStub.onFirstCall().returns(Promise.resolve(mockJsonResponse));
     return getAmpShareTracking('http://foo.bar').then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
         expect(fragments.outgoingFragment).to.equal('54321');
-        expect(viewerUpdateFragmentStub.withArgs('#.54321')).to.be.calledOnce;
+        expect(historyUpdateFragmentStub.withArgs('#.54321')).to.be.calledOnce;
       });
     });
   });
 
   it('should get empty outgoing fragment if vendor url is provided ' +
       'but the response format is NOT correct', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     xhrStub.onFirstCall().returns(Promise.resolve({foo: 'bar'}));
     return getAmpShareTracking('http://foo.bar').then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
-        expect(viewerUpdateFragmentStub).to.not.be.called;
+        expect(historyGetFragmentStub).to.be.calledOnce;
+        expect(historyUpdateFragmentStub).to.not.be.called;
         expect(fragments.outgoingFragment).to.equal('');
       });
     });
@@ -192,7 +193,7 @@ describe('amp-share-tracking', () => {
 
   it('should call fetchJson with correct request when getting outgoing' +
       'fragment', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     xhrStub.onFirstCall().returns(Promise.resolve({fragment: '54321'}));
     return getAmpShareTracking('http://foo.bar').then(ampShareTracking => {
       const xhrCall = xhrStub.getCall(0);
@@ -204,8 +205,8 @@ describe('amp-share-tracking', () => {
         body: {},
       });
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
-        expect(viewerUpdateFragmentStub.withArgs('#.54321')).to.be.calledOnce;
+        expect(historyGetFragmentStub).to.be.calledOnce;
+        expect(historyUpdateFragmentStub.withArgs('#.54321')).to.be.calledOnce;
         expect(fragments.outgoingFragment).to.equal('54321');
       });
     });
@@ -213,12 +214,12 @@ describe('amp-share-tracking', () => {
 
   it('should get empty outgoing fragment if vendor url is provided ' +
       'but the xhr fails', () => {
-    viewerGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
+    historyGetFragmentStub.onFirstCall().returns(Promise.resolve(''));
     xhrStub.onFirstCall().returns(Promise.reject('404'));
     return getAmpShareTracking('http://foo.bar').then(ampShareTracking => {
       return shareTrackingForOrNull(ampShareTracking.win).then(fragments => {
-        expect(viewerGetFragmentStub).to.be.calledOnce;
-        expect(viewerUpdateFragmentStub).to.not.be.called;
+        expect(historyGetFragmentStub).to.be.calledOnce;
+        expect(historyUpdateFragmentStub).to.not.be.called;
         expect(fragments.outgoingFragment).to.equal('');
       });
     });
