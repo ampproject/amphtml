87a66e100e1cea21fe109370659003d9df17a27f
Justin Ridgewell <jridgewell@google.com>
2016-12-13 16:41:53 -0500

    Construct an artificial referrer

--- src/service-worker/core.js
+++ src/service-worker/core.js
@@ -16,6 +16,7 @@
 
 import '../../third_party/babel/custom-babel-helpers';
 import {urls} from '../config';
+import {assign} from '../polyfills/object-assign';
 
 /**
  * An AMP Release version, not to be confused with an RTV version
@@ -161,7 +162,12 @@ function normalizedRequest(request, version) {
     return request;
   }
 
-  return new Request(urlWithVersion(url, version), request);
+  return new Request(urlWithVersion(url, version),
+      assign({}, request, {
+        // For Foreign Fetch, constructing a request using an origin that does
+        // not match the SW's is mutinous.
+        referer: `${urls.cdn}/sw.js`,
+      }));
 }
 
 /**
