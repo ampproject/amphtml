ceb06a3b2ee65349a74dbde715486841b2e9052f
chenshay <chenshay@users.noreply.github.com>
2016-12-13 09:50:55 -0800

    Moving video functionality to amp-video extension (#6622)
    * skeleton for amp-video extension
    
    * lint fix
    
    * remove md file
    
    * making an amp-video extension
    
    * renaming from video->video2
    
    * fix
    
    * remove from example

--- extensions/amp-video/0.1/amp-video.js
+++ extensions/amp-video/0.1/amp-video.js
@@ -14,5 +14,175 @@
   * limitations under the License.
   */
 
-export class AmpVideo {
+import {assertHttpsUrl} from '../../../src/url';
+import {isLayoutSizeDefined} from '../../../src/layout';
+import {getMode} from '../../../src/mode';
+import {dev} from '../../../src/log';
+import {VideoEvents} from '../../../src/video-interface';
+import {videoManagerForDoc} from '../../../src/video-manager';
+
+const TAG = 'amp-video2';
+
+/**
+ * @implements {../../../src/video-interface.VideoInterface}
+ */
+class AmpVideo2 extends AMP.BaseElement {
+
+    /** @param {!AmpElement} element */
+    constructor(element) {
+      super(element);
+
+      /** @private {?Element} */
+      this.video_ = null;
+    }
+
+    /** @override */
+    isLayoutSupported(layout) {
+      return isLayoutSizeDefined(layout);
+    }
+
+    /** @override */
+    buildCallback() {
+      this.video_ = this.element.ownerDocument.createElement('video');
+
+      const posterAttr = this.element.getAttribute('poster');
+      if (!posterAttr && getMode().development) {
+        console/*OK*/.error(
+            'No "poster" attribute has been provided for amp-video.');
+      }
+
+      // Enable inline play for iOS.
+      this.video_.setAttribute('playsinline', '');
+      this.video_.setAttribute('webkit-playsinline', '');
+      // Disable video preload in prerender mode.
+      this.video_.setAttribute('preload', 'none');
+      this.propagateAttributes(['poster', 'controls', 'aria-label',
+          'aria-describedby', 'aria-labelledby'], this.video_);
+      this.forwardEvents([VideoEvents.PLAY, VideoEvents.PAUSE], this.video_);
+      this.applyFillContent(this.video_, true);
+      this.element.appendChild(this.video_);
+
+      videoManagerForDoc(this.win.document).register(this);
+    }
+
+    /** @override */
+    viewportCallback(visible) {
+      this.element.dispatchCustomEvent(VideoEvents.VISIBILITY, {visible});
+    }
+
+    /** @override */
+    layoutCallback() {
+      this.video_ = dev().assertElement(this.video_);
+
+      if (!this.isVideoSupported_()) {
+        this.toggleFallback(true);
+        return Promise.resolve();
+      }
+
+      if (this.element.getAttribute('src')) {
+        assertHttpsUrl(this.element.getAttribute('src'), this.element);
+      }
+
+      // Do not propagate `autoplay`. Autoplay behaviour is managed by
+      // video manager since amp-video implements the VideoInterface
+      this.propagateAttributes(
+          ['src', 'loop'],
+          this.video_);
+
+      if (this.element.hasAttribute('preload')) {
+        this.video_.setAttribute(
+            'preload', this.element.getAttribute('preload'));
+      } else {
+        this.video_.removeAttribute('preload');
+      }
+
+      this.getRealChildNodes().forEach(child => {
+        // Skip the video we already added to the element.
+        if (this.video_ === child) {
+          return;
+        }
+        if (child.getAttribute && child.getAttribute('src')) {
+          assertHttpsUrl(child.getAttribute('src'),
+              dev().assertElement(child));
+        }
+        this.video_.appendChild(child);
+      });
+
+      // loadPromise for media elements listens to `loadstart`
+      return this.loadPromise(this.video_).then(() => {
+        this.element.dispatchCustomEvent(VideoEvents.LOAD);
+      });
+    }
+
+    /** @override */
+    pauseCallback() {
+      if (this.video_) {
+        this.video_.pause();
+      }
+    }
+
+    /** @private */
+    isVideoSupported_() {
+      return !!this.video_.play;
+    }
+
+    // VideoInterface Implementation. See ../src/video-interface.VideoInterface
+
+    /**
+     * @override
+     */
+    supportsPlatform() {
+      return this.isVideoSupported_();
+    }
+
+    /**
+     * @override
+     */
+    isInteractive() {
+      return this.element.hasAttribute('controls');
+    }
+
+    /**
+     * @override
+     */
+    play(unusedIsAutoplay) {
+      this.video_.play();
+    }
+
+    /**
+     * @override
+     */
+    pause() {
+      this.video_.pause();
+    }
+
+    /**
+     * @override
+     */
+    mute() {
+      this.video_.muted = true;
+    }
+
+    /**
+     * @override
+     */
+    unmute() {
+      this.video_.muted = false;
+    }
+
+    /**
+     * @override
+     */
+    showControls() {
+      this.video_.controls = true;
+    }
+
+    /**
+     * @override
+     */
+    hideControls() {
+      this.video_.controls = false;
+    }
 }
+
+AMP.registerElement(TAG, AmpVideo2);
