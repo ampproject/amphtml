0098bcbdffb21dad5fd0409f8e9e44b436d3a965
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2016-12-13 15:34:31 -0800

    apply default placeholder for ad (#6447)
    * apply default placeholder for ad
    
    * fix type check
    
    * use win.document
    
    * fix test
    
    * use innerHTML
    
    * nit
    
    * apply color

--- extensions/amp-ad/0.1/test/test-amp-ad-ui.js
+++ extensions/amp-ad/0.1/test/test-amp-ad-ui.js
@@ -67,7 +67,7 @@ describe('amp-ad-ui handler', () => {
       uiHandler.init();
       uiHandler.setDisplayState(AdDisplayState.LOADED_NO_CONTENT);
       return Promise.resolve().then(() => {
-        const holder = adImpl.element.querySelector('.amp-ad-default-fallback');
+        const holder = adImpl.element.querySelector('.amp-ad-default-display');
         expect(holder).to.not.be.null;
         expect(holder).to.have.attribute('fallback');
       });
