3392d1ee542677d3b227e164438cd37a830b3b0a
Tom Long <tomlong@google.com>
2016-12-13 10:57:30 +0000

    Using the configuration in <amp-auto-ads> to insert a single ad on the page.

--- extensions/amp-auto-ads/0.1/test/test-amp-auto-ads.js
+++ extensions/amp-auto-ads/0.1/test/test-amp-auto-ads.js
@@ -17,6 +17,7 @@
 import {AmpAutoAds} from '../amp-auto-ads';
 import {toggleExperiment} from '../../../../src/experiments';
 import {xhrFor} from '../../../../src/xhr';
+import {waitForChild} from '../../../../src/dom';
 import * as sinon from 'sinon';
 
 describe('amp-auto-ads', () => {
@@ -24,6 +25,9 @@ describe('amp-auto-ads', () => {
   const AD_CLIENT = 'ca-pub-1234';
 
   let sandbox;
+  let container;
+  let anchor1;
+  let anchor2;
   let ampAutoAds;
   let ampAutoAdsElem;
   let xhr;
@@ -31,13 +35,40 @@ describe('amp-auto-ads', () => {
   beforeEach(() => {
     toggleExperiment(window, 'amp-auto-ads', true);
     sandbox = sinon.sandbox.create();
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    anchor1 = document.createElement('div');
+    anchor1.id = 'anId1';
+    container.appendChild(anchor1);
+
+    anchor2 = document.createElement('div');
+    anchor2.id = 'anId2';
+    container.appendChild(anchor2);
+
     ampAutoAdsElem = document.createElement('amp-auto-ads');
     document.body.appendChild(ampAutoAdsElem);
 
     xhr = xhrFor(window);
     xhr.fetchJson = () => {
       return Promise.resolve({
-        'testKey': 'testValue',
+        placements: [
+          {
+            anchor: {
+              selector: 'DIV#anId1',
+            },
+            pos: 2,
+            type: 1,
+          },
+          {
+            anchor: {
+              selector: 'DIV#anId2',
+            },
+            pos: 2,
+            type: 1,
+          },
+        ],
       });
     };
     sandbox.spy(xhr, 'fetchJson');
@@ -47,7 +78,26 @@ describe('amp-auto-ads', () => {
 
   afterEach(() => {
     sandbox.restore();
+    document.body.removeChild(container);
     document.body.removeChild(ampAutoAdsElem);
+  });
+
+  it('should insert a single ad on page using config', done => {
+    ampAutoAdsElem.setAttribute('data-ad-client', AD_CLIENT);
+    ampAutoAdsElem.setAttribute('type', 'adsense');
+    ampAutoAds.buildCallback();
+
+    waitForChild(anchor1, parent => {
+      return parent.childNodes.length > 0;
+    }, () => {
+      expect(anchor1.childNodes.length).to.equal(1);
+      expect(anchor2.childNodes.length).to.equal(0);
+      const adElement = anchor1.childNodes[0];
+      expect(adElement.tagName).to.equal('AMP-AD');
+      expect(adElement.getAttribute('type')).to.equal('adsense');
+      expect(adElement.getAttribute('data-ad-client')).to.equal(AD_CLIENT);
+      done();
+    });
   });
 
   it('should fetch a config from the correct URL', () => {
@@ -55,16 +105,18 @@ describe('amp-auto-ads', () => {
     ampAutoAdsElem.setAttribute('type', 'adsense');
     ampAutoAds.buildCallback();
 
-    const hostname = window.location.hostname;
-    expect(xhr.fetchJson).to.have.been.calledWith(
-        '//pagead2.googlesyndication.com/getconfig/ama?client=' +
-        AD_CLIENT + '&plah=' + hostname, {
-          mode: 'cors',
-          method: 'GET',
-          credentials: 'omit',
-          requireAmpResponseSourceOrigin: false,
-        });
-    expect(xhr.fetchJson).to.be.calledOnce;
+    return ampAutoAds.layoutCallback().then(() => {
+      const hostname = window.location.hostname;
+      expect(xhr.fetchJson).to.have.been.calledWith(
+          '//pagead2.googlesyndication.com/getconfig/ama?client=' +
+          AD_CLIENT + '&plah=' + hostname + '&ama_t=amp', {
+            mode: 'cors',
+            method: 'GET',
+            credentials: 'omit',
+            requireAmpResponseSourceOrigin: false,
+          });
+      expect(xhr.fetchJson).to.be.calledOnce;
+    });
   });
 
   it('should throw an error if no type', () => {
@@ -77,7 +129,9 @@ describe('amp-auto-ads', () => {
   it('should not try and fetch config if unknown type', () => {
     ampAutoAdsElem.setAttribute('data-ad-client', AD_CLIENT);
     ampAutoAdsElem.setAttribute('type', 'unknowntype');
-    ampAutoAds.buildCallback();
+
+    expect(() => ampAutoAds.buildCallback())
+        .to.throw(/No AdNetworkConfig for type: unknowntype​​​/);
 
     expect(xhr.fetchJson).not.to.have.been.called;
   });
