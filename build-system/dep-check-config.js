b9916ff0998b613e6c50ef78c1586a8304d447e2
chenshay <chenshay@google.com>
2016-12-14 10:55:30 -0800

    dep-check fix

--- build-system/dep-check-config.js
+++ build-system/dep-check-config.js
@@ -139,6 +139,10 @@ exports.rules = [
           'src/service/variable-source.js',
       'extensions/amp-a4a/0.1/amp-a4a.js->' +
           'src/service/url-replacements-impl.js',
+      'extensions/amp-video/0.1/amp-video.js->' +
+          'src/service/video-manager-impl.js',
+      'extensions/amp-youtube/0.1/amp-youtube.js->' +
+          'src/service/video-manager-impl.js',
     ],
   },
   {
