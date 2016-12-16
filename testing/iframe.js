9d540d44c576953db0313cb7b153a0590176be67
Ali Ghassemi <aghassemi@google.com>
2016-12-15 18:10:32 -0800

    If page is an iframed test page, use parent's location to determine script's location (#6710)

--- testing/iframe.js
+++ testing/iframe.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-
+import {FakeLocation} from './fake-dom';
 import {Timer} from '../src/timer';
 import installCustomElements from
     'document-register-element/build/document-register-element.node';
@@ -82,6 +82,11 @@ export function createFixtureIframe(fixture, initialIframeHeight, opt_beforeLoad
       // Flag as being a test window.
       win.AMP_TEST_IFRAME = true;
       win.AMP_TEST = true;
+      // Set the testLocation on iframe to parent's location since location of
+      // the test iframe is about:srcdoc.
+      // Unfortunately location object is not configurable, so we have to define
+      // a new property.
+      win.testLocation = new FakeLocation(window.location.href, win);
       win.ampTestRuntimeConfig = window.ampTestRuntimeConfig;
       if (opt_beforeLoad) {
         opt_beforeLoad(win);
@@ -205,6 +210,8 @@ export function createIframePromise(opt_runtimeOff, opt_beforeLayoutCallback) {
     iframe.onload = function() {
       // Flag as being a test window.
       iframe.contentWindow.AMP_TEST_IFRAME = true;
+      iframe.contentWindow.testLocation = new FakeLocation(window.location.href,
+          iframe.contentWindow);
       if (opt_runtimeOff) {
         iframe.contentWindow.name = '__AMP__off=1';
       }
