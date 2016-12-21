a87e17c3b0b433c7f4815b19f075822fef51c895
Malte Ubl <cramforce@users.noreply.github.com>
2016-12-21 12:15:19 -0800

    Ensure that viewer handshake always happens quickly. (#6765)
    This fixes #6728. THe problem was that chunking can delay the handshake with the viewer for a very long time. During this time the viewer cannot tell the doc that it is now visible–and visibility triggers chunking to go into fast mode. With this change the aggressive chunking mode is only triggered when the doc has gotten into a state where it can communicate with the viewer; and the viewer communication code itself is never delayed in a chunk.

--- test/functional/test-chunk.js
+++ test/functional/test-chunk.js
@@ -17,13 +17,14 @@
 import {
   activateChunkingForTesting,
   chunk,
+  chunkInstanceForTesting,
   deactivateChunking,
   onIdle,
   resolvedObjectforTesting,
 } from '../../src/chunk';
 import {installDocService} from '../../src/service/ampdoc-impl';
 import {toggleExperiment} from '../../src/experiments';
-import {viewerForDoc} from '../../src/viewer';
+import {viewerForDoc, viewerPromiseForDoc} from '../../src/viewer';
 import * as sinon from 'sinon';
 
 
@@ -49,9 +50,20 @@ describe('chunk', () => {
     beforeEach(() => {
       toggleExperiment(env.win, 'chunked-amp', experimentOn);
       fakeWin = env.win;
+      // If there is a viewer, wait for it, so we run with it being
+      // installed.
+      if (env.win.services.viewer) {
+        return viewerPromiseForDoc(env.win.document).then(() => {
+          // Make sure we make a chunk instance, so all runs
+          // have a viewer.
+          chunkInstanceForTesting(env.win.document);
+        });
+      }
     });
 
     it('should execute a chunk', done => {
+      expect(chunkInstanceForTesting(env.win.document).active_).to.equal(
+          experimentOn);
       chunk(fakeWin.document, done);
     });
 
@@ -80,7 +92,7 @@ describe('chunk', () => {
     });
   }
 
-  describes.fakeWin('no amp', {
+  describes.fakeWin('visible no amp', {
     amp: false,
   }, env => {
 
@@ -88,6 +100,32 @@ describe('chunk', () => {
       installDocService(env.win, true);
       expect(env.win.services.viewer).to.be.undefined;
       env.win.document.hidden = false;
+    });
+
+    basicTests(env);
+  });
+
+  describes.fakeWin('invisible no amp', {
+    amp: false,
+  }, env => {
+
+    beforeEach(() => {
+      installDocService(env.win, true);
+      expect(env.win.services.viewer).to.be.undefined;
+      env.win.document.hidden = true;
+      env.win.requestIdleCallback = function() {
+        throw new Error('Should not be called');
+      };
+      env.win.postMessage = function(data, targetOrigin) {
+        expect(targetOrigin).to.equal('*');
+        Promise.resolve().then(() => {
+          const event = {
+            data,
+            type: 'message',
+          };
+          env.win.eventListeners.fire(event);
+        });
+      };
     });
 
     basicTests(env);
@@ -149,10 +187,9 @@ describe('chunk', () => {
     describe('invisible experiment off', () => {
       beforeEach(() => {
         experimentOn = false;
-        const viewer = viewerForDoc(env.win.document);
-        env.sandbox.stub(viewer, 'isVisible', () => {
-          throw new Error('No calls expected: isVisible');
-        });
+        env.win.postMessage = () => {
+          throw new Error('No calls expected: postMessage');
+        };
         env.win.requestIdleCallback = () => {
           throw new Error('No calls expected: requestIdleCallback');
         };
@@ -171,7 +208,7 @@ describe('chunk', () => {
         env.win.requestIdleCallback =
             resolvingIdleCallbackWithTimeRemaining(15);
         env.sandbox.stub(resolved, 'then', () => {
-          throw new Error('No calls expected');
+          throw new Error('No calls expected .then');
         });
         env.win.location.resetHref('test#visibilityState=hidden');
       });
