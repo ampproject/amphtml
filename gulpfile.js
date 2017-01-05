013ffef0c0d8334c61023ffae64204e3d9c4642b
Sriram Krishnan <camelburrito@users.noreply.github.com>
2017-01-04 16:29:29 -0800

    Add a default outline to amp-selector (#6878)
    * Add a default outline to amp-selector
    
    * Adding 2017 copyright to the presubmit check

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ declareExtension('amp-soundcloud', '0.1', false);
 declareExtension('amp-springboard-player', '0.1', false);
 declareExtension('amp-sticky-ad', '0.1', true);
 declareExtension('amp-sticky-ad', '1.0', true);
-declareExtension('amp-selector', '0.1', false);
+declareExtension('amp-selector', '0.1', true);
 
 /**
  * @deprecated `amp-slides` is deprecated and will be deleted before 1.0.
