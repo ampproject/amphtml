5f211d3e0660e1eac8dbcfc23a2c2e180456e602
William Chou <willchou@google.com>
2017-01-06 19:37:37 -0500

    Use intermediary AST for Bind expression evaluation (#6840)
    * initial commit for bind expression AST
    
    * fix lint and type errors
    
    * rename ASTType to ASTNodeType
    
    * move try/catch to evaluator
    
    * add some comments
    
    * move other try/catch to evaluator
    
    * PR comments
    
    * change enum to number, fix type errors
    
    * justin's comments

--- extensions/amp-bind/0.1/bind-expression.js
+++ extensions/amp-bind/0.1/bind-expression.js
@@ -14,7 +14,52 @@
  * limitations under the License.
  */
 
+import {AstNodeType} from './bind-expr-defines';
 import {parser} from './bind-expr-impl';
+
+/**
+ * Map of object type to function name to whitelisted function.
+ * @type {!Object<string, !Object<string, Function>>}
+ */
+const FUNCTION_WHITELIST = (function() {
+  const whitelist = {
+    '[object Array]':
+      [
+        Array.prototype.concat,
+        Array.prototype.indexOf,
+        Array.prototype.join,
+        Array.prototype.lastIndexOf,
+        Array.prototype.slice,
+      ],
+    '[object String]':
+      [
+        String.prototype.charAt,
+        String.prototype.charCodeAt,
+        String.prototype.concat,
+        String.prototype.indexOf,
+        String.prototype.lastIndexOf,
+        String.prototype.slice,
+        String.prototype.split,
+        String.prototype.substr,
+        String.prototype.substring,
+        String.prototype.toLowerCase,
+        String.prototype.toUpperCase,
+      ],
+  };
+  // Creates a prototype-less map of function name to the function itself.
+  // This makes function lookups faster (compared to Array.indexOf).
+  const out = Object.create(null);
+  Object.keys(whitelist).forEach(type => {
+    out[type] = Object.create(null);
+
+    const functions = whitelist[type];
+    for (let i = 0; i < functions.length; i++) {
+      const f = functions[i];
+      out[type][f.name] = f;
+    }
+  });
+  return out;
+})();
 
 /**
  * A single Bind expression.
@@ -22,25 +67,200 @@ import {parser} from './bind-expr-impl';
 export class BindExpression {
   /**
    * @param {string} expressionString
+   * @throws {Error} On malformed expressions.
    */
   constructor(expressionString) {
     /** @const {string} */
     this.expressionString = expressionString;
+
+    /** @const {!./bind-expr-defines.AstNode} */
+    this.ast_ = parser.parse(this.expressionString);
   }
 
   /**
    * Evaluates the expression given a scope.
    * @param {!Object} scope
+   * @throws {Error} On illegal function invocation.
    * @return {*}
    */
   evaluate(scope) {
-    // TODO(choumx): Improve performance by extracting AST construction
-    // and generating evaluator with Function constructor.
-    try {
-      parser.yy = scope;
-      return parser.parse(this.expressionString);
-    } finally {
-      parser.yy = null;
+    return this.eval_(this.ast_, scope);
+  }
+
+  /**
+   * Recursively evaluates and returns value of `node` and its children.
+   * @param {?./bind-expr-defines.AstNode} node
+   * @param {!Object} scope
+   * @throws {Error}
+   * @return {*}
+   * @private
+   */
+  eval_(node, scope) {
+    if (!node) {
+      return null;
     }
+
+    const {type, args, value} = node;
+
+    if (type === AstNodeType.LITERAL) {
+      return value;
+    }
+
+    switch (type) {
+      case AstNodeType.EXPRESSION:
+        return this.eval_(args[0], scope);
+
+      case AstNodeType.INVOCATION:
+        const caller = this.eval_(args[0], scope);
+        const params = this.eval_(args[1], scope);
+        const method = String(value);
+
+        const callerType = Object.prototype.toString.call(caller);
+        const whitelist = FUNCTION_WHITELIST[callerType];
+        if (whitelist) {
+          const func = caller[method];
+          if (func && func === whitelist[method]) {
+            if (Array.isArray(params) && !this.containsObject_(params)) {
+              return func.apply(caller, params);
+            } else {
+              throw new Error(`Unexpected argument type in ${method}().`);
+            }
+          }
+        }
+        throw new Error(`${method}() is not a supported function.`);
+
+      case AstNodeType.MEMBER_ACCESS:
+        const target = this.eval_(args[0], scope);
+        const member = this.eval_(args[1], scope);
+
+        if (target === null || member === null) {
+          return null;
+        }
+        const targetType = typeof target;
+        if (targetType !== 'string' && targetType !== 'object') {
+          return null;
+        }
+        const memberType = typeof member;
+        if (memberType !== 'string' && memberType !== 'number') {
+          return null;
+        }
+        // Ignore Closure's type constraint for `hasOwnProperty`.
+        if (Object.prototype.hasOwnProperty.call(
+              /** @type {Object} */ (target), member)) {
+          return target[member];
+        }
+        return null;
+
+      case AstNodeType.MEMBER:
+        return value || this.eval_(args[0], scope);
+
+      case AstNodeType.VARIABLE:
+        const variable = value;
+        if (Object.prototype.hasOwnProperty.call(scope, variable)) {
+          return scope[variable];
+        }
+        return null;
+
+      case AstNodeType.ARGS:
+      case AstNodeType.ARRAY_LITERAL:
+        return (args.length > 0) ? this.eval_(args[0], scope) : [];
+
+      case AstNodeType.ARRAY:
+        return args.map(element => this.eval_(element, scope));
+
+      case AstNodeType.OBJECT_LITERAL:
+        return (args.length > 0)
+            ? this.eval_(args[0], scope)
+            : Object.create(null);
+
+      case AstNodeType.OBJECT:
+        const object = Object.create(null);
+        args.forEach(keyValue => {
+          const {k, v} = this.eval_(keyValue, scope);
+          object[k] = v;
+        });
+        return object;
+
+      case AstNodeType.KEY_VALUE:
+        return {
+          k: this.eval_(args[0], scope),
+          v: this.eval_(args[1], scope),
+        };
+
+      case AstNodeType.NOT:
+        return !this.eval_(args[0], scope);
+
+      case AstNodeType.UNARY_MINUS:
+        return -Number(this.eval_(args[0], scope));
+
+      case AstNodeType.UNARY_PLUS:
+        return +Number(this.eval_(args[0], scope));
+
+      case AstNodeType.PLUS:
+        return this.eval_(args[0], scope) + this.eval_(args[1], scope);
+
+      case AstNodeType.MINUS:
+        return Number(this.eval_(args[0], scope)) -
+            Number(this.eval_(args[1], scope));
+
+      case AstNodeType.MULTIPLY:
+        return Number(this.eval_(args[0], scope)) *
+            Number(this.eval_(args[1], scope));
+
+      case AstNodeType.DIVIDE:
+        return Number(this.eval_(args[0], scope)) /
+            Number(this.eval_(args[1], scope));
+
+      case AstNodeType.MODULO:
+        return Number(this.eval_(args[0], scope)) %
+            Number(this.eval_(args[1], scope));
+
+      case AstNodeType.LOGICAL_AND:
+        return this.eval_(args[0], scope) && this.eval_(args[1], scope);
+
+      case AstNodeType.LOGICAL_OR:
+        return this.eval_(args[0], scope) || this.eval_(args[1], scope);
+
+      case AstNodeType.LESS_OR_EQUAL:
+        return this.eval_(args[0], scope) <= this.eval_(args[1], scope);
+
+      case AstNodeType.LESS:
+        return this.eval_(args[0], scope) < this.eval_(args[1], scope);
+
+      case AstNodeType.GREATER_OR_EQUAL:
+        return this.eval_(args[0], scope) >= this.eval_(args[1], scope);
+
+      case AstNodeType.GREATER:
+        return this.eval_(args[0], scope) > this.eval_(args[1], scope);
+
+      case AstNodeType.NOT_EQUAL:
+        return this.eval_(args[0], scope) != this.eval_(args[1], scope);
+
+      case AstNodeType.EQUAL:
+        return this.eval_(args[0], scope) == this.eval_(args[1], scope);
+
+      case AstNodeType.TERNARY:
+        return this.eval_(args[0], scope)
+            ? this.eval_(args[1], scope)
+            : this.eval_(args[2], scope);
+
+      default:
+        throw new Error(`${type} is not a valid node type.`);
+    }
+  }
+
+  /**
+   * Returns true if input array contains a plain object.
+   * @param {!Array} array
+   * @return {boolean}
+   * @private
+   */
+  containsObject_(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (Object.prototype.toString.call(array[i]) === '[object Object]') {
+        return true;
+      }
+    }
+    return false;
   }
 }
