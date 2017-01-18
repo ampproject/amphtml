2a7cfaaeee82ac6caf3fd498c74a52358ebc60ed
erwin mombay <erwinmombay@users.noreply.github.com>
2017-01-17 17:34:41 -0800

    add a way to override hostname (#6972)
    * add a way to override hostname
    
    * rollback 3p url change
    
    * org -> net

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,9 @@ var cssOnly = argv['css-only'];
 
 require('./build-system/tasks');
 
+var hostname = argv.hostname || 'cdn.ampproject.org';
+var hostname3p = argv.hostname3p || '3p.ampproject.net';
+
 // All declared extensions.
 var extensions = {};
 
@@ -728,7 +731,7 @@ function buildExperiments(options) {
   $$.util.log('Processing ' + htmlPath);
   var html = fs.readFileSync(htmlPath, 'utf8');
   var minHtml = html.replace('/dist.tools/experiments/experiments.js',
-      'https://cdn.ampproject.org/v0/experiments.js');
+      `https://${hostname}/v0/experiments.js`);
   gulp.src(htmlPath)
       .pipe($$.file('experiments.cdn.html', minHtml))
       .pipe(gulp.dest('dist.tools/experiments/'));
@@ -796,7 +799,7 @@ function buildLoginDoneVersion(version, options) {
   var html = fs.readFileSync(htmlPath, 'utf8');
   var minHtml = html.replace(
       '../../../dist/v0/amp-login-done-' + version + '.max.js',
-      'https://cdn.ampproject.org/v0/amp-login-done-' + version + '.js');
+      `https://${hostname}/v0/amp-login-done-` + version + '.js');
 
   mkdirSync('dist');
   mkdirSync('dist/v0');
