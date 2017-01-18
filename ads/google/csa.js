d77e07ec284736aedd9159d5e3907a8f95bd236f
Megan Johnson <meganj@google.com>
2017-01-17 22:51:19 -0800

    Append CSA container to 'c' element, not body

--- ads/google/csa.js
+++ ads/google/csa.js
@@ -61,8 +61,7 @@ export function csa(global, data) {
   const containerDiv = global.document.createElement('div');
   const containerId = 'csacontainer';
   containerDiv.id = containerId;
-  setStyle(containerDiv, 'position', 'absolute');
-  global.document.body.appendChild(containerDiv);
+  global.document.getElementById('c').appendChild(containerDiv);
 
   const pageOptions = {source: 'amp', referer: global.context.referrer};
   const adblockOptions = {container: containerId};
@@ -294,7 +293,7 @@ function createOverflow(global, container, height) {
   const overflow = getOverflowElement(global);
   // When overflow is clicked, resize to full height
   overflow.onclick = global.context.requestResize.bind(null, undefined, height);
-  global.document.body.appendChild(overflow);
+  global.document.getElementById('c').appendChild(overflow);
   // Resize the CSA container to not conflict with overflow
   resizeCsa(container, currentAmpHeight - overflowHeight);
 }
@@ -310,7 +309,6 @@ function getOverflowElement(global) {
   setStyles(overflow, {
     position: 'absolute',
     height: overflowHeight + 'px',
-    top: (currentAmpHeight - overflowHeight) + 'px',
     width: '100%',
   });
   overflow.appendChild(getOverflowLine(global));
