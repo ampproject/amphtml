57810d9743374b800059eb99858f966d7939e1f6
Justin Ridgewell <justin@ridgewell.name>
2017-02-01 19:23:43 -0500

    Add scopedQuerySelector helper (#7260)
    * Eliminate uses of childElement and childElements
    
    * Forbid uses of buggy querySelector
    
    Anytime you select a descendant of a descendant (`div div`, or, really,
    `* *`), your matching against the entire document then filtering to
    elements inside the scope. This is **not** what you'd expect.
    
    Fixes a selector bug in `<amp-analytics>`.
    
    * Add tests
    
    * Use `Date.now` to avoid `.` class selector conflict
    
    * Merge latest master
    
    * Linting
    
    * Fix types
    
    * Fix merge conflict
    
    * Return NodeList
    
    * Fix tests

--- src/runtime.js
+++ src/runtime.js
@@ -675,13 +675,14 @@ class MultidocManager {
     const extensionIds = [];
     if (doc.head) {
       const parentLinks = {};
-      childElementsByTag(dev().assertElement(this.win.document.head), 'link')
-          .forEach(link => {
-            const href = link.getAttribute('href');
-            if (href) {
-              parentLinks[href] = true;
-            }
-          });
+      const links = childElementsByTag(
+          dev().assertElement(this.win.document.head), 'link');
+      for (let i = 0; i < links.length; i++) {
+        const href = links[i].getAttribute('href');
+        if (href) {
+          parentLinks[href] = true;
+        }
+      }
 
       for (let n = doc.head.firstElementChild; n; n = n.nextElementSibling) {
         const tagName = n.tagName;
@@ -927,7 +928,7 @@ function maybeLoadCorrectVersion(win, fnOrStruct) {
   }
   // The :not is an extra prevention of recursion because it will be
   // added to script tags that go into the code path below.
-  const scriptInHead = win.document.head.querySelector(
+  const scriptInHead = win.document.head./*OK*/querySelector(
           `[custom-element="${fnOrStruct.n}"]:not([i-amphtml-inserted])`);
   dev().assert(scriptInHead, 'Expected to find script for extension: %s',
       fnOrStruct.n);
