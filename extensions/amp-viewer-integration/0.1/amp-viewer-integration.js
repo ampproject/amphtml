a5a1b283d814230db129cf1d779f79155f98b9d2
chenshay <chenshay@google.com>
2017-02-09 15:26:55 -0800

    fix

--- extensions/amp-viewer-integration/0.1/amp-viewer-integration.js
+++ extensions/amp-viewer-integration/0.1/amp-viewer-integration.js
@@ -64,7 +64,7 @@ export class AmpViewerIntegration {
     this.unconfirmedViewerOrigin_ = viewer.getParam('origin');
 
     if (!this.isWebView_ && !this.unconfirmedViewerOrigin_) {
-      return new Promise(() => {});
+      return Promise.resolve();
     }
 
     if (this.isWebView_) {
