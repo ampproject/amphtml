7e692b1b49cbce147bf0d7f8beb7d17b699f0571
Kevin Howald <kmh287@cornell.edu>
2017-02-09 12:57:20 -0500

    pr comments

--- src/polyfills.js
+++ src/polyfills.js
@@ -25,7 +25,7 @@ import {install as installDocContains} from './polyfills/document-contains';
 import {install as installMathSign} from './polyfills/math-sign';
 import {install as installObjectAssign} from './polyfills/object-assign';
 import {install as installPromise} from './polyfills/promise';
-import {install as installArray} from './polyfills/array-includes';
+import {install as installArrayIncludes} from './polyfills/array-includes';
 import {getMode} from './mode';
 
 /**
@@ -43,4 +43,4 @@ installMathSign(self);
 installObjectAssign(self);
 installPromise(self);
 installDocContains(self);
-installArray(self);
+installArrayIncludes(self);
