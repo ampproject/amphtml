4f918281fe5c070ca279f7e1fd1131d0022780ba
Unbug Lee <tidelgl@gmail.com>
2017-02-13 04:28:02 +0800

    Added a special param to measure activity on amp-hulu (#7322)

--- extensions/amp-hulu/0.1/amp-hulu.js
+++ extensions/amp-hulu/0.1/amp-hulu.js
@@ -79,7 +79,7 @@ class AmpHulu extends AMP.BaseElement {
   /** @return {string} */
   getVideoIframeSrc_() {
     dev().assert(this.eid_);
-    return `https://secure.hulu.com/dash/mobile_embed.html?eid=${encodeURIComponent(this.eid_ || '')}`;
+    return `https://secure.hulu.com/dash/mobile_embed.html?amp=1&eid=${encodeURIComponent(this.eid_ || '')}`;
   }
 
 };
