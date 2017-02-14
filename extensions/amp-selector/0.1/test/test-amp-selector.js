d58606a2707ff5a0adbf2af2b26b00cb105651a7
Malte Ubl <malte.ubl@gmail.com>
2017-02-13 21:35:29 -0800

    Remove .only

--- extensions/amp-selector/0.1/test/test-amp-selector.js
+++ extensions/amp-selector/0.1/test/test-amp-selector.js
@@ -16,7 +16,7 @@
 
 import '../amp-selector';
 
-describes.realWin.only('amp-selector', {
+describes.realWin('amp-selector', {
   win: { /* window spec */
     location: '...',
     historyOff: false,
