a8b36b0f4c0c3f159e8584b23dc949ff36c7fa0c
bradfrizzell <bradfrizzell@users.noreply.github.com>
2017-02-16 19:34:44 -0500

    Fixed route error (#7621)

--- build-system/server.js
+++ build-system/server.js
@@ -460,12 +460,12 @@ app.use('/min/', function(req, res) {
 // Nest the response in an iframe.
 // Example:
 // http://localhost:8000/iframe/examples/ads.amp.max.html
-app.get('/iframe/', function(req, res) {
+app.get('/iframe/*', function(req, res) {
   // Returns an html blob with an iframe pointing to the url after /iframe/.
   res.send(`<!doctype html>
           <html style="width:100%; height:100%;">
             <body style="width:98%; height:98%;">
-              <iframe src="${req.url}" style="width:100%; height:100%;">
+              <iframe src="${req.url.substr(7)}" style="width:100%; height:100%;">
               </iframe>
             </body>
           </html>`);
