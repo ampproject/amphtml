0d58105ceb18859541f2cf40fb31bec8da34421f
Kevin Howald <kmh287@users.noreply.github.com>
2017-02-27 19:44:57 -0500

    Prevent BindEvaluator from returning Error objects to web worker (#7599)
    * Prevent BindEvaluator from returning error objects to web worker
    
    * cleanup
    
    * pr comments
    
    * cleanup
    
    * cleanup
    
    * lint error
    
    * added tests and addressed error from CI
    
    * pr comments
    
    * pr comments
    
    * pr comments

--- src/web-worker/web-worker.js
+++ src/web-worker/web-worker.js
@@ -69,5 +69,8 @@ self.addEventListener('message', function(event) {
 
   /** @type {FromWorkerMessageDef} */
   const message = {method, returnValue, id};
+  // `message` may only contain values or objects handled by the
+  // structured clone algorithm.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
   self./*OK*/postMessage(message);
 });
