018ba7d28966a7ae74230a5cf311ac066879f3fd
Kevin Howald <kevinhowald@google.com>
2017-03-01 12:52:29 -0500

    timer

--- test/functional/test-history.js
+++ test/functional/test-history.js
@@ -22,7 +22,10 @@ import {
   installHistoryServiceForDoc,
 } from '../../src/service/history-impl';
 import {listenOncePromise} from '../../src/event-helper';
-import {installTimerService} from '../../src/service/timer-impl';
+import {
+  installTimerService,
+  timerServiceForTesting
+} from '../../src/service/timer-impl';
 import {parseUrl} from '../../src/url';
 import * as sinon from 'sinon';
 
@@ -188,7 +191,7 @@ describes.sandboxed('History install', {}, () => {
     win = {
       services: {
         'viewer': {obj: viewer},
-        'timer': {obj: installTimerService(window)},
+        'timer': {obj: timerServiceForTesting(window)},
       },
       history: {
         length: 0,
