1fc68e53ab1146916019d29d4a3c126bc54a67e4
Yuxi Chen <yuxichen@google.com>
2017-03-01 14:10:17 -0800

    Cid timeout error should not be dev error

--- extensions/amp-analytics/0.1/cid-impl.js
+++ extensions/amp-analytics/0.1/cid-impl.js
@@ -35,7 +35,7 @@ import {viewerForDoc} from '../../../src/viewer';
 import {cryptoFor} from '../../../src/crypto';
 import {tryParseJson} from '../../../src/json';
 import {timerFor} from '../../../src/timer';
-import {user, dev} from '../../../src/log';
+import {user, rethrowAsync} from '../../../src/log';
 
 const TAG_ = 'Cid';
 
@@ -326,7 +326,7 @@ export function viewerBaseCid(ampdoc, opt_data) {
     // it should resolve in milli seconds.
     return timerFor(ampdoc.win).timeoutPromise(10000, cidPromise, 'base cid')
         .catch(error => {
-          dev().error(TAG_, error);
+          rethrowAsync(TAG_, error);
           return undefined;
         });
   });
