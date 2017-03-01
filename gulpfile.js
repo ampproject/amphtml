6c677626538b783d0e2c4eb200c87ea0fb348e61
Carlos Vializ <cvializ@users.noreply.github.com>
2017-03-01 13:09:55 -0800

    Implement parallax effect extension (#7794)

--- gulpfile.js
+++ gulpfile.js
@@ -78,6 +78,7 @@ declareExtension('amp-font', '0.1', false, 'NO_TYPE_CHECK');
 declareExtension('amp-form', '0.1', true);
 declareExtension('amp-fresh', '0.1', true);
 declareExtension('amp-fx-flying-carpet', '0.1', true);
+declareExtension('amp-fx-parallax', '0.1', false);
 declareExtension('amp-gfycat', '0.1', false);
 declareExtension('amp-hulu', '0.1', false);
 declareExtension('amp-iframe', '0.1', false, 'NO_TYPE_CHECK');
