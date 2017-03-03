2ee8d4a72ff63657754bee985e86408bad91e3f3
Leandro Zanol <lzanol@gmail.com>
2017-03-03 18:52:02 -0300

    #7755 added event handlings
    implemented mute/unmute

--- extensions/amp-samba-player/0.1/amp-samba-player.js
+++ extensions/amp-samba-player/0.1/amp-samba-player.js
@@ -18,18 +18,28 @@ import {isLayoutSizeDefined} from '../../../src/layout';
 import {getDataParamsFromAttributes, removeElement} from '../../../src/dom';
 import {installVideoManagerForDoc} from '../../../src/service/video-manager-impl';
 import {videoManagerForDoc} from '../../../src/video-manager';
+import {VideoEvents} from '../../../src/video-interface';
+import {tryParseJson} from '../../../src/json';
 import {user} from '../../../src/log';
 
 /** @const {string} */
 const TAG = 'amp-samba-player';
 
 /** @private {Object} */
-const API_DICTIONARY = {
+const API_MAP = {
 	'web4-7091': '//playerapitest2.liquidplatform.com:7091/embed/',
 	'localhost-8080': '//localhost:8080/player-api/embed/',
 	'web4-7021': '//playerapitest2.liquidplatform.com:7021/embed/',
 	'staging': '//staging-player-api.sambavideos.sambatech.com/v3/embed/',
 	'prod': '//fast.player.liquidplatform.com/pApiv2/embed/'
+};
+
+/** @private {Object} */
+const EVENTS_MAP = {
+	'onPlay': VideoEvents.PLAY,
+	'onPause': VideoEvents.PAUSE,
+	'onMute': VideoEvents.MUTED,
+	'onUnmute': VideoEvents.UNMUTED
 };
 
 class AmpSambaPlayer extends AMP.BaseElement {
@@ -55,12 +65,21 @@ class AmpSambaPlayer extends AMP.BaseElement {
 
 		/** @private {?HTMLIFrameElement} */
 		this.iframe_ = null;
+
+		/** @private {?Function} */
+		this.playerLoadResolver_ = null;
+
+		/** @private {Promise} */
+		this.playerLoadPromise_ = new Promise(resolve => { this.playerLoadResolver_ = resolve });
+
+		// Event handlers bindings
+		this.playerMessageHandler_ = this.playerMessageHandler_.bind(this);
 	}
 
 	/** @override */
 	preconnectCallback(opt_onLayout) {
 		// host to serve the player
-		this.preconnect.url(API_DICTIONARY['prod'], opt_onLayout);
+		this.preconnect.url(API_MAP['prod'], opt_onLayout);
 		// host to serve media contents
 		this.preconnect.url('http://pvbps-sambavideos.akamaized.net', opt_onLayout);
 	}
@@ -112,13 +131,19 @@ class AmpSambaPlayer extends AMP.BaseElement {
 
 		iframe.setAttribute('frameborder', '0');
 		iframe.setAttribute('allowfullscreen', 'true');
-		iframe.src = `${this.protocol_}${API_DICTIONARY[this.env_]}${this.projectId_}/${this.mediaId_}?${params}`;
+		iframe.src = `${this.protocol_}${API_MAP[this.env_]}${this.projectId_}/${this.mediaId_}?${params}`;
 
 		this.applyFillContent(iframe);
 		this.element.appendChild(iframe);
 		this.iframe_ = iframe;
 
-		return this.loadPromise(iframe);
+		this.win.addEventListener('message', this.playerMessageHandler_);
+
+		return this.loadPromise(iframe)
+			.then(() => {
+				this.element.dispatchCustomEvent(VideoEvents.LOAD);
+				this.playerLoadResolver_(this.iframe_);
+			});
 	}
 
 	/** @override */
@@ -151,19 +176,23 @@ class AmpSambaPlayer extends AMP.BaseElement {
 
 	/** @override */
 	play(unusedIsAutoplay) {
-		this.iframe_.contentWindow./*OK*/postMessage('play', '*');
+		this.sendMessage_('play');
 	}
 
 	/** @override */
 	pause() {
-		this.iframe_.contentWindow./*OK*/postMessage('pause', '*');
+		this.sendMessage_('pause');
 	}
 
 	/** @override */
-	mute() {}
+	mute() {
+		this.sendMessage_('setVolume', 0);
+	}
 
 	/** @override */
-	unmute() {}
+	unmute() {
+		this.sendMessage_('setVolume', 75);
+	}
 
 	/** @override */
 	showControls() {}
@@ -172,6 +201,44 @@ class AmpSambaPlayer extends AMP.BaseElement {
 	hideControls() {}
 
 	// End of VideoInterface implementation
+
+	/**
+	 * Sends a message to the player through "postMessage" API.
+	 * @param {string} message
+	 * @param {?Object} opt_params
+	 * @private
+	 */
+	sendMessage_(message, opt_params) {
+		if (opt_params == null)
+			opt_params = '';
+		
+		if (typeof opt_params === 'object')
+			opt_params = encodeURIComponent(JSON.stringify(opt_params));
+
+		this.playerLoadPromise_.then((iframe) => {
+			iframe.contentWindow./*OK*/postMessage(`${message}:${opt_params}`, '*');
+		});
+	}
+
+	/**
+	 * Handles messages sent by the player.
+	 * @param {MessageEvent} event
+	 * @private
+	 */
+	playerMessageHandler_(event) {
+		if (this.iframe_.src.indexOf(event.origin) === -1
+			|| event.source !== this.iframe_.contentWindow
+			|| typeof event.data !== 'string'
+			|| event.data.indexOf('{') !== 0)
+			return;
+
+		const data = tryParseJson(event.data);
+
+		if (data == null || typeof data !== 'object')
+			return;
+
+		this.element.dispatchCustomEvent(EVENTS_MAP[data.event]);
+	}
 }
 
 AMP.registerElement(TAG, AmpSambaPlayer);
