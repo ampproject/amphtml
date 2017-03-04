5c2b3f44252a46e23f0e7c308e4188a6a5b4e526
Dima Voytenko <dvoytenko@google.com>
2017-03-03 18:13:04 -0800

    Restrict prerenderComplete signal to only prerenderable elements (#7961)

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -273,8 +273,6 @@ export class Resources {
    * @return {!Promise<!Array<!Resource>>}
    */
   getResourcesInRect(hostWin, rect, opt_isInPrerender) {
-    opt_isInPrerender = opt_isInPrerender || false;
-
     // First, wait for the `ready-scan` signal. Waiting for each element
     // individually is too expensive and `ready-scan` will cover most of
     // the initially parsed elements.
