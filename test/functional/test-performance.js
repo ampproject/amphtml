2f2b9d1f8b4f3bf13d5059e91a8e56ca71bfb5f0
Hongfei Ding <lannka@users.noreply.github.com>
2017-03-14 09:59:55 -0700

    Send RTV version number along with CSI pings. (#8111)
    * Send RTV version number along with CSI pings.
    
    * fix tests

--- test/functional/test-performance.js
+++ test/functional/test-performance.js
@@ -399,17 +399,20 @@ describe('performance', () => {
       });
 
       it('should call the flush callback', () => {
-        expect(viewerSendMessageStub.withArgs('sendCsi', undefined,
+        const payload = {
+          ampexp: '$internalRuntimeVersion$',
+        };
+        expect(viewerSendMessageStub.withArgs('sendCsi', payload,
             /* cancelUnsent */true)).to.have.callCount(0);
         // coreServicesAvailable calls flush once.
         return perf.coreServicesAvailable().then(() => {
-          expect(viewerSendMessageStub.withArgs('sendCsi', undefined,
+          expect(viewerSendMessageStub.withArgs('sendCsi', payload,
               /* cancelUnsent */true)).to.have.callCount(1);
           perf.flush();
-          expect(viewerSendMessageStub.withArgs('sendCsi', undefined,
+          expect(viewerSendMessageStub.withArgs('sendCsi', payload,
               /* cancelUnsent */true)).to.have.callCount(2);
           perf.flush();
-          expect(viewerSendMessageStub.withArgs('sendCsi', undefined,
+          expect(viewerSendMessageStub.withArgs('sendCsi', payload,
               /* cancelUnsent */true)).to.have.callCount(3);
         });
       });
@@ -669,8 +672,9 @@ describes.fakeWin('performance with experiment', {amp: true}, env => {
     sandbox.stub(perf, 'getHostname_', () => 'cdn.ampproject.org');
     return perf.coreServicesAvailable().then(() => {
       perf.flush();
-      expect(viewerSendMessageStub)
-          .to.be.calledWith('sendCsi', {ampexp: 'legacy-cdn-domain'});
+      expect(viewerSendMessageStub).to.be.calledWith('sendCsi', {
+        ampexp: '$internalRuntimeVersion$,legacy-cdn-domain',
+      });
     });
   });
 
@@ -678,7 +682,9 @@ describes.fakeWin('performance with experiment', {amp: true}, env => {
     sandbox.stub(perf, 'getHostname_', () => 'curls.cdn.ampproject.org');
     return perf.coreServicesAvailable().then(() => {
       perf.flush();
-      expect(viewerSendMessageStub).to.be.calledWith('sendCsi', undefined);
+      expect(viewerSendMessageStub).to.be.calledWith('sendCsi', {
+        ampexp: '$internalRuntimeVersion$',
+      });
     });
   });
 });
