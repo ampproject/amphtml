f89124d6eb9b2fefbeac4d5abc724ed9d0eef87c
MHambrick <mhambrick@cloud816.com>
2017-03-16 11:41:51 -0500

    Update amp-ngage-livechat.js

--- extensions/amp-ngage-livechat/0.1/amp-ngage-livechat.js
+++ extensions/amp-ngage-livechat/0.1/amp-ngage-livechat.js
@@ -17,8 +17,66 @@ import {CSS} from '../../../build/amp-ngage-livechat-0.1.css';
 
 class AmpNgageLiveChat extends AMP.BaseElement {
   /** @override */
-  buildCallback() {
-    this.element./*REVIEW*/innerHTML = '<input type="checkbox" id="ngageMobileInvite"><label for="ngageMobileInvite" class="ngageMobileFloat"><a class="ngageBoxClose" id="ngageBoxClose"></a><div class="ngageText"><div class="ngageMobilePicContainer" ><amp-img src="https://messenger.ngageics.com/Images/mobile.png" layout="responsive" alt="Welcome" height="60" width="60" id="ngageMobilePic"></div><div class="ngageHeader"><div class="ngageStrong">LIVE CHAT</div></div><div>Hi, we are here to help if you have questions.</div></div><A href="https://secure.ngagelive.com/chat/index.aspx?websiteid="' + this.element.getAttribute('id') + '" target="_blank"><div class="ngageMobileBar" ><div class="ngageStrong">YES</div><span>, start now!</span></div></A></label></input><a href="https://secure.ngagelive.com/chat/index.aspx?websiteid="' + this.element.getAttribute('id') + '"><div class="liveChatFloatingButtonMobile">LIVE CHAT</div></a>';
+  buildCallback() { 
+    var ja = document.createElement("input");
+    ja.id = "ngageMobileInvite";
+    ja.type = "checkbox";
+    this.element.appendChild(ja);
+    var x = document.createElement("label");
+    x.setAttribute("for","ngageMobileInvite");
+    x.className = "ngageMobileFloat"
+    this.element.appendChild(x);
+    var v = document.createElement("a");
+    v.className = "ngageBoxClose";
+    v.id = "ngageBoxClose"; 
+    x.appendChild(v);
+    var B = document.createElement("div");
+    B.className = "ngageText";
+    x.appendChild(B);
+    var A = document.createElement("div");
+    A.className = "ngageMobilePicContainer";
+    A.style.borderRadius = "0px";
+    A.style.border = "0px";
+    B.appendChild(A);
+    var z = document.createElement("amp-img");
+    z.setAttribute("height","60");
+    z.setAttribute("width","60");
+    z.setAttribute("layout","responsive");
+    z.setAttribute("alt","Welcome");
+    z.setAttribute("src","https://messenger.ngageics.com/Images/mobile.png");
+    z.id = "ngageMobilePic";
+    A.appendChild(z);
+    var y = document.createElement("div");
+    y.className = "ngageHeader";
+    var yh = document.createElement("div");
+    yh.className = "ngageStrong";
+    yh.textContent = "LIVE CHAT";
+    y.appendChild(yh);
+    B.appendChild(y);
+    var w = document.createElement("div");
+    w.textContent = "Hi, we are here to help if you have questions.";
+    B.appendChild(w);
+    var u = document.createElement("div");
+    u.className = "ngageMobileBar"; 
+    var ua = document.createElement("div");
+    ua.className = "ngageStrong";
+    ua.textContent = "YES";
+    var ub = document.createElement("span");
+    ub.textContent = ", start now!";
+    u.appendChild(ua);
+    u.appendChild(ub);
+    var uh = document.createElement("a");
+    uh.href = "https://secure.ngagelive.com/chat/index.aspx?websiteid=" + this.element.getAttribute('id');
+    uh.target = "_blank";
+    uh.appendChild(u);
+    x.appendChild(uh);  
+    var jc = document.createElement("a");
+	  jc.href = "https://secure.ngagelive.com/chat/index.aspx?websiteid=" + this.element.getAttribute('id');
+	  var jca = document.createElement("div");
+	  jca.className = "liveChatFloatingButtonMobile";
+  	jca.textContent = "LIVE CHAT";
+  	jc.appendChild(jca);
+  	this.element.appendChild(jc); 
   }
 }
 
