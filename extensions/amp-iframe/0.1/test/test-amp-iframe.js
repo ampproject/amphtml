9e044af63a256f58d0b938a850560c3a2d422e6a
William Chou <willchou@google.com>
2017-03-22 11:37:14 -0400

    amp-bind: Support amp-iframe (#8270)
    * initial commit for bindable iframes
    
    * unit test
    
    * integration test
    
    * revert validator change
    
    * pr comments
    
    * simplify beforeEach() and whitelist amp-iframe[src]

--- extensions/amp-iframe/0.1/test/test-amp-iframe.js
+++ extensions/amp-iframe/0.1/test/test-amp-iframe.js
@@ -645,4 +645,22 @@ describe('amp-iframe', () => {
       expect(newIntersection.height).to.equal(250);
     });
   });
+
+  it('should propagate `src` when container attribute is mutated', () => {
+    return getAmpIframe({
+      src: iframeSrc,
+      width: 100,
+      height: 100,
+    }).then(amp => {
+      const container = amp.container;
+      const impl = container.implementation_;
+      const iframe = amp.iframe;
+
+      const newSrc = 'https://foo.bar';
+      container.setAttribute('src', newSrc);
+      impl.mutatedAttributesCallback({src: newSrc});
+      expect(impl.iframeSrc).to.contain(newSrc);
+      expect(iframe.getAttribute('src')).to.contain(newSrc);
+    });
+  });
 });
