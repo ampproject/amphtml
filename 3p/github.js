dbbd784ac9b83084c3876c0d272a8f35fb0aac6b
Levi Durfee <levi.durfee@gmail.com>
2017-03-29 19:34:45 -0400

    encode URI parameters to be safe (#8489)

--- 3p/github.js
+++ 3p/github.js
@@ -17,7 +17,6 @@
 import {writeScript} from './3p';
 import {user} from '../src/log';
 
-
 /**
  * Get the correct script for the gist.
  *
@@ -44,11 +43,10 @@ export function github(global, data) {
     'The data-gistid attribute is required for <amp-gist> %s',
     data.element);
 
-  const gistid = data.gistid;
-  let gistUrl = 'https://gist.github.com/' + gistid + '.js';
+  let gistUrl = 'https://gist.github.com/' + encodeURIComponent(data.gistid) + '.js';
 
   if (data.file) {
-    gistUrl += '?file=' + data.file;
+    gistUrl += '?file=' + encodeURIComponent(data.file);
   }
 
   getGistJs(global, gistUrl, function() {
