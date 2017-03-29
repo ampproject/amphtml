c13858d970f03bedd2e3abad7351ba317c79d003
Justin Ridgewell <justin@ridgewell.name>
2017-03-29 14:04:30 -0400

    Use listen in listenOnce (#8456)
    * Use listen in listenOnce
    
    Fixes #7591.
    
    * lint
    
    * Add comments
    
    * Fix nullable type

--- src/event-helper.js
+++ src/event-helper.js
@@ -64,30 +64,16 @@ export function listen(element, eventType, listener, opt_capture) {
  * @return {!UnlistenDef}
  */
 export function listenOnce(element, eventType, listener, opt_capture) {
-  let localElement = element;
   let localListener = listener;
-  const capture = opt_capture || false;
-  let unlisten;
-  let proxy = event => {
+  const unlisten = internalListenImplementation(element, eventType, event => {
     try {
-      localListener(event);
-    } catch (e) {
-      // reportError is installed globally per window in the entry point.
-      self.reportError(e);
-      throw e;
+      localListener.call(this, event);
     } finally {
+      // Ensure listener is GC'd
+      localListener = null;
       unlisten();
     }
-  };
-  unlisten = () => {
-    if (localElement) {
-      localElement.removeEventListener(eventType, proxy, capture);
-    }
-    localElement = null;
-    proxy = null;
-    localListener = null;
-  };
-  localElement.addEventListener(eventType, proxy, capture);
+  }, opt_capture);
   return unlisten;
 }
 
