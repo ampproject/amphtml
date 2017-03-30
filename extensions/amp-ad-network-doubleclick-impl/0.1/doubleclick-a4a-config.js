911cebb8852607e412071a058c01c475bd2a8667
keithwrightbos <keithwrightbos@users.noreply.github.com>
2017-03-30 10:54:13 -0400

    carve-out doubleclick Fast Fetch if useSameDomainRenderingUntilDeprecated (#8503)

--- extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
@@ -78,6 +78,9 @@ export const BETA_ATTRIBUTE = 'data-use-beta-a4a-implementation';
  * @returns {boolean}
  */
 export function doubleclickIsA4AEnabled(win, element) {
+  if (element.hasAttribute('useSameDomainRenderingUntilDeprecated')) {
+    return false;
+  }
   const a4aRequested = element.hasAttribute(BETA_ATTRIBUTE);
   // Note: Under this logic, a4aRequested shortcuts googleAdsIsA4AEnabled and,
   // therefore, carves out of the experiment branches.  Any publisher using this
