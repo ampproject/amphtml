e5c3ddc03670f3f3c9e6426848148a309fbf84d4
Ed Hollinghurst <hello@edhollinghurst.com>
2017-04-03 00:44:07 +0100

    Implementation of AMP-Timeago component.

---
+++ extensions/amp-timeago/0.1/lib/timeago/locales/en.js
@@ -0,0 +1,18 @@
+export const en = function(number, index) {
+  return [
+    ['just now', 'right now'],
+    ['%s seconds ago', 'in %s seconds'],
+    ['1 minute ago', 'in 1 minute'],
+    ['%s minutes ago', 'in %s minutes'],
+    ['1 hour ago', 'in 1 hour'],
+    ['%s hours ago', 'in %s hours'],
+    ['1 day ago', 'in 1 day'],
+    ['%s days ago', 'in %s days'],
+    ['1 week ago', 'in 1 week'],
+    ['%s weeks ago', 'in %s weeks'],
+    ['1 month ago', 'in 1 month'],
+    ['%s months ago', 'in %s months'],
+    ['1 year ago', 'in 1 year'],
+    ['%s years ago', 'in %s years'],
+  ][index];
+};
---
+++ extensions/amp-timeago/0.1/test/test-amp-timeago.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import '../amp-timeago';
+
+describes.realWin('amp-timeago', {
+  amp: {
+    extensions: ['amp-timeago'],
+  },
+}, env => {
+
+  let win;
+  let element;
+
+  beforeEach(() => {
+    win = env.win;
+    element = win.document.createElement('amp-timeago');
+
+    const date = new Date();
+    date.setDate(date.getDate() - 2);
+    element.setAttribute('datetime', date.toISOString());
+    win.document.body.appendChild(element);
+  });
+
+  it('should display 2 days ago when built', () => {
+    element.build();
+    expect(element.textContent).to.equal('2 days ago');
+  });
+});
