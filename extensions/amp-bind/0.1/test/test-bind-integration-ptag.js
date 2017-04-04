c395452b74a2b0c5dd7741e67d66e65231865b2c
sreenidhipm <sreenidhimuralidharan@gmail.com>
2017-04-03 22:46:56 -0700

    Fixed indentation errors

--- extensions/amp-bind/0.1/test/test-bind-integration-ptag.js
+++ extensions/amp-bind/0.1/test/test-bind-integration-ptag.js
@@ -18,65 +18,64 @@ import {createFixtureIframe} from '../../../../testing/iframe';
 import {bindForDoc} from '../../../../src/bind';
 import {ampdocServiceFor} from '../../../../src/ampdoc';
 
-describe.configure().retryOnSaucelabs().run('amp-bind', function() {
-  const fixtureLocation = 'test/fixtures/amp-bind-integrations/p-tag.html';
+describe.configure().retryOnSaucelabs().run('amp-bind text integration',
+    function() {
+      const fixtureLocation = 'test/fixtures/p-tag.html';
 
-  let fixture;
-  let ampdoc;
+      let fixture;
+      let ampdoc;
 
-  this.timeout(5000);
+      this.timeout(5000);
 
-  beforeEach(() => {
-    return createFixtureIframe(fixtureLocation).then(f => {
-      fixture = f;
-      return waitForEvent('amp:bind:initialize');
-    }).then(() => {
-      const ampdocService = ampdocServiceFor(fixture.win);
-      ampdoc = ampdocService.getAmpDoc(fixture.doc);
-    });
-  });
+      beforeEach(() => {
+        return createFixtureIframe(fixtureLocation).then(f => {
+          fixture = f;
+          return waitForEvent('amp:bind:initialize');
+        }).then(() => {
+          const ampdocService = ampdocServiceFor(fixture.win);
+          ampdoc = ampdocService.getAmpDoc(fixture.doc);
+        });
+      });
 
  /**
   * @param {string} name
   * @return {!Promise}
   */
-  function waitForEvent(name) {
-    return new Promise(resolve => {
-      function callback() {
-        resolve();
-        fixture.win.removeEventListener(name, callback);
-      };
-      fixture.win.addEventListener(name, callback);
-    });
-  }
+      function waitForEvent(name) {
+        return new Promise(resolve => {
+          function callback() {
+            resolve();
+            fixture.win.removeEventListener(name, callback);
+          };
+          fixture.win.addEventListener(name, callback);
+        });
+      }
 
  /** @return {!Promise} */
-  function waitForBindApplication() {
+      function waitForBindApplication() {
    // Bind should be available, but need to wait for actions to resolve
    // service promise for bind and call setState.
-    return bindForDoc(ampdoc).then(unusedBind =>
+        return bindForDoc(ampdoc).then(unusedBind =>
        waitForEvent('amp:bind:setState'));
-  }
+      }
 
-  describe('text integration', () => {
-    it('should update text when text attribute binding changes', () => {
-      const textElement = fixture.doc.getElementById('textElement');
-      const button = fixture.doc.getElementById('changeTextButton');
-      expect(textElement.textContent).to.equal('unbound');
-      button.click();
-      return waitForBindApplication().then(() => {
-        expect(textElement.textContent).to.equal('hello world');
+      it('should update text when text attribute binding changes', () => {
+        const textElement = fixture.doc.getElementById('textElement');
+        const button = fixture.doc.getElementById('changeTextButton');
+        expect(textElement.textContent).to.equal('unbound');
+        button.click();
+        return waitForBindApplication().then(() => {
+          expect(textElement.textContent).to.equal('hello world');
+        });
+      });
+
+      it('should update CSS class when class binding changes', () => {
+        const textElement = fixture.doc.getElementById('textElement');
+        const button = fixture.doc.getElementById('changeTextClassButton');
+        expect(textElement.className).to.equal('original');
+        button.click();
+        return waitForBindApplication().then(() => {
+          expect(textElement.className).to.equal('new');
+        });
       });
     });
-
-    it('should update CSS class when class binding changes', () => {
-      const textElement = fixture.doc.getElementById('textElement');
-      const button = fixture.doc.getElementById('changeTextClassButton');
-      expect(textElement.className).to.equal('original');
-      button.click();
-      return waitForBindApplication().then(() => {
-        expect(textElement.className).to.equal('new');
-      });
-    });
-  });
-});
