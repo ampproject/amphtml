19032827250ff286bff1445653ea68704b12b0a4
Usman <muhamad.usman@widespace.com>
2017-04-04 16:41:11 +0200

    Widespace: adding out local amp code version to request url so we can tackle our amp code versions

--- ads/widespace.js
+++ ads/widespace.js
@@ -21,10 +21,14 @@ import {writeScript, validateData} from '../3p/3p';
  * @param {!Object} data
  */
 export function widespace(global, data) {
+
+  const WS_AMP_CODE_VER = '1.0.0';
+
   validateData(data, ['sid'], []);
 
-  const url = 'https://engine.widespace.com/map/engine/dynamic?isamp=1&sid='
-      + encodeURIComponent(data.sid);
+  const url = 'https://engine.widespace.com/map/engine/dynamic?isamp=1'
+      + '&ampver=' + WS_AMP_CODE_VER
+      + '&sid=' + encodeURIComponent(data.sid);
 
   writeScript(global, url);
 }
