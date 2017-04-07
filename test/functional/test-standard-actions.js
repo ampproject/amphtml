8e773d46b42bf8af9680ac34cb77aa809a15a4b3
Kevin Howald <kmh287@users.noreply.github.com>
2017-04-07 13:13:23 -0400

    Service Registration: History (#7885)

--- test/functional/test-standard-actions.js
+++ test/functional/test-standard-actions.js
@@ -16,7 +16,8 @@
 
 import {StandardActions} from '../../src/service/standard-actions-impl';
 import {AmpDocSingle} from '../../src/service/ampdoc-impl';
-import {bindForDoc} from '../../src/services';
+import {bindForDoc, historyForDoc} from '../../src/services';
+import {installHistoryServiceForDoc} from '../../src/service/history-impl';
 import {setParentWindow} from '../../src/service';
 
 
@@ -24,6 +25,7 @@ describes.sandboxed('StandardActions', {}, () => {
   let standardActions;
   let mutateElementStub;
   let deferMutateStub;
+  let ampdoc;
 
   function createElement() {
     return document.createElement('div');
@@ -70,7 +72,8 @@ describes.sandboxed('StandardActions', {}, () => {
   }
 
   beforeEach(() => {
-    standardActions = new StandardActions(new AmpDocSingle(window));
+    ampdoc = new AmpDocSingle(window);
+    standardActions = new StandardActions(ampdoc);
     mutateElementStub = stubMutate('mutateElement');
     deferMutateStub = stubMutate('deferMutate');
   });
@@ -150,7 +153,8 @@ describes.sandboxed('StandardActions', {}, () => {
 
   describe('"AMP" global target', () => {
     it('should implement goBack', () => {
-      const history = window.services.history.obj;
+      installHistoryServiceForDoc(ampdoc);
+      const history = historyForDoc(ampdoc);
       const goBackStub = sandbox.stub(history, 'goBack');
       standardActions.handleAmpTarget({method: 'goBack'});
       expect(goBackStub).to.be.calledOnce;
