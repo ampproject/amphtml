de4f5a4257356b75083dc1d674356a303360b6e9
Kevin Howald <kmh287@users.noreply.github.com>
2017-04-10 08:43:51 -0400

    Service Registration: Viewer (#7896)

--- test/functional/test-activity.js
+++ test/functional/test-activity.js
@@ -18,14 +18,13 @@ import {AmpDocSingle} from '../../src/service/ampdoc-impl';
 import {
   installActivityServiceForTesting,
 } from '../../extensions/amp-analytics/0.1/activity-impl';
-import {activityForDoc} from '../../src/services';
+import {activityForDoc, viewerForDoc, viewportForDoc} from '../../src/services';
 import {installPlatformService} from '../../src/service/platform-impl';
 import {installViewerServiceForDoc} from '../../src/service/viewer-impl';
 import {installTimerService} from '../../src/service/timer-impl';
 import {installViewportServiceForDoc} from '../../src/service/viewport-impl';
 import {markElementScheduledForTesting} from '../../src/custom-element';
 import {installVsyncService} from '../../src/service/vsync-impl';
-import {viewportForDoc} from '../../src/services';
 import {Observable} from '../../src/observable';
 import * as sinon from 'sinon';
 
@@ -101,7 +100,8 @@ describe('Activity getTotalEngagedTime', () => {
     installTimerService(fakeWin);
     installVsyncService(fakeWin);
     installPlatformService(fakeWin);
-    viewer = installViewerServiceForDoc(ampdoc);
+    installViewerServiceForDoc(ampdoc);
+    viewer = viewerForDoc(ampdoc);
 
     const whenFirstVisiblePromise = new Promise(resolve => {
       whenFirstVisibleResolve = resolve;
