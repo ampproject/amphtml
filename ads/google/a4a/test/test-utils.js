e7516f3f492c9670a693fb9c21eab9ef61c3c54c
Gene Levitzky <levitzky@google.com>
2017-04-12 10:24:18 -0400

    Lint fix.

--- ads/google/a4a/test/test-utils.js
+++ ads/google/a4a/test/test-utils.js
@@ -57,7 +57,7 @@ function noopMethods(impl, doc, sandbox) {
   sandbox.stub(impl, 'getAmpDoc', () => doc);
   sandbox.stub(impl, 'getIntersectionElementLayoutBox', () => {
     return {
-      top: 0, left: 0, right: 0, bottom: 0, width:0, height: 0
+      top: 0, left: 0, right: 0, bottom: 0, width: 0, height: 0,
     };
   });
 }
