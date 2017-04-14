2b67c6e2f5151e76872a530c80170976baf5c2f0
lannka <hungfei.ding@gmail.com>
2017-04-14 16:46:56 -0700

    fix linter

--- test/functional/test-amp-pixel.js
+++ test/functional/test-amp-pixel.js
@@ -18,7 +18,6 @@ import {
   installUrlReplacementsForEmbed,
 } from '../../src/service/url-replacements-impl';
 import {VariableSource} from '../../src/service/variable-source';
-import {createElementWithAttributes} from '../../src/dom';
 import {isReferrerPolicySupported} from '../../builtins/amp-pixel';
 
 describes.realWin('amp-pixel', {amp: true}, env => {
