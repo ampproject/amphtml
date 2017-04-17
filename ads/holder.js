5b458a085d19e6447c664a6a9da4ca54ba0e1590
Nando Sangenetto <nandosangenetto@gmail.com>
2017-04-17 13:48:57 -0300

    Condition more readable

--- ads/holder.js
+++ ads/holder.js
@@ -25,7 +25,7 @@ export function holder(global, data) {
   const wcl = global.context.location;
   const n = navigator.userAgent;
   let l = '&r' + Math.round((Math.random() * 10000000)) + '&h' + wcl.href;
-  if (!(n.includes('Safari') && !n.includes('Chrome'))) {
+  if (!n.includes('Safari') && n.includes('Chrome')) {
     l += '&c1';
   }
   data.queue = l;
