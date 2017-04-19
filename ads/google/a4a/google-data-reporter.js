65e75d993147a5f843f5835a53e0ecc898d57012
bradfrizzell <bradfrizzell@users.noreply.github.com>
2017-04-19 16:31:02 -0400

    Update google-data-reporter.js

--- ads/google/a4a/google-data-reporter.js
+++ ads/google/a4a/google-data-reporter.js
@@ -201,3 +201,4 @@ export function setGoogleLifecycleVarsFromHeaders(headers, reporter) {
   pingParameters[renderingMethodKey] = headers.get(renderingMethodHeader);
   reporter.setPingParameters(pingParameters);
 }
+
