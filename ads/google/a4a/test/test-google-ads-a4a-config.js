6ab87c147f77089b814994864ea7f1e086d2c2fe
jonkeller <jonkeller@google.com>
2017-04-19 16:54:33 -0400

    Linter fixes

--- ads/google/a4a/test/test-google-ads-a4a-config.js
+++ ads/google/a4a/test/test-google-ads-a4a-config.js
@@ -312,14 +312,16 @@ describe('a4a_config', () => {
       }
     });
 
-    it(`should force controlMeasureOnRender param from URL when pattern=${urlBase}`, () => {
+    it(`should force controlMeasureOnRender param from URL ` +
+       `when pattern=${urlBase}`, () => {
       win.location.search = urlBase.replace('PARAM', 'a4a:3');
       // Should not register as 'A4A enabled', but should still attach the
       // controlMeasureOnRender experiment ID.
       expect(googleAdsIsA4AEnabled(win, element, EXP_ID, EXTERNAL_BRANCHES,
           INTERNAL_BRANCHES), 'googleAdsIsA4AEnabled').to.be.false;
       expect(win.document.cookie).to.be.null;
-      expectThereCanBeOnlyOne(element, EXTERNAL_BRANCHES.controlMeasureOnRender);
+      expectThereCanBeOnlyOne(element,
+          EXTERNAL_BRANCHES.controlMeasureOnRender);
       expectExternallyTriggered(element);
     });
 
