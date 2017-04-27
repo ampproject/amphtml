94b597ff190aa4fc1e54be2008ead35e3b1f6410
Jose Antonio Cano <josea.cano@impresionesweb.com>
2017-04-27 16:05:46 +0200

    New ad network: Mobusi.

--- ads/mobusi.js
+++ ads/mobusi.js
@@ -14,44 +14,12 @@
  * limitations under the License.
  */
 
-import {writeScript, validateData} from '../3p/3p';
-import {addParamsToUrl} from '../src/url';
-
+import {writeScript} from '../3p/3p';
 
 export function mobusi(global, data) {
   const adsrc = data.src;
   const timestamp = ((new Date()).getTime() % 2147483648) + Math.random();
-  let url = '';
   if (typeof adsrc != 'undefined') {
     writeScript(global, adsrc + '&lgid=' + timestamp);
-  } else {
-    const options = ['a' , 'floa', 'idtm','mobdiv',
-      't' , 'dis_alg', 'popunders', 'direct',
-      'ip ', 'hotf', 'close', 'offer_key', 'idextra',
-      'offer_bundle','pubid'];
-    validateData(data, ['m','ads_server','mobformat'], options);
-    if (data.mobformat.toLowerCase() == 'banner') {
-      url = 'http://' + data.ads_server + '/banner_request.php?';
-    } else {
-      url = 'http://' + data.ads_server + '/inter_request.php?';
-    }
-    url = url + '&lgid=' + timestamp;
-    console.log(data);
-    data = removeItemFromArr(data,'ads_server');
-    console.log(data);
-    writeScript(global, addParamsToUrl(url ,data));
   }
-}
-//elimina un item de un arraglo
-function removeItemFromArr(arr, item) {
-  if (!arr) {
-    return;
-  }
-  if (Array.isArray(arr)) {
-    const index = arr.indexOf(item);
-    if (index > -1) {
-      arr = arr.splice(index, 1);
-    }
-  }
-  return (arr);
 }
