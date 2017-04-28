9ad0ed4db8d92204c731a9389084b378154951c1
Raghu Simha <rsimha@ampproject.org>
2017-04-28 18:46:40 -0400

    Flesh out the visual-diff gulp task.

--- build-system/tasks/visual-diff.js
+++ build-system/tasks/visual-diff.js
@@ -14,18 +14,146 @@
  * limitations under the License.
  */
 
-var gulp = require('gulp-help')(require('gulp'));
-var util = require('gulp-util');
+const argv = require('minimist')(process.argv.slice(2));
+const child_process = require('child_process');
+const gulp = require('gulp-help')(require('gulp'));
+const util = require('gulp-util');
 
+const percyCommand = 'percy snapshot';
+const defaultWidths = [750, 1080];  // iPhone: 750. Pixel: 1080.
+
+/**
+ * Executes the provided command; terminates this program in case of failure.
+ * Copied from pr-check.js.
+ * TODO(rsimha-amp): Refactor this into a shared library. Issue #9038.
+ *
+ * @param {string} cmd
+ */
+function execOrDie(cmd) {
+  const p =
+      child_process.spawnSync('/bin/sh', ['-c', cmd], {'stdio': 'inherit'});
+  if (p.status != 0) {
+    console.error(`\n${fileLogPrefix}exiting due to failing command: ${cmd}`);
+    process.exit(p.status)
+  }
+}
+
+/**
+ * Extracts Percy project keys from the environment.
+ *
+ * @return {!Object} Object containing Percy project and token.
+ */
+function extractPercyKeys() {
+  // Repo slug to which to upload snapshots. Same as the Github repo slug.
+  let percyProject = '';
+  if (process.env.PERCY_PROJECT) {
+    percyProject = process.env.PERCY_PROJECT;
+  } else {
+    util.log(util.colors.red(
+        'PERCY_PROJECT must be specified as an environment variable'));
+    done();
+  }
+  util.log('Percy project: ', util.colors.magenta(percyProject));
+
+  // Secret token for the percy project.
+  let percyToken = '';
+  if (process.env.PERCY_TOKEN) {
+    percyToken = process.env.PERCY_TOKEN;
+  } else {
+    util.log(util.colors.red(
+        'PERCY_TOKEN must be specified as an environment variable'));
+    done();
+  }
+  util.log('Percy token: ', util.colors.magenta(percyToken));
+  return {
+    percyProject: percyProject,
+    percyToken: percyToken
+  };
+}
+
+/**
+ * Extracts Percy args from the command line.
+ *
+ * @return {!Object} Object containing extracted args.
+ */
+function extractPercyArgs() {
+  // Webpage to snapshot. This is a path, relative to amphtml/.
+  let webpage = '';
+  if (argv.webpage) {
+    webpage = argv.webpage;
+  } else {
+    console./*OK*/error(util.colors.red(
+        'Must specify a webpage to diff via --webpage'));
+    process.exit(1);
+  }
+  util.log('Webpage: ', util.colors.magenta(webpage));
+
+  // Smartphone screen widths to snapshot.
+  let widths = defaultWidths;
+  if (argv.widths) {
+    widths = JSON.parse("[" + argv.widths + "]");
+  }
+  util.log('Widths: ', util.colors.magenta(widths.toString()));
+
+  // TODO(rsimha): Separate out some test pages into directories, and then
+  // add an arg to include the directory containing assets for those pages.
+
+  return {
+    webpage: webpage,
+    widths: widths
+  };
+}
+
+/**
+ * Constructs the Percy command line with various args.
+ *
+ * @param {!Object} percyKeys Object containing access keys for the Percy repo.
+ * @return {String} Full command line to be executed.
+ */
+function constructCommandLine(percyKeys) {
+  let commandLine = [];
+
+  // Percy project keys.
+  commandLine.push('PERCY_PROJECT=' + percyKeys.percyProject);
+  commandLine.push('PERCY_TOKEN=' + percyKeys.percyToken);
+
+  // Main snapshot command.
+  commandLine.push(percyCommand);
+
+  // Percy repo slug. This matches up exactly with the amphtml Github repo slug.
+  commandLine.push('--repo ' + percyKeys.percyProject);
+
+  // Percy args.
+  const percyArgs = extractPercyArgs();
+  commandLine.push('--baseurl /' + percyArgs.webpage);
+  commandLine.push('--widths ' + percyArgs.widths);
+
+  // The webpage being tested is typically the last arg.
+  commandLine.push(percyArgs.webpage);
+
+  util.log('Executing command line:');
+  commandLine.forEach((command) => {
+    util.log('\t', util.colors.cyan(command));
+  });
+  return commandLine.join(' ');
+}
 
 /**
  * Run visual diff tests
- *
- * @param {function} done callback
  */
-function visualDiff() {
+function runTests() {
   util.log(util.colors.yellow('Running visual diff tests...'));
+  const percyKeys = extractPercyKeys();
+  const commandLine = constructCommandLine(percyKeys);
+  execOrDie(commandLine);
 }
 
 
-gulp.task('visual-diff', 'Runs Visual diff tests.', visualDiff);
+gulp.task('visual-diff', 'Runs visual diff tests using Percy', runTests, {
+  options: {
+    'webpage': '  Path of the webpage being tested, relative to amphtml/.' +
+        ' Used this as the baseurl while looking up snapshots on Percy.',
+    'widths': '  CSV with the widths to test. Defaults to '
+        + defaultWidths.toString() + '.'
+  }
+});
