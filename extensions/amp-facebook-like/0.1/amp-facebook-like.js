dc62d89d367a864f112ab8c4d496ccf8efbb0bf2
Itamar Eduardo Gonçalves de Oliveira <3duardo.goncalves@gmail.com>
2017-04-28 15:25:02 -0400

    fix error Line 56 exceeds the maximum line length of 80  max-len

--- extensions/amp-facebook-like/0.1/amp-facebook-like.js
+++ extensions/amp-facebook-like/0.1/amp-facebook-like.js
@@ -53,7 +53,9 @@ class AmpFacebookLike extends AMP.BaseElement {
     this.applyFillContent(iframe);
     // Triggered by context.updateDimensions() inside the iframe.
     listenFor(iframe, 'embed-size', data => {
-      this.attemptChangeHeight(data.height).catch(() => { /* ignore failures */ });
+      this.attemptChangeHeight(data.height).catch(() => {
+        /* ignore failures */
+      });
     }, /* opt_is3P */true);
     this.element.appendChild(iframe);
     this.iframe_ = iframe;
