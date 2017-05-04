a3d3bf51fff6f2ca349f3eec24aaa7557cc7b89b
Edson A. Daquinta <edson@bidtellect.com>
2017-05-04 11:02:49 -0400

    bidtellect amp implementation…

---
+++ ads/bidtellect.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {writeScript, validateData} from '../3p/3p';
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ */
+export function bidtellect(global, data) {
+  validateData(data, ['t','pid'], ['sid','sname','pubid','pubname','renderid','bestrender','autoplay','playbutton','videotypeid','videocloseicon','targetid','bustframe']);
+  let params = '?t=' + encodeURIComponent(data.t) + '&pid=' + encodeURIComponent(data.pid);
+  if (data.width) {
+    params += '&w=' + encodeURIComponent(data.width);
+  }
+  if (data.height) {
+    params += '&h=' + encodeURIComponent(data.height);
+  }
+  if (data.sid) {
+    params += '&sid=' + encodeURIComponent(data.sid);
+  }
+  if (data.sname) {
+    params += '&sname=' + encodeURIComponent(data.sname);
+  }
+  if (data.pubid) {
+    params += '&pubid=' + encodeURIComponent(data.pubid);
+  }
+  if (data.pubname) {
+    params += '&pubname=' + encodeURIComponent(data.pubname);
+  }
+  if (data.renderid) {
+    params += '&renderid=' + encodeURIComponent(data.renderid);
+  }
+  if (data.bestrender) {
+    params += '&bestrender=' + encodeURIComponent(data.bestrender);
+  }
+  if (data.autoplay) {
+    params += '&autoplay=' + encodeURIComponent(data.autoplay);
+  }
+  if (data.playbutton) {
+    params += '&playbutton=' + encodeURIComponent(data.playbutton);
+  }
+  if (data.videotypeid) {
+    params += '&videotypeid=' + encodeURIComponent(data.videotypeid);
+  }
+  if (data.videocloseicon) {
+    params += '&videocloseicon=' + encodeURIComponent(data.videocloseicon);
+  }
+  if (data.targetid) {
+    params += '&targetid=' + encodeURIComponent(data.targetid);
+  }
+  if (data.bustframe) {
+    params += '&bustframe=' + encodeURIComponent(data.bustframe);
+  }
+  const url = 'https://cdn.bttrack.com/js/infeed/2.0/infeed.min.js' + params;
+  writeScript(global, url);
+}
---
+++ ads/bidtellect.md
@@ -0,0 +1,52 @@
+<!---
+Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# Bidtellect
+
+## Example
+
+```html
+<amp-ad width=320 height=200
+    type="bidtellect"
+    data-t="20bc0442-8vec-41f8-9892-04be6e6c3521"
+    data-pid="815676411">
+</amp-ad>
+```
+
+## Configuration
+
+Any questions, please [contact Bidtellect](mailto:technology@bidtellect.com).
+
+Required Parameters:
+
+- ```data-t```  - Parent publisher security token. 
+- ```data-pid```  - The unique identifier for your placement.
+
+
+Optional Parameters:
+
+- ```data-sid```  - Unique identifier for the site.
+- ```data-sname```  - Name of site that corresponds to the Site ID.
+- ```data-pubid```  - Unique identifier for the publisher.
+- ```data-pubname```  - Name of publisher that corresponds to the Publisher ID.
+- ```data-renderid```  - Unique identifier of the placement widget.
+- ```data-bestrender```  - Allow us to provide the best size and cropping for the placement.
+- ```data-autoplay```  - Enables autoplay for video placements.
+- ```data-playbutton```  - Onscreen play button for video placements.
+- ```data-videotypeid```  - Define how will be rendered the video player.
+- ```data-videocloseicon```  - Enable close button on the video player.
+- ```data-targetid```  - Allows the placement to render inside a target HTML element.
+- ```data-bustframe```  - Allows the placement to bust out of nested iframes recursively.
