e698379919ec12b29802e1427e5ead799e84cca4
zikas <ievgen@google.com>
2017-05-08 12:49:04 -0700

    missing test

--- extensions/amp-analytics/0.1/test/test-vendors.js
+++ extensions/amp-analytics/0.1/test/test-vendors.js
@@ -21,7 +21,7 @@ describe('analyaitcs vendors', () => {
       'expect the Client ID cookie name', () => {
     const gaConfig = ANALYTICS_CONFIG['googleanalytics'];
     expect(gaConfig).not.to
-        .deep.equal(ANALYTICS_CONFIG['alpha']);
+        .deep.equal(ANALYTICS_CONFIG['googleanalytics-alpha']);
 
     gaConfig.vars.clientId = 'CLIENT_ID(_ga)';
     expect(gaConfig).to
