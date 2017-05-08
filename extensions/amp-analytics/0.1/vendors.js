3d7d5b32853af72a64d1da26f522de5cf71c9916
zikas <ievgen@google.com>
2017-05-08 14:25:06 -0700

    updated googleanalytics-alpha config

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -679,7 +679,7 @@ export const ANALYTICS_CONFIG = /** @type {!JSONType} */ ({
       'st=${socialTarget}' +
       '${baseSuffix}',
       'timing': '${host}/collect?${basePrefix}&' +
-      't=timing&' +
+      't=${timingRequestType}&' +
       'jid=&' +
       'plt=${pageLoadTime}&' +
       'dns=${domainLookupTime}&' +
@@ -698,6 +698,17 @@ export const ANALYTICS_CONFIG = /** @type {!JSONType} */ ({
         'sampleSpec': {
           'sampleOn': '${clientId}',
           'threshold': 1,
+        },
+        'vars': {
+          'timingRequestType': 'timing',
+        },
+      },
+      'adwordsTiming': {
+        'on': 'visible',
+        'request': 'timing',
+        'enabled': '${queryParam(gclid)}',
+        'vars': {
+          'timingRequestType': 'adtiming',
         },
       },
     },
