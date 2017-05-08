99b81be88e07fafcc1e8b87b22a5f03442fff096
Dima Voytenko <dvoytenko@google.com>
2017-05-08 15:54:56 -0700

    Additional protection for destroyed iframes

--- src/custom-element.js
+++ src/custom-element.js
@@ -1251,7 +1251,10 @@ function createBaseCustomElementClass(win) {
           // Set a minimum delay in case the element loads very fast or if it
           // leaves the viewport.
           timerFor(this.ownerDocument.defaultView).delay(() => {
-            if (this.isInViewport_) {
+            // TODO(dvoytenko, #9177): cleanup `this.ownerDocument.defaultView`
+            // once investigation is complete. It appears that we get a lot of
+            // errors here once the iframe is destroyed due to timer.
+            if (this.isInViewport_ && this.ownerDocument.defaultView) {
               this.toggleLoading_(true);
             }
           }, 100);
