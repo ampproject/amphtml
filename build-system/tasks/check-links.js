811b2aa5940f093f2582070dd06b6fdbf9600524
Raghu Simha <rsimha@ampproject.org>
2017-05-09 11:37:48 -0400

    Change link checker to warning-only (#9219)

--- build-system/tasks/check-links.js
+++ build-system/tasks/check-links.js
@@ -60,21 +60,25 @@ function checkLinks() {
         }
       });
       if(deadLinksFoundInFile) {
+        filesWithDeadLinks.push(markdownFiles[index]);
         util.log(
-            util.colors.red('ERROR'), 'Dead links found in',
-            util.colors.magenta(markdownFiles[index]), '(please update it).');
-            filesWithDeadLinks.push(markdownFiles[index]);
+            util.colors.yellow('WARNING'),
+            'Possible dead link(s) found in',
+            util.colors.magenta(markdownFiles[index]),
+            '(please update if necessary).');
       } else {
         util.log(
-            util.colors.green('SUCCESS'), 'All links in',
+            util.colors.green('SUCCESS'),
+            'All links in',
             util.colors.magenta(markdownFiles[index]), 'are alive.');
       }
     });
     if (deadLinksFound) {
         util.log(
-            util.colors.red('ERROR'), 'Dead links found. Please update',
-            util.colors.magenta(filesWithDeadLinks.join(',')));
-       process.exit(1);
+            util.colors.yellow('WARNING'),
+            'Possible dead link(s) found. Please update',
+            util.colors.magenta(filesWithDeadLinks.join(',')),
+            'if necessary.');
     } else {
         util.log(
             util.colors.green('SUCCESS'),
