ed40301b52ab73a5aa195b457d1aadb05e109acd
ricardoe <mxcoder@users.noreply.github.com>
2017-05-12 13:01:17 -0500

    GumGum Ads integration (#9109)
    * Initial GumGum integration
    
    * Changes requested by reviewer

--- ads/_config.js
+++ ads/_config.js
@@ -292,6 +292,11 @@ export const adConfig = {
     prefetch: 'https://cdn.gmossp-sp.jp/ads/amp.js',
   },
 
+  gumgum: {
+    prefetch: 'https://g2.gumgum.com/javascripts/ad.js',
+    renderStartImplemented: true,
+  },
+
   holder: {
     prefetch: 'https://i.holder.com.ua/js2/holder/ajax/ampv1.js',
     preconnect: 'https://h.holder.com.ua',
