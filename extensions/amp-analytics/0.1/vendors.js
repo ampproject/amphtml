581c5ba17c36e50e72598f068c10ec19dcdd79db
wangpei07 <wangpei07@baidu.com>
2017-05-16 13:23:15 +0800

    change doubleuote to singlequote

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -80,26 +80,26 @@ export const ANALYTICS_CONFIG = /** @type {!JSONType} */ ({
   },
 
   'baiduanalytics': {
-    "requests": {
-      "host": "https://hm.baidu.com/hm.gif?",
-      "base": "${host}si=${token}&nv=0&st=4&v=pixel-1.0&rnd=${timestamp}",
-      "pageview": "${base}&et=0",
-      "trackEvent": "${base}&ep=${CATEGORY}*${ACTION}*${LABEL}*${VALUE}&et=4&api=8_0"
+    'requests': {
+      'host': 'https://hm.baidu.com/hm.gif?',
+      'base': '${host}si=${token}&nv=0&st=4&v=pixel-1.0&rnd=${timestamp}',
+      'pageview': '${base}&et=0',
+      'trackEvent': '${base}&ep=${CATEGORY}*${ACTION}*${LABEL}*${VALUE}&et=4&api=8_0'
     },
-    "triggers": {
-      "pageview": {
-        "on": "visible",
-        "request": "base"
+    'triggers': {
+      'pageview': {
+        'on': 'visible',
+        'request': 'base'
       },
-      "trackAnchorClick": {
-        "on": "click",
-        "request": "trackEvent"
+      'trackAnchorClick': {
+        'on': 'click',
+        'request': 'trackEvent'
       }
     },
-    "transport": {
-      "beacon": false,
-      "xhrpost": false,
-      "image": true
+    'transport': {
+      'beacon': false,
+      'xhrpost': false,
+      'image': true
     }
   },
 
