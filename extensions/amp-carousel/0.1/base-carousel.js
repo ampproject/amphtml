01eced4515064a5032f2b00411ce42a3d5872dba
Alanna Walton <alwalton@google.com>
2017-05-18 17:49:47 -0700

    Optional aria-label
    closes Issue #422

--- extensions/amp-carousel/0.1/base-carousel.js
+++ extensions/amp-carousel/0.1/base-carousel.js
@@ -71,7 +71,11 @@ export class BaseCarousel extends AMP.BaseElement {
     this.prevButton_.setAttribute('role', 'button');
     // TODO(erwinm): Does label need i18n support in the future? or provide
     // a way to be overridden.
-    this.prevButton_.setAttribute('aria-label', 'Previous item in carousel');
+    if (this.element.hasAttribute('data-previous-button-aria-label')) {
+        this.prevButton_.setAttribute('aria-label', this.element.getAttribute('data-previous-button-aria-label'));
+    } else {
+        this.prevButton_.setAttribute('aria-label', 'Previous item in carousel');
+    }
     this.prevButton_.setAttribute('tabindex', 0);
     this.prevButton_.onkeydown = event => {
       if (event.keyCode == Keycodes.ENTER || event.keyCode == Keycodes.SPACE) {
@@ -90,7 +94,11 @@ export class BaseCarousel extends AMP.BaseElement {
     this.nextButton_.classList.add('amp-carousel-button');
     this.nextButton_.classList.add('amp-carousel-button-next');
     this.nextButton_.setAttribute('role', 'button');
-    this.nextButton_.setAttribute('aria-label', 'Next item in carousel');
+    if (this.element.hasAttribute('data-next-button-aria-label')) {
+        this.nextButton_.setAttribute('aria-label', this.element.getAttribute('data-next-button-aria-label'));
+    } else {
+        this.nextButton_.setAttribute('aria-label', 'Previous item in carousel');
+    }
     this.nextButton_.setAttribute('tabindex', 0);
     this.nextButton_.onkeydown = event => {
       if (event.keyCode == Keycodes.ENTER || event.keyCode == Keycodes.SPACE) {
