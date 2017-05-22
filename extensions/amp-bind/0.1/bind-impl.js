cb91907844d01282b8de1f419c82fd0c36ff7f65
Kevin Howald <kevinhowald@google.com>
2017-05-22 16:01:37 -0400

    or local dev

--- extensions/amp-bind/0.1/bind-impl.js
+++ extensions/amp-bind/0.1/bind-impl.js
@@ -147,7 +147,7 @@ export class Bind {
     this.setStatePromise_ = null;
 
     // Expose for testing on dev.
-    if (getMode().development) {
+    if (getMode().development || getMode().localDev) {
       AMP.printState = this.printState_.bind(this);
     }
   }
