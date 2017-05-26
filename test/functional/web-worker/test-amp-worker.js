d046935ecd74bd6fd15c8cd1f76008eab6b71a59
Justin Ridgewell <justin@ridgewell.name>
2017-05-26 17:39:57 -0400

    XHR#fetchText now returns Response (#9567)
    * XHR#fetchText now returns Response
    
    Partial for #9448.
    
    * Lint
    
    * Fix test error

--- test/functional/web-worker/test-amp-worker.js
+++ test/functional/web-worker/test-amp-worker.js
@@ -48,7 +48,11 @@ describe('invokeWebWorker', () => {
 
     // Stub xhr.fetchText() to return a resolved promise.
     installXhrService(fakeWin);
-    sandbox.stub(xhrFor(fakeWin), 'fetchText', () => Promise.resolve());
+    sandbox.stub(xhrFor(fakeWin), 'fetchText', () => Promise.resolve({
+      text() {
+        return Promise.resolve();
+      },
+    }));
 
     const ampWorker = ampWorkerForTesting(fakeWin);
     workerReadyPromise = ampWorker.fetchPromiseForTesting();
