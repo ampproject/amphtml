ef9ecb0526a0f7e1aa9880a1f88a350888fabff3
zhouyx <zhouyx@google.com>
2017-05-26 10:08:17 -0700

    Do not build deprecated version, copy script at last

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ var hostname3p = argv.hostname3p || '3p.ampproject.net';
 
 // All declared extensions.
 var extensions = {};
+var aliasExtensionsCopyPath = {};
 
 // Each extension and version must be listed individually here.
 // NOTE: No new extensions must pass the NO_TYPE_CHECK argument.
@@ -111,7 +112,6 @@ declareExtension('amp-share-tracking', '0.1', false);
 declareExtension('amp-sidebar', '0.1', true);
 declareExtension('amp-soundcloud', '0.1', false);
 declareExtension('amp-springboard-player', '0.1', false);
-declareExtension('amp-sticky-ad', '0.1', true);
 declareExtension('amp-sticky-ad', '1.0', true);
 declareExtension('amp-selector', '0.1', true);
 
@@ -137,7 +137,8 @@ declareExtension('amp-viewer-integration', '0.1', {
 });
 declareExtension('amp-video', '0.1', false);
 declareExtension('amp-youtube', '0.1', false);
-
+declareAliasExtensionVersion(
+    'amp-sticky-ad', '0.1', /* lastestVersion */ '1.0', /* hasCss */ true);
 /**
  * @param {string} name
  * @param {string} version E.g. 0.1
@@ -163,6 +164,21 @@ function declareExtension(name, version, hasCssOrOptions, opt_noTypeCheck,
     noTypeCheck: !!opt_noTypeCheck,
     extraGlobs: opt_extraGlobs,
   }, options);
+}
+
+/**
+ * @param {string} name
+ * @param {string} version E.g. 0.1
+ * @param {string} lastestVersion
+ * @param {boolean} hasCss
+ */
+function declareAliasExtensionVersion(name, version, lastestVersion, hasCss) {
+  aliasExtensionsCopyPath[name + '-' + version + '.js'] =
+      name + '-' + lastestVersion + '.js';
+  if (hasCss) {
+    aliasExtensionsCopyPath[name + '-' + version + '.css'] =
+      name + '-' + lastestVersion + '.css';
+  }
 }
 
 /**
@@ -356,6 +372,15 @@ function copyCss() {
 }
 
 /**
+ * Copy built extension to alias extension
+ */
+function copyAliasExtensions() {
+  for (var key in aliasExtensionsCopyPath) {
+    fs.copySync('dist/v0/' + aliasExtensionsCopyPath[key], 'dist/v0/' + key);
+  }
+}
+
+/**
  * Enables watching for file changes in css, extensions.
  * @return {!Promise}
  */
@@ -398,15 +423,6 @@ function buildExtension(name, version, hasCss, options, opt_extraGlobs) {
   options = options || {};
   options.extraGlobs = opt_extraGlobs;
   var path = 'extensions/' + name + '/' + version;
-  if (name == 'amp-sticky-ad' && version == '0.1') {
-    // Special case for amp-sticky-ad force upgrade from v0.1 to v1.0
-    // to provide better UX. (related issue #6169).
-    // To deprecate 0.1, replace the build path so that amp-sticky-ad-0.1.js
-    // is built from extensions/amp-sticky-ad/1.0/amp-sticky-ad.js
-    // NOTE: The upgrade happens here to provide backward compatibility
-    // to existing pages with amp-sticky-ad 0.1 script.
-    path = 'extensions/' + name + '/1.0';
-  }
   var jsPath = path + '/' + name + '.js';
   var jsTestPath = path + '/test/' + 'test-' + name + '.js';
   if (argv.files && options.bundleOnlyIfListedInFiles) {
@@ -539,7 +555,9 @@ function dist() {
     buildExperiments({minify: true, watch: false, preventRemoveAndMakeDir: true}),
     buildLoginDone({minify: true, watch: false, preventRemoveAndMakeDir: true}),
     copyCss(),
-  ]);
+  ]).then(() => {
+    copyAliasExtensions();
+  });
 }
 
 /**
