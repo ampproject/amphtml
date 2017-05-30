e42f305d973b421fc713ab83ba75529fd68a6574
Kevin Howald <kmh287@users.noreply.github.com>
2017-05-30 08:22:33 -0400

    amp-bind: Handle attributes on input elements that sometimes only specify initial values (#9584)

--- test/integration/test-amp-bind.js
+++ test/integration/test-amp-bind.js
@@ -23,9 +23,13 @@ describe.configure().retryOnSaucelabs().run('amp-bind', function() {
   let fixture;
   let ampdoc;
   let sandbox;
+  let numSetStates;
+  let numMutated;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
+    numSetStates = 0;
+    numMutated = 0;
   });
 
   afterEach(() => {
@@ -51,13 +55,13 @@ describe.configure().retryOnSaucelabs().run('amp-bind', function() {
     // Bind should be available, but need to wait for actions to resolve
     // service promise for bind and call setState.
     return bindForDoc(ampdoc).then(unusedBind =>
-        fixture.awaitEvent('amp:bind:setState', 1));
+        fixture.awaitEvent('amp:bind:setState', ++numSetStates));
   }
 
   /** @return {!Promise} */
   function waitForAllMutations() {
     return bindForDoc(ampdoc).then(unusedBind =>
-        fixture.awaitEvent('amp:bind:mutated', 1));
+        fixture.awaitEvent('amp:bind:mutated', ++numMutated));
   }
 
   describe('[text] and [class] integration', () => {
@@ -158,6 +162,30 @@ describe.configure().retryOnSaucelabs().run('amp-bind', function() {
       checkbox.click();
       return waitForBindApplication().then(() => {
         expect(checkboxText.textContent).to.equal('Checked: true');
+      });
+    });
+
+    it('should update checkbox checked attr when its binding changes', () => {
+      // Does *NOT* have the `checked` attribute.
+      const checkbox = fixture.doc.getElementById('checkedBound');
+      const button = fixture.doc.getElementById('toggleCheckedButton');
+      // Some attributes on certain input elements, such as `checked` on
+      // checkbox, only specify an initial value. Clicking the checkbox
+      // ensures the element is no longer relying on `value` as
+      // an initial value.
+      checkbox.click();
+      expect(checkbox.hasAttribute('checked')).to.be.false;
+      expect(checkbox.checked).to.be.true;
+      button.click();
+      return waitForBindApplication().then(() => {
+        expect(checkbox.hasAttribute('checked')).to.be.false;
+        expect(checkbox.checked).to.be.false;
+        button.click();
+        return waitForBindApplication();
+      }).then(() => {
+        // When Bind checks the box back to true, it adds the checked attr.
+        expect(checkbox.hasAttribute('checked')).to.be.true;
+        expect(checkbox.checked).to.be.true;
       });
     });
 
