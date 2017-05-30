b8a5cc555c04462970ed50af682f68e444c24083
Justin Ridgewell <justin@ridgewell.name>
2017-05-30 17:13:25 -0400

    Test on Travis using Node v4 (#9575)
    * Test on Travis using Node v4
    
    * Use strict
    
    * Fix loop

--- build-system/tasks/clean.js
+++ build-system/tasks/clean.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+'use strict';
 
 var del = require('del');
 var gulp = require('gulp-help')(require('gulp'));
--- build-system/tasks/compile-css-expr.js
+++ build-system/tasks/compile-css-expr.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+'use strict';
 
 var jison = require('jison');
 var gulp = require('gulp');
