fbcc7887de01f518886f04b2766bbc559798cc03
glevitzky <levitzky@google.com>
2017-06-02 10:14:54 -0400

    Adds SFG experiment branches to doubleclick-a4a-config.js. (#9662)
    * Added experiment branches corresponding to exp=a4a:(5|6).
    
    * Removed test file.
    
    * Removed changes to yarn.lock + minor fixes.
    
    * Fixed adsense-a4a-config.js.
    
    * Fixed return statement.

--- extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
@@ -96,6 +96,14 @@ export const DOUBLECLICK_A4A_BETA_BRANCHES = {
   experiment: '2077831',
 };
 
+/**
+ * @const {!../../../ads/google/a4a/traffic-experiments.A4aExperimentBranches}
+ */
+export const DOUBLECLICK_SFG_INTERNAL_EXPERIMENT_BRANCHES = {
+  control: '21060540',
+  experiment: '21060541',
+};
+
 export const BETA_ATTRIBUTE = 'data-use-beta-a4a-implementation';
 
 /**
@@ -125,7 +133,8 @@ export function doubleclickIsA4AEnabled(win, element) {
   const enableA4A = googleAdsIsA4AEnabled(
           win, element, DOUBLECLICK_A4A_EXPERIMENT_NAME,
           externalBranches, internalBranches,
-          DOUBLECLICK_A4A_EXTERNAL_DELAYED_EXPERIMENT_BRANCHES_PRE_LAUNCH) ||
+          DOUBLECLICK_A4A_EXTERNAL_DELAYED_EXPERIMENT_BRANCHES_PRE_LAUNCH,
+          DOUBLECLICK_SFG_INTERNAL_EXPERIMENT_BRANCHES) ||
       (a4aRequested && (isProxyOrigin(win.location) ||
        getMode(win).localDev || getMode(win).test));
   if (enableA4A && a4aRequested && !isInManualExperiment(element)) {
