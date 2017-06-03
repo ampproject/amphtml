dc5763a24a7541190ffe7910b21bafb61239c371
Curt Lawrence <clawr@users.noreply.github.com>
2017-06-02 20:39:03 -0700

    Implementation of amp-ad-exit (#9390)
    * Implementation of the amp-ad-exit component for managing ad navigation (#8515).
    
    Changes from the I2I:
    - Only RANDOM, CLICK_X, and CLICK_Y variables can be used, alongside custom vars.
    - It doesn't work well with <a> tags - the tap action doesn't trigger on middle clicks. Recommendation is to use other elements.
    - ClickLocationFilter for border click protection is not yet implemented. It will be added in a future change.
    
    The component does some lightweight validation of the JSON config when it's
    built. This may be overkill.
    
    Tested:
    gulp test --files 'extensions/amp-ad-exit/0.1/test/*'
    gulp check-types
    
    * Update JSDoc for some functions. Fix lint issue (ignore unused parameters in an interface method).
    
    * sinon.spy -> sandbox.spy
    
    * Add amp-ad-exit to the list of allowed extensions in A4A pages and fix validation rules.
    
    Address PR: add experiment flag; use camelCase for config properties
    
    address some comments
    
    Instantiate a new Filter for each spec.
    
    * Instantiate a new Filter for each spec.
    
    * remove duplicate doctype tag in example
    
    * Make ActionEventDef public for type annotations.
    
    * Add filter factory.
    
    * fix event var
    
    * update type annotation for Filter.filter
    
    * address PR comments
    
    * Move filter config validation to the ctor. Remove Filter.buildCallback().
    
    * camelCase in documentation examples

---
+++ extensions/amp-ad-exit/0.1/filters/filter.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @enum {string} */
+export const FilterType = {
+  CLICK_DELAY: 'clickDelay',
+  CLICK_LOCATION: 'clickLocation',
+};
+
+export class Filter {
+  /** @param {string} name The name given to this filter. */
+  constructor(name) {
+    /** @const {string} */
+    this.name = name;
+  }
+
+  /**
+   * @param {!../../../../src/service/action-impl.ActionEventDef} event
+   * @return {boolean} Whether the exit is allowed.
+   */
+  /* eslint-disable no-unused-vars */
+  filter(event) {}
+}
