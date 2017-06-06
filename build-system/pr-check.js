6bdb9fc2707b7d3a5ecba21004c5e251ea10be83
Raghu Simha <rsimha@ampproject.org>
2017-06-06 14:43:23 -0400

    Fix 'gulp css' and remove the now redundant 'gulp build --css-only' (#9741)
    This PR does the following:
    
    - Makes sure that gulp css also builds CSS within extensions
    - Removes the now redundant --css-only flag to gulp build, now that there's no difference between gulp css and gulp build --css-only
    - Updates gulp dist to not need any prerequisite css build steps. You can now do gulp clean followed by gulp dist
    - Updates gulp watch to not need any prerequisite css build steps, and fixes it so it's not missing any dependencies
    
    Fixes #9640

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -216,9 +216,6 @@ const command = {
   runLintChecks: function() {
     timedExecOrDie(`${gulp} lint`);
   },
-  buildRuntimeCssOnly: function() {
-    timedExecOrDie(`${gulp} build --css-only`);
-  },
   buildRuntime: function() {
     timedExecOrDie(`${gulp} build`);
   },
@@ -275,7 +272,6 @@ function runAllCommands() {
   }
   if (process.env.BUILD_SHARD == "integration_tests") {
     command.cleanBuild();
-    command.buildRuntimeCssOnly();
     command.buildRuntimeMinified();
     command.runPresubmitTests();  // Needs runtime to be built and served.
     command.runVisualDiffTests();  // Only called during push builds.
