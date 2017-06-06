faad371b15b15cc4aa94999db8d850bab77bb065
jonkeller <jonkeller@google.com>
2017-06-06 18:11:31 -0400

    Adds file that enum and response container were extracted to

---
+++ src/3p-analytics-common.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @private @const {Object<string,string>} */
+export const MessageTypes = {
+  ampAnalytics3pReady: 'ampAnalytics3pExtraData',
+  ampAnalytics3pExtraData: 'ampAnalytics3pExtraData',
+  ampAnalytics3pMessages: 'ampAnalytics3pMessages',
+  /* ampAnalytics3pEvent may only be contained within ampAnalytics3pMessages */
+  ampAnalytics3pEvent: 'ampAnalytics3pEvent',
+  ampAnalytics3pResponse: 'ampAnalytics3pResponse',
+};
+
+/**
+ * A class for holding AMP Analytics third-party vendors responses to frames.
+ */
+export class ResponseMap {
+  /**
+   * Gets the backing data structure
+   * TODO: Is there a better place in the runtime object hierarchy to hang this?
+   * @returns {Object}
+   * @private
+   */
+  static getMap_() {
+    AMP.responseMap_ = AMP.responseMap_ || {};
+    return AMP.responseMap_;
+  }
+
+  /**
+   * Add a response
+   * @param {!string} frameType The identifier for the third-party frame that
+   * responded
+   * @param {!string} creativeUrl The URL of the creative being responded to
+   * @param {Object} response What the response was
+   */
+  static add(frameType, creativeUrl, response) {
+    if (!ResponseMap.getMap_()[frameType]) {
+      ResponseMap.getMap_()[frameType] = {};
+    }
+    ResponseMap.getMap_()[frameType][creativeUrl] = response;
+  }
+
+  /**
+   * Remove a response, for instance if a third-party frame is being destroyed
+   * @param {!string} frameType The identifier for the third-party frame
+   * whose responses are to be removed
+   */
+  static remove(frameType) {
+    delete ResponseMap.getMap_()[frameType];
+  }
+
+  /**
+   * Gets the most recent response given by a certain frame to a certain
+   * creative
+   * @param {!string} frameType The identifier for the third-party frame
+   * whose response is sought
+   * @param {!string} creativeUrl The URL of the creative that the sought
+   * response was about
+   * @returns {?Object}
+   */
+  static get(frameType, creativeUrl) {
+    if (ResponseMap.getMap_()[frameType] &&
+      ResponseMap.getMap_()[frameType][creativeUrl]) {
+      return ResponseMap.getMap_()[frameType][creativeUrl];
+    }
+    return {};
+  }
+}
