781596bb44b0820842b973f73a6f828823e9b13e
Anton Cataldi <antoncataldi@gmail.com>
2017-06-07 12:04:16 -0400

    add properties to Amp Events enum

--- src/amp-events.js
+++ src/amp-events.js
@@ -1,1 +1,33 @@
-export const AmpEvents = {};
+/*
+* Enum used to specify custom Amp Events
+*/
+export const AmpEvents = {
+  VISIBILITY_CHANGE: 'amp:visibilitychange',
+  BIND: {
+    INITIALIZE: 'amp:bind:initialize',
+    RESCAN_TEMPLATE: 'amp:bind:rescan-template',
+    SET_STATE: 'amp:bind:setState',
+  },
+  TEMPLATE_RENDERED: 'amp:template-rendered',
+  DOM_UPDATE: 'amp:dom-update',
+  BUILT: 'amp:built',
+  ATTACHED: 'amp:attached',
+  STUBBED: 'amp:stubbed',
+  LOAD: {
+    START: 'amp:load:start',
+    END: 'amp:load:end',
+  },
+  ERROR: 'amp:error',
+};
+// 'amp:visibilitychange'
+// 'amp:bind:setState'
+// 'amp:template-rendered'
+// 'amp:bind:initialize'
+// 'amp:bind:rescan-template'
+// 'amp:dom-update'
+// 'amp:built'
+// 'amp:attached'
+// 'amp:stubbed'
+// 'amp:load:start'
+// 'amp:load:end'
+// 'amp:error'
