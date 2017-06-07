1276e22b2bd7b88f8e71c31964826acfc264361e
Anton Cataldi <antoncataldi@gmail.com>
2017-06-07 17:26:21 -0400

    fix: remove unused AmpEvents import in amp bind test file

--- test/integration/test-amp-bind.js
+++ test/integration/test-amp-bind.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {AmpEvents} from '../../src/amp-events';
 import {BindEvents} from '../../extensions/amp-bind/0.1/bind-events';
 import {createFixtureIframe} from '../../testing/iframe';
 import {batchedXhrFor} from '../../src/services';
