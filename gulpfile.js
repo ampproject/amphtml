394c020d62dae241c21452e1e3a967bd722e4300
Aaron Turner <aaron@aaronthedev.com>
2017-06-08 11:57:31 -0700

    Fixed the gulpfile typo

--- gulpfile.js
+++ gulpfile.js
@@ -171,7 +171,7 @@ function declareExtension(name, version, hasCssOrOptions, opt_noTypeCheck,
  * This function is used for declaring deprecated extensions. It simply places the current
  * version code in place of the latest versions.
  * This has the ability to break an extension verison, so please be sure that this is
- * the correct to use.
+ * the correct one to use.
  * @param {string} name
  * @param {string} version E.g. 0.1
  * @param {string} lastestVersion
