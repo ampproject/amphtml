772e0292468d95591846b34483d7d504db4ac3d4
keithwrightbos <keithwrightbos@users.noreply.github.com>
2017-06-08 12:37:53 -0400

    Add SRA support for Doubleclick Fast Fetch Traffic (#9563)
    * Initial implementation of Doubleclick SRA
    
    * continued development
    
    * modify url building to pass parameters as object
    
    * fix type error
    
    * fix type errors
    
    * reset SraRequests global in unlayoutCallback
    
    * test fixes
    
    * line-delimited-response-handler test coverage
    
    * remove console log
    
    * PR feedback
    
    * PR feedback
    
    * PR review
    
    * PR review, test fixes
    
    * small refactor, test fix
    
    * make functions private
    
    * PR feedback
    
    * PR Review and additional test coverage
    
    * fix gulp presubmit failures
    
    * Test fixes/additions
    
    * slight tweak to how tests are called
    
    * PR feedback
    
    * lint fix
    
    * update sfv parameter
    
    * add still current check after xhr response; add comments
    
    * fix issue with error logging
    
    * Additional test coverage
    
    * remove only from test runner

---
+++ ads/google/a4a/line-delimited-response-handler.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ import {tryParseJson} from '../../../src/json';
+
+ /**
+  * Handles an XHR response by calling lineCallback for each line delineation.
+  * Uses streaming where possible otherwise falls back to text.
+  * @param {!Window} win
+  * @param {!../../../src/service/xhr-impl.FetchResponse} response
+  * @param {!function(string, boolean)} lineCallback
+  * @private
+  */
+ export function lineDelimitedStreamer(win, response, lineCallback) {
+   let line = '';
+   function streamer(text, done) {
+     const regex = /([^\n]*)(\n)?/g;
+     let match;
+     while ((match = regex.exec(text))) {
+       line += match[1];
+       if (match[2]) {
+         lineCallback(line, done && regex.lastIndex === text.length);
+         line = '';
+       }
+       if (regex.lastIndex === text.length) {
+         break;
+       }
+     }
+   }
+   if (!response.body || !win.TextDecoder) {
+     response.text().then(text => streamer(text, true));
+     return;
+   }
+
+   const decoder = new TextDecoder('utf-8');
+   const reader = response.body.getReader();
+   reader.read().then(function chunk(result) {
+     if (result.value) {
+       streamer(
+         decoder.decode(
+           /** @type {!ArrayBuffer} */(result.value), {'stream': true}),
+         result.done);
+     }
+     if (!result.done) {
+       // More chunks to read.
+       reader.read().then(chunk);
+     }
+   });
+ }
+
+ /**
+  * Given each line, groups such that the first is JSON parsed and second
+  * html unescaped.
+  * @param {!function(string, !Object<string, *>, boolean)} callback
+  * @private
+  */
+ export function metaJsonCreativeGrouper(callback) {
+   let first;
+   return function(line, done) {
+     if (first) {
+       callback(
+         unescapeLineDelimitedHtml_(line),
+         /** @type {!Object<string, *>} */(tryParseJson(first) || {}),
+         done);
+       first = null;
+     } else {
+       first = line;
+     }
+   };
+ }
+
+ /**
+  * Unescapes characters that are escaped in line-delimited JSON-HTML.
+  * @param {string} html An html snippet.
+  * @return {string}
+  * @private
+  */
+ function unescapeLineDelimitedHtml_(html) {
+   return html.replace(
+     /\\(n|r|\\)/g,
+     (_, match) => match == 'n' ? '\n' : match == 'r' ? '\r' : '\\');
+ }
