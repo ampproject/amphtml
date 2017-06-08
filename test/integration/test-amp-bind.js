1ee28dddfca7801f40baabf44c9c7af56c523271
Raghu Simha <rsimha@ampproject.org>
2017-06-08 18:35:46 -0400

    Change an integration test

--- test/integration/test-amp-bind.js
+++ test/integration/test-amp-bind.js
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Simple comment change to trigger integration tests. DO NOT MERGE.
  */
 
 import {createFixtureIframe} from '../../testing/iframe';
