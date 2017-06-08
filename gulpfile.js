3b0c1609dd3f2825c1d175095d1931dffede3a8b
Aaron Turner <torchh@google.com>
2017-06-08 12:59:15 -0700

    AMP Sidebar V1: Skeleton'd the files for the feature (#9784)
    * Made a skeleton of all of the required files
    
    This skeeletons all of the required files for amp-sidebar v1
    
    * Removed the test output,a nd updated the gulp file
    
    Now the gulp file will build and look for v1.0 of sidebar
    
    * Added some more skeleton to the main sidebar .js file
    
    Also, tested to ensure that the extension could be loaded and registered
    successfully
    
    * Updated the copyright year
    
    Suggested by @honeybadgerdontcare
    
    * Fixed Travis Linting errors
    
    * Fixed the typo on the alias declaration
    
    * Adding sidebar v1 exception to the dep check
    
    This is because of: #9642 , Was going to fix this myself, but it is
    currently in PR at: #9730
    
    * Fixed PR comments
    
    Was my error in not realizing alias'ing was done for deprecated
    extensions
    
    * Added a note about declareExtensionVersionAlias, and declared versions
    in JS
    
    * Fixed Sidebar 0.1 tests
    
    Needed a different way of building the window since we are now
    introducing versioning to amp-sidebar
    
    * Fixed the gulpfile typo

--- gulpfile.js
+++ gulpfile.js
@@ -110,6 +110,7 @@ declareExtension('amp-reach-player', '0.1', false);
 declareExtension('amp-reddit', '0.1', false);
 declareExtension('amp-share-tracking', '0.1', false);
 declareExtension('amp-sidebar', '0.1', true);
+declareExtension('amp-sidebar', '1.0', true);
 declareExtension('amp-soundcloud', '0.1', false);
 declareExtension('amp-springboard-player', '0.1', false);
 declareExtension('amp-sticky-ad', '1.0', true);
@@ -167,6 +168,10 @@ function declareExtension(name, version, hasCssOrOptions, opt_noTypeCheck,
 }
 
 /**
+ * This function is used for declaring deprecated extensions. It simply places the current
+ * version code in place of the latest versions.
+ * This has the ability to break an extension verison, so please be sure that this is
+ * the correct one to use.
  * @param {string} name
  * @param {string} version E.g. 0.1
  * @param {string} lastestVersion
