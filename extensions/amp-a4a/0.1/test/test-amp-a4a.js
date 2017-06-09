23b3fc51c6094e136669b93669ab689e7cabf497
Gene Levitzky <levitzky@google.com>
2017-06-09 18:13:29 -0400

    lint

--- extensions/amp-a4a/0.1/test/test-amp-a4a.js
+++ extensions/amp-a4a/0.1/test/test-amp-a4a.js
@@ -2175,7 +2175,7 @@ describe('amp-a4a', () => {
               return Promise.resolve();
             },
           };
-        }
+        };
 
         let resolver;
         const promise = new Promise(resolve => resolver = resolve);
