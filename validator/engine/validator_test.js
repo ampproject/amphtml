ce82ba3b83755f8bf8e46c9293d44fc7b8fc4a2d
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2017-06-09 15:06:39 -0700

    rebase

--- validator/engine/validator_test.js
+++ validator/engine/validator_test.js
@@ -86,13 +86,17 @@ function findHtmlFilesRelativeToTestdata() {
   for (const root of process.env['TESTDATA_ROOTS'].split(':')) {
     if (path.basename(root) === 'extensions') {
       for (const extension of readdir(root)) {
-        const extensionVersions = ['0.1', '1.0'];
-        for (const extensionVersion of extensionVersions) {
-          const testPath = path.join(extension, extensionVersion, 'test');
+        const extensionFolder = path.join(root, extension);
+        if (!isdir(extensionFolder)) {
+          // Skip if not an extension folder
+          continue;
+        }
+        // get all versions:
+        for (const version of readdir(extensionFolder)) {
+          const testPath = path.join(extension, version, 'test');
           if (isdir(path.join(root, testPath))) {
             testSubdirs.push({root: root, subdir: testPath});
           }
-        }
       }
     } else {
       for (const subdir of readdir(root)) {
