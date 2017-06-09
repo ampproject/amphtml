ebe5dc6fa4c8a42187bc4b17f228fae1ae4708b9
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2017-06-09 16:51:22 -0700

    Include version 1.0 extensions in testing and build.py (#9812)
    * Include version 1.0 extensions in testing and build.py
    
    * rebase
    
    * explicitly set version numbers for extensions
    
    * remove duplicate validator rules
    
    * correct regex
    
    * correct glob

--- validator/engine/validator_test.js
+++ validator/engine/validator_test.js
@@ -76,7 +76,8 @@ function isValidRegex(regex) {
 /**
  * Returns all html files underneath the testdata roots. This looks
  * both for feature_tests/*.html and for tests in extension directories.
- * E.g.: extensions/amp-accordion/0.1/test/*.html and
+ * E.g.: extensions/amp-accordion/0.1/test/*.html,
+ *       extensions/amp-sidebar/1.0/test/*.html, and
  *       testdata/feature_tests/amp_accordion.html.
  * @return {!Array<string>}
  */
@@ -85,14 +86,9 @@ function findHtmlFilesRelativeToTestdata() {
   for (const root of process.env['TESTDATA_ROOTS'].split(':')) {
     if (path.basename(root) === 'extensions') {
       for (const extension of readdir(root)) {
-        const extensionFolder = path.join(root, extension);
-        if (!isdir(extensionFolder)) {
-          // Skip if not an extension folder
-          continue;
-        }
-        // get all versions:
-        for (const version of readdir(extensionFolder)) {
-          const testPath = path.join(extension, version, 'test');
+        const extensionVersions = ['0.1', '1.0'];
+        for (const extensionVersion of extensionVersions) {
+          const testPath = path.join(extension, extensionVersion, 'test');
           if (isdir(path.join(root, testPath))) {
             testSubdirs.push({root: root, subdir: testPath});
           }
