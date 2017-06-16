65ba4df8c2ec0ec9c89e36ee0b607adaeb083c3d
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2017-06-16 16:34:16 -0700

    Clean up (#9998)

--- validator/engine/validator_test.js
+++ validator/engine/validator_test.js
@@ -548,8 +548,8 @@ describe('ValidatorRulesMakeSense', () => {
       // AMP4ADS Creative Format document is the source of this whitelist.
       // https://github.com/ampproject/amphtml/blob/master/extensions/amp-a4a/amp-a4a-format.md#amp-extensions-and-builtins
       const whitelistedAmp4AdsExtensions = {
-        'AMP-AD-EXIT': 0,
         'AMP-ACCORDION': 0,
+        'AMP-AD-EXIT': 0,
         'AMP-ANALYTICS': 0,
         'AMP-ANIM': 0,
         'AMP-AUDIO': 0,
