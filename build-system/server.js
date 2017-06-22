ee73a24a17c133968048ae301ac42a09424c7977
Raghu Simha <rsimha@ampproject.org>
2017-06-22 16:07:10 -0400

    Cleanly exit if port is in use

--- build-system/server.js
+++ build-system/server.js
@@ -31,6 +31,17 @@ const port = process.env.SERVE_PORT;
 const useHttps = process.env.SERVE_USEHTTPS == 'true' ? true : false;
 const gulpProcess = process.env.SERVE_PROCESS_ID;
 
+// Exit if the port is in use.
+process.on('uncaughtException', function(err) {
+  if(err.errno === 'EADDRINUSE') {
+    util.log(util.colors.red('Port', port, 'in use, shutting down server'));
+  } else {
+    util.log(util.colors.red(err));
+  }
+  process.kill(gulpProcess, 'SIGINT');
+  process.exit(1);
+});
+
 // Exit in the event of a crash in the parent gulp process.
 setInterval(function() {
   if (!isRunning(gulpProcess)) {
