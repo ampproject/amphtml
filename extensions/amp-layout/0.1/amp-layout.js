4732241662f0e05cd4ddc6433989fd38af5dd812
Subhankar <s1panda@ucsd.edu>
2017-06-24 11:26:33 +0530

    added support for sizes (#1 on the Implementation Strategy)

--- extensions/amp-layout/0.1/amp-layout.js
+++ extensions/amp-layout/0.1/amp-layout.js
@@ -2,5 +2,10 @@ import {isLayoutSizeDefined} from '../../../src/layout';
 
 class AmpLayout extends AMP.BaseElement {
 
-    
+    /** @override */
+    isLayoutSupported(layout){
+        return isLayoutSizeDefined(layout);
+    }   
 }
+
+AMP.registerElement('amp-layout', AMPLayout);
