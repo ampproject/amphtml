9081db24d7c0b15e3075c55f13a12070b39d4306
Eng Tat <engtat@innity.com>
2017-07-06 14:14:34 +0800

    Fix indentation (#9938)

--- ads/innity.js
+++ ads/innity.js
@@ -26,7 +26,7 @@ export function innity(global, data) {
     const innityAMPZone = global.innity_adZone;
     const innityAMPTag = new innityAMPZone(encodeURIComponent(data.pub),
       encodeURIComponent(data.zone), {width: data.width, height: data.height,
-      channel: data.channel ? encodeURIComponent(data.channel) : ''});
+        channel: data.channel ? encodeURIComponent(data.channel) : ''});
     // AMP handling or noContentAvailable
     innityAMPTag.amp(global.context);
     // else renderStart (with at least house ad)
