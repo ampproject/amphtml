70c23c642e24b673e7d852572327aac0cd9ec3f3
William Chou <willchou@google.com>
2017-07-07 17:37:12 -0400

    amp-bind: Fix amp-img[src] rewriting on cache (#10317)

--- src/sanitizer.js
+++ src/sanitizer.js
@@ -384,8 +384,10 @@ export function isValidAttr(tagName, attrName, attrValue) {
  * @return {string}
  */
 export function rewriteAttributeValue(tagName, attrName, attrValue) {
-  if (attrName == 'src' || attrName == 'href' || attrName == 'srcset') {
-    return resolveUrlAttr(tagName, attrName, attrValue, self.location);
+  const tag = tagName.toLowerCase();
+  const attr = attrName.toLowerCase();
+  if (attr == 'src' || attr == 'href' || attr == 'srcset') {
+    return resolveUrlAttr(tag, attr, attrValue, self.location);
   }
   return attrValue;
 }
