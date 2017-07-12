cbdd2d1427f2691ba61a5bfd90a00f0c7d20a8f9
Raghu Simha <rsimha@ampproject.org>
2017-07-12 19:26:59 -0400

    Add per-page custom CSS waits for visual diff tests (#10389)

--- build-system/tasks/visual-diff.rb
+++ build-system/tasks/visual-diff.rb
@@ -36,6 +36,11 @@ HOST = 'localhost'
 PORT = '8000'
 
 
+# Colorize logs.
+def red(text); "\e[31m#{text}\e[0m"; end
+def cyan(text); "\e[36m#{text}\e[0m"; end
+
+
 # Launches a background AMP webserver for unminified js using gulp.
 #
 # Returns:
@@ -107,6 +112,7 @@ end
 # - pagesToSnapshot: JSON object containing details about the pages to snapshot.
 def generateSnapshots(pagesToSnapshot)
   Percy.config.default_widths = DEFAULT_WIDTHS
+  Capybara.default_max_wait_time = 5
   server = "http://#{HOST}:#{PORT}"
   webpages = pagesToSnapshot["webpages"]
   assets_base_url = pagesToSnapshot["assets_base_url"]
@@ -122,7 +128,16 @@ def generateSnapshots(pagesToSnapshot)
     webpages.each do |webpage|
       url = webpage["url"]
       name = webpage["name"]
-      generateSnapshot(page, url, name)
+      forbidden_css = webpage["forbidden_css"]
+      loading_incomplete_css = webpage["loading_incomplete_css"]
+      loading_complete_css = webpage["loading_complete_css"]
+      generateSnapshot(
+          page,
+          url,
+          name,
+          forbidden_css,
+          loading_incomplete_css,
+          loading_complete_css)
     end
   end
 end
@@ -134,9 +149,44 @@ end
 # - page: Page object used by Percy for snapshotting.
 # - url: Relative URL of page to be snapshotted.
 # - name: Name of snapshot on Percy.
-def generateSnapshot(page, url, name)
+# - forbidden_css:
+#       Array of CSS elements that must not be found in the page.
+# - loading_incomplete_css:
+#       Array of CSS elements that must eventually be removed from the page.
+# - loading_complete_css:
+#       Array of CSS elements that must eventually appear on the page.
+def generateSnapshot(
+    page,
+    url,
+    name,
+    forbidden_css,
+    loading_incomplete_css,
+    loading_complete_css)
   page.visit(url)
   page.has_no_css?('.i-amphtml-loader-dot')  # Implicitly waits for page load.
+  if forbidden_css
+    forbidden_css.each do |css|
+      if page.has_css?(css)  # No implicit wait.
+        puts red("ERROR: ") + "page has CSS element " + cyan("#{css}")
+      end
+    end
+  end
+  if loading_incomplete_css
+    loading_incomplete_css.each do |css|
+      if !page.has_no_css?(css)  # Implicitly waits for element to disappear.
+        puts red("ERROR: ") + "page still has CSS element "\
+            + cyan("#{css}")
+      end
+    end
+  end
+  if loading_complete_css
+    loading_complete_css.each do |css|
+      if !page.has_css?(css)  # Implicitly waits for element to appear.
+        puts red("ERROR: ") + "page does not yet have CSS element "\
+            + cyan("#{css}")
+      end
+    end
+  end
   Percy::Capybara.snapshot(page, name: name)
 end
 
@@ -161,7 +211,7 @@ def main()
   setDebuggingLevel()
   pid = launchWebServer()
   if not waitForWebServer()
-    puts "Failed to start webserver"
+    puts red("ERROR: ") + "Failed to start webserver"
     closeWebServer(pid)
     exit(false)
   end
