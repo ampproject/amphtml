25cca1c8f4468ca7e68deb461ddcd2ea1331478b
spliu787273 <spliu78@users.noreply.github.com>
2017-07-13 16:22:25 +0800

    Revert "add sogou ads"
    This reverts commit 060c2d5ac639e3e9100e3d949fe14bf03ec6e6aa.
    
    	modified:   yarn.lock

--- ads/_config.js
+++ ads/_config.js
@@ -612,11 +612,6 @@ export const adConfig = {
     renderStartImplemented: true,
   },
 
-  sogou_ad: {
-    prefetch: 'https://theta.sogoucdn.com/wap/js/aw.js',
-    renderStartImplemented: true,
-  },
-
   sortable: {
     prefetch: 'https://www.googletagservices.com/tag/js/gpt.js',
     preconnect: [
