91b5aba4f7801a389ddca196c74320e1a5252425
William Chou <willchou@google.com>
2017-07-14 19:40:28 -0400

    Skip flaky video test (#10459)

--- test/integration/test-video-players-helper.js
+++ test/integration/test-video-players-helper.js
@@ -505,7 +505,7 @@ export function runVideoPlayerIntegrationTests(
       });
 
       // TODO(aghassemi, #9379): Flaky on Safari 9.
-      it.configure().skipSafari().run('should play/pause when video ' +
+      it.skip('should play/pause when video ' +
           'enters/exits viewport', () => {
         let video;
         let viewport;
