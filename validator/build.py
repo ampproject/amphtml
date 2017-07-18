a15a23f23d06babf72945311acaaf50d1c4bde94
Raghu Simha <rsimha@ampproject.org>
2017-07-18 14:26:12 -0400

    Better test logging for local development and Travis runs (#10479)

--- validator/build.py
+++ validator/build.py
@@ -131,9 +131,14 @@ def InstallNodeDependencies():
   # Install the project dependencies specified in package.json into
   # node_modules.
   logging.info('installing AMP Validator engine dependencies ...')
-  subprocess.check_call(['npm', 'install'])
+  subprocess.check_call(
+      ['npm', 'install'],
+      stdout=(open(os.devnull, 'wb') if os.environ['TRAVIS'] else sys.stdout))
   logging.info('installing AMP Validator nodejs dependencies ...')
-  subprocess.check_call(['npm', 'install'], cwd='nodejs')
+  subprocess.check_call(
+      ['npm', 'install'],
+      cwd='nodejs',
+      stdout=(open(os.devnull, 'wb') if os.environ['TRAVIS'] else sys.stdout))
   logging.info('... done')
 
 
