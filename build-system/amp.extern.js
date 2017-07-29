2bb132ea417baceae275f72d9a2da4e7b349050c
jonkeller <jonkeller@google.com>
2017-07-29 16:09:40 -0400

    Updates class name of IframeTransportResponseMap in amp.extern.js

--- build-system/amp.extern.js
+++ build-system/amp.extern.js
@@ -145,7 +145,7 @@ AmpViewerMessage.prototype.error;
 
 // AMP-Analytics Cross-domain iframes
 let IframeTransportEvent;
-let ResponseMap;
+let IframeTransportResponseMap;
 
 // amp-viz-vega related externs.
 /**
