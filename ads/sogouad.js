b3ae9193c1cdbc22a0cc6f66ab35e27b3db1787a
spliu787273 <spliu78@users.noreply.github.com>
2017-08-01 11:51:15 +0800

    lint......

--- ads/sogouad.js
+++ ads/sogouad.js
@@ -24,11 +24,20 @@ export function sogouad(global, data) {
   validateData(data, ['slot', 'w', 'h'], ['responsive']);
   const slot = global.document.getElementById('c');
   const ad = global.document.createElement('div');
-  global.sogou_un = window.sogou_un || [];
-  if(data.w === '100%'){
-    global.sogou_un.push({id: data.slot,ele:ad});
-  }else{
-    global.sogou_un.push({id: data.slot,ele:ad,w:data.w,h:data.h});
+  const sogouUn = 'sogou_un';
+  global[sogouUn] = window[sogouUn] || [];
+  if (data.w === '100%') {
+    global[sogouUn].push({
+      id: data.slot,
+      ele: ad
+    });
+  } else {
+    global[sogouUn].push({
+      id: data.slot,
+      ele: ad,
+      w: data.w,
+      h: data.h
+    });
   }
   slot.appendChild(ad);
   loadScript(global, 'https://theta.sogoucdn.com/wap/js/aw.js');
