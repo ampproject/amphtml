2b9bd6dfeaacd1ca69184c38e9c54c407f612f20
Malte Ubl <malte.ubl@gmail.com>
2017-08-02 08:34:37 -0700

    Switch to viewport based width.

--- test/functional/test-amp-img.js
+++ test/functional/test-amp-img.js
@@ -23,10 +23,19 @@ import * as sinon from 'sinon';
 describe('amp-img', () => {
   let sandbox;
   let screenWidth;
+  let windowWidth;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
     screenWidth = 320;
+    windowWidth = 320;
+    sandbox.stub(BaseElement.prototype, 'isInViewport')
+        .returns(true);
+    sandbox.stub(BaseElement.prototype, 'getViewport', () => {
+      return {
+        getWidth: () => windowWidth,
+      }
+    });
   });
 
   afterEach(() => {
@@ -34,13 +43,12 @@ describe('amp-img', () => {
   });
 
   function getImg(attributes, children) {
-    sandbox.stub(BaseElement.prototype, 'isInViewport')
-        .returns(true);
     return createIframePromise().then(iframe => {
       installImg(iframe.win);
       Object.defineProperty(iframe.win.screen, 'width', {
         get: () => screenWidth,
       });
+
       const img = iframe.doc.createElement('amp-img');
       for (const key in attributes) {
         img.setAttribute(key, attributes[key]);
@@ -103,6 +111,8 @@ describe('amp-img', () => {
   });
 
   it('should load an img with srcset', () => {
+    windowWidth = 320;
+    screenWidth = 4000;
     return getImg({
       srcset: 'bad.jpg 2000w, /examples/img/sample.jpg 1000w',
       width: 300,
@@ -116,6 +126,22 @@ describe('amp-img', () => {
   });
 
   it('should load larger image on larger screen', () => {
+    windowWidth = 3000;
+    screenWidth = 300;
+    return getImg({
+      srcset: 'large.jpg 2000w, small.jpg 1000w',
+      width: 300,
+      height: 200,
+    }).then(ampImg => {
+      const img = ampImg.querySelector('img');
+      expect(img.tagName).to.equal('IMG');
+      expect(img.getAttribute('src')).to.equal('large.jpg');
+      expect(img.hasAttribute('referrerpolicy')).to.be.false;
+    });
+  });
+
+  it('should falls back to screen width for srcset', () => {
+    windowWidth = 0;
     screenWidth = 3000;
     return getImg({
       srcset: 'large.jpg 2000w, small.jpg 1000w',
@@ -170,6 +196,11 @@ describe('amp-img', () => {
           },
         };
       };
+      impl.getViewport = function() {
+        return {
+          getWidth: () => windowWidth,
+        };
+      }
     });
 
     it('should not display fallback if loading succeeds', () => {
