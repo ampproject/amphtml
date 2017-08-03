befe36e1618b9a1c6f05b0416605e40e3563ecf0
Raphael Junior <rigwanzojunior@google.com>
2017-08-03 11:37:22 -0400

    updates

--- extensions/amp-a4a/0.1/test/test-legacy-signature-verifier.js
+++ extensions/amp-a4a/0.1/test/test-legacy-signature-verifier.js
@@ -20,10 +20,7 @@ import {
 } from './testdata/valid_css_at_rules_amp.reserialized';
 import {LegacySignatureVerifier} from '../legacy-signature-verifier';
 
-describes.realWin('LegacySignatureVerifier', {amp: true,
-  xhrMock: true,
-},env => {
-
+describes.realWin('LegacySignatureVerifier', {amp: true}, env => {
   let fetchMock;
   let keysetBody;
   let verifier;
