ce79e30981bec5f3e79674256ff1b6f876c16e6d
Aaron Turner <aaron@aaronthedev.com>
2017-08-04 11:32:55 -0700

    Added back 1.0 gulp, can be removed when 1.0 is deleted

--- gulpfile.js
+++ gulpfile.js
@@ -109,6 +109,7 @@ declareExtension('amp-reach-player', '0.1', false);
 declareExtension('amp-reddit', '0.1', false);
 declareExtension('amp-share-tracking', '0.1', false);
 declareExtension('amp-sidebar', '0.1', true);
+declareExtension('amp-sidebar', '1.0', true);
 declareExtension('amp-soundcloud', '0.1', false);
 declareExtension('amp-springboard-player', '0.1', false);
 declareExtension('amp-sticky-ad', '1.0', true);
