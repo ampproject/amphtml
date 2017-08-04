75b50e86e7eb5195dbc5df9f8fc3dcb8adba8ad1
Raphael Junior Owino <raphnano@gmail.com>
2017-08-04 16:22:07 -0400

    Fixed failing tests to conform to updates

--- testing/describes.js
+++ testing/describes.js
@@ -488,6 +488,7 @@ class FakeWinFixture {
   setup(env) {
     const spec = this.spec;
     env.win = new FakeWindow(this.spec.win || {});
+    console.log(spec.xhrMock);
     if (!(spec.xhrMock === false)) {
       fetchMock.constructor.global = env.win;
       fetchMock._mock();
