1c476c74face894509cf79d87482f7de7bd08eef
Chunlei Zhu <chunleiz@google.com>
2017-08-08 16:01:45 +0800

    Add the click location filter for amp-ad-exit

---
+++ extensions/amp-ad-exit/0.1/filters/click-location.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview Description of this file.
+ */
+import {FilterType, Filter} from './filter';
+import {user} from '../../../../src/log';
+
+export class ClickLocationFilter extends Filter {
+  /**
+   * @param {string} name The user-defined name of the filter.
+   * @param {!../config.ClickLocationConfig} spec
+   */
+  constructor(name, spec) {
+    super(name);
+    user().assert(isValidClickLocationSpec(spec), 'Invaid ClickLocation spec');
+
+    /** @private {number} */
+    this.leftBorder_ = spec.left || 0;
+
+    /** @private {number} */
+    this.rightBorder_ = spec.right || 0;
+
+    /** @private {number} */
+    this.topBorder_ = spec.top || 0;
+
+    /** @private {number} */
+    this.bottomBorder_ = spec.bottom || 0;
+
+    /** @private {string|undefined} */
+    this.relativeTo_ = spec.relativeTo;
+  }
+
+  /** @override */
+  filter(event) {
+    let left = 0;
+    let top = 0;
+    let right = window.innerWidth;
+    let bottom = window.innerHeight;
+    if (this.relativeTo_) {
+      const relativeElement = document.querySelector(this.relativeTo_);
+      user().assert(relativeElement, `relativeTo element ${this.relativeTo_} not found.`);
+      const elementRect = relativeElement.getBoundingClientRect();
+      left = elementRect.left;
+      top = elementRect.top;
+      right = elementRect.right;
+      bottom = elementRect.bottom;
+    }
+    if (event.clientX < (left + this.leftBorder_) ||
+        event.clientX > (right - this.rightBorder_) ||
+        event.clientY < (top + this.topBorder_) ||
+        event.clientY > (bottom - this.bottomBorder_)) {
+      return false;
+    }
+    return true;
+  }
+}
+
+/**
+ * @param {!../config.FilterConfig} spec
+ * @return {boolean} Whether the config defines a ClickLocation filter.
+ */
+function isValidClickLocationSpec(spec) {
+  return spec.type == FilterType.CLICK_LOCATION &&
+      (typeof spec.left == 'undefined' || typeof spec.left == 'number') &&
+      (typeof spec.right == 'undefined' || typeof spec.right == 'number') &&
+      (typeof spec.top == 'undefined' || typeof spec.top == 'number') &&
+      (typeof spec.bottom == 'undefined' || typeof spec.bottom == 'number') &&
+      (typeof spec.relativeTo == 'undefined' || typeof spec.relativeTo == 'string') ;
+}
+
