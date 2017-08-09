e22551dea88313d41b0415c950ab679120710847
Chunlei Zhu <chunleiz@google.com>
2017-08-09 17:34:09 +0800

    Fix the test error.

--- extensions/amp-ad-exit/0.1/filters/click-location.js
+++ extensions/amp-ad-exit/0.1/filters/click-location.js
@@ -1,6 +1,19 @@
 /**
- * @fileoverview Description of this file.
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 import {FilterType, Filter} from './filter';
 import {user} from '../../../../src/log';
 
@@ -69,7 +82,7 @@ function isValidClickLocationSpec(spec) {
       (typeof spec.right == 'undefined' || typeof spec.right == 'number') &&
       (typeof spec.top == 'undefined' || typeof spec.top == 'number') &&
       (typeof spec.bottom == 'undefined' || typeof spec.bottom == 'number') &&
-      (typeof spec.relativeTo == 'undefined'
-       || typeof spec.relativeTo == 'string') ;
+      (typeof spec.relativeTo == 'undefined' ||
+       typeof spec.relativeTo == 'string') ;
 }
 
