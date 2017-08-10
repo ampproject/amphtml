e84bc836986280787722ddb4d103cf9e47a52464
Taymon A. Beal <taymon@google.com>
2017-08-10 13:50:28 -0700

    Don't use Fast Fetch with AdSense or DoubleClick on non-Google AMP caches

--- src/config.js
+++ src/config.js
@@ -29,14 +29,16 @@ const thirdPartyFrameRegex = typeof env['thirdPartyFrameRegex'] == 'string' ?
 const cdnProxyRegex = typeof env['cdnProxyRegex'] == 'string' ?
     new RegExp(env['cdnProxyRegex']) : env['cdnProxyRegex'];
 
+export const googleCdnProxyRegex =
+    /^https:\/\/([a-zA-Z0-9_-]+\.)?cdn\.ampproject\.org/;
+
 /** @type {!Object<string, string|boolean|RegExp>} */
 export const urls = {
   thirdParty: env['thirdPartyUrl'] || 'https://3p.ampproject.net',
   thirdPartyFrameHost: env['thirdPartyFrameHost'] || 'ampproject.net',
   thirdPartyFrameRegex: thirdPartyFrameRegex || /^d-\d+\.ampproject\.net$/,
   cdn: env['cdnUrl'] || 'https://cdn.ampproject.org',
-  cdnProxyRegex: cdnProxyRegex ||
-      /^https:\/\/([a-zA-Z0-9_-]+\.)?cdn\.ampproject\.org/,
+  cdnProxyRegex: cdnProxyRegex || googleCdnProxyRegex,
   localhostRegex: /^https?:\/\/localhost(:\d+)?$/,
   errorReporting: env['errorReportingUrl'] ||
       'https://amp-error-reporting.appspot.com/r',
