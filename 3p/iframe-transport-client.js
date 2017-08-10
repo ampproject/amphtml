e72ba6cd31e6962a7b34b626a46346e082fcda85
tdao <tiendt@google.com>
2017-08-10 10:28:28 -0700

    iframe-transport-client

--- 3p/iframe-transport-client.js
+++ 3p/iframe-transport-client.js
@@ -63,7 +63,7 @@ export class IframeTransportClient {
               this.listener_ &&
                   this.listener_(event.message, event.transportId);
             } catch (e) {
-              user().error(TAG_,
+              user(this.win_.document.documentElement).error(TAG_,
                   'Exception in callback passed to onAnalyticsEvent: ' +
                   e.message);
             }
