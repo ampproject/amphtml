39aec9ed267d9853ee3eba4f2f12eb79b9b47eb1
tdao <tiendt@google.com>
2017-08-16 10:00:58 -0700

    update config vendor.js, vendor-request.json

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -697,6 +697,9 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
           'clt=${contentLoadTime}&' +
           'dit=${domInteractiveTime}' +
           '${baseSuffix}',
+      'user-error': '${host}/r/collect?${basePrefix}&' +
+          'em=${errorMessage}' +
+          '${baseSuffix}',
     },
     'triggers': {
       'performanceTiming': {
@@ -789,6 +792,10 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
       'clt=${contentLoadTime}&' +
       'dit=${domInteractiveTime}' +
       '${baseSuffix}',
+      'user-error': '${host}/r/collect?${basePrefix}&' +
+      'em=${errorMessage}' +
+      '${baseSuffix}',
+
     },
     'triggers': {
       'performanceTiming': {
