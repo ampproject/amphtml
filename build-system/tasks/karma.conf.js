ac86feb6ff73dbebf56da02206c38daacf9f75a2
jonkeller <jonkeller@google.com>
2017-08-16 11:19:58 -0400

    Turns off embedSource

--- build-system/tasks/karma.conf.js
+++ build-system/tasks/karma.conf.js
@@ -46,7 +46,7 @@ module.exports = {
         'browserify-istanbul',
         {
           instrumenterConfig: {
-            embedSource: true
+            embedSource: false
           }
         }
       ]
