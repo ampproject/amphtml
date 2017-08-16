6b0b5f9fc54978adf931b0ca60f2a5e013293154
tdao <tiendt@google.com>
2017-08-16 09:46:56 -0700

    revert Todo, space

--- src/3p-frame.js
+++ src/3p-frame.js
@@ -282,7 +282,6 @@ function getCustomBootstrapBaseUrl(
     return null;
   }
   if (opt_disallowCustom) {
-    //TODO(tiendt) get element from window
     user().error(TAG, `3p iframe url disabled for ${opt_type || 'unknown'}`);
     return null;
   }
