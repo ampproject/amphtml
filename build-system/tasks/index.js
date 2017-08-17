85e091ced38d762e281b377e74223c9d3443092f
erwin mombay <erwinmombay@users.noreply.github.com>
2017-08-17 16:20:36 -0700

    add CODEOWNERS (#10342)
    * add CODEOWNERS
    
    * temp
    
    * generate CODEOWNERS file from OWNERS.yaml files
    
    * add test to make sure CODEOWNERS is always in sync with OWNERS.yaml state
    
    * write to /tmp instead and run test on any .yaml change
    
    * update yarn.lock

--- build-system/tasks/index.js
+++ build-system/tasks/index.js
@@ -27,6 +27,7 @@ require('./compile-css-expr');
 require('./csvify-size');
 require('./dep-check');
 require('./get-zindex');
+require('./gen-codeowners');
 require('./extension-generator');
 require('./json-check');
 require('./lint');
