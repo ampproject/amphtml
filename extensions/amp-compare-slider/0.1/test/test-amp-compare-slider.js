0a19a65062f82ab2dbc6a4f9c3a1b0a612541607
Alanna Walton <alwalton@google.com>
2017-08-17 13:50:23 -0700

    fixed test

--- extensions/amp-compare-slider/0.1/test/test-amp-compare-slider.js
+++ extensions/amp-compare-slider/0.1/test/test-amp-compare-slider.js
@@ -31,8 +31,4 @@ describes.realWin('amp-compare-slider', {
     win.document.body.appendChild(element);
   });
 
-  it('should have hello world when built', () => {
-    element.build();
-    expect(element.querySelector('div').textContent).to.equal('hello world');
-  });
 });
