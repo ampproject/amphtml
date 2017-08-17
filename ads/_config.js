ad762d62bd378fb88d274c33fa0f465217ec7a75
ColombiaOnline <adtech.appservices@gmail.com>
2017-08-17 13:08:28 +0530

    Remove space

--- ads/_config.js
+++ ads/_config.js
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -53,7 +53,7 @@ export const adPrefetch = {
   colombia: 'https://static.clmbtech.com/ctn/commons/js/colombia-amp.js',
 };
 
-/** 
+/**
  * URLs to connect to for a given ad type.
  *
  * This MUST be kept in sync with actual implementation.
