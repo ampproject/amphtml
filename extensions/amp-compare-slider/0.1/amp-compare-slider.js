9673069a22f4c2c868a47467f4d09d237b7ec290
Alanna Walton <alwalton@google.com>
2017-08-16 18:37:10 -0700

    Progress (CSS doesnt work tho)

--- extensions/amp-compare-slider/0.1/amp-compare-slider.js
+++ extensions/amp-compare-slider/0.1/amp-compare-slider.js
@@ -15,29 +15,49 @@
  */
 
 import {Layout} from '../../../src/layout';
+import {CSS} from '../../../build/amp-compare-slider-0.1.css';
 
 export class AmpCompareSlider extends AMP.BaseElement {
 
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
-
+    
     /** @private {?Element} */
-    this.container_ = null;
+    this.topElementContainer_ = null;
+    
+    /** @private {?Element} */
+    this.bottomElementContainer_ = null;
+    
   }
 
   /** @override */
   buildCallback() {
-    this.container_ = this.win.document.createElement('div');
-    this.container_.textContent = this.myText_;
-    this.element.appendChild(this.container_);
-    this.applyFillContent(this.container_, /* replacedContent */ true);
+    this.buildCompareSliderElements();
   }
-
+  
   /** @override */
   isLayoutSupported(layout) {
     return layout == Layout.RESPONSIVE;
   }
+  
+  buildCompareSliderElements {
+    const originalChildren = this.element.children;
+    
+    this.topElementContainer_ = document.createElement('div');
+    this.bottomElementContainer_ = document.createElement('div');
+    
+    //Make Elements children of newly created divs
+    this.topElementContainer_.appendChild(originalChildren[0]);
+    this.bottomElementContainer_.appendChild(originalChildren[0]);
+    this.topElementContainer_.className = 'topElementContainer';
+    this.bottomElementContainer_.className = 'bottomElementContainer';
+    
+    // Add newly created elements to the view
+    this.element.appendChild(this.topElementContainer_);
+    this.element.appendChild(this.bottomElementContainer_);
+  }
+  
 }
 
-AMP.registerElement('amp-compare-slider', AmpCompareSlider);
+AMP.registerElement('amp-compare-slider', AmpCompareSlider, CSS);
