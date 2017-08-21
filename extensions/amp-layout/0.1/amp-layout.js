afb494dd4a93f057c4264161c248b0a91fadd042
Subhankar Panda <s1panda@ucsd.edu>
2017-08-22 02:46:01 +0530

    lint errs

--- extensions/amp-layout/0.1/amp-layout.js
+++ extensions/amp-layout/0.1/amp-layout.js
@@ -19,9 +19,9 @@ import {isLayoutSizeDefined} from '../../../src/layout';
 class AmpLayout extends AMP.BaseElement {
 
     /** @override */
-  isLayoutSupported(layout){
+  isLayoutSupported(layout) {
     return isLayoutSizeDefined(layout);
-  }   
+  }
 }
 
 AMP.registerElement('amp-layout', AmpLayout);
