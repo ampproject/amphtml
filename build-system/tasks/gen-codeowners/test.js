280fb949439f80d2c3eee6dd210952c98b1dfc54
erwin mombay <erwinmombay@users.noreply.github.com>
2017-08-21 11:11:09 -0700

    remove comma from username list to the right (#11007)

--- build-system/tasks/gen-codeowners/test.js
+++ build-system/tasks/gen-codeowners/test.js
@@ -34,9 +34,9 @@ test('sync - build out correct CODEOWNERS', t => {
   };
   t.plan(1);
   const result = m.buildCodeownersFile(owners);
-  const expected = `* @username1, @username2
+  const expected = `* @username1 @username2
 *.protoascii @ampproject/group
-ads/ @username3, @username1
+ads/ @username3 @username1
 some/deeply/nested/dir/ @username5
 some/deeply/nested/dir/some.js @ampproject/group2
 `;
