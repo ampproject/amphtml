4291fcf92316ac6bee77d6b0e676e4c9d1990e11
William Chou <willchou@google.com>
2017-08-24 13:22:32 -0400

    fix lint

--- src/service/history-impl.js
+++ src/service/history-impl.js
@@ -20,7 +20,7 @@ import {
   getService,
 } from '../service';
 import {getMode} from '../mode';
-import {dev, user} from '../log';
+import {dev} from '../log';
 import {dict, map} from '../utils/object';
 import {Services} from '../services';
 
