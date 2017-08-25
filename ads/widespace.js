4bda424fb9f7857a13fa6dca710cd17c7c0b195d
Jonas Mattsson <jonas.mattsson@widespace.com>
2017-08-25 19:15:21 +0200

    Feature/widespace adapter support demography parameters. (#10961)
    * Allow the publisher to add custom demography data in our amp-tag.
    
    * Updated documentation
    
    * Styling fix
    
    * Styling fix
    
    * Trying to get around the 80 char per line rule that makes Travis to fail the build.
    
    * Trying to get around the 80 char per line rule that makes Travis to fail the build.
    
    * Spelling fix
    
    * Removed parentheses around single function argument
    
    * Removed spaces in parentheses

--- ads/widespace.js
+++ ads/widespace.js
@@ -22,13 +22,19 @@ import {writeScript, validateData} from '../3p/3p';
  */
 export function widespace(global, data) {
 
-  const WS_AMP_CODE_VER = '1.0.0';
+  const WS_AMP_CODE_VER = '1.0.1';
+  // Optional demography parameters.
+  let demo = [];
 
-  validateData(data, ['sid'], []);
+  demo = ['Gender', 'Country', 'Region', 'City', 'Postal', 'Yob'].map(d => {
+    return 'demo' + d;
+  });
+
+  validateData(data, ['sid'], demo);
 
   const url = 'https://engine.widespace.com/map/engine/dynamic?isamp=1'
       + '&ampver=' + WS_AMP_CODE_VER
-      + '&sid=' + encodeURIComponent(data.sid);
+      + '&#sid=' + encodeURIComponent(data.sid);
 
   writeScript(global, url);
 }
