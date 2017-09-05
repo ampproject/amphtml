e2832e16d0977200e5c288a204add9db30bfde6a
Dima Voytenko <dvoytenko@google.com>
2017-09-05 13:51:12 -0700

    Patch from Caja on regex capitalization (#11159)
    * Patch from Caja on regex capitalization
    
    * readme notes

--- third_party/caja/html-sanitizer.js
+++ third_party/caja/html-sanitizer.js
@@ -1457,7 +1457,7 @@ var html = (function(html4) {
   var decimalEscapeRe = /^#(\d+)$/;
   var hexEscapeRe = /^#x([0-9A-Fa-f]+)$/;
   // contains every entity per http://www.w3.org/TR/2011/WD-html5-20110113/named-character-references.html
-  var safeEntityNameRe = /^[A-Za-z][A-za-z0-9]+$/;
+  var safeEntityNameRe = /^[A-Za-z][A-Za-z0-9]+$/;
   // Used as a hook to invoke the browser's entity parsing. <textarea> is used
   // because its content is parsed for entities but not tags.
   // TODO(kpreid): This retrieval is a kludge and leads to silent loss of
