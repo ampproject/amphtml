ab575b64e530e309b5bc5f122cc7d505366f08e1
glevitzky <levitzky@google.com>
2017-09-05 11:34:47 -0400

    Adds getBinaryType to experiments.js (#11153)

--- src/experiments.js
+++ src/experiments.js
@@ -66,6 +66,16 @@ export function isCanary(win) {
   return !!(win.AMP_CONFIG && win.AMP_CONFIG.canary);
 }
 
+/**
+ * Returns binary type, e.g., canary, control, or production.
+ * @param {!Window} win
+ * @return {string}
+ */
+export function getBinaryType(win) {
+  return win.AMP_CONFIG && win.AMP_CONFIG.type ?
+      win.AMP_CONFIG.type : 'unknown';
+}
+
 
 /**
  * Enable experiments detailed in an origin trials token iff the token is
