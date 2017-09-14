d0af3a8a452f11d65b540f841f284091535eb455
Raghu Simha <rsimha@ampproject.org>
2017-09-14 15:07:07 -0400

    Temporarily remove conflicting files

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -290,19 +290,6 @@ const command = {
   },
   runIntegrationTests: function(compiled) {
     // Integration tests with all saucelabs browsers
-    let committer = getStdout(`git log -1 --pretty=format:'%ae'`).trim();
-    if (process.env.SAUCE_USERNAME !== 'amphtml') {
-      util.log(fileLogPrefix,
-          'Using Sauce Labs credentials for', util.colors.cyan(committer),
-          'with username', util.colors.cyan(process.env.SAUCE_USERNAME));
-    } else {
-      util.log(fileLogPrefix,
-          'Could not find Sauce Labs credentials for',
-          util.colors.cyan(committer),
-          '(falling back to',
-          util.colors.cyan(process.env.SAUCE_USERNAME),
-          'credentials)');
-    }
     let cmd = `${gulp} test --nobuild --saucelabs --integration`;
     if (compiled) {
       cmd += ' --compiled';
