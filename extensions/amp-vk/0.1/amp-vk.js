2428f475e76d75393dada23d531b03a28921a0bf
andrey_abramov <andrew.abramov00@gmail.com>
2017-09-15 02:20:08 +0300

    vk.com: amp-vk extension and tests

--- extensions/amp-vk/0.1/amp-vk.js
+++ extensions/amp-vk/0.1/amp-vk.js
@@ -15,6 +15,9 @@
  */
 
 import {Layout} from '../../../src/layout';
+import {user} from '../../../src/log';
+import {removeElement} from '../../../src/dom';
+import {listen} from '../../../src/event-helper';
 
 export class AmpVk extends AMP.BaseElement {
 
@@ -22,24 +25,152 @@ export class AmpVk extends AMP.BaseElement {
   constructor(element) {
     super(element);
 
-    /** @private {string} */
-    this.myText_ = 'hello world';
+    /** @private {?String} */
+    this.iframeUrl_ = 'https://vk.com/widget_post.php';
 
-    /** @private {!Element} */
-    this.container_ = this.win.document.createElement('div');
+    /** @private {?Element} */
+    this.iframe_ = null;
+
+    /** @private {?Promise} */
+    this.iframePromise_ = null;
+
+    /** @private {?Number} */
+    this.height_ = 0;
+
+    /** @private {?Function} */
+    this.unlistenMessage_ = null;
+
+    this.pageReferrer_ = this.element.ownerDocument.referrer;
+    this.pageUrl_ = this.element.ownerDocument
+        .location.href.replace(/#.*$/, '');
+  }
+
+  /**
+   * @param {boolean=} opt_onLayout
+   * @override
+   */
+  preconnectCallback(opt_onLayout) {
+    this.preconnect.url('https://vk.com', opt_onLayout);
+  }
+
+  getIFrameSrc() {
+    let queryString = this.iframeUrl_;
+
+    const queryParams = this.getIframeParameters();
+
+    let i = 0;
+    for (const param in queryParams) {
+      const divider = i++ === 0 ? '?' : '&';
+      queryString += `${divider}${param}=${
+          encodeURIComponent(queryParams[param])}`;
+    }
+    return queryString;
+  }
+
+  getIframeParameters() {
+    return {
+      'app': 0,
+      'width': '100%',
+      'startWidth': this.element.offsetWidth,
+      '_ver': 1,
+      'owner_id': this.ownerId_,
+      'post_id': this.postId_,
+      'hash': this.hash_,
+      'url': this.pageUrl_,
+      'referrer': this.pageReferrer_,
+      'title': '',
+    };
   }
 
   /** @override */
   buildCallback() {
-    this.container_.textContent = this.myText_;
-    this.element.appendChild(this.container_);
-    this.applyFillContent(this.container_, /* replacedContent */ true);
+    user().assert(this.element.getAttribute('data-hash'),
+        'The data-hash attribute is required for <amp-vk-3p> %s',
+        this.element);
+
+    user().assert(this.element.getAttribute('data-ownerid'),
+        'The data-ownerid attribute is required for <amp-vk-3p> %s',
+        this.element);
+
+    user().assert(this.element.getAttribute('data-postid'),
+        'The data-postid attribute is required for <amp-vk-3p> %s',
+        this.element);
+
+    this.ownerId_ = this.element.getAttribute('data-ownerid');
+    this.postId_ = this.element.getAttribute('data-postid');
+    this.hash_ = this.element.getAttribute('data-hash');
+  }
+
+  /** @override */
+  layoutCallback() {
+    const iframe = this.element.ownerDocument.createElement('iframe');
+    this.iframe_ = iframe;
+
+    this.unlistenMessage_ = listen(
+        this.win,
+        'message',
+        this.handleVkIframeMessage_.bind(this)
+    );
+
+    iframe.src = this.getIFrameSrc();
+    iframe.setAttribute('name', 'fXD');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('frameborder', '0');
+
+    this.applyFillContent(iframe);
+    this.element.appendChild(iframe);
+
+    return this.iframePromise_ = this.loadPromise(iframe);
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  handleVkIframeMessage_(e) {
+    if (e.origin !== 'https://vk.com' ||
+        e.source !== this.iframe_.contentWindow) {
+      return;
+    }
+    const matches = e.data.match(/\[\"resize",\[(\d+)\]]/);
+    if (matches && matches[1]) {
+      const newHeight = parseInt(matches[1], 10);
+      if (this.height_ !== newHeight) {
+        this.height_ = newHeight;
+        this./*OK*/changeHeight(newHeight);
+      }
+    }
   }
 
   /** @override */
   isLayoutSupported(layout) {
-    return layout == Layout.RESPONSIVE;
+    return layout === Layout.RESPONSIVE ||
+        layout === Layout.FLEX_ITEM ||
+        layout === Layout.FIXED;
+  }
+
+  /** @override */
+  unlayoutOnPause() {
+    return true;
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    if (this.iframe_) {
+      removeElement(this.iframe_);
+      this.iframe_ = null;
+      this.iframePromise_ = null;
+    }
+    if (this.unlistenMessage_) {
+      this.unlistenMessage_();
+    }
+    return true;  // Call layoutCallback again.
   }
 }
 
-AMP.registerElement('amp-vk', AmpVk);
+if (!window.isAmpVkRegistered) {
+  AMP.extension('amp-vk', '0.1', AMP => {
+    AMP.registerElement('amp-vk', AmpVk);
+  });
+  window.isAmpVkRegistered = true;
+}
--- extensions/amp-vk/0.1/test/test-amp-vk.js
+++ extensions/amp-vk/0.1/test/test-amp-vk.js
@@ -14,25 +14,138 @@
  * limitations under the License.
  */
 
-import {AmpVk} from '../amp-vk';
+const POST_PARAMS = {
+  hash: 'Yc8_Z9pnpg8aKMZbVcD-jK45eAk',
+  ownerid: '1',
+  postid: '45616',
+};
+
+import '../amp-vk';
+import {Layout} from '../../../../src/layout';
 
 describes.realWin('amp-vk', {
   amp: {
     extensions: ['amp-vk'],
-  }
+  },
 }, env => {
 
-  let win;
-  let element;
+  let win, doc;
 
   beforeEach(() => {
     win = env.win;
-    element = win.document.createElement('amp-vk');
-    win.document.body.appendChild(element);
+    doc = win.document;
   });
 
-  it('should have hello world when built', () => {
-    element.build();
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  function createAmpVkElement(dataParams, layout) {
+    const ele = doc.createElement('amp-vk');
+
+    for (const param in dataParams) {
+      ele.setAttribute(`data-${param}`, dataParams[param]);
+    }
+
+    ele.setAttribute('width', 500);
+    ele.setAttribute('height', 300);
+
+    if (layout) {
+      ele.setAttribute('layout', layout);
+    }
+
+    doc.body.appendChild(ele);
+
+    return ele.build().then(() => {
+      return ele.layoutCallback();
+    }).then(() => ele);
+  }
+
+  it('requires data-hash', () => {
+    const params = Object.assign({}, POST_PARAMS);
+    delete params.hash;
+    return createAmpVkElement(params).should.eventually.be.rejectedWith(
+        /The data-hash attribute is required for/);
   });
+
+  it('requires data-ownerid', () => {
+    const params = Object.assign({}, POST_PARAMS);
+    delete params.ownerid;
+    return createAmpVkElement(params).should.eventually.be.rejectedWith(
+        /The data-ownerid attribute is required for/);
+  });
+
+  it('requires data-postid', () => {
+    const params = Object.assign({}, POST_PARAMS);
+    delete params.postid;
+    return createAmpVkElement(params).should.eventually.be.rejectedWith(
+        /The data-postid attribute is required for/);
+  });
+
+  it('renders iframe in amp-vk', () => {
+    return createAmpVkElement(POST_PARAMS).then(vkPost => {
+      const iframe = vkPost.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+    });
+  });
+
+  it('renders responsively', () => {
+    return createAmpVkElement(POST_PARAMS, Layout.RESPONSIVE).then(vkPost => {
+      const iframe = vkPost.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+      expect(iframe.className).to.match(/i-amphtml-fill-content/);
+    });
+  });
+
+  it('sets correct src url to the vk iFrame', () => {
+    return createAmpVkElement(POST_PARAMS, Layout.RESPONSIVE).then(vkPost => {
+      const iframe = vkPost.querySelector('iframe');
+      const referrer = encodeURIComponent(vkPost.ownerDocument.referrer);
+      const url = encodeURIComponent(
+          vkPost.ownerDocument.location.href.replace(/#.*$/, '')
+      );
+      const startWidth = iframe.offsetWidth;
+      const correctIFrameSrc = `https://vk.com/widget_post.php?\
+app=0&width=100%25&startWidth=${startWidth}\
+&_ver=1&owner_id=1&post_id=45616&hash=Yc8_Z9pnpg8aKMZbVcD-jK45eAk&\
+url=${url}&referrer=${referrer}&title=`;
+      expect(iframe).to.not.be.null;
+      expect(iframe.src).to.equal(correctIFrameSrc);
+    });
+  });
+
+  it('resizes amp-vk element in response to messages from VK iframe', () => {
+    return createAmpVkElement(POST_PARAMS).then(vkPost => {
+      const impl = vkPost.implementation_;
+      const iframe = vkPost.querySelector('iframe');
+      const changeHeight = sandbox.spy(impl, 'changeHeight');
+      const fakeHeight = 555;
+
+      expect(iframe).to.not.be.null;
+
+      generatePostMessage(vkPost, iframe, fakeHeight);
+
+      expect(changeHeight).to.be.calledOnce;
+      expect(changeHeight.firstCall.args[0]).to.equal(fakeHeight);
+    });
+  });
+
+  it('removes iframe after unlayoutCallback', () => {
+    return createAmpVkElement(POST_PARAMS).then(vkPost => {
+      const iframe = vkPost.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+      const obj = vkPost.implementation_;
+      obj.unlayoutCallback();
+      expect(vkPost.querySelector('iframe')).to.be.null;
+      expect(obj.iframe_).to.be.null;
+      expect(obj.unlayoutOnPause()).to.be.true;
+    });
+  });
+
+  function generatePostMessage(ins, iframe, height) {
+    ins.implementation_.handleVkIframeMessage_({
+      origin: 'https://vk.com',
+      source: iframe.contentWindow,
+      data: JSON.stringify([
+        'resize',
+        [height],
+      ]),
+    });
+  }
 });
