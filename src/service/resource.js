b52741ae84b34975603a3dfe09cabd5e8babe283
Gene Levitzky <levitzky@google.com>
2017-09-14 21:04:43 -0400

    Fixed isDisplayed

--- src/service/resource.js
+++ src/service/resource.js
@@ -556,7 +556,8 @@ export class Resource {
    * @return {boolean}
    */
   isDisplayed() {
-    return (this.layoutBox_.height > 0 || this.layoutBox_.width > 0) &&
+    return ((this.element.getAttribute('height') == 'fluid' ||
+          this.layoutBox_.height > 0) && this.layoutBox_.width > 0) &&
         !!this.element.ownerDocument &&
         !!this.element.ownerDocument.defaultView;
   }
