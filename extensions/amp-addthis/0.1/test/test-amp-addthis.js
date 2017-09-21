652b251f15c707e67e7699377a31ac3be837462f
Jason Rogers <jason.m.rogers@oracle.com>
2017-09-21 17:59:49 -0400

    Placate Travis (add LICENSE comments, comment out a link)

--- extensions/amp-addthis/0.1/test/test-amp-addthis.js
+++ extensions/amp-addthis/0.1/test/test-amp-addthis.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import {ConfigManager} from '../ConfigManager';
 import {getConfigManager} from '../amp-addthis';
 import {
