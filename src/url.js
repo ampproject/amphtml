04b077a836dbd70b2287a186a39c9355a3c14fce
Sriram Krishnan <sriramkrish85@gmail.com>
2017-09-27 16:38:26 -0700

    Lint

--- src/url.js
+++ src/url.js
@@ -54,7 +54,7 @@ export const SOURCE_ORIGIN_PARAM = '__amp_source_origin';
  * @return {string} origin
  */
 export function getWinOrigin(win) {
-  return win.origin || parseUrl(win.location.href).origin;
+    return win.origin || parseUrl(win.location.href).origin;
 }
 
 /**
