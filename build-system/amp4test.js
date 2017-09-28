b6a25492fc35e6ef5ba2dce6a08b0b8b26810fb3
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2017-09-28 13:52:42 -0700

    correct frame.html path (#11469)

--- build-system/amp4test.js
+++ build-system/amp4test.js
@@ -22,6 +22,9 @@ const app = module.exports = require('express').Router();
 app.use('/compose-doc', function(req, res) {
   res.setHeader('X-XSS-Protection', '0');
   const mode = process.env.SERVE_MODE == 'compiled' ? '' : 'max.';
+  const frameHtml = process.env.SERVE_MODE == 'compiled'
+      ? 'dist.3p/current-min/frame.html'
+      : 'dist.3p/current/frame.max.html';
   const extensions = req.query.extensions;
   let extensionScripts = '';
   if (!!extensions) {
@@ -56,7 +59,7 @@ app.use('/compose-doc', function(req, res) {
   </script>
   <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
   <script async src="/dist/${process.env.SERVE_MODE == 'compiled' ? 'v0' : 'amp'}.js"></script>
-  <meta name="amp-3p-iframe-src" content="http://localhost:9876/dist.3p/current/frame.max.html">
+  <meta name="amp-3p-iframe-src" content="http://localhost:9876/${frameHtml}">
   ${extensionScripts}
   <style amp-custom>${req.query.css}</style>
 </head>
