5a8e877fafd8a59bdec0f97acb632709761f72fe
Justin Ridgewell <jridgewell@google.com>
2017-10-02 17:52:10 -0400

    Fix merge conflict

--- src/custom-element.js
+++ src/custom-element.js
@@ -39,10 +39,6 @@ import {reportError} from './error';
 import {setStyle} from './style';
 import * as dom from './dom';
 import {toWin} from './types';
-import {setStyle} from './style';
-import {LayoutDelayMeter} from './layout-delay-meter';
-import {ResourceState} from './service/resource';
-import {AmpEvents} from './amp-events';
 import {LayoutElement} from './service/layers-impl';
 
 const TAG = 'CustomElement';
