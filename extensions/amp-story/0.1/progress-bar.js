211bd047842f3ce49dc182e5ff508fc1d9db7c69
Alan Orozco <alanorozco@users.noreply.github.com>
2017-10-03 14:30:08 -0500

    Implement system layer components for amp-story (#11527)

---
+++ extensions/amp-story/0.1/progress-bar.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {dev} from '../../../src/log';
+import {scale, setStyles} from '../../../src/style';
+import {scopedQuerySelector} from '../../../src/dom';
+import {Services} from '../../../src/services';
+
+
+/**
+ * Progress bar for <amp-story>.
+ */
+export class ProgressBar {
+  /**
+   * @param {!Window} win
+   */
+  constructor(win) {
+    /** @private @const {!Window} */
+    this.win_ = win;
+
+    /** @private {boolean} */
+    this.isBuilt_ = false;
+
+    /** @private {?Element} */
+    this.root_ = null;
+
+    /** @private {number} */
+    this.activePageIndex_ = -1;
+
+    /** @private {number} */
+    this.pageCount_ = 0;
+
+    /** @private @const {!../../../src/service/vsync-impl.Vsync} */
+    this.vsync_ = Services.vsyncFor(this.win_);
+  }
+
+  /**
+   * @param {!Window} win
+   */
+  static create(win) {
+    return new ProgressBar(win);
+  }
+
+  /**
+   * @param {number} pageCount The number of pages in the story.
+   * @return {!Element}
+   */
+  build(pageCount) {
+    if (this.isBuilt_) {
+      return this.getRoot();
+    }
+
+    dev().assertNumber(pageCount);
+    dev().assert(pageCount > 0);
+
+    this.isBuilt_ = true;
+    this.pageCount_ = pageCount;
+
+    this.root_ = this.win_.document.createElement('ol');
+    this.root_.classList.add('i-amphtml-story-progress-bar');
+
+    for (let i = 0; i < this.pageCount_; i++) {
+      const pageProgressBar = this.win_.document.createElement('li');
+      pageProgressBar.classList.add('i-amphtml-story-page-progress-bar');
+      const pageProgressValue = this.win_.document.createElement('div');
+      pageProgressValue.classList.add('i-amphtml-story-page-progress-value');
+      pageProgressBar.appendChild(pageProgressValue);
+      this.root_.appendChild(pageProgressBar);
+    }
+
+    return this.getRoot();
+  }
+
+
+  /**
+   * @return {!Element}
+   */
+  getRoot() {
+    return dev().assertElement(this.root_);
+  }
+
+
+  /**
+   * @param {number} pageIndex The index to assert whether it is in bounds.
+   * @private
+   */
+  assertValidPageIndex_(pageIndex) {
+    dev().assert(pageIndex >= 0 && pageIndex < this.pageCount_,
+        `Page index ${pageIndex} is not between 0 and ${this.pageCount_}.`);
+  }
+
+
+  /**
+   * @param {number} pageIndex The index of the new active page.
+   * @public
+   */
+  setActivePageIndex(pageIndex) {
+    this.assertValidPageIndex_(pageIndex);
+    for (let i = 0; i < this.pageCount_; i++) {
+      if (i < pageIndex) {
+        this.updateProgress(i, 1.0);
+      } else {
+        this.updateProgress(i, 0.0);
+      }
+    }
+  }
+
+  /**
+   * @param {number} pageIndex The index of the page whose progress should be
+   *     changed.
+   * @param {number} progress A number from 0.0 to 1.0, representing the
+   *     progress of the current page.
+   * @public
+   */
+  updateProgress(pageIndex, progress) {
+    this.assertValidPageIndex_(pageIndex);
+    // Offset the index by 1, since nth-child indices start at 1 while
+    // JavaScript indices start at 0.
+    const nthChildIndex = pageIndex + 1;
+    const progressEl = scopedQuerySelector(this.root_,
+        `.i-amphtml-story-page-progress-bar:nth-child(${nthChildIndex}) ` +
+        '.i-amphtml-story-page-progress-value');
+    this.vsync_.mutate(() => {
+      setStyles(progressEl, {
+        'transform': scale(`${progress},1`),
+      });
+    });
+  }
+}
---
+++ extensions/amp-story/0.1/test/test-system-layer.js
@@ -0,0 +1,158 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {SystemLayer} from '../system-layer';
+import {EventType} from '../events';
+import {ProgressBar} from '../progress-bar';
+import {Services} from '../../../../src/services';
+
+
+const NOOP = () => {};
+
+
+describes.fakeWin('amp-story system layer', {}, env => {
+  let win;
+  let systemLayer;
+  let progressBarStub;
+  let progressBarRoot;
+
+  function matchEventHandlerThatExecutes(spy) {
+    return sandbox.match(handler => {
+      handler(new Event('covfefe'));
+      return spy.called;
+    });
+  }
+
+  function matchEvent(name, bubbles) {
+    return sandbox.match.has('type', name)
+        .and(sandbox.match.has('bubbles', bubbles));
+  }
+
+  function expectEventTransform(eventHandler, expectedEventType) {
+    const dispatchEvent = sandbox.spy();
+    const stopPropagation = sandbox.spy();
+
+    sandbox.stub(systemLayer, 'getRoot').returns({dispatchEvent});
+
+    eventHandler({stopPropagation});
+
+    expect(stopPropagation).to.be.calledOnce;
+    expect(dispatchEvent).to.have.been.calledWith(
+        matchEvent(expectedEventType, /* bubbles */ true));
+  }
+
+  beforeEach(() => {
+    win = env.win;
+
+    progressBarRoot = win.document.createElement('div');
+
+    progressBarStub = {
+      build: sandbox.stub().returns(progressBarRoot),
+      getRoot: sandbox.stub().returns(progressBarRoot),
+      setActivePageIndex: sandbox.spy(),
+      updateProgress: sandbox.spy(),
+    };
+
+    sandbox.stub(ProgressBar, 'create').returns(progressBarStub);
+
+    systemLayer = new SystemLayer(win);
+
+    sandbox.stub(Services, 'vsyncFor').returns({
+      mutate: fn => fn(),
+    });
+  });
+
+  it('should build UI', () => {
+    const addEventHandlers =
+        sandbox.stub(systemLayer, 'addEventHandlers_', NOOP);
+
+    const root = systemLayer.build();
+
+    expect(root).to.not.be.null;
+    expect(systemLayer.exitFullScreenBtn_).to.not.be.null;
+
+    expect(addEventHandlers).to.have.been.called;
+  });
+
+  it('should attach event handlers', () => {
+    const onExitFullScreenClick =
+        sandbox.stub(systemLayer, 'onExitFullScreenClick_');
+    const onCloseBookendClick =
+        sandbox.stub(systemLayer, 'onCloseBookendClick_');
+    const onMuteAudioClick = sandbox.stub(systemLayer, 'onMuteAudioClick_');
+    const onUnmuteAudioClick = sandbox.stub(systemLayer, 'onUnmuteAudioClick_');
+
+    const exitFullScreenBtnMock = {addEventListener: sandbox.spy()};
+    const closeBookendBtnMock = {addEventListener: sandbox.spy()};
+    const muteAudioBtnMock = {addEventListener: sandbox.spy()};
+    const unmuteAudioBtnMock = {addEventListener: sandbox.spy()};
+
+    sandbox.stub(systemLayer, 'exitFullScreenBtn_', exitFullScreenBtnMock);
+    sandbox.stub(systemLayer, 'closeBookendBtn_', closeBookendBtnMock);
+    sandbox.stub(systemLayer, 'muteAudioBtn_', muteAudioBtnMock);
+    sandbox.stub(systemLayer, 'unmuteAudioBtn_', unmuteAudioBtnMock);
+
+    systemLayer.addEventHandlers_();
+
+    expect(exitFullScreenBtnMock.addEventListener).to.have.been.calledWith(
+        'click', matchEventHandlerThatExecutes(onExitFullScreenClick));
+
+    expect(closeBookendBtnMock.addEventListener).to.have.been.calledWith(
+        'click', matchEventHandlerThatExecutes(onCloseBookendClick));
+
+    expect(muteAudioBtnMock.addEventListener).to.have.been.calledWith(
+        'click', matchEventHandlerThatExecutes(onMuteAudioClick));
+
+    expect(unmuteAudioBtnMock.addEventListener).to.have.been.calledWith(
+        'click', matchEventHandlerThatExecutes(onUnmuteAudioClick));
+  });
+
+  it('should dispatch EXIT_FULLSCREEN when button is clicked', () => {
+    expectEventTransform(
+        e => systemLayer.onExitFullScreenClick_(e), EventType.EXIT_FULLSCREEN);
+  });
+
+  it('should dispatch CLOSE_BOOKEND when button is clicked', () => {
+    expectEventTransform(
+        e => systemLayer.onCloseBookendClick_(e), EventType.CLOSE_BOOKEND);
+  });
+
+  it('should hide exit fullscreen button when not in fullscreen', () => {
+    const button = win.document.createElement('button');
+
+    sandbox.stub(systemLayer, 'exitFullScreenBtn_', button);
+
+    systemLayer.setInFullScreen(false);
+
+    expect(button.hasAttribute('hidden')).to.be.true;
+  });
+
+  it('should show exit fullscreen button when in fullscreen', () => {
+    const button = win.document.createElement('button');
+
+    sandbox.stub(systemLayer, 'exitFullScreenBtn_', button);
+
+    systemLayer.setInFullScreen(true);
+
+    expect(button.hasAttribute('hidden')).to.be.false;
+  });
+
+  it('should set the active page index', () => {
+    [0, 1, 2, 3, 4].forEach(index => {
+      systemLayer.setActivePageIndex(index);
+      progressBarStub.setActivePageIndex.should.have.been.calledWith(index);
+    });
+  });
+});
