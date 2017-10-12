724f83e43361eb2210c8565ebc5c90f49c93f2c1
Jon Keller <jonkeller@users.noreply.github.com>
2017-10-12 14:00:35 -0400

    Fixes amp-ad-exit handling of responses for different instance (#11666)
    * Ignores messages not for me, instead of complaining
    
    * Removes blank line

--- extensions/amp-ad-exit/0.1/amp-ad-exit.js
+++ extensions/amp-ad-exit/0.1/amp-ad-exit.js
@@ -334,7 +334,8 @@ export class AmpAdExit extends AMP.BaseElement {
           }
           const responseMsg = deserializeMessage(getData(event));
           if (!responseMsg ||
-              responseMsg['type'] != MessageType.IFRAME_TRANSPORT_RESPONSE) {
+              responseMsg['type'] != MessageType.IFRAME_TRANSPORT_RESPONSE ||
+              responseMsg['creativeId'] != this.ampAdResourceId_) {
             return;
           }
           this.assertValidResponseMessage_(responseMsg, event.origin);
@@ -353,10 +354,6 @@ export class AmpAdExit extends AMP.BaseElement {
   assertValidResponseMessage_(responseMessage, eventOrigin) {
     user().assert(responseMessage['message'],
         'Received empty response from 3p analytics frame');
-    user().assert(
-        responseMessage['creativeId'] == this.ampAdResourceId_,
-        'Received malformed message from 3p analytics frame: ' +
-        'creativeId missing or invalid');
     user().assert(responseMessage['vendor'],
         'Received malformed message from 3p analytics frame: ' +
         'vendor missing');
