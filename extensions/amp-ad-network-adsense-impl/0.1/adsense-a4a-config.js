ae145dd3a5fccb37e5abae89d0eae3fdeb660b77
keithwrightbos <keithwrightbos@users.noreply.github.com>
2017-10-17 16:28:13 -0400

    Remove AdSense/Doubleclick Fast Fetch and AdSense FF Delayed Request holdbacks (#11726)

--- extensions/amp-ad-network-adsense-impl/0.1/adsense-a4a-config.js
+++ extensions/amp-ad-network-adsense-impl/0.1/adsense-a4a-config.js
@@ -27,10 +27,8 @@ import {
 } from '../../../ads/google/a4a/traffic-experiments';
 import {isGoogleAdsA4AValidEnvironment} from '../../../ads/google/a4a/utils';
 import {
-  /* eslint no-unused-vars: 0 */ ExperimentInfo,
   getExperimentBranch,
   forceExperimentBranch,
-  randomlySelectUnsetExperiments,
 } from '../../../src/experiments';
 import {dev} from '../../../src/log';
 
@@ -39,12 +37,6 @@ export const ADSENSE_A4A_EXPERIMENT_NAME = 'expAdsenseA4A';
 
 /** @const @enum{string} @visibleForTesting */
 export const ADSENSE_EXPERIMENT_FEATURE = {
-  HOLDBACK_EXTERNAL_CONTROL: '21060732',
-  HOLDBACK_EXTERNAL: '21060733',
-  DELAYED_REQUEST_HOLDBACK_CONTROL: '21061056',
-  DELAYED_REQUEST_HOLDBACK_EXTERNAL: '21061057',
-  HOLDBACK_INTERNAL_CONTROL: '2092615',
-  HOLDBACK_INTERNAL: '2092616',
   CACHE_EXTENSION_INJECTION_CONTROL: '21060953',
   CACHE_EXTENSION_INJECTION_EXP: '21060954',
   IDENTITY_CONTROL: '21060939',
@@ -58,12 +50,6 @@ const TAG = 'amp-ad-network-adsense-impl';
 export const URL_EXPERIMENT_MAPPING = {
   '-1': MANUAL_EXPERIMENT_ID,
   '0': null,
-  // Holdback
-  '1': ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_EXTERNAL_CONTROL,
-  '2': ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_EXTERNAL,
-  // Delay Request
-  '3': ADSENSE_EXPERIMENT_FEATURE.DELAYED_REQUEST_HOLDBACK_CONTROL,
-  '4': ADSENSE_EXPERIMENT_FEATURE.DELAYED_REQUEST_HOLDBACK_EXTERNAL,
   // AMP Cache extension injection
   '5': ADSENSE_EXPERIMENT_FEATURE.CACHE_EXTENSION_INJECTION_CONTROL,
   '6': ADSENSE_EXPERIMENT_FEATURE.CACHE_EXTENSION_INJECTION_EXP,
@@ -89,42 +75,12 @@ export function adsenseIsA4AEnabled(win, element) {
     experimentId = URL_EXPERIMENT_MAPPING[urlExperimentId];
     dev().info(
         TAG, `url experiment selection ${urlExperimentId}: ${experimentId}.`);
-  } else {
-    // Not set via url so randomly set.
-    const experimentInfoMap =
-        /** @type {!Object<string, !ExperimentInfo>} */ ({});
-    experimentInfoMap[ADSENSE_A4A_EXPERIMENT_NAME] = {
-      isTrafficEligible: () => true,
-      branches: [
-        ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_INTERNAL_CONTROL,
-        ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_INTERNAL,
-      ],
-    };
-    // Note: Because the same experimentName is being used everywhere here,
-    // randomlySelectUnsetExperiments won't add new IDs if
-    // maybeSetExperimentFromUrl has already set something for this
-    // experimentName.
-    randomlySelectUnsetExperiments(win, experimentInfoMap);
-    experimentId = getExperimentBranch(win, ADSENSE_A4A_EXPERIMENT_NAME);
-    dev().info(
-        TAG, `random experiment selection ${urlExperimentId}: ${experimentId}`);
-
   }
   if (experimentId) {
     addExperimentIdToElement(experimentId, element);
     forceExperimentBranch(win, ADSENSE_A4A_EXPERIMENT_NAME, experimentId);
   }
-  return ![ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_EXTERNAL,
-    ADSENSE_EXPERIMENT_FEATURE.HOLDBACK_INTERNAL].includes(experimentId);
-}
-
-/**
- * @param {!Window} win
- * @return {boolean} whether fast fetch delayed request experiment is enabled.
- */
-export function fastFetchDelayedRequestEnabled(win) {
-  return getExperimentBranch(win, ADSENSE_A4A_EXPERIMENT_NAME) !=
-      ADSENSE_EXPERIMENT_FEATURE.DELAYED_REQUEST_HOLDBACK_EXTERNAL;
+  return true;
 }
 
 /**
