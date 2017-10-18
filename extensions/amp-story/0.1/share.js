33a01bf9b8357a4f2796cb9cd390d37f36e8e9b2
Alan Orozco <alanorozco@users.noreply.github.com>
2017-10-18 09:41:05 -0700

    Support link share in amp-story (#11720)

--- extensions/amp-story/0.1/share.js
+++ extensions/amp-story/0.1/share.js
@@ -14,10 +14,16 @@
  * limitations under the License.
  */
 import {Services} from '../../../src/services';
-import {isObject} from '../../../src/types';
-import {renderAsElement, renderSimpleTemplate} from './simple-template';
+import {Toast} from './toast';
+import {
+  copyTextToClipboard,
+  isCopyingToClipboardSupported,
+} from '../../../src/clipboard';
 import {dev, user} from '../../../src/log';
 import {dict} from './../../../src/utils/object';
+import {isObject} from '../../../src/types';
+import {listen} from '../../../src/event-helper';
+import {renderAsElement, renderSimpleTemplate} from './simple-template';
 
 
 /**
@@ -37,26 +43,23 @@ const SHARE_PROVIDER_NAME = dict({
 const SHARE_LIST_TEMPLATE = {
   tag: 'ul',
   attrs: dict({'class': 'i-amphtml-story-share-list'}),
-  children: [
-    {
-      tag: 'li',
-      children: [
-        {
-          tag: 'div',
-          attrs: dict({
-            'class':
-                'i-amphtml-story-share-icon i-amphtml-story-share-icon-link',
-          }),
-          text: 'Get Link', // TODO(alanorozco): i18n
-        },
-      ],
-    },
-  ],
 };
 
 
 /** @private @const {!./simple-template.ElementDef} */
 const SHARE_ITEM_TEMPLATE = {tag: 'li'};
+
+
+/** @private @const {!./simple-template.ElementDef} */
+const LINK_SHARE_ITEM_TEMPLATE = {
+  tag: 'div',
+  attrs: dict({
+    'class':
+        'i-amphtml-story-share-icon i-amphtml-story-share-icon-link',
+  }),
+  text: 'Get Link', // TODO(alanorozco): i18n
+};
+
 
 
 /**
@@ -102,6 +105,30 @@ function buildProvider(doc, shareType, opt_params) {
 
 
 /**
+ * @param {!Document} doc
+ * @param {string} url
+ * @return {!Element}
+ */
+function buildCopySuccessfulToast(doc, url) {
+  return renderAsElement(doc, /** @type {!./simple-template.ElementDef} */ ({
+    tag: 'div',
+    attrs: dict({'class': 'i-amphtml-story-copy-successful'}),
+    children: [
+      {
+        tag: 'div',
+        text: 'Link copied!', // TODO(alanorozco): i18n
+      },
+      {
+        tag: 'div',
+        attrs: dict({'class': 'i-amphtml-story-copy-url'}),
+        text: url,
+      },
+    ],
+  }));
+}
+
+
+/**
  * Social share widget for story bookend.
  */
 export class ShareWidget {
@@ -133,10 +160,46 @@ export class ShareWidget {
 
     this.root_ = renderAsElement(this.win_.document, SHARE_LIST_TEMPLATE);
 
+    this.maybeAddLinkShareButton_();
     this.maybeAddNativeShare_();
 
     return this.root_;
   }
+
+  /** @private */
+  maybeAddLinkShareButton_() {
+    if (!isCopyingToClipboardSupported(this.win_.document)) {
+      return;
+    }
+
+    const linkShareButton =
+        renderAsElement(this.win_.document, LINK_SHARE_ITEM_TEMPLATE);
+
+    this.add_(linkShareButton);
+
+    // TODO(alanorozco): Listen for proper tap event (i.e. fastclick)
+    listen(linkShareButton, 'click', e => {
+      e.preventDefault();
+      this.copyUrlToClipboard_();
+    });
+  }
+
+
+  /** @private */
+  // TODO(alanorozco): i18n for toast.
+  copyUrlToClipboard_() {
+    const url = Services.documentInfoForDoc(
+        /** @type {!../../../src/service/ampdoc-impl.AmpDoc} */ (
+        dev().assert(this.ampdoc_))).canonicalUrl;
+
+    if (!copyTextToClipboard(this.win_.document, url)) {
+      Toast.show(this.win_, 'Could not copy link to clipboard :(');
+      return;
+    }
+
+    Toast.show(this.win_, buildCopySuccessfulToast(this.win_.document, url));
+  }
+
 
   /** @private */
   maybeAddNativeShare_() {
