cebdff76d2d1d8ee091ac88eb637177c1c803f1f
Ali Ghassemi <aghassemi@google.com>
2017-10-19 13:17:25 -0700

    more test

--- extensions/amp-twitter/0.1/test/test-amp-twitter.js
+++ extensions/amp-twitter/0.1/test/test-amp-twitter.js
@@ -98,12 +98,16 @@ describes.realWin('amp-twitter', {
       const bad3 = 'https://twitter.com/1cram2force/status?ref=';
       const bad4 = '<div>nonumber</div>';
       const bad5 = 'AA585110598171631616?ref=twsrc%5Etfw';
+      const bad6 = '';
+      const bad7 = '  ';
 
       expect(cleanupTweetId_(bad1)).to.equal(bad1);
       expect(cleanupTweetId_(bad2)).to.equal(bad2);
       expect(cleanupTweetId_(bad3)).to.equal(bad3);
       expect(cleanupTweetId_(bad4)).to.equal(bad4);
       expect(cleanupTweetId_(bad5)).to.equal(bad5);
+      expect(cleanupTweetId_(bad6)).to.equal(bad6);
+      expect(cleanupTweetId_(bad7)).to.equal(bad7);
     });
 
     it('cleans up bad tweet id with ref query string at end', () => {
