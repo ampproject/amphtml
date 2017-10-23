7082494f9de2000888dd113ad17957a17c6b96de
Jon Newmuis <newmuis@google.com>
2017-10-23 16:52:18 -0400

    Add missing class

---
+++ extensions/amp-story/0.1/page-advancement.js
@@ -0,0 +1,314 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {dev, user} from '../../../src/log';
+import {isFiniteNumber} from '../../../src/types';
+import {scopedQuerySelector} from '../../../src/dom';
+import {listenOnce} from '../../../src/event-helper';
+import {Services} from '../../../src/services';
+import {VideoEvents} from '../../../src/video-interface';
+
+
+/** @private @enum {!RegExp} */
+const TIME_REGEX = {
+  MILLISECONDS: /^(\d+)ms$/,
+  SECONDS: /^(\d+)s$/,
+};
+
+
+/** @const {number} */
+const POLL_INTERVAL_MS = 250;
+
+
+/**
+ * @param {string} autoAdvanceStr 
+ * @return {?number} The delay before the page should automatically advance to
+ *     the next page, or null if the page will not advance based on time.
+ * @private
+ */
+function getAutoAdvanceDelayMs(autoAdvanceStr) {
+  if (TIME_REGEX.MILLISECONDS.test(autoAdvanceStr)) {
+    return Number(TIME_REGEX.MILLISECONDS.exec(autoAdvanceStr)[1]);
+  } else if (TIME_REGEX.SECONDS.test(autoAdvanceStr)) {
+    return Number(TIME_REGEX.SECONDS.exec(autoAdvanceStr)[1]) * 1000;
+  }
+
+  return null;
+}
+
+/**
+ * 
+ */
+export class AdvancementConfig {
+  constructor(win) {
+    /** @private @const {!../../../src/service/timer-impl.Timer} */
+    this.timer_ = Services.timerFor(win);
+
+    /** @private @const {!Array<!>} */
+    this.progressListeners_ = [];
+
+    /** @private @const {!Array<!>} */
+    this.advanceListeners_ = [];
+
+    /** @private {boolean} */
+    this.isRunning_ = false;
+  }
+
+  addProgressListener(progressListener) {
+    this.progressListeners_.push(progressListener);
+  }
+
+  addAdvanceListener(advanceListener) {
+    this.advanceListeners_.push(advanceListener);
+  }
+
+  start() {
+    this.isRunning_ = true;
+  }
+
+  stop() {
+    this.isRunning_ = false;
+  }
+
+  isRunning() {
+    return this.isRunning_;
+  }
+
+  /**
+   * @return @protected {number}
+   */
+  getProgress() {
+    return 1;
+  }
+
+  onProgressUpdate() {
+    const progress = this.getProgress();
+    this.progressListeners_.forEach(progressListener => {
+      progressListener(progress);
+    });
+  }
+
+  onAdvance() {
+    this.advanceListeners_.forEach(advanceListener => {
+      advanceListener();
+    });
+  }
+
+
+  /**
+   * @param {!Element} rootElement 
+   * @return {!AdvancementConfig} 
+   */
+  static forElement(rootElement) {
+    const win = rootElement.ownerDocument.defaultView;
+    const autoAdvanceStr = rootElement.getAttribute('auto-advance-after');
+
+    if (!autoAdvanceStr) {
+      return new ManualAdvancement(win);
+    }
+
+    const delayMs = getAutoAdvanceDelayMs(autoAdvanceStr);
+    if (delayMs !== null) {
+      user().assert(isFiniteNumber(delayMs) && delayMs > 0,
+          `Invalid automatic advance delay '${autoAdvanceStr}' ` +
+          `for page '${rootElement.id}'.`);
+
+      return new TimeBasedAdvancement(win, delayMs);
+    }
+
+    return new MediaBasedAdvancement(win, user().assertElement(
+        scopedQuerySelector(rootElement, `#${autoAdvanceStr}`),
+        'ID specified for automatic advance does not refer to any element ' +
+        `on page '${rootElement.id}'.`));
+  }
+}
+
+class ManualAdvancement extends AdvancementConfig {}
+
+class TimeBasedAdvancement extends AdvancementConfig {
+  constructor(win, delayMs) {
+    super(win);
+
+    /** @private @const {number} */
+    this.delayMs_ = delayMs;
+
+    /** @private {?number} */
+    this.startTimeMs_ = null;
+
+    /** @private {number|string|null} */
+    this.timeoutId_ = null;
+  }
+
+
+  /**
+   * @return {number} 
+   * @private
+   */
+  getCurrentTimestampMs_() {
+    return Date.now();
+  }
+
+
+  /** @override */
+  start() {
+    super.start();
+    this.startTimeMs_ = this.getCurrentTimestampMs_();
+
+    this.timeoutId_ = this.timer_.delay(() => this.onAdvance(), this.delayMs_);
+
+    this.timer_.poll(POLL_INTERVAL_MS, () => {
+      this.onProgressUpdate();
+      return !this.isRunning();
+    });
+  }
+
+
+  /** @override */
+  stop() {
+    super.stop();
+
+    if (this.timeoutId_ !== null) {
+      this.timer_.cancel(this.timeoutId_);
+    }
+  }
+
+
+  /** @override */
+  getProgress() {
+    if (this.startTimeMs_ === null) {
+      return 0;
+    }
+
+    const progress =
+        (this.getCurrentTimestampMs_() - this.startTimeMs_) / this.delayMs_;
+
+    return Math.min(Math.max(progress, 0), 1);
+  }
+}
+
+class MediaBasedAdvancement extends AdvancementConfig {
+  constructor(win, element) {
+    super(win);
+
+    /** @private @const {!Element} */
+    this.element_ = element;
+
+    /** @private {?Element} */
+    this.mediaElement_ = null;
+
+    /** @private {?VideoInterface} */
+    this.video_ = null;
+  }
+
+
+  /**
+   * Determines whether the element for auto advancement implements the video
+   * interface.
+   * @return {boolean} true, if the specified element implements the video
+   *     interface.
+   * @private
+   */
+  isVideoInterfaceVideo_() {
+    return this.element_.classList.contains('i-amphtml-video-interface');
+  }
+
+
+  /**
+   * Gets an HTMLMediaElement from an element that wraps it.
+   * @return {?Element} The underlying HTMLMediaElement, if one exists.
+   * @private
+   */
+  getMediaElement_() {
+    const tagName = this.element_.tagName.toLowerCase();
+
+    if (this.element_ instanceof HTMLMediaElement) {
+      return this.element_;
+    } else if (this.element_.hasAttribute('background-audio') &&
+        (tagName === 'amp-story' || tagName === 'amp-story-page')) {
+      return scopedQuerySelector(this.element_,
+          '.i-amphtml-story-background-audio');
+    } else if (tagName === 'amp-audio') {
+      return scopedQuerySelector(this.element_, 'audio');
+    }
+
+    return null;
+  }
+
+
+  /** @override */
+  start() {
+    super.start();
+
+    if (this.isVideoInterfaceVideo_()) {
+      this.startVideoInterfaceElement_();
+      return;
+    }
+
+    if (!this.mediaElement_) {
+      this.mediaElement_ = this.getMediaElement_(this.element_);
+    }
+
+    if (this.mediaElement_) {
+      this.startHtmlMediaElement_();
+      return;
+    }
+
+    user().error(`Element with ID ${this.element_.id} is not a media element ` +
+        'supported for automatic advancement.');
+  }
+
+
+  startHtmlMediaElement_() {
+    listenOnce(this.mediaElement_, 'ended', () => this.onAdvance());
+    listenOnce(this.mediaElement_, 'timeupdate', () => this.onProgressUpdate());
+  }
+
+  startVideoInterfaceElement_() {
+    this.element_.getImpl().then(video => {
+      this.video_ = video;
+    });
+
+    listenOnce(this.element_, VideoEvents.ENDED, () => this.onAdvance(), {capture: true});
+
+    this.timer_.poll(POLL_INTERVAL_MS, () => {
+      this.onProgressUpdate();
+      return !this.isRunning();
+    });
+  }
+
+
+  /** @override */
+  stop() {
+    super.stop();
+  }
+
+
+  /** @override */
+  getProgress() {
+    if (this.isVideoInterfaceVideo_()) {
+      if (this.video_ && this.video_.getDuration()) {
+        return this.video_.getCurrentTime() / this.video_.getDuration();
+      }
+
+      return 0;
+    }
+
+    if (this.mediaElement_ && this.mediaElement_.duration) {
+      return this.mediaElement_.currentTime / this.mediaElement_.duration;
+    }
+
+    return super.getProgress();
+  }
+}
