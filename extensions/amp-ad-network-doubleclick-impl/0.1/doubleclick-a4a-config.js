75461ca569fe154dc8595a41a89dfed568834059
bradfrizzell <bradfrizzell@users.noreply.github.com>
2017-10-23 17:21:21 -0400

    Fix bug that useRemoteHtml wasn't being used in Doubleclick-a4a-config (#11794)
    * Actually use useRemoteHtml param
    
    * Check for RTC

--- extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config.js
@@ -130,7 +130,8 @@ export class DoubleclickA4aEligibility {
           win, DFP_CANONICAL_FF_EXPERIMENT_NAME, experimentId);
     }
 
-    if ('useSameDomainRenderingUntilDeprecated' in element.dataset ||
+    if ((useRemoteHtml && !element.getAttribute('rtc-config')) ||
+        'useSameDomainRenderingUntilDeprecated' in element.dataset ||
         element.hasAttribute('useSameDomainRenderingUntilDeprecated')) {
       return false;
     }
