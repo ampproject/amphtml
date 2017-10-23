632a34218b069a2a43726b4b87795b0a99cfccf2
Ali Ghassemi <aghassemi@google.com>
2017-10-23 10:06:08 -0700

    video-manager: honour

--- src/service/video-manager-impl.js
+++ src/service/video-manager-impl.js
@@ -238,8 +238,12 @@ export class VideoManager {
    * @private
    */
   maybeInstallVisibilityObserver_(entry) {
-    listen(entry.video.element, VideoEvents.VISIBILITY, () => {
-      entry.updateVisibility();
+    listen(entry.video.element, VideoEvents.VISIBILITY, details => {
+      if (details && details.data && details.data.visible) {
+        entry.updateVisibility(/* opt_forceVisible */ true);
+      } else {
+        entry.updateVisibility();
+      }
     });
 
     listen(entry.video.element, VideoEvents.RELOAD, () => {
@@ -832,6 +836,8 @@ class VideoEntry {
 
       // Only muted videos are allowed to autoplay
       this.video.mute();
+
+      //
 
       if (this.video.isInteractive()) {
         this.autoplayInteractiveVideoBuilt_();
@@ -1755,18 +1761,23 @@ class VideoEntry {
   /**
    * Called by all possible events that might change the visibility of the video
    * such as scrolling or {@link ../video-interface.VideoEvents#VISIBILITY}.
+   * @param {boolean} opt_forceVisible
    * @package
    */
-  updateVisibility() {
+  updateVisibility(opt_forceVisible) {
     const wasVisible = this.isVisible_;
 
     // Measure if video is now in viewport and what percentage of it is visible.
     const measure = () => {
-      // Calculate what percentage of the video is in viewport.
-      const change = this.video.element.getIntersectionChangeEntry();
-      const visiblePercent = !isFiniteNumber(change.intersectionRatio) ? 0
-          : change.intersectionRatio * 100;
-      this.isVisible_ = visiblePercent >= VISIBILITY_PERCENT;
+      if (opt_forceVisible) {
+        this.isVisible_ = true;
+      } else {
+        // Calculate what percentage of the video is in viewport.
+        const change = this.video.element.getIntersectionChangeEntry();
+        const visiblePercent = !isFiniteNumber(change.intersectionRatio) ? 0
+            : change.intersectionRatio * 100;
+        this.isVisible_ = visiblePercent >= VISIBILITY_PERCENT;
+      }
     };
 
     // Mutate if visibility changed from previous state
