9b538aa2423cc12bcf8e7f9a82112a5fec59b7f5
Jon Newmuis <newmuis@users.noreply.github.com>
2017-10-23 19:39:11 -0400

    Fix bug in domain whitelist check (#11755)
    * Change progress bar to not handle active page, since active page updates progress bar through PAGE_PROGRESS events
    
    * Revert "Change progress bar to not handle active page, since active page updates progress bar through PAGE_PROGRESS events"
    
    This reverts commit 0500899056e84a85138352aa6ea7edc8d9e033d0.
    
    * Fix bug in domain whitelist check
    
    * Refactor amp-story origin whitelist to be testable, and add unit tests.
    
    * Fix lint and type and jsdoc errors

--- extensions/amp-story/0.1/test/test-amp-story.js
+++ extensions/amp-story/0.1/test/test-amp-story.js
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 import {AnalyticsTrigger} from '../analytics';
+import {AmpStory} from '../amp-story';
 import {EventType} from '../events';
 import {KeyCodes} from '../../../../src/utils/key-codes';
 import {VariableService} from '../variable-service';
 
 
 const NOOP = () => {};
+const IDENTITY_FN = x => x;
 
 
 describes.realWin('amp-story', {
@@ -313,5 +315,60 @@ describes.realWin('amp-story', {
 
     expect(pages[0].hasAttribute('active')).to.be.false;
     expect(pages[1].hasAttribute('active')).to.be.true;
+  });
+});
+
+
+describes.realWin('amp-story origin whitelist', {
+  amp: {
+    extensions: ['amp-story'],
+  },
+}, env => {
+  let win;
+  let element;
+  let story;
+
+  beforeEach(() => {
+    win = env.win;
+    element = win.document.createElement('amp-story');
+    win.document.body.appendChild(element);
+
+    story = new AmpStory(element);
+    story.hashOrigin_ = IDENTITY_FN;
+  });
+
+  it('should allow exact whitelisted origin with https scheme', () => {
+    story.originWhitelist_ = ['example.com'];
+    expect(story.isOriginWhitelisted_('https://example.com')).to.be.true;
+  });
+
+  it('should allow exact whitelisted origin with http scheme', () => {
+    story.originWhitelist_ = ['example.com'];
+    expect(story.isOriginWhitelisted_('http://example.com')).to.be.true;
+  });
+
+  it('should allow www subdomain of origin', () => {
+    story.originWhitelist_ = ['example.com'];
+    expect(story.isOriginWhitelisted_('https://www.example.com')).to.be.true;
+  });
+
+  it('should allow subdomain of origin', () => {
+    story.originWhitelist_ = ['example.com'];
+    expect(story.isOriginWhitelisted_('https://foobar.example.com')).to.be.true;
+  });
+
+  it('should not allow exact whitelisted domain under different tld', () => {
+    story.originWhitelist_ = ['example.com'];
+    expect(story.isOriginWhitelisted_('https://example.co.uk')).to.be.false;
+  });
+
+  it('should not allow exact whitelisted domain infixed in another tld', () => {
+    story.originWhitelist_ = ['example.co.uk'];
+    expect(story.isOriginWhitelisted_('https://example.co')).to.be.false;
+  });
+
+  it('should not allow domain that contains whitelisted domain', () => {
+    story.originWhitelist_ = ['example.co'];
+    expect(story.isOriginWhitelisted_('https://example.co.uk')).to.be.false;
   });
 });
