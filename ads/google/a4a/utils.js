40d3c48e44b69b1d9eb382f998b39a3bc3c10e38
keithwrightbos <keithwrightbos@users.noreply.github.com>
2017-10-24 15:32:52 -0400

    AdSense/Doubleclick Fast Fetch: support debug experiment id force on local dev (#11800)
    * Remove AdSense/Doubleclick Fast Fetch and AdSense FF Delayed Request holdbacks
    
    * Doubleclick Fast Fetch config: remove beta attribute
    
    * Adsense/Doubleclick Fast Fetch: allow for debug experiment ids for local dev
    
    * PR feedback
    
    * fix build error

--- ads/google/a4a/utils.js
+++ ads/google/a4a/utils.js
@@ -20,8 +20,8 @@ import {makeCorrelator} from '../correlator';
 import {getBinaryType} from '../../../src/experiments';
 import {getOrCreateAdCid} from '../../../src/ad-cid';
 import {dev} from '../../../src/log';
-import {dict} from '../../../src/utils/object';
 import {getMode} from '../../../src/mode';
+import {dict} from '../../../src/utils/object';
 import {parseUrl} from '../../../src/url';
 import {parseJson} from '../../../src/json';
 import {DomFingerprint} from '../../../src/utils/dom-fingerprint';
@@ -229,6 +229,8 @@ export function googlePageParameters(win, nodeOrDoc, startTime) {
           'ish': win != win.top ? viewportSize.height : null,
           'art': art == '0' ? null : art,
           'vis': visibilityStateCodes[visibilityState] || '0',
+          'debug_experiment_id':
+              (/,?deid=(\d+)/i.exec(win.location.hash) || [])[1] || null,
           'url': documentInfo.canonicalUrl,
           'top': win != win.top ? topWindowUrlOrDomain(win) : null,
           'loc': win.location.href == documentInfo.canonicalUrl ?
