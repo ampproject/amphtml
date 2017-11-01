ec6b0907e46248a097e97744c579a96ad194d8c3
jonkeller <jonkeller@google.com>
2017-11-01 19:07:57 -0400

    Refines regex by using negative lookahead

--- src/service/variable-source.js
+++ src/service/variable-source.js
@@ -243,6 +243,6 @@ export class VariableSource {
     // FOO_BAR(arg1,arg2)
     // FOO_BAR(arg1, arg2)
     return new RegExp('\\$?(' + all + ')' +
-        '(?:\\(([0-9a-zA-Z-_.]+(?:,\\s*[0-9a-zA-Z-_.]*)*)\\))?', 'g');
+        '(?:\\((\\s*(?!,)(?:\\s*,?\\s*[0-9a-zA-Z-_.]*)*)\\))?', 'g');
   }
 }
