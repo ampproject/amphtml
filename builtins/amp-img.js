a74aeb33084918e3e507570c887c84e366b39dd2
keithwrightbos <keithwrightbos@users.noreply.github.com>
2017-11-07 17:55:57 -0500

    AMP creative inabox Server Side Rendering image inclusion (#11911)
    * Do not write image for amp-img/amp-pixel if already present
    
    * revert changes to examples/adsense.amp.html
    
    * check for i-amphtml-ssr attribute
    
    * PR feedback

--- builtins/amp-img.js
+++ builtins/amp-img.js
@@ -15,6 +15,7 @@
  */
 
 import {BaseElement} from '../src/base-element';
+import {scopedQuerySelector} from '../src/dom';
 import {isLayoutSizeDefined} from '../src/layout';
 import {registerElement} from '../src/service/custom-element-registry';
 import {srcsetFromElement, srcsetFromSrc} from '../src/srcset';
@@ -122,7 +123,12 @@ export class AmpImg extends BaseElement {
       this.allowImgLoadFallback_ = false;
     }
 
-    this.img_ = new Image();
+    // For inabox SSR, image will have been written directly to DOM so no need
+    // to recreate.  Calling appendChild again will have no effect.
+    if (this.element.hasAttribute('i-amphtml-ssr')) {
+      this.img_ = scopedQuerySelector(this.element, 'img');
+    }
+    this.img_ = this.img_ || new Image();
     this.img_.setAttribute('async', '');
     if (this.element.id) {
       this.img_.setAttribute('amp-img-id', this.element.id);
