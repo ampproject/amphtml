8c72724e03d6ef699621fbe72ecf43bfcdf7ceef
Justin Ridgewell <justin@ridgewell.name>
2017-11-13 16:29:59 -0500

    Remove iOS 8 Viewport (#11991)
    * Remove iOS 8 Viewport
    
    iOS 8 is a 4 generations ago. It has less than 10% market share (likely
    much less) as of 11/6/2017 (https://developer.apple.com/support/app-store/).
    Any iOS 8 device can update to iOS 9.
    
    Additionally, iOS 8 is not served AMP document in SERP.
    
    * lint

--- src/service/viewport/viewport-impl.js
+++ src/service/viewport/viewport-impl.js
@@ -41,9 +41,6 @@ import {ViewportBindingNatural_} from './viewport-binding-natural';
 import {
   ViewportBindingIosEmbedWrapper_,
 } from './viewport-binding-ios-embed-wrapper';
-import {
-  ViewportBindingNaturalIosEmbed_,
-} from './viewport-binding-natural-ios-embed';
 
 
 const TAG_ = 'Viewport';
@@ -1078,13 +1075,7 @@ function createViewport(ampdoc) {
   let binding;
   if (ampdoc.isSingleDoc() &&
       getViewportType(ampdoc.win, viewer) == ViewportType.NATURAL_IOS_EMBED) {
-    // The overriding of document.body fails in iOS7.
-    // Also, iOS8 sometimes freezes scrolling.
-    if (Services.platformFor(ampdoc.win).getIosMajorVersion() > 8) {
-      binding = new ViewportBindingIosEmbedWrapper_(ampdoc.win);
-    } else {
-      binding = new ViewportBindingNaturalIosEmbed_(ampdoc.win, ampdoc);
-    }
+    binding = new ViewportBindingIosEmbedWrapper_(ampdoc.win);
   } else {
     binding = new ViewportBindingNatural_(ampdoc, viewer);
   }
@@ -1108,7 +1099,6 @@ const ViewportType = {
    * device.
    * See:
    * https://github.com/ampproject/amphtml/blob/master/spec/amp-html-layout.md
-   * and {@link ViewportBindingNaturalIosEmbed_} for more details.
    */
   NATURAL_IOS_EMBED: 'natural-ios-embed',
 };
