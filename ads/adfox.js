74d1db6f93fb3c4eebce6bae25cd4b91f310c6bb
Petr Kuznetsov <KuznetPA@gmail.com>
2017-11-17 22:30:47 +0300

    Added onRender and improve onError callbacks in Yandex and Adfox ads (#11756)
    * Added onRender and improve onError callbacks in Yandex and Adfox ads
    
    * changes for review

--- ads/adfox.js
+++ ads/adfox.js
@@ -33,16 +33,16 @@ export function adfox(global, data) {
  */
 function initAdFox(global, data) {
   const params = JSON.parse(data.adfoxParams);
-  const container = global.document.createElement('div');
 
-  container.setAttribute('id', 'adfox_container');
-  global.document.getElementById('c').appendChild(container);
+  createContainer(global, 'adfox_container');
 
   global.Ya.adfoxCode.create({
     ownerId: data.ownerId,
     containerId: 'adfox_container',
     params,
-    onLoad: data => checkLoading(global, data),
+    onLoad: (data, onRender, onError) => {
+      checkLoading(global, data, onRender, onError);
+    },
     onRender: () => global.context.renderStart(),
     onError: () => global.context.noContentAvailable(),
     onStub: () => global.context.noContentAvailable(),
@@ -52,16 +52,29 @@ function initAdFox(global, data) {
 /**
  * @param {!Window} global
  * @param {!Object} data
+ * @param {!Object} onRender
+ * @param {!Object} onError
  */
-function checkLoading(global, data) {
+function checkLoading(global, data, onRender, onError) {
   if (data.bundleName === 'banner.direct') {
     const dblParams = {
       blockId: data.bundleParams.blockId,
       data: data.bundleParams.data,
-      isAdfox: true,
+      onRender,
+      onError,
     };
 
     yandex(global, dblParams);
     return false;
   }
+}
+
+/**
+ * @param {!Window} global
+ * @param {string} id
+ */
+function createContainer(global, id) {
+  const container = global.document.createElement('div');
+  container.setAttribute('id', id);
+  global.document.getElementById('c').appendChild(container);
 }
--- ads/yandex.js
+++ ads/yandex.js
@@ -23,7 +23,11 @@ const renderTo = 'yandex_rtb';
  * @param {!Object} data
  */
 export function yandex(global, data) {
-  validateData(data, ['blockId'], ['data', 'isAdfox']);
+  validateData(
+      data,
+      ['blockId'],
+      ['data', 'htmlAccessAllowed', 'onRender', 'onError']
+  );
 
   addToQueue(global, data);
   loadScript(global,
@@ -49,13 +53,17 @@ function addToQueue(global, data) {
       data: data.data,
       async: true,
       onRender: () => {
-        // Move adfox queue
-        if (data.isAdfox && global.Ya.adfoxCode.onRender) {
-          global.Ya.adfoxCode.onRender();
+        if (typeof data.onRender === 'function') {
+          data.onRender();
         }
+        global.context.renderStart();
       },
     }, () => {
-      global.context.noContentAvailable();
+      if (typeof data.onError === 'function') {
+        data.onError();
+      } else {
+        global.context.noContentAvailable();
+      }
     });
   });
 }
