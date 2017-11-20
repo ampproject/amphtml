c7ff111ee3b45e48e8fb87a6d65b612b2d38511d
Alfonso Corretti <acorretti@comscore.com>
2017-11-20 12:17:35 +0100

    Revert to 'pageId' as 'storyPageId' doesn't seem to be working

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -424,7 +424,7 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
         '&ns_type=view' +
         '&rn=${random}' +
         '&c8=${title}' +
-        '&c7=${canonicalUrl}%23${storyPageId}' +
+        '&c7=${canonicalUrl}%23${pageId}' +
         '&c9=${documentReferrer}' +
         '&cs_c7amp=${ampdocUrl}',
     },
