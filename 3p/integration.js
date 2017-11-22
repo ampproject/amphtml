a73ba90a8b048f14074671abe5c0f0e6ebe9612c
Marsel Zaripov <marszaripov@gmail.com>
2017-11-22 21:11:44 +0300

    Add amp-embed type=24smi (#12029)

--- 3p/integration.js
+++ 3p/integration.js
@@ -66,8 +66,10 @@ import {github} from './github';
 import {reddit} from './reddit';
 import {twitter} from './twitter';
 
-// 3P Ad Networks - please keep in alphabetic order
 import {_ping_} from '../ads/_ping_';
+
+// 3P Ad Networks - please keep in alphabetic order
+import {_24smi} from '../ads/24smi';
 import {a8} from '../ads/a8';
 import {a9} from '../ads/a9';
 import {accesstrade} from '../ads/accesstrade';
@@ -210,6 +212,7 @@ import {zucks} from '../ads/zucks';
  */
 const AMP_EMBED_ALLOWED = {
   _ping_: true,
+  '24smi': true,
   bringhub: true,
   dable: true,
   engageya: true,
@@ -238,6 +241,7 @@ if (getMode().test || getMode().localDev) {
 }
 
 // Keep the list in alphabetic order
+register('24smi', _24smi);
 register('a8', a8);
 register('a9', a9);
 register('accesstrade', accesstrade);
