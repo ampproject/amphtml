580974c9d41d5f65e28e6a1d62d617cc8bf71c21
sanjsanj <sanjsanj@hotmail.com>
2017-12-05 20:01:22 +0000

    Fix indentation

--- extensions/amp-story/0.1/system-layer.js
+++ extensions/amp-story/0.1/system-layer.js
@@ -214,10 +214,10 @@ export class SystemLayer {
       const target = dev().assertElement(e.target);
 
       if (matches(
-        target, `.${EXIT_FULLSCREEN_CLASS}, .${EXIT_FULLSCREEN_CLASS} *`)) {
+          target, `.${EXIT_FULLSCREEN_CLASS}, .${EXIT_FULLSCREEN_CLASS} *`)) {
         this.onExitFullScreenClick_(e);
       } else if (matches(
-        target, `.${ENTER_FULLSCREEN_CLASS}, .${ENTER_FULLSCREEN_CLASS} *`)) {
+          target, `.${ENTER_FULLSCREEN_CLASS}, .${ENTER_FULLSCREEN_CLASS} *`)) {
         this.onEnterFullScreenClick_(e);
       } else if (matches(target, `.${MUTE_CLASS}, .${MUTE_CLASS} *`)) {
         this.onMuteAudioClick_(e);
