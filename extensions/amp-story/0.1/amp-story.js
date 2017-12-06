9fcaff631de83ec95fab0d497bac51abdb049c92
Alan Orozco <orozcoalan@gmail.com>
2017-12-06 13:47:14 -0800

    Revert "Implement MediaPool for amp-story (#12156)"
    This reverts commit b29ade96df02a7cb8699161c8ea545d849a9cf1c.

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -40,7 +40,6 @@ import {Services} from '../../../src/services';
 import {relatedArticlesFromJson} from './related-articles';
 import {ShareWidget} from './share';
 import {
-  closest,
   fullscreenEnter,
   fullscreenExit,
   isFullscreenElement,
@@ -53,7 +52,7 @@ import {once} from '../../../src/utils/function';
 import {debounce} from '../../../src/utils/rate-limit';
 import {isExperimentOn, toggleExperiment} from '../../../src/experiments';
 import {registerServiceBuilder} from '../../../src/service';
-import {upgradeBackgroundAudio} from './audio';
+import {AudioManager, upgradeBackgroundAudio} from './audio';
 import {setStyle, setImportantStyles} from '../../../src/style';
 import {findIndex} from '../../../src/utils/array';
 import {ActionTrust} from '../../../src/action-trust';
@@ -65,7 +64,6 @@ import {Gestures} from '../../../src/gesture';
 import {SwipeXYRecognizer} from '../../../src/gesture-recognizers';
 import {dict} from '../../../src/utils/object';
 import {renderSimpleTemplate} from './simple-template';
-import {MediaPool, MediaType} from './media-pool';
 
 /** @private @const {string} */
 const PRE_ACTIVE_PAGE_ATTRIBUTE_NAME = 'pre-active';
@@ -84,12 +82,6 @@ const DESKTOP_WIDTH_THRESHOLD = 1024;
 
 /** @private @const {number} */
 const DESKTOP_HEIGHT_THRESHOLD = 550;
-
-/** @const {!Object<string, number>} */
-const MAX_MEDIA_ELEMENT_COUNTS = {
-  [MediaType.AUDIO]: 4,
-  [MediaType.VIDEO]: 8,
-};
 
 /**
  * @private @const {string}
@@ -163,6 +155,9 @@ export class AmpStory extends AMP.BaseElement {
     /** @const @private {!AmpStoryVariableService} */
     this.variableService_ = new AmpStoryVariableService();
 
+    /** @const @private {!AudioManager} */
+    this.audioManager_ = new AudioManager(this.win, this.element);
+
     /** @private @const {!function():!Promise<?./bookend.BookendConfigDef>} */
     this.loadBookendConfig_ = once(() => this.loadBookendConfigImpl_());
 
@@ -206,10 +201,6 @@ export class AmpStory extends AMP.BaseElement {
 
     /** @private {!AmpStoryHint} */
     this.ampStoryHint_ = new AmpStoryHint(this.win);
-
-    /** @private {!MediaPool} */
-    this.mediaPool_ = new MediaPool(this.win, MAX_MEDIA_ELEMENT_COUNTS,
-        element => this.getElementDistanceFromActivePage_(element));
   }
 
 
@@ -324,6 +315,18 @@ export class AmpStory extends AMP.BaseElement {
     this.element.addEventListener(EventType.SHOW_NO_PREVIOUS_PAGE_HELP, () => {
       this.ampStoryHint_.showFirstPageHintOverlay();
     });
+
+    this.element.addEventListener('play', e => {
+      if (e.target instanceof HTMLMediaElement) {
+        this.audioManager_.play(e.target);
+      }
+    }, true);
+
+    this.element.addEventListener('pause', e => {
+      if (e.target instanceof HTMLMediaElement) {
+        this.audioManager_.stop(e.target);
+      }
+    }, true);
 
     const gestures = Gestures.get(this.element,
         /* shouldNotPreventDefault */ true);
@@ -561,7 +564,6 @@ export class AmpStory extends AMP.BaseElement {
         (pageEl, index) => {
           return pageEl.getImpl().then(pageImpl => {
             this.pages_[index] = pageImpl;
-            pageImpl.setMediaPool(this.mediaPool_);
           });
         });
 
@@ -655,7 +657,6 @@ export class AmpStory extends AMP.BaseElement {
           }
         })
         .then(() => this.preloadPagesByDistance_())
-        .then(() => this.reapplyMuting_())
         .then(() => this.forceRepaintForSafari_());
   }
 
@@ -955,7 +956,9 @@ export class AmpStory extends AMP.BaseElement {
       pagesByDistance.forEach((pageIds, distance) => {
         pageIds.forEach(pageId => {
           const page = this.getPageById_(pageId);
-          page.setDistance(distance);
+          setImportantStyles(page.element, {
+            transform: `translateY(${100 * distance}%)`,
+          });
         });
       });
     });
@@ -1088,46 +1091,12 @@ export class AmpStory extends AMP.BaseElement {
     return findIndex(this.pages_, page => page === desiredPage);
   }
 
-
-  /**
-   * @param {!Element} element The element whose containing AmpStoryPage should
-   *     be retrieved
-   * @return {!./amp-story-page.AmpStoryPage} The AmpStoryPage containing the
-   *     specified element.
-   */
-  getPageContainingElement_(element) {
-    const pageIndex = findIndex(this.pages_, page => {
-      const pageEl = closest(element, el => {
-        return el === page.element;
-      });
-
-      return !!pageEl;
-    });
-
-    return dev().assert(this.pages_[pageIndex],
-        'Element not contained on any amp-story-page');
-  }
-
-
-  /**
-   * @param {!Element} element The element whose distance should be retrieved.
-   * @return {number} The number of pages the specified element is from the
-   *     currently active page.
-   */
-  getElementDistanceFromActivePage_(element) {
-    const page = this.getPageContainingElement_(element);
-    return page.getDistance();
-  }
-
-
   /**
    * Mutes the audio for the story.
    * @private
    */
   mute_() {
-    this.pages_.forEach(page => {
-      page.muteAllMedia();
-    });
+    this.audioManager_.muteAll();
     this.toggleMutedAttribute_(true);
   }
 
@@ -1136,33 +1105,9 @@ export class AmpStory extends AMP.BaseElement {
    * @private
    */
   unmute_() {
-    this.mediaPool_.blessAll().then(() => {
-      this.activePage_.unmuteAllMedia();
-    });
+    this.audioManager_.unmuteAll();
     this.toggleMutedAttribute_(false);
   }
-
-
-  /**
-   * Reapplies the muting status for the currently-active media in the story.
-   */
-  reapplyMuting_() {
-    const isMuted = this.isMuted_();
-    if (!isMuted) {
-      this.mute_();
-      this.unmute_();
-    }
-  }
-
-
-  /**
-   * @return {boolean} Whether the story is currently muted.
-   * @private
-   */
-  isMuted_() {
-    return this.element.hasAttribute(AUDIO_MUTED_ATTRIBUTE);
-  }
-
 
   /**
    * Toggles mute or unmute attribute on element.
