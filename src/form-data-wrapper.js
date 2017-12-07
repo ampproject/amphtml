ampproject/amphtml

commit 05fbc66042cc4f3629c61bea624cd3882eb2bb93

Use FormDataWrapper instead of native FormData (#12084)
This is a pre-requitesite for #11294.

Formdata#entries is not supported by IE, Edge and Safari, but is useful
for accessing content of the a FormData object. On IE, Edge and
Safari, the only available method on FormData is append, which means
that client code can't read from a FormData. Reading entries in a
FormData will be required by #11294 to serialize a FormData object
into a plain old JavaScript object so that it can be passed to
postMessage() (i.e., can be cloned using the structured clone
algorithm).

Since polyfilling FormData#entries requires patching both the constructor
and the append method and can be ugly to implement, a new unified API
FormDataWrapper is added to provide a consistent way to deal with
FormData. Callers will be banned from calling new FormData(), and
will have to use new FormDataWrapper(). When a native FormData is
needed to be passed to the native fetch or XMLHttpRequest API, callers
would need to call formDataWrapper.getFormData().

src/service/xhr-impl.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import {FormDataWrapper} from '../form-data-wrapper';
 import {Services} from '../services';
 import {dev, user} from '../log';
 import {registerServiceBuilder, getService} from '../service';
@@ -25,7 +26,7 @@ import {
   serializeQueryString,
 } from '../url';
 import {parseJson} from '../json';
-import {isArray, isObject, isFormData} from '../types';
+import {isArray, isObject} from '../types';
 import {utf8EncodeSync} from '../utils/bytes';
 import {getMode} from '../mode';
 
@@ -124,6 +125,14 @@ export class Xhr {
     dev().assert(
         creds === undefined || creds == 'include' || creds == 'omit',
         'Only credentials=include|omit support: %s', creds);
+
+    // After this point, both the native `fetch` and the `fetch` polyfill will
+    // expect a native `FormData` object in the `body` property, so the native
+    // `FormData` object needs to be unwrapped.
+    if (init.body instanceof FormDataWrapper) {
+      init.body = init.body.getFormData();
+    }
+
     // Fallback to xhr polyfill since `fetch` api does not support
     // responseType = 'document'. We do this so we don't have to do any parsing
     // and document construction on the UI thread which would be expensive.
@@ -212,7 +221,7 @@ export class Xhr {
    */
   fetchJson(input, opt_init, opt_allowFailure) {
     const init = setupInit(opt_init, 'application/json');
-    if (init.method == 'POST' && !isFormData(init.body)) {
+    if (init.method == 'POST' && !(init.body instanceof FormDataWrapper)) {
       // Assume JSON strict mode where only objects or arrays are allowed
       // as body.
       dev().assert(

build-system/tasks/presubmit-checks.js
@@ -580,6 +580,13 @@ const forbiddenTerms = {
       'src/event-helper.js',
     ],
   },
+  'new FormData\\(': {
+    message: 'Use new FormDataWrapper() instead and call ' +
+        'formDataWrapper.getFormData() to get the native FormData object.',
+    whitelist: [
+      'src/form-data-wrapper.js',
+    ],
+  },
   '([eE]xit|[eE]nter|[cC]ancel|[rR]equest)Full[Ss]creen\\(': {
     message: 'Use fullscreenEnter() and fullscreenExit() from dom.js instead.',
     whitelist: [

extensions/amp-form/0.1/amp-form.js
@@ -28,6 +28,7 @@ import {
   setFormForElement,
   getFormAsObject,
 } from '../../../src/form';
+import {FormDataWrapper} from '../../../src/form-data-wrapper';
 import {
   assertAbsoluteHttpOrHttpsUrl,
   assertHttpsUrl,
@@ -487,7 +488,7 @@ export class AmpForm {
       xhrUrl = addParamsToUrl(url, values);
     } else {
       xhrUrl = url;
-      body = new FormData(this.form_);
+      body = new FormDataWrapper(this.form_);
       for (const key in opt_extraFields) {
         body.append(key, opt_extraFields[key]);
       }

extensions/amp-form/0.1/test/test-amp-form.js
@@ -36,6 +36,8 @@ import '../../../amp-selector/0.1/amp-selector';
 import {user} from '../../../../src/log';
 import {whenCalled} from '../../../../testing/test-helper.js';
 import {AmpEvents} from '../../../../src/amp-events';
+import {FormDataWrapper} from '../../../../src/form-data-wrapper';
+import {fromIterator} from '../../../../src/utils/array';
 
 describes.repeated('', {
   'single ampdoc': {ampdoc: 'single'},
@@ -678,7 +680,11 @@ describes.repeated('', {
 
           const xhrCall = ampForm.xhr_.fetch.getCall(0);
           const config = xhrCall.args[1];
-          expect(config.body).to.not.be.null;
+          expect(config.body).to.be.an.instanceof(FormDataWrapper);
+          const entriesInForm =
+              fromIterator(new FormDataWrapper(getForm()).entries());
+          expect(fromIterator(config.body.entries())).to.have.deep.members(
+              entriesInForm);
           expect(config.method).to.equal('POST');
           expect(config.credentials).to.equal('include');
         });

src/types.js
@@ -73,15 +73,6 @@ export function isFiniteNumber(value) {
 }
 
 /**
- * Determines if value is of FormData type.
- * @param {*} value
- * @return {boolean}
- */
-export function isFormData(value) {
-  return toString(value) === '[object FormData]';
-}
-
-/**
  * Checks whether `s` is a valid value of `enumObj`.
  *
  * @param {!Object<T>} enumObj

test/functional/test-xhr.js
@@ -22,6 +22,7 @@ import {
   FetchResponse,
   assertSuccess,
 } from '../../src/service/xhr-impl';
+import {FormDataWrapper} from '../../src/form-data-wrapper';
 import {getCookie} from '../../src/cookies';
 import {Services} from '../../src/services';
 
@@ -130,7 +131,7 @@ describe.configure().skipSafari().run('XHR', function() {
         });
 
         it('should allow FormData as body', () => {
-          const formData = new FormData();
+          const formData = new FormDataWrapper();
           sandbox.stub(JSON, 'stringify');
           formData.append('name', 'John Miller');
           formData.append('age', 56);

commit 2848e39fb0f05d8b2b16d51485c636229f2a2d08

Fix regression caused by #12084

src/form-data-wrapper.js
@@ -127,3 +127,13 @@ export class FormDataWrapper {
     return this.formData_;
   }
 }
+
+/**
+ * Check if the given object is a FormDataWrapper instance
+ * @param {*} o
+ * @return {boolean} True if the object is a FormDataWrapper instance.
+ */
+export function isFormDataWrapper(o) {
+  // instanceof doesn't work as expected, so we detect with duck-typing.
+  return o && !!o.getFormData;
+}

src/service/xhr-impl.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {FormDataWrapper} from '../form-data-wrapper';
+import {isFormDataWrapper} from '../form-data-wrapper';
 import {Services} from '../services';
 import {dev, user} from '../log';
 import {registerServiceBuilder, getService} from '../service';
@@ -150,7 +150,7 @@ export class Xhr {
       // After this point, both the native `fetch` and the `fetch` polyfill will
       // expect a native `FormData` object in the `body` property, so the native
       // `FormData` object needs to be unwrapped.
-      if (init.body instanceof FormDataWrapper) {
+      if (isFormDataWrapper(init.body)) {
         init.body = init.body.getFormData();
       }
       // Fallback to xhr polyfill since `fetch` api does not support
@@ -258,7 +258,7 @@ export class Xhr {
    */
   toStructuredCloneable_(input, init) {
     const newInit = Object.assign({}, init);
-    if (init.body instanceof FormDataWrapper) {
+    if (isFormDataWrapper(init.body)) {
       newInit.headers = newInit.headers || {};
       newInit.headers['Content-Type'] = 'multipart/form-data;charset=utf-8';
       newInit.body = fromIterator(init.body.entries());
@@ -435,7 +435,7 @@ export class Xhr {
    */
   fetchJson(input, opt_init, opt_allowFailure) {
     const init = setupInit(opt_init, 'application/json');
-    if (init.method == 'POST' && !(init.body instanceof FormDataWrapper)) {
+    if (init.method == 'POST' && !isFormDataWrapper(init.body)) {
       // Assume JSON strict mode where only objects or arrays are allowed
       // as body.
       dev().assert(