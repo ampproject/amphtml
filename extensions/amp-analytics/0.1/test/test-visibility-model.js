3173d0006e32b43782e6298e95027053dfbc90ff
Jon Keller <jonkeller@users.noreply.github.com>
2017-12-13 19:15:47 -0500

    Edge case for visiblePercentageMin and Max = 0 (#12426)
    * Adds unit test for visibilityMin and Max = 0
    
    * Improves unit test
    
    * Addresses special case on 0% viz
    
    * Removes unused vars
    
    * Simplifies some logic
    
    * Updates a comment
    
    * Removes unnecessary if condition

--- extensions/amp-analytics/0.1/test/test-visibility-model.js
+++ extensions/amp-analytics/0.1/test/test-visibility-model.js
@@ -67,9 +67,9 @@ describes.sandboxed('VisibilityModel', {}, () => {
       expect(config({visiblePercentageMax: ''}).visiblePercentageMax)
           .to.equal(1);
       expect(config({visiblePercentageMax: 0}).visiblePercentageMax)
-          .to.equal(1);
+          .to.equal(0);
       expect(config({visiblePercentageMax: '0'}).visiblePercentageMax)
-          .to.equal(1);
+          .to.equal(0);
       expect(config({visiblePercentageMax: 50}).visiblePercentageMax)
           .to.equal(0.5);
       expect(config({visiblePercentageMax: '50'}).visiblePercentageMax)
@@ -988,9 +988,55 @@ describes.sandboxed('VisibilityModel', {}, () => {
       const eventSpy = vh.eventResolver_ = sandbox.spy();
       visibility = 0.99;
       clock.tick(200);
-      expect(eventSpy).to.not.be.called;
       vh.update();
+      expect(eventSpy).to.not.be.called;
       visibility = 1.0;
+      clock.tick(200);
+      vh.update();
+      expect(eventSpy).to.be.calledOnce;
+    });
+
+    it('should fire for visiblePercentageMin=visiblePercentageMax=0', () => {
+      const vh = new VisibilityModel({
+        visiblePercentageMin: 0,
+        visiblePercentageMax: 0,
+        repeat: true,
+      }, calcVisibility);
+      const eventSpy = vh.eventResolver_ = sandbox.spy();
+      sandbox.stub(vh, 'reset_').callsFake(() => {
+        vh.eventPromise_ = new Promise(unused => {
+          vh.eventResolver_ = eventSpy;
+        });
+        vh.eventPromise_.then(() => {
+          vh.onTriggerObservable_.fire();
+        });
+        vh.scheduleRepeatId_ = null;
+        vh.everMatchedVisibility_ = false;
+        vh.matchesVisibility_ = false;
+        vh.continuousTime_ = 0;
+        vh.maxContinuousVisibleTime_ = 0;
+        vh.totalVisibleTime_ = 0;
+        vh.firstVisibleTime_ = 0;
+        vh.firstSeenTime_ = 0;
+        vh.lastSeenTime_ = 0;
+        vh.lastVisibleTime_ = 0;
+        vh.minVisiblePercentage_ = 0;
+        vh.maxVisiblePercentage_ = 0;
+        vh.lastVisibleUpdateTime_ = 0;
+        vh.waitToReset_ = false;
+      });
+      visibility = 0;
+      clock.tick(200);
+      vh.update();
+      expect(eventSpy).to.be.calledOnce;
+
+      eventSpy.reset();
+      visibility = 1;
+      clock.tick(200);
+      vh.update();
+      expect(eventSpy).to.not.be.called;
+
+      visibility = 0;
       clock.tick(200);
       vh.update();
       expect(eventSpy).to.be.calledOnce;
