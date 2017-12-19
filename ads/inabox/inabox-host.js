4f31393d21314649e4b809f04c1fff59f50a7f17
keithwrightbos <kjwright@google.com>
2017-12-19 16:33:36 -0500

    Inabox host script clear array after load

--- ads/inabox/inabox-host.js
+++ ads/inabox/inabox-host.js
@@ -21,30 +21,54 @@
 
 import {dev, initLogConstructor, setReportError} from '../../src/log';
 import {reportError} from '../../src/error';
+import {isArray} from '../../src/types';
 import {InaboxMessagingHost} from './inabox-messaging-host';
 
 const TAG = 'inabox-host';
-run(self);
+const INITIALIZED_WIN_VAR_NAME = 'ampInaboxInitialized';
+const PENDING_MESSAGES_WIN_VAR_NAME = 'ampInaboxPendingMessages';
+const INABOX_AD_IFRAMES_IN_VAR_NAME= 'ampInaboxIframes';
 
 /**
- * @param win {!Window}
+ * Class for initializing host script and consuming queued messages.
+ * @visibleForTesting
  */
-function run(win) {
-  // Prevent double initialization
-  if (win['ampInaboxInitialized']) {
-    dev().info(TAG, 'Skip a 2nd attempt of initializing AMP inabox host.');
-    return;
+export class InaboxHost {
+  /** @param win {!Window}  */
+  constructor(win) {
+    // Prevent double initialization
+    if (win[INITIALIZED_WIN_VAR_NAME]) {
+      dev().info(TAG, 'Skip a 2nd attempt of initializing AMP inabox host.');
+      return;
+    }
+
+    // Assume we cannot recover from state initialization errors.
+    win[INITIALIZED_WIN_VAR_NAME] = true;
+    initLogConstructor();
+    setReportError(reportError);
+
+    const host = new InaboxMessagingHost(
+      win, win[INABOX_AD_IFRAMES_IN_VAR_NAME]);
+
+    const queuedMsgs = win[PENDING_MESSAGES_WIN_VAR_NAME];
+    if (queuedMsgs) {
+      if (isArray(queuedMsgs)) {
+        queuedMsgs.forEach(message => {
+          try {
+            host.processMessage(message);
+          } catch (err) {
+            dev().info('Error processing inabox message', message, err);
+          }
+        });
+      } else {
+        dev().info(TAG, `Invalid ${PENDING_MESSAGES_WIN_VAR_NAME}`, queuedMsgs);
+      }
+    }
+    // Empty and ensure that future messages are no longer stored in the array.
+    win[PENDING_MESSAGES_WIN_VAR_NAME] = [];
+    win[PENDING_MESSAGES_WIN_VAR_NAME]['push'] = () => {};
+    win.addEventListener('message', host.processMessage.bind(host));
   }
-
-  win['ampInaboxInitialized'] = true;
-  initLogConstructor();
-  setReportError(reportError);
-
-  const host = new InaboxMessagingHost(win, win['ampInaboxIframes']);
-
-  win['ampInaboxPendingMessages'].forEach(message => {
-    host.processMessage(message);
-  });
-
-  win.addEventListener('message', host.processMessage.bind(host));
 }
+
+new InaboxHost(self);
