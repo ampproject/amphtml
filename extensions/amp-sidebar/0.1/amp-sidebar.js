fbbd6a0e68245444a81a88279df82a513214fa17
Ali Ghassemi <aghassemi@google.com>
2017-12-20 14:46:30 -0800

    reverting effect of #12502 for now since it overrides effects of local navigation

--- extensions/amp-sidebar/0.1/amp-sidebar.js
+++ extensions/amp-sidebar/0.1/amp-sidebar.js
@@ -396,14 +396,14 @@ export class AmpSidebar extends AMP.BaseElement {
         toggle(this.element, /* display */false);
         this.schedulePause(this.getRealChildren());
         // Return focus to source element if applicable
-        if (this.openerElement_) {
-          // TODO(cathyxz, 12479): Re-enable with updated heuristic on whether
-          // returning focus to opener is the right choice or not. Current
-          // issue is that if an item insidebar has user .focus or .scrollTo
-          // or just local # navigation before closing the sidebar, moving the
-          // fous would override their effects.
-          // tryFocus(this.openerElement_);
-        }
+        // if (this.openerElement_) {
+        //   // TODO(cathyxz, 12479): Re-enable with updated heuristic on whether
+        //   // returning focus to opener is the right choice or not. Current
+        //   // issue is that if an item insidebar has user .focus or .scrollTo
+        //   // or just local # navigation before closing the sidebar, moving the
+        //   // fous would override their effects.
+        //   tryFocus(this.openerElement_);
+        // }
         this.triggerEvent_(SidebarEvents.CLOSE);
       });
     }
