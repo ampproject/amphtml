2ad2e44bf25e8c229faba9fd498f0a9a010dcd99
Brad Frizzell <bradfrizzell@google.com>
2017-12-22 14:36:19 -0500

    Respond to feedback

--- ads/inabox/inabox-messaging-host.js
+++ ads/inabox/inabox-messaging-host.js
@@ -229,21 +229,20 @@ export class InaboxMessagingHost {
     for (let i = 0; i < this.iframes_.length; i++) {
       const iframe = this.iframes_[i];
       if (iframe.contentWindow === source) {
-	  // Walk up on the window tree and verify that there is no cross-domain
-	  // frame in between the source of the postMessage and the host doc.
-	  // If there is, the host doc will not be able to accurately measure
-	  // the creative's positions in the page, so return null.
-	  // Limit to 10 iterations.
-	  let tempWin = source.parent;
-	  for (let i = 0; i < 10; i++) {
-	      if (tempWin == this.win_ || tempWin == this.win_.top) {
-		  break;
-	      }
-	      if (!canInspectWindow(tempWin)) {
-		  return null;
-	      }
-	      tempWin = tempWin.parent;
+        // Walk up on the window tree and verify that there is no cross-domain
+        // frame in between the source of the postMessage and the host doc.
+        // If there is, the host doc will not be able to accurately measure
+        // the creative's positions in the page, so return null.
+        // Limit to 10 iterations.
+        for (let i = 0, tempWin = source.parent;
+	     tempWin == this.win_ || tempWin == this.win_.top;
+	     i++, tempWin = tempWin.parent) {
+	    if (i == 10 || !canInspectWindow(tempWin)) {
+            dev().warn(
+		    TAG, 'Ignored message from within cross-domain iframe', tempWin);
+	      return null;
 	  }
+        }
         // Cache the found iframe with its sentinel.
         this.iframeMap_[sentinel] = iframe;
         return iframe;
@@ -262,7 +261,7 @@ export class InaboxMessagingHost {
  */
 function canInspectWindow(win) {
   try {
-    return !!win && win.location.href != null && canTouchProperty(win, 'test');
+    return win.location.href != null && canTouchProperty(win, 'test');
   } catch (err) {
     return false;
   }
