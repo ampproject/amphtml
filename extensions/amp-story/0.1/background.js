16cd79e03e1881b1d34a56dbf6f05b03d4cfd8af
Alan Orozco <orozcoalan@gmail.com>
2017-12-26 09:53:15 -0800

    Polish and optimize desktop background

--- extensions/amp-story/0.1/background.js
+++ extensions/amp-story/0.1/background.js
@@ -14,7 +14,18 @@
  * limitations under the License.
  */
 
-import {setStyle, resetStyles} from '../../../src/style';
+import {Services} from '../../../src/services';
+import {listenOncePromise} from '../../../src/event-helper';
+import {setStyle} from '../../../src/style';
+
+
+/** @const {number} */
+const SWAP_TIMEOUT_MS = 500;
+
+
+/** @const {number} */
+const XFADE_DURATION_MS = 300;
+
 
 const BACKGROUND_CLASS = 'i-amphtml-story-background';
 
@@ -22,26 +33,56 @@ const BACKGROUND_CONTAINER_CLASS = 'i-amphtml-story-background-container';
 
 const BACKGROUND_OVERLAY_CLASS = 'i-amphtml-story-background-overlay';
 
+
+
+/**
+ * @param {?string} url
+ * @return {!Promise}
+ */
+function maybeLoadImage(url) {
+  if (!url) {
+    return Promise.resolve();
+  }
+  const img = new Image;
+  img.src = url;
+  return listenOncePromise(img, 'load');
+}
+
+
 /**
  * TODO(cvializ): Investigate pre-rendering blurred backgrounds to canvas to
  * possibly improve performance?
  */
 export class AmpStoryBackground {
   /**
+   * @param {!Window} win
    * @param {!Element} element
    */
-  constructor(element) {
-    this.element = element;
+  constructor(win, element) {
+    /* @private @const {!Element} */
+    this.element_ = element;
 
-    this.bgMap_ = {};
+    /* @private @const {!Window} */
+    this.win_ = win;
 
-    this.container_ = this.element.ownerDocument.createElement('div');
+    /* @private {number} */
+    this.count_ = 0;
+
+    /** @private @const */
+    this.container_ = this.element_.ownerDocument.createElement('div');
+
+    /** @private @const */
+    this.containerOverlay_ = this.element_.ownerDocument.createElement('div');
+
+    /** @private {!Element} */
+    this.hidden_ = this.createBackground_();
+
+    /** @private {!Element} */
+    this.active_ = this.createBackground_();
+
     this.container_.classList.add(BACKGROUND_CONTAINER_CLASS);
-    this.containerOverlay_ = this.element.ownerDocument.createElement('div');
     this.containerOverlay_.classList.add(BACKGROUND_OVERLAY_CLASS);
 
-    this.hidden_ = this.createBackground_();
-    this.active_ = this.createBackground_();
     this.container_.appendChild(this.hidden_);
     this.container_.appendChild(this.active_);
     this.container_.appendChild(this.containerOverlay_);
@@ -51,7 +92,7 @@ export class AmpStoryBackground {
    * @return {!Element}
    */
   createBackground_() {
-    const bg = this.element.ownerDocument.createElement('div');
+    const bg = this.element_.ownerDocument.createElement('div');
     bg.classList.add(BACKGROUND_CLASS);
     return bg;
   }
@@ -60,39 +101,60 @@ export class AmpStoryBackground {
    * Attach the backgrounds to the document.
    */
   attach() {
-    this.element.insertBefore(this.container_, this.element.firstChild);
+    this.element_.insertBefore(this.container_, this.element_.firstChild);
   }
 
   /**
    * Update the background with new background image URL.
-   * @param {?string} newUrl
+   * @param {string} color
+   * @param {?string} url
+   * @param {boolean=} initial
    */
-  setBackground(newUrl) {
-    if (!newUrl) {
-      return;
-    }
+  setBackground(color, url, initial = false) {
+    const countAtAdd = ++this.count_;
 
-    setStyle(this.hidden_, 'background-image', `url(${newUrl})`);
-    this.rotateActiveBackground_();
+    const whenFresh = (promise, callback) => promise.then(() => {
+      if (this.count_ == countAtAdd) {
+        callback();
+      }
+    });
+
+    const imgLoad = maybeLoadImage(url);
+    const timeout =
+        Services.timerFor(this.win_).promise(initial ? 0 : SWAP_TIMEOUT_MS);
+
+    const hidden = this.hidden_;
+
+    // Image will be swapped on load.
+    whenFresh(imgLoad, () => {
+      setStyle(hidden, 'background-image', url ? `url(${url})` : null);
+    });
+
+    // Color will always be swapped on timeout.
+    whenFresh(Promise.race([imgLoad, timeout]), () => {
+      setStyle(hidden, 'background-color', color);
+      this.rotateActiveBackground_(/* withTransition */ !initial);
+    });
   }
 
-  /**
-   * Removes background image from page background.
-   */
-  removeBackground() {
-    resetStyles(this.hidden_, ['background-image']);
-    this.rotateActiveBackground_();
-  }
-
-  /**
+  /*
    * Rotates the classes on page background to bring the new bacground in foreground.
+   * @param {boolean} withTransition
    * @private
    */
-  rotateActiveBackground_() {
+  rotateActiveBackground_(withTransition) {
     const newHidden = this.active_;
     this.active_ = this.hidden_;
     this.hidden_ = newHidden;
     this.active_.classList.add('active');
     this.hidden_.classList.remove('active');
+
+    if (withTransition) {
+      setStyle(this.active_, 'transition', `opacity ${XFADE_DURATION_MS}ms 0s`);
+      setStyle(this.hidden_, 'transition', `opacity 0s ${XFADE_DURATION_MS}ms`);
+    } else {
+      setStyle(this.active_, 'transition', 'none');
+      setStyle(this.hidden_, 'transition', 'none');
+    }
   }
 }
