d734655bd4f93ae1ef59aefe6d726a43746865ed
keithwrightbos <kjwright@google.com>
2017-12-28 14:49:12 -0500

    POC: Adzerk Fast Fetch with template based preferential render

---
+++ extensions/amp-ad-network-adzerk-impl/0.1/test/test-amp-ad-network-adzerk-impl.js
@@ -0,0 +1,163 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Need the following side-effect import because in actual production code,
+// Fast Fetch impls are always loaded via an AmpAd tag, which means AmpAd is
+// always available for them. However, when we test an impl in isolation,
+// AmpAd is not loaded already, so we need to load it separately.
+import '../../../amp-ad/0.1/amp-ad';
+import {
+  AmpAdNetworkAdzerkImpl,
+  AMP_TEMPLATED_CREATIVE_HEADER_NAME,
+} from '../amp-ad-network-adzerk-impl';
+import {createElementWithAttributes} from '../../../../src/dom';
+import {Xhr} from '../../../../src/service/xhr-impl';
+import {utf8EncodeSync, utf8Decode} from '../../../../src/utils/bytes';
+
+describes.fakeWin('amp-ad-network-adzerk-impl', {amp: true}, env => {
+  let win, doc;
+  let element, impl;
+  let fetchTextMock;
+
+  beforeEach(() => {
+    win = env.win;
+    doc = win.document;
+    fetchTextMock = sandbox.stub(Xhr.prototype, 'fetchText');
+    element = createElementWithAttributes(doc, 'amp-ad', {
+      'type': 'adzerk',
+      'src': 'https://adzerk.com?id=1234',
+      'width': '320',
+      'height': '50',
+    });
+    doc.body.appendChild(element);
+    impl = new AmpAdNetworkAdzerkImpl(element);
+  });
+
+  describe('#getAdUrl', () => {
+    it('should be valid', () => {
+      win['DOMParser'] = true;
+      win.AMP_MODE = {localDev: false};
+      ['https://adzerk.com?id=1234',
+        'https://aDzErK.com?id=1234',
+        'https://adzerk.com?id=9'].forEach(src => {
+        element.setAttribute('src', src);
+        expect(impl.isValidElement()).to.be.true;
+        expect(impl.getAdUrl()).to.equal(src);
+      });
+    });
+
+    it('should not be valid', () => {
+      win['DOMParser'] = true;
+      win.AMP_MODE = {localDev: false};
+      ['http://adzerk.com?id=1234',
+        'https://adzerk.com?id=a',
+        'https://www.adzerk.com?id=1234',
+        'https://adzerk.com?id=1234&a=b',
+        'foohttps://adzer.com?id=1234'].forEach(src => {
+        element.setAttribute('src', src);
+        expect(impl.isValidElement()).to.be.false;
+        expect(impl.getAdUrl()).to.equal('');
+      });
+    });
+
+    it('should not be valid if missing DOMParser', () => {
+      win['DOMParser'] = false;
+      win.AMP_MODE = {localDev: false};
+      expect(impl.isValidElement()).to.be.false;
+    });
+  });
+
+  describe('#getSigningServiceNames', () => {
+    it('should be empty array', () => {
+      expect(impl.getSigningServiceNames()).to.deep.equal([]);
+    });
+  });
+
+  describe('#maybeValidateAmpCreative', () => {
+    it('should properly inflate template', () => {
+      const adResponseBody = {
+        ampCreativeTemplateId: 456,
+        templateMacroValues: {
+          USER_NAME: 'some_user',
+          USER_NUM: 9876,
+          HTML_CONTENT: '<img src=https://img.com/>',
+          IMG_SRC: 'https://some.img.com?a=b',
+        },
+      };
+      const template = '<!doctype html>' +
+          '<html ⚡4ads><head>' +
+          '<meta charset="utf-8">' +
+          '<meta name="viewport" content="width=device-width,' +
+          'minimum-scale=1">' +
+          '<style amp4ads-boilerplate>body{visibility:hidden}</style>' +
+          '<style amp-custom>amp-fit-text: {border: 1px;}</style>' +
+          '<script async src="https://cdn.ampproject.org/amp4ads-v0.js">' +
+          '</script>' +
+          '<script async custom-element="amp-fit-text" ' +
+          'src="https://cdn.ampproject.org/v0/amp-fit-text-0.1.js"></script>' +
+          '<link rel="stylesheet" type="text/css" ' +
+          'href="https://fonts.googleapis.com/css?family=Raleway">' +
+        '</head>' +
+        '<body>' +
+          '<amp-fit-text width="300" height="200">hello {{USER_NAME}}! ' +
+          '{{USER_NUM}}</amp-fit-text>' +
+          'Expect encoding {{HTML_CONTENT}}' +
+          '<amp-img src={{IMG_SRC}}/>' +
+          'Missing {{UNKNOWN}} item' +
+        '</body>' +
+        '</html>';
+      fetchTextMock.withArgs(
+          'https://cdn.ampproject.org/c/s/adzerk/456',
+          {
+            mode: 'cors',
+            method: 'GET',
+            ampCors: false,
+            credentials: 'omit',
+          }).returns(Promise.resolve(
+          {
+            headers: {},
+            text: () => template,
+          }));
+      return impl.maybeValidateAmpCreative(
+          utf8EncodeSync(JSON.stringify(adResponseBody)).buffer,
+          {
+            get: name => {
+              expect(name).to.equal(AMP_TEMPLATED_CREATIVE_HEADER_NAME);
+              return 'true';
+            },
+          },
+          () => {})
+          .then(buffer => utf8Decode(buffer))
+          .then(creative => {
+            expect(creative).to.equal(
+                '<html ⚡4ads=""><head><meta charset="utf-8">' +
+              '<meta name="viewport" content="width=device-width,' +
+              'minimum-scale=1"><style amp-custom="">amp-fit-text: ' +
+              '{border: 1px;}</style></head><body><amp-fit-text ' +
+              'width="300" height="200">hello some_user! 9876</amp-fit-text>' +
+              'Expect encoding &lt;img src=https://img.com/&gt;<amp-img ' +
+              'src="https://some.img.com/?a=b/">Missing  item</amp-img>' +
+              '</body></html>');
+            expect(impl.getAmpAdMetadata()).to.jsonEqual({
+              minifiedCreative: creative,
+              customElementExtensions: ['amp-fit-text'],
+              customStylesheets: [
+                {'href': 'https://fonts.googleapis.com/css?family=Raleway'}],
+            });
+          });
+    });
+  });
+});
