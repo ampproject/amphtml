ampproject/amphtml

commit 40b0f37aa5e6468ddc81540d17515a380c2ad2c2

Fix selector specificity for distance (#12632)

extensions/amp-story/0.1/amp-story.css
@@ -206,19 +206,23 @@ amp-story:not([desktop]) >
 }
 
 amp-story-page[active],
-amp-story:not([desktop]) > amp-story-page[distance=0] {
+amp-story:not([desktop]) >
+    amp-story-page.i-amphtml-layout-container[distance="0"] {
   transform: translateY(0) !important;
 }
 
-amp-story:not([desktop]) > amp-story-page[distance=1] {
+amp-story:not([desktop]) >
+    amp-story-page.i-amphtml-layout-container[distance="1"] {
   transform: translateY(100%) !important;
 }
 
-amp-story:not([desktop]) > amp-story-page[distance=2] {
+amp-story:not([desktop]) >
+    amp-story-page.i-amphtml-layout-container[distance="2"] {
   transform: translateY(200%) !important;
 }
 
-amp-story:not([desktop]) > amp-story-page[distance=3] {
+amp-story:not([desktop]) >
+    amp-story-page.i-amphtml-layout-container[distance="3"] {
   transform: translateY(300%) !important;
 }
 

commit 6003f97e6d745018ab6c43859b6ed143e82870a3

Set of amp-story bookend fixes (#12661)
* Use CSS transition for toggle. Animating by JS is no longer needed.
* Fixes replay. Broken when made async on #12511
* Fixes page blur bleed. Broken by #12632

extensions/amp-story/0.1/amp-story-page.js
@@ -566,20 +566,23 @@ export class AmpStoryPage extends AMP.BaseElement {
    *     by an automatic advancement after a timeout.
    */
   next(opt_isAutomaticAdvance) {
-    this.switchTo_(this.getNextPageId_(opt_isAutomaticAdvance));
+    const pageId = this.getNextPageId_(opt_isAutomaticAdvance);
+
+    if (pageId === null) {
+      dispatch(this.element, EventType.SHOW_BOOKEND, /* opt_bubbles */ true);
+      return;
+    }
+
+    this.switchTo_(pageId);
   }
 
 
   /**
-   * @param {?string} targetPageIdOrNull
+   * @param {string} targetPageId
    * @private
    */
-  switchTo_(targetPageIdOrNull) {
-    if (!targetPageIdOrNull) {
-      return;
-    }
-
-    const payload = {targetPageId: dev().assert(targetPageIdOrNull)};
+  switchTo_(targetPageId) {
+    const payload = {targetPageId};
     const eventInit = {bubbles: true};
     dispatchCustom(this.win, this.element, EventType.SWITCH_PAGE, payload,
         eventInit);

extensions/amp-story/0.1/amp-story.css
@@ -226,7 +226,8 @@ amp-story:not([desktop]) >
   transform: translateY(300%) !important;
 }
 
-.i-amphtml-story-bookend-active:not([desktop]) > amp-story-page[active] {
+.i-amphtml-story-bookend-active:not([desktop]) >
+    amp-story-page.i-amphtml-layout-container[active] {
   transform: translateY(0) scale(1.2) !important;
   filter: blur(15px) !important;
 }
@@ -383,11 +384,13 @@ amp-story-grid-layer * {
   right: 0 !important;
   top: 0 !important;
   z-index: 100001 !important;
+  transition: transform 0.3s ease-out !important;
+}
 
-  /* arbitrarily high number to avoid visual jumps before transition starts */
-  /* TODO(alanorozco): Find a better solution for this, as value can be
-  /* overriden. */
-  transform: translateY(4000px);
+.i-amphtml-story-bookend.i-amphtml-hidden {
+  transition-timing-function: ease-in !important;
+  transform: translateY(100vh) !important;
+  pointer-events: none !important;
 }
 
 .i-amphtml-story-bookend-hidden {

extensions/amp-story/0.1/amp-story.js
@@ -313,14 +313,12 @@ export class AmpStory extends AMP.BaseElement {
     });
 
     this.element.addEventListener(EventType.SWITCH_PAGE, e => {
-      const targetPageId = e.detail.targetPageId;
-
-      if (targetPageId === 'i-amphtml-story-bookend') {
-        this.showBookend_();
-      } else {
-        this.switchTo_(targetPageId);
+      if (this.bookend_.isActive()) {
+        // Disallow switching pages while the bookend is active.
+        return;
       }
 
+      this.switchTo_(e.detail.targetPageId);
       this.ampStoryHint_.hideAllNavigationHint();
     });
 
@@ -655,11 +653,6 @@ export class AmpStory extends AMP.BaseElement {
    */
   // TODO(newmuis): Update history state
   switchTo_(targetPageId) {
-    if (this.bookend_.isActive()) {
-      // Disallow switching pages while the bookend is active.
-      return Promise.resolve();
-    }
-
     const targetPage = this.getPageById_(targetPageId);
     const pageIndex = this.getPageIndex(targetPage);
 

extensions/amp-story/0.1/bookend.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Animation} from '../../../src/animation';
 import {KeyCodes} from '../../../src/utils/key-codes';
 import {ShareWidget} from './share';
 import {EventType, dispatch} from './events';
@@ -26,7 +25,6 @@ import {isArray} from '../../../src/types';
 import {parseUrl} from '../../../src/url';
 import {renderAsElement, renderSimpleTemplate} from './simple-template';
 import {throttle} from '../../../src/utils/rate-limit';
-import * as tr from '../../../src/transition';
 
 
 /**
@@ -49,13 +47,14 @@ const FULLBLEED_THRESHOLD = 88;
 const FULLBLEED_CLASSNAME = 'i-amphtml-story-bookend-fullbleed';
 
 
+/** @private @const {string} */
+const HIDDEN_CLASSNAME = 'i-amphtml-hidden';
+
+
 /** @private @const {!./simple-template.ElementDef} */
 const ROOT_TEMPLATE = {
   tag: 'section',
-  attrs: dict({
-    'class': 'i-amphtml-story-bookend',
-    'hidden': true,
-  }),
+  attrs: dict({'class': `i-amphtml-story-bookend ${HIDDEN_CLASSNAME}`}),
   children: [
     // Overflow container that gets pushed to the bottom when content height is
     // smaller than viewport.
@@ -215,9 +214,6 @@ export class Bookend {
 
     /** @private {!ShareWidget} */
     this.shareWidget_ = ShareWidget.create(win);
-
-    /** @private {boolean} */
-    this.isActive_ = false;
   }
 
   /**
@@ -267,7 +263,8 @@ export class Bookend {
 
   /** @return {boolean} */
   isActive() {
-    return this.isActive_;
+    return this.isBuilt() &&
+        !this.getRoot().classList.contains(HIDDEN_CLASSNAME);
   }
 
   /**
@@ -324,49 +321,24 @@ export class Bookend {
     }, {});
   }
 
-  /**
-   * Hides bookend with a transition.
-   * Uses animation utils instead of CSS transition for convenience and
-   * coordination (i.e. listening to transition end).
-   */
+  /** Hides bookend with a transition. */
   hide() {
-    const transition = tr.setStyles(this.getRoot(), {
-      transform: tr.translateY(tr.numeric(0, this.getViewportHeight_())),
-    });
-
-    this.isActive_ = false;
-
-    Animation.animate(this.getRoot(), transition, 300, 'ease-in')
-        .thenAlways(() => {
-          this.getRoot().setAttribute('hidden', true);
-        });
+    this.toggle_(false);
   }
 
-  /**
-   * Shows bookend with a transition.
-   * Uses animation utils instead of CSS transition for convenience and
-   * coordination (i.e. listening to transition end).
-   */
+  /** Shows bookend with a transition. */
   show() {
-    const transition = tr.setStyles(this.getRoot(), {
-      transform: tr.translateY(tr.numeric(this.getViewportHeight_(), 0)),
-    });
-
-    this.isActive_ = true;
-
-    this.getRoot().classList.remove(FULLBLEED_CLASSNAME);
-    this.getRoot().removeAttribute('hidden');
-    this.getRoot()./*OK*/scrollTop = 0;
-
-    Animation.animate(this.getRoot(), transition, 300, 'ease-out');
+    this.toggle_(true);
   }
 
   /**
-   * @return {number}
+   * @param {boolean} show
    * @private
    */
-  getViewportHeight_() {
-    return Services.viewportForDoc(this.getRoot()).getSize().height;
+  toggle_(show) {
+    Services.vsyncFor(this.win_).mutate(() => {
+      this.getRoot().classList.toggle(HIDDEN_CLASSNAME, !show);
+    });
   }
 
   /**