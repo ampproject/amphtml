94a3eacd1d225cb445d9673eae0892727d76471c
Daniel Rozenberg <me@danielrozenberg.com>
2018-01-12 10:29:41 -0800

    Update third_party/caja to v6012 (#12680)
    * Updated compile.sh to maintain required modifications
    * Removed outdated modifications from the list in README.md

--- third_party/caja/compile.sh
+++ third_party/caja/compile.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Run this file with bash to generate new caja sanitizer library for inclusion
 # into AMP.
 
@@ -7,7 +8,7 @@
 readonly CAJA_HOME=$1
 
 echo "CAJA Home: $CAJA_HOME"
-echo "Java: `java --version`"
+echo "Java: `java -version`"
 echo "Ant: `ant -version`"
 
 readonly GIT_COMMIT=`sh -c "cd $CAJA_HOME && git log --pretty=format:'%H' -n 1"`
@@ -20,6 +21,12 @@ popd
 echo "/* Generated from CAJA Sanitizer library commit $GIT_COMMIT */" > html-sanitizer.js
 echo "" >> html-sanitizer.js
 cat $CAJA_HOME/ant-lib/com/google/caja/plugin/html-sanitizer-bundle.js \
-    | grep -v "window\[\'html" | grep -v "window\[\'URI"  >> html-sanitizer.js
+    | grep -v "window\['html" | grep -v "window\['URI"  >> html-sanitizer.js
 echo "" >> html-sanitizer.js
 echo "export var htmlSanitizer = html;" >> html-sanitizer.js
+
+# Change html attribute regex to optionally accept bracketed attribute names for amp-bind
+OLD_PATTERN="'([-.:\\\\w]+)'"
+NEW_PATTERN="'(\\\\\\\\[[-.:\\\\\\\\w]+\\\\\\\\]|[-.:\\\\\\\\w]+)'"  # literally '(\\[[-.:\\w]+\\]|[-.:\\w]+)'
+LINENUMBER=$(grep -F -n "$OLD_PATTERN" html-sanitizer.js | cut -d ':' -f1)
+sed -i "${LINENUMBER}s/'.*'/$NEW_PATTERN/" html-sanitizer.js
