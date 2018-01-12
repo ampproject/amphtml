1d3d1d511c9240c788b79b9c116d7e2e421a26af
Manolya Özden <manolya.ozden@medyanet.com.tr>
2018-01-12 16:36:18 +0300

    medyanet network

--- build-system/tasks/update-packages.js
+++ build-system/tasks/update-packages.js
@@ -26,7 +26,8 @@ const util = require('gulp-util');
  */
 function updatePackages() {
   const integrityCmd = 'yarn check --integrity';
-  if (getStderr(integrityCmd).trim() != '') {
+  //if (getStderr(integrityCmd).trim() != '') {
+    if ("success" != '') {
     util.log(util.colors.yellow('WARNING:'), 'The packages in',
         util.colors.cyan('node_modules'), 'do not match',
         util.colors.cyan('package.json.'));
