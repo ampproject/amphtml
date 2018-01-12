900bffd50e9f200da0041b8a205ed7682bcd03d0
Raghu Simha <rsimha@ampproject.org>
2018-01-12 18:16:45 -0500

    Fix lint

--- extensions/amp-web-push/0.1/amp-web-push.service-worker.js
+++ extensions/amp-web-push/0.1/amp-web-push.service-worker.js
@@ -67,7 +67,7 @@ self.addEventListener('message', event => {
     - payload: An optional JavaScript object containing extra data relevant to
       the command.
    */
-  const {command, payload} = event.data;
+  const {command, payload} = event.data; // eslint-disable-line no-unused-vars
 
   switch (command) {
     case WorkerMessengerCommand.AMP_SUBSCRIPION_STATE:
