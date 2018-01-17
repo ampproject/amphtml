70e7b22601609c53227c095d2b159c81d56f16d5
Adam Silverstein <adam@10up.com>
2018-01-17 13:05:29 -0500

    Remove built in mathjax margins, remove extra padding

--- 3p/mathml.js
+++ 3p/mathml.js
@@ -55,9 +55,12 @@ export function mathml(global, data) {
       global.document.body.appendChild(div);
       mathjax.Hub.Queue( function () {
         const rendered = document.getElementById('MathJax-Element-1-Frame');
+        // Remove built in mathjax margins.
+        const display = document.getElementsByClassName('MJXc-display');
+        display[0].setAttribute('style','margin-top:0;margin-bottom:0');
         context.requestResize(
-                rendered./*OK*/offsetWidth + 20,
-                rendered./*OK*/offsetHeight + 20
+                rendered./*OK*/offsetWidth,
+                rendered./*OK*/offsetHeight
             );
       } );
 
