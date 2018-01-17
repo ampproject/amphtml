aa351284a07f02c1ba2348d8b34b7a9751aa49ce
Ali Ghassemi <aghassemi@google.com>
2018-01-16 19:06:04 -0800

    Revert "Simplify isLocalDev logic in mode.js (#12667)"
    This reverts commit 0ac36a88fac225572fb0ce53b4c1e35f4f2c676c.

--- extensions/amp-a4a/0.1/real-time-config-manager.js
+++ extensions/amp-a4a/0.1/real-time-config-manager.js
@@ -19,6 +19,7 @@ import {dev, user} from '../../../src/log';
 import {Services} from '../../../src/services';
 import {isArray, isObject} from '../../../src/types';
 import {isSecureUrl} from '../../../src/url';
+import {getMode} from '../../../src/mode';
 
 /** @type {string} */
 const TAG = 'real-time-config';
@@ -152,7 +153,7 @@ function inflateAndSendRtc_(a4aElement, url, seenUrls, promiseArray,
     url = urlReplacements.expandUrlSync(
         url, macros, /** opt_collectVars */undefined, whitelist);
   }
-  if (!isSecureUrl(url)) {
+  if (!isSecureUrl(url) && !(getMode(win).localDev || getMode(win).test)) {
     return logAndAddErrorResponse_(promiseArray, RTC_ERROR_ENUM.INSECURE_URL,
         opt_vendor || url);
   }
