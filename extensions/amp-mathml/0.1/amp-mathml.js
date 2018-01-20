6353cfef88e2ba709c506e892d9e0001db5a9630
Adam Silverstein <adam@10up.com>
2018-01-20 12:38:07 -0500

    removed unused isLayoutSizeDefined import

--- extensions/amp-mathml/0.1/amp-mathml.js
+++ extensions/amp-mathml/0.1/amp-mathml.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {isLayoutSizeDefined} from '../../../src/layout';
 import {Layout} from '../../../src/layout';
 import {getIframe} from '../../../src/3p-frame';
 import {removeElement} from '../../../src/dom';
