4d0430a00f1524021b0c178010b3e3e1895f0562
Gene Levitzky <levitzky@google.com>
2018-01-23 14:38:53 -0500

    Added tests for templates and cache.

--- extensions/amp-a4a/0.1/amp-ad-templates.js
+++ extensions/amp-a4a/0.1/amp-ad-templates.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ import {Services} from '../../../src/services';
 import {dev} from '../../../src/log';
 import {getMode} from '../../../src/mode';
 import {getAmpdoc} from '../../../src/service';
+import {LRUCache} from '../../../src/utils/lru-cache';
 import {Bind} from '../../amp-bind/0.1/bind-impl';
 
 /** @private {!Object<string, string|boolean>} */
@@ -30,18 +31,12 @@ const TEMPLATE_CORS_CONFIG = {
   credentials: 'omit',
 };
 
-/** @const {string} */
-const TAG = 'amp-ad-templates';
-
 export class AmpAdTemplates {
 
   /**
    * @param {!Window} win
-   * @param {function(string)=} opt_onRetrieve Function to be executed upon
-   *   the successful retrieval of a template. If one is not supplied, we will
-   *   default to a no-op.
    */
-  constructor(win, opt_onRetrieve = unusedTemplate => {}) {
+  constructor(win) {
     /** @private {!Window} */
     this.win_ = win;
 
@@ -73,52 +68,15 @@ export class AmpAdTemplates {
   /**
    * @param {!JsonObject} templateValues The values to macro in.
    * @param {!Element} element Parent element containing template.
+   * @return {!Promise} Promise which resolves after rendering completes.The
+   *   return value is mainly used for testing.
    */
   render(templateValues, element) {
     const win = element.ownerDocument.defaultView;
     if (win) {
       const bind = new Bind(getAmpdoc(element), win);
-      bind.setState(templateValues);
+      return bind.setState(templateValues);
     }
-  }
-}
-
-/** @typedef {{
-      payload: *,
-      access: number,
-    }} */
-export let Cacheable;
-
-class LRUCache {
-  /** @param {number} capacity */
-  constructor(capacity) {
-    /** @private {number} */
-    this.capacity_ = capacity;
-
-    /** @private {!Object<(number|string), !Cacheable>} */
-    this.cache_ = {};
-  }
-
-  /**
-   * @param {number|string} id
-   * @return {*} The cached payload.
-   */
-  get(id) {
-    return this.cache_[id] ? this.cache_[id].payload : undefined;
-  }
-
-  /**
-   * @param {number|string} id
-   * @param {*} payload The payload to cache.
-   */
-  put(id, payload) {
-    this.cache_[id] = {payload, access: Date.now()};
-    const cacheKeys = /**@type {!Array<number>}*/ (Object.keys(this.cache_));
-    if (cacheKeys.length > this.capacity_) {
-      dev().warn(TAG, 'Trimming template cache');
-      // Evict oldest entry to ensure memory usage is minimized.
-      cacheKeys.sort((a, b) => this.cache_[b].access - this.cache_[a].access);
-      delete this.cache_[cacheKeys[cacheKeys.length - 1]];
-    }
+    return Promise.resolve(null);
   }
 }
