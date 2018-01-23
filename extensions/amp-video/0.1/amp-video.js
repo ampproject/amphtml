bb8ba1c47403c731f779ba4d8f24323ec25abfc6
Jon Newmuis <newmuis@google.com>
2018-01-23 15:30:50 -0500

    Update amp-video to conditionally drop HLS sources

--- extensions/amp-video/0.1/amp-video.js
+++ extensions/amp-video/0.1/amp-video.js
@@ -23,6 +23,7 @@ import {
   fullscreenExit,
   isFullscreenElement,
   insertAfterOrAtStart,
+  scopedQuerySelectorAll,
 } from '../../../src/dom';
 import {toArray} from '../../../src/types';
 import {listen} from '../../../src/event-helper';
@@ -37,6 +38,8 @@ import {Services} from '../../../src/services';
 import {assertHttpsUrl, isProxyOrigin} from '../../../src/url';
 import {EMPTY_METADATA} from '../../../src/mediasession-helper';
 import {VisibilityState} from '../../../src/visibility-state';
+import {isExperimentOn} from '../../../src/experiments';
+
 
 const TAG = 'amp-video';
 
@@ -88,6 +91,12 @@ class AmpVideo extends AMP.BaseElement {
 
     /** @private @const {!Array<!UnlistenDef>} */
     this.unlisteners_ = [];
+
+    /** @private @const {boolean} */
+    this.isStoryVideo_ = !!closestByTag(this.element, 'amp-story');
+
+    /** @private @const {boolean} */
+    this.storySupportsHls_ = !isExperimentOn(this.win, 'disable-amp-story-hls');
   }
 
   /**
@@ -205,7 +214,7 @@ class AmpVideo extends AMP.BaseElement {
     // visibility.
     // TODO(alanorozco, #12712): amp-story should coordinate resumeCallback.
     Services.videoManagerForDoc(this.element).register(this,
-        /* manageAutoplay */ !closestByTag(this.element, 'amp-story'));
+        /* manageAutoplay */ !this.isStoryVideo_);
   }
 
   /** @override */
@@ -314,7 +323,7 @@ class AmpVideo extends AMP.BaseElement {
     // Only cached sources are added during prerender.
     // Origin sources will only be added when document becomes visible.
     sources.forEach(source => {
-      if (this.isCachedByCDN_(source)) {
+      if (this.isCachedByCDN_(source) && this.isValidSource_(source)) {
         this.video_.appendChild(source);
       }
     });
@@ -337,10 +346,14 @@ class AmpVideo extends AMP.BaseElement {
     }
 
     sources.forEach(source => {
-      // Cached sources should have been moved from <amp-video> to <video>.
-      dev().assert(!this.isCachedByCDN_(source));
-      assertHttpsUrl(source.getAttribute('src'), source);
-      this.video_.appendChild(source);
+      if (this.isValidSource_(source)) {
+        // Cached sources should have been moved from <amp-video> to <video>.
+        dev().assert(!this.isCachedByCDN_(source));
+        assertHttpsUrl(source.getAttribute('src'), source);
+        this.video_.appendChild(source);
+      } else {
+        this.element.removeChild(source);
+      }
     });
 
     // To handle cases where cached source may 404 if not primed yet,
@@ -399,6 +412,22 @@ class AmpVideo extends AMP.BaseElement {
     }
 
     return false;
+  }
+
+  /**
+   * @param {!Element} source The <source> element to check for validity.
+   * @return {boolean} true if the source is allowed to be propagated to the
+   *     created video.
+   * @private
+   */
+  isValidSource_(source) {
+    if (!this.isStoryVideo_ || this.storySupportsHls_) {
+      return true;
+    }
+
+    const type = (source.getAttribute('type') || '').toLowerCase();
+    return type !== 'application/x-mpegurl' &&
+        type !== 'application/vnd.apple.mpegurl';
   }
 
   /**
