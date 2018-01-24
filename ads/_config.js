9aa00c0dd2f4a1a1b79764dc8fe782340c0aa57d
glevitzky <levitzky@google.com>
2018-01-24 14:27:22 -0500

    POC: Adzerk Fast Fetch with template based preferential render (#12765)
    * POC: Adzerk Fast Fetch with template based preferential render
    
    * fix dep check
    
    * fix test failures
    
    * Using templating service.
    
    * yarn.lock revert
    
    * yarn.lock
    
    * yarn.lock
    
    * Attempt #4 at yarn fix...
    
    * Test fix.
    
    * Update mock templates to use <template>s.
    
    * Reverting mode-object.
    
    * Yarrrrn
    
    * Moving toward amp-bind.
    
    * amp-bind integration.
    
    * Removing the need for amp-state.
    
    * Revert "Removing the need for amp-state."
    
    This reverts commit 2134f8c7baf4f653f87b7e79ab804c423094c83f.
    
    * Modifying transformed template.
    
    * Factored our ad templating into new extension.
    
    * Deleting test file.
    
    * Lint fixes and other minutiea.
    
    * Added comments.
    
    * PR feedback.
    
    * Removed unused function.
    
    * Added tests for templates and cache.
    
    * Adding tests.
    
    * presubmit fixes.
    
    * Reverting to amp-mustache.
    
    * Url transforming, uncommenting, and other feedback.
    
    * Removing duplicate files.
    
    * Moving url transform to amp-ad-templates.
    
    * Test fixes
    
    * header values + minor comment change.

--- ads/_config.js
+++ ads/_config.js
@@ -209,6 +209,8 @@ export const adConfig = {
     renderStartImplemented: true,
   },
 
+  adzerk: {},
+
   affiliateb: {
     prefetch: 'https://track.affiliate-b.com/amp/a.js',
     renderStartImplemented: true,
---
+++ extensions/amp-a4a/0.1/amp-ad-templates.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Services} from '../../../src/services';
+import {dev} from '../../../src/log';
+import {getMode} from '../../../src/mode';
+import {urls} from '../../../src/config';
+import {parseUrl} from '../../../src/url';
+import {LRUCache} from '../../../src/utils/lru-cache';
+
+/** @private {!Object<string, string|boolean>} */
+const TEMPLATE_CORS_CONFIG = {
+  mode: 'cors',
+  method: 'GET',
+  // This should be cached across publisher domains, so don't append
+  // __amp_source_origin to the URL.
+  ampCors: false,
+  credentials: 'omit',
+};
+
+export class AmpAdTemplates {
+
+  /**
+   * @param {!Window} win
+   */
+  constructor(win) {
+    /** @private {!Window} */
+    this.win_ = win;
+
+    /** @private {LRUCache} */
+    this.cache_ = new LRUCache(5);
+  }
+
+  /**
+   * Fetch and parse template from AMP cache.  Result is stored in global in
+   * order to reduce overhead when template is used multiple times.
+   * @param {string} templateUrl Canonical URL to template.
+   * @return {!Promise<string>}
+   */
+  fetch(templateUrl) {
+    const proxyUrl = getMode(this.win_).localDev
+      ? templateUrl
+      : this.getTemplateProxyUrl_(templateUrl);
+    let templatePromise = this.cache_.get(proxyUrl);
+    if (!templatePromise) {
+      templatePromise = Services.xhrFor(this.win_)
+          .fetchText(getMode(this.win_).localDev
+            ? `http://ads.localhost:${this.win_.location.port}` +
+                `/a4a_template/adzerk/${proxyUrl}`
+            : proxyUrl, TEMPLATE_CORS_CONFIG)
+          .then(response => response.text());
+      this.cache_.put(proxyUrl, templatePromise);
+    }
+    dev().assert(templatePromise);
+    return /** @type{!Promise<string>} */ (templatePromise);
+  }
+
+  /**
+   * @param {!JsonObject} templateValues The values to macro in.
+   * @param {!Element} element Parent element containing template.
+   * @return {!Promise<!Element>} Promise which resolves after rendering completes.
+   */
+  render(templateValues, element) {
+    return Services.templatesFor(this.win_)
+        .findAndRenderTemplate(element, templateValues);
+  }
+
+  /**
+   * Converts the canonical template URL to the CDN proxy URL.
+   * @param {string} url
+   * @return {string}
+   */
+  getTemplateProxyUrl_(url) {
+    const loc = parseUrl(url);
+    return 'https://' +
+        loc.hostname.replace(/-/g, '--').replace(/\./g, '-') +
+        '.' + urls.cdn.slice(8) + '/c/s/' + loc.hostname + loc.pathname;
+  }
+}
