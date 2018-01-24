adb62617fea65920c60a887cbc3dce97c8064661
zhouyx <zhouyx@google.com>
2018-01-24 11:47:04 -0800

    try commit again

--- extensions/amp-analytics/0.1/amp-analytics.js
+++ extensions/amp-analytics/0.1/amp-analytics.js
@@ -437,7 +437,6 @@ export class AmpAnalytics extends AMP.BaseElement {
     assertHttpsUrl(remoteConfigUrl, this.element);
     const TAG = this.getName_();
     dev().fine(TAG, 'Fetching remote config', remoteConfigUrl);
-
     const fetchConfig = {};
     if (this.element.hasAttribute('data-credentials')) {
       fetchConfig.credentials = this.element.getAttribute('data-credentials');
