8366b642a1bba5cf2d0f8fdf34b00debcc560737
Roberto Bisello <vash1486@hotmail.com>
2018-01-25 03:28:11 +0100

    Initial eadv.it commit (#12318)
    * Initial eadv.it commit
    
    * implemented renderStart()
    
    * fixed mandatory fields check
    
    * fix missing trailing comma on ads/_config.js

--- ads/_config.js
+++ ads/_config.js
@@ -336,6 +336,15 @@ export const adConfig = {
     masterFrameAccessibleType: 'google_network',
   },
 
+  eadv: {
+    renderStartImplemented: true,
+    clientIdScope: 'AMP_ECID_EADV',
+    prefetch: [
+      'https://www.eadv.it/track/esr.min.js',
+      'https://www.eadv.it/track/ead.min.js',
+    ],
+  },
+
   eas: {
     prefetch: 'https://amp.emediate.eu/amp.v0.js',
     renderStartImplemented: true,
