7c55094f2fea30a5634ccc5bc4069ea220c07e9a
Cathy Zhu <cathyxz@gmail.com>
2018-01-25 19:30:15 -0800

    Lightbox controls should be actually hidden and not hidden by setting opacity to 0 (#13042)
    * Lightbox controls hidden should be actually hidden and not by opacity
    
    * Refactor toggle description and update into a single function

--- extensions/amp-lightbox-viewer/0.1/amp-lightbox-viewer.js
+++ extensions/amp-lightbox-viewer/0.1/amp-lightbox-viewer.js
@@ -341,25 +341,7 @@ export class AmpLightboxViewer extends AMP.BaseElement {
     // text area is a div that does not contain descendant elements.
     this.descriptionTextArea_./*OK*/innerText = descText;
     if (!descText) {
-      this.descriptionBox_.classList.add('hide');
-    }
-  }
-
-  /**
-   * Toggle description box if it has text content
-   * @param {boolean=} opt_display
-   * @private
-   */
-  toggleDescriptionBox_(opt_display) {
-    this.updateDescriptionBox_();
-    dev().assert(this.descriptionBox_);
-    if (opt_display == undefined) {
-      opt_display = this.descriptionBox_.classList.contains('hide');
-    }
-    if (this.descriptionBox_.textContent) {
-      this.descriptionBox_.classList.toggle('hide', !opt_display);
-    } else {
-      this.descriptionBox_.classList.add('hide');
+      toggle(dev().assertElement(this.descriptionBox_), false);
     }
   }
 
@@ -368,6 +350,7 @@ export class AmpLightboxViewer extends AMP.BaseElement {
    * @private
    */
   toggleDescriptionOverflow_() {
+    // TODO: if there is nothing to expand into, don't shim.
     if (this.descriptionBox_.classList.contains('standard')) {
       const measureBeforeExpandingDescTextArea = state => {
         state.prevDescTextAreaHeight =
@@ -447,19 +430,6 @@ export class AmpLightboxViewer extends AMP.BaseElement {
   }
 
   /**
-   * Toggle lightbox top bar
-   * @param {boolean=} opt_display
-   * @private
-   */
-  toggleTopBar_(opt_display) {
-    dev().assert(this.topBar_);
-    if (opt_display == undefined) {
-      opt_display = this.topBar_.classList.contains('hide');
-    }
-    this.topBar_.classList.toggle('hide', !opt_display);
-  }
-
-  /**
    * Builds the top bar containing buttons and appends them to the container.
    * @private
    */
@@ -508,17 +478,29 @@ export class AmpLightboxViewer extends AMP.BaseElement {
   }
 
   /**
+   * Toggle description box if it has text content
+   * @param {boolean} display
+   * @private
+   */
+  toggleDescriptionBox_(display) {
+    this.updateDescriptionBox_();
+    toggle(dev().assertElement(this.descriptionBox_), display);
+  }
+
+  /**
    * Toggle lightbox controls including topbar and description.
    * @private
    */
   toggleControls_() {
     if (this.controlsMode_ == LightboxControlsModes.CONTROLS_HIDDEN) {
-      this.toggleDescriptionBox_(/* opt_display */true);
-      this.toggleTopBar_(/* opt_display */true);
+      toggle(dev().assertElement(this.topBar_), true);
+      if (!this.container_.hasAttribute('gallery-view')) {
+        this.toggleDescriptionBox_(true);
+      }
       this.controlsMode_ = LightboxControlsModes.CONTROLS_DISPLAYED;
     } else {
-      this.toggleDescriptionBox_(/* opt_display */false);
-      this.toggleTopBar_(/* opt_display */false);
+      this.toggleDescriptionBox_(false);
+      toggle(dev().assertElement(this.topBar_), false);
       this.controlsMode_ = LightboxControlsModes.CONTROLS_HIDDEN;
     }
   }
@@ -890,9 +872,6 @@ export class AmpLightboxViewer extends AMP.BaseElement {
     this.cleanupEventListeners_();
 
     this.vsync_.mutate(() => {
-      // Reset the state of the description box
-      this.descriptionBox_.classList.remove('hide');
-
       // If there's gallery, set gallery to display none
       this.container_.removeAttribute('gallery-view');
 
@@ -944,7 +923,7 @@ export class AmpLightboxViewer extends AMP.BaseElement {
     this.container_.setAttribute('gallery-view', '');
     this.topBar_.classList.add('fullscreen');
     toggle(dev().assertElement(this.carousel_), false);
-    this.toggleDescriptionBox_(false);
+    toggle(dev().assertElement(this.descriptionBox_), false);
   }
 
   /**
@@ -957,7 +936,8 @@ export class AmpLightboxViewer extends AMP.BaseElement {
       this.topBar_.classList.remove('fullscreen');
     }
     toggle(dev().assertElement(this.carousel_), true);
-    this.toggleDescriptionBox_(true);
+    this.updateDescriptionBox_();
+    toggle(dev().assertElement(this.descriptionBox_), true);
   }
 
   /**
