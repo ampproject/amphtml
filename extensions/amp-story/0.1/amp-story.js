e1f60df0780fdc0ce033091cf57613e999f6370f
Alan Orozco <alanorozco@users.noreply.github.com>
2018-01-31 15:12:51 -0600

    Remove images that fail loading (#13189)

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -1161,7 +1161,7 @@ export class AmpStory extends AMP.BaseElement {
       if (bookendConfig !== null) {
         this.bookend_.setConfig(dev().assert(bookendConfig));
       }
-      this.scheduleResume(this.bookend_.getRoot());
+      this.scheduleLayout(this.bookend_.getRoot());
     });
   }
 
