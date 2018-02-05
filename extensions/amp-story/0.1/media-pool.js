6cc7ea7a99aa1af62e8873515bebd734cc7a6926
Jon Newmuis <newmuis@google.com>
2018-02-05 07:12:10 -0500

    Add vsync

--- extensions/amp-story/0.1/media-pool.js
+++ extensions/amp-story/0.1/media-pool.js
@@ -27,6 +27,7 @@ import {toWin} from '../../../src/types';
 import {BLANK_AUDIO_SRC, BLANK_VIDEO_SRC} from './default-media';
 import {listenOncePromise} from '../../../src/event-helper';
 import {dispatch} from './events';
+import {Services} from '../../../src/services';
 
 
 
@@ -283,6 +284,9 @@ export class MediaPool {
     /** @private @const {!Window} */
     this.win_ = win;
 
+    /** @private @const */
+    this.vsync_ = Services.vsyncFor(win);
+
     /**
      * The function used to retrieve the distance between an element and the
      * current position in the document.
@@ -521,7 +525,9 @@ export class MediaPool {
       return null;
     }
 
-    this.swapPoolMediaElementOutOfDom_(poolMediaEl);
+    this.vsync_.mutate(() => {
+      this.swapPoolMediaElementOutOfDom_(poolMediaEl);
+    });
     return poolMediaEl;
   }
 
@@ -743,7 +749,10 @@ export class MediaPool {
       return null;
     }
 
-    this.swapPoolMediaElementIntoDom_(domMediaEl, poolMediaEl, sources);
+    this.vsync_.mutate(() => {
+      this.swapPoolMediaElementIntoDom_(domMediaEl, poolMediaEl, sources);
+    });
+
     this.allocateMediaElement_(mediaType, poolMediaEl);
     return poolMediaEl;
   }
