d4e8bd7d7839ada85e87f46b4c0a9bfc89385777
Raghu Simha <rsimha@ampproject.org>
2018-02-05 23:09:16 -0500

    Make sure polyfills.js is imported before anything else (#13277)

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -622,6 +622,7 @@ const forbiddenTerms = {
     message: 'Use a line-level "no-unused-vars" rule instead.',
     whitelist: [
       'viewer-api/swipe-api.js',
+      'dist.3p/current/integration.js',
     ],
   },
 };
