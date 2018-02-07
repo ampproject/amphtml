b7431073e969a8dd8a8a927cf80265fc93dbf7fa
Jon Newmuis <newmuis@google.com>
2018-02-07 03:30:01 -0500

    Add missing added files

---
+++ extensions/amp-story/0.1/sources.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  removeElement,
+  scopedQuerySelectorAll,
+} from '../../../src/dom';
+import {ampMediaElementFor} from './utils';
+
+
+
+export class Sources {
+  /**
+   * @param {string} srcAttr The 'src' attribute of the media element.
+   * @param {!IArrayLike<!Element>} srcEls Any child <source> tags of the media
+   *     element.
+   */
+  constructor(srcAttr, srcEls) {
+    /** @private @const {?string} */
+    this.srcAttr_ = srcAttr && srcAttr.length ? srcAttr : null;
+
+    /** @private @const {!IArrayLike<!Element>} */
+    this.srcEls_ = srcEls;
+  }
+
+
+  /**
+   * Applies the src attribute and source tags to a specified element.
+   * @param {!HTMLMediaElement} element The element to adopt the sources
+   *     represented by this object.
+   */
+  applyToElement(element) {
+    Sources.removeFrom(element);
+
+    if (!this.srcAttr_) {
+      element.removeAttribute('src');
+    } else {
+      element.setAttribute('src', this.srcAttr_);
+    }
+
+    Array.prototype.forEach.call(this.srcEls_,
+        srcEl => element.appendChild(srcEl));
+  }
+
+
+  /**
+   * Removes and returns the sources from a specified element.
+   * @param {!Element} element The element whose sources should be removed and
+   *     returned.
+   * @return {!Sources} An object representing the sources of the specified
+   *     element.
+   */
+  static removeFrom(element) {
+    const elementToUse = ampMediaElementFor(element) || element;
+    const srcAttr = elementToUse.getAttribute('src');
+    elementToUse.removeAttribute('src');
+    const srcEls = scopedQuerySelectorAll(elementToUse, 'source');
+    Array.prototype.forEach.call(srcEls, srcEl => removeElement(srcEl));
+
+    return new Sources(srcAttr, srcEls);
+  }
+}
