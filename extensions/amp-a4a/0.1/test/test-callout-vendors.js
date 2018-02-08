9bce00336423bc52564feea31f4d405a8ac937a3
Brad Frizzell <bradleyfrizzell@gmail.com>
2018-02-08 09:50:15 -0500

    Add test

---
+++ extensions/amp-a4a/0.1/test/test-callout-vendors.js
@@ -0,0 +1,11 @@
+import {RTC_VENDORS} from '../callout-vendors';
+
+// The keys of RTC_VENDORS are not allowed to have any capital letters.
+// This test acts as a presubmit to enforce that.
+describe('RTC_VENDORS', () => {
+  it('should have all lowercase keys', () => {
+    Object.keys(RTC_VENDORS).forEach(key => {
+      expect(key).to.equal(key.toLowerCase());
+    });
+  });
+});
