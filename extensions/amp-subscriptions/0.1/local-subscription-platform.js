e09c7342b1622aaa2b8345dbd3c72d270fb376ee
Prateek Bhatnagar <prateek89born@gmail.com>
2018-02-09 11:04:48 -0800

    Amp subscriptions (#13305)
    * fix animating circle fixed position
    
    * adding amp-subscriptions to extensions
    
    * placeholder files for amp-subscriptions
    
    * amp-subscription first commit
    
    * adding interface
    
    * incorporating comments
    
    * fixing local subsriptions platform
    
    * amp-subscriptions
    
    * fixing types
    
    * Update app.js
    
    * fixing tests

---
+++ extensions/amp-subscriptions/0.1/local-subscription-platform.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Services} from '../../../src/services';
+
+/**
+ * This implements the methods to interact with various subscription platforms.
+ *
+ * @implements {./subscription-platform.SubscriptionPlatform}
+ */
+export class LocalSubscriptionPlatform {
+
+  /**
+   * @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc
+   * @param {string} serviceUrl
+   */
+  constructor(ampdoc, serviceUrl) {
+    /** @const */
+    this.ampdoc_ = ampdoc;
+
+    /** @const @private {string} */
+    this.serviceUrl_ = serviceUrl;
+
+    /** @const @private {!../../../src/service/xhr-impl.Xhr} */
+    this.xhr_ = Services.xhrFor(this.ampdoc_.win);
+  }
+
+  /**
+   * TODO(@prateekbh): Define object below once we have a defination of entitlement
+   * @return {!Promise<!JsonObject>}
+   */
+  getEntitlements() {
+    return this.xhr_
+        .fetchJson(this.serviceUrl_)
+        .then(res => res.json());
+  }
+
+}
