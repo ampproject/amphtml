eb37d8c38574cc9f398ef97734100e01e33b4923
Cathy Zhu <cathyxz@gmail.com>
2018-02-21 18:06:56 -0800

    <amp-lightbox-gallery> launch-blocking manual tests (#13577)
    * Add lightbox launch tests and fix basic bugs
    
    * Add amp-anim test
    
    * Make the error message more user friendly

--- extensions/amp-lightbox-gallery/0.1/amp-lightbox-gallery.js
+++ extensions/amp-lightbox-gallery/0.1/amp-lightbox-gallery.js
@@ -801,10 +801,6 @@ export class AmpLightboxGallery extends AMP.BaseElement {
     const anim = new Animation(this.element);
     let duration = MIN_TRANSITION_DURATION;
     const currentElementMetadata = this.getCurrentElement_();
-    const imageBox = /**@type {?}*/ (currentElementMetadata.imageViewer)
-        .implementation_.getImageBoxWithOffset();
-    const image = /**@type {?}*/ (currentElementMetadata.imageViewer)
-        .implementation_.getImage();
     const sourceElement = currentElementMetadata.sourceElement;
     // Try to transition to the source image.
     let transLayer = null;
@@ -820,6 +816,11 @@ export class AmpLightboxGallery extends AMP.BaseElement {
         && this.shouldAnimate_(sourceElement)
         && (sourceElement == this.sourceElement_
         || this.manager_.hasCarousel(this.currentLightboxGroupId_))) {
+
+        const imageBox = /**@type {?}*/ (currentElementMetadata.imageViewer)
+            .implementation_.getImageBoxWithOffset();
+        const image = /**@type {?}*/ (currentElementMetadata.imageViewer)
+            .implementation_.getImage();
 
         sourceElement.classList.add('i-amphtml-ghost');
         transLayer = this.element.ownerDocument.createElement('div');
