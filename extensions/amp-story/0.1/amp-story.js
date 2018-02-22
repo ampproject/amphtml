12eb5a14651eb1a22b9fdef02adb940b54b4e897
Caleb Cordry <ccordry@google.com>
2018-02-22 10:07:15 -0800

    ✨ amp-story-auto-ads showing in the right places (#13284)
    * work on ads
    
    * showing pages
    
    * make better jsdoc
    
    * refactor after feedback
    
    * type fixes
    
    * refactor next ad loading
    
    * set up async
    
    * remove fetch
    
    * fix rebase error
    
    * check for auto-advancement

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -24,6 +24,7 @@
  * </amp-story>
  * </code>
  */
+import './amp-story-auto-ads';
 import './amp-story-grid-layer';
 import './amp-story-page';
 import {ActionTrust} from '../../../src/action-trust';
@@ -90,6 +91,16 @@ const DESKTOP_HEIGHT_THRESHOLD = 550;
 
 /** @private @const {number} */
 const MIN_SWIPE_FOR_HINT_OVERLAY_PX = 50;
+
+/** @private @const {string} */
+const ADVANCE_TO_ATTR = 'i-amphtml-advance-to';
+
+/** @private @const {string} */
+const RETURN_TO_ATTR = 'i-amphtml-return-to';
+
+/** @private @const {string} */
+const AUTO_ADVANCE_TO_ATTR = 'auto-advance-to';
+
 
 /**
  * The duration of time (in milliseconds) to wait for a page to be loaded,
@@ -224,6 +235,9 @@ export class AmpStory extends AMP.BaseElement {
 
     /** @private @const {!Array<!./amp-story-page.AmpStoryPage>} */
     this.pages_ = [];
+
+    /** @private @const {!Array<!./amp-story-page.AmpStoryPage>} */
+    this.adPages_ = [];
 
     /** @const @private {!AmpStoryVariableService} */
     this.variableService_ = new AmpStoryVariableService();
@@ -385,7 +399,9 @@ export class AmpStory extends AMP.BaseElement {
         return;
       }
 
-      this.systemLayer_.updateProgress(pageId, progress);
+      if (!this.activePage_.isAd()) {
+        this.systemLayer_.updateProgress(pageId, progress);
+      }
     });
 
     this.element.addEventListener(EventType.REPLAY, () => {
@@ -711,8 +727,8 @@ export class AmpStory extends AMP.BaseElement {
         'No active page set when navigating to next page.');
 
     const lastPage = this.pages_[this.getPageCount() - 1];
-
-    if (activePage !== lastPage) {
+    if (activePage.element.hasAttribute(ADVANCE_TO_ATTR) ||
+        activePage !== lastPage) {
       activePage.next(opt_isAutomaticAdvance);
     } else {
       this.hasBookend_().then(hasBookend => {
@@ -766,7 +782,9 @@ export class AmpStory extends AMP.BaseElement {
     this.updateBackground_(targetPage.element, /* initial */ !this.activePage_);
 
     // TODO(alanorozco): decouple this using NavigationState
-    this.systemLayer_.setActivePageId(targetPageId);
+    if (!targetPage.isAd()) {
+      this.systemLayer_.setActivePageId(targetPageId);
+    }
 
     // TODO(alanorozco): check if autoplay
     this.navigationState_.updateActivePage(
@@ -1279,7 +1297,7 @@ export class AmpStory extends AMP.BaseElement {
    * @return {number}
    */
   getPageCount() {
-    return this.pages_.length;
+    return this.pages_.length - this.adPages_.length;
   }
 
   /**
@@ -1436,6 +1454,55 @@ export class AmpStory extends AMP.BaseElement {
       dispatch(this.element, EventType.CLOSE_BOOKEND);
     }
     this.switchTo_(dev().assertElement(this.pages_[0].element).id);
+  }
+
+  /** @return {!NavigationState} */
+  getNavigationState() {
+    return this.navigationState_;
+  }
+
+
+  /**
+   * Add page to back of pages_ array
+   * @param {!./amp-story-page.AmpStoryPage} page
+   */
+  addPage(page) {
+    this.pages_.push(page);
+
+    if (page.isAd()) {
+      this.adPages_.push(page);
+    }
+  }
+
+  /**
+   * Insert a new page in navigation flow by changing the attr pointers
+   * on amp-story-page elements
+   * @param {string} currentPageId
+   * @param {string} pageToBeInsertedId
+   */
+  insertPage(currentPageId, pageToBeInsertedId) {
+    // TODO(ccordry): make sure this method moves to PageManager when implemented
+    const pageToBeInserted = this.getPageById_(pageToBeInsertedId);
+    const pageToBeInsertedEl = pageToBeInserted.element;
+
+    const currentPage = this.getPageById_(currentPageId);
+    const currentPageEl = currentPage.element;
+
+    const nextPageId = currentPage
+        .getNextPageId(true /*opt_isAutomaticAdvance */);
+
+
+    if (nextPageId) {
+      currentPageEl.setAttribute(ADVANCE_TO_ATTR, pageToBeInsertedId);
+      currentPageEl.setAttribute(AUTO_ADVANCE_TO_ATTR, pageToBeInsertedId);
+      pageToBeInsertedEl.setAttribute(RETURN_TO_ATTR, currentPageId);
+
+      const nextPage = this.getPageById_(nextPageId);
+      const nextPageEl = nextPage.element;
+      pageToBeInsertedEl.setAttribute(ADVANCE_TO_ATTR, nextPageEl.id);
+      pageToBeInsertedEl.setAttribute(AUTO_ADVANCE_TO_ATTR, nextPageEl.id);
+      nextPageEl.setAttribute(RETURN_TO_ATTR, pageToBeInsertedId);
+    }
   }
 
   /**
