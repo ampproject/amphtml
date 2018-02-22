9c1b08ade0024d342d8f92a9ab7a0123bcf21612
Aaron Labiaga <alabiaga@google.com>
2018-02-22 07:47:53 -0500

    add experiment for inline style for non fixed elements

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -281,6 +281,12 @@ const EXPERIMENTS = [
     spec: 'https://github.com/ampproject/amphtml/issues/6469',
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/12267',
   },
+  {
+    id: 'style-restriction-changes-phase-1',
+    name: 'Enables the usage of inline styles for non fixed elements',
+    spec: 'https://github.com/ampproject/amphtml/issues/11881',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/13595',
+  },  
 ];
 
 if (getMode().localDev) {
