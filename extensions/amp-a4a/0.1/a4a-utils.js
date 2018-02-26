bf81b94f399557862ed7b6424483171747abfff9
Gene Levitzky <levitzky@google.com>
2018-02-26 15:35:22 -0500

    Initial commit.

---
+++ extensions/amp-a4a/0.1/a4a-utils.js
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Services} from '../../../src/services';
+import {dev} from '../../../src/log';
+import {isArray, isObject} from '../../../src/types';
+import {isSecureUrl} from '../../../src/url';
+import {parseJson} from '../../../src/json';
+
+/** @typedef {{width: string, height: string}} */
+export let LayoutInfoDef;
+
+/** @typedef {{
+      minifiedCreative: string,
+      customElementExtensions: !Array<string>,
+      customStylesheets: !Array<{href: string}>,
+      images: (Array<string>|undefined),
+    }} */
+export let CreativeMetaDataDef;
+
+/** @type {Array<string>} */
+const METADATA_STRINGS = [
+  '<script amp-ad-metadata type=application/json>',
+  '<script type="application/json" amp-ad-metadata>',
+  '<script type=application/json amp-ad-metadata>'];
+
+/**
+ * Sends a CORS XHR request to the given URL.
+ * @param {string} url Request URL to send XHR to.
+ * @param {!Window} win
+ * @return {!Promise<!../../../src/service/xhr-impl.FetchResponse>}
+ */
+export function sendXhrRequest(url, win) {
+  return Services.xhrFor(win).fetch(url, {
+    mode: 'cors',
+    method: 'GET',
+    credentials: 'include',
+  });
+}
+
+/**
+ *
+ * Throws {@code SyntaxError} if the metadata block delimiters are missing
+ * or corrupted or if the metadata content doesn't parse as JSON.
+ * @param {string} creative from which CSS is extracted
+ * @param {string} tag
+ * @param {string} type
+ * @return {?CreativeMetaDataDef} Object result of parsing JSON data blob inside
+ *     the metadata markers on the ad text, or null if no metadata markers are
+ *     found.
+ * TODO(keithwrightbos@): report error cases
+ */
+export function getAmpAdMetadata(creative, tag, type) {
+  const TAG = tag;
+  let metadataStart = -1;
+  let metadataString;
+  for (let i = 0; i < METADATA_STRINGS.length; i++) {
+    metadataString = METADATA_STRINGS[i];
+    metadataStart = creative.lastIndexOf(metadataString);
+    if (metadataStart >= 0) {
+      break;
+    }
+  }
+  if (metadataStart < 0) {
+    // Couldn't find a metadata blob.
+    dev().warn(TAG, type,
+        'Could not locate start index for amp meta data in: %s', creative);
+    return null;
+  }
+  const metadataEnd = creative.lastIndexOf('</script>');
+  if (metadataEnd < 0) {
+    // Couldn't find a metadata blob.
+    dev().warn(TAG, type,
+        'Could not locate closing script tag for amp meta data in: %s',
+        creative);
+    return null;
+  }
+  try {
+    const metaDataObj = parseJson(
+        creative.slice(metadataStart + metadataString.length, metadataEnd));
+    const ampRuntimeUtf16CharOffsets =
+        metaDataObj['ampRuntimeUtf16CharOffsets'];
+    if (!isArray(ampRuntimeUtf16CharOffsets) ||
+          ampRuntimeUtf16CharOffsets.length != 2 ||
+          typeof ampRuntimeUtf16CharOffsets[0] !== 'number' ||
+          typeof ampRuntimeUtf16CharOffsets[1] !== 'number') {
+      throw new Error('Invalid runtime offsets');
+    }
+    const metaData = {};
+    if (metaDataObj['customElementExtensions']) {
+      metaData.customElementExtensions =
+          metaDataObj['customElementExtensions'];
+      if (!isArray(metaData.customElementExtensions)) {
+        throw new Error(
+            'Invalid extensions', metaData.customElementExtensions);
+      }
+    } else {
+      metaData.customElementExtensions = [];
+    }
+    if (metaDataObj['customStylesheets']) {
+      // Expect array of objects with at least one key being 'href' whose
+      // value is URL.
+      metaData.customStylesheets = metaDataObj['customStylesheets'];
+      const errorMsg = 'Invalid custom stylesheets';
+      if (!isArray(metaData.customStylesheets)) {
+        throw new Error(errorMsg);
+      }
+      metaData.customStylesheets.forEach(stylesheet => {
+        if (!isObject(stylesheet) || !stylesheet['href'] ||
+              typeof stylesheet['href'] !== 'string' ||
+              !isSecureUrl(stylesheet['href'])) {
+          throw new Error(errorMsg);
+        }
+      });
+    }
+    if (isArray(metaDataObj['images'])) {
+      // Load maximum of 5 images.
+      metaData.images = metaDataObj['images'].splice(0, 5);
+    }
+    // TODO(keithwrightbos): OK to assume ampRuntimeUtf16CharOffsets is before
+    // metadata as its in the head?
+    metaData.minifiedCreative =
+        creative.slice(0, ampRuntimeUtf16CharOffsets[0]) +
+        creative.slice(ampRuntimeUtf16CharOffsets[1], metadataStart) +
+        creative.slice(metadataEnd + '</script>'.length);
+    return metaData;
+  } catch (err) {
+    dev().warn(TAG, type, 'Invalid amp metadata: %s',
+        creative.slice(metadataStart + metadataString.length, metadataEnd));
+    return null;
+  }
+}
+
---
+++ extensions/amp-ad-network-base/0.1/amp-ad-network-base.js
@@ -0,0 +1,239 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  LayoutInfoDef,
+  getAmpAdMetadata,
+  sendXhrRequest,
+} from '../../amp-a4a/0.1/a4a-utils';
+import {
+  RendererDef,
+  RendererInputDef,
+  ValidatorDef, // eslint-disable-line no-unused-vars
+  ValidatorOutputDef,
+  ValidatorResultType, // eslint-disable-line no-unused-vars
+} from '../../amp-a4a/0.1/a4a-render';
+import {dev} from '../../../src/log';
+
+const TAG = 'amp-ad-network-base';
+
+export class AmpAdNetworkBase extends AMP.BaseElement {
+
+  constructor(element) {
+    super(element);
+
+    /** @private {Object<ValidatorResultType, !RendererDef>} */
+    this.boundRenderers_ = {};
+
+    /** @private {?ValidatorDef} */
+    this.boundValidator_ = null;
+
+    /** @private {?string} */
+    this.adUrl_ = null;
+
+    /** @private {?string} */
+    this.expandedAdUrl_ = null;
+
+    /** @private {?ArrayBuffer} */
+    this.unvalidatedBytes_ = null;
+
+    /** @private {!LayoutInfoDef} */
+    this.initialSize_ = {
+      // TODO(levitzky) handle non-numeric values.
+      width: element.getAttribute('width'),
+      height: element.getAttribute('height'),
+    };
+
+    /** @private {string} @const */
+    this.networkType_ = element.getAttribute('type') || 'anon';
+
+    /**
+     * @private {number} unique ID of the currently executing promise to allow
+     * for cancellation.
+     */
+    this.freshnessId_ = 0;
+
+    /** @private {function():boolean} a function that will return true if the
+     * freshness id changes due to unlayout. Initialized to always return
+     * false, and has an actual value set in onLayoutMeasure.
+     */
+    this.isStale_ = () => false;
+  }
+
+  /**
+   * @param {string} adUrl
+   * @protected
+   */
+  bindAdRequestUrl(adUrl) {
+    if (this.adUrl_) {
+      dev().warn(TAG, `Ad Request URL already bound: '${this.adUrl_}'`);
+    }
+    this.adUrl_ = adUrl;
+  }
+
+  /**
+   * @param {ValidatorResultType} resultType
+   * @param {!RendererDef} renderer
+   * @protected
+   */
+  bindRenderer(resultType, renderer) {
+    if (this.boundRenderers_[resultType]) {
+      dev().warn(TAG, `Rendering mode already bound for type '${resultType}'`);
+    }
+    this.boundRenderers_[resultType] = renderer;
+  }
+
+  /**
+   * @param {!ValidatorDef} validator
+   * @protected
+   */
+  bindValidator(validator) {
+    if (this.boundValidator_) {
+      dev().warn(TAG, 'Validator already bound.');
+    }
+    this.boundValidator_ = validator;
+  }
+
+  /**
+   * Collapses slot by setting its size to 0x0.
+   */
+  forceCollapse() {
+    super.attemptChangeSize(0, 0);
+  }
+
+  /**
+   * @return {string} The finalized ad request URL.
+   * @private
+   */
+  getExpandedUrl_() {
+    dev().assert(this.adUrl_, 'Ad Request URL never registered!');
+    // TODO add expansion logic
+    this.expandedAdUrl_ = /** @type {string} */ (this.adUrl_);
+    return this.expandedAdUrl_;
+  }
+
+  /**
+   * @return {function():boolean} function that when called will verify if
+   *    current ad retrieval is current (meaning unlayoutCallback was not
+   *    executed). If not, will return false.
+   * @throws {Error}
+   */
+  getFreshnessVerifier() {
+    const id = this.freshnessId_;
+    return () => {
+      if (id != this.freshnessId_) {
+        this.handleStaleExecution();
+        return true;
+      }
+      return false;
+    };
+  }
+
+  /**
+   * @param {!ValidatorOutputDef} validatorOutput
+   * @return {!RendererInputDef}
+   */
+  getRenderingDataInput_(validatorOutput) {
+    const creative = validatorOutput.creative;
+    return /** @type {!RendererInputDef} */ ({
+      creativeMetadata: getAmpAdMetadata(creative, TAG, this.networkType_),
+      // TODO(levitzky) This may change based on the ad response.
+      size: this.initialSize_,
+      adUrl: this.expandedAdUrl_ ? this.expandedAdUrl_ : this.adUrl_,
+    });
+  }
+
+  /**
+   * Processes the ad response as soon as the XHR request returns. This can be
+   * overridden and used as a hook to perform any desired logic before passing
+   * the response to the validator.
+   * @param {{bytes: !ArrayBuffer, headers: !Headers}} response
+   * @protected
+   */
+  handleAdResponse(response) {
+    if (this.isStale_()) {
+      return;
+    }
+    const unvalidatedBytes = response.bytes;
+    const headers = response.headers;
+    if (!unvalidatedBytes) {
+      // TODO(levitzky) Add error reporting.
+      this.forceCollapse();
+      return;
+    }
+    dev().assert(this.boundValidator_, 'Validator never bound!');
+    this.unvalidatedBytes_ = unvalidatedBytes;
+    this.boundValidator_(unvalidatedBytes, headers, this)
+        .then(validatedBytes =>
+          this.handleValidatorResponse(validatedBytes));
+  }
+
+  handleAdResponseError(error) {
+    // TODO(levitzky) add actual error processing logic.
+    dev().warn(TAG, error);
+  }
+
+  /**
+   * Handler invoked when the current freshness id has expired.
+   */
+  handleStaleExecution() {}
+
+
+  /**
+   * Processes validator response and delegates further action to appropriate
+   *   renderer.
+   * @param {!ValidatorOutputDef} validatedResponse The utf-8 decoded ad
+   *   response.
+   */
+  handleValidatorResponse(validatedResponse) {
+    if (this.isStale_()) {
+      return;
+    }
+    if (!validatedResponse.creative) {
+      // TODO(levitzky) Add error reporting.
+      this.forceCollapse();
+      return;
+    }
+    dev().assert(this.boundRenderers_[validatedResponse.result],
+        'Renderer for AMP creatives never bound!');
+    this.boundRenderers_[validatedResponse.result](
+        this.getRenderingDataInput_(validatedResponse), this);
+  }
+
+  /** @override */
+  isLayoutSupported(unusedLayout) {
+    return true;
+  }
+
+  /** @override */
+  onLayoutMeasure() {
+    ++this.freshnessId_;
+    this.isStale_ = this.getFreshnessVerifier();
+    sendXhrRequest(this.getExpandedUrl_())
+        .then(response => this.handleAdResponse(response))
+        .catch(error => this.handleAdResponseError(error));
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    this.freshnessId_++;
+    return true;
+  }
+}
+
+AMP.extension(TAG, '0.1', AMP => {
+  AMP.registerElement(TAG, AmpAdNetworkBase);
+});
