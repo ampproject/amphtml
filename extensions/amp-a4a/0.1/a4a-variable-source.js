c8b03575e9c5d380421adece4910b06d19c3d521
jonkeller <jonkeller@google.com>
2018-02-28 10:39:27 -0500

    Fixes HTML_ATTR test

--- extensions/amp-a4a/0.1/a4a-variable-source.js
+++ extensions/amp-a4a/0.1/a4a-variable-source.js
@@ -68,7 +68,6 @@ const WHITELISTED_VARIABLES = [
   'FIRST_CONTENTFUL_PAINT',
   'FIRST_VIEWPORT_READY',
   'MAKE_BODY_VISIBLE',
-  'HTML_ATTR',
 ];
 
 /** Provides A4A specific variable substitution. */
