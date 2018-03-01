87fc222b00b7f78032c737e08a2a7f33c44327ed
Prateek Bhatnagar <prateek89born@gmail.com>
2018-02-28 17:00:25 -0800

    Adds selectPlatform method for amp-subscriptions (#13682)
    * fix animating circle fixed position
    
    * adding selectPlatform method
    
    * fixing comments
    
    * fixing types

--- extensions/amp-subscriptions/0.1/test/test-entitlement-store.js
+++ extensions/amp-subscriptions/0.1/test/test-entitlement-store.js
@@ -79,7 +79,7 @@ describes.realWin('entitlement-store', {}, () => {
           });
     });
 
-    it('should resolve false with null for negative entitlement', done => {
+    it('should resolve false for negative entitlement', done => {
       const negativeEntitlement1 =
           new Entitlement(serviceIds[0], ['product1'], '');
       const negativeEntitlements = new Entitlements(
@@ -95,6 +95,67 @@ describes.realWin('entitlement-store', {}, () => {
             }
           });
     });
+
+    it('should resolve false if all future entitlement '
+        + 'are also negative ', done => {
+      const negativeEntitlement1 =
+          new Entitlement(serviceIds[0], ['product1'], '');
+      const negativeEntitlements = new Entitlements(
+          serviceIds[0], '', [negativeEntitlement1], currentProduct);
+      entitlementStore.entitlements_[serviceIds[0]] = negativeEntitlements;
+      entitlementStore.getGrantStatus()
+          .then(entitlements => {
+            if (entitlements === false) {
+              done();
+            } else {
+              throw new Error('Incorrect entitlement resolved');
+            }
+          });
+      entitlementStore.resolveEntitlement(serviceIds[1],
+          entitlementsForService2);
+    });
   });
+
+  describe('areAllPlatformsResolved_', () => {
+    it('should return true if all entitlements are present', () => {
+      entitlementStore.resolveEntitlement(serviceIds[1],
+          entitlementsForService2);
+      expect(entitlementStore.areAllPlatformsResolved_()).to.be.equal(false);
+      entitlementStore.resolveEntitlement(serviceIds[0],
+          entitlementsForService1);
+      expect(entitlementStore.areAllPlatformsResolved_()).to.be.equal(true);
+    });
+  });
+
+  describe('getAvailablePlatformsEntitlements_', () => {
+    it('should return all available entitlements', () => {
+      entitlementStore.resolveEntitlement(serviceIds[1],
+          entitlementsForService2);
+      expect(entitlementStore.getAvailablePlatformsEntitlements_())
+          .to.deep.equal([entitlementsForService2]);
+      entitlementStore.resolveEntitlement(serviceIds[0],
+          entitlementsForService1);
+      expect(entitlementStore.getAvailablePlatformsEntitlements_())
+          .to.deep.equal([entitlementsForService2, entitlementsForService1]);
+    });
+  });
+
+  describe('selectPlatform', () => {
+    it('should call selectApplicablePlatform_ if areAllPlatformsResolved_ '
+        + 'is true', done => {
+      const fakeResult = [entitlementsForService1, entitlementsForService2];
+      const getAllPlatformsStub = sandbox.stub(entitlementStore,
+          'getAllPlatformsEntitlements_').callsFake(
+          () => Promise.resolve(fakeResult));
+      const selectApplicablePlatformSpy = sandbox.stub(entitlementStore,
+          'selectApplicablePlatform_').callsFake(() => Promise.resolve());
+      entitlementStore.selectPlatform().then(() => {
+        expect(getAllPlatformsStub).to.be.calledOnce;
+        expect(selectApplicablePlatformSpy).to.be.calledWith(fakeResult);
+        done();
+      });
+    });
+  });
+
 });
 
