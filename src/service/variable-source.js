ef978246182ca7ce68baed5f3c1ca5ed891ea70a
Hamoon <hamousavi@users.noreply.github.com>
2018-03-01 18:40:23 -0500

    Support whitelisting of AMP variable substitutions (#13533)
    * parse meta tag to obtain the whitelist of variables
    
    * fixes lint errors
    
    * changing the nullability of ampdoc in variable-source
    
    * calling VariableSource's constructor from a4a-variable-source with ampdoc
    
    * slight changes
    
    * removing the const modifier on whitelist
    
    * fixed lannka review requests

--- src/service/variable-source.js
+++ src/service/variable-source.js
@@ -109,7 +109,13 @@ export function getNavigationData(win, attribute) {
  * and override initialize() to add more supported variables.
  */
 export class VariableSource {
-  constructor() {
+  /**
+   * @param {!./ampdoc-impl.AmpDoc} ampdoc
+   */
+  constructor(ampdoc) {
+    /** @protected @const {!./ampdoc-impl.AmpDoc} */
+    this.ampdoc = ampdoc;
+
     /** @private {!RegExp|undefined} */
     this.replacementExpr_ = undefined;
 
@@ -121,6 +127,8 @@ export class VariableSource {
 
     /** @private {boolean} */
     this.initialized_ = false;
+
+    this.getUrlMacroWhitelist_();
   }
 
   /**
@@ -246,6 +254,12 @@ export class VariableSource {
    * @private
    */
   buildExpr_(keys, isV2) {
+    // If a whitelist is present, the keys must belong to the whitelist.
+    // We filter the keys one last time to ensure no unwhitelisted key is
+    // allowed.
+    if (this.getUrlMacroWhitelist_()) {
+      keys = keys.filter(key => this.getUrlMacroWhitelist_().includes(key));
+    }
     // The keys must be sorted to ensure that the longest keys are considered
     // first. This avoids a problem where a RANDOM conflicts with RANDOM_ONE.
     keys.sort((s1, s2) => s2.length - s1.length);
@@ -263,5 +277,49 @@ export class VariableSource {
       regexStr += '(?:\\(((?:\\s*[0-9a-zA-Z-_.]*\\s*(?=,|\\)),?)*)\\s*\\))?';
     }
     return new RegExp(regexStr, 'g');
+  }
+
+  /**
+   * @return {?Array<string>} The whitelist of allowed AMP variables. (if provided in
+   *     a meta tag).
+   * @private
+   */
+  getUrlMacroWhitelist_() {
+    if (this.variableWhitelist_) {
+      return this.variableWhitelist_;
+    }
+
+    const head = this.ampdoc.getRootNode().head;
+    if (!head) {
+      return null;
+    }
+
+    // A meta[name="amp-allowed-url-macros"] tag, if present,
+    // contains, in its content attribute, a whitelist of variable substitution.
+    const meta =
+      head.querySelector('meta[name="amp-allowed-url-macros"]');
+    if (!meta) {
+      return null;
+    }
+
+    /**
+     * The whitelist of variables allowed for variable substitution.
+     * @private {?Array<string>}
+     */
+    this.variableWhitelist_ = meta.getAttribute('content').split(',')
+        .map(variable => variable.trim());
+    return this.variableWhitelist_;
+  }
+
+  /**
+   * Returns `true` if a variable whitelist is *not* present or the present
+   * whitelist contains the given variable name.
+   * @param {string} varName
+   * @return {boolean}
+   * @private
+   */
+  isWhitelisted_(varName) {
+    return !this.getUrlMacroWhitelist_() ||
+      this.getUrlMacroWhitelist_().includes(varName);
   }
 }
