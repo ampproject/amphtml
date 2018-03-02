d437130b8a745f7a0aa7423d27f9282108fbed7a
leonalicious <plztellmewhy1015@hotmail.com>
2018-03-02 11:54:50 +0800

    updated test-scripts and tiny bugs for offset-seconds unit

--- extensions/amp-date-countdown/0.1/test/test-amp-date-countdown.js
+++ extensions/amp-date-countdown/0.1/test/test-amp-date-countdown.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-//import {AmpDateCountdown} from '../amp-date-countdown';
+require('../amp-date-countdown');
 
 describes.realWin('amp-date-countdown', {
   amp: {
@@ -24,41 +24,177 @@ describes.realWin('amp-date-countdown', {
 
   let win;
   let element;
+  let impl;
+  const ISOEndDate = '2020-06-01T00:00:00+08:00';
+  const endDate = new Date(ISOEndDate);
+  const twoDaysBeforeEndDate = new Date(endDate - 86400000 * 2); //substract 2 days
 
   beforeEach(() => {
     win = env.win;
     element = win.document.createElement('amp-date-countdown');
     win.document.body.appendChild(element);
+    //element.build();
+    impl = element.implementation_;
+    impl.mutateElement = function(callback) {
+      callback();
+    };
   });
 
-  it('should have milliseconds from now to ISO format datetime', () => {
-    //const ms = new Date('2020-06-01T00:00:00+08:00') - new Date();
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should display timeleft in the format ' +
+    '{d} {days} {h} {hours} {m} {minutes} {s} {seconds}', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('en')); //English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('2 Days 0 Hours 0 Minutes 0 Seconds');
   });
 
-  it('should have ', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should display timeleft in the format ' +
+    '{d} {days} {h} {hours} {m} {minutes} {s} {seconds},' +
+    'in i18n Chinese Simplified', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('zh-CN')); // Chinese
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent).to.equal('2 天 0 小时 0 分钟 0 秒');
   });
 
-  it('should able to set a countdown date based on timestamp-seconds', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should display timeleft in the format {hh}:{mm}:{ss}', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('en')); //English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.dd + ':' +
+      timeObj.hh + ':' +
+      timeObj.mm + ':' +
+      timeObj.ss;
+    expect(itemElement.textContent)
+        .to.equal('02:00:00:00');
   });
 
-  it('should have different locale unit-text returned', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should display timeleft in the format ' +
+    '{h} {hours} and {m} {minutes} and {s} {seconds}', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_((endDate - twoDaysBeforeEndDate) - 1000),
+          impl.getLocaleWord_('en')); //English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.h + ' ' + timeObj.hours + ' and ' +
+      timeObj.m + ' ' + timeObj.minutes + ' and ' +
+      timeObj.s + ' ' + timeObj.seconds;
+    expect(itemElement.textContent)
+        .to.equal('23 Hours and 59 Minutes and 59 Seconds');
+    //1 day 23 Hours and 59 minutes and 59 seconds
   });
 
-  it('should have ended upon < = 1000 milliseconds', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should display timeleft in the format ' +
+    '{d} {days} {h}:{mm}', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_((endDate - twoDaysBeforeEndDate) - 1000),
+          impl.getLocaleWord_('en')); //English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ':' + timeObj.mm;
+    expect(itemElement.textContent)
+        .to.equal('1 Days 23:59');
   });
 
-  it('should ', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should calculate timeleft and have biggest-unit up to hours', () => {
+    element.setAttribute('biggest-unit', 'hours');
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('en')); // English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('0 Days 48 Hours 0 Minutes 0 Seconds');
   });
 
-
-
-  it('should have test ', () => {
-    expect(element.querySelector('div').textContent).to.equal('hello world');
+  it('should calculate timeleft and have biggest-unit up to minutes', () => {
+    element.setAttribute('biggest-unit', 'minutes');
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('en')); // English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('0 Days 0 Hours 2880 Minutes 0 Seconds');
   });
+
+  it('should calculate timeleft and have biggest-unit up to seconds', () => {
+    element.setAttribute('biggest-unit', 'seconds');
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate),
+          impl.getLocaleWord_('en')); // English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('0 Days 0 Hours 0 Minutes 172800 Seconds');
+  });
+
+  it('should calculate the timeleft after added offset-seconds', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate
+            + (24 * 60 * 60 * 1000)), // hours * minutes * seconds * ms
+          impl.getLocaleWord_('en')); // English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('3 Days 0 Hours 0 Minutes 0 Seconds');
+  });
+
+  it('should calculate the timeleft after substracted offset-seconds', () => {
+    element.build();
+    const timeObj =
+      Object.assign(
+          impl.getYDHMSFromMs_(endDate - twoDaysBeforeEndDate
+            + (- 24 * 60 * 60 * 1000)), // hours * minutes * seconds * ms
+          impl.getLocaleWord_('en')); // English
+    const itemElement = win.document.createElement('div');
+    itemElement.textContent = timeObj.d + ' ' + timeObj.days + ' ' +
+      timeObj.h + ' ' + timeObj.hours + ' ' +
+      timeObj.m + ' ' + timeObj.minutes + ' ' +
+      timeObj.s + ' ' + timeObj.seconds ;
+    expect(itemElement.textContent)
+        .to.equal('1 Days 0 Hours 0 Minutes 0 Seconds');
+  });
+
 });
