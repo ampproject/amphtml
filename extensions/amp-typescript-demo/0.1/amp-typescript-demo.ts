57c3bcea28b9d618c7af312f844867a109365479
William Chou <willchou@google.com>
2018-03-03 10:53:32 -0500

    remove amp-typescript-demo

--- extensions/amp-typescript-demo/0.1/amp-typescript-demo.ts
+++
@@ -1,7 +0,0 @@
-import {greeting} from './utils';
-import {camelCaseToDash} from '../../../src/string';
-
-function hello(compiler: string) {
-    console.log(`${greeting} from ${camelCaseToDash(compiler)}`);
-}
-hello("TypeScript");
