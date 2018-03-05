2b4378aee48f514d3a8309b1544ac8e90326d45d
Brad Frizzell <bradleyfrizzell@gmail.com>
2018-03-05 16:46:16 -0500

    Allow to lint a single file

--- build-system/tasks/lint.js
+++ build-system/tasks/lint.js
@@ -127,7 +127,7 @@ function lint() {
   if (argv.fix) {
     options.fix = true;
   }
-  const stream = initializeStream(config.lintGlobs, {});
+  const stream = initializeStream(argv.file || config.lintGlobs, {});
   return runLinter('.', stream, options);
 }
 
