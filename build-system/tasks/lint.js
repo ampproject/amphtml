3f4c561879f0785c75723026e858ed24c80ba48e
bradfrizzell <bradfrizzell@users.noreply.github.com>
2018-03-07 17:09:27 -0500

    Fix linter file globs (#13858)

--- build-system/tasks/lint.js
+++ build-system/tasks/lint.js
@@ -127,7 +127,10 @@ function lint() {
   if (argv.fix) {
     options.fix = true;
   }
-  const stream = initializeStream(argv.files || config.lintGlobs, {});
+  if (argv.files) {
+    config.lintGlobs[config.lintGlobs.indexOf('**/*.js')] = argv.files;
+  }
+  const stream = initializeStream(config.lintGlobs, {});
   return runLinter('.', stream, options);
 }
 
