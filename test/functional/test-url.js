06d9b97874bf27bc9af89f740ca4246918fe657e
Christoper De Leon <cjdeleon215@gmail.com>
2018-03-09 18:41:21 -0500

    stronger unit tests

--- test/functional/test-url.js
+++ test/functional/test-url.js
@@ -120,11 +120,21 @@ describe('parseUrl', () => {
   it('caches up to 100 results', () => {
     const url = 'https://foo.com:123/abc?123#foo';
     const a1 = parseUrl(url);
+
+    // should grab url from the cache
+    expect(a1).to.equal(parseUrl(url));
+
     // cache 99 more urls in order to reach max capacity of LRU cache: 100
     for (let i = 0; i < 100; i++) {
       parseUrl(`${url}-${i}`);
     }
+
     const a2 = parseUrl(url);
+
+    // the old cached url should not be in the cache anymore
+    // the newer instance should
+    expect(a1).to.not.equal(parseUrl(url));
+    expect(a2).to.equal(parseUrl(url));
     expect(a1).to.not.equal(a2);
   });
   it('should handle ports', () => {
