f291bbf7ecbc398da6d8364d1594ae900beaff70
Juan Lizarazo <gabriellg4@gmail.com>
2018-03-08 23:25:36 -0700

    amp-embedly: Embedly doc service placeholder

--- extensions/amp-embedly/0.1/amp-embedly.js
+++ extensions/amp-embedly/0.1/amp-embedly.js
@@ -15,9 +15,11 @@
  */
 
 import {AmpEmbedlyKey, TAG as KEY_TAG} from './amp-embedly-key';
-import {AmpEmbedlyMain, TAG} from './amp-embedly-main';
+import {AmpEmbedlyMain, TAG} from './amp-embedly-impl';
+import {EmbedlyService, SERVICE_NAME} from './embedly-service';
 
 AMP.extension(TAG, '0.1', function(AMP) {
+  AMP.registerServiceForDoc(SERVICE_NAME, EmbedlyService);
   AMP.registerElement(TAG, AmpEmbedlyMain);
   AMP.registerElement(KEY_TAG, AmpEmbedlyKey);
 });
---
+++ extensions/amp-embedly/0.1/embedly-service.js
@@ -0,0 +1,12 @@
+/**
+ * Service used by the amp-embedly extension components.
+ * @const {string}
+ */
+export const SERVICE_NAME = 'embedly';
+
+/**
+ * Provides the api key used with the embedly xhr requests.
+ */
+export class EmbedlyService {
+
+}
