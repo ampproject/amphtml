41d29b0fae439940227b18b7c7e6f4d655e7cbb1
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2018-03-09 15:09:06 -0500

    Refactors animation presets ♻️ (#13864)
    * Refactors animation presets by adding a set of common util functions for animations.
    
    * Change string concat to string interpolation.
    
    * Fixes typo and adds pulse animation to examples.
    
    * Fixes typo, adds relevant class for JSDoc.
    
    * Making linter happy.
    
    * Remove line break.
    
    * Removes unnecessary @export.

---
+++ extensions/amp-story/0.1/animation-presets-utils.js
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Common animations utility functions used in the animation
+ * presets.
+ */
+
+import {KeyframesDef} from './animation-types';
+import {
+  rotate,
+  scale,
+  translate,
+} from '../../../src/style';
+
+/**
+ * Translates the element on the 2d plane according to the given points.
+ * @param {number} startX Starting point in the abscissa.
+ * @param {number} startY Starting point in the ordinate.
+ * @param {number} endX Ending point in the abscissa.
+ * @param {number} endY Ending point in the ordinate.
+ * @return {KeyframesDef} Keyframes that make up the animation.
+ */
+export function translate2d(startX, startY, endX, endY) {
+  return [
+    {transform: translate(startX, startY)},
+    {transform: translate(endX, endY)},
+  ];
+}
+
+/**
+ * Translates and rotates the element on the 2d plane.
+ * @param {number} startX Starting point in the abscissa.
+ * @param {number} startY Starting point in the ordinate.
+ * @param {number} endX Ending point in the abscissa.
+ * @param {number} endY Ending point in the ordinate.
+ * @param {number} direction -1 for left, 1 for right
+ * @return {KeyframesDef} Keyframes that make up the animation.
+ */
+export function rotateAndTranslate(startX, startY, endX, endY, direction) {
+  return [
+    {transform: translate(startX, startY) + ' ' + rotate(direction * 360)},
+    {transform: translate(endX, endY) + ' ' + rotate(0)},
+  ];
+}
+
+/**
+ * Gradually shows and grows the element while translating it on the 2d plane.
+ * @param {number} startX Starting point in the abscissa.
+ * @param {number} startY Starting point in the ordinate.
+ * @param {number} endX Ending point in the abscissa.
+ * @param {number} endY Ending point in the ordinate.
+ * @return {KeyframesDef} Keyframes that make up the animation.
+ */
+export function whooshIn(startX, startY, endX, endY) {
+  return [
+    {
+      opacity: 0,
+      transform: translate(startX, startY) + ' ' + scale(0.15),
+    },
+    {
+      opacity: 1,
+      transform: translate(endX, endY) + ' ' + scale(1),
+    },
+  ];
+}
