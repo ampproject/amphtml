b6959076036fe067fa9b87a4613e4bf6cd536394
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2018-03-09 15:09:06 -0500

    Refactors animation presets ♻️ (#13864)
    * Refactors animation presets by adding a set of common util functions for animations.
    
    * Change string concat to string interpolation.
    
    * Fixes typo and adds pulse animation to examples.
    
    * Fixes typo, adds relevant class for JSDoc.
    
    * Making linter happy.
    
    * Remove line break.
    
    * Removes unnecessary @export.

--- src/style.js
+++ src/style.js
@@ -172,9 +172,17 @@ export function toggle(element, opt_display) {
  * @return {string}
  */
 export function px(value) {
-  return value + 'px';
+  return `${value}px`;
 }
 
+/**
+ * Returns a degree value.
+ * @param {number} value
+ * @return {string}
+ */
+export function deg(value) {
+  return `${value}deg`;
+}
 
 /**
  * Returns a "translateX" for CSS "transform" property.
@@ -216,6 +224,18 @@ export function translate(x, opt_y) {
  */
 export function scale(value) {
   return `scale(${value})`;
+}
+
+/**
+ * Returns a "rotate" for CSS "transform" property.
+ * @param {number|string} value
+ * @return {string}
+ */
+export function rotate(value) {
+  if (typeof value == 'number') {
+    value = deg(value);
+  }
+  return `rotate(${value})`;
 }
 
 /**
