ca41c7e03f0b6cfa84c38d5c07021d56a82a15de
glevitzky <levitzky@google.com>
2018-03-14 18:07:32 -0400

    Introduces amp-ad-base, an abstract, lightweight, alternative to amp-a4a. (#13675)
    * Initial commit.
    
    * Minor fixes: Grouped typedefs more logically; fixed typo in jsdoc.
    
    * Removing a4a-render and a4a-utils for smaller PR.
    
    * Undoing dep-check changes.
    
    * Changes to allow for non-AMP rendering.
    
    * Ad type field.
    
    * Minimizing logic in amp-ad-network-base.
    
    * Minor tweaks (import clean up, etc)
    
    * Removing network type instance field.
    
    * Fixing accidental removal of code from last commit.
    
    * bind -> register + minor tweaks.
    
    * Free resources + using whenFirstVisible.
    
    * Multiple validators + type decls.
    
    * Added error  recovery.
    
    * Shuffling methods around + minor renaming + method annotations.
    
    * Simplifying error handling.
    
    * Comment fix + freeing validator before calling response handler.
    
    * Consolidating flow in layoutCallback.
    
    * Added amp-ad-context.js
    
    * Removing AmpAdContext class, changing Validator#validate signature to take bytes/header and return Promise<ValidationResult>, and similar changes.
    
    * Naming consistency.
    
    * Type fixes.
    
    * Fixed validate signature, and added public method for adding fields to context.
    
    * Sticter types.
    
    * Annotation typo.
    
    * Removing isReusable instance field.
    
    * PR feedback.
    
    * Clearing context obj.
    
    * Fixed abstract method sigs.
    
    * More type fixes.
    
    * Another type fix.
    
    * Adding util module.
    
    * PR feedback.
    
    * Added adResponseType to ValidatorOutput def.
    
    * Added getContext() and modified LayoutInfoDef.

---
+++ extensions/amp-ad-network-base/0.1/amp-ad-type-defs.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @typedef {{
+      width: string,
+      height: string,
+      layout: string,
+    }} */
+export let LayoutInfoDef;
+
+/** @enum {string} */
+export const FailureType = {
+  REQUEST_ERROR: 'REQUEST_ERROR',
+  INVALID_RESPONSE: 'INVALID_RESPONSE',
+  EMPTY_RESPONSE: 'EMPTY_RESPONSE',
+  VALIDATOR_ERROR: 'VALIDATOR_ERROR',
+  RENDERER_ERROR: 'RENDERER_ERROR',
+};
+
+/** @enum {string} */
+export const RecoveryModeType = {
+  COLLAPSE: 'COLLAPSE',
+  RETRY: 'RETRY',
+};
+
+/** @enum {string} */
+export const ValidatorResult = {
+  AMP: 'AMP',
+  NON_AMP: 'NON_AMP',
+};
+
+/** @typedef {{
+      type: !ValidatorResult,
+      adResponseType: string
+      creativeData: !Object,
+    }} */
+export let ValidatorOutput;
+
+/**
+ * @abstract
+ */
+export class Validator {
+  /**
+   * @param {!Object} unusedContext
+   * @param {!ArrayBuffer} unusedUnvalidatedBytes
+   * @param {!../../../src/service/xhr-impl.FetchResponseHeaders} unusedHeaders
+   * @return {!Promise<!ValidatorOutput>}
+   * @abstract
+   */
+  validate(unusedContext, unusedUnvalidatedBytes, unusedHeaders) {}
+}
+
+/**
+ * @abstract
+ */
+export class Renderer {
+  /**
+   * @param {!Object} unusedContext
+   * @param {!Element} unusedContainerElement
+   * @param {!Object} unusedCreativeData
+   * @return {!Promise}
+   * @abstract
+   */
+  render(unusedContext, unusedContainerElement, unusedCreativeData) {}
+}
