d7e7c58a8fa126810f580c6b7c656f064cc998e2
Naina Raisinghani <nainar@google.com>
2018-03-15 16:19:23 -0700

    Add <amp-bodymovin-player> element (#13810)
    * WIP - Initial implementation
    
    * First draft of code
    
    * remove unused test
    
    * WIP
    
    * Fix aghassemi@ suggestions
    
    * remove WIP tests
    
    * Rename
    
    * Stop using data prefixed variables
    
    * testing
    
    * Add tentative tests
    
    * @aghassemi suggestions
    
    * Test works
    
    * Post rebase lint fixes
    
    * Fix all the compile issues
    
    * Validator fixes
    
    * Validator changes suggested by honeybadgerdontcare@
    
    * more validator changes
    
    * TIL: no commas in validator protoascii
    
    * Please BUILD

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ declareExtension('amp-app-banner', '0.1', {hasCss: true});
 declareExtension('amp-audio', '0.1');
 declareExtension('amp-auto-ads', '0.1');
 declareExtension('amp-bind', '0.1');
+declareExtension('amp-bodymovin-animation', '0.1', {hasCss: false});
 declareExtension('amp-brid-player', '0.1');
 declareExtension('amp-brightcove', '0.1');
 declareExtension('amp-byside-content', '0.1', {hasCss: true});
