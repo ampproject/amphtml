ae969054afce99ae5a2605bb9fe3212d6b5f59a8
Jon Newmuis <newmuis@google.com>
2018-03-16 11:35:15 -0400

    Add English strings for messages that cannot be conveyed without strings in the current UI.

--- extensions/amp-story/0.1/messages/default.js
+++ extensions/amp-story/0.1/messages/default.js
@@ -13,13 +13,52 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {MessageBundleDef} from '../messages';
+import {MessageBundleDef, MessageId} from '../messages';
 
 
 /**
- * Default message bundle used for fallback for unknown/unsupported languages,
- * or if messages are unspecified in the user's language.
- *
+ * Message bundle used for English messages.
  * @const {!MessageBundleDef}
  */
-export default {};
+export default {
+  [MessageId.AMP_STORY_EXPERIMENT_ENABLE_BUTTON_LABEL]: {
+    'message': 'Enable',
+    'description': 'Label for a button that enables the amp-story experiment.',
+  },
+  [MessageId.AMP_STORY_EXPERIMENT_ENABLED_TEXT]: {
+    'message': 'Experiment enabled.  Please reload.',
+    'description': 'Text that is shown once the amp-story experiment has ' +
+        'been successfully enabled.',
+  },
+  [MessageId.AMP_STORY_SHARING_CLIPBOARD_FAILURE_TEXT]: {
+    'message': ':(',
+    'description': 'Message shown in a toast to inform the user that a link ' +
+        'could not be successfully copied to their clipboard.',
+  },
+  [MessageId.AMP_STORY_SYSTEM_LAYER_SHARE_WIDGET_LABEL]: {
+    'message': 'Share',
+    'description': 'Label for the expandable share widget shown in the ' +
+        'desktop UI.',
+  },
+  [MessageId.AMP_STORY_WARNING_DESKTOP_SIZE_TEXT]: {
+    'message': 'Expand your window to view this experience',
+    'description': 'Text for a warning screen that informs the user that ' +
+        'stories are only supported in larger browser windows.',
+  },
+  [MessageId.AMP_STORY_WARNING_EXPERIMENT_DISABLED_TEXT]: {
+    'message': 'You must enable the amp-story experiment to view this content.',
+    'description': 'Text for a warning screen that informs the user that ' +
+        'they must enable an experiment to use stories.',
+  },
+  [MessageId.AMP_STORY_WARNING_LANDSCAPE_ORIENTATION_TEXT]: {
+    'message': 'The page is best viewed in portrait mode',
+    'description': 'Text for a warning screen that informs the user that ' +
+        'stories are only supported in portrait orientation.',
+  },
+  [MessageId.AMP_STORY_WARNING_UNSUPPORTED_BROWSER_TEXT]: {
+    'message': 'We\'re sorry, it looks like your browser doesn\'t support ' +
+        'this experience',
+    'description': 'Text for a warning screen that informs the user that ' +
+        'their browser does not support stories.',
+  },
+};
