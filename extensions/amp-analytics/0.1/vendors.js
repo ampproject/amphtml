4e8219d07f78363251507ff087630acf138e3de9
Stephan Günther <stephan.guenther@webtrekk.com>
2018-03-17 02:49:45 +0100

    add new webtrekk tracking version 2 (#12406)

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -1779,6 +1779,54 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
     },
   },
 
+  'webtrekk_v2': {
+    'vars': {
+      'actionName': 'webtrekk_ignore',
+      'contentId': '${title}',
+      'mediaName': '${id}',
+      'everId': '${clientId(amp-wt3-eid)}',
+    },
+    'requests': {
+      'trackURL': 'https://${trackDomain}/${trackId}/wt',
+      'basePrefix': '?p=440,${contentId},1,' +
+        '${screenWidth}x${screenHeight},${screenColorDepth},1,',
+      'baseSuffix': ',${documentReferrer},' +
+        '${viewportWidth}x${viewportHeight},0' +
+        '&tz=${timezone}&eid=${everId}&la=${browserLanguage}',
+      'parameterPrefix': '${basePrefix}${timestamp}${baseSuffix}',
+      'parameterSuffix': '&pu=${sourceUrl}&eor=1',
+      'pageview': '${trackURL}${parameterPrefix}&${extraUrlParams}' +
+        '&cp570=${pageLoadTime}${parameterSuffix}',
+      'event': '${trackURL}${parameterPrefix}&ct=${actionName}' +
+        '&${extraUrlParams}${parameterSuffix}',
+      'scroll': '${trackURL}${parameterPrefix}&ct=${actionName}' +
+        '&ck540=${verticalScrollBoundary}${parameterSuffix}',
+      'mediaPrefix': '${trackURL}${basePrefix}${baseSuffix}' +
+        '&mi=${mediaName}',
+      'mediaSuffix': '&mt1=${currentTime}&mt2=${duration}' +
+        '&${extraUrlParams}${parameterSuffix}&x=${playedTotal}',
+      'mediaPlay': '${mediaPrefix}&mk=play${mediaSuffix}',
+      'mediaPause': '${mediaPrefix}&mk=pause${mediaSuffix}',
+      'mediaPosition': '${mediaPrefix}&mk=pos${mediaSuffix}',
+      'mediaEnded': '${mediaPrefix}&mk=eof${mediaSuffix}',
+    },
+    'extraUrlParamsReplaceMap': {
+      'pageParameter': 'cp',
+      'contentGroup': 'cg',
+      'actionParameter': 'ck',
+      'sessionParameter': 'cs',
+      'ecommerceParameter': 'cb',
+      'urmCategory': 'uc',
+      'campaignParameter': 'cc',
+      'mediaCategory': 'mg',
+    },
+    'transport': {
+      'beacon': false,
+      'xhrpost': false,
+      'image': true,
+    },
+  },
+
   'mpulse': {
     'requests': {
       'onvisible': 'https://${beacon_url}?' +
