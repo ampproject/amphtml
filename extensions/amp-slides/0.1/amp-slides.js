b5c64797971a4ea3fb50280737bd120958ed9351
Alan Orozco <alanorozco@users.noreply.github.com>
2018-03-20 12:15:41 -0600

    Remove `amp-slides` implementation (#14129)

--- extensions/amp-slides/0.1/amp-slides.js
+++ extensions/amp-slides/0.1/amp-slides.js
@@ -14,222 +14,25 @@
  * limitations under the License.
  */
 
-import * as st from '../../../src/style';
-import * as tr from '../../../src/transition';
-import {Animation} from '../../../src/animation';
-import {isLayoutSizeDefined} from '../../../src/layout';
+import {user} from '../../../src/log';
 
 
-/**
- * Deprecated `amp-slides` is deprecated and will be deleted before 1.0.
- * Please see {@link AmpCarousel} with `type=slides` attribute instead.
- */
+/** @private @const {string} */
+const TAG = 'amp-slides';
+
+
+/** Deprecated. Please see {@link AmpCarousel} with `type=slides` attribute. */
 class AmpSlides extends AMP.BaseElement {
-
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
-    /** @private {?Array<!Element>} */
-    this.slides_ = null;
-
-    /** @private {number} */
-    this.currentIndex_ = 0;
-
-    this.prevButton_ = null;
-
-    this.nextButton_ = null;
-  }
-
-  /** @override */
-  isLayoutSupported(layout) {
-    return isLayoutSizeDefined(layout);
-  }
-
-  /** @override */
-  buildCallback() {
-    this.slides_ = this.getRealChildren();
-    this.slides_.forEach((slide, i) => {
-      this.setAsOwner(slide);
-      // Only the first element is initially visible.
-      st.setStyle(slide, 'display', i > 0 ? 'none' : 'block');
-      this.applyFillContent(slide);
-    });
-
-    this.prevButton_ = this.element.ownerDocument.createElement('button');
-    this.prevButton_.textContent = '\u276E';
-    st.setStyles(this.prevButton_, {
-      opacity: 0.6,
-      position: 'absolute',
-      zIndex: 10,
-      left: '16px',
-      top: '50%',
-      padding: '8px',
-      fontSize: '24px',
-      marginTop: '-20px',
-      pointerEvents: 'all',
-    });
-    this.prevButton_.onclick = () => {
-      this.go(-1, true);
-    };
-    this.element.appendChild(this.prevButton_);
-
-    this.nextButton_ = this.element.ownerDocument.createElement('button');
-    this.nextButton_.textContent = '\u276F';
-    st.setStyles(this.nextButton_, {
-      opacity: 0.6,
-      position: 'absolute',
-      zIndex: 10,
-      right: '16px',
-      top: '50%',
-      padding: '8px',
-      fontSize: '24px',
-      marginTop: '-20px',
-      pointerEvents: 'all',
-    });
-    this.nextButton_.onclick = () => {
-      this.go(1, true);
-    };
-    this.element.appendChild(this.nextButton_);
-  }
-
-  /** @override */
-  prerenderAllowed() {
-    return true;
-  }
-
-  /** @override */
-  isRelayoutNeeded() {
-    return true;
-  }
-
-  /** @override */
-  layoutCallback() {
-    this.scheduleLayout(this.slides_[this.currentIndex_]);
-    this.preloadNext_(1);
-    return Promise.resolve();
-  }
-
-  /** @override */
-  viewportCallback(inViewport) {
-    this.updateInViewport(this.slides_[this.currentIndex_], inViewport);
-  }
-
-  /**
-   * Proceeds to the next slide in the desired direction.
-   * @param {number} dir -1 or 1
-   * @param {boolean} animate
-   */
-  go(dir, animate) {
-    const newIndex = this.nextIndex_(dir);
-    if (newIndex != this.currentIndex_) {
-      const newSlide = this.slides_[newIndex];
-      const oldSlide = this.slides_[this.currentIndex_];
-      this.currentIndex_ = newIndex;
-      this.prepareSlide_(newSlide, dir);
-      if (!animate) {
-        this.commitSwitch_(oldSlide, newSlide);
-      } else {
-        st.setStyle(oldSlide, 'zIndex', 0);
-        Animation.animate(this.element,
-            this.createTransition_(oldSlide, newSlide, dir),
-            200, 'ease-out').thenAlways(() => {
-          this.commitSwitch_(oldSlide, newSlide);
-          this.preloadNext_(dir);
-        });
-      }
-    }
-  }
-
-  /**
-   * @param {!Element} slide
-   * @param {number} dir
-   */
-  prepareSlide_(slide, dir) {
-    const containerWidth = this.element./*OK*/offsetWidth;
-    st.setStyles(slide, {
-      transform: st.translateX(dir * containerWidth),
-      zIndex: 1,
-      display: 'block',
-    });
-
-    this.scheduleLayout(slide);
-  }
-
-  /**
-   * @param {!Element} oldSlide
-   * @param {!Element} newSlide
-   * @param {number} dir
-   * @return {!TransitionDef}
-   */
-  createTransition_(oldSlide, newSlide, dir) {
-    const containerWidth = this.element./*OK*/offsetWidth;
-    return tr.all([
-      tr.setStyles(newSlide, {
-        transform: tr.translateX(tr.numeric(dir * containerWidth, 0)),
-        opacity: tr.numeric(0.8, 1),
-      }),
-      tr.setStyles(oldSlide, {
-        transform: tr.scale(tr.numeric(1, 0.98)),
-        opacity: tr.numeric(1, 0.4),
-      }),
-    ]);
-  }
-
-  /**
-   * @param {!Element} oldSlide
-   * @param {!Element} newSlide
-   * @private
-   */
-  commitSwitch_(oldSlide, newSlide) {
-    st.setStyles(oldSlide, {
-      display: 'none',
-      zIndex: 0,
-      transform: '',
-      transition: '',
-      opacity: 1,
-    });
-    st.setStyles(newSlide, {
-      display: 'block',
-      zIndex: 0,
-      transform: '',
-      transition: '',
-      opacity: 1,
-    });
-    this.scheduleLayout(newSlide);
-    this.updateInViewport(oldSlide, false);
-    this.updateInViewport(newSlide, true);
-  }
-
-  /**
-   * @param {number} dir
-   * @private
-   */
-  nextIndex_(dir) {
-    // TODO(dvoytenko): disable loop by spec.
-    let newIndex = this.currentIndex_ + dir;
-    if (newIndex < 0) {
-      newIndex = this.slides_.length + newIndex;
-    } else if (newIndex >= this.slides_.length) {
-      newIndex = newIndex % this.slides_.length;
-    }
-    return newIndex;
-  }
-
-  /**
-   * @param {number} dir
-   * @private
-   */
-  preloadNext_(dir) {
-    // TODO(dvoytenko): can we actually preload it here? There's no
-    // guarantee of it has display!=none.
-    const nextIndex = this.nextIndex_(dir);
-    if (nextIndex != this.currentIndex_) {
-      this.schedulePreload(this.slides_[nextIndex]);
-    }
+    user().error(TAG,
+        'No longer available. ' +
+          'Use `amp-carousel` with `type="slides"` for an alternative');
   }
 }
 
 
-AMP.extension('amp-slides', '0.1', AMP => {
-  AMP.registerElement('amp-slides', AmpSlides);
+AMP.extension(TAG, '0.1', AMP => {
+  AMP.registerElement(TAG, AmpSlides);
 });
