956528da10ee968f9ef8f311f71b17025d33d442
Gene Levitzky <levitzky@google.com>
2018-03-21 15:28:12 -0400

    Shortened test.

--- extensions/amp-a4a/0.1/test/test-friendly-frame-renderer.js
+++ extensions/amp-a4a/0.1/test/test-friendly-frame-renderer.js
@@ -22,12 +22,14 @@ const realWinConfig = {
   allowExternalResources: true,
 };
 
-describes.realWin('amp-ad-render', realWinConfig, env => {
+describes.realWin('FriendlyFrameRenderer', realWinConfig, env => {
 
   const minifiedCreative = '<p>Hello, World!</p>';
 
+  let containerElement;
   let context;
   let creativeData;
+  let renderer;
 
   beforeEach(() => {
     context = {
@@ -44,46 +46,38 @@ describes.realWin('amp-ad-render', realWinConfig, env => {
         extensions: [],
       },
     };
+
+    renderer = new FriendlyFrameRenderer();
+    containerElement = document.createElement('div');
+    containerElement.signals = () => ({
+      whenSignal: () => Promise.resolve(),
+    });
+    containerElement.renderStarted = () => {};
+    containerElement.getLayoutBox = () => ({
+      left: 0, top: 0, width: 0, height: 0,
+    });
+    document.body.appendChild(containerElement);
   });
 
-  describe('FriendlyFrameRenderer', () => {
+  afterEach(() => {
+    document.body.removeChild(containerElement);
+  });
 
-    let renderer;
-    let containerElement;
-
-    beforeEach(() => {
-      renderer = new FriendlyFrameRenderer();
-      containerElement = document.createElement('div');
-      containerElement.signals = () => ({
-        whenSignal: () => Promise.resolve(),
-      });
-      containerElement.renderStarted = () => {};
-      containerElement.getLayoutBox = () => ({
-        left: 0, top: 0, width: 0, height: 0,
-      });
-      document.body.appendChild(containerElement);
-    });
-
-    afterEach(() => {
-      document.body.removeChild(containerElement);
-    });
-
-    it('should append iframe child', () => {
-      return renderer.render(context, containerElement, creativeData).then(
-          () => {
-            const iframe = containerElement.querySelector('iframe');
-            expect(iframe).to.be.ok;
-            expect(iframe.contentWindow.document.body.innerHTML)
-                .to.equal(minifiedCreative);
-          });
-    });
-    it('should contain the appended iframe child', () => {
-      return renderer.render(context, containerElement, creativeData).then(
-          () => {
-            expect(renderer.getIframe()).to.be.ok;
-            expect(renderer.getIframe()).to.equal(
-                containerElement.querySelector('iframe'));
-          });
-    });
+  it('should append iframe child', () => {
+    return renderer.render(context, containerElement, creativeData).then(
+        () => {
+          const iframe = containerElement.querySelector('iframe');
+          expect(iframe).to.be.ok;
+          expect(iframe.contentWindow.document.body.innerHTML)
+              .to.equal(minifiedCreative);
+        });
+  });
+  it('should contain the appended iframe child', () => {
+    return renderer.render(context, containerElement, creativeData).then(
+        () => {
+          expect(renderer.getIframe()).to.be.ok;
+          expect(renderer.getIframe()).to.equal(
+              containerElement.querySelector('iframe'));
+        });
   });
 });
