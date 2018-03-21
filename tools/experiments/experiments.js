93fe2045e96a47d79aa8d5dea776b099f465427e
Aaron <alabiaga@google.com>
2018-03-21 17:33:06 -0400

    add experiment for inline style for non fixed elements (#13608)
    * add experiment for inline style for non fixed elements
    
    * change experiment name to inline-styles
    
    * Lint fixes

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -245,6 +245,12 @@ const EXPERIMENTS = [
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/12267',
   },
   {
+    id: 'inline-styles',
+    name: 'Enables the usage of inline styles for non fixed elements',
+    spec: 'https://github.com/ampproject/amphtml/issues/11881',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/13595',
+  },
+  {
     id: 'url-replacement-v2',
     name: 'new parsing engine for url variables',
     spec: 'https://github.com/ampproject/amphtml/issues/12119',
