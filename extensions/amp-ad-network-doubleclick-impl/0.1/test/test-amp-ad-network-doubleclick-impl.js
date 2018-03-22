3ed4155d923f4b04840a65f6462748c4072a61f8
glevitzky <levitzky@google.com>
2018-03-22 15:00:16 -0400

    Removing nameframe loading style experiment. (#14198)

--- extensions/amp-ad-network-doubleclick-impl/0.1/test/test-amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/test/test-amp-ad-network-doubleclick-impl.js
@@ -225,35 +225,6 @@ describes.realWin('amp-ad-network-doubleclick-impl', realWinConfig, env => {
       expect(fireDelayedImpressionsSpy.withArgs(
           'https://c.com?e=f,https://d.com?g=h', true)).to.be.calledOnce;
     });
-
-    it('should specify nameframe loading behavior; single arg', () => {
-      impl.extractSize({
-        get(name) {
-          return name == 'amp-nameframe-exp' ? 'instantLoad' : undefined;
-        },
-        has(name) {
-          return !!this.get(name);
-        },
-      });
-      const {instantLoad, writeInBody} = impl.getNameFrameExperimentConfig();
-      expect(instantLoad).to.be.true;
-      expect(writeInBody).to.be.false;
-    });
-
-    it('should specify nameframe loading behavior; two args', () => {
-      impl.extractSize({
-        get(name) {
-          return name == 'amp-nameframe-exp' ?
-            'instantLoad;writeInBody' : undefined;
-        },
-        has(name) {
-          return !!this.get(name);
-        },
-      });
-      const {instantLoad, writeInBody} = impl.getNameFrameExperimentConfig();
-      expect(instantLoad).to.be.true;
-      expect(writeInBody).to.be.true;
-    });
   });
 
   describe('#onCreativeRender', () => {
