b03d7c987c93f4421461ea9ddc6bb8cf4e8ccde2
Jon Newmuis <newmuis@google.com>
2018-03-22 13:41:37 -0400

    Enable the amp-story experiment for file:// URLs.

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -661,7 +661,8 @@ export class AmpStory extends AMP.BaseElement {
 
   /** @private */
   isAmpStoryEnabled_() {
-    if (isExperimentOn(this.win, TAG) || getMode().test) {
+    if (isExperimentOn(this.win, TAG) || getMode().test ||
+        this.win.location.origin.indexOf('file://') === 0) {
       return true;
     }
 
