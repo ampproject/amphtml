ed6106add89ad43587ee97b5e2c7e07bffd6b10d
Dima Voytenko <dvoytenko@google.com>
2018-03-26 12:44:41 -0700

    SwG release 0.1.22.2 (#14250)

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /** Version: 0.1.22-1ba2fd9 */
+ /** Version: 0.1.22.2 */
 'use strict';
 
 
