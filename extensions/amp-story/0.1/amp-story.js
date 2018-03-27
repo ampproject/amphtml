7edda639f4b16ce9e56cee1f9ee07269812a72c4
Jon Newmuis <newmuis@google.com>
2018-03-27 12:14:30 -0400

    Actually fix platform-impl path

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -338,7 +338,7 @@ export class AmpStory extends AMP.BaseElement {
     /** @private @const {!../../../src/service/timer-impl.Timer} */
     this.timer_ = Services.timerFor(this.win);
 
-    /** @private @const {!../../../service/platform-impl.Platform} */
+    /** @private @const {!../../../src/service/platform-impl.Platform} */
     this.platform_ = Services.platformFor(this.win);
 
     /** @private @const {!LocalizationService} */
