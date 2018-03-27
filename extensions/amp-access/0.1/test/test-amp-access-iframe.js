cdae87f269a1a12618c2eb7c09a1a002e2ca7d37
Dima Voytenko <dvoytenko@google.com>
2018-03-27 11:44:09 -0700

    Access iframe: resolve config vars, define authorization better, docs (#14254)
    * Access iframe: resolve config vars, define authorization better, docs
    
    * fixes
    
    * fixes
    
    * fixes

--- extensions/amp-access/0.1/test/test-amp-access-iframe.js
+++ extensions/amp-access/0.1/test/test-amp-access-iframe.js
@@ -16,6 +16,7 @@
 
 import * as lolex from 'lolex';
 import {AccessIframeAdapter} from '../amp-access-iframe';
+import {Messenger} from '../iframe-api/messenger';
 
 
 describes.realWin('AccessIframeAdapter', {
@@ -37,6 +38,7 @@ describes.realWin('AccessIframeAdapter', {
 
     context = {
       buildUrl: () => {},
+      collectUrlVars: () => {},
     };
     contextMock = sandbox.mock(context);
   });
@@ -52,6 +54,18 @@ describes.realWin('AccessIframeAdapter', {
       const adapter = new AccessIframeAdapter(ampdoc, validConfig, context);
       expect(adapter.getConfig()).to.deep.equal({
         'iframeSrc': 'https://acme.com/iframe',
+        'iframeVars': null,
+      });
+      expect(adapter.isAuthorizationEnabled()).to.be.true;
+      expect(adapter.isPingbackEnabled()).to.be.true;
+    });
+
+    it('should load valid config with vars', () => {
+      validConfig['iframeVars'] = ['VAR1'];
+      const adapter = new AccessIframeAdapter(ampdoc, validConfig, context);
+      expect(adapter.getConfig()).to.deep.equal({
+        'iframeSrc': 'https://acme.com/iframe',
+        'iframeVars': ['VAR1'],
       });
       expect(adapter.isAuthorizationEnabled()).to.be.true;
       expect(adapter.isPingbackEnabled()).to.be.true;
@@ -70,6 +84,61 @@ describes.realWin('AccessIframeAdapter', {
         new AccessIframeAdapter(ampdoc, validConfig, context);
       }).to.throw(/https/);
     });
+
+    it('should disallow non-array vars', () => {
+      validConfig['iframeVars'] = {};
+      expect(() => {
+        new AccessIframeAdapter(ampdoc, validConfig, context);
+      }).to.throw(/array/);
+    });
+  });
+
+
+  describe('runtime connect', () => {
+    it('should NOT connect until necessary', () => {
+      const connectStub = sandbox.stub(Messenger.prototype, 'connect');
+      const adapter = new AccessIframeAdapter(ampdoc, validConfig, context);
+      expect(adapter.connectedPromise_).to.be.null;
+      expect(adapter.iframe_.parentNode).to.be.null;
+      expect(connectStub).to.not.be.called;
+    });
+
+    it('should connect on first and only first authorize', () => {
+      const connectStub = sandbox.stub(Messenger.prototype, 'connect');
+      const adapter = new AccessIframeAdapter(ampdoc, validConfig, context);
+      adapter.authorize();
+      expect(adapter.connectedPromise_).to.not.be.null;
+      expect(adapter.iframe_.parentNode).to.not.be.null;
+      expect(connectStub).to.be.calledOnce;
+    });
+
+    it('should resolve vars', () => {
+      contextMock.expects('collectUrlVars')
+          .withExactArgs('VAR1&VAR2', false)
+          .returns(Promise.resolve({
+            'VAR1': 'A',
+            'VAR2': 'B',
+          }))
+          .once();
+      validConfig['iframeVars'] = ['VAR1', 'VAR2'];
+      const sendStub = sandbox.stub(Messenger.prototype, 'sendCommandRsvp')
+          .returns(Promise.resolve({}));
+      const adapter = new AccessIframeAdapter(ampdoc, validConfig, context);
+      const promise = adapter.connect();
+      adapter.handleCommand_('connect');
+      return promise.then(() => {
+        expect(sendStub).to.be.calledOnce;
+        expect(sendStub).to.be.calledWithExactly('start', {
+          'protocol': 'amp-access',
+          'config': Object.assign({}, validConfig, {
+            'iframeVars': {
+              'VAR1': 'A',
+              'VAR2': 'B',
+            },
+          }),
+        });
+      });
+    });
   });
 
 
@@ -87,6 +156,7 @@ describes.realWin('AccessIframeAdapter', {
           })
           .returns(Promise.resolve())
           .once();
+      adapter.connect();
       adapter.handleCommand_('connect');
     });
 
