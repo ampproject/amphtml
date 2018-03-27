c9dd40df6e9582659c15c7485265a513b8a3bea8
Jon Newmuis <newmuis@google.com>
2018-03-27 14:46:02 -0400

    Update test to use TOGGLE_FALLBACK

--- extensions/amp-story/0.1/test/test-amp-story-store-service.js
+++ extensions/amp-story/0.1/test/test-amp-story-store-service.js
@@ -123,7 +123,7 @@ describes.fakeWin('amp-story-store-service actions', {}, env => {
   it('should set the fallback state', () => {
     const listenerSpy = sandbox.spy();
     storeService.subscribe(StateProperty.FALLBACK_STATE, listenerSpy);
-    storeService.dispatch(Action.ENTER_FALLBACK);
+    storeService.dispatch(Action.TOGGLE_FALLBACK, true);
     expect(listenerSpy).to.have.been.calledOnce;
     expect(listenerSpy).to.have.been.calledWith(true);
   });
