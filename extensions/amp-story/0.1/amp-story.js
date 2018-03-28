4822abb6dee06f9afe6a70a376b71b9ec3069847
Jon Newmuis <newmuis@users.noreply.github.com>
2018-03-28 09:10:16 -0400

    Only do CSS.supports detection on browsers that support it (#14270)
    * Only do CSS.supports detection on browsers that support it.
    
    * Always return boolean from isBrowserSupported

--- extensions/amp-story/0.1/amp-story.js
+++ extensions/amp-story/0.1/amp-story.js
@@ -1635,7 +1635,8 @@ export class AmpStory extends AMP.BaseElement {
    *     for amp-story.
    */
   static isBrowserSupported(win) {
-    return win.CSS.supports('display', 'grid');
+    return Boolean(win.CSS && win.CSS.supports &&
+        win.CSS.supports('display', 'grid'));
   }
 }
 
