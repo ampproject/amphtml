3f5e732c93bf32b7127f79779e2c9d3dabd98e9a
Dima Voytenko <dvoytenko@google.com>
2018-04-02 16:37:36 -0700

    SwG release 0.1.22.3 (#14356)

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,8 +13,200 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /** Version: 0.1.22.2 */
+ /** Version: 0.1.22.3 */
 'use strict';
+
+
+
+/**
+ * @param {!Document} doc
+ * @return {string}
+ */
+function getReadyState(doc) {
+  return /** @type {string} */ (doc['readyState']);
+}
+
+
+/**
+ * Whether the document is ready.
+ * @param {!Document} doc
+ * @return {boolean}
+ */
+function isDocumentReady(doc) {
+  const readyState = getReadyState(doc);
+  return readyState != 'loading' && readyState != 'uninitialized';
+}
+
+/**
+ * Calls the callback when document is ready.
+ * @param {!Document} doc
+ * @param {function(!Document)} callback
+ */
+function onDocumentReady(doc, callback) {
+  onDocumentState(doc, isDocumentReady, callback);
+}
+
+/**
+ * Calls the callback when document's state satisfies the stateFn.
+ * @param {!Document} doc
+ * @param {function(!Document):boolean} stateFn
+ * @param {function(!Document)} callback
+ */
+function onDocumentState(doc, stateFn, callback) {
+  let ready = stateFn(doc);
+  if (ready) {
+    callback(doc);
+  } else {
+    const readyListener = () => {
+      if (stateFn(doc)) {
+        if (!ready) {
+          ready = true;
+          callback(doc);
+        }
+        doc.removeEventListener('readystatechange', readyListener);
+      }
+    };
+    doc.addEventListener('readystatechange', readyListener);
+  }
+}
+
+/**
+ * Returns a promise that is resolved when document is ready.
+ * @param {!Document} doc
+ * @return {!Promise<!Document>}
+ */
+function whenDocumentReady(doc) {
+  return new Promise(resolve => {
+    onDocumentReady(doc, resolve);
+  });
+}
+
+
+
+
+/**
+ * @interface
+ */
+class Doc {
+
+  /**
+   * @return {!Window}
+   */
+  getWin() {}
+
+  /**
+   * The `Document` node or analog.
+   * @return {!Node}
+   */
+  getRootNode() {}
+
+  /**
+   * The `Document.documentElement` element or analog.
+   * @return {!Element}
+   */
+  getRootElement() {}
+
+  /**
+   * The `Document.head` element or analog. Returns `null` if not available
+   * yet.
+   * @return {!Element}
+   */
+  getHead() {}
+
+  /**
+   * The `Document.body` element or analog. Returns `null` if not available
+   * yet.
+   * @return {?Element}
+   */
+  getBody() {}
+
+  /**
+   * Whether the document has been fully constructed.
+   * @return {boolean}
+   */
+  isReady() {}
+
+  /**
+   * Resolved when document has been fully constructed.
+   * @return {!Promise}
+   */
+  whenReady() {}
+}
+
+
+/** @implements {Doc} */
+class GlobalDoc {
+
+  /**
+   * @param {!Window|!Document} winOrDoc
+   */
+  constructor(winOrDoc) {
+    const isWin = !!winOrDoc.document;
+    /** @private @const {!Window} */
+    this.win_ = isWin ?
+        /** @type {!Window} */ (winOrDoc) :
+        /** @type {!Window} */ (
+            (/** @type {!Document} */ (winOrDoc)).defaultView);
+    /** @private @const {!Document} */
+    this.doc_ = isWin ?
+        /** @type {!Window} */ (winOrDoc).document :
+        /** @type {!Document} */ (winOrDoc);
+  }
+
+  /** @override */
+  getWin() {
+    return this.win_;
+  }
+
+  /** @override */
+  getRootNode() {
+    return this.doc_;
+  }
+
+  /** @override */
+  getRootElement() {
+    return this.doc_.documentElement;
+  }
+
+  /** @override */
+  getHead() {
+    // `document.head` always has a chance to be parsed, at least partially.
+    return /** @type {!Element} */ (this.doc_.head);
+  }
+
+  /** @override */
+  getBody() {
+    return this.doc_.body;
+  }
+
+  /** @override */
+  isReady() {
+    return isDocumentReady(this.doc_);
+  }
+
+  /** @override */
+  whenReady() {
+    return whenDocumentReady(this.doc_);
+  }
+}
+
+
+/**
+ * @param {!Document|!Window|!Doc} input
+ * @return {!Doc}
+ */
+function resolveDoc(input) {
+  // Is it a `Document`
+  if ((/** @type {!Document} */ (input)).nodeType === /* DOCUMENT */ 9) {
+    return new GlobalDoc(/** @type {!Document} */ (input));
+  }
+  // Is it a `Window`?
+  if ((/** @type {!Window} */ (input)).document) {
+    return new GlobalDoc(/** @type {!Window} */ (input));
+  }
+  return /** @type {!Doc} */ (input);
+}
+
 
 
 
@@ -124,72 +316,6 @@ function isArray(value) {
 
 
 
-
-/**
- * @param {!Document} doc
- * @return {string}
- */
-function getReadyState(doc) {
-  return /** @type {string} */ (doc['readyState']);
-}
-
-
-/**
- * Whether the document is ready.
- * @param {!Document} doc
- * @return {boolean}
- */
-function isDocumentReady(doc) {
-  const readyState = getReadyState(doc);
-  return readyState != 'loading' && readyState != 'uninitialized';
-}
-
-/**
- * Calls the callback when document is ready.
- * @param {!Document} doc
- * @param {function(!Document)} callback
- */
-function onDocumentReady(doc, callback) {
-  onDocumentState(doc, isDocumentReady, callback);
-}
-
-/**
- * Calls the callback when document's state satisfies the stateFn.
- * @param {!Document} doc
- * @param {function(!Document):boolean} stateFn
- * @param {function(!Document)} callback
- */
-function onDocumentState(doc, stateFn, callback) {
-  let ready = stateFn(doc);
-  if (ready) {
-    callback(doc);
-  } else {
-    const readyListener = () => {
-      if (stateFn(doc)) {
-        if (!ready) {
-          ready = true;
-          callback(doc);
-        }
-        doc.removeEventListener('readystatechange', readyListener);
-      }
-    };
-    doc.addEventListener('readystatechange', readyListener);
-  }
-}
-
-/**
- * Returns a promise that is resolved when document is ready.
- * @param {!Document} doc
- * @return {!Promise<!Document>}
- */
-function whenDocumentReady(doc) {
-  return new Promise(resolve => {
-    onDocumentReady(doc, resolve);
-  });
-}
-
-
-
 /**
  * Simple wrapper around JSON.parse that casts the return value
  * to JsonObject.
@@ -231,11 +357,11 @@ const ALREADY_SEEN = '__SUBSCRIPTIONS-SEEN__';
 class PageConfigResolver {
 
   /**
-   * @param {!Window} win
+   * @param {!Window|!Document|!Doc} winOrDoc
    */
-  constructor(win) {
-    /** @private @const {!Window} */
-    this.win_ = win;
+  constructor(winOrDoc) {
+    /** @private @const {!Doc} */
+    this.doc_ = resolveDoc(winOrDoc);
 
     /** @private {?function((!PageConfig|!Promise))} */
     this.configResolver_ = null;
@@ -246,11 +372,11 @@ class PageConfigResolver {
     });
 
     /** @private @const {!MetaParser} */
-    this.metaParser_ = new MetaParser(win);
+    this.metaParser_ = new MetaParser(this.doc_);
     /** @private @const {!JsonLdParser} */
-    this.ldParser_ = new JsonLdParser(win);
+    this.ldParser_ = new JsonLdParser(this.doc_);
     /** @private @const {!MicrodataParser} */
-    this.microdataParser_ = new MicrodataParser(win);
+    this.microdataParser_ = new MicrodataParser(this.doc_);
   }
 
   /**
@@ -259,7 +385,7 @@ class PageConfigResolver {
   resolveConfig() {
     // Try resolve the config at different times.
     Promise.resolve().then(this.check.bind(this));
-    whenDocumentReady(this.win_.document).then(this.check.bind(this));
+    this.doc_.whenReady().then(this.check.bind(this));
     return this.configPromise_;
   }
 
@@ -271,7 +397,6 @@ class PageConfigResolver {
     if (!this.configResolver_) {
       return null;
     }
-
     let config = this.metaParser_.check();
     if (!config) {
       config = this.ldParser_.check();
@@ -279,12 +404,11 @@ class PageConfigResolver {
     if (!config) {
       config = this.microdataParser_.check();
     }
-
     if (config) {
       // Product ID has been found: initialize the rest of the config.
       this.configResolver_(config);
       this.configResolver_ = null;
-    } else if (isDocumentReady(this.win_.document)) {
+    } else if (this.doc_.isReady()) {
       this.configResolver_(Promise.reject(
           new Error('No config could be discovered in the page')));
       this.configResolver_ = null;
@@ -296,31 +420,32 @@ class PageConfigResolver {
 
 class MetaParser {
   /**
-   * @param {!Window} win
+   * @param {!Doc} doc
    */
-  constructor(win) {
-    /** @private @const {!Window} */
-    this.win_ = win;
+  constructor(doc) {
+    /** @private @const {!Doc} */
+    this.doc_ = doc;
   }
 
   /**
    * @return {?PageConfig}
    */
   check() {
-    if (!this.win_.document.body) {
+    if (!this.doc_.getBody()) {
       // Wait until the whole `<head>` is parsed.
       return null;
     }
 
     // Try to find product id.
-    const productId = getMetaTag(this.win_, 'subscriptions-product-id');
+    const productId = getMetaTag(this.doc_.getRootNode(),
+        'subscriptions-product-id');
     if (!productId) {
       return null;
     }
 
     // Is locked?
-    const accessibleForFree =
-        getMetaTag(this.win_, 'subscriptions-accessible-for-free');
+    const accessibleForFree = getMetaTag(this.doc_.getRootNode(),
+        'subscriptions-accessible-for-free');
     const locked = (accessibleForFree &&
         accessibleForFree.toLowerCase() == 'false') || false;
 
@@ -331,26 +456,26 @@ class MetaParser {
 
 class JsonLdParser {
   /**
-   * @param {!Window} win
+   * @param {!Doc} doc
    */
-  constructor(win) {
-    /** @private @const {!Window} */
-    this.win_ = win;
+  constructor(doc) {
+    /** @private @const {!Doc} */
+    this.doc_ = doc;
   }
 
   /**
    * @return {?PageConfig}
    */
   check() {
-    if (!this.win_.document.body) {
+    if (!this.doc_.getBody()) {
       // Wait until the whole `<head>` is parsed.
       return null;
     }
 
-    const domReady = isDocumentReady(this.win_.document);
+    const domReady = this.doc_.isReady();
 
     // type: 'application/ld+json'
-    const elements = this.win_.document.querySelectorAll(
+    const elements = this.doc_.getRootNode().querySelectorAll(
         'script[type="application/ld+json"]');
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
@@ -486,62 +611,168 @@ class JsonLdParser {
 
 class MicrodataParser {
   /**
-   * @param {!Window} win
+   * @param {!Doc} doc
    */
-  constructor(win) {
-    /** @private @const {!Window} */
-    this.win_ = win;
+  constructor(doc) {
+    /** @private @const {!Doc} */
+    this.doc_ = doc;
+    /** @private {?boolean} */
+    this.access_ = null;
+    /** @private {?string} */
+    this.productId_ = null;
+  }
+
+  /**
+   * Returns false if access is restricted, otherwise true
+   * @param {!Element} root An element that is an item of type 'NewsArticle'
+   * @return {?boolean} locked access
+   * @private
+   */
+  discoverAccess_(root) {
+    const ALREADY_SEEN = 'alreadySeenForAccessInfo';
+    const nodeList = root
+        .querySelectorAll("[itemprop='isAccessibleForFree']");
+    for (let i = 0; nodeList[i]; i++) {
+      const element = nodeList[i];
+      const content = element.getAttribute('content') || element.textContent;
+      if (!content) {
+        continue;
+      }
+      if (this.isValidElement_(element, root, ALREADY_SEEN)) {
+        let accessForFree = null;
+        if (content.toLowerCase() == 'true') {
+          accessForFree = true;
+        } else if (content.toLowerCase() == 'false') {
+          accessForFree = false;
+        }
+        return accessForFree;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Verifies if an element is valid based on the following
+   * - child of an item of type 'NewsArticle'
+   * - not a child of an item of any other type
+   * - not seen before, marked using the alreadySeen tag
+   * @param {?Element} current the element to be verified
+   * @param {!Element} root the parent to track up to
+   * @param {!string} alreadySeen used to tag already visited nodes
+   * @return {!boolean} valid node
+   * @private
+   */
+  isValidElement_(current, root, alreadySeen) {
+    for (let node = current;
+        node && !node[alreadySeen]; node = node.parentNode) {
+      node[alreadySeen] = true;
+      if (node.hasAttribute('itemscope')) {
+        /**{?string} */
+        const type = node.getAttribute('itemtype');
+        if (type.indexOf('http://schema.org/NewsArticle') >= 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Obtains the product ID that meets the requirements
+   * - child of an item of type 'NewsArticle'
+   * - Not a child of an item of type 'Section'
+   * - child of an item of type 'productID'
+   * @param {!Element} root An element that is an item of type 'NewsArticle'
+   * @return {?string} product ID, if found
+   * @private
+   */
+  discoverProductId_(root) {
+    const ALREADY_SEEN = 'alreadySeenForProductInfo';
+    const nodeList = root
+        .querySelectorAll('[itemprop="productID"]');
+    for (let i = 0; nodeList[i]; i++) {
+      const element = nodeList[i];
+      const content = element.getAttribute('content') || element.textContent;
+      const item = element.closest('[itemtype][itemscope]');
+      const type = item.getAttribute('itemtype');
+      if (type.indexOf('http://schema.org/Product') <= -1) {
+        continue;
+      }
+      if (this.isValidElement_(item.parentElement, root, ALREADY_SEEN)) {
+        return content;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Returns PageConfig if available
+   * @return {?PageConfig} PageConfig found so far
+   */
+  getPageConfig_() {
+    let locked = null;
+    if (this.access_ != null) {
+      locked = !this.access_;
+    } else if (this.doc_.isReady()) {
+      // Default to unlocked
+      locked = false;
+    }
+    if (this.productId_ != null && locked != null) {
+      return new PageConfig(this.productId_, locked);
+    }
+    return null;
+  }
+
+  /**
+   * Extracts page config from Microdata in the DOM
+   * @return {?PageConfig} PageConfig found
+   */
+  tryExtractConfig_() {
+    let config = this.getPageConfig_();
+    if (config) {
+      return config;
+    }
+    const nodeList = this.doc_.getRootNode().querySelectorAll(
+        '[itemscope][itemtype*="http://schema.org/NewsArticle"]');
+    for (let i = 0; nodeList[i] && config == null; i++) {
+      const element = nodeList[i];
+      if (this.access_ == null) {
+        this.access_ = this.discoverAccess_(element);
+      }
+      if (!this.productId_) {
+        this.productId_ = this.discoverProductId_(element);
+      }
+      config = this.getPageConfig_();
+    }
+    return config;
   }
 
   /**
    * @return {?PageConfig}
    */
   check() {
-    if (!this.win_.document.body) {
+    if (!this.doc_.getBody()) {
       // Wait until the whole `<head>` is parsed.
       return null;
     }
-
-    // TODO(sohanirao): create page config from DOM
-    /* Psuedo code
-      Initialize an empty list of items
-      Get all items (query elements that have itemscope)
-        For each element
-          get a list of properties
-            if a property is 'isAccessibleForFree', create an entry in the items list:
-            {'isAccessibleForFree': booleanvalue, 'element': elementWithTheProperty}
-            if a property is 'productID', create an entry in the items list as follows:
-            {'productID': value, 'element': elementWithTheProperty}
-      Initialize an empty list of results
-      Iterate through entries in the items list
-        Pop an entry,
-          if the element's type is "NewsArticle"
-            add it to the results list
-          else if this element is not the root
-            create an entry with either 'isAccessibleForFree' or 'productID' and corresponding value,
-            and the parent of the element as the entry for 'element' key, add this entry to items list
-      When the items list is processed, results list contains entries that meet the requirements:
-        type is NewsArticle, contains either isAccessibleForFree value or productID value.
-        When the results array contains entries for both and the corresponding elements are the same,
-          return PageConfig, otherwise return null
-    */
-    return null;
+    return this.tryExtractConfig_();
   }
 }
-
 
 /**
  * Returns the value from content attribute of a meta tag with given name.
  *
  * If multiple tags are found, the first value is returned.
  *
- * @param {!Window} win
+ * @param {!Node} rootNode
  * @param {string} name The tag name to look for.
  * @return {?string} attribute value or empty string.
  * @private
  */
-function getMetaTag(win, name) {
-  const el = win.document.querySelector(`meta[name="${name}"]`);
+function getMetaTag(rootNode, name) {
+  const el = rootNode.querySelector(`meta[name="${name}"]`);
   if (el) {
     return el.getAttribute('content');
   }
@@ -552,6 +783,7 @@ function getMetaTag(win, name) {
 
 
 export {
+  Doc,
   PageConfig,
   PageConfigResolver,
 };
