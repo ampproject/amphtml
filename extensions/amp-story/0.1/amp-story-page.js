14d6ecbac50fbdff90a9027b838318007192fff0
Alan <alanorozco@users.noreply.github.com>
2018-04-02 14:43:53 -0500

    🔀Gate `VideoService` implementation with experiment (#14324)

--- extensions/amp-story/0.1/amp-story-page.js
+++ extensions/amp-story/0.1/amp-story-page.js
@@ -33,6 +33,7 @@ import {Layout} from '../../../src/layout';
 import {LoadingSpinner} from './loading-spinner';
 import {MediaPool} from './media-pool';
 import {PageScalingService} from './page-scaling';
+import {Services} from '../../../src/services';
 import {
   closestBySelector,
   matches,
@@ -43,6 +44,7 @@ import {dev} from '../../../src/log';
 import {getLogEntries} from './logging';
 import {getMode} from '../../../src/mode';
 import {listen} from '../../../src/event-helper';
+import {toArray} from '../../../src/types';
 import {upgradeBackgroundAudio} from './audio';
 
 /**
@@ -138,6 +140,7 @@ export class AmpStoryPage extends AMP.BaseElement {
   /** @override */
   buildCallback() {
     upgradeBackgroundAudio(this.element);
+    this.ownVideoAutoplay_();
     this.markMediaElementsWithPreload_();
     this.initializeMediaPool_();
     this.maybeCreateAnimationManager_();
@@ -148,6 +151,18 @@ export class AmpStoryPage extends AMP.BaseElement {
         navigationDirection => this.navigateOnTap(navigationDirection));
     this.advancement_
         .addProgressListener(progress => this.emitProgress_(progress));
+  }
+
+
+  /** @private */
+  ownVideoAutoplay_() {
+    const videos = this.element.querySelectorAll('amp-video');
+    if (videos.length < 1) {
+      return;
+    }
+    toArray(videos).forEach(el => {
+      Services.videoManagerForDoc(this.element).delegateAutoplay(el);
+    });
   }
 
 
---
+++ src/service/video-service-sync-impl.js
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Services} from '../services';
+import {dev} from '../log';
+import {getAmpdoc} from '../service';
+import {getElementServiceForDoc} from '../element-service';
+import {isExperimentOn} from '../experiments';
+
+
+/** @private @const {string} */
+const EXTENSION = 'amp-video-service';
+
+
+/**
+ * @typedef
+ * {../../extensions/amp-video-service/0.1/amp-video-service.VideoService}
+ */
+let VideoServiceDef; // alias for line length.
+
+
+/**
+ * Provides unified behavior for all videos regardless of implementation.
+ *
+ * This service is a façade around an async-loaded implementation.
+ * See {@link AmpVideoService} for the underlying service.
+ *
+ * This co-eexists with `VideoManager` (deprecated) while the implementation
+ * is migrated.
+ *
+ * @implements {./video-manager-impl.VideoService}
+ */
+export class VideoServiceSync {
+
+  /** @param {!./ampdoc-impl.AmpDoc} ampdoc */
+  constructor(ampdoc) {
+    const {win} = ampdoc;
+
+    /** @private @const {!Promise<!VideoServiceDef>}  */
+    this.asyncImpl_ = VideoServiceSync.videoServiceFor(win, ampdoc);
+  }
+
+  /**
+   * @param {!Window} win
+   * @return {boolean}
+   * @visibleForTesting
+   */
+  static shouldBeUsedIn(win) {
+    return isExperimentOn(win, 'video-service');
+  }
+
+  /**
+   * @param {!Window} win
+   * @param {!Node|!./ampdoc-impl.AmpDoc} nodeOrDoc
+   * @return {!Promise<!VideoServiceDef>}
+   * @visibleForTesting
+   */
+  // Not exposed in ../services.js since we don't want other modules to
+  // instantiate or access the service.
+  static videoServiceFor(win, nodeOrDoc) {
+    const extensions = Services.extensionsFor(win);
+    const ampdoc = getAmpdoc(nodeOrDoc);
+    return extensions.installExtensionForDoc(ampdoc, EXTENSION)
+        .then(() => /** @type {!Promise<!VideoServiceDef>} */ (
+          getElementServiceForDoc(nodeOrDoc, 'video-service', EXTENSION)));
+  }
+
+  /** @override */
+  register(video, unusedFromV1manageAutoplay = true) {
+    this.asyncImpl_.then(impl =>
+      impl.register(video));
+  }
+
+  /** @override */
+  delegateAutoplay(video, optObservable = null) {
+    // TODO(alanorozco): Make observable required once implementation of
+    // `VideoService` finalizes.
+    const observable = dev().assert(optObservable,
+        '`VideoService` requires an observable for autoplay delegation.');
+
+    this.asyncImpl_.then(impl =>
+      impl.delegateAutoplay(video, observable));
+  }
+
+  /** @override */
+  getAnalyticsDetails(video) {
+    return this.asyncImpl_.then(impl =>
+      impl.getAnalyticsDetails(video));
+  }
+}
