b592f0ed18509c1c6469de7c7f4669901f526563
zhouyx <zhouyx@google.com>
2018-04-04 14:51:33 -0700

    amp-consent revoke action

--- extensions/amp-consent/0.1/amp-consent.js
+++ extensions/amp-consent/0.1/amp-consent.js
@@ -87,11 +87,27 @@ export class AmpConsent extends AMP.BaseElement {
 
     /** @private {!Object<string, function()>} */
     this.dialogResolver_ = map();
+
+    /** @private {!Object<string, boolean>} */
+    this.consentUIPendingMap_ = map();
   }
 
   getConsentPolicy() {
     // amp-consent should not be blocked by itself
     return null;
+  }
+
+  /**
+   * Handles the revoke action.
+   * Display consent UI.
+   * @param {string} consentId
+   */
+  handleRevoke_(consentId) {
+    user().assert(consentId, 'revoke must specify a consent instance id');
+    user().assert(this.consentConfig_[consentId],
+        `consent with id ${consentId} not found`);
+    // toggle the UI for this consent
+    this.scheduleDisplay_(consentId);
   }
 
   buildCallback() {
@@ -101,11 +117,6 @@ export class AmpConsent extends AMP.BaseElement {
 
     user().assert(this.element.getAttribute('id'),
         'amp-consent should have an id');
-
-    this.registerAction('accept', () => this.handleAction_(ACTION_TYPE.ACCEPT));
-    this.registerAction('reject', () => this.handleAction_(ACTION_TYPE.REJECT));
-    this.registerAction('dismiss',
-        () => this.handleAction_(ACTION_TYPE.DISMISS));
 
     // TODO: Decide what to do with incorrect configuration.
     this.assertAndParseConfig_();
@@ -135,7 +146,52 @@ export class AmpConsent extends AMP.BaseElement {
     Promise.all([consentStateManagerPromise, notificationUiManagerPromise])
         .then(() => {
           this.init_();
+          this.enableInteractions_();
         });
+  }
+
+  /**
+   * Register a list of user action functions
+   */
+  enableInteractions_() {
+    this.registerAction('accept', () => this.handleAction_(ACTION_TYPE.ACCEPT));
+    this.registerAction('reject', () => this.handleAction_(ACTION_TYPE.REJECT));
+    this.registerAction('dismiss',
+        () => this.handleAction_(ACTION_TYPE.DISMISS));
+    this.registerAction('revoke', invocation => {
+      const args = invocation.args;
+      const consentId = args && args['consentId'];
+      this.handleRevoke_(consentId);
+    });
+  }
+
+  /**
+   * Returns a promise that attempt to show prompt UI for instanceId
+   * @param {string} instanceId
+   */
+  scheduleDisplay_(instanceId) {
+    dev().assert(this.notificationUiManager_,
+        'notification ui manager not found');
+
+    if (this.consentUIPendingMap_[instanceId]) {
+      // Already pending to be shown. Do nothing.
+      // TODO(@zhouyx): Need to fix this
+      // We still need to show management UI even consent not required.
+      return;
+    }
+
+    if (!this.consentUIRequired_[instanceId]) {
+      // If consent not required.
+      return;
+    }
+
+    if (!this.consentUI_[instanceId]) {
+      // If consent UI not found. Do nothing.
+      return;
+    }
+
+    this.consentUIPendingMap_[instanceId] = true;
+    this.notificationUiManager_.registerUI(this.show_.bind(this, instanceId));
   }
 
   /**
@@ -144,10 +200,6 @@ export class AmpConsent extends AMP.BaseElement {
    * @return {!Promise}
    */
   show_(instanceId) {
-    if (!this.consentUIRequired_[instanceId] || !this.consentUI_[instanceId]) {
-      return Promise.resolve();
-    }
-
     dev().assert(!this.currentDisplayInstance_,
         'Other consent instance on display');
 
@@ -187,6 +239,7 @@ export class AmpConsent extends AMP.BaseElement {
       this.dialogResolver_[this.currentDisplayInstance_]();
       this.dialogResolver_[this.currentDisplayInstance_] = null;
     }
+    this.consentUIPendingMap_[this.currentDisplayInstance_] = false;
     this.currentDisplayInstance_ = null;
   }
 
@@ -372,9 +425,7 @@ export class AmpConsent extends AMP.BaseElement {
     this.consentStateManager_.getConsentInstanceState(instanceId)
         .then(state => {
           if (state == CONSENT_ITEM_STATE.UNKNOWN) {
-
-            this.notificationUiManager_.registerUI(
-                this.show_.bind(this, instanceId));
+            this.scheduleDisplay_(instanceId);
           }
         });
   }
