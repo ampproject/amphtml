0fe7fd2609e7ae3d12a487913835af93800703ef
Prateek Bhatnagar <prateek89born@gmail.com>
2018-04-06 13:19:43 -0700

    WIP viewer platform

--- extensions/amp-subscriptions/0.1/amp-subscriptions.js
+++ extensions/amp-subscriptions/0.1/amp-subscriptions.js
@@ -33,6 +33,7 @@ import {getMode} from '../../../src/mode';
 import {getSourceOrigin, getWinOrigin} from '../../../src/url';
 import {installStylesForDoc} from '../../../src/style-installer';
 import {tryParseJson} from '../../../src/json';
+import { ViewerSubscriptionPlatform } from './viewer-subscription-platform';
 
 /** @const */
 const TAG = 'amp-subscriptions';
@@ -292,93 +293,22 @@ export class SubscriptionService {
         'Product id is null'
     ));
     this.platformStore_ = new PlatformStore(serviceIds);
-    // TODO: Implement viewer authentication class
     this.platformConfig_['services'].forEach(service => {
-      this.initializeLocalPlatforms_(service);
+      if ((service['serviceId'] || 'local') == 'local') {
+        const viewerPlatform = new ViewerSubscriptionPlatform(
+            this.ampdoc_,
+            service,
+            this.serviceAdapter_
+        );
+        this.platformStore_.resolvePlatform('local', viewerPlatform);
+        viewerPlatform.getEntitlements(publicationId, currentProductId, origin)
+            .then(entitlement => {
+              entitlement.setCurrentProduct(currentProductId);
+              // Viewer authorization is redirected to use local platform instead.
+              this.platformStore_.resolveEntitlement('local', entitlement);
+            });
+      }
     });
-    this.viewer_.sendMessageAwaitResponse('auth', dict({
-      'publicationId': publicationId,
-      'productId': currentProductId,
-      'origin': origin,
-    })).then(entitlementData => {
-      const authData = (entitlementData || {})['authorization'];
-      if (!authData) {
-        return this.platformStore_.resolveEntitlement('local',
-            Entitlement.empty('local'));
-      }
-
-      return this.verifyAuthToken_(authData).then(entitlement => {
-        entitlement.setCurrentProduct(currentProductId);
-        // Viewer authorization is redirected to use local platform instead.
-        this.platformStore_.resolveEntitlement('local', entitlement);
-      }).catch(reason => {
-        this.sendAuthTokenErrorToViewer_(reason.message);
-        throw reason;
-      });
-
-    }, reason => {
-      throw user().createError('Viewer authorization failed', reason);
-    });
-  }
-
-  /**
-   * Logs error and sends message to viewer
-   * @param {string} token
-   * @return {!Promise<!Entitlement>}
-   * @private
-   */
-  verifyAuthToken_(token) {
-    return new Promise(resolve => {
-      const origin = getWinOrigin(this.ampdoc_.win);
-      const sourceOrigin = getSourceOrigin(this.ampdoc_.win.location);
-      const decodedData = this.jwtHelper_.decode(token);
-      const currentProductId = /** @type {string} */ (user().assert(
-          this.pageConfig_.getProductId(),
-          'Product id is null'
-      ));
-      if (decodedData['aud'] != origin && decodedData['aud'] != sourceOrigin) {
-        throw user().createError(
-            `The mismatching "aud" field: ${decodedData['aud']}`);
-      }
-      if (decodedData['exp'] < Math.floor(Date.now() / 1000)) {
-        throw user().createError('Payload is expired');
-      }
-
-      const entitlements = decodedData['entitlements'];
-      let entitlementJson;
-      if (Array.isArray(entitlements)) {
-        for (let index = 0; index < entitlements.length; index++) {
-          const entitlementObject =
-              Entitlement.parseFromJson(entitlements[index]);
-          if (entitlementObject.enables(currentProductId)) {
-            entitlementJson = entitlements[index];
-            break;
-          }
-        }
-      } else if (entitlements) { // Not null
-        entitlementJson = entitlements;
-      }
-
-      let entitlement;
-      if (entitlementJson) {
-        entitlement = Entitlement.parseFromJson(entitlementJson, token);
-      } else {
-        entitlement = Entitlement.empty('local');
-      }
-      entitlement.service = 'local';
-      resolve(entitlement);
-    });
-  }
-
-  /**
-   * Logs error and sends message to viewer
-   * @param {string} errorString
-   * @private
-   */
-  sendAuthTokenErrorToViewer_(errorString) {
-    this.viewer_.sendMessage('auth-rejected', dict({
-      'reason': errorString,
-    }));
   }
 
   /**
