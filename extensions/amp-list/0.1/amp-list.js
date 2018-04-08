66a1ddec138ea6f8520194167cc1e17255f222d4
William Chou <willchou@google.com>
2018-04-08 03:07:03 -0400

    Add comment

--- extensions/amp-list/0.1/amp-list.js
+++ extensions/amp-list/0.1/amp-list.js
@@ -88,6 +88,8 @@ export class AmpList extends AMP.BaseElement {
 
   /** @override */
   buildCallback() {
+    // Store this in buildCallback() because `this.element` sometimes 
+    // is missing attributes in the constructor.
     this.initialSrc_ = this.element.getAttribute('src');
     
     this.container_ = this.win.document.createElement('div');
