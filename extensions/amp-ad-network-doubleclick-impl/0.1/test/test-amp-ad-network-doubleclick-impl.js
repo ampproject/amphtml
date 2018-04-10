8f6b972fd5b00748eab4abb89a97b28396f55aa4
Gene Levitzky <levitzky@google.com>
2018-04-10 19:17:19 -0400

    Fixing misplaced comment.

--- extensions/amp-ad-network-doubleclick-impl/0.1/test/test-amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/test/test-amp-ad-network-doubleclick-impl.js
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-import '../../../amp-ad/0.1/amp-ad';
 // Need the following side-effect import because in actual production code,
 // Fast Fetch impls are always loaded via an AmpAd tag, which means AmpAd is
 // always available for them. However, when we test an impl in isolation,
 // AmpAd is not loaded already, so we need to load it separately.
+import '../../../amp-ad/0.1/amp-ad';
 import * as sinon from 'sinon';
 import {
   AMP_SIGNATURE_HEADER,
