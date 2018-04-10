6e219f5d82c1ee530479b89812b88065e20eefee
Lukasz Rosa <lukasz.rosa@grupawp.pl>
2018-04-10 10:20:23 +0200

    WP Media Ad - First version for testing

--- ads/_config.js
+++ ads/_config.js
@@ -144,7 +144,7 @@ export const adConfig = {
   adpicker: {
     renderStartImplemented: true,
   },
-
+  
   adplugg: {
     prefetch: 'https://www.adplugg.com/serve/js/ad.js',
     renderStartImplemented: true,
@@ -879,6 +879,10 @@ export const adConfig = {
 
   widespace: {},
 
+  wpmedia: {
+    prefetch: 'https://std.wpcdn.pl/wpjslib/amp/wpjslib-amp-debug.js',
+  },
+  
   xlift: {
     prefetch: 'https://cdn.x-lift.jp/resources/common/xlift_amp.js',
     renderStartImplemented: true,
