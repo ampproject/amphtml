86bde1314326fc50b63a42a8a0391cf116db04ff
Alan <alanorozco@users.noreply.github.com>
2018-04-13 12:14:51 -0500

    ♻️✨Register common video actions and events (#14384)
    Partial for #13674
    
    For comparison, se [`video-manager-impl.js`](https://github.com/ampproject/amphtml/blob/b3af26ce04c264495f231269273c5c78d8bf447a/src/service/video-manager-impl.js).

--- extensions/amp-video-service/0.1/amp-video-service.js
+++ extensions/amp-video-service/0.1/amp-video-service.js
@@ -21,18 +21,30 @@
  * it gets automatically inserted by the runtime when required.
  */
 
+import {ActionTrust} from '../../../src/action-trust';
+import {CommonSignals} from '../../../src/common-signals';
+import {Observable} from '../../../src/observable';
+import {Services} from '../../../src/services';
+import {VideoEvents} from '../../../src/video-interface';
+import {createCustomEvent} from '../../../src/event-helper';
 import {dev} from '../../../src/log';
+import {isFiniteNumber} from '../../../src/types';
+import {listen} from '../../../src/event-helper';
 
 
 /** @private @const {string} */
 const TAG = 'amp-video-service';
 
 
+/** @private @const {string} */
+const ENTRY_PROP = '__AMP_VIDEO_ENTRY__';
+
+
 /**
  * Manages all AMP video players that implement the common Video API
  * {@see ../src/video-interface.VideoInterface}.
  *
-* Provides unified behavior for all videos regardless of implementation.
+ * Provides unified behavior for all videos regardless of implementation.
  *
  *
  * __          __              _
@@ -45,8 +57,8 @@ const TAG = 'amp-video-service';
  *                                     |___/
  *
  * This service is instantiated asynchronously by
- * {@see ../../../src/service/video-service-impl.VideoService}. That should be
- * used by consumers of the APIs exposed here.
+ * {@see ../../../src/service/video-service-sync-impl.VideoServiceSync}. That
+ * service should be used by consumers of the APIs exposed here.
  *
  * If you need to add methods to this class that are public to components,
  * it's most likely that you'll want to implement them here and set wrappers for
@@ -56,17 +68,75 @@ export class VideoService {
 
   /** @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc */
   constructor(ampdoc) {
+    const {win} = ampdoc;
+
     /** @private @const {!../../../src/service/ampdoc-impl.AmpDoc} */
     this.ampdoc_ = ampdoc;
+
+    /** @private @const {!../../../src/service/timer-impl.Timer} */
+    this.timer_ = Services.timerFor(win);
+
+    /** @private {?../../../src/observable.Observable<void>} */
+    this.tick_ = null;
+
+    /** @private @const {function()} */
+    this.boundTick_ = () => this.startTicking_();
   }
 
-  /** @param {!../../../src/video-interface.VideoInterface} unusedVideo */
-  register(unusedVideo) {
-    warnUnimplemented('Video registration');
+  /** @private */
+  startTicking_() {
+    this.tick_.fire();
+    this.timer_.delay(this.boundTick_, 1000);
+  }
+
+  /** @param {!../../../src/video-interface.VideoInterface} video */
+  register(video) {
+    const {element} = video;
+
+    if (this.getEntryOrNull(element)) {
+      return dev().assert(this.getEntryOrNull(element));
+    }
+
+    if (!video.supportsPlatform()) {
+      return null;
+    }
+
+    const entry = VideoEntry.create(this.ampdoc_, this, video);
+
+    this.setEntry_(element, entry);
+
+    return entry;
   }
 
   /**
-   * @param {!AmpElement} unusedVideo
+   * @param {!Element} element
+   * @return {?VideoEntry}
+   */
+  getEntryOrNull(element) {
+    return element[ENTRY_PROP];
+  }
+
+  /** @param {function()} handler */
+  onTick(handler) {
+    this.tick_ = this.tick_ || new Observable();
+    this.tick_.add(handler);
+
+    if (this.tick_.getHandlerCount() == 1) {
+      this.startTicking_();
+    }
+  }
+
+  /**
+   * @param {!Element} element
+   * @param {!VideoEntry} entry
+   * @private
+   */
+  setEntry_(element, entry) {
+    element[ENTRY_PROP] = entry;
+  }
+
+  /**
+   * @param {!Element} unusedVideo
    * @return {!Promise}
    */
   getAnalyticsDetails(unusedVideo) {
@@ -76,10 +146,156 @@ export class VideoService {
 
   /**
    * @param {!AmpElement} unusedVideo
-   * @param {!../../../src/observable.Observable<boolean>} unusedObservable
+   * @param {!Observable<boolean>} unusedObservable
    */
   delegateAutoplay(unusedVideo, unusedObservable) {
     warnUnimplemented('Autoplay delegation');
+  }
+}
+
+
+/**
+ * Handler for a registered video component.
+ * @visibleForTesting
+ */
+export class VideoEntry {
+
+  /**
+   * @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc
+   * @param {!VideoService} videoService
+   * @param {!../../../src/video-interface.VideoInterface} video
+   */
+  constructor(ampdoc, videoService, video) {
+
+    /** @private @const {!../../../src/service/ampdoc-impl.AmpDoc} */
+    this.ampdoc_ = ampdoc;
+
+    /** @private @const {!VideoService} */
+    this.service_ = videoService;
+
+    /** @private @const {!../../../src/video-interface.VideoInterface} */
+    this.video_ = video;
+
+    /** @private {boolean} */
+    this.isPlaying_ = false;
+  }
+
+  /**
+   * @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc
+   * @param {!VideoService} videoService
+   * @param {!../../../src/video-interface.VideoInterface} video
+   */
+  static create(ampdoc, videoService, video) {
+    const entry = new VideoEntry(ampdoc, videoService, video);
+    // Called here as opposed to constructor so that tests can stub VideoEntry
+    // methods before install.
+    entry.install();
+    return entry;
+  }
+
+  /** @param {function()} handler */
+  onPlaybackTick(handler) {
+    this.service_.onTick(() => {
+      if (!this.isPlaying_) {
+        return;
+      }
+      handler();
+    });
+  }
+
+  /** */
+  install() {
+    const {element} = this.video_;
+    const signals = element.signals();
+
+    element.dispatchCustomEvent(VideoEvents.REGISTERED);
+
+    // Unlike events, signals are permanent. We can wait for `REGISTERED` at any
+    // moment in the element's lifecycle and the promise will resolve
+    // appropriately each time.
+    signals.signal(VideoEvents.REGISTERED);
+
+    element.whenBuilt()
+        .then(() => this.onBuilt_());
+
+    signals.whenSignal(CommonSignals.LOAD_START)
+        .then(() => this.onLoadStart_());
+  }
+
+  /** @private */
+  onBuilt_() {
+    const {element} = this.video_;
+
+    this.registerCommonActions_();
+    this.addEventHandlers_();
+
+    element.classList.add('i-amphtml-video-interface');
+  }
+
+  /** @private */
+  onLoadStart_() {
+    this.triggerTimeUpdate_();
+  }
+
+  /** @private */
+  addEventHandlers_() {
+    const {element} = this.video_;
+
+    listen(element, VideoEvents.PAUSE, () => {
+      this.isPlaying_ = false;
+    });
+
+    listen(element, VideoEvents.PLAYING, () => {
+      this.isPlaying_ = true;
+    });
+  }
+
+  /**
+   * Register common actions such as play, pause, etc... so they can be called
+   * using AMP Actions, e.g.: `<button on="tap:myVideo.play">`.
+   * @private
+   */
+  registerCommonActions_() {
+    const video = this.video_;
+
+    // Only require ActionTrust.LOW for video actions to defer to platform
+    // specific handling (e.g. user gesture requirement for unmuted playback).
+    const trust = ActionTrust.LOW;
+
+    video.registerAction('play', () => video.play(/* isAuto */ false), trust);
+    video.registerAction('pause', () => video.pause(), trust);
+    video.registerAction('mute', () => video.mute(), trust);
+    video.registerAction('unmute', () => video.unmute(), trust);
+    video.registerAction('fullscreen', () => video.fullscreenEnter(), trust);
+  }
+
+  /**
+   * Triggers a LOW-TRUST timeupdate event consumable by AMP actions if
+   * required.
+   * Frequency of this event is controlled by VideoService.onTick() and is
+   * every second for now.
+   * @private
+   */
+  triggerTimeUpdate_() {
+    this.onPlaybackTick(() => {
+      const video = this.video_;
+      const time = video.getCurrentTime();
+      const duration = video.getDuration();
+
+      if (!isFiniteNumber(time) ||
+          !isFiniteNumber(duration) ||
+          duration <= 0) {
+        return;
+      }
+
+      const {win} = this.ampdoc_;
+      const {element} = this.video_;
+      const actions = Services.actionServiceForDoc(this.ampdoc_);
+      const name = 'timeUpdate';
+      const percent = time / duration;
+      const event = createCustomEvent(win, `${TAG}.${name}`, {time, percent});
+      actions.trigger(element, name, event, ActionTrust.LOW);
+    });
   }
 }
 
