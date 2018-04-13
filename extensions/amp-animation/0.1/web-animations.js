9e12d894ea0d716923fcea83178e45164a218f50
Aaron Labiaga <alabiaga@google.com>
2018-04-13 17:29:02 -0400

    fix

--- extensions/amp-animation/0.1/web-animations.js
+++ extensions/amp-animation/0.1/web-animations.js
@@ -41,7 +41,7 @@ import {getMode} from '../../../src/mode';
 import {isArray, isObject, toArray} from '../../../src/types';
 import {map} from '../../../src/utils/object';
 import {parseCss} from './css-expr';
-import {setStyle} from '../../../src/style';
+import {setStyles} from '../../../src/style';
 
 
 /** @const {string} */
@@ -126,11 +126,8 @@ export class WebAnimationRunner {
     this.players_ = this.requests_.map(request => {
       // Apply vars.
       if (request.vars) {
-        for (const k in request.vars) {
-          setStyle(request.target, String(request.vars[k]));
-        }
+        setStyles(request.target, request.vars);
       }
-
       const player = request.target.animate(request.keyframes, request.timing);
       player.pause();
       return player;
