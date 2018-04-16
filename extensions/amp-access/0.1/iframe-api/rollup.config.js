9e1ffeed8692fe933e7f296daf8aeeab1836ce59
Prateek Bhatnagar <prateek89born@gmail.com>
2018-04-16 11:28:10 -0700

    WIP es5 transpilation

--- extensions/amp-access/0.1/iframe-api/rollup.config.js
+++ extensions/amp-access/0.1/iframe-api/rollup.config.js
@@ -1,23 +1,16 @@
 import babel from 'rollup-plugin-babel';
-import fs from 'fs';
 import minify from 'rollup-plugin-minify';
-const packageDetils = require(__dirname + '/package.json');
-var babelRc = JSON.parse(fs.readFileSync('.babelrc','utf8')); // eslint-disable-line
 
 export default {
-  entry: 'src/index.js',
+  entry: './amp-iframe-api-export.js',
   format: 'umd',
   sourceMap: true,
-  moduleName: packageDetils.name,
+  moduleName: 'amp-access-iframe-api',
   dest: 'dist/index.js',
   plugins: [
     babel({
-      babelrc: false,
-      presets: [
-        ['es2015', {loose: true, modules: false}],
-      ].concat(babelRc.presets.slice(1)),
-      plugins: babelRc.plugins,
-      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+      externalHelpers: true,
     }),
     minify({umd: 'dist/index.min.js'}),
   ],
