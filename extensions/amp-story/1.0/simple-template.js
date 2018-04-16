52576fb108abf486d37fb514a0858c37b805109e
Jon Newmuis <newmuis@users.noreply.github.com>
2018-04-16 17:35:30 -0400

    🐛 Use hasOwnProperty in simple-template checks instead of dot access (#14607)
    * Use hasOwnProperty in simple-template checks instead of dot access
    
    * Fix types
    
    * Use hasOwn fromo object.js instead of native hasOwnProperty.

--- extensions/amp-story/1.0/simple-template.js
+++ extensions/amp-story/1.0/simple-template.js
@@ -17,6 +17,7 @@ import {LocalizedStringId} from './localization'; // eslint-disable-line no-unus
 import {Services} from '../../../src/services';
 import {createElementWithAttributes} from '../../../src/dom';
 import {dev} from '../../../src/log';
+import {hasOwn} from '../../../src/utils/object';
 import {isArray, toWin} from '../../../src/types';
 
 
@@ -74,11 +75,12 @@ function renderMulti(doc, elementsDef) {
  * @return {!Element}
  */
 function renderSingle(doc, elementDef) {
-  const el = elementDef.attrs ?
-    createElementWithAttributes(doc, elementDef.tag, elementDef.attrs) :
+  const el = hasOwn(elementDef, 'attrs') ?
+    createElementWithAttributes(doc, elementDef.tag,
+        /** @type {!JsonObject} */ (elementDef.attrs)) :
     doc.createElement(elementDef.tag);
 
-  if (elementDef.localizedStringId) {
+  if (hasOwn(elementDef, 'localizedStringId')) {
     const win = toWin(doc.defaultView);
     Services.localizationServiceForOrNull(win).then(localizationService => {
       dev().assert(localizationService,
@@ -89,12 +91,13 @@ function renderSingle(doc, elementDef) {
     });
   }
 
-  if (elementDef.unlocalizedString) {
+  if (hasOwn(elementDef, 'unlocalizedString')) {
     el.textContent = elementDef.unlocalizedString;
   }
 
-  if (elementDef.children) {
-    el.appendChild(renderMulti(doc, elementDef.children));
+  if (hasOwn(elementDef, 'children')) {
+    el.appendChild(renderMulti(doc,
+        /** @type {!Array<!ElementDef>} */ (elementDef.children)));
   }
 
   return el;
