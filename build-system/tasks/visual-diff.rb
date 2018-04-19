8b71a1a0ae1e3c9c2fe9666de1a851331f8dada7
Raghu Simha <rsimha@ampproject.org>
2018-04-19 17:07:45 -0400

    🏗 🐛 Print messages to keep Travis alive while waiting for Percy status (#14748)

--- build-system/tasks/visual-diff.rb
+++ build-system/tasks/visual-diff.rb
@@ -42,8 +42,9 @@ CONFIGS = %w(canary prod)
 AMP_RUNTIME_FILE = 'dist/amp.js'
 AMP_3P_FRAME_FILE = 'dist.3p/current/integration.js'
 BUILD_STATUS_URL = 'https://amphtml-percy-status-checker.appspot.com/status'
-BUILD_PROCESSING_POLLING_INTERVAL_SECS = 5
-BUILD_PROCESSING_TIMEOUT_SECS = 60 * 10
+BUILD_PROCESSING_POLLING_INTERVAL_SECS = 5 # Poll every 5 seconds
+BUILD_PROCESSING_PROGRESS_POLLS = 12 # Print a message every minute
+BUILD_PROCESSING_TIMEOUT_SECS = 60 * 10 # Wait for up to 10 minutes
 PERCY_BUILD_URL = 'https://percy.io/ampproject/amphtml/builds'
 OUT = ENV['TRAVIS'] ? '/dev/null' : :out
 
@@ -150,6 +151,11 @@ def wait_for_build_completion(build_id)
       (status = get_build_status(build_id))['state'])
     sleep(BUILD_PROCESSING_POLLING_INTERVAL_SECS)
     tries += 1
+    if tries % BUILD_PROCESSING_PROGRESS_POLLS == 0
+      log('info',
+          'Still waiting for Percy build ' + cyan("#{build_id}") +
+          ' to be processed...')
+    end
     break if tries > (
         BUILD_PROCESSING_TIMEOUT_SECS / BUILD_PROCESSING_POLLING_INTERVAL_SECS)
   end
