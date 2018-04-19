6853188a56a62651215756c38d3ab3db17654899
Raghu Simha <rsimha@ampproject.org>
2018-04-19 12:04:20 -0400

    🏗 🐛 Automatically install / update custom lint rules in build-system/esli… (#14674)

--- build-system/exec.js
+++ build-system/exec.js
@@ -36,13 +36,16 @@ function spawnProcess(cmd, options) {
 }
 
 /**
- * Executes the provided command, returning the process object.
+ * Executes the provided command with the given options, returning the process
+ * object.
  *
  * @param {string} cmd Command line to execute.
+ * @param {<Object>} options
  * @return {<Object>} Process info.
  */
-exports.exec = function(cmd) {
-  return spawnProcess(cmd, {'stdio': 'inherit'});
+exports.exec = function(cmd, options) {
+  options = options || {'stdio': 'inherit'};
+  return spawnProcess(cmd, options);
 };
 
 /**
