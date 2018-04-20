11cfcb1e3da6774d56f2f57f3e8735a654484682
William Chou <willchou@google.com>
2018-04-20 22:57:02 +0800

    Refactor action whitelisting and apply to amp-story (#14628)
    * Move whitelist from standard-actions-impl to action-impl.
    
    * Move and rework unit test.
    
    * Disable actions in amp-story by setting whitelist to [].
    
    * Clean up error messaging.
    
    * Add comment.
    
    * Fix types.
    
    * Fix tests.
    
    * Save a couple bytes by making user errors more concise. Bump size caps as necessary.
    
    * Only query DOM once.
    
    * Avoid unhandled Promise rejection.

--- gulpfile.js
+++ gulpfile.js
@@ -822,7 +822,7 @@ function performBuild(watch) {
  */
 function checkBinarySize(compiled) {
   const file = compiled ? './dist/v0.js' : './dist/amp.js';
-  const size = compiled ? '76.85KB' : '336.10KB';
+  const size = compiled ? '76.95KB' : '336.66KB';
   const cmd = `npx bundlesize -f "${file}" -s "${size}"`;
   log(green('Running ') + cyan(cmd) + green('...\n'));
   const p = exec(cmd);
