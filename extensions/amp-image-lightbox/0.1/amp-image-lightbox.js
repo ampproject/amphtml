85efffd08d110ac6b20e24e63e9bb0fdd7d9a473
Cathy Zhu <cathyxz@gmail.com>
2018-04-19 21:06:46 -0700

    Update source width and height for amp-image-lightbox (#14719)
    * Update source width and height for amp-image-lightbox
    
    * Update amp-image-lightbox tests
    
    * Add cathyzhu to amp-image-lightbox owners

--- extensions/amp-image-lightbox/0.1/amp-image-lightbox.js
+++ extensions/amp-image-lightbox/0.1/amp-image-lightbox.js
@@ -229,6 +229,36 @@ export class ImageViewer {
   }
 
   /**
+   * @param {!Element} sourceElement
+   * @param {?Element} sourceImage
+   * @return {number}
+   * @private
+   */
+  getSourceWidth_(sourceElement, sourceImage) {
+    if (sourceElement.hasAttribute('width')) {
+      return parseInt(sourceElement.getAttribute('width'), 10);
+    } else {
+      return sourceImage ? sourceImage.naturalWidth
+        : sourceElement./*OK*/offsetWidth;
+    }
+  }
+
+  /**
+   * @param {!Element} sourceElement
+   * @param {?Element} sourceImage
+   * @return {number}
+   * @private
+   */
+  getSourceHeight_(sourceElement, sourceImage) {
+    if (sourceElement.hasAttribute('height')) {
+      return parseInt(sourceElement.getAttribute('height'), 10);
+    } else {
+      return sourceImage ? sourceImage.naturalHeight
+        : sourceElement./*OK*/offsetHeight;
+    }
+  }
+
+  /**
    * Initializes the image viewer to the target image element such as
    * "amp-img". The target image element may or may not yet have the img
    * element initialized.
@@ -236,8 +266,8 @@ export class ImageViewer {
    * @param {?Element} sourceImage
    */
   init(sourceElement, sourceImage) {
-    this.sourceWidth_ = sourceElement./*OK*/offsetWidth;
-    this.sourceHeight_ = sourceElement./*OK*/offsetHeight;
+    this.sourceWidth_ = this.getSourceWidth_(sourceElement, sourceImage);
+    this.sourceHeight_ = this.getSourceHeight_(sourceElement, sourceImage);
     this.srcset_ = srcsetFromElement(sourceElement);
 
     Object.keys(this.ariaAttributes_).forEach(key => {
