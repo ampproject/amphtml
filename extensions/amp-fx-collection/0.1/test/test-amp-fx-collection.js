fcda3fa336735a8e8a12f8951b6169b25b7138e7
Naina Raisinghani <nainar@google.com>
2018-04-24 13:44:45 -0700

    Initial test code

---
+++ extensions/amp-fx-collection/0.1/test/test-amp-fx-collection.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {AmpFxCollection} from '../amp-fx-collection';
+import {Services} from '../../../../src/services';
+import {isExperimentOn, toggleExperiment} from '../../../../src/experiments';
+
+describes.realWin('Creates the relevant fx presets correctly', {
+  amp: {
+    ampdoc: 'single',
+    extensions: ['amp-fx-collection'],
+  },
+}, env => {
+  let win;
+  let ampdoc;
+
+  beforeEach(() => {
+    win = env.win;
+    ampdoc = env.ampdoc;
+    toggleExperiment(win, 'amp-fx-fade-in', true);
+    toggleExperiment(win, 'amp-fx-fade-in-scroll', true);
+  });
+
+  afterEach(() => {
+    toggleExperiment(win, 'amp-fx-fade-in', false);
+    toggleExperiment(win, 'amp-fx-fade-in-scroll', false);
+  });
+
+  function createAmpFx(fxType, opt_attrs) {
+    const element = win.document.createElement('div');
+    element.setAttribute('amp-fx', fxType);
+    if (opt_attrs) {
+      for (const k in opt_attrs) {
+        element.setAttribute(k, opt_attrs[k]);
+      }
+    }
+
+    win.document.body.appendChild(element);
+
+    const ampFxCollection = new AmpFxCollection(ampdoc);
+    return ampFxCollection;
+  }
+
+  it('creates amp-fx=`parallax` correctly', () => {
+    const ampFx = createAmpFx('parallax', {
+      'data-parallax-factor': 1.2,
+    });
+    expect(ampFx).to.not.be.null;
+    expect(ampFx.getFxProvider_()).to.not.be.null;
+  });
+
+  it('creates amp-fx=`fade-in` correctly', () => {
+    expect(isExperimentOn(win, 'amp-fx-fade-in')).to.be.true;
+    const ampFx = createAmpFx('fade-in');
+    expect(ampFx).to.not.be.null;
+    expect(ampFx.getFxProvider_()).to.not.be.null;
+  });
+
+  it('creates amp-fx=`fade-in-scroll` correctly', () => {
+    expect(isExperimentOn(win, 'amp-fx-fade-in-scroll')).to.be.true;
+    const ampFx = createAmpFx('fade-in-scroll');
+    expect(ampFx).to.not.be.null;
+    expect(ampFx.getFxProvider_()).to.not.be.null;
+  });
+
+});
+
+
+
