2e8c82cd61f3074585c0ed02ae9f4aa77e7cf14e
John Pettitt <jpettitt@google.com>
2018-04-24 15:32:30 -0700

    Fix types

--- extensions/amp-geo/0.1/amp-geo.js
+++ extensions/amp-geo/0.1/amp-geo.js
@@ -87,7 +87,7 @@ export class AmpGeo extends AMP.BaseElement {
     this.country_ = 'unknown';
     /** @private {Array<string>} */
     this.matchedGroups_ = [];
-    /** @private {?Promise} */
+    /** @private {?function()} */
     this.res_ = null;
     /** @private {!Promise<!Object<string, (string|Array<string>)>>} */
     this.geoPromise_ = new Promise(resolve => {
