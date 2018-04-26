99ee6521e221928b5169c6337cadd75882401224
Gabriel Majoulet <gmajoulet@google.com>
2018-04-26 13:17:23 -0400

    Remove the hacky page.getImpl from the story tests. (#14879)

--- extensions/amp-story/1.0/test/test-amp-story.js
+++ extensions/amp-story/1.0/test/test-amp-story.js
@@ -15,7 +15,6 @@
  */
 import {Action} from '../amp-story-store-service';
 import {AmpStory} from '../amp-story';
-import {AmpStoryPage} from '../amp-story-page';
 import {EventType} from '../events';
 import {KeyCodes} from '../../../../src/utils/key-codes';
 import {MediaType} from '../media-pool';
@@ -51,7 +50,6 @@ describes.realWin('amp-story', {
     return Array(count).fill(undefined).map((unused, i) => {
       const page = win.document.createElement('amp-story-page');
       page.id = opt_ids && opt_ids[i] ? opt_ids[i] : `-page-${i}`;
-      page.getImpl = () => Promise.resolve(new AmpStoryPage(page));
       container.appendChild(page);
       return page;
     });
