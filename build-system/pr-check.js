12b6dab002c920c9d696fadf330551a95059d5da
Raghu Simha <rsimha@ampproject.org>
2018-04-26 15:11:12 -0400

    🐛 Temporarily disable Percy build verification (#14902)

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -593,20 +593,24 @@ function main() {
       command.cleanBuild();
       command.buildRuntime();
       command.runVisualDiffTests();
+    } else {
+      // Generates a blank Percy build to satisfy the required Github check.
+      command.runVisualDiffTests(/* opt_mode */ 'skip');
     }
     command.runPresubmitTests();
     if (buildTargets.has('INTEGRATION_TEST') ||
         buildTargets.has('RUNTIME')) {
       command.runIntegrationTests(/* compiled */ false);
     }
-    if (buildTargets.has('INTEGRATION_TEST') ||
-        buildTargets.has('RUNTIME') ||
-        buildTargets.has('VISUAL_DIFF')) {
-      command.verifyVisualDiffTests();
-    } else {
-      // Generates a blank Percy build to satisfy the required Github check.
-      command.runVisualDiffTests(/* opt_mode */ 'skip');
-    }
+    // TODO(rsimha, #14851): Failing due to long proessing times.
+    // if (buildTargets.has('INTEGRATION_TEST') ||
+    //     buildTargets.has('RUNTIME') ||
+    //     buildTargets.has('VISUAL_DIFF')) {
+    //   command.verifyVisualDiffTests();
+    // } else {
+    //   // Generates a blank Percy build to satisfy the required Github check.
+    //   command.runVisualDiffTests(/* opt_mode */ 'skip');
+    // }
     if (buildTargets.has('VALIDATOR_WEBUI')) {
       command.buildValidatorWebUI();
     }
