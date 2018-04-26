2170266b88db56d02ba2b5636c40caf6825eef33
Prateek Bhatnagar <prateek89born@gmail.com>
2018-04-26 13:43:18 -0700

    Subscriptions: amp-access iframe access api npm package (#14718)
    * WIP npm package
    
    * WIP es5 transpilation
    
    * WIP es5 transpilation
    
    * es5 transpile
    
    * fixing polyfills
    
    * fixing non export
    
    * adding build step
    
    * fixing packages
    
    * new rollup config
    
    * bug fix
    
    * no type check for rollup configs
    
    * presubmit fix

--- build-system/tasks/compile.js
+++ build-system/tasks/compile.js
@@ -198,6 +198,8 @@ function compile(entryModuleFilenames, outputDir,
       // Not sure what these files are, but they seem to duplicate code
       // one level below and confuse the compiler.
       '!node_modules/core-js/modules/library/**.js',
+      // Don't include rollup configs
+      '!**/rollup.config.js',
       // Don't include tests.
       '!**_test.js',
       '!**/test-*.js',
