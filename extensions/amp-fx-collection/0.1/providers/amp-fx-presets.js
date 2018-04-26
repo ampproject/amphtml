42bc3b44a17449527acfd1adbb5f8f64ac629516
Justin Ridgewell <justin@ridgewell.name>
2018-04-25 23:40:53 -0400

    ♻️Cleanup FxProvider's private fields (#14804)
    * Cleanup FxProvider's private fields
    
    - `ampdoc_` appears unused,
    - `initialize_` is called externally (meaning it's really public)
      - Additionally, it's only ever called after construction, so why isn't
      it just in the constructor?
    
    * Lint
    
    * Make adjustedViewportHeight public
    
    It's accessed from outside the class, meaning it's public.

--- extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
+++ extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
@@ -34,10 +34,10 @@ export const Presets = {
     },
     update(entry) {
       const fxElement = this;
-      dev().assert(fxElement.adjustedViewportHeight_);
+      dev().assert(fxElement.adjustedViewportHeight);
       const top = entry.positionRect ? entry.positionRect.top : null;
       // outside viewport
-      if (!top || top > fxElement.adjustedViewportHeight_) {
+      if (!top || top > fxElement.adjustedViewportHeight) {
         return;
       }
 
@@ -47,7 +47,7 @@ export const Presets = {
       const adjustedFactor = -(parseFloat(fxElement.getFactor()) - 1);
       // Offset is how much extra to move the element which is position within
       // viewport times adjusted factor.
-      const offset = (fxElement.adjustedViewportHeight_ - top) * adjustedFactor;
+      const offset = (fxElement.adjustedViewportHeight - top) * adjustedFactor;
       fxElement.setOffset(offset);
 
       if (fxElement.isMutateScheduled()) {
@@ -82,11 +82,11 @@ export const Presets = {
     },
     update(entry) {
       const fxElement = this;
-      dev().assert(fxElement.adjustedViewportHeight_);
+      dev().assert(fxElement.adjustedViewportHeight);
       const top = entry.positionRect ? entry.positionRect.top : null;
       // Outside viewport
       if (!top || top > (1 - fxElement.getMarginStart()) *
-        fxElement.adjustedViewportHeight_) {
+        fxElement.adjustedViewportHeight) {
         return;
       }
 
@@ -132,11 +132,11 @@ export const Presets = {
     },
     update(entry) {
       const fxElement = this;
-      dev().assert(fxElement.adjustedViewportHeight_);
+      dev().assert(fxElement.adjustedViewportHeight);
       const top = entry.positionRect ? entry.positionRect.top : null;
       // Outside viewport or margins
       if (!top || (top > (1 - fxElement.getMarginStart()) *
-        fxElement.adjustedViewportHeight_)) {
+        fxElement.adjustedViewportHeight)) {
         return;
       }
 
@@ -152,9 +152,9 @@ export const Presets = {
       const marginDelta = fxElement.getMarginEnd() - fxElement.getMarginStart();
       // Offset is how much extra to move the element which is position within
       // viewport times adjusted factor.
-      const offset = 1 * (fxElement.adjustedViewportHeight_ - top -
-        (fxElement.getMarginStart() * fxElement.adjustedViewportHeight_)) /
-        (marginDelta * fxElement.adjustedViewportHeight_);
+      const offset = 1 * (fxElement.adjustedViewportHeight - top -
+        (fxElement.getMarginStart() * fxElement.adjustedViewportHeight)) /
+        (marginDelta * fxElement.adjustedViewportHeight);
       fxElement.setOffset(offset);
 
       if (fxElement.isMutateScheduled()) {
