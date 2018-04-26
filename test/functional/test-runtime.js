401132e3bbc54a39ebbf0f9e54c4929af2d19cc4
Peter Josling <peterjosling@users.noreply.github.com>
2018-04-26 19:18:41 +0100

    🐛 Fix AmpShadowDoc#declareExtension_(string) behaviour when minified (#14896)
    Previously marked as private, but used from the exported function `declareExtension` outside of the class (in the same module). This happens to work fine for `AmpDocSingle`, but property renaming screws up `AmpDocShadow` and the method breaks.
    
    Discovered by running the `amp-next-page` example from minified AMP sources, which fails to install any extensions on the second page.

--- test/functional/test-runtime.js
+++ test/functional/test-runtime.js
@@ -535,7 +535,7 @@ describes.fakeWin('runtime', {
       const servicePromise = getServicePromise(win, 'amp-ext');
       const installStylesStub = sandbox.stub(styles, 'installStylesForDoc');
 
-      ampdoc.declareExtension_('amp-ext');
+      ampdoc.declareExtension('amp-ext');
       win.AMP.push({
         n: 'amp-ext',
         f: amp => {
@@ -573,7 +573,7 @@ describes.fakeWin('runtime', {
                 installStylesCallback = cb;
               });
 
-      ampdoc.declareExtension_('amp-ext');
+      ampdoc.declareExtension('amp-ext');
       win.AMP.push({
         n: 'amp-ext',
         f: amp => {
@@ -612,7 +612,7 @@ describes.fakeWin('runtime', {
     it('should register doc-service as ctor and install imm', function* () {
       class Service1 {}
       const ampdoc = new AmpDocSingle(win);
-      ampdoc.declareExtension_('amp-ext');
+      ampdoc.declareExtension('amp-ext');
       ampdocServiceMock.expects('getAmpDoc')
           .returns(ampdoc)
           .atLeast(1);
@@ -643,7 +643,7 @@ describes.fakeWin('runtime', {
         return {str: 'A'};
       }
       const ampdoc = new AmpDocSingle(win);
-      ampdoc.declareExtension_('amp-ext');
+      ampdoc.declareExtension('amp-ext');
       ampdocServiceMock.expects('getAmpDoc')
           .returns(ampdoc)
           .atLeast(1);
