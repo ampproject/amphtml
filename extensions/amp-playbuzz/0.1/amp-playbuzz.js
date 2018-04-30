095759ce11039b37b3bd0a2a93e063c0832ac19b
Cathy Zhu <cathyxz@gmail.com>
2018-04-30 15:09:39 -0700

    Fix unused variable lint issue for ui extensions (#14965)
    * Remove unused variable from amp-riddle-quiz
    
    * Remove unused variable from amp-lightbox-gallery
    
    * Remove unused iframePromise_ variable from amp-instagram
    
    * Fix unlistenMessage for amp-facebook-* components
    
    * Remove unused iframePromise_ variable for amp-playbuzz
    
    * Remove unused this.iframe_ variable from amp-google-vrview-image
    
    * Revert "Fix unlistenMessage for amp-facebook-* components"
    
    This reverts commit 11b06071f24621df6e5bf0bc556bd337a38487e4.
    
    * Revert "Remove unused iframePromise_ variable from amp-instagram"
    
    This reverts commit ed78e1534720b310cc4ec8dc6c44cae6019b2aba.
    
    * Revert "Remove unused iframePromise_ variable for amp-playbuzz"
    
    This reverts commit cd67be2a6168a79d54a48275e14e758f2ef255ff.
    
    * Make iframePromise_ visbleForTesting

--- extensions/amp-playbuzz/0.1/amp-playbuzz.js
+++ extensions/amp-playbuzz/0.1/amp-playbuzz.js
@@ -65,7 +65,7 @@ class AmpPlaybuzz extends AMP.BaseElement {
     /** @private {?Element} */
     this.iframe_ = null;
 
-    /** @private {?Promise} */
+    /** @visibleForTesting {?Promise} */
     this.iframePromise_ = null;
 
     /** @private {?number} */
