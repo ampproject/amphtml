3c2645eddc74c795db6099c7021d85eaf2bcedb4
Jon Newmuis <newmuis@users.noreply.github.com>
2018-05-01 17:48:05 -0400

    ✨ Adds an embed mode that removes sharing (#14990)
    * Add embed modes and states.
    
    * Apply CSS class when sharing is allowed; remove otherwise
    
    * Add CSS and negate CSS class name
    
    * Wrap classlist toggle in vsync mutate
    
    * Fix variable name in JsDoc

--- extensions/amp-story/1.0/bookend/amp-story-bookend.js
+++ extensions/amp-story/1.0/bookend/amp-story-bookend.js
@@ -316,6 +316,10 @@ export class Bookend {
       this.onBookendStateUpdate_(isActive);
     });
 
+    this.storeService_.subscribe(StateProperty.CAN_SHOW_SHARING_UIS, show => {
+      this.onCanShowSharingUisUpdate_(show);
+    }, true /** callToInitialize */);
+
     this.storeService_.subscribe(StateProperty.DESKTOP_STATE, isDesktop => {
       this.onDesktopStateUpdate_(isDesktop);
     }, true /** callToInitialize */);
@@ -346,6 +350,19 @@ export class Bookend {
    */
   onBookendStateUpdate_(isActive) {
     this.toggle_(isActive);
+  }
+
+  /**
+   * Reacts to updates to whether sharing UIs may be shown, and updates the UI
+   * accordingly.
+   * @param {boolean} canShowSharingUis
+   * @private
+   */
+  onCanShowSharingUisUpdate_(canShowSharingUis) {
+    this.vsync_.mutate(() => {
+      this.getShadowRoot()
+          .classList.toggle('i-amphtml-story-no-sharing', !canShowSharingUis);
+    });
   }
 
   /**
