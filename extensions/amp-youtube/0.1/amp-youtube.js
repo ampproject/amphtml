df9324994370f91e0e973cb455d3b82168056dab
Cathy Zhu <cathyzhu@google.com>
2018-05-02 14:15:25 -0700

    Remove no longer applicable comment

--- extensions/amp-youtube/0.1/amp-youtube.js
+++ extensions/amp-youtube/0.1/amp-youtube.js
@@ -264,9 +264,6 @@ class AmpYoutube extends AMP.BaseElement {
 
   /** @override */
   pauseCallback() {
-    // Only send pauseVideo command if the player is playing. Otherwise
-    // The player breaks if the user haven't played the video yet specially
-    // on mobile.
     if (this.iframe_ && this.iframe_.contentWindow) {
       this.pause();
     }
