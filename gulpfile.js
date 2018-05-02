34d472043ccbb342f09ecad2e3a24a5cf7882259
John Pettitt <jpettitt@google.com>
2018-05-01 17:51:23 -0700

    bump bundle size

--- gulpfile.js
+++ gulpfile.js
@@ -823,7 +823,7 @@ function performBuild(watch) {
  */
 function checkBinarySize(compiled) {
   const file = compiled ? './dist/v0.js' : './dist/amp.js';
-  const size = compiled ? '77.05KB' : '336.72KB';
+  const size = compiled ? '77.05KB' : '336.76KB';
   const cmd = `npx bundlesize -f "${file}" -s "${size}"`;
   log(green('Running ') + cyan(cmd) + green('...\n'));
   const p = exec(cmd);
