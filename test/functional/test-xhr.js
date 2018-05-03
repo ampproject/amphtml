a776d4efdeefdee5f853bb89c8d371c5d0d6fe89
Aaron Labiaga <alabiaga@google.com>
2018-05-03 14:05:01 -0400

    XHR fetch when view first becomes visible (#14837)
    * fetch when viewer is visible
    
    * fetch by default when viewer first becomes visible
    
    * fix typo
    
    * fixes from kris' comments
    
    * more fixes from kris' comments
    
    * add test
    
    * address jridgewell comments
    
    * fix usage of spy
    
    * fix xhr, to resolve earlier if not a single doc
    
    * fix per justin comment.
    
    * minor refactor
    
    * get ampdoc fetch with isSingleDoc check to fix dev error thrown when fetching the amp doc. for test test-bind-impl
    
    * address jridgewell comments
    
    * address jridgewell comments
    
    * address comments
    
    * return promise fix

--- test/functional/test-xhr.js
+++ test/functional/test-xhr.js
@@ -30,7 +30,9 @@ import {utf8FromArrayBuffer} from '../../extensions/amp-a4a/0.1/amp-a4a';
 describe.configure().skipSafari().run('XHR', function() {
   let sandbox;
   let ampdocServiceForStub;
+  let ampdocViewerStub;
   let xhrCreated;
+  let viewer;
 
   const location = {href: 'https://acme.com/path'};
   const nativeWin = {
@@ -73,7 +75,15 @@ describe.configure().skipSafari().run('XHR', function() {
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
     ampdocServiceForStub = sandbox.stub(Services, 'ampdocServiceFor');
-    ampdocServiceForStub.returns({isSingleDoc: () => false});
+    ampdocViewerStub = sandbox.stub(Services, 'viewerForDoc');
+    ampdocViewerStub.returns({
+      whenFirstVisible: () => Promise.resolve(),
+    });
+    ampdocServiceForStub.returns({
+      isSingleDoc: () => false,
+      getAmpDoc: () => ampdocViewerStub,
+    });
+
     location.href = 'https://acme.com/path';
   });
 
@@ -83,14 +93,15 @@ describe.configure().skipSafari().run('XHR', function() {
 
   scenarios.forEach(test => {
     let xhr;
-
-    // Since if it's the Native fetch, it won't use the XHR object so
+    beforeEach(() => {
+      xhr = xhrServiceForTesting(test.win);
+    });
+    // Since it's the Native fetch, it won't use the XHR object so
     // mocking and testing the request becomes not doable.
     if (test.desc != 'Native') {
 
       describe('#XHR', () => {
         beforeEach(() => {
-          xhr = xhrServiceForTesting(test.win);
           setupMockXhr();
         });
 
@@ -249,11 +260,37 @@ describe.configure().skipSafari().run('XHR', function() {
             expect(error.message).to.contain('Response must contain');
           });
         });
+
+        describe('viewer visibility', () => {
+          afterEach(() => {
+            test.win.fetch.restore();
+          });
+          it('should not call fetch if view is not visible ', () => {
+            const fetchCall = sandbox.spy(test.win, 'fetch');
+            ampdocViewerStub.returns({
+              whenFirstVisible: () => Promise.reject(),
+            });
+            xhr.fetchJson('/get', {ampCors: false});
+            expect(fetchCall.notCalled).to.be.true;
+          });
+          it('should call fetch if view is visible ', () => {
+            const fetchCall = sandbox.spy(test.win, 'fetch');
+            ampdocViewerStub.returns({
+              whenFirstVisible: () => Promise.resolve(),
+            });
+            const fetch = xhr.fetchJson('/get', {ampCors: false});
+            fetch.then(() => {
+              expect(fetchCall.calledOnce).to.be.true;
+            });
+          });
+        });
       });
     }
 
     describe('AMP-Same-Origin', () => {
-      beforeEach(() => xhr = xhrServiceForTesting(test.win));
+      beforeEach(() => {
+        xhr = xhrServiceForTesting(test.win);
+      });
 
       it('should not be set for cross origin requests', () => {
         const init = {};
@@ -289,7 +326,9 @@ describe.configure().skipSafari().run('XHR', function() {
     });
 
     describe(test.desc, () => {
-      beforeEach(() => xhr = xhrServiceForTesting(test.win));
+      beforeEach(() => {
+        xhr = xhrServiceForTesting(test.win);
+      });
 
       describe('assertSuccess', () => {
         function createResponseInstance(body, init) {
@@ -765,9 +804,8 @@ describe.configure().skipSafari().run('XHR', function() {
     const origin = 'https://acme.com';
 
     let interceptionEnabledWin;
-    let viewer;
+    let optedInDoc;
     let sendMessageStub;
-
     function getDefaultResponseOptions() {
       return {
         headers: [
@@ -781,23 +819,22 @@ describe.configure().skipSafari().run('XHR', function() {
     }
 
     beforeEach(() => {
-      const optedInDoc = window.document.implementation.createHTMLDocument('');
+      optedInDoc = window.document.implementation.createHTMLDocument('');
       optedInDoc.documentElement.setAttribute('allow-xhr-interception', '');
 
       ampdocServiceForStub.returns({
         isSingleDoc: () => true,
         getAmpDoc: () => ({getRootNode: () => optedInDoc}),
       });
-
       viewer = {
         hasCapability: () => true,
         isTrustedViewer: () => Promise.resolve(true),
         sendMessageAwaitResponse: getDefaultResponsePromise,
+        whenFirstVisible: () => Promise.resolve(),
       };
       sendMessageStub = sandbox.stub(viewer, 'sendMessageAwaitResponse');
       sendMessageStub.returns(getDefaultResponsePromise());
-      sandbox.stub(Services, 'viewerForDoc').returns(viewer);
-
+      ampdocViewerStub.returns(viewer);
       interceptionEnabledWin = {
         location: {
           href: `${origin}/path`,
@@ -808,7 +845,10 @@ describe.configure().skipSafari().run('XHR', function() {
     });
 
     it('should not intercept if AMP doc is not single', () => {
-      ampdocServiceForStub.returns({isSingleDoc: () => false});
+      ampdocServiceForStub.returns({
+        isSingleDoc: () => false,
+        getAmpDoc: () => ({getRootNode: () => optedInDoc}),
+      });
       const xhr = xhrServiceForTesting(interceptionEnabledWin);
 
       return xhr.fetch('https://cdn.ampproject.org')
