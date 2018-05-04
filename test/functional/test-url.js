c6e8b4c33b7c3b304ae9aceb56dc3f773f87edbd
Aaron Labiaga <alabiaga@google.com>
2018-05-04 12:05:27 -0400

    fix test-url

--- test/functional/test-url.js
+++ test/functional/test-url.js
@@ -818,69 +818,70 @@ describe('rewriteAttributeValue', () => {
 
 
 describe('resolveUrlAttr', () => {
-
+  const TAG = 'url-test';
+  const resolveUrlAttr_ = resolveUrlAttr.bind(null, TAG);
   it('should throw if __amp_source_origin is set', () => {
-    allowConsoleError(() => { expect(() => resolveUrlAttr('a', 'href',
+    allowConsoleError(() => { expect(() => resolveUrlAttr_('a', 'href',
         '/doc2?__amp_source_origin=https://google.com',
         'http://acme.org/doc1')).to.throw(/Source origin is not allowed in/); });
   });
 
 
   it('should resolve non-hash href', () => {
-    expect(resolveUrlAttr('a', 'href',
+    expect(resolveUrlAttr_('a', 'href',
         '/doc2',
         'http://acme.org/doc1'))
         .to.equal('http://acme.org/doc2');
-    expect(resolveUrlAttr('a', 'href',
+    expect(resolveUrlAttr_('a', 'href',
         '/doc2',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('http://acme.org/doc2');
-    expect(resolveUrlAttr('a', 'href',
+    expect(resolveUrlAttr_('a', 'href',
         'http://non-acme.org/doc2',
         'http://acme.org/doc1'))
         .to.equal('http://non-acme.org/doc2');
   });
 
   it('should ignore hash URLs', () => {
-    expect(resolveUrlAttr('a', 'href',
+    expect(resolveUrlAttr_('a', 'href',
         '#hash1',
         'http://acme.org/doc1'))
         .to.equal('#hash1');
   });
 
   it('should resolve src', () => {
-    expect(resolveUrlAttr('amp-video', 'src',
+    expect(resolveUrlAttr_('amp-video', 'src',
         '/video1',
         'http://acme.org/doc1'))
         .to.equal('http://acme.org/video1');
-    expect(resolveUrlAttr('amp-video', 'src',
+    expect(resolveUrlAttr_('amp-video', 'src',
         '/video1',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('http://acme.org/video1');
-    expect(resolveUrlAttr('amp-video', 'src',
+    expect(resolveUrlAttr_('amp-video', 'src',
         'http://non-acme.org/video1',
         'http://acme.org/doc1'))
         .to.equal('http://non-acme.org/video1');
   });
 
   it('should rewrite image http(s) src', () => {
-    expect(resolveUrlAttr('amp-img', 'src',
+    expect(resolveUrlAttr_('amp-img', 'src',
         '/image1?a=b#h1',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('https://cdn.ampproject.org/i/acme.org/image1?a=b#h1');
-    expect(resolveUrlAttr('amp-img', 'src',
+    expect(resolveUrlAttr_('amp-img', 'src',
         'https://acme.org/image1?a=b#h1',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('https://cdn.ampproject.org/i/s/acme.org/image1?a=b#h1');
   });
 
   it('should rewrite image http(s) srcset', () => {
-    expect(resolveUrlAttr('amp-img', 'srcset',
+    expect(resolveUrlAttr_('amp-img', 'srcset',
         '/image2?a=b#h1 2x, /image1?a=b#h1 1x',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('https://cdn.ampproject.org/i/acme.org/image1?a=b#h1 1x, ' +
             'https://cdn.ampproject.org/i/acme.org/image2?a=b#h1 2x');
-    expect(resolveUrlAttr('amp-img', 'srcset',
+    expect(resolveUrlAttr_('amp-img', 'srcset',
         'https://acme.org/image2?a=b#h1 2x, /image1?a=b#h1 1x',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('https://cdn.ampproject.org/i/acme.org/image1?a=b#h1 1x, ' +
@@ -888,14 +889,14 @@ describe('resolveUrlAttr', () => {
   });
 
   it('should NOT rewrite image http(s) src when not on proxy', () => {
-    expect(resolveUrlAttr('amp-img', 'src',
+    expect(resolveUrlAttr_('amp-img', 'src',
         '/image1',
         'http://acme.org/doc1'))
         .to.equal('http://acme.org/image1');
   });
 
   it('should NOT rewrite image data src', () => {
-    expect(resolveUrlAttr('amp-img', 'src',
+    expect(resolveUrlAttr_('amp-img', 'src',
         'data:12345',
         'https://cdn.ampproject.org/c/acme.org/doc1'))
         .to.equal('data:12345');
