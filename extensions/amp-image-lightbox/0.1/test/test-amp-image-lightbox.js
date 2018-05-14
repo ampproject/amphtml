6f54b3935cd781a5e90751292ef387b347c7eae4
Cathy Zhu <cathyxz@gmail.com>
2018-05-14 14:05:11 -0700

    Fix width auto bug in amp-image-lightbox (#15206)
    * Fix width auto bug in amp-image-lightbox
    
    * Add manual test verifying fix
    
    * Add stub and refactor tests
    
    * Refactor to double quotes
    
    * Rename e to elem for readability

--- extensions/amp-image-lightbox/0.1/test/test-amp-image-lightbox.js
+++ extensions/amp-image-lightbox/0.1/test/test-amp-image-lightbox.js
@@ -238,6 +238,19 @@ describes.realWin('amp-image-lightbox image viewer', {
   let imageViewer;
   let loadPromiseStub;
 
+  const sourceElement = {
+    offsetWidth: 101,
+    offsetHeight: 201,
+    getAttribute: name => {
+      if (name == 'src') {
+        return 'image1';
+      }
+      return undefined;
+    },
+    hasAttribute: () => undefined,
+    getImpl: () => Promise.resolve(sourceElement.implementation_),
+  };
+
   beforeEach(() => {
     win = env.win;
     doc = win.document;
@@ -275,64 +288,27 @@ describes.realWin('amp-image-lightbox image viewer', {
 
 
   it('should init to the source element without image', () => {
-    const sourceElement = {
-      offsetWidth: 101,
-      offsetHeight: 201,
-      getAttribute: name => {
-        if (name == 'src') {
-          return 'image1';
-        }
-        return undefined;
-      },
-      hasAttribute: () => undefined,
-    };
-
     imageViewer.init(sourceElement, null);
-
     expect(imageViewer.sourceWidth_).to.equal(101);
     expect(imageViewer.sourceHeight_).to.equal(201);
     expect(imageViewer.getImage().src).to.equal('');
   });
 
   it('should init to the source element with unloaded image', () => {
-    const sourceElement = {
-      offsetWidth: 101,
-      offsetHeight: 201,
-      getAttribute: name => {
-        if (name == 'src') {
-          return 'image1';
-        }
-        return undefined;
-      },
-      hasAttribute: () => undefined,
-    };
     const sourceImage = {
       complete: false,
       src: 'image1-smaller',
     };
-
     imageViewer.init(sourceElement, sourceImage);
 
     expect(imageViewer.getImage().src).to.equal('');
   });
 
   it('should init to the source element with loaded image', () => {
-    const sourceElement = {
-      offsetWidth: 101,
-      offsetHeight: 201,
-      getAttribute: name => {
-        if (name == 'src') {
-          return 'image1';
-        }
-        return undefined;
-      },
-      hasAttribute: () => undefined,
-    };
     const sourceImage = {
       complete: true,
       src: 'image1-smaller',
     };
-
     imageViewer.init(sourceElement, sourceImage);
 
     expect(imageViewer.getImage().getAttribute('src')).to
