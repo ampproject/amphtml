8a2dbede01e1a18f6b5cbc9353759ed2345b3b41
arturro <artchru@gmail.com>
2018-05-14 19:43:17 +0200

    remove not used

--- ads/noddus.js
+++ ads/noddus.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {loadScript, writeScript, validateData} from '../3p/3p';
+import {writeScript, validateData} from '../3p/3p';
 
 /**
  * @param {!Window} global
