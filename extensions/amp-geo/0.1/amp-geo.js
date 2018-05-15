8c2da58c34ac7b27e609a1fabbc7256f01e6cb78
John Pettitt <jpettitt@google.com>
2018-05-15 11:18:55 -0700

    add mutateElement to amp-geo for layers support. (#15226)
    * add mutateElement for layers support.
    
    * Move mutate to buildCallback, fix unit tests accodingly.
    
    * missed cleanup
    
    * Move amp-state into mutateElement callback
    
    * map() instead of for()
    
    * nits
    
    * Oh IE how much do I hate you, let me count the ways.
    
    * destructure classList

--- extensions/amp-geo/0.1/amp-geo.js
+++ extensions/amp-geo/0.1/amp-geo.js
@@ -110,6 +110,7 @@ export class AmpGeo extends AMP.BaseElement {
     geoDeferred.resolve(geo);
   }
 
+
   /**
    * findCountry_, sets this.country_ and this.mode_
    * @param {Document} doc
@@ -174,22 +175,21 @@ export class AmpGeo extends AMP.BaseElement {
 
   /**
    * clearPreRender_()
-   * Removes classes and amp-state block addred by server side
-   * pre-render when debug override is in effect
+   * Returns a list of classes to remove if pre-render has
+   * been invalidated by way of being on an amp cache
    * @param {Document} doc
+   * @returns {Array<string>}
    */
   clearPreRender_(doc) {
-    const klasses = doc.body.classList;
+    const {classList} = doc.body;
+    const classesToRemove = [];
     const stripRe = new RegExp('^' + COUNTRY_PREFIX + '|^' + GROUP_PREFIX ,'i');
-    for (let i = klasses.length - 1; i > 0; i--) {
-      if (stripRe.test(klasses[i])) {
-        doc.body.classList.remove(klasses[i]);
+    for (let i = classList.length - 1; i > 0; i--) {
+      if (stripRe.test(classList[i])) {
+        classesToRemove.push(classList[i]);
       }
     }
-    const geoState = doc.getElementById(GEO_ID);
-    if (geoState) {
-      geoState.parentNode.removeChild(geoState);
-    }
+    return classesToRemove;
   }
 
   /**
@@ -210,34 +210,54 @@ export class AmpGeo extends AMP.BaseElement {
       self.findCountry_(doc);
       self.matchCountryGroups_(config);
 
+      let classesToRemove = [];
+
       switch (self.mode_) {
         case mode.GEO_OVERRIDE:
-          self.clearPreRender_(doc);
+          classesToRemove = self.clearPreRender_(doc);
           // Intentionally fall through.
         case mode.GEO_HOT_PATCH:
           // Build the AMP State, add classes
           states.ISOCountry = self.country_;
 
-          for (let group = 0; group < self.matchedGroups_.length; group++) {
-            doc.body.classList.add(GROUP_PREFIX + self.matchedGroups_[group]);
-            states[self.matchedGroups_[group]] = true;
-          }
-          doc.body.classList.add(COUNTRY_PREFIX + this.country_);
-          doc.body.classList.remove('amp-geo-pending');
-          states.ISOCountryGroups = self.matchedGroups_;
+          const classesToAdd = self.matchedGroups_.map(group => {
+            states[group] = true;
+            return GROUP_PREFIX + group;
+          });
 
-          // Only include amp state if user requests it to avoid validator issue
-          // with missing amp-bind js
-          if (config.AmpBind) {
-            const state = doc.createElement('amp-state');
-            const confScript = doc.createElement('script');
-            confScript.setAttribute('type', 'application/json');
-            confScript.textContent =
-                JSON.stringify(/** @type {!JsonObject} */(states)) ;
-            state.appendChild(confScript);
-            state.id = GEO_ID;
-            doc.body.appendChild(state);
-          }
+          states.ISOCountryGroups = self.matchedGroups_;
+          classesToAdd.push(COUNTRY_PREFIX + this.country_);
+
+          // Let the runtime know we're mutating the doc.body
+          // Actual change happens in callback to runtime can
+          // optimize dom mutations.
+          self.mutateElement(() => {
+            const {classList} = doc.body;
+            // Always remove the pending class
+            classesToRemove.push('amp-geo-pending');
+            classesToRemove.forEach(toRemove => classList.remove(toRemove));
+
+            // add the new classes to <body>
+            classesToAdd.forEach(toAdd => classList.add(toAdd));
+
+            // Only include amp state if user requests it to
+            // avoid validator issue with missing amp-bind js
+            if (config.AmpBind) {
+              const geoState = doc.getElementById(GEO_ID);
+              if (geoState) {
+                geoState.parentNode.removeChild(geoState);
+              }
+              const state = doc.createElement('amp-state');
+              const confScript = doc.createElement('script');
+              confScript.setAttribute('type', 'application/json');
+              confScript.textContent =
+                  JSON.stringify(/** @type {!JsonObject} */(states)) ;
+              state.appendChild(confScript);
+              state.id = GEO_ID;
+              doc.body.appendChild(state);
+            }
+          }, doc.body);
+
           break;
         case mode.GEO_PRERENDER:
           break;
