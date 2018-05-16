b3feb6f22d8cbc57c3225f0e73853419d1af35d0
William Chou <willchou@google.com>
2018-05-16 14:42:05 -0400

    Revert "🐛 Add last check time stamp to the polling url to bypass caching (#14662)" (#15358)
    This reverts commit 39c5af8662856b887b9a4d10d8beec8b335bd5b2.

--- extensions/amp-live-list/0.1/live-list-manager.js
+++ extensions/amp-live-list/0.1/live-list-manager.js
@@ -67,9 +67,6 @@ export class LiveListManager {
     /** @private {time} */
     this.latestUpdateTime_ = 0;
 
-    /** @private {time} */
-    this.lastCheckTime_ = 0;
-
     /** @private @const {function(): Promise} */
     this.work_ = this.fetchDocument_.bind(this);
 
@@ -83,7 +80,6 @@ export class LiveListManager {
           .map(key => this.liveLists_[key].getUpdateTime());
       this.latestUpdateTime_ = Math.max.apply(Math, initialUpdateTimes);
 
-      this.lastCheckTime_ = Number(new Date());
       // For testing purposes only, we speed up the interval of the update.
       // This should NEVER be allowed in production.
       if (getMode().localDev) {
@@ -134,12 +130,6 @@ export class LiveListManager {
       url = addParamToUrl(url, 'amp_latest_update_time',
           String(this.latestUpdateTime_));
     }
-    // This is important for cache busting as some environments force a
-    // cache-control: max-age header.
-    if (this.lastCheckTime_ > 0) {
-      url = addParamToUrl(url, 'amp_last_check_time',
-          String(this.lastCheckTime_));
-    }
     return Services.xhrFor(this.ampdoc.win)
         // TODO(erwinm): add update time here when possible.
         .fetchDocument(url, {
@@ -161,7 +151,6 @@ export class LiveListManager {
     if (latestUpdateTime > 0) {
       this.latestUpdateTime_ = latestUpdateTime;
     }
-    this.lastCheckTime_ = Number(new Date());
     // We need to do this after calling `updateLiveList` since that
     // would apply the disabled attribute if any exist from the server.
     if (!this.hasActiveLiveLists_()) {
