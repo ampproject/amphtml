74ae582e9bd7a9d3d333d94e9b19e8c0293e2974
Enrique Marroquin <artezan@google.com>
2018-05-16 16:32:10 -0400

    adds landscape card to bookend components.

--- extensions/amp-story/1.0/bookend/components/portrait.js
+++ extensions/amp-story/1.0/bookend/components/portrait.js
@@ -88,11 +88,11 @@ export class PortraitComponent {
         html`
         <a class="i-amphtml-story-bookend-portrait"
           target="_top">
-          <h2 class="i-amphtml-story-bookend-portrait-category"
+          <h2 class="i-amphtml-story-bookend-component-category"
             ref="category"></h2>
           <amp-img class="i-amphtml-story-bookend-portrait-image"
             layout="fixed" width="0" height="0" ref="image"></amp-img>
-          <div class="i-amphtml-story-bookend-portrait-meta"
+          <div class="i-amphtml-story-bookend-component-meta"
             ref="meta"></div>
         </a>`;
     addAttributesToElement(template, dict({'href': portraitData.url}));
