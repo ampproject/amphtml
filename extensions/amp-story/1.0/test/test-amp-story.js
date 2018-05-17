d4188e94668b70c02b5fba647b64ebb065ce73e4
calebcordry <ccordry@google.com>
2018-05-17 14:48:54 -0700

    add test

--- extensions/amp-story/1.0/test/test-amp-story.js
+++ extensions/amp-story/1.0/test/test-amp-story.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as utils from '../utils';
 import {Action} from '../amp-story-store-service';
 import {AmpStory} from '../amp-story';
 import {EventType} from '../events';
@@ -358,6 +359,69 @@ describes.realWin('amp-story', {
         .then(() => {
           return expect(replaceStub).to.not.have.been.called;
         });
+  });
+
+
+  describe('desktop attributes', () => {
+    it('should ad next page attribute', function() {
+      sandbox.stub(win.history, 'replaceState');
+      sandbox.stub(utils, 'setAtributeInMutate').callsFake(
+          (el, attr) => el.element.setAttribute(attr, ''));
+
+      const pages = createPages(story.element, 3, ['page-1', 'page-2']);
+      const page2 = pages[1];
+      story.buildCallback();
+      return story.layoutCallback()
+          .then(() => {
+            expect(page2.hasAttribute('i-amphtml-next-page')).to.be.true;
+          });
+    });
+
+    it('should add next page attribute', () => {
+      sandbox.stub(win.history, 'replaceState');
+      sandbox.stub(utils, 'setAtributeInMutate').callsFake(
+          (el, attr) => el.element.setAttribute(attr, ''));
+
+      const pages = createPages(story.element, 2, ['page-0', 'page-1']);
+      const page1 = pages[1];
+      story.buildCallback();
+      return story.layoutCallback()
+          .then(() => {
+            expect(page1.hasAttribute('i-amphtml-next-page')).to.be.true;
+          });
+    });
+
+    it('should add previous page attribute', () => {
+      sandbox.stub(win.history, 'replaceState');
+      sandbox.stub(story, 'maybePreloadBookend_').returns();
+      sandbox.stub(utils, 'setAtributeInMutate').callsFake(
+          (el, attr) => el.element.setAttribute(attr, ''));
+
+      const pages = createPages(story.element, 2, ['page-0', 'page-1']);
+      const page0 = pages[0];
+      story.buildCallback();
+      return story.layoutCallback()
+          .then(() => story.switchTo_('page-1'))
+          .then(() => {
+            expect(page0.hasAttribute('i-amphtml-previous-page')).to.be.true;
+          });
+    });
+
+    it('should add previous visited attribute', () => {
+      sandbox.stub(win.history, 'replaceState');
+      sandbox.stub(story, 'maybePreloadBookend_').returns();
+      sandbox.stub(utils, 'setAtributeInMutate').callsFake(
+          (el, attr) => el.element.setAttribute(attr, ''));
+
+      const pages = createPages(story.element, 2, ['page-0', 'page-1']);
+      const page0 = pages[0];
+      story.buildCallback();
+      return story.layoutCallback()
+          .then(() => story.switchTo_('page-1'))
+          .then(() => {
+            expect(page0.hasAttribute('i-amphtml-visited')).to.be.true;
+          });
+    });
   });
 
   describe('amp-story audio', () => {
