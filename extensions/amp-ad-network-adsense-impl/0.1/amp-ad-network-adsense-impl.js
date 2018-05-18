9bec9b8b1fc21f21b0f8e18b05b8ccadd15bb023
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2018-05-18 13:58:31 -0700

    fix1

--- extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
+++ extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
@@ -293,6 +293,7 @@ export class AmpAdNetworkAdsenseImpl extends AmpA4A {
     let consent = null;
     switch (consentState) {
       case undefined:
+      case null:
       case CONSENT_POLICY_STATE.UNKNOWN_NOT_REQUIRED:
         break;
       case CONSENT_POLICY_STATE.INSUFFICIENT:
