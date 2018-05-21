501940b1b58f317ff206e9c986a2e6b805685f7c
William Chou <willchou@google.com>
2018-05-21 01:46:03 -0400

    Constrain navigation triggered by <amp-iframe> (#15162)
    * Prevent privilege escalation for top navigation from iframe messaging.
    
    * Clean up tests.
    
    * Fix type check.
    
    * Move element-specific logic to amp-iframe.
    
    * Fix test-standard-actions.
    
    * Add amp-iframe tests.
    
    * Use generator function for tests.
    
    * Rename navigationError to throwIfCannotNavigate.
    
    * Add sandboxContainsToken() helper, fix lint errors.
    
    * Fix types.
    
    * Fix tests.
    
    * Justin's comment.
    
    * Fix regex.
    
    * Bump bundle-size.

--- src/service/standard-actions-impl.js
+++ src/service/standard-actions-impl.js
@@ -20,6 +20,7 @@ import {Services} from '../services';
 import {computedStyle, getStyle, toggle} from '../style';
 import {dev, user} from '../log';
 import {registerServiceBuilderForDoc} from '../service';
+import {startsWith} from '../string';
 import {toWin} from '../types';
 import {tryFocus} from '../dom';
 
@@ -110,9 +111,21 @@ export class StandardActions {
         });
 
       case 'navigateTo':
-        Services.navigationForDoc(this.ampdoc).navigateTo(
-            win, args['url'], `AMP.${method}`);
-        return null;
+        // Some components have additional constraints on allowing navigation.
+        let permission = Promise.resolve();
+        if (startsWith(node.tagName, 'AMP-')) {
+          permission = node.getImpl().then(impl => {
+            if (typeof impl.throwIfCannotNavigate == 'function') {
+              impl.throwIfCannotNavigate();
+            }
+          });
+        }
+        return permission.then(() => {
+          Services.navigationForDoc(this.ampdoc).navigateTo(
+              win, args['url'], `AMP.${method}`);
+        }, /* onrejected */ e => {
+          user().error(TAG, e.message);
+        });
 
       case 'goBack':
         Services.historyForDoc(this.ampdoc).goBack();
