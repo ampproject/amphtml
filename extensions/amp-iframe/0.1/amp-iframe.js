884359122ea39138f98ec9907c31e672bd39f24f
William Chou <willchou@google.com>
2018-05-20 21:40:00 -0400

    Fix regex.

--- extensions/amp-iframe/0.1/amp-iframe.js
+++ extensions/amp-iframe/0.1/amp-iframe.js
@@ -181,7 +181,7 @@ export class AmpIframe extends AMP.BaseElement {
    * @private
    */
   sandboxContainsToken_(sandbox, token) {
-    const re = new RegExp(`/\s${token}\s/`, 'i');
+    const re = new RegExp(`\\s${token}\\s`, 'i');
     return re.test(' ' + sandbox + ' ');
   }
 
