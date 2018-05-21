0feeedab80d0396f20cd8d75901adbda8af1b9c5
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2018-05-21 13:21:59 -0400

    ♻️ cleanups to amp-story bookend components (#15431)
    * cleanups
    
    * remove 'container' in 'bookend-component-container' css class.

--- extensions/amp-story/1.0/bookend/components/cta-link.js
+++ extensions/amp-story/1.0/bookend/components/cta-link.js
@@ -49,15 +49,15 @@ export class CtaLinkComponent {
    * */
   assertValidity(ctaLinksJson) {
     user().assert(ctaLinksJson['links'] && isArray(ctaLinksJson['links']) &&
-      ctaLinksJson['links'].length > 0, 'CTA link component must be an array' +
-      ' and contain at least one link inside it.');
+      ctaLinksJson['links'].length > 0, 'CTA link component must be an array ' +
+      'and contain at least one link inside it.');
 
     ctaLinksJson['links'].forEach(ctaLink => {
       user().assert('text' in ctaLink && 'url' in ctaLink, 'All links in CTA ' +
         'link component must contain `text` field and a `url`.');
 
-      user().assert(isProtocolValid(ctaLink['url']), 'Unsupported protocol' +
-        ` for CTA link URL ${ctaLink['url']}`);
+      user().assert(isProtocolValid(ctaLink['url']), 'Unsupported protocol ' +
+        `for CTA link URL ${ctaLink['url']}`);
     });
   }
 
@@ -83,7 +83,8 @@ export class CtaLinkComponent {
     const html = htmlFor(doc);
     const container =
         html`
-        <div class="i-amphtml-story-bookend-cta-link-wrapper">
+        <div class="i-amphtml-story-bookend-cta-link-wrapper
+          i-amphtml-story-bookend-component">
         </div>`;
 
     let linkSeed =
