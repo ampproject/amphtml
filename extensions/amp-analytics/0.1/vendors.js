f0712b73df878669fae4de28364572660c1f0011
martinzorn <37836539+martinzorn@users.noreply.github.com>
2018-05-22 15:42:45 +0200

    Update vendors.js
    added new variable "ps" for INFOnline

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -1539,6 +1539,7 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
       'pageview': '${url}?st=${st}' +
         '&sv=${sv}' +
         '&ap=${ap}' +
+	'&ps=${ps}' +
         '&co=${co}' +
         '&cp=${cp}' +
         '&host=${canonicalHost}' +
