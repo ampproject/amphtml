bcdc23758aabe3459686f7490496291066b9e471
mister-ben <git@misterben.me>
2018-05-24 00:10:44 +0100

    ✨ amp-brightcove : Implementation of video interface for (#15115)

--- build-system/dep-check-config.js
+++ build-system/dep-check-config.js
@@ -197,7 +197,7 @@ exports.rules = [
       'extensions/amp-youtube/0.1/amp-youtube.js->' +
           'src/service/video-manager-impl.js',
       'extensions/amp-viqeo-player/0.1/amp-viqeo-player.js->' +
-      'src/service/video-manager-impl.js',
+          'src/service/video-manager-impl.js',
       'extensions/amp-dailymotion/0.1/amp-dailymotion.js->' +
           'src/service/video-manager-impl.js',
       'extensions/amp-brid-player/0.1/amp-brid-player.js->' +
