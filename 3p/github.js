df866e029db8406efbce1ac15df0edc3e3a3a30f
Hongfei Ding <lannka@users.noreply.github.com>
2018-05-25 10:44:45 -0700

    Fix lint JsDoc for 3p/*.js (#15572)
    * Fix lint JsDoc for 3p/*.js
    
    * Fix type checks

--- 3p/github.js
+++ 3p/github.js
@@ -26,6 +26,7 @@ import {writeScript} from './3p';
  *
  * @param {!Window} global
  * @param {string} scriptSource The source of the script, different for post and comment embeds.
+ * @param {function(*)} cb
  */
 function getGistJs(global, scriptSource, cb) {
   writeScript(global, scriptSource, function() {
