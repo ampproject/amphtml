8f3b96e74fcae90ac9a499a1ea2be19324eff3e6
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2018-05-25 19:30:30 -0700

    Separate consent state enum file (#15567)
    * seperate file
    
    * import
    
    * add consent.js
    
    * dep-check-fix
    
    * fix test

--- ads/google/adsense.js
+++ ads/google/adsense.js
@@ -15,6 +15,7 @@
  */
 
 import {ADSENSE_RSPV_WHITELISTED_HEIGHT} from './utils';
+import {CONSENT_POLICY_STATE} from '../../src/consent-state';
 import {camelCaseToDash} from '../../src/string';
 import {setStyles} from '../../src/style';
 import {user} from '../../src/log';
@@ -72,12 +73,12 @@ export function adsense(global, data) {
   });
   const initializer = {};
   switch (global.context.initialConsentState) {
-    case 4: // CONSENT_POLICY_STATE.UNKNOWN
+    case CONSENT_POLICY_STATE.UNKNOWN:
       if (data['npaOnUnknownConsent'] != 'true') {
         // Unknown w/o NPA results in no ad request.
         return;
       }
-    case 2: // CONSENT_POLICY_STATE.INSUFFICIENT
+    case CONSENT_POLICY_STATE.INSUFFICIENT:
       (global.adsbygoogle = global.adsbygoogle || [])
           ['requestNonPersonalizedAds'] = true;
       break;
