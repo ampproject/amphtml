b83f7720c4d2b1b09adea365130ff267c8bd3af1
Ugo Stephant <ugo@poool.fr>
2018-05-28 17:10:33 +0200

    refactor: clean code after linter almost puked itself
    Co-Authored-By: Nicolas AUGER <nicolasauger@users.noreply.github.com>

--- 3p/poool.js
+++ 3p/poool.js
@@ -16,34 +16,35 @@
 
 import {loadScript} from './3p';
 
-import {user} from '../src/log';
+import {camelCaseToDash, dashToUnderline} from '../src/string';
 import {dict} from '../src/utils/object';
+import {user} from '../src/log';
 
 const CONFIG = {
-    debug: false,
-    force_widget: null,
-    login_button_enabled: true,
-    signature_enabled: true,
-    user_is_premium: false,
-    video_client: "vast",
-    custom_segment: null,
-    cookies_enabled: false
+  debug: false,
+  forceWidget: null,
+  loginButtonEnabled: true,
+  signatureEnabled: true,
+  userIsPremium: false,
+  videoClient: 'vast',
+  customSegment: null,
+  cookiesEnabled: false,
 };
 
 const EVENTS = [
-    "lock",
-    "release",
-    "hidden",
-    "disabled",
-    "register",
-    "error",
-    "adblock",
-    "outdatedBrowser",
-    "userOutsideCohort",
-    "identityAvailable",
-    "subscribeClick",
-    "loginClick",
-    "dataPolicyClick"
+  'lock',
+  'release',
+  'hidden',
+  'disabled',
+  'register',
+  'error',
+  'adblock',
+  'outdatedBrowser',
+  'userOutsideCohort',
+  'identityAvailable',
+  'subscribeClick',
+  'loginClick',
+  'dataPolicyClick',
 ];
 
 /**
@@ -53,73 +54,72 @@ const EVENTS = [
  * @param {function(!Object)} cb
  */
 function getPooolSDK(global, cb) {
-    loadScript(global, 'https://assets.poool.fr/poool.min.js', function() {
-        cb(global.poool);
-    });
+  loadScript(global, 'https://assets.poool.fr/poool.min.js', function() {
+    cb(global.poool);
+  });
 }
 
 export function poool(global, data) {
 
-    // Check for required attributes
-    const bundleID = user().assert(
-        data.appId,
-        'The data-app-id attribute is required for <amp-poool> %s',
-        data.element
-    );
+  // Check for required attributes
+  const bundleID = user().assert(
+      data.appId,
+      'The data-app-id attribute is required for <amp-poool> %s',
+      data.element
+  );
 
-    const pageType = user().assert(
-        data.pageType,
-        'The data-page-type attribute is required for <amp-poool> %s',
-        data.element
-    );
+  const pageType = user().assert(
+      data.pageType,
+      'The data-page-type attribute is required for <amp-poool> %s',
+      data.element
+  );
 
-    // Create paywall container
-    const container = document.createElement("div");
-    container.id = "poool-widget";
-    global.document.querySelector("#c").appendChild(container);
+  // Create paywall container
+  const container = document.createElement('div');
+  container.id = 'poool-widget';
+  global.document.querySelector('#c').appendChild(container);
 
-    getPooolSDK(global, (_poool) => {
+  getPooolSDK(global, _poool => {
 
-        // Init poool
-        _poool("init", data.appId);
-        _poool("config", "mode", "custom", true);
-        _poool("event", "onIdentityAvailable", function(){
-            const size = container.getBoundingClientRect();
-            global.context.updateDimensions(size.width, size.height);
-        });
-
-        // Set config
-        for (const [configKey, configDefaultValue] of Object.entries(CONFIG)) {
-            const configValue = data[configKey] || configDefaultValue;
-
-            if(configValue){
-                _poool("config", configKey, configValue);
-            }
-        }
-
-        // Set event handlers
-        for (const eventName of EVENTS) {
-            _poool(
-                "event",
-                `on${eventName[0].toUpperCase()}${eventName.slice(1)}`,
-                function(eventData){
-                    const message = JSON.stringify(dict({
-                        "action": eventName,
-                        "data": eventData
-                    }));
-
-                    global.parent.postMessage(message, "*");
-                }
-            );
-        }
-
-        // Send conversion event, if provided
-        if(data.conversion){
-            _poool("send", "conversion");
-        }
-
-        // Create hit
-        _poool("send", "page-view", pageType);
-
+    // Init poool
+    _poool('init', bundleID);
+    _poool('config', 'mode', 'custom', true);
+    _poool('event', 'onIdentityAvailable', function() {
+      const size = container.getBoundingClientRect();
+      global.context.updateDimensions(size.width, size.height);
     });
+
+    // Set config
+    for (const configEntry in Object.entries(CONFIG)) {
+      const configKey = configEntry[0],
+          configDefaultValue = configEntry[1];
+
+      const configValue = data[configKey] || configDefaultValue;
+
+      if (configValue) {
+        _poool('config', dashToUnderline(camelCaseToDash(configKey)),
+            configValue);
+      }
+    }
+
+    // Set event handlers
+    for (const eventName in EVENTS) {
+      _poool(
+          'event',
+          `on${eventName[0].toUpperCase()}${eventName.slice(1)}`,
+          function(eventData) {
+            const message = JSON.stringify(dict({
+              'action': eventName,
+              'data': eventData,
+            }));
+
+            global.parent.postMessage(message, '*');
+          }
+      );
+    }
+
+    // Create hit
+    _poool('send', 'page-view', pageType);
+
+  });
 }
--- extensions/amp-poool/0.1/amp-poool.js
+++ extensions/amp-poool/0.1/amp-poool.js
@@ -13,205 +13,151 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {ActionTrust} from '../../../src/action-constants';
 import {CSS} from '../../../build/amp-poool-0.1.css';
 import {Layout} from '../../../src/layout';
-import {user} from '../../../src/log';
+import {Services} from '../../../src/services';
+import {createCustomEvent, getData, listen} from '../../../src/event-helper';
+import {getIframe, preloadBootstrap} from '../../../src/3p-frame';
+import {isObject} from '../../../src/types';
+import {listenFor} from '../../../src/iframe-helper';
+import {removeElement} from '../../../src/dom';
+import {tryParseJson} from '../../../src/json';
 
-const PooolConfig = {
-    debug: { default: false, noString: true},
-    mode: { default: "", noString: false},
-    percent: { default: 80, noString: true},
-    post_container: { default: "[data-poool]", noString: false},
-    widget_container: { default: "#poool-widget", noString: true},
-    force_widget: { default: "", noString: false},
-    subscription_url: { default: "", noString: false},
-    newsletter_name: { default: "", noString: false},
-    newsletter_id: { default: "", noString: true},
-    login_url: { default: "", noString: false},
-    login_button_enabled: { default: true, noString: true},
-    signature_enabled: { default: true, noString: true},
-    user_is_premium: { default: true, noString: true},
-    video_primary_mode: { default: "html5", noString: false},
-    video_client: { default: "vast", noString: false},
-    popover_enabled: { default: false, noString: true},
-    alternative_enabled: { default: true, noString: true},
-    alternative_widget: { default: "video", noString: false},
-    adblock_enabled: { default: false, noString: true},
-    vast: { default: "", noString: false},
-    mobile_vast: { default: "", noString: false},
-    custom_segment: { default: null, noString: false},
-    cookies_enabled: { default: true, noString: true},
-    data_policy_url: { default: "", noString: false}
-};
+/** @const {string} */
+const TAG = 'amp-poool';
 
-const PooolStyles = [
-    "main_color",
-    "brand_logo",
-    "brand_cover",
-    "background_color"
-];
-
-const PooolEvents = [
-  "on_lock",
-  "on_release",
-  "on_hidden",
-  "on_disabled",
-  "on_register",
-  "on_subscribeclick",
-  "on_error",
-  "on_adblock",
-  "on_outdatedbrowser",
-  "on_useroutsidecohort",
-  "on_identityavailable",
-  "on_datapolicyclick"
+const EVENTS = [
+  'lock',
+  'release',
+  'hidden',
+  'disabled',
+  'register',
+  'error',
+  'adblock',
+  'outdatedBrowser',
+  'userOutsideCohort',
+  'identityAvailable',
+  'subscribeClick',
+  'loginClick',
+  'dataPolicyClick',
 ];
 
 class AmpPoool extends AMP.BaseElement {
 
-    /** @param {!AmpElement} element */
-    constructor(element) {
-        super(element);
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    super(element);
 
-        /** @private {!Element} */
-        this.container_ = this.win.document.createElement('div');
+    /** @private {?HTMLIFrameElement} */
+    this.iframe_ = null;
 
+    /** @private {?../../../src/service/action-impl.ActionService} */
+    this.action_ = null;
+
+    /** @private {?Function} */
+    this.unlistenMessage_ = null;
+  }
+
+  /**
+  * @param {boolean=} opt_onLayout
+  * @override
+  */
+  preconnectCallback(opt_onLayout) {
+    this.preconnect.url('https://api.poool.fr', opt_onLayout);
+    this.preconnect.url('https://assets.poool.fr', opt_onLayout);
+    this.preconnect.preload(
+        'https://assets.poool.fr/poool.min.js',
+        'script'
+    );
+
+    preloadBootstrap(this.win, this.preconnect);
+  }
+
+  /** @override */
+  buildCallback() {
+    // Register actions
+    this.action_ = Services.actionServiceForDoc(this.element);
+  }
+
+  /** @override */
+  layoutCallback() {
+    const iframe = getIframe(this.win, this.element, 'poool');
+    this.applyFillContent(iframe);
+
+    // Triggered by context.updateDimensions() inside the iframe.
+    listenFor(iframe, 'embed-size', data => {
+      this.changeHeight(data['height']);
+    }, /* opt_is3P */true);
+
+    // Listen for everything else
+    this.unlistenMessage_ = listen(
+        this.win,
+        'message',
+        this.handlePooolMessages_.bind(this)
+    );
+
+    this.element.appendChild(iframe);
+    this.iframe_ = iframe;
+    return this.loadPromise(iframe);
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    return layout == Layout.RESPONSIVE;
+  }
+
+  /**
+  * @private
+  * @param {Event=} event
+  */
+  handlePooolMessages_(event) {
+    if (this.iframe_ && event.source != this.iframe_.contentWindow) {
+      return;
     }
 
-    /** @override */
-    buildCallback() {
-        // Check if required parameter init (bundle-id) isn't missing
-        const bundle_id = user().assert(
-            this.element.getAttribute('data-app-id'),
-            'The init attribute is required for <amp-poool> %s',
-            this.element
-        );
-
-        // Check if required parameter page-view (page-type) isn't missing
-        const page_type = user().assert(
-            this.element.getAttribute('data-page-view'),
-            'The page_view attribute is required for <amp-poool> %s',
-            this.element
-        );
-
-        // Assign poool conversion variable
-        const conversion = this.element.getAttribute("data-conversion");
-
-        // Create a div with "poool-widget" id to display it
-        this.container_.id = "poool-widget";
-        this.element.appendChild(this.container_);
-
-        var head = this.win.document.head;
-        var script = document.createElement("script");
-
-        // Start Poool tag with init value
-        script.innerHTML = `
-            !function(w,d,s,u,p,t,o){
-                w[p]=w[p]||function(){(w[p]._q=w[p]._q||[]).push(arguments)},
-                t=d.createElement(s),o=d.getElementsByTagName(s)[0],
-                t.async=1,t.src=u,o.parentNode.insertBefore(t,o)
-            }(window, document, "script", "https://assets.poool.fr/poool.min.js"
-            , "poool");
-
-            poool("init", "${bundle_id}");
-        `;
-
-        // Apply publisher custom configs
-        for (config in PooolConfig) {
-            this.updatePooolConfig_(
-                "config",
-                config,
-                this.element.getAttribute(`data-${config.replace(/_/g, "-")}`),
-                script,
-                PooolConfig[config].noString
-            );
-        }
-
-        // Apply publisher custom styles
-        for (style of PooolStyles) {
-            this.updatePooolConfig_(
-                "style",
-                style,
-                this.element.getAttribute(`data-${style.replace("_", "-")}`),
-                script
-            );
-        }
-
-        // Apply publisher custom events
-        var events = this.win.document.getElementById(
-            this.element.getAttribute("data-events")
-        );
-
-        // Only if publisher want to set custom events
-        if(events) {
-            custom_events = JSON.parse(events.innerHTML)
-
-            for (event of PooolEvents) {
-                // Only if event is specified
-                if (custom_events[event]) {
-                    this.updatePooolConfig_(
-                        "event",
-                        event.replace("_", ""),
-                        custom_events[event],
-                        script,
-                        true
-                    );
-                }
-            }
-        }
-
-        // Send email when specified
-        this.updatePooolConfig_(
-            "send",
-            "email",
-            this.element.getAttribute("data-email"),
-            script
-        );
-
-        // Send conversion if needed
-        if(conversion){ script.innerHTML += "poool('send', 'conversion');\n" }
-
-        // Send page-view to Poool
-        this.updatePooolConfig_(
-            "send",
-            "page-view",
-            page_type,
-            script
-        );
-
-        // Append script to head tag
-        head.appendChild(script);
+    const eventData = getData(event);
+    if (!eventData) {
+      return;
     }
 
-    /** @override */
-    isLayoutSupported(layout) {
-        return layout == Layout.RESPONSIVE;
+    const parsedEventData = isObject(eventData) ?
+      eventData : tryParseJson(eventData);
+    if (!parsedEventData) {
+      return;
     }
 
+    if (EVENTS.indexOf(eventData['action']) > -1) {
+      this.fireEvent_(eventData['action'], eventData['data']);
+    }
+  }
 
-    /**
-     * Update current Poool config with custom data
-     * @param {string} type Type of config (config, style, event)
-     * @param {string} name Poool config parameter name
-     * @param {string} value Poool config parameter value
-     * @param {string} script Poool script to update
-     * @param {boolean} no_string True if parameter isn't a string
-     * @private
-     */
-    updatePooolConfig_ (type, name, value, script, no_string) {
-        if(value != null){
-            if(no_string){
-                script.innerHTML +=
-                    `poool('${type}', '${name}', ${value});\n`;
-            }
-            else{
-                script.innerHTML +=
-                    `poool('${type}', '${name}', '${value}');\n`;
-            }
-        }
-    };
+  /**
+  * @private
+  * @param {string=} name
+  * @param {Object=} data
+  */
+  fireEvent_(name, data) {
+    const event = createCustomEvent(this.win, `${TAG}.${name}`, data);
+    this.action_.trigger(this.element, name, event, ActionTrust.HIGH);
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    if (this.iframe_) {
+      removeElement(this.iframe_);
+      this.iframe_ = null;
+    }
+
+    if (this.unlistenMessage_) {
+      this.unlistenMessage_();
+    }
+
+    return true;
+  }
 }
 
 
 AMP.extension('amp-poool', '0.1', AMP => {
-    AMP.registerElement('amp-poool', AmpPoool, CSS);
+  AMP.registerElement('amp-poool', AmpPoool, CSS);
 });
