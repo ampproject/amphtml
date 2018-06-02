aa36f3b2269e46abbe3052f99afc70a684ff4b9f
Cathy Zhu <cathyxz@gmail.com>
2018-06-01 17:22:34 -0700

    Prototype for <amp-content-viewer> with svg support only (#15545)
    * Add amp-content-viewer
    
    * Solve tap target issue
    
    * Fix jsdoc
    
    * Add license to css file
    
    * Add experiment
    
    * Address review
    
    * Fix pan / zoom bug
    
    * Remove tapzoom
    
    * Add zoomEndEvent
    
    * Rename amp-content-viewer to amp-pan-zoom
    
    * Add issue number
    
    * Add target to gesture-recognizer test
    
    * Define action service
    
    * Add zoomEnd event usage to pan/zoom example
    
    * Fix test

--- gulpfile.js
+++ gulpfile.js
@@ -180,6 +180,7 @@ declareExtension('amp-date-picker', '0.1', {hasCss: true});
 declareExtension('amp-image-viewer', '0.1', {hasCss: true});
 declareExtension('amp-subscriptions', '0.1', {hasCss: true});
 declareExtension('amp-subscriptions-google', '0.1', {hasCss: true});
+declareExtension('amp-pan-zoom', '0.1', {hasCss: true});
 /**
  * @deprecated `amp-slides` is deprecated and will be deleted before 1.0.
  * Please see {@link AmpCarousel} with `type=slides` attribute instead.
