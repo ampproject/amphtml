3154428c749aea2f36a69aed2ea97c3cc5298001
Cathy Zhu <cathyzhu@google.com>
2018-06-04 17:18:38 -0700

    Improve error message for future debugging

--- extensions/amp-carousel/0.1/slidescroll.js
+++ extensions/amp-carousel/0.1/slidescroll.js
@@ -554,7 +554,9 @@ export class AmpSlideScroll extends BaseSlides {
     const newSlideInView = this.slides_[newIndex];
 
     if (newSlideInView === undefined) {
-      const error = new Error('Attempting to access a non-existant slide');
+      const error = new Error(
+          `Attempting to access a non-existant slide ${newIndex}/${noOfSlides_}`
+      );
       error.args = {
         'index': newIndex,
         'noOfSlides': noOfSlides_,
