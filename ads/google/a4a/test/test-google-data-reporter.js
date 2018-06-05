049d194bec526921c887055ccd618f616c33ea2c
keithwrightbos <keithwrightbos@users.noreply.github.com>
2018-06-05 10:03:28 -0400

    Doubleclick remove fast fetch upgrade predicate (#15804)
    * initial commit
    
    * initial commit
    
    * fix compile error
    
    * fix experiment to url mapping
    
    * fix tests
    
    * remove ampdoc requirement
    
    * complete test fixes
    
    * revert changes to traffic-experiments
    
    * fix type check error
    
    * PR feedback
    
    * fix test failure

--- ads/google/a4a/test/test-google-data-reporter.js
+++ ads/google/a4a/test/test-google-data-reporter.js
@@ -21,9 +21,6 @@ import {
   BaseLifecycleReporter,
   GoogleAdLifecycleReporter,
 } from '../performance';
-import {
-  DOUBLECLICK_A4A_EXPERIMENT_NAME,
-} from '../../../../extensions/amp-ad-network-doubleclick-impl/0.1/doubleclick-a4a-config';
 import {EXPERIMENT_ATTRIBUTE, QQID_HEADER} from '../utils';
 import {createIframePromise} from '../../../../testing/iframe';
 import {forceExperimentBranch} from '../../../../src/experiments';
@@ -47,7 +44,6 @@ describe('#getLifecycleReporter', () => {
 
   it('should not create reporter if sampling is not enabled', () => {
     forceExperimentBranch(win, 'a4aProfilingRate', null);
-    forceExperimentBranch(win, DOUBLECLICK_A4A_EXPERIMENT_NAME, '1234');
     const element = doc.createElement('div');
     element.setAttribute('type', 'doubleclick');
     doc.body.appendChild(element);
@@ -70,7 +66,6 @@ describe('#getLifecycleReporter', () => {
 
   it('should create reporter for doubleclick', () => {
     forceExperimentBranch(win, 'a4aProfilingRate', 'unused');
-    forceExperimentBranch(win, DOUBLECLICK_A4A_EXPERIMENT_NAME, '1234');
     const element = doc.createElement('div');
     element.setAttribute('type', 'doubleclick');
     doc.body.appendChild(element);
@@ -138,7 +133,8 @@ describes.sandboxed('#googleLifecycleReporterFactory', {}, () => {
       fakeElt.setAttribute('type', 'doubleclick');
       fakeElt.setAttribute(EXPERIMENT_ATTRIBUTE, '1234');
       fakeElt.setAttribute('data-a4a-upgrade-type', 'foo');
-      forceExperimentBranch(env.win, DOUBLECLICK_A4A_EXPERIMENT_NAME, '1234');
+      // Google lifecycle disabled by default for test environment.
+      env.win.AMP_MODE = {localDev: true};
       env.win.document.body.appendChild(fakeElt);
       env.win.ampAdPageCorrelator = 7777777;
       const a4aContainer = {
