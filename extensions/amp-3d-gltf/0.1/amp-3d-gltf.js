ef8e5c83b16ae57ac7997f366233b37980baeaa5
mixture <turkeevm@gmail.com>
2018-06-06 15:12:02 +0300

    amp-3d-gltf: use this.getAmpdoc().getUrl()

--- extensions/amp-3d-gltf/0.1/amp-3d-gltf.js
+++ extensions/amp-3d-gltf/0.1/amp-3d-gltf.js
@@ -96,7 +96,7 @@ export class Amp3dGltf extends AMP.BaseElement {
         this.element);
 
     this.context_ = dict({
-      'src': resolveRelativeUrl(src, this.win.location),
+      'src': resolveRelativeUrl(src, this.getAmpdoc().getUrl()),
       'renderer': {
         'alpha': getOption('alpha', bool, false),
         'antialias': getOption('antialiasing', bool, true),
