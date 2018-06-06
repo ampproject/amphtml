bab48194ef758e10cbdce7d32bc67a753062a824
morleos <morleos@users.noreply.github.com>
2018-06-07 00:50:46 +0100

    ✨Add New Ads Network - Epeex (#15676)
    * Add new network - Epeex
    
    * add epeex to list in amp-ad.md & capitalize
    
    * v0.2.0
    
    * v1.0.0
    
    * Revert "v1.0.0"
    
    This reverts commit e75564a3e0b976108f14ecbf6dd4b2b44f0fc855.
    
    * Revert "v0.2.0"
    
    This reverts commit 9d683dbbb7fc5b6bddca7462ee3032ab890aaaf1.
    
    * capitalize epeex
    
    * Epeex to epeex
    
    * render Start Implemented
    
    * Revert "render Start Implemented"
    
    This reverts commit 2ffc21ecf8d1e26d0a9f2c963dba099c3f38f3d0.

--- 3p/integration.js
+++ 3p/integration.js
@@ -128,6 +128,7 @@ import {dotandads} from '../ads/dotandads';
 import {eadv} from '../ads/eadv';
 import {eas} from '../ads/eas';
 import {engageya} from '../ads/engageya';
+import {epeex} from '../ads/epeex';
 import {eplanning} from '../ads/eplanning';
 import {ezoic} from '../ads/ezoic';
 import {f1e} from '../ads/f1e';
@@ -248,6 +249,7 @@ const AMP_EMBED_ALLOWED = {
   bringhub: true,
   dable: true,
   engageya: true,
+  epeex: true,
   kuadio: true,
   'mantis-recommend': true,
   mywidget: true,
@@ -331,6 +333,7 @@ register('dotandads', dotandads);
 register('eadv', eadv);
 register('eas', eas);
 register('engageya', engageya);
+register('epeex', epeex);
 register('eplanning', eplanning);
 register('ezoic', ezoic);
 register('f1e', f1e);
