8e2cd9dbf0551c265f5e6d187451d54e59999a35
Gabriel Majoulet <gmajoulet@google.com>
2018-06-07 19:51:52 -0400

    Adding translation keys for the bookend prompt consent link. (#15910)

--- extensions/amp-story/0.1/_locales/en.js
+++ extensions/amp-story/0.1/_locales/en.js
@@ -21,6 +21,16 @@ import {LocalizedStringBundleDef, LocalizedStringId} from '../localization';
  * @const {!LocalizedStringBundleDef}
  */
 export default {
+  [LocalizedStringId.AMP_STORY_BOOKEND_PRIVACY_SETTINGS_TITLE]: {
+    string: 'Privacy settings',
+    description: 'Title for a section that allows the user to configure ' +
+        'their privacy settings',
+  },
+  [LocalizedStringId.AMP_STORY_BOOKEND_PRIVACY_SETTINGS_BUTTON_LABEL]: {
+    string: 'Change data privacy settings',
+    description: 'Label for a button that allows the user to change their ' +
+        'choice to consent to providing their cookie access.',
+  },
   [LocalizedStringId.AMP_STORY_CONSENT_ACCEPT_BUTTON_LABEL]: {
     string: 'Accept',
     description: 'Label for a button that allows the user to consent to ' +
