bd749a739f9c51ceef9a96d072cb9dd6b9416ea6
Gabriel Majoulet <gmajoulet@google.com>
2018-06-08 10:59:52 -0400

    Deprecate dismiss consent translation key. (#15908)

--- extensions/amp-story/1.0/_locales/no.js
+++ extensions/amp-story/1.0/_locales/no.js
@@ -27,9 +27,6 @@ export default {
   [LocalizedStringId.AMP_STORY_CONSENT_DECLINE_BUTTON_LABEL]: {
     string: 'Avslå',
   },
-  [LocalizedStringId.AMP_STORY_CONSENT_DISMISS_DIALOG_BUTTON_LABEL]: {
-    string: 'Ok',
-  },
   [LocalizedStringId.AMP_STORY_DOMAIN_DIALOG_HEADING_LABEL]: {
     string: 'Vis på det opprinnelige domenet:',
   },
