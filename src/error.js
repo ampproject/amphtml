260ed94164281fa6bee394bc7e3e2df8db3320eb
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2018-06-08 14:52:08 -0700

    class to constructor (#15943)

--- src/error.js
+++ src/error.js
@@ -552,7 +552,7 @@ export function resetAccumulatedErrorMessagesForTesting() {
  * @visibleForTesting
  */
 export function detectJsEngineFromStack() {
-  /** @constructor */ // eslint-disable-line
+  /** @constructor */
   function Fn() {}
   Fn.prototype.t = function() {
     throw new Error('message');
