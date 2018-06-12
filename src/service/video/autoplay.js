8330b4aca014df124c93f0aeea5a7efd95028872
Alan Orozco <alanorozco@users.noreply.github.com>
2018-06-12 08:59:03 -0700

    🚀♻️Delegate autoplay using signal (#15952)

--- src/service/video/autoplay.js
+++ src/service/video/autoplay.js
@@ -177,14 +177,13 @@ export class Autoplay {
 
   /**
    * @param {!Element} element
-   * @param {!../../observable.Observable<boolean>} observable
    */
-  delegate(element, observable) {
+  delegate(element) {
     const entry = this.getEntryFor_(element);
     if (!entry) {
       return;
     }
-    entry.delegateTo(observable);
+    entry.delegate();
   }
 
   /**
@@ -228,7 +227,7 @@ export class AutoplayEntry {
     /** @private {boolean} */
     this.isVisible_ = false;
 
-    /** @private {!UnlistenDef} */
+    /** @private {!UnlistenDef|null} */
     this.unlistener_ = this.observeOn_(positionObserver);
 
     // Only muted videos are allowed to autoplay
@@ -253,15 +252,15 @@ export class AutoplayEntry {
   }
 
   /**
-   * @param {!../../observable.Observable<boolean>} playbackObservable
+   * Delegates autoplay so that it's triggered by a different module.
    * @public
    */
-  delegateTo(playbackObservable) {
+  delegate() {
     if (this.unlistener_) {
       this.unlistener_();
     }
-    this.unlistener_ =
-        playbackObservable.add(isPlaying => this.trigger_(isPlaying));
+    this.video.pause();
+    this.unlistener_ = null;
   }
 
   /** @private */
