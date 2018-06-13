e8a92d1f649d33063b3992b1862ef8d210034f8c
Alan Orozco <alanorozco@users.noreply.github.com>
2018-06-13 12:05:08 -0700

    📊✨ Show equalizer icon in stories (#15967)

--- build-system/dep-check-config.js
+++ build-system/dep-check-config.js
@@ -248,11 +248,14 @@ exports.rules = [
           'src/service/notification-ui-manager.js',
       'extensions/amp-consent/0.1/amp-consent.js->' +
           'src/service/notification-ui-manager.js',
-      // For autoplay delegation.
+      // For autoplay delegation:
       'extensions/amp-story/0.1/amp-story-page.js->' +
           'src/service/video-service-sync-impl.js',
       'extensions/amp-story/1.0/amp-story-page.js->' +
           'src/service/video-service-sync-impl.js',
+      // Accessing USER_INTERACTED constant:
+      'extensions/amp-story/1.0/media-pool.js->' +
+          'src/service/video-service-interface.js',
       'extensions/amp-story/1.0/page-advancement.js->' +
           'src/service/action-impl.js',
       'extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js->' +
