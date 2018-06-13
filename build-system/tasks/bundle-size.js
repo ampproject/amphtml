aafbb8e80ee17777a8132961aabf340a06933695
William Chou <willchou@google.com>
2018-06-13 12:31:58 -0400

    Unbreak master (#16016)

--- build-system/tasks/bundle-size.js
+++ build-system/tasks/bundle-size.js
@@ -22,7 +22,7 @@ const log = require('fancy-log');
 const {getStdout} = require('../exec');
 
 const runtimeFile = './dist/v0.js';
-const maxSize = '78.01KB';
+const maxSize = '78.02KB';
 
 const {green, red, cyan, yellow} = colors;
 
