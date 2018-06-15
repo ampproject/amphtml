00b4809f9f9a2acb7577f14eb442122320bd3ff4
sergeydaub <sergeydaub@gmail.com>
2018-06-15 13:52:11 +0300

    -- intersection calculating removed -- parameters for mark tag added

--- gulpfile.js
+++ gulpfile.js
@@ -1595,7 +1595,6 @@ function mkdirSync(path) {
  * @return {Promise}
  */
 function toPromise(readable) {
-  // eslint-disable-next-line amphtml-internal/resolve-inside-promise-resolver
   return new Promise(function(resolve, reject) {
     readable.on('error', reject).on('end', resolve);
   });
