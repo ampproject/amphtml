eb97cc90afb32250e5936d9bd0148d98c1a02d74
zhouyx <zhouyx@google.com>
2018-06-18 16:38:53 -0700

    readme example

--- ads/_ping_.js
+++ ads/_ping_.js
@@ -28,7 +28,7 @@ export function _ping_(global, data) {
   // for testing only. see #10628
   global.networkIntegrationDataParamForTesting = data;
 
-  validateData(data, []);
+  validateData(data, ['url']);
   user().assert(!data['error'], 'Fake user error!');
   global.document.getElementById('c').textContent = data.ping;
   global.ping = Object.create(null);
