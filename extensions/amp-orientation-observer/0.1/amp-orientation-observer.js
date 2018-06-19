4013ef0aff059c50988868dcd535b6449e525606
Naina Raisinghani <nainar@google.com>
2018-06-18 17:07:30 -0700

    Initial code commit

---
+++ extensions/amp-orientation-observer/0.1/amp-orientation-observer.js
@@ -0,0 +1,200 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Observable} from '../../../src/observable';
+import {user} from '../../../src/log';
+
+
+const TAG = 'amp-orientation-observer';
+
+/**
+ * @typedef {{
+ *   absolute: !number,
+ *   alpha: !number,
+ *   beta: !number,
+ *   gamma: !number,
+ * }}
+ */
+let DeviceOrientationDef;
+
+/** @const */
+const MIN_EVENT_INTERVAL_IN_MS = 100;
+
+/** @const */
+const DeviceOrientationEvents = ['alpha', 'beta', 'gamma', 'landscape', 'portrait'];
+
+
+export class AmpOrientationObserver extends AMP.BaseElement {
+
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    super(element);
+
+    /** @private @const */
+    this.ampdoc_ = Services.ampdoc(this.element);
+
+    /** @private {?../../../src/service/action-impl.ActionService} */
+    this.action_ = null;
+
+    /** @private {?Observable} */
+    this.deviceOrientationObservable_ = null;
+
+    /** @private {Array<number>} */
+    this.alphaRange_ = [0, 360];
+
+    /** @private {Array<number>} */
+    this.betaRange_ = [-180, 180];
+
+    /** @private {Array<number>} */
+    this.gammaRange_ = [-90, 90];
+
+    /** @private {number} */
+    this.alphaValue_ = 0;
+
+    /** @private {number} */
+    this.betaValue_ = 0;
+
+    /** @private {number} */
+    this.gammaValue_ = 0;
+
+  }
+
+  /** @override */
+  buildCallback() {
+    // Since this is a functional component and not visual,
+    // layoutCallback is meaningless. We delay the heavy work until
+    // we become visible.
+    this.action_ = Services.actionServiceForDoc(this.element);
+    const viewer = Services.viewerForDoc(this.ampdoc_);
+    viewer.whenFirstVisible().then(this.init_.bind(this));
+  }
+
+  /**
+   * @private
+   */
+  init_() {
+    this.parseAttributes_();
+
+    user().assert(this.win.DeviceOrientationEvent, 
+      'The current browser doesn\'t support the `window.DeviceOrientationEvent`');
+
+    if (!this.deviceOrientationObservable_) {
+      this.deviceOrientationObservable_ = new Observable();
+      const listener = throttle(this.win_, event => {
+        this.deviceOrientationObservable_.fire(event);
+      }, MIN_EVENT_INTERVAL_IN_MS);
+      this.win.addEventListener('deviceorientation', listener, true);
+    }
+    return this.deviceOrientationObservable_.add(() => {
+      callback(this.deviceOrientationHandler_(event));
+    });
+  }
+
+  /**
+   * @private
+   */
+  parseAttributes_() {
+    const alphaRange = this.element.getAttribute('alpha-range');
+    const betaRange = this.element.getAttribute('beta-range');
+    const gammaRange = this.element.getAttribute('gamma-range');
+    if (alphaRange) {
+      const range = alphaRange.trim().split(' ');
+      this.alphaRange_ = [parseInt(range[0]), parseInt(range[1])];
+    }
+    if (betaRange) {
+      const range = betaRange.trim().split(' ');
+      this.betaRange_ = [parseInt(range[0]), parseInt(range[1])];
+    }
+    if (gammaRange) {
+      const range = gammaRange.trim().split(' ');
+      this.gammaRange_ = [parseInt(range[0]), parseInt(range[1])];
+    }
+  }
+
+  /**
+   * @param element {!Event}
+   * @return {!DeviceOrientationDef}
+   * @private
+   */
+  deviceOrientationHandler_(event) {
+    console.log(event);
+  }
+
+  /**
+   * Dispatches the `alpha` event to signify change in the device orientation
+   * along alpha axis.
+   * @private
+   */
+  triggerAlpha_() {
+    const name = 'alpha';
+    const percentValue = this.alphaRange_[0] < 0 ?
+      (this.alphaPosition_ - this.alphaRange_[0]) :
+      this.alphaPosition_;
+    const event = createCustomEvent(this.win, `${TAG}.${name}`, {
+      angle: this.alphaPosition_,
+      percent: percentValue / (this.alphaRange_[1] - this.alphaRange_[0])
+    });
+    this.action_.trigger(this.element, name, event, ActionTrust.LOW);
+  }
+
+  /**
+   * Dispatches the `beta` event to signify change in the device orientation
+   * along beta axis.
+   * @private
+   */
+  triggerBeta_() {
+    const name = 'beta';
+    const percentValue = this.betaRange_[0] < 0 ?
+      (this.betaPosition_ - this.betaRange_[0]) :
+      this.betaPosition_;
+    const event = createCustomEvent(this.win, `${TAG}.${name}`, {
+      angle: this.betaPosition_,
+      percent: percentValue / (this.betaRange_[1] - this.betaRange_[0])
+    });
+    this.action_.trigger(this.element, name, event, ActionTrust.LOW);
+  }
+
+  /**
+   * Dispatches the `gamma` event to signify change in the device orientation
+   * along gamma axis.
+   * @private
+   */
+  triggerGamma_() {
+    const name = 'gamma';
+    const percentValue = this.gammaRange_[0] < 0 ?
+      (this.gammaPosition_ - this.gammaRange_[0]) :
+      this.gammaPosition_;
+    const event = createCustomEvent(this.win, `${TAG}.${name}`, {
+      angle: this.gammaPosition_,
+      percent: percentValue / (this.gammaRange_[1] - this.gammaRange_[0])
+    });
+    this.action_.trigger(this.element, name, event, ActionTrust.LOW);
+  }
+
+  /**
+   * Whether the current browser is Firefox.
+   * Firefox does not handle the angles the same way, so on some axes the
+   * direction is reversed.
+   * @param ua {string}
+   * @return {boolean}
+   */
+  isFirefox(ua) {
+    return /Firefox/i.test(ua);
+  }
+
+AMP.extension(TAG, '0.1', AMP => {
+  AMP.registerElement(TAG, AmpVisibilityObserver);
+});
---
+++ extensions/amp-orientation-observer/amp-orientation-observer.md
@@ -0,0 +1,60 @@
+<!---
+Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# <a name="amp-orientation-observer"></a> `amp-orientation-observer`
+
+<table>
+  <tr>
+    <td width="40%"><strong>Description</strong></td>
+    <td>Monitors the orientation of an element within the viewport as a user scrolls, and dispatches <code>enter</code>, <code>exit</code> and <code>scroll</code> events that can be used with
+    other components, such as <code>&lt;amp-animation>.</code>
+    </td>
+  </tr>
+  <tr>
+    <td width="40%"><strong>Required Script</strong></td>
+    <td><code>&lt;script async custom-element="amp-orientation-observer" src="https://cdn.ampproject.org/v0/amp-orientation-observer-0.1.js">&lt;/script></code></td>
+  </tr>
+  <tr>
+    <td class="col-fourty"><strong><a href="https://www.ampproject.org/docs/guides/responsive/control_layout.html">Supported Layouts</a></strong></td>
+    <td>nodisplay</td>
+  </tr>
+  <tr>
+    <td width="40%"><strong>Examples</strong></td>
+    <td>
+      <ul>
+        <li><a href="https://ampbyexample.com/visual_effects/basics_of_scrollbound_effects/">Basics of scrollbound effects tutorial</a></li>
+        <li><a href="https://codepen.io/collection/nMJYrv/">CodePen collection with samples</a></li>
+      </ul>
+    </td>
+  </tr>
+</table>
+
+[TOC]
+
+## What is amp-orientation-observer?
+
+## What can I do with amp-orientation-observer?
+
+
+### Scroll-bound animations
+
+#### Example: Animation rotates as user scrolls
+
+## Attributes
+
+## Validation
+
+See [amp-orientation-observer rules](https://github.com/ampproject/amphtml/blob/master/extensions/amp-orientation-observer/validator-amp-orientation-observer.protoascii) in the AMP validator specification.
