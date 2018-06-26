bc9e64a74adff80f62451c96038de5587cd4b02f
William Chou <willchou@google.com>
2018-06-26 16:07:27 -0400

    Remove 'svg-in-mustache' experiment (#16329)
    * Remove 'svg-in-mustache' experiment.
    
    * Restore config changes (will move them to another PR).

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -303,12 +303,6 @@ const EXPERIMENTS = [
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/15158',
   },
   {
-    id: 'svg-in-mustache',
-    name: 'Enables SVG support in amp-mustache templates',
-    spec: 'https://github.com/ampproject/amphtml/issues/15123',
-    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/15360',
-  },
-  {
     id: 'disable-faster-amp-list',
     name: 'Disables new default behavior where <amp-list> will not evaluate ' +
        'bindings on rendered children before first setState() mutation.',
