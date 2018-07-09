ce79f22146e3f5f1d5cad96b483aa5ed2af7acdb
Cathy Zhu <cathyxz@gmail.com>
2018-07-09 15:37:55 -0700

    Launch amp-pan-zoom and clean up experiment (#16343)

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -317,13 +317,6 @@ const EXPERIMENTS = [
     cleanupIssue: 'TODO',
   },
   {
-    id: 'amp-pan-zoom',
-    name: 'Enables zoom / pan manipulation of arbitrary elements' +
-      ' with amp-pan-zoom',
-    spec: 'https://github.com/ampproject/amphtml/issues/13602',
-    cleanupissue: 'https://github.com/ampproject/amphtml/issues/15594',
-  },
-  {
     id: 'amp-orientation-observer',
     name: 'Enables actions based on the orientation of a mobile device',
     spec: 'https://github.com/ampproject/amphtml/issues/14740',
