9b9f55a878608ceab84852390777bb5296dbc3fa
Aaron Labiaga <alabiaga@google.com>
2018-07-11 17:14:45 -0400

    Do not XHR intercept first party (1P) CDN resources (#16650)
    * do not intercept 1p cdn resources
    
    * fix test
    
    * address choumx comment
    
    * address comments
    
    * fix unit test
    
    * revert change

--- src/service/xhr-impl.js
+++ src/service/xhr-impl.js
@@ -22,6 +22,7 @@ import {
   getCorsUrl,
   getSourceOrigin,
   getWinOrigin,
+  isProxyOrigin,
   parseUrlDeprecated,
   serializeQueryString,
 } from '../url';
@@ -166,6 +167,7 @@ export class Xhr {
    *
    * XHRs are intercepted if all of the following are true:
    * - The AMP doc is in single doc mode
+   * - The requested resource is not a 1p request.
    * - The viewer has the `xhrInterceptor` capability
    * - The Viewer is a trusted viewer or AMP is currently in developement mode
    * - The AMP doc is opted-in for XHR interception (`<html>` tag has
@@ -185,7 +187,7 @@ export class Xhr {
     }
     const viewer = Services.viewerForDoc(this.ampdocSingle_);
     const whenFirstVisible = viewer.whenFirstVisible();
-    if (!viewer.hasCapability('xhrInterceptor')) {
+    if (isProxyOrigin(input) || !viewer.hasCapability('xhrInterceptor')) {
       return whenFirstVisible;
     }
     const htmlElement = this.ampdocSingle_.getRootNode().documentElement;
