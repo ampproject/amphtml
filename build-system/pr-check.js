6c796078c10ef28b67280cd66308a08b5c290236
Daniel Rozenberg <me@danielrozenberg.com>
2018-07-11 13:50:32 -0400

    🏗 Precede `gulp visual-diff` with a build step (unless `--nobuild`) (#16504)
    * :%s/--verify/--verify_status/ in `gulp visual-diff`
    
    * Precede `gulp visual-diff` with a build step (unless `--nobuild`)
    
    * Also skip building when running `gulp visual-diff --verify_status`
    
    * Add `--nobuild` to `gulp visual-diff` when executed from `gulp pr-check`
    
    * Print the `--nobuild` help text when running `gulp visual-diff`
    
    * Update `DEVELOPING.md` help text regarding `gulp visual-diff --nobuild`

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -359,7 +359,7 @@ const command = {
           colors.cyan('PERCY_TOKEN') + '. Skipping visual diff tests.');
       return;
     }
-    let cmd = 'gulp visual-diff --headless';
+    let cmd = 'gulp visual-diff --nobuild --headless';
     if (opt_mode === 'skip') {
       cmd += ' --skip';
     } else if (opt_mode === 'master') {
