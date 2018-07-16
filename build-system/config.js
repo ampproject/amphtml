aa679d4f5d29dc82e501fb2f9962a35b735bba25
Raghu Simha <rsimha@ampproject.org>
2018-07-16 15:18:27 -0700

    🏗🐛 Move negative lint globs to `.eslintignore` (#16759)

--- build-system/config.js
+++ build-system/config.js
@@ -94,6 +94,11 @@ const integrationTestPaths = [
   'extensions/**/test/integration/**/*.js',
 ];
 
+const lintGlobs = [
+  '**/*.js',
+  // To ignore a file / directory, add it to .eslintignore.
+];
+
 /** @const  */
 module.exports = {
   testPaths,
@@ -104,23 +109,7 @@ module.exports = {
   unitTestPaths,
   unitTestOnSaucePaths,
   integrationTestPaths,
-  lintGlobs: [
-    '**/*.js',
-    '!**/*.extern.js',
-    '!{node_modules,build,dist,dist.3p,dist.tools,' +
-        'third_party,test/coverage}/**/*.*',
-    '!examples/**/*.*',
-    '!{validator/dist,validator/webui/dist,' +
-        'validator/node_modules,validator/nodejs/node_modules,' +
-        'validator/webui/node_modules}/**/*.*',
-    '!eslint-rules/**/*.*',
-    '!karma.conf.js',
-    '!**/local-amp-chrome-extension/background.js',
-    '!extensions/amp-a4a/0.1/test/testdata/*',
-    '!extensions/amp-access/0.1/access-expr-impl.js',
-    '!extensions/amp-animation/0.1/css-expr-impl.js',
-    '!extensions/amp-bind/0.1/bind-expr-impl.js',
-  ],
+  lintGlobs,
   jsonGlobs: [
     '**/*.json',
     '!{node_modules,build,dist,dist.3p,dist.tools,' +
