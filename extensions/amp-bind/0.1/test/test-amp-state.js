2d408343c7dcc9d6a39a877d3fdd1cea6c06cf34
William Chou <willchou@google.com>
2018-07-17 13:08:05 -0400

    Fix amp-state mutation bug when opening new tab (#16772)
    * Check viewer hasBeenVisible instead of isVisible to support opening tabs.
    
    * Fix other tests.
    
    * Fix tests for real.

--- extensions/amp-bind/0.1/test/test-amp-state.js
+++ extensions/amp-bind/0.1/test/test-amp-state.js
@@ -120,13 +120,15 @@ describes.realWin('AmpState', {
     element.build();
 
     // IMPORTANT: No CORS fetch should happen until viewer is visible.
-    const isVisibleStub = sandbox.stub(viewer, 'isVisible');
-    isVisibleStub.returns(false);
-    element.mutatedAttributesCallback({src: 'https://foo.com/bar?baz=1'});
+    const hasBeenVisibleStub = sandbox.stub(viewer, 'hasBeenVisible');
+    hasBeenVisibleStub.returns(false);
+    allowConsoleError(() => {
+      element.mutatedAttributesCallback({src: 'https://foo.com/bar?baz=1'});
+    });
     expect(ampState.fetchAndUpdate_).to.not.have.been.called;
     expect(ampState.fetch_).to.not.have.been.called;
 
-    isVisibleStub.returns(true);
+    hasBeenVisibleStub.returns(true);
     element.mutatedAttributesCallback({src: 'https://foo.com/bar?baz=1'});
     expect(ampState.fetchAndUpdate_).calledWithExactly(/* isInit */ false);
     return ampState.fetch_().then(() => {
