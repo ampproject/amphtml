59df7517691d69019c9a6807dccd674ba44127eb
Dima Voytenko <dvoytenko@google.com>
2018-07-17 18:57:23 -0700

    Shadow root viewport binding for iOS (#16642)
    * Shadow root viewport binding for iOS
    
    * lint
    
    * docs
    
    * Review fixes
    
    * lints
    
    * Fix scheduling
    
    * license text
    
    * Restrict only to Safari 11+
    
    * rename viewport.html
    
    * fix unknown styles

--- src/service/viewport/viewport-impl.js
+++ src/service/viewport/viewport-impl.js
@@ -20,6 +20,9 @@ import {Observable} from '../../observable';
 import {Services} from '../../services';
 import {ViewportBindingDef} from './viewport-binding-def';
 import {
+  ViewportBindingIosEmbedShadowRoot_,
+} from './viewport-binding-ios-embed-sd';
+import {
   ViewportBindingIosEmbedWrapper_,
 } from './viewport-binding-ios-embed-wrapper';
 import {ViewportBindingNatural_} from './viewport-binding-natural';
@@ -1127,7 +1130,14 @@ function createViewport(ampdoc) {
   let binding;
   if (ampdoc.isSingleDoc() &&
       getViewportType(ampdoc.win, viewer) == ViewportType.NATURAL_IOS_EMBED) {
-    binding = new ViewportBindingIosEmbedWrapper_(ampdoc.win);
+    if (isExperimentOn(ampdoc.win, 'ios-embed-sd') &&
+        ampdoc.win.Element.prototype.attachShadow &&
+        // Even though iOS 10 supports Shadow DOM, the support is buggy.
+        Services.platformFor(ampdoc.win).getMajorVersion() >= 11) {
+      binding = new ViewportBindingIosEmbedShadowRoot_(ampdoc.win);
+    } else {
+      binding = new ViewportBindingIosEmbedWrapper_(ampdoc.win);
+    }
   } else {
     binding = new ViewportBindingNatural_(ampdoc);
   }
