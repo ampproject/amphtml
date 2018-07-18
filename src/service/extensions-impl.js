481c7e14003ce89f4ec99553d66261cb62dc1336
Malte Ubl <cramforce@users.noreply.github.com>
2018-07-18 08:28:15 -0700

    Code-cleanup of splittable fork. (#16846)
    - Removes support for browser masking in NPM.
    - Simplify code based on lack of base module separate from main.
    - Consolidates file patching in update-packages gulp task.
    - Lots of other small stuff
    - As a follow up to #16837 switch document-register-element to not use a default export from CommonJS, because this confuses our build system across babel, browserify, closure, etc.

--- src/service/extensions-impl.js
+++ src/service/extensions-impl.js
@@ -32,6 +32,8 @@ import {
 import {cssText} from '../../build/css';
 import {dev, rethrowAsync} from '../log';
 import {getMode} from '../mode';
+import {installCustomElements} from
+  'document-register-element/build/document-register-element.patched';
 import {
   install as installDOMTokenListToggle,
 } from '../polyfills/domtokenlist-toggle';
@@ -42,8 +44,6 @@ import {installPixel} from '../../builtins/amp-pixel';
 import {installStylesForDoc, installStylesLegacy} from '../style-installer';
 import {map} from '../utils/object';
 import {toWin} from '../types';
-import installCustomElements from
-  'document-register-element/build/document-register-element.patched';
 
 const TAG = 'extensions';
 const UNKNOWN_EXTENSION = '_UNKNOWN_';
