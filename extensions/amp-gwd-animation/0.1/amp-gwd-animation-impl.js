51ccab43d17917b73d356cada6049adb2f750780
Daniel Rozenberg <me@danielrozenberg.com>
2018-07-19 14:33:14 -0700

    ♻️ Replace direct `hasOwnProperty` with `src/utils/object.js#hasOwn` [extensions/amp-[e-s]*] (#16927)

--- extensions/amp-gwd-animation/0.1/amp-gwd-animation-impl.js
+++ extensions/amp-gwd-animation/0.1/amp-gwd-animation-impl.js
@@ -15,6 +15,7 @@
  */
 import {createCustomEvent} from '../../../src/event-helper';
 import {escapeCssSelectorIdent} from '../../../src/dom';
+import {hasOwn} from '../../../src/utils/object';
 import {toArray} from '../../../src/types';
 import {user} from '../../../src/log';
 
@@ -100,7 +101,7 @@ const LOG_ID = 'GWD';
  */
 function getCounter(receiver, counterName) {
   if (receiver[GOTO_COUNTER_PROP] &&
-      receiver[GOTO_COUNTER_PROP].hasOwnProperty(counterName)) {
+      hasOwn(receiver[GOTO_COUNTER_PROP], counterName)) {
     return receiver[GOTO_COUNTER_PROP][counterName];
   }
   return 0;
@@ -118,7 +119,7 @@ function setCounter(receiver, counterName, counterValue) {
   if (!receiver[GOTO_COUNTER_PROP]) {
     receiver[GOTO_COUNTER_PROP] = {};
   }
-  if (!receiver[GOTO_COUNTER_PROP].hasOwnProperty(counterName)) {
+  if (!hasOwn(receiver[GOTO_COUNTER_PROP], counterName)) {
     receiver[GOTO_COUNTER_PROP][counterName] = 0;
   }
   receiver[GOTO_COUNTER_PROP][counterName] = counterValue;
