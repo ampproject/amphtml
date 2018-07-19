33fe9332ed286936cceb78c1a4395966ab6b3498
Raghu Simha <rsimha@ampproject.org>
2018-07-19 11:26:11 -0700

    🏗🐛 Patch `node_modules` during `gulp update-packages`  when contents change (#16872)

--- build-system/tasks/update-packages.js
+++ build-system/tasks/update-packages.js
@@ -24,6 +24,21 @@ const {exec, getStderr} = require('../exec');
 const yarnExecutable = 'npx yarn';
 
 /**
+ * Writes the given contents to the patched file if updated
+ * @param {string} patchedName Name of patched file
+ * @param {string} file Contents to write
+ */
+function writeIfUpdated(patchedName, file) {
+  if (!fs.existsSync(patchedName) ||
+      fs.readFileSync(patchedName) != file) {
+    fs.writeFileSync(patchedName, file);
+    if (!process.env.TRAVIS) {
+      log(colors.green('Patched'), colors.cyan(patchedName));
+    }
+  }
+}
+
+/**
  * Patches Web Animations API by wrapping its body into `install` function.
  * This gives us an option to call polyfill directly on the main window
  * or a friendly iframe.
@@ -32,9 +47,6 @@ function patchWebAnimations() {
   // Copies web-animations-js into a new file that has an export.
   const patchedName = 'node_modules/web-animations-js/' +
       'web-animations.install.js';
-  if (fs.existsSync(patchedName)) {
-    return;
-  }
   let file = fs.readFileSync(
       'node_modules/web-animations-js/' +
       'web-animations.min.js').toString();
@@ -49,10 +61,7 @@ function patchWebAnimations() {
       }) +
       '\n' +
       '}\n';
-  fs.writeFileSync(patchedName, file);
-  if (!process.env.TRAVIS) {
-    log(colors.green('Patched'), colors.cyan(patchedName));
-  }
+  writeIfUpdated(patchedName, file);
 }
 
 /**
@@ -68,32 +77,26 @@ function patchRegisterElement() {
   // Details https://github.com/google/closure-compiler/issues/1831
   const patchedName = 'node_modules/document-register-element' +
       '/build/document-register-element.patched.js';
-  if (!fs.existsSync(patchedName)) {
-    file = fs.readFileSync(
-        'node_modules/document-register-element/build/' +
-        'document-register-element.node.js').toString();
-    // Eliminate the immediate side effect.
-    if (!/installCustomElements\(global\);/.test(file)) {
-      throw new Error('Expected "installCustomElements(global);" ' +
-          'to appear in document-register-element');
-    }
-    file = file.replace('installCustomElements(global);', '');
-    // Closure Compiler does not generate a `default` property even though
-    // to interop CommonJS and ES6 modules. This is the same issue typescript
-    // ran into here https://github.com/Microsoft/TypeScript/issues/2719
-    if (!/module.exports = installCustomElements;/.test(file)) {
-      throw new Error('Expected "module.exports = installCustomElements;" ' +
-          'to appear in document-register-element');
-    }
-    file = file.replace('module.exports = installCustomElements;',
-        'exports.installCustomElements = installCustomElements;');
-    fs.writeFileSync(patchedName, file);
-    if (!process.env.TRAVIS) {
-      log(colors.green('Patched'), colors.cyan(patchedName));
-    }
+  file = fs.readFileSync(
+      'node_modules/document-register-element/build/' +
+      'document-register-element.node.js').toString();
+  // Eliminate the immediate side effect.
+  if (!/installCustomElements\(global\);/.test(file)) {
+    throw new Error('Expected "installCustomElements(global);" ' +
+        'to appear in document-register-element');
   }
+  file = file.replace('installCustomElements(global);', '');
+  // Closure Compiler does not generate a `default` property even though
+  // to interop CommonJS and ES6 modules. This is the same issue typescript
+  // ran into here https://github.com/Microsoft/TypeScript/issues/2719
+  if (!/module.exports = installCustomElements;/.test(file)) {
+    throw new Error('Expected "module.exports = installCustomElements;" ' +
+        'to appear in document-register-element');
+  }
+  file = file.replace('module.exports = installCustomElements;',
+      'exports.installCustomElements = installCustomElements;');
+  writeIfUpdated(patchedName, file);
 }
-
 
 /**
  * Installs custom lint rules in build-system/eslint-rules to node_modules.
@@ -129,7 +132,7 @@ function runYarnCheck() {
 
 /**
  * Installs custom lint rules, updates node_modules (for local dev), and patches
- * web-animations-js if necessary.
+ * web-animations-js and document-register-element if necessary.
  */
 function updatePackages() {
   installCustomEslintRules();
