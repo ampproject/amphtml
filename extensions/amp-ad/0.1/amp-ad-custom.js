37014c40471c28aa694980d92d25789807a2ae3d
Daniel Rozenberg <me@danielrozenberg.com>
2018-07-19 14:31:02 -0700

    ♻️ Replace direct `hasOwnProperty` with `src/utils/object.js#hasOwn` [extensions/amp-[a-b]*] (#16925)

--- extensions/amp-ad/0.1/amp-ad-custom.js
+++ extensions/amp-ad/0.1/amp-ad-custom.js
@@ -24,6 +24,7 @@ import {
   childElementByTag,
   removeChildren,
 } from '../../../src/dom';
+import {hasOwn} from '../../../src/utils/object';
 import {isLayoutSizeDefined} from '../../../src/layout';
 import {user} from '../../../src/log';
 
@@ -98,7 +99,7 @@ export class AmpAdCustom extends AMP.BaseElement {
       // We will get here when the data has been fetched from the server
       let templateData = data;
       if (this.slot_ !== null) {
-        templateData = data.hasOwnProperty(this.slot_) ? data[this.slot_] :
+        templateData = hasOwn(data, this.slot_) ? data[this.slot_] :
           null;
       }
 
