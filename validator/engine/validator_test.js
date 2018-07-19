40b3f5765ea3021f148329ab5a961bbcf0fc6acc
alin04 <38020234+alin04@users.noreply.github.com>
2018-07-19 14:40:42 -0700

    Validator rollup (#16937)
    * cl/205110971 Convert value_regex/value_regex_casei
    
    * cl/205124475 Rename allowed_protocol to protocol.
    
    * cl/205178052 Rename DescendantTagList.allowed_tags
    
    * cl/205267495 Add test to prevent [style] attribute
    
    * cl/205269927 Rename CssSpec.allowed_declarations t
    
    * cl/205274609 Revision bump for #16624
    
    * cl/205289326 Revision bump for #16899
    
    * Remove duplicated merge and fix lint errors.

--- validator/engine/validator_test.js
+++ validator/engine/validator_test.js
@@ -628,22 +628,25 @@ function attrRuleShouldMakeSense(attrSpec, rules) {
 
     expect(attrSpecNameRegex.test(attrSpec.name)).toBe(true);
   });
+  it('attr_spec name can not be [style]', () => {
+    expect(attrSpec.name).not.toEqual('[style]');
+  });
   if (attrSpec.valueUrl !== null) {
-    const allowedProtocolRegex = new RegExp('[a-z-]+');
-    // UrlSpec allowed_protocols are matched against lowercased protocol names
+    const protocolRegex = new RegExp('[a-z-]+');
+    // UrlSpec protocol is matched against lowercased protocol names
     // so the rules *must* also be lower case.
-    it('allowed_protocol must be lower case', () => {
-      for (const allowedProtocol of attrSpec.valueUrl.allowedProtocol) {
-        expect(allowedProtocolRegex.test(allowedProtocol)).toBe(true);
+    it('protocol must be lower case', () => {
+      for (const protocol of attrSpec.valueUrl.protocol) {
+        expect(protocolRegex.test(protocol)).toBe(true);
       }
     });
-    // If allowed_protocol is http then allow_relative should not be false
+    // If protocol is http then allow_relative should not be false
     // except for `data-` attributes.
     if (!attrSpec.name.startsWith('data-')) {
-      for (const allowedProtocol of attrSpec.valueUrl.allowedProtocol) {
-        if ((allowedProtocol === 'http') &&
+      for (const protocol of attrSpec.valueUrl.protocol) {
+        if ((protocol === 'http') &&
             (attrSpec.valueUrl.allowRelative !== null)) {
-          it('allow_relative can not be false if allowed_protocol is http: ' +
+          it('allow_relative can not be false if protocol is http: ' +
                  attrSpec.name,
           () => {
             expect(attrSpec.valueUrl.allowRelative).toEqual(true);
@@ -690,8 +693,8 @@ function attrRuleShouldMakeSense(attrSpec, rules) {
   }
   // value_url must have at least one allowed protocol.
   if (attrSpec.valueUrl !== null) {
-    it('value_url must have at least one allowed protocol', () => {
-      expect(attrSpec.valueUrl.allowedProtocol.length).toBeGreaterThan(0);
+    it('value_url must have at least one protocol', () => {
+      expect(attrSpec.valueUrl.protocol.length).toBeGreaterThan(0);
     });
   }
   // only has one of value set.
@@ -759,6 +762,32 @@ describe('ValidatorRulesMakeSense', () => {
   it('amp_layout_attrs defined', () => {
     expect(rules.ampLayoutAttrs.length).toBeGreaterThan(0);
   });
+
+  for (const list of rules.directAttrLists) {
+    for (const index of list) {
+      if (index < 0) {
+        it('attr_spec name can not be [style]', () => {
+          expect(rules.internedStrings[-1 - index]).not.toEqual('[style]');
+        });
+      } else {
+        it('attr_spec name can not be [style]', () => {
+          expect(rules.attrs[index].name).not.toEqual('[style]');
+        });
+      }
+    }
+  }
+
+  for (const index of rules.globalAttrs) {
+    it('attr_spec name can not be [style]', () => {
+      expect(rules.internedStrings[-1 - index]).not.toEqual('[style]');
+    });
+  }
+
+  for (const index of rules.ampLayoutAttrs) {
+    it('attr_spec name can not be [style]', () => {
+      expect(rules.internedStrings[-1 - index]).not.toEqual('[style]');
+    });
+  }
 
   it('min_validator_revision_required defined', () => {
     expect(rules.minValidatorRevisionRequired).toBeGreaterThan(0);
