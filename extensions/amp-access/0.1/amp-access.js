722678ed1102371d5091226993d82a0cc9f3e602
Dima Voytenko <dvoytenko@gmail.com>
2018-07-20 15:41:10 -0700

    revert

--- extensions/amp-access/0.1/amp-access.js
+++ extensions/amp-access/0.1/amp-access.js
@@ -44,7 +44,6 @@ const TEMPLATE_PROP = '__AMP_ACCESS__TEMPLATE';
 
 /**
  * AccessService implements the complete lifecycle of the AMP Access system.
- * QQQQ: amp-analytics is no longer required /REMOVE.
  * @implements {AccessVars}
  */
 export class AccessService {
