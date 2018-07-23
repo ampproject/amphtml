0d1108103640fb2fe34918c6fa7111a0e297410a
Peter Josling <peterjosling@gmail.com>
2018-07-23 11:49:51 +0100

    Add support for amp-next-page type="adsense"
    Fetches document recommendations from the AdSense Matched Content
    product.

--- extensions/amp-next-page/0.1/test/test-amp-next-page.js
+++ extensions/amp-next-page/0.1/test/test-amp-next-page.js
@@ -211,8 +211,8 @@ env => {
   describe('remote config', () => {
     it('errors when no config specified', () => {
       return nextPage.buildCallback().should.be.rejectedWith(
-          'amp-next-page should contain only one <script> child, or a URL '
-          + 'specified in [src]​​​');
+          'amp-next-page should contain a <script> child, a URL specified in '
+          + '[src], or a [type]');
     });
 
     it('fetches remote config when specified in src', function* () {
@@ -248,6 +248,206 @@ env => {
       expect(registerSpy.calledWith(element, config)).to.be.true;
     });
   });
+
+  describe('AdSense config', () => {
+    it('errors without client', () => {
+      const error = 'amp-next-page AdSense client should be of the format '
+          + '\'ca-pub-123456\'';
+      expectAsyncConsoleError(error);
+      element.setAttribute('type', 'adsense');
+      element.setAttribute('data-slot', '12345');
+      return nextPage.buildCallback().should.be.rejectedWith(error);
+    });
+
+    it('errors without slot', () => {
+      const error = 'amp-next-page AdSense slot should be a number';
+      expectAsyncConsoleError(error);
+      element.setAttribute('type', 'adsense');
+      element.setAttribute('data-client', 'ca-pub-12345');
+      return nextPage.buildCallback().should.be.rejectedWith(error);
+    });
+
+    it('errors for invalid client format', () => {
+      const error = 'amp-next-page AdSense client should be of the format '
+          + '\'ca-pub-123456\'';
+      expectAsyncConsoleError(error);
+      element.setAttribute('type', 'adsense');
+      element.setAttribute('data-client', 'doggos');
+      element.setAttribute('data-slot', '12345');
+      return nextPage.buildCallback().should.be.rejectedWith(error);
+    });
+
+    it('errors for invalid slot format', () => {
+      const error = 'amp-next-page AdSense slot should be a number';
+      expectAsyncConsoleError(error);
+      element.setAttribute('type', 'adsense');
+      element.setAttribute('data-client', 'ca-pub-12345');
+      element.setAttribute('data-slot', 'doggos');
+      return nextPage.buildCallback().should.be.rejectedWith(error);
+    });
+  });
+
+  describe('valid AdSense config', () => {
+    const client = 'ca-pub-12345';
+    const slot = '12345';
+    let url;
+
+    beforeEach(() => {
+      ampdoc.getUrl = () => 'https://example.com/parent';
+      element.setAttribute('type', 'adsense');
+      element.setAttribute('data-client', client);
+      element.setAttribute('data-slot', slot);
+
+      url = 'https://googleads.g.doubleclick.net/pagead/ads?'
+      + `client=${client}&slotname=${slot}`
+      + `&url=${encodeURIComponent(ampdoc.getUrl())}`
+      + '&ecr=1&crui=title&is_amp=3&output=xml';
+    });
+
+    it('fetches recommendations from AdSense', function* () {
+      fetchDocumentMock.expects('fetchDocument')
+          .withExactArgs(win, url, {credentials: 'include'})
+          .returns(Promise.resolve(createXmlDoc(`
+<GSP VER="3.2">
+  <ADS>
+    <AD n="1" type="text/narrow" 
+        url="https://googleads.g.doubleclick.net/aclk?adurl=https://example.com/article" 
+        visible_url="https://example.com/article">
+    <LINE1>
+      Page title
+    </LINE1>
+    <LINE2/>
+    <LINE3/>
+    <BIDTYPE>CPM</BIDTYPE>
+    <MEDIA_TEMPLATE_DATA>
+    [ { "core_image_url" : "https://example.com/image.png" } ];
+    </MEDIA_TEMPLATE_DATA>
+    </AD>
+  </ADS>
+</GSP>`)));
+
+      const config = {
+        pages: [
+          {
+            ampUrl: 'https://googleads.g.doubleclick.net/aclk?adurl=https://example.com/article',
+            title: 'Page title',
+            image: 'https://example.com/image.png',
+          },
+        ],
+      };
+
+      const nextPageService =
+          yield getServicePromiseForDoc(ampdoc, 'next-page');
+      const registerSpy = sandbox.spy(nextPageService, 'register');
+
+      yield nextPage.buildCallback();
+      yield macroTask();
+
+      expect(registerSpy.calledWith(element, config)).to.be.true;
+    });
+
+    it('makes an unpersonalized request if missing consent', function* () {
+      fetchDocumentMock.expects('fetchDocument')
+          .withExactArgs(win, url, {credentials: 'omit'})
+          .returns(Promise.resolve());
+      element.setAttribute('data-block-on-consent', true);
+      yield nextPage.buildCallback();
+    });
+
+    it('filters pages with visible_urls from different origins', function* () {
+      fetchDocumentMock.expects('fetchDocument')
+          .withExactArgs(win, url, {credentials: 'include'})
+          .returns(Promise.resolve(createXmlDoc(`
+<GSP VER="3.2">
+  <ADS>
+    <AD n="1" type="text/narrow" 
+        url="https://googleads.g.doubleclick.net/aclk?adurl=https://other.com/article" 
+        visible_url="https://other.com/article">
+    <LINE1>
+      Other 1
+    </LINE1>
+    <LINE2/>
+    <LINE3/>
+    <BIDTYPE>CPM</BIDTYPE>
+    <MEDIA_TEMPLATE_DATA>
+    [ { "core_image_url" : "https://other.com/image1.png" } ];
+    </MEDIA_TEMPLATE_DATA>
+    </AD>
+    <AD n="2" type="text/narrow" 
+        url="https://googleads.g.doubleclick.net/aclk?adurl=https://example.com/article" 
+        visible_url="https://example.com/article">
+    <LINE1>
+      Example 1
+    </LINE1>
+    <LINE2/>
+    <LINE3/>
+    <BIDTYPE>CPM</BIDTYPE>
+    <MEDIA_TEMPLATE_DATA>
+    [ { "core_image_url" : "https://example.com/image2.png" } ];
+    </MEDIA_TEMPLATE_DATA>
+    </AD>
+    <AD n="3" type="text/narrow" 
+        url="https://googleads.g.doubleclick.net/aclk?adurl=https://other2.com/article" 
+        visible_url="https://other2.com/article">
+    <LINE1>
+      Other 2
+    </LINE1>
+    <LINE2/>
+    <LINE3/>
+    <BIDTYPE>CPM</BIDTYPE>
+    <MEDIA_TEMPLATE_DATA>
+    [ { "core_image_url" : "https://other2.com/image3.png" } ];
+    </MEDIA_TEMPLATE_DATA>
+    </AD>
+  </ADS>
+</GSP>`)));
+
+      const config = {
+        pages: [
+          {
+            ampUrl: 'https://googleads.g.doubleclick.net/aclk?adurl=https://example.com/article',
+            title: 'Example 1',
+            image: 'https://example.com/image2.png',
+          },
+        ],
+      };
+
+      const nextPageService =
+          yield getServicePromiseForDoc(ampdoc, 'next-page');
+      const registerSpy = sandbox.spy(nextPageService, 'register');
+
+      yield nextPage.buildCallback();
+      yield macroTask();
+
+      expect(registerSpy.calledWith(element, config)).to.be.true;
+    });
+
+    it('falls back to inline config pages if the AdSense request fails',
+        function* () {
+          const config = {
+            pages: [
+              {
+                ampUrl: 'https://example.com/fallback',
+                title: 'Fallback article',
+                image: 'https://example.com/fallback.png',
+              },
+            ],
+          };
+          fetchDocumentMock.expects('fetchDocument').returns(Promise.reject());
+          element.innerHTML = `<script type="application/json">
+                                 ${JSON.stringify(config)}
+                               </script>`;
+
+          const nextPageService =
+              yield getServicePromiseForDoc(ampdoc, 'next-page');
+          const registerSpy = sandbox.spy(nextPageService, 'register');
+
+          yield nextPage.buildCallback();
+          yield macroTask();
+
+          expect(registerSpy.calledWith(element, config)).to.be.true;
+        });
+  });
 });
 
 /**
@@ -270,4 +470,14 @@ function createExampleDocument(doc) {
       <div style="height:1000px"></div>
       <footer>Footer</footer>`;
   return childDoc;
+}
+
+/**
+ * Creates a new XML document from the specified XML text.
+ * @param {string} text XML document text.
+ * @return {!Document} New XML document.
+ */
+function createXmlDoc(text) {
+  const parser = new DOMParser();
+  return parser.parseFromString(text, 'text/xml');
 }
