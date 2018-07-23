b6753d221149ff6b6c2c4dd56ebf6d08100a3d52
Carlos Vializ <cvializ@users.noreply.github.com>
2018-07-23 12:31:24 -0700

    Revert "Add verify-error state and template to amp-form" (#17015)
    * Revert "Allow validation-for, visible-when-invalid, and verify-xhr in mustache templates (#16994)"
    
    This reverts commit 5d0a3af53d9c449a023943cbe0f5575fa1b0820c.
    
    * Revert "Add verify-error state and template to amp-form (#16881)"
    
    This reverts commit 9c28ff83243c981b8f91e55e2943f179e6a0ffd4.

--- extensions/amp-form/0.1/form-events.js
+++ extensions/amp-form/0.1/form-events.js
@@ -16,12 +16,6 @@
 
 /** @enum {string} */
 export const FormEvents = {
-  INVALID: 'invalid',
-  SERVICE_INIT: 'amp:form-service:initialize', // Dispatched by the window when AmpFormService initializes.
-  SUBMIT_ERROR: 'submit-error',
-  SUBMIT_SUCCESS: 'submit-success',
-  SUBMIT: 'submit',
-  VALID: 'valid',
-  VERIFY_ERROR: 'verify-error',
-  VERIFY: 'verify',
+  // Dispatched by the window when AmpFormService initializes.
+  SERVICE_INIT: 'amp:form-service:initialize',
 };
