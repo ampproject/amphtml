87d1acb4a7c7dfc12b5c0a17b638f0292ac92318
Malte Ubl <cramforce@users.noreply.github.com>
2018-07-24 10:54:23 -0700

    Turn on "font-display-swap" experiment in prod. (#16744)

--- build-system/tasks/bundle-size.js
+++ build-system/tasks/bundle-size.js
@@ -22,7 +22,7 @@ const log = require('fancy-log');
 const {getStdout} = require('../exec');
 
 const runtimeFile = './dist/v0.js';
-const maxSize = '79.23KB';
+const maxSize = '79.24KB';
 
 const {green, red, cyan, yellow} = colors;
 
