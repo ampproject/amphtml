d44fda1f4534414381bbb795dddc86b5534f8532
Nicolas AUGER <nicolas.auger@ynov.com>
2018-07-25 12:18:42 +0200

    tests: poool access/paywall tests

--- extensions/amp-access-poool/0.1/test/test-amp-access-poool.js
+++ extensions/amp-access-poool/0.1/test/test-amp-access-poool.js
@@ -13,3 +13,133 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {PooolVendor} from '../poool-impl';
+
+describes.fakeWin('PooolVendor', {
+  amp: true,
+  location: 'https://pub.com/doc1',
+}, env => {
+  let win, document, ampdoc;
+  let accessSource;
+  let accessService;
+  let accessSourceMock;
+  let xhrMock;
+  let pooolConfig;
+  let vendor;
+
+  beforeEach(() => {
+    win = env.win;
+    ampdoc = env.ampdoc;
+    document = win.document;
+
+    pooolConfig = {
+      bundleID: 'Q9X1R-27SFS-MYC31-MCYQ1',
+      pageType: 'premium',
+      itemID: 'amp-test-article',
+    };
+
+    accessSource = {
+      getAdapterConfig: () => { return pooolConfig; },
+      buildUrl: () => {},
+      getReaderId_: () => {},
+      getLoginUrl: () => {},
+    };
+
+    accessService = {
+      ampdoc,
+      getSource: () => accessSource,
+    };
+
+    accessSourceMock = sandbox.mock(accessSource);
+
+    vendor = new PooolVendor(accessService, accessSource);
+    xhrMock = sandbox.mock(vendor.xhr_);
+  });
+
+  afterEach(() => {
+    accessSourceMock.verify();
+    xhrMock.verify();
+  });
+
+  describe('authorize', () => {
+    beforeEach(() => {
+      sandbox.stub(vendor, 'renderPoool_');
+    });
+
+    it('successful authorization', () => {
+      vendor.accessUrl_ = 'https://baseurl?param';
+      accessSourceMock.expects('buildUrl')
+          .withExactArgs('https://baseurl?param&iid=amp-test-article', false)
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+      accessSourceMock.expects('getLoginUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+      xhrMock.expects('fetchJson')
+          .returns(Promise.resolve({
+            json() {
+              return Promise.resolve({access: true});
+            },
+          }))
+          .once();
+      return vendor.authorize().then(resp => {
+        expect(resp.access).to.be.true;
+      });
+    });
+
+    it('authorization fails because of wrong or missing server config', () => {
+      accessSourceMock.expects('buildUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+      accessSourceMock.expects('getLoginUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+      xhrMock.expects('fetchJson')
+          .returns(Promise.resolve({
+            json() {
+              return Promise.resolve({access: true});
+            },
+          }))
+          .once();
+      return vendor.authorize().catch(err => {
+        expect(err.message).to.exist;
+      });
+    });
+
+    it('authorization response fails - 402 error', () => {
+      accessSourceMock.expects('buildUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+      xhrMock.expects('fetchJson')
+          .returns(Promise.reject({
+            response: {
+              status: 402,
+            },
+          }))
+          .once();
+      return vendor.authorize().then(err => {
+        expect(err.access).to.be.false;
+      });
+    });
+  });
+
+
+  describe('create poool paywall', () => {
+    let container;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      container.id = 'poool-widget';
+      document.body.appendChild(container);
+      vendor.renderPoool_();
+    });
+
+    afterEach(() => {
+      container.parentNode.removeChild(container);
+    });
+
+    it('renders poool paywall', () => {
+      expect(container.firstChild).to.not.be.undefined;
+    });
+  });
+});
