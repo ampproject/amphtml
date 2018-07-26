2f08bc8a0dac8a8338b7dcfc4ad7bbc481275f8b
sergeydaub <sergeydaub@gmail.com>
2018-07-26 11:12:30 +0300

    -- one more attempt while build is green 2

--- extensions/amp-viqeo-player/0.1/test/test-amp-viqeo-player.js
+++ extensions/amp-viqeo-player/0.1/test/test-amp-viqeo-player.js
@@ -24,7 +24,7 @@ describes.realWin('amp-viqeo-player', {
   },
   allowExternalResources: true,
 }, function(env) {
-  this.timeout(3000);
+  this.timeout(5000);
   let win, doc;
 
   beforeEach(() => {
