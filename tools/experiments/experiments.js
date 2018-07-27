94ad0e01392ae74bf908d18766e2eeb9280c1366
Naina Raisinghani <nainar@google.com>
2018-07-27 13:14:43 -0700

    remove experiments (#17156)

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -304,12 +304,6 @@ const EXPERIMENTS = [
     cleanupIssue: 'TODO',
   },
   {
-    id: 'amp-orientation-observer',
-    name: 'Enables actions based on the orientation of a mobile device',
-    spec: 'https://github.com/ampproject/amphtml/issues/14740',
-    cleanupissue: 'https://github.com/ampproject/amphtml/issues/16075',
-  },
-  {
     id: 'blurry-placeholder',
     name: 'Enables a blurred image placeholder as an amp-img loads',
     spec: 'https://github.com/ampproject/amphtml/issues/15146',
