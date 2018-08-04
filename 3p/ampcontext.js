9318d29c1f6a80f2ef14e0bc3b6850ca745c1f51
Hongfei Ding <lannka@users.noreply.github.com>
2018-08-04 12:13:21 -0700

    Introduce experiment flag to disable initialIntersection callback (#17271)
    * Introduce experiment flag to disable initialIntersection callback
    
    * Use 3p isExperimentOn
    
    * fix test
    
    * Fix test-integration.js
    
    * skip test
    
    * Fix test-3p-frame

--- 3p/ampcontext.js
+++ 3p/ampcontext.js
@@ -18,8 +18,8 @@ import {IframeMessagingClient} from './iframe-messaging-client';
 import {MessageType} from '../src/3p-frame-messaging';
 import {dev} from '../src/log';
 import {dict} from '../src/utils/object';
+import {isExperimentOn, nextTick} from './3p';
 import {isObject} from '../src/types';
-import {nextTick} from './3p';
 import {parseUrlDeprecated} from '../src/url';
 import {tryParseJson} from '../src/json';
 
@@ -172,13 +172,15 @@ export class AbstractAmpContext {
           callback(intersection.changes);
         });
 
-    // Call the callback with the value that was transmitted when the
-    // iframe was drawn. Called in nextTick, so that callers don't
-    // have to specially handle the sync case.
-    // TODO(lannka, #8562): Deprecate this behavior
-    nextTick(this.win_, () => {
-      callback([this.initialIntersection]);
-    });
+    if (!isExperimentOn('no-initial-intersection')) { // eslint-disable-line
+      // Call the callback with the value that was transmitted when the
+      // iframe was drawn. Called in nextTick, so that callers don't
+      // have to specially handle the sync case.
+      // TODO(lannka, #8562): Deprecate this behavior
+      nextTick(this.win_, () => {
+        callback([this.initialIntersection]);
+      });
+    }
 
     return unlisten;
   }
