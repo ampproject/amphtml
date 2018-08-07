6c886b2d1ae2b8aa4809bf1e708ea9ae060b7869
Prateek Bhatnagar <prateek89born@gmail.com>
2018-08-07 12:59:41 -0700

    falling back to fetchResponse

--- src/document-fetcher.js
+++ src/document-fetcher.js
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {FetchResponse, assertSuccess, getViewerInterceptResponse, setupAMPCors, setupInit, setupInput, verifyAmpCORSHeaders} from './utils/xhr-utils';
 import {Services} from './services';
-import {assertSuccess, getViewerInterceptResponse, setupAMPCors, setupInit, setupInput, verifyAmpCORSHeaders} from './utils/xhr-utils';
-import {dict} from './utils/object';
 import {user} from './log';
 
 /**
@@ -82,14 +81,7 @@ function xhrRequest(input, init) {
       // whole document loading to complete. This is fine for the use cases
       // we have now, but may need to be reimplemented later.
       if (xhr.readyState == /* COMPLETE */ 4) {
-        const options = {
-          status: xhr.status,
-          statusText: xhr.statusText,
-          headers: parseHeaders(xhr.getAllResponseHeaders()),
-        };
-        const body = 'response' in xhr
-          ? xhr.response : xhr.responseText;
-        const response = new Response(/** @type {string} */ (body || ''), /** @type {!ResponseInit} */ (options));
+        const response = new FetchResponse(xhr);
         const promise = assertSuccess(response)
             .then(response => ({response, xhr}));
         resolve(promise);
@@ -107,26 +99,4 @@ function xhrRequest(input, init) {
       xhr.send();
     }
   });
-}
-
-/**
- *
- * @param {string} rawHeaders
- * @return {JsonObject}
- */
-function parseHeaders(rawHeaders) {
-  const headers = dict({});
-  // Replace instances of \r\n and \n followed by at least one
-  // space or horizontal tab with a space
-  // https://tools.ietf.org/html/rfc7230#section-3.2
-  const preProcessedHeaders = rawHeaders.replace(/\r?\n[\t ]+/g, ' ');
-  preProcessedHeaders.split(/\r?\n/).forEach(function(line) {
-    const parts = line.split(':');
-    const key = parts.shift().trim();
-    if (key) {
-      const value = parts.join(':').trim();
-      headers[key] = value;
-    }
-  });
-  return headers;
 }
