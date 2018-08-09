075d11c13b5ba81213c019e37eda8d8c09ccbf86
Cathy Zhu <cathyzhu@google.com>
2018-08-09 10:33:27 -0700

    Add autosuggest test and experiment

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -338,6 +338,16 @@ const EXPERIMENTS = [
     name: 'Enables scroll snap on carousel across all browsers/OSes',
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/16508',
   },
+  {
+    id: 'amp-carousel-new-arrows',
+    name: 'Enables new AMP carousel navigation arrows',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/17510',
+  },
+  {
+    id: 'amp-list-resize',
+    name: 'Enables container-based resizing on amp list',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/16508',
+  },
 ];
 
 if (getMode().localDev) {
