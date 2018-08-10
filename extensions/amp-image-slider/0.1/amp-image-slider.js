4173a22337633bf16018a42743125a65077932f1
Kevin (Kun) 'Kassimo' Qian <kevinkassimo@gmail.com>
2018-08-10 14:46:12 -0700

    Rename initial-percent to initial-slider-position

--- extensions/amp-image-slider/0.1/amp-image-slider.js
+++ extensions/amp-image-slider/0.1/amp-image-slider.js
@@ -163,7 +163,8 @@ export class AmpImageSlider extends AMP.BaseElement {
       }
     }, ActionTrust.LOW);
 
-    const initialPercentString = this.element.getAttribute('initial-percent');
+    const initialPercentString =
+        this.element.getAttribute('initial-slider-position');
     // TODO(kqian): move this before building child components on issue
     // This is the only step when content tree is attached to document
     return this.mutateElement(() => {
