c489d2f567dc1e6d2c1745de2923ffa670e1afa1
Cathy Zhu <cathyxz@gmail.com>
2018-08-13 15:54:49 -0700

    Add controls option to <amp-pan-zoom> (#17344)
    * Add default zoom buttons
    
    * Fix buttons
    
    * Add controls attribute to validator
    
    * Remove unnecessary mutateElement
    
    * Make CSS classes public
    
    * Add documentation for public CSS classes
    
    * Update validator tests

--- extensions/amp-pan-zoom/0.1/amp-pan-zoom.js
+++ extensions/amp-pan-zoom/0.1/amp-pan-zoom.js
@@ -141,6 +141,9 @@ export class AmpPanZoom extends AMP.BaseElement {
 
     /** @private */
     this.resetOnResize_ = false;
+
+    /** @private {?Element} */
+    this.zoomButton_ = null;
   }
 
   /** @override */
@@ -168,12 +171,24 @@ export class AmpPanZoom extends AMP.BaseElement {
         return;
       }
       const scale = args['scale'] || 1;
-      this.updatePanZoomBounds_(scale);
-      const x = this.boundX_(args['x'] || 0, /*allowExtent*/ false);
-      const y = this.boundY_(args['y'] || 0, /*allowExtent*/ false);
-      return this.set_(scale, x, y, /*animate*/ true)
-          .then(() => this.onZoomRelease_());
+      const x = args['x'] || 0;
+      const y = args['y'] || 0;
+      return this.transform(x, y, scale);
     });
+  }
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} scale
+   */
+  transform(x, y, scale) {
+    this.updatePanZoomBounds_(scale);
+    const boundX = this.boundX_(x, /*allowExtent*/ false);
+    const boundY = this.boundY_(y, /*allowExtent*/ false);
+    return this.set_(scale, boundX, boundY, /*animate*/ true)
+        .then(() => this.onZoomRelease_());
   }
 
   /** @override */
@@ -185,6 +200,9 @@ export class AmpPanZoom extends AMP.BaseElement {
 
   /** @override */
   layoutCallback() {
+    if (this.element.hasAttribute('controls')) {
+      this.createZoomButton_();
+    }
     return this.resetContentDimensions_().then(this.setupGestures_());
   }
 
@@ -224,6 +242,27 @@ export class AmpPanZoom extends AMP.BaseElement {
    */
   elementIsSupported_(element) {
     return ELIGIBLE_TAGS[element.tagName];
+  }
+
+  /**
+   * Creates zoom buttoms
+   */
+  createZoomButton_() {
+    this.zoomButton_ = this.element.ownerDocument.createElement('div');
+    this.zoomButton_.classList.add('amp-pan-zoom-in-icon');
+    this.zoomButton_.classList.add('amp-pan-zoom-button');
+    this.zoomButton_.addEventListener('click', () => {
+      if (this.zoomButton_.classList.contains('amp-pan-zoom-in-icon')) {
+        this.transform(0, 0, this.maxScale_);
+        this.zoomButton_.classList.remove('amp-pan-zoom-in-icon');
+        this.zoomButton_.classList.add('amp-pan-zoom-out-icon');
+      } else {
+        this.transform(0, 0, this.minScale_);
+        this.zoomButton_.classList.add('amp-pan-zoom-in-icon');
+        this.zoomButton_.classList.remove('amp-pan-zoom-out-icon');
+      }
+    });
+    this.element.appendChild(this.zoomButton_);
   }
 
   /**
@@ -640,8 +679,12 @@ export class AmpPanZoom extends AMP.BaseElement {
       // After the scale is updated, also register or unregister panning
       if (this.scale_ <= 1) {
         this.unregisterPanningGesture_();
+        this.zoomButton_.classList.add('amp-pan-zoom-in-icon');
+        this.zoomButton_.classList.remove('amp-pan-zoom-out-icon');
       } else {
         this.registerPanningGesture_();
+        this.zoomButton_.classList.remove('amp-pan-zoom-in-icon');
+        this.zoomButton_.classList.add('amp-pan-zoom-out-icon');
       }
     });
   }
