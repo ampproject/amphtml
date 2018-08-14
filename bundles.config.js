89e60d8e9cec7626d5dc113e81d770a36a501f68
Sepand Parhami <sparhami@google.com>
2018-08-14 14:23:49 -0700

    Use `setImportantStyles` instead of CSS. (#17473)

--- bundles.config.js
+++ bundles.config.js
@@ -47,12 +47,7 @@ exports.extensionBundles = [
     version: '0.1', options: {hasCss: true},
     type: TYPES.MISC,
   },
-  {
-    name: 'amp-accordion',
-    version: '0.1',
-    options: {hasCss: true},
-    type: TYPES.MISC,
-  },
+  {name: 'amp-accordion',version: '0.1',type: TYPES.MISC},
   {name: 'amp-ad', version: '0.1', options: {hasCss: true}, type: TYPES.AD},
   {name: 'amp-ad-network-adsense-impl', version: '0.1', type: TYPES.AD},
   {name: 'amp-ad-network-adzerk-impl', version: '0.1', type: TYPES.AD},
