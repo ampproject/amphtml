344933c7f501e0da50b981f0aedcc06f9eb076ee
DErekXx <derekgoha@gmail.com>
2018-08-16 16:34:47 +0300

    config for runative ad

--- ads/_config.js
+++ ads/_config.js
@@ -779,6 +779,11 @@ export const adConfig = {
 
   'rubicon': {},
 
+  'runative': {
+    prefetch: 'https://cdn.run-syndicate.com/sdk/v1/n.js',
+    renderStartImplemented: true,
+  },
+
   'sekindo': {
     renderStartImplemented: true,
   },
