6135481cf914aa25597fad24308f513a51ba7877
Prateek Bhatnagar <prateek89born@gmail.com>
2018-08-20 08:28:57 -0700

    adding copyright

--- test/functional/test-xhr-fetch-polyfill.js
+++ test/functional/test-xhr-fetch-polyfill.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import {FormDataWrapper} from '../../src/form-data-wrapper';
 import {Response, fetchPolyfill} from '../../src/polyfills/fetch';
 
@@ -131,7 +148,6 @@ describes.sandboxed('fetch', {}, () => {
         body: JSON.stringify(bodyData),
       });
       return xhrCreated.then(xhr => {
-        console.log(xhr);
         expect(xhr.requestBody).to.be.equal(JSON.stringify(bodyData));
       });
     });
@@ -141,7 +157,6 @@ describes.sandboxed('fetch', {}, () => {
         credentials: 'include',
       });
       return xhrCreated.then(xhr => {
-        console.log(xhr);
         expect(xhr.withCredentials).to.be.equal(true);
       });
     });
