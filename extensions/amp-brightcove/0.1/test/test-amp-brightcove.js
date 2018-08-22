012b844ac66610a88f58bae1375de5232fd86bcb
mister-ben <git@misterben.me>
2018-08-22 20:06:49 +0100

    lint

--- extensions/amp-brightcove/0.1/test/test-amp-brightcove.js
+++ extensions/amp-brightcove/0.1/test/test-amp-brightcove.js
@@ -15,9 +15,9 @@
  */
 
 import '../amp-brightcove';
-import {parseUrlDeprecated} from '../../../../src/url';
 import {VideoEvents} from '../../../../src/video-interface';
 import {listenOncePromise} from '../../../../src/event-helper';
+import {parseUrlDeprecated} from '../../../../src/url';
 
 
 describes.realWin('amp-brightcove', {
@@ -45,7 +45,7 @@ describes.realWin('amp-brightcove', {
     doc.body.appendChild(bc);
     return bc.build().then(() => bc.layoutCallback()).then(() => bc);
   }
-  
+
   function fakePostMessage(bc, info) {
     bc.implementation_.handlePlayerMessage_({
       origin: 'https://players.brightcove.net',
@@ -63,8 +63,8 @@ describes.realWin('amp-brightcove', {
       expect(iframe).to.not.be.null;
       expect(iframe.tagName).to.equal('IFRAME');
       expect(iframe.src).to.equal(
-          'https://players.brightcove.net/1290862519001' +
-          '/default_default/index.html?videoId=ref:amp-test-video&playsinline=true');
+          'https://players.brightcove.net/1290862519001/default_default' +
+          '/index.html?videoId=ref:amp-test-video&playsinline=true');
     });
   });
 
@@ -82,7 +82,7 @@ describes.realWin('amp-brightcove', {
   it('requires data-account', () => {
     expectAsyncConsoleError(/The data-account attribute is required for/, 1);
     return getBrightcove({}).should.eventually.be.rejectedWith(
-      /The data-account attribute is required for/);
+        /The data-account attribute is required for/);
   });
 
   it('removes iframe after unlayoutCallback', () => {
@@ -118,8 +118,9 @@ describes.realWin('amp-brightcove', {
     }).then(bc => {
       const iframe = bc.querySelector('iframe');
 
-      expect(iframe.src).to.equal('https://players.brightcove.net/1290862519001' +
-          '/default_default/index.html?videoId=ref:amp-test-video&playsinline=true');
+      expect(iframe.src).to.equal(
+          'https://players.brightcove.net/1290862519001/default_default' +
+          '/index.html?videoId=ref:amp-test-video&playsinline=true');
 
       bc.setAttribute('data-account', '12345');
       bc.setAttribute('data-video-id', 'abcdef');
@@ -132,53 +133,58 @@ describes.realWin('amp-brightcove', {
           '12345/default_default/index.html?videoId=abcdef&playsinline=true');
     });
   });
-  
+
   it('should forward events', () => {
     return getBrightcove({
       'data-account': '1290862519001',
       'data-video-id': 'ref:amp-test-video',
     }).then(bc => {
       return Promise.resolve()
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.LOAD);
-          fakePostMessage(bc, {event: 'ready', muted: false, playing: false});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.AD_START);
-          fakePostMessage(bc, {event: 'ads-ad-started', muted: false, playing: false});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.AD_END);
-          fakePostMessage(bc, {event: 'ads-ad-ended', muted: false, playing: false});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.PLAYING);
-          fakePostMessage(bc, {event: 'playing', muted: false, playing: true});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.MUTED);
-          fakePostMessage(bc, {event: 'volumechange', muted: true, playing: true});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.UNMUTED);
-          fakePostMessage(bc, {event: 'volumechange', muted: false, playing: true});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.PAUSE);
-          fakePostMessage(bc, {event: 'pause', muted: false, playing: false});
-          return p;
-        })
-        .then(() =>{
-          const p = listenOncePromise(bc, VideoEvents.ENDED);
-          fakePostMessage(bc, {event: 'ended', muted: false, playing: false});
-          return p;
-        });
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.LOAD);
+            fakePostMessage(bc, {event: 'ready', muted: false, playing: false});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.AD_START);
+            fakePostMessage(bc,
+                {event: 'ads-ad-started', muted: false, playing: false});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.AD_END);
+            fakePostMessage(bc,
+                {event: 'ads-ad-ended', muted: false, playing: false});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.PLAYING);
+            fakePostMessage(bc,
+                {event: 'playing', muted: false, playing: true});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.MUTED);
+            fakePostMessage(bc,
+                {event: 'volumechange', muted: true, playing: true});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.UNMUTED);
+            fakePostMessage(bc,
+                {event: 'volumechange', muted: false, playing: true});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.PAUSE);
+            fakePostMessage(bc, {event: 'pause', muted: false, playing: false});
+            return p;
+          })
+          .then(() => {
+            const p = listenOncePromise(bc, VideoEvents.ENDED);
+            fakePostMessage(bc, {event: 'ended', muted: false, playing: false});
+            return p;
+          });
     });
   });
 });
