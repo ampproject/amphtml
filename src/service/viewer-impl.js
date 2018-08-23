972267a8523d12e72e90c5e7deef39a99a62e689
lannka <hungfei.ding@gmail.com>
2018-08-23 14:19:07 -0700

    Fix @deprecated errors

--- src/service/viewer-impl.js
+++ src/service/viewer-impl.js
@@ -1110,7 +1110,7 @@ export class Viewer {
   /**
    * Resolves when there is a messaging channel established with the viewer.
    * Will be null if no messaging is needed like in an non-embedded document.
-   * @deprecated do not use. sendMessage and sendMessageAwaitResponse already
+   * Deprecated: do not use. sendMessage and sendMessageAwaitResponse already
    *             wait for messaging channel ready.
    * @return {?Promise}
    */
