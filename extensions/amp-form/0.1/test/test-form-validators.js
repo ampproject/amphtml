545bc33acf86226e1ed824b91d14bf14bc9bab44
William Chou <willchou@google.com>
2018-08-27 14:28:01 -0400

    Revert test name.

--- extensions/amp-form/0.1/test/test-form-validators.js
+++ extensions/amp-form/0.1/test/test-form-validators.js
@@ -527,7 +527,7 @@ describes.realWin('form-validators', {amp: true}, env => {
       expect(validations[2].className).to.not.contain('visible');
     });
 
-    it('should report on interaction for active inputs on submit', () => {
+    it('should report on interaction for non-active inputs on submit', () => {
       validator.report();
 
       expect(doc.activeElement).to.equal(form.elements[0]);
