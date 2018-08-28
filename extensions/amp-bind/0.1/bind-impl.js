835457ac8239649ee00bfd117f5d297d6df11bbf
William Chou <willchou@google.com>
2018-08-28 14:42:16 -0400

    amp-bind: More console debugging tools (#16830)
    * Add amp-bind debugging: elements, eval.
    
    * Aaron's comments.
    
    * Revert early-out for eval.

--- extensions/amp-bind/0.1/bind-impl.js
+++ extensions/amp-bind/0.1/bind-impl.js
@@ -25,7 +25,7 @@ import {debounce} from '../../../src/utils/rate-limit';
 import {deepEquals, parseJson} from '../../../src/json';
 import {deepMerge, dict} from '../../../src/utils/object';
 import {dev, user} from '../../../src/log';
-import {filterSplice} from '../../../src/utils/array';
+import {filterSplice, findIndex} from '../../../src/utils/array';
 import {getDetail} from '../../../src/event-helper';
 import {getMode} from '../../../src/mode';
 import {getValueForExpr} from '../../../src/json';
@@ -191,10 +191,11 @@ export class Bind {
     /** @private @const {!../../../src/utils/signals.Signals} */
     this.signals_ = new Signals();
 
-    // AMP.printState() for debugging in the console.
-    if (!self.AMP.printState) {
-      self.AMP.printState = this.printState_.bind(this);
-    }
+    // Install debug tools.
+    const g = self.AMP;
+    g.printState = g.printState || this.debugPrintState_.bind(this);
+    g.setState = g.setState || (state => this.setState(state));
+    g.eval = g.eval || this.debugEvaluate_.bind(this);
   }
 
   /** @override */
@@ -1349,21 +1350,65 @@ export class Bind {
 
   /**
    * Print out the current state in the console.
-   * @param {string=} opt_expression
+   * @param {(!Element|string)=} opt_elementOrExpr
    * @private
    */
-  printState_(opt_expression) {
-    if (opt_expression) {
-      if (typeof opt_expression !== 'string') {
-        user().error(TAG, 'Invalid JSON expression. Example: ' +
-            'AMP.printState("foo.bar") to print current value of "foo.bar".');
-        return;
+  debugPrintState_(opt_elementOrExpr) {
+    if (opt_elementOrExpr) {
+      if (typeof opt_elementOrExpr == 'string') {
+        const value = getValueForExpr(this.state_, opt_elementOrExpr);
+        user().info(TAG, value);
+      } else if (opt_elementOrExpr.nodeType == Node.ELEMENT_NODE) {
+        const element = user().assertElement(opt_elementOrExpr);
+        this.debugPrintElement_(element);
+      } else {
+        user().info(TAG, 'Invalid argument. Pass a JSON expression or an ' +
+            'element instead e.g. AMP.printState("foo.bar") or ' +
+            'AMP.printState($0) after selecting an element.');
       }
-      const value = getValueForExpr(this.state_, opt_expression);
-      user().info(TAG, value);
     } else {
       user().info(TAG, this.state_);
     }
+  }
+
+  /**
+   * Print out the element's bound attributes and respective expression values.
+   * @param {!Element} element
+   * @private
+   */
+  debugPrintElement_(element) {
+    const index = findIndex(this.boundElements_, boundElement => {
+      return boundElement.element == element;
+    });
+    if (index < 0) {
+      user().info(TAG, 'Element has no bindings:', element);
+      return;
+    }
+    // Evaluate expressions in bindings in `element`.
+    const promises = [];
+    const {boundProperties} = this.boundElements_[index];
+    boundProperties.forEach(boundProperty => {
+      const {expressionString} = boundProperty;
+      promises.push(this.evaluateExpression_(expressionString, this.state_));
+    });
+    // Print the map of attribute to expression value for `element`.
+    Promise.all(promises).then(results => {
+      const output = map();
+      boundProperties.forEach((boundProperty, i) => {
+        const {property} = boundProperty;
+        output[property] = results[i];
+      });
+      user().info(TAG, output);
+    });
+  }
+
+  /**
+   * @param {string} expression
+   */
+  debugEvaluate_(expression) {
+    this.evaluateExpression_(expression, this.state_).then(result => {
+      user().info(TAG, result);
+    });
   }
 
   /**
