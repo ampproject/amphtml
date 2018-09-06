70695fb1a0aa296c82c9c25b80143a26ffebe5f9
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-06 14:56:21 -0700

    Remove remaining experimental marks from amp-image-slider (#17912)

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -303,12 +303,6 @@ const EXPERIMENTS = [
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/15158',
   },
   {
-    id: 'amp-image-slider',
-    name: 'Enables the experimental amp-image-slider for image comparison',
-    spec: 'TODO',
-    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/17165',
-  },
-  {
     id: 'blurry-placeholder',
     name: 'Enables a blurred image placeholder as an amp-img loads',
     spec: 'https://github.com/ampproject/amphtml/issues/15146',
