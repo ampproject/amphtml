35f09a6f14321792fd0152e52f285fa84b76322f
Aaron Turner <aaron@aaronthedev.com>
2018-09-07 15:43:47 -0700

    ✨<amp-recaptcha-input> Created the Initial Example & Service Skeleton (#17867)
    * Created the AMP Recaptcha service skeleton
    
    * Started to get some test passing
    
    * Got a basic example running
    
    * Got layout callback to be called
    
    * Made test as minimal as possible
    
    * Added experiment, and started some clean up for a PR
    
    * Removed all dangling amp-gist references
    
    * Got Everything linted and typed. Need to write tests
    
    * Wrote tests for the recaptcha service registration flow
    
    * Moved css out of amp, and moved into apply styles in build callback
    
    * Enforced layout nodisplay, and finished css fixes
    
    * Moved experiment check to build callback
    
    * Fixed all tests and implmeneted reaming PR Changes
    
    * Added missing hasCss
    
    * Forgot I removed hasCSS for a reason since we dont have it
    
    * Changed to registeredElementsCount
    
    * Attached iframe to body, and additional cleanup
    
    * Fixed tests, and all submit errors
    
    * More PR Changes
    
    * Bumped Bundle Size for Rounding errors, suggested by @jridgewell
    
    * Bumped Bundle Size after Pulling Master for Rounding errors
    
    Suggested by @jridgewell

--- extensions/amp-recaptcha-input/0.1/amp-recaptcha-input.js
+++ extensions/amp-recaptcha-input/0.1/amp-recaptcha-input.js
@@ -13,3 +13,86 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * @fileoverview Async Input Element that uses the
+ * amp-recaptcha-service to dispatch actions, and return
+ * recaptcha tokens
+ */
+
+import {CSS} from '../../../build/amp-recaptcha-input-0.1.css';
+import {Layout} from '../../../src/layout';
+import {
+  installRecaptchaService,
+  recaptchaServiceFor,
+} from './amp-recaptcha-service';
+import {isExperimentOn} from '../../../src/experiments';
+import {setStyles, toggle} from '../../../src/style';
+
+/** @const */
+const TAG = 'amp-recaptcha-input';
+
+export class AmpRecaptchaInput extends AMP.BaseElement {
+
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    super(element);
+
+    /** @private {!./amp-recaptcha-service.AmpRecaptchaService} */
+    this.recaptchaService_ = recaptchaServiceFor(this.win);
+
+    /** @private {boolean} */
+    this.isExperimentEnabled_ = isExperimentOn(this.win, 'amp-recaptcha-input');
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    return layout == Layout.NODISPLAY;
+  }
+
+  /** @override */
+  buildCallback() {
+    if (!this.isExperimentEnabled_) {
+      return;
+    }
+
+    return this.mutateElement(() => {
+      toggle(this.element);
+      /**
+       * We are applying styles here, to minizime the amp.css file.
+       * These styles will create an in-place element, that is 1x1,
+       * but invisible. Absolute positioning keeps it where it would have
+       * been, without taking up space. Thus, layoutCallback will still
+       * be called at the appropriate time
+       */
+      setStyles(this.element, {
+        'position': 'absolute',
+        'width': '1px',
+        'height': '1px',
+        'overflow': 'hidden',
+        'visibility': 'hidden',
+      });
+    });
+  }
+
+  /** @override */
+  layoutCallback() {
+    /**
+     * TODO(torch2424): Fix possible race condition,
+     * of this getting called twice.
+     * See <amp-analytics> layoutCallback()
+     */
+    return this.recaptchaService_.register(this.element);
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    this.recaptchaService_.unregister();
+    return true;
+  }
+}
+
+AMP.extension(TAG, '0.1', AMP => {
+  installRecaptchaService(AMP.win);
+  AMP.registerElement(TAG, AmpRecaptchaInput, CSS);
+});
