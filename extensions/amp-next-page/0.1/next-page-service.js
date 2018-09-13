c11152cc566baae9a3103b6d828b735be5562529
Peter Josling <peterjosling@users.noreply.github.com>
2018-09-13 18:46:08 +0100

    🐛 amp-next-page: Restore functionality to drop amp-analytics configs from child pages (#18026)

--- extensions/amp-next-page/0.1/next-page-service.js
+++ extensions/amp-next-page/0.1/next-page-service.js
@@ -26,6 +26,7 @@ import {
 } from '../../../src/service/position-observer/position-observer-impl';
 import {installStylesForDoc} from '../../../src/style-installer';
 import {layoutRectLtwh} from '../../../src/layout-rect';
+import {removeElement} from '../../../src/dom';
 import {setStyle, toggle} from '../../../src/style';
 import {triggerAnalyticsEvent} from '../../../src/analytics';
 
@@ -179,6 +180,14 @@ export class NextPageService {
       for (let i = 0; i < elements.length; i++) {
         elements[i].classList.add('i-amphtml-next-page-hidden');
       }
+    }
+
+    // Drop any amp-analytics tags from the child doc. We want to reuse the
+    // parent config instead.
+    const analytics = doc.querySelectorAll('amp-analytics');
+    for (let i = 0; i < analytics.length; i++) {
+      const item = analytics[i];
+      removeElement(item);
     }
 
     const amp =
