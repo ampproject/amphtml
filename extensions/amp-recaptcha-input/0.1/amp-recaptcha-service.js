14d3efbda5d1d4f527714622dde25f450a0d8c0e
Aaron Turner <aaron@aaronthedev.com>
2018-09-13 16:35:16 -0700

    ✨Developed the <amp-recaptcha-input> .getValue() / .execute() flow (#17971)
    * Started listeners for recaptcha api iframe
    
    * Finished the recaptchaApi ready flow
    
    * Got most of the execute flow skeleton'd out
    
    * Created a counting ID system for handling and dispatching messages
    
    * Found a solution to the ID problem
    
    * Fixed bugs with the example
    
    * Wrote remanining unit tests
    
    * Fixed linting errors
    
    * Cleaned up all pre-commit requirements
    
    * Got getValue and execute working
    
    * Moved messaging to context, and the recaptcha token flow works great!
    
    * Finished the integration test
    
    * Fixed all linting and presubmit errors
    
    * Creating a mock for the recaptcha API
    
    * Added some docs on Messages, and more mock script fixes
    
    * Mocked out the recaptcha API for integration tests
    
    * Removed the integration tests. Going to make a functional one later
    
    * Made PR Changes
    
    * Addressed more PR Comments

--- extensions/amp-recaptcha-input/0.1/amp-recaptcha-service.js
+++ extensions/amp-recaptcha-input/0.1/amp-recaptcha-service.js
@@ -19,10 +19,36 @@
  * interacting with the 3p recaptcha bootstrap iframe
  */
 
+import {Deferred} from '../../../src/utils/promise';
+import {dev} from '../../../src/log';
+import {dict} from '../../../src/utils/object';
 import {getIframe} from '../../../src/3p-frame';
 import {getService, registerServiceBuilder} from '../../../src/service';
+import {listenFor, postMessage} from '../../../src/iframe-helper';
 import {loadPromise} from '../../../src/event-helper';
 import {removeElement} from '../../../src/dom';
+
+/**
+ * @fileoverview
+ * Service used by AMP recaptcha elements, to utilize
+ * the recaptcha API that is within a bootstrap Iframe.
+ *
+ * Here are the following iframe messages using .postMessage()
+ * used between the iframe and recaptcha service:
+ * amp-recaptcha-ready / Service <- Iframe :
+ *   Iframe and Recaptcha API are ready.
+ * amp-recaptcha-action / Service -> Iframe :
+ *   Execute and action using supplied data
+ * amp-recaptcha-token / Service <- Iframe :
+ *   Response to 'amp-recaptcha-action'. The token
+ *   returned by the recaptcha API.
+ * amp-recaptcha-error / Service <- Iframe :
+ *   Response to 'amp-recaptcha-action'. Error
+ *   From attempting to get a token from action.
+ */
+
+/** @const {string} */
+const MESSAGE_TAG = 'amp-recaptcha-';
 
 export class AmpRecaptchaService {
 
@@ -45,6 +71,15 @@ export class AmpRecaptchaService {
 
     /** @private {number} */
     this.registeredElementCount_ = 0;
+
+    /** @private {!Deferred} */
+    this.recaptchaApiReady_ = new Deferred();
+
+    /** @private {Array} */
+    this.unlisteners_ = [];
+
+    /** @private {Object} */
+    this.executeMap_ = {};
   }
 
   /**
@@ -73,6 +108,48 @@ export class AmpRecaptchaService {
   }
 
   /**
+   * Function to call .execute() on the recaptcha API within
+   * our iframe, to dispatch recaptcha actions.
+   * Takes in an element resource ID, sitekey, and the action to execute.
+   * Returns a Promise that resolves the recaptcha token.
+   * @param {number} resourceId
+   * @param {string} sitekey
+   * @param {string} action
+   * @return {Promise}
+   */
+  execute(resourceId, sitekey, action) {
+    if (!this.iframe_) {
+      return Promise.reject(new Error(
+          'An iframe is not created. You must register before executing'
+      ));
+    }
+
+    const executePromise = new Deferred();
+    const messageId = resourceId;
+    this.executeMap_[messageId] = {
+      resolve: executePromise.resolve,
+      reject: executePromise.reject,
+    };
+    this.recaptchaApiReady_.promise.then(() => {
+
+      const message = dict({
+        'id': messageId,
+        'sitekey': sitekey,
+        'action': 'amp_' + action,
+      });
+
+      // Send the message
+      postMessage(
+          dev().assertElement(this.iframe_),
+          MESSAGE_TAG + 'action',
+          message,
+          '*',
+          true);
+    });
+    return executePromise.promise;
+  }
+
+  /**
    * Function to create our recaptcha boostrap iframe.
    * @param {!Element} element
    * @private
@@ -81,6 +158,19 @@ export class AmpRecaptchaService {
 
     /* the third parameter 'recaptcha' ties it to the 3p/recaptcha.js */
     this.iframe_ = getIframe(this.win_, element, 'recaptcha');
+
+    this.unlisteners_ = [
+      this.listenIframe_(
+          MESSAGE_TAG + 'ready', this.recaptchaApiReady_.resolve
+      ),
+      this.listenIframe_(
+          MESSAGE_TAG + 'token', this.tokenMessageHandler_.bind(this)
+      ),
+      this.listenIframe_(
+          MESSAGE_TAG + 'error', this.errorMessageHandler_.bind(this)
+      ),
+    ];
+    this.executeMap_ = {};
 
     this.iframe_.classList.add('i-amphtml-recaptcha-iframe');
     this.body_.appendChild(this.iframe_);
@@ -94,9 +184,46 @@ export class AmpRecaptchaService {
   dispose_() {
     if (this.iframe_) {
       removeElement(this.iframe_);
+      this.unlisteners_.forEach(unlistener => unlistener());
       this.iframe_ = null;
       this.iframeLoadPromise_ = null;
+      this.recaptchaApiReady_ = new Deferred();
+      this.unlisteners_ = [];
+      this.executeMap_ = {};
     }
+  }
+
+  /**
+   * Function to create a listener for our iframe
+   * @param {string} evName
+   * @param {Function} cb
+   * @return {Function}
+   * @private
+   */
+  listenIframe_(evName, cb) {
+    return listenFor(
+        dev().assertElement(this.iframe_),
+        evName,
+        cb,
+        true);
+  }
+
+  /**
+   * Function to handle token messages from the recaptcha iframe
+   * @param {Object} data
+   */
+  tokenMessageHandler_(data) {
+    this.executeMap_[data.id].resolve(data.token);
+    delete this.executeMap_[data.id];
+  }
+
+  /**
+   * Function to handle error messages from the recaptcha iframe
+   * @param {Object} data
+   */
+  errorMessageHandler_(data) {
+    this.executeMap_[data.id].reject(new Error(data.error));
+    delete this.executeMap_[data.id];
   }
 }
 
