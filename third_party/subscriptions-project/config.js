cc8b4dc61e4710071f5e9c3e0238b8fdd8215dc5
Dima Voytenko <dvoytenko@google.com>
2018-09-17 16:44:56 -0700

    SwG release 0.1.22.27 (#18061)
    * SwG release 0.1.22.27
    
    * fix tests

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.25 */
+/** Version: 0.1.22.27 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
