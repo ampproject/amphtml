efc9b3d0240fa4899c36780d6382743bc39c66e6
Naina Raisinghani <nainar@google.com>
2018-09-19 14:34:58 -0700

    Fix last failing test

--- test/integration/test-amp-carousel.js
+++ test/integration/test-amp-carousel.js
@@ -285,43 +285,59 @@ config.run('amp-carousel', function() {
       expect(prevBtn).to.not.have.class('amp-disabled');
       expect(nextBtn).to.have.class('amp-disabled');
     });
+  });
 
-    it.skip('(type=slides loop) should always have a prev and next button be ' +
+  const slidesMultipleImagesControlsLoop = `
+  <amp-carousel width=400 height=300 type=slides id="carousel-1" controls loop>
+    <amp-img src="https://lh3.googleusercontent.com/pSECrJ82R7-AqeBCOEPGPM9iG9OEIQ_QXcbubWIOdkY=w400-h300-no" layout=fill></amp-img>
+    <amp-img src="https://lh3.googleusercontent.com/5rcQ32ml8E5ONp9f9-Rf78IofLb9QjS5_0mqsY1zEFc=w400-h300-no" width=400 height=300></amp-img>
+    <amp-img src="https://lh3.googleusercontent.com/Z4gtm5Bkxyv21Z2PtbTf95Clb9AE4VTR6olbBKYrenM=w400-h300-no" width=400 height=300></amp-img>
+  </amp-carousel>
+  `;
+
+  describes.integration('type=slides with multiple images', {
+    body: slidesMultipleImagesControlsLoop,
+    extensions,
+  }, env => {
+    let document;
+
+    beforeEach(() => {
+      document = env.win.document;
+    });
+
+    it('should be present', () => {
+      expect(document.querySelectorAll('amp-carousel')).to.have.length.above(0);
+    });
+
+    it('(type=slides loop) should always have a prev and next button be ' +
     'able to get past the first and last item', () => {
       document.body.classList.add('amp-mode-mouse');
       const amp = document.querySelector('#carousel-1');
-      amp.setAttribute('controls', '');
-      amp.setAttribute('loop', '');
-      return Promise.resolve().then(timeout(0))
-          .then(() => {
-            const prevBtn = amp.querySelector('.amp-carousel-button-prev');
-            const nextBtn = amp.querySelector('.amp-carousel-button-next');
-            expect(amp.hasAttribute('loop')).to.be.true;
-            expect(amp.hasAttribute('controls')).to.be.true;
-            expect(prevBtn).to.not.be.null;
-            expect(nextBtn).to.not.be.null;
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-            nextBtn.click();
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-            nextBtn.click();
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-            nextBtn.click();
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-            nextBtn.click();
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-            nextBtn.click();
-            expect(prevBtn).to.not.have.class('amp-disabled');
-            expect(nextBtn).to.not.have.class('amp-disabled');
-          });
+      const prevBtn = amp.querySelector('.amp-carousel-button-prev');
+      const nextBtn = amp.querySelector('.amp-carousel-button-next');
+      expect(amp.hasAttribute('loop')).to.be.true;
+      expect(amp.hasAttribute('controls')).to.be.true;
+      expect(prevBtn).to.not.be.null;
+      expect(nextBtn).to.not.be.null;
+      expect(prevBtn).to.be.visible;
+      expect(nextBtn).to.be.visible;
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
+      nextBtn.click();
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
+      nextBtn.click();
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
+      nextBtn.click();
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
+      nextBtn.click();
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
+      nextBtn.click();
+      expect(prevBtn).to.not.have.class('amp-disabled');
+      expect(nextBtn).to.not.have.class('amp-disabled');
     });
   });
 });
-
-function timeout(ms) {
-  return () => new Promise(resolve => setTimeout(resolve, ms));
-}
