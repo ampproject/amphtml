80a0493c4569e701676069e5aa11e7cb7df9624a
Greg Grothaus <ggrothau@gmail.com>
2018-09-19 10:42:09 -0700

    Remove remnants of validator LIGHT mode. (#18015)
    * Remove remnants of validator LIGHT mode. Previously these were only
    removed from the main validator.js file, not these supporting libraries.

--- validator/engine/definitions.js
+++ validator/engine/definitions.js
@@ -14,11 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the license.
  */
-goog.provide('amp.validator.LIGHT');
 goog.provide('amp.validator.VALIDATE_CSS');
-
-/** @define {boolean} */
-amp.validator.LIGHT = false;
 
 /** @define {boolean} */
 amp.validator.VALIDATE_CSS = true;
