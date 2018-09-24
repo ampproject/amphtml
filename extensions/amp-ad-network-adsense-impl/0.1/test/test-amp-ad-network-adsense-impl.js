6d10e5495fd9b1134469136dd58c79cbca419561
atl-love <thejbnomore@gmail.com>
2018-09-24 15:52:40 -0700

    Fix spelling corrections for test-amp-ad-network-adsense-impl.js (Codelab 23) (#18323)

--- extensions/amp-ad-network-adsense-impl/0.1/test/test-amp-ad-network-adsense-impl.js
+++ extensions/amp-ad-network-adsense-impl/0.1/test/test-amp-ad-network-adsense-impl.js
@@ -943,7 +943,7 @@ describes.realWin('amp-ad-network-adsense-impl', {
         impl.onLayoutMeasure();
         // Right margin is 9px from containerContainer and 25px from container.
         // TODO(charliereams): In the test harness it is also offset by 15px due
-        // to strange scrollbar behaviour. Figure out how to disable this.
+        // to strange scrollbar behavior. Figure out how to disable this.
         expect(element.style.marginRight).to.be.equal('-124px');
         expect(element.style.marginLeft).to.be.equal('');
       });
