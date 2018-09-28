784afb702a39b973a1410d1b5d81d536b3c29aa8
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2018-09-28 11:17:47 -0700

    ✨Analytics: CookieWriter (#18406)
    * init commit
    
    * linter
    
    * address comment
    
    * apply stricter rule
    
    * disable inabox

--- extensions/amp-analytics/0.1/amp-analytics.js
+++ extensions/amp-analytics/0.1/amp-analytics.js
@@ -17,6 +17,7 @@
 import {Activity} from './activity-impl';
 import {AnalyticsConfig, mergeObjects} from './config';
 import {AnalyticsEventType} from './events';
+import {CookieWriter} from './cookie-writer';
 import {
   ExpansionOptions,
   installVariableService,
@@ -212,6 +213,10 @@ export class AmpAnalytics extends AMP.BaseElement {
             })
             .then(config => {
               this.config_ = config;
+              return new CookieWriter(this.win,
+                  this.element, this.config_).write();
+            })
+            .then(() => {
               this.transport_ =
                   new Transport(this.win, this.config_['transport'] || {});
             })
