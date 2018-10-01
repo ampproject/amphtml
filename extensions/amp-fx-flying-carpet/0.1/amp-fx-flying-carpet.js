a221e109357e0c6c1bcc9b33a43eefb528d6982b
Justin Ridgewell <jridgewell@google.com>
2018-10-01 19:13:42 -0400

    Track, but do not transfer, amp-fx-flying-carpet

--- extensions/amp-fx-flying-carpet/0.1/amp-fx-flying-carpet.js
+++ extensions/amp-fx-flying-carpet/0.1/amp-fx-flying-carpet.js
@@ -87,6 +87,8 @@ export class AmpFlyingCarpet extends AMP.BaseElement {
     childNodes.forEach(child => container.appendChild(child));
     clip.appendChild(container);
     this.element.appendChild(clip);
+
+    this.getViewport().addToFixedLayer(container, false);
   }
 
   /** @override */
