99d4254b6e3806b7532db29a4d2193aa1ae3faa2
Naina Raisinghani <nainar@google.com>
2018-10-05 15:48:54 -0700

    Remove recently added references to `keyCodes` (#18576)
    * Remove keyCodes
    
    * Fix test too
    
    * Fix tests :o

--- src/utils/key-codes.js
+++ src/utils/key-codes.js
@@ -15,6 +15,19 @@
  */
 
 /**
+ * @enum {number}
+ */
+export const KeyCodes = {
+  ENTER: 13,
+  ESCAPE: 27,
+  SPACE: 32,
+  LEFT_ARROW: 37,
+  UP_ARROW: 38,
+  RIGHT_ARROW: 39,
+  DOWN_ARROW: 40,
+};
+
+/**
  * @enum {string}
  */
 export const Keys = {
