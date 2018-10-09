4afda6bd6f68ee6f7e61bf014ed6e42edbb37e48
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2018-10-09 10:32:13 -0400

    🐛 Do not set last page as active if bookend is open (#18526)
    * make sure bookend is not open before setting last page as active
    
    * add tests
    
    * update tests
    
    * register the listener between opening and closing the bookend

--- extensions/amp-story/1.0/test/test-amp-story.js
+++ extensions/amp-story/1.0/test/test-amp-story.js
@@ -25,7 +25,6 @@ import {
 import {ActionTrust} from '../../../../src/action-constants';
 import {AmpStory} from '../amp-story';
 import {AmpStoryConsent} from '../amp-story-consent';
-import {EventType} from '../events';
 import {Keys} from '../../../../src/utils/key-codes';
 import {LocalizationService} from '../localization';
 import {MediaType} from '../media-pool';
@@ -35,7 +34,6 @@ import {Services} from '../../../../src/services';
 import {registerServiceBuilder} from '../../../../src/service';
 
 
-const NOOP = () => {};
 // Represents the correct value of KeyboardEvent.which for the Right Arrow
 const KEYBOARD_EVENT_WHICH_RIGHT_ARROW = 39;
 
@@ -50,21 +48,6 @@ describes.realWin('amp-story', {
   let element;
   let story;
   let replaceStateStub;
-
-  /**
-   * @param {!Element} container
-   * @param {boolean=} opt_active
-   * @return {!Element}
-   */
-  function appendEmptyPage(container, opt_active) {
-    const page = document.createElement('amp-story-page');
-    page.id = '-empty-page';
-    if (opt_active) {
-      page.setAttribute('active', true);
-    }
-    container.appendChild(page);
-    return page;
-  }
 
   /**
    * @param {!Element} container
@@ -209,20 +192,6 @@ describes.realWin('amp-story', {
         });
   });
 
-  // TODO(#11639): Re-enable this test.
-  it.skip('should hide bookend when CLOSE_BOOKEND is triggered', () => {
-    const hideBookendStub = sandbox.stub(
-        element.implementation_, 'hideBookend_', NOOP);
-
-    appendEmptyPage(element);
-
-    element.build();
-
-    element.dispatchEvent(new Event(EventType.CLOSE_BOOKEND));
-
-    expect(hideBookendStub).to.have.been.calledOnce;
-  });
-
   it('should return a valid page index', () => {
     createPages(story.element, 4, ['cover', 'page-1', 'page-2', 'page-3']);
     return story.layoutCallback()
@@ -241,7 +210,6 @@ describes.realWin('amp-story', {
           }
         });
   });
-
 
   it('should pause/resume pages when switching pages', () => {
     createPages(story.element, 2, ['cover', 'page-1']);
@@ -711,7 +679,6 @@ describes.realWin('amp-story', {
           });
     });
   });
-
 
   describe('desktop attributes', () => {
     it('should add next page attribute', () => {
