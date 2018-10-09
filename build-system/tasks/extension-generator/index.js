2f44b2900ca77ba460034175f11b3d3598a25b3c
Carlos Vializ <cvializ@vt.edu>
2018-10-09 09:56:40 -0400

    Remove anchor with deprecated name property. Markdown viewers like GitHub's automatically add anchors for headings. AMPProject.org doesn't use the anchor with the name attribute anyway.

--- build-system/tasks/extension-generator/index.js
+++ build-system/tasks/extension-generator/index.js
@@ -87,7 +87,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 
-# <a name="\`${name}\`"></a> \`${name}\`
+# \`${name}\`
 
 <table>
   <tr>
