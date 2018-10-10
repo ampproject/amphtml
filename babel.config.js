2cd82a763c607f25541b075dddfd7871203d8c68
Raghu Simha <rsimha@ampproject.org>
2018-10-10 14:50:01 -0400

    Fix single pass integration tests

--- babel.config.js
+++ babel.config.js
@@ -30,7 +30,8 @@ const argv = minimist(process.argv.slice(2));
 
 module.exports = function(api) {
   api.cache(true);
-  if (argv.single_pass) {
+  // Single pass builds do not use any of the default settings below.
+  if (argv._.includes('dist') && argv.single_pass) {
     return {};
   }
   return {
