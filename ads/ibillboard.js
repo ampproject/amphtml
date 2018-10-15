dc261fd9976ce80ca7cfac98e6e52df1b37f7668
pabi <pavel.bilik@ibillboard.com>
2018-10-15 13:51:36 +0200

    + go.cz.bbelements.com into validHosts

--- ads/ibillboard.js
+++ ads/ibillboard.js
@@ -22,6 +22,7 @@ const validHosts = [
   'https://go.goldbachpoland.bbelements.com',
   'https://go.pol.bbelements.com',
   'https://go.idmnet.bbelements.com',
+  'https://go.cz.bbelements.com'
 ];
 
 /**
