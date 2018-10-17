b9f51f14ed2e34bff9886311ef1ceb5a753df733
Alan Orozco <alanorozco@users.noreply.github.com>
2018-10-17 15:07:21 -0700

    🖍 Compact listing view (#18767)

--- build-system/app-index/index.js
+++ build-system/app-index/index.js
@@ -106,7 +106,7 @@ function serveIndex(req, res, next) {
 
 function serveListing(req, res, next) {
   serveListingWithReplacements(req, res, next, req.url.replace(/^\/~/, '/'), {
-    '<!-- bottom_of_header -->': listingHeaderFile,
+    '<!-- right_of_logo -->': listingHeaderFile,
   });
 }
 
