62f6eed008f1d175aae25d35b4688699aa8bd1f4
Alan Orozco <alanorozco@users.noreply.github.com>
2018-10-19 14:49:06 -0700

    ♻️ Componentize entire listing dashboard (#18848)

--- build-system/app-index/index.js
+++ build-system/app-index/index.js
@@ -21,27 +21,17 @@ const bundler = require('./bundler');
 const fs = BBPromise.promisifyAll(require('fs'));
 const {join, normalize, sep} = require('path');
 
-// TODO(alanorozco): Use JSX once we're ready.
 // HTML Templates
 const templateFile = join(__dirname, '/template.html');
-const proxyFormFile = join(__dirname, '/proxy-form.html');
-const listingHeaderFile = join(__dirname, '/listing-header.html');
 
 // JS Component
-const proxyFormComponent = join(__dirname, '/components/proxy-form.js');
+const mainComponent = join(__dirname, '/components/main.js');
+
+// CSS
 const mainCssFile = join(__dirname, '/main.css');
 
 
-
-function renderFileLink(base, location) {
-  return `<li>
-    <a class="file-link" href="${base.replace(/\/$/, '')}/${location}"
-      data-default="${base.replace(/^\/|\/$/, '')}/${location}">${location}</a>
-  </li>`;
-}
-
-
-function renderListing(basepath) {
+async function getListing(basepath) {
   // currently sitting on build-system/app-index, so we go back two dirs for the
   // repo root.
   const rootPath = join(__dirname, '../../');
@@ -51,105 +41,95 @@ function renderListing(basepath) {
 
   // null byte(s), bad request
   if (~path.indexOf('\0')) {
-    return Promise.resolve(null);
+    return null;
   }
 
   // malicious path
   if ((path + sep).substr(0, rootPath.length) !== rootPath) {
-    return Promise.resolve(null);
+    return null;
   }
 
-  return fs.statAsync(path).then(stat => {
-    if (!stat.isDirectory()) {
-      return null;
+  try {
+    if ((await fs.statAsync(path)).isDirectory()) {
+      return fs.readdirAsync(path);
     }
-
-    return Promise.all([
-      fs.readdirAsync(path),
-      fs.readFileAsync(templateFile),
-      fs.readFileAsync(mainCssFile),
-    ]).then(result => {
-      const files = result[0];
-      const template = result[1].toString();
-      const css = result[2].toString();
-
-      return template
-          .replace('<!-- main_style -->', `<style>${css}</style>`)
-          .replace('<!-- basepath -->', basepath)
-          .replace('<!-- listing -->',
-              files.map(file => renderFileLink(basepath, file)).join(''));
-    });
-  }).catch(() => /* empty catch for fallbacks */ null);
+  } catch (unusedE) {
+    /* empty catch for fallbacks */
+    return null;
+  }
 }
 
-
-function serveListingWithReplacements(
-  req, res, next, path, replacements = {}) {
-
-  Promise.all(
-      [renderListing(path)].concat(
-          Object.values(replacements).map(p =>
-            fs.readFileAsync(p)))).then(result => {
-    let output = result[0];
-
-    if (!output) {
-      next();
-      return;
-    }
-
-    let i = 1;
-    Object.keys(replacements).forEach(key => {
-      output = output.replace(key, result[i++]);
-    });
-
-    res.end(output);
-  });
-}
 
 let shouldCache = true;
 function setCacheStatus(cacheStatus) {
   shouldCache = cacheStatus;
 }
 
-let serveIndexCache;
-function serveIndex(req, res) {
 
-  if (shouldCache && serveIndexCache) {
-    res.end(serveIndexCache);
-    return;
+let mainBundleCache;
+async function bundleMain() {
+  if (shouldCache && mainBundleCache) {
+    return mainBundleCache;
   }
-
-  const serveIndexTask = async() => {
-    const bundle = await bundler.bundleComponent(proxyFormComponent);
-    let renderedHtml = await renderListing('/examples');
-
-    renderedHtml = renderedHtml.replace(
-        '<!-- bottom_of_header -->',
-        fs.readFileSync(proxyFormFile, 'utf8').toString()
-    );
-    renderedHtml = renderedHtml.replace(
-        '<!-- bundle -->',
-        bundle
-    );
-
-    if (shouldCache) {
-      serveIndexCache = renderedHtml;
-    }
-    res.end(renderedHtml);
-    return renderedHtml;
-  };
-  return serveIndexTask();
+  const bundle = await bundler.bundleComponent(mainComponent);
+  if (shouldCache) {
+    mainBundleCache = bundle;
+  }
+  return bundle;
 }
 
 
-function serveListing(req, res, next) {
-  serveListingWithReplacements(req, res, next, req.url.replace(/^\/~/, '/'), {
-    '<!-- right_of_logo -->': listingHeaderFile,
-  });
+function getListingPath(url) {
+  if (url == '/') {
+    return '/examples';
+  }
+  if (url == '/~') {
+    return '/';
+  }
+  return url;
+}
+
+
+function isMainPageFromUrl(url) {
+  return url == '/';
+}
+
+function serveIndex(req, res, next) {
+  const isMainPage = isMainPageFromUrl(req.url);
+  const basepath = getListingPath(req.url);
+
+  return (async() => {
+    const fileSet = await getListing(basepath);
+
+    if (fileSet == null) {
+      next();
+      return;
+    }
+
+    const bundle = await bundleMain();
+    const template = (await fs.readFileAsync(templateFile)).toString();
+    const css = (await fs.readFileAsync(mainCssFile)).toString();
+
+    const initialState = {
+      basepath,
+      fileSet,
+      isMainPage,
+      selectModePrefix: '/',
+    };
+
+    const renderedHtml = template
+        .replace('<!-- bundle -->', bundle)
+        .replace('<!-- main_style -->', css)
+        .replace('<!-- initial_state -->',
+            `window.AMP_PREACT_STATE = ${JSON.stringify(initialState)};`);
+
+    res.end(renderedHtml);
+
+    return renderedHtml; // for testing
+  })();
 }
 
 module.exports = {
   setCacheStatus,
   serveIndex,
-  serveListing,
 };
