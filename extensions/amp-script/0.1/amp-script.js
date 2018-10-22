b096a84f928f59aef81ada01f0c174005e76013d
William Chou <willchou@google.com>
2018-10-21 23:31:20 -0400

    amp-script: Validation and experiment (#18846)
    * amp-script validation and experiment check.
    
    * A few validation changes.
    
    * Update copyright year.
    
    * Add prateekbh to owners.
    
    * Fix presubmit.
    
    * Update validator test.

--- extensions/amp-script/0.1/amp-script.js
+++ extensions/amp-script/0.1/amp-script.js
@@ -21,6 +21,7 @@ import {
 } from '../../../src/service/extension-location';
 import {dev, user} from '../../../src/log';
 import {getMode} from '../../../src/mode';
+import {isExperimentOn} from '../../../src/experiments';
 import {
   sanitizer,
   upgradeElement,
@@ -39,6 +40,11 @@ export class AmpScript extends AMP.BaseElement {
 
   /** @override */
   layoutCallback() {
+    if (!isExperimentOn(this.win, 'amp-script')) {
+      const error = 'Experiment "amp-script" is not enabled.';
+      user().error(TAG, error);
+      return Promise.reject(error);
+    }
     // Configure worker-dom's sanitizer with AMP-specific config and hooks.
     const config = purifyConfig();
     sanitizer.configure(config, {
@@ -55,9 +61,7 @@ export class AmpScript extends AMP.BaseElement {
 
     const url = this.workerThreadUrl_();
     dev().fine(TAG, 'Fetching amp-script-worker from:', url);
-
     upgradeElement(this.element, url);
-
     return Promise.resolve();
   }
 
