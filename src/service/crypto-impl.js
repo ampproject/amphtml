dc3061cd63d4bfa8902b2afc28a82402463e39a2
William Chou <willchou@google.com>
2018-10-23 01:48:04 -0400

    Fix amp-crypto-polyfill module error (#18856)
    * Fix amp-crypto-polyfill module error.
    
    * Babel should treat sha384-generated as a module.
    
    * Fix types.
    
    * Change sha384 output wrapper to support Babel import semantics.

--- src/service/crypto-impl.js
+++ src/service/crypto-impl.js
@@ -24,8 +24,12 @@ import {stringToBytes, utf8Encode} from '../utils/bytes';
 const TAG = 'Crypto';
 const FALLBACK_MSG = 'SubtleCrypto failed, fallback to closure lib.';
 
-export class Crypto {
+/**
+ * @typedef {function((string|Uint8Array))}
+ */
+let CryptoPolyfillDef;
 
+export class Crypto {
   /**
    * Creates an instance of Crypto.
    * @param {!Window} win
@@ -57,7 +61,7 @@ export class Crypto {
     /** @private @const {boolean} */
     this.isLegacyWebkit_ = isLegacyWebkit;
 
-    /** @private {?Promise<{sha384: function((string|Uint8Array))}>} */
+    /** @private {?Promise<!CryptoPolyfillDef>} */
     this.polyfillPromise_ = null;
   }
 
@@ -76,7 +80,7 @@ export class Crypto {
     if (!this.subtle || this.polyfillPromise_) {
       // means native Crypto API is not available or failed before.
       return (this.polyfillPromise_ || this.loadPolyfill_())
-          .then(polyfill => polyfill.sha384(input));
+          .then(polyfillSha384 => polyfillSha384(input));
     }
 
     try {
@@ -131,7 +135,7 @@ export class Crypto {
 
   /**
    * Loads Crypto polyfill library.
-   * @return {!Promise<{sha384: function((string|Uint8Array))}>}
+   * @return {!Promise<!CryptoPolyfillDef>}
    * @private
    */
   loadPolyfill_() {
--- third_party/closure-library/compile.sh
+++ third_party/closure-library/compile.sh
@@ -28,9 +28,11 @@ java -jar $1 \
     --js "$2/goog/reflect/reflect.js" \
     --js "$2/goog/string/string.js" \
     --js_output_file "sha384-generated.js" \
-    --output_wrapper "/* Generated from closure library commit $GIT_COMMIT
-*/%output%;exports.base64=function base64(input) { return ampBase64(input) };
-exports.sha384=function sha384(input) { return ampSha384Digest(input) };" \
+    --output_wrapper "/* Generated from closure library commit $GIT_COMMIT */
+export const sha384 = (function(){
+%output%;
+return ampSha384Digest;
+}).call(window);" \
     --manage_closure_dependencies \
     --process_closure_primitives \
     --use_types_for_optimization \
