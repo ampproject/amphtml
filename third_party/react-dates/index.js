3518e7f29fb7282a9d1b98c483341f9126bc58fe
Carlos Vializ <cvializ@vt.edu>
2018-10-24 16:44:43 -0400

    Fix IE11 with a basic polyfill

--- third_party/react-dates/index.js
+++ third_party/react-dates/index.js
@@ -1,2 +1,18 @@
 import 'react-dates/initialize';
 import 'react-dates';
+
+// A very basic polyfill for Array.from,
+// to avoid needing to use a full 3p implementation
+if (!Array.from) {
+  Array.from = function (arrayLike) {
+    if (!arrayLike) {
+      return [];
+    }
+
+    if (!arrayLike.length) {
+      return [];
+    }
+
+    return [].concat(arrayLike);
+  }
+}
