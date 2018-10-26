b46bc8d15c58507f93f78f4d60bcc4440932a3ef
Yu Watanabe <yunabe.public@gmail.com>
2018-10-27 01:06:11 +0900

    Adjust the scroll position to center highlighted texts if the right position is changed after animation. (#18918)

--- extensions/amp-viewer-integration/0.1/highlight-handler.js
+++ extensions/amp-viewer-integration/0.1/highlight-handler.js
@@ -37,6 +37,17 @@ const HIGHLIGHT_DISMISS = 'highlightDismiss';
 const HIGHLIGHT_STATE = 'highlightState';
 
 /**
+ *
+ * @type {string}
+ */
+const PARAM_OLD_TOP_DISCREPANCY = 'od';
+
+/**
+ * @type {string}
+ */
+const PARAM_NEW_TOP_DISCREPANCY = 'nd';
+
+/**
  * The length limit of highlight param to avoid parsing
  * a incredibley large string as JSON. The limit is 100kB.
  * @type {number}
@@ -263,6 +274,32 @@ export class HighlightHandler {
   }
 
   /**
+   * Adjust scroll-top if the right scroll position is changed or
+   * the final scroll position is wrong after animation.
+   * This is necessary to center highlighted texts properly in pages reported
+   * in #18917
+   * @param {number} oldTop
+   * @return {?JsonObject}
+   */
+  mayAdjustTop_(oldTop) {
+    // Double-check the highlighted nodes are centered after animation.
+    const newTop = this.calcTopToCenterHighlightedNodes_();
+    const current = this.viewport_.getScrollTop();
+    if (current == newTop && current == oldTop) {
+      return null;
+    }
+    const shownParam = dict();
+    if (current != newTop) {
+      this.viewport_.setScrollTop(newTop);
+      shownParam[PARAM_NEW_TOP_DISCREPANCY] = current - newTop;
+    }
+    if (current != oldTop) {
+      shownParam[PARAM_OLD_TOP_DISCREPANCY] = current - oldTop;
+    }
+    return shownParam;
+  }
+
+  /**
    * @param {number} top
    * @private
    */
@@ -292,8 +329,8 @@ export class HighlightHandler {
     body.appendChild(sentinel);
     this.sendHighlightState_('auto_scroll');
     this.viewport_.animateScrollIntoView(sentinel).then(() => {
-      this.sendHighlightState_('shown');
       body.removeChild(sentinel);
+      this.sendHighlightState_('shown', this.mayAdjustTop_(top));
     });
   }
 
