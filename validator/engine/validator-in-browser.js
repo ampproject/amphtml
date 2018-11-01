a4ef1ef77973a64de6c05dfae22b25da20833030
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2018-11-01 12:42:03 -0700

    do not validate AMP Cache URLs with #development=1

--- validator/engine/validator-in-browser.js
+++ validator/engine/validator-in-browser.js
@@ -45,6 +45,15 @@ function getUrl(url) {
 }
 
 /**
+ * Checks if the given URL is an AMP Cache URL.
+ * @param {string} url
+ * @return {bool}
+ */
+amp.validator.isAmpCacheUrl = function(url) {
+  return (url.toLowerCase().indexOf('cdn.ampproject.org') !== -1);
+};
+
+/**
  * Validates doc in the browser by inspecting elements, attributes, etc. in
  * the DOM. This method is exported so it can be unittested.
  * @param {!Document} doc
@@ -66,8 +75,6 @@ amp.validator.validateInBrowser = function(doc) {
 /**
  * Validates a URL input, logging to the console the result.
  * Careful when modifying this; it's called from
- * https://github.com/ampproject/amphtml/blob/master/test/integration/test-example-validation.js
- * and
  * https://github.com/ampproject/amphtml/blob/master/src/validator-integration.js
  * @param {string} url
  * @param {!Document=} opt_doc
@@ -76,6 +83,12 @@ amp.validator.validateInBrowser = function(doc) {
  */
 amp.validator.validateUrlAndLog = function(
   url, opt_doc, opt_errorCategoryFilter) {
+  if (amp.validator.isAmpCacheUrl(url)) {
+    console.error(
+        'Attempting to validate an AMP Cache URL. Please use' +
+        '#development=1 on the origin URL instead.');
+    return;
+  }
   getUrl(url).then(
       function(html) { // Success
         const validationResult = amp.validator.validateString(html);
