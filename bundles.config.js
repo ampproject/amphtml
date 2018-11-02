bb82e61edfdbb7b76206edd9772802a14e1ecab0
Chad Ladensack <chadladensack@gmail.com>
2018-11-02 13:39:00 -0400

    ✨ amp-powr-player component (#19043)
    ✨

--- bundles.config.js
+++ bundles.config.js
@@ -390,6 +390,7 @@ exports.extensionBundles = [
   {name: 'amp-yotpo', version: '0.1', type: TYPES.MISC},
   {name: 'amp-youtube', version: '0.1', type: TYPES.MEDIA},
   {name: 'amp-mowplayer', version: '0.1', type: TYPES.MEDIA},
+  {name: 'amp-powr-player', version: '0.1', type: TYPES.MEDIA},
 ];
 
 exports.aliasBundles = [
