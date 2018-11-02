5fa7c09fd4b21d5e06248572309549f2b6541208
Ryan Ashcraft <ryan@amplitude.com>
2018-11-02 16:56:14 -0700

    Add Amplitude as an analytics vendor

--- extensions/amp-analytics/0.1/vendors.js
+++ extensions/amp-analytics/0.1/vendors.js
@@ -23,6 +23,7 @@ import {hasOwn} from '../../../src/utils/object';
 import {ACQUIALIFT_CONFIG} from './vendors/acquialift';
 import {AFSANALYTICS_CONFIG} from './vendors/afsanalytics';
 import {ALEXAMETRICS_CONFIG} from './vendors/alexametrics';
+import {AMPLITUDE_CONFIG} from './vendors/amplitude';
 import {ATINTERNET_CONFIG} from './vendors/atinternet';
 import {UMENGANALYTICS_CONFIG} from './vendors/umenganalytics';
 import {BAIDUANALYTICS_CONFIG} from './vendors/baiduanalytics';
@@ -174,6 +175,7 @@ export const ANALYTICS_CONFIG = /** @type {!JsonObject} */ ({
   'adobeanalytics_nativeConfig': ADOBEANALYTICS_NATIVECONFIG_CONFIG,
   'afsanalytics': AFSANALYTICS_CONFIG,
   'alexametrics': ALEXAMETRICS_CONFIG,
+  'amplitude': AMPLITUDE_CONFIG,
   'atinternet': ATINTERNET_CONFIG,
   'baiduanalytics': BAIDUANALYTICS_CONFIG,
   'bg': BG_CONFIG,
