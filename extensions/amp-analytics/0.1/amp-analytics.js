d7435376402761e589efea887f3de2d6c3c52ce2
Hongfei Ding <lannka@users.noreply.github.com>
2018-11-08 14:17:28 -0800

    Fix HTML_ATTR (#19185)
    * Fix HTML_ATTR
    
    * Fix tests
    
    * Fix lint

--- extensions/amp-analytics/0.1/amp-analytics.js
+++ extensions/amp-analytics/0.1/amp-analytics.js
@@ -470,7 +470,7 @@ export class AmpAnalytics extends AMP.BaseElement {
         if (hasOwn(this.config_['requests'], k)) {
           const request = this.config_['requests'][k];
           requests[k] = new RequestHandler(
-              this.getAmpDoc(), request, this.preconnect,
+              this.element, request, this.preconnect,
               this.transport_,
               this.isSandbox_);
         }
