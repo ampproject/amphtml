681c1e683077489db1e0d2e440712d80d35ff78e
Erwin Mombay <erwinm@google.com>
2018-11-12 09:15:32 -0800

    revert modified files by linter fix

--- ads/_ping_.js
+++ ads/_ping_.js
@@ -71,7 +71,8 @@ export function _ping_(global, data) {
     if (data.enableIo) {
       global.context.observeIntersection(function(changes) {
         changes.forEach(function(c) {
-          dev().info('AMP-AD', 'Intersection: (WxH)%sx%s', c.intersectionRect.width,c.intersectionRect.height);
+          dev().info('AMP-AD', 'Intersection: (WxH)' +
+              `${c.intersectionRect.width}x${c.intersectionRect.height}`);
         });
         // store changes to global.lastIO for testing purpose
         global.ping.lastIO = changes[changes.length - 1];
