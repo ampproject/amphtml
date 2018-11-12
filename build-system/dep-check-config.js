0f7e6136aaaf431d8a3ab3af575a61008d4f7f5c
Matthias Wolff <matthias.wolff@xymatic.com>
2018-11-12 22:22:43 +0100

    ✨ amp-delight-player (#17939)

--- build-system/dep-check-config.js
+++ build-system/dep-check-config.js
@@ -245,6 +245,8 @@ exports.rules = [
           'src/service/video-manager-impl.js',
       'extensions/amp-wistia-player/0.1/amp-wistia-player.js->' +
           'src/service/video-manager-impl.js',
+      'extensions/amp-delight-player/0.1/amp-delight-player.js->' +
+          'src/service/video-manager-impl.js',
       'extensions/amp-analytics/0.1/iframe-transport.js->' +
           'src/service/extension-location.js',
       'extensions/amp-analytics/0.1/iframe-transport.js->' +
