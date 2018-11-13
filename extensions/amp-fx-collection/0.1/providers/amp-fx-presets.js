96aa16652128c5a9b1ca5a592eaf8e74417fb672
Naina Raisinghani <nainar@google.com>
2018-11-13 14:43:55 -0800

    Remove inlined mutation. Also do for left

--- extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
+++ extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
@@ -94,8 +94,9 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const topAsLength = style.top == 'auto' ? '0px' : style.top;
               setStyles(fxElement.getElement(), {
-                'top': `calc(${style.top == 'auto' ? '0px' : style.top} +
+                'top': `calc(${topAsLength} +
                   ${fxElement.getFlyInDistance()}vh)`,
                 'visibility': 'visible',
               });
@@ -147,9 +148,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const leftAsLength = style.left == 'auto' ? '0px' : style.left;
               setStyles(fxElement.getElement(), {
                 'left':
-                  `calc(${style.left} - ${fxElement.getFlyInDistance()}vw)`,
+                  `calc(${leftAsLength} - ${fxElement.getFlyInDistance()}vw)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
@@ -200,9 +202,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const leftAsLength = style.left == 'auto' ? '0px' : style.left;
               setStyles(fxElement.getElement(), {
                 'left':
-                  `calc(${style.left} + ${fxElement.getFlyInDistance()}vw)`,
+                  `calc(${leftAsLength} + ${fxElement.getFlyInDistance()}vw)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
@@ -255,8 +258,9 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const topAsLength = style.top == 'auto' ? '0px' : style.top;
               setStyles(fxElement.getElement(), {
-                'top': `calc(${style.top == 'auto' ? '0px' : style.top} -
+                'top': `calc(${topAsLength} -
                   ${fxElement.getFlyInDistance()}vh)`,
                 'visibility': 'visible',
               });
