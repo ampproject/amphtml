6f4a29df30d069fd2f342087c2f99eaff3c120d1
Naina Raisinghani <nainar@google.com>
2018-11-14 09:38:08 -0800

    Fix <amp-fx="fly-in-{left|right}"> for IE/Edge. (#19301)
    * Push fix for IE/Edge. Enable tests on all platforms
    
    * Remove inlined mutation. Also do for left
    
    * Test

--- extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
+++ extensions/amp-fx-collection/0.1/providers/amp-fx-presets.js
@@ -81,8 +81,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const topAsLength = style.top == 'auto' ? '0px' : style.top;
               setStyles(fxElement.getElement(), {
-                'top': `calc(${style.top} + ${fxElement.getFlyInDistance()}vh)`,
+                'top': `calc(${topAsLength} +
+                  ${fxElement.getFlyInDistance()}vh)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
@@ -124,9 +126,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const leftAsLength = style.left == 'auto' ? '0px' : style.left;
               setStyles(fxElement.getElement(), {
                 'left':
-                  `calc(${style.left} - ${fxElement.getFlyInDistance()}vw)`,
+                  `calc(${leftAsLength} - ${fxElement.getFlyInDistance()}vw)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
@@ -168,9 +171,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const leftAsLength = style.left == 'auto' ? '0px' : style.left;
               setStyles(fxElement.getElement(), {
                 'left':
-                  `calc(${style.left} + ${fxElement.getFlyInDistance()}vw)`,
+                  `calc(${leftAsLength} + ${fxElement.getFlyInDistance()}vw)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
@@ -214,8 +218,10 @@ export const Presets = {
             fxElement.getElement(), function() {
               const style = computedStyle(fxElement.getAmpDoc().win,
                   fxElement.getElement());
+              const topAsLength = style.top == 'auto' ? '0px' : style.top;
               setStyles(fxElement.getElement(), {
-                'top': `calc(${style.top} - ${fxElement.getFlyInDistance()}vh)`,
+                'top': `calc(${topAsLength} -
+                  ${fxElement.getFlyInDistance()}vh)`,
                 'visibility': 'visible',
               });
               fxElement.initialTrigger = true;
