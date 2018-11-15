155c8c4f4bcfc688e5e666d08617b4c66737d91a
Raghu Simha <rsimha@ampproject.org>
2018-11-15 13:26:32 -0500

    ✨🏗 Upgrade Closure Compiler to v20180101 (#18794)

--- extensions/amp-mustache/0.1/test/test-amp-mustache.js
+++ extensions/amp-mustache/0.1/test/test-amp-mustache.js
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-import * as mustache from '../../../../third_party/mustache/mustache';
 import * as sanitizer from '../../../../src/sanitizer';
 import * as service from '../../../../src/service';
 import {AmpMustache} from '../amp-mustache';
+import mustache from '../../../../third_party/mustache/mustache';
 
 describe('amp-mustache 0.1', () => {
   let sandbox;
