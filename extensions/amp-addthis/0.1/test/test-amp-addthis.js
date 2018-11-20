81afea2057560888022c1c76f23df550403ba947
Jing Tao <taojing823@gmail.com>
2018-11-19 17:06:10 -0700

    amp-add-this: Add CSS for Addthis floating tool (#19382)

--- extensions/amp-addthis/0.1/test/test-amp-addthis.js
+++ extensions/amp-addthis/0.1/test/test-amp-addthis.js
@@ -38,6 +38,8 @@ describes.realWin('amp-addthis', {
   const configManager = getConfigManager();
   const pubId = 'ra-5988ef04ee1db125';
   const widgetId = '29nf';
+  const floatingPubId = 'ra-5b2947993c86010c';
+  const floatingWidgetId = '4hvd';
   let doc;
   let registerStub;
   let unregisterStub;
@@ -370,5 +372,15 @@ describes.realWin('amp-addthis', {
     const lastYear = new Date();
     lastYear.setFullYear(lastYear.getFullYear() - 1);
     expect(isDateInFuture(lastYear)).to.equal(false);
+  });
+
+  it('gives id for iframe if floating tool loaded', () => {
+    return getAT({pubId: floatingPubId, widgetId: floatingWidgetId}).then(
+        ({at}) => {
+          testIframe(at.querySelector('iframe'));
+          const iframe = at.querySelector('iframe');
+          expect(iframe.getAttribute('id')).to.equal(floatingWidgetId);
+        }
+    );
   });
 });
