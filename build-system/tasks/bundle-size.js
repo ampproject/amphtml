a20b1730ece266c0918b4eff63c1cdb2f7f1ecef
Daniel Rozenberg <rodaniel@ampproject.org>
2018-12-04 10:55:27 -0500

    Split gitBranchPoint to two function

--- build-system/tasks/bundle-size.js
+++ build-system/tasks/bundle-size.js
@@ -26,7 +26,7 @@ const path = require('path');
 const requestPost = BBPromise.promisify(require('request').post);
 const url = require('url');
 const {getStdout} = require('../exec');
-const {gitBranchPoint, gitCommitHash} = require('../git');
+const {gitCommitHash, gitTravisCommitRangeStart} = require('../git');
 
 const runtimeFile = './dist/v0.js';
 
@@ -108,7 +108,7 @@ function isPullRequest() {
  * @return {string} the `master` ancestor's bundle size.
  */
 async function getAncestorBundleSize() {
-  const gitBranchPointSha = gitBranchPoint();
+  const gitBranchPointSha = gitTravisCommitRangeStart();
   const gitBranchPointShortSha = gitBranchPointSha.substring(0, 7);
   log('Branch point from master is', cyan(gitBranchPointShortSha));
   return await octokit.repos.getContents(
