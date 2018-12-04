f7265835c7a6cb66a94b56db41947be4a853311f
Erwin Mombay <erwinm@google.com>
2018-12-04 09:55:46 -0800

    add TODO

--- extensions/amp-subscriptions/0.1/test/test-dialog.js
+++ extensions/amp-subscriptions/0.1/test/test-dialog.js
@@ -56,6 +56,7 @@ describes.realWin('AmpSubscriptions Dialog', {amp: true}, env => {
     expect(styles.position).to.equal('fixed');
   });
 
+  // (TODO, #19625): test is flakey
   it.skip('should open content when invisible', () => {
     const promise = dialog.open(content, false);
     expect(dialog.getRoot()).to.have.display('none');
