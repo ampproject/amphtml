e2e38af47090ca3e221b348fe6a657442e6ed899
erwin mombay <erwinmombay@users.noreply.github.com>
2018-12-04 10:32:57 -0800

    Message Extraction: while removing assert methods as a preprocess step, add back in the type (#19441)
    * ignore any null literals passed in as the argument
    
    * fix all tests
    
    * fix some minor type issues
    
    * fix lint issues and failing tests
    
    * add symbol test annotation
    
    * apply recommendation

--- build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-element/output.js
+++ build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-element/output.js
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-element;
-let result = element;
+
+/** @type {!Element} */
+(element);
+let result =
+/** @type {!Element} */
+(element);
 let result2 = undefined;
