9947f7319915faa4685f97cd7580566aaefd6c9d
William Chou <willchou@google.com>
2018-12-05 19:34:51 -0500

    Disable headless mode.

--- build-system/tasks/runtime-test.js
+++ build-system/tasks/runtime-test.js
@@ -60,10 +60,11 @@ function getConfig() {
   if (argv.ie) {
     return Object.assign({}, karmaDefault, {browsers: ['IE']});
   }
-  if (argv.headless) {
-    return Object.assign({}, karmaDefault,
-        {browsers: ['Chrome_no_extensions_headless']});
-  }
+  // TODO(choumx, #19658): --headless disabled until Chrome 72.
+  // if (argv.headless) {
+  //   return Object.assign({}, karmaDefault,
+  //       {browsers: ['Chrome_no_extensions_headless']});
+  // }
   if (argv.saucelabs || argv.saucelabs_lite) {
     if (!process.env.SAUCE_USERNAME) {
       throw new Error('Missing SAUCE_USERNAME Env variable');
@@ -193,10 +194,11 @@ function printArgvMessages() {
       log(green('⤷ Use'), cyan('--testnames'),
           green('to see the names of all tests being run.'));
     }
-    if (!argv.headless) {
-      log(green('⤷ Use'), cyan('--headless'),
-          green('to run tests in a headless Chrome window.'));
-    }
+    // TODO(choumx, #19658): --headless disabled until Chrome 72.
+    // if (!argv.headless) {
+    //   log(green('⤷ Use'), cyan('--headless'),
+    //       green('to run tests in a headless Chrome window.'));
+    // }
     if (!argv.compiled) {
       log(green('Running tests against unminified code.'));
     }
