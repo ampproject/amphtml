7dc287afd37e45ba21fdae80e3096460ae5e4a00
Hongfei Ding <lannka@users.noreply.github.com>
2018-12-07 14:21:11 -0800

    Add integration test for CLIENT_ID in amp-analytics (#19709)

--- test/integration/test-amp-analytics.js
+++ test/integration/test-amp-analytics.js
@@ -21,8 +21,12 @@ describe.configure().skipIfPropertiesObfuscated().run('amp' +
   this.timeout(15000);
 
   describes.integration('amp-analytics basic request', {
-    body:
-      `<amp-analytics>
+    body: `
+      <script>
+        // initialize _cid cookie with a CLIENT_ID
+        document.cookie='_cid=amp-12345';
+      </script>
+      <amp-analytics>
         <script type="application/json">
         {
           "requests": {
@@ -39,18 +43,86 @@ describe.configure().skipIfPropertiesObfuscated().run('amp' +
           },
           "extraUrlParams": {
             "a": 1,
-            "b": "\${title}"
+            "b": "\${title}",
+            "cid": "\${clientId(_cid)}"
           }
         }
         </script>
       </amp-analytics>`,
     extensions: ['amp-analytics'],
   }, () => {
+    afterEach(() => {
+      // clean up written _cid cookie
+      document.cookie = '_cid=;expires=' + new Date(0).toUTCString();
+    });
+
     it('should send request', () => {
       return RequestBank.withdraw().then(req => {
-        expect(req.url).to.equal('/?a=2&b=AMP%20TEST');
+        expect(req.url).to.equal('/?a=2&b=AMP%20TEST&cid=amp-12345');
         expect(req.headers.referer,
             'should keep referrer if no referrerpolicy specified').to.be.ok;
+      });
+    });
+  });
+
+  describes.integration('amp-analytics CLIENT_ID new user', {
+    body: `
+      <script>
+        // expires existing _cid cookie if any
+        document.cookie='_cid=;expires=' + new Date(0).toUTCString();
+      </script>
+      <amp-analytics>
+        <script type="application/json">
+        {
+          "requests": {
+            "pageview": "${RequestBank.getUrl(1)}?cid=CLIENT_ID(_cid)"
+          },
+          "triggers": {
+            "pageview": {
+              "on": "visible",
+              "request": "pageview"
+            }
+          }
+        }
+        </script>
+      </amp-analytics>
+      <amp-analytics>
+        <script type="application/json">
+        {
+          "requests": {
+            "pageview": "${RequestBank.getUrl(2)}?cid=CLIENT_ID(_cid)"
+          },
+          "triggers": {
+            "pageview": {
+              "on": "visible",
+              "request": "pageview"
+            }
+          }
+        }
+        </script>
+      </amp-analytics>
+      `,
+    extensions: ['amp-analytics'],
+  }, () => {
+    afterEach(() => {
+      // clean up written _cid cookie
+      document.cookie = '_cid=;expires=' + new Date(0).toUTCString();
+    });
+
+    it('should assign new cid', () => {
+      return Promise.all([
+        RequestBank.withdraw(1),
+        RequestBank.withdraw(2),
+      ]).then(reqs => {
+        const req1 = reqs[0];
+        const req2 = reqs[1];
+        expect(req1.url).to.match(/^\/\?cid=/);
+        expect(req2.url).to.match(/^\/\?cid=/);
+        const cid1 = req1.url.substr('/?cid='.length);
+        const cid2 = req2.url.substr('/?cid='.length);
+        expect(cid1).to.match(/^amp-/);
+        expect(cid2).to.equal(cid1);
+        expect(document.cookie).to.contain('_cid=' + cid1);
       });
     });
   });
