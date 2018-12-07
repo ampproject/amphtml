027f132b23a02fa941ba8716a06538ca9c0fea44
Hongfei Ding <lannka@users.noreply.github.com>
2018-12-06 17:49:45 -0800

    Adding integration for AMPHTML ads rendered on AMP page (A4A) (#19698)
    * Add integration tests for amp-analytics in A4A
    
    * Add integration tests for amp-img amp-pixel amp-analytics in A4A
    
    * Fix presubmit

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -868,6 +868,7 @@ const forbiddenTermsSrcInclusive = {
       'ads/_a4a-config.js',
       'build-system/app.js',
       'build-system/app-index/template.js',
+      'build-system/amp4test.js',
       'dist.3p/current/integration.js',
       'extensions/amp-iframe/0.1/amp-iframe.js',
       'src/config.js',
