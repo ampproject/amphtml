5e53dede38341a4238fe729a7718be92a41b7df2
lannka <hungfei.ding@gmail.com>
2018-12-06 18:54:44 -0800

    remove

--- test/integration/test-amp-analytics.js
+++ test/integration/test-amp-analytics.js
@@ -191,6 +191,36 @@ describe.configure().skipIfPropertiesObfuscated().run('amp' +
     });
   });
 
+  describes.integration('amp-analytics referrerPolicy', {
+    body:
+      `<amp-analytics>
+          <script type="application/json">
+          {
+            "requests": {
+              "endpoint": "${RequestBank.getUrl()}"
+            },
+            "triggers": {
+              "pageview": {
+                "on": "visible",
+                "request": "endpoint"
+              }
+            },
+            "transport": {
+              "referrerPolicy": "no-referrer"
+            }
+          }
+          </script>
+      </amp-analytics>`,
+    extensions: ['amp-analytics'],
+  }, () => {
+    it('should remove referrer if referrerpolicy=no-referrer', () => {
+      return RequestBank.withdraw().then(req => {
+        expect(req.url).to.equal('/');
+        expect(req.headers.referer).to.not.be.ok;
+      });
+    });
+  });
+
   describes.integration('amp-analytics type=googleanalytics', {
     body: `
       <script>
@@ -256,116 +286,6 @@ describe.configure().skipIfPropertiesObfuscated().run('amp' +
         expect(queries['jid']).to.match(isRandomNumber); // ${random}
         expect(queries['a']).to.match(isNumber); // ${pageViewId}
         expect(queries['z']).to.match(isRandomNumber); // ${random}
-      });
-    });
-  });
-
-  describes.integration('amp-analytics type=googleanalytics new user', {
-    body: `
-      <script>
-        // expires existing _ga cookie if any
-        document.cookie='_ga=;expires=' + new Date(0).toUTCString();
-      </script>
-      <amp-analytics type="googleanalytics">
-        <script type="application/json">
-        {
-          "vars": {
-            "account": "UA-67833617-1"
-          },
-          "requests": {
-            "host": "${RequestBank.getUrl(1)}"
-          },
-          "triggers": {
-            "pageview": {
-              "on": "visible",
-              "request": "pageview"
-            },
-            "performanceTiming": {
-              "enabled": false
-            },
-            "adwordsTiming": {
-              "enabled": false
-            }
-          }
-        }
-        </script>
-      </amp-analytics>
-      <amp-analytics type="googleanalytics">
-        <script type="application/json">
-        {
-          "vars": {
-            "account": "UA-67833617-1"
-          },
-          "requests": {
-            "host": "${RequestBank.getUrl(2)}"
-          },
-          "triggers": {
-            "pageview": {
-              "on": "visible",
-              "request": "pageview"
-            },
-            "performanceTiming": {
-              "enabled": false
-            },
-            "adwordsTiming": {
-              "enabled": false
-            }
-          }
-        }
-        </script>
-      </amp-analytics>
-      `,
-    extensions: ['amp-analytics'],
-  }, () => {
-    afterEach(() => {
-      // clean up written _ga cookie
-      document.cookie = '_ga=;expires=' + new Date(0).toUTCString();
-    });
-
-    it('should assign new cid', () => {
-      return Promise.all([
-        RequestBank.withdraw(1),
-        RequestBank.withdraw(2),
-      ]).then(reqs => {
-        const req1 = reqs[0];
-        const req2 = reqs[1];
-        expect(req1.url).to.match(/^\/r\/collect\?/);
-        expect(req2.url).to.match(/^\/r\/collect\?/);
-        const queries1 = parseQueryString(req1.url.substr('/r/collect'.length));
-        const queries2 = parseQueryString(req2.url.substr('/r/collect'.length));
-        expect(queries1['cid']).to.match(/^amp-/);
-        expect(queries1['cid']).to.equal(queries2['cid']);
-        expect(document.cookie).to.contain('ga=' + queries1['cid']);
-      });
-    });
-  });
-
-  describes.integration('amp-analytics referrerPolicy', {
-    body:
-      `<amp-analytics>
-          <script type="application/json">
-          {
-            "requests": {
-              "endpoint": "${RequestBank.getUrl()}"
-            },
-            "triggers": {
-              "pageview": {
-                "on": "visible",
-                "request": "endpoint"
-              }
-            },
-            "transport": {
-              "referrerPolicy": "no-referrer"
-            }
-          }
-          </script>
-      </amp-analytics>`,
-    extensions: ['amp-analytics'],
-  }, () => {
-    it('should remove referrer if referrerpolicy=no-referrer', () => {
-      return RequestBank.withdraw().then(req => {
-        expect(req.url).to.equal('/');
-        expect(req.headers.referer).to.not.be.ok;
       });
     });
   });
