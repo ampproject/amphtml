779b39c6ecaf19dfd9c7c61a41492519fc325f10
Sepand Parhami <sparhami@google.com>
2018-12-12 10:38:21 -0800

    Fix wrong carousel slide being shown when closing lightbox.
    - Check the index of the slide within all slides (rather than all
    children) as the parent container may contain things other than slides.
    - Get rid of `div` in the selector, it is overly specific.

--- extensions/amp-lightbox-gallery/0.1/amp-lightbox-gallery.js
+++ extensions/amp-lightbox-gallery/0.1/amp-lightbox-gallery.js
@@ -35,6 +35,7 @@ import {
   closestBySelector,
   elementByTag,
   escapeCssSelectorIdent,
+  scopedQuerySelectorAll,
 } from '../../../src/dom';
 import {clamp} from '../../../src/utils/math';
 import {dev, user} from '../../../src/log';
@@ -1129,9 +1130,11 @@ export class AmpLightboxGallery extends AMP.BaseElement {
     const parentCarousel = closestBySelector(target,
         'amp-carousel[type="slides"]');
     if (parentCarousel) {
-      const targetSlide = closestBySelector(target, 'div.i-amphtml-slide-item');
-      const targetSlideIndex = toArray(targetSlide.parentNode.children)
-          .indexOf(targetSlide);
+      const slideSelector = '.i-amphtml-slide-item';
+      const allSlides = toArray(
+          scopedQuerySelectorAll(parentCarousel, slideSelector));
+      const targetSlide = closestBySelector(target, slideSelector);
+      const targetSlideIndex = allSlides.indexOf(targetSlide);
       dev().assert(parentCarousel).getImpl()
           .then(carousel => carousel.showSlideWhenReady(targetSlideIndex));
     }
