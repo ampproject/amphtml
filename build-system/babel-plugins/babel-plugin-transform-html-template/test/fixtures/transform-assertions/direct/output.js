8fd1d8b5c994cd631a5de8e6556781647476ab1e
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-12-13 13:04:51 -0800

    🚀 Hoist templates and ensure duplicates are not added to bundle (#19812)
    * Hoist templates and ensure duplicates are not added to bundle
    
    * Additional test cases for hoisting
    
    * Only find program when hoisting
    
    * Address PR feedback
    
    * Updated to not use branching for html and htmlFor.

--- build-system/babel-plugins/babel-plugin-transform-html-template/test/fixtures/transform-assertions/direct/output.js
+++ build-system/babel-plugins/babel-plugin-transform-html-template/test/fixtures/transform-assertions/direct/output.js
@@ -1,3 +1,8 @@
+const _template = ["html usage"],
+      _template2 = ["<div>html usage</div>"],
+      _template3 = ["<div>third sibling</div>"],
+      _template4 = ["<div>fouth sibling</div>"];
+
 /**
  * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
  *
@@ -35,3 +40,17 @@ console.log(htmlFor(element)(["<p><span>html usage</span> <span>whitespace befor
 console.log(htmlFor(element)(["<p><span>html usage</span> <span>comment before entry tag</span></p>"]));
 console.log(htmlFor(element)(["<p><span>html usage</span> <span>comment after entry tag</span></p>"]));
 console.log(htmlFor(element)(["<p><span>html usage</span> <span>with comment sibling</span></p>"]));
+
+function pleaseHoistInternalUsage() {
+  console.log(html(_template));
+  console.log(htmlFor(element)(_template));
+}
+
+function pleaseHoistDifferentTemplates() {
+  console.log(html(_template));
+  console.log(htmlFor(element)(_template));
+  console.log(html(_template2));
+  console.log(htmlFor(element)(_template2));
+  console.log(html(_template3));
+  console.log(htmlFor(element)(_template4));
+}
