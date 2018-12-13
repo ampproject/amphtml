26f18f7538d5f98b979e7c25390074258692433a
Erwin Mombay <erwinm@google.com>
2018-12-13 10:07:03 -0800

    add tests

---
+++ build-system/babel-plugins/babel-plugin-transform-log-methods/test/fixtures/transform-log-methods/transform-asserts/output.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+user().assert(1 + 1);
+let result = user().assert(user(), getLogUrlz('hello', 'world'));
+let result2 = user().assert();
