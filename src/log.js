f47f575f5704e0c9895bb26d51db9cbf5aa3a11c
Cathy Zhu <cathyzhu@google.com>
2018-12-17 14:47:31 -0800

    Add assertArray function

--- src/log.js
+++ src/log.js
@@ -405,6 +405,20 @@ export class Log {
   }
 
   /**
+   * Throws an error if the first argument is not an array.
+   * The array can be empty.
+   *
+   * @param {*} shouldBeArray
+   * @param {string=} opt_message The assertion message
+   * @return {!Array} The array value
+   */
+  assertArray(shouldBeArray, opt_message) {
+    this.assert(Array.isArray(shouldBeArray),
+        (opt_message || 'Array expected') + ': %s', shouldBeArray);
+    return /** @type {!Array} */ (shouldBeArray);
+  }
+
+  /**
    * Throws an error if the first argument isn't a boolean.
    *
    * For more details see `assert`.
