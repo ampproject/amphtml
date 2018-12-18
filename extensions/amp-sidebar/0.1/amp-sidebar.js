01630a6b964f7fcb3ced3fa77f1b545823c88bb6
Rebecca Zhang <rbczhang90@gmail.com>
2018-12-18 10:07:22 -0800

    Fix CSS transitions on sidebar (#19811)
    * Fix CSS transitions on sidebar
    
    * Use i-amphtml-ghost css class instead of amp-visible

--- extensions/amp-sidebar/0.1/amp-sidebar.js
+++ extensions/amp-sidebar/0.1/amp-sidebar.js
@@ -373,7 +373,7 @@ export class AmpSidebar extends AMP.BaseElement {
       });
       this.maskElement_ = mask;
     }
-    toggle(this.maskElement_, /* display */true);
+    this.maskElement_.classList.toggle('i-amphtml-ghost', false);
   }
 
   /**
@@ -381,7 +381,7 @@ export class AmpSidebar extends AMP.BaseElement {
    */
   closeMask_() {
     if (this.maskElement_) {
-      toggle(this.maskElement_, /* display */false);
+      this.maskElement_.classList.toggle('i-amphtml-ghost', true);
     }
   }
 
