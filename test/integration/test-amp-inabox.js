934d9aaf835a7a4f8e06350f6d82f77209a85b9c
lannka <hungfei.ding@gmail.com>
2018-12-19 10:41:22 -0800

    remove BTF test

--- test/integration/test-amp-inabox.js
+++ test/integration/test-amp-inabox.js
@@ -17,7 +17,7 @@
 import {RequestBank} from '../../testing/test-helper';
 import {parseQueryString} from '../../src/url';
 
-describe.configure().skipIfPropertiesObfuscated().run('inabox', function() {
+describe.configure().run('inabox', function() {
   this.timeout(15000);
 
   describes.integration('AMPHTML ads rendered on non-AMP page ATF', {
@@ -50,45 +50,5 @@ describe.configure().skipIfPropertiesObfuscated().run('inabox', function() {
     });
   });
 
-  describes.integration('AMPHTML ads rendered on non-AMP page BTF', {
-    amp: false,
-    body: `
-      <div style="height: 100vh"></div>
-      <iframe
-      src="//ads.localhost:9876/amp4test/inabox/${RequestBank.getBrowserId()}"
-          scrolling="no"
-          width="300" height="250">
-      </iframe>
-      <script src="/examples/inabox-tag-integration.js"></script>
-      `,
-  }, env => {
-    it('should layout amp-img, amp-pixel, amp-analytics', () => {
-      // See amp4test.js for creative content
-
-      // The iframe starts BTF. "visible" trigger should be after scroll.
-      // We will record scrolling time for comparison.
-      let scrollTime = Infinity;
-      const imgPromise = RequestBank.withdraw('image').then(req => {
-        expect(Date.now()).to.be.below(scrollTime);
-        expect(req.url).to.equal('/');
-      });
-      const pixelPromise = RequestBank.withdraw('pixel').then(req => {
-        expect(Date.now()).to.be.below(scrollTime);
-        expect(req.url).to.equal('/foo?cid=');
-      });
-      const analyticsPromise = RequestBank.withdraw('analytics').then(req => {
-        expect(req.url).to.match(/^\/bar\?/);
-        const queries =
-            parseQueryString(req.url.substr('/bar'.length));
-        expect(queries['cid']).to.equal('');
-        expect(parseInt(queries['timestamp'], 10)).to.be.above(scrollTime);
-        expect(Date.now()).to.be.above(scrollTime);
-      });
-      setTimeout(() => {
-        scrollTime = Date.now();
-        env.win.scrollTo(0, 1000);
-      }, 2000);
-      return Promise.all([imgPromise, pixelPromise, analyticsPromise]);
-    });
-  });
+  // TODO: add visible trigger test for BTF case. Currently blocked by #14010
 });
