0864b054f1a2043afe84adb7024b95eacae5aed5
Malte Ubl <cramforce@users.noreply.github.com>
2018-12-21 13:23:04 -0800

    Make dep-check an order of magnitude faster. (#20050)
    Runtime goes down from over 4 minutes to 20 seconds.
    
    Change: Instead of processing every entry point by itself, create a temporary file that imports all entry points and process that.

--- build-system/tasks/dep-check.js
+++ build-system/tasks/dep-check.js
@@ -157,6 +157,15 @@ function getSrcs() {
         .map(getEntryModule)
         // Concat the core binary and integration binary as entry points.
         .concat('src/amp.js', '3p/integration.js'));
+  }).then(files => {
+    // Write all the entry modules into a single file so they can be processed
+    // together.
+    fs.mkdirpSync('./.amp-build');
+    const filename = './.amp-build/gulp-dep-check-collection.js';
+    fs.writeFileSync(filename, files.map(file => {
+      return `import '../${file}';`;
+    }).join('\n'));
+    return [filename];
   });
 }
 
@@ -195,7 +204,9 @@ function getGraph(entryModule) {
       .pipe(source(entryModule))
       // Unfortunately we need to write the files out.
       .pipe(gulp.dest('./.amp-build'))
-      .on('end', resolve.bind(null, module));
+      .on('end', () => {
+        resolve(module);
+      });
   return promise;
 }
 
