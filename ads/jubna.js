317e887092a763e73a7da060c61825942ac98d5f
Anshuman Mishra <er.anshuman91@gmail.com>
2019-01-03 19:10:37 +0530

    Fixing: Expected indentation of 2 spaces but found 1 tab   indent in ads/jubna.js

--- ads/jubna.js
+++ ads/jubna.js
@@ -21,11 +21,11 @@ import {loadScript, validateData} from '../3p/3p';
 * @param {!Object} data
 */
 export function jubna(global, data) {
-	validateData(data, ['wid', 'pid']);
-	global._jubna = global._jubna || {
-		widgetID: data['wid'],
-		pubID: data['pid'],
-		referrer: global.context.referrer,
-	};
-	loadScript(global, 'https://cdn.jubna.com/adscripts/jb_amp_loader.js');
+  validateData(data, ['wid', 'pid']);
+  global._jubna = global._jubna || {
+    widgetID: data['wid'],
+    pubID: data['pid'],
+    referrer: global.context.referrer,
+  };
+  loadScript(global, 'https://cdn.jubna.com/adscripts/jb_amp_loader.js');
 }
