f36ad1bd6a79892ee89b22d00f8c7858aaece390
Vincent Comby <vincent@skimlinks.com>
2019-01-03 15:05:12 +0100

    Add safety test to make sure amp-skimlinks  still works when the config needed only for the tests is not set.

--- test/integration/test-amp-skimlinks.js
+++ test/integration/test-amp-skimlinks.js
@@ -2,6 +2,9 @@ import {BrowserController, RequestBank} from '../../testing/test-helper';
 import {PLATFORM_NAME} from '../../extensions/amp-skimlinks/0.1/constants';
 import {parseQueryString} from '../../src/url';
 
+// Create fake test urls to replace skimlinks API urls.
+// RequestBank allow us to check if an API request has been made
+// or not by calling RequestBank.withdraw later.
 const pageTrackingUrl = RequestBank.getUrl('pageTrackingUrl') +
   '/track.php?data=${data}';
 const linksTrackingUrl = RequestBank.getUrl('linksTrackingUrl') +
@@ -10,50 +13,68 @@ const nonAffiliateTrackingUrl = RequestBank.getUrl('nonAffiliateTrackingUrl') +
   '?call=track&data=${data}';
 const waypointUrl = `${RequestBank.getUrl('waypointUrl')}`;
 
-describe('amp-skimlinks', function() {
-  const setup = {
-    extensions: ['amp-skimlinks'],
-    body: `
-            <amp-skimlinks
-                layout="nodisplay"
-                publisher-code="123X123"
-                tracking="true"
-            >
-              <script type="application/json">
-                {
-                    "pageTrackingUrl": "${pageTrackingUrl}",
-                    "linksTrackingUrl": "${linksTrackingUrl}",
-                    "nonAffiliateTrackingUrl": "${nonAffiliateTrackingUrl}",
-                    "waypointUrl": "${waypointUrl}"
-                }
-              </script>
-            </amp-skimlinks>
-            <div>
-                <a id="merchant-link" href="https://nordstrom.com"> Test Merchant </a>
-                <a id="non-merchant-link" href="https://google.com"> Test non-Merchant </a>
-            </div>
-        `,
-  };
 
-  describes.integration('Basic page', setup, env => {
+// Simulated click event created by browser.click() does not trigger
+// the browser navigation when dispatched on a link.
+// Using MouseEvent("click") instead of Event("click") does,
+// which is what we need for those tests.
+function clickLink_(doc, selector) {
+  const element = doc.querySelector(selector);
+  if (element) {
+    const clickEvent = new MouseEvent('click', {bubbles: true});
+    element.dispatchEvent(clickEvent);
+  }
+}
+
+const setupBasic = {
+  extensions: ['amp-skimlinks'],
+  body: `
+    <amp-skimlinks
+        layout="nodisplay"
+        publisher-code="123X123"
+        tracking="true"
+    >
+      <script type="application/json">
+        {
+            "pageTrackingUrl": "${pageTrackingUrl}",
+            "linksTrackingUrl": "${linksTrackingUrl}",
+            "nonAffiliateTrackingUrl": "${nonAffiliateTrackingUrl}",
+            "waypointUrl": "${waypointUrl}"
+        }
+      </script>
+    </amp-skimlinks>
+    <div>
+        <a id="merchant-link" href="https://nordstrom.com"> Test Merchant </a>
+        <a id="non-merchant-link" href="https://google.com"> Test non-Merchant </a>
+    </div>
+  `,
+};
+
+const setupNoConfig = {
+  extensions: ['amp-skimlinks'],
+  body: `
+      <amp-skimlinks
+          layout="nodisplay"
+          publisher-code="123X123"
+          tracking="true"
+      >
+      </amp-skimlinks>
+      <div>
+          <a id="merchant-link" href="https://nordstrom.com"> Test Merchant </a>
+          <a id="non-merchant-link" href="https://google.com"> Test non-Merchant </a>
+      </div>
+  `,
+};
+
+describe('amp-skimlinks', function() {
+  describes.integration('Basic features', setupBasic, env => {
     let browser = null;
     let clickLink = null;
 
     beforeEach(() => {
-      // Simulated click event created by browser.click() does not trigger
-      // the browser navigation when dispatched on a link.
-      // Using MouseEvent("click") instead of Event("click") does,
-      // which is what we want in those tests.
-      clickLink = selector => {
-        const element = env.win.document.querySelector(selector);
-        if (element) {
-          const clickEvent = new MouseEvent('click', {bubbles: true});
-          element.dispatchEvent(clickEvent);
-        }
-      };
+      clickLink = selector => clickLink_(env.win.document, selector);
       browser = new BrowserController(env.win);
       return browser.waitForElementBuild('amp-skimlinks');
-
     });
 
     it('Should send the page impression tracking request', () => {
@@ -94,9 +115,9 @@ describe('amp-skimlinks', function() {
     });
 
     it('Should send NA-tracking on non-merchant link click ', () => {
-      // Give 500ms for amp-skimlinks to setup
+      // Give 500ms for amp-skimlinks to set up
       return browser.wait(500).then(() => {
-        clickLink('a#non-merchant-link');
+        clickLink('#non-merchant-link');
 
         return RequestBank.withdraw('nonAffiliateTrackingUrl').then(req => {
           const regex = /^\/\?call=track&data=([^&]*)&?.*$/;
@@ -113,9 +134,9 @@ describe('amp-skimlinks', function() {
     });
 
     it('Should send to waypoint on merchant link click', () => {
-      // Give 500ms for amp-skimlinks to setup
+      // Give 500ms for amp-skimlinks to set up
       return browser.wait(500).then(() => {
-        clickLink('a#merchant-link');
+        clickLink('#merchant-link');
         return RequestBank.withdraw('waypointUrl').then(req => {
           // Remove "/?..." in the url
           const queryString = req.url.slice(2);
@@ -131,4 +152,42 @@ describe('amp-skimlinks', function() {
       });
     });
   });
+
+
+  // The purpose of these tests is to make sure that amp-skimlinks still
+  // works when the JSON config necessary to run our tests is not
+  // injected (similar to live environment).
+  // Since the JSON config is not set we can not use the proxy and
+  // therefore can only test a small subset of features.
+  describes.integration('Works without test config', setupNoConfig, env => {
+    let browser = null;
+    let clickLink = null;
+
+    beforeEach(() => {
+      clickLink = selector => clickLink_(env.win.document, selector);
+      browser = new BrowserController(env.win);
+      return browser.waitForElementBuild('amp-skimlinks');
+    });
+
+
+    it('Should send to waypoint on merchant link click', () => {
+      // Give 500ms for amp-skimlinks to set up
+      return browser.wait(500).then(() => {
+        clickLink('#merchant-link');
+        const link = env.win.document.querySelector('#merchant-link');
+        const regex = /^https\:\/\/go\.skimresources\.com\/\?(.*)$/;
+        const match = regex.exec(link.href);
+        expect(match).to.be.lengthOf(2);
+        const queryParams = parseQueryString(match[1]);
+        expect(queryParams.id).to.equal('123X123');
+        expect(queryParams.jv).to.equal(PLATFORM_NAME);
+        expect(queryParams.xuuid).to.have.lengthOf(32);
+        expect(queryParams.url).to.equal('https://nordstrom.com/');
+        expect(queryParams.sref).to.equal('http://nonblocking.io/');
+        expect(queryParams.pref).to.have.lengthOf.above(1);
+        expect(queryParams.xs).to.equal('1');
+      });
+    });
+  });
+
 });
