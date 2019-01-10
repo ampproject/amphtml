66b5da0be1e5a0bbdd834ec249fc5e262ce55c6a
Daniel Rozenberg <rodaniel@ampproject.org>
2019-01-10 14:54:44 -0500

    Fix lint error

--- build-system/git.js
+++ build-system/git.js
@@ -47,8 +47,8 @@ exports.gitTravisMasterBaseline = function() {
 
 /**
  * Shortens a commit SHA to 7 characters for human readability.
- * @param {string} 40 characters SHA.
- * @return {string}
+ * @param {string} sha 40 characters SHA.
+ * @return {string} 7 characters SHA.
  */
 exports.shortSha = function(sha) {
   return sha.substr(0, 7);
