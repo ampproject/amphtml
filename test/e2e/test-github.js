8a77f653fd0e48d693f75d52c1cb69cb13f9f896
Esther Kim <esth@google.com>
2019-01-14 15:44:08 -0500

    First e2e test with chromedriver

---
+++ test/e2e/test-github.js
@@ -0,0 +1,51 @@
+import 'babel-regenerator-runtime';
+import * as describes from '../../testing/e2e/describes-e2e';
+import {expect} from '../../testing/e2e/expect';
+
+const Keys = {
+  ENTER: 'enter',
+};
+
+describes.endtoend('GitHub search results', {
+}, async env => {
+  let controller;
+
+  beforeEach(async() => {
+    controller = env.controller;
+    await controller.navigateTo('https://github.com/');
+  });
+
+  it('should contain a result for the search term', async() => {
+    const searchButtonHandle =
+      await controller.findElement('.header-search-input');
+    await controller.type(searchButtonHandle, 'TestCafe');
+    await controller.type(null, Keys.ENTER);
+
+    await expect(controller.getTitle()).to.match(/TestCafe/);
+
+    const itemHandle = await controller.findElement('.repo-list-item');
+    await expect(controller.getElementText(itemHandle))
+        .to.contain('DevExpress/testcafe');
+  });
+});
+
+describes.endtoend('GitHub login', {
+}, async env => {
+  let controller;
+
+  beforeEach(async() => {
+    controller = env.controller;
+    await controller.navigateTo('https://github.com/login');
+  });
+
+  it('should fail to login with no credentials', async() => {
+    const loginButton =
+      await controller.findElement('.btn.btn-primary.btn-block');
+    await controller.click(loginButton);
+
+    const errorHandle =
+      await controller.findElement('#js-flash-container > div > div');
+    await expect(controller.getElementText(errorHandle))
+        .to.include('Incorrect username or password.');
+  });
+});
