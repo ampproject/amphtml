256ab60e39d6d558d5c9aa2198a63baa3f13af65
Esther Kim <44627152+estherkim@users.noreply.github.com>
2019-01-14 17:45:49 -0500

    First dummy e2e test with ChromeDriver (#20316)
    * First e2e test with chromedriver
    
    * Add yarn lock file
    
    * PR comments
    
    * Fix gulp presubmit and gulp lint errors

---
+++ testing/e2e/expect.js
@@ -0,0 +1,150 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ControllerPromise} from './functional-test-controller';
+import chai, {expect as chaiExpect} from 'chai';
+
+let installed;
+/**
+ * @param {*} actual
+ * @param {string=} opt_message
+ * @return
+ */
+export function expect(actual, opt_message) {
+  if (!installed) {
+    installed = true;
+    // See https://www.chaijs.com/guide/helpers/ for details on implementation
+    chai.use(installEqualWrapper);
+    chai.use(installIncludeWrapper);
+    chai.use(installMatchWrapper);
+    chai.use(installLengthWrapper);
+    chai.use(installAboveWrapper);
+    chai.use(installBelowWrapper);
+  }
+
+  return chaiExpect(actual, opt_message);
+}
+
+function installEqualWrapper(chai, utils) {
+  const {Assertion} = chai;
+
+  const condition = (expected, value) => value == expected;
+  const overwrite = overwriteWithCondition(utils, condition);
+  Assertion.overwriteMethod('equal', overwrite);
+  Assertion.overwriteMethod('equals', overwrite);
+  Assertion.overwriteMethod('eq', overwrite);
+}
+
+function installIncludeWrapper(chai, utils) {
+  const {Assertion} = chai;
+
+  const condition = (expected, value) => value.indexOf(expected) > -1;
+  const overwrite = overwriteWithCondition(utils, condition);
+  Assertion.overwriteChainableMethod(
+      'include', overwrite, inheritChainingBehavior);
+  Assertion.overwriteChainableMethod(
+      'includes', overwrite, inheritChainingBehavior);
+  Assertion.overwriteChainableMethod(
+      'contain', overwrite, inheritChainingBehavior);
+  Assertion.overwriteChainableMethod(
+      'contains', overwrite, inheritChainingBehavior);
+}
+
+function installMatchWrapper(chai, utils) {
+  const {Assertion} = chai;
+
+  const matchCondition = (expected, value) => expected.exec(value);
+  const overwriteMatch = overwriteWithCondition(utils, matchCondition);
+  Assertion.overwriteMethod('match', overwriteMatch);
+  Assertion.overwriteMethod('matches', overwriteMatch);
+}
+
+function installLengthWrapper(chai, utils) {
+  const {Assertion} = chai;
+
+  const condition = (expected, value) => value.length == expected;
+  const overwrite = overwriteWithCondition(utils, condition);
+  Assertion.overwriteChainableMethod(
+      'length', overwrite, inheritChainingBehavior);
+  Assertion.overwriteChainableMethod(
+      'lengthOf', overwrite, inheritChainingBehavior);
+}
+
+function installAboveWrapper(chai, utils) {
+  const {Assertion} = chai;
+  const {flag} = utils;
+
+  const condition = function(expected, value) {
+    if (flag(this, 'doLength')) {
+      value = value.length;
+    }
+    return value > expected;
+  };
+  const overwrite = overwriteWithCondition(utils, condition);
+  Assertion.overwriteMethod('above', overwrite);
+  Assertion.overwriteMethod('gt', overwrite);
+  Assertion.overwriteMethod('greaterThan', overwrite);
+}
+
+function installBelowWrapper(chai, utils) {
+  const {Assertion} = chai;
+  const {flag} = utils;
+
+  const condition = function(expected, value) {
+    if (flag(this, 'doLength')) {
+      value = value.length;
+    }
+    return value < expected;
+  };
+  const overwrite = overwriteWithCondition(utils, condition);
+  Assertion.overwriteMethod('below', overwrite);
+  Assertion.overwriteMethod('lt', overwrite);
+  Assertion.overwriteMethod('lessThan', overwrite);
+}
+
+
+function overwriteWithCondition(utils, condition) {
+  const {flag} = utils;
+
+  return function(_super) {
+    return async function(expected) {
+      const obj = this._obj;
+      const isControllerPromise = obj instanceof ControllerPromise;
+      if (!isControllerPromise) {
+        return _super.apply(this, arguments);
+      }
+      const {waitForValue} = obj;
+      if (!waitForValue) {
+        const result = await obj;
+        flag(this, 'object', result);
+        return _super.apply(this, arguments);
+      }
+
+      const boundCondition = condition.bind(this, expected);
+      const maybeNegatedCondition = flag(this, 'negate') ?
+        value => !boundCondition(value) : boundCondition;
+      flag(this, 'object', await waitForValue(maybeNegatedCondition));
+
+      return _super.apply(this, arguments);
+    };
+  };
+}
+
+function inheritChainingBehavior(_super) {
+  return function() {
+    _super.apply(this, arguments);
+  };
+}
