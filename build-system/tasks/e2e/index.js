3edf3a5642fe2aa3f58a138224141e7cde753cc6
Esther Kim <44627152+estherkim@users.noreply.github.com>
2019-01-17 12:05:45 -0500

    Update gulp e2e (#20386)
    * Update gulp e2e
    
    * Update path
    
    * extensions e2e tests need to be in test-e2e dir
    
    * Turn e2e test paths into build-system/config const

--- build-system/tasks/e2e/index.js
+++ build-system/tasks/e2e/index.js
@@ -16,16 +16,15 @@
 
 'use strict';
 
+const config = require('../../config');
 const gulp = require('gulp-help')(require('gulp'));
 const mocha = require('gulp-mocha');
 
 function e2e() {
-  return gulp.src(['test/e2e/*.js'], {read: false})
+  return gulp.src(config.e2eTestPaths, {read: false})
       .pipe(mocha({
         require: ['@babel/register', '../../../testing/e2e/test-module'],
       }));
 }
 
-gulp.task('e2e', 'Runs e2e tests', function() {
-  return e2e();
-});
+gulp.task('e2e', 'Runs e2e tests', e2e);
