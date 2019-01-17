bffcf238ccb6ed1c9523f3c2a38dfa7a0ba7243d
Esther Kim <esth@google.com>
2019-01-17 10:01:57 -0500

    extensions e2e tests need to be in test-e2e dir

--- build-system/tasks/e2e/index.js
+++ build-system/tasks/e2e/index.js
@@ -22,8 +22,7 @@ const mocha = require('gulp-mocha');
 function e2e() {
   return gulp.src([
     'test/e2e/*.js',
-    'extensions/**/*e2e*/*.js',
-    'extensions/**/test-e2e*.js',
+    'extensions/**/test-e2e/*.js',
   ], {read: false})
       .pipe(mocha({
         require: ['@babel/register', '../../../testing/e2e/test-module'],
