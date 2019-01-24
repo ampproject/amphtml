2ca1b37d1f6876469b2198ed5c5f3aee1893b388
Pet Ramanudom <budnampet@google.com>
2019-01-24 16:30:45 -0500

    removed analytics test, added variable service test

--- extensions/amp-story/1.0/test/test-analytics.js
+++ extensions/amp-story/1.0/test/test-analytics.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {AdvancementMode, AmpStoryAnalytics} from '../analytics';
+import {AmpStoryAnalytics} from '../analytics';
 import {StateChangeType} from '../navigation-state';
 
 
@@ -34,13 +34,5 @@ describes.fakeWin('amp-story analytics', {}, env => {
     });
 
     expect(trigger).to.have.been.calledWith('story-page-visible');
-  });
-
-  it('should trigger `manualAdvance` on change', () => {
-    const trigger = sandbox.stub(analytics, 'triggerEvent_');
-
-    analytics.onAdvancementModeStateChange(AdvancementMode.MANUAL_ADVANCE);
-
-    expect(trigger).to.have.been.calledWith('manualAdvance');
   });
 });
