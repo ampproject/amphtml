fd8a92d707cfeb0e50f7a668095db96cfb8be4ff
Jose Salgueiro <jsalgueiro@adsensor.com>
2019-01-29 16:03:08 +0000

    removed renderStartImplemented from the ads _config

--- ads/_config.js
+++ ads/_config.js
@@ -176,8 +176,7 @@ export const adConfig = {
 
   'adsensor': {
     prefetch: 'https://wfpscripts.webspectator.com/amp/adsensor-amp.js',
-    clientIdScope: 'amp_ecid_adensor',
-    renderStartImplemented: true,
+    clientIdScope: 'amp_ecid_adensor'
   },
 
   'adsnative': {
