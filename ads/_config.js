d4f2d10b1ca443af5e165149075196316b704a7e
Jose Salgueiro <jsalgueiro@adsensor.com>
2019-01-29 17:03:01 +0000

    missing trailing comma

--- ads/_config.js
+++ ads/_config.js
@@ -176,7 +176,7 @@ export const adConfig = {
 
   'adsensor': {
     prefetch: 'https://wfpscripts.webspectator.com/amp/adsensor-amp.js',
-    clientIdScope: 'amp_ecid_adensor'
+    clientIdScope: 'amp_ecid_adensor',
   },
 
   'adsnative': {
