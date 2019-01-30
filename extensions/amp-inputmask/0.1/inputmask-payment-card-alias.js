d0a41acf92b8828ed5d80fb777a715a9bb6e49e1
Carlos Vializ <cvializ@users.noreply.github.com>
2019-01-29 20:50:25 -0500

    🐛Prevent letters in credit cards. Allow editing beginning. (#20576)
    * Prevent letters in credit cards. Allow editing beginning.
    
    * Simplify boolean logic

--- extensions/amp-inputmask/0.1/inputmask-payment-card-alias.js
+++ extensions/amp-inputmask/0.1/inputmask-payment-card-alias.js
@@ -24,7 +24,8 @@ export function factory(Inputmask) {
   // https://baymard.com/checkout-usability/credit-card-patterns
   Inputmask.extendAliases({
     'payment-card': {
-      mask: function(opts) {
+      'postValidation': buffer => /[\s\d]+/.test(buffer.join('')),
+      'mask': function(opts) {
         opts.definitions = {
           'x': {
             'validator': function(chrs, buffer) {
@@ -45,7 +46,12 @@ export function factory(Inputmask) {
             'cardinality': 2,
           },
         };
-        return ['x99 9999 9999 9999', 'y99 999999 99999'];
+        return [
+          'y99 999999 99999',
+          'x99 9999 9999 9999',
+          '9999 999999 99999',
+          '9999 9999 9999 9999',
+        ];
       },
     },
   });
