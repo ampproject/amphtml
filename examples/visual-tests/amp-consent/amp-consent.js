0a2f50903cac9e6f080e63a30823f923fd9624d2
Aaron Turner <aaron@aaronthedev.com>
2019-01-31 15:38:29 -0800

    Started writing amp-consent visual diff tests

---
+++ examples/visual-tests/amp-consent/amp-consent.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const {verifyCssElements} = require('../../build-system/tasks/visual-diff/helpers');
+
+module.exports = {
+  'accept consent': async (page, name) => {
+    await page.tap('#consent-ui #accept');
+    await verifyCssElements(page, name,
+      /* forbiddenCss */ null,
+      /* loadingIncompleteCss */ null,
+      /* loadingCompleteCss */ [
+        'amp-img[data-block-on-consent="_till_responded"] img',
+        'amp-img[data-block-on-consent="_till_accepted"] img',
+        'amp-img[data-block-on-consent="default"] img',
+        '#not-specified img'
+      ]);
+  },
+ };
