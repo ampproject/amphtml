881dac7381e13b117bd5acf79222c501d3e0d652
Alan Orozco <orozcoalan@gmail.com>
2019-02-03 14:25:57 -0800

    Test html helpers

---
+++ build-system/app-index/test/test-html.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const assert = require('assert');
+
+const {html, joinFragments} = require('../html');
+
+describe('devdash', () => {
+
+  describe('html helpers', () => {
+
+    describe('joinFragments', () => {
+
+      it('joins simple fragments', () => {
+        assert.strictEqual(joinFragments(['a', 'b', 'c']), 'abc');
+      });
+
+      it('joins mapped fragments', () => {
+        assert.strictEqual(joinFragments([1, 2, 3], a => a + 1), '234');
+      });
+
+    });
+
+    describe('html', () => {
+
+      it('passes through simple string', () => {
+        assert.strictEqual(html`foo`, 'foo');
+      });
+
+      it('concatenates interpolated args', () => {
+        assert.strictEqual(
+            html`quesadilla ${'de'} chicharrón ${'con'} queso`,
+            'quesadilla de chicharrón con queso');
+      });
+
+    });
+
+  });
+
+});
