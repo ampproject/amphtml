1d96715193b84dd84fe925a6a521d64b22d82560
Caroline Liu <carolineliu@google.com>
2019-02-05 09:47:27 -0500

    change references from 'Pin It' to 'Save' in amp-pinterest

--- extensions/amp-pinterest/0.1/pin-widget.js
+++ extensions/amp-pinterest/0.1/pin-widget.js
@@ -59,7 +59,7 @@ export class PinWidget {
     if (href) {
       assertAbsoluteHttpOrHttpsUrl(href);
       if (shouldPop) {
-        openWindowDialog(window, href, '_pinit', POP);
+        openWindowDialog(window, href, '_save', POP);
       } else {
         openWindowDialog(window, `${href}?amp=1&guid=${Util.guid}`, '_blank');
       }
