22df0b9e715f5a98e4c1fe589dbb859cca8c32d0
delima02 <47327855+delima02@users.noreply.github.com>
2019-02-08 14:33:12 -0500

    🐛 Have purifier.js allow unknown protocols (#20710)
    * Have purifier.js allow unknown protocols.
    
    * Add unit test for allowing arbitrary protocols.
    
    * Add required 'target' attribute to tags in arbitrary protocols test.
    
    * Lint fix for unknown protocols test.

--- src/purifier.js
+++ src/purifier.js
@@ -280,6 +280,9 @@ export function purifyConfig() {
     FORCE_BODY: true,
     // Avoid need for serializing to/from string by returning Node directly.
     RETURN_DOM: true,
+    // BLACKLISTED_ATTR_VALUES are enough. Other unknown protocols are safe.
+    // This allows native app deeplinks.
+    ALLOW_UNKNOWN_PROTOCOLS: true,
   }));
   return /** @type {!DomPurifyConfig} */ (config);
 }
