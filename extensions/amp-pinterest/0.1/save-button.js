17ed299f87d05873b508e810da09ae56330ef9cf
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2019-02-12 13:16:27 -0500

    🖍Clean up amp-pinterest button styling (#20784)
    * Add additional check when setting amp-pinterest 'round' attribute.
    
    * Add attribute checks before getting values.
    
    * Lint

--- extensions/amp-pinterest/0.1/save-button.js
+++ extensions/amp-pinterest/0.1/save-button.js
@@ -53,7 +53,11 @@ export class SaveButton {
     this.color = rootElement.getAttribute('data-color');
     this.count = rootElement.getAttribute('data-count');
     this.lang = rootElement.getAttribute('data-lang');
-    this.round = rootElement.getAttribute('data-round');
+    const hasOneToOneDimensions = rootElement.hasAttribute('height') &&
+      rootElement.hasAttribute('width') &&
+      rootElement.getAttribute('height') === rootElement.getAttribute('width');
+    this.round = rootElement.getAttribute('data-round') ||
+      hasOneToOneDimensions;
     this.tall = rootElement.getAttribute('data-tall');
     this.description = rootElement.getAttribute('data-description');
     /** @type {?string} */
