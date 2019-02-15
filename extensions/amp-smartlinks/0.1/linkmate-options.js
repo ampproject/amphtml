ee5cbd348ebce80962631c6448f555bcda549c7c
Phil Winchester <philipwinc@gmail.com>
2019-02-15 14:20:23 -0500

    ✨Create new extension - AMP-smartlinks (#20494)
    Resolves https://github.com/ampproject/amphtml/issues/19470
    
    ## What it does
    Create an extension that lets Narrativ offer our publishers our Linkmate service in AMP pages. The extension scans the page for any outbound links the publisher wants targeted and sends them to our API for monetization. Upon API response we use the `link-rewriter` service to map that response to the links in the initial payload. When a user clicks one of the monetized links we swap the href with the monetized link. More about Linkmate [here](http://docs.narrativ.com/en/stable/linkmate.html#linkmate).

---
+++ extensions/amp-smartlinks/0.1/linkmate-options.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {userAssert} from '../../../src/log';
+
+/**
+ * Get the config values from the tag on the amp page
+ * @param {!Element} element
+ * @return {!Object}
+ */
+export function getConfigOptions(element) {
+  return {
+    nrtvSlug: getNrtvAccountName_(element),
+    linkmateEnabled: getLinkmateFlag_(element),
+    exclusiveLinks: getExclusiveLinksFlag_(element),
+    linkAttribute: getLinkAttribute_(element),
+    linkSelector: getLinkSelector_(element),
+  };
+}
+
+/**
+ * The slug used to distinguish Narrativ accounts.
+ * @param {!Element} element
+ * @return {string}
+ * @private
+ */
+function getNrtvAccountName_(element) {
+  const nrtvSlug = element.getAttribute('nrtv-account-name');
+  userAssert(nrtvSlug,
+      'amp-smartlinks: nrtv-account-name is a required field');
+
+  return nrtvSlug.toLowerCase();
+}
+
+/**
+ * Flag to specify if we are to run our Linkmate service.
+ * @param {!Element} element
+ * @return {boolean}
+ * @private
+ */
+function getLinkmateFlag_(element) {
+  const linkmateEnabled = element.getAttribute('linkmate').toLowerCase();
+  userAssert(linkmateEnabled,
+      'amp-smartlinks: linkmate is a required field');
+
+  return linkmateEnabled === 'true';
+}
+
+/**
+ * Flag to mark links as exlusive or not.
+ * @param {!Element} element
+ * @return {boolean}
+ */
+function getExclusiveLinksFlag_(element) {
+  const exclusiveLinks = element.getAttribute('exclusive-links');
+
+  return !!exclusiveLinks && exclusiveLinks.toLowerCase() === 'true';
+}
+
+/**
+ * What attribute the outbound link variable is stored in an anchor.
+ * @param {!Element} element
+ * @return {string}
+ * @private
+ */
+function getLinkAttribute_(element) {
+  const linkAttribute = element.getAttribute('link-attribute');
+
+  return linkAttribute ? linkAttribute.toLowerCase() : 'href';
+}
+
+/**
+ * Selector used to get all links that are meant to be monetized.
+ * @param {!Element} element
+ * @return {string}
+ * @private
+ */
+function getLinkSelector_(element) {
+  const linkSelector = element.getAttribute('link-selector');
+
+  return linkSelector ? linkSelector.toLowerCase() : 'a';
+}
