2ba6477431178e5d85d77f2588d7709f0ad5b6eb
PhilWinchester <phil@narrativ.com>
2019-02-20 15:34:24 -0500

    add exception to compile.js for amp-skimlinks

--- build-system/tasks/compile.js
+++ build-system/tasks/compile.js
@@ -245,6 +245,8 @@ function compile(entryModuleFilenames, outputDir, outputFilename, options) {
       'extensions/amp-viewer-assistance/**/*.js',
       // Needed for AmpViewerIntegrationVariableService
       'extensions/amp-viewer-integration/**/*.js',
+      // Needed for amp-smartlinks dep on amp-skimlinks
+      'extensions/amp-skimlinks/0.1/**/*.js',
       'src/*.js',
       'src/**/*.js',
       '!third_party/babel/custom-babel-helpers.js',
