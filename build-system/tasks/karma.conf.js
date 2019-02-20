202619ba72fe9b222c86993746c2b30ffaeb0038
Daniel Rozenberg <me@danielrozenberg.com>
2019-02-20 17:38:13 -0500

    Change all references to test server port (8081) to a constant in the Karma config file (#20896)

--- build-system/tasks/karma.conf.js
+++ build-system/tasks/karma.conf.js
@@ -18,6 +18,8 @@
 const {gitCommitterEmail} = require('../git');
 const {isTravisBuild, travisJobNumber} = require('../travis');
 
+const TEST_SERVER_PORT = 8081;
+
 const COMMON_CHROME_FLAGS = [
   // Dramatically speeds up iframe creation time.
   '--disable-extensions',
@@ -245,6 +247,7 @@ module.exports = {
     },
     captureConsole: false,
     verboseLogging: false,
+    testServerPort: TEST_SERVER_PORT,
   },
 
   singleRun: true,
