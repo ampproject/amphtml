6ebff16439e66218e8273beaca212b5585c8d1b7
Aaron Turner <aaron@aaronthedev.com>
2019-02-21 15:38:17 -0800

    Revert "🏗 Added simple Visual Diff Tests for amp-consent (#20783)" (#20999)
    This reverts commit 30d121baee5b4fb520762184fd9ec1c986bd3558.

--- build-system/app.js
+++ build-system/app.js
@@ -213,7 +213,6 @@ app.use('/api/echo/post', (req, res) => {
  * @type {RegExp}
  */
 const ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
-    '^http://.+\.localhost:8000|' +
     '^https?://.+\.herokuapp\.com');
 
 /**
@@ -223,7 +222,6 @@ const ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
  * @type {RegExp}
  */
 const SOURCE_ORIGIN_REGEX = new RegExp('^http://localhost:8000|' +
-    '^http://.+\.localhost:8000|' +
     '^https?://.+\.herokuapp\.com');
 
 app.use('/form/html/post', (req, res) => {
