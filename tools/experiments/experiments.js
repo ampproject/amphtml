420db9d7187a935585e54dbacca175a82e916109
Alan Orozco <alanorozco@users.noreply.github.com>
2019-02-25 22:43:49 -0800

    ✨auto-lightbox carousels under experiment (#20910)
    * Introduces criteria to accept any carousel that:
        1. Has one image on every slide
        2. Has no valid `on=tap` actions.
    * Since `amp-img` has to be measured against the slide element, some APIs are now async.
    * Introduces a `mixed` manual test for several cases.

--- tools/experiments/experiments.js
+++ tools/experiments/experiments.js
@@ -396,6 +396,12 @@ const EXPERIMENTS = [
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/20394',
   },
   {
+    id: 'amp-auto-lightbox-carousel',
+    name: 'Automatically detects carousels to group in a lightbox.',
+    spec: 'https://github.com/ampproject/amphtml/issues/20395',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/20394',
+  },
+  {
     id: 'fixed-elements-in-lightbox',
     name: 'Transfer fixed elements in lightboxes for smooth iOS scrolling',
     spec: 'https://github.com/ampproject/amphtml/issues/20964',
