f03cdd48ab3e3c02a64341094dea37bd56099953
松田 武大 <matsuda_takehiro@aja-kk.co.jp>
2019-02-26 10:53:55 +0900

    fix

--- ads/caaja.js
+++ ads/caaja.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {loadScript, validateData} from '../3p/3p';
+import {loadScript} from '../3p/3p';
 
 /**
  * @param {!Window} global
@@ -22,37 +22,11 @@ import {loadScript, validateData} from '../3p/3p';
  */
 export function caaja(global, data) {
 
-  
-  //validateData(data, ['aja-ssp-asi']);　キャメルか？
-
-  (global._caaja = global._caaja || {//オブジェクトリテラル　ハッシュ　きー　ばりゅー
+  (global._caaja = global._caaja || {
     sspCode: data['sspCode'],
-    //mediaId: data['mediaid'],
-
-    //htmlURL: data['htmlurl'] || global.context.canonicalUrl,
-    //ampURL: data['ampurl'] || global.context.sourceUrl,
-    //fbk: data['fbk'] || '',
-    //testMode: data['testmode'] || 'false',
-    //styleFile: data['stylefile'] || '',
-    //referrer: data['referrer'] || global.context.referrer,
   });
-  //contest ????
 
-  //amp.html case
-    url = `https://static.aja-recommend.com/html/amp.html?ssp_code=` + `encodeURIComponent(data['sspCode'])`;
-    loadScript(global,url)
-
-  //CDN case
-    //var el = document.createElement('script');
-    //el.setAttribute('src', 'https://cdn.as.amanad.adtdp.com/tag/' + ZZZZZ(media-id) + '.js');
-    //el.setAttribute('data-aja-ssp-asi', sspCode);
-    //el.setAttribute('async', 'true');
-  //完成例'Completion example'→  <script src="https://cdn.as.amanad.adtdp.com/tag/ZZZZZ.js" data-aja-ssp-asi="SSPCODE" async="true"></script>
-
-  //asot.js+render.js case
-    //todo
-    //loadScript(global, 'https://cdn.as.amanad.adtdp.com/sdk/asot-v2.js');
-
-  //amp.html nakami todo  case
+  var url = 'https://static.aja-recommend.com/html/amp.html?ssp_code=' + `encodeURIComponent(data['sspCode'])`;
+  loadScript(global,url);
 
 }
