80e1032f552c8998e38c39bfc96f6abff13d7063
Raghu Simha <rsimha@ampproject.org>
2019-02-27 17:22:52 -0500

    🏗🐛 Assorted bug fixes for PR check code (#21161)

--- build-system/pr-check/checks.js
+++ build-system/pr-check/checks.js
@@ -34,48 +34,42 @@ const FILENAME = 'checks.js';
 const timedExecOrDie =
   (cmd, unusedFileName) => timedExecOrDieBase(cmd, FILENAME);
 
+function runCommonChecks() {
+  timedExecOrDie('gulp presubmit');
+  timedExecOrDie('gulp lint');
+  timedExecOrDie('gulp ava');
+  timedExecOrDie('node node_modules/jest/bin/jest.js');
+  timedExecOrDie('gulp caches-json');
+  timedExecOrDie('gulp json-syntax');
+}
+
 function main() {
   const startTime = startTimer(FILENAME, FILENAME);
   const buildTargets = determineBuildTargets();
 
-  timedExecOrDie('gulp update-packages');
-  timedExecOrDie('gulp presubmit');
-  timedExecOrDie('gulp lint');
-
   if (!isTravisPullRequestBuild()) {
-    timedExecOrDie('gulp ava');
-    timedExecOrDie('node node_modules/jest/bin/jest.js');
-    timedExecOrDie('gulp caches-json');
-    timedExecOrDie('gulp json-syntax');
+    timedExecOrDie('gulp update-packages');
+    runCommonChecks();
+    timedExecOrDie('gulp dev-dashboard-tests');
     timedExecOrDie('gulp dep-check');
     timedExecOrDie('gulp check-types');
-    timedExecOrDie('gulp dev-dashboard-tests');
   } else {
     printChangeSummary(FILENAME);
-    if (buildTargets.has('RUNTIME') ||
-        buildTargets.has('BUILD_SYSTEM')) {
+    timedExecOrDie('gulp update-packages');
+    runCommonChecks();
 
-      timedExecOrDie('gulp ava');
-      timedExecOrDie('node node_modules/jest/bin/jest.js');
-    }
-
-    if (buildTargets.has('RUNTIME') ||
-        buildTargets.has('BUILD_SYSTEM') ||
-        buildTargets.has('UNIT_TEST') ||
-        buildTargets.has('INTEGRATION_TEST')) {
-
-      timedExecOrDie('gulp caches-json');
-      timedExecOrDie('gulp json-syntax');
-      timedExecOrDie('gulp dep-check');
-      timedExecOrDie('gulp check-types');
-    }
-
+    // Check document links only for PR builds.
     if (buildTargets.has('DOCS')) {
       timedExecOrDie('gulp check-links');
     }
 
     if (buildTargets.has('DEV_DASHBOARD')) {
       timedExecOrDie('gulp dev-dashboard-tests');
+    }
+
+    if (buildTargets.has('RUNTIME')) {
+      timedExecOrDie('gulp dep-check');
+      timedExecOrDie('gulp check-types');
     }
   }
 
