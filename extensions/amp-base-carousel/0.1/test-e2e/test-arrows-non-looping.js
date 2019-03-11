b3277c4c5a31fdd4a1477dd7eac3e0ceab125827
Carlos Vializ <cvializ@users.noreply.github.com>
2019-03-11 15:44:03 -0400

    Split up test-arrows and test-mixed-lengths (#21354)

---
+++ extensions/amp-base-carousel/0.1/test-e2e/test-arrows-non-looping.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  getNextArrow,
+  getNextArrowSlot,
+  getPrevArrowSlot,
+  getScrollingElement,
+} from './helpers';
+
+/** The total number of slides in the carousel */
+const SLIDE_COUNT = 7;
+const pageWidth = 600;
+const pageHeight = 600;
+
+describes.endtoend('AMP carousel arrows when non-looping', {
+  testUrl: 'http://localhost:8000/test/manual/amp-base-carousel/non-looping.amp.html',
+  experiments: ['amp-base-carousel', 'layers'],
+  initialRect: {width: pageWidth, height: pageHeight},
+}, async env => {
+  let controller;
+  let nextArrow;
+  let prevArrowSlot;
+  let nextArrowSlot;
+
+  function css(handle, name) {
+    return controller.getElementCssValue(handle, name);
+  }
+
+  beforeEach(async() => {
+    controller = env.controller;
+
+    prevArrowSlot = await getPrevArrowSlot(controller);
+    nextArrowSlot = await getNextArrowSlot(controller);
+    nextArrow = await getNextArrow(controller);
+  });
+
+  it('should have the arrows in the correct initial state', async() => {
+    await expect(css(prevArrowSlot, 'opacity')).to.equal('0');
+    await expect(css(nextArrowSlot, 'opacity')).to.equal('1');
+  });
+
+  it('should show the prev arrow when going to the first slide', async() => {
+    await controller.click(nextArrow);
+    await expect(css(prevArrowSlot, 'opacity')).to.equal('1');
+    await expect(css(nextArrowSlot, 'opacity')).to.equal('1');
+  });
+
+  it('should hide the next arrow when going to the end', async() => {
+    const el = await getScrollingElement(controller);
+    await controller.scrollBy(el, {left: SLIDE_COUNT * pageWidth});
+
+    await expect(css(prevArrowSlot, 'opacity')).to.equal('1');
+    await expect(css(nextArrowSlot, 'opacity')).to.equal('0');
+  });
+});
--- extensions/amp-base-carousel/0.1/test-e2e/test-mixed-lengths-no-snap.js
+++ extensions/amp-base-carousel/0.1/test-e2e/test-mixed-lengths-no-snap.js
@@ -25,7 +25,7 @@ const pageHeight = 600;
 
 describes.endtoend('AMP carousel mixed length slides', {
   testUrl: 'http://localhost:8000/test/manual/amp-base-carousel/' +
-      'mixed-lengths.amp.html',
+      'mixed-lengths-no-snap.amp.html',
   experiments: ['amp-base-carousel', 'layers'],
   initialRect: {width: pageWidth, height: pageHeight},
 }, async env => {
@@ -44,9 +44,9 @@ describes.endtoend('AMP carousel mixed length slides', {
     controller = env.controller;
   });
 
-  // Test mixed lengths with snapping. This is center aligned as that seems
-  // make the most sense for snapping mixed lengths.
-  describe('snap', () => {
+  // Test mixed lengths without snapping. This is start aligned as that seems
+  // make the most sense for non-snapping mixed lengths.
+  describe('no snap', () => {
     const slideOneWidth = 600;
     const slideTwoWidth = 400;
 
@@ -55,27 +55,22 @@ describes.endtoend('AMP carousel mixed length slides', {
 
       // First slide has width 75%, and viewport is 600 pixels wide
       await expect(prop(slides[0], 'offsetWidth')).to.equal(slideOneWidth);
-      await expect(controller.getElementRect(slides[0])).to.include({
-        x: (pageWidth - slideOneWidth) / 2,
-      });
+      await expect(controller.getElementRect(slides[0])).to.include({x: 0});
       await assertSpacerWidth(0, slideOneWidth);
       // Second slide has width 50%, and viewport is 400 pixels wide
       await expect(prop(slides[1], 'offsetWidth')).to.equal(slideTwoWidth);
       await expect(controller.getElementRect(slides[1])).to.include({
-        x: slideOneWidth + (pageWidth - slideOneWidth) / 2,
+        x: slideOneWidth,
       });
       await assertSpacerWidth(1, slideTwoWidth);
     });
 
-    it('should snap on the center point', async() => {
+    it('should scroll freely', async() => {
       const el = await getScrollingElement(controller);
       const slides = await getSlides(controller);
-      const scrollAmount = 1 + (slideOneWidth + slideTwoWidth) / 2;
 
-      await controller.scrollBy(el, {left: scrollAmount});
-      await expect(controller.getElementRect(slides[1])).to.include({
-        x: (pageWidth - slideTwoWidth) / 2,
-      });
+      await controller.scrollBy(el, {left: 10});
+      await expect(controller.getElementRect(slides[0])).to.include({x: -10});
     });
   });
 });
