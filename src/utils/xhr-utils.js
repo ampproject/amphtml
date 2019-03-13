8dde4f2bff202d0dae20fdff0020ef3b65c2a74d
Aaron Labiaga <alabiaga@google.com>
2019-03-13 09:09:48 -0400

    Add missing xhr-utils test (#21335)
    * toggle maybeChangeHeight in layers after remeasures to indicate to runtime that doc height could have changed
    
    * revert this change from master
    
    * add missing xhr-utils test
    
    * add test
    
    * fix lint
    
    * choumx comments
    
    * fixes and more tests
    
    * choumx comments
    
    * fix format

--- src/utils/xhr-utils.js
+++ src/utils/xhr-utils.js
@@ -33,7 +33,8 @@ import {isFormDataWrapper} from '../form-data-wrapper';
 const allowedMethods_ = ['GET', 'POST'];
 
 /** @private @const {string} */
-const ALLOW_SOURCE_ORIGIN_HEADER = 'AMP-Access-Control-Allow-Source-Origin';
+export const ALLOW_SOURCE_ORIGIN_HEADER =
+    'AMP-Access-Control-Allow-Source-Origin';
 
 /** @private @const {!Array<function(*):boolean>} */
 const allowedJsonBodyTypes_ = [isArray, isObject];
@@ -227,7 +228,7 @@ export function getViewerInterceptResponse(win, ampdocSingle, input, init) {
 }
 
 /**
- * Setsup URL based on ampCors
+ * Sets up URL based on ampCors
  * @param {!Window} win
  * @param {string} input
  * @param {!FetchInitDef} init The options of the XHR which may get
