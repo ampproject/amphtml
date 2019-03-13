ba98c08e171e5b4e533257fd912bd0530490edb7
Aaron Labiaga <alabiaga@google.com>
2019-03-13 16:14:52 -0400

    add missing file...

---
+++ src/url-utils.js
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {LruCache} from './utils/lru-cache';
+import {getMode} from './mode';
+import {tryDecodeUriComponent_} from './url-try-decode-uri-component';
+import {urls} from './config';
+
+/**
+ * URL related functions that don't call log functions. These functions reside
+ * here as log.js need to call cookie related functions that in turn
+ * call these URL functions. If these resided in url.js, there would be the
+ * following circular dependency log -> cookie -> url -> log, as url.js also
+ * makes calls to log.js
+ */
+
+/**
+ * Cached a-tag to avoid memory allocation during URL parsing.
+ * @type {HTMLAnchorElement}
+ */
+let a;
+
+/**
+ * We cached all parsed URLs. As of now there are no use cases
+ * of AMP docs that would ever parse an actual large number of URLs,
+ * but we often parse the same one over and over again.
+ * @type {LruCache}
+ */
+let cache;
+
+/**
+ * Tries to decode a URI component, falling back to opt_fallback (or an empty
+ * string)
+ *
+ * @param {string} component
+ * @param {string=} opt_fallback
+ * @return {string}
+ */
+export function tryDecodeUriComponent(component, opt_fallback) {
+  return tryDecodeUriComponent_(component, opt_fallback);
+}
+
+/**
+ * Returns a Location-like object for the given URL. If it is relative,
+ * the URL gets resolved.
+ * Consider the returned object immutable. This is enforced during
+ * testing by freezing the object.
+ * @param {string} url
+ * @param {boolean=} opt_nocache
+ * @return {!Location}
+ */
+export function parseUrlDeprecated(url, opt_nocache) {
+  if (!a) {
+    a = /** @type {!HTMLAnchorElement} */ (self.document.createElement('a'));
+    cache = self.UrlCache || (self.UrlCache = new LruCache(100));
+  }
+
+  return parseUrlWithA(a, url, opt_nocache ? null : cache);
+}
+
+/**
+ * Returns a Location-like object for the given URL. If it is relative,
+ * the URL gets resolved.
+ * Consider the returned object immutable. This is enforced during
+ * testing by freezing the object.
+ * @param {!HTMLAnchorElement} a
+ * @param {string} url
+ * @param {LruCache=} opt_cache
+ * @return {!Location}
+ * @restricted
+ */
+export function parseUrlWithA(a, url, opt_cache) {
+  if (opt_cache && opt_cache.has(url)) {
+    return opt_cache.get(url);
+  }
+
+  a.href = url;
+
+  // IE11 doesn't provide full URL components when parsing relative URLs.
+  // Assigning to itself again does the trick #3449.
+  if (!a.protocol) {
+    a.href = a.href;
+  }
+
+  const info = /** @type {!Location} */({
+    href: a.href,
+    protocol: a.protocol,
+    host: a.host,
+    hostname: a.hostname,
+    port: a.port == '0' ? '' : a.port,
+    pathname: a.pathname,
+    search: a.search,
+    hash: a.hash,
+    origin: null, // Set below.
+  });
+
+  // Some IE11 specific polyfills.
+  // 1) IE11 strips out the leading '/' in the pathname.
+  if (info.pathname[0] !== '/') {
+    info.pathname = '/' + info.pathname;
+  }
+
+  // 2) For URLs with implicit ports, IE11 parses to default ports while
+  // other browsers leave the port field empty.
+  if ((info.protocol == 'http:' && info.port == 80)
+      || (info.protocol == 'https:' && info.port == 443)) {
+    info.port = '';
+    info.host = info.hostname;
+  }
+
+  // For data URI a.origin is equal to the string 'null' which is not useful.
+  // We instead return the actual origin which is the full URL.
+  if (a.origin && a.origin != 'null') {
+    info.origin = a.origin;
+  } else if (info.protocol == 'data:' || !info.host) {
+    info.origin = info.href;
+  } else {
+    info.origin = info.protocol + '//' + info.host;
+  }
+
+  // Freeze during testing to avoid accidental mutation.
+  const frozen = (getMode().test && Object.freeze) ? Object.freeze(info) : info;
+
+  if (opt_cache) {
+    opt_cache.put(url, frozen);
+  }
+
+  return frozen;
+}
+
+/**
+ * Returns whether the URL has the origin of a proxy.
+ * @param {string|!Location} url URL of an AMP document.
+ * @return {boolean}
+ */
+export function isProxyOrigin(url) {
+  if (typeof url == 'string') {
+    url = parseUrlDeprecated(url);
+  }
+  return urls.cdnProxyRegex.test(url.origin);
+}
