6199885f862e950e467a1bbaae8360a41df743ee
Kirk Elliott <kirk.elliott@oracle.com>
2019-03-22 13:08:14 -0400

    allows for overriding of widget via anonymous mode

---
+++ extensions/amp-addthis/0.1/addthis-utils/getWidgetIdOverloadedWithJSONForAnonymousMode.js
@@ -0,0 +1,44 @@
+import {isBoolean} from './boolean';
+import {isNumber} from './number';
+import {isString} from './string';
+
+const overrideKeys = ['backgroundColor', 'borderRadius', 'counterColor',
+  'counts', 'countsFontSize', 'desktopPosition', 'elements', 'hideDevice',
+  'hideEmailSharingConfirmation', 'hideLabel','iconColor', 'label',
+  'mobilePosition', 'numPreferredServices', 'offset', 'originalServices',
+  'postShareFollowMsg', 'postShareRecommendedMsg', 'postShareTitle',
+  'responsive', 'shareCountThreshold', 'size', 'style', 'textColor',
+  'thankyou', 'titleFontSize', '__hideOnHomepage',
+];
+
+/**
+ * Get Widget ID Overloaded With JSON For Anonymous Mode
+ * If no argument or self doesnt have element.getAttribute, returns empty string
+ * For each existing attribute: `data-attr-NAME`, check and add value for key
+ * If object is not empty, return only the JSON of the override object
+ * If an error happens return empty string
+ * @param {AmpAddThis} self
+ * @return {string} JSON | empty string means there is no override object
+ */
+export const getWidgetOverload = self => {
+  const getAttribute = self && self.element && self.element.getAttribute;
+  if (!getAttribute) {
+    return '';
+  }
+  const override = {};
+  overrideKeys.forEach(item => {
+    const data = self.element.getAttribute(`data-attr-${item}`);
+    if (data !== null) {
+      if (isString(data) || isNumber(data) || isBoolean(data)) {
+        override[item] = data;
+      }
+    }
+  });
+  let returnValue = '';
+  if (Object.keys(override).length > 0) {
+    try {
+      returnValue = JSON.stringify(override);
+    } catch (e) {}
+  }
+  return returnValue;
+};
