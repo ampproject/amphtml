f2f603604705859abc103f369143c862b9f508ca
Carlos Vializ <cvializ@users.noreply.github.com>
2019-03-26 16:38:03 -0400

    🐛Fix bind data when start and end are the same (#21566)
    * Fix bind data when start and end are the same
    
    * Use standard for loop instead to avoid complexity
    
    * Fix circular reference error from bind
    
    * Use isSameOrAfter
    
    * Fix missing externs

--- extensions/amp-date-picker/0.1/amp-date-picker.js
+++ extensions/amp-date-picker/0.1/amp-date-picker.js
@@ -38,7 +38,7 @@ import {createDeferred} from './react-utils';
 import {createSingleDatePicker} from './single-date-picker';
 import {dashToCamelCase} from '../../../src/string';
 import {dev, user, userAssert} from '../../../src/log';
-import {dict} from '../../../src/utils/object';
+import {dict, map} from '../../../src/utils/object';
 import {escapeCssSelectorIdent} from '../../../src/css';
 import {once} from '../../../src/utils/function';
 import {requireExternal} from '../../../src/module';
@@ -78,10 +78,10 @@ class BindDatesDetails {
     this['dates'] = dates;
 
     /** @const */
-    this['start'] = dates[0];
+    this['start'] = map(dates[0]);
 
     /** @const */
-    this['end'] = dates[dates.length - 1];
+    this['end'] = map(dates[dates.length - 1]);
   }
 }
 
@@ -1453,14 +1453,13 @@ export class AmpDatePicker extends AMP.BaseElement {
    */
   iterateDateRange_(startDate, endDate, cb) {
     const normalizedEndDate = endDate || startDate;
-    if (!normalizedEndDate.isAfter(startDate)) {
+    if (!normalizedEndDate.isSameOrAfter(startDate)) {
       return;
     }
 
-    const index = startDate.clone();
-    while (!index.isAfter(normalizedEndDate)) {
-      cb(index.clone());
-      index.add(1, 'days');
+    const days = normalizedEndDate.diff(startDate, 'days') + 1;
+    for (let i = 0; i < days; i++) {
+      cb(startDate.clone().add(i + 1, 'days'));
     }
   }
 
