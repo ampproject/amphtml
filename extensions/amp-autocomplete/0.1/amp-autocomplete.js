d177cf01260eec09eb4c3ee8923a9cc9645ae80e
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2019-03-27 11:55:08 -0400

    ✨Expose the `select` event on amp-autocomplete. (#21582)
    * Expose select action on autocomplete
    
    * Update jsdoc and lint
    
    * Update documentation
    
    * Add unit test
    
    * Carlos comments

--- extensions/amp-autocomplete/0.1/amp-autocomplete.js
+++ extensions/amp-autocomplete/0.1/amp-autocomplete.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import {ActionTrust} from '../../../src/action-constants';
 import {CSS} from '../../../build/amp-autocomplete-0.1.css';
 import {Keys} from '../../../src/utils/key-codes';
 import {Layout} from '../../../src/layout';
@@ -22,6 +23,7 @@ import {UrlReplacementPolicy,
   batchFetchJsonFor} from '../../../src/batched-json';
 import {childElementsByTag,
   removeChildren} from '../../../src/dom';
+import {createCustomEvent} from '../../../src/event-helper';
 import {dev, user, userAssert} from '../../../src/log';
 import {getValueForExpr, tryParseJson} from '../../../src/json';
 import {includes, startsWith} from '../../../src/string';
@@ -120,6 +122,9 @@ export class AmpAutocomplete extends AMP.BaseElement {
      * @private {?Element}
      */
     this.templateElement_ = null;
+
+    /** @private {?../../../src/service/action-impl.ActionService} */
+    this.action_ = null;
   }
 
   /** @override */
@@ -127,6 +132,7 @@ export class AmpAutocomplete extends AMP.BaseElement {
     userAssert(isExperimentOn(this.win, 'amp-autocomplete'),
         `Experiment ${EXPERIMENT} is not turned on.`);
 
+    this.action_ = Services.actionServiceForDoc(this.element);
     const jsonScript =
       this.element.querySelector('script[type="application/json"]');
     if (jsonScript) {
@@ -222,7 +228,6 @@ export class AmpAutocomplete extends AMP.BaseElement {
     container.classList.add('i-amphtml-autocomplete-results');
     container.setAttribute('role', 'list');
     toggle(container, false);
-    this.applyFillContent(container, /* replacedContent */ true);
     return container;
   }
 
@@ -479,7 +484,20 @@ export class AmpAutocomplete extends AMP.BaseElement {
       return;
     }
     this.inputElement_.value = this.userInput_ = element.getAttribute('value');
+    this.fireSelectEvent_(this.userInput_);
     this.clearAllItems_();
+  }
+
+  /**
+   * Triggers a 'select' event with the given value as the value emitted.
+   * @param {string} value
+   * @private
+   */
+  fireSelectEvent_(value) {
+    const name = 'select';
+    const selectEvent = createCustomEvent(this.win,
+        `amp-autocomplete.${name}`, value);
+    this.action_.trigger(this.element, name, selectEvent, ActionTrust.HIGH);
   }
 
   /**
