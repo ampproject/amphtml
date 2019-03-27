f24307d765e1a4d398ca45ff0071416357f8be87
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2019-03-27 17:39:53 -0400

    🐛Accept "ol" as a child of amp-sidebar > nav (#21560)
    * Accept "ol" as a child of amp-sidebar > nav
    
    * Add additional validator examples
    
    * Ali comments
    
    * Alpha sort "OL" over "UL"
    
    * Consistent angle brackets
    
    * Add two visual tests for amp-sidebar's toolbar

---
+++ examples/visual-tests/amp-sidebar/amp-sidebar-toolbar.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview Description of this file.
+ */
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const {verifySelectorsVisible} = require('../../../build-system/tasks/visual-diff/helpers');
+
+module.exports = {
+  'open sidebar toolbar': async (page, name) => {
+    await page.tap('[on="tap:sidebar.toggle"]');
+    await verifySelectorsVisible(page, name, ['amp-sidebar[open]']);
+    await verifySelectorsVisible(page, name, ['nav[toolbar]']);
+  },
+};
