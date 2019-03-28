c9ccaf974f468423aa4c4d89337ac144fe3461d5
Raghu Simha <rsimha@ampproject.org>
2019-03-28 19:17:28 -0400

    Fix single-pass build

--- build-system/amp.multipass.extern.js
+++ build-system/amp.multipass.extern.js
@@ -34,3 +34,45 @@ SomeBaseElementLikeClass.prototype.defaultActionAlias_;
 // Externed explicitly because this private property is read across
 // binaries.
 Element.prototype.implementation_ = {};
+
+// Externed explicitly because we do not export Class shaped names
+// by default.
+/**
+ * This uses the internal name of the type, because there appears to be no
+ * other way to reference an ES6 type from an extern that is defined in
+ * the app.
+ * @constructor @struct
+ * @extends {BaseElement$$module$src$base_element}
+ */
+AMP.BaseElement = class {
+  /** @param {!AmpElement} element */
+  constructor(element) {}
+};
+
+/**
+ * This uses the internal name of the type, because there appears to be no
+ * other way to reference an ES6 type from an extern that is defined in
+ * the app.
+ * @constructor @struct
+ * @extends {AmpAdXOriginIframeHandler$$module$extensions$amp_ad$0_1$amp_ad_xorigin_iframe_handler}
+ */
+AMP.AmpAdXOriginIframeHandler = class {
+  /**
+   * @param {!AmpAd3PImpl$$module$extensions$amp_ad$0_1$amp_ad_3p_impl|!AmpA4A$$module$extensions$amp_a4a$0_1$amp_a4a} baseInstance
+   */
+  constructor(baseInstance) {}
+};
+
+/**
+ * This uses the internal name of the type, because there appears to be no
+ * other way to reference an ES6 type from an extern that is defined in
+ * the app.
+ * @constructor @struct
+ * @extends {AmpAdUIHandler$$module$extensions$amp_ad$0_1$amp_ad_ui}
+ */
+AMP.AmpAdUIHandler = class {
+  /**
+   * @param {!AMP.BaseElement} baseInstance
+   */
+  constructor(baseInstance) {}
+};
