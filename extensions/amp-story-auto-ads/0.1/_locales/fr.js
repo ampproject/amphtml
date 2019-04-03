039129046c20a81fb4d29c02c4d4fa0bb88387c9
Jon Newmuis <newmuis@users.noreply.github.com>
2019-04-03 12:08:20 -0400

    🌐 Add ability for amp-story-auto-ads CTA strings to be localized (#21253)
    * Add localized strings for amp-story-auto-ads
    
    * Update path to localization service
    
    * Use localization service to lookup string
    
    * Update file paths
    
    * Move amp-story-auto-ads strings from amp-story to amp-story-auto-ads
    
    * Wrap line
    
    * Use same set of languages for amp-story-auto-ads that amp-story is using
    
    * Update dep check whitelist
    
    * Remove whitespace
    
    * Whitelist amp-story-auto-ads to depend on localization

---
+++ extensions/amp-story-auto-ads/0.1/_locales/fr.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  LocalizedStringBundleDef,
+  LocalizedStringId,
+} from '../../../../src/localized-strings';
+
+/**
+ * Localized string bundle used for fr strings.
+ * @const {!LocalizedStringBundleDef}
+ */
+export default /** @const {!LocalizedStringBundleDef} */ ({
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_APPLY_NOW]: {
+    string: 'Inscrivez-vous dès maintenant',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_BOOK_NOW]: {
+    string: 'Réserver',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_BUY_TICKETS]: {
+    string: 'Acheter des billets',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_DOWNLOAD]: {
+    string: 'Télécharger',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_EXPLORE]: {
+    string: 'Découvrir',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_GET_NOW]: {
+    string: 'Obtenir',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_INSTALL]: {
+    string: 'Installer',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_LEARN_MORE]: {
+    string: 'En savoir plus',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_LISTEN]: {
+    string: 'Écouter',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_MORE]: {
+    string: 'Plus',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_OPEN_APP]: {
+    string: 'Ouvrir l\'application',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_ORDER_NOW]: {
+    string: 'Commander',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_PLAY]: {
+    string: 'Lire',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_READ]: {
+    string: 'Lire',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_SHOP]: {
+    string: 'Acheter',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_SHOW]: {
+    string: 'Afficher',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_SHOWTIMES]: {
+    string: 'Horaires des séances',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_SIGN_UP]: {
+    string: 'S\'inscrire',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_SUBSCRIBE]: {
+    string: 'S\'abonner',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_USE_APP]: {
+    string: 'Utiliser l\'application',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_VIEW]: {
+    string: 'Afficher',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_WATCH]: {
+    string: 'Regarder',
+  },
+  [LocalizedStringId.AMP_STORY_AUTO_ADS_BUTTON_LABEL_WATCH_EPISODE]: {
+    string: 'Regarder l\'épisode',
+  },
+});
