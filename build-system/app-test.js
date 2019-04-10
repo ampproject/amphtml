4dc65258c6e5fa074875a2607d409e0973d1435d
Carlos Vializ <cvializ@vt.edu>
2019-04-10 10:39:04 -0400

    Remove unused endpoints

--- build-system/app-test.js
+++ build-system/app-test.js
@@ -19,51 +19,6 @@
  * @param {Express} app
  */
 function appTestEndpoints(app) {
-  app.use('/get', function(req, res) {
-    res.json({
-      args: req.query,
-      headers: req.headers,
-    });
-  });
-
-  app.use('/redirect-to', function(req, res) {
-    res.redirect(302, req.query.url);
-  });
-
-
-  app.use('/status/404', function(req, res) {
-    res.status(404).end();
-  });
-
-  app.use('/status/500', function(req, res) {
-    res.status(500).end();
-  });
-
-  app.use('/cookies/set', function(req, res) {
-    delete req.query.__amp_source_origin;
-    for (const name in req.query) {
-      res./*OK*/cookie(name, req.query[name]);
-    }
-    res.json({
-      cookies: req.cookies || {},
-    });
-  });
-
-  app.use('/response-headers', function(req, res) {
-    delete req.query.__amp_source_origin;
-    for (const name in req.query) {
-      res.setHeader(name, req.query[name]);
-    }
-    res.json({});
-  });
-
-  app.use('/post', function(req, res) {
-    delete req.query.__amp_source_origin;
-    res.json({
-      json: req.body,
-    });
-  });
-
   app.use('/form/post/success', function(req, res) {
     const sourceOrigin = req.query['__amp_source_origin'];
     if (sourceOrigin) {
@@ -77,40 +32,6 @@ function appTestEndpoints(app) {
         {title: 'Basketball'},
         {title: 'Writing'},
       ],
-    });
-  });
-
-  app.use('/form/post/error', function(req, res) {
-    const sourceOrigin = req.query['__amp_source_origin'];
-    if (sourceOrigin) {
-      res.setHeader('AMP-Access-Control-Allow-Source-Origin', sourceOrigin);
-    }
-    delete req.query.__amp_source_origin;
-    res.status(500).json({
-      error: 'alreadySubscribed',
-      name: 'John Miller',
-      email: 'john@miller.what',
-    });
-  });
-
-  app.use('/form/post', function(req, res) {
-    const sourceOrigin = req.query['__amp_source_origin'];
-    if (sourceOrigin) {
-      res.setHeader('AMP-Access-Control-Allow-Source-Origin', sourceOrigin);
-    }
-    delete req.query.__amp_source_origin;
-    res.json({
-      json: req.body,
-    });
-  });
-
-  app.use('/form/verify-error', function(req, res) {
-    const sourceOrigin = req.query['__amp_source_origin'];
-    if (sourceOrigin) {
-      res.setHeader('AMP-Access-Control-Allow-Source-Origin', sourceOrigin);
-    }
-    res.status(400).json({
-      verifyErrors: [{name: 'email', message: 'That email is already taken.'}],
     });
   });
 }
