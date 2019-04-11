94a195012dfd2df7364348feb60145ccf6d9fb3b
Caroline Liu <carolineliu@google.com>
2019-04-11 18:21:04 -0400

    Add third test case

--- extensions/amp-autocomplete/0.1/test-e2e/test-amp-autocomplete.js
+++ extensions/amp-autocomplete/0.1/test-e2e/test-amp-autocomplete.js
@@ -42,14 +42,40 @@ describes.endtoend('amp-autocomplete', {
   it('<amp-autocomplete> should display results on focus', async() => {
     const renderedResults =
       await controller.findElement('.i-amphtml-autocomplete-results');
+    await expect(controller.getElementAttribute(renderedResults, 'hidden'))
+        .not.to.be.null;
     const focusButton = await controller.findElement('#focusButton');
     await controller.click(focusButton);
+
+    // Displays all suggested items on focus.
     const itemElements =
       await controller.findElements('.i-amphtml-autocomplete-item');
     await expect(renderedResults).not.to.be.null;
-    await expect(controller.getElementCssValue(renderedResults, 'visibility'))
-        .to.equal('visible');
+    await expect(controller.getElementAttribute(renderedResults, 'hidden'))
+        .to.be.null;
     await expect(itemElements).to.have.length(3);
+  });
+
+  it('<amp-autocomplete> should narrow suggestions to input', async() => {
+    const renderedResults =
+      await controller.findElement('.i-amphtml-autocomplete-results');
+    const focusButton = await controller.findElement('#focusButton');
+    const input = await controller.findElement('#input');
+    await expect(input).not.to.be.null;
+    await controller.click(focusButton);
+
+    // Displays all suggested items on focus.
+    const itemElements =
+      await controller.findElements('.i-amphtml-autocomplete-item');
+    await expect(itemElements).to.have.length(3);
+
+    // New input narrows down suggested items.
+    await controller.type(input, 'an');
+    await expect(controller.getElementAttribute(renderedResults, 'hidden'))
+        .to.be.null;
+    const newItemElements =
+      await controller.findElements('.i-amphtml-autocomplete-item');
+    await expect(newItemElements).to.have.length(2);
   });
 
 });
