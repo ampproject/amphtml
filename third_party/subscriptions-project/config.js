49d693a3fa8ffb64735af20ab29d8e6e629f5985
sohanirao <35511558+sohanirao@users.noreply.github.com>
2019-04-11 15:18:08 -0700

    SwG Release 0.1.22.49 (#21825)
    * SwG Release 0.1.22.49
    
    * Fix error in PR check
    
    * Remove type
    
    * Fixed error

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.48 */
+/** Version: 0.1.22.49 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
