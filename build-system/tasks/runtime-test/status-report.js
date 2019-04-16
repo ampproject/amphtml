112aa2a917230deb20d955704fe70e8f570d11a7
Daniel Rozenberg <rodaniel@ampproject.org>
2019-04-16 15:25:54 +0900

    Debugging statements

--- build-system/tasks/runtime-test/status-report.js
+++ build-system/tasks/runtime-test/status-report.js
@@ -98,7 +98,9 @@ exports.reportTestStarted = () => {
 
 exports.reportAllExpectedTests = async buildTargets => {
   for (const [type, subTypes] of TEST_TYPE_SUBTYPES) {
-    const action = TEST_TYPE_ACTIVATORS.get(type).some(buildTargets.has)
+    const testTypeActivators = TEST_TYPE_ACTIVATORS.get(type);
+    log(green('DEBUG:'), type, testTypeActivators, buildTargets);
+    const action = testTypeActivators.some(buildTargets.has)
       ? 'queued' : 'skipped';
     for (const subType of subTypes) {
       await postReport(`${type}/${subType}`, action);
