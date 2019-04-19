1deec95ba7fa256b40a443ec041ac441d0826b89
Kirk Elliott <1675992+dmvjs@users.noreply.github.com>
2019-04-18 23:22:30 -0400

    ✨add wp anonymous mode (#21552)
    * register product code for anonymous mode
    
    * adds util methods for identifying strings, numbers, and booleans
    
    * allows for overriding of widget via anonymous mode
    
    * sets mode
    gets widget overload for anonymous mode transport
    
    * adds method to tell if a candidate is a function
    
    * creates an override for anonymous mode widgets
    
    * adds tests for new utils and override functionality
    
    * updates protoascii for validator for new attributes
    pub id is no longer mandatory with anonymous tools
    
    * fixes a typo
    
    * minor fixes
    
    * updates year for Copyright
    
    * updates protoascii and validtion html and output
    
    * reverting a typo
    
    * string is too long so using length instead
    
    * removes redundant check
    
    * removes unnecessary attrs
    
    * gets canonical url correctly
    
    * add license
    
    * uses a more general type for self
    
    * uses a more general type for self
    
    * uses a more general type for self
    
    * using string instead of JsonObject
    
    * fixes mode to be a number type
    
    * removing utility methods in favor of using conventional typeof
    
    * removed string method and cleaned up to match repo conventions
    
    * uses new filename
    
    * changes year back to 2018
    
    * changes year back to 2018
    
    * removes utility check methods
    exports JSON
    
    * removes utility check methods
    exports JSON
    
    * allows for candidate to be any type
    
    * removes utility check methods
    exports JSON
    
    * removes tests that we dont use anymore
    due to reduced cyclomatic complexity
    
    * changes year back to 2018 for files not created in 2019

---
+++ extensions/amp-addthis/0.1/addthis-utils/get-widget-id-overloaded-with-json-for-anonymous-mode.js
@@ -0,0 +1,48 @@
+/**
+* Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS-IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {dict} from '../../../../src/utils/object';
+
+const overrideKeys = ['backgroundColor', 'borderRadius', 'counterColor',
+  'counts', 'countsFontSize', 'desktopPosition', 'elements', 'hideDevice',
+  'hideEmailSharingConfirmation', 'hideLabel','iconColor', 'label',
+  'mobilePosition', 'numPreferredServices', 'offset', 'originalServices',
+  'postShareFollowMsg', 'postShareRecommendedMsg', 'postShareTitle',
+  'responsive', 'shareCountThreshold', 'size', 'style', 'textColor',
+  'thankyou', 'titleFontSize', '__hideOnHomepage',
+];
+
+/**
+ * Get Widget ID Overloaded With JSON For Anonymous Mode
+ * If no argument or self doesnt have element.getAttribute, returns empty string
+ * For each existing attribute: `data-attr-NAME`, check and add value for key
+ * If object is not empty, return only the JSON of the override object
+ * If an error happens return empty string
+ * @param {AMP.BaseElement} self
+ * @return {string} empty string means there is no override object
+ */
+export function getWidgetOverload(self) {
+  const override = dict({});
+  overrideKeys.forEach(item => {
+    const data = self.element.getAttribute(`data-attr-${item}`);
+    if (typeof data === 'string' ||
+      typeof data === 'number' ||
+      typeof data === 'boolean') {
+      override[String(item)] = data;
+    }
+  });
+  const overrideString = JSON.stringify(override);
+  return overrideString === '{}' ? '' : overrideString;
+}
