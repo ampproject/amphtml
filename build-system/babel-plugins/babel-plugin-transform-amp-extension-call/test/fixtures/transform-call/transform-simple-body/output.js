8e2a2dcdbabcc43561e535f4064017507f38c3cc
Erwin Mombay <erwinm@google.com>
2019-04-30 02:50:50 -0700

    add plugin transformer to AMP.extension call for singlepass

---
+++ build-system/babel-plugins/babel-plugin-transform-amp-extension-call/test/fixtures/transform-call/transform-simple-body/output.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function (AMP) {
+  // First, unregister template to avoid "Duplicate template type"
+  // error due to multiple versions of amp-mustache in the same unit test run.
+  // This is due to transpilation of test code to ES5 which uses require() and,
+  // unlike import, causes side effects (AMP.registerTemplate) to be run.
+  // For unit tests, it doesn't actually matter which version of amp-mustache is
+  // registered. Integration tests should only have one script version included.
+  if (getMode().test) {
+    Services.templatesFor(window).unregisterTemplate(TAG);
+  }
+
+  AMP.registerTemplate(TAG, AmpMustache);
+})(self.AMP);
