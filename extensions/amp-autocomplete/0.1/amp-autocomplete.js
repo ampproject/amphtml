c2cadf035379b13c7fe31e5936ed215bd7bbadfa
Caroline Liu <carolineliu@google.com>
2019-05-08 11:51:10 -0400

    Restore distanceToContainerTop_ workaround

--- extensions/amp-autocomplete/0.1/amp-autocomplete.js
+++ extensions/amp-autocomplete/0.1/amp-autocomplete.js
@@ -789,11 +789,8 @@ export class AmpAutocomplete extends AMP.BaseElement {
     let shouldScroll, newTop;
 
     return this.measureMutateElement(() => {
-      const categoryEl = newActiveElement.closest(
-          '.i-amphtml-autocomplete-results > [data-category]');
-      const addedOffset = categoryEl ? categoryEl./*OK*/offsetTop : 0;
-      const itemTop = activeIndex === 0 ? 0 :
-        newActiveElement./*OK*/offsetTop + addedOffset;
+      const itemTop = activeIndex === 0 ? 
+        0 : this.distanceToContainerTop_(newActiveElement);
       const {offsetHeight: itemHeight} = newActiveElement;
       const {scrollTop: resultTop, offsetHeight: resultHeight} =
         this.container_;
@@ -810,6 +807,22 @@ export class AmpAutocomplete extends AMP.BaseElement {
       this.activeElement_ = newActiveElement;
     });
   }
+
+  /** 
+   * Calculates the total offset distance from the given element to the 
+   * container_. This assumes the given parameter is a descendent of the 
+   * results container. 
+   * 
+   * @param {Element} element 
+   * @return {number}
+   * @private
+   */
+  distanceToContainerTop_(element) {
+    const {offsetTop: top, parentElement: parent} = element;
+    if (!parent || parent === this.container_) {
+      return top;
+    }
+    return top + this.distanceToContainerTop_(parent);
 
   /** Returns all item elements in the results container that do not have the
    * 'data-disabled' attribute.
