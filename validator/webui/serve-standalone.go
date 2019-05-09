40da075a5dc687c2f070209415ac4a2ee006be0a
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2019-05-09 16:58:09 -0700

    validator.amp.dev is live, update links (#22231)
    * validator.amp.dev is live, update links
    
    * Escape local instance url

--- validator/webui/serve-standalone.go
+++ validator/webui/serve-standalone.go
@@ -138,7 +138,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		req.Header.Add("User-Agent",
 			"Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MTC19V) "+
 				"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile "+
-				"Safari/537.36 (compatible; validator.ampproject.org)")
+				"Safari/537.36 (compatible; validator.amp.dev)")
 		resp, err := netClient.Do(req)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Bad gateway (%v)", err.Error()),
