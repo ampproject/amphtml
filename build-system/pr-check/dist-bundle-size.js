e354db8928bef9959f7d5903ae9f551692d2230a
Raghu Simha <rsimha@ampproject.org>
2019-05-13 19:09:21 -0400

    🏗✨🐛 Run tests against minified code during PR checks (#22263)

--- build-system/pr-check/dist-bundle-size.js
+++ build-system/pr-check/dist-bundle-size.js
@@ -17,34 +17,65 @@
 
 /**
  * @fileoverview
- * This script builds the AMP runtime for production.
+ * This script builds the AMP runtime for production and runs the bundle size
+ * check.
  * This is run during the CI stage = build; job = dist.
  */
 
 const colors = require('ansi-colors');
 const {
+  areValidBuildTargets,
+  determineBuildTargets,
+} = require('./build-targets');
+const {
   printChangeSummary,
   startTimer,
   stopTimer,
   timedExecOrDie: timedExecOrDieBase,
-  uploadDistOutput} = require('./utils');
+  uploadDistOutput,
+} = require('./utils');
 const {isTravisPullRequestBuild} = require('../travis');
-const FILENAME = 'dist.js';
+const {runYarnChecks} = require('./yarn-checks');
+
+const FILENAME = 'dist-bundle-size.js';
 const FILELOGPREFIX = colors.bold(colors.yellow(`${FILENAME}:`));
 const timedExecOrDie =
   (cmd, unusedFileName) => timedExecOrDieBase(cmd, FILENAME);
 
+
 function main() {
   const startTime = startTimer(FILENAME, FILENAME);
+  const buildTargets = determineBuildTargets();
+  if (!runYarnChecks(FILENAME) ||
+      !areValidBuildTargets(buildTargets, FILENAME)) {
+    stopTimer(FILENAME, FILENAME, startTime);
+    process.exitCode = 1;
+    return;
+  }
 
   if (!isTravisPullRequestBuild()) {
     timedExecOrDie('gulp update-packages');
     timedExecOrDie('gulp dist --fortesting');
+    timedExecOrDie('gulp bundle-size --on_push_build');
     uploadDistOutput(FILENAME);
   } else {
-    printChangeSummary();
-    console.log(`${FILELOGPREFIX} Skipping ` + colors.cyan('Dist ') +
-        'because this is a PR build');
+    printChangeSummary(FILENAME);
+    if (buildTargets.has('RUNTIME') ||
+        buildTargets.has('UNIT_TEST') ||
+        buildTargets.has('INTEGRATION_TEST') ||
+        buildTargets.has('BUILD_SYSTEM') ||
+        buildTargets.has('FLAG_CONFIG')) {
+      timedExecOrDie('gulp update-packages');
+      timedExecOrDie('gulp dist --fortesting');
+      timedExecOrDie('gulp bundle-size --on_pr_build');
+      uploadDistOutput(FILENAME);
+    } else {
+      timedExecOrDie('gulp bundle-size --on_skipped_build');
+      console.log(`${FILELOGPREFIX} Skipping ` +
+          colors.cyan('Dist, Bundle Size ') +
+          'because this commit does not affect the runtime, build system, ' +
+          'test files, or visual diff files');
+    }
   }
 
   stopTimer(FILENAME, FILENAME, startTime);
