cc8df160e836a56a90b0f677241540723909ca9b
sushan04 <45952279+sushan04@users.noreply.github.com>
2019-05-14 16:20:13 -0400

    Update iframe-transport-vendors.js

--- extensions/amp-analytics/0.1/iframe-transport-vendors.js
+++ extensions/amp-analytics/0.1/iframe-transport-vendors.js
@@ -28,3 +28,8 @@ export const IFRAME_TRANSPORTS = /** @type {!JsonObject} */ ({
   'bg': 'https://tpc.googlesyndication.com/b4a/b4a-runner.html',
   'moat': 'https://z.moatads.com/ampanalytics093284/iframe.html',
 });
+
+export const IFRAME_TRANSPORTS_CANARY = /** @type {!JsonObject} */ ({
+  'bg': 'https://tpc.googlesyndication.com/b4a/experimental/b4a-runner.html',
+  'moat': 'https://z.moatads.com/ampanalytics093284/iframe.html',
+});
