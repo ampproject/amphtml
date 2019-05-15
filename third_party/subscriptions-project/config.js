90edc9b96b84933f5780a47722ea72e8ff39a4f6
Dipak <dipak.parikh@gmail.com>
2019-05-15 08:28:56 -0700

    SwG release 0.1.22.51 (#22301)

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.50 */
+/** Version: 0.1.22.51 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
