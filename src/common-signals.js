7746e2897c7af707398f6e0dfb72be65bb0839c8
zhouyx <zhouyx@google.com>
2019-05-16 15:58:27 -0700

    improve comment

--- src/common-signals.js
+++ src/common-signals.js
@@ -32,9 +32,14 @@ export const CommonSignals = {
 
   /**
    * Rendering has been confirmed to have been started.
-   * RENDER_START is an optional signal, implemented by ads, shadowdoc.
-   * The signal instruct the content has started rendering, and is related
-   * to toggle visibility.
+   * It is a signal to indicate meaningful display (e.g. text could be displayed
+   * CSS is correctly installed/applied).
+   *
+   * Elements can optionally implement RENDER_START signal. (e.g. ad, shadowdoc)
+   * if it want to define its own meaningful display time and toggle visibility.
+   *
+   * Simpler elements's RENDER_START can be equal to the start of the
+   * buildCallback
    */
   RENDER_START: 'render-start',
 
@@ -47,9 +52,12 @@ export const CommonSignals = {
 
   /**
    * The initial contents of an element/document/embed have been loaded.
-   * INI_LOAD is an optional signal, implemented by ads, story.
-   * It instructs that all critical resources has been loade, and is related
-   * to more acurate measurement.
+   * INI_LOAD is an optional signal, implemented by ads, story, and elements
+   * that consist of other resources.
+   * It instructs that all critical resources has been loaded, and can be used
+   * for more acurate visibility measurement.
+   * When an element doesn't consist multiple child resources, LOAD_END signal
+   * can be used to indicate resource load completion.
    * Note: Based on the implementation, INI_LOAD can trigger before or after
    * LOAD_END.
    */
