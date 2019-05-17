0973d301a0360954287b098307a28990776f1bcf
William Chou <willchou@google.com>
2019-05-17 00:08:17 -0400

    [AoG] Fix two race conditions in amp-bind/amp-list when backgrounded (#22227)
    * Fix two race conditions in amp-bind and amp-list.
    
    * Actually the amp-list issue is due to pre-layout mutation.
    
    * Clean up amp-bind changes.
    
    * Don't fetch empty string src.
    
    * Only reset if laid out.
    
    * Fix types.
    
    * Fix tests.
    
    * Fix other tests.
    
    * Call parseAndUpdate() instead of build().
    
    * Adjust tests.
    
    * Fix tests.
    
    * Add CustomElement.whenUpgraded() for use in getImpl().
    
    * Wait for CE upgrade.
    
    * Fix lint and bad merge.
    
    * Fix tests again.
    
    * Fix comment grammar.
    
    * Minor readability improvements.

--- extensions/amp-list/0.1/test/test-amp-list.js
+++ extensions/amp-list/0.1/test/test-amp-list.js
@@ -387,12 +387,11 @@ describes.repeated(
             });
           });
 
-          it('fetch should resolve if `src` is empty', () => {
+          it('fetch should not be called if `src` is missing or empty', () => {
             const spy = sandbox.spy(list, 'fetchList_');
             element.setAttribute('src', '');
-
             return list.layoutCallback().then(() => {
-              expect(spy).to.have.been.calledOnce;
+              expect(spy).to.not.be.called;
             });
           });
 
@@ -709,7 +708,7 @@ describes.repeated(
             setBindService(bind);
           });
 
-          it('should _not_ refetch if [src] attr changes (before layout)', () => {
+          it('should not fetch if [src] mutates with URL (before layout)', () => {
             // Not allowed before layout.
             listMock.expects('fetchList_').never();
 
@@ -722,7 +721,17 @@ describes.repeated(
             );
           });
 
-          it('should render and remove `src` if [src] points to local data', () => {
+          it('should not render if [src] mutates with data (before layout)', () => {
+            // Not allowed before layout.
+            listMock.expects('scheduleRender_').never();
+
+            element.setAttribute('src', 'https://new.com/list.json');
+            list.mutatedAttributesCallback({'src': [{title: 'Title1'}]});
+            // `src` attribute should still be set to empty string.
+            expect(element.getAttribute('src')).to.equal('');
+          });
+
+          it('should render if [src] mutates with data', () => {
             const foo = doc.createElement('div');
             expectFetchAndRender(DEFAULT_FETCHED_DATA, [foo]);
 
