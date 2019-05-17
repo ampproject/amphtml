b114733fb17ba2954340401e1ad522508e39d06e
Dima Voytenko <dvoytenko@gmail.com>
2019-05-17 11:26:26 -0700

    Fix tests

--- test/unit/test-dom.js
+++ test/unit/test-dom.js
@@ -769,7 +769,7 @@ describes.sandboxed('DOM', {}, env => {
         setTimeout(() => {
           doc.body = {};
         }, 50);
-        dom.waitForBody(doc, () => {
+        dom.waitForBodyOpen(doc, () => {
           try {
             expect(doc.body).to.exist;
             resolve();
