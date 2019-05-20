1ccd6b3db17968c658d430784551912f1640f0e9
Caleb Cordry <ccordry@google.com>
2019-05-20 14:56:26 -0700

    Reformat beaverads contribution (#22396)

--- ads/beaverads.js
+++ ads/beaverads.js
@@ -23,13 +23,19 @@ import {loadScript, validateData} from '../3p/3p';
 export function beaverads(global, data) {
   validateData(data, ['blockId']);
 
-  const url = 'https://code.beaverads.com/data/' +
-      encodeURIComponent(data['blockId']) + '.js?async=1&div=c';
+  const url =
+    'https://code.beaverads.com/data/' +
+    encodeURIComponent(data['blockId']) +
+    '.js?async=1&div=c';
 
-  loadScript(global, url, () => {
-    global.context.renderStart();
-  }, () => {
-    global.context.noContentAvailable();
-  });
-
+  loadScript(
+    global,
+    url,
+    () => {
+      global.context.renderStart();
+    },
+    () => {
+      global.context.noContentAvailable();
+    }
+  );
 }
