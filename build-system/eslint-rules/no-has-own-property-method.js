2d9c1181dbda4337962d27c9539f9d251f306739
Raghu Simha <rsimha@ampproject.org>
2019-05-21 13:02:14 -0400

    🏗 Clean up `.eslintignore` (#22411)

--- build-system/eslint-rules/no-has-own-property-method.js
+++ build-system/eslint-rules/no-has-own-property-method.js
@@ -19,12 +19,16 @@ module.exports = {
   create(context) {
     return {
       CallExpression(node) {
-        if (node.callee.type == 'MemberExpression' && !node.callee.computed &&
-            node.callee.property.name == 'hasOwnProperty') {
+        if (
+          node.callee.type == 'MemberExpression' &&
+          !node.callee.computed &&
+          node.callee.property.name == 'hasOwnProperty'
+        ) {
           context.report({
             node,
-            message: 'Do not use hasOwnProperty directly. ' +
-                'Use hasOwn from src/utils/object.js instead.',
+            message:
+              'Do not use hasOwnProperty directly. ' +
+              'Use hasOwn from src/utils/object.js instead.',
           });
         }
       },
