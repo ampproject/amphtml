f3c5b48238260ba3ce7d28c465c14d4b44a14014
Esther Kim <esth@google.com>
2019-05-22 11:40:15 -0400

    skip amp4email tests in shadow

--- test/e2e/test-amp-bind-email.js
+++ test/e2e/test-amp-bind-email.js
@@ -29,34 +29,38 @@ describes.endtoend(
     });
 
     // The only difference in amp4email is that URL attributes cannot be bound.
-    describe('amp4email', () => {
-      it('should NOT allow mutation of a[href]', async () => {
-        const button = await controller.findElement('#changeHrefButton');
-        const a = await controller.findElement('#anchorElement');
+    // TODO(choumx): fails in shadow env when using shadow-v0.js but succeeds when using amp-shadow.js
+    describe
+      .configure()
+      .skipShadowDemo()
+      .run('amp4email', () => {
+        it('should NOT allow mutation of a[href]', async () => {
+          const button = await controller.findElement('#changeHrefButton');
+          const a = await controller.findElement('#anchorElement');
 
-        await expect(controller.getElementAttribute(a, 'href')).to.equal(
-          'https://foo.com'
-        );
+          await expect(controller.getElementAttribute(a, 'href')).to.equal(
+            'https://foo.com'
+          );
 
-        await controller.click(button);
-        await expect(controller.getElementAttribute(a, 'href')).to.equal(
-          'https://foo.com'
-        );
+          await controller.click(button);
+          await expect(controller.getElementAttribute(a, 'href')).to.equal(
+            'https://foo.com'
+          );
+        });
+
+        it('should NOT allow mutation of img[src]', async () => {
+          const button = await controller.findElement('#changeImgSrcButton');
+          const image = await controller.findElement('#image');
+
+          await expect(controller.getElementAttribute(image, 'src')).to.equal(
+            'https://foo.com/foo.jpg'
+          );
+
+          await controller.click(button);
+          await expect(controller.getElementAttribute(image, 'src')).to.equal(
+            'https://foo.com/foo.jpg'
+          );
+        });
       });
-
-      it('should NOT allow mutation of img[src]', async () => {
-        const button = await controller.findElement('#changeImgSrcButton');
-        const image = await controller.findElement('#image');
-
-        await expect(controller.getElementAttribute(image, 'src')).to.equal(
-          '/examples/img/ampicon.png'
-        );
-
-        await controller.click(button);
-        await expect(controller.getElementAttribute(image, 'src')).to.equal(
-          '/examples/img/ampicon.png'
-        );
-      });
-    });
   }
 );
