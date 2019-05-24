711e9362ec3a090e2c028519daf4f676d7d84297
Raghu Simha <rsimha@ampproject.org>
2019-05-24 01:27:34 -0400

    Fix JSDoc

--- 3p/github.js
+++ 3p/github.js
@@ -26,7 +26,7 @@ import {writeScript} from './3p';
  *
  * @param {!Window} global
  * @param {string} scriptSource The source of the script, different for post and comment embeds.
- * @param {function(*)} cb
+ * @param {function()} cb
  */
 function getGistJs(global, scriptSource, cb) {
   writeScript(global, scriptSource, function() {
