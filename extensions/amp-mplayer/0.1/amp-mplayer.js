1e31edf0cc7b86392156d1fae3ac14a02e34c3ef
Ofir Shlifer <ofir.s@minutemedia.com>
2019-05-26 15:49:36 +0300

    initial commit first version of MPlayer extension, including changes in package.json and yarn.lock

---
+++ extensions/amp-mplayer/0.1/amp-mplayer.js
@@ -0,0 +1,124 @@
+import {addParamsToUrl} from '../../../src/url';
+import {dict} from '../../../src/utils/object';
+import {removeElement} from '../../../src/dom';
+import {isLayoutSizeDefined} from '../../../src/layout';
+
+/** @const */
+const TAG = 'amp-mplayer';
+
+class AmpMPlayer extends AMP.BaseElement {
+
+  /** @implements {../../../src/video-interface.VideoInterface} */
+  constructor(element) {
+    super(element);
+
+    // Declare instance variables with type annotations.
+
+    /** @private {Element} */
+    this.iframe = null;
+
+    /** @private {?string} */
+    this.player_id = null;
+
+    /** @private {?string} */
+    this.content_id = '';
+
+    /** @private {?string} */
+    this.monti_id = null;
+
+    /** @private {?string} */
+    this.scanned_element = '';
+
+    /** @private {?string} */
+    this.tags = '';
+
+    /** @private {?string} */
+    this.minimum_date_factor = '';
+
+    /** @private {?string} */
+    this.scanned_element_type = '';
+  }
+
+  /**
+   * @param {boolean=} onLayout
+   * @override
+   */
+  preconnectCallback(onLayout) {
+
+    // Host that serves player configuration and content redirects
+    this.preconnect.url("https://www.oo-syringe.com", onLayout);
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    //Define which layouts our element support
+
+    //Size-defined layouts: fixed, fixed height, responsive and fill
+    return isLayoutSizeDefined(layout);
+  }
+
+  /** @override */
+  buildCallback() {
+    // Get attributes, assertions of values, assign instance variables.
+    // Build lightweight DOM and append to this.element.
+
+    // Warn if the player does not have video interface support
+    this.readyTimeout_ = /** @type {number} */ (
+      Services.timerFor(window).delay(() => {
+        user().warn(TAG,
+          'Did not receive ready callback from player %s.' +
+          ' Ensure it has the videojs-amp-support plugin.', this.player_id);
+    }, 3000));
+  }
+
+  iframeSource() {
+    this.content_id = (element.getAttribute('data-content-id') || '');
+    this.scanned_element = (element.getAttribute('data-scanned-element') || '');
+    this.tags = (element.getAttribute('data-tags') || '');
+    this.minimum_date_factor = (element.getAttribute('data-minimum_date_factor') || '');
+    this.scanned_element_type = (element.getAttribute('data-scanned_element_type') || '');
+
+    const source = '/Users/ofirshlifer/Downloads/mplayer.html' +
+      ((this.content_id !== '') ?
+        '?content_id=' +
+      `${encodeURIComponent(this.content_id)}` :
+        ('?scanned_element=' +
+          `${encodeURIComponent(this.scanned_element)}` +
+          '&tags=' +
+          `${encodeURIComponent(this.tags)}` +
+          '&minimum_date_factor=' +
+          `${encodeURIComponent(this.minimum_date_factor)}` +
+          '&scanned_element_type=' +
+          `${encodeURIComponent(this.scanned_element_type)}`));
+
+    const more_query_params = dict({
+      'player_id': (element.getAttribute('data-player-id') || undefined),
+      'monti_id': (element.getAttribute('data-monti-id') || undefined)
+    });
+
+    return addParamsToUrl(source, more_query_params);
+  }
+
+  /** @override */
+  layoutCallback() {
+    // Actually load your resource or render more expensive resources.
+
+    const iframe = createFrameFor(this, this.iframeSource());
+    this.iframe = iframe;
+    return this.loadPromise(iframe);
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    if (this.iframe) {
+      removeElement(this.iframe);
+      this.iframe = null;
+    }
+    return true; // Call layoutCallback again.
+  }
+}
+
+  AMP.extension(TAG, '0.1', AMP => {
+    AMP.registerElement(TAG, AmpMPlayer);
+});
+
