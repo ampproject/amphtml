43ad922a4b93e4803239a6d7a5269258bc1e062e
Josh <josh@permutive.com>
2019-05-27 22:07:51 +0100

    Adds support for tracking custom events

--- extensions/amp-analytics/0.1/vendors/permutive.js
+++ extensions/amp-analytics/0.1/vendors/permutive.js
@@ -31,6 +31,7 @@ export const PERMUTIVE_CONFIG = /** @type {!JsonObject} */ ({
       '&_ep_geo_info=%24ip_geo_info',
     'engagement': '${track}&e=PageviewEngagement&_ep_engaged_time=5',
     'completion': '${track}&e=PageviewEngagement&_ep_completion=0.25',
+    'custom': '${track}&e=${event}',
   },
   'triggers': {
     'trackPageview': {
