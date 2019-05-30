d34c547b6e740dd5c587f092994d9e3c6a1b5cc8
Dima Voytenko <dvoytenko@gmail.com>
2019-05-30 11:15:07 -0700

    cleanup

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -414,7 +414,11 @@ const forbiddenTerms = {
   // Global documentState service.
   'globalDocumentStateFor': {
     message: 'Global document API. In the process of being deprecated.',
-    whitelist: ['src/service/viewer-impl.js', 'src/service/vsync-impl.js'],
+    whitelist: [
+      'src/services.js',
+      'src/service/viewer-impl.js',
+      'src/service/vsync-impl.js',
+    ],
   },
   'getBaseCid': {
     message: requiresReviewPrivacy,
