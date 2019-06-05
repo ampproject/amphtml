90032f701e5c20fd8f3d8e0fc937d38ed1736899
Matt Hinegardner <matt.hinegardner@oracle.com>
2019-06-05 12:09:28 -0400

    Adding Open Graph image meta tag as fallback for media property.

--- extensions/amp-addthis/0.1/addthis-utils/meta.js
+++ extensions/amp-addthis/0.1/addthis-utils/meta.js
@@ -31,3 +31,15 @@ export const getDetailsForMeta = meta => {
     content,
   };
 };
+
+export const getOgImage = doc => {
+  const ogImages = doc.head.querySelectorAll('meta[property="og:image"]');
+  if (ogImages && ogImages.length > 0) {
+    // If there is more than one og:image, just pick the first
+    const imageUrl = ogImages[0].content;
+    if (imageUrl) {
+      return imageUrl;
+    }
+  }
+  return '';
+};
