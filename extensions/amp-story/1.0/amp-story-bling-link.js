fad6e1246a2551f369c0960d94f44b7ca81c88a1
Esther Kim <esth@google.com>
2019-06-07 16:57:30 -0400

    initial custom element

---
+++ extensions/amp-story/1.0/amp-story-bling-link.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Bling link component that turns into a tooltip when clicked.
+ */
+
+import {Layout} from '../../../src/layout';
+
+/** @const */
+const HOVER_CLASS = 'i-amphtml-story-bling-link-hover';
+
+/** @const */
+const PULSE_CLASS = 'pulse';
+
+export class AmpStoryBlingLink extends AMP.BaseElement {
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    super(element);
+    // Declare instance variables with type annotations.
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    return layout == Layout.FIXED;
+  }
+
+  /** @override */
+  buildCallback() {
+    // Get attributes, assertions of values, assign instance variables.
+    // Build lightweight DOM and append to this.element.
+  }
+
+  /** @override */
+  layoutCallback() {
+    this.element.classList.add(PULSE_CLASS);
+    setClassOnHover(this.element);
+  }
+}
+
+/**
+ * @param {!Element} el
+ */
+function setClassOnHover(el) {
+  el.addEventListener('mouseenter', () => {
+    el.classList.add(HOVER_CLASS);
+    el.classList.remove(PULSE_CLASS);
+  });
+  el.addEventListener('mouseleave', () => {
+    el.classList.remove(HOVER_CLASS);
+    el.classList.add(PULSE_CLASS);
+  });
+}
