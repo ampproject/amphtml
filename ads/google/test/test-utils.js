190557f064d13ec797d189b4bf8d5bf322b0aa06
glevitzky <levitzky@google.com>
2019-06-12 14:03:16 -0400

    Ensure that fluid dummy size '320x50' is the first size included in the ad request. (#22780)
    * initial
    
    * Update for latest changes.
    
    * Remove empty space.
    
    * Lint fix.
    
    * JSdoc.
    
    * Undo test page changes.
    
    * Sort imports.
    
    * Update util tests.
    
    * Another test fix.
    
    * Unit test.

--- ads/google/test/test-utils.js
+++ ads/google/test/test-utils.js
@@ -111,12 +111,6 @@ describe('#getMultiSizeDimensions', () => {
     verifyArray(actual, 0, multiSizes.length);
   });
 
-  it('should add dummy size for fluid', () => {
-    expect(
-      getMultiSizeDimensions('fluid', 300, 300, /* useLowerBound */ false)
-    ).to.deep.equal([[320, 50]]);
-  });
-
   it('should not add dummy size for fluid if fluid is primary size', () => {
     expect(
       getMultiSizeDimensions(
@@ -137,7 +131,7 @@ describe('#getMultiSizeDimensions', () => {
         300,
         /* useLowerBound */ false
       )
-    ).to.deep.equal([[320, 50], [300, 300]]);
+    ).to.deep.equal([[300, 300]]);
     expect(
       getMultiSizeDimensions(
         '300x300,fluid',
@@ -145,7 +139,7 @@ describe('#getMultiSizeDimensions', () => {
         300,
         /* useLowerBound */ false
       )
-    ).to.deep.equal([[300, 300], [320, 50]]);
+    ).to.deep.equal([[300, 300]]);
   });
 });
 
