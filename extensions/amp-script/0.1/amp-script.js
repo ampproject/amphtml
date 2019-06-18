74a39716135b6b66df4433cd7d5877fefd1f35e5
William Chou <willchou@google.com>
2019-06-18 17:00:14 -0400

    Upgrade worker-dom to 0.12.3 (#22848)
    * First pass on upgrading worker-dom.
    
    * Workaround stupid browserify subfolder issue.
    
    * Revise comment.

--- extensions/amp-script/0.1/amp-script.js
+++ extensions/amp-script/0.1/amp-script.js
@@ -35,7 +35,7 @@ import {
 } from '../../../src/service/origin-experiments-impl';
 import {isExperimentOn} from '../../../src/experiments';
 import {rewriteAttributeValue} from '../../../src/url-rewrite';
-import {upgrade} from '@ampproject/worker-dom/dist/unminified.index.safe.mjs.patched';
+import {upgrade} from '@ampproject/worker-dom/dist/amp.main.mjs';
 
 /** @const {string} */
 const TAG = 'amp-script';
