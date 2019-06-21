f628031a13c7687f16e5492e6e7877f78dfecbec
Alekander Kadygrob <gL1ghty@gmail.com>
2019-06-21 21:55:52 +0300

    ✨ Ads: 1WorldOnline ad integration (#22898)
    * Implemented 1WorldOnline ad integration
    
    * - Updated Copyright year
    - Liked to 1wo.md from amp-ad.md

--- 3p/integration.js
+++ 3p/integration.js
@@ -60,6 +60,7 @@ import {yotpo} from './yotpo';
 import {_ping_} from '../ads/_ping_';
 
 // 3P Ad Networks - please keep in alphabetic order
+import {_1wo} from '../ads/1wo';
 import {_24smi} from '../ads/24smi';
 import {a8} from '../ads/a8';
 import {a9} from '../ads/a9';
@@ -269,6 +270,7 @@ import {zucks} from '../ads/zucks';
  */
 const AMP_EMBED_ALLOWED = {
   _ping_: true,
+  '1wo': true,
   '24smi': true,
   adsloom: true,
   bringhub: true,
@@ -304,6 +306,7 @@ if (getMode().test || getMode().localDev) {
 }
 
 // Keep the list in alphabetic order
+register('1wo', _1wo);
 register('24smi', _24smi);
 register('3d-gltf', gltfViewer);
 register('a8', a8);
