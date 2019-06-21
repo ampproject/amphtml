e4132b3cf78b54ad2aa262b3ece304eec2d36263
Matt Borofsky <mborof@google.com>
2019-06-21 10:54:08 -0700

    remove extra space

--- extensions/amp-subscriptions/0.1/test/test-local-subscription-rendering.js
+++ extensions/amp-subscriptions/0.1/test/test-local-subscription-rendering.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import {Action} from '../analytics';
 import {Dialog} from '../dialog';
 import {Entitlement} from '../entitlement';
