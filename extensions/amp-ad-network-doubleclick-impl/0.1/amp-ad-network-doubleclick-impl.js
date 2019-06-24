61d035277750df44672207fdaa86389aacfcfe13
Gene Levitzky <levitzky@google.com>
2019-06-24 14:26:45 -0400

    Cast to int.

--- extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
@@ -1206,8 +1206,8 @@ export class AmpAdNetworkDoubleclickImpl extends AmpA4A {
    * @private
    */
   handleResize_(width, height) {
-    const pWidth = this.element.getAttribute('width');
-    const pHeight = this.element.getAttribute('height');
+    const pWidth = parseInt(this.element.getAttribute('width'), 10);
+    const pHeight = parseInt(this.element.getAttribute('height'), 10);
     const isFluidRequestAndFixedResponse = !!(
       this.isFluidRequest_ &&
       width &&
