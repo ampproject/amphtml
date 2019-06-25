edf1684eedb6f85eeb89ea2800bf7fd64ec24197
Daniel Kuhn <daniel.kuhn@burda.com>
2019-06-25 16:27:40 +0200

    fixed lint problems

--- ads/tracdelight.js
+++ ads/tracdelight.js
@@ -21,9 +21,7 @@ import {validateData, writeScript} from '../3p/3p';
  * @param {!Object} data
  */
 export function tracdelight(global, data) {
-
   const mandatoryFields = ['widget_id', 'access_key'];
-
   const optionalFields = ['mode'];
 
   validateData(data, mandatoryFields, optionalFields);
