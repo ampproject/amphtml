a7cb11ef452c842f934a0e2510e72dde5d55a5f5
Sepand Parhami <sparhami@google.com>
2019-06-25 11:19:53 -0700

    amp-truncate-text: support custom actions. (#22817)
    - Rename "expand" slot into "collapsed", since it may be used to do something
    other than expand.
    - Rename "collapse" slot into "expanded", since it may be used to do something
    other than collapse.
    - Support custom tap actions as well as anchor tags, which will override
    the default expand/collapse behavior of the "collapsed" and "expanded"
    slots.
    - Add a "persistent" slot that is always present. This is useful when
    you have something like a short description (which may be truncated,
    depending on available space), but you want to link to another page
    regardless of truncation.

--- extensions/amp-truncate-text/0.1/truncate-text.js
+++ extensions/amp-truncate-text/0.1/truncate-text.js
@@ -172,10 +172,10 @@ function getOverflowY(element) {
  *
  * @param {{
  *   container: !Element,
- *   overflowElement: ?Element,
+ *   overflowNodes: !Array<!Node>,
  * }} config
  */
-export function truncateText({container, overflowElement} = {}) {
+export function truncateText({container, overflowNodes} = {}) {
   clearTruncated(container);
 
   // If everything fits while the overflow button is hidden, we are done.
@@ -189,7 +189,7 @@ export function truncateText({container, overflowElement} = {}) {
   // Set the container as truncateed, so we show the overflow element and we can
   // truncate taking the size into account.
   setTruncated(container);
-  runTruncation(container, containerRect, overflowElement);
+  runTruncation(container, containerRect, overflowNodes);
 }
 
 /**
@@ -214,12 +214,12 @@ function getAllNodes(root, filter, nodes = []) {
  * truncation and truncating it.
  * @param {!Element} container The Element to do truncation for.
  * @param {!ClientRect} containerRect The rect for `container`.
- * @param {?Element} overflowElement An Element that shows when overflowing,
- *    or null if none is specified.
+ * @param {!Array<!Node>} overflowNodes Any nodes that should be showing
+ *    when there is overflow.
  */
-function runTruncation(container, containerRect, overflowElement) {
+function runTruncation(container, containerRect, overflowNodes) {
   const nodes = getAllNodes(container, node => {
-    return node != overflowElement;
+    return !overflowNodes.includes(node);
   });
 
   // Work backwards, truncating nodes from the end until we find one that can
