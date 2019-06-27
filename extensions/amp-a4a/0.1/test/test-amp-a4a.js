28d7462b7298785f5affe15345946e187fb09256
lannka <hungfei.ding@gmail.com>
2019-06-26 21:26:31 -0700

    fix test

--- extensions/amp-a4a/0.1/test/test-amp-a4a.js
+++ extensions/amp-a4a/0.1/test/test-amp-a4a.js
@@ -241,7 +241,7 @@ describe('amp-a4a', () => {
    * @param {string} sfVersion
    * @param {boolean=} shouldSandbox
    */
-  function verifySafeFrameRender(element, sfVersion, shouldSandbox = false) {
+  function verifySafeFrameRender(element, sfVersion, shouldSandbox = true) {
     expect(element.tagName.toLowerCase()).to.equal('amp-a4a');
     expect(element).to.be.visible;
     expect(element.querySelectorAll('iframe')).to.have.lengthOf(1);
@@ -277,7 +277,7 @@ describe('amp-a4a', () => {
    * @param {!Element} element
    * @param {boolean} shouldSandbox
    */
-  function verifyNameFrameRender(element, shouldSandbox = false) {
+  function verifyNameFrameRender(element, shouldSandbox = true) {
     expect(element.tagName.toLowerCase()).to.equal('amp-a4a');
     expect(element).to.be.visible;
     expect(element.querySelectorAll('iframe')).to.have.lengthOf(1);
@@ -299,7 +299,7 @@ describe('amp-a4a', () => {
   function verifyCachedContentIframeRender(
     element,
     srcUrl,
-    shouldSandbox = false
+    shouldSandbox = true
   ) {
     expect(element.tagName.toLowerCase()).to.equal('amp-a4a');
     expect(element).to.be.visible;
