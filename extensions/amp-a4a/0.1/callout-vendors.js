86a047ce65be8f437a12eda64c9952ec8b136913
eohashi <51883659+eohashi@users.noreply.github.com>
2019-07-09 11:30:11 -0300

    Update callout-vendors.js

--- extensions/amp-a4a/0.1/callout-vendors.js
+++ extensions/amp-a4a/0.1/callout-vendors.js
@@ -121,6 +121,7 @@ export const RTC_VENDORS = {
   navegg: {
     url: 'https://amp.navdmp.com/usr?acc=NVG_ACC&wst=0&v=10',
     macros: ['NVG_ACC'],
+    disableKeyAppend: true,
   },
   sonobi: {
     url:
