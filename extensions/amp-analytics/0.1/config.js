ab8c84dc293338826733c60462571187358e4909
Jonathan Ng <jonathantyng@google.com>
2019-07-11 15:48:10 -0700

    add experiment param in GA pageview request

--- extensions/amp-analytics/0.1/config.js
+++ extensions/amp-analytics/0.1/config.js
@@ -66,7 +66,26 @@ export class AnalyticsConfig {
 
     return this.fetchRemoteConfig_()
       .then(this.processConfigs_.bind(this))
+      .then(this.addExperimentParams_.bind(this))
       .then(() => this.config_);
+  }
+
+  /**
+   * TODO: cleanup #22757 @jonathantyng
+   * Append special param to pageview request for RC and experiment builds
+   * for the googleanalytics component. This is to track pageview changes
+   * in AB experiment
+   */
+  addExperimentParams_() {
+    const type = this.element_.getAttribute('type');
+    const {rtvVersion} = getMode();
+    const isRc = rtvVersion ? rtvVersion.substring(0, 2) === '03' : false;
+    // eslint-disable-next-line no-undef
+    const isExperiment = ANALYTICS_VENDOR_SPLIT;
+
+    if (type === 'googleanalytics' && (isRc || isExperiment)) {
+      this.config_['requests']['pageview']['baseUrl'] += `&aae=${isExperiment}`;
+    }
   }
 
   /**
