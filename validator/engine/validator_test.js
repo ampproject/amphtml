9c60a28ac5182eab9f5537d0b73d7d8b99d36481
Ugo Stephant <ugo@poool.fr>
2019-07-15 16:11:06 +0200

    🐛 Avoid redirects inside amp-access-poool iframe on click events (#23122)
    * fix(amp-access-poool): handle clicks event from inside paywall iframe
    
    Co-Authored-By: Ugo Stephant <ugo@poool.fr>
    
    * fix(amp-access-poool): add validator definition for amp-access-poool attributes
    
    Co-Authored-By: Ugo Stephant <ugo@poool.fr>
    
    * fix(amp-access-poool): fix typo in validator test not updated in output file
    
    * fix(amp-access-poool): fix validator rules
    
    Co-Authored-By: Ugo Stephant <ugo@poool.fr>
    
    * tests(amp-access-poool): update common-extension-attrs spec index
    
    after poool attr-lists addition
    
    Co-Authored-By: Ugo Stephant <ugo@poool.fr>

--- validator/engine/validator_test.js
+++ validator/engine/validator_test.js
@@ -1359,7 +1359,7 @@ describe('ValidatorRulesMakeSense', () => {
         // getNameByAttrSpecId() looks like it would do what we want, but it's
         // sufficiently wrapped in private context inside the validator that I
         // don't see a way to call it.  For now just gold the current index.
-        expect(tagSpec.attrLists[0]).toEqual(18);
+        expect(tagSpec.attrLists[0]).toEqual(19);
       });
     }
 
