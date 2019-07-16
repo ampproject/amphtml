bfbaf71ad64890883f42f464f95e5075d09c2366
wroberts-comscore <50838026+wroberts-comscore@users.noreply.github.com>
2019-07-16 18:24:39 -0400

    ✨Add Consent State Macro into Comscore's Vendor Script (#22641)
    * Adding two new labels, cs_consent for the consent value and cs_ucfr for the translated value. Insufficient should return 0, sufficient should return 1, and anything else will return an empty string in cs_ucfr
    
    * Removing comscore translated label for now, collecting consent state to determine if it is inline with our expectations, TAG-4790
    
    * Adding the new parameter into the vendor-requests.json file

--- extensions/amp-analytics/0.1/vendors/comscore.js
+++ extensions/amp-analytics/0.1/vendors/comscore.js
@@ -24,6 +24,7 @@ export const COMSCORE_CONFIG = /** @type {!JsonObject} */ ({
     'pageview':
       '${base}c1=2' +
       '&c2=${c2}' +
+      '&cs_amp_consent=${consentState}' +
       '&cs_pv=${pageViewId}' +
       '&c12=${clientId(comScore)}' +
       '&rn=${random}' +
