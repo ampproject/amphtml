227958d79d988886fbfcba6a0eabf1dc6e37a6e2
Prateek Bhatnagar <prateek89born@gmail.com>
2019-07-16 15:32:46 -0700

    ✨ Es 2015 code generation (#23330)
    * changes for ES_2015 v0-esm
    
    * fixes esm build flag
    
    * v0-esm is no longer needed
    
    * fixing esm presets in babel config
    
    * lint fixes
    
    * fixing nested arrays
    
    * use babel transpile only for esm build
    
    * bug fix
    
    * addressing comments
    
    * fixing comment

--- build-system/compile/compile.js
+++ build-system/compile/compile.js
@@ -265,9 +265,12 @@ function compile(entryModuleFilenames, outputDir, outputFilename, options) {
       compilation_level: options.compilationLevel || 'SIMPLE_OPTIMIZATIONS',
       // Turns on more optimizations.
       assume_function_wrapper: true,
-      // Transpile from ES6 to ES5.
+      /*
+       * Transpile from ES6 to ES5 if not running with `--esm`
+       * otherwise transpilation is done by Babel
+       */
       language_in: 'ECMASCRIPT6',
-      language_out: 'ECMASCRIPT5',
+      language_out: argv.esm ? 'NO_TRANSPILE' : 'ECMASCRIPT5',
       // We do not use the polyfills provided by closure compiler.
       // If you need a polyfill. Manually include them in the
       // respective top level polyfills.js files.
