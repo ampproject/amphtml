1e54b4751aa9c57155f3ec37e30c3fd4219a0bf5
jonathantyng-amp <jonathantyng@google.com>
2019-07-16 17:01:19 -0700

    ✨ Add support for minify-replace babel plugin (#23175)
    * add support for minify-replace babel plugin
    
    * update
    
    * changes from comments
    
    * change define to defineExperimentConstant
    
    * remove custom value feature

--- build-system/build.conf.js
+++ build-system/build.conf.js
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+const argv = require('minimist')(process.argv.slice(2));
+const experimentsConfig = require('./global-configs/experiments-config.json');
+
 const defaultPlugins = [
   require.resolve('./babel-plugins/babel-plugin-transform-html-template'),
   require.resolve(
@@ -26,9 +29,66 @@ const defaultPlugins = [
   require.resolve('./babel-plugins/babel-plugin-transform-version-call'),
 ];
 
+/**
+ * @return {Array<string|Object>} the minify-replace plugin options that can be
+ * pushed into the babel plugins array
+ */
+function getReplacePlugin() {
+  /**
+   * @param {string} identifierName the identifier name to replace
+   * @param {boolean} value the value to replace with
+   * @return {!Object} replacement options used by minify-replace plugin
+   */
+  function createReplacement(identifierName, value) {
+    return {
+      identifierName,
+      replacement: {
+        type: 'booleanLiteral',
+        value,
+      },
+    };
+  }
+
+  const replacements = [];
+  const defineFlag = argv.defineExperimentConstant;
+
+  // add define flags from arguments
+  if (Array.isArray(defineFlag)) {
+    if (defineFlag.length > 1) {
+      throw new Error('Only one defineExperimentConstant flag is allowed');
+    } else {
+      replacements.push(createReplacement(defineFlag[0], true));
+    }
+  } else if (defineFlag) {
+    replacements.push(createReplacement(defineFlag, true));
+  }
+
+  // default each experiment flag constant to false
+  Object.keys(experimentsConfig).forEach(experiment => {
+    const experimentDefine =
+      experimentsConfig[experiment]['defineExperimentConstant'];
+
+    function flagExists(element) {
+      return element['identifierName'] === experimentDefine;
+    }
+
+    // only add default replacement if it already doesn't exist in array
+    if (experimentDefine && !replacements.some(flagExists)) {
+      replacements.push(createReplacement(experimentDefine, false));
+    }
+  });
+
+  const replacePlugin = [
+    require.resolve('babel-plugin-minify-replace'),
+    {replacements},
+  ];
+
+  return replacePlugin;
+}
+
 module.exports = {
   plugins({isEsmBuild, isForTesting, isSinglePass}) {
-    let pluginsToApply = defaultPlugins;
+    let pluginsToApply = defaultPlugins.slice(0);
     // TODO(erwinm): This is temporary until we remove the assert/log removals
     // from the java transformation to the babel transformation.
     // There is currently a weird interaction where when we do the transform
@@ -67,6 +127,7 @@ module.exports = {
         require.resolve('./babel-plugins/babel-plugin-amp-mode-transformer'),
       ]);
     }
+    pluginsToApply.push(getReplacePlugin());
     return pluginsToApply;
   },
 
@@ -75,4 +136,5 @@ module.exports = {
       require.resolve('./babel-plugins/babel-plugin-transform-prune-namespace'),
     ];
   },
+  getReplacePlugin,
 };
