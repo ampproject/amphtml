efd97da0f95c70a2018a53380a3e279275d3dc4d
lannka <hungfei.ding@gmail.com>
2019-07-17 10:16:44 -0700

    Fix linter

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import {CommonSignals} from '../common-signals';
 import {FiniteStateMachine} from '../finite-state-machine';
 import {FocusHistory} from '../focus-history';
 import {Pass} from '../pass';
