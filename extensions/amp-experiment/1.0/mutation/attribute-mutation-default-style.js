da2b4526c9d189bfbb4b00bb7bb5829e05b37a1f
Aaron Turner <aaron@aaronthedev.com>
2019-07-17 18:39:52 -0700

    ✨amp-experiment-1.0: Refactor, Select multiple Elements (querySelectorAll) (#23058)
    * Created the interface for the mutations
    
    * Almost finished the refactor. Need to write the interfaces, and apply
    
    * Finished the impolementation refactor
    
    * Implemented all linting and things
    
    * Added back class support
    
    * Made requested changes
    
    * Added a specified element for a more descriptive error
    
    * Fixed current tests, need to migrate old tests
    
    * Added more tests, just need mutation tests
    
    * Fixed linting and check-types
    
    * Added all remaining tests
    
    * Finished precommit checks
    
    * Made re quested changes

---
+++ extensions/amp-experiment/1.0/mutation/attribute-mutation-default-style.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assertAttributeMutationFormat} from './mutation';
+
+/**
+ * Mutation for attribute (style) mutations on unspecified elements.
+ *
+ * @implements {./mutation.Mutation}
+ */
+export class AttributeMutationDefaultStyle {
+  /**
+   * @param {!JsonObject} mutationRecord
+   * @param {!Array<Element>} elements
+   */
+  constructor(mutationRecord, elements) {
+    this.mutationRecord_ = mutationRecord;
+    this.elements_ = elements;
+    assertAttributeMutationFormat(this.mutationRecord_);
+  }
+
+  /** @override */
+  validate() {
+    const value = this.mutationRecord_['value'];
+
+    // Do not allow Important or HTML Comments
+    if (value.match(/(!\s*important|<!--)/)) {
+      return false;
+    }
+
+    // Allow Color
+    if (value.match(/^color:\s*#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3});?$/)) {
+      return true;
+    }
+
+    // Allow Background color
+    if (
+      value.match(/^background-color:\s*#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3});?$/)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /** @override */
+  mutate() {
+    this.elements_.forEach(element => {
+      element.setAttribute('style', this.mutationRecord_['value']);
+    });
+  }
+
+  /** @override */
+  toString() {
+    return JSON.stringify(this.mutationRecord_);
+  }
+}
