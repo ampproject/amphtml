479d000f6cb5e8e83975743e74d54587e65d725d
William Chou <willchou@google.com>
2019-07-23 17:59:18 -0400

    amp-script: Implement localStorage/sessionStorage (#23305)
    * Implement getStorage/changeStorage.
    
    * Add tests.
    
    * Fix lint, presubmit, types.
    
    * Upgrade worker-dom to 0.15.0.
    
    * Update storage 'scope' to 'location'.
    
    * Add private annotation.
    
    * Add localStorage example.

--- extensions/amp-script/0.1/test/unit/test-amp-script.js
+++ extensions/amp-script/0.1/test/unit/test-amp-script.js
@@ -28,63 +28,63 @@ describe('SanitizerImpl', () => {
     el = win.document.createElement('div');
   });
 
-  describe('mutateAttribute', () => {
+  describe('changeAttribute', () => {
     it('should remove attributes when value is null', () => {
       el.setAttribute('class', 'foo');
-      s.mutateAttribute(el, 'class', null);
+      s.changeAttribute(el, 'class', null);
       expect(el.hasAttribute('class')).to.be.false;
     });
 
     it('should set attributes when value is non-null', () => {
-      s.mutateAttribute(el, 'class', 'foo');
+      s.changeAttribute(el, 'class', 'foo');
       expect(el.getAttribute('class')).to.equal('foo');
     });
 
     it('should be case-insensitive to attribute name', () => {
-      s.mutateAttribute(el, 'CLASS', 'foo');
+      s.changeAttribute(el, 'CLASS', 'foo');
       expect(el.getAttribute('class')).to.equal('foo');
     });
 
     it('should set a[target] if [href] exists', () => {
       const a = win.document.createElement('a');
-      s.mutateAttribute(a, 'href', '/foo.html');
+      s.changeAttribute(a, 'href', '/foo.html');
       expect(a.getAttribute('target')).to.equal('_top');
     });
 
     it('should not allow changes to invalid tags', () => {
       const base = win.document.createElement('base');
       // 'href' attribute is allowed but 'base' tag isn't.
-      s.mutateAttribute(base, 'href', '/foo.html');
+      s.changeAttribute(base, 'href', '/foo.html');
       expect(base.getAttribute('href')).to.be.null;
     });
 
     it('should allow changes to built-in AMP tags except amp-pixel', () => {
       const img = win.document.createElement('amp-img');
-      s.mutateAttribute(img, 'src', 'foo.jpg');
+      s.changeAttribute(img, 'src', 'foo.jpg');
       expect(img.getAttribute('src')).to.include('foo.jpg');
 
       const layout = win.document.createElement('amp-layout');
-      s.mutateAttribute(layout, 'width', '10');
+      s.changeAttribute(layout, 'width', '10');
       expect(layout.getAttribute('width')).to.equal('10');
 
       const pixel = win.document.createElement('amp-pixel');
-      s.mutateAttribute(pixel, 'src', '/foo/track');
+      s.changeAttribute(pixel, 'src', '/foo/track');
       expect(pixel.getAttribute('src')).to.be.null;
     });
 
     it('should not allow changes to other AMP tags', () => {
       const analytics = win.document.createElement('amp-analytics');
-      s.mutateAttribute(analytics, 'data-credentials', 'include');
+      s.changeAttribute(analytics, 'data-credentials', 'include');
       expect(analytics.getAttribute('data-credentials')).to.be.null;
     });
 
     it('should not allow changes to form elements', () => {
       const form = win.document.createElement('form');
-      s.mutateAttribute(form, 'action-xhr', 'https://example.com/post');
+      s.changeAttribute(form, 'action-xhr', 'https://example.com/post');
       expect(form.getAttribute('action-xhr')).to.be.null;
 
       const input = win.document.createElement('input');
-      s.mutateAttribute(input, 'value', 'foo');
+      s.changeAttribute(input, 'value', 'foo');
       expect(input.getAttribute('value')).to.be.null;
     });
 
@@ -92,14 +92,105 @@ describe('SanitizerImpl', () => {
       s = new SanitizerImpl(win, ['allow-forms']);
 
       const form = win.document.createElement('form');
-      s.mutateAttribute(form, 'action-xhr', 'https://example.com/post');
+      s.changeAttribute(form, 'action-xhr', 'https://example.com/post');
       expect(form.getAttribute('action-xhr')).to.equal(
         'https://example.com/post'
       );
 
       const input = win.document.createElement('input');
-      s.mutateAttribute(input, 'value', 'foo');
+      s.changeAttribute(input, 'value', 'foo');
       expect(input.getAttribute('value')).to.equal('foo');
+    });
+  });
+
+  describe('storage', () => {
+    it('getStorage()', () => {
+      it('should be initially empty', () => {
+        expect(s.getStorage('local')).to.deep.equal({});
+        expect(s.getStorage('session')).to.deep.equal({});
+      });
+
+      it('should return localStorage data', () => {
+        win.localStorage.setItem('foo', 'bar');
+        expect(s.getStorage('local')).to.deep.equal({foo: 'bar'});
+        expect(s.getStorage('session')).to.deep.equal({});
+      });
+
+      it('should return sessionStorage data', () => {
+        win.sessionStorage.setItem('abc', '123');
+        expect(s.getStorage('local')).to.deep.equal({});
+        expect(s.getStorage('session')).to.deep.equal({abc: '123'});
+      });
+
+      it('should filter amp-* keys', () => {
+        win.localStorage.setItem('amp-foo', 'bar');
+        win.sessionStorage.setItem('amp-baz', 'qux');
+        expect(s.getStorage('local')).to.deep.equal({foo: 'bar'});
+        expect(s.getStorage('session')).to.deep.equal({});
+      });
+    });
+
+    describe('changeStorage()', () => {
+      it('should set items', () => {
+        s.changeStorage('local', 'x', '1');
+        expect(win.localStorage.length).to.equal(1);
+        expect(win.localStorage.getItem('x')).to.equal('1');
+
+        s.changeStorage('session', 'y', '2');
+        expect(win.sessionStorage.length).to.equal(1);
+        expect(win.sessionStorage.getItem('y')).to.equal('2');
+      });
+
+      it('should not set items with amp-* keys', () => {
+        allowConsoleError(() => {
+          s.changeStorage('local', 'amp-x', '1');
+        });
+        expect(win.localStorage.length).to.equal(0);
+        expect(win.localStorage.getItem('amp-x')).to.be.null;
+
+        allowConsoleError(() => {
+          s.changeStorage('session', 'amp-y', '2');
+        });
+        expect(win.sessionStorage.length).to.equal(0);
+        expect(win.sessionStorage.getItem('amp-y')).to.be.null;
+      });
+
+      it('should remove items', () => {
+        win.localStorage.setItem('x', '1');
+        s.changeStorage('local', 'x', null);
+        expect(win.localStorage.length).to.equal(0);
+        expect(win.localStorage.getItem('x')).to.be.null;
+
+        win.sessionStorage.setItem('y', '2');
+        s.changeStorage('session', 'y', null);
+        expect(win.sessionStorage.length).to.equal(0);
+        expect(win.sessionStorage.getItem('y')).to.be.null;
+      });
+
+      it('should not remove items with amp-* keys', () => {
+        win.localStorage.setItem('amp-x', '1');
+        allowConsoleError(() => {
+          s.changeStorage('local', 'amp-x', null);
+        });
+        expect(win.localStorage.length).to.equal(1);
+        expect(win.localStorage.getItem('amp-x')).to.equal('1');
+
+        win.sessionStorage.setItem('amp-y', '2');
+        allowConsoleError(() => {
+          s.changeStorage('session', 'amp-y', null);
+        });
+        expect(win.sessionStorage.length).to.equal(1);
+        expect(win.sessionStorage.getItem('amp-y')).to.equal('2');
+      });
+
+      it('should not support Storage.clear()', () => {
+        win.localStorage.setItem('x', '1');
+        allowConsoleError(() => {
+          s.changeStorage('local', null, null);
+        });
+        expect(win.localStorage.length).to.equal(1);
+        expect(win.localStorage.getItem('x')).to.equal('1');
+      });
     });
   });
 });
