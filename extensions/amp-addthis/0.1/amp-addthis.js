1b4965e67a4566351dbfbeed8166a2a80f456c19
Jing <chris.tao@oracle.com>
2019-07-23 15:03:31 -0400

    create css and close button for listing message tool

--- extensions/amp-addthis/0.1/amp-addthis.js
+++ extensions/amp-addthis/0.1/amp-addthis.js
@@ -223,6 +223,25 @@ class AmpAddThis extends AMP.BaseElement {
       // Only the component that registers the page view listens for x-frame
       // events.
       this.setupListeners_({ampDoc, loc, pubId: this.pubId_});
+
+      // Create close button for listing tool
+      if (this.element.getAttribute('data-widget-type') === 'messages'){
+        const closeButton = createElementWithAttributes(
+          this.win.document,
+          'div',
+          dict({
+            'id': 'closeButton',
+          })
+        );
+        setStyle(closeButton, 'float', 'right');
+        setStyle(closeButton, 'cursor', 'pointer');
+        setStyle(closeButton, 'position', 'relative');
+        setStyle(closeButton, 'z-index', '1');
+        closeButton.onclick = function(){
+          this.parentNode.parentNode.removeChild(this.parentNode);
+        }
+        this.element.appendChild(closeButton);
+      }
     }
   }
 
