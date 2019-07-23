0056da221408a6359337f9c910ad960929c77e11
Erwin Mombay <erwinm@google.com>
2019-07-23 13:25:46 -0700

    remove package changes

--- build-system/eslint-rules/no-duplicate-name-typedef.js
+++ build-system/eslint-rules/no-duplicate-name-typedef.js
@@ -35,7 +35,7 @@ module.exports = function(context) {
 
       // We can assume theres only 1 variable declaration  when a typedef
       // annotation is found. This is because Closure Compiler does not allow
-      // declaration of multiple variable with a shared type information.
+      // declaration of multiple variables with a shared type information.
       const typedefName = node.declarations[0].id.name;
 
       const typedefLocation = typedefs.get(typedefName);
