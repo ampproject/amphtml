976b1b37f8fb61e87202a9a2e08c1ae52d4da155
Raghu Simha <rsimha@amp.dev>
2019-07-23 17:48:47 -0400

    Check if the PR branch was properly forked from `master`

--- build-system/pr-check/dist-bundle-size.js
+++ build-system/pr-check/dist-bundle-size.js
@@ -29,6 +29,7 @@ const {
   stopTimer,
   timedExecOrDie: timedExecOrDieBase,
   uploadDistOutput,
+  verifyBranchCreationPoint,
 } = require('./utils');
 const {determineBuildTargets} = require('./build-targets');
 const {isTravisPullRequestBuild} = require('../travis');
@@ -53,6 +54,11 @@ function main() {
     timedExecOrDie('gulp bundle-size --on_push_build');
     uploadDistOutput(FILENAME);
   } else {
+    if (!verifyBranchCreationPoint(FILENAME)) {
+      stopTimer(FILENAME, FILENAME, startTime);
+      process.exitCode = 1;
+      return;
+    }
     printChangeSummary(FILENAME);
     const buildTargets = determineBuildTargets(FILENAME);
     if (
