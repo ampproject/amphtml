f391cb3c0e73e5b7db97490da9bbafe1c1e658c4
Alan Orozco <alanorozco@users.noreply.github.com>
2019-07-23 14:25:09 -0700

    🗑 Disable auto-lightbox on formats other than `amp/⚡` (#23228)
    amp-lightbox-gallery is not allowed on amp4email, which makes the loading of this extension unnecessary and could lead to bugs.
    
    
    This enables it only for `<html amp>` (or the cute version  `<html ⚡>`) and disables it everywhere else.
    
    Proxy-origin check also moves from extension into core.

--- src/auto-lightbox.js
+++ src/auto-lightbox.js
@@ -17,6 +17,8 @@
 import {ChunkPriority, chunk} from './chunk';
 import {Services} from './services';
 import {dev} from './log';
+import {getMode} from './mode';
+import {isAmphtml} from './format';
 
 /** @const @enum {string} */
 export const AutoLightboxEvents = {
@@ -27,12 +29,46 @@ export const AutoLightboxEvents = {
 
 /**
  * @param {!./service/ampdoc-impl.AmpDoc} ampdoc
+ * @return {boolean}
+ */
+function isProxyOriginOrLocalDev(ampdoc) {
+  // Allow `localDev` in lieu of proxy origin for manual testing, except in
+  // tests where we need to actually perform the check.
+  const {win} = ampdoc;
+  if (getMode(win).localDev && !getMode(win).test) {
+    return true;
+  }
+
+  // An attached node is required for proxy origin check. If no elements are
+  // present, short-circuit.
+  if (!ampdoc.isSingleDoc()) {
+    return false;
+  }
+
+  const {documentElement} = ampdoc.getRootNode();
+  if (!documentElement) {
+    return false;
+  }
+
+  // TODO(alanorozco): Additionally check for transformed, webpackaged flag.
+  // See git.io/fhQ0a (#20359) for details.
+  return Services.urlForDoc(documentElement).isProxyOrigin(win.location);
+}
+
+/**
+ * @param {!./service/ampdoc-impl.AmpDoc} ampdoc
  */
 export function installAutoLightboxExtension(ampdoc) {
+  const {win} = ampdoc;
+  // Only enabled on proxy origins for which the document is tagged as
+  // <html amp> or <html ⚡>.
+  if (!isAmphtml(win.document) || !isProxyOriginOrLocalDev(ampdoc)) {
+    return;
+  }
   chunk(
     ampdoc,
     () => {
-      Services.extensionsFor(ampdoc.win).installExtensionForDoc(
+      Services.extensionsFor(win).installExtensionForDoc(
         ampdoc,
         'amp-auto-lightbox'
       );
