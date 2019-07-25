6933e36f309b54623170f3dddd99b64ea35107db
Shihua Zheng <powerivq@users.noreply.github.com>
2019-07-25 14:39:47 -0700

    Use experiment branch to gate fie-css-cleanup (#23437)
    * Use experiment branch to gate fie-css-cleanup
    
    * Lint
    
    * Remove FIE experiment selection
    
    * Remove control group
    
    * Fix typo
    
    * remove env
    
    * Review comments
    
    * Refactor
    
    * Lint

--- extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
+++ extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
@@ -28,7 +28,6 @@ import {
 } from '../../../ads/google/utils';
 import {
   ADX_ADY_EXP,
-  FIE_CSS_CLEANUP_EXP,
   QQID_HEADER,
   SANDBOX_HEADER,
   ValidAdContainerTypes,
@@ -47,6 +46,7 @@ import {
 import {AdsenseSharedState} from './adsense-shared-state';
 import {AmpA4A} from '../../amp-a4a/0.1/amp-a4a';
 import {CONSENT_POLICY_STATE} from '../../../src/consent-state';
+import {FIE_CSS_CLEANUP_EXP} from '../../../src/friendly-iframe-embed';
 import {Navigation} from '../../../src/service/navigation';
 import {Services} from '../../../src/services';
 import {
