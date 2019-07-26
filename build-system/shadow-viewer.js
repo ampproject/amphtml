b6d45be92c3ae7585a07afad4a03a47c63e47677
Raghu Simha <rsimha@amp.dev>
2019-07-26 14:37:34 -0400

    🏗🚮 Remove `yarn` hack for local `eslint` rules (#23545)

--- build-system/shadow-viewer.js
+++ build-system/shadow-viewer.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* eslint-disable amphtml-internal/html-template */
+/* eslint-disable local/html-template */
 /* eslint-disable indent */
 
 const {html} = require('./app-index/html');
