13da9b1a06adeee857f2fc1084f203f8ab08f903
Hongfei Ding <lannka@users.noreply.github.com>
2019-07-26 11:25:42 -0700

    Separate install / access of fie to different files to avoid circular dependencies (#23531)
    * Separate install / access of fie to different files to avoid circular dependencies.
    
    * Move getFie() to iframe-helper.js
    
    * move
    
    * lint

--- extensions/amp-animation/0.1/web-animations.js
+++ extensions/amp-animation/0.1/web-animations.js
@@ -41,7 +41,7 @@ import {extractKeyframes} from './parsers/keyframes-extractor';
 import {getMode} from '../../../src/mode';
 import {isArray, isObject, toArray} from '../../../src/types';
 import {isExperimentOn} from '../../../src/experiments';
-import {isInFie} from '../../../src/friendly-iframe-embed';
+import {isInFie} from '../../../src/iframe-helper';
 import {map} from '../../../src/utils/object';
 import {parseCss} from './parsers/css-expr';
 
