917372f568510225535f9d05a3b4dd700655bb54
glevitzky <levitzky@google.com>
2019-07-30 14:00:37 -0400

    Add user warnings for failed fluid expansion. (#23354)

--- extensions/amp-ad-network-doubleclick-impl/0.1/safeframe-host.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/safeframe-host.js
@@ -15,7 +15,7 @@
  */
 
 import {Services} from '../../../src/services';
-import {dev, devAssert} from '../../../src/log';
+import {dev, devAssert, user} from '../../../src/log';
 import {dict, hasOwn} from '../../../src/utils/object';
 import {getData} from '../../../src/event-helper';
 import {getStyle, setStyles} from '../../../src/style';
@@ -746,12 +746,7 @@ export class SafeframeHostApi {
         this.checkStillCurrent_();
         this.onFluidResize_(newHeight);
       })
-      .catch(err => {
-        if (err.message == 'CANCELLED') {
-          dev().error(TAG, err);
-          return;
-        }
-      });
+      .catch(err => user().warn(TAG, err));
   }
 
   /**
