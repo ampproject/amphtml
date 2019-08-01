cb4bb3091fa1c7750677d11a6ad241e09a81e98f
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2019-07-31 21:00:52 -0700

    localStorage usage Guideline (#23589)
    * doc
    
    * address comments
    
    * update list
    
    * update presubmit check info
    
    * lint

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -475,7 +475,12 @@ const forbiddenTerms = {
     whitelist: ['extension/amp-bind/0.1/test/test-bind-expr.js'],
   },
   'storageForDoc': {
-    message: requiresReviewPrivacy,
+    message:
+      requiresReviewPrivacy +
+      ' Please refer to spec/amp-localstorage.md for more information on' +
+      ' the storage service usage.' +
+      ' Once approved, please also update the spec/amp-localstorage.md to' +
+      ' include your usage.',
     whitelist: [
       // Storage service is not allowed in amp4ads. No usage should there be
       // in extensions listed in the amp4ads spec:
