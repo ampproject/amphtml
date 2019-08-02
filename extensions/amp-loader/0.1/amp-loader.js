b109e3a3a5485fcb8d4a37751b1f7eb856e3743c
Sepand Parhami <sparhami@google.com>
2019-08-02 14:34:45 -0700

    Support small loader, cache loader DOM.

--- extensions/amp-loader/0.1/amp-loader.js
+++ extensions/amp-loader/0.1/amp-loader.js
@@ -16,6 +16,7 @@
 
 import {CSS} from '../../../build/amp-loader-0.1.css';
 import {Services} from '../../../src/services';
+import {createSpinnerDom} from './spinner';
 import {htmlFor} from '../../../src/static-template';
 import {installStylesForDoc} from '../../../src/style-installer';
 import {isIframeVideoPlayerComponent} from '../../../src/layout';
@@ -48,6 +49,41 @@ const DEFAULT_PLACEHOLDER_WHITELIST_NONE_VIDEO = {
 };
 
 /**
+ * Used to cache the loader DOM once created, so we do not need to recreate it
+ * each time.
+ * @type {?Element}
+ */
+let loaderDom = null;
+
+/**
+ * @param {!AmpElement} element Used to get a document to build HTML with.
+ * @return {!Element} The loader DOM.
+ */
+function getLoaderDom(element) {
+  if (!loaderDom) {
+    const html = htmlFor(element);
+    /*
+     * There is an extra inner div here for backward compatibility with
+     * customizing loaders. The common and documented CSS for customizing
+     * loaders includes a style to hide the old three dots via:
+     *  .my-custom-loader .amp-active > div {
+     *     display: none;
+     *  }
+     * The extra div mimic a similar DOM.
+     */
+    loaderDom = html`
+      <div>
+        <div class="i-amphtml-new-loader-shim"></div>
+        <div class="i-amphtml-new-loader-logo"></div>
+      </div>
+    `;
+    loaderDom.appendChild(createSpinnerDom(html));
+  }
+
+  return loaderDom.cloneNode(true);
+}
+
+/**
  * Helper class to build the new loader's DOM.
  */
 class LoaderBuilder {
@@ -75,140 +111,9 @@ class LoaderBuilder {
    * Builds the loader's DOM.
    */
   build() {
-    this.buildContainers_();
+    this.domRoot_.appendChild(getLoaderDom(this.element_));
     this.maybeAddDefaultPlaceholder_();
     this.maybeAddLoaderAnimation_();
-  }
-
-  /**
-   * Builds the wrappers for the loader.
-   * @private
-   */
-  buildContainers_() {
-    // TODO(sparhami) Cache the generated spinner DOM and `cloneNode(true)`.
-    this.domRoot_.appendChild(this.createSpinnerDom_());
-  }
-
-  /**
-   * @return {!Element}
-   */
-  createSpinnerDom_() {
-    /*
-     * There is an extra inner div here for backward compatibility with
-     * customizing loaders. The common and documented CSS for customizing
-     * loaders includes a style to hide the old three dots via:
-     *  .my-custom-loader .amp-active > div {
-     *     display: none;
-     *  }
-     * The extra div mimic a similar DOM.
-     */
-    const html = htmlFor(this.element_);
-    return html`
-      <div>
-        <div class="i-amphtml-new-loader-shim"></div>
-        <div class="i-amphtml-new-loader-logo"></div>
-        <svg class="i-amphtml-new-loader-spinner" viewbox="0 0 48 48">
-          <path
-            class="i-amphtml-new-loader-spinner-path"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1.5px"
-            d="
-                M 24 2 A 22 22 0 1 0 24.01 2
-                M 24.00 2.00 a 22 22 0 1 0 0.00 0.00
-                M 73.15 2.03 a 22 22 0 1 0 3.42 0.45
-                M 122.30 2.12 a 22 22 0 1 0 6.65 1.78
-                M 171.44 2.27 a 22 22 0 1 0 9.49 3.93
-                M 220.57 2.48 a 22 22 0 1 0 11.78 6.80
-                M 269.69 2.75 a 22 22 0 1 0 13.36 10.25
-                M 318.80 3.08 a 22 22 0 1 0 14.12 14.12
-                M 367.88 3.46 a 22 22 0 1 0 14.00 18.24
-                M 416.95 3.90 a 22 22 0 1 0 12.93 22.40
-                M 465.99 4.40 a 22 22 0 1 0 10.94 26.40
-                M 515.00 4.95 a 22 22 0 1 0 8.05 30.05
-                M 563.98 5.55 a 22 22 0 1 0 4.37 33.17
-                M 612.93 6.20 a 22 22 0 1 0 0.00 35.60
-                M 661.85 6.90 a 22 22 0 1 0 -4.90 37.20
-                M 710.72 7.65 a 22 22 0 1 0 -10.15 37.87
-                M 759.56 8.44 a 22 22 0 1 0 -15.56 37.56
-                M 808.35 9.28 a 22 22 0 1 0 -20.92 36.24
-                M 857.10 10.15 a 22 22 0 1 0 -26.05 33.94
-                M 905.80 11.07 a 22 22 0 1 0 -30.73 30.73
-                M 954.45 12.02 a 22 22 0 1 0 -34.80 26.70
-                M 1003.05 13.00 a 22 22 0 1 0 -38.11 22.00
-                M 1051.60 14.01 a 22 22 0 0 0 -40.53 16.79
-                M 1100.10 15.05 a 22 22 0 0 0 -41.98 11.25
-                M 1148.54 16.12 a 22 22 0 0 0 -42.42 5.58
-                M 1196.92 17.20 a 22 22 0 0 0 -41.85 -0.00
-                M 1245.25 18.31 a 22 22 0 0 0 -40.30 -5.31
-                M 1293.52 19.43 a 22 22 0 0 0 -37.87 -10.15
-                M 1341.73 20.56 a 22 22 0 0 0 -34.66 -14.36
-                M 1389.88 21.70 a 22 22 0 0 0 -30.83 -17.80
-                M 1438.00 24.00 a 22 22 0 0 0 -22.00 -22.00
-                M 1485.73 27.44 a 22 22 0 0 0 -20.58 -25.41
-                M 1532.92 30.80 a 22 22 0 0 0 -18.62 -28.68
-                M 1579.60 33.99 a 22 22 0 0 0 -16.16 -31.72
-                M 1625.80 36.93 a 22 22 0 0 0 -13.22 -34.45
-                M 1671.56 39.56 a 22 22 0 0 0 -9.86 -36.81
-                M 1716.93 41.80 a 22 22 0 0 0 -6.13 -38.72
-                M 1761.99 43.60 a 22 22 0 0 0 -2.10 -40.14
-                M 1806.80 44.92 a 22 22 0 0 0 2.15 -41.02
-                M 1851.44 45.73 a 22 22 0 0 0 6.55 -41.33
-                M 1896.00 46.00 a 22 22 0 0 0 11.00 -41.05
-                M 1940.56 45.73 a 22 22 0 0 0 15.42 -40.18
-                M 1985.20 44.92 a 22 22 0 0 0 19.73 -38.72
-                M 2030.01 43.60 a 22 22 0 0 0 23.83 -36.70
-                M 2075.07 41.80 a 22 22 0 0 0 27.65 -34.15
-                M 2120.44 39.56 a 22 22 0 0 0 31.11 -31.11
-                M 2166.20 36.93 a 22 22 0 1 0 34.15 -27.65
-                M 2212.40 33.99 a 22 22 0 1 0 36.70 -23.83
-                M 2259.08 30.80 a 22 22 0 1 0 38.72 -19.73
-                M 2306.27 27.44 a 22 22 0 1 0 40.18 -15.42
-                M 2354.00 24.00 a 22 22 0 1 0 41.05 -11.00
-                M 2402.27 20.56 a 22 22 0 1 0 41.33 -6.55
-                M 2451.08 17.20 a 22 22 0 1 0 41.02 -2.15
-                M 2500.40 14.01 a 22 22 0 1 0 40.14 2.10
-                M 2550.20 11.07 a 22 22 0 1 0 38.72 6.13
-                M 2600.44 8.44 a 22 22 0 1 0 36.81 9.86
-                M 2651.07 6.20 a 22 22 0 1 0 34.45 13.22
-                M 2702.01 4.40 a 22 22 0 1 0 31.72 16.16
-                M 2753.20 3.08 a 22 22 0 1 0 28.68 18.62
-                M 2804.56 2.27 a 22 22 0 1 0 25.41 20.58
-                M 2856.00 2.00 a 22 22 0 1 0 22.00 22.00
-                M 2905.15 2.03 a 22 22 0 1 0 20.58 25.41
-                M 2954.30 2.12 a 22 22 0 1 0 18.62 28.68
-                M 3003.44 2.27 a 22 22 0 1 0 16.16 31.72
-                M 3052.57 2.48 a 22 22 0 1 0 13.22 34.45
-                M 3101.69 2.75 a 22 22 0 1 0 9.86 36.81
-                M 3150.80 3.08 a 22 22 0 1 0 6.13 38.72
-                M 3199.88 3.46 a 22 22 0 1 0 2.10 40.14
-                M 3248.95 3.90 a 22 22 0 1 0 -2.15 41.02
-                M 3297.99 4.40 a 22 22 0 1 0 -6.55 41.33
-                M 3347.00 4.95 a 22 22 0 1 0 -11.00 41.05
-                M 3395.98 5.55 a 22 22 0 1 0 -15.42 40.18
-                M 3444.93 6.20 a 22 22 0 1 0 -19.73 38.72
-                M 3493.85 6.90 a 22 22 0 1 0 -23.83 36.70
-                M 3542.72 7.65 a 22 22 0 1 0 -27.65 34.15
-                M 3591.56 8.44 a 22 22 0 0 0 -31.11 31.11
-                M 3640.35 9.28 a 22 22 0 0 0 -34.15 27.65
-                M 3689.10 10.15 a 22 22 0 0 0 -36.70 23.83
-                M 3737.80 11.07 a 22 22 0 0 0 -38.72 19.73
-                M 3786.45 12.02 a 22 22 0 0 0 -40.18 15.42
-                M 3835.05 13.00 a 22 22 0 0 0 -41.05 11.00
-                M 3883.60 14.01 a 22 22 0 0 0 -41.33 6.55
-                M 3932.10 15.05 a 22 22 0 0 0 -41.02 2.15
-                M 3980.54 16.12 a 22 22 0 0 0 -40.14 -2.10
-                M 4028.92 17.20 a 22 22 0 0 0 -38.72 -6.13
-                M 4077.25 18.31 a 22 22 0 0 0 -36.81 -9.86
-                M 4125.52 19.43 a 22 22 0 0 0 -34.45 -13.22
-                M 4173.73 20.56 a 22 22 0 0 0 -31.72 -16.16
-                M 4221.88 21.70 a 22 22 0 0 0 -28.68 -18.62
-                M 4269.97 22.85 a 22 22 0 0 0 -25.41 -20.58
-            "
-          />
-        </svg>
-      </div>
-    `;
   }
 
   /**
@@ -308,28 +213,33 @@ class LoaderBuilder {
    * }}
    */
   getCustomLogo_() {
+    if (isIframeVideoPlayerComponent(this.element_.tagName)) {
+      return {
+        content: this.getVideoPlayerLogo_(),
+      };
+    }
+    return this.element_.createLoaderLogo();
+  }
+
+  /**
+   * @return {!Element} The logo for video players
+   * @private
+   */
+  getVideoPlayerLogo_() {
     // Keeping the video logo here short term.
     // This is because there is no single CSS for all players, there is
     // video-interface but not all players implement it. Also the SVG is not
     // that big.
-    // We need to move most of loaders code out of v0 anyway, see
-    // https://github.com/ampproject/amphtml/issues/23108.
-    if (isIframeVideoPlayerComponent(this.element_.tagName)) {
-      const html = htmlFor(this.element_);
-      const content = html`
-        <svg viewBox="0 0 72 72">
-          <path
-            class="i-amphtml-new-loader-white-on-shim"
-            fill="currentColor"
-            d="M41,34.5V31c0-0.5-0.4-1-1-1H27c-0.5,0-1,0.5-1,1v10c0,0.6,0.5,1,1,1h13c0.6,0,1-0.4,1-1v-3.5l5,4v-11L41,34.5z"
-          />
-        </svg>
-      `;
-      return {
-        content,
-      };
-    }
-    return this.element_.createLoaderLogo();
+    const html = htmlFor(this.element_);
+    return html`
+      <svg viewBox="0 0 72 72">
+        <path
+          class="i-amphtml-new-loader-white-on-shim"
+          fill="currentColor"
+          d="M41,34.5V31c0-0.5-0.4-1-1-1H27c-0.5,0-1,0.5-1,1v10c0,0.6,0.5,1,1,1h13c0.6,0,1-0.4,1-1v-3.5l5,4v-11L41,34.5z"
+        />
+      </svg>
+    `;
   }
 
   /**
