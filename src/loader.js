73c6feb623bfcf1fb353ecbfbf896cb3530b810a
Sepand Parhami <sparhami@google.com>
2019-08-06 17:41:35 -0700

    Implement new loader for ads (#23687)
    - Implement new loader for ads (amp-ad)
    - Make amp-embed use the default loader

--- src/loader.js
+++ src/loader.js
@@ -115,11 +115,6 @@ export function createNewLoaderElement(
  * @return {boolean}
  */
 export function isNewLoaderExperimentEnabled(element) {
-  // TODO(sparhami): Implement loader for Ads
-  // Temporarily excluding the amp-ads from this experiment
-  if (element.tagName == 'AMP-AD' || element.tagName == 'AMP-EMBED') {
-    return false;
-  }
   const win = toWin(element.ownerDocument.defaultView);
   return isExperimentOn(win, 'new-loaders');
 }
