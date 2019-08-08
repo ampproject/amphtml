c22d9b896cbc832bbf188b283f3e0c7f2de322a2
Kunal Purohit <kunalpurohit@google.com>
2019-08-08 15:47:39 -0700

    lint

--- extensions/amp-form/0.1/amp-form.js
+++ extensions/amp-form/0.1/amp-form.js
@@ -610,7 +610,7 @@ export class AmpForm {
 
   /**
    * Handle form error for presubmit async calls
-   * @param {*} error
+   * @param error
    * @return {Promise}
    */
   handlePresubmitError_(error) {
