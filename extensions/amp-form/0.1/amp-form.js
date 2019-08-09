673ba0450a902c9644344a0419a8301bd20a09d7
Kunal Purohit <kunalpurohit@google.com>
2019-08-09 10:38:42 -0700

    param doc

--- extensions/amp-form/0.1/amp-form.js
+++ extensions/amp-form/0.1/amp-form.js
@@ -610,7 +610,7 @@ export class AmpForm {
 
   /**
    * Handle form error for presubmit async calls
-   * @param error
+   * @param {Object} error
    * @return {Promise}
    */
   handlePresubmitError_(error) {
