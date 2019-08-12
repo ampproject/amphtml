5ef76d582ddaba11fe55c4e16c3f0e21a7dc1bef
Steven Ye <8905737+leafsy@users.noreply.github.com>
2019-08-12 13:15:49 -0700

    🐛Prevent automatic sizing for amp-imgs with intrinsic layout (#23880)
    * Skip auto-sizes for imgs with intrinsic layout
    
    * Add unit test for intrinsic img sizes
    
    * Link to original issue in comment

--- builtins/amp-img.js
+++ builtins/amp-img.js
@@ -198,6 +198,12 @@ export class AmpImg extends BaseElement {
     if (sizes) {
       return;
     }
+    // Auto-sizes are not compatible with intrinsic layout.
+    // See https://github.com/ampproject/amphtml/issues/23453 for context.
+    const layout = this.getLayout();
+    if (layout === Layout.INTRINSIC) {
+      return;
+    }
     // Sizes is useless without the srcset attribute or if the srcset
     // attribute uses the x descriptor.
     const srcset = this.element.getAttribute('srcset');
@@ -215,7 +221,7 @@ export class AmpImg extends BaseElement {
     const entry = `(max-width: ${viewportWidth}px) ${width}px, `;
     let defaultSize = width + 'px';
 
-    if (this.getLayout() !== Layout.FIXED) {
+    if (layout !== Layout.FIXED) {
       const ratio = Math.round((width * 100) / viewportWidth);
       defaultSize = Math.max(ratio, 100) + 'vw';
     }
