1d41c316c8e286c274945c468b1d68ddbf9982af
Jon Newmuis <newmuis@google.com>
2019-08-14 15:38:38 -0400

    First pass at implementation

--- src/amp-story-embed.js
+++ src/amp-story-embed.js
@@ -14,6 +14,30 @@
  * limitations under the License.
  */
 
+/** @const {string} */
+const SCRIPT_TAG_NAME = 'script';
+
+/** @const {string} */
+const SCRIPT_TYPE = 'application/json';
+
+/** @enum {string} */
+const LoadStateClass = {
+  LOADING: 'loading',
+  LOADED: 'loaded',
+  ERROR: 'error',
+};
+
+/** @const {string} */
+const CSS = `
+  :host { all: initial; display: block; border-radius: 0; width: 405px; height: 720px; }
+  main { background: #202125; }
+  iframe { height: 100%; width: 100%; border: 0; opacity: 0; transition: opacity 500ms ease }
+  .loaded iframe { opacity: 1; }
+`;
+
+/** @typedef {{src: string}} */
+let AmpStoryEmbedConfig;
+
 /**
  * Note that this is a vanilla JavaScript class and should not depend on AMP
  * services, as v0.js is not expected to be loaded in this context.
@@ -22,11 +46,76 @@ export class AmpStoryEmbed extends HTMLElement {
   /** @constructor */
   constructor() {
     super();
-    console.log('embed!');
+
+    /** @private {!AmpStoryEmbedConfig} */
+    this.config_;
+
+    /** @private {!Element} */
+    this.rootEl_;
+
+    /** @private {!HTMLIframeElement} */
+    this.iframeEl_;
   }
 
   /** @override */
   connectedCallback() {
-    console.log('connected');
+    this.parseConfig_();
+    this.initializeShadowRoot_();
+    this.buildIframe_();
+  }
+
+  /** @private */
+  parseConfig_() {
+    console.assert(this.childElementCount > 0, 'Missing configuration.');
+
+    const scriptEl = this.children[0];
+    console.log(scriptEl);
+    console.assert(
+      this.childElementCount === 1 &&
+        scriptEl.tagName.toLowerCase() === SCRIPT_TAG_NAME &&
+        scriptEl.getAttribute('type').toLowerCase() === SCRIPT_TYPE,
+      'Must have one <script type="application/json"> configuration'
+    );
+
+    // Parse JSON
+    this.config_ = JSON.parse(scriptEl.textContent);
+  }
+
+  /** @private */
+  initializeShadowRoot_() {
+    // Create shadow root
+    const shadowRoot = this.attachShadow({mode: 'open'});
+
+    // Inject default styles
+    const styleEl = document.createElement('style');
+    styleEl.textContent = CSS;
+    shadowRoot.appendChild(styleEl);
+
+    this.rootEl_ = document.createElement('main');
+    shadowRoot.appendChild(this.rootEl_);
+  }
+
+  /** @private */
+  buildIframe_() {
+    this.iframeEl_ = document.createElement('iframe');
+    this.iframeEl_.setAttribute('src', this.config_.src);
+    this.initializeLoadingListeners_();
+    this.rootEl_.appendChild(this.iframeEl_);
+  }
+
+  /** @private  */
+  initializeLoadingListeners_() {
+    this.rootEl_.classList.add(LoadStateClass.LOADING);
+
+    this.iframeEl_.onload = () => {
+      this.rootEl_.classList.remove(LoadStateClass.LOADING);
+      this.rootEl_.classList.add(LoadStateClass.LOADED);
+    };
+    this.iframeEl_.onerror = () => {
+      this.rootEl_.classList.remove(LoadStateClass.LOADING);
+      this.rootEl_.classList.add(LoadStateClass.ERROR);
+    };
   }
 }
+
+window.customElements.define('amp-story-embed', AmpStoryEmbed);
