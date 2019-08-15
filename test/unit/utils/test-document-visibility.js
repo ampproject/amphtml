ea7231ec7c342ebfc5222a0a27f65f88e01b4e33
Dima Voytenko <dvoytenko@gmail.com>
2019-08-15 13:48:08 -0700

    nits

--- test/unit/utils/test-document-visibility.js
+++ test/unit/utils/test-document-visibility.js
@@ -31,7 +31,7 @@ describes.sandboxed('document-visibility', {}, env => {
     };
   });
 
-  function prop(name, value) {
+  function overridePropertyForDoc(name, value) {
     env.sandbox.defineProperty(doc, name, {value});
   }
 
@@ -41,33 +41,33 @@ describes.sandboxed('document-visibility', {}, env => {
   });
 
   it('should resolve non-vendor hidden property', () => {
-    prop('hidden', true);
+    overridePropertyForDoc('hidden', true);
     expect(isDocumentHidden(doc)).to.be.true;
     expect(getDocumentVisibilityState(doc)).to.equal('hidden');
   });
 
   it('should resolve non-vendor visibilityState property', () => {
-    prop('visibilityState', 'hidden');
+    overridePropertyForDoc('visibilityState', 'hidden');
     expect(isDocumentHidden(doc)).to.be.true;
     expect(getDocumentVisibilityState(doc)).to.equal('hidden');
   });
 
   it('should prefer visibilityState property to hidden', () => {
-    prop('hidden', true);
-    prop('visibilityState', 'visible');
+    overridePropertyForDoc('hidden', true);
+    overridePropertyForDoc('visibilityState', 'visible');
     expect(isDocumentHidden(doc)).to.be.false;
     expect(getDocumentVisibilityState(doc)).to.equal('visible');
   });
 
   it('should consider prerender as visible', () => {
-    prop('visibilityState', 'prerender');
+    overridePropertyForDoc('visibilityState', 'prerender');
     expect(isDocumentHidden(doc)).to.be.true;
     expect(getDocumentVisibilityState(doc)).to.equal('prerender');
   });
 
   it('should resolve non-vendor visibilitychange event', () => {
     function handler() {}
-    prop('onvisibilitychange', null);
+    overridePropertyForDoc('onvisibilitychange', null);
 
     addDocumentVisibilityChangeListener(doc, handler);
     expect(doc.addEventListener).to.be.calledOnce.calledWith(
@@ -83,21 +83,21 @@ describes.sandboxed('document-visibility', {}, env => {
   });
 
   it('should resolve vendor hidden property', () => {
-    prop('webkitHidden', true);
+    overridePropertyForDoc('webkitHidden', true);
     expect(isDocumentHidden(doc)).to.be.true;
     expect(getDocumentVisibilityState(doc)).to.equal('hidden');
   });
 
   it('should resolve vendor visibilityState property', () => {
-    prop('webkitVisibilityState', 'prerender');
+    overridePropertyForDoc('webkitVisibilityState', 'prerender');
     expect(isDocumentHidden(doc)).to.be.true;
     expect(getDocumentVisibilityState(doc)).to.equal('prerender');
   });
 
   it('should resolve vendor visibilitychange event', () => {
     function handler() {}
-    prop('webkitHidden', true);
-    prop('onwebkitvisibilitychange', null);
+    overridePropertyForDoc('webkitHidden', true);
+    overridePropertyForDoc('onwebkitvisibilitychange', null);
 
     addDocumentVisibilityChangeListener(doc, handler);
     expect(doc.addEventListener).to.be.calledOnce.calledWith(
