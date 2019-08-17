cbde1a9c9a36437dad013cd8d1d4a33d0de643fa
Justin Ridgewell <jridgewell@google.com>
2019-08-16 20:25:57 -0400

    Refactor transform-log-asserts
    The only big change here is now we'll try to statically analyze assertions.
    
    - If we're asserting a type and we know that the argument is not of that type, we rewrite to a throw expression
    - Else if we're assertiong truthiness and the argument is falsey, we rewrite to a throw expression
    
    This is to better support stubbed out methods, eg, layers:
    
    ```js
    class Resources {
      layersStuff() {
        devAssert(this.layersEnabled_);
        // lots of code that can be DCE'd when layers is statically disabled
      }
    }
    ```

--- build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-string/input.js
+++ build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-string/input.js
@@ -17,5 +17,4 @@ let str = 'foo';
 dev().assertString(str);
 dev().assertString('hello');
 let result = dev().assertString('world');
-let result2 = dev().assertString();
-let result3 = dev().assertString(null);
+let result2 = dev().assertString('');
