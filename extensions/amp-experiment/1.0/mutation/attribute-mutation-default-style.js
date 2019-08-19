89c670e5094e57f6154394ba1babfc58229b7bec
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2019-08-19 16:29:34 -0700

    <amp-experiment> style mutation fix and improvment (#23669)
    * style mutation
    
    * address comment
    
    * address comment
    
    * lint

--- extensions/amp-experiment/1.0/mutation/attribute-mutation-default-style.js
+++ extensions/amp-experiment/1.0/mutation/attribute-mutation-default-style.js
@@ -15,6 +15,16 @@
  */
 
 import {assertAttributeMutationFormat} from './mutation';
+import {assertDoesNotContainDisplay, setStyles} from '../../../../src/style';
+import {dev} from '../../../../src/log';
+import {dict, hasOwn} from '../../../../src/utils/object';
+
+const SUPPORTED_STYLE_VALUE = {
+  'color': /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3});?$/,
+  'background-color': /.*/,
+};
+
+const NON_SPACE_REGEX = /\S/;
 
 /**
  * Mutation for attribute (style) mutations on unspecified elements.
@@ -24,43 +34,80 @@ import {assertAttributeMutationFormat} from './mutation';
 export class AttributeMutationDefaultStyle {
   /**
    * @param {!JsonObject} mutationRecord
-   * @param {!Array<Element>} elements
+   * @param {!Array<!Element>} elements
    */
   constructor(mutationRecord, elements) {
+    /** @private {!JsonObject} */
     this.mutationRecord_ = mutationRecord;
+
+    /** @private {!Array<Element>} */
     this.elements_ = elements;
+
+    /** @private {!JsonObject} */
+    this.styles_ = dict({});
+
     assertAttributeMutationFormat(this.mutationRecord_);
   }
 
   /** @override */
-  validate() {
+  parseAndValidate() {
     const value = this.mutationRecord_['value'];
-
-    // Do not allow Important or HTML Comments
-    if (value.match(/(!\s*important|<!--)/)) {
+    // First check for !important and <;
+    if (value.match(/(!\s*important|<)/)) {
       return false;
     }
 
-    // Allow Color
-    if (value.match(/^color:\s*#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3});?$/)) {
-      return true;
+    // Then seperate the style values to pairs in the format "name : value;"
+    // Already guareentee that ['value'] is defined
+    const pairs = value.split(';');
+    for (let i = 0; i < pairs.length; i++) {
+      if (!NON_SPACE_REGEX.test(pairs[i])) {
+        // Note: treat empty string as valid;
+        continue;
+      }
+      // In format of key:value
+      const pair = pairs[i].split(':');
+      if (pair.length != 2) {
+        // more than one ":" or no ":"
+        // invalid format
+        return false;
+      }
+
+      const key = pair[0].trim();
+      const value = pair[1].trim();
+      if (!this.validateStylePair_(key, value)) {
+        return false;
+      }
+      this.styles_[key] = value;
     }
 
-    // Allow Background color
-    if (
-      value.match(/^background-color:\s*#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3});?$/)
-    ) {
-      return true;
-    }
-
-    return false;
+    return true;
   }
 
   /** @override */
   mutate() {
     this.elements_.forEach(element => {
-      element.setAttribute('style', this.mutationRecord_['value']);
+      setStyles(
+        dev().assertElement(element),
+        assertDoesNotContainDisplay(this.styles_)
+      );
     });
+  }
+
+  /**
+   * Validate the style key value pair is valid
+   * @param {string} key
+   * @param {string} value
+   * @return {boolean}
+   */
+  validateStylePair_(key, value) {
+    if (!hasOwn(SUPPORTED_STYLE_VALUE, key)) {
+      return false;
+    }
+    if (value.match(SUPPORTED_STYLE_VALUE[key])) {
+      return true;
+    }
+    return false;
   }
 
   /** @override */
