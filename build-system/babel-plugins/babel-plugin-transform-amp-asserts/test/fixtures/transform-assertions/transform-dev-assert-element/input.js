08bc72fd568a265a64e0c02b34712bb3c7661f05
Justin Ridgewell <jridgewell@google.com>
2019-08-19 18:21:29 -0400

    🏗 Refactor transform-log-asserts (#24028)

--- build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-element/input.js
+++ build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-dev-assert-element/input.js
@@ -15,4 +15,3 @@
  */
 dev().assertElement(element);
 let result = dev().assertElement(element, 'hello', 'world');
-let result2 = dev().assertElement();
--- build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-devAssert-null/input.js
+++ build-system/babel-plugins/babel-plugin-transform-amp-asserts/test/fixtures/transform-assertions/transform-devAssert-null/input.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/** @type {!Element} */
-(element);
-let result =
-/** @type {!Element} */
-(element);
+devAssert(null);
