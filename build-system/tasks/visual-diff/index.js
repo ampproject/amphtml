53403f9210d3a9d1f9a18b5319c34ba75a492a6d
Shihua Zheng <powerivq@users.noreply.github.com>
2019-08-20 10:41:23 -0700

    Unblock data url for visual diff test (#24059)

--- build-system/tasks/visual-diff/index.js
+++ build-system/tasks/visual-diff/index.js
@@ -244,7 +244,8 @@ async function newPage(browser, viewport = null) {
     );
 
     if (
-      requestUrl.hostname == HOST ||
+      requestUrl.protocol === 'data:' ||
+      requestUrl.hostname === HOST ||
       requestUrl.hostname.endsWith(`.${HOST}`)
     ) {
       return interceptedRequest.continue();
