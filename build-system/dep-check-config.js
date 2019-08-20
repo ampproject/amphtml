1b981479cbe494c862a29c1ca6ec80ff10a1f2f7
Caleb Cordry <ccordry@google.com>
2019-08-20 12:40:40 -0700

    ✨Story Ads: read amp-ad-exit  (#24044)
    * read outlink
    
    * types
    
    * whitelist dep
    
    * fix e2e
    
    * fix compilation
    
    * better todos
    
    * try catch

--- build-system/dep-check-config.js
+++ build-system/dep-check-config.js
@@ -257,6 +257,8 @@ exports.rules = [
       'extensions/amp-story/1.0/animation.js->extensions/amp-animation/0.1/web-animation-types.js',
       // Story ads
       'extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js->extensions/amp-story/1.0/amp-story-store-service.js',
+      // TODO(#24080) Remove this when story ads have full ad network support.
+      'extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js->extensions/amp-ad-exit/0.1/config.js',
       // TODO(ccordry): remove this after createShadowRootWithStyle is moved to src
       'extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js->extensions/amp-story/1.0/utils.js',
 
