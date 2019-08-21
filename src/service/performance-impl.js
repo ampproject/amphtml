b81028f18c2f2c89a444775d8829fb833f7681ff
Wassim Gharbi <wassgha@gmail.com>
2019-08-21 11:04:48 -0700

    🚮 Removes `beforeunload` event use in performance impl (#24065)
    * Removes performance unload use
    
    * Same for shift score
    
    * Removed tests

--- src/service/performance-impl.js
+++ src/service/performance-impl.js
@@ -35,9 +35,6 @@ const QUEUE_LIMIT = 50;
 /** @const {string} */
 const VISIBILITY_CHANGE_EVENT = 'visibilitychange';
 
-/** @const {string} */
-const BEFORE_UNLOAD_EVENT = 'beforeunload';
-
 /**
  * Fields:
  * {{
@@ -162,8 +159,6 @@ export class Performance {
       this.win.PerformanceObserver.supportedEntryTypes.includes('layout-shift');
 
     this.boundOnVisibilityChange_ = this.onVisibilityChange_.bind(this);
-    this.boundTickLayoutJankScore_ = this.tickLayoutJankScore_.bind(this);
-    this.boundTickLayoutShiftScore_ = this.tickLayoutShiftScore_.bind(this);
     this.onViewerVisibilityChange_ = this.onViewerVisibilityChange_.bind(this);
 
     // Add RTV version as experiment ID, so we can slice the data by version.
@@ -220,18 +215,6 @@ export class Performance {
         {capture: true}
       );
 
-      // Safari does not reliably fire the `pagehide` or `visibilitychange`
-      // events when closing a tab, so we have to use `beforeunload`.
-      // See https://bugs.webkit.org/show_bug.cgi?id=151234
-      const platform = Services.platformFor(this.win);
-      if (platform.isSafari()) {
-        // TODO(#23634): Remove, explain or adjust the usage of the unload listener
-        this.win.addEventListener(
-          BEFORE_UNLOAD_EVENT,
-          this.boundTickLayoutJankScore_
-        );
-      }
-
       this.viewer_.onVisibilityChanged(this.onViewerVisibilityChange_);
     }
 
@@ -246,17 +229,6 @@ export class Performance {
         this.boundOnVisibilityChange_,
         {capture: true}
       );
-
-      // Safari does not reliably fire the `pagehide` or `visibilitychange`
-      // events when closing a tab, so we have to use `beforeunload`.
-      // See https://bugs.webkit.org/show_bug.cgi?id=151234
-      const platform = Services.platformFor(this.win);
-      if (platform.isSafari()) {
-        this.win.addEventListener(
-          BEFORE_UNLOAD_EVENT,
-          this.boundTickLayoutShiftScore_
-        );
-      }
 
       this.viewer_.onVisibilityChanged(this.onViewerVisibilityChange_);
     }
@@ -479,10 +451,6 @@ export class Performance {
         this.boundOnVisibilityChange_,
         {capture: true}
       );
-      this.win.removeEventListener(
-        BEFORE_UNLOAD_EVENT,
-        this.boundTickLayoutJankScore_
-      );
     }
   }
 
@@ -513,10 +481,6 @@ export class Performance {
         VISIBILITY_CHANGE_EVENT,
         this.boundOnVisibilityChange_,
         {capture: true}
-      );
-      this.win.removeEventListener(
-        BEFORE_UNLOAD_EVENT,
-        this.boundTickLayoutShiftScore_
       );
     }
   }
