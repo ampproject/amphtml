ba971724c53d0dd8852dc232f47000f07953b688
max-tgam <mbondarenko@globeandmail.com>
2019-08-27 15:22:07 -0400

    Bugfix

--- extensions/amp-analytics/0.1/vendors/snowplow_v2.js
+++ extensions/amp-analytics/0.1/vendors/snowplow_v2.js
@@ -28,25 +28,25 @@ const SNOWPLOW_V2_CONFIG = jsonLiteral({
   'cookies': {
     'enabled': true,
     '_sp_lsid': {
-      'value': '$IF(LINKER_PARAM(linker,_sp_asid), LINKER_PARAM(linker,_sp_asid), QUERY_PARAM(_sp_asid))',
+      'value': '$IF(LINKER_PARAM(linker,_sp_asid),LINKER_PARAM(linker,_sp_asid),QUERY_PARAM(_sp_asid))',
     },
     '_sp_lduid': {
-      'value': '$IF(LINKER_PARAM(linker,_sp_aduid), LINKER_PARAM(linker,_sp_aduid), QUERY_PARAM(_sp_aduid))',
+      'value': '$IF(LINKER_PARAM(linker,_sp_aduid),LINKER_PARAM(linker,_sp_aduid),QUERY_PARAM(_sp_aduid))',
     },
     '_sp_asid': {
-      'value': '$IF(COOKIE(_sp_asid), COOKIE(_sp_asid), COOKIE(_sp_lsid))',
+      'value': '$IF(COOKIE(_sp_asid),COOKIE(_sp_asid),COOKIE(_sp_lsid))',
     },
     '_sp_aduid': {
-      'value': '$IF(COOKIE(_sp_aduid), COOKIE(_sp_aduid), COOKIE(_sp_lduid))',
+      'value': '$IF(COOKIE(_sp_aduid),COOKIE(_sp_aduid),COOKIE(_sp_lduid))',
     },
   },
   'vars': {
     'ampVistorId': 'CLIENT_ID(_sp_id)',
     'generatedSessionId': ['616d70',['a'].map(a=>{return Math.random()}).join().slice(2,4),'-',['a'].map(a=>{return Math.random()}).join().slice(2,6),'-4',['a'].map(a=>{return Math.random()}).join().slice(2,5),'-8',['a'].map(a=>{return Math.random()}).join().slice(2,5),'-',['a'].map(a=>{return Math.random()}).join().slice(2,14)].join(''),
-    'linkerSessionId': '$IF(COOKIE(_sp_asid), COOKIE(_sp_asid), ${generatedSessionId})',
-    'sid': '$IF(${sessionId}, ${sessionId}, ${linkerSessionId})',
-    'vid': '$IF(${spVisitIndex}, ${spVisitIndex}, 0)',
-    'duid': '$IF(${sessionId}, ${domainUserId}, ${ampVistorId})',
+    'linkerSessionId': '$IF(COOKIE(_sp_asid),COOKIE(_sp_asid),${generatedSessionId})',
+    'sid': '$IF(${sessionId},${sessionId},${linkerSessionId})',
+    'vid': '$IF(${spVisitIndex},${spVisitIndex},0)',
+    'duid': '$IF(${sessionId},${domainUserId},${ampVistorId})',
     'customEventTemplate': [
       '{',
         '"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",',
@@ -62,25 +62,25 @@ const SNOWPLOW_V2_CONFIG = jsonLiteral({
   'requests': {
     'aaVersion': 'amp_v2-0.1',
     'basePrefix': 'https://${collectorHost}/i?p=web' +
-      'url=${canonicalUrl}' +
-      'page=${title}' +
-      'res=${screenWidth}x${screenHeight}' +
-      'stm=${timestamp}' +
-      'tz=${timezoneCode}' +
-      'aid=${appId}' +
-      'tv=${aaVersion}' +
-      'cd=${screenColorDepth}' +
-      'cs=${documentCharset}' +
-      'duid=${duid}' +
-      'lang=${browserLanguage}' +
-      'refr=${documentReferrer}' +
-      'vp=${viewportWidth}x${viewportHeight}' +
-      'ua=${userAgent}' +
-      'ds=${scrollWidth}x${scrollHeight}' +
-      'uid=${userId}' +
-      'vid=${vid}' +
-      'sid=${sid}' +
-      'co=${customContexts}',
+      '&url=${canonicalUrl}' +
+      '&page=${title}' +
+      '&res=${screenWidth}x${screenHeight}' +
+      '&stm=${timestamp}' +
+      '&tz=${timezoneCode}' +
+      '&aid=${appId}' +
+      '&tv=${aaVersion}' +
+      '&cd=${screenColorDepth}' +
+      '&cs=${documentCharset}' +
+      '&duid=${duid}' +
+      '&lang=${browserLanguage}' +
+      '&refr=${documentReferrer}' +
+      '&vp=${viewportWidth}x${viewportHeight}' +
+      '&ua=${userAgent}' +
+      '&ds=${scrollWidth}x${scrollHeight}' +
+      '&uid=${userId}' +
+      '&vid=${vid}' +
+      '&sid=${sid}' +
+      '&co=${customContexts}',
     'pageView':
       '${basePrefix}&e=pv',
     'structEvent':
