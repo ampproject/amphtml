a255589e98109fc8e0f05431419aa4953a03ed6c
Wassim Gharbi <wassgha@gmail.com>
2019-08-27 17:59:31 -0700

    Fixes double-opening of windows with noopener

--- src/dom.js
+++ src/dom.js
@@ -22,7 +22,7 @@ import {
 } from './css';
 import {dev, devAssert} from './log';
 import {dict} from './utils/object';
-import {startsWith} from './string';
+import {includes, startsWith} from './string';
 import {toWin} from './types';
 
 const HTML_ESCAPE_CHARS = {
@@ -636,7 +636,7 @@ export function iterateCursor(iterable, cb) {
  * @param {string=} opt_features
  * @return {?Window}
  */
-export function openWindowDialog(win, url, target, opt_features) {
+export function openWindowDialog(win, url, target, opt_features = '') {
   // Try first with the specified target. If we're inside the WKWebView or
   // a similar environments, this method is expected to fail by default for
   // all targets except `_top`.
@@ -648,7 +648,7 @@ export function openWindowDialog(win, url, target, opt_features) {
   }
 
   // Then try with `_top` target.
-  if (!res && target != '_top') {
+  if (!res && target != '_top' && !includes(opt_features, 'noopener')) {
     res = win.open(url, '_top');
   }
   return res;
