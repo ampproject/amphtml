9e3a942ce790ccf4f48833b66f6fea1d0c0669ca
William Chou <willchou@google.com>
2019-09-03 15:28:22 -0400

    Revert "amp-bind: Rate-limit history operations (#23938)" (#24315)
    This reverts commit fb19bc7f03eaac3574183fede4f23ebc03b3b7ae.

--- extensions/amp-bind/0.1/bind-impl.js
+++ extensions/amp-bind/0.1/bind-impl.js
@@ -87,23 +87,6 @@ let BoundPropertyDef;
 let BoundElementDef;
 
 /**
- * @enum {number}
- */
-const HistoryOp = {
-  REPLACE: 0,
-  PUSH: 1,
-};
-
-/**
- * @typedef {{
- *   op: !HistoryOp,
- *   data: (!JsonObject|undefined),
- *   onPop: (!Function|undefined)
- * }}
- */
-let HistoryTaskDef;
-
-/**
  * A map of tag names to arrays of attributes that do not have non-bind
  * counterparts. For instance, amp-carousel allows a `[slide]` attribute,
  * but does not support a `slide` attribute.
@@ -157,7 +140,7 @@ export class Bind {
      */
     this.actionSequenceIds_ = [];
 
-    /** @const @private {function()} */
+    /** @const @private {!Function} */
     this.eventuallyClearActionSequenceIds_ = debounce(
       this.win_,
       () => {
@@ -236,24 +219,11 @@ export class Bind {
     /** @const @private {!Deferred} */
     this.addMacrosDeferred_ = new Deferred();
 
-    /** @private {?Promise} */
+    /** @private {Promise} */
     this.setStatePromise_ = null;
 
     /** @private @const {!../../../src/utils/signals.Signals} */
     this.signals_ = new Signals();
-
-    /**
-     * A queue of history replace/push to adhere to browser rate-limits.
-     * @private @const {!Array<!HistoryTaskDef>}
-     */
-    this.historyQueue_ = [];
-
-    /** @private @const {!Function} */
-    this.debouncedFlushHistoryQueue_ = debounce(
-      this.win_,
-      () => this.flushHistoryQueue_(),
-      1000
-    );
 
     // Install debug tools.
     const g = self.AMP;
@@ -376,12 +346,12 @@ export class Bind {
     dev().info(TAG, 'setState:', expression);
     this.setStatePromise_ = this.evaluateExpression_(expression, scope)
       .then(result => this.setState(result))
-      .then(() => {
-        return this.getDataForHistory_().then(data => {
-          if (data) {
-            this.pushToHistoryQueue_({op: HistoryOp.REPLACE, data});
-          }
-        });
+      .then(() => this.getDataForHistory_())
+      .then(data => {
+        // Don't bother calling History.replace with empty data.
+        if (data) {
+          this.history_.replace(data);
+        }
       });
     return this.setStatePromise_;
   }
@@ -408,76 +378,32 @@ export class Bind {
       });
 
       const onPop = () => this.setState(oldState);
-      return this.setState(result).then(() => {
-        return this.getDataForHistory_().then(data => {
-          this.pushToHistoryQueue_({op: HistoryOp.PUSH, data, onPop});
+      return this.setState(result)
+        .then(() => this.getDataForHistory_())
+        .then(data => {
+          this.history_.push(onPop, data);
         });
-      });
     });
-  }
-
-  /**
-   * @param {!HistoryTaskDef} task
-   * @private
-   */
-  pushToHistoryQueue_(task) {
-    let enqueue = true;
-
-    // Collapse if consecutive "replace" operations.
-    if (task.op === HistoryOp.REPLACE && this.historyQueue_.length > 0) {
-      const lastTask = this.historyQueue_[this.historyQueue_.length - 1];
-      if (lastTask.op === HistoryOp.REPLACE) {
-        lastTask.data = task.data;
-        enqueue = false;
-      }
-    }
-
-    if (enqueue) {
-      this.historyQueue_.push(task);
-    }
-
-    if (task.op === HistoryOp.PUSH) {
-      // Immediately flush queue on "push" to avoid adding latency to
-      // the browser "back" functionality.
-      this.flushHistoryQueue_();
-    } else {
-      // Debounce flush queue on "replace" to rate limit History.replace.
-      this.debouncedFlushHistoryQueue_();
-    }
-  }
-
-  /**
-   * @private
-   */
-  flushHistoryQueue_() {
-    this.historyQueue_.forEach(({op, data, onPop}) => {
-      if (op === HistoryOp.REPLACE) {
-        this.history_.replace(data);
-      } else if (op === HistoryOp.PUSH) {
-        this.history_.push(onPop, data);
-      }
-    });
-    this.historyQueue_.length = 0;
   }
 
   /**
    * Returns data that should be saved in browser history on AMP.setState() or
    * AMP.pushState(). This enables features like restoring browser tabs.
-   * @return {!Promise<(!JsonObject|undefined)>}
+   * @return {!Promise<?JsonObject>}
    */
   getDataForHistory_() {
-    // Copy so subsequent changes don't modify the stored object reference.
     const data = dict({
-      'data': dict({'amp-bind': this.copyJsonObject_(this.state_)}),
+      'data': dict({'amp-bind': this.state_}),
       'title': this.localWin_.document.title,
     });
     if (!this.viewer_.isEmbedded()) {
-      return Promise.resolve(data);
+      // CC doesn't recognize !JsonObject as a subtype of (JsonObject|null).
+      return /** @type {!Promise<?JsonObject>} */ (Promise.resolve(data));
     }
     // Only pass state for history updates to trusted viewers, since they
     // may contain user data e.g. form input.
     return this.viewer_.isTrustedViewer().then(trusted => {
-      return trusted ? data : undefined;
+      return trusted ? data : null;
     });
   }
 
