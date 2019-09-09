403c0d9793a3e7a8e29344c1a876a42365708f7b
Ofir Shlifer <45335051+OfirShlifer@users.noreply.github.com>
2019-09-09 22:38:59 +0300

    MPlayer AMP Extension (#22903)
    * initial commit first version of MPlayer extension, including changes in package.json and yarn.lock
    
    * added new files according to the amp demand
    
    * changes according to lint test
    
    * basic AMP version
    
    * base version of MPlayer AMP
    
    * lint fixes
    
    * renamed to minute-media-player and some fixes
    
    * added the relevant content to the md file and some changes
    
    * fixing tagname in validator html file
    
    * changes according to demand
    
    * updating test file
    
    * dock changes and some little changes
    
    * changing the message handling
    
    * lint changes
    
    * small changes
    
    * minor change
    
    * adding validator ruleset for the new AMP extended component
    
    * changes accoring to demand
    
    * lint changes
    
    * adding data-scoped-keywords attribute to the extension
    
    * converting .then to await
    
    * remove async
    
    * changing to isLayoutSizeDefined
    
    * lint change
    
    * removing nodisplay layout support from md and protoascii files

--- bundles.config.js
+++ bundles.config.js
@@ -1131,6 +1131,12 @@ exports.extensionBundles = [
     latestVersion: '0.1',
     type: TYPES.MISC,
   },
+  {
+    name: 'amp-minute-media-player',
+    version: '0.1',
+    latestVersion: '0.1',
+    type: TYPES.MEDIA,
+  },
 ];
 
 /**
