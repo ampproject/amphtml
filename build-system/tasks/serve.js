db3bfd9c9fcc96a873a2b40497b5aefa6ce7b464
Raghu Simha <rsimha@amp.dev>
2019-09-09 19:03:41 -0400

    Don't pre-build during gulp serve unless it's part of the default gulp task

--- build-system/tasks/serve.js
+++ build-system/tasks/serve.js
@@ -68,7 +68,7 @@ function getMiddleware() {
   if (argv.cache) {
     middleware.push(header({'cache-control': 'max-age=600'}));
   }
-  if (!argv.eager_build) {
+  if (!argv._.includes('serve') && !argv.eager_build) {
     middleware.push(lazyBuildExtensions);
     middleware.push(lazyBuildJs);
   }
@@ -134,7 +134,7 @@ function restartServer() {
  * Initiates pre-build steps requested via command line args.
  */
 function initiatePreBuildSteps() {
-  if (!argv.eager_build) {
+  if (!argv._.includes('serve') && !argv.eager_build) {
     preBuildCoreRuntime();
     if (argv.extensions || argv.extensions_from) {
       preBuildSomeExtensions();
