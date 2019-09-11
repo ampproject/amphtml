7444cb36afc3a91a059dd56fa33c4df60eb9f765
lannka <hungfei.ding@gmail.com>
2019-09-11 13:17:14 -0700

    lint

--- build-system/tasks/helpers.js
+++ build-system/tasks/helpers.js
@@ -82,12 +82,7 @@ const UNMINIFIED_TARGETS = [
  * Note: keep this list in sync with release script. Contact @ampproject/wg-infra
  * for details.
  */
-const MINIFIED_TARGETS = [
-  'alp.js',
-  'amp4ads-v0.js',
-  'shadow-v0.js',
-  'v0.js',
-];
+const MINIFIED_TARGETS = ['alp.js', 'amp4ads-v0.js', 'shadow-v0.js', 'v0.js'];
 
 /**
  * Settings for the global Babelify transform while compiling unminified code
