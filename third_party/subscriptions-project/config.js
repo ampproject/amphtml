1875364c1957dc6010b7882218415804f8dfbb88
Chris Antaki <cfa@google.com>
2019-09-18 11:08:07 -0700

    ✨ SwG release 0.1.22.69

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.68 */
+/** Version: 0.1.22.69 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
