a193b0e029e5b0d5db2b5833df103c78b56be12b
calebcordry <ccordry@google.com>
2019-10-01 15:28:03 -0700

    cta tests

--- extensions/amp-story-auto-ads/0.1/test/test-story-ad-page.js
+++ extensions/amp-story-auto-ads/0.1/test/test-story-ad-page.js
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
+import * as dom from '../../../../src/dom';
 import {CommonSignals} from '../../../../src/common-signals';
+import {StoryAdLocalization} from '../story-ad-localization';
 import {StoryAdPage} from '../story-ad-page';
 
 const NOOP = () => {};
@@ -27,21 +29,19 @@ const baseConfig = {
   'type': 'doubleclick',
 };
 
-const localizationMock = {
-  getLocalizedString: NOOP,
-};
-
 const pageImplMock = {
   delegateVideoAutoplay: NOOP,
 };
 
 describes.realWin('amp-story-auto-ads', {amp: true}, env => {
+  let win;
   let doc;
   let storyAutoAdsEl;
   let storyAdPage;
 
   beforeEach(() => {
-    doc = env.win.document;
+    win = env.win;
+    doc = win.document;
     storyAutoAdsEl = doc.createElement('amp-story-auto-ads');
     doc.body.appendChild(storyAutoAdsEl);
     storyAutoAdsEl.getAmpDoc = () => env.ampdoc;
@@ -49,7 +49,7 @@ describes.realWin('amp-story-auto-ads', {amp: true}, env => {
       storyAutoAdsEl,
       baseConfig,
       1, // index
-      localizationMock
+      new StoryAdLocalization(win)
     );
   });
 
@@ -169,14 +169,213 @@ describes.realWin('amp-story-auto-ads', {amp: true}, env => {
       storyAdPage.toggleVisibility();
       expect(iframeBody).not.to.have.attribute('amp-story-visible');
     });
+
+    it('should propagate the visible attribute to alternate body', async () => {
+      const pageElement = storyAdPage.build();
+      // Stub delegateVideoAutoplay.
+      pageElement.getImpl = () => Promise.resolve(pageImplMock);
+      doc.body.appendChild(pageElement);
+
+      const ampAdElement = doc.querySelector('amp-ad');
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <head>
+        <meta name="amp4ads-vars-cta-type" content="SHOP">
+        <meta name="amp4ads-vars-cta-url" content="https://www.example.com">
+        </head>
+        <body>
+        <div id=google_uploaded_a4a>
+            <div id=x-a4a-former-body on="tap:exit-api.exit(target=redirectUrl)" role=link tabindex=0>
+              <div class=a4a-wrap></div>
+            </div>
+        </div>
+        </body>`);
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+
+      const altBody = iframe.contentDocument.querySelector(
+        '#x-a4a-former-body'
+      );
+      expect(altBody).not.to.have.attribute('amp-story-visible');
+      storyAdPage.toggleVisibility();
+      expect(altBody).to.have.attribute('amp-story-visible');
+      storyAdPage.toggleVisibility();
+      expect(altBody).not.to.have.attribute('amp-story-visible');
+    });
   });
 
   describe('#maybeCreateCta', () => {
-    it('works', () => {
-      // something
+    let pageElement;
+    let ampAdElement;
+
+    beforeEach(() => {
+      pageElement = storyAdPage.build();
+      // Stub delegateVideoAutoplay.
+      pageElement.getImpl = () => Promise.resolve(pageImplMock);
+      doc.body.appendChild(pageElement);
+
+      ampAdElement = doc.querySelector('amp-ad');
+    });
+
+    it('reads cta values as data-attributes added by a4a impl', async () => {
+      ampAdElement.setAttribute('data-vars-ctaurl', 'https://amp.dev');
+      ampAdElement.setAttribute('data-vars-ctatype', 'INSTALL');
+
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+
+      const ctaLayer = doc.querySelector('amp-story-cta-layer');
+      expect(ctaLayer).to.exist;
+      const anchor = ctaLayer.firstChild;
+      expect(anchor.tagName).to.equal('A');
+      expect(anchor.target).to.equal('_blank');
+      expect(anchor.href).to.equal('https://amp.dev/');
+      expect(anchor).to.have.attribute(
+        'style',
+        'font-size: 0px; transform: scale(0);'
+      );
+      expect(anchor).to.have.class('i-amphtml-story-ad-link');
+      expect(anchor.textContent).to.equal('Install Now');
+    });
+
+    it('reads CTA values from amp4ads-vars meta tags', async () => {
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <head>
+        <meta name="amp4ads-vars-cta-type" content="SHOP">
+        <meta name="amp4ads-vars-cta-url" content="https://www.example.com">
+        </head>
+        <body></body>`);
+
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+      const anchor = doc.querySelector('a');
+      expect(anchor.href).to.equal('https://www.example.com/');
+      expect(anchor.textContent).to.equal('Shop');
+    });
+
+    it('prefers CTA url from amp-ad-exit if it exists', async () => {
+      ampAdElement.setAttribute('data-vars-ctaurl', 'https://dontuse.com');
+      ampAdElement.setAttribute('data-vars-ctatype', 'LEARN_MORE');
+
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <body>
+          <amp-ad-exit id="exit-api">
+            <script type="application/json">
+            {
+              "targets": {
+                "url_0": { "finalUrl": "https://amp.dev/" }
+              }
+            }
+            </script>
+          </amp-ad-exit>
+        </body>`);
+
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+      const anchor = doc.querySelector('a');
+      expect(anchor.href).to.equal('https://amp.dev/');
+      expect(anchor.textContent).to.equal('Learn More');
+    });
+
+    it('throws on missing cta url', () => {
+      expectAsyncConsoleError(
+        '[amp-story-auto-ads:page] Both CTA Type & CTA Url are required in ad-server response.'
+      );
+      ampAdElement.setAttribute('data-vars-ctatype', 'INSTALL');
+
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.false;
+    });
+
+    it('throws on missing cta type', () => {
+      expectAsyncConsoleError(
+        '[amp-story-auto-ads:page] Both CTA Type & CTA Url are required in ad-server response.'
+      );
+      ampAdElement.setAttribute('data-vars-ctaurl', 'INSTALL');
+
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.false;
+    });
+
+    it('creates attribution badge with outlink', async () => {
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <head>
+          <meta name="amp4ads-vars-cta-type" content="SHOP">
+          <meta name="amp4ads-vars-cta-url" content="https://www.example.com">
+          <meta name="amp4ads-vars-attribution-icon" content="https://googleads.g.doubleclick.net/pagead/images/mtad/ad_choices_blue.png">
+          <meta name="amp4ads-vars-attribution-url" content="https://www.google.com">
+        </head>
+        <body></body>`);
+
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+      const attribution = doc.querySelector('.i-amphtml-story-ad-attribution');
+      expect(attribution).to.exist;
+      expect(attribution.tagName).to.equal('IMG');
+      expect(attribution).to.have.attribute(
+        'src',
+        'https://googleads.g.doubleclick.net/pagead/images/mtad/ad_choices_blue.png'
+      );
+
+      const openWindowDialogStub = sandbox.stub(dom, 'openWindowDialog');
+      attribution.click();
+      expect(openWindowDialogStub).to.be.calledOnce;
+      expect(openWindowDialogStub).to.be.calledWithExactly(
+        win,
+        'https://www.google.com',
+        '_blank'
+      );
+    });
+
+    it('does not create attribution when missing icon', async () => {
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <head>
+          <meta name="amp4ads-vars-cta-type" content="SHOP">
+          <meta name="amp4ads-vars-cta-url" content="https://www.example.com">
+          <meta name="amp4ads-vars-attribution-url" content="https://www.google.com">
+        </head>
+        <body></body>`);
+
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+      const attribution = doc.querySelector('.i-amphtml-story-ad-attribution');
+      expect(attribution).not.to.exist;
+    });
+
+    it('does not create attribution when missing url', async () => {
+      const iframe = doc.createElement('iframe');
+      ampAdElement.appendChild(iframe);
+      iframe.contentDocument.write(`
+        <head>
+          <meta name="amp4ads-vars-cta-type" content="SHOP">
+          <meta name="amp4ads-vars-cta-url" content="https://www.example.com">
+          <meta name="amp4ads-vars-attribution-icon" content="https://googleads.g.doubleclick.net/pagead/images/mtad/ad_choices_blue.png">
+        </head>
+        <body></body>`);
+
+      await ampAdElement.signals().signal(CommonSignals.INI_LOAD);
+      const created = storyAdPage.maybeCreateCta();
+      expect(created).to.be.true;
+      const attribution = doc.querySelector('.i-amphtml-story-ad-attribution');
+      expect(attribution).not.to.exist;
     });
   });
 });
+// IMPORTANT:::::::: alternate body test
 
 // describe('CTA button', () => {
 //   beforeEach(async () => {
@@ -318,3 +517,8 @@ describes.realWin('amp-story-auto-ads', {amp: true}, env => {
 //     'viewTime': sinon.match.number,
 //   });
 // });
+
+// <meta name="amp-cta-type" content="INSTALL">
+// <meta name="amp-cta-url" content="https://www.example.com">
+// <meta name="amp4ads-vars-attribution-icon" content="https://tpc.googlesyndication.com/pagead/images/adchoices/icon.png">
+// <meta name="amp4ads-vars-attribution-url" content="https://www.amp.dev">
