1a5e6d67aad560371606b76b19e1bf9aa4dfc61e
wille <w.sandstrom@gmail.com>
2019-10-18 14:49:48 +0200

    fix tests

--- test/unit/test-purifier.js
+++ test/unit/test-purifier.js
@@ -224,7 +224,7 @@ describe
               '<a target="other">other</a>'
           )
         ).to.equal(
-          '<a target="_top">_self</a>' +
+          '<a target="_self">_self</a>' +
             '<a target="_top">_parent</a>' +
             '<a target="_top">_other</a>' +
             '<a target="_top">_OTHER</a>' +
