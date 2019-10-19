61e8a02804981af56a6891fa75fe31a9454ec008
Justin Ridgewell <jridgewell@google.com>
2019-10-18 22:20:33 -0400

    remove mit license shortener

--- build-system/compile/shorten-license.js
+++ build-system/compile/shorten-license.js
@@ -20,32 +20,6 @@ const pumpify = require('pumpify');
 const replace = require('gulp-regexp-sourcemaps');
 
 /* eslint-disable */
-const MIT_FULL = [
-'Permission is hereby granted, free of charge, to any person obtaining a copy',
-'of this software and associated documentation files (the "Software"), to deal',
-'in the Software without restriction, including without limitation the rights',
-'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell',
-'copies of the Software, and to permit persons to whom the Software is',
-'furnished to do so, subject to the following conditions:',
-'',
-'The above copyright notice and this permission notice shall be included in',
-'all copies or substantial portions of the Software.',
-'',
-'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR',
-'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,',
-'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE',
-'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER',
-'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,',
-'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN',
-'THE SOFTWARE.'
-].join('\n');
-
-const MIT_SHORT = [
-'Use of this source code is governed by a MIT-style',
-'license that can be found in the LICENSE file or at',
-'https://opensource.org/licenses/MIT.'
-].join('\n');
-
 const POLYMER_BSD_FULL = [
 'This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt',
 'The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt',
@@ -62,7 +36,7 @@ const BSD_SHORT = [
 
 /* eslint-enable */
 
-const LICENSES = [[MIT_FULL, MIT_SHORT], [POLYMER_BSD_FULL, BSD_SHORT]];
+const LICENSES = [[POLYMER_BSD_FULL, BSD_SHORT]];
 
 const PATHS = ['third_party/webcomponentsjs/ShadowCSS.js'];
 
