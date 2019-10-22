68fc39e724654c6b488f2023eab3518ee3691fff
Daniel Rozenberg <rodaniel@amp.dev>
2019-10-22 14:12:42 -0400

    Change how child processes are spawn to align with child_process.spawn docs

--- build-system/common/exec.js
+++ build-system/common/exec.js
@@ -22,7 +22,6 @@
 const childProcess = require('child_process');
 
 const shellCmd = process.platform == 'win32' ? 'cmd' : '/bin/bash';
-const shellFlag = process.platform == 'win32' ? '/C' : '-c';
 
 /**
  * Spawns the given command in a child process with the given options.
@@ -32,7 +31,7 @@ const shellFlag = process.platform == 'win32' ? '/C' : '-c';
  * @return {!Object}
  */
 function spawnProcess(cmd, options) {
-  return childProcess.spawnSync(shellCmd, [shellFlag, cmd], options);
+  return childProcess.spawnSync(cmd, Object.assign({shell: shellCmd}, options));
 }
 
 /**
@@ -56,7 +55,7 @@ function exec(cmd, options) {
  * @return {!Object}
  */
 function execScriptAsync(script, options) {
-  return childProcess.spawn(shellCmd, [shellFlag, script], options);
+  return childProcess.spawn(script, Object.assign({shell: shellCmd}, options));
 }
 
 /**
