5f0c7c74264d1d20466d531be2b68b4331cbb2f5
Justin Ridgewell <jridgewell@google.com>
2019-10-24 18:10:56 -0400

    Remove RRule support (#25230)
    It doesn't work.
    
    Fixes https://github.com/ampproject/amphtml/issues/24297

--- build-system/externs/amp.extern.js
+++ build-system/externs/amp.extern.js
@@ -308,19 +308,6 @@ window.vg;
  * @type {function(*)}
  */
 let ReactRender = function() {};
-let RRule;
-/**
- * @param {Date} unusedDt
- * @param {boolean} unusedInc
- * @return {?Date}
- */
-RRule.prototype.before = function(unusedDt, unusedInc) {};
-/**
- * @param {Date} unusedDt
- * @param {boolean} unusedInc
- * @return {?Date}
- */
-RRule.prototype.after = function(unusedDt, unusedInc) {};
 
 /**
  * @dict
