cf235f19ca27f7fa5a40783596bd6620187c9252
Fabian Denk <fabian.denk@icloud.com>
2019-10-24 10:43:19 +0200

    Add Red Bull video player extension

---
+++ extensions/amp-redbull-player/0.1/amp-redbull-player.js
@@ -0,0 +1,298 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  SandboxOptions,
+  createFrameFor,
+  isJsonOrObj,
+  objOrParseJson,
+  originMatches,
+} from '../../../src/iframe-video';
+import {Services} from '../../../src/services';
+import {VideoEvents} from '../../../src/video-interface';
+import {dev, userAssert} from '../../../src/log';
+import {dict} from '../../../src/utils/object';
+import {disableScrollingOnIframe} from '../../../src/iframe-helper';
+import {getData, listen} from '../../../src/event-helper';
+import {installVideoManagerForDoc} from '../../../src/service/video-manager-impl';
+import {isLayoutSizeDefined} from '../../../src/layout';
+import {once} from '../../../src/utils/function';
+import {removeElement} from '../../../src/dom';
+import addAttributesToElement from './utils/addAttributesToElement';
+import getPlayerParams from './utils/getPlayerParams'
+
+/** @private @const */
+const TAG = 'amp-redbull-player';
+
+/** @private @const */
+const ANALYTICS_EVENT_TYPE_PREFIX = 'video-custom-';
+
+/** @private @const */
+const SANDBOX = [
+  SandboxOptions.ALLOW_SCRIPTS,
+  SandboxOptions.ALLOW_SAME_ORIGIN,
+  SandboxOptions.ALLOW_POPUPS,
+  SandboxOptions.ALLOW_POPUPS_TO_ESCAPE_SANDBOX,
+  SandboxOptions.ALLOW_TOP_NAVIGATION_BY_USER_ACTIVATION,
+];
+
+/**
+ * @return {!RegExp}
+ * @private
+ */
+const getAnalyticsEventTypePrefixRegex = once(
+  () => new RegExp(`^${ANALYTICS_EVENT_TYPE_PREFIX}`)
+);
+
+/** @implements {../../../src/video-interface.VideoInterface} */
+class AmpRedBullPlayer extends AMP.BaseElement {
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    const el = addAttributesToElement(element);
+    super(el);
+
+    /** @private {?Element} */
+    this.iframe_ = null;
+
+    /** @private {!UnlistenDef|null} */
+    this.unlistenFrame_ = null;
+
+    /** @private {string} */
+    this.tagId_ = null;
+
+    /** @private {string} */
+    this.locale_ = null;
+
+    /**
+      * @param {!Event} e
+      * @return {*} TODO(#23582): Specify return type
+      * @private
+     */
+    this.boundOnMessage_ = e => this.onMessage_(e);
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    return isLayoutSizeDefined(layout);
+  }
+
+  /** @override */
+  buildCallback() {
+    const {element} = this;
+    /*
+     * Required Paramaters: data-video-id, data-skin
+     * Break creation if required parameters are missing!
+     */
+    userAssert(
+      element.getAttribute('data-videoid'),
+      'The data-videoid attribute is required for %s',
+      element
+    );
+
+    userAssert(
+      element.getAttribute('data-skin'),
+      'The data-skin attribute is required for %s',
+      element
+    );
+    this.tagId_ = element.getAttribute('id') || '';
+    this.locale_ = element.getAttribute('data-locale') || 'global';
+
+    installVideoManagerForDoc(element);
+  }
+
+  /** @override */
+  layoutCallback() {
+    const {element} = this;
+    const origin = 'https://player.redbull.com/amp/amp-iframe.html?';
+    // const origin = 'http://localhost:9000/amp-iframe.html?';
+    const params = `${getPlayerParams(element)}&ampTagId=${
+      this.tagId_
+    }&locale=${this.locale_}`;
+    const src = `${origin}${params}`;
+
+    const name = JSON.stringify(this.getMetadata_());
+    this.iframe_ = disableScrollingOnIframe(
+      createFrameFor(this, src, name, SANDBOX)
+    );
+
+    this.unlistenFrame_ = listen(this.win, 'message', this.boundOnMessage_);
+    const loaded = this.loadPromise(this.iframe_).then(() => {
+      this.onReady_();
+    });
+    return loaded;
+  }
+
+  /** @private */
+  onReady_() {
+    const {element} = this;
+    Services.videoManagerForDoc(element).register(this);
+
+    this.sendCommand_({msg: 'amp-loaded'});
+    element.dispatchCustomEvent(VideoEvents.LOAD);
+  }
+
+  /**
+   * @return {!JsonObject}
+   * @private
+   */
+  getMetadata_() {
+    const {sourceUrl, canonicalUrl} = Services.documentInfoForDoc(this.element);
+
+    return dict({
+      'sourceUrl': sourceUrl,
+      'canonicalUrl': canonicalUrl,
+    });
+  }
+
+  /**
+   * Sends a command to the player through postMessage.
+   * @param {string} command
+   * @private
+   */
+  sendCommand_(command) {
+    if (this.iframe_ && this.iframe_.contentWindow) {
+      if (!command.id) {
+        command.id = `${TAG}-${this.tagId_}`;
+      }
+      this.postMessage_(command);
+    }
+  }
+
+  /** @override */
+  unlayoutCallback() {
+    this.removeIframe_();
+    return true; // layout again.
+  }
+
+  /** @private */
+  removeIframe_() {
+    if (this.iframe_) {
+      removeElement(this.iframe_);
+      this.iframe_ = null;
+    }
+    if (this.unlistenFrame_) {
+      this.unlistenFrame_();
+      this.unlistenFrame_ = null;
+    }
+  }
+
+  /**
+   * @param {!Event} event
+   * @private
+   */
+  originMatches_(event) {
+    return originMatches(event, this.iframe_, /.*/);
+  }
+
+  /**
+   * @param {!Event} event
+   * @private
+   */
+  onMessage_(event) {
+    if (!this.iframe_) {
+      return;
+    }
+
+    if (!this.originMatches_(event)) {
+      return;
+    }
+
+    const eventData = getData(event);
+    if (!isJsonOrObj(eventData)) {
+      return;
+    }
+
+    const data = objOrParseJson(eventData);
+
+    if (data.id === `redbull-amp-video-tracking-${this.tagId_}`) {
+      const type = ANALYTICS_EVENT_TYPE_PREFIX + data.type;
+      this.dispatchCustomAnalyticsEvent_(type, data);
+    }
+  }
+
+  /**
+   * @param {string} eventType
+   * @param {!Object<string, string>=} vars
+   */
+  dispatchCustomAnalyticsEvent_(eventType, vars = {}) {
+    userAssert(
+      getAnalyticsEventTypePrefixRegex().test(eventType),
+      'Invalid analytics `eventType`. Value must start with `%s`.',
+      ANALYTICS_EVENT_TYPE_PREFIX
+    );
+    this.element.dispatchCustomEvent(
+      VideoEvents.CUSTOM_TICK,
+      dict({
+        'eventType': `video-custom-tracking-${this.tagId_}`,
+        'vars': vars,
+      })
+    );
+  }
+
+  /**
+   * @param {!JsonObject} message
+   * @private
+   */
+  postMessage_(message) {
+    if (!message.msg) {
+      message = JSON.stringify(message);
+    }
+
+    this.iframe_.contentWindow.postMessage(message, '*');
+  }
+  /** @override */
+  isInteractive() {
+    return true;
+  }
+
+  /** @override */
+  supportsPlatform() {
+    return true;
+  }
+
+  /** @override */
+  preimplementsMediaSessionAPI() {
+    return this.element.hasAttribute('implements-media-session');
+  }
+
+  /** @override */
+  preimplementsAutoFullscreen() {
+    return this.element.hasAttribute('implements-rotate-to-fullscreen');
+  }
+
+  /** @override */
+  getCurrentTime() {
+    return 0;
+  }
+
+  /** @override */
+  getDuration() {
+    return 0;
+  }
+
+  /** @override */
+  getMetadata() {
+    return null;
+  }
+
+  /** @override */
+  getPlayedRanges() {
+    return [];
+  }
+}
+
+AMP.extension(TAG, '0.1', AMP => {
+  AMP.registerElement(TAG, AmpRedBullPlayer);
+});
---
+++ extensions/amp-redbull-player/amp-redbull-player.md
@@ -0,0 +1,72 @@
+---
+$category@: media
+formats:
+  - websites
+teaser:
+  text: Displays an Red Bull video.
+---
+<!---
+Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# amp-redbull-player
+
+Displays an <a href="https://www.redbull.com/">Red Bull</a> video.
+
+<table>
+  <tr>
+    <td width="40%"><strong>Required Script</strong></td>
+    <td><code>&lt;script async custom-element="amp-redbull-player" src="https://cdn.ampproject.org/v0/amp-redbull-player-0.1.js">&lt;/script></code></td>
+  </tr>
+  <tr>
+    <td class="col-fourty"><strong><a href="https://amp.dev/documentation/guides-and-tutorials/develop/style_and_layout/control_layout">Supported Layouts</a></strong></td>
+    <td>fixed, flex-item, responsive</td>
+  </tr>
+</table>
+
+[TOC]
+
+## Example
+
+```html
+<amp-redbull-player
+      id="rbvideo"
+      data-videoid="rrn:content:videos:3965a26c-052e-575f-a28b-ded6bee23ee1:en-INT"
+      data-skin="com"
+      height="360"
+      width="640"></amp-redbull-player>
+```
+
+## Attributes
+
+<table>
+  <tr>
+    <td width="40%"><strong>data-videoid (required)</strong></td>
+    <td>The video ID</a>.</td>
+  </tr>
+  <tr>
+    <td width="40%"><strong>data-skin (required)</strong></td>
+    <td>The ID of the skin to display.</td>
+  </tr>
+  <tr>
+    <td width="40%"><strong>id (optional)</strong></td>
+    <td>Selector for an amp-analytics triggers configuration.</td>
+  </tr>
+</table>
+
+
+## Validation
+
+See [amp-redbull-player rules](https://github.com/ampproject/amphtml/blob/master/extensions/amp-redbull-player/validator-amp-player-player.protoascii) in the AMP validator specification.
