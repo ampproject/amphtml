bddcdf9d7ad184596d79dc03b2726cceab73db68
Filip Stanis <fstanis@google.com>
2019-10-29 19:31:26 +0000

    fix tests

--- extensions/amp-viewer-integration/0.1/test/integration/test-messaging-handshake.js
+++ extensions/amp-viewer-integration/0.1/test/integration/test-messaging-handshake.js
@@ -20,110 +20,115 @@ import {getWinOrigin, serializeQueryString} from '../../../../../src/url';
 describes.sandboxed('AmpViewerMessagingIntegration', {}, () => {
   const ampDocSrc = '/test/fixtures/served/ampdoc-with-messaging.html';
 
-  describe.configure().run('Handshake', () => {
-    let viewerIframe;
-    let iframeOrigin;
+  describe
+    .configure()
+    .ifChrome()
+    .run('Handshake', () => {
+      let viewerIframe;
+      let iframeOrigin;
 
-    beforeEach(() => {
-      const loc = window.location;
-      viewerIframe = document.createElement('iframe');
-      iframeOrigin = `${loc.protocol}//iframe.${loc.hostname}:${loc.port}`;
-      document.body.appendChild(viewerIframe);
-    });
-
-    afterEach(() => {
-      document.body.removeChild(viewerIframe);
-    });
-
-    it('should wait for the handshake', () => {
-      const params = serializeQueryString({
-        origin: getWinOrigin(window),
+      beforeEach(() => {
+        const loc = window.location;
+        viewerIframe = document.createElement('iframe');
+        iframeOrigin = `http://iframe.localhost:${loc.port}`;
+        document.body.appendChild(viewerIframe);
       });
-      const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
-      viewerIframe.setAttribute('src', ampDocUrl);
 
-      return Messaging.waitForHandshakeFromDocument(
-        window,
-        viewerIframe.contentWindow,
-        iframeOrigin
-      )
-        .then(messaging => {
-          messaging.setDefaultHandler(() => {});
-          return new Promise(resolve =>
-            messaging.registerHandler('documentLoaded', resolve)
-          );
-        })
-        .then(name => {
-          expect(name).to.equal('documentLoaded');
+      afterEach(() => {
+        document.body.removeChild(viewerIframe);
+      });
+
+      it('should wait for the handshake', () => {
+        const params = serializeQueryString({
+          origin: getWinOrigin(window),
         });
-    });
+        const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
+        viewerIframe.setAttribute('src', ampDocUrl);
 
-    it('should work on opaque origin with messaging token', () => {
-      const params = serializeQueryString({
-        origin: getWinOrigin(window),
-        messagingToken: 'test-token',
+        return Messaging.waitForHandshakeFromDocument(
+          window,
+          viewerIframe.contentWindow,
+          iframeOrigin
+        )
+          .then(messaging => {
+            messaging.setDefaultHandler(() => Promise.resolve());
+            return new Promise(resolve =>
+              messaging.registerHandler('documentLoaded', resolve)
+            );
+          })
+          .then(name => {
+            expect(name).to.equal('documentLoaded');
+          });
       });
-      const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
-      viewerIframe.setAttribute('sandbox', 'allow-scripts');
-      viewerIframe.setAttribute('src', ampDocUrl);
 
-      return Messaging.waitForHandshakeFromDocument(
-        window,
-        viewerIframe.contentWindow,
-        'null',
-        'test-token'
-      )
-        .then(messaging => {
-          messaging.setDefaultHandler(() => {});
-          return new Promise(resolve =>
-            messaging.registerHandler('documentLoaded', resolve)
-          );
-        })
-        .then(name => {
-          expect(name).to.equal('documentLoaded');
+      it('should work on opaque origin with messaging token', () => {
+        const params = serializeQueryString({
+          origin: getWinOrigin(window),
+          messagingToken: 'test-token',
         });
-    });
+        const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
+        viewerIframe.setAttribute('sandbox', 'allow-scripts');
+        viewerIframe.setAttribute('src', ampDocUrl);
 
-    it('should fail if messaging token is wrong', () => {
-      const params = serializeQueryString({
-        origin: getWinOrigin(window),
-        messagingToken: 'foo',
+        return Messaging.waitForHandshakeFromDocument(
+          window,
+          viewerIframe.contentWindow,
+          'null',
+          'test-token'
+        )
+          .then(messaging => {
+            messaging.setDefaultHandler(() => Promise.resolve());
+            return new Promise(resolve =>
+              messaging.registerHandler('documentLoaded', resolve)
+            );
+          })
+          .then(name => {
+            expect(name).to.equal('documentLoaded');
+          });
       });
-      const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
-      viewerIframe.setAttribute('src', ampDocUrl);
 
-      return Messaging.waitForHandshakeFromDocument(
-        window,
-        viewerIframe.contentWindow,
-        iframeOrigin,
-        'bar'
-      ).then(messaging => {
-        const handlerStub = sandbox.stub();
-        messaging.setDefaultHandler(handlerStub);
-        expect(handlerStub).to.not.have.been.called;
-      });
-    });
-
-    it('should perform polling handshake', function() {
-      this.timeout(5000);
-
-      const params = serializeQueryString({
-        origin: getWinOrigin(window),
-        cap: 'handshakepoll',
-      });
-      const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
-      viewerIframe.setAttribute('src', ampDocUrl);
-
-      return Messaging.initiateHandshakeWithDocument(viewerIframe.contentWindow)
-        .then(messaging => {
-          messaging.setDefaultHandler(() => {});
-          return new Promise(resolve =>
-            messaging.registerHandler('documentLoaded', resolve)
-          );
-        })
-        .then(name => {
-          expect(name).to.equal('documentLoaded');
+      it('should fail if messaging token is wrong', () => {
+        const params = serializeQueryString({
+          origin: getWinOrigin(window),
+          messagingToken: 'foo',
         });
+        const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
+        viewerIframe.setAttribute('src', ampDocUrl);
+
+        return Messaging.waitForHandshakeFromDocument(
+          window,
+          viewerIframe.contentWindow,
+          iframeOrigin,
+          'bar'
+        ).then(messaging => {
+          const handlerStub = sandbox.stub();
+          messaging.setDefaultHandler(handlerStub);
+          expect(handlerStub).to.not.have.been.called;
+        });
+      });
+
+      it('should perform polling handshake', function() {
+        this.timeout(5000);
+
+        const params = serializeQueryString({
+          origin: getWinOrigin(window),
+          cap: 'handshakepoll',
+        });
+        const ampDocUrl = `${iframeOrigin}${ampDocSrc}#${params}`;
+        viewerIframe.setAttribute('src', ampDocUrl);
+
+        return Messaging.initiateHandshakeWithDocument(
+          viewerIframe.contentWindow
+        )
+          .then(messaging => {
+            messaging.setDefaultHandler(() => Promise.resolve());
+            return new Promise(resolve =>
+              messaging.registerHandler('documentLoaded', resolve)
+            );
+          })
+          .then(name => {
+            expect(name).to.equal('documentLoaded');
+          });
+      });
     });
-  });
 });
