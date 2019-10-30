9adc04466e1324ac1b1be782bec36bb0b3880014
Filip Stanis <fstanis@google.com>
2019-10-30 10:32:26 +0000

    Add handshake logic and opaque origin support to amp-viewer-messaging (#24797)
    Also updates package.json and renames NPM package.

--- extensions/amp-viewer-integration/0.1/test/integration/test-amp-viewer-integration.js
+++ extensions/amp-viewer-integration/0.1/test/integration/test-amp-viewer-integration.js
@@ -456,7 +456,7 @@ describes.sandboxed('AmpViewerIntegration', {}, () => {
 
           expect(logErrorSpy).to.have.been.calledOnce;
           const state =
-            'amp-viewer-messaging: sendResponseError_, Message name: name';
+            'amp-viewer-messaging: sendResponseError_, message name: name';
           expect(logErrorSpy).to.have.been.calledWith(state, errString);
         });
       });
