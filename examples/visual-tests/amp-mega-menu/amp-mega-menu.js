1bcb9560038e46b0a78775896703d11d69ea0593
Steven Ye <stevenye@google.com>
2019-11-01 13:30:41 -0700

    toggle experiment on

--- examples/visual-tests/amp-mega-menu/amp-mega-menu.js
+++ examples/visual-tests/amp-mega-menu/amp-mega-menu.js
@@ -17,7 +17,7 @@
 
 module.exports = {
   'click menu item one': async (page, name) => {
-    await page.tap('amp-mega-menu .i-amphtml-mega-menu-item:first-child');
+    await page.tap('amp-mega-menu .i-amphtml-mega-menu-item:first-child .i-amphtml-mega-menu-heading');
     await verifySelectorsVisible(page, name,
         ['amp-mega-menu[open] .i-amphtml-mega-menu-item:first-child[open] .i-amphtml-mega-menu-content']);
     await verifySelectorsVisible(page, name,
