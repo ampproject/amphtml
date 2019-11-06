83fbaca6170058adb8fcc229dc76360b767f93b5
Jake Fried <samouri@users.noreply.github.com>
2019-11-06 17:09:58 -0500

    fix types again

--- src/cookies.js
+++ src/cookies.js
@@ -182,8 +182,8 @@ export function getHighestAvailableDomain(win) {
  * @param {string} value
  * @param {time} expirationTime
  * @param {string|undefined} domain
- * @param {!SameSite|undefined} sameSite
- * @param {boolean} secure
+ * @param {!SameSite=} sameSite
+ * @param {boolean|undefined=} secure
  */
 function trySetCookie(win, name, value, expirationTime, domain, sameSite, secure) {
   // We do not allow setting cookies on the domain that contains both
