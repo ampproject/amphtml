7add6676b1dd6fbea97312cc0dd96e5c3d796d16
Steven Ye <8905737+leafsy@users.noreply.github.com>
2019-11-07 03:31:48 +0800

    Add sidebar 0.1 support for nested menu and guard behind experiment (#25355)
    * add lazy loading nested menu for sidebar 0.1
    
    * use dom update
    
    * small fixes

--- tools/experiments/experiments-config.js
+++ tools/experiments/experiments-config.js
@@ -68,6 +68,11 @@ export const EXPERIMENTS = [
     cleanupIssue: 'https://github.com/ampproject/amphtml/issues/24898',
   },
   {
+    id: 'amp-nested-menu',
+    name: 'AMP extension for a nested drilldown menu',
+    cleanupIssue: 'https://github.com/ampproject/amphtml/issues/25343',
+  },
+  {
     id: 'amp-sidebar-v2',
     name: 'Updated sidebar component with nested menu and animations',
     spec: 'https://github.com/ampproject/amphtml/issues/25049',
