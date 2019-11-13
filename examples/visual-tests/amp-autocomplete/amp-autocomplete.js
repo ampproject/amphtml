ad77f02b04f128bb0972672f27e413a7cba01c9a
Caroline Liu <carolineliu@google.com>
2019-11-13 09:56:16 -0500

    Add visual-diff test

--- examples/visual-tests/amp-autocomplete/amp-autocomplete.js
+++ examples/visual-tests/amp-autocomplete/amp-autocomplete.js
@@ -15,22 +15,42 @@
  */
 'use strict';
 
-const {verifySelectorsVisible} = require('../../../build-system/tasks/visual-diff/helpers');
+const {
+  verifySelectorsVisible,
+  verifySelectorsInvisible,
+} = require('../../../build-system/tasks/visual-diff/helpers');
 
 module.exports = {
-
   'tap input1 to focus and display results': async (page, name) => {
     await page.tap('input#input1');
-    await verifySelectorsVisible(page, name, ['amp-autocomplete#autocomplete1 > .i-amphtml-autocomplete-results']);
+    await verifySelectorsVisible(page, name, [
+      'amp-autocomplete#autocomplete1 > .i-amphtml-autocomplete-results',
+    ]);
   },
 
   'tap input-rtl to focus and display results': async (page, name) => {
     await page.tap('input#input-rtl');
-    await verifySelectorsVisible(page, name, ['amp-autocomplete#autocomplete-rtl > .i-amphtml-autocomplete-results']);
+    await verifySelectorsVisible(page, name, [
+      'amp-autocomplete#autocomplete-rtl > .i-amphtml-autocomplete-results',
+    ]);
   },
 
   'tap input2 to focus and display results': async (page, name) => {
     await page.tap('input#input2');
-    await verifySelectorsVisible(page, name, ['amp-autocomplete#autocomplete2 > .i-amphtml-autocomplete-results']);
+    await verifySelectorsVisible(page, name, [
+      'amp-autocomplete#autocomplete2 > .i-amphtml-autocomplete-results',
+    ]);
+  },
+
+  'type into inputInline to display results': async (page, name) => {
+    await page.tap('input#inputInline');
+    await page.keyboard.type('hello ');
+    await verifySelectorsInvisible(page, name, [
+      'amp-autocomplete#autocompleteInline > .i-amphtml-autocomplete-results',
+    ]);
+    await page.keyboard.type('@');
+    await verifySelectorsVisible(page, name, [
+      'amp-autocomplete#autocompleteInline > .i-amphtml-autocomplete-results',
+    ]);
   },
 };
