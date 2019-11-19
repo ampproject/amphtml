2abca24fa856c6f4998800c3534fec721598e308
Micajuine Ho <micajuineho@google.com>
2019-11-19 09:05:17 -0800

    added experiment flag

--- extensions/amp-consent/0.1/amp-consent.js
+++ extensions/amp-consent/0.1/amp-consent.js
@@ -115,6 +115,13 @@ export class AmpConsent extends AMP.BaseElement {
       'amp-consent should have an id'
     );
 
+    const expOn = isExperimentOn(this.win, 'amp-consent-ccpa');
+    if (expOn) {
+      console.log('CCPA flag is on');
+    } else {
+      console.log('nope not on');
+    }
+
     const config = new ConsentConfig(this.element);
 
     this.consentConfig_ = config.getConsentConfig();
