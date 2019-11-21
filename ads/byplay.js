e82f61e39645d2cecc3a99a5ebf9c0130daa5c30
Dai Sato <d-sato@tsumikiinc.com>
2019-11-21 12:12:02 +0900

    fix check-types

--- ads/byplay.js
+++ ads/byplay.js
@@ -22,7 +22,7 @@ import {loadScript, validateData} from '../3p/3p';
 export function byplay(global, data) {
   validateData(data, ['vastUrl']);
 
-  global.BYPLAY_VAST_URL = data.vastUrl;
+  global.BYPLAY_VAST_URL = data['vastUrl'];
 
   loadScript(global, 'https://cdn.byplay.net/amp-byplay-v2.js');
 }
