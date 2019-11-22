e3b93610b10c2b90de4df417412565c5b703f80c
Justin Ridgewell <jridgewell@google.com>
2019-11-22 17:55:33 -0500

    Bump Preact to v10 (#25743)

--- third_party/react-dates/build.sh
+++ third_party/react-dates/build.sh
@@ -3,7 +3,7 @@
 -t [ babelify --presets [ @babel/preset-env ] ] \
 -g [ envify purge --NODE_ENV production ] \
 -g uglifyify \
--r prop-types -r preact:react -r preact-compat:react-dom \
+-r prop-types -r preact:react -r preact/compat:react-dom \
 -r moment/min/moment-with-locales:moment \
 -r react-dates -r react-dates/initialize -r react-dates/constants \
 ./third_party/react-dates/index.js | \
