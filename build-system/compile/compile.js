8eb76ef51ee75e73e1cc2de275f69a430ff9214d
erwin mombay <erwinm@google.com>
2019-12-02 15:12:50 -0800

    add simple destructure plugin (#25521)
    * add simple destructure plugin
    
    Co-authored-by: Justin Ridgewell <jridgewell@google.com>
    
    * lint fixes
    
    * add back param destructure
    
    * add eslint rule banning object destructuring assignment in params
    
    * remove param destructuring assignments
    
    * Update build-system/babel-plugins/babel-plugin-transform-simple-object-destructure/index.js
    
    Co-Authored-By: Justin Ridgewell <justin@ridgewell.name>
    
    * pull down destructure assignment in params to body
    
    * apply recs
    
    * addtl fixes
    
    * fix shadowing bug caused by renaming
    
    * fix more lint issues

--- build-system/compile/compile.js
+++ build-system/compile/compile.js
@@ -345,7 +345,7 @@ function compile(
       delete compilerOptions.define;
     }
 
-    if (!argv.single_pass && !options.typeCheckOnly) {
+    if (!argv.single_pass) {
       compilerOptions.js_module_root.push(SRC_TEMP_DIR);
     }
 
@@ -365,9 +365,13 @@ function compile(
       }
     });
 
+    const gulpSrcs = !argv.single_pass ? convertPathsToTmpRoot(srcs) : srcs;
+    const gulpBase = !argv.single_pass ? SRC_TEMP_DIR : '.';
+
     if (options.typeCheckOnly) {
       return gulp
-        .src(srcs, {base: '.'})
+        .src(gulpSrcs, {base: gulpBase})
+        .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(gulpClosureCompile(compilerOptionsArray, checkTypesNailgunPort))
         .on('error', err => {
           handleTypeCheckError();
@@ -376,8 +380,6 @@ function compile(
         .pipe(nop())
         .on('end', resolve);
     } else {
-      const gulpSrcs = argv.single_pass ? srcs : convertPathsToTmpRoot(srcs);
-      const gulpBase = argv.single_pass ? '.' : SRC_TEMP_DIR;
       timeInfo.startTime = Date.now();
       return gulp
         .src(gulpSrcs, {base: gulpBase})
