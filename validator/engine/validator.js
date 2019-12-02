62c15274a6122440cb27bb046e8dd515e3de0a29
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2019-12-02 14:47:46 -0800

    s/height/heights

--- validator/engine/validator.js
+++ validator/engine/validator.js
@@ -4096,7 +4096,7 @@ function validateLayout(parsedTagSpec, context, encounteredTag, result) {
         getTagSpecUrl(spec), result);
     return;
   }
-  // Height attribute is only allowed for RESPONSIVE layout.
+  // heights attribute is only allowed for RESPONSIVE layout.
   if (heightsAttr !== undefined &&
       layout !== amp.validator.AmpLayout.Layout.RESPONSIVE) {
     const code = layoutAttr === undefined ?
