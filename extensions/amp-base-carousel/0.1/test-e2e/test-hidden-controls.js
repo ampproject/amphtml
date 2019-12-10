2fbed7dfff27630bef48e4244b13dad1f91ede65
Sepand Parhami <sparhami@google.com>
2019-12-10 13:29:18 -0800

    Fix expected error.

--- extensions/amp-base-carousel/0.1/test-e2e/test-hidden-controls.js
+++ extensions/amp-base-carousel/0.1/test-e2e/test-hidden-controls.js
@@ -25,6 +25,26 @@ describes.endtoend(
     environments: ['single'],
   },
   async function(env) {
+    /**
+     * A helper function for expecting an error from an async function since we
+     * don't have ChaiAsExpected and we cannot wait for errors from
+     * @param {function()} fn A function to run
+     * @param {!RegExp} regExp A regular expression to match the error message.
+     */
+    async function expectAysncError(fn, regExp) {
+      let error;
+
+      try {
+        await fn();
+      } catch (e) {
+        error = e;
+      } finally {
+        await expect(error, 'Expected an error to be thrown.').to.not.be
+          .undefined;
+        await expect(error.toString()).to.match(regExp);
+      }
+    }
+
     let controller;
 
     beforeEach(async () => {
@@ -34,8 +54,24 @@ describes.endtoend(
     it('should not go to the next slide', async () => {
       const nextArrow = await getNextArrow(controller);
 
-      // The test driver should throw an error, since the element is not clickable.
-      await expect(() => controller.click(nextArrow)).to.throw;
+      // The test driver should throw an error, since the element is not
+      // clickable.
+      // TODO(sparhami) Ideally we would do something like:
+      // `await expect(() => controller.click(nextArrow)).to.throw();`, but
+      // that  does not work for a few reasons, including:
+      // * Click does not return a ControllerPromise
+      // * ControllerPromise uses a Promise rejection to singal it is not done
+      //   yet. The wrapper logic would need to be modified to allow for an
+      //   expected error type or message to sort circuit the waiting, allowing
+      //   use of something like:
+      //   ```
+      //   await expect(() => controller.click(nextArrow))
+      //      .to.throw(/ElementClickInterceptedError/);
+      //   ```
+      expectAysncError(
+        () => controller.click(nextArrow),
+        /ElementClickInterceptedError/
+      );
     });
   }
 );
