0a0da4337bfd09c94d417fef31fe4d01daba99a5
Micajuine Ho <micajuineho@google.com>
2019-12-10 17:13:05 -0800

    tests

---
+++ extensions/amp-consent/0.1/test-e2e/test-amp-consent.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Test client side and server side
+ */
+describes.endtoend(
+  'amp-consent',
+  {
+    // can we change the url?
+    testUrl:
+      'http://localhost:8000/test/manual/amp-consent/amp-consent-basic-uses.amp.html',
+    experiments: ['amp-consent-geo-override'],
+  },
+  env => {
+    let controller;
+    // let requestBank;
+
+    beforeEach(async () => {
+      controller = env.controller;
+      // requestBank = env.requestBank;
+      // Maybe need this
+      // env.win.localStorage = {};
+    });
+
+    it('should listen to client side decision', async () => {
+      const ui1 = await controller.findElement('#ui1');
+      const ui2 = await controller.findElement('#ui2');
+      const postPromptUi = await controller.findElement('#postPromptUI');
+      const tillResponded = await controller.findElement(
+        '[data-block-on-consent="_till_responded"]'
+      );
+      const accepted = await controller.findElement(
+        '[data-block-on-consent="_till_accepted"]'
+      );
+      const autoReject = await controller.findElement(
+        '[data-block-on-consent="_auto_reject"]'
+      );
+
+      // Images are either loaded or not loaded
+      await expect(
+        controller.getElementAttribute(tillResponded, 'class')
+      ).to.match(/amp-notbuilt/);
+      await expect(controller.getElementAttribute(accepted, 'class')).to.match(
+        /amp-notbuilt/
+      );
+      await expect(
+        controller.getElementAttribute(autoReject, 'class')
+      ).to.does.not.match(/amp-notbuilt/);
+
+      // Correct prompt is showing
+      await expect(controller.getElementProperty(ui1, 'hidden')).to.be.true;
+      await expect(controller.getElementProperty(ui2, 'hidden')).to.be.false;
+
+      const acceptButton = await controller.findElement('#accept');
+      await controller.click(acceptButton);
+
+      // Prompt UI disappears
+      await expect(controller.getElementProperty(ui2, 'hidden')).to.be.true;
+
+      // PostPrompUI appears
+      await expect(controller.getElementProperty(postPromptUi, 'hidden')).to.be
+        .false;
+
+      // Images load
+      await expect(
+        controller.getElementAttribute(tillResponded, 'class')
+      ).to.does.not.match(/amp-notbuilt/);
+      await expect(
+        controller.getElementAttribute(accepted, 'class')
+      ).to.does.not.match(/amp-notbuilt/);
+      await expect(
+        controller.getElementAttribute(autoReject, 'class')
+      ).to.does.not.match(/amp-notbuilt/);
+
+      console.log(env);
+
+      // Refresh
+      // await controller.type(tillResponded, '\uE035');
+    });
+
+    // it('should resepect server-side decision', async () => {
+    // });
+  }
+);
