505fa1a4495d79e76502e7b28f81675bd92ad0d9
Elijah Soria <elijahsoria@google.com>
2019-12-12 14:59:59 -0800

    Create wrapper getEntitlements_ and tests.

--- extensions/amp-subscriptions/0.1/amp-subscriptions.js
+++ extensions/amp-subscriptions/0.1/amp-subscriptions.js
@@ -273,6 +273,30 @@ export class SubscriptionService {
   }
 
   /**
+   * Internal function to wrap SwG decryption handling
+   * @param {!SubscriptionPlatform} platform
+   * @return {!Promise<?./entitlement.Entitlement>}
+   */
+  getEntitlements_(platform) {
+    return platform.getEntitlements().then(entitlements => {
+      if (
+        entitlements.granted &&
+        this.cryptoHandler_.isDocumentEncrypted() &&
+        !entitlements.decryptedDocumentKey
+      ) {
+        const logChannel = platform.getServiceId() == 'local' ? user() : dev();
+        logChannel.error(
+          TAG,
+          `${platform.getServiceId()}: Subscription granted and encryption enabled, ` +
+            'but no decrypted document key returned.'
+        );
+        return null;
+      }
+      return entitlements;
+    });
+  }
+
+  /**
    * @param {!SubscriptionPlatform} subscriptionPlatform
    * @return {!Promise}
    */
@@ -288,21 +312,11 @@ export class SubscriptionService {
       : this.ampdoc_.whenFirstVisible();
     return visiblePromise.then(() => {
       return this.timer_
-        .timeoutPromise(timeout, subscriptionPlatform.getEntitlements())
+        .timeoutPromise(timeout, this.getEntitlements_(subscriptionPlatform))
         .then(entitlement => {
           entitlement =
             entitlement ||
             Entitlement.empty(subscriptionPlatform.getServiceId());
-          if (this.checkEntitlementsAndEncryptionInvalid_(entitlement)) {
-            const userOrDevLog =
-              subscriptionPlatform.getServiceId() == 'local' ? user() : dev();
-            userOrDevLog.error(
-              TAG,
-              '${serviceId}: Subscription granted and encryption enabled, ' +
-                'but no decrypted document key returned.'
-            );
-            return null;
-          }
           this.resolveEntitlementsToStore_(
             subscriptionPlatform.getServiceId(),
             entitlement
@@ -403,18 +417,9 @@ export class SubscriptionService {
           origin
         );
         this.platformStore_.resolvePlatform('local', viewerPlatform);
-        viewerPlatform
-          .getEntitlements()
+        this.getEntitlements_(viewerPlatform)
           .then(entitlement => {
             devAssert(entitlement, 'Entitlement is null');
-            if (this.checkEntitlementsAndEncryptionInvalid_(entitlement)) {
-              user().error(
-                TAG,
-                `${service['serviceId']}: Subscription granted and encryption enabled, ` +
-                  `but no decrypted document key returned.`
-              );
-              return null;
-            }
             // Viewer authorization is redirected to use local platform instead.
             this.resolveEntitlementsToStore_(
               'local',
@@ -455,20 +460,6 @@ export class SubscriptionService {
    */
   getEncryptedDocumentKey(serviceId) {
     return this.cryptoHandler_.getEncryptedDocumentKey(serviceId);
-  }
-
-  /**
-   * Returns true if entitlement is granting, encryption is enabled, and
-   * the decrypted document key is null.
-   * @param {?./entitlement.Entitlement} entitlement
-   * @return {boolean}
-   */
-  checkEntitlementsAndEncryptionInvalid_(entitlement) {
-    return (
-      entitlement.granted &&
-      this.cryptoHandler_.isDocumentEncrypted() &&
-      !entitlement.decryptedDocumentKey
-    );
   }
 
   /**
