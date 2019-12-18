509614484f2fb155b7bd484bb175a47e88812a9c
Khoi Doan <khoid@google.com>
2019-12-18 16:06:21 -0500

    a4a envelopes: (mock) mraid envelope, and no host script if friendly (#24663)
    * a4a test envelopes: support for (mock) mraid envelope, and do not load the host script in certain cases.
    
    * Lint

--- build-system/server/app-index/document-modes.js
+++ build-system/server/app-index/document-modes.js
@@ -19,7 +19,9 @@ module.exports = {
   'shadow': '/shadow/',
   'a4a': '/a4a/',
   'a4a-3p': '/a4a-3p/',
+  'mraid': '/mraid/',
   'inabox': '/inabox/',
   'inabox-friendly': '/inabox-friendly/',
   'inabox-safeframe': '/inabox-safeframe/',
+  'inabox-mraid': '/inabox-mraid/',
 };
