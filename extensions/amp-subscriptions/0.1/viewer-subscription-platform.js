ec06183a209931e21f5ccac0a8c1e75c2cf14836
John Pettitt <jpettitt@google.com>
2019-12-19 15:25:20 -0800

    🐛 Metering viewer fix (#26110)
    * refactor to correctly handle nongranting entitlements
    
    * add tests

--- extensions/amp-subscriptions/0.1/viewer-subscription-platform.js
+++ extensions/amp-subscriptions/0.1/viewer-subscription-platform.js
@@ -149,26 +149,41 @@ export class ViewerSubscriptionPlatform {
       }
       const entitlements = decodedData['entitlements'];
       let entitlement = Entitlement.empty('local');
-      if (Array.isArray(entitlements)) {
-        for (let index = 0; index < entitlements.length; index++) {
-          if (
-            entitlements[index]['products'].indexOf(currentProductId) !== -1
-          ) {
-            const entitlementObject = entitlements[index];
-            entitlement = new Entitlement({
-              source: 'viewer',
-              raw: token,
-              granted: true,
-              grantReason: entitlementObject.subscriptionToken
-                ? GrantReason.SUBSCRIBER
-                : '',
-              dataObject: entitlementObject,
-              decryptedDocumentKey,
-            });
-            break;
+      let entitlementObject;
+
+      if (entitlements) {
+        // Not null
+        if (Array.isArray(entitlements)) {
+          // Multi entitlement case
+          for (let index = 0; index < entitlements.length; index++) {
+            if (
+              entitlements[index]['products'].indexOf(currentProductId) !== -1
+            ) {
+              entitlementObject = entitlements[index];
+              break;
+            }
           }
+        } else if (entitlements['products'].indexOf(currentProductId) !== -1) {
+          // Single entitlment case
+          entitlementObject = entitlements;
         }
-      } else if (decodedData['metering'] && !decodedData['entitlements']) {
+
+        if (entitlementObject) {
+          // Found a match
+          entitlement = new Entitlement({
+            source: 'viewer',
+            raw: token,
+            granted: true,
+            grantReason: entitlementObject.subscriptionToken
+              ? GrantReason.SUBSCRIBER
+              : '',
+            dataObject: entitlementObject,
+            decryptedDocumentKey,
+          });
+        }
+      }
+
+      if (decodedData['metering'] && !entitlement.granted) {
         // Special case where viewer gives metering but no entitlement
         entitlement = new Entitlement({
           source: decodedData['iss'] || '',
@@ -176,18 +191,6 @@ export class ViewerSubscriptionPlatform {
           granted: true,
           grantReason: GrantReason.METERING,
           dataObject: decodedData['metering'],
-          decryptedDocumentKey,
-        });
-      } else if (entitlements) {
-        // Not null
-        entitlement = new Entitlement({
-          source: 'viewer',
-          raw: token,
-          granted: entitlements.granted,
-          grantReason: entitlements.subscriptionToken
-            ? GrantReason.SUBSCRIBER
-            : '',
-          dataObject: entitlements,
           decryptedDocumentKey,
         });
       }
