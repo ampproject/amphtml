a21c7abefe32c9e0605444b04e2b78f6fc0fb068
Justin Ridgewell <jridgewell@google.com>
2019-12-21 02:34:38 -0500

    Include preact package.json

--- build-system/compile/sources.js
+++ build-system/compile/sources.js
@@ -51,7 +51,9 @@ const COMMON_GLOBS = [
   'node_modules/@ampproject/animations/dist/animations.mjs',
   'node_modules/@ampproject/worker-dom/package.json',
   'node_modules/@ampproject/worker-dom/dist/amp/main.mjs',
+  'node_modules/preact/package.json',
   'node_modules/preact/dist/preact.module.js',
+  'node_modules/preact/hooks/package.json',
   'node_modules/preact/hooks/dist/hooks.module.js',
 ];
 
