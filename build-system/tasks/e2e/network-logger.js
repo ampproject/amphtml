4ed3f968623ca73fa65e9dda14a40a8f9c8aaf53
Esther Kim <esth@google.com>
2020-01-02 15:38:35 -0500

    modules?

--- build-system/tasks/e2e/network-logger.js
+++ build-system/tasks/e2e/network-logger.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const logging = require('selenium-webdriver');
+import {logging} from 'selenium-webdriver';
 
 /** @enum {string} */
 const NetworkMethods = {
