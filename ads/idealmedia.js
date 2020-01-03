2d551353545c17b1283aadf2186db74f45ad52d5
Denys Siroshtan <denis.siroshtan@mgid.com>
2020-01-03 15:55:12 +0200

    idealmedia. adding uniques widget id

--- ads/idealmedia.js
+++ ads/idealmedia.js
@@ -44,12 +44,16 @@ export function idealmedia(global, data) {
     `${encodeURIComponent(data.widget)}.js?t=` +
     Math.floor(Date.now() / 36e5);
 
-  window['uniqId'] = ("00000" + Math.round(Math.random() * 100000).toString(16)).slice(-5);
+  window['uniqId'] = (
+    '00000' + Math.round(Math.random() * 100000).toString(16)
+  ).slice(-5);
 
   window.context.observeIntersection(function(changes) {
     changes.forEach(function(c) {
-      window['intersectionRect' + data.widget + '_' + window.uniqId] = c.intersectionRect;
-      window['boundingClientRect' + data.widget + '_' + window.uniqId] = c.boundingClientRect;
+      window['intersectionRect' + data.widget + '_' + window.uniqId] =
+        c.intersectionRect;
+      window['boundingClientRect' + data.widget + '_' + window.uniqId] =
+        c.boundingClientRect;
     });
   });
 
