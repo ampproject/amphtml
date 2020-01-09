53c18a2e1db2020788e83f79049dc8bd9bbfc4b3
honeybadgerdontcare <sedano@google.com>
2020-01-08 16:23:29 -0800

    Some files were missing licenses, added them.
    PiperOrigin-RevId: 288798060

--- validator/htmlparser/bin/entitytablegen.cc
+++ validator/htmlparser/bin/entitytablegen.cc
@@ -30,7 +30,23 @@
 #include "htmlparser/strings.h"
 
 const char kFileHeader[] =
-    R"HEADER(// AUTO GENERATED; DO NOT EDIT.
+    R"HEADER(//
+// Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the license.
+//
+
+// AUTO GENERATED; DO NOT EDIT.
 // To regenerate this file, see comments in bin/entitytablegen.cc
 
 #ifndef HTMLPARSER__ENTITY_H_
