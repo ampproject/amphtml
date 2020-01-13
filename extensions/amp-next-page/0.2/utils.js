e72528e601215c7262325057266f8510d2ea72c1
Wassim Gharbi <wassgha@gmail.com>
2020-01-13 11:55:12 -0800

    ✨Handling sticky, fixed and hidden elements in `amp-next-page` v2 (#26106)
    * Prototyping sticky element handling in amp-next-page
    
    * Deprecate amp-next-page-keep in favor of amp-next-page-hidden
    
    * Visibility bug fix
    
    * Remove animation added for testing
    
    * Exported host-page-specific parameters into HostPage
    
    * Undo validator changes for now

--- extensions/amp-next-page/0.2/utils.js
+++ extensions/amp-next-page/0.2/utils.js
@@ -20,8 +20,6 @@ import {
   parseUrlDeprecated,
   resolveRelativeUrl,
 } from '../../../src/url';
-import {removeElement} from '../../../src/dom';
-import {toArray} from '../../../src/types';
 import {user, userAssert} from '../../../src/log';
 
 /**
@@ -69,18 +67,4 @@ export function validatePage(page, hostUrl) {
       (url.search || '') +
       (url.hash || '');
   }
-}
-
-/**
- * Removes redundancies and unauthorized extensions and elements
- * @param {!Document} doc Document to attach.
- */
-export function sanitizeDoc(doc) {
-  // TODO(wassgha): Implement handling of sticky elements
-  // TODO(wassgha): Implement persistence of repeating elements (e.g amp-sidebar)
-
-  // TODO(wassgha): Parse for more pages to queue
-
-  // TODO(wassgha): Allow amp-analytics after bug bash
-  toArray(doc.querySelectorAll('amp-analytics')).forEach(removeElement);
 }
