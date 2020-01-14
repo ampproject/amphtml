d67ff22963a4562660958881435d11f95cf3256a
Wassim Gharbi <wassgha@gmail.com>
2020-01-14 11:43:13 -0800

    Cleanup

--- testing/amp-performance-extension/background.js
+++ testing/amp-performance-extension/background.js
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
+/* eslint-disable no-undef */
 chrome.browserAction.setBadgeText({
   text: 'ON',
 });
 chrome.browserAction.setBadgeBackgroundColor({
   color: '#15a341',
 });
+/* eslint-enable no-undef */
