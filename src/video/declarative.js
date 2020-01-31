8b18fdf417b122892feed69fb955d9cbada633e8
Alan Orozco <orozcoalan@gmail.com>
2020-01-31 10:55:31 -0800

    type

--- src/video/declarative.js
+++ src/video/declarative.js
@@ -19,7 +19,7 @@ import GenericIframeVideoPlayer from './generic';
  * @param {./generic.VideoPlayerElementDef=} config
  * @return {typeof AMP.BaseElement}
  */
-export const defineVideoPlayerElement = (config = {}) => element => {
+export const defineVideoPlayerElement = config => element => {
   const impl = new GenericIframeVideoPlayer(element);
   impl.vendorComponentConfig = config;
   return impl;
