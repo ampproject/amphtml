68dab673e87db9199f7622a8a1894fbba2255639
Enrique Marroquin <artezan@google.com>
2020-02-04 10:53:33 -0500

    split iframe css into different file

--- build-system/tasks/css.js
+++ build-system/tasks/css.js
@@ -59,6 +59,11 @@ const cssEntryPoints = [
     outJs: 'amp-story-player.css.js',
     outCss: 'amp-story-player-v0.css',
   },
+  {
+    path: 'amp-story-player-iframe.css',
+    outJs: 'amp-story-player-iframe.css.js',
+    outCss: 'amp-story-player-iframe-v0.css',
+  },
 ];
 
 /**
