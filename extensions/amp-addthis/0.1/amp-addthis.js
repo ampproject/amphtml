f6e12ac0feec0e5502860b3c86169f4722ac158b
Alan Orozco <alanorozco@users.noreply.github.com>
2020-02-12 10:27:06 -0800

    🐛 Bust cached document for amp-addthis (#26761)
    Adds ?_amp_=VERSION query param to bust broken versions of document when cached
    (origin has overly long max-age of 999 days, workaround for #24848)

--- extensions/amp-addthis/0.1/amp-addthis.js
+++ extensions/amp-addthis/0.1/amp-addthis.js
@@ -52,7 +52,6 @@ import {DwellMonitor} from './addthis-utils/monitors/dwell-monitor';
 import {PostMessageDispatcher} from './post-message-dispatcher';
 import {ScrollMonitor} from './addthis-utils/monitors/scroll-monitor';
 import {Services} from '../../../src/services';
-
 import {callEng} from './addthis-utils/eng';
 import {callLojson} from './addthis-utils/lojson';
 import {callPjson} from './addthis-utils/pjson';
@@ -66,6 +65,7 @@ import {
 } from './addthis-utils/mode';
 import {getOgImage} from './addthis-utils/meta';
 import {getWidgetOverload} from './addthis-utils/get-widget-id-overloaded-with-json-for-anonymous-mode';
+import {internalRuntimeVersion} from '../../../src/internal-version';
 import {isLayoutSizeDefined} from '../../../src/layout';
 import {listen} from '../../../src/event-helper';
 import {parseUrlDeprecated} from '../../../src/url';
@@ -299,7 +299,10 @@ class AmpAddThis extends AMP.BaseElement {
       dict({
         'frameborder': 0,
         'title': ALT_TEXT,
-        'src': `${ORIGIN}/dc/amp-addthis.html`,
+        // Document has overly long cache age: go.amp.dev/issue/24848
+        // Adding AMP runtime version as a meaningless query param to force bust
+        // cached versions.
+        'src': `${ORIGIN}/dc/amp-addthis.html?_amp_=${internalRuntimeVersion()}`,
         'id': this.widgetId_,
         'pco': this.productCode_,
         'containerClassName': this.containerClassName_,
