67dbc76385b58f935266d0e340c17e56a57d37e7
Alan Orozco <alanorozco@users.noreply.github.com>
2020-02-12 10:27:06 -0800

    🐛 Bust cached document for amp-addthis (#26761)
    Adds ?_amp_=VERSION query param to bust broken versions of document when cached
    (origin has overly long max-age of 999 days, workaround for #24848)

--- extensions/amp-addthis/0.1/test/test-amp-addthis.js
+++ extensions/amp-addthis/0.1/test/test-amp-addthis.js
@@ -31,6 +31,7 @@ import {getDetailsForMeta, getMetaElements} from './../addthis-utils/meta';
 import {getKeywordsString} from './../addthis-utils/classify';
 import {getSessionId} from '../addthis-utils/session';
 import {getWidgetOverload} from '../addthis-utils/get-widget-id-overloaded-with-json-for-anonymous-mode';
+import {startsWith} from '../../../../src/string';
 import {toArray} from '../../../../src/types';
 
 describes.realWin(
@@ -101,9 +102,11 @@ describes.realWin(
 
     function testIframe(iframe) {
       expect(iframe).to.not.equal(null);
-      expect(iframe.getAttribute('src')).to.equal(
-        `${ORIGIN}/dc/amp-addthis.html`
-      );
+      const srcPrefix = `${ORIGIN}/dc/amp-addthis.html?`;
+      expect(
+        startsWith(iframe.getAttribute('src'), srcPrefix),
+        `iframe src starts with ${srcPrefix}`
+      ).to.be.true;
       expect(iframe.getAttribute('title')).to.equal(ALT_TEXT);
     }
 
