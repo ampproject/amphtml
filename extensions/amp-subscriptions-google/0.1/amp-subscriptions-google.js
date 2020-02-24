2ade2017430366b76acf69c71dcd23cec32bb4de
Matt Borofsky <mborof@google.com>
2020-02-24 14:30:28 -0800

    add sendBeacon method to be overriden later

--- extensions/amp-subscriptions-google/0.1/amp-subscriptions-google.js
+++ extensions/amp-subscriptions-google/0.1/amp-subscriptions-google.js
@@ -596,6 +596,9 @@ class AmpFetcher {
   constructor(win) {
     /** @const @private {!../../../src/service/xhr-impl.Xhr} */
     this.xhr_ = Services.xhrFor(win);
+
+    /** @private @const {!Window} */
+    this.win_ = win;
   }
 
   /** @override */
@@ -611,6 +614,34 @@ class AmpFetcher {
   /** @override */
   fetch(input, opt_init) {
     return this.xhr_.fetch(input, opt_init); //needed to kepp closure happy
+  }
+
+  /**
+   *
+   * @param {string} url
+   * @param {*} data
+   */
+  sendBeacon(url, data) {
+    const contentType = 'application/x-www-form-urlencoded;charset=UTF-8';
+    const body =
+      'f.req=' +
+      JSON.stringify(/** @type {JsonObject} */ (data.toArray(false)));
+    const {navigator} = this.win_;
+
+    if (navigator.sendBeacon) {
+      const blob = new Blob([body], {type: contentType});
+      navigator.sendBeacon(url, blob);
+      return;
+    }
+
+    // Only newer browsers support beacon.  Fallback to standard XHR POST.
+    const init = {
+      method: 'POST',
+      headers: {'Content-Type': contentType},
+      credentials: 'include',
+      body,
+    };
+    this.fetch(url, init);
   }
 }
 
--- extensions/amp-subscriptions-google/0.1/test/test-amp-subscriptions-google.js
+++ extensions/amp-subscriptions-google/0.1/test/test-amp-subscriptions-google.js
@@ -20,6 +20,11 @@ import {
   SubscriptionAnalytics,
 } from '../../../amp-subscriptions/0.1/analytics';
 import {
+  AmpFetcher,
+  GoogleSubscriptionsPlatform,
+} from '../amp-subscriptions-google';
+import {
+  AnalyticsContext,
   ConfiguredRuntime,
   Entitlements,
   SubscribeResponse,
@@ -29,7 +34,6 @@ import {
   Entitlement,
   GrantReason,
 } from '../../../amp-subscriptions/0.1/entitlement';
-import {GoogleSubscriptionsPlatform} from '../amp-subscriptions-google';
 import {PageConfig} from '../../../../third_party/subscriptions-project/config';
 import {ServiceAdapter} from '../../../amp-subscriptions/0.1/service-adapter';
 import {Services} from '../../../../src/services';
@@ -38,6 +42,63 @@ import {toggleExperiment} from '../../../../src/experiments';
 
 const PLATFORM_ID = 'subscribe.google.com';
 const AMP_URL = 'myAMPurl.amp';
+
+describes.realWin('AmpFetcher', {amp: true}, env => {
+  let win;
+  let ampdoc;
+  let fetcher;
+  let xhr;
+
+  const sentUrl = 'url';
+  const sentContent = new AnalyticsContext([
+    'AnalyticsContext',
+    'embed',
+    'tx',
+    'refer',
+    'utmS',
+    'utmC',
+    'utmM',
+    'sku',
+    true,
+    ['exp1', 'exp2'],
+    'version',
+    'baseUrl',
+  ]);
+  const contentType = 'application/x-www-form-urlencoded;charset=UTF-8';
+  const expectedBodyString =
+    'f.req=' + JSON.stringify(sentContent.toArray(false));
+
+  beforeEach(() => {
+    win = env.win;
+    ampdoc = env.ampdoc;
+    fetcher = new AmpFetcher(ampdoc.win);
+    xhr = Services.xhrFor(env.win);
+  });
+
+  it('should support beacon when beacon supported', async () => {
+    const expectedBlob = new Blob([expectedBodyString], {type: contentType});
+    env.sandbox.stub(win.navigator, 'sendBeacon').callsFake((url, body) => {
+      expect(url).to.equal(sentUrl);
+      expect(body).to.deep.equal(expectedBlob);
+    });
+    fetcher.sendBeacon(sentUrl, sentContent);
+  });
+
+  it('should support beacon when beacon not supported', async () => {
+    win.navigator.sendBeacon = null;
+    env.sandbox.stub(xhr, 'fetch').callsFake((url, init) => {
+      expect(url).to.equal(sentUrl);
+      expect(init).to.deep.equal({
+        method: 'POST',
+        headers: {'Content-Type': contentType},
+        credentials: 'include',
+        body: expectedBodyString,
+      });
+    });
+
+    fetcher.sendBeacon(sentUrl, sentContent);
+  });
+});
 
 describes.realWin('amp-subscriptions-google', {amp: true}, env => {
   let ampdoc;
