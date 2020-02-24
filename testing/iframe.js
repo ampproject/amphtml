0d23a4de6eee14945bdd3ec87a2c7915cf3e3920
Matt Mower <mdmower@cmphys.com>
2020-02-24 12:39:08 -0800

    integration: Improve amp-img test reliability (#26832)
    * integration: Improve amp-img test reliability
    
    Increase number of LOAD_START expected to account for 2 amp-img updates
    (from 3 to 5, instead of from 3 to 4) after window resize.
    
    * Revert "integration: Improve amp-img test reliability"
    
    This reverts commit a326a397cd758fa81ed8d01ce928bf4c046f052a.
    
    * integration: Fix flaky amp-img test
    
    The 'should respect media queries' amp-img integration test fails
    intermittently due to a race between setting/getting class
    i-amphtml-hidden-by-media-query when the window resizes. The race
    results because expect() is performed immediately after an amp-img
    LOAD_START event fires without waiting for the other amp-img to comlete
    its UNLOAD event. Remedy this by waiting for both amp-img layout changes
    to complete before checking their final classes.
    
    - Add UNLOAD event to custom-element test event dispatches
    - Fix names of test events LOAD_START, LOAD_END so that they match the
      corresponding signal names

--- testing/iframe.js
+++ testing/iframe.js
@@ -74,6 +74,7 @@ export function createFixtureIframe(
       [AmpEvents.LOAD_END]: 0,
       [AmpEvents.LOAD_START]: 0,
       [AmpEvents.STUBBED]: 0,
+      [AmpEvents.UNLOAD]: 0,
       [BindEvents.INITIALIZE]: 0,
       [BindEvents.SET_STATE]: 0,
       [BindEvents.RESCAN_TEMPLATE]: 0,
