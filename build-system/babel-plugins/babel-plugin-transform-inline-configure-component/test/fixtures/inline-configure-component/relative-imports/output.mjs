10acf18fc32a39cdad37c313010220d792b26516
Alan Orozco <alanorozco@users.noreply.github.com>
2020-02-26 11:05:40 -0800

    🏗 Transform aliased configured components (#26541)
    Partial for #26572.
    
    `transform-inline-configure-component` finds `configureComponent(MyConstructor, {foo: 'bar'})` calls and:
    
    1. Inlines imported `MyConstructor` in current scope.
    
    2. Replaces `*.*.STATIC_CONG_.foo` access with its value as defined in config object `{foo: 'bar'}`.
    
    3. Replaces `configureComponent()` call with inlined `MyConstructor`'s id.
    
    For runtime use of `configureComponent` like:
    
    ```js
    import {MyConstructor} from './my-ctor';
    const TAG = 'amp-foo';
    const MyWrappedConstructor = configureComponent(
      MyConstructor,
      {TAG, foo: 'foo'}
    );
    ```
    
    Composed class gets aliased, hoisted and its configuration replaced inline:
    
    ```js
    import {something} from './imported-by-constructor';
    
    class MyConstructor {
      constructor() {
        // references to config object are replaced here, from:
        // `console.log(this.STATIC_CONFIG_.foo)`
        console.log(_foo);
    
        // ids in global scope are kept:
        const TAG = TAG;
    
        // unset properties are replaced with undefined, so this would
        // get minified
        const something = undefined || 'default value';
    
        // destructuring keeps only the default value when prop undefined:
        // `const {bar = 'default value'} = this.STATIC_CONFIG_`
        const bar = 'default value';
      }
    }
    
    const _foo = 'foo';
    
    const TAG = 'amp-foo';
    const MyWrappedConstructor = MyConstructor;
    ```

---
+++ build-system/babel-plugins/babel-plugin-transform-inline-configure-component/test/fixtures/inline-configure-component/relative-imports/output.mjs
@@ -0,0 +1,28 @@
+const _a = 'value for a',
+      _b = 'value for b',
+      _d = 'value for c';
+
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import "../backwards";
+import "./input-nested-directory/for/wards";
+import { leaveThis } from 'alone';
+import { baz } from "./input-nested-directory/foo/bar";
+
+class RelativeImports {}
+
+import { RelativeImports as _RelativeImports } from './input-nested-directory/input-base-class';
+foo(_RelativeImports);
