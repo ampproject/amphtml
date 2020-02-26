f7d547bfde5037d988862b954810d765e084ab5e
Justin Ridgewell <jridgewell@google.com>
2020-02-26 17:15:27 -0500

    🏗 Allow array destructuring on preact hooks (#26901)
    * Allow array destructuring on preact hooks
    
    * Use object destructure

--- build-system/babel-plugins/babel-plugin-transform-simple-array-destructure/test/fixtures/transform/deep-array/input.js
+++ build-system/babel-plugins/babel-plugin-transform-simple-array-destructure/test/fixtures/transform/deep-array/input.js
@@ -14,24 +14,4 @@
  * limitations under the License.
  */
 
-import {useResourcesNotify} from '../../../src/preact/utils';
-import {useState} from '../../../src/preact';
-
-/**
- * Renders the children prop, waiting for it to resolve if it is a promise.
- *
- * @param {!JsonObject} props
- * @return {Preact.Renderable}
- */
-export function AsyncRender(props) {
-  const children = props['children'];
-  const [state, set] = useState(children);
-  useResourcesNotify();
-
-  if (state && state.then) {
-    Promise.resolve(children).then(set);
-    return null;
-  }
-
-  return state;
-}
+const [[first]] = document;
