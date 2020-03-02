4f37fff93f582f3de760f9d395f8d052b46ba881
Gabriel Chicoye <gabriel.chicoye@gmail.com>
2020-03-02 19:27:30 +0100

    Update callout-vendors.js

--- extensions/amp-a4a/0.1/callout-vendors.js
+++ extensions/amp-a4a/0.1/callout-vendors.js
@@ -115,7 +115,7 @@ const RTC_VENDORS = jsonConfiguration({
   },
   glxm: {
     url: 
-      'https://pbserver.galaxiemedia.com/openrtb2/amp?tag_id=REQUEST_ID&w=ATTR(width)&h=ATTR(height)&ow=ATTR(data-override-width)&oh=ATTR(data-override-height)&ms=ATTR(data-multi-size)&slot=ATTR(data-slot)&targeting=TGT&curl=CANONICAL_URL&timeout=TIMEOUT&adc=ADCID&purl=HREF&gdpr_consent=CONSENT_STRING&account=ACCOUNT_ID',
+      'https://pbserver.galaxiemedia.fr/openrtb2/amp?tag_id=REQUEST_ID&w=ATTR(width)&h=ATTR(height)&ow=ATTR(data-override-width)&oh=ATTR(data-override-height)&ms=ATTR(data-multi-size)&slot=ATTR(data-slot)&targeting=TGT&curl=CANONICAL_URL&timeout=TIMEOUT&adc=ADCID&purl=HREF&gdpr_consent=CONSENT_STRING&account=ACCOUNT_ID',
     macros: ['REQUEST_ID', 'CONSENT_STRING', 'ACCOUNT_ID'],
     disableKeyAppend: true,
   },
