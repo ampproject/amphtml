c7e3602d03f15a430720b1ddb80b4b82d959235b
Alex Ivasyuv <industral@gmail.com>
2020-03-03 11:06:03 +0200

    fixed issue with TypeError: Assignment to constant variable.

--- build-system/tasks/check-links.js
+++ build-system/tasks/check-links.js
@@ -134,7 +134,8 @@ function checkLinksInFile(file) {
         return;
       }
       let containsDeadLinks = false;
-      for (const {link, status, statusCode} of results) {
+      // eslint-disable-next-line prefer-const
+      for (let {link, status, statusCode} of results) {
         // Skip links to files that were introduced by the PR.
         if (isLinkToFileIntroducedByPR(link) && status == 'dead') {
           // Log links with the correct github base as alive, otherwise flag deadlinks.
