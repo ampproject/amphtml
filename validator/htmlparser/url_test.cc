d76e44277a8fbb9910fc252d4c471bb12b4ef0f1
honeybadgerdontcare <sedano@google.com>
2020-03-04 09:40:30 -0800

    Add licenses that were missing
    PiperOrigin-RevId: 298871126

--- validator/htmlparser/url_test.cc
+++ validator/htmlparser/url_test.cc
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the license.
+//
 
 #include "url.h"
 #include "gtest/gtest.h"
