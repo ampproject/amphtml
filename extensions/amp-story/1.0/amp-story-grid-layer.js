8f193b5452eba48d74c3296d23fab8566a8ed965
Dima Voytenko <dvoytenko@gmail.com>
2020-03-05 11:48:32 -0800

    switch to service

--- extensions/amp-story/1.0/amp-story-grid-layer.js
+++ extensions/amp-story/1.0/amp-story-grid-layer.js
@@ -26,6 +26,7 @@
  * </code>
  */
 
+import {StateProperty, getStoreService} from './amp-story-store-service';
 import {AmpStoryBaseLayer} from './amp-story-base-layer';
 import {assertDoesNotContainDisplay, px, setStyles} from '../../../src/style';
 import {matches, scopedQuerySelectorAll} from '../../../src/dom';
@@ -84,6 +85,9 @@ export class AmpStoryGridLayer extends AmpStoryBaseLayer {
 
     /** @private {boolean} */
     this.prerenderAllowed_ = false;
+
+    /** @private {?{horiz: number, vert: number}} */
+    this.aspectRatio_ = null;
   }
 
   /** @override */
@@ -101,6 +105,7 @@ export class AmpStoryGridLayer extends AmpStoryBaseLayer {
     this.applyTemplateClassName_();
     this.setOwnCssGridStyles_();
     this.setDescendentCssGridStyles_();
+    this.initializeListeners_();
   }
 
   /** @override */
@@ -108,41 +113,50 @@ export class AmpStoryGridLayer extends AmpStoryBaseLayer {
     return this.prerenderAllowed_;
   }
 
-  // TODO(26866): switch back to onMeasuredChange and remove the layoutBox
-  // equality checks.
-  /** @override */
-  onLayoutMeasure() {
-    super.onLayoutMeasure();
-
+  /** @private */
+  initializeListeners_() {
     const aspectRatio = this.element.getAttribute('aspect-ratio');
-    if (!aspectRatio) {
+    if (aspectRatio) {
+      const aspectRatioSplits = aspectRatio.split(':');
+      const horiz = parseInt(aspectRatioSplits[0], 10);
+      const vert = parseInt(aspectRatioSplits[1], 10);
+      if (horiz > 0 && vert > 0) {
+        this.aspectRatio_ = {horiz, vert};
+        const storeService = getStoreService(this.win);
+        storeService.subscribe(
+          StateProperty.PAGE_SIZE,
+          this.updatePageSize_.bind(this),
+          true /* callToInitialize */
+        );
+      }
+    }
+  }
+
+  /**
+   * @param {{width: number, height: number}} pageSize
+   * @private
+   */
+  updatePageSize_(pageSize) {
+    if (!pageSize) {
       return;
     }
-
-    const aspectRatioSplits = aspectRatio.split(':');
-    const horiz = parseInt(aspectRatioSplits[0], 10);
-    const vert = parseInt(aspectRatioSplits[1], 10);
-
-    return this.getVsync().runPromise(
-      {
-        measure: state => {
-          const parent = this.element.parentElement;
-          const vw = parent./*OK*/ clientWidth;
-          const vh = parent./*OK*/ clientHeight;
-          state.width = Math.min(vw, vh * horiz / vert);
-          state.height = Math.min(vh, vw * vert / horiz);
-        },
-        mutate: ({width, height}) => {
-          if (width === 0 && height === 0) {
-            return;
-          }
-          this.element.classList.add('i-amphtml-story-grid-template-aspect');
-          this.element.style.setProperty('--i-amphtml-story-layer-width', px(width));
-          this.element.style.setProperty('--i-amphtml-story-layer-height', px(height));
-        },
-      },
-      {}
-    );
+    const {width: vw, height: vh} = pageSize;
+    const {horiz, vert} = this.aspectRatio_;
+    const width = Math.min(vw, (vh * horiz) / vert);
+    const height = Math.min(vh, (vw * vert) / horiz);
+    if (width > 0 && height > 0) {
+      this.getVsync().mutate(() => {
+        this.element.classList.add('i-amphtml-story-grid-template-aspect');
+        this.element.style.setProperty(
+          '--i-amphtml-story-layer-width',
+          px(width)
+        );
+        this.element.style.setProperty(
+          '--i-amphtml-story-layer-height',
+          px(height)
+        );
+      });
+    }
   }
 
   /**
