4cd6bef7c531b079b5a0567a59d46afd76f45313
Caroline Liu <carolineliu@google.com>
2020-03-06 15:17:50 -0500

    Import and pass props to SVG

--- extensions/amp-social-share/0.2/social-share.js
+++ extensions/amp-social-share/0.2/social-share.js
@@ -14,8 +14,9 @@
  * limitations under the License.
  */
 
+import * as Preact from '../../../src/preact';
 import {Keys} from '../../../src/utils/key-codes';
-import {createElement, useContext} from '../../../src/preact';
+import {SocialShareIcon} from '../../../third_party/optimized-svg-icons/social-share-svgs';
 import {dev, devAssert, userAssert} from '../../../src/log';
 import {getAmpContext} from '../../../src/preact/context';
 import {openWindowDialog} from '../../../src/dom';
@@ -23,22 +24,12 @@ import {parseQueryString} from '../../../src/url';
 import {startsWith} from '../../../src/string';
 import {useResourcesNotify} from '../../../src/preact/utils';
 
-const socialShareStyle = `background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  text-decoration: none;
-  cursor: pointer;
-  position: relative;
-  background-color: black; 
-  background-image: url('data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20512%20512%22%3E%3Cpath%20fill%3D%22%23ffffff%22%20d%3D%22M211.9%20197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0%200%200-22.1%200-33.7%200-13.9%202.8-19.5%2016.3-19.5%2010.9%200%2038.2%200%2038.2%200V82.9c0%200-40.2%200-48.8%200%20-52.5%200-76.1%2023.1-76.1%2067.3C211.9%20188.8%20211.9%20197.4%20211.9%20197.4z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E');
-`;
-
 /**
  * @param {!JsonObject} props
- * @return {Preact.Renderable}
+ * @return {PreactDef.Renderable}
  */
 export function SocialShare(props) {
-  const context = useContext(getAmpContext());
+  const context = Preact.useContext(getAmpContext());
 
   /**
    * Handle key presses on the element.
@@ -70,16 +61,17 @@ export function SocialShare(props) {
     }
   }
 
-  const attrs = {
-    onClick: handleActivation,
-    onKeydown: handleKeyPress,
-    className: `social-share-${props['type']}`,
-    style: socialShareStyle + props['style'],
-    role: 'button',
-    tabIndex: props['tabindex'] || '0',
-  };
-
-  const render = props['render'];
+  const styleType = `SOCIAL_SHARE_${props['type'].toUpperCase()}`;
   useResourcesNotify();
-  return render(null, props['children'], createElement('div', attrs));
+  return (
+    <SocialShareIcon
+      className={`social-share-${props['type']}`}
+      onClick={handleActivation}
+      onKeydown={handleKeyPress}
+      role="button"
+      style={CSS.BASE_STYLE + CSS[styleType] + props['style']}
+      styleType={styleType}
+      tabIndex={props['tabIndex'] || '0'}
+    />
+  );
 }
