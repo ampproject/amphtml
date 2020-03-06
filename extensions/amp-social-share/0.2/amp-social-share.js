15a9623420a774d484e49dff31e249e91f6a6340
Caroline Liu <carolineliu@google.com>
2020-03-06 15:22:43 -0500

    Remove unused imports

--- extensions/amp-social-share/0.2/amp-social-share.js
+++ extensions/amp-social-share/0.2/amp-social-share.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Fragment, createElement} from '../../../src/preact';
 import {PreactBaseElement} from '../../../src/preact/base-element';
 import {Services} from '../../../src/services';
 import {SocialShare} from './social-share';
