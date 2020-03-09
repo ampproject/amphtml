10cc2a7c30885a16da53564677c62ff617f583d7
Gabriel Majoulet <gmajoulet@gmail.com>
2020-03-09 19:08:40 -0400

    Story navigation education. (#27140)
    * Squash commits.
    
    * s/this.getAmpDoc().win/this.win

--- extensions/amp-story-education/0.1/amp-story-education.js
+++ extensions/amp-story-education/0.1/amp-story-education.js
@@ -14,37 +14,215 @@
  * limitations under the License.
  */
 
+import {
+  Action,
+  StateProperty,
+} from '../../amp-story/1.0/amp-story-store-service';
 import {CSS} from '../../../build/amp-story-education-0.1.css';
 import {Layout} from '../../../src/layout';
+import {LocalizedStringId} from '../../../src/localized-strings';
+import {Services} from '../../../src/services';
+import {createShadowRootWithStyle} from '../../amp-story/1.0/utils';
+import {dev} from '../../../src/log';
 import {htmlFor} from '../../../src/static-template';
+import {removeChildren} from '../../../src/dom';
+import {toggle} from '../../../src/style';
+
+/** @type {string} */
+const TAG = 'amp-story-education';
 
 /**
- * Generates the amp-story-education template.
+ * Generates the navigation education template.
  * @param {!Element} element
  * @return {!Element}
  */
-const buildFooTemplate = element => {
+const buildNavigationEl = element => {
   const html = htmlFor(element);
   return html`
-    <div class="i-amphtml-story-education"></div>
+    <div class="i-amphtml-story-education-navigation">
+      <div class="i-amphtml-story-education-navigation-gesture">
+        <div class="i-amphtml-story-education-navigation-gesture-outer"></div>
+        <div class="i-amphtml-story-education-navigation-gesture-inner"></div>
+      </div>
+      <div class="i-amphtml-story-education-navigation-progress"></div>
+      <div class="i-amphtml-story-education-navigation-instructions"></div>
+      <button class="i-amphtml-story-education-navigation-button"></button>
+    </div>
   `;
+};
+
+/** @enum */
+export const State = {
+  HIDDEN: 0,
+  NAVIGATION_TAP: 1,
+  NAVIGATION_SWIPE: 2,
 };
 
 export class AmpStoryEducation extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
+
+    /** @private {!Element} */
+    this.containerEl_ = this.win.document.createElement('div');
+
+    /** @private @const {!../../../src/service/localization.LocalizationService} */
+    this.localizationService_ = Services.localizationService(this.win);
+
+    /** @private {?boolean} */
+    this.storyPausedStateToRestore_ = null;
+
+    /** @private {!State} */
+    this.state_ = State.HIDDEN;
+
+    /** @private @const {!../../amp-story/1.0/amp-story-store-service.AmpStoryStoreService} */
+    this.storeService_ = /** @type {!../../amp-story/1.0/amp-story-store-service.AmpStoryStoreService} */ (Services.storyStoreService(
+      this.win
+    ));
   }
 
   /** @override */
   buildCallback() {
-    const template = buildFooTemplate(this.element);
-    this.element.appendChild(template);
+    this.containerEl_.classList.add('i-amphtml-story-education');
+    toggle(this.containerEl_, false);
+    this.startListening_();
+    createShadowRootWithStyle(this.element, this.containerEl_, CSS);
   }
 
   /** @override */
   isLayoutSupported(layout) {
     return layout === Layout.CONTAINER;
+  }
+
+  /**
+   * @private
+   */
+  startListening_() {
+    this.containerEl_.addEventListener(
+      'click',
+      () => this.onClick_(),
+      true /** useCapture */
+    );
+
+    this.storeService_.subscribe(
+      StateProperty.RTL_STATE,
+      rtlState => this.onRtlStateUpdate_(rtlState),
+      true /** callToInitialize */
+    );
+  }
+
+  /**
+   * Handles click events.
+   * @private
+   */
+  onClick_() {
+    if (this.state_ === State.NAVIGATION_TAP) {
+      this.setState_(State.NAVIGATION_SWIPE);
+      return;
+    }
+
+    this.setState_(State.HIDDEN);
+  }
+
+  /**
+   * @param {boolean} rtlState
+   * @private
+   */
+  onRtlStateUpdate_(rtlState) {
+    this.mutateElement(() => {
+      rtlState
+        ? this.containerEl_.setAttribute('dir', 'rtl')
+        : this.containerEl_.removeAttribute('dir');
+    });
+  }
+
+  /**
+   * @param {!State} state
+   * @private
+   */
+  setState_(state) {
+    if (this.state_ === state) {
+      return;
+    }
+
+    this.state_ = state;
+    let el;
+
+    switch (state) {
+      case State.HIDDEN:
+        this.mutateElement(() => {
+          removeChildren(this.containerEl_);
+          toggle(this.containerEl_, false);
+          this.storeService_.dispatch(
+            Action.TOGGLE_PAUSED,
+            this.storyPausedStateToRestore_
+          );
+        });
+        break;
+      case State.NAVIGATION_TAP:
+        el = buildNavigationEl(this.element);
+        el.setAttribute('step', 'tap');
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-progress'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_TAP_PROGRESS
+        );
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-instructions'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_TAP_INSTRUCTIONS
+        );
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-button'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_TAP_DISMISS
+        );
+        this.showTemplate_(el);
+        break;
+      case State.NAVIGATION_SWIPE:
+        el = buildNavigationEl(this.element);
+        el.setAttribute('step', 'swipe');
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-progress'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_SWIPE_PROGRESS
+        );
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-instructions'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_SWIPE_INSTRUCTIONS
+        );
+        el.querySelector(
+          '.i-amphtml-story-education-navigation-button'
+        ).textContent = this.localizationService_.getLocalizedString(
+          LocalizedStringId.AMP_STORY_EDUCATION_NAVIGATION_SWIPE_DISMISS
+        );
+        this.showTemplate_(el);
+        break;
+      default:
+        dev().error(TAG, 'Unknown state %s.', state);
+        break;
+    }
+  }
+
+  /**
+   * @param {!Element} template
+   * @private
+   */
+  showTemplate_(template) {
+    if (this.storyPausedStateToRestore_ === null) {
+      this.storyPausedStateToRestore_ = !!this.storeService_.get(
+        StateProperty.PAUSED_STATE
+      );
+    }
+
+    this.storeService_.dispatch(Action.TOGGLE_PAUSED, true);
+
+    this.mutateElement(() => {
+      removeChildren(this.containerEl_);
+      toggle(this.containerEl_, true);
+      this.containerEl_.appendChild(template);
+    });
   }
 }
 
--- extensions/amp-story-education/0.1/test/test-amp-story-education.js
+++ extensions/amp-story-education/0.1/test/test-amp-story-education.js
@@ -14,23 +14,164 @@
  * limitations under the License.
  */
 
-import {AmpStoryEducation} from '../amp-story-education';
+import {
+  Action,
+  AmpStoryStoreService,
+  StateProperty,
+} from '../../../amp-story/1.0/amp-story-store-service';
+import {AmpStoryEducation, State} from '../amp-story-education';
+import {LocalizationService} from '../../../../src/service/localization';
+import {registerServiceBuilder} from '../../../../src/service';
 
 describes.realWin('amp-story-education', {amp: true}, env => {
+  let storeService;
   let storyEducation;
   let win;
 
   beforeEach(() => {
     win = env.win;
+
+    const localizationService = new LocalizationService(win);
+    registerServiceBuilder(win, 'localization', function() {
+      return localizationService;
+    });
+
+    storeService = new AmpStoryStoreService(win);
+    registerServiceBuilder(win, 'story-store', function() {
+      return storeService;
+    });
+
     const element = win.document.createElement('amp-story-education');
     win.document.body.appendChild(element);
     storyEducation = new AmpStoryEducation(element);
+
+    env.sandbox.stub(storyEducation, 'mutateElement').callsFake(fn => fn());
   });
 
   it('should render', () => {
     storyEducation.buildCallback();
-    expect(storyEducation.element.firstElementChild).to.have.class(
+    expect(storyEducation.containerEl_).to.have.class(
       'i-amphtml-story-education'
     );
+  });
+
+  it('should be hidden by default', () => {
+    storyEducation.buildCallback();
+    expect(storyEducation.containerEl_).to.have.attribute('hidden');
+  });
+
+  it('should be visible when rendering an education step', () => {
+    storyEducation.buildCallback();
+    // TODO(gmajoulet): remove private method call when viewer messaging is
+    // introduced.
+    storyEducation.setState_(State.NAVIGATION_TAP);
+    expect(storyEducation.containerEl_).to.not.have.attribute('hidden');
+  });
+
+  it('should propagate the dir attribute', () => {
+    storyEducation.buildCallback();
+    storeService.dispatch(Action.TOGGLE_RTL, true);
+    expect(storyEducation.containerEl_).to.have.attribute('dir', 'rtl');
+  });
+
+  describe('amp-story-education paused state', () => {
+    it('should not update the paused state when hidden', () => {
+      storeService.dispatch(Action.TOGGLE_PAUSED, false);
+      storyEducation.buildCallback();
+
+      expect(storeService.get(StateProperty.PAUSED_STATE)).to.be.false;
+    });
+
+    it('should pause the story when visible', () => {
+      storeService.dispatch(Action.TOGGLE_PAUSED, false);
+      storyEducation.buildCallback();
+
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_TAP);
+
+      expect(storeService.get(StateProperty.PAUSED_STATE)).to.be.true;
+    });
+
+    it('should unpause the story once the education is dismissed', () => {
+      storeService.dispatch(Action.TOGGLE_PAUSED, false);
+      storyEducation.buildCallback();
+
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_SWIPE);
+      const clickEvent = new MouseEvent('click', {clientX: 100, clientY: 100});
+      storyEducation.containerEl_.dispatchEvent(clickEvent);
+
+      expect(storeService.get(StateProperty.PAUSED_STATE)).to.be.false;
+    });
+
+    it('should not unpause a story that was already paused', () => {
+      storeService.dispatch(Action.TOGGLE_PAUSED, true);
+      storyEducation.buildCallback();
+
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_SWIPE);
+      const clickEvent = new MouseEvent('click', {clientX: 100, clientY: 100});
+      storyEducation.containerEl_.dispatchEvent(clickEvent);
+
+      expect(storeService.get(StateProperty.PAUSED_STATE)).to.be.true;
+    });
+  });
+
+  describe('amp-story-education navigation', () => {
+    it('should render the first navigation education step', () => {
+      storyEducation.buildCallback();
+
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_TAP);
+      const navigationTapEl = storyEducation.containerEl_.querySelector(
+        '[step="tap"]'
+      );
+
+      expect(navigationTapEl).to.exist;
+    });
+
+    it('should render the second navigation education step', () => {
+      storyEducation.buildCallback();
+
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_SWIPE);
+      const navigationSwipeEl = storyEducation.containerEl_.querySelector(
+        '[step="swipe"]'
+      );
+
+      expect(navigationSwipeEl).to.exist;
+    });
+
+    it('should navigate to the next step on tap', () => {
+      storyEducation.buildCallback();
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_TAP);
+
+      const clickEvent = new MouseEvent('click', {clientX: 100, clientY: 100});
+      storyEducation.containerEl_.dispatchEvent(clickEvent);
+
+      const navigationSwipeEl = storyEducation.containerEl_.querySelector(
+        '[step="swipe"]'
+      );
+      expect(navigationSwipeEl).to.exist;
+    });
+
+    it('should hide the education on last step tap', () => {
+      storyEducation.buildCallback();
+      // TODO(gmajoulet): remove private method call when viewer messaging is
+      // introduced.
+      storyEducation.setState_(State.NAVIGATION_SWIPE);
+
+      const clickEvent = new MouseEvent('click', {clientX: 100, clientY: 100});
+      storyEducation.containerEl_.dispatchEvent(clickEvent);
+
+      expect(storyEducation.containerEl_).to.have.attribute('hidden');
+    });
   });
 });
