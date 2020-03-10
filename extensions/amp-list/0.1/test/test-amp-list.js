649fe6e3b88774a8caeddf5e3d06c08310a2f7a9
Jake Fried <samouri@users.noreply.github.com>
2020-03-10 16:17:49 -0400

    remove feature flag

--- extensions/amp-list/0.1/test/test-amp-list.js
+++ extensions/amp-list/0.1/test/test-amp-list.js
@@ -20,10 +20,6 @@ import {AmpEvents} from '../../../../src/amp-events';
 import {AmpList} from '../amp-list';
 import {Deferred} from '../../../../src/utils/promise';
 import {Services} from '../../../../src/services';
-import {
-  resetExperimentTogglesForTesting,
-  toggleExperiment,
-} from '../../../../src/experiments';
 
 describes.repeated(
   'amp-list',
@@ -809,9 +805,7 @@ describes.repeated(
               );
             });
 
-            it('"amp-state:" uri should skip rendering and emit an error', () => {
-              toggleExperiment(win, 'amp-list-init-from-state', true);
-
+            it('"amp-state:" uri should skip rendering and emit an error', () => { 
               const ampStateEl = doc.createElement('amp-state');
               ampStateEl.setAttribute('id', 'okapis');
               const ampStateJson = doc.createElement('script');
@@ -1230,24 +1224,14 @@ describes.repeated(
           });
 
           describe('Using amp-state: protocol', () => {
-            const experimentName = 'amp-list-init-from-state';
-
             beforeEach(() => {
-              resetExperimentTogglesForTesting(win);
               element = createAmpListElement();
               element.setAttribute('src', 'amp-state:okapis');
               element.toggleLoading = () => {};
               list = createAmpList(element);
             });
 
-            it('should throw an error if used without the experiment enabled', async () => {
-              const errorMsg = /Invalid value: amp-state:okapis/;
-              expectAsyncConsoleError(errorMsg);
-              expect(list.layoutCallback()).to.eventually.throw(errorMsg);
-            });
-
             it('should throw error if there is no associated amp-state el', async () => {
-              toggleExperiment(win, experimentName, true);
               bind.getStateAsync = () => Promise.reject();
 
               const errorMsg = /element with id 'okapis' was not found/;
@@ -1256,7 +1240,6 @@ describes.repeated(
             });
 
             it('should log an error if amp-bind was not included', async () => {
-              toggleExperiment(win, experimentName, true);
               Services.bindForDocOrNull.returns(Promise.resolve(null));
 
               const ampStateEl = doc.createElement('amp-state');
@@ -1272,7 +1255,6 @@ describes.repeated(
             });
 
             it('should render a list using local data', async () => {
-              toggleExperiment(win, experimentName, true);
               bind.getStateAsync = () => Promise.resolve({items: [1, 2, 3]});
 
               const ampStateEl = doc.createElement('amp-state');
@@ -1292,7 +1274,6 @@ describes.repeated(
             });
 
             it('should render a list using async data', async () => {
-              toggleExperiment(win, experimentName, true);
               const {resolve, promise} = new Deferred();
               bind.getStateAsync = () => promise;
 
