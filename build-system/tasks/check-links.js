ampproject/amphtml

commit 41eb482d1a9d3bd6ca283769f8877ff149ed56c1

🐛 Bug fix: check links test (#26739)

build-system/tasks/check-links.js
@@ -27,6 +27,7 @@ const {linkCheckGlobs} = require('../test-configs/config');
 const {maybeUpdatePackages} = require('./update-packages');
 
 const LARGE_REFACTOR_THRESHOLD = 20;
+const GITHUB_BASE_PATH = 'https://github.com/ampproject/amphtml/blob/master/';
 
 let filesIntroducedByPr;
 
@@ -135,8 +136,14 @@ function checkLinksInFile(file) {
       let containsDeadLinks = false;
       for (const {link, status, statusCode} of results) {
         // Skip links to files that were introduced by the PR.
-        if (isLinkToFileIntroducedByPR(link)) {
-          continue;
+        if (isLinkToFileIntroducedByPR(link) && status == 'dead') {
+          // Log links with the correct github base as alive, otherwise flag deadlinks.
+          const isValid = filesIntroducedByPr.some(file => {
+            return link === GITHUB_BASE_PATH + file;
+          });
+          if (isValid) {
+            status = 'alive';
+          }
         }
         switch (status) {
           case 'alive':

commit 4cc6f775dfc382e6922d88a52aa278dff2e00690

🐛 [check-links] Don't reassign const
This breaks due to reassigning `const`, introduced in #26739

build-system/tasks/check-links.js
@@ -134,7 +134,9 @@ function checkLinksInFile(file) {
         return;
       }
       let containsDeadLinks = false;
-      for (const {link, status, statusCode} of results) {
+      for (const result of results) {
+        const {link, statusCode} = result;
+        let {status} = result;
         // Skip links to files that were introduced by the PR.
         if (isLinkToFileIntroducedByPR(link) && status == 'dead') {
           // Log links with the correct github base as alive, otherwise flag deadlinks.