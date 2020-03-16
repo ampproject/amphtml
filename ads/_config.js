74935b0f1e886e058c90d14808e3d6769fb8055a
Vidyome <35255015+vidyome@users.noreply.github.com>
2020-03-16 11:37:29 +0300

    ✨🚀Widget Optimized [TE Medya] (#27122)
    * Add files via upload
    
    * Add files via upload
    
    * Update temedya.js
    
    * Update integration.js
    
    * Update _config.js
    
    * Update temedya.js
    
    * Update ads.amp.html
    
    * Update _config.js
    
    * Update amp-ad.md
    
    * Update temedya.md
    
    * Update _config.js
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update _config.js
    
    * Update _config.js
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.js
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.md
    
    * Update temedya.js
    
    * Update temedya.js
    
    * Update temedya.md
    
    * Update temedya.js
    
    * Update _config.js
    
    * Update ads.amp.html
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.md
    
    * Update temedya.md

--- ads/_config.js
+++ ads/_config.js
@@ -1050,7 +1050,7 @@ const adConfig = jsonConfiguration({
 
   'temedya': {
     prefetch: [
-      'https://vidyome-com.cdn.vidyome.com/vidyome/builds/temedya-amp.js',
+      'https://widget.cdn.vidyome.com/builds/loader-amp.js',
       'https://vidyome-com.cdn.vidyome.com/vidyome/builds/widgets.js',
     ],
     renderStartImplemented: true,
