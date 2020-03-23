af06dc92db2cd2f3d53b00ac343d8f4235b07d5e
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2020-03-23 09:04:07 -0400

    ✨Initial amp-social-share Bento component (#26543)
    * Initial social share v2
    
    * Clean up manual example
    
    * Replace CSS approach with AmpContext approach
    
    * General cleanup
    
    * Pass in props['children']
    
    * Rename and alphabetize props
    
    * Remove handleClick
    
    * Allow multiple renders
    
    * Replace querySelectorAll with querySelector
    
    * Quote properties in return
    
    * Isolate AMP-only services
    
    * Add comments
    
    * Update example page
    
    * TAG as first param to user().warn()
    
    * Fix types
    
    * Restore CSS file to be duplicate from v1
    
    * Unreigster built CSS from v2
    
    * Use SVGs as JSX
    
    * amp-social-share takes no children
    
    * Import and pass props to SVG
    
    * Import styles
    
    * Remove unused imports
    
    * Styles are objects
    
    * Name default values
    
    * Remove CSS file
    
    * Same latestVersion
    
    * 3p dep check allowlist
    
    * Remove unused properties
    
    * Quote properties
    
    * Justin comments
    
    * Check layout=responsive in amp-social-share
    
    * Wrap init return in dict
    
    * Restore old example page indentations
    
    * Force transform social-share-svgs
    
    * Fix type
    
    * Clear indents
    
    * Make context_ protected (Is this OK?)
    
    * Rename 3p glob variable and add comment for it
    
    * Quoted PreactBaseElement properties
    
    * Use `micromatch` to check file against 3p glob
    
    * Move out module fns
    
    * s/ownerDocument/document
    
    * Dima comments
    
    * Remove assertions and add PropTypes
    
    * Quote properties and checkPropTypes
    
    * Fix PropType import
    
    * Do not use context for url
    
    * Remove amp-social-share- classname
    
    * useDefaultDimensions prop
    
    * Remove PropTypes
    
    * Default sizing logic
    
    * Define and use mutateProps
    
    * Size div[role=button]
    
    * Default return svg with props
    
    * Check defaultProps_ not null/undefined
    
    * Rename getHrefAndTarget and update comments
    
    * No return
    
    * Simplify responsive logic

---
+++ extensions/amp-social-share/0.2/social-share.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as CSS from './social-share.css';
+import * as Preact from '../../../src/preact';
+import {Keys} from '../../../src/utils/key-codes';
+import {SocialShareIcon} from '../../../third_party/optimized-svg-icons/social-share-svgs';
+import {openWindowDialog} from '../../../src/dom';
+import {parseQueryString} from '../../../src/url';
+import {startsWith} from '../../../src/string';
+import {useResourcesNotify} from '../../../src/preact/utils';
+
+const DEFAULT_WIDTH = 60;
+const DEFAULT_HEIGHT = 44;
+
+/**
+ * @param {!JsonObject} props
+ * @return {PreactDef.Renderable}
+ */
+export function SocialShare(props) {
+  /**
+   * Handle key presses on the element.
+   * @param {!Event} event
+   * @private
+   */
+  function handleKeyPress(event) {
+    const {key} = event;
+    if (key == Keys.SPACE || key == Keys.ENTER) {
+      event.preventDefault();
+      handleActivation();
+    }
+  }
+
+  /** @private */
+  function handleActivation() {
+    const href = props['href'];
+    const target = props['target'] || '_blank';
+    if (!href) {
+      throw new Error('Clicked before href is set.');
+    }
+    if (startsWith(href, 'navigator-share:')) {
+      if (navigator.share !== undefined) {
+        throw new Error('navigator.share disappeared.');
+      }
+      // navigator.share() fails 'gulp check-types' validation on Travis
+      navigator['share'](parseQueryString(href.substr(href.indexOf('?'))));
+    } else {
+      const windowFeatures = 'resizable,scrollbars,width=640,height=480';
+      openWindowDialog(window, href, target, windowFeatures);
+    }
+  }
+
+  const styleType = `SOCIAL_SHARE_${props['type'].toUpperCase()}`;
+  const baseStyle = CSS.BASE_STYLE;
+  const backgroundStyle = CSS[styleType];
+  const size = {
+    width: props['width'] || DEFAULT_WIDTH,
+    height: props['height'] || DEFAULT_HEIGHT,
+  };
+  useResourcesNotify();
+  return (
+    <div
+      role="button"
+      tabindex={props['tabIndex'] || '0'}
+      onKeyDown={handleKeyPress}
+      onClick={handleActivation}
+      style={{...size, ...props['style']}}
+      {...props}
+    >
+      <SocialShareIcon
+        style={{...backgroundStyle, ...baseStyle, ...size}}
+        styleType={styleType}
+      />
+    </div>
+  );
+}
