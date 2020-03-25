08f5829ce82a1a6f52b843889e5a3a3a8223da20
Enrique Marroquin <artezan@google.com>
2020-03-25 10:39:47 -0400

    fix test

--- extensions/amp-story/1.0/test/test-full-bleed-animations.js
+++ extensions/amp-story/1.0/test/test-full-bleed-animations.js
@@ -45,6 +45,9 @@ describes.realWin(
     beforeEach(() => {
       win = env.win;
 
+      // Required by the bookend code.
+      win.document.title = 'Story';
+
       env.sandbox.stub(win.history, 'replaceState');
 
       const viewer = Services.viewerForDoc(env.ampdoc);
