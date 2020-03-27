b03813e3b05a3409faac9d46b6944aadcc3a25b4
Gabriel Majoulet <gmajoulet@gmail.com>
2020-03-27 16:16:16 -0400

    Auto advance from audio on story cover page. (#27366)
    * Auto advance from audio on story cover page.
    
    * Storing the registerAllMedia promise.

--- extensions/amp-story/1.0/test/test-amp-story-page.js
+++ extensions/amp-story/1.0/test/test-amp-story-page.js
@@ -107,10 +107,14 @@ describes.realWin('amp-story-page', {amp: true}, env => {
   });
 
   it('should start the advancement when state becomes active', async () => {
+    page.registerAllMediaPromise_ = Promise.resolve();
     page.buildCallback();
     const advancementStartStub = env.sandbox.stub(page.advancement_, 'start');
     await page.layoutCallback();
     page.setState(PageState.PLAYING);
+
+    // Microtask tick
+    await Promise.resolve();
 
     expect(advancementStartStub).to.have.been.calledOnce;
   });
