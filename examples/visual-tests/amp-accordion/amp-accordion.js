716459cdf2e2b64531f0760a9e680e95377f0485
Alan Orozco <alanorozco@users.noreply.github.com>
2020-03-30 11:55:15 -0700

    ✅ Unflake amp-accordion visual test (#27461)
    We only care about the final state, not the transition. The screenshot could otherwise be taken halfway through (https://percy.io/ampproject/amphtml/builds/1828636).
    
    Fixes #22161

--- examples/visual-tests/amp-accordion/amp-accordion.js
+++ examples/visual-tests/amp-accordion/amp-accordion.js
@@ -15,12 +15,15 @@
  */
 'use strict';
 
-const {verifySelectorsVisible} = require('../../../build-system/tasks/visual-diff/helpers');
+const {
+  verifySelectorsVisible,
+} = require('../../../build-system/tasks/visual-diff/helpers');
 
 module.exports = {
   'click section one': async (page, name) => {
     await page.tap('amp-accordion section:first-child');
-    await verifySelectorsVisible(page, name,
-        ['amp-accordion section:first-child[expanded] .i-amphtml-accordion-content[style=""]']);
+    await verifySelectorsVisible(page, name, [
+      'amp-accordion section:first-child[expanded] .i-amphtml-accordion-content',
+    ]);
   },
- };
+};
