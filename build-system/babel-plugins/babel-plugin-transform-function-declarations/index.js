932fffe3c2e79443f26f7ae780aabbdbccdc5b4e
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-03-31 12:19:36 -0700

    🏗 Refactor babel-plugin-transform-function-declarations (#27438)
    * Refactor babel plugin per request
    * allow transformation of inner functions inside exported members
    * Prevent full file traversal for newed bailed out condition
    * Insert inside top of enclosing block when parent is not a program
    * Simplify logic for finding place to insert
    * Only allow functions referred to as CallExpressions to be converted
    * Move from applying cloned comments to t.inherits
    * Update build-system/babel-plugins/babel-plugin-transform-function-declarations/index.js

--- build-system/babel-plugins/babel-plugin-transform-function-declarations/index.js
+++ build-system/babel-plugins/babel-plugin-transform-function-declarations/index.js
@@ -14,145 +14,120 @@
  * limitations under the License.
  */
 
-const BAIL_OUT_CONDITIONS = {
-  // If this isn't a FunctionDeclaration, bail out on modification.
-  isNotFunction: (t, path) => !t.isFunctionDeclaration(path.node),
-
-  // If this FunctionDeclaration is a Generator, bail out on modification.
-  isGenerator: (t, path) => path.node.generator,
-
-  // If this FunctionDeclaration doesn't have a single return statement, bail out on modification
-  isNotSingleReturnStatment: (t, path) => {
-    if (!path.get('body').isBlockStatement()) {
-      return false;
-    }
-    const body = path.get('body.body.0');
-    return !body || !body.isReturnStatement();
-  },
-
-  // Since we don't know if exported members are 'new'd, bail out on modification.
-  isExported: (t, path) =>
-    path.findParent(
-      (path) =>
-        path.isExportNamedDeclaration() || path.isExportDefaultDeclaration()
-    ),
-
-  // If the function contains usage of `arguments`, bail out on modification.
-  containsArgumentsUsage: (t, path) => {
-    let containsArgumentsUsage = false;
-    path.traverse({
-      Identifier(path) {
-        if (t.isIdentifier(path.node, {name: 'arguments'})) {
-          containsArgumentsUsage = true;
-          path.stop();
-        }
-      },
-    });
-    return containsArgumentsUsage;
-  },
-
-  // If the function contains a ThisExpression, converting from a FunctionDeclaration to a
-  // VariableDeclaration => VariableDeclarator => ArrowFunctionExpression isn't necessarily valid.
-  // Bail out on modification.
-  containsThisExpression: (t, path) => {
-    let containsThis = false;
-    path.traverse({
-      ThisExpression() {
-        containsThis = true;
-        path.stop();
-      },
-    });
-    return containsThis;
-  },
-
-  // If the FunctionDeclaration identifier is newed in the scope of this program, bail out on modification.
-  isNewedInProgramScope: (t, path) => {
-    const {name} = path.get('id').node;
-    let isNewed = false;
-    path
-      .findParent((path) => path.isProgram())
-      .traverse({
-        NewExpression(path) {
-          if (t.isIdentifier(path.node.callee, {name})) {
-            isNewed = true;
-            path.stop();
-          }
-        },
-      });
-    return isNewed;
-  },
-};
-
-function createVariableDeclaration(t, path) {
-  const {params, body, async, id} = t.cloneNode(path.node);
-  const arrowFunction = t.arrowFunctionExpression(
-    params,
-    body.body[0].argument,
-    async
-  );
-  const declarations = t.variableDeclarator(
-    t.identifier(id.name),
-    arrowFunction
-  );
-
-  // Making an existing FunctionDeclaration a let VariableDeclaration is safe.
-  // The name was already reserved for the FunctionDeclaration.
-  return t.variableDeclaration('let', [declarations]);
-}
+const DEBUG = false;
 
 // Attempt to convert simple single ReturnStatement FunctionDeclarations to ArrowFunctionExpressions.
 // See BAIL_OUT_CONDITIONS for reasons why FunctionDeclarations would not be modified.
 module.exports = function ({types: t}) {
-  const DEBUG = false;
-  const REPORT = false;
+  const BAIL_OUT_CONDITIONS = {
+    // If this isn't a FunctionDeclaration, bail out on modification.
+    isNotFunction(path) {
+      return !t.isFunctionDeclaration(path.node);
+    },
+
+    // If this FunctionDeclaration is a Generator, bail out on modification.
+    isGenerator(path) {
+      return path.node.generator;
+    },
+
+    // If this FunctionDeclaration doesn't have a single return statement, bail out on modification
+    isNotSingleReturnStatment(path) {
+      if (!path.get('body').isBlockStatement()) {
+        return false;
+      }
+      const body = path.get('body.body.0');
+      return !body || !body.isReturnStatement();
+    },
+
+    // If the function contains usage of `arguments`, bail out on modification.
+    containsArgumentsUsage(path) {
+      let containsArgumentsUsage = false;
+      path.traverse({
+        Identifier(path) {
+          if (t.isIdentifier(path.node, {name: 'arguments'})) {
+            containsArgumentsUsage = true;
+            path.stop();
+          }
+        },
+      });
+      return containsArgumentsUsage;
+    },
+
+    // If the function contains a ThisExpression, converting from a FunctionDeclaration to a
+    // VariableDeclaration => VariableDeclarator => ArrowFunctionExpression isn't necessarily valid.
+    // Bail out on modification.
+    containsThisExpression(path) {
+      let containsThis = false;
+      path.traverse({
+        ThisExpression() {
+          containsThis = true;
+          path.stop();
+        },
+      });
+      return containsThis;
+    },
+
+    // If the FunctionDeclaration identifier is used a manner besides a CallExpression, bail.
+    referencesAreOnlyCallExpressions(path) {
+      const {name} = path.get('id').node;
+      const binding = path.scope.getBinding(name);
+      return !binding.referencePaths.every((p) =>
+        p.parentPath.isCallExpression({callee: p.node})
+      );
+    },
+  };
+
+  // Making an existing FunctionDeclaration a let VariableDeclaration is safe.
+  // The name was already reserved for the FunctionDeclaration.
+  function createVariableDeclaration(path) {
+    const {params, body, async, id} = t.cloneNode(path.node);
+    const arrowFunction = t.arrowFunctionExpression(
+      params,
+      body.body[0].argument,
+      async
+    );
+    const declarations = t.variableDeclarator(
+      t.identifier(id.name),
+      arrowFunction
+    );
+    const declaration = t.variableDeclaration('let', [declarations]);
+
+    t.inherits(declaration, path.node);
+    return declaration;
+  }
 
   return {
     name: 'safe-arrows',
-    pre() {
-      this.bailoutCount = {...BAIL_OUT_CONDITIONS};
-      Object.keys(this.bailoutCount).forEach(
-        (key) => (this.bailoutCount[key] = [])
-      );
-      this.successCount = [];
-    },
     visitor: {
       FunctionDeclaration(path) {
-        const name = (path.get('id') && path.get('id').node.name) || '';
         for (const [id, method] of Object.entries(BAIL_OUT_CONDITIONS)) {
-          if (method(t, path)) {
-            this.bailoutCount[id].push(name);
+          if (method(path)) {
             if (DEBUG) {
+              const pathId = path.get('id');
+              const message = pathId
+                ? `Bail on function ${pathId.node.name}: Reason ${id}.`
+                : `Bail on item: Reason ${id}`;
               console /*OK*/
-                .log(
-                  `Bail on ${
-                    name !== '' ? `function ${name}:` : 'item:'
-                  } Reason ${id}.`
-                );
+                .log(message);
             }
             return;
           }
         }
-        this.successCount.push(name);
+
         if (DEBUG) {
+          const {name} = path.node.id;
           console /*OK*/
-            .log(`Success for function ${path.get('id').node.name}.`);
+            .log(`Success for function ${name}.`);
         }
-        path.replaceWith(createVariableDeclaration(t, path));
-      },
-    },
-    post() {
-      if (REPORT) {
-        console /*OK*/
-          .log(`Success Count: ${this.successCount.length}`);
-        console /*OK*/
-          .log('Bail Out Reason Counts');
-        Object.keys(this.bailoutCount).forEach((name) =>
-          console /*OK*/
-            .log(`${name}: ${this.bailoutCount[name].length}`)
+
+        const usableBlock = path.findParent(
+          (path) => path.isBlockStatement() || path.isProgram()
         );
-        console /*OK*/
-          .log('\n');
-      }
+        if (usableBlock) {
+          usableBlock.unshiftContainer('body', createVariableDeclaration(path));
+          path.remove();
+        }
+      },
     },
   };
 };
