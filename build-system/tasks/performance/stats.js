a03fe40843e2e89c5a153f12188cfeeb2f76285f
esth <esth@google.com>
2020-04-02 10:23:53 -0400

    account for 1 run

--- build-system/tasks/performance/stats.js
+++ build-system/tasks/performance/stats.js
@@ -76,7 +76,8 @@ const q1 = array => {
 const q3 = array => {
   sort(array);
   const mid = Math.ceil(array.length / 2);
-  const half = array.slice(mid);
+  // account for arrays of length 1
+  const half = mid < array.length ? array.slice(mid) : array;
   return median(half);
 };
 
