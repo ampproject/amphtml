e79b07fc9af4e67529bc6b66a85bf1ea69795a0a
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-03 13:44:46 -0700

    🚀 Function expression rewriting during post closure transforms (#27512)
    * Refactor babel plugin per request
    * allow transformation of inner functions inside exported members
    * Prevent full file traversal for newed bailed out condition
    * Insert inside top of enclosing block when parent is not a program
    * disable debugging
    * Simplify logic for finding place to insert
    * Only allow functions referred to as CallExpressions to be converted
    * Move from applying cloned comments to t.inherits
    * New Prettier
    * function expression support
    * Exemption list for CallExpression names
    * typo
    * Prettier formatting error in unrelated file

--- build-system/babel-plugins/babel-plugin-transform-function-declarations/index.js
+++ build-system/babel-plugins/babel-plugin-transform-function-declarations/index.js
@@ -14,119 +14,198 @@
  * limitations under the License.
  */
 
-const DEBUG = false;
-
 // Attempt to convert simple single ReturnStatement FunctionDeclarations to ArrowFunctionExpressions.
 // See BAIL_OUT_CONDITIONS for reasons why FunctionDeclarations would not be modified.
 module.exports = function ({types: t}) {
-  const BAIL_OUT_CONDITIONS = {
-    // If this isn't a FunctionDeclaration, bail out on modification.
-    isNotFunction(path) {
-      return !t.isFunctionDeclaration(path.node);
-    },
+  /**
+   * This transform is targetted toward these types only.
+   * @param {*} path
+   * @return {boolean}
+   */
+  function isNotFunction(path) {
+    return (
+      !t.isFunctionDeclaration(path.node) && !t.isFunctionExpression(path.node)
+    );
+  }
 
-    // If this FunctionDeclaration is a Generator, bail out on modification.
-    isGenerator(path) {
-      return path.node.generator;
-    },
+  /**
+   * This transform cannot safely convert generator functions.
+   * @param {*} path
+   * @return {boolean}
+   */
+  function isGenerator(path) {
+    return path.node.generator;
+  }
 
-    // If this FunctionDeclaration doesn't have a single return statement, bail out on modification
-    isNotSingleReturnStatment(path) {
-      if (!path.get('body').isBlockStatement()) {
-        return false;
-      }
-      const body = path.get('body.body.0');
-      return !body || !body.isReturnStatement();
-    },
+  /**
+   * Only transform a body with a single return statement.
+   * @param {*} path
+   * @return {boolean}
+   */
+  function isNotSingleReturnStatement(path) {
+    if (!path.get('body').isBlockStatement()) {
+      return false;
+    }
+    const body = path.get('body.body.0');
+    return !body || !body.isReturnStatement();
+  }
 
-    // If the function contains usage of `arguments`, bail out on modification.
-    containsArgumentsUsage(path) {
-      let containsArgumentsUsage = false;
-      path.traverse({
-        Identifier(path) {
-          if (t.isIdentifier(path.node, {name: 'arguments'})) {
-            containsArgumentsUsage = true;
-            path.stop();
-          }
-        },
-      });
-      return containsArgumentsUsage;
-    },
-
-    // If the function contains a ThisExpression, converting from a FunctionDeclaration to a
-    // VariableDeclaration => VariableDeclarator => ArrowFunctionExpression isn't necessarily valid.
-    // Bail out on modification.
-    containsThisExpression(path) {
-      let containsThis = false;
-      path.traverse({
-        ThisExpression() {
-          containsThis = true;
+  /**
+   * Only convert functions that do not contains usage of `arguments`.
+   * @param {*} path
+   * @return {boolean}
+   */
+  function containsArgumentsUsage(path) {
+    let containsArgumentsUsage = false;
+    path.traverse({
+      Identifier(path) {
+        if (t.isIdentifier(path.node, {name: 'arguments'})) {
+          containsArgumentsUsage = true;
           path.stop();
-        },
-      });
-      return containsThis;
-    },
+        }
+      },
+    });
+    return containsArgumentsUsage;
+  }
 
-    // If the FunctionDeclaration identifier is used a manner besides a CallExpression, bail.
-    referencesAreOnlyCallExpressions(path) {
-      const {name} = path.get('id').node;
-      const binding = path.scope.getBinding(name);
-      return !binding.referencePaths.every((p) =>
-        p.parentPath.isCallExpression({callee: p.node})
-      );
-    },
-  };
+  /**
+   * If the FunctionDeclaration contains a ThisExpression, converting from a FunctionDeclaration to a
+   * VariableDeclaration => VariableDeclarator => ArrowFunctionExpression isn't necessarily valid.
+   * @param {*} path
+   * @return {boolean}
+   */
+  function containsThisExpression(path) {
+    let containsThis = false;
+    path.traverse({
+      ThisExpression() {
+        containsThis = true;
+        path.stop();
+      },
+    });
+    return containsThis;
+  }
 
-  // Making an existing FunctionDeclaration a let VariableDeclaration is safe.
-  // The name was already reserved for the FunctionDeclaration.
+  /**
+   * If the FunctionDeclaration identifier is used a manner besides a CallExpression, bail.
+   * @param {*} path
+   * @param {string} name
+   * @return {boolean}
+   */
+  function referencesAreOnlyCallExpressions(path, name) {
+    const binding = path.scope.getBinding(name);
+    return !binding.referencePaths.every((p) =>
+      p.parentPath.isCallExpression({callee: p.node})
+    );
+  }
+
+  function createArrowFunctionExpression(node) {
+    const {params, body, async} = t.cloneNode(node);
+    return t.arrowFunctionExpression(params, body.body[0].argument, async);
+  }
+
+  function createVariableDeclarator(path, name) {
+    const arrowFunction = createArrowFunctionExpression(path.node);
+    return t.variableDeclarator(t.identifier(name), arrowFunction);
+  }
+
   function createVariableDeclaration(path) {
-    const {params, body, async, id} = t.cloneNode(path.node);
-    const arrowFunction = t.arrowFunctionExpression(
-      params,
-      body.body[0].argument,
-      async
-    );
-    const declarations = t.variableDeclarator(
-      t.identifier(id.name),
-      arrowFunction
-    );
-    const declaration = t.variableDeclaration('let', [declarations]);
+    const declarator = createVariableDeclarator(path, path.node.id.name);
+    const declaration = t.variableDeclaration('let', [declarator]);
 
     t.inherits(declaration, path.node);
     return declaration;
   }
 
+  const DECLARATION_BAIL_OUT_CONDITIONS = [
+    isNotFunction,
+    isGenerator,
+    isNotSingleReturnStatement,
+    containsArgumentsUsage,
+    containsThisExpression,
+    (path) => referencesAreOnlyCallExpressions(path, path.get('id').node.name),
+  ];
+
+  // If CallExpression names should be exhempt from arrow conversion, denote them here.
+  const EXEMPTED_EXPRESSION_NAME_REGEXS = [/registerService/];
+
+  const EXPRESSION_BAIL_OUT_CONDITIONS = [
+    isNotFunction,
+    isGenerator,
+    isNotSingleReturnStatement,
+    containsArgumentsUsage,
+    containsThisExpression,
+    (path) => {
+      const callExpression = path.findParent((p) => p.isCallExpression());
+      if (callExpression) {
+        const {name} = (callExpression.node && callExpression.node.callee) || {
+          name: null,
+        };
+        return EXEMPTED_EXPRESSION_NAME_REGEXS.every((regexp) =>
+          regexp.test(name)
+        );
+      }
+
+      const declarator = path.findParent((p) => p.isVariableDeclarator());
+      if (declarator) {
+        return referencesAreOnlyCallExpressions(path, declarator.node.id.name);
+      }
+
+      return true;
+    },
+  ];
+
   return {
     name: 'safe-arrows',
     visitor: {
-      FunctionDeclaration(path) {
-        for (const [id, method] of Object.entries(BAIL_OUT_CONDITIONS)) {
-          if (method(path)) {
-            if (DEBUG) {
-              const pathId = path.get('id');
-              const message = pathId
-                ? `Bail on function ${pathId.node.name}: Reason ${id}.`
-                : `Bail on item: Reason ${id}`;
-              console /*OK*/
-                .log(message);
+      FunctionExpression(path) {
+        const insideCallExpression = path.findParent((p) =>
+          p.isCallExpression()
+        );
+        if (insideCallExpression) {
+          for (const method of EXPRESSION_BAIL_OUT_CONDITIONS) {
+            if (method(path)) {
+              return;
             }
+          }
+          path.replaceWith(createArrowFunctionExpression(path.node));
+        } else {
+          const declarator = path.findParent((p) => p.isVariableDeclarator());
+          if (!declarator) {
+            return;
+          }
+
+          for (const method of EXPRESSION_BAIL_OUT_CONDITIONS) {
+            if (method(path)) {
+              return;
+            }
+          }
+          const name =
+            (declarator.node &&
+              declarator.node.id &&
+              declarator.node.id.name) ||
+            null;
+          declarator.replaceWith(createVariableDeclarator(path, name));
+        }
+      },
+      FunctionDeclaration(path) {
+        const usableBlock = path.findParent(
+          (path) => path.isBlockStatement() || path.isProgram()
+        );
+
+        if (!usableBlock) {
+          return;
+        }
+
+        for (const method of DECLARATION_BAIL_OUT_CONDITIONS) {
+          if (method(path)) {
             return;
           }
         }
 
-        if (DEBUG) {
-          const {name} = path.node.id;
-          console /*OK*/
-            .log(`Success for function ${name}.`);
-        }
-
-        const usableBlock = path.findParent(
-          (path) => path.isBlockStatement() || path.isProgram()
-        );
-        if (usableBlock) {
-          usableBlock.unshiftContainer('body', createVariableDeclaration(path));
-          path.remove();
-        }
+        // Making an existing FunctionDeclaration a let VariableDeclaration is safe.
+        // The name was already reserved for the FunctionDeclaration.
+        usableBlock.unshiftContainer('body', createVariableDeclaration(path));
+        path.remove();
       },
     },
   };
