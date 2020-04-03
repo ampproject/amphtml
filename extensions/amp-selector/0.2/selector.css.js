33103b78b5dbf7e9f9fa3455a6b59239a1e7abd5
Caroline Liu <carolineliu@google.com>
2020-04-03 15:04:14 -0400

    Comment about styles

--- extensions/amp-selector/0.2/selector.css.js
+++ extensions/amp-selector/0.2/selector.css.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// TODO(wg-bento): These are non-overridable non-essential styles.
+// See https://github.com/ampproject/wg-bento/issues/12.
 
 export const OPTION = {
   'cursor': 'pointer',
