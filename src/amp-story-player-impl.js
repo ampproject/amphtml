7d71df51902e30f105623ebef88ae0e88257ecb0
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2020-04-06 18:36:34 -0400

    ✨[amp-story-player] Upgrading amp-story-player to CE (#27320)
    * upgrade to CEv1, move ctor to init, change from onload to load listener
    
    * add viewpoer meta tag to sample
    
    * failing test
    
    * fix tests
    
    * expand comment on tests
    
    * call connectedCallback() instead of init
    
    * fix test
    
    * change to suffix underscore for convention
    
    * rely on connectedCallback rather than calling build + layout manaully
    
    * prettier parens
    
    * Revert "change to suffix underscore for convention"
    
    This reverts commit 3118d00c06085d325f1c40d5a3d462e3f24e70e9.
    
    * call buildcallback & layoutWHenVisible from connectedCallback, making each player responsible to call them individually.

--- src/amp-story-player-impl.js
+++ src/amp-story-player-impl.js
@@ -14,18 +14,19 @@
  * limitations under the License.
  */
 
+import {IframePool} from './amp-story-player-iframe-pool';
 import {Messaging} from '@ampproject/viewer-messaging';
+import {VisibilityState} from './visibility-state';
 import {
   addParamsToUrl,
   getFragment,
   parseUrlWithA,
   removeFragment,
 } from './url';
+import {applySandbox} from './3p-frame';
 import {dict, map} from './utils/object';
 // Source for this constant is css/amp-story-player-iframe.css
-import {IframePool} from './amp-story-player-iframe-pool';
-import {VisibilityState} from './visibility-state';
-import {applySandbox} from './3p-frame';
+import {AmpStoryPlayerManager} from './amp-story-player-manager';
 import {cssText} from '../build/amp-story-player-iframe.css';
 import {resetStyles, setStyle, setStyles} from './style';
 import {toArray} from './types';
@@ -66,29 +67,21 @@ export const IFRAME_IDX = '__AMP_IFRAME_IDX__';
  * Note that this is a vanilla JavaScript class and should not depend on AMP
  * services, as v0.js is not expected to be loaded in this context.
  */
-export class AmpStoryPlayer {
+export class AmpStoryPlayer extends HTMLElement {
   /**
-   * @param {!Window} win
-   * @param {!Element} element
-   * @constructor
+   * Player is appended in the document.
    */
-  constructor(win, element) {
-    console./*OK*/ assert(
-      element.childElementCount > 0,
-      'Missing configuration.'
-    );
+  connectedCallback() {
+    console./*OK*/ assert(this.childElementCount > 0, 'Missing configuration.');
 
     /** @private {!Window} */
-    this.win_ = win;
+    this.win_ = self;
 
     /** @private {!Array<!Element>} */
     this.iframes_ = [];
 
-    /** @private {!Element} */
-    this.element_ = element;
-
     /** @private {!Document} */
-    this.doc_ = win.document;
+    this.doc_ = this.win_.document;
 
     /** @private {!Element} */
     this.cachedA_ = this.doc_.createElement('a');
@@ -121,6 +114,10 @@ export class AmpStoryPlayer {
       lastX: 0,
       isSwipeX: null,
     };
+
+    this.buildCallback_();
+    const manager = new AmpStoryPlayerManager(self);
+    manager.layoutWhenVisible(this);
   }
 
   /**
@@ -128,12 +125,12 @@ export class AmpStoryPlayer {
    * @return {!Element}
    */
   getElement() {
-    return this.element_;
+    return this;
   }
 
-  /** @public */
-  buildCallback() {
-    this.stories_ = toArray(this.element_.querySelectorAll('a'));
+  /** @private */
+  buildCallback_() {
+    this.stories_ = toArray(this.querySelectorAll('a'));
 
     this.initializeShadowRoot_();
     this.initializeIframes_();
@@ -158,7 +155,7 @@ export class AmpStoryPlayer {
     this.rootEl_ = this.doc_.createElement('main');
 
     // Create shadow root
-    const shadowRoot = this.element_.attachShadow({mode: 'open'});
+    const shadowRoot = this.attachShadow({mode: 'open'});
 
     // Inject default styles
     const styleEl = this.doc_.createElement('style');
@@ -250,12 +247,12 @@ export class AmpStoryPlayer {
     iframeEl.onload = () => {
       this.rootEl_.classList.remove(LoadStateClass.LOADING);
       this.rootEl_.classList.add(LoadStateClass.LOADED);
-      this.element_.classList.add(LoadStateClass.LOADED);
+      this.classList.add(LoadStateClass.LOADED);
     };
     iframeEl.onerror = () => {
       this.rootEl_.classList.remove(LoadStateClass.LOADING);
       this.rootEl_.classList.add(LoadStateClass.ERROR);
-      this.element_.classList.add(LoadStateClass.ERROR);
+      this.classList.add(LoadStateClass.ERROR);
     };
   }
 
