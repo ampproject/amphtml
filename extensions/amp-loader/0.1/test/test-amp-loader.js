967ebaf8ee6f90dbbe71bdec8f9e4ae7508ac536
Jake Fried <samouri@users.noreply.github.com>
2020-04-07 13:31:03 -0400

    Add test-amp-loader

---
+++ extensions/amp-loader/0.1/test/test-amp-loader.js
@@ -0,0 +1,15 @@
+
+describes.fakeWin(
+  'amp-loader',
+  {
+    amp: {
+      extensions: ['amp-loader'],
+    },
+  },
+  function (env) {
+
+    it('should let me make a test', () => {
+        expect(true).ok;
+    })
+  }
+);
