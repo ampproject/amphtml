c3373c7da09e82610669fe11013622a405b8bd1f
Jake Fried <samouri@users.noreply.github.com>
2020-04-08 17:25:46 -0400

    test-loader lives!

--- test/unit/test-loader.js
+++ test/unit/test-loader.js
@@ -1,39 +1,54 @@
 import * as lolex from 'lolex';
 import {createLoaderElement} from '../../src/loader';
-import {LoaderService} from '../../extensions/amp-loader'
-import {Services} from '../../../../src/services';
+import {Services} from '../../src/services';
 
 describes.fakeWin('Loader', {amp: true}, (env) => {
-    let clock;
-    let ampdoc;
-    let loaderService;
-    let el;
+  let clock;
+  let ampdoc;
+  let loaderService;
+  let el;
+  const loaderLatency = 100;
 
   beforeEach(() => {
     ampdoc = env.ampdoc;
     el = document.createElement('div');
-    clock = lolex.install({now: 50});
+    clock = lolex.install({target: env.win, now: 50});
+
     env.sandbox
-      .stub(Services, 'loader')
-      .returns(new Promise((res) => {
-          loaderService = new LoaderService();
-          setTimeout(() => res(loaderService), 100)
-      })); 
+      .stub(Services, 'extensionsFor')
+      .returns({installExtensionForDoc: () => Promise.resolve()});
+    env.sandbox.stub(Services, 'loaderServiceForDoc').returns(
+      new Promise((res) => {
+        loaderService = {initializeLoader: env.sandbox.spy()};
+        env.win.setTimeout(() => res(loaderService), loaderLatency);
+      })
+    );
   });
 
-  it('By default, the delay in retrieving LoaderService should be the initDelay', () => {
-      env.sandbox.spy(loaderService, 'initializeLoader')
-      createLoaderElement(ampdoc, el, 400, 400); 
-      clock.tick(100);
+  it('By default, the delay in retrieving LoaderService should be the initDelay', async () => {
+    createLoaderElement(ampdoc, el, 400, 400);
+    await clock.tickAsync(loaderLatency);
 
-      expect(loaderService).calledOnceWithMatch(el, env.sandbox.match.any(), 100, 400, 400)
+    expect(loaderService.initializeLoader).calledOnceWith(
+      el,
+      env.sandbox.match.any,
+      100,
+      400,
+      400
+    );
   });
-  
-  it('If specified, startTime should contribute to the initDelay', () => {
-      env.sandbox.spy(loaderService, 'initializeLoader')
-      createLoaderElement(ampdoc, el, 400, 400, 0); 
-      clock.tick(100); 
 
-      expect(loaderService).calledOnceWithMatch(el, env.sandbox.match.any(), 150, 400, 400)
+  it('If specified, startTime should contribute to the initDelay', async () => {
+    // startTime: 0, now: 50, loaderLatency: 100 --> initDelay: 150.
+    createLoaderElement(ampdoc, el, 400, 400, 0);
+    await clock.tickAsync(loaderLatency);
+
+    expect(loaderService.initializeLoader).calledOnceWith(
+      el,
+      env.sandbox.match.any,
+      150,
+      400,
+      400
+    );
   });
 });
