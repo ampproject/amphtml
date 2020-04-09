a3827fffcc5728ff6a1ca2df3e01f8e902e207ac
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-09 13:12:34 -0700

    Current working point

--- build-system/babel-config/pre-closure-config.js
+++ build-system/babel-config/pre-closure-config.js
@@ -20,8 +20,81 @@ const {getDevDependencies} = require('./dev-dependencies');
 const {getReplacePlugin} = require('./replace-plugin');
 const {isTravisBuild} = require('../common/travis');
 
+const isCheckTypes = argv._.includes('check-types');
+const filterImportsPlugin = [
+  'filter-imports',
+  {
+    imports: {
+      // Imports removed for all ESM builds.
+      './polyfills/document-contains': ['installDocContains'],
+      './polyfills/domtokenlist': ['installDOMTokenList'],
+      './polyfills/fetch': ['installFetch'],
+      './polyfills/math-sign': ['installMathSign'],
+      './polyfills/object-assign': ['installObjectAssign'],
+      './polyfills/object-values': ['installObjectValues'],
+      './polyfills/promise': ['installPromise'],
+      './polyfills/array-includes': ['installArrayIncludes'],
+      './ie-media-bug': ['ieMediaCheckAndFix'],
+      '../third_party/css-escape/css-escape': ['cssEscape'],
+      // Imports that are not needed for valid transformed documents.
+      '../build/ampshared.css': ['cssText', 'ampSharedCss'],
+      '../build/ampdoc.css': ['cssText', 'ampDocCss'],
+    },
+  },
+];
+const preClosurePlugins = [
+  './build-system/babel-plugins/babel-plugin-transform-fix-leading-comments',
+  '@babel/plugin-transform-react-constant-elements',
+  [
+    '@babel/plugin-transform-react-jsx',
+    {
+      pragma: 'Preact.createElement',
+      pragmaFrag: 'Preact.Fragment',
+      useSpread: true,
+    },
+  ],
+  argv.esm ? null : ['@babel/plugin-transform-classes', {loose: false}],
+  './build-system/babel-plugins/babel-plugin-transform-inline-configure-component',
+  // TODO(alanorozco): Remove `replaceCallArguments` once serving infra is up.
+  [
+    './build-system/babel-plugins/babel-plugin-transform-log-methods',
+    {replaceCallArguments: false},
+  ],
+  './build-system/babel-plugins/babel-plugin-transform-parenthesize-expression',
+  './build-system/babel-plugins/babel-plugin-is_minified-constant-transformer',
+  './build-system/babel-plugins/babel-plugin-transform-amp-extension-call',
+  './build-system/babel-plugins/babel-plugin-transform-html-template',
+  './build-system/babel-plugins/babel-plugin-transform-version-call',
+  './build-system/babel-plugins/babel-plugin-transform-simple-array-destructure',
+  getReplacePlugin(),
+  argv.single_pass
+    ? './build-system/babel-plugins/babel-plugin-transform-amp-asserts'
+    : null,
+  argv.esm ? filterImportsPlugin : null,
+  argv.esm
+    ? './build-system/babel-plugins/babel-plugin-transform-function-declarations'
+    : null,
+  isCheckTypes
+    ? './build-system/babel-plugins/babel-plugin-transform-simple-object-destructure'
+    : './build-system/babel-plugins/babel-plugin-transform-json-configuration',
+  argv.esm
+    ? [
+        './build-system/babel-plugins/babel-plugin-amp-mode-transformer',
+        {isEsmBuild: !!argv.esm},
+      ]
+    : null,
+  !(argv.fortesting || isCheckTypes)
+    ? './build-system/babel-plugins/babel-plugin-is_dev-constant-transformer'
+    : null,
+  'transform-es2015-modules-commonjs',
+].filter(Boolean);
+
+/**
+ * Gets the preset configuration for Babel before Closure Compiler executes.
+ *
+ * @return {!Object}
+ */
 function getPreClosurePresets() {
-  const modules = argv.esm ? false : 'commonjs';
   const targets = argv.esm
     ? {'esmodules': true}
     : isTravisBuild()
@@ -29,11 +102,12 @@ function getPreClosurePresets() {
     : {'browsers': ['Last 2 versions']};
 
   const options = {
-    'modules': modules,
+    'modules': false,
     'targets': targets,
-    'bugfixes': true,
   };
   if (argv.esm) {
+    options['bugfixes'] = true;
+  } else {
     options['loose'] = true;
   }
 
@@ -46,75 +120,6 @@ function getPreClosurePresets() {
  * @return {!Object}
  */
 function getPreClosureConfig() {
-  const isCheckTypes = argv._.includes('check-types');
-  const filterImportsPlugin = [
-    'filter-imports',
-    {
-      imports: {
-        // Imports removed for all ESM builds.
-        './polyfills/document-contains': ['installDocContains'],
-        './polyfills/domtokenlist': ['installDOMTokenList'],
-        './polyfills/fetch': ['installFetch'],
-        './polyfills/math-sign': ['installMathSign'],
-        './polyfills/object-assign': ['installObjectAssign'],
-        './polyfills/object-values': ['installObjectValues'],
-        './polyfills/promise': ['installPromise'],
-        './polyfills/array-includes': ['installArrayIncludes'],
-        './ie-media-bug': ['ieMediaCheckAndFix'],
-        '../third_party/css-escape/css-escape': ['cssEscape'],
-        // Imports that are not needed for valid transformed documents.
-        '../build/ampshared.css': ['cssText', 'ampSharedCss'],
-        '../build/ampdoc.css': ['cssText', 'ampDocCss'],
-      },
-    },
-  ];
-  const reactJsxPlugin = [
-    '@babel/plugin-transform-react-jsx',
-    {
-      pragma: 'Preact.createElement',
-      pragmaFrag: 'Preact.Fragment',
-      useSpread: true,
-    },
-  ];
-  const replacePlugin = getReplacePlugin();
-  const preClosurePlugins = [
-    './build-system/babel-plugins/babel-plugin-transform-fix-leading-comments',
-    '@babel/plugin-transform-react-constant-elements',
-    reactJsxPlugin,
-    argv.esm ? null : ['@babel/plugin-transform-classes', {loose: false}],
-    './build-system/babel-plugins/babel-plugin-transform-inline-configure-component',
-    // TODO(alanorozco): Remove `replaceCallArguments` once serving infra is up.
-    [
-      './build-system/babel-plugins/babel-plugin-transform-log-methods',
-      {replaceCallArguments: false},
-    ],
-    './build-system/babel-plugins/babel-plugin-transform-parenthesize-expression',
-    './build-system/babel-plugins/babel-plugin-is_minified-constant-transformer',
-    './build-system/babel-plugins/babel-plugin-transform-amp-extension-call',
-    './build-system/babel-plugins/babel-plugin-transform-html-template',
-    './build-system/babel-plugins/babel-plugin-transform-version-call',
-    './build-system/babel-plugins/babel-plugin-transform-simple-array-destructure',
-    replacePlugin,
-    argv.single_pass
-      ? './build-system/babel-plugins/babel-plugin-transform-amp-asserts'
-      : null,
-    argv.esm ? filterImportsPlugin : null,
-    argv.esm
-      ? './build-system/babel-plugins/babel-plugin-transform-function-declarations'
-      : null,
-    isCheckTypes
-      ? './build-system/babel-plugins/babel-plugin-transform-simple-object-destructure'
-      : './build-system/babel-plugins/babel-plugin-transform-json-configuration',
-    argv.esm
-      ? [
-          './build-system/babel-plugins/babel-plugin-amp-mode-transformer',
-          {isEsmBuild: !!argv.esm},
-        ]
-      : null,
-    !(argv.fortesting || isCheckTypes)
-      ? './build-system/babel-plugins/babel-plugin-is_dev-constant-transformer'
-      : null,
-  ].filter(Boolean);
   const devDependencies = getDevDependencies();
   const preClosureConfig = {
     compact: false,
