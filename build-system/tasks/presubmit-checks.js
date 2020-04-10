2602a8d9db4177aa20234fd2f9fad87946f6722f
Raghu Simha <rsimha@amp.dev>
2020-04-10 17:31:53 -0400

    Lay groundwork for testing ESM code during CI

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -114,6 +114,7 @@ const forbiddenTerms = {
       'build-system/pr-check/build-targets.js',
       'build-system/pr-check/checks.js',
       'build-system/pr-check/dist-bundle-size.js',
+      'build-system/pr-check/module-dist-bundle-size.js',
       'build-system/pr-check/experiment-tests.js',
       'build-system/pr-check/e2e-tests.js',
       'build-system/pr-check/local-tests.js',
