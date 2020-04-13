623383de27e0aeca17c33640a49a8a8a65935a06
Jake Fried <samouri@users.noreply.github.com>
2020-04-13 16:44:14 -0400

    🚀 amp-actions hide() can skipRemeasure (#27654)
    * optimization: hide() can skipRemeasure
    
    * update comment & add a TODO
    
    * should be measuring non-els
    
    * collapse is /*OK*/
    
    * typo
    
    * lint

--- src/service/standard-actions-impl.js
+++ src/service/standard-actions-impl.js
@@ -319,13 +319,18 @@ export class StandardActions {
   handleHide_(invocation) {
     const target = dev().assertElement(invocation.node);
 
-    this.mutator_.mutateElement(target, () => {
-      if (target.classList.contains('i-amphtml-element')) {
-        target./*OK*/ collapse();
-      } else {
-        toggle(target, false);
-      }
-    });
+    if (target.classList.contains('i-amphtml-element')) {
+      this.mutator_.mutateElement(
+        target,
+        () => target./*OK*/ collapse(),
+        // It is safe to skip measuring, because `mutator-impl.collapseElement`
+        // will set the size of the element as well as trigger a remeasure of
+        // everything below the collapsed element.
+        /* skipRemeasure */ true
+      );
+    } else {
+      this.mutator_.mutateElement(target, () => toggle(target, false));
+    }
 
     return null;
   }
