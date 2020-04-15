1099209e05c11d82011d8b7b9aaec79d87d6cbab
Justin Ridgewell <jridgewell@google.com>
2020-04-14 20:57:35 -0400

    Do not write sourcesContent in sourcemaps unless requested (#27761)
    Also adds sourcemaps debugging at each stage in the compilcation process.

--- build-system/compile/helpers.js
+++ build-system/compile/helpers.js
@@ -38,6 +38,7 @@ function getSourceMapBase() {
 function writeSourcemaps() {
   return sourcemaps.write('.', {
     sourceRoot: getSourceMapBase(),
+    includeContent: !!argv.full_sourcemaps,
   });
 }
 
