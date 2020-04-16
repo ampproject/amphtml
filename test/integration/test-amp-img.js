1207db61cc12b1e916b5b6853e4fb232cd62fbf1
Justin Ridgewell <jridgewell@google.com>
2020-04-16 19:14:40 -0400

    🏗✅ Fix IE11 Integration Test Support (#27796)
    * Fix IE11 Integration Test Support
    
    Some of our dev-dependencies are imported to run the tests, and some of those use ES6 code. We need to transpile **everything** before testing, so that even IE11 can understand it.
    
    * Fix tests, and remove devDependencies ignores
    
    * Remove it.only

--- test/integration/test-amp-img.js
+++ test/integration/test-amp-img.js
@@ -15,6 +15,7 @@
  */
 
 import {AmpEvents} from '../../src/amp-events';
+import {createCustomEvent} from '../../src/event-helper';
 import {
   createFixtureIframe,
   expectBodyToBecomeVisible,
@@ -57,7 +58,9 @@ describe
           12
         );
         fixture.iframe.height = 2000;
-        fixture.win.dispatchEvent(new fixture.win.Event('resize'));
+        fixture.win.dispatchEvent(
+          createCustomEvent(fixture.win, 'resize', null)
+        );
         return fixture.awaitEvent(AmpEvents.LOAD_START, 13).then(function () {
           expect(
             fixture.doc.querySelectorAll('amp-img img[src]')
@@ -65,7 +68,7 @@ describe
         });
       });
       fixture.iframe.height = 1500;
-      fixture.win.dispatchEvent(new fixture.win.Event('resize'));
+      fixture.win.dispatchEvent(createCustomEvent(fixture.win, 'resize', null));
       return p;
     });
 
@@ -87,7 +90,9 @@ describe
           expect(smallScreen.offsetHeight).to.not.equal(0);
           expect(largeScreen.offsetHeight).to.equal(0);
           fixture.iframe.width = 600;
-          fixture.win.dispatchEvent(new fixture.win.Event('resize'));
+          fixture.win.dispatchEvent(
+            createCustomEvent(fixture.win, 'resize', null)
+          );
           return fixture
             .awaitEvent(AmpEvents.LOAD_START, 4)
             .then(() => fixture.awaitEvent(AmpEvents.UNLOAD, 1))
