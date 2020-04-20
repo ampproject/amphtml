52c9903a69d136ff6d444c394956ecf3d89aa633
Khoi Doan <khoid@google.com>
2020-04-20 16:51:10 -0400

    Fix

--- src/inabox/inabox-viewport.js
+++ src/inabox/inabox-viewport.js
@@ -24,7 +24,6 @@ import {dev, devAssert} from '../log';
 import {getFrameOverlayManager} from '../../ads/inabox/frame-overlay-manager.js';
 import {getPositionObserver} from '../../ads/inabox/position-observer';
 import {iframeMessagingClientFor} from './inabox-iframe-messaging-client';
-import {isExperimentOn} from '../experiments';
 import {isIframed} from '../dom';
 import {
   layoutRectFromDomRect,
