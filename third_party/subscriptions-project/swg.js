07677f1f56b850651096a6a550e52691d59c1000
qidonna <968756+qidonna@users.noreply.github.com>
2020-04-20 20:16:10 -0400

    SwG Release 0.1.22.102 (#27893)

--- third_party/subscriptions-project/swg.js
+++ third_party/subscriptions-project/swg.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.101 */
+/** Version: 0.1.22.102 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
@@ -5653,7 +5653,7 @@ function feCached(url) {
  */
 function feArgs(args) {
   return Object.assign(args, {
-    '_client': 'SwG 0.1.22.101',
+    '_client': 'SwG 0.1.22.102',
   });
 }
 
@@ -6767,7 +6767,7 @@ class ActivityPorts$1 {
         'analyticsContext': context.toArray(),
         'publicationId': pageConfig.getPublicationId(),
         'productId': pageConfig.getProductId(),
-        '_client': 'SwG 0.1.22.101',
+        '_client': 'SwG 0.1.22.102',
         'supportsEventManager': true,
       },
       args || {}
@@ -7609,7 +7609,7 @@ class AnalyticsService {
       context.setTransactionId(getUuid());
     }
     context.setReferringOrigin(parseUrl$1(this.getReferrer_()).origin);
-    context.setClientVersion('SwG 0.1.22.101');
+    context.setClientVersion('SwG 0.1.22.102');
 
     const utmParams = parseQueryString$1(this.getQueryString_());
     const campaign = utmParams['utm_campaign'];
