5f3f0d1b13ddbb6543382c5349b92c0a3b54cfeb
Leonid Doroshenko <player_dev@anyclip.com>
2020-04-23 02:25:05 +0300

    Added "amp-ad" integration for Anyclip player

---
+++ ads/anyclip.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {loadScript, validateData} from '../3p/3p';
+
+const requiredParams = ['pubname', 'widgetname'];
+const aclreMessagePrefix = 'lre:playerReady://';
+
+const scriptHost = 'player.anyclip.com';
+const scriptPath = 'anyclip-widget/lre-widget/prod/v1/src';
+const scriptName = 'aclre-amp-loader.js';
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ */
+export function anyclip(global, data) {
+  validateData(data, requiredParams);
+
+  global.addEventListener('message', (event) => {
+    if (event.data && event.data.indexOf(aclreMessagePrefix) === 0) {
+      const data = JSON.parse(event.data.replace(aclreMessagePrefix, ''));
+      const widget = global.anyclip.getWidget(null, data.sessionId);
+      if (widget) {
+        global.context.renderStart();
+      }
+    }
+  });
+
+  loadScript(global, `https://${scriptHost}/${scriptPath}/${scriptName}`);
+}
