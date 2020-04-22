b3e50e022fa65d29633982a2beb96c9cd5c06083
Matias Szylkowski <mszylkowski@google.com>
2020-04-22 10:30:34 -0400

    ✨Added play/pause button (#27815)
    * added play/pause button
    
    * enable click navigation on pause
    
    * added tests for system layer and story
    
    * fix amp-story-test
    
    * fixed comments from PR
    
    * renamed flags, simplified check conditions
    
    * finish fixes on tests
    
    * updated naming to elementWithPlayable and fix more PR comments
    
    * updated test descriptions

--- extensions/amp-story/1.0/amp-story-page.js
+++ extensions/amp-story/1.0/amp-story-page.js
@@ -509,6 +509,7 @@ export class AmpStoryPage extends AMP.BaseElement {
         ? this.maybeFinishAnimations_()
         : this.maybeStartAnimations_();
       this.checkPageHasAudio_();
+      this.checkPageHasElementWithPlayback_();
       this.renderOpenAttachmentUI_();
       this.findAndPrepareEmbeddedComponents_();
       this.startMeasuringVideoPerformance_();
@@ -1360,6 +1361,7 @@ export class AmpStoryPage extends AMP.BaseElement {
       return;
     }
 
+    this.storeService_.dispatch(Action.TOGGLE_PAUSED, false);
     this.switchTo_(pageId, NavigationDirection.PREVIOUS);
   }
 
@@ -1382,6 +1384,7 @@ export class AmpStoryPage extends AMP.BaseElement {
       return;
     }
 
+    this.storeService_.dispatch(Action.TOGGLE_PAUSED, false);
     this.switchTo_(pageId, NavigationDirection.NEXT);
   }
 
@@ -1422,6 +1425,22 @@ export class AmpStoryPage extends AMP.BaseElement {
     return Array.prototype.some.call(
       ampVideoEls,
       (video) => !video.hasAttribute('noaudio')
+    );
+  }
+
+  /**
+   * Checks if the page has elements with playback.
+   * @private
+   */
+  checkPageHasElementWithPlayback_() {
+    const pageHasElementWithPlayback =
+      this.isAutoAdvance() ||
+      this.element.hasAttribute('background-audio') ||
+      this.getAllMedia_().length > 0;
+
+    this.storeService_.dispatch(
+      Action.TOGGLE_PAGE_HAS_ELEMENT_WITH_PLAYBACK,
+      pageHasElementWithPlayback
     );
   }
 
@@ -1554,7 +1573,9 @@ export class AmpStoryPage extends AMP.BaseElement {
         this.registerMedia_(mediaPool, videoEl)
           .then(() => this.preloadMedia_(mediaPool, videoEl))
           .then((poolVideoEl) => {
-            this.playMedia_(mediaPool, poolVideoEl);
+            if (!this.storeService_.get(StateProperty.PAUSED_STATE)) {
+              this.playMedia_(mediaPool, poolVideoEl);
+            }
             if (!this.storeService_.get(StateProperty.MUTED_STATE)) {
               this.unmuteAllMedia();
             }
