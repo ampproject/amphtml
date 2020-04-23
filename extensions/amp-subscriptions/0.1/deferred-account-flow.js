9705f3466fd84acc0b63708522df4fc795c6a583
Vale <vderosa@google.com>
2020-04-22 19:05:45 -0700

    create deferred account flow in AMP subscriptions

---
+++ extensions/amp-subscriptions/0.1/deferred-account-flow.js
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Services} from '../../../src/services';
+import {UrlBuilder} from '../../amp-subscriptions/0.1/url-builder';
+import {stringifyForPingback} from '../../../src/utils/xhr-utils';
+
+const LOCAL_STORAGE_KEYS = {
+  HAS_PUBLISHER_ACCOUNT: 'account-exists-on-publisher-side',
+  HAS_REJECTED_ACCOUNT_CREATION: 'user-rejected-account-creation-request',
+};
+/**
+ * AMP-side implementation of deferred account creation, a flow responsible
+ * for reconciling account existance discrepancies across platforms.
+ */
+export class DeferredAccountFlow {
+  /**
+   * @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc
+   * @param {string} hasAssociatedAccountUrl
+   * @param {string} accountCreationRedirectUrl
+   */
+  constructor(ampdoc, hasAssociatedAccountUrl, accountCreationRedirectUrl) {
+    /** @const @private {!../../../src/service/xhr-impl.Xhr} */
+    this.xhr_ = Services.xhrFor(ampdoc.win);
+    /**
+     * @const @private {function(string):void}
+     * @param {string} url
+     */
+    this.navigateTo_ = (url) => {
+      Services.navigationForDoc(ampdoc).navigateTo(ampdoc.win, url);
+    };
+
+    /** @const @private {!../../../src/service/storage-impl.Storage} */
+    this.storage_ = Services.storageForDoc(ampdoc);
+    /** @private @const {?string} */
+    this.hasAssociatedAccountUrl_ = hasAssociatedAccountUrl || null;
+
+    /** @private @const {?string} */
+    this.accountCreationRedirectUrl_ = accountCreationRedirectUrl || null;
+    /**
+     * @private @const {function():!UrlBuilder}
+     * @param {string} platformId
+     * @return {!UrlBuilder}
+     */
+    this.getUrlBuilder_ = (platformId) => new UrlBuilder(ampdoc, platformId);
+  }
+
+  /**
+   * @return {bool} whether deferred account creation is enabled.
+   */
+  isDeferredAccountCreationEnabled() {
+    return this.hasAssociatedAccountUrl_ && this.accountCreationRedirectUrl_;
+  }
+
+  /**
+   * @param {../subscription-platform.SubscriptionPlatform} platform
+   * @param {?./entitlement.Entitlement} selectedEntitlement
+   * @return {!Promise<{found: boolean}>}
+   * @private
+   */
+  hasAssociatedUserAccount_(platform, selectedEntitlement) {
+    let storage;
+    return this.storage_
+      .then((s) => {
+        storage = s;
+        return s.get(LOCAL_STORAGE_KEYS.HAS_PUBLISHER_ACCOUNT);
+      })
+      .then((publisherAccountLocalValue) => {
+        if (publisherAccountLocalValue !== undefined) {
+          // We found a cached value for the API call, return.
+          return {found: publisherAccountLocalValue};
+        }
+
+        // Do the actual call and then store the value in storage
+        return this.getUrlBuilder_(platform.getServiceId())
+          .buildUrl(this.hasAssociatedAccountUrl_, /* useAuthData */ true)
+          .then((url) =>
+            this.xhr_.fetchJson(url, {
+              method: 'POST',
+              credentials: 'include',
+              body: {
+                entitlements: stringifyForPingback(selectedEntitlement),
+              },
+            })
+          )
+          .then((result) => result.json())
+          .then((jsonResult) => {
+            storage.set(
+              LOCAL_STORAGE_KEYS.HAS_PUBLISHER_ACCOUNT,
+              jsonResult.found
+            );
+
+            return jsonResult;
+          });
+      });
+  }
+
+  /**
+   * Runs the flow, checking if an account corresponding to the
+   * entitlement exists on the given platform. If the account doesn't
+   * exist and the user gives consent, the user will then be redirected
+   * to the accountCreationRedirectUrl passed in the constructor.
+   *
+   * @param {../subscription-platform.SubscriptionPlatform} platform
+   * @param {?./entitlement.Entitlement} entitlements
+   * @return {!Promise|undefined}
+   */
+  run(platform, entitlements) {
+    // Check if user has denied account creation already
+    let storage;
+    return this.storage_
+      .then((s) => {
+        storage = s;
+        return s.get(LOCAL_STORAGE_KEYS.HAS_REJECTED_ACCOUNT_CREATION);
+      })
+      .then((rejected) => {
+        if (rejected) {
+          return;
+        }
+        // Check if there is an associated user account
+        return this.hasAssociatedUserAccount_(platform, entitlements).then(
+          (jsonResult) => {
+            if (jsonResult.found) {
+              return;
+            }
+
+            platform.completeDeferredAccountCreation().then((accepted) => {
+              if (accepted) {
+                // ...redirect the user to URL provided for this purpose.
+                this.navigateTo_(this.accountCreationRedirectUrl_);
+              } else {
+                // The user did not authorize the creation of a linked account.
+                // Save response so we won't ask again (for a while).
+                return storage.set(
+                  LOCAL_STORAGE_KEYS.HAS_REJECTED_ACCOUNT_CREATION,
+                  true
+                );
+              }
+            });
+          }
+        );
+      });
+  }
+}
---
+++ extensions/amp-subscriptions/0.1/test/test-deferred-account-flow.js
@@ -0,0 +1,257 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {DeferredAccountFlow} from '../deferred-account-flow';
+import {Entitlement, GrantReason} from '../entitlement';
+import {Services} from '../../../../src/services';
+
+const HAS_ACCOUNT_URL = 'https://fakeUrl.com/hasAccount';
+const CREATE_ACCOUNT_URL = 'https://fakeUrl.com/createAccount';
+const PLATFORM_ID = 'fake_platform_id';
+
+const ENTITLEMENT = new Entitlement({
+  service: PLATFORM_ID,
+  granted: true,
+  grantReason: GrantReason.SUBSCRIBER,
+  dataObject: {data: 'this is the data'},
+});
+const RAW_ENTITLEMENT =
+  '{"raw":"","service":"fake_platform_id","granted":true,"grantReason":"SUBSCRIBER","data":{"data":"this is the data"}}';
+
+describes.realWin('deferred-account-flow', {amp: true}, (env) => {
+  let xhr;
+  let navigator;
+  let navigateToStub;
+  let storage;
+  let platform;
+  let fetchStub;
+  let deferredCreationStub;
+
+  beforeEach(() => {
+    xhr = Services.xhrFor(env.win);
+    navigator = Services.navigationForDoc(env.ampdoc);
+    navigateToStub = env.sandbox.stub(navigator, 'navigateTo');
+    navigateToStub.callsFake(() => {
+      /* make fake to avoid redirect */
+    });
+    storage = Services.storageForDoc(env.ampdoc);
+    platform = {
+      getServiceId: () => Promise.resolve(PLATFORM_ID),
+      completeDeferredAccountCreation: () => {},
+    };
+    fetchStub = env.sandbox.stub(xhr, 'fetchJson');
+    deferredCreationStub = env.sandbox.stub(
+      platform,
+      'completeDeferredAccountCreation'
+    );
+  });
+
+  it('should not call deferred account creation when user is already found', async () => {
+    const actualStorage = await storage;
+    env.sandbox
+      .stub(actualStorage, 'get')
+      .callsFake(() => Promise.resolve(undefined));
+    env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+    fetchStub.withArgs(HAS_ACCOUNT_URL).returns(
+      Promise.resolve({
+        json: () => Promise.resolve({found: true}),
+      })
+    );
+
+    deferredCreationStub.returns(Promise.resolve());
+
+    const deferredFlow = new DeferredAccountFlow(
+      env.ampdoc,
+      HAS_ACCOUNT_URL,
+      CREATE_ACCOUNT_URL
+    );
+    await deferredFlow.run(platform, ENTITLEMENT);
+
+    expect(deferredCreationStub).to.not.be.called;
+  });
+
+  it('should redirect to new account creation page when associated account absent', async () => {
+    const actualStorage = await storage;
+    env.sandbox
+      .stub(actualStorage, 'get')
+      .callsFake(() => Promise.resolve(undefined));
+    env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+    fetchStub.withArgs(HAS_ACCOUNT_URL).returns(
+      Promise.resolve({
+        json: () => Promise.resolve({found: false}),
+      })
+    );
+
+    deferredCreationStub.returns(Promise.resolve(true));
+
+    const deferredFlow = new DeferredAccountFlow(
+      env.ampdoc,
+      HAS_ACCOUNT_URL,
+      CREATE_ACCOUNT_URL
+    );
+    await deferredFlow.run(platform, ENTITLEMENT);
+
+    expect(fetchStub).to.be.calledWith(HAS_ACCOUNT_URL, {
+      body: {
+        entitlements: RAW_ENTITLEMENT,
+      },
+      credentials: 'include',
+      method: 'POST',
+    });
+    expect(navigateToStub).to.be.calledWith(env.win, CREATE_ACCOUNT_URL);
+  });
+
+  it('should not redirect to new account creation page when user gives no consent', async () => {
+    const actualStorage = await storage;
+    env.sandbox
+      .stub(actualStorage, 'get')
+      .callsFake(() => Promise.resolve(undefined));
+    env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+    fetchStub.withArgs(HAS_ACCOUNT_URL).returns(
+      Promise.resolve({
+        json: () => Promise.resolve({found: false}),
+      })
+    );
+
+    deferredCreationStub.returns(Promise.resolve(false));
+
+    const deferredFlow = new DeferredAccountFlow(
+      env.ampdoc,
+      HAS_ACCOUNT_URL,
+      CREATE_ACCOUNT_URL
+    );
+    await deferredFlow.run(platform, ENTITLEMENT);
+
+    expect(fetchStub).to.be.calledWith(HAS_ACCOUNT_URL, {
+      body: {
+        entitlements: RAW_ENTITLEMENT,
+      },
+      credentials: 'include',
+      method: 'POST',
+    });
+    expect(navigateToStub).to.not.be.called;
+  });
+
+  describe('local storage', async () => {
+    it('should skip call to publisher API when user is already found', async () => {
+      const actualStorage = await storage;
+      env.sandbox
+        .stub(actualStorage, 'get')
+        .withArgs('account-exists-on-publisher-side')
+        .callsFake(() => Promise.resolve(true));
+      env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+      deferredCreationStub.returns(Promise.resolve(true));
+
+      const deferredFlow = new DeferredAccountFlow(
+        env.ampdoc,
+        HAS_ACCOUNT_URL,
+        CREATE_ACCOUNT_URL
+      );
+      await deferredFlow.run(platform, ENTITLEMENT);
+
+      expect(fetchStub).to.not.be.called;
+      expect(deferredCreationStub).to.not.be.called;
+    });
+
+    it('should still redirect when user not found fetched by local storage', async () => {
+      const actualStorage = await storage;
+      env.sandbox
+        .stub(actualStorage, 'get')
+        .withArgs('account-exists-on-publisher-side')
+        .callsFake(() => Promise.resolve(false));
+      env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+      fetchStub.withArgs(HAS_ACCOUNT_URL).returns(
+        Promise.resolve({
+          json: () => Promise.resolve({found: false}),
+        })
+      );
+
+      deferredCreationStub.returns(Promise.resolve(true));
+
+      const deferredFlow = new DeferredAccountFlow(
+        env.ampdoc,
+        HAS_ACCOUNT_URL,
+        CREATE_ACCOUNT_URL
+      );
+      await deferredFlow.run(platform, ENTITLEMENT);
+
+      expect(fetchStub).to.not.be.called;
+      expect(deferredCreationStub).to.be.called;
+    });
+
+    it('should save user found status and rejection', async () => {
+      const actualStorage = await storage;
+      env.sandbox
+        .stub(actualStorage, 'get')
+        .withArgs('account-exists-on-publisher-side')
+        .callsFake(() => Promise.resolve());
+      const saveStorageStub = env.sandbox
+        .stub(actualStorage, 'set')
+        .callsFake(() => Promise.resolve());
+
+      fetchStub.withArgs(HAS_ACCOUNT_URL).returns(
+        Promise.resolve({
+          json: () => Promise.resolve({found: false}),
+        })
+      );
+
+      deferredCreationStub.returns(Promise.resolve(false));
+
+      const deferredFlow = new DeferredAccountFlow(
+        env.ampdoc,
+        HAS_ACCOUNT_URL,
+        CREATE_ACCOUNT_URL
+      );
+      await deferredFlow.run(platform, ENTITLEMENT);
+
+      expect(fetchStub).to.be.called;
+      expect(deferredCreationStub).to.be.called;
+      expect(saveStorageStub).to.be.calledWith(
+        'account-exists-on-publisher-side',
+        false
+      );
+      expect(saveStorageStub).to.be.calledWith(
+        'user-rejected-account-creation-request',
+        true
+      );
+    });
+
+    it('should not make any call if user has already rejected consent', async () => {
+      const actualStorage = await storage;
+      env.sandbox
+        .stub(actualStorage, 'get')
+        .withArgs('user-rejected-account-creation-request')
+        .callsFake(() => Promise.resolve(true));
+      env.sandbox.stub(actualStorage, 'set').callsFake(() => Promise.resolve());
+
+      deferredCreationStub.returns(Promise.resolve());
+
+      const deferredFlow = new DeferredAccountFlow(
+        env.ampdoc,
+        HAS_ACCOUNT_URL,
+        CREATE_ACCOUNT_URL
+      );
+      await deferredFlow.run(platform, ENTITLEMENT);
+
+      expect(fetchStub).to.not.be.called;
+      expect(deferredCreationStub).to.not.be.called;
+    });
+  });
+});
