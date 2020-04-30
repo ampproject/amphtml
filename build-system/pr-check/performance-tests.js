94f952931be974c8c28b787ec15ab603ca876bae
esth <esth@google.com>
2020-04-30 15:07:23 -0400

    perf job

---
+++ build-system/pr-check/performance-tests.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+/**
+ * @fileoverview
+ * This script runs performance tests.
+ * This is run during the CI stage = test; job = performance tests.
+ */
+
+const colors = require('ansi-colors');
+const {
+  downloadDistOutput,
+  printChangeSummary,
+  startTimer,
+  stopTimer,
+  timedExecOrDie: timedExecOrDieBase,
+} = require('./utils');
+const {determineBuildTargets} = require('./build-targets');
+const {isTravisPullRequestBuild} = require('../common/travis');
+
+const FILENAME = 'performance-tests.js';
+const FILELOGPREFIX = colors.bold(colors.yellow(`${FILENAME}:`));
+const timedExecOrDie = (cmd) => timedExecOrDieBase(cmd, FILENAME);
+
+async function main() {
+  const startTime = startTimer(FILENAME, FILENAME);
+
+  if (!isTravisPullRequestBuild()) {
+    downloadDistOutput(FILENAME);
+    timedExecOrDie('gulp update-packages');
+    timedExecOrDie('gulp serve --compiled');
+    timedExecOrDie('gulp performance --nobuild --headless');
+  } else {
+    printChangeSummary(FILENAME);
+    const buildTargets = determineBuildTargets(FILENAME);
+    if (buildTargets.has('RUNTIME') || buildTargets.has('FLAG_CONFIG')) {
+      downloadDistOutput(FILENAME);
+      timedExecOrDie('gulp update-packages');
+      timedExecOrDie('gulp serve --compiled');
+      timedExecOrDie('gulp performance --nobuild --headless');
+    } else {
+      console.log(
+        `${FILELOGPREFIX} Skipping`,
+        colors.cyan('Performance Tests'),
+        'because this commit does not affect the runtime or flag configs'
+      );
+    }
+  }
+  stopTimer(FILENAME, FILENAME, startTime);
+}
+
+main();
