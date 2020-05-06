90b12cdc2d39be451b3f4a249048ca6b949dabb4
Justin Ridgewell <jridgewell@google.com>
2020-05-06 18:36:11 -0400

    Fix type

--- src/preact/slot.js
+++ src/preact/slot.js
@@ -42,7 +42,7 @@ export function createSlot(element, name, props) {
  */
 export function Slot(props) {
   const context = useContext(getAmpContext());
-  const ref = useRef(null);
+  const ref = useRef(/** @type {?Element} */ (null));
   const slotProps = {...props, ref};
   useEffect(() => {
     const {current} = dev().assertElement(ref);
