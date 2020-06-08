39973f702f93ad16e88bdad679b9c3480df4f5aa
Malte Ubl <malte.ubl@gmail.com>
2020-06-07 20:40:42 -0700

    Also store story state in localStorage (on top of history.state)
    This allows state restoration when stories are shown in players that recycles iframes or webviews.
    
    - Expires state after 10 minutes.
    - Keys state on the URL minus the fragment
    
    This should also fix state restoration when restoring iframed Stories (e.g. on back navigation after a swipe-up).
    
    To be discussed:
    
    - Is 10 minutes a good value?
    - Should this only be active with a viewer/player present?
    
    Fixes #28720

---
+++ extensions/amp-story/1.0/history.js
@@ -0,0 +1,153 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {getState} from '../../../src/history';
+import {parseJson} from '../../../src/json';
+import {dict} from '../../../src/utils/object';
+
+const EXPIRATION_DURATION_MILLIS = 10 * 60 * 1000; // 10 Minutes
+const CREATION_TIME = 'time';
+/** @visibleForTesting */
+export const LOCAL_STORAGE_KEY = 'amp-story-state';
+
+/** @enum {string} */
+export const HistoryState = {
+  ATTACHMENT_PAGE_ID: 'ampStoryAttachmentPageId',
+  BOOKEND_ACTIVE: 'ampStoryBookendActive',
+  NAVIGATION_PATH: 'ampStoryNavigationPath',
+};
+
+/**
+ * Updates the value for a given state in the window history.
+ * Also persists state in localStorage for 10 minutes, so that state is
+ * persisted if Stories are shown within viewers that may recycle
+ * iframes or webviews.
+ * @param {!Window} win
+ * @param {string} stateName
+ * @param {string|boolean|Array<string>|null} value
+ */
+export function setHistoryState(win, stateName, value) {
+  const {history} = win;
+  const state = getState(history) || {};
+  const newHistory = {
+    ...state,
+    [stateName]: value,
+  };
+
+  history.replaceState(newHistory, '');
+  setLocalStorageState(win, newHistory);
+}
+
+/**
+ * Returns the value of a given state of the window history or localStorage.
+ * See `setHistoryState` for an explanation of the use of localStorage.
+ * @param {!Window} win
+ * @param {string} stateName
+ * @return {string|boolean|Array<string>|null}
+ */
+export function getHistoryState(win, stateName) {
+  const {history} = win;
+  if (history) {
+    const state = getState(history) || getLocalStorageState(win);
+    if (state) {
+      return /** @type {string|boolean|Array<string>|null} */ (state[
+        stateName
+      ] || null);
+    }
+  }
+  return null;
+}
+
+/**
+ * Get history state from localStorage.
+ * @param {!Window} win
+ * @return {*}
+ */
+function getLocalStorageState(win) {
+  const container = getLocalStorageStateContainer(win);
+  return container && container[getDocumentKey(win)];
+}
+
+/**
+ * Set history state in localStorage.
+ * State is keyed off of the URL-minus fragment of the Story.
+ * @param {!Window} win
+ * @param {*} state
+ */
+function setLocalStorageState(win, state) {
+  const container = getLocalStorageStateContainer(win);
+  state[CREATION_TIME] = Date.now();
+  container[getDocumentKey(win)] = state;
+  writeLocalStorage(win, container);
+}
+
+/**
+ * Gets a dictionary of all history states on the current origin.
+ * If an entry is expired, removes it from the set and writes the new
+ * set back to storage.
+ * @param {!Window} win
+ * @return {!JsonObject}
+ */
+function getLocalStorageStateContainer(win) {
+  const container = readLocalStorage(win);
+  if (!container) {
+    return dict();
+  }
+  const now = Date.now();
+  let expired = false;
+  Object.keys(container).forEach((href) => {
+    const item = container[href];
+    if (now > item[CREATION_TIME] + EXPIRATION_DURATION_MILLIS) {
+      delete container[href];
+      expired = true;
+    }
+  });
+  if (expired) {
+    writeLocalStorage(win, container);
+  }
+  return container;
+}
+
+/**
+ * Returns the key under which we store state for the current document.
+ * @param {!Window} win
+ * @return {string}
+ */
+function getDocumentKey(win) {
+  return win.location.href.replace(/\#.*/, '');
+}
+
+/**
+ * @param {!Window} win
+ * @return {?JsonObject}
+ */
+function readLocalStorage(win) {
+  try {
+    return parseJson(win.localStorage.getItem(LOCAL_STORAGE_KEY));
+  } catch (e) {
+    return null;
+  }
+}
+
+/**
+ * @param {!Window} win
+ * @param {JsonObject} container
+ */
+function writeLocalStorage(win, container) {
+  try {
+    win.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(container));
+  } catch (e) {}
+}
