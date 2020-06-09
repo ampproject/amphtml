8453d12f90407a6563ba71a8791ea0d40cd45e62
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-06-09 10:17:07 -0700

    ♻️ Move removal of info statements from transform-amp-asserts (#28754)
    * Move removal of info statements from transform-amp-asserts to transform-dev-methods
    * Clarifying comment suggested by Erwin

--- build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/imported-both-no-usage/output.mjs
+++ build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/imported-both-no-usage/output.mjs
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-let result;
-let result2 = undefined;
+import { dev, user } from '../../../../../../../src/log';
--- build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/no-import/output.js
+++ build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/no-import/output.js
@@ -15,3 +15,7 @@
  */
 dev().info(TAG, 'Removing iframe query string before navigation:', fromLocation.search);
 dev().info;
+dev().fine(TAG, 'fine');
+dev().fine;
+user().fine(TAG, 'user fine');
+user().fine;
