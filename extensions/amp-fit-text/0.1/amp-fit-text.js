d3579c57ad138970d81e38c6385ecabc2f8f8882
Malte Ubl <malte.ubl@gmail.com>
2020-06-09 11:22:43 -0700

    Add support for writing and reading textContent on amp-fit-text.
    Doesn't support changing HTML children, which would be much more complicated without Shadow DOM.
    
    Fixes #17895

--- extensions/amp-fit-text/0.1/amp-fit-text.js
+++ extensions/amp-fit-text/0.1/amp-fit-text.js
@@ -72,7 +72,7 @@ class AmpFitText extends AMP.BaseElement {
     this.getRealChildNodes().forEach((node) => {
       this.contentWrapper_.appendChild(node);
     });
-    this.measurer_./*OK*/ innerHTML = this.contentWrapper_./*OK*/ innerHTML;
+    this.updateMeasurerContent_();
     this.element.appendChild(this.content_);
     this.element.appendChild(this.measurer_);
 
@@ -81,6 +81,22 @@ class AmpFitText extends AMP.BaseElement {
 
     this.maxFontSize_ =
       getLengthNumeral(this.element.getAttribute('max-font-size')) || 72;
+
+    // Make it so that updates to the textContent of the amp-fit-text element
+    // actually update the text of the content element.
+    Object.defineProperty(this.element, 'textContent', {
+      set: (v) => {
+        this.textContent_ = v;
+        this.mutateElement(() => {
+          this.contentWrapper_.textContent = v;
+          this.updateMeasurerContent_();
+          this.updateFontSize_();
+        });
+      },
+      get: () => {
+        return this.textContent_ || this.contentWrapper_.textContent;
+      },
+    });
   }
 
   /** @override */
@@ -98,6 +114,13 @@ class AmpFitText extends AMP.BaseElement {
     return this.mutateElement(() => {
       this.updateFontSize_();
     });
+  }
+
+  /**
+   * Copies text from the displayed content to the measurer element.
+   */
+  updateMeasurerContent_() {
+    this.measurer_./*OK*/ innerHTML = this.contentWrapper_./*OK*/ innerHTML;
   }
 
   /** @private */
