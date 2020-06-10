040d11c8f30ae1da42d91c6b888d368891de601d
Justin Ridgewell <jridgewell@google.com>
2020-06-10 19:07:00 -0400

    Fix function name

--- src/service/ie-intrinsic-bug.js
+++ src/service/ie-intrinsic-bug.js
@@ -46,6 +46,6 @@ export function checkAndFix(win, opt_platform) {
     }
     const width = getLengthNumeral(element.getAttribute('width'));
     const height = getLengthNumeral(element.getAttribute('height'));
-    intrinsic.src = transparent(width, height);
+    intrinsic.src = transparentPng(width, height);
   }
 }
