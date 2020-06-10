63432064c1a146d0629113d9408619cb45b33402
Jake Fried <samouri@users.noreply.github.com>
2020-06-09 23:54:16 -0400

    if working before, this should work

--- src/video-iframe-integration.js
+++ src/video-iframe-integration.js
@@ -185,8 +185,7 @@ export class AmpVideoIntegration {
     if (!(method in this.methods_)) {
       return;
     }
-    // TODO: is this a bug? Why should `this` be set to undefined.
-    this.methods_[method].call();
+    this.methods_[method]();
   }
 
   /**
