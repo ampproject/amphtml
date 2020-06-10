1dee5e7550baf90191361f4fca43e17ae96ec028
Enrique Marroquin <artezan@google.com>
2020-06-10 17:48:04 -0400

    delete generation of 0.1 bundle

--- build-system/compile/bundles.config.js
+++ build-system/compile/bundles.config.js
@@ -865,26 +865,6 @@ exports.extensionBundles = [
   },
   {
     name: 'amp-story',
-    version: '0.1',
-    latestVersion: '1.0',
-    options: {
-      hasCss: true,
-      cssBinaries: [
-        'amp-story-bookend',
-        'amp-story-consent',
-        'amp-story-hint',
-        'amp-story-unsupported-browser-layer',
-        'amp-story-viewport-warning-layer',
-        'amp-story-info-dialog',
-        'amp-story-share',
-        'amp-story-share-menu',
-        'amp-story-system-layer',
-      ],
-    },
-    type: TYPES.MISC,
-  },
-  {
-    name: 'amp-story',
     version: '1.0',
     latestVersion: '1.0',
     options: {
