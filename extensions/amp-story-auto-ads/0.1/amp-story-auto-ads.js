7b257c9e97332a4e018e6a2a629d2537742f6f41
calebcordry <ccordry@google.com>
2020-06-12 11:47:02 -0700

    fix stuck

--- extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js
+++ extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js
@@ -263,7 +263,6 @@ export class AmpStoryAutoAds extends AMP.BaseElement {
   /**
    * Determine if enough pages in the story are left for ad placement to be
    * possible.
-   * TODO(ccordry): also use this on subsequent ad requests.
    * @param {number} pageIndex
    * @return {boolean}
    * @private
@@ -432,6 +431,8 @@ export class AmpStoryAutoAds extends AMP.BaseElement {
   }
 
   /**
+   * Respond to page navigation event. This method is not called for the first
+   * page that is shown on load.
    * @param {number} pageIndex
    * @param {string} pageId
    * @private
@@ -487,12 +488,14 @@ export class AmpStoryAutoAds extends AMP.BaseElement {
       this.idOfAdShowing_ = adIndex;
     }
 
-    // If there is already an ad inserted, but not viewed it doesn't matter how
-    // many pages we have seen, we should not keep trying to insert more ads.
     if (
       !this.pendingAdView_ &&
       this.enoughContentPagesViewed_() &&
-      !this.tryingToPlace_
+      // If there is already an ad inserted, but not viewed it doesn't matter how
+      // many pages we have seen, we should not keep trying to insert more ads.
+      !this.tryingToPlace_ &&
+      // Prevent edge case where we try to place an ad twice. See #28840.
+      this.adsPlaced_ < this.adPagesCreated_
     ) {
       this.tryToPlaceAdAfterPage_(pageId).then((adState) => {
         this.tryingToPlace_ = false;
