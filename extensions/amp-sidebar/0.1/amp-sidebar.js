0306c5b20f1caf5da19a5124f5614cf32107400c
Wassim Gharbi <wassgha@gmail.com>
2020-06-12 11:47:27 -0700

    Warn when amp-sidebar is not a direct descendent of body

--- extensions/amp-sidebar/0.1/amp-sidebar.js
+++ extensions/amp-sidebar/0.1/amp-sidebar.js
@@ -150,6 +150,15 @@ export class AmpSidebar extends AMP.BaseElement {
 
     this.action_ = Services.actionServiceForDoc(element);
 
+    if (
+      this.element.parentNode != this.element.ownerDocument.body &&
+      this.element.parentNode != this.getAmpDoc().getBody()
+    ) {
+      this.user().warn(
+        `${TAG} is recommended to be a direct child of the <body> element to preserve a logical DOM order.`
+      );
+    }
+
     if (this.side_ != Side.LEFT && this.side_ != Side.RIGHT) {
       this.side_ = this.setSideAttribute_(
         isRTL(this.document_) ? Side.RIGHT : Side.LEFT
