75051d17231de462448ccd8bf9e5f1888c1251da
Shihua Zheng <powerivq@users.noreply.github.com>
2020-06-12 18:00:34 -0700

    Add padding to amp-sticky-ad on all-screen iPhone (#28845)
    * Add padding to amp-sticky-ad on all-screen iPhone
    
    * CHange to safe-area-inset-bottom
    
    * remove device gate
    
    * Comments

--- extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
@@ -28,6 +28,7 @@ import {
   QQID_HEADER,
   RENDER_ON_IDLE_FIX_EXP,
   SANDBOX_HEADER,
+  STICKY_AD_PADDING_BOTTOM_EXP,
   ValidAdContainerTypes,
   addCsiSignalsToAmpAnalyticsConfig,
   extractAmpAnalyticsConfig,
@@ -492,6 +493,13 @@ export class AmpAdNetworkDoubleclickImpl extends AmpA4A {
       [NO_SIGNING_EXP.id]: {
         isTrafficEligible: () => true,
         branches: [[NO_SIGNING_EXP.control], [NO_SIGNING_EXP.experiment]],
+      },
+      [STICKY_AD_PADDING_BOTTOM_EXP.id]: {
+        isTrafficEligible: () => true,
+        branches: [
+          [STICKY_AD_PADDING_BOTTOM_EXP.control],
+          [STICKY_AD_PADDING_BOTTOM_EXP.experiment],
+        ],
       },
       ...AMPDOC_FIE_EXPERIMENT_INFO_MAP,
     });
