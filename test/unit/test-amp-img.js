3b7abb131075fea5c531877fc752911eff0a8b32
Justin Ridgewell <jridgewell@google.com>
2020-06-16 14:41:30 -0400

    Fix amp-img SSR with blurry-image placeholder (#28901)
    Blurry image placeholders are also `<img>` tags inserted as a direct child of the `<amp-img>` element. This will cause conflicts with the `<amp-img>`'s SSR rendering.
    
    We want to ignore the blurry image placeholder when finding the real `<img>` child to reuse.
    
    Re: https://github.com/ampproject/amphtml/pull/28758

--- test/unit/test-amp-img.js
+++ test/unit/test-amp-img.js
@@ -481,6 +481,8 @@ describes.sandboxed('amp-img', {}, (env) => {
     function getImgWithBlur(addPlaceholder, addBlurClass) {
       const el = document.createElement('amp-img');
       const img = document.createElement('img');
+      el.setAttribute('src', '/examples/img/sample.jpg');
+      img.src = 'data:image/svg+xml;charset=utf-8,%3Csvg%3E%3C/svg%3E';
       if (addPlaceholder) {
         img.setAttribute('placeholder', '');
         el.getPlaceholder = () => img;
@@ -533,6 +535,49 @@ describes.sandboxed('amp-img', {}, (env) => {
       el = impl.element;
       img = el.firstChild;
       expect(impl.togglePlaceholder).to.have.been.calledWith(false);
+    });
+
+    it('does not interfere with SSR img creation', () => {
+      const impl = getImgWithBlur(true, true);
+      const ampImg = impl.element;
+      ampImg.setAttribute('i-amphtml-ssr', '');
+      impl.buildCallback();
+      impl.layoutCallback();
+
+      expect(ampImg.querySelector('img[src*="sample.jpg"]')).to.exist;
+      expect(ampImg.querySelector('img[src*="image/svg+xml"]')).to.exist;
+    });
+
+    it('does not interfere with SSR img before placeholder', () => {
+      const impl = getImgWithBlur(true, true);
+      const ampImg = impl.element;
+      ampImg.setAttribute('i-amphtml-ssr', '');
+
+      const img = document.createElement('img');
+      img.src = ampImg.getAttribute('src');
+      ampImg.insertBefore(img, impl.getPlaceholder());
+
+      impl.buildCallback();
+      impl.layoutCallback();
+
+      expect(ampImg.querySelector('img[src*="sample.jpg"]')).to.exist;
+      expect(ampImg.querySelector('img[src*="image/svg+xml"]')).to.exist;
+    });
+
+    it('does not interfere with SSR img after placeholder', () => {
+      const impl = getImgWithBlur(true, true);
+      const ampImg = impl.element;
+      ampImg.setAttribute('i-amphtml-ssr', '');
+
+      const img = document.createElement('img');
+      img.src = ampImg.getAttribute('src');
+      ampImg.appendChild(img);
+
+      impl.buildCallback();
+      impl.layoutCallback();
+
+      expect(ampImg.querySelector('img[src*="sample.jpg"]')).to.exist;
+      expect(ampImg.querySelector('img[src*="image/svg+xml"]')).to.exist;
     });
   });
 
