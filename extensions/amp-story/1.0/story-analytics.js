96c911412964c6092471a4bb39828fef10bd2c2a
Matias Szylkowski <mszylkowski@google.com>
2020-06-17 13:22:18 -0400

    ♻️Rename reaction components to interactive components (#28923)
    * Changed reaction -> interactive
    
    * Fix naming
    
    * interaction -> interactive
    
    * Change test name
    
    * Update presubmit-checks.js
    
    * Changed name to test
    
    * Force test restart with useless commit

--- extensions/amp-story/1.0/story-analytics.js
+++ extensions/amp-story/1.0/story-analytics.js
@@ -37,7 +37,7 @@ export const StoryAnalyticsEvent = {
   PAGE_ATTACHMENT_ENTER: 'story-page-attachment-enter',
   PAGE_ATTACHMENT_EXIT: 'story-page-attachment-exit',
   PAGE_VISIBLE: 'story-page-visible',
-  REACTION: 'story-reaction',
+  INTERACTIVE: 'story-interactive',
   STORY_MUTED: 'story-audio-muted',
   STORY_UNMUTED: 'story-audio-unmuted',
 };
