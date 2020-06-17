a961af0fd3383a1e6683a50ddc97cd215c0aaecf
William Chou <willchou@google.com>
2020-06-16 23:02:28 -0400

    Upgrade karma-sauce-launcher to 4.1.5 (#28906)
    * Upgrade karma-sauce-launcher.
    
    * Disallow failures in remote-tests.js.
    
    * Move timeout to sauceLabs config.
    
    * Revert unblocking change to move to separate PR.

--- build-system/tasks/runtime-test/runtime-test-base.js
+++ build-system/tasks/runtime-test/runtime-test-base.js
@@ -57,10 +57,6 @@ function updateBrowsers(config) {
           'SL_Edge',
           'SL_Safari',
           'SL_IE',
-          // TODO(amp-infra): Evaluate and add more platforms here.
-          //'SL_Chrome_Android_7',
-          //'SL_iOS_11',
-          //'SL_iOS_12',
           'SL_Chrome_Beta',
           'SL_Firefox_Beta',
         ],
