d232c012b9ad551148adb0339de9db4073dea044
William Chou <willchou@google.com>
2020-06-17 14:46:08 -0400

    Punt on test-amp-recaptcha-input.js for now.

--- test/integration/test-amp-recaptcha-input.js
+++ test/integration/test-amp-recaptcha-input.js
@@ -18,7 +18,9 @@ import {BrowserController, RequestBank} from '../../testing/test-helper';
 import {Deferred} from '../../src/utils/promise';
 import {poll} from '../../testing/iframe';
 
-const t = describe.configure();
+// TODO(wg-ui-and-a11y, #28343): These tests cause SauceLabs disconnections,
+// possibly related to the recaptcha inner iframe at *.recaptcha.localhost.
+const t = describe.configure().skipSauceLabs();
 
 t.run('amp-recaptcha-input', function () {
   describes.integration(
