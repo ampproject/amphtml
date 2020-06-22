116d8ae36f75664d6e9e6a25915891c82d286409
Kevin Dwan <25626770+krdwan@users.noreply.github.com>
2020-06-22 15:08:48 -0400

    ✨ [amp-social-share][bento][amp] Amp-social-share for bento with some re-design of preact component (#28465)
    * First draft amp-social-share for bento
    
    * Update social-share to spread params
    
    * Adding isLayoutSupported()
    
    * Remove binding functionality from Preact component
    
    * Removed unneeded constants
    
    * Changed Preact endpoint to be a function
    
    * Clean up code and fix travis check
    
    * update storybook
    
    * Fix travis checks
    
    * Fix type-check issues
    
    * Attempt some changes on closure types
    
    * Update type definitions on config file
    
    * Initial changes for social-share, move params to its own prop
    
    * Interim changes to Preact storybook
    
    * Fix type error by assigning to static dict
    
    * Fix type issue, problem was putting a prototype-less object as an attribute in html element
    
    * Update social share config file and rename
    
    * Update to storybook

--- extensions/amp-social-share/1.0/social-share.js
+++ extensions/amp-social-share/1.0/social-share.js
@@ -20,24 +20,35 @@ import {Keys} from '../../../src/utils/key-codes';
 import {SocialShareIcon} from '../../../third_party/optimized-svg-icons/social-share-svgs';
 import {addParamsToUrl, parseQueryString} from '../../../src/url';
 import {dict} from '../../../src/utils/object';
-import {getSocialConfig} from './amp-social-share-config';
-import {isObject} from '../../../src/types';
+import {getSocialConfig} from './social-share-config';
 import {openWindowDialog} from '../../../src/dom';
 import {useResourcesNotify} from '../../../src/preact/utils';
 
 const DEFAULT_WIDTH = 60;
 const DEFAULT_HEIGHT = 44;
+const DEFAULT_TARGET = '_blank';
 const NAME = 'SocialShare';
+
 /**
  * @param {!JsonObject} props
+ *  type: !string,
+ *  endpoint: ?string,
+ *  params: ?JsonObject,
+ *  target: ?string,
+ *  width: ?string,
+ *  height: ?string,
+ *  tabIndex: ?string,
+ *  style: ?string,
  * @return {PreactDef.Renderable}
  */
 export function SocialShare(props) {
   useResourcesNotify();
-  const {typeConfig, baseEndpoint, checkedWidth, checkedHeight} = checkProps(
-    props
-  );
-  const finalEndpoint = createEndpoint(typeConfig, baseEndpoint, props);
+  const {
+    finalEndpoint,
+    checkedWidth,
+    checkedHeight,
+    checkedTarget,
+  } = checkProps(props);
 
   const type = props['type'].toUpperCase();
   const baseStyle = CSS.BASE_STYLE;
@@ -50,10 +61,9 @@ export function SocialShare(props) {
     <div
       role="button"
       tabindex={props['tabIndex'] || '0'}
-      onKeyDown={(e) => handleKeyPress(e, finalEndpoint)}
-      onClick={() => handleActivation(finalEndpoint)}
+      onKeyDown={(e) => handleKeyPress(e, finalEndpoint, checkedTarget)}
+      onClick={() => handleActivation(finalEndpoint, checkedTarget)}
       style={{...size, ...props['style']}}
-      {...props}
     >
       <SocialShareIcon
         style={{...backgroundStyle, ...baseStyle, ...size}}
@@ -66,20 +76,20 @@ export function SocialShare(props) {
 /**
  * @param {!JsonObject} props
  * @return {{
- *   typeConfig: !JsonObject,
- *   baseEndpoint: ?string,
- *   checkedWidth: ?number,
- *   checkedHeight: ?number,
+ *   finalEndpoint: string,
+ *   checkedWidth: number,
+ *   checkedHeight: number,
+ *   checkedTarget: string,
  * }}
  */
 function checkProps(props) {
   const {
     'type': type,
-    'shareEndpoint': shareEndpoint,
-    'params': params,
-    'bindings': bindings,
+    'endpoint': endpoint,
+    'target': target,
     'width': width,
     'height': height,
+    'params': params,
   } = props;
 
   // Verify type is provided
@@ -87,31 +97,37 @@ function checkProps(props) {
     throw new Error(`The type attribute is required. ${NAME}`);
   }
 
-  // bindings and params props must be objects
-  if (params && !isObject(params)) {
-    throw new Error(`The params property should be an object. ${NAME}`);
-  }
-  if (bindings && !isObject(bindings)) {
-    throw new Error(`The bindings property should be an object. ${NAME}`);
-  }
-
-  // User must provide shareEndpoint if they choose a type that is not
+  // User must provide endpoint if they choose a type that is not
   // pre-configured
   const typeConfig = getSocialConfig(type) || dict();
-  const baseEndpoint = typeConfig['shareEndpoint'] || shareEndpoint;
+  let baseEndpoint = endpoint || typeConfig['shareEndpoint'];
   if (baseEndpoint === undefined) {
     throw new Error(
-      `A shareEndpoint is required if not using a pre-configured type. ${NAME}`
+      `An endpoint is required if not using a pre-configured type. ${NAME}`
     );
   }
 
+  // Special case when type is 'email'
+  if (type === 'email' && !endpoint) {
+    baseEndpoint = `mailto:${params['recipient'] || ''}`;
+  }
+
+  // Add params to baseEndpoint
+  const finalEndpoint = addParamsToUrl(
+    /** @type {string} */ (baseEndpoint),
+    /** @type {!JsonObject} */ (params)
+  );
+
+  // Defaults
   const checkedWidth = width || DEFAULT_WIDTH;
   const checkedHeight = height || DEFAULT_HEIGHT;
+  const checkedTarget = target || DEFAULT_TARGET;
+
   return {
-    typeConfig,
-    baseEndpoint,
+    finalEndpoint,
     checkedWidth,
     checkedHeight,
+    checkedTarget,
   };
 }
 
@@ -124,44 +140,11 @@ function throwWarning(message) {
 }
 
 /**
- * @param {!JsonObject} typeConfig
- * @param {?string} baseEndpoint
- * @param {!JsonObject} props
- * @return {?string}
- */
-function createEndpoint(typeConfig, baseEndpoint, props) {
-  const {'params': params, 'bindings': bindings} = props;
-  const combinedParams = {...typeConfig['defaultParams'], ...params};
-  const endpointWithParams = addParamsToUrl(
-    /** @type {string} */ (baseEndpoint),
-    /** @type {!JsonObject} */ (combinedParams)
-  );
-
-  const combinedBindings = dict();
-  const bindingVars = typeConfig['bindings'];
-  if (bindingVars) {
-    /** @type {Array<string>} */ (bindingVars).forEach((name) => {
-      combinedBindings[name.toUpperCase()] = combinedParams[name] || '';
-    });
-  }
-  if (bindings) {
-    Object.keys(bindings).forEach((name) => {
-      combinedBindings[name.toUpperCase()] = bindings[name] || '';
-    });
-  }
-  const finalEndpoint = Object.keys(combinedBindings).reduce(
-    (endpoint, binding) =>
-      endpoint.replace(new RegExp(binding, 'g'), combinedBindings[binding]),
-    endpointWithParams
-  );
-  return finalEndpoint;
-}
-
-/**
  * Opens a new window with the fully processed endpoint
  * @param {?string} finalEndpoint
+ * @param {string} target
  */
-function handleActivation(finalEndpoint) {
+function handleActivation(finalEndpoint, target) {
   const protocol = finalEndpoint.split(':', 1)[0];
   const windowFeatures = 'resizable,scrollbars,width=640,height=480';
   if (protocol === 'navigator-share') {
@@ -181,11 +164,11 @@ function handleActivation(finalEndpoint) {
     openWindowDialog(
       window,
       finalEndpoint.replace('?', '?&'),
-      '_blank',
+      target,
       windowFeatures
     );
   } else {
-    openWindowDialog(window, finalEndpoint, '_blank', windowFeatures);
+    openWindowDialog(window, finalEndpoint, target, windowFeatures);
   }
 }
 
@@ -205,11 +188,12 @@ function getQueryString(endpoint) {
 /**
  * @param {!Event} event
  * @param {?string} finalEndpoint
+ * @param {string} target
  */
-function handleKeyPress(event, finalEndpoint) {
+function handleKeyPress(event, finalEndpoint, target) {
   const {key} = event;
   if (key == Keys.SPACE || key == Keys.ENTER) {
     event.preventDefault();
-    handleActivation(finalEndpoint);
+    handleActivation(finalEndpoint, target);
   }
 }
