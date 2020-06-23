15dd3b88c3b172f573ccf21730affd2ab462e6ac
Enrique Marroquin <artezan@google.com>
2020-06-23 11:12:17 -0400

    leverage manager code for runtime in entry points

--- src/amp-story-player/amp-story-entry-point/amp-story-entry-point.js
+++ src/amp-story-player/amp-story-entry-point/amp-story-entry-point.js
@@ -13,3 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import {AmpStoryPlayerManager} from '../amp-story-player-manager';
+
+self.onload = () => {
+  const manager = new AmpStoryPlayerManager(self);
+  manager.loadEntryPoints();
+};
