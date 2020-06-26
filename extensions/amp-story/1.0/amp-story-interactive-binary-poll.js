82a2935e505166694f2181e17ac82ba40e6d4d37
Matias Szylkowski <mszylkowski@google.com>
2020-06-26 13:43:03 -0400

    Merged changes from Fixes display of small %

--- extensions/amp-story/1.0/amp-story-interactive-binary-poll.js
+++ extensions/amp-story/1.0/amp-story-interactive-binary-poll.js
@@ -28,6 +28,9 @@ export const FontSize = {
   DOUBLE_LINE: 14,
 };
 
+/** @const {number} */
+const MIN_HORIZONTAL_PADDING = 27;
+
 /**
  * Generates the template for the binary poll.
  *
@@ -204,6 +207,21 @@ export class AmpStoryInteractiveBinaryPoll extends AmpStoryInteractive {
         '.i-amphtml-story-interactive-option-percentage-text'
       ).textContent = `${percentage}%`;
       currOption.setAttribute('style', `flex-grow: ${percentage} !important`);
+
+      if (percentage < 20) {
+        const textContainer = currOption.querySelector(
+          '.i-amphtml-story-interactive-option-text-container'
+        );
+        textContainer.setAttribute(
+          'style',
+          `transform: translateX(${
+            index === 0 ? MIN_HORIZONTAL_PADDING : -MIN_HORIZONTAL_PADDING
+          }%) !important`
+        );
+        if (percentage === 0) {
+          textContainer.setAttribute('style', 'opacity: 0 !important');
+        }
+      }
     });
   }
 }
