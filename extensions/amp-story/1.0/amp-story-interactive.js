6451c32f20488c1950d5be81e91811cc2bada1e2
Matias Szylkowski <mszylkowski@google.com>
2020-06-29 10:54:17 -0400

    Added styles for poll

--- extensions/amp-story/1.0/amp-story-interactive.js
+++ extensions/amp-story/1.0/amp-story-interactive.js
@@ -577,11 +577,37 @@ export class AmpStoryInteractive extends AMP.BaseElement {
    * @private
    */
   retrieveInteractiveData_() {
-    return this.executeInteractiveRequest_('GET').then((response) => {
-      this.handleSuccessfulDataRetrieval_(
-        /** @type {InteractiveResponseType} */ (response)
-      );
+    // return this.executeInteractiveRequest_('GET').then((response) => {
+    //   console.log(response);
+    //   this.handleSuccessfulDataRetrieval_(
+    //     /** @type {InteractiveResponseType} */ (response)
+    //   );
+    // });
+    this.handleSuccessfulDataRetrieval_({
+      'options': [
+        {
+          'optionIndex': 0,
+          'selectedByUser': false,
+          'totalCount': Math.random() * 100,
+        },
+        {
+          'optionIndex': 1,
+          'selectedByUser': false,
+          'totalCount': Math.random() * 100,
+        },
+        {
+          'optionIndex': 2,
+          'selectedByUser': false,
+          'totalCount': Math.random() * 100,
+        },
+        {
+          'optionIndex': 3,
+          'selectedByUser': false,
+          'totalCount': Math.random() * 100,
+        },
+      ],
     });
+    return Promise.resolve();
   }
 
   /**
