1b25e09eeb6314719e72c04b1809683cc2670910
Jake Fried <samouri@users.noreply.github.com>
2020-07-01 10:50:09 -0400

    amp-date-picker: restore rrule support (#28887)
    * amp-date-picker: restore rrule support, via a fork
    
    * patch instead of fork, but also dont auto-update
    
    * misplaced semicolon
    
    * Revert "Remove RRule support (#25230)"
    
    This reverts commit 11e9de2afe47eeac78082130ad18cb1e1f3500a0.
    
    * liven up the test fixture
    
    * use es5 bc cc cant handle the build
    
    * support both browserify/cc
    
    * lint
    
    * remove renovate change
    
    * remove unecessary tslib
    
    * reduce yarn.lock diffsize
    
    * cc typefix
    
    * thanks justin
    
    * bring back warnings, but incorrect behavior
    
    * super nit

--- build-system/tasks/update-packages.js
+++ build-system/tasks/update-packages.js
@@ -96,6 +96,29 @@ function patchIntersectionObserver() {
 }
 
 /**
+ * TODO(samouri): remove this patch when a better fix is upstreamed (https://github.com/jakubroztocil/rrule/pull/410).
+ *
+ * Patches rrule to remove references to luxon. Even though rrule marks luxon as an optional dependency,
+ * it is used as if it's a required one (static import). rrule relies on its consumers either
+ * installing luxon or adding it as a webpack-style external. We don't want the former and
+ * can't yet do the latter.
+ *
+ * This function replaces the reference to luxon with a mock that throws (which the code handles well).
+ */
+function patchRRule() {
+  const path = 'node_modules/rrule/dist/es5/rrule.min.js';
+  const patchedContents = fs
+    .readFileSync(path)
+    .toString()
+    .replace(
+      /require\("luxon"\)/g,
+      `{ DateTime: { fromJSDate() { throw TypeError() } } }`
+    );
+
+  writeIfUpdated(path, patchedContents);
+}
+
+/**
  * Does a yarn check on node_modules, and if it is outdated, runs yarn.
  */
 function runYarnCheck() {
@@ -145,6 +168,7 @@ async function updatePackages() {
   }
   patchWebAnimations();
   patchIntersectionObserver();
+  patchRRule();
 }
 
 module.exports = {
