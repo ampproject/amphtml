419cea49116afcd2f07b0f221bc35625137a89b8
Matias Szylkowski <mszylkowski@google.com>
2020-07-06 12:49:20 -0400

    ✨ Implement story Interactive poll (#29079)
    * Renamed binary polls
    
    * Started implementation
    
    * Worked on styles
    
    * Added styles for poll
    
    * Changed example story
    
    * Started implementation
    
    * Worked on styles
    
    * Added styles for poll
    
    * Changed example story
    
    * Tabs to spaces
    
    * Removed log
    
    * Added rtl
    
    * Fixed rtl animations
    
    * Added no-endpoint style, split components
    
    * Split story components into pages
    
    * Fixed translateX, moved variables
    
    * Removed rtl
    
    * Fixed -200%
    
    * Removed unncecessary styles
    
    * Fixed year
    
    * Commented ltr css
    
    * Renamed animations
    
    * Fixed css
    
    * Forgot comma
    
    * Modified color

--- build-system/compile/bundles.config.js
+++ build-system/compile/bundles.config.js
@@ -881,6 +881,7 @@ exports.extensionBundles = [
         'amp-story-info-dialog',
         'amp-story-interactive',
         'amp-story-interactive-binary-poll',
+        'amp-story-interactive-poll',
         'amp-story-interactive-quiz',
         'amp-story-share',
         'amp-story-share-menu',
