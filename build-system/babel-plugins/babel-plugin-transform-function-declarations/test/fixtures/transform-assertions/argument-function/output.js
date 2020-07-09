37c8b29079d4abd6ab58df67d101d78af71a4271
Derek X. R. Tse <arrestrose@gmail.com>
2020-07-09 15:27:48 -0700

    🐛 #28698: Fix for bug that was incorrectly transforming anonymous function to arrow function. (#29109)
    * added condition to bail out registerService
    
    * updated existing unit test for bug

--- build-system/babel-plugins/babel-plugin-transform-function-declarations/test/fixtures/transform-assertions/argument-function/output.js
+++ build-system/babel-plugins/babel-plugin-transform-function-declarations/test/fixtures/transform-assertions/argument-function/output.js
@@ -16,3 +16,6 @@
 registerServiceBuilder(win, 'story-analytics', function () {
   return new StoryAnalyticsService();
 });
+
+geoDeferred = new Deferred();
+AMP.registerServiceForDoc('foo', function(){return geoDeferred.promise;});
