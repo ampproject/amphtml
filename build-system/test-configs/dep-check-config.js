12cf5af37ec2c851264a9d2748610f7f650dfd40
Caleb Cordry <ccordry@google.com>
2020-07-10 13:05:37 -0700

    ✨A4A: introduce head validation (#29167)
    * head validation
    
    * rename
    
    * add tests
    
    * docs and types
    
    * allow preloadExtension
    
    * comments
    
    * rename func
    
    * dep check
    
    * dep check again
    
    * toUpperCase

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -278,6 +278,9 @@ exports.rules = [
         'src/service/variable-source.js',
       'extensions/amp-a4a/0.1/amp-a4a.js->' +
         'src/service/url-replacements-impl.js',
+      // Parsing extension urls.
+      'extensions/amp-a4a/0.1/head-validation.js->' +
+        'src/service/extension-location.js',
       'extensions/amp-video/0.1/amp-video.js->' +
         'src/service/video-manager-impl.js',
       'extensions/amp-video-iframe/0.1/amp-video-iframe.js->' +
