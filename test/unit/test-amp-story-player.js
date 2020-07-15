f9529afe7f2854990d6fafa6d9385a73bbabe0c9
bettyashley <bettytuckerr@gmail.com>
2020-07-15 12:12:34 -0400

    Make tests check if event was dispatched with correct arguments.

--- test/unit/test-amp-story-player.js
+++ test/unit/test-amp-story-player.js
@@ -371,7 +371,22 @@ describes.realWin('AmpStoryPlayer', {amp: false}, (env) => {
       );
     });
 
-    it.only('signals when player changed story', async () => {
+    it('signals when player changed story using next method', async () => {
+      const playerEl = win.document.createElement('amp-story-player');
+      appendStoriesToPlayer(playerEl, 5);
+
+      const player = new AmpStoryPlayer(win, playerEl);
+
+      await player.load();
+
+      const navigationSpy = env.sandbox.spy();
+      playerEl.addEventListener('navigation', navigationSpy);
+      player.next_();
+      expect(navigationSpy.firstCall.args[0].type).to.eql('navigation');
+      expect(navigationSpy.firstCall.args[0].detail).to.eql({index: 1, remaining: 3});
+    });
+
+    it('signals when player changed story using previous method', async () => {
       const playerEl = win.document.createElement('amp-story-player');
       appendStoriesToPlayer(playerEl, 5);
 
@@ -383,7 +398,8 @@ describes.realWin('AmpStoryPlayer', {amp: false}, (env) => {
       playerEl.addEventListener('navigation', navigationSpy);
       player.next_();
       player.previous_();
-      expect(navigationSpy).to.have.been.calledTwice;
+      expect(navigationSpy.secondCall.args[0].type).to.eql('navigation');
+      expect(navigationSpy.secondCall.args[0].detail).to.eql({index: 0, remaining: 4});
     });
   });
 });
