ea420563b87ff42bf3af847b61e1e86b33a887f4
processprocess <philip.hunter.bell@gmail.com>
2020-07-20 11:58:40 -0400

    Format function level constants.

--- extensions/amp-story/1.0/interactive-confetti.js
+++ extensions/amp-story/1.0/interactive-confetti.js
@@ -57,19 +57,19 @@ const buildconfettiTemplate = (element) => {
  * @return {void}
  */
 export function emojiConfetti(rootEl, win, confettiEmoji) {
-  const CONFETTI_COUNT = 5;
-  const SLICE = (Math.PI * 2) / CONFETTI_COUNT;
+  const confettiCount = 5;
+  const angleSlice = (Math.PI * 2) / confettiCount;
 
-  const ANGLE_RANDOMNESS = SLICE * 0.2;
-  const FONT_SIZE_RANDOM_RANGE = [30, 50];
-  const ROTATION_RANDOMNESS = 20;
-  const ADDITIONAL_DISTANCE = 5;
+  const angleRandomness = angleSlice * 0.2;
+  const fontSizeRandomRange = [30, 50];
+  const rotationRandomness = 20;
+  const additionalDistance = 5;
 
-  const ANIMATION_TIME = 300;
-  const ANIMATION_OUT_DELAY = 1000;
+  const animationTime = 300;
+  const animationOutDelay = 1000;
 
   // To calculate confetti transform distance.
-  const ROOT_EL_RECT = rootEl./*OK*/ getBoundingClientRect();
+  const rootElRect = rootEl./*OK*/ getBoundingClientRect();
 
   /** @private @const {!../../../src/service/timer-impl.Timer} */
   const timer = Services.timerFor(win);
@@ -79,21 +79,18 @@ export function emojiConfetti(rootEl, win, confettiEmoji) {
 
   timer.delay(() => {
     // Generate confetti. Set their ending position, size and rotation.
-    for (let i = 0; i < CONFETTI_COUNT; i++) {
+    for (let i = 0; i < confettiCount; i++) {
       const confetti = buildconfettiTemplate(rootEl);
       confettiWrapper.appendChild(confetti);
       confetti.textContent = confettiEmoji;
 
       const fontSize =
-        randomInRange(FONT_SIZE_RANDOM_RANGE[0], FONT_SIZE_RANDOM_RANGE[1]) +
-        'px';
+        randomInRange(fontSizeRandomRange[0], fontSizeRandomRange[1]) + 'px';
       const angle =
-        SLICE * i + randomInRange(-ANGLE_RANDOMNESS, ANGLE_RANDOMNESS);
-      const x =
-        Math.sin(angle) * (ROOT_EL_RECT.width / 2 + ADDITIONAL_DISTANCE);
-      const y =
-        Math.cos(angle) * (ROOT_EL_RECT.height / 2 + ADDITIONAL_DISTANCE);
-      const rotation = randomInRange(-ROTATION_RANDOMNESS, ROTATION_RANDOMNESS);
+        angleSlice * i + randomInRange(-angleRandomness, angleRandomness);
+      const x = Math.sin(angle) * (rootElRect.width / 2 + additionalDistance);
+      const y = Math.cos(angle) * (rootElRect.height / 2 + additionalDistance);
+      const rotation = randomInRange(-rotationRandomness, rotationRandomness);
       setStyles(confetti, {
         fontSize,
         transform: `translate(${x}px, ${y}px) rotate(${rotation}deg)`,
@@ -110,9 +107,9 @@ export function emojiConfetti(rootEl, win, confettiEmoji) {
         'i-amphtml-story-interactive-confetti-wrapper-animate-out'
       );
       // Remove the wrapper from the dom.
-      timer.delay(() => rootEl.removeChild(confettiWrapper), ANIMATION_TIME);
-    }, ANIMATION_OUT_DELAY);
-  }, ANIMATION_TIME);
+      timer.delay(() => rootEl.removeChild(confettiWrapper), animationTime);
+    }, animationOutDelay);
+  }, animationTime);
 }
 
 /**
