08e5e860fecbe48197279669cc9577eb4084e204
Rafer45 <ataque992@hotmail.com>
2020-07-21 11:44:56 -0400

    🏗 [test-case-reporting] Add report flag to gulp tasks of tests (#29370)
    * Add report flag to test gulp tasks
    
    * Add flags in unit.js and integration.js
    
    * Remove resultFilename function
    
    * Remove bad require

--- build-system/tasks/runtime-test/runtime-test-base.js
+++ build-system/tasks/runtime-test/runtime-test-base.js
@@ -28,12 +28,10 @@ const {createKarmaServer, getAdTypes} = require('./helpers');
 const {getFilesFromArgv} = require('../../common/utils');
 const {green, yellow, cyan, red} = require('ansi-colors');
 const {isGithubActionsBuild} = require('../../common/github-actions');
-const {isTravisBuild, isTravisPushBuild} = require('../../common/travis');
+const {isTravisBuild} = require('../../common/travis');
 const {reportTestStarted} = require('.././report-test-status');
 const {startServer, stopServer} = require('../serve');
 const {unitTestsToRun} = require('./helpers-unit');
-
-const JSON_REPORT_TEST_TYPES = new Set(['unit', 'integration']);
 
 /**
  * Updates the browsers based off of the test type
@@ -168,10 +166,10 @@ function updateReporters(config) {
     config.reporters.push('coverage-istanbul');
   }
 
-  if (isTravisPushBuild() && JSON_REPORT_TEST_TYPES.has(config.testType)) {
+  if (argv.report) {
     config.reporters.push('json-result');
     config.jsonResultReporter = {
-      outputFile: `results_${config.testType}.json`,
+      outputFile: `result-reports/${config.testType}.json`,
     };
   }
 }
