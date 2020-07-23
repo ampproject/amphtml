948a62db1c80cf1ac51ef0d73ecd92c1c49eaf22
Connie Lei <23634958+connielei@users.noreply.github.com>
2020-07-23 12:17:20 -0400

    Update src/amp-story-player/amp-story-player-impl.js
    Co-authored-by: Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>

--- src/amp-story-player/amp-story-player-impl.js
+++ src/amp-story-player/amp-story-player-impl.js
@@ -899,13 +899,7 @@ export class AmpStoryPlayer {
    * @private
    */
   dispatchPageAttachmentEvent_(isPageAttachmentOpen) {
-    isPageAttachmentOpen
-      ? this.element_.dispatchEvent(
-          createCustomEvent(this.win_, 'page-attachment-open')
-        )
-      : this.element_.dispatchEvent(
-          createCustomEvent(this.win_, 'page-attachment-close')
-        );
+    this.element_.dispatchEvent(createCustomEvent(this.win_, isPageAttachmentOpen ? 'page-attachment-open' : 'page-attachment-closed');
   }
 
   /**
