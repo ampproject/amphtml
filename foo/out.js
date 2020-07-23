32e504d480da80e34c570cf4d17b378818822935
Derek Tse <derektse@google.com>
2020-07-22 18:47:19 -0700

    remove extra folder

--- foo/out.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello, World!");var b=function(){return 6};function remove(a,f){for(var e=[],b=0,c=0;c<a.length;c++){var d=a[c];f(d,c,a)?e.push(d):(b<c&&(a[b]=d),b++)}b<a.length&&(a.length=b);return e};
