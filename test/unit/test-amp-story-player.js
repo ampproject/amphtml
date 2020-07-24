b2d47fe3252ae6562a74df80f005d9d4cca7006b
Connie Lei <cl878@cornell.edu>
2020-07-24 09:19:49 -0400

    fix register handler test

--- test/unit/test-amp-story-player.js
+++ test/unit/test-amp-story-player.js
@@ -208,6 +208,7 @@ describes.realWin('AmpStoryPlayer', {amp: false}, (env) => {
     messagingMock.expects('registerHandler').withArgs('touchstart');
     messagingMock.expects('registerHandler').withArgs('touchmove');
     messagingMock.expects('registerHandler').withArgs('touchend');
+    messagingMock.expects('registerHandler').withArgs('documentStateUpdate');
     messagingMock.expects('setDefaultHandler');
 
     await manager.loadPlayers();
