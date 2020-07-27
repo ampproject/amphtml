b1f910d7dcf9cf6aa43b8bcaf3a790a56e62da55
bettyashley <bettytuckerr@gmail.com>
2020-07-27 17:29:08 -0400

    Use PR 29492 as a starting point.

--- src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
+++ src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
@@ -15,7 +15,23 @@
  */
 
 // Source for this constant is css/amp-story-entry-point.css
+import {createCustomEvent} from '../../event-helper';
 import {cssText} from '../../../build/amp-story-entry-point.css';
+import {escapeCssSelectorIdent} from '../../css';
+import {setStyle} from '../../style';
+import {toArray} from '../../types';
+
+/** @enum {string} */
+const STYLE_TYPES = {
+  RECTANGULAR: 'rectangle',
+  CIRCULAR: 'circle',
+};
+
+/** @enum {string} */
+const STYLE_CLASSES = {
+  [STYLE_TYPES.RECTANGULAR]: 'rectangular-entry-point',
+  [STYLE_TYPES.CIRCULAR]: 'circular-entry-point',
+};
 
 /**
  * <amp-story-entry-point> component for embedding stories and launching them in
@@ -48,6 +64,12 @@ export class AmpStoryEntryPoint {
 
     /** @private {?Element} */
     this.rootEl_ = null;
+
+    /** @private {?AmpStoryPlayer} */
+    this.player_ = null;
+
+    /** @private {!Array<!HTMLAnchorElement>} */
+    this.stories_ = [];
   }
 
   /** @public */
@@ -55,6 +77,12 @@ export class AmpStoryEntryPoint {
     if (this.isBuilt_) {
       return;
     }
+
+    const playerId = this.element_.getAttribute('player-id');
+    this.player_ = this.doc_.querySelector(
+      escapeCssSelectorIdent(`#${playerId}`)
+    );
+    this.stories_ = toArray(this.player_.querySelectorAll('a'));
 
     this.initializeShadowRoot_();
 
@@ -67,6 +95,8 @@ export class AmpStoryEntryPoint {
       return;
     }
 
+    this.initializeCarousel_();
+    this.initializeCarouselArrow_();
     this.isLaidOut_ = true;
   }
 
@@ -82,6 +112,122 @@ export class AmpStoryEntryPoint {
     styleEl.textContent = cssText;
     shadowRoot.appendChild(styleEl);
     shadowRoot.appendChild(this.rootEl_);
+  }
+
+  /** @private */
+  initializeGrid_() {
+    this.element_.setAttribute('entry-point-style', rectangle);
+    const style = this.element_.getAttribute('entry-point-style');
+    const styleClass = !Object.values(STYLE_TYPES).includes(style)
+      ? STYLE_CLASSES['rectangle']
+      : STYLE_CLASSES[style];
+
+    const entryPointsContainer = this.doc_.createElement('div');
+    entryPointsContainer.classList.add('entry-points');
+
+    for (let i = 0; i < this.stories_.length; i++) {
+      const story = this.stories_[i];
+      const posterSrc = story.getAttribute('data-poster-portrait-src');
+      const poster = this.initializePoster_(
+        posterSrc,
+        styleClass,
+        story.href,
+        i
+      );
+      entryPointsContainer.append(poster);
+    }
+
+    this.rootEl_.append(entryPointsContainer);
+  }
+
+  /** @private */
+  initializeCarousel_() {
+    const style = this.element_.getAttribute('entry-point-style');
+    const styleClass = !Object.values(STYLE_TYPES).includes(style)
+      ? STYLE_CLASSES['rectangle']
+      : STYLE_CLASSES[style];
+
+    const entryPointsContainer = this.doc_.createElement('div');
+    entryPointsContainer.classList.add('entry-points');
+
+    for (let i = 0; i < this.stories_.length; i++) {
+      const story = this.stories_[i];
+      const posterSrc = story.getAttribute('data-poster-portrait-src');
+      const poster = this.initializePoster_(
+        posterSrc,
+        styleClass,
+        story.href,
+        i
+      );
+      entryPointsContainer.append(poster);
+    }
+
+    this.rootEl_.append(entryPointsContainer);
+  }
+
+  /**
+   * Initializes an image element to be displayed.
+   * @param {string} posterSrc
+   * @param {string} styleClass
+   * @param {string} href
+   * @param {number} index
+   * @return {HTMLImageElement} poster
+   * @private
+   */
+  initializePoster_(posterSrc, styleClass, href, index) {
+    const poster = this.doc_.createElement('img');
+    poster.src = posterSrc;
+    poster.classList.add(styleClass);
+    poster.classList.add('entry-point');
+    this.setPosterWidthHeight_(poster, styleClass);
+    this.onPosterClick_(poster, href, index);
+    return poster;
+  }
+
+  /**
+   * Sets the correct height and width for poster based on client height.
+   * @param {HTMLImageElement} poster
+   * @param {string} styleClass
+   * @private
+   */
+  setPosterWidthHeight_(poster, styleClass) {
+    poster.height = this.element_.clientHeight;
+    if (styleClass === 'circular-entry-point') {
+      poster.width = this.element_.clientHeight;
+    }
+  }
+
+  /**
+   * Reacts to on click event for an entry point.
+   * @param {HTMLImageElement} poster
+   * @param {string} href
+   * @param {number} index
+   * @private
+   */
+  onPosterClick_(poster, href, index) {
+    poster.addEventListener('click', () => {
+      this.player_.show(href);
+      this.element_.dispatchEvent(
+        createCustomEvent(this.win_, 'entryPointClicked', {href, index})
+      );
+    });
+  }
+
+  /** @private */
+  initializeCarouselArrow_() {
+    // check if desktop mode
+    const leftButton = this.doc_.createElement('button');
+    const rightButton = this.doc_.createElement('button');
+    leftButton.classList.add('entry-point-left-arrow');
+    rightButton.classList.add('entry-point-right-arrow');
+
+    const top = this.element_.clientHeight / 2 - 20;
+    setStyle(leftButton, 'top', top);
+    setStyle(rightButton, 'top', top);
+    setStyle(rightButton, 'left', this.element_.clientWidth - 10);
+
+    this.rootEl_.append(leftButton);
+    this.rootEl_.append(rightButton);
   }
 
   /**
