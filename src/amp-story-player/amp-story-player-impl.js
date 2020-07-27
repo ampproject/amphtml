dcfd7aff7b9f6dc851bb065fbe92b86f79906db0
Jose Ramirez Fuentes <josedramirez@google.com>
2020-07-27 13:20:35 -0400

    Change for checking autoplay attribute

--- src/amp-story-player/amp-story-player-impl.js
+++ src/amp-story-player/amp-story-player-impl.js
@@ -143,7 +143,7 @@ export class AmpStoryPlayer {
     this.currentIdx_ = 0;
 
     /** @private {boolean} */
-    this.isAutoplaying_ = this.element_.getAttribute('autoplay') !== null;
+    this.isAutoplaying_ = this.element_.hasAttribute('autoplay');
 
     /** @private {!SwipingState} */
     this.swipingState_ = SwipingState.NOT_SWIPING;
