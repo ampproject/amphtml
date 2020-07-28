4fdb4a37202bcc9a38b4cad0fc4333ada4c1878d
bettyashley <bettytuckerr@gmail.com>
2020-07-28 16:27:09 -0400

    Update CSS styling.

--- src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
+++ src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
@@ -17,7 +17,6 @@
 // Source for this constant is css/amp-story-entry-point.css
 import {createCustomEvent} from '../../event-helper';
 import {cssText} from '../../../build/amp-story-entry-point.css';
-import {escapeCssSelectorIdent} from '../../css';
 import {setStyle} from '../../style';
 import {toArray} from '../../types';
 
@@ -79,9 +78,7 @@ export class AmpStoryEntryPoint {
     }
 
     const playerId = this.element_.getAttribute('player-id');
-    this.player_ = this.doc_.querySelector(
-      escapeCssSelectorIdent(`#${playerId}`)
-    );
+    this.player_ = this.doc_.querySelector(`#${playerId}`);
     this.stories_ = toArray(this.player_.querySelectorAll('a'));
 
     this.initializeShadowRoot_();
@@ -95,6 +92,7 @@ export class AmpStoryEntryPoint {
       return;
     }
 
+    this.initializeGrid_();
     this.initializeCarousel_();
     this.initializeCarouselArrow_();
     this.isLaidOut_ = true;
@@ -116,28 +114,27 @@ export class AmpStoryEntryPoint {
 
   /** @private */
   initializeGrid_() {
-    this.element_.setAttribute('entry-point-style', rectangle);
     const style = this.element_.getAttribute('entry-point-style');
     const styleClass = !Object.values(STYLE_TYPES).includes(style)
       ? STYLE_CLASSES['rectangle']
       : STYLE_CLASSES[style];
 
-    const entryPointsContainer = this.doc_.createElement('div');
-    entryPointsContainer.classList.add('entry-points');
+      const cardsContainer = this.doc_.createElement('div');
+      cardsContainer.classList.add('entry-points');
+      this.rootEl_.append(cardsContainer);
 
     for (let i = 0; i < this.stories_.length; i++) {
       const story = this.stories_[i];
-      const posterSrc = story.getAttribute('data-poster-portrait-src');
-      const poster = this.initializePoster_(
-        posterSrc,
+      const src = story.getAttribute('data-poster-portrait-src');
+      const card = this.initializeCard_(
+        src,
         styleClass,
         story.href,
         i
       );
-      entryPointsContainer.append(poster);
-    }
-
-    this.rootEl_.append(entryPointsContainer);
+      const cardContainer = this.doc_.createElement('div');
+      cardContainer.append(card);
+      cardsContainer.append(cardContainer);
   }
 
   /** @private */
@@ -147,65 +144,66 @@ export class AmpStoryEntryPoint {
       ? STYLE_CLASSES['rectangle']
       : STYLE_CLASSES[style];
 
-    const entryPointsContainer = this.doc_.createElement('div');
-    entryPointsContainer.classList.add('entry-points');
+    const cardsContainer = this.doc_.createElement('div');
+    cardsContainer.classList.add('entry-points');
+    this.rootEl_.append(cardsContainer);
 
     for (let i = 0; i < this.stories_.length; i++) {
       const story = this.stories_[i];
-      const posterSrc = story.getAttribute('data-poster-portrait-src');
-      const poster = this.initializePoster_(
-        posterSrc,
+      const src = story.getAttribute('data-poster-portrait-src');
+      const card = this.initializeCard_(
+        src,
         styleClass,
         story.href,
         i
       );
-      entryPointsContainer.append(poster);
+      const cardContainer = this.doc_.createElement('div');
+      cardContainer.append(card);
+      cardsContainer.append(cardContainer);
     }
-
-    this.rootEl_.append(entryPointsContainer);
   }
 
   /**
    * Initializes an image element to be displayed.
-   * @param {string} posterSrc
+   * @param {string} src
    * @param {string} styleClass
    * @param {string} href
    * @param {number} index
    * @return {HTMLImageElement} poster
    * @private
    */
-  initializePoster_(posterSrc, styleClass, href, index) {
-    const poster = this.doc_.createElement('img');
-    poster.src = posterSrc;
-    poster.classList.add(styleClass);
-    poster.classList.add('entry-point');
-    this.setPosterWidthHeight_(poster, styleClass);
-    this.onPosterClick_(poster, href, index);
-    return poster;
+  initializeCard_(src, styleClass, href, index) {
+    const card = this.doc_.createElement('img');
+    card.src = src;
+    card.classList.add(styleClass);
+    card.classList.add('entry-point');
+    this.setCardWidthHeight_(card, styleClass);
+    this.onCardClick_(card, href, index);
+    return card;
   }
 
   /**
    * Sets the correct height and width for poster based on client height.
-   * @param {HTMLImageElement} poster
+   * @param {HTMLImageElement} card
    * @param {string} styleClass
    * @private
    */
-  setPosterWidthHeight_(poster, styleClass) {
-    poster.height = this.element_.clientHeight;
+  setCardWidthHeight_(card, styleClass) {
+    card.height = this.element_.clientHeight;
     if (styleClass === 'circular-entry-point') {
-      poster.width = this.element_.clientHeight;
+      card.width = this.element_.clientHeight;
     }
   }
 
   /**
    * Reacts to on click event for an entry point.
-   * @param {HTMLImageElement} poster
+   * @param {HTMLImageElement} card
    * @param {string} href
    * @param {number} index
    * @private
    */
-  onPosterClick_(poster, href, index) {
-    poster.addEventListener('click', () => {
+  onCardClick_(card, href, index) {
+    card.addEventListener('click', () => {
       this.player_.show(href);
       this.element_.dispatchEvent(
         createCustomEvent(this.win_, 'entryPointClicked', {href, index})
@@ -225,6 +223,27 @@ export class AmpStoryEntryPoint {
     setStyle(leftButton, 'top', top);
     setStyle(rightButton, 'top', top);
     setStyle(rightButton, 'left', this.element_.clientWidth - 10);
+    const container = this.rootEl_.querySelector('div.entry-points');
+
+    let interval;
+
+    rightButton.addEventListener('mousedown', (e) => {
+      interval = setInterval( function() {
+        container.scrollLeft += 5;
+      }, 100);
+    });
+
+    leftButton.addEventListener('mousedown', (e) => {
+      interval = setInterval( function() {
+        container.scrollLeft -= 5;
+      }, 100);
+    });
+
+    rightButton.addEventListener('mouseout', (e) => clearInterval(interval));
+    leftButton.addEventListener('mouseout', (e) => clearInterval(interval));
+
+    rightButton.addEventListener('mouseup', (e) => clearInterval(interval));
+    leftButton.addEventListener('mouseup', (e) => clearInterval(interval));
 
     this.rootEl_.append(leftButton);
     this.rootEl_.append(rightButton);
