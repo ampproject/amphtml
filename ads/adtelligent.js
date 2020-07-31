7a53691a141b911f9b9153e4720cd9ca93cf21a9
moroz <wertixvost@gmail.com>
2020-07-31 16:16:47 +0300

    Check types

--- ads/adtelligent.js
+++ ads/adtelligent.js
@@ -29,7 +29,7 @@ export function adtelligent(global, data) {
 
   const doc = global.document;
   const container = doc.createElement('div');
-  doc.body.append(container);
+  doc.body.appendChild(container);
   if (data.source) {
     const url =
       `https://s.adtelligent.com/?floor=${data.floor || 0}` +
@@ -42,22 +42,23 @@ export function adtelligent(global, data) {
     loadScript(global, url);
   } else {
     const HTML_ELEMENT_ID = 'adt-placement';
-    const vpbSrc = `//player.adtelligent.com/prebid/wrapper_hb_${data.hbmpPubId}_${data.hbmpSiteId}.js`;
+    const vpbSrc = `//player.adtelligent.com/prebid/wrapper_hb_${data['hbmpPubId']}_${data['hbmpSiteId']}.js`;
     const pbSrc = vpbSrc.replace('wrapper_hb', 'hb');
     container.id = HTML_ELEMENT_ID;
     global.vpb = window.vpb || {
       cmd: [],
       fastLoad: true,
       amp: true,
+      startAuction: 1,
     };
 
     loadScript(global, vpbSrc);
     loadScript(global, pbSrc);
 
     global.vpb.cmd.push(function () {
-      global.vpb.auctionByAdUnitID({
+      global.vpb.startAuction({
         code: HTML_ELEMENT_ID,
-        adUnitId: parseInt(data.hbmpUnitId, 10),
+        adUnitId: parseInt(data['hbmpUnitId'], 10),
         sizes: [[data.width, data.height]],
         render: true,
       });
