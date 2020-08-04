f36b4d48c2430fcde94bd0ce689c376a904f7502
Connie Lei <cl878@cornell.edu>
2020-08-03 23:31:54 -0400

    update srcdoc

--- examples/visual-tests/amp-story-player/story-loaded.js
+++ examples/visual-tests/amp-story-player/story-loaded.js
@@ -17,11 +17,10 @@
 
 module.exports = {
   'player ready': async page => {
-    await page.waitFor(2000);
+    await page.waitFor(1000);
     await page.$eval('iframe', (iframe) => {
       iframe.src = '';
       iframe.srcdoc = '<html><head></head><body style=\"background: darkgray;\"></body></html>';
     });
-    await page.waitFor(1000);
   }
 };
