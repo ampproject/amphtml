494b5d69b318e760da403e2809040ee179653298
Connie Lei <cl878@cornell.edu>
2020-08-05 09:24:00 -0400

    fixed arrow styling and scrolling

--- src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
+++ src/amp-story-player/amp-story-entry-point/amp-story-entry-point-impl.js
@@ -70,7 +70,6 @@ export class AmpStoryEntryPoint {
     /** @private {!Array<!HTMLAnchorElement>} */
     this.stories_ = [];
 
-
     this.scrollX_ = 0;
   }
 
@@ -184,14 +183,14 @@ export class AmpStoryEntryPoint {
     const card = this.doc_.createElement('img');
     card.src = src;
     card.classList.add(styleClass);
-    card.classList.add('entry-point');
+    card.classList.add('entry-point-card-img');
     this.setCardWidthHeight_(card, styleClass);
     this.onCardClick_(card, href, index);
     cardContainer.append(card);
   }
 
   /**
-   * Sets the correct height and width for poster based on client height.
+   * Sets the correct height and width for poster based on style.
    * @param {HTMLImageElement} card
    * @param {string} styleClass
    * @private
@@ -220,54 +219,58 @@ export class AmpStoryEntryPoint {
     });
   }
 
-  /** @private */
+  /**
+   * Creates arrows to be displayed.
+   * @private
+   * */
   initializeCarouselArrow_() {
-    const leftButton = this.createCarouselArrow_(true);
-    const rightButton = this.createCarouselArrow_(false);
+    const scrollContainer = this.rootEl_.querySelector('div.entry-points');
+    const cards = this.rootEl_.querySelectorAll(
+      'div.entry-point-card-container'
+    );
+    const maxScroll =
+      scrollContainer.clientWidth - cards.length * cards[0].clientWidth;
+    if (maxScroll >= 0) {
+      return;
+    }
 
-    this.addScrollingToArrows_(leftButton, rightButton);
-
-    this.rootEl_.append(leftButton);
-    this.rootEl_.append(rightButton);
+    this.createCarouselArrow_(true, maxScroll, cards);
+    this.createCarouselArrow_(false, maxScroll, cards);
   }
 
-
   /**
-   * Creates a button element.
-   * @param {boolean} isLeft 
-   * @return {Element}
+   * Creates a button element, adds scrolling ability to it and displays it on the page.
+   * @param {boolean} isLeft
+   * @param {number} maxScroll
+   * @param {Array<Element>} cards
    * @private
    */
-  createCarouselArrow_(isLeft) {
+  createCarouselArrow_(isLeft, maxScroll, cards) {
     const button = this.doc_.createElement('button');
-    button.classList.add(isLeft ? 'entry-point-left-arrow' : 'entry-point-right-arrow');
+    button.classList.add(
+      isLeft ? 'entry-point-left-arrow' : 'entry-point-right-arrow'
+    );
     button.classList.add('entry-point-arrow');
-    return button;
+    this.rootEl_.appendChild(button);
+    this.addScrollingToArrow_(button, isLeft, maxScroll, cards);
   }
 
   /**
-   * Add ability to scroll to both buttons.
-   * @param {Element} leftButton 
-   * @param {Element} rightButton 
+   * Add ability to scroll to button.
+   * @param {Element} button
+   * @param {boolean} isLeft
+   * @param {number} maxScroll
+   * @param {Array<Element>} cards
    * @private
    */
-  addScrollingToArrows_(leftButton, rightButton) {
-    const cards = this.rootEl_.querySelectorAll('div.entry-point-card-container');
-    let card;
+  addScrollingToArrow_(button, isLeft, maxScroll, cards) {
+    button.addEventListener('click', () => {
+      this.scrollX_ = isLeft
+        ? Math.min(0, this.scrollX_ + 50)
+        : Math.max(maxScroll, this.scrollX_ - 50);
 
-    rightButton.addEventListener('click', () => {
-      this.scrollX_ = this.scrollX_ <= -100 ? -100 : this.scrollX_ - 50;
-
-      for(card of cards){
-        card.style.transform = 'translateX(' + this.scrollX_ + 'px)';
-      }
-    });
-
-    leftButton.addEventListener('click', () => {
-      this.scrollX_ = this.scrollX_ >= 0 ? 0 : this.scrollX_ + 50;
-
-      for(card of cards){
-        card.style.transform = 'translateX(' + this.scrollX_ + 'px)';
+      for (let i = 0; i < cards.length; i++) {
+        setStyle(cards[i], 'transform', `translateX(${this.scrollX_}px)`);
       }
     });
   }
