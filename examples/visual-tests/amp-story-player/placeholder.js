74aac6e24df4026b65ace7103650ef816e938417
Connie Lei <cl878@cornell.edu>
2020-08-05 21:37:16 -0400

    fix spacing

--- examples/visual-tests/amp-story-player/placeholder.js
+++ examples/visual-tests/amp-story-player/placeholder.js
@@ -18,11 +18,11 @@
 'use strict';
 
 module.exports = {
-    'player ready': async page => {
-        await page.waitFor(1000);
-        await page.$eval('iframe', (iframe) => {
-          iframe.src = '';
-          iframe.srcdoc = '<html><head></head><body style=\"background: darkgray;\"></body></html>';
-        });
-    }
+  'player ready': async page => {
+    await page.waitFor(1000);
+    await page.$eval('iframe', (iframe) => {
+      iframe.src = '';
+      iframe.srcdoc = '<html><head></head><body style=\"background: darkgray;\"></body></html>';
+    });
+  }
 };
