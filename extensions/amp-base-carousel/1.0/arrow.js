7ca5a7b5c0f79e5111ad00da4c4c183dc3e8f9b7
Jake Fried <samouri@users.noreply.github.com>
2020-08-06 08:21:06 -0400

    bento: add jss styling

--- extensions/amp-base-carousel/1.0/arrow.js
+++ extensions/amp-base-carousel/1.0/arrow.js
@@ -15,8 +15,7 @@
  */
 
 import * as Preact from '../../../src/preact';
-import * as styles from './base-carousel.css';
-import {useEffect, useState} from '../../../src/preact';
+import {useStyles} from './base-carousel.jss';
 
 /**
  * @param {!BaseCarouselDef.ArrowProps} props
@@ -34,16 +33,13 @@ export function Arrow({customArrow, by, advance, disabled}) {
     }
     advance(by);
   };
+  const classes = useStyles();
+  const classNames = `${classes.arrowPlacement} ${
+    by < 0 ? classes.arrowPrev : classes.arrowNext
+  } ${isDisabled ? classes.arrowDisabled : ''}`;
+
   return (
-    <div
-      style={{
-        ...styles.arrowPlacement,
-        // Offset button from the edge.
-        [by < 0 ? 'left' : 'right']: '0px',
-        opacity: isDisabled ? 0 : 1,
-        pointerEvents: isDisabled ? 'none' : 'auto',
-      }}
-    >
+    <div class={classNames}>
       {Preact.cloneElement(customArrow, {
         'onClick': onClick,
         'disabled': isDisabled,
@@ -85,50 +81,26 @@ export function ArrowNext({customArrow, ...rest}) {
  * @param {!BaseCarouselDef.ArrowProps} props
  * @return {PreactDef.Renderable}
  */
-function DefaultArrow({by, disabled, ...rest}) {
-  // TODO(wg-bento#7): Replace with :hover and :active pseudoselectors.
-  const [hover, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Component should reset hover and active states when disabled.
-  useEffect(() => {
-    if (disabled) {
-      setHover(false);
-      setActive(false);
-    }
-  }, [disabled]);
-
+function DefaultArrow({by, ...rest}) {
+  const classes = useStyles();
   return (
     <button
-      style={{
-        ...styles.defaultArrowButton,
-        color: hover ? '#222' : '#fff',
-        transitionDuration: active ? '0ms' : '',
-      }}
+      class={classes.defaultArrowButton}
       aria-label={
         by < 0 ? 'Previous item in carousel' : 'Next item in carousel'
       }
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      onMouseDown={() => setActive(true)}
-      onMouseUp={() => setActive(false)}
       {...rest}
     >
-      <div style={{...styles.arrowBaseStyle, ...styles.arrowFrosting}}></div>
-      <div style={{...styles.arrowBaseStyle, ...styles.arrowBackdrop}}></div>
       <div
-        style={{
-          ...styles.arrowBaseStyle,
-          ...styles.arrowBackground,
-          backgroundColor: active
-            ? 'rgba(255, 255, 255, 1.0)'
-            : hover
-            ? 'rgba(255, 255, 255, 0.8)'
-            : 'rgba(0, 0, 0, 0.3)',
-          transitionDuration: active ? '0ms' : '',
-        }}
+        class={`${classes.arrowBaseStyle} ${classes.arrowFrosting}`}
       ></div>
-      <svg style={styles.arrowIcon} viewBox="0 0 24 24">
+      <div
+        class={`${classes.arrowBaseStyle} ${classes.arrowBackdrop}`}
+      ></div>
+      <div
+        class={`${classes.arrowBaseStyle} ${classes.arrowBackground}`}
+      ></div>
+      <svg class={classes.arrowIcon} viewBox="0 0 24 24">
         {by < 0 ? (
           <path
             d="M14,7.4 L9.4,12 L14,16.6"
