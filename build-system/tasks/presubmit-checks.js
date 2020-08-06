24d106472fcc83686e78b6ff7170434490955429
Matias Szylkowski <mszylkowski@google.com>
2020-08-06 17:10:41 -0400

    ✨ Move interactive components to its own extension (#29177)
    * split extension
    
    * Fixed extension
    
    * Fixed deps and pr-check
    
    * Included binary poll in the PR
    
    * Added polls to elements registered
    
    * Fixed interactives importing
    
    * Added notify to gmajoulet
    
    Co-authored-by: Gabriel Majoulet <gmajoulet@google.com>
    
    * Fixed paths
    
    * Added empty interactives.css
    
    * Requiring imports
    
    * Change name to interactive
    
    * Updated test
    
    * Using services...
    
    * Started moving to Services.
    
    * Move latest files to extension, wait for initializeState
    
    * Removed comments
    
    * Update references to abstract class
    
    * Fixed quizzes get localization
    
    * Added results to interactive import
    
    * Fixed tests for binary poll
    
    * Fixed linting
    
    * Tests now use registerServiceBuilder for request service
    
    * Using registerService in tests
    
    * Changed styles to match Hong designs
    
    * Moved updateState to utils, restructured init
    
    * Account for null options
    
    * Using buildcallback to initialize
    
    * Fixed deduplicateIds
    
    * Fixed unused import
    
    * Moved results test to extension
    
    * Moved initializeListeners
    
    * Fixed all tests and restructured buildcallback
    
    * Fixed tests for bin poll
    
    * Fixed linting errors
    
    * Fixed linting v2
    
    * Removed localizationService protected variable
    
    * Fixed changes
    
    * Deleted empty validator, will add when we implement it
    
    Co-authored-by: Gabriel Majoulet <gmajoulet@google.com>

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -432,7 +432,7 @@ const forbiddenTerms = {
       'extensions/amp-experiment/1.0/variant.js',
       'extensions/amp-user-notification/0.1/amp-user-notification.js',
       'extensions/amp-consent/0.1/consent-config.js',
-      'extensions/amp-story/1.0/amp-story-interactive.js',
+      'extensions/amp-story-interactive/1.0/amp-story-interactive-abstract.js',
     ],
   },
   'getBaseCid': {
