4f2fe2553a629b63367e2ed8ae2306690ff83008
honeybadgerdontcare <sedano@google.com>
2020-08-07 13:32:30 -0700

    Internal refactoring
    PiperOrigin-RevId: 325498121

--- validator/cpp/engine/update_text.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-grep -rl 'third_party/ampvalidator' ./ | xargs sed -i 's/quality\/dni\/validator/third_party\/ampvalidator/g'
-
-grep -rl 'namespace amp::validator' ./ | xargs sed -i 's/namespace amp::validator/namespace amp::validator/g'
-
-grep -rl 'THIRD_PARTY_AMPVALIDATOR' ./ | xargs sed -i 's/THIRD_PARTY_AMPVALIDATOR/THIRD_PARTY_AMPVALIDATOR/g'
