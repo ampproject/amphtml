c421455ab7436c98f58f89819dacdc5837856a36
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2020-08-12 11:23:42 -0700

    Collect additional visible info for debugging (#29629)
    * collect addtional information for activeview zero size investigation
    
    * update error msg
    
    * type check

--- extensions/amp-analytics/0.1/visibility-model.js
+++ extensions/amp-analytics/0.1/visibility-model.js
@@ -15,8 +15,9 @@
  */
 
 import {Deferred} from '../../../src/utils/promise';
+import {GOOGLE_ACTIVEVIEW_ERROR_TAG} from './requests';
 import {Observable} from '../../../src/observable';
-import {devAssert} from '../../../src/log';
+import {dev, devAssert} from '../../../src/log';
 import {dict} from '../../../src/utils/object';
 
 /**
@@ -28,10 +29,28 @@ export class VisibilityModel {
   /**
    * @param {!JsonObject} spec
    * @param {function():number} calcVisibility
+   * @param {?function():?../../../src/layout-rect.LayoutRectDef} calcLayoutBoxTemp
+   * @param {?function():?../../../src/layout-rect.LayoutRectDef} calcBoundingClientRectTemp
+   * @param {?../../../src/service/viewport/viewport-impl.ViewportImpl} viewportTemp
    */
-  constructor(spec, calcVisibility) {
+  constructor(
+    spec,
+    calcVisibility,
+    calcLayoutBoxTemp = null,
+    calcBoundingClientRectTemp = null,
+    viewportTemp = null
+  ) {
     /** @const @private */
     this.calcVisibility_ = calcVisibility;
+
+    // TODO(#29618): Remove after ampim investigation
+    this.calcLayoutBoxTemp_ = calcLayoutBoxTemp;
+
+    this.calcBoundingClientRectTemp_ = calcBoundingClientRectTemp;
+
+    this.viewportTemp_ = viewportTemp;
+
+    this.errorInfoTemp_ = dict({});
 
     /**
      * Spec parameters.
@@ -303,6 +322,20 @@ export class VisibilityModel {
   }
 
   /**
+   * TODO(#29618): Remove after ampim investigation
+   * Return object
+   * {
+   *   'layoutBoxAtMaxRatio': AMP calculated layout box at max intersect ratio
+   *   'boundingClientRectAtMaxRatio': InOb calculated clientRect at max intersect ratio
+   *   'viewportSizeAtMaxRatio': viewport size at max intersect ratio
+   * }
+   * @return {!JsonObject}
+   */
+  getErrorInfoTemp() {
+    return this.errorInfoTemp_;
+  }
+
+  /**
    * @param {number} visibility
    * @private
    */
@@ -437,6 +470,30 @@ export class VisibilityModel {
         this.minVisiblePercentage_ > 0
           ? Math.min(this.minVisiblePercentage_, visibility)
           : visibility;
+
+      // TODO(#29618): Remove after ampim investigation
+      if (visibility > this.maxVisiblePercentage_) {
+        try {
+          if (this.calcLayoutBoxTemp_) {
+            this.errorInfoTemp_[
+              'layoutBoxAtMaxRatio'
+            ] = this.calcLayoutBoxTemp_();
+          }
+          if (this.calcBoundingClientRectTemp_) {
+            this.errorInfoTemp_[
+              'boundingClientRectAtMaxRatio'
+            ] = this.calcBoundingClientRectTemp_();
+          }
+          if (this.viewportTemp_) {
+            this.errorInfoTemp_[
+              'viewportSizeAtMaxRatio'
+            ] = this.viewportTemp_.getSize();
+          }
+        } catch (e) {
+          dev().error(GOOGLE_ACTIVEVIEW_ERROR_TAG, e);
+        }
+      }
+
       this.maxVisiblePercentage_ = Math.max(
         this.maxVisiblePercentage_,
         visibility
