506cb12fa7388c246abc935a4c04827dc63883b3
Amaltas Bohra <amaltas@google.com>
2020-08-14 17:54:53 -0700

    Fix broken test
    PiperOrigin-RevId: 326762653

--- validator/cpp/htmlparser/minimal_recursion_test.cc
+++ validator/cpp/htmlparser/minimal_recursion_test.cc
@@ -1,6 +1,8 @@
 #include <fstream>
 
 #include "gtest/gtest.h"
+#include "absl/flags/declare.h"
+#include "absl/flags/flag.h"
 #include "defer.h"
 #include "fileutil.h"
 #include "parser.h"
@@ -10,6 +12,8 @@
 #define EXPECT_NOT_NULL(p) EXPECT_TRUE((p) != nullptr)
 
 DECLARE_int32(fibers_default_thread_stack_size);
+
+ABSL_DECLARE_FLAG(uint32_t, htmlparser_max_nodes_depth_count);
 
 namespace htmlparser {
 
@@ -24,6 +28,10 @@ std::string ReadFileContent(std::ifstream* fd) {
 }
 
 TEST(MinimalRecursionTest, TestComplexDocument) {
+  // For the purposes of this test with hundreds of deeply nested nodes increase
+  // the htmlparser_max_nodes_depth_count value.
+  ::absl::SetFlag(&FLAGS_htmlparser_max_nodes_depth_count, 500);
+
   std::ifstream fd(
       FLAGS_test_srcdir +
       "testdata/largehtmldoc.html");
