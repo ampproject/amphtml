61cd850164b133294d00fd9afa38171d9f38962d
Nic Gallardo <nic.gallardo@freestar.io>
2020-08-17 14:52:58 -0600

    ✨ Freestar RTC Module (#29589)
    * freestarmodule and example page
    
    * cleanup examples
    
    * example extra tag removed
    
    * ENG-4481 - initial Frestar-RTC build
    
    * freestar - updates from pr notes
    
    * remove extra line
    
    * markdown upates

--- extensions/amp-a4a/0.1/callout-vendors.js
+++ extensions/amp-a4a/0.1/callout-vendors.js
@@ -189,6 +189,11 @@ const RTC_VENDORS = jsonConfiguration({
       'https://events.browsiprod.com/events/amp?e=ERROR_TYPE&h=HREF&et=predict_error',
     disableKeyAppend: true,
   },
+  freestar: {
+    url: 'https://prebid-amp.pub.network/openrtb2/amp?tag_id=TAG_ID',
+    macros: ['TAG_ID'],
+    disableKeyAppend: true,
+  },
   hubvisor: {
     url:
       'https://pbs.hubvisor.io/openrtb2/amp?tag_id=PLACEMENT_ID&slot=ATTR(data-slot)&targeting=TGT&curl=CANONICAL_URL&timeout=TIMEOUT&adcid=ADCID&purl=HREF&gdpr_consent=CONSENT_STRING',
