7efa240d098d98e08b7e4e36ecfe22b38309755d
Kevin Kimball <kwkimball@google.com>
2020-08-20 10:09:17 -0700

    Don't log user/dev errors to console in ESM

--- build-system/babel-plugins/babel-plugin-transform-dev-methods/index.js
+++ build-system/babel-plugins/babel-plugin-transform-dev-methods/index.js
@@ -27,14 +27,14 @@ function defaultCalleeToPropertiesMap() {
       'dev',
       {
         detected: false,
-        removeable: ['info', 'fine'],
+        removeable: ['info', 'fine', 'warn', 'error'],
       },
     ],
     [
       'user',
       {
         detected: false,
-        removeable: ['fine'],
+        removeable: ['info', 'fine', 'warn', 'error'],
       },
     ],
   ]);
