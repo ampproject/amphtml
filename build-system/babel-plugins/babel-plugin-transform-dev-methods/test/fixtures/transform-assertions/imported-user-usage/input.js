8a5004b7e53d4dc7e1a005da52d8174c75086fd3
Jake Fried <samouri@users.noreply.github.com>
2020-08-27 16:58:12 -0400

    gulp babel-plugin-test failure should fail ci (#29998)
    * gulp babel-plugin-test failure should fail ci
    
    * fix tests
    
    * point to the type

--- build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/imported-user-usage/input.js
+++ build-system/babel-plugins/babel-plugin-transform-dev-methods/test/fixtures/transform-assertions/imported-user-usage/input.js
@@ -18,23 +18,23 @@ import {user} from '../../../../../../../src/log';
 
 user().fine(TAG, 'fine');
 user().fine;
-user().info('Should not be removed');
+user().error('Should not be removed');
 
 function hello() {
   user().fine(TAG, 'fine');
-  user().info('Should not be removed');
+  user().info('Should be removed');
   return false;
 }
 
 export function helloAgain() {
   user().fine(TAG, 'fine');
-  user().info('Should not be removed');
+  user().error('Should not be removed');
   return false;
 }
 
 class Foo {
   method() {
     user().fine(TAG, 'fine');
-    user().info('Should not be removed');
+    user().info('Should be removed');
   }
 }
