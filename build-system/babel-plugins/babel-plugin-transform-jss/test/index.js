ee761491214f6dbb25dd1fd529fdec4c687ad37b
Jake Fried <samouri@users.noreply.github.com>
2020-09-03 18:02:40 -0400

    remove describes

--- build-system/babel-plugins/babel-plugin-transform-jss/test/index.js
+++ build-system/babel-plugins/babel-plugin-transform-jss/test/index.js
@@ -21,33 +21,31 @@ const runner = require('@babel/helper-plugin-test-runner').default;
 runner(__dirname);
 
 // eslint-disable-next-line no-undef
-describe('jss transform tests', () => {
-  const fileContents = `
+const fileContents = `
 import {createUseStyles} from 'react-jss';
 export const useStyles = createUseStyles({button: {fontSize: 12}});`;
 
-  const plugins = [path.join(__dirname, '..')];
-  const caller = {name: 'babel-jest'};
+const plugins = [path.join(__dirname, '..')];
+const caller = {name: 'babel-jest'};
 
-  // eslint-disable-next-line no-undef
-  test.only('transforming the same file contents twice should throw if there is a hash collision with filename', () => {
-    let filename;
-    expect(() => {
-      stubCreateHash(() => {
-        filename = 'test1.jss.js';
-        babel.transformSync(fileContents, {filename, plugins, caller});
-        filename = 'test2.jss.js';
-        babel.transformSync(fileContents, {filename, plugins, caller});
-      });
-    }).toThrow(/Classnames must be unique across all files/);
-  });
+// eslint-disable-next-line no-undef
+test('transforming the same file contents twice should throw if there is a hash collision with filename', () => {
+  let filename;
+  expect(() => {
+    stubCreateHash(() => {
+      filename = 'test1.jss.js';
+      babel.transformSync(fileContents, {filename, plugins, caller});
+      filename = 'test2.jss.js';
+      babel.transformSync(fileContents, {filename, plugins, caller});
+    });
+  }).toThrow(/Classnames must be unique across all files/);
+});
 
-  // eslint-disable-next-line no-undef
-  test('transforming same exact file twice is fine (e.g. watch mode)', () => {
-    const filename = 'test.jss.js';
-    babel.transformSync(fileContents, {filename, plugins, caller});
-    babel.transformSync(fileContents, {filename, plugins, caller});
-  });
+// eslint-disable-next-line no-undef
+test('transforming same exact file twice is fine (e.g. watch mode)', () => {
+  const filename = 'test.jss.js';
+  babel.transformSync(fileContents, {filename, plugins, caller});
+  babel.transformSync(fileContents, {filename, plugins, caller});
 });
 
 function stubCreateHash(fn) {
