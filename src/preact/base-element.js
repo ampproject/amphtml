04c0a9135f68729b8a810c2c475f944d60a3c384
Dima Voytenko <dvoytenko@google.com>
2020-09-03 13:37:41 -0700

    Declare types for AmpContext (#30102)
    * Declare types for AmpContext
    
    * small cleanup
    
    * memoize context

--- src/preact/base-element.js
+++ src/preact/base-element.js
@@ -94,7 +94,7 @@ export class PreactBaseElement extends AMP.BaseElement {
     /** @private {boolean} */
     this.scheduledRender_ = false;
 
-    /** @private {!Object} */
+    /** @private {!AmpContextDef.ContextType} */
     this.context_ = {
       renderable: false,
       playable: false,
