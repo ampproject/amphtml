5b74002c352398cb4c811b2f904569b51ab85d89
Matias Szylkowski <mszylkowski@google.com>
2020-09-08 14:29:43 -0400

    Added base for two lines

--- extensions/amp-story-interactive/0.1/amp-story-interactive-poll.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-poll.js
@@ -19,6 +19,7 @@ import {
   InteractiveType,
 } from './amp-story-interactive-abstract';
 import {CSS} from '../../../build/amp-story-interactive-poll-0.1.css';
+import {dev} from '../../../src/log';
 import {htmlFor} from '../../../src/static-template';
 import {setStyle} from '../../../src/style';
 
@@ -79,6 +80,13 @@ export class AmpStoryInteractivePoll extends AmpStoryInteractive {
     return this.rootEl_;
   }
 
+  /** @override */
+  layoutCallback() {
+    return this.adaptFontSize_(dev().assertElement(this.rootEl_)).then(() =>
+      super.layoutCallback()
+    );
+  }
+
   /**
    * Finds the prompt and options content
    * and adds it to the quiz element.
@@ -133,5 +141,56 @@ export class AmpStoryInteractivePoll extends AmpStoryInteractive {
       ).textContent = `${percentage}`;
       setStyle(currOption, '--option-percentage', percentages[index] + '%');
     });
+  }
+
+  /**
+   * This method changes the font-size on post-select to best display the titles.
+   *
+   * It measures the number of lines and chars on the titles and generates an appropriate font-size.
+   * - font-size: 28px - Both titles are emojis or short text (yes/no)
+   * - font-size: 16px - Both titles have at most one line
+   * - font-size: 14px - At least one title has two lines
+   *
+   * The title container will shrink 50% on post-select to indicate the safe-zone for the title is smaller.
+   * To keep the font-size (original font-size:28px) true to the guidelines above, a post-select-scale is applied counteracting it.
+   * Eg. post-select-scale:1 corresponds to font-size:14px after the 50% scale (for 2-lined title),
+   * but post-select-scale:1.14 corresponds to font-size:16px after the 50% scale (for 1-lined titles),
+   * and post-select-scale:2 corresponds to font-size:28px after the 50% scale (for emoji titles).
+   * @private
+   * @param {!Element} root
+   * @return {!Promise}
+   */
+  adaptFontSize_(root) {
+    let largestFontSize = FontSize.EMOJI;
+    const allTitles = toArray(
+      root.querySelectorAll('.i-amphtml-story-interactive-option-title-text')
+    );
+    return this.measureMutateElement(
+      () => {
+        allTitles.forEach((e) => {
+          const lines = Math.round(
+            e./*OK*/ clientHeight /
+              parseFloat(
+                computedStyle(this.win, e)['line-height'].replace('px', '')
+              )
+          );
+          if (e.textContent.length <= 3 && largestFontSize >= FontSize.EMOJI) {
+            largestFontSize = FontSize.EMOJI;
+          } else if (lines == 1 && largestFontSize >= FontSize.SINGLE_LINE) {
+            largestFontSize = FontSize.SINGLE_LINE;
+          } else if (lines == 2) {
+            largestFontSize = FontSize.DOUBLE_LINE;
+          }
+        });
+      },
+      () => {
+        setStyle(
+          root,
+          '--post-select-scale-variable',
+          `${(largestFontSize / FontSize.DOUBLE_LINE).toFixed(2)}`
+        );
+      },
+      root
+    );
   }
 }
