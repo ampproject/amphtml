d07342a0542c0f66237a4237b0498024c6976d17
Jake Fried <samouri@users.noreply.github.com>
2020-09-09 16:59:59 -0400

    amp-list: support amp-state and amp-script for load-more (#30101)
    * amp-list: support amp-state and amp-script for loadMore
    
    * clean up separation between append/reg fetchlist
    
    * undo vscode setting
    
    * new fixture for e2e testing protocol adapters and loadmore
    
    * lint and add todo and fix tests
    
    * Update test-function-src.js

--- extensions/amp-list/0.1/test-e2e/test-function-src.js
+++ extensions/amp-list/0.1/test-e2e/test-function-src.js
@@ -29,17 +29,46 @@ describes.endtoend(
       controller = env.controller;
     });
 
-    it.configure().run(
-      'should render list backed by amp-script data',
-      async function () {
-        const container = await getListContainer(controller);
-        await verifyContainer(controller, container);
+    it('should render list backed by amp-script data', async function () {
+      const container = await getListContainer(controller);
+      await verifyContainer(controller, container);
 
-        // Verify that all items rendered.
-        const listItems = await getListItems(controller);
-        await expect(listItems).to.have.length(2);
-      }
-    );
+      // Verify that all items rendered.
+      const listItems = await getListItems(controller);
+      await expect(listItems).to.have.length(2);
+    });
+  }
+);
+
+describes.endtoend(
+  'amp-list "amp-script:" with load-more',
+  {
+    testUrl:
+      'http://localhost:8000/test/fixtures/e2e/amp-list/amp-list-function-load-more.html',
+    experiments: ['protocol-adapters'],
+    environments: ['single'],
+  },
+  async (env) => {
+    let controller;
+
+    beforeEach(async () => {
+      controller = env.controller;
+    });
+
+    it('should load more when button is clicked until no more load-more-src', async function () {
+      // These two assertions are just to create a wait period ensuring the loadMore button
+      // is "clickable".
+      // TODO(samouri): expose a method on controller to wait for an element to be interactable.
+      await controller.findElement('[role=listitem]:nth-child(3)');
+      const loadMore = await controller.findElement('[load-more-clickable]');
+
+      await controller.click(loadMore);
+
+      const fifteenthItem = await controller.findElement(
+        '[role=listitem]:nth-child(15)'
+      );
+      await expect(fifteenthItem).to.be.ok;
+    });
   }
 );
 
