a98a450ec51719d0d1605c28b8f3110491297701
moroz <wertixvost@gmail.com>
2020-09-10 16:33:19 +0300

    fix linter

--- ads/adtelligent.js
+++ ads/adtelligent.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { loadScript, validateData } from '../3p/3p';
+import {loadScript, validateData} from '../3p/3p';
 
 /**
  * @param {!Window} global
