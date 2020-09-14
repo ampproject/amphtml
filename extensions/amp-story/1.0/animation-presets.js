b950a979343d3f8045dbf3e345c275fd58e12025
processprocess <philip.hunter.bell@gmail.com>
2020-09-14 13:45:56 -0400

    Lint fix

--- extensions/amp-story/1.0/animation-presets.js
+++ extensions/amp-story/1.0/animation-presets.js
@@ -225,7 +225,7 @@ export const presets = {
       return [
         {
           offset: 0.0,
-          transform: `translateY(${px(-maxBounceHeight * 1.0)})`,
+          transform: `translateY(${px(-maxBounceHeight * 0)})`,
           easing: 'cubic-bezier(.5, 0, 1, 1)',
         },
         {
