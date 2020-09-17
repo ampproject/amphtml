60ec55e939d5bf46d16ee40d172fd6ca0a774bbe
Alain Schlesser <alain.schlesser@gmail.com>
2020-09-17 07:41:42 +0200

    Fix variable redeclaration in test

--- validator/js/engine/parse-srcset_test.js
+++ validator/js/engine/parse-srcset_test.js
@@ -286,10 +286,10 @@ describe('Srcset parseSrcset', () => {
     let result = parse_srcset.parseSrcset('image.png 500.0w');
 
     expect(result.success).toBe(false);
-    let result = parse_srcset.parseSrcset('image.png 1.5w');
+    result = parse_srcset.parseSrcset('image.png 1.5w');
 
     expect(result.success).toBe(false);
-    let result = parse_srcset.parseSrcset('image.png 0.1w');
+    result = parse_srcset.parseSrcset('image.png 0.1w');
 
     expect(result.success).toBe(false);
   });
