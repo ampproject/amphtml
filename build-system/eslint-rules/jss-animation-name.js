b788466df16f49cf5f7d8512a1887894b70ebf9d
Alan Orozco <orozcoalan@gmail.com>
2020-09-18 13:44:46 -0700

    This should used resolved prop name

--- build-system/eslint-rules/jss-animation-name.js
+++ build-system/eslint-rules/jss-animation-name.js
@@ -44,7 +44,7 @@ module.exports = function (context) {
       if (typeof node.value.value !== 'string') {
         context.report({
           loc: node.value.loc,
-          message: `Use string literals for ${node.key.name} values.`,
+          message: `Use string literals for ${keyName} values.`,
         });
         return;
       }
