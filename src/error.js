5d87289346bdec94763ca7f84720dcc92365c7f3
Derek Tse <derektse@google.com>
2020-09-21 14:54:09 -0700

    error report for sxg

--- src/error.js
+++ src/error.js
@@ -544,6 +544,9 @@ export function getErrorReportData(
   if (IS_ESM) {
     runtime = 'esm';
     data['esm'] = '1';
+  } else if (IS_SXG) {
+    runtime = 'sxg';
+    data['sxg'] = '1';
   } else if (self.context && self.context.location) {
     data['3p'] = '1';
     runtime = '3p';
