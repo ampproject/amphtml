385f3ebf0501b2051a18d3cf11e43afe9ad1adac
Yuxuan Zhou <zhouyx@users.noreply.github.com>
2020-09-21 16:31:10 -0700

    🐛 Inabox visibility reporting: listen to window resize event (#30267)
    * add resize listener
    
    * add change from 30105
    
    * nit

--- extensions/amp-analytics/0.1/test/test-amp-analytics.js
+++ extensions/amp-analytics/0.1/test/test-amp-analytics.js
@@ -1469,7 +1469,10 @@ describes.realWin(
       beforeEach(() => {
         // Unfortunately need to fake sandbox analytics element's parent
         // to an AMP element
+        // Set the doc width/height to 1 to trigger visible event.
         doc.body.classList.add('i-amphtml-element');
+        doc.body.style.minWidth = '1px';
+        doc.body.style.minHeight = '1px';
       });
 
       afterEach(() => {
@@ -1750,6 +1753,11 @@ describes.realWin(
 
     describe('parentPostMessage', () => {
       let postMessageSpy;
+
+      beforeEach(() => {
+        doc.body.style.minWidth = '1px';
+        doc.body.style.minHeight = '1px';
+      });
 
       function waitForParentPostMessage(opt_max) {
         if (postMessageSpy.callCount) {
