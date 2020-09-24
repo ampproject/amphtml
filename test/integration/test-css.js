180b38d78d198f89369500720d01fc7a4660705c
Ryan Cebulko <ryan@cebulko.com>
2020-09-24 17:13:52 -0400

    Configure CSS integration tests to skip IE11 (#30393)

--- test/integration/test-css.js
+++ test/integration/test-css.js
@@ -17,7 +17,7 @@
 import {AmpEvents} from '../../src/amp-events.js';
 import {createFixtureIframe} from '../../testing/iframe.js';
 
-describe('CSS', () => {
+describe.configure().run('CSS', () => {
   it('should include height of [overflow] child in size before build', async () => {
     const fixture = await createFixtureIframe(
       'test/fixtures/overflow.html',
