3dfd2597d054e9025751a92f4cfd08130921a542
Jake Fried <samouri@users.noreply.github.com>
2020-09-24 16:49:09 -0400

    jss transform: use @enum instead of json.parse (#30304)
    * Revert "resources: fix loading sequence of fill and intersection-observer"
    
    This reverts commit f5ff363ab334e73d547117af856aadfb244aab1e.
    
    * jss transform: use const instead of json.parse
    
    * separate CSS export
    
    * remove eslint warnings
    
    * remove accidentally added file
    
    * lint

--- build-system/babel-plugins/babel-plugin-transform-jss/index.js
+++ build-system/babel-plugins/babel-plugin-transform-jss/index.js
@@ -31,8 +31,9 @@
  * Out:
  * ```
  * const jss = { button: { fontSize: 12 }}
- * const _classes = {button: 'button-1', CSS: 'button-1 { font-size: 12 }'}
+ * const _classes = {button: 'button-1'}
  * export const useStyles = () => _classes;
+ * export const CSS = 'button-1 { font-size: 12px }'
  * ```
  */
 
@@ -42,7 +43,7 @@ const {default: preset} = require('jss-preset-default');
 const {relative, join} = require('path');
 const {spawnSync} = require('child_process');
 
-module.exports = function ({types: t, template}) {
+module.exports = function ({template}) {
   function isJssFile(filename) {
     return filename.endsWith('.jss.js');
   }
@@ -99,18 +100,25 @@ module.exports = function ({types: t, template}) {
           );
         }
 
+        // Create the classes var.
         const id = path.scope.generateUidIdentifier('classes');
-        path.scope.push({
-          id,
-          init: template.expression.ast`JSON.parse(${t.stringLiteral(
-            JSON.stringify({
-              ...sheet.classes,
-              'CSS': transformCssSync(sheet.toString()),
-            })
-          )})`,
-        });
+        const init = template.expression.ast`${JSON.stringify(sheet.classes)}`;
+        path.scope.push({id, init});
+        path.scope.bindings[id.name].path.parentPath.addComment(
+          'leading',
+          '* @enum {string}'
+        );
 
+        // Replace useStyles with a getter for the new `classes` var.
         path.replaceWith(template.expression.ast`(() => ${id})`);
+
+        // Export a variable named CSS with the compiled CSS.
+        const cssExport = template.ast`export const CSS = "${transformCssSync(
+          sheet.toString()
+        )}"`;
+        path
+          .findParent((p) => p.type === 'ExportNamedDeclaration')
+          .insertAfter(cssExport);
       },
 
       // Remove the import for react-jss
