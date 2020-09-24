671c464c11f4556f69e4d38c6ad7c30e8e2fd9c3
processprocess <philip.hunter.bell@gmail.com>
2020-09-24 11:36:34 -0400

    use listenOncePromise for playing.

--- extensions/amp-story-360/0.1/video-player-360.js
+++ extensions/amp-story-360/0.1/video-player-360.js
@@ -3,7 +3,7 @@ import {dev, user} from '../../../src/log';
 import {CommonSignals} from '../../../src/common-signals';
 import {TAG, CameraAnimation} from './amp-story-360';
 import {Renderer} from '../../../third_party/zuho/zuho';
-import {listen} from '../../../src/event-helper';
+import {listenOncePromise} from '../../../src/event-helper';
 
 export class VideoPlayer360 {
   constructor(ampStory360, ampVideoEl) {
@@ -16,28 +16,27 @@ export class VideoPlayer360 {
       .then(() => {
         return this.ampVideoEl.signals().whenSignal(CommonSignals.LOAD_END);
       })
+      .then(() => listenOncePromise(this.ampVideoEl, 'playing'))
       .then(
         () => {
-          listen(this.ampVideoEl, 'playing', () => {
-            this.ampStory360.renderer_ = new Renderer(this.ampStory360.canvas_);
-            this.ampStory360.renderer_.setImageOrientation(
-              this.ampStory360.sceneHeading_,
-              this.ampStory360.scenePitch_,
-              this.ampStory360.sceneRoll_
-            );
-            this.ampStory360.renderer_.setImage(
-              dev().assertElement(this.ampVideoEl.querySelector('video'))
-            );
-            this.ampStory360.renderer_.resize();
-            if (this.ampStory360.orientations_.length < 1) {
-              return;
-            }
-            this.ampStory360.renderInitialPosition_();
-            this.ampStory360.isReady_ = true;
-            if (this.ampStory360.isPlaying_) {
-              this.ampStory360.animate_();
-            }
-          });
+          this.ampStory360.renderer_ = new Renderer(this.ampStory360.canvas_);
+          this.ampStory360.renderer_.setImageOrientation(
+            this.ampStory360.sceneHeading_,
+            this.ampStory360.scenePitch_,
+            this.ampStory360.sceneRoll_
+          );
+          this.ampStory360.renderer_.setImage(
+            dev().assertElement(this.ampVideoEl.querySelector('video'))
+          );
+          this.ampStory360.renderer_.resize();
+          if (this.ampStory360.orientations_.length < 1) {
+            return;
+          }
+          this.ampStory360.renderInitialPosition_();
+          this.ampStory360.isReady_ = true;
+          if (this.ampStory360.isPlaying_) {
+            this.ampStory360.animate_();
+          }
         },
         () => {
           user().error(TAG, 'Failed to load the amp-video.');
