81cf61714b9119c608e1277d2baa5cc77684f056
Derek Tse <derektse@google.com>
2020-09-25 12:08:00 -0700

    rewrite

--- build-system/server/new-server/transforms/sxg/sxg-transform.ts
+++ build-system/server/new-server/transforms/sxg/sxg-transform.ts
@@ -16,52 +16,34 @@
 
 import {PostHTML} from 'posthtml';
 import {URL} from 'url';
-import {isValidScript, ScriptNode} from '../utilities/script';
-import {CDNURLToLocalDistURL} from '../utilities/cdn';
+import {isValidScript} from '../utilities/script';
 import {OptionSet} from '../utilities/option-set';
 import minimist from 'minimist';
 const argv = minimist(process.argv.slice(2));
 
 /**
- * @param head
- * @param script
- * @param compiled 
- */
-function appendSxg(script: ScriptNode, compiled: boolean): PostHTML.Node {
-  /*
-  if (argv.compiled || compiled) {
-    let sxgPath = CDNURLToLocalDistURL(
-      new URL(script.attrs.src || ''),
-      undefined
-    ).toString();
-    script.attrs.src = sxgPath.replace('.js', '.sxg.js');
-  }
-  else {
-    const urlName = script.attrs.src.toString();
-    if (urlName.includes('js?')){
-        script.attrs.src = urlName + '&f=sxg';
-    }
-    else{
-        script.attrs.src = urlName + '?f=sxg';
-    }
-  }
-
-  script.attrs.type = 'sxg';
-  */
- if(argv.compiled || compiled){
-   script.attrs
- }
-  return script;
-}
-
-/**
- * Returns a function that will transform script node sources into module/nomodule pair.
+ * Returns a function that will transform script node sources into their sxg counterparts.
  * @param options
  */
 export default function(options: OptionSet = {}): (tree: PostHTML.Node) => void {
   return function(tree: PostHTML.Node) {
     tree.match({tag: 'script'}, (script) => {
-      script = 'l';
+      if(!isValidScript(script)){
+        return script;
+      }
+
+      let url;
+      if(options.compiled || argv.compiled){
+        url = script.attrs.src.toString();
+        script.attrs.src = url.replace('.js', '.sxg.js');
+      }
+      else{
+        url = new URL(script.attrs.src);
+        url.searchParams.append('f', 'sxg');
+        script.attrs.src = url.toString();
+      }
+      script.attrs.type = 'sxg';
+      return script;
     });
   }
 }
