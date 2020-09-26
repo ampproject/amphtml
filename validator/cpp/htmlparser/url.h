bc7e8fa3006dc399b138c9fad1d769bbc32fbbb1
Amaltas Bohra <amaltas@google.com>
2020-09-26 12:43:19 -0700

    URL class ported from amp_validator/js/engine/parse-url.js
    PiperOrigin-RevId: 333933095

--- validator/cpp/htmlparser/url.h
+++ validator/cpp/htmlparser/url.h
@@ -18,17 +18,90 @@
 #ifndef HTMLPARSER__URL_H_
 #define HTMLPARSER__URL_H_
 
+#include <arpa/inet.h>
+#include <cctype>
+#include <optional>
+#include <string>
 #include <string_view>
 
 namespace htmlparser {
 
 class URL {
  public:
+  enum ProtocolType {
+    UNKNOWN = 0,
+    HTTP,
+    HTTPS,
+    FTP,
+    SFTP
+  };
+
   // returns: the protocol (scheme) of the URL, if one could be found, according
   // to a strict interpretation of RFC 3986, Section 3.1. Otherwise returns the
   // empty string. No normalization (e.g. lower casing) is performed, and
   // no part of the URL is validated except the protocol. */
   static std::string_view ProtocolStrict(std::string_view url);
+
+  explicit URL(std::string_view url);
+
+  // getters.
+  bool is_valid() { return is_valid_; }
+  bool has_protocol() { return has_protocol_; }
+  ProtocolType protocol() { return protocol_; }
+  std::string hostname() { return host_; }
+  std::string login() { return login_; }
+  int port() { return port_; }
+
+ private:
+  static bool IsAlphaNum(int8_t c) {
+    return (('0' <= c && c <= '9') ||
+            ('a' <= c && c <= 'z') ||
+            ('A' <= c && c <= 'Z'));
+  }
+
+  static bool IsProtocolCharValidChar(int8_t c) {
+    return IsAlphaNum(c) || c == '+' || c == '-';
+  }
+
+  static bool IsPositiveInteger(std::string_view may_be_int) {
+    for (auto c : may_be_int) {
+      if (!std::isdigit(c)) return false;
+    }
+
+    return true;
+  }
+
+  static bool HostCharIsEnd(int8_t c) {
+    return c == '#' || c == '/' || c == '?' || c == '\\';
+  }
+
+  static bool HostCharIsValid(int8_t c) {
+    static constexpr std::string_view illegal_chars =
+        " !\"#$%&'()*+,/:;<=>?@[\\]^`{|}~";
+    return c > 0x1f /* unprintable */ &&
+        illegal_chars.find_first_of(c) == std::string_view::npos;
+  }
+
+  static bool IsIPv6Valid(const std::string& host) {
+    struct in6_addr result;
+    return inet_pton(AF_INET6, host.c_str(), &result) == 1;
+  }
+
+  static std::optional<std::string> UnescapeAndCheckHostname(
+      std::string_view hostname);
+  void ParseProtocol();
+  void ParseAuthority();
+  void ProcessHostDots();
+
+  std::string_view url_;
+  bool is_valid_;
+  bool has_protocol_;
+  ProtocolType protocol_;
+  std::string_view scheme_specific_port_;
+  bool starts_with_double_slash_;
+  std::string login_;
+  std::string host_;
+  int port_;
 };
 
 }  // namespace htmlparser
--- validator/cpp/htmlparser/url_test.cc
+++ validator/cpp/htmlparser/url_test.cc
@@ -17,16 +17,223 @@
 #include "url.h"
 #include "gtest/gtest.h"
 
+namespace htmlparser {
+
 TEST(URLTest, ProtocolStrictTest) {
   std::string url("http://www.google.com");
-  EXPECT_EQ(htmlparser::URL::ProtocolStrict(url), "http");
+  EXPECT_EQ(URL::ProtocolStrict(url), "http");
 
   url = "ftp://server1.google.com";
-  EXPECT_EQ(htmlparser::URL::ProtocolStrict(url), "ftp");
+  EXPECT_EQ(URL::ProtocolStrict(url), "ftp");
 
   url = "/hello.html";
-  EXPECT_TRUE(htmlparser::URL::ProtocolStrict(url).empty());
+  EXPECT_TRUE(URL::ProtocolStrict(url).empty());
 
   url = "://google.com";
-  EXPECT_TRUE(htmlparser::URL::ProtocolStrict(url).empty());
+  EXPECT_TRUE(URL::ProtocolStrict(url).empty());
 }
+
+TEST(URLTest, BasicTests) {
+  EXPECT_TRUE(URL("http://abc.com/").is_valid());
+  EXPECT_TRUE(URL("https://abc.com/").is_valid());
+  EXPECT_TRUE(URL("ftp://abc.com/").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::abcd]/").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::abcd]/foobar").is_valid());
+  EXPECT_TRUE(URL("ftp://foo.com").is_valid());
+  EXPECT_TRUE(URL("HTTp://abc.com").is_valid());
+  EXPECT_TRUE(URL("HTTPs://[1:2:3::abcd]").is_valid());
+
+  // 0080 is a valid port number, browsers ignore leading zeros.
+  EXPECT_TRUE(URL("http://google.com:0080/").is_valid());
+  // IPv4
+  EXPECT_TRUE(URL("http://1.2.3.4").is_valid());
+  EXPECT_TRUE(URL("http://100.254.200.199:80").is_valid());
+
+  URL url("http://www.google.com/");
+  EXPECT_TRUE(url.has_protocol());
+  EXPECT_TRUE(url.is_valid());
+  EXPECT_EQ(url.protocol(), URL::ProtocolType::HTTP);
+  EXPECT_EQ(url.hostname(), "www.google.com");
+  EXPECT_EQ(url.port(), 80);
+
+  URL https_url("https://www.google.com/");
+  EXPECT_TRUE(https_url.has_protocol());
+  EXPECT_TRUE(https_url.is_valid());
+  EXPECT_EQ(https_url.protocol(), URL::ProtocolType::HTTPS);
+  EXPECT_EQ(https_url.hostname(), "www.google.com");
+  EXPECT_EQ(https_url.port(), 443);
+
+  URL ftp_url("ftp://www.google.com/");
+  EXPECT_TRUE(ftp_url.has_protocol());
+  EXPECT_TRUE(ftp_url.is_valid());
+  EXPECT_EQ(ftp_url.protocol(), URL::ProtocolType::FTP);
+  EXPECT_EQ(ftp_url.hostname(), "www.google.com");
+
+  URL sftp_url("sftp://www.google.com/");
+  EXPECT_TRUE(sftp_url.has_protocol());
+  EXPECT_TRUE(sftp_url.is_valid());
+  EXPECT_EQ(sftp_url.protocol(), URL::ProtocolType::SFTP);
+  EXPECT_EQ(sftp_url.hostname(), "www.google.com");
+
+  URL space_url("    http://www.google.com/");
+  EXPECT_TRUE(space_url.has_protocol());
+  EXPECT_TRUE(space_url.is_valid());
+  EXPECT_EQ(space_url.protocol(), URL::ProtocolType::HTTP);
+  EXPECT_EQ(space_url.hostname(), "www.google.com");
+
+  // Invalid char.
+  URL invalid_protocol("h||p://www.google.com");
+  EXPECT_FALSE(invalid_protocol.has_protocol());
+  EXPECT_TRUE(invalid_protocol.is_valid());
+  EXPECT_EQ(invalid_protocol.protocol(),
+            URL::ProtocolType::HTTPS /* default to https */);
+
+  URL invalid_protocol2("foo bar:baz");
+  EXPECT_FALSE(invalid_protocol2.has_protocol());
+  EXPECT_TRUE(invalid_protocol2.is_valid());
+  EXPECT_EQ(invalid_protocol2.protocol(),
+            URL::ProtocolType::HTTPS /* default to https */);
+
+  // Unrecognized protocol.
+  URL unrecognized("telnet://1.2.3.4");
+  EXPECT_TRUE(unrecognized.has_protocol());
+  EXPECT_TRUE(unrecognized.is_valid());
+  EXPECT_EQ(unrecognized.protocol(),
+            URL::ProtocolType::UNKNOWN);
+  EXPECT_EQ(url.hostname(), "www.google.com");
+
+  URL port_url("http://www.google.com:8080/");
+  EXPECT_TRUE(port_url.has_protocol());
+  EXPECT_TRUE(port_url.is_valid());
+  EXPECT_EQ(port_url.protocol(), URL::ProtocolType::HTTP);
+  EXPECT_EQ(port_url.hostname(), "www.google.com");
+  EXPECT_EQ(port_url.port(), 8080);
+
+  URL port_url2("http://www.google.com:0080/foo:8080");
+  EXPECT_EQ(port_url2.port(), 80);
+
+  // Invalid port.
+  URL invalid_port("http://www.google.com:99999/foo");
+  EXPECT_FALSE(invalid_port.is_valid());
+
+  // Username
+  URL username("http://foo:bar@www.google.com:8080/bio.html");
+  EXPECT_TRUE(username.is_valid());
+  EXPECT_EQ(username.login(), "foo:bar");
+
+  URL username2("http://foo@www.google.com:8080/bio.html");
+  EXPECT_EQ(username2.login(), "foo");
+
+  // IPV6.
+  URL ipv6("http://[::2]/foo");
+  EXPECT_TRUE(ipv6.is_valid());
+  EXPECT_EQ(ipv6.hostname(), "::2");
+
+  URL ipv62("http://foo:bar@[1:2:3::4]:8080/foo");
+  EXPECT_TRUE(ipv62.is_valid());
+  EXPECT_EQ(ipv62.hostname(), "1:2:3::4");
+  EXPECT_EQ(ipv62.protocol(), URL::ProtocolType::HTTP);
+  EXPECT_EQ(ipv62.login(), "foo:bar");
+  EXPECT_EQ(ipv62.port(), 8080);
+
+  // Invalid ipv6.
+  URL invalidipv6("http://[12345]/foo");
+  EXPECT_FALSE(invalidipv6.is_valid());
+
+  // Encoded hostname.
+  URL encoded("http://%77%77w%2e%67o%6f%67%6c%65%2e%63%6f%6d/");
+  EXPECT_TRUE(encoded.is_valid());
+
+  // Invalid characters in hostname.
+  EXPECT_FALSE(URL("http://example!.com/").is_valid());
+  EXPECT_FALSE(URL("http://example\x10.com/").is_valid());
+  EXPECT_FALSE(URL("http://example.com&/").is_valid());
+  EXPECT_FALSE(URL("http://example-%FF.com/").is_valid());
+
+  // Accepts utf-8 chars in hostname.
+  EXPECT_TRUE(URL("⚡.com").is_valid());
+
+  // Empty host is invalid.
+  EXPECT_FALSE(URL("http:///").is_valid());
+}
+
+TEST(URLTest, IPv6Urls) {
+  // Checks all compression scenarios.
+  EXPECT_TRUE(URL("http://[1::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[11::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[111::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1111::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5::abcd]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5:6::a]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5:6::ab]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5:6::abc]").is_valid());
+  EXPECT_TRUE(URL("http://[1:2:3:4:5:6::abcd]").is_valid());
+
+  // Illegal characeters.
+  EXPECT_FALSE(URL("http://[abcd:123_:abcd::]").is_valid());
+  EXPECT_FALSE(URL("http://[abcd:123O:abcd::]").is_valid());
+  // Two compression not valid.
+  EXPECT_FALSE(URL("http://[a:b::c::d:e:f]").is_valid());
+  // Non hexadecimal character.
+  EXPECT_FALSE(URL("http://[xyz::abcd:0a]").is_valid());
+  // More than 4 bytes.
+  EXPECT_FALSE(URL("http://[abcd:1234:abcde::]").is_valid());
+
+  // Empty ipv6. atleast [::] needed.
+  EXPECT_FALSE(URL("http://[]").is_valid());
+  EXPECT_TRUE(URL("http://[::]").is_valid());
+}
+
+TEST(URLTest, PortNumbers) {
+  // Port number with userinfo field.
+  EXPECT_TRUE(URL("http://foo:bar@www.google.com:65535/").is_valid());
+  EXPECT_TRUE(URL("http://foo:@www.google.com:65535/").is_valid());
+  EXPECT_TRUE(URL("http://foo@www.google.com:65535/").is_valid());
+
+  // Port number > 65535.
+  EXPECT_FALSE(URL("http://www.google.com:100000/").is_valid());
+  EXPECT_FALSE(URL("http://www.google.com:65536/").is_valid());
+  EXPECT_FALSE(URL("http://foo:bar@www.google.com:65536/").is_valid());
+  EXPECT_FALSE(URL("http://foo:@www.google.com:65536/").is_valid());
+  EXPECT_FALSE(URL("http://foo@www.google.com:65536/").is_valid());
+
+  // Empty port.
+  URL url("https://www.google.com:/");
+  EXPECT_TRUE(url.is_valid());
+  EXPECT_EQ(url.port(), 443);
+}
+
+TEST(URLTest, UserInfoFields) {
+  EXPECT_TRUE(URL("http://foo:bar@google.com").is_valid());
+
+  // Can contain any character including space.
+  EXPECT_TRUE(URL("http://foo bar:baz@google.com").is_valid());
+
+  // Both userinfo and port.
+  EXPECT_TRUE(URL("http://foo:bar@google.com:9000/").is_valid());
+  // Empty password in userinfo.
+  EXPECT_TRUE(URL("http://foo:@google.com:12345/").is_valid());
+  EXPECT_TRUE(URL("http://foo@google.com:12345/").is_valid());
+}
+
+}  // namespace htmlparser
