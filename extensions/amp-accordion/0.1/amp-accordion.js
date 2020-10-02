056f82bf5ec3a9aac06a3747b37bea0b8e42e842
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2020-10-02 16:55:36 -0400

    amp-accordion: Enable display locking based on `beforematch` (#30482)
    * Enable display locking based on `beforematch`
    
    * Place on one line

--- extensions/amp-accordion/0.1/amp-accordion.js
+++ extensions/amp-accordion/0.1/amp-accordion.js
@@ -46,10 +46,7 @@ const COLLAPSE_CURVE_ = bezierCurve(0.39, 0.575, 0.565, 1);
 const isDisplayLockingEnabledForAccordion = (win) => {
   return (
     isExperimentOn(win, 'amp-accordion-display-locking') &&
-    (('CSS' in window &&
-      window.CSS.supports &&
-      window.CSS.supports('content-visibility', 'hidden-matchable')) ||
-      getMode().test)
+    (document.body.onbeforematch !== undefined || getMode().test)
   );
 };
 
