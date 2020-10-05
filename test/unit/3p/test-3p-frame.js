76f9a633e7fb02be08b5b71d8c278cc9b6aee757
Dima Voytenko <dvoytenko@google.com>
2020-10-05 15:24:29 -0700

    fix canary flag in test-3p-frame

--- test/unit/3p/test-3p-frame.js
+++ test/unit/3p/test-3p-frame.js
@@ -150,6 +150,7 @@ describes.realWin('3p-frame', {amp: true}, (env) => {
 
       // TODO(bradfrizzell) break this out into a test-iframe-attributes
       it('should create an iframe', () => {
+        window.AMP_CONFIG = {canary: true};
         mockMode({
           localDev: true,
           development: false,
@@ -225,7 +226,7 @@ describes.realWin('3p-frame', {amp: true}, (env) => {
               'version': '$internalRuntimeVersion$',
               'esm': false,
             },
-            'canary': false, //QQQQQ: true
+            'canary': true,
             'hidden': false,
             // Note that DOM fingerprint will change if the document DOM changes
             // Note also that running it using --files uses different DOM.
