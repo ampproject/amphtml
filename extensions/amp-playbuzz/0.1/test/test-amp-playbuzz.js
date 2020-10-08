fb0bef6ad377c67c3291172086053364b0e11b77
Iddan <iddanha@ex.co>
2020-10-08 10:51:16 +0300

    Modified tests according to CR

--- extensions/amp-playbuzz/0.1/test/test-amp-playbuzz.js
+++ extensions/amp-playbuzz/0.1/test/test-amp-playbuzz.js
@@ -36,7 +36,7 @@ describes.realWin(
     function createOptionalParams(
       displayInfo,
       displayShareBar,
-      displayComments
+      displayComments,
     ) {
       return {
         displayItemInfo: displayInfo,
@@ -100,7 +100,7 @@ describes.realWin(
 
     function testIframe(iframe, itemSrcUrl) {
       expect(iframe).to.not.be.null;
-      expect(iframe.src.includes(itemSrcUrl)).to.be.true;
+      expect(iframe.src.startsWith(itemSrcUrl)).to.be.true;
       expect(iframe.className).to.match(/i-amphtml-fill-content/);
       // This is important to avoid sizing issues.
       expect(iframe.getAttribute('scrolling')).to.equal('no');
@@ -108,22 +108,32 @@ describes.realWin(
 
     it('renders', () => {
       const src = createItemSrc().withUrl(
-        'https://www.playbuzz.com/bob/bobs-life'
+        'https://app.ex.co/stories/bob/bobs-life',
       );
       return getIns(src).then((ins) => {
         const iframe = ins.querySelector('iframe');
-        testIframe(iframe, '/bob/bobs-life');
+        testIframe(iframe, '//app.ex.co/stories/bob/bobs-life');
         // TODO: test playbuzz placeholder loader
+      });
+    });
+
+    it('renders with old url', () => {
+      const src = createItemSrc().withUrl(
+        'https://www.playbuzz.com/bob/bobs-life',
+      );
+      return getIns(src).then((ins) => {
+        const iframe = ins.querySelector('iframe');
+        testIframe(iframe, '//app.ex.co/stories/bob/bobs-life');
       });
     });
 
     it('renders with false for each optional param', () => {
       const src = createItemSrc().withUrl(
-        'https://www.playbuzz.com/bob/bobs-life'
+        'https://app.ex.co/stories/bob/bobs-life',
       );
       return getIns(src).then((ins) => {
         const iframe = ins.querySelector('iframe');
-        testIframe(iframe, '/bob/bobs-life');
+        testIframe(iframe, '//app.ex.co/stories/bob/bobs-life');
         expect(iframe.src)
           .to.contain('&useComments=false')
           .and.to.contain('&gameInfo=false')
@@ -135,28 +145,28 @@ describes.realWin(
       const src = createItemSrc().withItemId('some-item-id');
       return getIns(src).then((ins) => {
         const iframe = ins.querySelector('iframe');
-        testIframe(iframe, '/item/some-item-id');
+        testIframe(iframe, '//app.ex.co/stories/item/some-item-id');
       });
     });
 
     it('renders with item id when submitted both with item url & item id', () => {
       const src = createItemSrc()
-        .withUrl('https://www.playbuzz.com/bob/bobs-life')
+        .withUrl('https://app.ex.co/stories/bob/bobs-life')
         .withItemId('some-item-id');
 
       return getIns(src).then((ins) => {
         const iframe = ins.querySelector('iframe');
-        testIframe(iframe, '/item/some-item-id');
+        testIframe(iframe, '//app.ex.co/stories/item/some-item-id');
       });
     });
 
     it('renders with true for each true optional param', () => {
       const src = createItemSrc().withUrl(
-        'https://www.playbuzz.com/bob/bobs-life'
+        'https:///app.ex.co/stories/bob/bobs-life',
       );
       return getIns(src, createOptionalParams(true, true, true)).then((ins) => {
         const iframe = ins.querySelector('iframe');
-        testIframe(iframe, '/bob/bobs-life');
+        testIframe(iframe, '//app.ex.co/stories/bob/bobs-life');
         expect(iframe.src)
           .to.contain('&useComments=true')
           .and.to.contain('&gameInfo=true')
@@ -166,7 +176,7 @@ describes.realWin(
 
     it('builds a placeholder image without inserting iframe', () => {
       const src = createItemSrc().withUrl(
-        'https://www.playbuzz.com/bob/bobs-life'
+        'https://app.ex.co/stories/bob/bobs-life',
       );
       return getIns(src, createOptionalParams(), true, (ins) => {
         // console.log(ins);
@@ -175,7 +185,7 @@ describes.realWin(
         expect(iframe).to.be.null;
         expect(placeholder).to.not.have.display('');
         expect(placeholder.getAttribute('aria-label')).to.equal(
-          'Loading interactive element'
+          'Loading interactive element',
         );
       }).then((ins) => {
         const placeholder = ins.querySelector('[placeholder]');
@@ -186,7 +196,7 @@ describes.realWin(
             mutate: (fn) => fn(),
           };
         };
-        testIframe(iframe, '/bob/bobs-life');
+        testIframe(iframe, '//app.ex.co/stories/bob/bobs-life');
         //Should test placeholder too
         ins.implementation_.iframePromise_.then(() => {
           expect(placeholder).to.have.display('none');
@@ -195,13 +205,13 @@ describes.realWin(
     });
     it('propagates aria label to placeholder', () => {
       const src = createItemSrc().withUrl(
-        'https://www.playbuzz.com/bob/bobs-life'
+        'https://app.ex.co/stories/bob/bobs-life',
       );
       return getIns(src, {'arialabel': 'captivating quiz'}, true, (ins) => {
         // console.log(ins);
         const placeholder = ins.querySelector('[placeholder]');
         expect(placeholder.getAttribute('aria-label')).to.equal(
-          'Loading - captivating quiz'
+          'Loading - captivating quiz',
         );
       });
     });
@@ -209,9 +219,9 @@ describes.realWin(
       const src = createItemSrc().withUrl('');
       allowConsoleError(() => {
         expect(getIns(src)).to.be.rejectedWith(
-          /The item attribute is required for/
+          /The item attribute is required for/,
         );
       });
     });
-  }
+  },
 );
