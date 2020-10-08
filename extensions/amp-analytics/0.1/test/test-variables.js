812e0f548624f7c7b0a603cf7a14d5a2fd4e445e
Justin Ridgewell <jridgewell@google.com>
2020-10-07 22:57:33 -0400

    Add new AMPDOC_META analytics var and macro (#30520)
    * Sort
    
    * Add new AMPDOC_META analytics var and macro
    
    This new analytics var will return the value of any `<meta>` tag registered on the page.
    
    This is part of supporting SSR experiments in CSI pings. Stories skip the regular viewer ping, and instead talk directly with the CSI endpoint via amp-analytics.
    
    Re: https://github.com/ampproject/amphtml/pull/30504
    Re: cl/335090010
    
    * Remove AMPDOC_META from analytics sandbox allowlist

--- extensions/amp-analytics/0.1/test/test-variables.js
+++ extensions/amp-analytics/0.1/test/test-variables.js
@@ -586,6 +586,32 @@ describes.fakeWin('amp-analytics.VariableService', {amp: true}, (env) => {
       scrollTopValue = 99.5;
       await check('SCROLL_TOP', '100');
     });
+
+    describe('AMPDOC_META', () => {
+      it('should replace with meta tag content', () => {
+        env.sandbox.stub(env.ampdoc, 'getMeta').returns({
+          'foo': 'bar',
+        });
+        return check('AMPDOC_META(foo)', 'bar');
+      });
+
+      it('should replace with "" when no meta tag', () => {
+        env.sandbox.stub(env.ampdoc, 'getMeta').returns({});
+        return check('AMPDOC_META(foo)', '');
+      });
+
+      it('should replace with default_value when no meta tag', () => {
+        env.sandbox.stub(env.ampdoc, 'getMeta').returns({});
+        return check('AMPDOC_META(foo, default_value)', 'default_value');
+      });
+
+      it('should prefer empty meta tag over default_value', () => {
+        env.sandbox.stub(env.ampdoc, 'getMeta').returns({
+          'foo': '',
+        });
+        return check('AMPDOC_META(foo, default_value)', '');
+      });
+    });
   });
 
   describe('getNameArgs:', () => {
