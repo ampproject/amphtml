831f6c7b96484cdcea4dde1fe18501c27b807514
Caroline Liu <carolineliu@google.com>
2020-10-09 16:53:44 -0400

    Add unit tests

---
+++ extensions/amp-stream-gallery/1.0/test/test-amp-stream-gallery.js
@@ -0,0 +1,169 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import '../../../amp-base-carousel/1.0/amp-base-carousel';
+import '../amp-stream-gallery';
+import {
+  createElementWithAttributes,
+  waitForChildPromise,
+} from '../../../../src/dom';
+import {setStyles} from '../../../../src/style';
+import {toArray} from '../../../../src/types';
+import {toggleExperiment} from '../../../../src/experiments';
+import {useStyles} from '../../../amp-base-carousel/1.0/base-carousel.jss';
+import {waitFor} from '../../../../testing/test-helper';
+
+describes.realWin(
+  'amp-stream-gallery',
+  {
+    amp: {
+      runtimeOn: true,
+      extensions: ['amp-stream-gallery:1.0'],
+    },
+  },
+  (env) => {
+    let win;
+    let element;
+    const userSuppliedChildren = [];
+
+    const styles = useStyles();
+
+    beforeEach(async () => {
+      win = env.win;
+      toggleExperiment(win, 'amp-stream-gallery-bento', true);
+      element = createElementWithAttributes(
+        win.document,
+        'amp-stream-gallery',
+        {
+          'layout': 'fixed',
+          'width': '300px',
+          'height': '200px',
+        }
+      );
+
+      // Add slides.
+      for (let i = 0; i < 3; i++) {
+        const slide = newSlide(`${i}`);
+        userSuppliedChildren[i] = slide;
+        element.appendChild(slide);
+      }
+    });
+
+    afterEach(() => {
+      toggleExperiment(win, 'amp-stream-gallery-bento', false);
+    });
+
+    function newSlide(id) {
+      const slide = document.createElement('div');
+      slide.className = 'test-slide';
+      setStyles(slide, {
+        flexShrink: '0',
+        height: '100px',
+        width: '100%',
+      });
+      slide.textContent = 'slide ' + id;
+      return slide;
+    }
+
+    async function getSlideWrappersFromShadow() {
+      await element.build();
+      const shadow = element.shadowRoot;
+      await waitForChildPromise(shadow, (shadow) => {
+        return shadow.querySelectorAll('[class*=hideScrollbar]');
+      });
+      await waitFor(
+        () =>
+          shadow.querySelectorAll(`[class*=${styles.hideScrollbar}] `).length >
+          0,
+        'slots rendered'
+      );
+      return shadow.querySelectorAll(
+        `[class*=${styles.hideScrollbar}] [class*=${styles.slideElement}]`
+      );
+    }
+
+    async function getSlidesFromShadow() {
+      const wrappers = await getSlideWrappersFromShadow();
+      const slots = Array.from(wrappers)
+        .map((wrapper) => wrapper.querySelector('slot'))
+        .filter((slot) => !!slot);
+      return toArray(slots).reduce(
+        (acc, slot) => acc.concat(slot.assignedElements()),
+        []
+      );
+    }
+
+    it('should render slides and arrows when built', async () => {
+      win.document.body.appendChild(element);
+
+      const renderedSlides = await getSlidesFromShadow();
+      expect(renderedSlides).to.have.ordered.members(
+        userSuppliedChildren.slice(0, 2)
+      );
+      const buttons = element.shadowRoot.querySelectorAll('button');
+      expect(buttons).to.have.length(2);
+    });
+
+    it('should render custom arrows when given', async () => {
+      const customPrev = createElementWithAttributes(win.document, 'button', {
+        'slot': 'prev-arrow',
+      });
+      const customNext = createElementWithAttributes(win.document, 'button', {
+        'slot': 'next-arrow',
+      });
+      element.appendChild(customPrev);
+      element.appendChild(customNext);
+      win.document.body.appendChild(element);
+      await element.build();
+
+      const renderedSlides = await getSlidesFromShadow();
+      expect(renderedSlides).to.have.ordered.members(
+        userSuppliedChildren.slice(0, 2)
+      );
+
+      const defaultButtons = element.shadowRoot.querySelectorAll('button');
+      expect(defaultButtons).to.have.length(0);
+
+      const slotButtons = element.shadowRoot.querySelectorAll(
+        'slot[name="prev-arrow"], slot[name="next-arrow"]'
+      );
+      expect(slotButtons).to.have.length(2);
+      expect(slotButtons[0].assignedElements()).to.have.ordered.members([
+        customPrev,
+      ]);
+      expect(slotButtons[1].assignedElements()).to.have.ordered.members([
+        customNext,
+      ]);
+    });
+
+    it('should render in preparation for looping with loop prop', async () => {
+      element.setAttribute('loop', '');
+      win.document.body.appendChild(element);
+      await element.build();
+
+      const renderedSlideWrappers = await getSlideWrappersFromShadow();
+      // Given slides [0][1][2] should be rendered as [2][0][1]. But [2] is
+      // a placeholder.
+      expect(renderedSlideWrappers).to.have.lengthOf(3);
+      expect(renderedSlideWrappers[0].querySelector('slot')).to.be.null;
+      expect(
+        renderedSlideWrappers[1].querySelector('slot').assignedElements()
+      ).to.deep.equal([userSuppliedChildren[0]]);
+      expect(
+        renderedSlideWrappers[2].querySelector('slot').assignedElements()
+      ).to.deep.equal([userSuppliedChildren[1]]);
+    });
+  }
+);
---
+++ extensions/amp-stream-gallery/1.0/test/test-stream-gallery.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../../src/preact';
+import {StreamGallery} from '../stream-gallery';
+import {mount} from 'enzyme';
+
+describes.sandboxed('StreamGallery preact component', {}, () => {
+  it('should render BaseCarousel', () => {
+    const jsx = (
+      <StreamGallery>
+        <div>slide 1</div>
+        <div>slide 2</div>
+        <div>slide 3</div>
+      </StreamGallery>
+    );
+    const wrapper = mount(jsx);
+    const carousel = wrapper.find('BaseCarousel');
+    expect(carousel).to.have.lengthOf(1);
+    expect(wrapper.find('Arrow')).to.have.lengthOf(2);
+
+    const slides = wrapper.find('[data-slide]');
+    expect(slides).to.have.lengthOf(3);
+
+    expect(slides.first().text()).to.equal('slide 1');
+    expect(slides.at(1).text()).to.equal('slide 2');
+    expect(slides.last().text()).to.equal('');
+  });
+
+  it('should render custom Arrows when given', () => {
+    const arrowPrev = <div class="my-custom-arrow-prev">left</div>;
+    const arrowNext = <div class="my-custom-arrow-next">right</div>;
+    const jsx = (
+      <StreamGallery arrowPrev={arrowPrev} arrowNext={arrowNext}>
+        <div>slide 1</div>
+        <div>slide 2</div>
+        <div>slide 3</div>
+      </StreamGallery>
+    );
+    const wrapper = mount(jsx);
+    const arrows = wrapper.find('Arrow');
+    expect(arrows).to.have.lengthOf(2);
+    expect(arrows.first().props().customArrow).to.equal(arrowPrev);
+    expect(arrows.last().props().customArrow).to.equal(arrowNext);
+  });
+
+  it('should not loop by default', () => {
+    const jsx = (
+      <StreamGallery>
+        <div class="my-slide">slide 1</div>
+        <div class="my-slide">slide 2</div>
+        <div class="my-slide">slide 3</div>
+      </StreamGallery>
+    );
+    const wrapper = mount(jsx);
+    const slides = wrapper.find('[data-slide]');
+    expect(slides).to.have.lengthOf(3);
+
+    // Given slides [1][2][3] should be rendered as is, but [3] is a
+    // placeholder.
+    expect(slides.first().text()).to.equal('slide 1');
+    expect(slides.at(1).text()).to.equal('slide 2');
+    expect(slides.last().text()).to.equal('');
+  });
+
+  it('should render in preparation for looping with loop prop', () => {
+    const jsx = (
+      <StreamGallery loop>
+        <div class="my-slide">slide 1</div>
+        <div class="my-slide">slide 2</div>
+        <div class="my-slide">slide 3</div>
+      </StreamGallery>
+    );
+    const wrapper = mount(jsx);
+    const slides = wrapper.find('[data-slide]');
+    expect(slides).to.have.lengthOf(3);
+
+    // Given slides [1][2][3] should be rendered as [3][1][2]. But [3] is a
+    // placeholder.
+    expect(slides.at(0).text()).to.equal('');
+    expect(slides.at(1).text()).to.equal('slide 1');
+    expect(slides.at(2).text()).to.equal('slide 2');
+  });
+});
