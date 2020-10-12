d67dc61675b84376c08d71ae8e62dd4138db6cc3
Matias Szylkowski <mszylkowski@google.com>
2020-10-12 16:23:25 -0400

    🐛Video analytics bug (#30481)
    * Started researching the video bug
    
    * Call load event
    
    * Changing video-manager-impl
    
    * Cleaned up
    
    * Cleaned up more
    
    * Cleaned up even more
    
    * Removed unused import script
    
    * Reverted videos.html
    
    * Fixed underlying bug
    
    * Forgot to remove mediapool fix
    
    * Simplified amp-video changes
    
    * Fix comment
    
    * Remove console.log
    
    * Removed example story
    
    * Removed unused import
    
    * onVideoLoaded private no comment

--- src/service/video-manager-impl.js
+++ src/service/video-manager-impl.js
@@ -38,13 +38,7 @@ import {Services} from '../services';
 import {VideoSessionManager} from './video-session-manager';
 import {VideoUtils, getInternalVideoElementFor} from '../utils/video';
 import {clamp} from '../utils/math';
-import {
-  createCustomEvent,
-  getData,
-  listen,
-  listenOnce,
-  listenOncePromise,
-} from '../event-helper';
+import {createCustomEvent, getData, listen, listenOnce} from '../event-helper';
 import {dev, devAssert, user, userAssert} from '../log';
 import {dict, map} from '../utils/object';
 import {getMode} from '../mode';
@@ -517,9 +511,7 @@ class VideoEntry {
       this.video.pause();
     };
 
-    listenOncePromise(video.element, VideoEvents.LOAD).then(() =>
-      this.videoLoaded()
-    );
+    listen(video.element, VideoEvents.LOAD, () => this.videoLoaded());
     listen(video.element, VideoEvents.PAUSE, () => this.videoPaused_());
     listen(video.element, VideoEvents.PLAY, () => {
       this.hasSeenPlayEvent_ = true;
