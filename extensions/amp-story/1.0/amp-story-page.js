a1e06218c67632ed01dbb7441625bf10549a95fa
Matias Szylkowski <mszylkowski@google.com>
2020-10-12 10:49:53 -0400

    Chained promises

--- extensions/amp-story/1.0/amp-story-page.js
+++ extensions/amp-story/1.0/amp-story-page.js
@@ -409,14 +409,14 @@ export class AmpStoryPage extends AMP.BaseElement {
       'amp-story-page must be a descendant of amp-story.'
     );
 
-    whenUpgradedToCustomElement(storyEl).then(() => {
-      storyEl.getImpl().then(
+    whenUpgradedToCustomElement(storyEl)
+      .then(() => storyEl.getImpl())
+      .then(
         (storyImpl) => {
           this.mediaPoolResolveFn_(MediaPool.for(storyImpl));
         },
         (reason) => this.mediaPoolRejectFn_(reason)
       );
-    });
   }
 
   /**
