2c3f8219d9cbbf89e34529364b4ae0222fae1dc9
Alan Orozco <alanorozco@users.noreply.github.com>
2020-10-13 13:49:34 -0700

    ✨ Bento <amp-video> (#30280)
    Tracker #30330

---
+++ extensions/amp-video/1.0/base-element.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {ActionTrust} from '../../../src/action-constants';
+import {CSS} from './autoplay.jss';
+import {PreactBaseElement} from '../../../src/preact/base-element';
+import {VideoWrapper} from './video-wrapper';
+import {isLayoutSizeDefined} from '../../../src/layout';
+
+/** @extends {PreactBaseElement<VideoWrapperDef.Api>} */
+export class VideoBaseElement extends PreactBaseElement {
+  /** @override */
+  init() {
+    this.registerApiAction_('play', (api) => api.play());
+    this.registerApiAction_('pause', (api) => api.pause());
+    this.registerApiAction_('mute', (api) => api.mute());
+    this.registerApiAction_('unmute', (api) => api.unmute());
+
+    // Ugly that this action has three names, but:
+    // - requestFullscreen for consistency with Element.requestFullscreen
+    // - fullscreenenter / fullscreen are for backwards compatibility
+    const requestFullscreen = (api) => api.requestFullscreen();
+    this.registerApiAction_('requestFullscreen', requestFullscreen);
+    this.registerApiAction_('fullscreenenter', requestFullscreen);
+    this.registerApiAction_('fullscreen', requestFullscreen);
+  }
+
+  /**
+   * @param {string} alias
+   * @param {function(!VideoWrapperDef.Api, !../../../src/service/action-impl.ActionInvocation)} handler
+   * @param {!../../../src/action-constants.ActionTrust=} minTrust
+   * @private
+   */
+  registerApiAction_(alias, handler, minTrust = ActionTrust.HIGH) {
+    this.registerApiAction(
+      alias,
+      (api, invocation) => {
+        if (invocation.trust >= ActionTrust.HIGH) {
+          // TODO(alanorozco): There may be a better solution that doesn't
+          // require this method which is not standard in HTMLMediaElement, like
+          // potentially toggling `autoplay` instead.
+          api.userInteracted();
+        }
+        handler(api, invocation);
+      },
+      minTrust
+    );
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    return isLayoutSizeDefined(layout);
+  }
+}
+
+/** @override */
+VideoBaseElement['Component'] = VideoWrapper;
+
+/** @override */
+VideoBaseElement['layoutSizeDefined'] = true;
+
+/**
+ * Defaults to `{component: 'video'}` from `VideoWrapper` component.
+ * Subclasses may set:
+ * ```
+ *   AmpMyPlayer['staticProps'] = dict({
+ *     'component': MyPlayerComponent,
+ *   });
+ * ```
+ * @override
+ */
+VideoBaseElement['staticProps'];
+
+/** @override */
+VideoBaseElement['props'] = {
+  'album': {attr: 'album'},
+  'alt': {attr: 'alt'},
+  'artist': {attr: 'artist'},
+  'artwork': {attr: 'artwork'},
+  'attribution': {attr: 'attribution'},
+  'autoplay': {attr: 'autoplay', type: 'boolean'},
+  'controls': {attr: 'controls', type: 'boolean'},
+  'controlslist': {attr: 'controlslist'},
+  'crossorigin': {attr: 'crossorigin'},
+  'disableremoteplayback': {attr: 'disableremoteplayback'},
+  'loop': {attr: 'loop', type: 'boolean'},
+  'noaudio': {attr: 'noaudio', type: 'boolean'},
+  'poster': {attr: 'poster'},
+  'src': {attr: 'src'},
+  'title': {attr: 'title'},
+
+  // TODO(alanorozco): These props have no internal implementation yet.
+  'dock': {attr: 'dock'},
+  'rotate-to-fullscreen': {attr: 'rotate-to-fullscreen', type: 'boolean'},
+};
+
+/** @override */
+VideoBaseElement['children'] = {
+  'sources': {
+    name: 'sources',
+    selector: 'source',
+    single: false,
+    clone: true,
+  },
+};
+
+/** @override */
+VideoBaseElement['shadowCss'] = CSS;
---
+++ extensions/amp-video/1.0/test/test-amp-video.js
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../amp-video';
+import {htmlFor} from '../../../../src/static-template';
+import {waitForChildPromise} from '../../../../src/dom';
+import {whenCalled} from '../../../../testing/test-helper.js';
+
+describes.realWin(
+  'amp-video-v1.0',
+  {
+    amp: {
+      runtimeOn: true,
+      extensions: ['amp-video:1.0'],
+      canonicalUrl: 'https://canonicalexample.com/',
+    },
+  },
+  (env) => {
+    let html;
+    let element;
+
+    const waitForRender = async () => {
+      await whenCalled(env.sandbox.spy(element, 'attachShadow'));
+      const shadow = element.shadowRoot;
+      await waitForChildPromise(shadow, (shadow) => {
+        return shadow.querySelector('video');
+      });
+    };
+
+    beforeEach(() => {
+      html = htmlFor(env.win.document);
+    });
+
+    it('renders video element', async () => {
+      element = html`
+        <amp-video layout="responsive" width="16" height="9">
+          <source src="foo" type="bar/baz" />
+        </amp-video>
+      `;
+
+      env.win.document.body.appendChild(element);
+
+      await waitForRender();
+
+      const video = element.shadowRoot.querySelector('video');
+      expect(video).to.not.be.null;
+    });
+
+    it('passes attributes through to <video>', async () => {
+      element = html`
+        <amp-video
+          layout="responsive"
+          width="16"
+          height="9"
+          src="something.mp4"
+          poster="foo.png"
+          loop
+        ></amp-video>
+      `;
+
+      env.win.document.body.appendChild(element);
+
+      await waitForRender();
+
+      const video = element.shadowRoot.querySelector('video');
+      expect(video.getAttribute('src')).to.equal(element.getAttribute('src'));
+      expect(video.getAttribute('poster')).to.equal(
+        element.getAttribute('poster')
+      );
+      expect(video.getAttribute('loop')).to.equal(element.getAttribute('loop'));
+    });
+
+    it('clones <source> elements', async () => {
+      element = html`
+        <amp-video layout="responsive" width="16" height="9">
+          <source src="foo" type="bar/baz" />
+          <source src="something.mp4" type="application/mp4" />
+        </amp-video>
+      `;
+
+      env.win.document.body.appendChild(element);
+
+      const inputSources = element.querySelectorAll('source');
+
+      await waitForRender();
+
+      const outputSources = element.shadowRoot.querySelectorAll('source');
+
+      for (let i = 0; i < inputSources.length; i++) {
+        expect(outputSources[i]).to.not.be.null;
+        expect(outputSources[i].getAttribute('src')).to.eql(
+          inputSources[i].getAttribute('src')
+        );
+        expect(outputSources[i].getAttribute('type')).to.eql(
+          inputSources[i].getAttribute('type')
+        );
+      }
+    });
+  }
+);
