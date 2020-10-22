9533b2e789ed07c990c7a1d897cbd08f4ab425be
Jake Fried <samouri@users.noreply.github.com>
2020-10-22 16:35:08 -0400

    amp-list: single-item a11y warning.

--- extensions/amp-list/0.1/amp-list.js
+++ extensions/amp-list/0.1/amp-list.js
@@ -699,8 +699,16 @@ export class AmpList extends AMP.BaseElement {
       itemsExpr,
       this.element
     );
-    if (this.element.hasAttribute('single-item') && !isArray(items)) {
-      items = [items];
+    if (this.element.hasAttribute('single-item')) {
+      if (!isArray(items)) {
+        items = [items];
+      } else {
+        user().warn(
+          TAG,
+          'items must be a non-array Object if "single-item" is enabled.',
+          this.element
+        );
+      }
     }
     items = user().assertArray(items);
     if (this.element.hasAttribute('max-items')) {
