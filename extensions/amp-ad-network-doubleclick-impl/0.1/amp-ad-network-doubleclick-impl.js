d5a3c21c866f208c5947ace03621192002806afb
Jake Fried <samouri@users.noreply.github.com>
2020-10-30 16:02:10 -0400

    Remove BaseElement.viewportCallback from ads. (#30859)
    * amp-ads / a4a: remove BaseElement.viewportCallback
    
    * private --> protected
    
    * fix doubleclick test
    
    * protected fix, this.isInViewport fix, delay observe
    
    * Update test-doubleclick-fluid.js
    
    * make code fine with a test that calls layooutCallback twice in a row

--- extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
@@ -1150,8 +1150,8 @@ export class AmpAdNetworkDoubleclickImpl extends AmpA4A {
   }
 
   /** @override */
-  viewportCallback(inViewport) {
-    super.viewportCallback(inViewport);
+  viewportCallbackTemp(inViewport) {
+    super.viewportCallbackTemp(inViewport);
     if (this.reattemptToExpandFluidCreative_ && !inViewport) {
       // If the initial expansion attempt failed (e.g., the slot was within the
       // viewport), then we will re-attempt to expand it here whenever the slot
