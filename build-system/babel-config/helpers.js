e9bf0c353b20f8bd9fa6c0c902b19d75a1a5c1da
Erwin Mombay <erwinm@google.com>
2020-11-02 13:10:18 -0800

    apply recs

--- build-system/babel-config/helpers.js
+++ build-system/babel-config/helpers.js
@@ -63,7 +63,6 @@ function getReplacePlugin() {
   const replacements = [
     createReplacement('IS_ESM', argv.esm || argv.sxg),
     createReplacement('IS_SXG', argv.sxg),
-    createReplacement('IS_FORTESTING', argv.fortesting),
   ];
 
   const experimentConstant = getExperimentConstant();
