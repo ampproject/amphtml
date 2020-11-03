3c1d10a842984e7017281b848ff819839b0b23c9
Jake Fried <samouri@users.noreply.github.com>
2020-11-03 17:35:07 -0500

    resource: remove slow element ratio (ser) (#30972)

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -208,18 +208,6 @@ export class ResourcesImpl {
       this.ampdoc.getVisibilityState()
     );
 
-    /**
-     * Number of resources that were laid out after entering viewport.
-     * @private {number}
-     */
-    this.slowLayoutCount_ = 0;
-
-    /**
-     * Total number of laid out resources.
-     * @private {number}
-     */
-    this.totalLayoutCount_ = 0;
-
     /** @private {?IntersectionObserver} */
     this.intersectionObserver_ = null;
 
@@ -831,14 +819,6 @@ export class ResourcesImpl {
       this.ampdoc.signals().signal(READY_SCAN_SIGNAL);
       dev().fine(TAG_, 'signal: ready-scan');
     }
-  }
-
-  /** @override */
-  getSlowElementRatio() {
-    if (this.totalLayoutCount_ === 0) {
-      return 0;
-    }
-    return this.slowLayoutCount_ / this.totalLayoutCount_;
   }
 
   /**
@@ -1671,11 +1651,6 @@ export class ResourcesImpl {
    * @private
    */
   taskComplete_(task, success, opt_reason) {
-    this.totalLayoutCount_++;
-    if (task.resource.isInViewport() && this.firstVisibleTime_ >= 0) {
-      this.slowLayoutCount_++;
-    }
-
     this.exec_.dequeue(task);
     this.schedulePass(POST_TASK_PASS_DELAY_);
     if (!success) {
