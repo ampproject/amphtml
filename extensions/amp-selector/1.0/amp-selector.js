8f1b21f40f705ebdcb782dbcf1d97d86badd6bd1
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2020-11-04 10:40:10 -0500

    ✅ [amp-selector:1.0] Turn on e2e tests (#28576)
    * Turn on e2e test
    
    * New line EOF
    
    * Enable tests
    
    * Fix default-state bugs
    
    * Replace dot access with property access

--- extensions/amp-selector/1.0/amp-selector.js
+++ extensions/amp-selector/1.0/amp-selector.js
@@ -86,7 +86,17 @@ class AmpSelector extends PreactBaseElement {
           'value',
           []
         ));
-        const toggleValue = toggle(index, initialValue, options, opt_select);
+        const isMultiple = /** @type {boolean} */ (this.getProp(
+          'multiple',
+          false
+        ));
+        const toggleValue = toggle(
+          index,
+          initialValue,
+          options,
+          isMultiple,
+          opt_select
+        );
         if (!toggleValue) {
           return;
         }
@@ -153,8 +163,8 @@ function getOptionState(element, mu) {
           '[option]'
         )
     )
-    .forEach((child) => {
-      const option = child.getAttribute('option');
+    .forEach((child, index) => {
+      const option = child.getAttribute('option') || index.toString();
       const selected = child.hasAttribute('selected');
       const disabled = child.hasAttribute('disabled');
       const props = {
@@ -208,11 +218,12 @@ function fireSelectEvent(win, action, el, option, value, trust) {
  * @param {number} index
  * @param {!Array<string>} value
  * @param {Array<string>} options
+ * @param {boolean} isMultiple
  * @param {boolean=} opt_select
  * @return {JsonObject|undefined}
  */
-function toggle(index, value, options, opt_select) {
-  userAssert(index, "'index' must be specified");
+function toggle(index, value, options, isMultiple, opt_select) {
+  userAssert(typeof index === 'number', "'index' must be specified");
   userAssert(index >= 0, "'index' must be greater than 0");
   userAssert(
     index < options.length,
@@ -224,7 +235,11 @@ function toggle(index, value, options, opt_select) {
     if (opt_select == false) {
       return;
     }
-    value.push(option);
+    if (isMultiple) {
+      value.push(option);
+    } else {
+      value = [option];
+    }
   } else {
     if (opt_select == true) {
       return;
@@ -283,9 +298,12 @@ function OptionShim({
     if (!onClick) {
       return;
     }
-    shimDomElement.addEventListener('click', onClick);
+    shimDomElement.addEventListener('click', (e) => onClick(e));
     return () => {
-      shimDomElement.removeEventListener('click', devAssert(onClick));
+      shimDomElement.removeEventListener(
+        'click',
+        devAssert((e) => onClick(e))
+      );
     };
   }, [shimDomElement, onClick]);
 
@@ -336,13 +354,13 @@ function SelectorShim({
 }
 
 /** @override */
-AmpSelector.Component = SelectorShim;
+AmpSelector['Component'] = SelectorShim;
 
 /** @override */
-AmpSelector.detached = true;
+AmpSelector['detached'] = true;
 
 /** @override */
-AmpSelector.props = {
+AmpSelector['props'] = {
   'disabled': {attr: 'disabled', type: 'boolean'},
   'multiple': {attr: 'multiple', type: 'boolean'},
   'name': {attr: 'name'},
