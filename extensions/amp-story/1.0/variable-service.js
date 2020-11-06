93214d3bb4e697bbe33354a909da8f8ea1772172
Raksha Muthukumar <10503669+raxsha@users.noreply.github.com>
2020-11-06 13:04:35 -0500

    🐛 Fixing storyProgress calculation so that values are [0,1] (#31024)
    * Fixing storyProgress calculation
    
    * fixing breaking test

--- extensions/amp-story/1.0/variable-service.js
+++ extensions/amp-story/1.0/variable-service.js
@@ -121,7 +121,7 @@ export class AmpStoryVariableService {
           .length;
         if (numberOfPages > 0) {
           this.variables_[AnalyticsVariable.STORY_PROGRESS] =
-            pageIndex / numberOfPages;
+            pageIndex / (numberOfPages - 1);
         }
       },
       true /* callToInitialize */
