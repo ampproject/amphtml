84837a31b9682750d7a44663fe40b9e34aa3ca19
Priyam Mohanty <primoh@google.com>
2020-11-13 15:20:13 -0800

    Bento port of amp-youtube (#30444)
    Design doc: https://docs.google.com/document/d/1PaQZyQw1HEi5G-B6DW0uO-UG53rkBqmQ5WLyEfQTKrI/edit?usp=sharing
    Tracker: #30443

---
+++ extensions/amp-youtube/1.0/storybook/Basic.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../../src/preact';
+import {Youtube} from '../youtube';
+import {boolean, number, object, text, withKnobs} from '@storybook/addon-knobs';
+import {withA11y} from '@storybook/addon-a11y';
+
+export default {
+  title: 'YouTube',
+  component: Youtube,
+  decorators: [withA11y, withKnobs],
+};
+
+export const _default = () => {
+  const width = number('width', 300);
+  const height = number('height', 200);
+  const videoid = text('videoid', 'IAvf-rkzNck');
+  const autoplay = boolean('autoplay', false);
+  const loop = boolean('loop', false);
+  const params = object('params', {});
+  const credentials = text('credentials', 'include');
+  return (
+    <Youtube
+      autoplay={autoplay}
+      loop={loop}
+      videoid={videoid}
+      params={params}
+      style={{width, height}}
+      credentials={credentials}
+    />
+  );
+};
+
+export const liveChannelId = () => {
+  const width = number('width', 300);
+  const height = number('height', 200);
+  const liveChannelid = text('liveChannelid', 'sKCkM-f2Qk4');
+  const autoplay = boolean('autoplay', false);
+  const loop = boolean('loop', false);
+  const params = object('params', {});
+  const credentials = text('credentials', 'include');
+  return (
+    <Youtube
+      autoplay={autoplay}
+      loop={loop}
+      liveChannelid={liveChannelid}
+      params={params}
+      style={{width, height}}
+      credentials={credentials}
+    />
+  );
+};
