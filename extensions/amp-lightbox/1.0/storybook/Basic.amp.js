12fd30a9df02611fd58c56e725f10a66cb2444d3
Priyam Mohanty <primoh@google.com>
2020-11-13 16:31:47 -0800

    ✨ Bento port of amp-lightbox (#30836)
    * First pass of amp-lightbox. Still many TODOs left.
    
    * Added animations, cleaned up Preact code
    
    * Added AMP lightbox (barebones) + storybook
    
    * ImperativeAPI for actions
    
    * Sampling AMP actions
    
    * Usage of imperative API. TODO: portaling
    
    * removed console.log
    
    * fixed height/width and aria-button onClick
    
    * Updated typedefs and reverted width/height change
    
    * Updated storybook
    
    * Used two states to get animations to work + addressed comments, etc.
    
    * Used two states to get animations to work + addressed comments, etc.
    
    * Fixed some small errors
    
    * Fixed animation flickering!
    
    * Small code quality changes + AMP mode doesn't work currently
    
    * Added some AMP config stuff
    
    * Addressed comments for Preact mode.
    
    * Updated async getRefElement code
    
    * Added explanatory comment.
    
    * Commit for others to try debugging amp mode (not reviewable commit)
    
    * Added AMP mode to AMP lightbox
    
    * Update experiments-config.js
    
    * Addressed comments
    
    * Addressed comments
    
    * Added CSS support to amp-lightbox
    
    * New, clearer flow for states. enableAnimation flag added.
    
    * Fixed enableAnimation error
    
    * Fix for gulp task fails
    
    * replaced setTimeout
    
    * keep visible and mounted in sync outside of transitions
    
    * Removed onfinish from cleanup
    
    * useValueRef due to useRef initializing not reflecting prop changes
    
    * No refName needed
    
    * Fixed types
    
    * Full usage of useValueRef
    
    * Removed console.log

---
+++ extensions/amp-lightbox/1.0/storybook/Basic.amp.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../../src/preact';
+import {boolean, select, withKnobs} from '@storybook/addon-knobs';
+import {withA11y} from '@storybook/addon-a11y';
+import {withAmp} from '@ampproject/storybook-addon';
+
+export default {
+  title: 'amp-lightbox',
+  decorators: [withKnobs, withA11y, withAmp],
+
+  parameters: {
+    extensions: [{name: 'amp-lightbox', version: '1.0'}],
+  },
+};
+
+export const Default = () => {
+  const animateIn = select('animate-in', [
+    'fade-in',
+    'fly-in-top',
+    'fly-in-bottom',
+  ]);
+  const enableAnimation = boolean('enable animation', true);
+  return (
+    <main>
+      <amp-lightbox
+        id="lightbox"
+        layout="nodisplay"
+        animate-in={animateIn}
+        enable-animation={enableAnimation}
+      >
+        <p>Test</p>
+        <button on="tap:lightbox.close">Close</button>
+      </amp-lightbox>
+      <div class="buttons" style={{marginTop: 8}}>
+        <button on="tap:lightbox.open">Open</button>
+      </div>
+    </main>
+  );
+};
+
+Default.story = {
+  name: 'Default',
+};
---
+++ extensions/amp-lightbox/1.0/storybook/Basic.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../../src/preact';
+import {Lightbox} from '../lightbox';
+import {boolean, select, withKnobs} from '@storybook/addon-knobs';
+import {useRef} from '../../../../src/preact';
+import {withA11y} from '@storybook/addon-a11y';
+
+export default {
+  title: 'Lightbox',
+  component: Lightbox,
+  decorators: [withA11y, withKnobs],
+};
+
+/**
+ * @param {!Object} props
+ * @return {*}
+ */
+function LightboxWithActions(props) {
+  const ref = useRef();
+  return (
+    <section>
+      <Lightbox ref={ref} {...props} />
+      <div style={{marginTop: 8}}>
+        <button onClick={() => ref.current.open()}>open</button>
+      </div>
+    </section>
+  );
+}
+
+export const _default = () => {
+  const open = boolean('initialOpen', false);
+  const animateIn = select('animateIn', [
+    'fade-in',
+    'fly-in-top',
+    'fly-in-bottom',
+  ]);
+  const enableAnimation = boolean('enable animation', true);
+  return (
+    <div>
+      <LightboxWithActions
+        id="lightbox"
+        layout="nodisplay"
+        initialOpen={open}
+        animateIn={animateIn}
+        enableAnimation={enableAnimation}
+      >
+        Lorem <i>ips</i>um dolor sit amet, has nisl nihil convenire et, vim at
+        aeque inermis reprehendunt.
+      </LightboxWithActions>
+    </div>
+  );
+};
