ceeb745ecd767fd19b2e086d52d7e5fc1616c54a
processprocess <philip.hunter.bell@gmail.com>
2020-12-01 16:27:51 -0500

    Replace canvas with image for percy dom state.

---
+++ build-system/tasks/visual-diff/snippets/freeze-canvas-image.js
@@ -0,0 +1,14 @@
+// This file is executed via Puppeteer's page.evaluate on a document to copy the
+// current image data of the canvas to an attribute so that it will be passed in the
+// snapshots to Percy.
+
+const canvases = document.querySelectorAll('canvas');
+canvases.forEach((canvas) => {
+  const parentNode = canvas.parentElement;
+  const img = document.createElement('img');
+  img.style.width = '100%';
+  img.style.height = '100%';
+  img.setAttribute('src', canvas.toDataURL('image/png', 1));
+  parentNode.appendChild(img);
+  parentNode.removeChild(canvas);
+});
