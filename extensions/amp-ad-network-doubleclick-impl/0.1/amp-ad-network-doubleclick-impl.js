000e5cf203bd456b18297e6922aee8c6e7a0382b
lannka <hungfei.ding@gmail.com>
2020-12-04 15:12:02 -0800

    no nested

--- extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
+++ extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js
@@ -481,7 +481,7 @@ export class AmpAdNetworkDoubleclickImpl extends AmpA4A {
       },
       {
         experimentId: IDLE_CWV_EXP,
-        isTrafficEligible: () => () => {
+        isTrafficEligible: () => {
           return (
             !!this.performance_ &&
             !this.element.getAttribute('data-loading-strategy')
