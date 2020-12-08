500e7b1368430e3dd7283422f31b72ace8cc5101
processprocess <philip.hunter.bell@gmail.com>
2020-12-08 12:55:01 -0500

    Position vertical and horizontal image elements.

--- extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
@@ -16,25 +16,65 @@
 
 import {CSS} from '../../../build/amp-story-panning-media-0.1.css';
 import {Layout} from '../../../src/layout';
+import {whenUpgradedToCustomElement} from '../../../src/dom';
+import {dev, user} from '../../../src/log';
+import {CommonSignals} from '../../../src/common-signals';
+import {setStyles} from '../../../src/style';
+
+/** @const {string} */
+const TAG = 'AMP_STORY_PANNING_MEDIA';
 
 export class AmpStoryPanningMedia extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
 
-    /** @private {string} */
-    this.myText_ = 'hello world';
+    /** @private {?Element} */
+    this.element_ = element;
 
     /** @private {?Element} */
     this.container_ = null;
+
+    /** @private {?Element} */
+    this.image_ = null;
+
+    this.x_ = element.getAttribute('x');
+    this.y_ = element.getAttribute('y');
+    this.zoom_ = parseFloat(element.getAttribute('zoom') || 1);
   }
 
   /** @override */
   buildCallback() {
-    this.container_ = this.element.ownerDocument.createElement('div');
-    this.container_.textContent = this.myText_;
-    this.element.appendChild(this.container_);
-    this.applyFillContent(this.container_, /* replacedContent */ true);
+    console.log(this.x_);
+    console.log(this.y_);
+    console.log(this.zoom_);
+  }
+
+  /** @override */
+  layoutCallback() {
+    const ampImgEl = this.element_.querySelector('amp-img');
+    setStyles(ampImgEl, {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    });
+
+    return whenUpgradedToCustomElement(ampImgEl)
+      .then(() => {
+        return ampImgEl.signals().whenSignal(CommonSignals.LOAD_END);
+      })
+      .then(
+        () => {
+          this.image_ = dev().assertElement(this.element.querySelector('img'));
+          this.image_.classList = '';
+          this.image_.width > this.image_.height
+            ? (this.image_.style.height = '100%')
+            : (this.image_.style.width = '100%');
+        },
+        () => {
+          user().error(TAG, 'Failed to load the amp-img.');
+        }
+      );
   }
 
   /** @override */
