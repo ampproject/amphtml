4c1ed4b3608ad0555b2251b9d73feff25075c2a4
Philip Bell <philip.hunter.bell@gmail.com>
2020-12-10 15:35:05 -0500

    ✨ [panning media] positioning api (#31517)
    * Position vertical and horizontal image elements.
    
    * Position element. Visual debug coords on image.
    
    * lint fix
    
    * Implicit return. Page ID fix. Styles in CSS.
    
    * Delete code.
    
    * Implicit return. Newline.
    
    * Update test. Fix width and height in html template.
    
    * newline
    
    * newline
    
    * space.
    
    * space.
    
    * Comment format.
    
    * Test image child.
    
    * Update tests
    
    * Remove whitespace in css
    
    * await layout callback.
    
    * Await amp-img loading

--- extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
@@ -15,26 +15,72 @@
  */
 
 import {CSS} from '../../../build/amp-story-panning-media-0.1.css';
+import {CommonSignals} from '../../../src/common-signals';
 import {Layout} from '../../../src/layout';
+import {dev, user} from '../../../src/log';
+import {setStyles} from '../../../src/style';
+import {whenUpgradedToCustomElement} from '../../../src/dom';
+
+/** @const {string} */
+const TAG = 'AMP_STORY_PANNING_MEDIA';
 
 export class AmpStoryPanningMedia extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
 
-    /** @private {string} */
-    this.myText_ = 'hello world';
+    /** @private {?Element} */
+    this.element_ = element;
 
     /** @private {?Element} */
-    this.container_ = null;
+    this.image_ = null;
+
+    /** @private {?string} */
+    this.x_ = null;
+
+    /** @private {?string} */
+    this.y_ = null;
+
+    /** @private {?string} */
+    this.zoom_ = null;
   }
 
   /** @override */
   buildCallback() {
-    this.container_ = this.element.ownerDocument.createElement('div');
-    this.container_.textContent = this.myText_;
-    this.element.appendChild(this.container_);
-    this.applyFillContent(this.container_, /* replacedContent */ true);
+    this.x_ = this.element_.getAttribute('x') || '0%';
+    this.y_ = this.element_.getAttribute('y') || '0%';
+    this.zoom_ = this.element_.getAttribute('zoom') || '1';
+  }
+
+  /** @override */
+  layoutCallback() {
+    const ampImgEl = dev().assertElement(
+      this.element_.querySelector('amp-img')
+    );
+    return whenUpgradedToCustomElement(ampImgEl)
+      .then(() => ampImgEl.signals().whenSignal(CommonSignals.LOAD_END))
+      .then(() => {
+        this.image_ = dev().assertElement(this.element.querySelector('img'));
+        // Remove layout="fill" classes so image is not clipped.
+        this.image_.classList = '';
+        // Fill image to 100% height of viewport.
+        // TODO(#31515): Handle base zoom of aspect ratio wider than image
+        setStyles(this.image_, {height: '100%'});
+        return this.updatePosition_();
+      })
+      .catch(() => user().error(TAG, 'Failed to load the amp-img.'));
+  }
+
+  /**
+   * @return {!Promise}
+   * @private
+   */
+  updatePosition_() {
+    return this.mutateElement(() =>
+      setStyles(this.image_, {
+        transform: `scale(${this.zoom_}) translate(${this.x_}, ${this.y_})`,
+      })
+    );
   }
 
   /** @override */
