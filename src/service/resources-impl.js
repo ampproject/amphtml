f698c260c3fb31f2bfbf910950a14eac5ce32139
Jake Fried <samouri@users.noreply.github.com>
2020-12-11 15:09:01 -0500

    revert resoruces-impl

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -234,23 +234,22 @@ export class ResourcesImpl {
       // Classic IntersectionObserver doesn't support viewport tracking and
       // rootMargin in x-origin iframes (#25428). As of 1/2020, only Chrome 81+
       // supports it via {root: document}, which throws on other browsers.
+      const root = /** @type {?Element} */ (this.ampdoc.isSingleDoc() && iframed
+        ? /** @type {*} */ (this.win.document)
+        : null);
       try {
-        const root = /** @type {?Element} */ (this.ampdoc.isSingleDoc() &&
-        iframed
-          ? /** @type {*} */ (this.win.document)
-          : null);
         this.intersectionObserver_ = new IntersectionObserver(
           (e) => this.intersect(e),
           // rootMargin matches size of loadRect: (150vw 300vh) * 1.25.
           {root, rootMargin: '250% 31.25%'}
         );
+
+        // Wait for intersection callback instead of measuring all elements
+        // during the first pass.
+        this.relayoutAll_ = false;
       } catch (e) {
         dev().warn(TAG_, 'Falling back to classic Resources:', e);
       }
-
-      // Wait for intersection callback instead of measuring all elements
-      // during the first pass.
-      this.relayoutAll_ = false;
     }
 
     // When user scrolling stops, run pass to check newly in-viewport elements.
