7ec5d79c19e2c3a77a0a9c62c1e3eb1d563dc61d
krdwan <kdwan@google.com>
2020-12-11 15:42:10 -0500

    Update tests

--- extensions/amp-sidebar/0.1/test/test-amp-sidebar.js
+++ extensions/amp-sidebar/0.1/test/test-amp-sidebar.js
@@ -694,6 +694,30 @@ describes.realWin(
           expect(impl.disableSwipeClose_).to.be.true;
         });
       });
+
+      it('should not allow swipe on input range (slider) element', () => {
+        return getAmpSidebar().then((sidebarElement) => {
+          const impl = sidebarElement.implementation_;
+
+          const swipeMoveStub = env.sandbox.stub(
+            impl.swipeToDismiss_,
+            'swipeMove'
+          );
+
+          // Do not trigger swipeMove when swiping on an input range element
+          const slider = document.createElement('input');
+          slider.setAttribute('type', 'range');
+          const fakeEvent = {target: slider};
+          impl.handleSwipe_({}, fakeEvent);
+          expect(swipeMoveStub).to.not.be.called;
+
+          // Call swipeMove when swiping on any other type of element
+          const input = document.createElement('input');
+          const fakeEvent2 = {target: input};
+          impl.handleSwipe_({}, fakeEvent2);
+          expect(swipeMoveStub).to.be.called.calledOnce;
+        });
+      });
     });
 
     describe('amp-sidebar - toolbars in amp-sidebar', () => {
