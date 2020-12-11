95367bf8bcbcc2a0bdca770bbf8c02749a75f235
krdwan <kdwan@google.com>
2020-12-11 15:17:58 -0500

    Add basic unit test for disable swipe close

--- extensions/amp-sidebar/0.1/test/test-amp-sidebar.js
+++ extensions/amp-sidebar/0.1/test/test-amp-sidebar.js
@@ -82,6 +82,9 @@ describes.realWin(
       if (options.side) {
         ampSidebar.setAttribute('side', options.side);
       }
+      if (options.disableSwipe) {
+        ampSidebar.setAttribute('data-disable-swipe-close', '');
+      }
       if (options.closeText) {
         ampSidebar.setAttribute(
           'data-close-button-aria-label',
@@ -682,6 +685,13 @@ describes.realWin(
             env.sandbox.match.any,
             /* trust */ 456
           );
+        });
+      });
+
+      it('should disable swipe to close when attribute specified', () => {
+        return getAmpSidebar({disableSwipe: true}).then((sidebarElement) => {
+          const impl = sidebarElement.implementation_;
+          expect(impl.disableSwipeClose_).to.be.true;
         });
       });
     });
