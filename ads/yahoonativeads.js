0da830b666d6314af286a00283bc3afedf261a51
Kieran Boyle <kboyle@verizonmedia.com>
2020-12-14 14:36:00 -0800

    🐛 🔥 [yahoonativeads] update configs and documentation (#31573)
    * update native ad tag configs and documentation
    
    * fix formatting errors

--- ads/yahoonativeads.js
+++ ads/yahoonativeads.js
@@ -21,13 +21,22 @@ import {loadScript, validateData} from '../3p/3p';
  * @param {!Object} data
  */
 export function yahoonativeads(global, data) {
-  validateData(data, ['key', 'code', 'url']);
+  validateData(data, ['code', 'url']);
 
-  (global.native = global.native || []).push(data.code);
-
-  global.apiKey = data.key;
   global.publisherUrl = data.url;
   global.amp = true;
+
+  const config = {};
+
+  if (data.key) {
+    config.apiKey = data.key;
+  }
+
+  Object.keys(data).forEach((property) => {
+    config[property] = data[property];
+  });
+
+  (global.native = global.native || []).push(config);
 
   loadScript(global, 'https://s.yimg.com/dy/ads/native.js', () =>
     global.context.renderStart()
