3e0f3cbdbee71ac9bddd5d8c26bf928c44126fe4
Enrique Marroquin <artezan@google.com>
2020-12-17 13:42:27 -0500

    add tests

---
+++ extensions/amp-story/1.0/test/test-amp-story-localization-strings.js
@@ -0,0 +1,186 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {LocalizedStringId} from '../../../../src/localized-strings';
+import LocalizedStringaf from '../_locales/af.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringam from '../_locales/am.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringar from '../_locales/ar.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringbg from '../_locales/bg.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringbn from '../_locales/bn.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringbs from '../_locales/bs.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringca from '../_locales/ca.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringcs from '../_locales/cs.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringda from '../_locales/da.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringde from '../_locales/de.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringdefault from '../_locales/default.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringel from '../_locales/el.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringen from '../_locales/en.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringenGB from '../_locales/en-GB.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringes from '../_locales/es.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringes419 from '../_locales/es-419.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringet from '../_locales/et.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringeu from '../_locales/eu.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringfa from '../_locales/fa.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringfi from '../_locales/fi.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringfil from '../_locales/fil.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringfr from '../_locales/fr.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringgl from '../_locales/gl.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringgu from '../_locales/gu.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringhi from '../_locales/hi.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringhr from '../_locales/hr.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringhu from '../_locales/hu.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringid from '../_locales/id.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringis from '../_locales/is.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringit from '../_locales/it.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringiw from '../_locales/iw.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringja from '../_locales/ja.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringka from '../_locales/ka.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringkm from '../_locales/km.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringkn from '../_locales/kn.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringko from '../_locales/ko.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringlo from '../_locales/lo.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringlt from '../_locales/lt.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringlv from '../_locales/lv.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringmk from '../_locales/mk.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringml from '../_locales/ml.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringmn from '../_locales/mn.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringmr from '../_locales/mr.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringms from '../_locales/ms.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringmy from '../_locales/my.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringne from '../_locales/ne.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringnl from '../_locales/nl.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringno from '../_locales/no.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringpa from '../_locales/pa.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringpl from '../_locales/pl.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringptBR from '../_locales/pt-BR.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringptPT from '../_locales/pt-PT.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringro from '../_locales/ro.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringru from '../_locales/ru.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsi from '../_locales/si.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsk from '../_locales/sk.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsl from '../_locales/sl.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsq from '../_locales/sq.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsr from '../_locales/sr.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsv from '../_locales/sv.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringsw from '../_locales/sw.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringta from '../_locales/ta.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringte from '../_locales/te.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringth from '../_locales/th.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringtr from '../_locales/tr.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringuk from '../_locales/uk.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringur from '../_locales/ur.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringvi from '../_locales/vi.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringzhCN from '../_locales/zh-CN.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringzhTW from '../_locales/zh-TW.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+import LocalizedStringzu from '../_locales/zu.json' assert {type: 'json'}; // lgtm[js/syntax-error]
+
+describes.fakeWin('amp-story-localization-strings', {amp: true}, () => {
+  describe('localized strings', () => {
+    const languageObjs = [
+      {'LocalizedStringaf': LocalizedStringaf},
+      {'LocalizedStringam': LocalizedStringam},
+      {'LocalizedStringar': LocalizedStringar},
+      {'LocalizedStringbg': LocalizedStringbg},
+      {'LocalizedStringbn': LocalizedStringbn},
+      {'LocalizedStringbs': LocalizedStringbs},
+      {'LocalizedStringca': LocalizedStringca},
+      {'LocalizedStringcs': LocalizedStringcs},
+      {'LocalizedStringda': LocalizedStringda},
+      {'LocalizedStringde': LocalizedStringde},
+      {'LocalizedStringdefault': LocalizedStringdefault},
+      {'LocalizedStringel': LocalizedStringel},
+      {'LocalizedStringen': LocalizedStringen},
+      {'LocalizedStringenGB': LocalizedStringenGB},
+      {'LocalizedStringes': LocalizedStringes},
+      {'LocalizedStringes419': LocalizedStringes419},
+      {'LocalizedStringet': LocalizedStringet},
+      {'LocalizedStringeu': LocalizedStringeu},
+      {'LocalizedStringfa': LocalizedStringfa},
+      {'LocalizedStringfi': LocalizedStringfi},
+      {'LocalizedStringfil': LocalizedStringfil},
+      {'LocalizedStringfr': LocalizedStringfr},
+      {'LocalizedStringgl': LocalizedStringgl},
+      {'LocalizedStringgu': LocalizedStringgu},
+      {'LocalizedStringhi': LocalizedStringhi},
+      {'LocalizedStringhr': LocalizedStringhr},
+      {'LocalizedStringhu': LocalizedStringhu},
+      {'LocalizedStringid': LocalizedStringid},
+      {'LocalizedStringis': LocalizedStringis},
+      {'LocalizedStringit': LocalizedStringit},
+      {'LocalizedStringiw': LocalizedStringiw},
+      {'LocalizedStringja': LocalizedStringja},
+      {'LocalizedStringka': LocalizedStringka},
+      {'LocalizedStringkm': LocalizedStringkm},
+      {'LocalizedStringkn': LocalizedStringkn},
+      {'LocalizedStringko': LocalizedStringko},
+      {'LocalizedStringlo': LocalizedStringlo},
+      {'LocalizedStringlt': LocalizedStringlt},
+      {'LocalizedStringlv': LocalizedStringlv},
+      {'LocalizedStringmk': LocalizedStringmk},
+      {'LocalizedStringml': LocalizedStringml},
+      {'LocalizedStringmn': LocalizedStringmn},
+      {'LocalizedStringmr': LocalizedStringmr},
+      {'LocalizedStringms': LocalizedStringms},
+      {'LocalizedStringmy': LocalizedStringmy},
+      {'LocalizedStringne': LocalizedStringne},
+      {'LocalizedStringnl': LocalizedStringnl},
+      {'LocalizedStringno': LocalizedStringno},
+      {'LocalizedStringpa': LocalizedStringpa},
+      {'LocalizedStringpl': LocalizedStringpl},
+      {'LocalizedStringptBR': LocalizedStringptBR},
+      {'LocalizedStringptPT': LocalizedStringptPT},
+      {'LocalizedStringro': LocalizedStringro},
+      {'LocalizedStringru': LocalizedStringru},
+      {'LocalizedStringsi': LocalizedStringsi},
+      {'LocalizedStringsk': LocalizedStringsk},
+      {'LocalizedStringsl': LocalizedStringsl},
+      {'LocalizedStringsq': LocalizedStringsq},
+      {'LocalizedStringsr': LocalizedStringsr},
+      {'LocalizedStringsv': LocalizedStringsv},
+      {'LocalizedStringsw': LocalizedStringsw},
+      {'LocalizedStringta': LocalizedStringta},
+      {'LocalizedStringte': LocalizedStringte},
+      {'LocalizedStringth': LocalizedStringth},
+      {'LocalizedStringtr': LocalizedStringtr},
+      {'LocalizedStringuk': LocalizedStringuk},
+      {'LocalizedStringur': LocalizedStringur},
+      {'LocalizedStringvi': LocalizedStringvi},
+      {'LocalizedStringzhCN': LocalizedStringzhCN},
+      {'LocalizedStringzhTW': LocalizedStringzhTW},
+      {'LocalizedStringzu': LocalizedStringzu},
+    ];
+
+    languageObjs.forEach((obj) => {
+      const languageName = Object.keys(obj)[0];
+      const languageStrings = obj[languageName];
+      const languageKeys = Object.keys(languageStrings);
+
+      it(languageName + ' should have unique keys', () => {
+        const uniqueKeys = new Set(languageKeys);
+
+        expect(uniqueKeys.size).to.eql(languageKeys.length);
+      });
+
+      it(languageName + ' keys should exist in LocalizedStringId', () => {
+        const localizedStringIdKeys = Object.values(LocalizedStringId);
+
+        languageKeys.forEach((key) => {
+          expect(localizedStringIdKeys.indexOf(key)).to.not.eql(-1);
+        });
+      });
+    });
+  });
+});
