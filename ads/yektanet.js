feb276b230fb6fa1d9ccd1c002341809b94ee49f
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-12-21 21:35:09 +0330

    Add banners to yektanet script (#31681)

--- ads/yektanet.js
+++ ads/yektanet.js
@@ -21,25 +21,31 @@ import {loadScript, validateData} from '../3p/3p';
  * @param {!Object} data
  */
 export function yektanet(global, data) {
-  validateData(data, ['publisherName', 'scriptName', 'posId']);
+  validateData(data, ['publisherName', 'scriptName', 'posId'], ['adType']);
 
-  const container = document.getElementById('c'),
-    adDiv = document.createElement('div');
+  const isBanner = data['adType'] === 'banner';
+
+  const container = document.getElementById('c');
+  const adDiv = document.createElement('div');
   adDiv.setAttribute('id', data['posId']);
+  if (isBanner) {
+    adDiv.setAttribute('class', 'yn-bnr');
+  }
   container.appendChild(adDiv);
 
-  const now = new Date(),
-    version = [
-      now.getFullYear(),
-      now.getMonth(),
-      now.getDate(),
-      now.getHours(),
-    ].join('0');
+  const now = new Date();
+  const version = [
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    now.getHours(),
+  ].join('0');
 
-  loadScript(
-    global,
-    `https://cdn.yektanet.com/js/${encodeURIComponent(
-      data['publisherName']
-    )}/${encodeURIComponent(data['scriptName'])}?v=${version}`
-  );
+  const scriptSrc = isBanner
+    ? 'https://cdn.yektanet.com/template/bnrs/yn_bnr.min.js'
+    : `https://cdn.yektanet.com/js/${encodeURIComponent(
+        data['publisherName']
+      )}/${encodeURIComponent(data['scriptName'])}`;
+
+  loadScript(global, `${scriptSrc}?v=${version}`);
 }
