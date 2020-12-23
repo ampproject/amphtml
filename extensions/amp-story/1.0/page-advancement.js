2de3e1b0664c5de0ad3e990ffec2d19bd876ae78
Raksha Muthukumar <raksha.muth@gmail.com>
2020-12-23 12:13:53 -0500

    too-large links should also not be overriden for the same reason

--- extensions/amp-story/1.0/page-advancement.js
+++ extensions/amp-story/1.0/page-advancement.js
@@ -520,7 +520,7 @@ export class ManualAdvancement extends AdvancementConfig {
 
     if (
       this.isInStoryPageSideEdge_(event, pageRect) ||
-      this.isTooLargeOnPage_(target, pageRect)
+      this.isTooLargeOnPage_(event, target, pageRect)
     ) {
       event.preventDefault();
       return false;
@@ -596,12 +596,19 @@ export class ManualAdvancement extends AdvancementConfig {
   /**
    * Checks if click target is too large on the page and preventing navigation.
    * If yes, the link is ignored & logged.
+   * @param {!Event} event
    * @param {!Element} target
    * @param {!ClientRect} pageRect
    * @return {boolean}
    * @private
    */
-  isTooLargeOnPage_(target, pageRect) {
+  isTooLargeOnPage_(event, target, pageRect) {
+    // Clicks with coordinates (0,0) are assumed to be from keyboard or Talkback.
+    // These clicks should never be overriden for navigation.
+    if (event.clientX === 0 && event.clientY === 0) {
+      return false;
+    }
+
     const targetRect = target./*OK*/ getBoundingClientRect();
     if (
       (targetRect.height * targetRect.width) /
