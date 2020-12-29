4778cc433533a733a98bf25db8643cdbe9793cfc
tanish-fork-media-group <tanish@forkmedia.in>
2020-12-29 17:14:37 +0530

    forkmedia amp changes

--- ads/forkmedia.js
+++ ads/forkmedia.js
@@ -14,30 +14,49 @@
  * limitations under the License.
  */
 
-import {loadScript} from '../3p/3p';
+import { loadScript } from '../3p/3p';
 
 /**
  * @param {!Window} global
  * @param {!Object} data
  */
 export function forkmedia(global, data) {
-  let src = null;
-  if (data.product === 'inread') {
-    src = 'https://delivery.forkcdn.com/rappio/inread/v1.1/amp/inread.js';
-  } else if (data.product === 'vibe') {
-    src = 'https://vibecdn.forkcdn.com/Inarticle/amp/iav.js';
-  } else {
-    src = 'https://delivery.forkcdn.com/amp/default.js';
-  }
-
-  loadScript(
-    global,
-    src,
-    () => {
-      global.context.renderStart();
-    },
-    () => {
-      global.context.noContentAvailable();
+    let src = null;
+    if (data.product === 'contextads') {
+        switch (data.format) {
+            case 'inread':
+                src = 'https://amp.contextads.live/inread/inread.js';
+                break;
+            case 'vibe':
+                src = 'https://amp.contextads.live/vibe/iav_ia.js';
+                break;
+            case 'display':
+                src = 'https://amp.contextads.live/display/display.js';
+                break;
+            case "impulse":
+                src = 'https://amp.contextads.live/impulse/impulse.js';
+                break;
+            case 'interscroller':
+                src = 'https://amp.contextads.live/interscroller/fis.js';
+                break;
+            case 'spark':
+                src = 'https://amp.contextads.live/spark/spark.js';
+                break;
+            default:
+                src = 'https://amp.contextads.live/default.js'
+        }
+    } else {
+        src = 'https://amp.contextads.live/default.js'
     }
-  );
+
+    loadScript(
+        global,
+        src,
+        () => {
+            global.context.renderStart();
+        },
+        () => {
+            global.context.noContentAvailable();
+        }
+    );
 }
