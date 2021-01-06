f6c1f161159938173401e1b0dd2a78ce82e7ce38
keithwrightbos <keithwrightbos@users.noreply.github.com>
2021-01-06 16:03:59 -0500

    Launch ptt for AdSense & GAM (#31827)

--- extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
+++ extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js
@@ -72,15 +72,6 @@ const ADSENSE_BASE_URL = 'https://googleads.g.doubleclick.net/pagead/ads';
 /** @const {string} */
 const TAG = 'amp-ad-network-adsense-impl';
 
-/** @const {string} */
-const PTT_EXP = 'adsense-ptt-exp';
-
-/** @const @enum{string} */
-const PTT_EXP_BRANCHES = {
-  CONTROL: '21068091',
-  EXPERIMENT: '21068092',
-};
-
 /**
  * Shared state for AdSense ad slots. This is used primarily for ad request url
  * parameters that depend on previous slots.
@@ -235,13 +226,7 @@ export class AmpAdNetworkAdsenseImpl extends AmpA4A {
    * @visibleForTesting
    */
   divertExperiments() {
-    const experimentInfoList = /** @type {!Array<!../../../src/experiments.ExperimentInfo>} */ ([
-      {
-        experimentId: PTT_EXP,
-        isTrafficEligible: () => true,
-        branches: Object.values(PTT_EXP_BRANCHES),
-      },
-    ]);
+    const experimentInfoList = /** @type {!Array<!../../../src/experiments.ExperimentInfo>} */ ([]);
     const setExps = randomlySelectUnsetExperiments(
       this.win,
       experimentInfoList
@@ -358,10 +343,7 @@ export class AmpAdNetworkAdsenseImpl extends AmpA4A {
       'format': format,
       'w': sizeToSend.width,
       'h': sizeToSend.height,
-      'ptt':
-        getExperimentBranch(this.win, PTT_EXP) === PTT_EXP_BRANCHES.EXPERIMENT
-          ? 12
-          : null,
+      'ptt': 12,
       'iu': slotname,
       'npa':
         consentState == CONSENT_POLICY_STATE.INSUFFICIENT ||
