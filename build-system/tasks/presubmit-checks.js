3db42c7117fcbae897d88b50a8d6fb2c4190dc07
Alan Orozco <alanorozco@users.noreply.github.com>
2021-01-06 06:41:06 -0800

    🏗  Allow "Copyright 2021" (#31811)
    I had a PR Travis [job](https://travis-ci.com/github/ampproject/amphtml/jobs/469003388) fail because it contained a new file annotated as 2021. 🥂

--- build-system/tasks/presubmit-checks.js
+++ build-system/tasks/presubmit-checks.js
@@ -1215,7 +1215,7 @@ const forbiddenTermsSrcInclusive = {
 
 // Terms that must appear in a source file.
 const requiredTerms = {
-  'Copyright 20(15|16|17|18|19|20) The AMP HTML Authors\\.': dedicatedCopyrightNoteSources,
+  'Copyright 20(15|16|17|18|19|2\\d) The AMP HTML Authors\\.': dedicatedCopyrightNoteSources,
   'Licensed under the Apache License, Version 2\\.0': dedicatedCopyrightNoteSources,
   'http\\://www\\.apache\\.org/licenses/LICENSE-2\\.0': dedicatedCopyrightNoteSources,
 };
