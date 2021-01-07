38e28e23a4e1dc0003cd89802d3035ec6d67dc8b
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2021-01-07 09:51:19 -0500

    🐛 Bento Carousel: Correctly disable next arrow for mixed-length carousels (#31765)
    * Re-enable smooth scrolling in `advance`
    
    * Disable arrows by measuring for mixedLength
    
    * Use ResizeObserver to update scroll position
    
    * Pass axis
    
    * Guard for existence of children
    
    * Check disabled state at render phase
    
    * Copy mixed-length sample
    
    * Replace imperative api property with getters
    
    * Increase unit test timeouts for Travis
    
    * Add e2e test for mixed-length
    
    * Add e2e test for non-snapping mixed-length
    
    * Set viewport based widths for e2e test
    
    * Revert "Set viewport based widths for e2e test"
    
    This reverts commit 5b653c857ef856e39d4fbbceba4ecc4cb2c6e83c.
    
    * Measure offset of target element instead of scroll position of container for disable state
    
    * s/disabled/isDisabled
    
    * Allow use of offset properties
    
    * Try verifying the slide measurements
    
    * Update imperative API test with slide measurements
    
    * Remove poll

--- extensions/amp-stream-gallery/1.0/test/test-amp-stream-gallery.js
+++ extensions/amp-stream-gallery/1.0/test/test-amp-stream-gallery.js
@@ -21,7 +21,6 @@ import {
   createElementWithAttributes,
   waitForChildPromise,
 } from '../../../../src/dom';
-import {poll} from '../../../../testing/iframe';
 import {setStyles} from '../../../../src/style';
 import {toArray} from '../../../../src/types';
 import {toggleExperiment} from '../../../../src/experiments';
@@ -169,11 +168,12 @@ describes.realWin(
 
     describe('imperative api', () => {
       let scroller;
+      let slides;
 
       beforeEach(async () => {
         element.setAttribute('max-visible-count', '1');
         win.document.body.appendChild(element);
-        await getSlidesFromShadow();
+        slides = await getSlidesFromShadow();
 
         scroller = element.shadowRoot.querySelector(
           `[class*=${styles.scrollContainer}]`
@@ -203,21 +203,15 @@ describes.realWin(
 
       it('should execute next and prev actions', async () => {
         element.enqueAction(invocation('next'));
-        await waitFor(() => scroller.scrollLeft > 0, 'advanced to next slide');
-
-        // Make sure internal state index is updated before attempting to call prev(),
-        // Since this is typically updated automatically on debounce, there is a risk that
-        // the test will call prev() on the slide at the 0th index unless we force is here.
-        element.enqueAction(invocation('goToSlide', {index: 1}));
-        await waitFor(() => scroller.scrollLeft > 0, 'to slide 1');
+        await waitFor(
+          () => scroller.scrollLeft === slides[1].offsetLeft,
+          'advanced to next slide'
+        );
 
         element.enqueAction(invocation('prev'));
-        // Wait for a longer timeout than the 200 default in waitFor.
-        await poll(
-          'returned to prev slide',
-          () => scroller.scrollLeft == 0,
-          undefined /* opt_onError */,
-          400 /* opt_timeout */
+        await waitFor(
+          () => scroller.scrollLeft === slides[0].offsetLeft,
+          'returned to prev slide'
         );
       });
 
