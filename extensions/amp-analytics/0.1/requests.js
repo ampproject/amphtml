10024b9647a13dec07293fe8306c509345f15fcc
Alan Orozco <alanorozco@users.noreply.github.com>
2021-01-13 12:58:55 -0800

    🐛 Malformed resolved analytics URL will trigger user error (#31896)
    After #31594, URLs are parsed using native `URL()`.
    
    Previously, invalid URLs parsed with `<a>` would have been parsed without error, but incorrectly, for example:
    
    ```js
    parseUrlWithA('https://?foo'); // 'https:/?foo'
    new URL('https://?foo'); // throws error
    ```
     
    In this case, #31883 is triggered by `expandUrlAsync` failing with a certain vendor config:
    
    https://github.com/ampproject/amphtml/blob/b445001b99f6ccddc86a6f6e3812a774d4c9ec5a/extensions/amp-analytics/0.1/vendors/mpulse.json#L2-L4
    
    When `beacon_url` fails to resolve, we get the above case:
    
    ```js
    new URL('https://?h.d=...'); // throws error
    ```
    
    This can be triggered by any vendor request whose required interpolation values are missing or create an invalid URL, so it's appropriate to redirect them as user errors.

--- extensions/amp-analytics/0.1/requests.js
+++ extensions/amp-analytics/0.1/requests.js
@@ -72,9 +72,6 @@ export class RequestHandler {
     this.baseUrlPromise_ = null;
 
     /** @private {?Promise<string>} */
-    this.baseUrlTemplatePromise_ = null;
-
-    /** @private {?Promise<string>} */
     this.requestOriginPromise_ = null;
 
     /** @private {!Array<!Promise<!BatchSegmentDef>>} */
@@ -116,9 +113,9 @@ export class RequestHandler {
    * Real ping may be batched and send out later.
    * @param {?JsonObject} configParams
    * @param {!JsonObject} trigger
-   * @param {!./variables.ExpansionOptions} expansionOption
+   * @param {!./variables.ExpansionOptions} expansionOptions
    */
-  send(configParams, trigger, expansionOption) {
+  send(configParams, trigger, expansionOptions) {
     const isImportant = trigger['important'] === true;
     if (!this.reportRequest_ && !isImportant) {
       // Ignore non important trigger out reportWindow
@@ -135,52 +132,29 @@ export class RequestHandler {
     );
 
     if (!this.baseUrlPromise_) {
-      expansionOption.freezeVar('extraUrlParams');
+      expansionOptions.freezeVar('extraUrlParams');
 
-      this.baseUrlTemplatePromise_ = this.variableService_.expandTemplate(
+      this.baseUrlPromise_ = this.expandTemplateUrl_(
         this.baseUrl,
-        expansionOption,
-        this.element_,
-        bindings,
-        this.allowlist_
+        expansionOptions,
+        bindings
       );
-
-      this.baseUrlPromise_ = this.baseUrlTemplatePromise_.then((baseUrl) => {
-        return this.urlReplacementService_.expandUrlAsync(
-          baseUrl,
-          bindings,
-          this.allowlist_
-        );
-      });
     }
 
     // expand requestOrigin if it is declared
     if (!this.requestOriginPromise_ && this.requestOrigin_) {
       // do not encode vars in request origin
-      const requestOriginExpansionOpt = new ExpansionOptions(
-        expansionOption.vars,
-        expansionOption.iterations,
-        true // opt_noEncode
+      const requestOriginExpansionOptions = new ExpansionOptions(
+        expansionOptions.vars,
+        expansionOptions.iterations,
+        /* opt_noEncode */ true
       );
 
-      this.requestOriginPromise_ = this.variableService_
-        // expand variables in request origin
-        .expandTemplate(
-          this.requestOrigin_,
-          requestOriginExpansionOpt,
-          this.element_,
-          bindings,
-          this.allowlist_
-        )
-        // substitute in URL values e.g. DOCUMENT_REFERRER -> https://example.com
-        .then((expandedRequestOrigin) => {
-          return this.urlReplacementService_.expandUrlAsync(
-            expandedRequestOrigin,
-            bindings,
-            this.allowlist_,
-            true // opt_noEncode
-          );
-        });
+      this.requestOriginPromise_ = this.expandTemplateUrl_(
+        this.requestOrigin_,
+        requestOriginExpansionOptions,
+        bindings
+      );
     }
 
     const params = {...configParams, ...trigger['extraUrlParams']};
@@ -189,7 +163,7 @@ export class RequestHandler {
       this.variableService_,
       this.urlReplacementService_,
       params,
-      expansionOption,
+      expansionOptions,
       bindings,
       this.element_,
       this.allowlist_
@@ -223,6 +197,30 @@ export class RequestHandler {
   }
 
   /**
+   * @param {string} url
+   * @param {!ExpansionOptions} expansionOptions
+   * @param {!Object<string, (!../../../src/service/variable-source.ResolverReturnDef|!../../../src/service/variable-source.SyncResolverDef)>=} bindings
+   * @return {!Promise<string>}
+   */
+  expandTemplateUrl_(url, expansionOptions, bindings) {
+    return this.variableService_
+      .expandTemplate(
+        url,
+        expansionOptions,
+        this.element_,
+        bindings,
+        this.allowlist_
+      )
+      .then((url) =>
+        this.urlReplacementService_
+          .expandUrlAsync(url, bindings, this.allowlist_)
+          .catch((e) =>
+            userAssert(false, `Could not expand URL "${url}": ${e.message}`)
+          )
+      );
+  }
+
+  /**
    * Function that schedule the actual request send.
    * @param {boolean} isImmediate
    * @private
@@ -246,17 +244,16 @@ export class RequestHandler {
   fire_() {
     const {
       requestOriginPromise_: requestOriginPromise,
-      baseUrlTemplatePromise_: baseUrlTemplatePromise,
       baseUrlPromise_: baseUrlPromise,
       batchSegmentPromises_: segmentPromises,
     } = this;
     const trigger = /** @type {!JsonObject} */ (this.lastTrigger_);
     this.reset_();
 
-    // preconnect to requestOrigin if available, otherwise baseUrlTemplate
+    // preconnect to requestOrigin if available, otherwise baseUrl
     const preconnectPromise = requestOriginPromise
       ? requestOriginPromise
-      : baseUrlTemplatePromise;
+      : baseUrlPromise;
 
     preconnectPromise.then((preUrl) => {
       this.preconnect_.url(this.ampdoc_, preUrl, true);
@@ -298,7 +295,6 @@ export class RequestHandler {
   reset_() {
     this.queueSize_ = 0;
     this.baseUrlPromise_ = null;
-    this.baseUrlTemplatePromise_ = null;
     this.batchSegmentPromises_ = [];
     this.lastTrigger_ = null;
   }
