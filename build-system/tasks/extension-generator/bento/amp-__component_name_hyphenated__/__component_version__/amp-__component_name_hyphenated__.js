336319f109940151dfc3c55059f220814970d0c1
Ryan Cebulko <ryan@cebulko.com>
2021-01-14 13:39:19 -0500

    🏗 Create `gulp init-bento-component` task to generate new Bento component boilerplate (#31947)
    * Initial bento component boilerplate and eslintignore
    
    * Skeleton code for task
    
    * Fix bugs and typos in templates and add storybook
    
    * Logic for generating new component from templates
    
    * Define gulp task for init-bento-component
    
    * Colorize commands in output
    
    * Fix lint/typos
    
    * Exclude from Prettier (prettierignore fails)
    
    * Reorder imports per prettier suggestion
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: line break
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: Sort imports
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: Add missing semicolon
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: Missing semicolon
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: Missing semicolon
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: whitespace
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Prettier: whitespace
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * Move into extension-generator directory/task
    
    * Add output for updating bundles config
    
    * Update eslintignore/prettier config paths
    
    * Exclude templates from "required terms" presubmit
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>

---
+++ build-system/tasks/extension-generator/bento/amp-__component_name_hyphenated__/__component_version__/amp-__component_name_hyphenated__.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright __current_year__ The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {__component_name_pascalcase__} from './__component_name_hyphenated__';
+import {PreactBaseElement} from '../../../src/preact/base-element';
+import {dict} from '../../../src/utils/object';
+import {isExperimentOn} from '../../../src/experiments';
+import {userAssert} from '../../../src/log';
+
+/** @const {string} */
+const TAG = 'amp-__component_name_hyphenated__';
+
+class Amp__component_name_pascalcase__ extends PreactBaseElement {
+  /** @override */
+  init() {
+    this.registerApiAction('exampleToggle', (api) => api./*OK*/exampleToggle());
+
+    return dict({
+      // Extra props passed by wrapper AMP component
+      'exampleTagNameProp': this.element.tagName,
+    });
+  }
+
+  /** @override */
+  isLayoutSupported(layout) {
+    userAssert(
+      isExperimentOn(this.win, 'bento') ||
+        isExperimentOn(this.win, 'bento-__component_name_hyphenated__'),
+      'expected global "bento" or specific "bento-__component_name_hyphenated__" experiment to be enabled'
+    );
+    return super.isLayoutSupported(layout);
+  }
+}
+
+/** @override */
+Amp__component_name_pascalcase__['Component'] = __component_name_pascalcase__;
+
+/** @override */
+Amp__component_name_pascalcase__['props'] = {};
+
+/** @override */
+Amp__component_name_pascalcase__['passthrough'] = true;
+// Amp__component_name_pascalcase__['passthroughNonEmpty'] = true;
+// Amp__component_name_pascalcase__['children'] = {};
+
+/** @override */
+Amp__component_name_pascalcase__['layoutSizeDefined'] = true;
+
+AMP.extension(TAG, '__component_version__', (AMP) => {
+  AMP.registerElement(TAG, Amp__component_name_pascalcase__);
+});
