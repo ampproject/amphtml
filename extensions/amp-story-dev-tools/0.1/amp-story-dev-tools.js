1f6993f102651889bbca80db27ba977799c99a29
Matias Szylkowski <mszylkowski@google.com>
2021-01-15 11:23:57 -0500

    ✨ [Story devtools] Add success page for logs (#31906)
    * Push image
    
    * Split success and errors into separate methods
    
    * Bring back errorList
    
    * Changed methods names to template
    
    * Remove custom styles and simplify building
    
    * Forgor return
    
    * Moved image to binary
    
    * Remove comment
    
    * Brought back errorList
    
    * Moved message to template and simplified CSS
    
    * Bring back errorList
    
    * Updated image and font
    
    * Reverted errorList

--- extensions/amp-story-dev-tools/0.1/amp-story-dev-tools.js
+++ extensions/amp-story-dev-tools/0.1/amp-story-dev-tools.js
@@ -32,7 +32,7 @@ import {parseQueryString} from '../../../src/url';
 import {toggle} from '../../../src/style';
 import {updateHash} from './utils';
 
-/** @const {Array<Object>} fontFaces with urls from https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&amp;display=swap */
+/** @const {Array<Object>} fontFaces with urls from https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&amp;display=swap */
 const fontsToLoad = [
   {
     family: 'Poppins',
@@ -42,9 +42,9 @@ const fontsToLoad = [
   },
   {
     family: 'Poppins',
-    weight: '700',
+    weight: '600',
     src:
-      "url(https://fonts.gstatic.com/s/poppins/v9/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2) format('woff2')",
+      "url(https://fonts.gstatic.com/s/poppins/v15/pxiByp8kv8JHgFVrLEj6Z1xlFd2JQEk.woff2) format('woff2')",
   },
 ];
 
@@ -141,6 +141,7 @@ export class AmpStoryDevTools extends AMP.BaseElement {
   /** @override */
   buildCallback() {
     this.loadFonts_();
+    this.removeCustomCSS_();
     this.buildLayout_();
     this.initializeListeners_();
 
@@ -259,6 +260,13 @@ export class AmpStoryDevTools extends AMP.BaseElement {
         });
       });
     }
+  }
+
+  /** @private */
+  removeCustomCSS_() {
+    this.element.ownerDocument
+      .querySelectorAll('style[amp-custom]')
+      .forEach((e) => e.remove());
   }
 }
 
