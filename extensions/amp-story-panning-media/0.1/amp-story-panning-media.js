41cc038d4d8cd8cbda313890e0ca4481cc95da40
Philip Bell <philip.hunter.bell@gmail.com>
2021-01-21 15:00:59 -0500

    Transition elements on navigation (#31559)

--- extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
+import {
+  Action,
+  StateProperty,
+} from '../../../extensions/amp-story/1.0/amp-story-store-service';
 import {CSS} from '../../../build/amp-story-panning-media-0.1.css';
 import {CommonSignals} from '../../../src/common-signals';
 import {Layout} from '../../../src/layout';
+import {Services} from '../../../src/services';
+import {closest, whenUpgradedToCustomElement} from '../../../src/dom';
 import {dev, user} from '../../../src/log';
-import {setStyles} from '../../../src/style';
-import {whenUpgradedToCustomElement} from '../../../src/dom';
+import {setImportantStyles} from '../../../src/style';
 
 /** @const {string} */
 const TAG = 'AMP_STORY_PANNING_MEDIA';
@@ -32,17 +37,23 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
     /** @private {?Element} */
     this.element_ = element;
 
-    /** @private {?Element} */
-    this.image_ = null;
+    /** @private {?Element} The element that is transitioned. */
+    this.ampImgEl_ = null;
 
-    /** @private {?string} */
+    /** @private {?string} Sent to siblings to update their position. */
     this.x_ = null;
 
-    /** @private {?string} */
+    /** @private {?string} Sent to siblings to update their position. */
     this.y_ = null;
 
-    /** @private {?string} */
+    /** @private {?string} Sent to siblings to update their position. */
     this.zoom_ = null;
+
+    /** @private {?../../../extensions/amp-story/1.0/amp-story-store-service.AmpStoryStoreService} */
+    this.storeService_ = null;
+
+    /** @private {boolean} */
+    this.isOnActivePage_ = false;
   }
 
   /** @override */
@@ -50,37 +61,103 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
     this.x_ = this.element_.getAttribute('x') || '0%';
     this.y_ = this.element_.getAttribute('y') || '0%';
     this.zoom_ = this.element_.getAttribute('zoom') || '1';
+
+    return Services.storyStoreServiceForOrNull(this.win).then(
+      (storeService) => {
+        this.storeService_ = storeService;
+      }
+    );
   }
 
   /** @override */
   layoutCallback() {
-    const ampImgEl = dev().assertElement(
+    this.ampImgEl_ = dev().assertElement(
       this.element_.querySelector('amp-img')
     );
-    return whenUpgradedToCustomElement(ampImgEl)
-      .then(() => ampImgEl.signals().whenSignal(CommonSignals.LOAD_END))
+    this.initializeListeners_();
+    return whenUpgradedToCustomElement(this.ampImgEl_)
+      .then(() => this.ampImgEl_.signals().whenSignal(CommonSignals.LOAD_END))
       .then(() => {
-        this.image_ = dev().assertElement(this.element.querySelector('img'));
+        const imgEl = dev().assertElement(this.element_.querySelector('img'));
         // Remove layout="fill" classes so image is not clipped.
-        this.image_.classList = '';
-        // Fill image to 100% height of viewport.
+        imgEl.classList = '';
+        // Centers the amp-img horizontally. The image does not load if this is done in CSS.
         // TODO(#31515): Handle base zoom of aspect ratio wider than image
-        setStyles(this.image_, {height: '100%'});
-        return this.updatePosition_();
+        setImportantStyles(this.ampImgEl_, {
+          left: 'auto',
+          right: 'auto',
+        });
       })
       .catch(() => user().error(TAG, 'Failed to load the amp-img.'));
   }
 
-  /**
-   * @return {!Promise}
-   * @private
-   */
-  updatePosition_() {
-    return this.mutateElement(() =>
-      setStyles(this.image_, {
-        transform: `scale(${this.zoom_}) translate(${this.x_}, ${this.y_})`,
-      })
+  /** @private */
+  initializeListeners_() {
+    this.storeService_.subscribe(
+      StateProperty.CURRENT_PAGE_ID,
+      (currPageId) => {
+        this.isOnActivePage_ = currPageId === this.getPageId_();
+        this.onPageNavigation_();
+      },
+      true /** callToInitialize */
     );
+    this.storeService_.subscribe(
+      StateProperty.PANNING_MEDIA_STATE,
+      (panningMediaState) => this.onPanningMediaStateChange_(panningMediaState)
+    );
+  }
+
+  /** @private */
+  onPageNavigation_() {
+    if (this.isOnActivePage_) {
+      // TODO(#31932): A key could be sent here to update elements of the same group.
+      // Note, this will not work when there are 2 or more panning components on the same page.
+      // It might need to dynamic to hold more than 1 set of positions.
+      this.storeService_.dispatch(Action.SET_PANNING_MEDIA_STATE, {
+        x: this.x_,
+        y: this.y_,
+        zoom: this.zoom_,
+      });
+    }
+  }
+
+  /**
+   * @private
+   * @param {!Object<string, string>} panningMediaState
+   */
+  onPanningMediaStateChange_(panningMediaState) {
+    if (panningMediaState) {
+      // TODO(#31932): Update siblings that are part of the same group.
+      this.updateTransform_(panningMediaState);
+    }
+  }
+
+  /**
+   * @private
+   * @param {!Object<string, string>} panningMediaState
+   * @return {!Promise}
+   */
+  updateTransform_(panningMediaState) {
+    const {x, y, zoom} = panningMediaState;
+    return this.mutateElement(() => {
+      setImportantStyles(this.ampImgEl_, {
+        transform: `scale(${zoom}) translate(${x}, ${y})`,
+      });
+    });
+  }
+
+  /**
+   * @private
+   * @return {string} the page id
+   */
+  getPageId_() {
+    if (this.pageId_ == null) {
+      this.pageId_ = closest(
+        dev().assertElement(this.element),
+        (el) => el.tagName.toLowerCase() === 'amp-story-page'
+      ).getAttribute('id');
+    }
+    return this.pageId_;
   }
 
   /** @override */
