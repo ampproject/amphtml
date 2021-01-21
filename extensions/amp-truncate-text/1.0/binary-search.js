085c4cbc27e824644494d5000cb22fb06f881cc2
Ryan Cebulko <ryan@cebulko.com>
2021-01-21 12:30:00 -0500

    Fix lint issues

--- extensions/amp-truncate-text/1.0/binary-search.js
+++ extensions/amp-truncate-text/1.0/binary-search.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import {devAssert} from '../../../src/log';
 
 /**
  * When no matching index is found, whether to stop on the next or previous
