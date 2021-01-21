96ce5026faf6350e6936ee6f4bd0b7913dc820c3
Philip Bell <philip.hunter.bell@gmail.com>
2021-01-21 15:00:59 -0500

    Transition elements on navigation (#31559)

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -261,6 +261,8 @@ exports.rules = [
       // AMP Story 360
       'extensions/amp-story-360/0.1/amp-story-360.js->extensions/amp-story/1.0/amp-story-store-service.js',
       'extensions/amp-story-360/0.1/amp-story-360.js->extensions/amp-story/1.0/utils.js',
+      // AMP Story Panning Media
+      'extensions/amp-story-panning-media/0.1/amp-story-panning-media.js->extensions/amp-story/1.0/amp-story-store-service.js',
       // Story ads
       'extensions/amp-story-auto-ads/0.1/amp-story-auto-ads.js->extensions/amp-story/1.0/amp-story-store-service.js',
       'extensions/amp-story-auto-ads/0.1/story-ad-page.js->extensions/amp-story/1.0/amp-story-store-service.js',
