b60b9b18dbbf8af7811c0f25996eab11c554abec
Raghu Simha <rsimha@amp.dev>
2021-01-20 19:05:28 -0500

    ❄️ Skip flaky carousel E2E test (#32086)

--- extensions/amp-base-carousel/0.1/test-e2e/test-advance.js
+++ extensions/amp-base-carousel/0.1/test-e2e/test-advance.js
@@ -47,7 +47,8 @@ describes.endtoend(
       nextArrow = await getNextArrow(controller);
     });
 
-    it('should move forwards once', async () => {
+    // TODO(micajuine-ho, #24195): This test is flaky during CI.
+    it.skip('should move forwards once', async () => {
       await controller.click(nextArrow);
       await sleep(500);
       prevArrow = await getPrevArrow(controller);
