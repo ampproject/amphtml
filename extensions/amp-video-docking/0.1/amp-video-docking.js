63fade2cdbf11cb3341a94a6d9c5aa20faf43661
Alan Orozco <alan@orozco.xyz>
2021-01-27 12:46:44 -0800

    typo

--- extensions/amp-video-docking/0.1/amp-video-docking.js
+++ extensions/amp-video-docking/0.1/amp-video-docking.js
@@ -1049,7 +1049,7 @@ export class VideoDocking {
 
       // Since the AMP element container is position: relative, the media
       // element is relative to AMP element corner when position: absolute, so
-      // we need to offset it by outer contianer's position.
+      // we need to offset it by outer container's position.
       const offset = position === 'absolute' ? clientRect : {left: 0, top: 0};
 
       this.getElementsOnDockArea_(video).forEach((el) => {
