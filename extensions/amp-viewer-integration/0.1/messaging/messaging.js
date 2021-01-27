bc9b8447f67338811168a3b6ed60623e574c125d
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2021-01-27 15:28:11 -0500

    [amp-viewer-integration] Don't import from AMP, use regex as constant (#32109)
    * dont import from amp, use regex as constant
    
    * pass regex as param

--- extensions/amp-viewer-integration/0.1/messaging/messaging.js
+++ extensions/amp-viewer-integration/0.1/messaging/messaging.js
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-import {urls} from '../../../../src/config';
-
 const TAG = 'amp-viewer-messaging';
 const CHANNEL_OPEN_MSG = 'channelOpen';
 const HANDSHAKE_POLL_MSG = 'handshake-poll';
@@ -163,9 +161,16 @@ export class Messaging {
    * @param {!Window} target - window containing AMP document to perform handshake with (usually contentWindow of iframe)
    * @param {string} origin - origin of target window (use "null" if opaque)
    * @param {?string=} opt_token - message token to verify on incoming messages (must be provided as viewer parameter)
+   * @param {?RegExp=} opt_cdnProxyRegex
    * @return {!Promise<!Messaging>}
    */
-  static waitForHandshakeFromDocument(source, target, origin, opt_token) {
+  static waitForHandshakeFromDocument(
+    source,
+    target,
+    origin,
+    opt_token,
+    opt_cdnProxyRegex
+  ) {
     return new Promise((resolve) => {
       const listener = (event) => {
         const message = parseMessage(event.data);
@@ -173,7 +178,8 @@ export class Messaging {
           return;
         }
         if (
-          (event.origin == origin || urls.cdnProxyRegex.test(event.origin)) &&
+          (event.origin == origin ||
+            (opt_cdnProxyRegex && opt_cdnProxyRegex.test(event.origin))) &&
           (!event.source || event.source == target) &&
           message.app === APP &&
           message.name === CHANNEL_OPEN_MSG
