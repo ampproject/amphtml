e8ff7f185a969bad499781dab35a43df1802be8c
Alan Orozco <alan@orozco.xyz>
2021-01-29 11:57:13 -0800

    🏗 Lint .js in examples/

--- examples/ac-creative.js
+++ examples/ac-creative.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 if (!window.context) {
   // window.context doesn't exist yet, must perform steps to create it
   // before using it
@@ -15,16 +30,12 @@ function intersectionCallback(changes) {
   var latestChange = changes[changes.length - 1];
 
   // Amp-ad width and height.
-  var w = latestChange.boundingClientRect.width;
-  var h = latestChange.boundingClientRect.height;
+  var {width: w, height: h, x: vx, y: vy} = latestChange.boundingClientRect;
 
   // Visible width and height.
-  var vw = latestChange.intersectionRect.width;
-  var vh = latestChange.intersectionRect.height;
+  var {width: vw, height: vh} = latestChange.intersectionRect;
 
   // Position in the viewport.
-  var vx = latestChange.boundingClientRect.x;
-  var vy = latestChange.boundingClientRect.y;
 
   // Viewable percentage.
   var viewablePerc = ((vw * vh) / (w * h)) * 100;
--- examples/visual-tests/font.amp.404/font.amp.js
+++ examples/visual-tests/font.amp.404/font.amp.js
@@ -23,8 +23,10 @@ module.exports = {
     ];
     for (const selector of forbiddenSelectors) {
       if ((await page.$(selector)) !== null) {
-        throw new Error(`${colors.cyan(testName)} | The forbidden CSS ` +
-            `element ${colors.cyan(selector)} exists in the page`);
+        throw new Error(
+          `${colors.cyan(testName)} | The forbidden CSS ` +
+            `element ${colors.cyan(selector)} exists in the page`
+        );
       }
     }
   },
