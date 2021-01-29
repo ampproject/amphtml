28f8c5c3e1f51854040554d2dbcfa25802868bd6
Raghu Simha <rsimha@amp.dev>
2021-01-29 00:59:09 -0500

    🏗  Consolidate remaining CircleCI VM setup steps into separate scripts (#32290)

--- .circleci/fetch_merge_commit.sh
+++ .circleci/fetch_merge_commit.sh
@@ -22,15 +22,18 @@
 set -e
 err=0
 
+GREEN() { echo -e "\n\033[0;32m$1\033[0m"; }
+RED() { echo -e "\n\033[0;31m$1\033[0m"; }
+
 # CIRCLE_PULL_REQUEST is present for PR builds, and absent for push builds.
 if [[ -z "$CIRCLE_PULL_REQUEST" ]]; then
-  echo -e "Nothing to do because this is not a PR build."
+  echo $(GREEN "Nothing to do because this is not a PR build.")
   exit 0
 fi
 
 # Make sure the PR is on ampproject/amphtml and not on a fork.
 if [[ ! "$CIRCLE_PULL_REQUEST" =~ ^https://github.com/ampproject/amphtml* ]]; then
-  echo -e "This is a PR build, but on a repo other than ampproject/amphtml."
+  echo $(RED "This is a PR build, but on a repo other than ampproject/amphtml.")
   exit 1
 fi
 
@@ -47,16 +50,15 @@ fi
 # every PR branch that can be cleanly merged to master. For more details, see:
 # https://discuss.circleci.com/t/show-test-results-for-prospective-merge-of-a-github-pr/1662
 MERGE_BRANCH="refs/pull/$PR_NUMBER/merge"
+echo $(GREEN "Fetching merge commit from $MERGE_BRANCH...")
 (set -x && git pull --ff-only origin "$MERGE_BRANCH") || err=$?
-
 
 # If a clean merge is not possible, do not proceed with the build. GitHub's UI
 # will show an error indicating there was a merge conflict.
 if [[ "$err" -ne "0" ]]; then
-  echo
-  echo -e "ERROR: Detected a merge conflict between $CIRCLE_BRANCH and master."
-  echo -e "Please rebase your PR branch."
+  echo $(RED "Detected a merge conflict between $CIRCLE_BRANCH and master.")
+  echo $(RED "Please rebase your PR branch.")
   exit $err
 fi
 
-echo -e "Successfully fetched merge commit of $CIRCLE_BRANCH with master."
+echo $(GREEN "Successfully fetched merge commit of $CIRCLE_BRANCH with master.")
