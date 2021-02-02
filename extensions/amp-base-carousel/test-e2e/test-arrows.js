4622fcce029971524edaabd8128494ddaa9e223c
Ryan Cebulko <ryan@cebulko.com>
2021-02-02 14:40:47 -0500

    Unify test-arrows

--- extensions/amp-base-carousel/test-e2e/test-arrows.js
+++ extensions/amp-base-carousel/test-e2e/test-arrows.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {getNextArrow, getPrevArrow, getSlide} from './helpers';
+import ctrlHelpers from './helpers';
 
 /** The total number of slides in the carousel */
 const SLIDE_COUNT = 7;
@@ -23,40 +23,47 @@ describes.endtoend(
   'amp-base-carousel:0.1 - arrows with custom arrows',
   {
     fixture: 'amp-base-carousel/custom-arrows.amp.html',
-    experiments: [
-      'amp-base-carousel',
-      'layers',
-      'amp-lightbox-gallery-base-carousel',
-    ],
-    //TODO(spaharmi): fails on shadow demo
     environments: ['single', 'viewer-demo'],
+
+    versions: {
+      '0.1': {
+        experiments: [
+          'amp-base-carousel',
+          'layers',
+          'amp-lightbox-gallery-base-carousel',
+        ],
+      },
+      '1.0': {
+        experiments: ['bento-carousel'],
+      },
+    },
   },
-  async function (env) {
-    let controller;
-    let prevArrow;
-    let nextArrow;
+  async (env) => {
+    let ctrl;
 
-    beforeEach(async () => {
-      controller = env.controller;
-
-      nextArrow = await getNextArrow(controller);
-      prevArrow = await getPrevArrow(controller);
+    beforeEach(() => {
+      ctrl = ctrlHelpers(env);
+      ctrl.maybeSwitchToShadow();
     });
 
-    it('should go to the next slide', async () => {
-      const secondSlide = await getSlide(controller, 1);
+    afterEach(() => ctrl.maybeSwitchToLight());
 
-      await controller.click(nextArrow);
-      await expect(controller.getElementRect(secondSlide)).to.include({
+    it('should go to the next slide', async () => {
+      const nextArrow = await ctrl.getNextArrowSlot();
+      const secondSlide = await ctrl.getSlide(1);
+
+      await ctrl.click(nextArrow);
+      await expect(ctrl.rect(secondSlide)).to.include({
         'x': 0,
       });
     });
 
     it('should go to the previous slide', async () => {
-      const lastSlide = await getSlide(controller, SLIDE_COUNT - 1);
+      const prevArrow = await ctrl.getPrevArrowSlot();
+      const lastSlide = await ctrl.getSlide(SLIDE_COUNT - 1);
 
-      await controller.click(prevArrow);
-      await expect(controller.getElementRect(lastSlide)).to.include({
+      await ctrl.click(prevArrow);
+      await expect(ctrl.rect(lastSlide)).to.include({
         'x': 0,
       });
     });
