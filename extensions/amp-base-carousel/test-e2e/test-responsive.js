ec21e069b2efffd4150fc52c8b41e178d7f1d064
Ryan Cebulko <ryan@cebulko.com>
2021-02-02 14:40:52 -0500

    Unify test-responsive

--- extensions/amp-base-carousel/test-e2e/test-responsive.js
+++ extensions/amp-base-carousel/test-e2e/test-responsive.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {getScrollingElement, getSlide, waitForCarouselImg} from './helpers';
+import ctrlHelpers from './helpers';
 
 const pageWidth = 1000;
 const pageHeight = 600;
@@ -23,85 +23,85 @@ describes.endtoend(
   'amp-base-carousel:0.1 - responsive attributes',
   {
     fixture: 'amp-base-carousel/responsive.amp.html',
-    experiments: ['amp-base-carousel', 'layers'],
     initialRect: {width: pageWidth, height: pageHeight},
-    //TODO(spaharmi): fails on shadow demo
     environments: ['single', 'viewer-demo'],
+    versions: {
+      '1.0': {
+        experiments: ['bento-carousel'],
+      },
+      '0.1': {
+        environments: ['single', 'viewer-demo'],
+      },
+    },
   },
   async (env) => {
-    let controller;
-
-    function prop(el, name) {
-      return controller.getElementProperty(el, name);
-    }
+    let ctrl;
 
     beforeEach(async () => {
-      controller = env.controller;
+      ctrl = ctrlHelpers(env);
+      ctrl.maybeSwitchToShadow();
     });
 
     it('should layout correctly initially', async () => {
-      const firstSlide = await getSlide(controller, 0);
+      const firstSlide = await ctrl.getSlide(0);
 
-      await waitForCarouselImg(controller, 0);
+      ctrl.expectSlideImgLoaded(0);
       // 3 slides width width 1000 = 333 width per slide.
-      await expect(controller.getElementRect(firstSlide)).to.include({
+      await expect(ctrl.rect(firstSlide)).to.include({
         width: 333,
         x: 0,
       });
     });
 
     it('should layout correctly after resize', async () => {
-      const firstSlide = await getSlide(controller, 0);
+      const firstSlide = await ctrl.getSlide(0);
 
-      await waitForCarouselImg(controller, 0);
-      await controller.setWindowRect({
-        width: 600,
-        height: 600,
-      });
+      ctrl.expectSlideImgLoaded(0);
+      await ctrl.setWin(600, 600);
       // 2 slides width width 600 = 300 width per slide.
-      await expect(controller.getElementRect(firstSlide)).to.include({
+      await expect(ctrl.rect(firstSlide)).to.include({
         width: 300,
         x: 0,
       });
     });
 
     it('should retain position when changing the visible count', async () => {
-      const el = await getScrollingElement(controller);
-      const secondSlide = await getSlide(controller, 1);
+      const el = await ctrl.getScrollingElement();
+      const secondSlide = await ctrl.getSlide(1);
 
-      await controller.scrollTo(el, {left: 333});
-      await expect(prop(el, 'scrollLeft')).to.equal(333);
-      await controller.setWindowRect({
-        width: 600,
-        height: 600,
-      });
+      await ctrl.scrollTo(el, {left: 333});
+      await expect(ctrl.prop(el, 'scrollLeft')).to.equal(333);
 
-      await expect(controller.getElementRect(secondSlide)).to.include({x: 0});
+      // Wait for render with updated active slide.
+      await ctrl.sleep(100);
+      await ctrl.setWin(600, 600);
+
+      await expect(ctrl.rect(secondSlide)).to.include({x: 0});
     });
 
     it('should respond to attribute changes', async () => {
-      const firstSlide = await getSlide(controller, 0);
+      const firstSlide = await ctrl.getSlide(0);
 
       // 3 slides width width 1000 = 333 width per slide.
-      await expect(controller.getElementRect(firstSlide)).to.include({
+      await expect(ctrl.rect(firstSlide)).to.include({
         width: 333,
         x: 0,
       });
       // Switch over to `visible-count="(min-width: 650px) 5, 4".
-      const btn = await controller.findElement('#responsive-5-4');
-      await controller.click(btn);
+      await ctrl.maybeSwitchToLight();
+      const btn = await ctrl.findElement('#responsive-5-4');
+      await ctrl.click(btn);
+      await ctrl.maybeSwitchToShadow();
+
       // 5 slides width width 1000 = 200 width per slide
-      await expect(controller.getElementRect(firstSlide)).to.include({
+      await expect(ctrl.rect(firstSlide)).to.include({
         width: 200,
         x: 0,
       });
       // Now make sure new media query is active.
-      await controller.setWindowRect({
-        width: 600,
-        height: 600,
-      });
+      await ctrl.setWin(600, 600);
       // 4 slides width width 600 = 150 width per slide.
-      await expect(controller.getElementRect(firstSlide)).to.include({
+      await expect(ctrl.rect(firstSlide)).to.include({
         width: 150,
         x: 0,
       });
