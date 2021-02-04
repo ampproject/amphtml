a7f57c34fd3fa8094a5c78f36bf9620a4cd94200
Dima Voytenko <dvoytenko@google.com>
2021-02-03 21:33:52 -0800

    review fixes

--- src/ini-load.js
+++ src/ini-load.js
@@ -104,63 +104,58 @@ export function whenContentIniLoadMeasure(
  */
 export function whenContentIniLoadInOb(elementOrAmpDoc, opt_prerenderableOnly) {
   const ampdoc = Services.ampdoc(elementOrAmpDoc);
-  return (
-    ampdoc
-      .signals()
-      // First, wait for the `ready-scan` signal. Waiting for each element
-      // individually is too expensive and `ready-scan` will cover most of
-      // the initially parsed elements.
-      .whenSignal(READY_SCAN_SIGNAL)
-      .then(() => {
-        // Filter elements.
-        const resources = Services.resourcesForDoc(ampdoc);
-        const elements = resources
-          .get()
-          .filter((r) => {
-            if (opt_prerenderableOnly && !r.prerenderAllowed()) {
-              return false;
-            }
-            return !EXCLUDE_INI_LOAD.includes(r.element.tagName);
-          })
-          .map((r) => r.element);
-
-        if (elements.length === 0) {
-          return Promise.resolve([]);
+  // First, wait for the `ready-scan` signal. Waiting for each element
+  // individually is too expensive and `ready-scan` will cover most of
+  // the initially parsed elements.
+  const whenReady = ampdoc.signals().whenSignal(READY_SCAN_SIGNAL);
+  return whenReady.then(() => {
+    // Filter elements.
+    const resources = Services.resourcesForDoc(ampdoc);
+    const elements = resources
+      .get()
+      .filter((r) => {
+        if (opt_prerenderableOnly && !r.prerenderAllowed()) {
+          return false;
         }
+        return !EXCLUDE_INI_LOAD.includes(r.element.tagName);
+      })
+      .map((r) => r.element);
 
-        // Find intersecting elements.
-        return new Promise((resolve) => {
-          const {win} = ampdoc;
-          const io = new win.IntersectionObserver(
-            (entries) => {
-              io.disconnect();
-              const intersecting = [];
-              for (let i = 0; i < entries.length; i++) {
-                const {target, isIntersecting} = entries[i];
-                if (isIntersecting) {
-                  intersecting.push(target);
-                }
-              }
-              resolve(intersecting);
-            },
-            {
-              // We generally always want `root: document` here. However, in
-              // many browsers this is still polyfilled and `{root: null}` is
-              // a lot faster.
-              root: isIframed(win) ? win.document : null,
-              threshold: 0.01,
+    if (elements.length === 0) {
+      return Promise.resolve([]);
+    }
+
+    // Find intersecting elements.
+    return new Promise((resolve) => {
+      const {win} = ampdoc;
+      const io = new win.IntersectionObserver(
+        (entries) => {
+          io.disconnect();
+          const intersecting = [];
+          for (let i = 0; i < entries.length; i++) {
+            const {target, isIntersecting} = entries[i];
+            if (isIntersecting) {
+              intersecting.push(target);
             }
-          );
-          // Limit check to the first 100 elements.
-          for (let i = 0; i < Math.min(elements.length, 100); i++) {
-            io.observe(elements[i]);
           }
-        });
-      })
-      .then((elements) => {
-        return Promise.all(elements.map((element) => element.whenLoaded()));
-      })
-  );
+          resolve(intersecting);
+        },
+        {
+          // We generally always want `root: document` here. However, in
+          // many browsers this is still polyfilled and `{root: null}` is
+          // a lot faster.
+          root: isIframed(win) ? win.document : null,
+          threshold: 0.01,
+        }
+      );
+      // Limit check to the first 100 elements.
+      for (let i = 0; i < Math.min(elements.length, 100); i++) {
+        io.observe(elements[i]);
+      }
+    }).then((elements) => {
+      return Promise.all(elements.map((element) => element.whenLoaded()));
+    });
+  });
 }
 
 /**
