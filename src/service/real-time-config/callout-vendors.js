c2c511acea16274559bc560262060f90f7882fa0
Luigi Sayson <48766825+luigi-sayson@users.noreply.github.com>
2021-02-04 13:56:21 -0800

    ✨ Add prebidopenx as a vendor to RTC config (#32247)
    * add prebidopenx as a vendor to RTC config
    * add vendor to documentation pages
    
    Co-authored-by: Kenan Gillet <kenan.gillet@openx.com>

--- src/service/real-time-config/callout-vendors.js
+++ src/service/real-time-config/callout-vendors.js
@@ -243,6 +243,12 @@ const RTC_VENDORS = jsonConfiguration({
     macros: ['TAG_ID', 'CONSENT_STRING', 'ACCOUNT_ID'],
     disableKeyAppend: true,
   },
+  prebidopenx: {
+    url:
+      'https://prebid.openx.net/openrtb2/amp?tag_id=REQUEST_ID&w=ATTR(width)&h=ATTR(height)&ow=ATTR(data-override-width)&oh=ATTR(data-override-height)&ms=ATTR(data-multi-size)&slot=ATTR(data-slot)&targeting=TGT&curl=CANONICAL_URL&timeout=TIMEOUT&adc=ADCID&purl=HREF&gdpr_consent=CONSENT_STRING&account=ACCOUNT_ID',
+    macros: ['REQUEST_ID', 'CONSENT_STRING', 'ACCOUNT_ID'],
+    disableKeyAppend: true,
+  },
 });
 
 // DO NOT MODIFY: Setup for tests
