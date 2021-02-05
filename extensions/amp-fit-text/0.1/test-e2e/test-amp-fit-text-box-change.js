86b426ffe7a2bae9e77662e3ca2ad571800b39f6
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-02-05 12:09:07 -0800

    Update amp-fit-text-box-change to use `fixture` instead of `testUrl`.

--- extensions/amp-fit-text/0.1/test-e2e/test-amp-fit-text-box-change.js
+++ extensions/amp-fit-text/0.1/test-e2e/test-amp-fit-text-box-change.js
@@ -17,8 +17,7 @@
 describes.endtoend(
   'amp-fit-text',
   {
-    testUrl:
-      'http://localhost:8000/test/fixtures/e2e/amp-fit-text/0.1/amp-fit-text-box-change.html',
+    fixture: 'amp-fit-text/0.1/amp-fit-text-box-change.html',
     environments: 'ampdoc-amp4ads-preset',
   },
   (env) => {
