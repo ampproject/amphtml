f0c263b5d6cee658f4b939e10f53d78b7b75fcd7
Matias Szylkowski <mszylkowski@google.com>
2021-02-09 12:38:57 -0500

    ✨ [Story autoanalytics] Added repeat: false on story-page-visible (#32296)
    * Added repeat
    
    * Added to last page
    
    * Trigger travis
    
    * Trigger again

--- extensions/amp-story-auto-analytics/0.1/auto-analytics-configs.js
+++ extensions/amp-story-auto-analytics/0.1/auto-analytics-configs.js
@@ -37,6 +37,9 @@ export const buildGtagConfig = (gtagId) => ({
         'event_label': '${storyPageCount}',
         'send_to': [gtagId],
       },
+      'storySpec': {
+        'repeat': false,
+      },
     },
     'storyEnd': {
       'on': 'story-last-page-visible',
@@ -47,6 +50,9 @@ export const buildGtagConfig = (gtagId) => ({
         'event_label': '${title}',
         'send_to': [gtagId],
       },
+      'storySpec': {
+        'repeat': false,
+      },
     },
     'storyPageIndex': {
       'on': 'story-page-visible',
@@ -56,6 +62,9 @@ export const buildGtagConfig = (gtagId) => ({
         'event_category': '${title}',
         'event_label': '${storyPageIndex}',
         'send_to': [gtagId],
+      },
+      'storySpec': {
+        'repeat': false,
       },
     },
   },
