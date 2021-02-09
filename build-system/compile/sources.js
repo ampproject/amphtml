1ed964e6371e3e8ec4bf63a96a787582ae96943f
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2021-02-09 17:49:01 -0500

    amp-fit-text: Add cross-version dependencies to source files (#32564)
    * Add dependency to sources
    
    * Allow *.js instead
    
    * Add TODO

--- build-system/compile/sources.js
+++ build-system/compile/sources.js
@@ -112,6 +112,9 @@ const CLOSURE_SRC_GLOBS = [
   'extensions/amp-bind/**/*.js',
   // Needed to access to Variant interface from other extensions
   'extensions/amp-experiment/**/*.js',
+  // TODO(#32523) Remove this when Bento experiment is done.
+  // Needed to access across versions
+  'extensions/amp-fit-text/1.0/*.js',
   // Needed to access form impl from other extensions
   'extensions/amp-form/**/*.js',
   // Needed by amp-facebook-* for the loader logo
