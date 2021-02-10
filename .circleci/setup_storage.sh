f6d4ca7701e8f6532e5cdb69ad8fcda0fe7cd119
Raghu Simha <rsimha@amp.dev>
2021-02-10 13:09:36 -0500

    Revert "🏗 Add fail-fast behavior to CircleCI builds" (#32589)
    This reverts commit 36d86e2c6fb86f95e5f9a6a5225b648b348369b6.

--- .circleci/setup_storage.sh
+++ .circleci/setup_storage.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the license.
-
+#
 # Script used by AMP's CI builds to authenticate with GCP storage on CircleCI.
 # TODO(rsimha, ampproject/amp-github-apps#1110): Update storage details.
 
