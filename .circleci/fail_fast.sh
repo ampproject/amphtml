5612e016a4dd1d0720058b2a2a902883747b9ed1
Raghu Simha <rsimha@amp.dev>
2021-02-10 15:20:16 -0500

    Fix comment

--- .circleci/fail_fast.sh
+++ .circleci/fail_fast.sh
@@ -22,7 +22,9 @@ set -e
 RED() { echo -e "\n\033[0;31m$1\033[0m"; }
 YELLOW() { echo -e "\n\033[0;33m$1\033[0m"; }
 
-# For push builds, continue in spite of failures so baselines are established.
+# For push builds, continue in spite of failures so that other jobs like
+# bundle-size and visual-diff can establish their baselines for this commit.
+# Without this, our custom bots will not be able to function correctly.
 if [[ "$CIRCLE_BRANCH" == "master" || "$CIRCLE_BRANCH" =~ ^amp-release-* ]]; then
   echo $(YELLOW "Not canceling build in spite of failures because $CIRCLE_BRANCH is not a PR branch.")
   exit 0
