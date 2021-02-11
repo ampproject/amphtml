dc8620c5d51d6e9ad2a4c6f16d0edebc2903facc
Raghu Simha <rsimha@amp.dev>
2021-02-10 23:30:44 -0500

    🏗 Clean up log coloring, enable on GH Actions (#32604)

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -29,6 +29,9 @@ if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
   echo "$HOME/.npm/bin" >> $GITHUB_PATH # For later
 fi
 
+echo $(GREEN "Enabling log coloring...")
+echo "FORCE_COLOR=1" >> $GITHUB_ENV
+
 echo $(GREEN "Installing gulp-cli...")
 npm install --global gulp-cli
 
