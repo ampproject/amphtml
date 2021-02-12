86cf9ff4d395f6dc9424d348ac3ee0798d1b136f
Caroline Liu <carolineliu@google.com>
2021-02-12 11:19:42 -0500

    Specify cross-component file dependencies

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -246,6 +246,10 @@ exports.rules = [
       'extensions/amp-stream-gallery/1.0/amp-stream-gallery.js->extensions/amp-base-carousel/1.0/base-carousel.jss.js',
       'extensions/amp-stream-gallery/1.0/stream-gallery.js->extensions/amp-base-carousel/1.0/base-carousel.js',
 
+      // Autolightboxing dependencies
+      'extensions/amp-base-carousel/1.0/base-carousel.js->extensions/amp-lightbox-gallery/1.0/component.js',
+      'extensions/amp-lightbox-gallery/1.0/component.js->extensions/amp-lightbox/1.0/lightbox.js',
+
       // Facebook components
       'extensions/amp-facebook-page/0.1/amp-facebook-page.js->extensions/amp-facebook/0.1/facebook-loader.js',
       'extensions/amp-facebook-comments/0.1/amp-facebook-comments.js->extensions/amp-facebook/0.1/facebook-loader.js',
