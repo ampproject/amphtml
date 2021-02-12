6130dedea2070d7d880af520ec7ba62ff81fce4b
Kevin Dwan <25626770+krdwan@users.noreply.github.com>
2021-02-12 15:09:07 -0500

    ✨ [bento][amp-sidebar] Update animations to obey APIs mid-animation (#32584)
    * mostly working
    
    * Update animations to obey apis mid animation
    
    * Clean up typos and style
    
    * Update animation time
    
    * Refactor to custom hook
    
    * Review comments
    
    * Use useValueRef for side and add useValueRef to utils
    
    * Update Direction constant to AnimationState
    
    * Simplify animation states
    
    * Move value ref location
    
    * Removed test code
    
    * Review comments
    
    * Add a comment explaning side
    
    * Tests for animations

--- extensions/amp-sidebar/1.0/sidebar-config.js
+++ extensions/amp-sidebar/1.0/sidebar-config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-export {ContainWrapper} from './contain';
-export {Wrapper} from './wrapper';
-export {useRenderer} from './renderer';
-export {useValueRef} from './value-ref';
+/** @protected @enum {string} */
+export const Side = {
+  LEFT: 'left',
+  RIGHT: 'right',
+};
