32c9620b797757cdffaec87723c7d004130a7b08
Jake Fried <samouri@users.noreply.github.com>
2021-02-16 15:15:02 -0500

    bind-expression: remove fn.name check

--- extensions/amp-bind/0.1/bind-expression.js
+++ extensions/amp-bind/0.1/bind-expression.js
@@ -183,11 +183,6 @@ function generateFunctionAllowlist() {
     Object.keys(functionsForType).forEach((name) => {
       const func = functionsForType[name];
       if (func) {
-        devAssert(
-          !func.name || name === func.name,
-          'Listed function name ' +
-            `"${name}" doesn't match name property "${func.name}".`
-        );
         out[type][name] = func;
       } else {
         // This can happen if a browser doesn't support a built-in function.
