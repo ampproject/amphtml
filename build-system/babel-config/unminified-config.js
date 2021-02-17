11c8e22bd2246e8ab7a2fee92ead06b927f2ec6f
Justin Ridgewell <jridgewell@google.com>
2021-02-17 15:50:24 -0500

    🏗️  Create a babel plugin to transform $internalRuntimeVersion$ (#32700)
    * Create a babel plugin to transform $internalRuntimeVersion$
    
    This eliminates the sourcemap break cause by an unaware find/replace.
    
    * Review updates
    
    * Handle tempalte elements
    
    * Prettify

--- build-system/babel-config/unminified-config.js
+++ build-system/babel-config/unminified-config.js
@@ -16,6 +16,9 @@
 'use strict';
 
 const argv = require('minimist')(process.argv.slice(2));
+const {
+  VERSION: internalRuntimeVersion,
+} = require('../compile/internal-version');
 const {getReplacePlugin} = require('./helpers');
 
 /**
@@ -46,6 +49,10 @@ function getUnminifiedConfig() {
     argv.coverage ? 'babel-plugin-istanbul' : null,
     replacePlugin,
     './build-system/babel-plugins/babel-plugin-transform-json-import',
+    [
+      './build-system/babel-plugins/babel-plugin-transform-internal-version',
+      {version: internalRuntimeVersion},
+    ],
     './build-system/babel-plugins/babel-plugin-transform-json-configuration',
     './build-system/babel-plugins/babel-plugin-transform-jss',
     './build-system/babel-plugins/babel-plugin-transform-fix-leading-comments',
