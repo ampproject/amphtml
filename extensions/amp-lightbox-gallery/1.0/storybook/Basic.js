bdad95643f33ab9cedaf278c5ea82e25707bd381
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2021-02-22 10:09:27 -0500

    ✨ Bento Lightbox Gallery (#32008)
    * Prototype
    
    * Update with deeply nested example
    
    * Preact skeleton
    
    * cloneElement takes a children param
    
    * Adopt new file structure
    
    * Only use WithLightbox
    
    * Make WithLightbox more customizable
    
    * Lightbox content should support scrolling
    
    * Support lightboxing BaseCarousel
    
    * Fix types
    
    * Specify cross-component file dependencies
    
    * Dedupe contentRef and lightboxRef
    
    * With lightbox does not need a ref
    
    * Fix a11y attributes
    
    * Don't render elements until the LightboxGallery opens
    
    * Don't take thumbnailSrc for lightbox
    
    * Split up files
    
    * LightboxGallery -> LightboxGalleryProvider
    
    * Update dependency list
    
    * LightboxGalleryProvider render prop
    
    * Remove key
    
    * Rename autoLightbox as enableActivation
    
    * Update type definitions
    
    * Always render <Comp> layer
    
    * Export directly
    
    * lightbox.js moved to component.js

---
+++ extensions/amp-lightbox-gallery/1.0/storybook/Basic.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../../src/preact';
+import {BaseCarousel} from '../../../amp-base-carousel/1.0/base-carousel';
+import {LightboxGalleryProvider, WithLightbox} from '../component';
+import {withA11y} from '@storybook/addon-a11y';
+import {withKnobs} from '@storybook/addon-knobs';
+
+export default {
+  title: 'LightboxGallery',
+  component: LightboxGalleryProvider,
+  decorators: [withKnobs, withA11y],
+};
+
+export const _default = () => {
+  return (
+    <>
+      <LightboxGalleryProvider>
+        <style>{`
+        img {
+          width: 240px;
+          height: 160px;
+        }
+      `}</style>
+        <WithLightbox>
+          <img src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1498&q=80" />
+        </WithLightbox>
+        <p>abc</p>
+        <WithLightbox
+          as="img"
+          alt="larger img"
+          id="foo"
+          src="https://images.unsplash.com/photo-1583511666407-5f06533f2113?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80"
+          render={() => (
+            <img
+              alt="smaller img"
+              src="https://images.unsplash.com/photo-1583511666407-5f06533f2113?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
+            />
+          )}
+        />
+        <p>abc</p>
+        <WithLightbox>
+          <img src="https://images.unsplash.com/photo-1599839575945-a9e5af0c3fa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQwMzA0fQ&auto=format&fit=crop&w=1498&q=80" />
+        </WithLightbox>
+        <div>
+          <div>
+            <div>
+              <WithLightbox>
+                <img src="https://images.unsplash.com/photo-1603123853880-a92fafb7809f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80" />
+              </WithLightbox>
+            </div>
+          </div>
+        </div>
+      </LightboxGalleryProvider>
+    </>
+  );
+};
+
+export const carousel = () => {
+  return (
+    <>
+      <style>{`
+    img {
+      width: 240px;
+      height: 160px;
+    }
+  `}</style>
+      <LightboxGalleryProvider>
+        <BaseCarousel lightbox style={{width: '240px', height: '160px'}}>
+          <img
+            src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=120&q=80"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1583511666407-5f06533f2113?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1583511666407-5f06533f2113?ixlib=rb-1.2.1&auto=format&fit=crop&w=120&q=80"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1599839575945-a9e5af0c3fa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQwMzA0fQ&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1599839575945-a9e5af0c3fa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQwMzA0fQ&auto=format&fit=crop&w=1498&q=80"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1583512603806-077998240c7a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1583512603806-077998240c7a?ixlib=rb-1.2.1&auto=format&fit=crop&w=120&q=80"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1598133893773-de3574464ef0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1598133893773-de3574464ef0?ixlib=rb-1.2.1&auto=format&fit=crop&w=120&q=80"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1603123853880-a92fafb7809f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80"
+            thumbnailSrc="https://images.unsplash.com/photo-1603123853880-a92fafb7809f?ixlib=rb-1.2.1&auto=format&fit=crop&w=120&q=80"
+          />
+        </BaseCarousel>
+      </LightboxGalleryProvider>
+    </>
+  );
+};
