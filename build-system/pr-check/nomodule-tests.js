bc2e4b7dd05e846af632e8334de2d5b87574d6c7
Daniel Rozenberg <rodaniel@amp.dev>
2021-02-23 16:04:50 -0500

    Execute integration tests on both prod and canary config, with experiments derandomized

--- build-system/pr-check/nomodule-tests.js
+++ build-system/pr-check/nomodule-tests.js
@@ -26,13 +26,23 @@ const {
   timedExecOrThrow,
 } = require('./utils');
 const {buildTargetsInclude, Targets} = require('./build-targets');
+const {MINIFIED_TARGETS} = require('../tasks/helpers');
 const {runCiJob} = require('./ci-job');
 
 const jobName = 'nomodule-tests.js';
 
+function prependConfig() {
+  for (const target of MINIFIED_TARGETS) {
+    timedExecOrDie(
+      `gulp prepend-global --${process.env.config} --local_dev --fortesting --derandomize --target=dist/${target}.js`
+    );
+  }
+}
+
 function pushBuildWorkflow() {
   downloadNomoduleOutput();
   timedExecOrDie('gulp update-packages');
+  prependConfig();
   try {
     timedExecOrThrow(
       'gulp integration --nobuild --headless --compiled --report',
@@ -51,6 +61,7 @@ function prBuildWorkflow() {
   if (buildTargetsInclude(Targets.RUNTIME, Targets.INTEGRATION_TEST)) {
     downloadNomoduleOutput();
     timedExecOrDie('gulp update-packages');
+    prependConfig();
     timedExecOrDie('gulp integration --nobuild --compiled --headless');
   } else {
     printSkipMessage(
