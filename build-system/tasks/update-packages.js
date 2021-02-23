c7c3f5aafc57efc2bad1127dd1aea71939205cfc
Dima Voytenko <dvoytenko@google.com>
2021-02-22 21:23:19 -0800

    Shadow DOM polyfill for elements that need it

--- build-system/tasks/update-packages.js
+++ build-system/tasks/update-packages.js
@@ -125,6 +125,29 @@ function patchResizeObserver() {
 }
 
 /**
+ * Patches Shadow DOM polyfill by wrapping its body into `install`
+ * function.
+ * This gives us an option to control when and how the polyfill is installed.
+ * The polyfill can only be installed on the root context.
+ */
+function patchShadowDom() {
+  // Copies webcomponents-sd into a new file that has an export.
+  const patchedName =
+    'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-sd.install.js';
+  let file = fs
+    .readFileSync(
+      'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-sd.js'
+    )
+    .toString();
+
+  // Wrap the contents inside the install function.
+  // Also need to remove `nocompile` directive, otherwise Closure skips
+  // the entire import.
+  file = `export function install() {${file.replace('@nocompile', '')}}`;
+  writeIfUpdated(patchedName, file);
+}
+
+/**
  * Deletes the map file for rrule, which breaks closure compiler.
  * TODO(rsimha): Remove this workaround after a fix is merged for
  * https://github.com/google/closure-compiler/issues/3720.
@@ -185,6 +208,7 @@ async function updatePackages() {
   patchWebAnimations();
   patchIntersectionObserver();
   patchResizeObserver();
+  patchShadowDom();
   removeRruleSourcemap();
 }
 
