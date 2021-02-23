67789c7b035f8e983b0d873d9a47d7682b7cf781
Su Zhang (張甦) <me@zhang.su>
2021-02-23 15:25:02 -0500

    Shorten URL in error messages (#32818)
    Currently the full URL is too long for Gmail since the URL includes
    `__amp_source_origin`, which is the full data URL for the AMP document
    itself.
    
    This follows the eliding pattern for the non-SSR path in xhr-impl:
    
    https://github.com/ampproject/amphtml/blob/a0f71dfb0381e236d6c6be287549748838731467/src/service/xhr-impl.js#L11

--- extensions/amp-list/0.1/test/test-amp-list.js
+++ extensions/amp-list/0.1/test/test-amp-list.js
@@ -829,7 +829,7 @@ describes.repeated(
               return expect(
                 list.layoutCallback()
               ).to.eventually.be.rejectedWith(
-                /XHR Failed fetching \(https:\/\/data.com.+?\): error/
+                /XHR Failed fetching \(https:\/\/data.com\/\.\.\.\): error/
               );
             });
 
@@ -853,7 +853,7 @@ describes.repeated(
               return expect(
                 list.layoutCallback()
               ).to.eventually.be.rejectedWith(
-                /fetching JSON data \(https:\/\/data.com.+?\): HTTP error 400/
+                /fetching JSON data \(https:\/\/data.com\/\.\.\.\): HTTP error 400/
               );
             });
 
