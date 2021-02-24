b4c0f6d878dbaef807c7bf93facff35039081aa9
Raghu Simha <rsimha@amp.dev>
2021-02-23 23:10:29 -0500

    🏗 Rewrite `dep-check` with `esbuild` and `babel` (#32845)

--- build-system/babel-config/dep-check-config.js
+++ build-system/babel-config/dep-check-config.js
@@ -39,6 +39,9 @@ function getDepCheckConfig() {
     },
   ];
   const depCheckPlugins = [
+    './build-system/babel-plugins/babel-plugin-transform-json-import',
+    './build-system/babel-plugins/babel-plugin-transform-json-configuration',
+    './build-system/babel-plugins/babel-plugin-transform-jss',
     './build-system/babel-plugins/babel-plugin-transform-fix-leading-comments',
     './build-system/babel-plugins/babel-plugin-transform-promise-resolve',
     '@babel/plugin-transform-react-constant-elements',
