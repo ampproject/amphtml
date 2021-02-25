9a37826fed423c0317e3d43d5737404824c2bed3
Ya-Chen Hsieh <1697814+ychsieh@users.noreply.github.com>
2021-02-25 23:36:07 +0800

    ✨  Assist.js: Add a shared FrameService to assistjs extension (#32784)
    * Create the skeleton of the new extension for assist.js: amp-google-assistant-assistjs. It consists of one shared service and multiple custom elements.
    
    * Remove unneeded regenerator-
    runtime dependency in package.json
    
    * - Add/modify copyright statement and header comment for each file
    - Add appropriate validator rules for the new extension
    - Add more detailed description about what this extension does
    
    * Add a shared service(AssistjsConfigService) and a new config element. Also includes some minor refactoring.
    
    The service provides config to other components and the element take prespecified config json.
    
    * Fix styling and remove commented code in AssistjsFrameService.
    
    * Improve config service:
    - Ensure config json retrieval
    - Move widget iframe url creation into one single place.
    - Add assertion to ensure there's only one config element on the page
    
    Minor fixes:
    - Replace await usage with then to avoid extra dependencies.
    - Update validator rules for the invisible config element
    
    * Fix prettify errors.
    
    * Unit test fix and some minor fixes.
    
    * Fix lint errors.
    
    * Update extensions/amp-google-assistant-assistjs/0.1/assistjs-frame-service.js
    
    Co-authored-by: Justin Ridgewell <justin@ridgewell.name>
    
    * Minor fixes.
    
    * Fix errors caused by not having this.win in a service class(non-AMP.BaseElement).
    Also add a config service mock to the unit test.
    
    * ampdoc.getWin() is forbidden. Replace it with "window" directly.
    
    * - Append iframe to the document after frame src is retrieved
    - Parse config in config element to save a DOM query
    - Delete assistjs-frame-service.js. Add it in next PR with real usage.
    
    * Fix FrameService reference errors and lint errors.
    
    * Add a shared FrameService for all custom elements to handle requests from underlying iframes.
    Actual implementation of two endpoints: OpenMic and SendTextQuery is empty now and will be implemented in upcoming PRs.
    
    * Use ampdoc.whenready instead to fix the loading error.
    Add sandbox attributes to all iframes.
    
    * Update extensions/amp-google-assistant-assistjs/0.1/assistjs-frame-service.js
    
    Co-authored-by: Justin Ridgewell <justin@ridgewell.name>
    
    * Update extensions/amp-google-assistant-assistjs/0.1/assistjs-frame-service.js
    
    Co-authored-by: Justin Ridgewell <justin@ridgewell.name>
    
    Co-authored-by: Justin Ridgewell <justin@ridgewell.name>

--- extensions/amp-google-assistant-assistjs/0.1/amp-google-assistant-inline-suggestion-bar.js
+++ extensions/amp-google-assistant-assistjs/0.1/amp-google-assistant-inline-suggestion-bar.js
@@ -30,11 +30,15 @@ export class AmpGoogleAssistantInlineSuggestionBar extends AMP.BaseElement {
 
     /** @private {?AssistjsConfigService} */
     this.configService_ = null;
+
+    /** @private {?AssistjsFrameService} */
+    this.frameService_ = null;
   }
 
   /** @override */
   buildCallback() {
     this.configService_ = Services.assistjsConfigServiceForDoc(this.element);
+    this.frameService_ = Services.assistjsFrameServiceForDoc(this.element);
   }
 
   /** @override */
@@ -51,6 +55,7 @@ export class AmpGoogleAssistantInlineSuggestionBar extends AMP.BaseElement {
       .then((iframeUrl) => {
         addAttributesToElement(iframe, {
           src: iframeUrl,
+          sandbox: 'allow-scripts',
         });
 
         // applyFillContent so that frame covers the entire component.
@@ -58,6 +63,11 @@ export class AmpGoogleAssistantInlineSuggestionBar extends AMP.BaseElement {
 
         this.element.appendChild(iframe);
       });
+
+    iframe.addEventListener('load', () => {
+      // TODO: create a channel to receive requests from underlying assist.js iframe.
+      this.frameService_.sendTextQuery();
+    });
 
     // Return a load promise for the frame so the runtime knows when the
     // component is ready.
