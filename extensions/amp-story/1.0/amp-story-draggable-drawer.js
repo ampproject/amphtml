2ce204b70ae46c9493f35e94ca93fabb6a838b8e
Matias Szylkowski <mszylkowski@google.com>
2021-02-26 16:45:56 -0500

    ♿  [Story a11y] Improve tabbing on social-share and bookend (#32859)
    * Started updating tabindex
    
    * More work
    
    * Fixed share icons on start
    
    * Removed console logs
    
    * Use visibility CSS
    
    * Reverted original code
    
    * Visible -> initial
    
    * Added comment and tabbable get link
    
    * Using root container for visibility
    
    * Add visibility on drawer when dragging
    
    * Bring back supports landscape
    
    * i18n of get link
    
    * Added comment for a11y on visibility inherit
    
    * Added comment again
    
    * Fixed rendering of bot
    
    * Added comment to trigger on enter or space

--- extensions/amp-story/1.0/amp-story-draggable-drawer.js
+++ extensions/amp-story/1.0/amp-story-draggable-drawer.js
@@ -484,6 +484,7 @@ export class DraggableDrawer extends AMP.BaseElement {
       setImportantStyles(this.element, {
         transform: translate,
         transition: 'none',
+        visibility: 'visible',
       });
     });
   }
@@ -503,7 +504,7 @@ export class DraggableDrawer extends AMP.BaseElement {
 
     this.mutateElement(() => {
       this.element.setAttribute('aria-hidden', false);
-      resetStyles(this.element, ['transform', 'transition']);
+      resetStyles(this.element, ['transform', 'transition', 'visibility']);
 
       if (!shouldAnimate) {
         // Resets the 'transition' property, and removes this override in the
