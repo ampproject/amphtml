239c387cf8131fb7dc0aac99b8169b6158dc3aa1
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-02-26 10:38:32 -0800

    [`amp-minute-media-player`] Rename `utils` to `dom` in imports.

--- extensions/amp-minute-media-player/0.1/test/test-amp-minute-media-player.js
+++ extensions/amp-minute-media-player/0.1/test/test-amp-minute-media-player.js
@@ -15,7 +15,7 @@
  */
 
 import '../amp-minute-media-player';
-import * as utils from '../../../../src/dom';
+import * as dom from '../../../../src/dom';
 
 const WIDTH = '16';
 const HEIGHT = '9';
@@ -161,13 +161,13 @@ describes.realWin(
       });
 
       it('can enter fullscreen', () => {
-        const spy = env.sandbox.spy(utils, 'fullscreenEnter');
+        const spy = env.sandbox.spy(dom, 'fullscreenEnter');
         impl.fullscreenEnter();
         expect(spy).calledWith(impl.iframe_);
       });
 
       it('can exit fullscreen', () => {
-        const spy = env.sandbox.spy(utils, 'fullscreenExit');
+        const spy = env.sandbox.spy(dom, 'fullscreenExit');
         impl.fullscreenExit();
         expect(spy).calledWith(impl.iframe_);
         expect(impl.isFullscreen()).to.be.false;
