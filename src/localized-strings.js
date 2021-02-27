f76e60a0a04a9c1800f53dbd0a7b00c446577c10
nickFalcone <nfalcone@u.rochester.edu>
2021-02-26 23:02:52 -0500

    Create string for 360 gyroscope experience prompt

--- src/localized-strings.js
+++ src/localized-strings.js
@@ -24,7 +24,7 @@ import {parseJson} from './json';
  *   - NOT be reused; to deprecate an ID, comment it out and prefix its key with
  *     the string "DEPRECATED_"
  *
- * Next ID: 90
+ * Next ID: 91
  *
  * @const @enum {string}
  */
@@ -43,6 +43,7 @@ export const LocalizedStringId = {
   AMP_STORY_CONSENT_ACCEPT_BUTTON_LABEL: '22',
   AMP_STORY_CONSENT_DECLINE_BUTTON_LABEL: '23',
   AMP_STORY_CONTINUE_ANYWAY_BUTTON_LABEL: '27',
+  AMP_STORY_DISCOVERY_DIALOG_TEXT: '90',
   AMP_STORY_DOMAIN_DIALOG_HEADING_LABEL: '25',
   AMP_STORY_DOMAIN_DIALOG_HEADING_LINK: '26',
   AMP_STORY_EDUCATION_NAVIGATION_SWIPE_PROGRESS: '78',
