61a69d154dde5a8dc846f2c48e7f2cdb90600b7e
ed <ed.brandmark@verizonmedia.com>
2021-03-02 10:27:02 -0500

    Update verizonmedia.js
    suggested updates

--- ads/vendors/verizonmedia.js
+++ ads/vendors/verizonmedia.js
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-import {
-  writeScript,
-  validateData,
-} from '../../3p/3p';
-
+import {validateData, writeScript} from '../../3p/3p';
+/**
+ * @param {!Window} global
+ * @param {{config: string}} data
+ */
 export function verizonmedia(global, data) {
-  validateData(data,['config']);
+  validateData(data, ['config']);
   global.jacData = data;
   writeScript(global, 'https://jac.yahoosandbox.com/amp/jac.js');
 }
