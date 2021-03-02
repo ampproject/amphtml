bc95eb80292531306ed171e1e4f25cfcc164be7f
Caroline Liu <carolineliu@google.com>
2021-03-02 13:45:17 -0500

    || preferred to ?? for NaN

--- extensions/amp-base-carousel/0.1/carousel.js
+++ extensions/amp-base-carousel/0.1/carousel.js
@@ -313,7 +313,7 @@ export class Carousel {
      * restingIndex to currentIndex.
      * @private {number}
      */
-    this.currentIndex_ = initialIndex ?? 0;
+    this.currentIndex_ = initialIndex || 0;
 
     /**
      * Whether or not looping is requested. Do not use directly, but rather use
