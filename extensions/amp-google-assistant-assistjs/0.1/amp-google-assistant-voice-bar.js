15696456f41be80b7496f3f0717c91034494f4c9
Ya-Chen Hsieh <ychsieh@google.com>
2021-03-02 00:06:38 -0800

    Remove "allow-same-origin" value from sandbox attribute.

--- extensions/amp-google-assistant-assistjs/0.1/amp-google-assistant-voice-bar.js
+++ extensions/amp-google-assistant-assistjs/0.1/amp-google-assistant-voice-bar.js
@@ -57,7 +57,7 @@ export class AmpGoogleAssistantVoiceBar extends AMP.BaseElement {
     this.configService_.getWidgetIframeUrl('voicebar').then((iframeUrl) => {
       addAttributesToElement(iframe, {
         src: iframeUrl,
-        sandbox: 'allow-scripts allow-same-origin'
+        sandbox: 'allow-scripts'
       });
 
       // applyFillContent so that frame covers the entire component.
