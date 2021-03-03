b2b4bd88be86553f97bbf244ce9011111dd6e2b3
Raksha Muthukumar <raksha.muth@gmail.com>
2021-03-03 15:44:08 -0500

    create open-attachment helper class

--- extensions/amp-story/1.0/amp-story-page.js
+++ extensions/amp-story/1.0/amp-story-page.js
@@ -79,6 +79,7 @@ import {renderPageDescription} from './semantic-render';
 import {setTextBackgroundColor} from './utils';
 import {toArray} from '../../../src/types';
 import {upgradeBackgroundAudio} from './audio';
+import {renderPageAttachmentUI, buildOpenAttachmentElement} from './amp-story-open-page-attachment';
 
 /**
  * CSS class for an amp-story-page that indicates the entire page is loaded.
@@ -162,22 +163,6 @@ const buildErrorMessageElement = (element) =>
         <span class="i-amphtml-story-page-error-label"></span>
         <span class='i-amphtml-story-page-error-icon'></span>
       </div>`;
-
-/**
- * @param {!Element} element
- * @return {!Element}
- */
-const buildOpenAttachmentElement = (element) =>
-  htmlFor(element)`
-      <a class="
-          i-amphtml-story-page-open-attachment i-amphtml-story-system-reset"
-          role="button">
-        <span class="i-amphtml-story-page-open-attachment-icon">
-          <span class="i-amphtml-story-page-open-attachment-bar-left"></span>
-          <span class="i-amphtml-story-page-open-attachment-bar-right"></span>
-        </span>
-        <span class="i-amphtml-story-page-open-attachment-label"></span>
-      </a>`;
 
 /**
  * amp-story-page states.
@@ -1761,28 +1746,13 @@ export class AmpStoryPage extends AMP.BaseElement {
 
     if (!this.openAttachmentEl_) {
       this.openAttachmentEl_ = buildOpenAttachmentElement(this.element);
-      // If the attachment is a link, copy href to the element so it can be previewed on hover and long press.
-      const attachmentHref = attachmentEl.getAttribute('href');
-      if (attachmentHref) {
-        this.openAttachmentEl_.setAttribute('href', attachmentHref);
-      }
       this.openAttachmentEl_.addEventListener('click', () =>
         this.openAttachment()
       );
 
-      const textEl = this.openAttachmentEl_.querySelector(
-        '.i-amphtml-story-page-open-attachment-label'
-      );
-
-      const openLabelAttr = attachmentEl.getAttribute('data-cta-text');
-      const openLabel =
-        (openLabelAttr && openLabelAttr.trim()) ||
-        getLocalizationService(this.element).getLocalizedString(
-          LocalizedStringId.AMP_STORY_PAGE_ATTACHMENT_OPEN_LABEL
-        );
+      renderPageAttachmentUI(this.element, attachmentEl, this.openAttachmentEl_);
 
       this.mutateElement(() => {
-        textEl.textContent = openLabel;
         this.element.appendChild(this.openAttachmentEl_);
       });
     }
