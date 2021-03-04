b046c6bbbf9159a1a3ca0978df54cf1938e84e5f
processprocess <philip.hunter.bell@gmail.com>
2021-03-04 12:59:55 -0500

    Implementation sketch.

--- extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
@@ -125,6 +125,37 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
       this.element_.querySelector('amp-img')
     );
 
+<<<<<<< HEAD
+=======
+    let x = 0;
+    const xAttr = this.element_.getAttribute('x');
+    if (xAttr === 'left') {
+      // get half of container
+      // const containerHalfWidth = this.element.offsetWidth / 2;
+      // const imageHalfWidth = this.ampImgEl_.getAttribute('width') / 2;
+      // console.log(imageHalfWidth);
+      // percentage value
+
+      const containerHeight = this.element.offsetHeight;
+      const containerWidth = this.element.offsetWidth;
+      const ampImgHeight = this.ampImgEl_.getAttribute('height');
+      const ampImgWidth = this.ampImgEl_.getAttribute('width');
+      const scaledFraction = containerHeight / ampImgHeight;
+      const scaledImageWidth = scaledFraction * ampImgWidth;
+
+      x =
+        ((scaledImageWidth / 2 - containerWidth / 2) / scaledImageWidth) * 100;
+    } else {
+      x = parseFloat(xAttr) || 0;
+    }
+
+    this.animateTo_ = {
+      x,
+      y: parseFloat(this.element_.getAttribute('y')) || 0,
+      zoom: parseFloat(this.element_.getAttribute('zoom')) || 1,
+    };
+
+>>>>>>> c43c508f5 (Implementation sketch.)
     this.groupId_ =
       this.element_.getAttribute('group-id') ||
       this.ampImgEl_.getAttribute('src');
