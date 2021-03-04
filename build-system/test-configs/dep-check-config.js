3a6109be734d58723eaf2ea1a2aff6b8621fb8f6
thuyhq <61451682+thuyhq@users.noreply.github.com>
2021-03-05 00:21:41 +0700

    ✨ Fast fetch implementation for Valueimpression network (#32061)
    * Add new Valueimpression fast fetch implementation
    
    * Fix code style and copyright year
    
    * Edit build-system/test-configs/dep-check-config.js
    
    * Fix Travis CI error code style
    
    * Lint build-system/compile/bundles.config.extensions.json
    
    * Restore build-system/compile/bundles.config.extensions.json
    
    * Edit amp-ad-network-valueimpression-impl.js remove document.referrer
    
    * Edit build-system/test-configs/dep-check-config.js
    
    * Change rtc-config endpoint
    
    * Format code
    
    * revert wrong change build-system/compile/bundles.config.extensions.json
    
    * fix lint error
    
    * Update amp-ad-network-valueimpression-impl-internal.md

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -197,6 +197,7 @@ exports.rules = [
       'extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js->extensions/amp-a4a/0.1/amp-a4a.js',
       'extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js->extensions/amp-a4a/0.1/amp-a4a.js',
       'extensions/amp-ad-network-oblivki-impl/0.1/amp-ad-network-oblivki-impl.js->extensions/amp-a4a/0.1/amp-a4a.js',
+      'extensions/amp-ad-network-valueimpression-impl/0.1/amp-ad-network-valueimpression-impl.js->extensions/amp-a4a/0.1/amp-a4a.js',
 
       // A4A impls importing amp fast fetch header name
       'extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js->extensions/amp-a4a/0.1/signature-verifier.js',
@@ -211,6 +212,7 @@ exports.rules = [
       'extensions/amp-ad-network-adzerk-impl/0.1/amp-ad-network-adzerk-impl.js->extensions/amp-a4a/0.1/amp-ad-template-helper.js',
       'extensions/amp-ad-network-adzerk-impl/0.1/amp-ad-network-adzerk-impl.js->extensions/amp-a4a/0.1/amp-ad-type-defs.js',
       'extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js->extensions/amp-a4a/0.1/refresh-manager.js',
+      'extensions/amp-ad-network-valueimpression-impl/0.1/amp-ad-network-valueimpression-impl.js->extensions/amp-a4a/0.1/refresh-manager.js',
 
       // AMP access depends on AMP access
       'extensions/amp-access-scroll/0.1/scroll-impl.js->extensions/amp-access/0.1/amp-access-client.js',
@@ -426,6 +428,7 @@ exports.rules = [
       // Ads remote config manager
       'extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js->src/service/real-time-config/callout-vendors.js',
       'extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js->src/service/real-time-config/real-time-config-impl.js',
+      'extensions/amp-ad-network-valueimpression-impl/0.1/amp-ad-network-valueimpression-impl.js->src/service/real-time-config/real-time-config-impl.js',
     ],
   },
   {
