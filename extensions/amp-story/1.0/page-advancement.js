aa7a06eb86afc996ecb888e25bd50822d6fe8ea5
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2021-03-04 15:58:21 -0500

    Revert "[amp-story] Prevent clicks on top 80% (#32518)" (#33079)
    This reverts commit daa95ee61db5add37fbbfb714aa1ef28ca54e13e.
    
    (cherry picked from commit e8f2e37e22e1b789cd4c69dc3c7d7f15b83bc7da)

--- extensions/amp-story/1.0/page-advancement.js
+++ extensions/amp-story/1.0/page-advancement.js
@@ -440,12 +440,11 @@ export class ManualAdvancement extends AdvancementConfig {
    * We want clicks on certain elements to be exempted from normal page
    * navigation
    * @param {!Event} event
-   * @param {!ClientRect} pageRect
    * @return {boolean}
    * @private
    */
-  isBottomCtaTappableTarget_(event, pageRect) {
-    const target = closest(
+  isProtectedTarget_(event) {
+    return !!closest(
       dev().assertElement(event.target),
       (el) => {
         const elementRole = el.getAttribute('role');
@@ -457,8 +456,6 @@ export class ManualAdvancement extends AdvancementConfig {
       },
       /* opt_stopAt */ this.element_
     );
-
-    return !!target && this.isInScreenBottom_(target, pageRect);
   }
 
   /**
@@ -711,7 +708,7 @@ export class ManualAdvancement extends AdvancementConfig {
     if (
       !this.isRunning() ||
       !this.isNavigationalClick_(event) ||
-      this.isBottomCtaTappableTarget_(event, pageRect) ||
+      this.isProtectedTarget_(event) ||
       !this.shouldHandleEvent_(event)
     ) {
       // If the system doesn't need to handle this click, then we can simply
@@ -720,7 +717,6 @@ export class ManualAdvancement extends AdvancementConfig {
     }
 
     event.stopPropagation();
-    event.preventDefault();
 
     this.storeService_.dispatch(
       Action.SET_ADVANCEMENT_MODE,
