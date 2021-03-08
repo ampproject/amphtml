36fbf1873d48c4dda60e2175a7f772daeb39c40e
Ryan Cebulko <ryan@cebulko.com>
2021-03-08 17:53:21 -0500

    Fix my bork

--- src/core/mode/dev-mode.js
+++ src/core/mode/dev-mode.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {isTestingMode} from './mode';
+import {isTestingMode} from './testing-mode';
 import {parseQueryString} from '../url';
 
 /**
