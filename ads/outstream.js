2753ef7d2dda76cff93eed7f73cca8fee73d1e0c
Marc Mulvidson <marc@digiteka.com>
2021-03-09 09:50:53 +0100

    remove useless file

--- ads/outstream.js
+++
@@ -1,3 +0,0 @@
-document.write('<script type="text/javascript">ULTIMEDIAOUTSTREAM_target = "ultimedia-outstream-player";ULTIMEDIAOUTSTREAM_mdtk = "01540779";ULTIMEDIAOUTSTREAM_zone = 1;</script>');
-document.write('<script src="https://ot.digiteka.com/dtk-outstreamplayer-master.js" async type="text/javascript"></script>');
-document.write('<div id="ultimedia-outstream-player"></div>');
