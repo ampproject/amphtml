a00e3e650bbce55b99c9638aa9c24f6ef47adf30
Caroline Liu <carolineliu@google.com>
2021-03-15 16:51:39 -0400

    Remove scrollable

--- extensions/amp-lightbox/1.0/component.jss.js
+++ extensions/amp-lightbox/1.0/component.jss.js
@@ -46,31 +46,21 @@ const wrapper = {
   height: '100%',
   position: 'fixed',
   boxSizing: 'border-box',
-};
 
-// User overridable styles
-const defaultStyles = {
+  // User overridable styles
   zIndex: 1000,
   backgroundColor: 'rgba(0, 0, 0, 0.9)',
   color: '#fff',
 };
 
-/* 
-  overflow: hidden does not trigger overscrollBehavior, so 
-  overflow: auto is applied even though the lightbox should not scroll.
-  We rely instead on the overflow: hidden in ContainWrapper to prevent 
-  scrolling inside the lightbox.
-*/
-const containScroll = {
-  overflow: 'auto', // Prevent scrolling inside lightbox.
-  overscrollBehavior: 'none', // Prevent scrolling outside lightbox.
+const content = {
+  overflow: 'auto !important',
 };
 
 const JSS = {
   closeButton,
   wrapper,
-  defaultStyles,
-  containScroll,
+  content,
 };
 
 // useStyles gets replaced for AMP builds via `babel-plugin-transform-jss`.
