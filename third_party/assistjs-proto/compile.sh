a89006133038ff5176e6cd86bdbbdbcb3db08056
Ya-Chen Hsieh <ychsieh@google.com>
2021-03-16 09:50:36 -0700

    Update protoc from protobug Github repo.

--- third_party/assistjs-proto/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# doc: https://developers.google.com/protocol-buffers/docs/reference/javascript-generated#commonjs-imports
-
-PROTOC=protoc/bin/protoc
-
-$PROTOC --js_out=import_style=commonjs,binary:. frame-service.proto
