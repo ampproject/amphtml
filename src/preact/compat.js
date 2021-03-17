ac3d0631fe487982b07b21590f87358703391483
Kevin Dwan <25626770+krdwan@users.noreply.github.com>
2021-03-17 16:21:54 -0400

    🏗 [bento] Add portals to preact compat (#33213)
    * Add portals to preact compat
    
    * Update container type per review comments

--- src/preact/compat.js
+++ src/preact/compat.js
@@ -24,3 +24,12 @@ import * as compat from /*OK*/ 'preact/compat';
 export function forwardRef(fn) {
   return compat.forwardRef(fn);
 }
+
+/**
+ * @param {PreactDef.VNode} vnode
+ * @param {HTMLElement} container
+ * @return {PreactDef.VNode}
+ */
+export function createPortal(vnode, container) {
+  return compat.createPortal(vnode, container);
+}
