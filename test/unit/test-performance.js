b35274f92e7d9bfe299989037a11cb9353b6fd74
Jake Fried <samouri@users.noreply.github.com>
2021-03-16 20:16:51 -0400

    performance-impl: use fcp and ofv entryTime when summing for cls-fcp and cls-ofv (#33295)
    * performance-impl: ensure cls and cls-fcp are ticked at the same time.
    
    * performance-impl: use event times rather to ensure cls-ofv and cls-fcp are accurate
    
    * fix tests + add a new one
    
    * better fallback
    
    * prevent unbounded growth

--- test/unit/test-performance.js
+++ test/unit/test-performance.js
@@ -300,7 +300,7 @@ describes.realWin('performance', {amp: true}, (env) => {
           ]).then(() => {
             expect(flushSpy).to.have.callCount(4);
             expect(perf.isMessagingReady_).to.be.false;
-            const count = 6;
+            const count = 5;
             expect(perf.events_.length).to.equal(count);
           });
         });
@@ -653,15 +653,13 @@ describes.realWin('performance', {amp: true}, (env) => {
           const initialCount = tickSpy.callCount;
           return ampdoc.whenFirstVisible().then(() => {
             clock.tick(400);
-            // TODO(#33207): reduce call counts by 1 after data collection
-            expect(tickSpy).to.have.callCount(initialCount + 2);
-            expect(tickSpy.withArgs('cls-ofv')).to.be.calledOnce;
+            expect(tickSpy).to.have.callCount(initialCount + 1);
             whenViewportLayoutCompleteResolve();
             return perf.whenViewportLayoutComplete_().then(() => {
-              expect(tickSpy).to.have.callCount(initialCount + 2);
+              expect(tickSpy).to.have.callCount(initialCount + 1);
               expect(tickSpy.withArgs('ofv')).to.be.calledOnce;
               return whenFirstVisiblePromise.then(() => {
-                expect(tickSpy).to.have.callCount(initialCount + 3);
+                expect(tickSpy).to.have.callCount(initialCount + 2);
                 expect(tickSpy.withArgs('pc')).to.be.calledOnce;
                 expect(Number(tickSpy.withArgs('pc').args[0][1])).to.equal(400);
               });
@@ -851,6 +849,8 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
   let windowEventListeners;
   let performanceObserver;
   let viewerVisibilityState;
+  let whenFirstVisiblePromise;
+  let whenFirstVisibleResolve;
 
   function setupFakesForVisibilityStateManipulation() {
     // Fake window to fake `document.visibilityState`.
@@ -900,12 +900,15 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
     // Install services on fakeWin so some behaviors can be stubbed.
     installRuntimeServices(fakeWin);
 
+    whenFirstVisiblePromise = new Promise((resolve) => {
+      whenFirstVisibleResolve = resolve;
+    });
     const unresolvedPromise = new Promise(() => {});
     const viewportSize = {width: 0, height: 0};
     env.sandbox.stub(Services, 'ampdoc').returns({
       hasBeenVisible: () => {},
       onVisibilityChanged: () => {},
-      whenFirstVisible: () => unresolvedPromise,
+      whenFirstVisible: () => whenFirstVisiblePromise,
       getVisibilityState: () => viewerVisibilityState,
       getFirstVisibleTime: () => 0,
       isSingleDoc: () => true,
@@ -967,7 +970,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
 
       const perf = Services.performanceFor(env.win);
 
-      expect(perf.events_.length).to.equal(4);
+      expect(perf.events_.length).to.equal(3);
       expect(perf.events_[0]).to.be.jsonEqual(
         {
           label: 'fp',
@@ -979,10 +982,6 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
         },
         {
           label: 'fcpv',
-          delta: 15,
-        },
-        {
-          label: 'cls-fcp',
           delta: 15,
         }
       );
@@ -1033,7 +1032,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
       };
       // Fake a triggering of the first-input event.
       performanceObserver.triggerCallback(list);
-      expect(perf.events_.length).to.equal(4);
+      expect(perf.events_.length).to.equal(3);
       expect(perf.events_[0]).to.be.jsonEqual(
         {
           label: 'fp',
@@ -1045,10 +1044,6 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
         },
         {
           label: 'fcpv',
-          delta: 15,
-        },
-        {
-          label: 'cls-fcp',
           delta: 15,
         }
       );
@@ -1223,6 +1218,82 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
       (windowEventListeners[eventName] || []).forEach((cb) => cb(event));
     }
 
+    // TODO(#33207): Remove after data collection
+    it('Forwards cls-fcp and cls-ofv', async () => {
+      fakeWin.PerformanceObserver.supportedEntryTypes = [
+        'layout-shift',
+        'paint',
+      ];
+      fakeWin.document.visibilityState = 'hidden';
+
+      // Document should be initially hidden.
+      expect(fakeWin.document.visibilityState).to.equal('hidden');
+
+      // visibilitychange/beforeunload listeners are now added.
+      const perf = getPerformance();
+      perf.coreServicesAvailable();
+
+      // Pre-visible and pre-fcp layout shifts
+      performanceObserver.triggerCallback({
+        getEntries() {
+          return [
+            {
+              entryType: 'layout-shift',
+              value: 0.25,
+              hadRecentInput: false,
+              startTime: 0,
+            },
+            {
+              entryType: 'layout-shift',
+              value: 0.25,
+              hadRecentInput: false,
+              startTime: 50,
+            },
+          ];
+        },
+      });
+
+      whenFirstVisibleResolve();
+      await new Promise(setTimeout);
+      toggleVisibility(fakeWin, true);
+
+      // Post visible, pre-fcp layout-shift
+      performanceObserver.triggerCallback({
+        getEntries() {
+          return [
+            {
+              entryType: 'layout-shift',
+              value: 0.5,
+              hadRecentInput: false,
+              startTime: 100,
+            },
+            {
+              entryType: 'paint',
+              name: 'first-contentful-paint',
+              startTime: 150,
+              duration: 0,
+            },
+            {
+              entryType: 'layout-shift',
+              value: 0.5,
+              hadRecentInput: false,
+              startTime: 200,
+            },
+          ];
+        },
+      });
+
+      toggleVisibility(fakeWin, false);
+      const clsEvents = perf.events_.filter((event) =>
+        event.label.startsWith('cls')
+      );
+      expect(clsEvents).jsonEqual([
+        {label: 'cls-ofv', delta: 0.5},
+        {label: 'cls-fcp', delta: 1},
+        {label: 'cls', delta: 1.5},
+      ]);
+    });
+
     it('for Chromium 77', () => {
       // Specify an Android Chrome user agent, which supports the
       // visibilitychange event.
@@ -1258,7 +1329,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
       toggleVisibility(fakeWin, false);
       let clsEvents = perf.events_.filter((event) => event.label === 'cls');
       expect(clsEvents.length).equal(1);
-      expect(perf.events_[0]).to.be.jsonEqual({
+      expect(perf.events_).deep.includes({
         label: 'cls',
         delta: 0.55,
       });
@@ -1285,7 +1356,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
 
       toggleVisibility(fakeWin, false);
       clsEvents = perf.events_.filter((event) => event.label.startsWith('cls'));
-      expect(clsEvents.length).to.equal(2);
+      expect(clsEvents.length).to.equal(4);
       expect(clsEvents).to.deep.include({
         label: 'cls-2',
         delta: 1.5501,
@@ -1301,7 +1372,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
 
       toggleVisibility(fakeWin, false);
       clsEvents = perf.events_.filter((event) => event.label.startsWith('cls'));
-      expect(clsEvents.length).to.equal(2);
+      expect(clsEvents.length).to.equal(4);
     });
 
     it('when the viewer visibility changes to inactive', () => {
@@ -1336,7 +1407,7 @@ describes.realWin('PeformanceObserver metrics', {amp: true}, (env) => {
       const clsEvents = perf.events_.filter((evt) =>
         evt.label.startsWith('cls')
       );
-      expect(clsEvents.length).to.equal(1);
+      expect(clsEvents.length).to.equal(3);
       expect(perf.events_).deep.include({
         label: 'cls',
         delta: 0.55,
