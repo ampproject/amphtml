3344eee66e16bda43d8fc3f31b2ed2ece6a52767
Jake Fried <samouri@users.noreply.github.com>
2021-03-18 15:55:57 -0400

    esbuild cache invalidation: add argv to optionsHash (#33366)

--- build-system/common/esbuild-babel.js
+++ build-system/common/esbuild-babel.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+const argv = require('minimist')(process.argv.slice(2));
 const babel = require('@babel/core');
 const crypto = require('crypto');
 const fs = require('fs-extra');
@@ -167,7 +168,7 @@ function getEsbuildBabelPlugin(
 
       const babelOptions =
         babel.loadOptions({caller: {name: callerName}}) || {};
-      const optionsHash = md5(babelOptions);
+      const optionsHash = md5({babelOptions, argv});
 
       build.onLoad({filter: /\.[cm]?js$/, namespace: ''}, async (file) => {
         const filename = file.path;
