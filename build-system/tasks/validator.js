9087e499feee7d5644a42104071b4dd652d53ef1
Boxiao Cao <caoboxiao@gmail.com>
2021-03-19 21:15:20 +0000

    Add description to validatorCpp

--- build-system/tasks/validator.js
+++ build-system/tasks/validator.js
@@ -64,6 +64,9 @@ validator.flags = {
   'update_tests': 'Updates validation test output files',
 };
 
+validator.description = 'Builds and tests the AMP C++ validator.'
+// TODO(antiphoton): Add the ability to update validation test output files.
+
 validatorWebui.description = 'Builds and tests the AMP validator web UI.';
 validatorWebui.flags = {
   'update_tests': 'Updates validation test output files',
