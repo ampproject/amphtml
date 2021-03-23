c54c44c2cb753d1e477a78156e2d733c704e5d28
Boxiao Cao <caoboxiao@gmail.com>
2021-03-23 17:44:52 +0000

    Reorder tasks in validator tests

--- build-system/pr-check/validator-tests.js
+++ build-system/pr-check/validator-tests.js
@@ -46,16 +46,16 @@ function prBuildWorkflow() {
     return;
   }
 
-  if (buildTargetsInclude(Targets.VALIDATOR)) {
-    timedExecOrDie('amp validator-cpp');
+  if (buildTargetsInclude(Targets.VALIDATOR_WEBUI)) {
+    timedExecOrDie('amp validator-webui');
   }
 
   if (buildTargetsInclude(Targets.RUNTIME, Targets.VALIDATOR)) {
     timedExecOrDie('amp validator');
   }
 
-  if (buildTargetsInclude(Targets.VALIDATOR_WEBUI)) {
-    timedExecOrDie('amp validator-webui');
+  if (buildTargetsInclude(Targets.VALIDATOR)) {
+    timedExecOrDie('amp validator-cpp');
   }
 }
 
