addd4877332bf434898a5c9c1b5fbaae60827533
Philip Bell <philip.hunter.bell@gmail.com>
2021-03-23 12:39:18 -0400

    ✨ [Panning media] Add lock-bounds attribute (#33197)
    * Implementation sketch.
    
    * Remove implementaion comments.
    
    * Factor in zoom.
    
    * Test with left and right
    
    * Calculate X.
    
    * calc y
    
    * Update example. Only calculate if lock-bounds.
    
    * Tabbing in html template.
    
    * Add comment for percentScaled
    
    * css spaces.
    
    * Clamp values if lock-bounds.
    
    * Add comment.
    
    * Handle page resize
    
    * maxbounds method and variable. Tests.
    
    * Private variable
    
    * template css update
    
    * Update comment.
    
    * Const for distance to center edge.
    
    * Get page size from store
    
    * lint
    
    * rebase
    
    * Implementation sketch.
    
    * Remove implementaion comments.
    
    * Factor in zoom.
    
    * Test with left and right
    
    * Calculate X.
    
    * calc y
    
    * Update example. Only calculate if lock-bounds.
    
    * Tabbing in html template.
    
    * Add comment for percentScaled
    
    * css spaces.
    
    * Clamp values if lock-bounds.
    
    * Add comment.
    
    * Handle page resize
    
    * maxbounds method and variable. Tests.
    
    * Private variable
    
    * template css update
    
    * Update comment.
    
    * Const for distance to center edge.
    
    * Get page size from store
    
    * lint
    
    * fix merge conflicts.
    
    * Template
    
    * Update comment.
    
    * Revert template change.
    
    * Update template
    
    * Update tests
    
    * Optional chaining on matches.
    
    * Set page size in beforEach
    
    * Update extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
    
    Co-authored-by: Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
    
    * Update typedef name. Add percentage.
    
    * lock-bounds codepath.
    
    * Revise comment.
    
    * call animate directly
    
    * Remove private variable for maxBounds.
    
    * Throw error if no height or width and using lock-bounds.
    
    * Read transform in tests.
    
    * Add TAG to user error.
    
    * Basic readme.
    
    * Revert readme
    
    * Calculation refactor.
    
    * Rename percent scaled variable in getmaxbounds.
    
    Co-authored-by: Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>

--- extensions/amp-story-panning-media/0.1/test/test-amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/test/test-amp-story-panning-media.js
@@ -85,6 +85,10 @@ describes.realWin(
       registerServiceBuilder(win, 'story-store', function () {
         return storeService;
       });
+      storeService.dispatch(Action.SET_PAGE_SIZE, {
+        width: 700,
+        height: 1000,
+      });
     });
 
     it('should build', async () => {
@@ -115,10 +119,29 @@ describes.realWin(
       await panningMedia.layoutCallback();
       await storeService.dispatch(Action.CHANGE_PAGE, {id: 'page1', index: 0});
       await afterRenderPromise();
-      expect(panningMedia.ampImgEl_.style.transform).to.equal(
+      expect(panningMedia.element.firstChild.style.transform).to.equal(
         `translate3d(${attributes.x}, ${attributes.y}, ${
           (attributes.zoom - 1) / attributes.zoom
         }px)`
+      );
+    });
+
+    it('calculates transform with lock-bounds', async () => {
+      const attributes = {
+        'group-id': 'group-1',
+        'y': '50%',
+        'zoom': 0.2,
+        'lock-bounds': '',
+      };
+      await createAmpStoryPanningMedia(
+        '/examples/amp-story/img/conservatory-coords.jpg',
+        attributes
+      );
+      await storeService.dispatch(Action.CHANGE_PAGE, {id: 'page1', index: 0});
+      await panningMedia.layoutCallback();
+      await afterRenderPromise();
+      expect(panningMedia.element.firstChild.style.transform).to.equal(
+        `translate3d(0%, 0%, 0px)`
       );
     });
   }
