efb709bb400569a5f71f705ec13dd117418fb657
Ryan Cebulko <ryan@cebulko.com>
2021-03-23 17:26:08 -0400

    Fix import as

--- src/preact/slot.js
+++ src/preact/slot.js
@@ -16,7 +16,7 @@
 
 import * as Preact from './index';
 import {CanPlay, CanRender, LoadingProp} from '../core/contextprops';
-import {devAssert} from '../core/assert';
+import {pureDevAssert as devAssert} from '../core/assert';
 import {rediscoverChildren, removeProp, setProp} from '../context';
 import {useAmpContext} from './context';
 import {useEffect, useLayoutEffect, useRef} from './index';
