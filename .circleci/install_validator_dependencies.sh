859e7146c17c1154742b18f9a35d28a71282c261
Boxiao Cao <caoboxiao@gmail.com>
2021-03-23 17:45:48 +0000

    No longer explicitly install curl and gnupg

--- .circleci/install_validator_dependencies.sh
+++ .circleci/install_validator_dependencies.sh
@@ -22,7 +22,6 @@ set -e
 GREEN() { echo -e "\033[0;32m$1\033[0m"; }
 
 echo $(GREEN "Adding Bazel repo...")
-sudo apt install curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
