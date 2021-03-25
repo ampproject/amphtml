d479db845f9d0ed26aea35b83a098bbe1d1d9111
Matias Szylkowski <mszylkowski@google.com>
2021-03-25 13:53:39 -0400

    ✨  [Story video] Run sortSources/load only if hasLowerBitrate (#33065)
    * Hide sortSources behind hasLowerBitrate
    
    * Fix call
    
    * Added test
    
    * Run flexible-bitrate
    
    * Updated codestyle for comments
    
    * Added test for not loading
    
    * Linted

--- extensions/amp-video/0.1/test/test-flexible-bitrate.js
+++ extensions/amp-video/0.1/test/test-flexible-bitrate.js
@@ -226,6 +226,26 @@ describes.fakeWin('amp-video flexible-bitrate', {}, (env) => {
         })
       ).to.jsonEqual([null, null, null, null, null, null, 'CACHE', null, null]);
     });
+
+    it("should sort sources only when it's not already sorted", () => {
+      const m = getManager('4g');
+      const v0 = getVideo([4000, 1000, 3000, 2000]);
+      m.manage(v0);
+
+      // Sorting once should work, but second time it should be a noop.
+      expect(m.sortSources_(v0)).to.be.true;
+      expect(m.sortSources_(v0)).to.be.false;
+    });
+
+    it('should not call load if there are no lower bitrates', () => {
+      const m = getManager('4g');
+      const v0 = getVideo([4000, 1000, 3000, 2000]);
+      m.manage(v0);
+      m.sortSources_(v0);
+      v0.load = env.sandbox.spy();
+      m.switchToLowerBitrate_(v0, m.acceptableBitrate_);
+      expect(v0.load).to.not.have.been.called;
+    });
   });
 
   function currentBitrates(video) {
