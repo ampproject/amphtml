5ea0759e71f3ab50b8d5a35d484536a017efc148
Justin Ridgewell <jridgewell@google.com>
2021-03-24 23:04:45 -0400

    Fix dist compilation

--- build-system/compile/sources.js
+++ build-system/compile/sources.js
@@ -65,6 +65,7 @@ const COMMON_GLOBS = [
   'node_modules/@ampproject/worker-dom/dist/amp-production/main.mjs',
   'node_modules/preact/package.json',
   'node_modules/preact/dist/*.js',
+  'node_modules/preact/dom/*.js',
   'node_modules/preact/hooks/package.json',
   'node_modules/preact/hooks/dist/*.js',
   'node_modules/preact/compat/package.json',
