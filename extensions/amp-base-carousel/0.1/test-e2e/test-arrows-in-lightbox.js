6ec171176cbea86d6795b0c2f4a863fd9f57bc3a
Micajuine Ho <micajuineho@google.com>
2021-03-30 14:20:17 -0700

    🐛 [amp-base-carousel 0.1] E2E tests for amp carousel arrows in lightbox (#33307)
    * init
    
    * init w/ tests
    
    * fix tests
    
    * Better tests
    
    * Base carousel e2e test
    
    * Remove everything but e2e
    
    * Update extensions/amp-carousel/0.2/test/test-e2e/test-arrows-in-lightbox.js
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>

---
+++ extensions/amp-base-carousel/0.1/test-e2e/test-arrows-in-lightbox.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {getNextArrow, getPrevArrow} from './helpers';
+
+describes.endtoend(
+  'amp base carousel in lightbox',
+  {
+    fixture: 'amp-base-carousel/arrows-in-lightbox.amp.html',
+    environments: ['single'],
+  },
+  async (env) => {
+    let controller;
+    let nextArrow;
+    let prevArrow;
+
+    beforeEach(async () => {
+      controller = env.controller;
+    });
+
+    it('should open with both arrows', async () => {
+      // Click on image 2
+      const secondImage = await controller.findElement('#second');
+      await controller.click(secondImage);
+
+      // Wait for lightbox to load the carousel and image
+      const lightbox = await controller.findElement('#lightbox1');
+      await expect(await controller.getElementProperty(lightbox, 'style')).to
+        .not.be.null;
+
+      // Both arrows should be showing
+      prevArrow = await getPrevArrow(controller);
+      nextArrow = await getNextArrow(controller);
+      await expect(
+        controller.getElementCssValue(prevArrow, 'opacity')
+      ).to.equal('1');
+      await expect(
+        controller.getElementCssValue(nextArrow, 'opacity')
+      ).to.equal('1');
+    });
+
+    it('should open with one arrow', async () => {
+      // Click on last image
+      const lastImage = await controller.findElement('#fourth');
+      await controller.click(lastImage);
+
+      // Wait for lightbox to load the carousel and image
+      const lightbox = await controller.findElement('#lightbox1');
+      await expect(await controller.getElementProperty(lightbox, 'style')).to
+        .not.be.null;
+
+      // Only prev arrow should be showing since non-looping carousel is on the last slide.
+      prevArrow = await getPrevArrow(controller);
+      nextArrow = await getNextArrow(controller);
+      await expect(
+        controller.getElementCssValue(prevArrow, 'opacity')
+      ).to.equal('1');
+      await expect(
+        controller.getElementCssValue(nextArrow, 'opacity')
+      ).to.equal('0');
+    });
+  }
+);
