3862c43d21fc53dd6b6d32c6e94e14338752c45d
Caroline Liu <carolineliu@google.com>
2021-04-05 11:05:59 -0400

    Add 3p-frame and iframe files to eslint allowlist

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -59,6 +59,9 @@ module.exports = {
         './context/prop.js',
         './context/scheduler.js',
         './context/values.js',
+        // TEMPORARY, follow tracking issue #33631
+        './preact/component/3p-frame.js',
+        './preact/component/iframe.js',
       ],
       'rules': {'import/no-restricted-paths': isCiBuild() ? 0 : 1},
     },
