85fa95fc9b995c9e2d52f26bdf7ae4b2f04b9bfb
Matias Szylkowski <mszylkowski@google.com>
2021-04-06 12:58:06 -0400

    Created e2e test

---
+++ extensions/amp-video/0.1/test-e2e/test-amp-video-flexible-bitrate.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describes.endtoend(
+  'amp-video flexible bitrate',
+  {
+    fixture: 'amp-video/videos-cdn.html',
+    environments: ['single'],
+  },
+  (env) => {
+    let controller;
+
+    beforeEach(async () => {
+      controller = env.controller;
+
+      await expect(controller.findElement('amp-story.i-amphtml-story-loaded'))
+        .to.exist;
+    });
+
+    it('should manage the first video in the story', async () => {
+      const videoEl = await controller.findElement('#video1 video');
+
+      await expect(controller.getElementProperty(videoEl, 'changedSources')).to
+        .not.be.undefined;
+    });
+
+    it('should not manage a video far away in the story', async () => {
+      const videoEl = await controller.findElement('#video4 video');
+
+      await expect(controller.getElementProperty(videoEl, 'changedSources')).to
+        .be.undefined;
+    });
+  }
+);
