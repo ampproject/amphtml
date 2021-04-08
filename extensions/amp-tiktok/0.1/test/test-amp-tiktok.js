4788934990634ab331e82db93fcd10bf9fec04fc
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-04-08 13:10:25 -0700

    ✨ Create `amp-tiktok` component. (#32651)
    * [`amp-tiktok`] Create amp-tiktok component with unit test.
    
    * [`amp-tiktok`] Add documentation in the `amp-tiktok.md`.
    
    * [`amp-tiktok`] Add CSS which sets the style of the child iframe element.
    
    * [`amp-tiktok`] Add CSS file to style the child iframe inside the tiktok
    component.
    
    * [`amp-tiktok`] Respond to PR comments.
    
    * [`amp-tiktok`] Further responses to comments.
    
    * [`amp-tiktok`] Continue responding to reviewer feedback.
    
    * [`amp-tiktok`] Add custom loader to Tiktok element.
    
    * [`amp-tiktok`] Update name handling and add a resize fallback for when
    no messages are received.
    
    * [`amp-tiktok`] Fix typo in amp-tiktok.md
    
    * [`amp-tiktok`] Updated package-lock file.
    
    * [`amp-tiktok`] Formated amp-tiktok.md throught prettier.
    
    * [`amp-tiktok`] Remove unused code from amp-tiktok.js.
    
    * [`amp-tiktok`] Respond to PR reviewer comments.
    
    * [`amp-tiktok`] Move fallback height to class level variable for clarity.
    
    * [`amp-tiktok`] Undo accidental commit of package-lock.json
    
    * [`amp-tiktok`] Add unit tests to verify that messages are being received
    from TikTok endpoint and that the fallback mechanism is working as
    expected.
    
    * Apply suggestions from code review
    
    Co-authored-by: CrystalOnScript <crystallambert@google.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: CrystalOnScript <crystallambert@google.com>
    
    * [`amp-tiktok`] Update documentation and unit tests.
    
    * [`amp-tiktok`] Update example TikTok's.
    
    * [`amp-tiktok`] Resolve error in unit test which was expecting a size
    change in the incorrect element.
    
    * [`amp-tiktok`] Resolve package lock error.
    
    * [`amp-tiktok`] Respond to reviewer comments, extract variables to const.
    
    * [`amp-tiktok`] Update tests to use a constant example videoId.
    
    * [`amp-tiktok`] Add copywright statement to amp-tiktok.css
    
    * [`amp-tiktok`] Remove unused imports.
    
    * [`amp-tiktok`] Remove dead links from amp-tiktok.md
    
    Co-authored-by: CrystalOnScript <crystallambert@google.com>

---
+++ extensions/amp-tiktok/0.1/test/test-amp-tiktok.js
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../amp-tiktok';
+import * as dom from '../../../../src/dom';
+import {computedStyle} from '../../../../src/style';
+import {isAmpElement} from '../../../../src/dom';
+
+const VIDEOID = '6948210747285441798';
+
+describes.realWin(
+  'amp-tiktok',
+  {
+    amp: {
+      extensions: ['amp-tiktok'],
+    },
+  },
+  (env) => {
+    let win;
+    let doc;
+    let createElementWithAttributes;
+
+    beforeEach(() => {
+      win = env.win;
+      doc = win.document;
+      createElementWithAttributes = dom.createElementWithAttributes;
+
+      env.sandbox
+        .stub(dom, 'createElementWithAttributes')
+        .callsFake((document, tagName, attributes) => {
+          if (tagName === 'iframe' && attributes.src) {
+            // Serve a blank page, since these tests don't require an actual page.
+            // hash # at the end so path is not affected by param concat
+            attributes.src = `http://localhost:${location.port}/test/fixtures/served/blank.html#${attributes.src}`;
+          }
+          return createElementWithAttributes(document, tagName, attributes);
+        });
+    });
+
+    async function getTiktok(attrs = {}) {
+      const tiktok = dom.createElementWithAttributes(
+        win.document,
+        'amp-tiktok',
+        {
+          layout: 'responsive',
+          width: '325px',
+          height: '730px',
+          ...attrs,
+        }
+      );
+      doc.body.appendChild(tiktok);
+      return tiktok
+        .buildInternal()
+        .then(() => {
+          return tiktok.layoutCallback();
+        })
+        .then(() => tiktok);
+    }
+
+    it('renders with videoId', async () => {
+      const player = await getTiktok({'data-src': VIDEOID});
+      const iframe = player.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+      expect(iframe.getAttribute('src')).to.contain(VIDEOID);
+      expect(iframe.getAttribute('src')).to.contain('en-US');
+    });
+
+    it('renders with videoId', async () => {
+      const videoSrc =
+        'https://www.tiktok.com/@scout2015/video/6948210747285441798';
+      const player = await getTiktok({'data-src': videoSrc});
+      const iframe = player.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+      expect(iframe.getAttribute('src')).to.contain(VIDEOID);
+      expect(iframe.getAttribute('src')).to.contain('en-US');
+    });
+
+    it('renders with videoId and locale', async () => {
+      const player = await getTiktok({
+        'data-src': VIDEOID,
+        'data-locale': 'fr-FR',
+      });
+      const iframe = player.querySelector('iframe');
+      expect(iframe).to.not.be.null;
+      expect(iframe.getAttribute('src')).to.contain(VIDEOID);
+      expect(iframe.getAttribute('src')).to.contain('fr-FR');
+    });
+
+    it('resizes using the fallback mechanism when no messages are received', async () => {
+      const player = await getTiktok({'data-src': VIDEOID});
+      const playerIframe = player.querySelector('iframe');
+      const impl = await player.getImpl(false);
+      env.sandbox.stub(impl, 'handleTiktokMessages_');
+
+      // Wait 1100ms for resize fallback to be invoked.
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 1100);
+      });
+
+      expect(computedStyle(win, playerIframe).height).to.equal('775.25px');
+    });
+
+    it('removes iframe after unlayoutCallback', async () => {
+      const player = await getTiktok({'data-src': VIDEOID});
+      const playerIframe = player.querySelector('iframe');
+      expect(playerIframe).to.not.be.null;
+
+      const impl = await player.getImpl(false);
+      impl.unlayoutCallback();
+      expect(player.querySelector('iframe')).to.be.null;
+      expect(impl.iframe_).to.be.null;
+    });
+  }
+);
