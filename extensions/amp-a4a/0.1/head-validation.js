a12b8d67f149e7b520a0e9f3b5564a63103f5ac8
Caleb Cordry <ccordry@google.com>
2021-04-09 10:07:17 -0700

    comments

--- extensions/amp-a4a/0.1/head-validation.js
+++ extensions/amp-a4a/0.1/head-validation.js
@@ -75,8 +75,18 @@ const EXTENSION_ALLOWLIST = map({
   'amp-video': true,
 });
 
+/**
+ * Escape any regex chars from given string.
+ * https://developer.cdn.mozilla.net/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+ * @param {string} string
+ * @return {string}
+ */
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
 const EXTENSION_URL_PREFIX = new RegExp(
-  '^' + urls.cdn.replace(/\./g, '\\.') + '/(rtv/\\d*/)?v0/'
+  '^' + escapeRegExp(urls.cdn) + '/(rtv/\\d+/)?v0/'
 );
 
 /**
