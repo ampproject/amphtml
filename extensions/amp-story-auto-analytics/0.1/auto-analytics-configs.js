ef90b93a924fe26ff56f6e9f066d08083ec7b2e4
Matias Szylkowski <mszylkowski@google.com>
2021-04-13 11:49:00 -0400

    Using pageview

--- extensions/amp-story-auto-analytics/0.1/auto-analytics-configs.js
+++ extensions/amp-story-auto-analytics/0.1/auto-analytics-configs.js
@@ -29,7 +29,7 @@ export const buildGtagConfig = (gtagId) => ({
   },
   'triggers': {
     'storyPageCount': {
-      'on': 'story-page-visible',
+      'on': 'pageview',
       'vars': {
         'event_name': 'custom',
         'event_action': 'story_page_count',
