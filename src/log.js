9761366ac6dbdccbbb9780564fdd3ea187eb1362
Ryan Cebulko <ryan@cebulko.com>
2021-04-13 17:17:47 -0400

    ♻️ Hoist associatedElement extraction out of core (#33810)
    * Extract associatedElement out of core
    
    * Comments and tweaks
    
    * Fix/update tests
    
    * Dont use Array.find

--- src/log.js
+++ src/log.js
@@ -18,9 +18,9 @@ import {
   USER_ERROR_SENTINEL,
   elementStringOrPassThru,
 } from './core/error-message-helpers';
+import {findIndex, isArray} from './core/types/array';
 import {getMode} from './mode';
 import {internalRuntimeVersion} from './internal-version';
-import {isArray} from './core/types/array';
 import {isEnumValue} from './types';
 import {once} from './utils/function';
 import {pureDevAssert, pureUserAssert} from './core/assert';
@@ -540,6 +540,16 @@ export class Log {
    */
   prepareError_(error) {
     error = duplicateErrorIfNecessary(error);
+
+    // `associatedElement` is used to add the i-amphtml-error class; in
+    // `#development=1` mode, it also adds `i-amphtml-element-error` to the
+    // element and sets the `error-message` attribute.
+    if (error.messageArray) {
+      const elIndex = findIndex(error.messageArray, (item) => item?.tagName);
+      if (elIndex > -1) {
+        error.associatedElement = error.messageArray[elIndex];
+      }
+    }
     if (this.suffix_) {
       if (!error.message) {
         error.message = this.suffix_;
