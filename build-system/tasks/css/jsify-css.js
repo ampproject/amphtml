bc4fc6584373471c9f3cc7e4924569f08feb270c
Raghu Simha <rsimha@amp.dev>
2021-04-14 14:46:43 -0400

    🏗 Move directory management into `TransformCache` (#33834)

--- build-system/tasks/css/jsify-css.js
+++ build-system/tasks/css/jsify-css.js
@@ -79,13 +79,11 @@ function getEnvironmentHash() {
   return environmentHash;
 }
 
-const CACHE_DIR = path.join(__dirname, '..', '..', '..', '.css-cache');
-
 /**
  * Used to cache css transforms done by postcss.
- * @const {!TransformCache}
+ * @const {TransformCache}
  */
-const transformCache = new TransformCache(CACHE_DIR, '.css');
+let transformCache;
 
 /**
  * @typedef {{css: string, warnings: string[]}}:
@@ -130,6 +128,9 @@ async function jsifyCssAsync(filename) {
  * @return {Promise<CssTransformResultDef>}
  */
 async function transformCss(contents, hash, opt_filename) {
+  if (!transformCache) {
+    transformCache = new TransformCache('.css-cache', '.css');
+  }
   const cached = transformCache.get(hash);
   if (cached) {
     return JSON.parse((await cached).toString());
