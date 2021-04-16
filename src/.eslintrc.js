f7e9c988f7ab8cc572c759b5d17165b823a7109d
Ryan Cebulko <ryan@cebulko.com>
2021-04-16 18:10:54 -0400

    Remove src/context/scheduler from allowlist

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -55,7 +55,6 @@ module.exports = {
         './preact/slot.js',
         './core/contextprops.js',
         './context/node.js',
-        './context/scheduler.js',
         // TEMPORARY, follow tracking issue #33631
         './preact/component/3p-frame.js',
       ],
