f73a8f9bd147fcc9e0ce401f65f4c94d61474b81
Viktor Lytvynchuk <viktor.lytvynchuk.devmrks@gmail.com>
2021-04-17 00:07:03 +0300

    amp-link-rewriter - test added:
    Should rewrite urls in attribute scope

--- extensions/amp-link-rewriter/0.1/test/test-noscope-document.js
+++ extensions/amp-link-rewriter/0.1/test/test-noscope-document.js
@@ -150,5 +150,69 @@ describes.fakeWin(
         'https://retailer-example.local/some-path/'
       );
     });
+
+    it('Should rewrite urls in attribute scope', () => {
+      const template = `
+        <div id='in-scope'>
+            <a rel='nofollow'
+                class='sidebar'
+                href='https://www.retailer-example.local/some-path/'>Should pass</a>
+            <a rel='nofollow'
+                class='sidebar'
+                href='https://retailer-example.local/some-path/'
+                data-vars-event-id='234'>Should pass</a>
+            <a rel='mustfail'
+                class='sidebar'
+                href='https://retailer-example.local/some-path/'
+                data-vars-event-id='234'>Should fail by rel attribute</a>
+            <a rel='nofollow'
+                class='mustfail'
+                href='https://retailer-example.local/some-path/'
+                data-vars-event-id='234'>Should fail by class attribute</a>
+            <a rel='nofollow'
+                class='sidebar'
+                href='https://other-domain.local/some-path/'
+                data-vars-event-id='234'>Should fail by href attribute</a>
+        </div>
+      `;
+      const rootNode = env.ampdoc.getRootNode();
+
+      const config = helpers.createConfig({
+        'attribute': {
+          'rel': 'nofollow',
+          'class': 'sidebar',
+          'href': '(https:\\/\\/(www\\.)?retailer-example\\.local).*',
+        },
+      });
+
+      const linkRewriterElement = helpers.createLinkRewriterElement(config);
+      rootNode.body.appendChild(linkRewriterElement);
+      rootNode.body.insertAdjacentHTML('afterbegin', template);
+
+      const rewriter = new LinkRewriter('', linkRewriterElement, env.ampdoc);
+
+      const links = rootNode.body.querySelectorAll('a');
+
+      expect(links.length).to.equal(5);
+
+      rewriter.handleClick(links[0]);
+      expect(links[0].href).to.equal(
+        'https://visit.foo.net/visit?pid=110&url=https%3A%2F%2Fwww.retailer-example.local%2Fsome-path%2F&cid=12345&ref=&location=http%3A%2F%2Fpartnersite.com%2F123&rel=nofollow&productId='
+      );
+      rewriter.handleClick(links[1]);
+      expect(links[1].href).to.equal(
+        'https://visit.foo.net/visit?pid=110&url=https%3A%2F%2Fretailer-example.local%2Fsome-path%2F&cid=12345&ref=&location=http%3A%2F%2Fpartnersite.com%2F123&rel=nofollow&productId=234'
+      );
+      rewriter.handleClick(links[2]);
+      expect(links[2].href).to.equal(
+        'https://retailer-example.local/some-path/'
+      );
+      rewriter.handleClick(links[3]);
+      expect(links[3].href).to.equal(
+        'https://retailer-example.local/some-path/'
+      );
+      rewriter.handleClick(links[4]);
+      expect(links[4].href).to.equal('https://other-domain.local/some-path/');
+    });
   }
 );
