76a76fa41b1f776af233cab52542786e4e3510f0
Caleb Cordry <ccordry@google.com>
2021-04-19 16:37:35 -0700

    another test

--- extensions/amp-story/1.0/test/test-story-analytics.js
+++ extensions/amp-story/1.0/test/test-story-analytics.js
@@ -57,4 +57,37 @@ describes.realWin('amp-story-analytics', {amp: true}, (env) => {
     });
     expect(triggerAnalyticsStub).not.to.be.called;
   });
+
+  it('sends story-page-visible on content page after ad page', () => {
+    const triggerAnalyticsStub = env.sandbox.stub(
+      analytics,
+      'triggerAnalyticsEvent'
+    );
+    storeService.dispatch(Action.CHANGE_PAGE, {
+      id: 'page-1',
+      index: 0,
+    });
+    expect(triggerAnalyticsStub).to.have.been.calledOnceWithExactly(
+      el,
+      'story-page-visible',
+      env.sandbox.match({storyPageIndex: 0, storyPageId: 'page-1'})
+    );
+    storeService.dispatch(Action.TOGGLE_AD, true);
+    storeService.dispatch(Action.CHANGE_PAGE, {
+      id: 'ad-page-1',
+      index: 1,
+    });
+    expect(triggerAnalyticsStub).to.have.been.calledOnce;
+    storeService.dispatch(Action.TOGGLE_AD, false);
+    storeService.dispatch(Action.CHANGE_PAGE, {
+      id: 'page-2',
+      index: 2,
+    });
+    expect(triggerAnalyticsStub).to.have.been.calledTwice;
+    expect(triggerAnalyticsStub.secondCall).to.have.been.calledWithExactly(
+      el,
+      'story-page-visible',
+      env.sandbox.match({storyPageIndex: 2, storyPageId: 'page-2'})
+    );
+  });
 });
