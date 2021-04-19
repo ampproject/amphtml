314e56cb1caf2914eb204d4275f5ca1e48b0a79d
Caleb Cordry <ccordry@google.com>
2021-04-19 16:59:30 -0700

    whitespace

--- extensions/amp-story/1.0/story-analytics.js
+++ extensions/amp-story/1.0/story-analytics.js
@@ -123,7 +123,7 @@ export class StoryAnalyticsService {
     this.storeService_.subscribe(
       StateProperty.CURRENT_PAGE_ID,
       (pageId) => {
-        const isAd = this.storeService_.get(StateProperty.AD_STATE); 
+        const isAd = this.storeService_.get(StateProperty.AD_STATE);
         if (!pageId || isAd) {
           return;
         }
