9503916a2bb1468a9a3dc7eafb199488d2a3a5ab
Daniel Rozenberg <rodaniel@amp.dev>
2021-04-19 11:26:45 -0400

    Change email address

--- .circleci/fail_fast.sh
+++ .circleci/fail_fast.sh
@@ -37,11 +37,11 @@ send_email() {
 # Without this, our custom bots will not be able to function correctly.
 if [[ "$CIRCLE_BRANCH" == "main" ]]; then
   echo $(YELLOW "This main branch build failed, notifying @ampproject/build-on-duty.")
-  send_mail "rodaniel@google.com"  # "amp-build-on-duty@grotations.appspotmail.com"
+  send_mail "amp-build-on-duty@grotations.appspotmail.com"
   exit 0
 elif [[ "$CIRCLE_BRANCH" =~ ^amp-release-* ]]; then
   echo $(YELLOW "This release branch build failed, notifying @ampproject/release-on-duty.")
-  send_mail "rodaniel@google.com"  # "amp-release-on-duty@grotations.appspotmail.com"
+  send_mail "amp-release-on-duty@grotations.appspotmail.com"
   exit 0
 fi
 
