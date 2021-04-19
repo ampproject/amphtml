447e68f388e660d3aaa3e6be8bb64613056c7d5c
Daniel Rozenberg <rodaniel@amp.dev>
2021-04-19 11:55:42 -0400

    :%s/send_mail/send_email/g

--- .circleci/fail_fast.sh
+++ .circleci/fail_fast.sh
@@ -57,15 +57,15 @@ send_email() {
 # Without this, our custom bots will not be able to function correctly.
 if [[ "$CIRCLE_BRANCH" == "main" ]]; then
   echo $(YELLOW "This main branch build failed, notifying @ampproject/build-on-duty.")
-  send_mail "amp-build-on-duty@grotations.appspotmail.com" "AMP Build On-Duty"
+  send_email "amp-build-on-duty@grotations.appspotmail.com" "AMP Build On-Duty"
   exit 0
 elif [[ "$CIRCLE_BRANCH" =~ ^amp-release-* ]]; then
   echo $(YELLOW "This release branch build failed, notifying @ampproject/release-on-duty.")
-  send_mail "amp-release-on-duty@grotations.appspotmail.com" "AMP Release On-Duty"
+  send_email "amp-release-on-duty@grotations.appspotmail.com" "AMP Release On-Duty"
   exit 0
 else
   echo $(YELLOW "DO NOT MERGE")
-  send_mail "rodaniel@google.com" "Daniel Rozenberg"
+  send_email "rodaniel@google.com" "Daniel Rozenberg"
 fi
 
 # For PR builds, cancel when the first job fails.
