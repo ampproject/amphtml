dd9e8d88641f700fa7000e1b17ba10311cf17334
Raghu Simha <rsimha@amp.dev>
2021-04-21 12:02:28 -0400

    🏗 Check dot files and directories for invalid whitespaces (#33927)

--- .circleci/maybe_gracefully_halt.sh
+++ .circleci/maybe_gracefully_halt.sh
@@ -32,7 +32,7 @@ fi
 if [[ $CIRCLE_JOB == Experiment* ]]; then
   # Extract the experiment name from the job name in `config.yml`.
   EXP=$(echo $CIRCLE_JOB | awk '{print $2}')
-  
+
   # Extract the commit SHA. For PR jobs, this is written to .CIRCLECI_MERGE_COMMIT.
   if [[ -f /tmp/restored-workspace/.CIRCLECI_MERGE_COMMIT ]]; then
     COMMIT_SHA="$(cat /tmp/restored-workspace/.CIRCLECI_MERGE_COMMIT)"
@@ -58,4 +58,4 @@ if [[ $CIRCLE_JOB == Experiment* ]]; then
     circleci-agent step halt
     exit 0
   fi
-fi  
+fi
