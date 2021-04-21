153007a2e1841f4e0320bc51734f0e7b3bb273ce
Ryan Cebulko <ryan@cebulko.com>
2021-04-20 20:07:52 -0400

    Fix imports in test files

--- test/unit/core/data-structures/test-signals.js
+++ test/unit/core/data-structures/test-signals.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import {Signals} from '../../../src/core/data-structures/signals';
+import {Signals} from '../../../../src/core/data-structures/signals';
 
 describes.sandboxed('Signals', {}, (env) => {
   let clock;
