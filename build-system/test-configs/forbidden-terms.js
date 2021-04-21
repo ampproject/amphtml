46a0eb2910998a1a45fcebe343c6913742282512
Ryan Cebulko <ryan@cebulko.com>
2021-04-21 11:08:55 -0400

    Update .getTime allowlist entry in forbidden-terms

--- build-system/test-configs/forbidden-terms.js
+++ build-system/test-configs/forbidden-terms.js
@@ -994,7 +994,7 @@ const forbiddenTermsSrcInclusive = {
       '.github/workflows/create-design-review-issue.js',
       'extensions/amp-timeago/0.1/amp-timeago.js',
       'extensions/amp-timeago/1.0/component.js',
-      'src/utils/date.js',
+      'src/core/types/date.js',
     ],
   },
   '\\.expandStringSync\\(': {
