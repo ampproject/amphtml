33a6d099eb5dcc71beb4165ba1eb631c19435274
Matias Szylkowski <mszylkowski@google.com>
2021-04-21 15:30:52 -0400

    ✨ [Story Video] Connect CacheUrl service to amp-video and load sources (#33466)
    * Added canonical to videos example
    
    * Started video cache
    
    * Use cdn url from url-impl
    
    * Remove unnecessary | null
    
    * Undo videos.html
    
    * Remove console.log
    
    * Using ternary operator for reduce
    
    * Account for cached docs
    
    * Update logic with toolbox
    
    * Added service
    
    * Undo amp-video
    
    * Added cache url service
    
    * Added owners
    
    * Cache url to video
    
    * Simplified cache example
    
    * Updated title
    
    * Changed name to amp-cache-url of folder
    
    * Update readme
    
    * Update comments
    
    * Remove example
    
    * Updated readme
    
    * Added code
    
    * Fixed tests
    
    * Removed comment
    
    * Added some more tests
    
    * Fixed cdnurl
    
    * Update names of docSupports
    
    * Fix cacheUrl not replacing
    
    * updated cacheDomain param name
    
    * Check not cached doc to add cache sources
    
    * improved implementation from comments
    
    * added tests
    
    * Added gregable file on examples
    
    * Added warning
    
    * Uppercase user error
    
    * Updated opt in
    
    * Fix response.json is promise
    
    * Updated test
    
    * Fixed toArray import
    
    * Fixed visibilityState
    
    * Use helper functions
    
    * Added tests to amp-cache-url
    
    * Update extensions/amp-cache-url/0.1/test/test-amp-cache-url.js
    
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>
    
    * Using extensionScriptInNode with win
    
    * Updated tests to match comments
    
    * Not export applySources
    
    Co-authored-by: Gabriel Majoulet <gmajoulet@google.com>
    
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>
    Co-authored-by: Gabriel Majoulet <gmajoulet@google.com>

--- extensions/amp-cache-url/0.1/amp-cache-url.js
+++ extensions/amp-cache-url/0.1/amp-cache-url.js
@@ -14,11 +14,27 @@
  * limitations under the License.
  */
 
+import * as ampToolboxCacheUrl from '@ampproject/toolbox-cache-url';
+import {urls} from '../../../src/config';
+
 export class AmpCacheUrlService {
   /**
    * Create cache url service
    */
   constructor() {}
+
+  /**
+   *
+   * @param {string} url
+   * @param {string=} cacheDomain the cache domain name (eg: cdn.approject.org)
+   * @return {!Promise<string>}
+   */
+  createCacheUrl(url, cacheDomain = urls.cdn) {
+    return ampToolboxCacheUrl.createCacheUrl(
+      cacheDomain.replace(/https?:\/\//, ''),
+      url
+    );
+  }
 }
 
 AMP.extension('amp-cache-url', '0.1', (AMP) => {
--- extensions/amp-cache-url/0.1/test/test-amp-cache-url.js
+++ extensions/amp-cache-url/0.1/test/test-amp-cache-url.js
@@ -13,3 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import {AmpCacheUrlService} from '../amp-cache-url';
+
+describes.fakeWin(
+  'amp-cache-url',
+  {amp: {extensions: ['amp-cache-url']}},
+  () => {
+    it('should return a cached url', async () => {
+      const cacheUrlService = new AmpCacheUrlService();
+      const result = await cacheUrlService.createCacheUrl(
+        'https://amp.dev/stories'
+      );
+      expect(result).to.equal(
+        'https://amp-dev.cdn.ampproject.org/c/s/amp.dev/stories'
+      );
+    });
+
+    it('should not throw with empty url', async () => {
+      const cacheUrlService = new AmpCacheUrlService();
+      expect(cacheUrlService.createCacheUrl('')).to.not.be.rejected;
+    });
+
+    it('should not throw with invalid url', async () => {
+      const cacheUrlService = new AmpCacheUrlService();
+      expect(cacheUrlService.createCacheUrl('invalid url')).to.not.be.rejected;
+    });
+  }
+);
