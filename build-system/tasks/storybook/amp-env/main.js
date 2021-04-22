ff321d3b9cb00f13bcf5e0ba78402c3da5d41bb6
Alan Orozco <alan@orozco.xyz>
2021-04-22 12:30:10 -0700

    clearer

--- build-system/tasks/storybook/amp-env/main.js
+++ build-system/tasks/storybook/amp-env/main.js
@@ -22,8 +22,8 @@ module.exports = {
   addons: [
     // TODO(alanorozco): AMP previews are loaded inside an iframe, so the a11y
     // addon is not able to inspect the tree inside it. Its results are incorrect,
-    // since it checks this wrapper iframe. Enable this eventually, once we find a
-    // way to inspect the iframe's tree.
+    // since it only checks the structure of the outer iframe element.
+    // Enable this once we find a way to inspect the iframe document's tree.
     // '@storybook/addon-a11y',
     '@storybook/addon-viewport',
     '@storybook/addon-knobs',
