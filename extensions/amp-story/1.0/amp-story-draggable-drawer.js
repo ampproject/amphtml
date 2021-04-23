47f0190fd1c4526c818b1cbd10bab137e5a6f779
Jake Fried <samouri@users.noreply.github.com>
2021-04-23 17:41:04 -0400

    amp-story-bookend: fix circular dependency (#33994)
    * amp-story-bookend: fix circular dependency
    
    * you missed a spot

--- extensions/amp-story/1.0/amp-story-draggable-drawer.js
+++ extensions/amp-story/1.0/amp-story-draggable-drawer.js
@@ -29,7 +29,7 @@ import {createShadowRootWithStyle} from './utils';
 import {dev, devAssert} from '../../../src/log';
 import {getLocalizationService} from './amp-story-localization-service';
 import {htmlFor} from '../../../src/static-template';
-import {isPageAttachmentUiV2ExperimentOn} from './amp-story-open-page-attachment';
+import {isPageAttachmentUiV2ExperimentOn} from './amp-story-page-attachment-ui-v2';
 import {listen} from '../../../src/event-helper';
 import {resetStyles, setImportantStyles, toggle} from '../../../src/style';
 
