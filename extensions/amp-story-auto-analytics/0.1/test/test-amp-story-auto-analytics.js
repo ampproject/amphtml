3de6e12ca011b1beee00dc79f1f939079d1e42f1
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2021-04-23 14:08:03 -0400

    [amp-story-auto-analytics] Add default linker (#33922)
    * wip
    
    * alternative version
    
    * add sample
    
    * without destinationDomain
    
    * add test
    
    * add cookieWriter config and fix linker config. Add cookieWriter test
    
    * add comments

--- extensions/amp-story-auto-analytics/0.1/test/test-amp-story-auto-analytics.js
+++ extensions/amp-story-auto-analytics/0.1/test/test-amp-story-auto-analytics.js
@@ -53,5 +53,45 @@ describes.realWin(
       expect(autoAnalyticsEl.querySelector('amp-analytics[type="gtag"]')).to
         .exist;
     });
+
+    it('should add linker config', async () => {
+      await autoAnalyticsEl.whenBuilt();
+      const config = {
+        'linkers': {
+          'ampStoryAutoAnalyticsLinker': {
+            'ids': {
+              'cid': '${clientId}',
+            },
+            'enabled': true,
+            'proxyOnly': false,
+          },
+        },
+      };
+
+      const strConfig = JSON.stringify(config);
+      const configContents = strConfig.substr(1, strConfig.length - 2);
+
+      expect(autoAnalyticsEl.querySelector('script').textContent).to.contain(
+        configContents
+      );
+    });
+
+    it('should add cookieWriter config', async () => {
+      await autoAnalyticsEl.whenBuilt();
+      const config = {
+        'cookies': {
+          'ampStoryAutoAnalyticsCookies': {
+            'value': 'LINKER_PARAM(ampStoryAutoAnalyticsLinker, cid)',
+          },
+        },
+      };
+
+      const strConfig = JSON.stringify(config);
+      const configContents = strConfig.substr(1, strConfig.length - 2);
+
+      expect(autoAnalyticsEl.querySelector('script').textContent).to.contain(
+        configContents
+      );
+    });
   }
 );
