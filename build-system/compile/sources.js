9d0571686fb77344eaebf54fdf14df9fa450d1ea
Alan Orozco <alan@orozco.xyz>
2021-04-23 10:48:34 -0700

    add to sources.js

--- build-system/compile/sources.js
+++ build-system/compile/sources.js
@@ -142,6 +142,8 @@ const CLOSURE_SRC_GLOBS = [
   'extensions/amp-video/1.0/**/*.js',
   // Needed for VideoService
   'extensions/amp-video-service/**/*.js',
+  // amp-vimeo 0.1 and 1.0 share this file.
+  'extensions/amp-vimeo/def.js',
   // Needed to access ConsentPolicyManager from other extensions
   'extensions/amp-consent/**/*.js',
   // Needed to access AmpGeo type for service locator
