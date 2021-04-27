94009e1a21261a717b73a395801e39c56937e7d7
Torben Brodt <torben@opinary.com>
2021-04-27 18:33:45 +0200

    Opinary Ads: Add support for Embed integration (#33518)

--- ads/vendors/opinary.js
+++ ads/vendors/opinary.js
@@ -18,30 +18,60 @@ import {loadScript, validateData} from '../../3p/3p';
 
 /**
  * @param {!Window} global
+ * @description Make canonicalUrl available from iframe
+ */
+function addCanonicalLinkTag(global) {
+  if (global.context.canonicalUrl) {
+    const link = global.document.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    link.setAttribute('href', global.context.canonicalUrl);
+    global.document.head.appendChild(link);
+  }
+}
+
+/**
+ * @param {!Window} global
+ * @param {!Object} data
+ * @return {?Node}
+ */
+function createContainer(global, data) {
+  // create div
+  const div = global.document.createElement('div');
+  if (data.poll) {
+    div.className = 'opinary-widget-embed';
+    div.dataset.customer = data.client;
+    div.dataset.poll = data.poll;
+  } else {
+    div.setAttribute('id', 'opinary-automation-placeholder');
+  }
+
+  return div;
+}
+
+/**
+ * @param {!Window} global
  * @param {!Object} data
  */
 export function opinary(global, data) {
   validateData(data, ['client']);
 
-  // create div for automatic poll placement
-  const div = global.document.createElement('div');
-  div.setAttribute('id', 'opinary-automation-placeholder');
+  addCanonicalLinkTag(global);
 
-  // create div for internal event system AMP awknowledgement
-  const div2 = global.document.createElement('div');
-  div2.setAttribute('id', 'opinaryAMP');
+  // c element is created by AMP
+  const c = global.document.getElementById('c');
 
-  // append divs to page
-  global.document.getElementById('c').appendChild(div).appendChild(div2);
+  // create div to detect if we are in AMP context
+  const isAmp = global.document.createElement('div');
+  isAmp.setAttribute('id', 'opinaryAMP');
+  c.appendChild(isAmp);
 
-  if (!document.querySelector("link[rel='canonical']")) {
-    const link = document.createElement('link');
-    link.rel = 'canonical';
-    link.href = global.context.canonicalUrl;
-    global.document.head.appendChild(link);
+  // create div where poll should be shown
+  c.appendChild(createContainer(global, data));
+
+  // load script
+  if (data.poll) {
+    loadScript(global, `https://widgets.opinary.com/embed.js`);
+  } else {
+    loadScript(global, `https://widgets.opinary.com/a/${data.client}.js`);
   }
-
-  const url = `https://widgets.opinary.com/a/${data.client}.js`;
-
-  loadScript(global, url);
 }
