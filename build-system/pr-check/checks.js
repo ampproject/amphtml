acf6b2947e41d8e858af6c4edd37b6d2d6765ad0
Alan Orozco <alan@orozco.xyz>
2021-04-27 16:39:52 -0700

    use amp-t for name

--- build-system/pr-check/checks.js
+++ build-system/pr-check/checks.js
@@ -39,8 +39,8 @@ function pushBuildWorkflow() {
   timedExecOrDie('amp check-exact-versions');
   timedExecOrDie('amp check-renovate-config');
   timedExecOrDie('amp server-tests');
-  timedExecOrDie('amp make-extension name=for-test --test --cleanup');
-  timedExecOrDie('amp make-extension name=for-test --test --cleanup --bento');
+  timedExecOrDie('amp make-extension --name=t --test --cleanup');
+  timedExecOrDie('amp make-extension --name=t --test --cleanup --bento');
   timedExecOrDie('amp dep-check');
   timedExecOrDie('amp check-types');
   timedExecOrDie('amp check-sourcemaps');
@@ -115,8 +115,8 @@ async function prBuildWorkflow() {
   }
 
   if (buildTargetsInclude(Targets.AVA, Targets.RUNTIME)) {
-    timedExecOrDie('amp make-extension name=for-test --test --cleanup');
-    timedExecOrDie('amp make-extension name=for-test --test --cleanup --bento');
+    timedExecOrDie('amp make-extension --name=t --test --cleanup');
+    timedExecOrDie('amp make-extension --name=t --test --cleanup --bento');
   }
 
   if (buildTargetsInclude(Targets.RUNTIME)) {
