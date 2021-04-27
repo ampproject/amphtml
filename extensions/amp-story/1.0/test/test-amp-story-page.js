29a00021d282c0e422ef2feac670274ef35a90cd
Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>
2021-04-27 14:20:59 -0400

    [amp-story] 🐛 Add target="_top" to page attachment (#34030)
    * wip
    
    * test
    
    * add to new cta too
    
    (cherry picked from commit 15ab429c3c5fbc7c6e46812ff51555166480717f)

--- extensions/amp-story/1.0/test/test-amp-story-page.js
+++ extensions/amp-story/1.0/test/test-amp-story-page.js
@@ -645,6 +645,44 @@ describes.realWin('amp-story-page', {amp: {extensions}}, (env) => {
     expect(openAttachmentEl).to.exist;
   });
 
+  it('should build the open attachment UI with target="_top" to navigate in top window', async () => {
+    const attachmentEl = win.document.createElement(
+      'amp-story-page-attachment'
+    );
+    attachmentEl.setAttribute('layout', 'nodisplay');
+    element.appendChild(attachmentEl);
+
+    page.buildCallback();
+    await page.layoutCallback();
+    page.setState(PageState.PLAYING);
+
+    const openAttachmentEl = element.querySelector(
+      '.i-amphtml-story-page-open-attachment'
+    );
+
+    expect(openAttachmentEl.getAttribute('target')).to.eql('_top');
+  });
+
+  it('should build the new default outlink page attachment UI with target="_top" to navigate in top window', async () => {
+    toggleExperiment(win, 'amp-story-page-attachment-ui-v2', true);
+
+    const attachmentEl = win.document.createElement(
+      'amp-story-page-attachment'
+    );
+    attachmentEl.setAttribute('layout', 'nodisplay');
+    element.appendChild(attachmentEl);
+
+    page.buildCallback();
+    await page.layoutCallback();
+    page.setState(PageState.PLAYING);
+
+    const openAttachmentEl = element.querySelector(
+      '.i-amphtml-story-page-open-attachment'
+    );
+
+    expect(openAttachmentEl.getAttribute('target')).to.eql('_top');
+  });
+
   it('should build the inline page attachment UI with one image', async () => {
     toggleExperiment(win, 'amp-story-page-attachment-ui-v2', true);
 
