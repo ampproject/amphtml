701eb1d94734b48ffbfab82257e63148a892aa70
Alan Orozco <alanorozco@users.noreply.github.com>
2021-04-29 11:05:30 -0700

    🚮 Remove nonexistent directory from `sources.js` (#34086)

--- build-system/compile/sources.js
+++ build-system/compile/sources.js
@@ -140,8 +140,6 @@ const CLOSURE_SRC_GLOBS = [
   'extensions/amp-user-notification/**/*.js',
   // Needed for video components in Bento.
   'extensions/amp-video/1.0/**/*.js',
-  // Needed for VideoService
-  'extensions/amp-video-service/**/*.js',
   // amp-vimeo 0.1 and 1.0 share this file.
   'extensions/amp-vimeo/vimeo-api.js',
   // Needed to access ConsentPolicyManager from other extensions
