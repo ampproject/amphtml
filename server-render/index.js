86127a3fdc08f7df3009d8c30408bfeb89cd508f
Jake Fried <samouri@users.noreply.github.com>
2021-05-06 13:32:31 -0400

    add writeDocumentToAst

--- server-render/index.js
+++ server-render/index.js
@@ -15,27 +15,64 @@ export function renderElement(ast) {
   return el.outerHTML;
 }
 
+// TODO: handle comment nodes
+
 /**
  *
  * @param {*} ast
- * @param {*} doc
+ * @param {*} document
  * @param {*} parent
  * @returns
  */
-export function writeAstToDocument(ast, doc = createDocument(), parent = doc) {
+export function writeAstToDocument(
+  ast,
+  document = createDocument(),
+  parent = document
+) {
   for (let astNode of ast) {
     // Text node
     if (typeof astNode === 'string') {
-      parent.appendChild(doc.createTextNode(astNode));
+      parent.appendChild(document.createTextNode(astNode));
       continue;
     }
 
-    const domNode = doc.createElement(astNode.tag);
+    const domNode = document.createElement(astNode.tag);
     for (let [k, v] of Object.entries(astNode.attrs ?? [])) {
       domNode.setAttribute(k, v);
     }
-    writeAstToDocument(astNode.content ?? [], doc, domNode);
+    writeAstToDocument(astNode.content ?? [], document, domNode);
     parent.appendChild(domNode);
   }
-  return doc;
+  return document;
+}
+
+export function writeDocumentToAst(document, parent = []) {
+  for (let domNode of document.childNodes ?? []) {
+    // Text node
+    if (domNode.nodeType === 3) {
+      parent.content.push(domNode.data);
+      continue;
+    }
+
+    let astNode;
+    try {
+      astNode = {tag: domNode.tagName.toLowerCase()};
+    } catch (err) {
+      throw new Error('hello' + domNode.nodeType);
+    }
+    if (domNode.hasAttributes()) {
+      astNode.attrs = Object.fromEntries(
+        Array.from(domNode.attributes).map((attr) => [attr.name, attr.value])
+      );
+    }
+
+    if (domNode.childNodes.length > 0) {
+      astNode.content = [];
+      writeDocumentToAst(domNode, astNode);
+    }
+
+    // top-level parent is an array.
+    (parent.content ?? parent).push(astNode);
+  }
+  return parent;
 }
