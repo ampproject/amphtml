8d6eb413630dfb1dda3ef00cdc8e44c2d9d69b3d
Jake Fried <samouri@users.noreply.github.com>
2021-05-06 13:44:12 -0400

    🏗  runtime: allow for iterator polyfill (#34249)
    * runtime: allow for iterator polyfill
    
    * Empty commit
    
    * lint!

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
     'local/no-dynamic-import': 0,
     'local/no-es2015-number-props': 0,
     'local/no-export-side-effect': 0,
-    'local/no-for-of-statement': 0,
     'local/no-forbidden-terms': 0,
     'local/no-function-async': 0,
     'local/no-function-generator': 0,
