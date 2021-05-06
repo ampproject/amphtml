e84936c91ff5558cb44a44cd17347fbd0b29ccb3
Alan Orozco <alanorozco@users.noreply.github.com>
2021-05-06 09:28:56 -0700

    🐛 Fix bad type (#34254)

--- extensions/amp-video/1.0/storybook/_helpers.js
+++ extensions/amp-video/1.0/storybook/_helpers.js
@@ -17,8 +17,6 @@ import * as Preact from '../../../../src/preact';
 
 /**
  * @param {*} props
- * @param {*} props.id
- * @param {*} props.children
  * @return {*}
  */
 export const VideoElementWithActions = ({
