c2d3bf5bdb51e37e9b62d4112e62c4eddc3ac537
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-05-11 06:42:26 -0700

    Bad merge

--- builtins/amp-img/amp-img.js
+++ builtins/amp-img/amp-img.js
@@ -21,7 +21,7 @@ import {Services} from '../../src/services';
 import {dev} from '../../src/log';
 import {guaranteeSrcForSrcsetUnsupportedBrowsers} from '../../src/utils/img';
 import {listen} from '../../src/event-helper';
-import {propagateAttributes} from '../src/core/dom/propagate-attributes';
+import {propagateAttributes} from '../../src/core/dom/propagate-attributes';
 import {propagateObjectFitStyles, setImportantStyles} from '../../src/style';
 import {registerElement} from '../../src/service/custom-element-registry';
 import {removeElement, scopedQuerySelector} from '../../src/dom';
