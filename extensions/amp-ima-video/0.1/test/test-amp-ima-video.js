aaa548baedb9dc661e891127c1e3eeb24785dd85
Alan Orozco <alanorozco@users.noreply.github.com>
2021-05-11 09:32:35 -0700

    ♻️  `<amp-ima-video>`: Serialize children (#34229)
    `<amp-ima-video>` takes children like:
    
    ```
    <amp-ima-video>
      <source data-foo="bar" src="src" />
      <track src="src" />
    </amp-ima-video>
    ```
    
    These were previously serialized as HTML on `buildCallback` to be pass to the iframe to (unsafely) write onto `innerHTML`:
    
    ```
    <amp-ima-video
      data-child-elements='<source data-foo="bar" src="src" /><track src="src" />'
    ></amp-ima-video>
    ```
    
    To prepare for a Bento implementation, we serialize into a JSON format that a Preact component can build more simply and safely:
    
    ```
    [
      ["SOURCE", {"data-foo": "bar", "src": "src"}],
      ["TRACK", {"src": "src"}]
    ]
    ```
    
    This change only affects the interface between the `<amp-ima-video>` element and its own proxy `<iframe>`. It does not affect anything author-facing since they will continue to write children `<source>` and `<track>` elements as such.

---
+++ extensions/amp-ima-video/0.1/test/test-amp-ima-video.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../amp-ima-video';
+import {htmlFor} from '../../../../src/static-template';
+import {installResizeObserverStub} from '../../../../testing/resize-observer-stub';
+import {waitForChildPromise} from '../../../../src/dom';
+
+describes.realWin(
+  'amp-ima-video',
+  {
+    amp: {
+      runtimeOn: true,
+      extensions: ['amp-ima-video'],
+    },
+  },
+  (env) => {
+    let html;
+
+    beforeEach(() => {
+      html = htmlFor(env.win.document);
+      installResizeObserverStub(env.sandbox, env.win);
+    });
+
+    it('passes sourceChildren into iframe context', async () => {
+      const element = html`
+        <amp-ima-video data-tag="https://example.com" width="1" height="1">
+          <source data-foo="bar" src="src" />
+          <track any-attribute />
+          <span>Other elements should be excluded</span>
+        </amp-ima-video>
+      `;
+
+      env.win.document.body.appendChild(element);
+      await element.whenBuilt();
+
+      let iframe;
+      element.layoutCallback();
+      await waitForChildPromise(
+        element,
+        () => (iframe = element.querySelector('iframe'))
+      );
+
+      const parsedName = JSON.parse(iframe.name);
+      const sourceChildren = parsedName?.attributes?._context?.sourceChildren;
+      expect(sourceChildren).to.not.be.null;
+      expect(sourceChildren).to.have.length(2);
+      expect(sourceChildren[0][0]).to.eql('SOURCE');
+      expect(sourceChildren[0][1]).to.eql({'data-foo': 'bar', src: 'src'});
+      expect(sourceChildren[1][0]).to.eql('TRACK');
+      expect(sourceChildren[1][1]).to.eql({'any-attribute': ''});
+    });
+  }
+);
