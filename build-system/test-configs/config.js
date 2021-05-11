afc5f40464d40dc0e896ff05d14d111456a051f6
Raghu Simha <rsimha@amp.dev>
2021-05-10 21:30:10 -0400

    🏗 Improve usability of `describes` by making it configurable (#34286)

--- build-system/test-configs/config.js
+++ build-system/test-configs/config.js
@@ -24,7 +24,7 @@
  *   watched: boolean
  * }>
  */
-const initTestsPath = ['test/_init_tests.js'];
+const initTestsPath = ['testing/_init_tests.js'];
 
 const karmaHtmlFixturesPath = 'test/fixtures/*.html';
 
