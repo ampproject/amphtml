89778b1a7f0fa6ee140467bdfa0f45a297250337
Ryan Cebulko <ryan@cebulko.com>
2021-05-12 18:58:11 -0400

    Fix test change

--- test/unit/polyfills/test-resize-observer.js
+++ test/unit/polyfills/test-resize-observer.js
@@ -292,7 +292,7 @@ describes.fakeWin('ResizeObserverStub', {}, (env) => {
       ro.unobserve(element1);
       const native = upgrade(ro);
       expect(native.observe).to.not.be.called;
-      expect(ro.elements_).to.equal([]);
+      expect(ro.elements_.length).to.equal(0);
     });
 
     it('should re-observe previously observed elements', () => {
@@ -303,7 +303,7 @@ describes.fakeWin('ResizeObserverStub', {}, (env) => {
       expect(native.observe).to.be.calledTwice;
       expect(native.observe).to.be.calledWith(element1);
       expect(native.observe).to.be.calledWith(element2);
-      expect(ro.elements_).to.equal([]);
+      expect(ro.elements_.length).to.equal(0);
     });
 
     it('should observe new elements only on native', () => {
