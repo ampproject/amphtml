be9eefd6b6816742791f7580c89522e55a4cd480
Alan Orozco <alan@orozco.xyz>
2021-05-13 18:24:03 -0700

    test

--- extensions/amp-ima-video/0.1/test/test-amp-ima-video.js
+++ extensions/amp-ima-video/0.1/test/test-amp-ima-video.js
@@ -64,5 +64,26 @@ describes.realWin(
       expect(sourceChildren[1][0]).to.eql('TRACK');
       expect(sourceChildren[1][1]).to.eql({'any-attribute': ''});
     });
+
+    it('creates placeholder image from data-poster attribute', async () => {
+      const element = html`
+        <amp-ima-video
+          data-tag="https://example.com"
+          data-poster="https://example.com/foo.png"
+          width="1"
+          height="1"
+        ></amp-ima-video>
+      `;
+
+      env.win.document.body.appendChild(element);
+      await element.whenBuilt();
+
+      const img = element.querySelector('img');
+      expect(img).to.not.be.null;
+      expect(img).to.have.attribute('placeholder');
+      expect(img).to.have.class('i-amphtml-fill-content');
+      expect(img.getAttribute('loading')).to.equal('lazy');
+      expect(img.getAttribute('src')).to.equal('https://example.com/foo.png');
+    });
   }
 );
