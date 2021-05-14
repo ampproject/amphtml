d512ff8d6b71010a2e0ec1327f837fe5450f290b
Riley Jones <78179109+rileyajones@users.noreply.github.com>
2021-05-14 11:56:40 -0700

    Remove FunctionalTestController, make ControllerPromise extend Promise (#33844)
    * remove functionaltestcontroller, rename functional-test-controller file
    
    * rename functional-test-types to e2e-types
    
    Co-authored-by: Enrique Marroquin <5449100+Enriqe@users.noreply.github.com>

--- test/e2e/test-controller-promise.js
+++ test/e2e/test-controller-promise.js
@@ -280,8 +280,7 @@ describe('ControllerPromise', () => {
         opt_mutate = opt_mutate || ((x) => x);
         return new Promise((resolve, reject) => {
           /**
-           * Poll for the new value. This simulates behavior in the concrete
-           * implementations of the `FunctionalTestController` implementations.
+           * Poll for the new value.
            * See {@link ../../build-system/tasks/e2e/selenium-webdriver-controller.js#getWaitFn_}
            */
           const id = setInterval(async () => {
