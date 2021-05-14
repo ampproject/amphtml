41539eb53254006f9fae19d276f547b3b52b7b41
Ryan Cebulko <ryan@cebulko.com>
2021-05-14 17:34:52 -0400

    Move query string parsing helper tests

--- test/unit/test-url.js
+++ test/unit/test-url.js
@@ -259,58 +259,6 @@ describes.sandboxed('parseUrlDeprecated', {}, () => {
   });
 });
 
-describes.sandboxed('parseQueryString', {}, () => {
-  it('should return empty params when query string is empty or null', () => {
-    expect(parseQueryString(null)).to.deep.equal({});
-    expect(parseQueryString('')).to.deep.equal({});
-  });
-  it('should parse single key-value', () => {
-    expect(parseQueryString('a=1')).to.deep.equal({
-      'a': '1',
-    });
-  });
-  it('should parse two key-values', () => {
-    expect(parseQueryString('a=1&b=2')).to.deep.equal({
-      'a': '1',
-      'b': '2',
-    });
-  });
-  it('should ignore leading ?', () => {
-    expect(parseQueryString('?a=1&b=2')).to.deep.equal({
-      'a': '1',
-      'b': '2',
-    });
-  });
-  it('should ignore leading #', () => {
-    expect(parseQueryString('#a=1&b=2')).to.deep.equal({
-      'a': '1',
-      'b': '2',
-    });
-  });
-  it('should parse empty value', () => {
-    expect(parseQueryString('a=&b=2')).to.deep.equal({
-      'a': '',
-      'b': '2',
-    });
-    expect(parseQueryString('a&b=2')).to.deep.equal({
-      'a': '',
-      'b': '2',
-    });
-  });
-  it('should decode names and values', () => {
-    expect(parseQueryString('a%26=1%26&b=2')).to.deep.equal({
-      'a&': '1&',
-      'b': '2',
-    });
-  });
-  it('should return last dupe', () => {
-    expect(parseQueryString('a=1&b=2&a=3')).to.deep.equal({
-      'a': '3',
-      'b': '2',
-    });
-  });
-});
-
 describes.sandboxed('serializeQueryString', {}, () => {
   it('should return empty string for empty params', () => {
     expect(serializeQueryString({})).to.equal('');
