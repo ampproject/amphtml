8cb5da6968807e8ed77b22d7e1d1c2b34e1fe95d
Ryan Cebulko <ryan@cebulko.com>
2021-05-17 13:33:18 -0400

    ♻️ Enable passing type-checking on src/context (#34387)
    * enable src-context type-check target
    
    * Fix type errors in src/context
    
    * export ContextPropDef
    
    * for...of in scan
    
    * clean up scheduler types
    
    * Update ContextPropDef in subscriber
    
    * update types in index
    
    * update types in index
    
    * update types in contextprops
    
    * update types and for...of in node
    
    * Drastically narrow types in values
    
    * Add DEP template type to contextpropdef
    
    * type-narrow subscriber
    
    * Clean up scheduler type decl
    
    * use templates in scan
    
    * Update index prop and setter types
    
    * Allow subscriber ID template type
    
    * Add deps template arg for contextprops
    
    * Add SID for return type
    
    * Assert non-null subscriber IDs
    
    * Code review tweak
    
    * Remove template type from static function
    
    * forEach -> for..of
    
    * Revert forEach changes to Maps

--- build-system/tasks/check-types.js
+++ build-system/tasks/check-types.js
@@ -110,8 +110,8 @@ const TYPE_CHECK_TARGETS = {
     warningLevel: 'QUIET',
   },
   'src-context': {
-    srcGlobs: ['src/context/**/*.js'],
-    warningLevel: 'QUIET',
+    srcGlobs: ['src/context/**/*.js', ...CORE_SRCS_GLOBS],
+    externGlobs: ['src/context/**/*.extern.js', ...CORE_EXTERNS_GLOBS],
   },
   'src-core': {
     srcGlobs: CORE_SRCS_GLOBS,
