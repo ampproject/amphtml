9bc3e7de5b0782afe55c7be7b18126e73ff5420a
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-05-18 12:25:15 -0700

    [bento-dailymotion] Add initial files for bento amp-dailymotion component.

---
+++ extensions/amp-dailymotion/1.0/component.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '../../../src/preact';
+import {ContainWrapper} from '../../../src/preact/component';
+import {forwardRef} from '../../../src/preact/compat';
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from '../../../src/preact';
+/**
+ * @param {!DailymotionDef.Props} props
+ * @param ref
+ * @return {PreactDef.Renderable}
+ */
+export function DailymotionWithRef({videoId, ...rest}, ref) {
+  const src = useMemo(
+    () =>
+      'https://www.dailymotion.com/embed/video/' +
+      encodeURIComponent(videoId || '') +
+      '?api=1&html=1&app=amp',
+    [videoId]
+  );
+  const makeMethodMessage = useCallback(() => '{}', []);
+
+  const onMessage = useCallback((e) => {
+    console.log(e);
+  }, []);
+  return (
+    <VideoIframe
+      ref={ref}
+      {...rest}
+      src={src}
+      makeMethodMessage={makeMethodMessage}
+      onMessage={onMessage}
+    />
+  );
+}
+
+const Dailymotion = forwardRef(DailymotionWithRef);
+Dailymotion.displayName = 'Dailymotion';
+export {Dailymotion};
