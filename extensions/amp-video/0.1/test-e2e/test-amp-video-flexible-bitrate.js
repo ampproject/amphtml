7c63f986c05d3dc90bd73756aa6be7f4e998e1aa
Matias Szylkowski <mszylkowski@google.com>
2021-05-18 11:33:19 -0400

    Updated tests

--- extensions/amp-video/0.1/test-e2e/test-amp-video-flexible-bitrate.js
+++ extensions/amp-video/0.1/test-e2e/test-amp-video-flexible-bitrate.js
@@ -47,177 +47,226 @@ describes.endtoend(
       debugField = await controller.findElement('input#flexible-bitrate-debug');
     });
 
-    /**
-     * Tests for first load
-     */
+    describe('first load', () => {
+      it('plays the best quality when loading the page with good connection', async () => {
+        const video1El = await controller.findElement('#video1 video');
+        await expect(
+          await controller.getElementProperty(video1El, 'currentSrc')
+        ).contains('#high');
+      });
 
-    it('when loading the page with good connection, the best quality source is played', async () => {
-      const video1El = await controller.findElement('#video1 video');
-      await expect(
-        await controller.getElementProperty(video1El, 'currentSrc')
-      ).contains('#high');
+      it('does not load a video far away when on the first page', async () => {
+        const video4El = await controller.findElement('#video4 video');
+        await expect(
+          await controller.getElementProperty(video4El, 'currentSrc')
+        ).to.equal('');
+      });
     });
 
-    it("when on the first page, a further video doesn't load", async () => {
-      const video4El = await controller.findElement('#video4 video');
-      await expect(
-        await controller.getElementProperty(video4El, 'currentSrc')
-      ).to.equal('');
+    describe('downgrades on active videos', () => {
+      it('changes the video to a lower quality when it buffers', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
+
+        const video1El = await controller.findElement('#video1 video');
+
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+
+        await expect(
+          await controller.getElementProperty(video1El, 'currentSrc')
+        ).contains('#med');
+      });
+
+      it('keeps playing the video at the same time when a video downgrades to a lower quality source', async () => {
+        const video1El = await controller.findElement('#video1 video');
+
+        await sleep(100);
+
+        const currentTime = await controller.getElementProperty(
+          video1El,
+          'currentTime'
+        );
+
+        await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+
+        await expect(
+          await controller.getElementProperty(video1El, 'currentTime')
+        ).to.be.gte(currentTime);
+      });
+
+      it('does not keep reloading the video when a video buffers multiple times', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+
+        const video1El = await controller.findElement(
+          '#video1 video.i-amphtml-pool-video'
+        );
+
+        await expect(
+          await controller.getElementProperty(video1El, 'currentSrc')
+        ).contains('#low');
+        await expect(await controller.getElementProperty(video1El, 'paused')).is
+          .false;
+      });
     });
 
-    /**
-     * Tests for downgrades on active video
-     */
+    describe('downgrades on inactive videos', () => {
+      it('does not load videos out of range when a video buffers', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
 
-    it('when a video buffers, the video changes to a lower quality source', async () => {
-      await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
+        const video2El = await controller.findElement('#video4 video');
 
-      const video1El = await controller.findElement('#video1 video');
+        // If currentSrc is empty, video is not loaded.
+        await expect(
+          await controller.getElementProperty(video2El, 'currentSrc')
+        ).equal('');
+      });
 
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+      it('lowers the quality on close videos that are not already loaded when a video buffers', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 2);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
 
-      await expect(
-        await controller.getElementProperty(video1El, 'currentSrc')
-      ).contains('#med');
+        const video2El = await controller.findElement('#video2 video');
+
+        await expect(
+          await controller.getElementProperty(video2El, 'currentSrc')
+        ).contains('#med');
+      });
+
+      it('does not lower the quality on close videos that are already loaded when a video buffers', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.LOAD, 2);
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+
+        const video4El = await controller.findElement(
+          '#video2 video.i-amphtml-pool-video'
+        );
+
+        await expect(
+          await controller.getElementProperty(video4El, 'currentSrc')
+        ).contains('#high');
+      });
     });
 
-    it('when a video buffers, the low quality source starts playing at the same place the previous source was stopped at', async () => {
-      const video1El = await controller.findElement('#video1 video');
+    describe('source generation', () => {
+      it('configures the 3 qualities for CDN sources when a video contains a CDN url source element', async () => {
+        await controller.click(story);
+        await controller.click(story);
 
-      await sleep(100);
+        await controller.findElement('amp-story-page#page-3[active]');
 
-      const currentTime = await controller.getElementProperty(
-        video1El,
-        'currentTime'
-      );
+        const sources = await controller.findElements(
+          '#video3 video.i-amphtml-pool-media source'
+        );
 
-      await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[0],
+          '?amp_video_quality=high',
+          '2000'
+        );
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[1],
+          '?amp_video_quality=medium',
+          '720'
+        );
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[2],
+          '?amp_video_quality=low',
+          '400'
+        );
+      });
 
-      await expect(
-        await controller.getElementProperty(video1El, 'currentTime')
-      ).to.be.gte(currentTime);
+      it('configures the 3 qualities for CDN sources when a video contains a CDN url src attribute', async () => {
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
+
+        await controller.findElement('amp-story-page#page-6[active]');
+
+        const sources = await controller.findElements(
+          '#video6 video.i-amphtml-pool-media source'
+        );
+
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[0],
+          '?amp_video_quality=high',
+          '2000'
+        );
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[1],
+          '?amp_video_quality=medium',
+          '720'
+        );
+        await testElementForSrcAndBitrate(
+          controller,
+          sources[2],
+          '?amp_video_quality=low',
+          '400'
+        );
+      });
+
+      it('sends a network request to fetch cached sources when video is configured with the cache attribute', async () => {
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
+
+        await controller.findElement('amp-story-page#page-5[active]');
+
+        await controller.findElement(
+          '#video5 video.i-amphtml-replaced-content'
+        );
+
+        await expect(
+          'https://amp-dev.cdn.ampproject.org/mbv/s/amp.dev/static/samples/video/tokyo.mp4?amp_video_host_url=https%3A%2F%2Famp.dev%2F&__amp_source_origin=http%3A%2F%2Flocalhost%3A8000'
+        ).to.have.been.sent;
+      });
     });
 
-    it("when a video buffers many times, it doesn't keep reloading", async () => {
-      await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 1);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+    describe('navigation', () => {
+      it('loads a video on a far page when navigating to the page', async () => {
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
 
-      const video1El = await controller.findElement(
-        '#video1 video.i-amphtml-pool-video'
-      );
+        await controller.findElement('amp-story-page#page-4[active]');
 
-      await expect(
-        await controller.getElementProperty(video1El, 'currentSrc')
-      ).contains('#low');
-      await expect(await controller.getElementProperty(video1El, 'paused')).is
-        .false;
-    });
+        const video4El = await controller.findElement(
+          '#video4 video.i-amphtml-pool-media'
+        );
 
-    /**
-     * Tests for downgrades on inactive videos
-     */
+        await expect(
+          await controller.getElementProperty(video4El, 'currentSrc')
+        ).contains('#high');
+      });
 
-    it('when a video buffers, other videos that are not already loaded lower the quality source', async () => {
-      await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 2);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
-      await controller.click(story);
-      await controller.findElement('amp-story-page#page-2[active]');
+      it('loads a low quality source on a far video when the connection drops and the user advances to that page', async () => {
+        await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+        await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 4);
+        await controller.click(story);
+        await controller.click(story);
+        await controller.click(story);
 
-      const video2El = await controller.findElement('#video2 video');
+        await controller.findElement('amp-story-page#page-4[active]');
 
-      await expect(
-        await controller.getElementProperty(video2El, 'currentSrc')
-      ).contains('#med');
-    });
+        await sleep(100);
 
-    it("when a video buffers, other videos that are already loaded don't lower the quality source", async () => {
-      await forceEventOnVideo(VIDEO_EVENTS.LOAD, 2);
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
+        const video4El = await controller.findElement(
+          '#video4 video.i-amphtml-pool-video'
+        );
 
-      const video4El = await controller.findElement(
-        '#video2 video.i-amphtml-pool-video'
-      );
-
-      await expect(
-        await controller.getElementProperty(video4El, 'currentSrc')
-      ).contains('#high');
-    });
-
-    /**
-     * Tests on source generation
-     */
-
-    it('when a video contains a proxy url, it loads the cached sources', async () => {
-      await controller.click(story);
-      await controller.click(story);
-
-      await controller.findElement('amp-story-page#page-3[active]');
-
-      const video4SourceEl = await controller.findElement(
-        '#video3 video.i-amphtml-pool-media source'
-      );
-
-      await expect(
-        await controller.getElementProperty(video4SourceEl, 'src')
-      ).contains('amp-dev.cdn.ampproject.org');
-    });
-
-    it('when a video contains a cache attribute, it fetches the cache', async () => {
-      await controller.click(story);
-      await controller.click(story);
-      await controller.click(story);
-      await controller.click(story);
-
-      await controller.findElement('amp-story-page#page-5[active]');
-
-      await controller.findElement('#video5 video.i-amphtml-replaced-content');
-
-      await expect(
-        'https://amp-dev.cdn.ampproject.org/mbv/s/amp.dev/static/samples/video/tokyo.mp4?amp_video_host_url=https%3A%2F%2Famp.dev%2F&__amp_source_origin=http%3A%2F%2Flocalhost%3A8000'
-      ).to.have.been.sent;
-    });
-
-    /**
-     * Tests on navigation
-     */
-
-    it('when advancing to a further page, the video on that page loads', async () => {
-      await controller.click(story);
-      await controller.click(story);
-      await controller.click(story);
-
-      await controller.findElement('amp-story-page#page-4[active]');
-
-      const video4El = await controller.findElement(
-        '#video4 video.i-amphtml-pool-media'
-      );
-
-      await expect(
-        await controller.getElementProperty(video4El, 'currentSrc')
-      ).contains('#high');
-    });
-
-    it('when the connection drops, advancing to a further page loads a low quality source on the further video', async () => {
-      await forceEventOnVideo(VIDEO_EVENTS.DOWNGRADE, 1);
-      await forceEventOnVideo(VIDEO_EVENTS.UNLOAD, 4);
-      await controller.click(story);
-      await controller.click(story);
-      await controller.click(story);
-
-      await controller.findElement('amp-story-page#page-4[active]');
-
-      await sleep(100);
-
-      const video4El = await controller.findElement(
-        '#video4 video.i-amphtml-pool-video'
-      );
-
-      await expect(
-        await controller.getElementProperty(video4El, 'currentSrc')
-      ).contains('#med');
+        await expect(
+          await controller.getElementProperty(video4El, 'currentSrc')
+        ).contains('#med');
+      });
     });
 
     function sleep(ms) {
@@ -230,3 +279,20 @@ describes.endtoend(
     }
   }
 );
+
+/**
+ * Tests that the element contains the {srcContains} in the [src], and the [data-bitrate] is {bitrateEquals}
+ */
+async function testElementForSrcAndBitrate(
+  controller,
+  element,
+  srcContains,
+  bitrateEquals
+) {
+  await expect(await controller.getElementProperty(element, 'src')).contains(
+    srcContains
+  );
+  await expect(
+    await controller.getElementAttribute(element, 'data-bitrate')
+  ).equals(bitrateEquals);
+}
