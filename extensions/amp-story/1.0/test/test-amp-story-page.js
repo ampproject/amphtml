799bcd8251a17f5ae1f9125bf22e11aa019b292a
Matias Szylkowski <mszylkowski@google.com>
2021-05-20 14:26:07 -0400

    Remove false test

--- extensions/amp-story/1.0/test/test-amp-story-page.js
+++ extensions/amp-story/1.0/test/test-amp-story-page.js
@@ -121,18 +121,6 @@ describes.realWin('amp-story-page', {amp: {extensions}}, (env) => {
     expect(page.animationManager_).to.exist;
   });
 
-  it('should not build the animation manager if `prefers-reduced-motion` is on', async () => {
-    env.sandbox.stub(MediaQueryProps, 'prefersReducedMotion').returns(true);
-
-    const animatedEl = html`<div animate-in="fade-in"></div>`;
-
-    element.appendChild(animatedEl);
-    element.getAmpDoc = () => new AmpDocSingle(win);
-
-    page.buildCallback();
-    expect(page.animationManager_).to.be.null;
-  });
-
   it('should set an active attribute when state becomes active', async () => {
     page.buildCallback();
     await page.layoutCallback();
