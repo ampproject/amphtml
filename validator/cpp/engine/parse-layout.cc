b04185d18dd0436744ff257f075bdeeceb8fee07
Justin Ridgewell <jridgewell@google.com>
2021-05-25 18:38:23 +0000

    Add SSR support for FLUID layout
    PiperOrigin-RevId: 375756231

--- validator/cpp/engine/parse-layout.cc
+++ validator/cpp/engine/parse-layout.cc
@@ -204,12 +204,10 @@ CssLength CalculateHeight(AmpLayout::Layout input_layout,
 
 CssLength CalculateHeight(const AmpLayout& spec, AmpLayout::Layout input_layout,
                           const CssLength& input_height) {
-  static const auto kOne =
-      new CssLength("1", /*allow_auto=*/false, /*allow_fluid=*/false);
   if ((input_layout == AmpLayout::UNKNOWN || input_layout == AmpLayout::FIXED ||
        input_layout == AmpLayout::FIXED_HEIGHT) &&
       !input_height.is_set && spec.defines_default_height())
-    return *kOne;
+    return *kOnePx;
   return input_height;
 }
 
@@ -290,6 +288,8 @@ std::string GetLayoutStyle(AmpLayout::Layout layout, const CssLength& width,
                     GetCssLengthStyle(height, "height"));
     case AmpLayout::FIXED_HEIGHT:
       return GetCssLengthStyle(height, "height");
+    case AmpLayout::FLUID:
+      return "width:100%;height:0;";
     case AmpLayout::NODISPLAY:
     case AmpLayout::RESPONSIVE:
     case AmpLayout::FILL:
