92a37c46e11a78ab50cd62997c1151293024ed69
Raghu Simha <rsimha@amp.dev>
2021-05-25 13:40:36 -0400

    🏗  Update build-system subpackages before checking types (#34525)

--- build-system/tasks/check-build-system.js
+++ build-system/tasks/check-build-system.js
@@ -13,15 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+const globby = require('globby');
+const path = require('path');
 const {cyan, green} = require('../common/colors');
 const {execOrThrow} = require('../common/exec');
 const {log} = require('../common/logging');
+const {updateSubpackages} = require('../common/update-packages');
+
+/**
+ * Helper that updates build-system subpackages so their types can be verified.
+ */
+async function updateBuildSystemSubpackages() {
+  const packageFiles = globby.sync('build-system/tasks/*/package.json');
+  for (const packageFile of packageFiles) {
+    await updateSubpackages(path.dirname(packageFile));
+  }
+}
 
 /**
  * Performs type checking on the /build-system directory using TypeScript.
  * Configuration is defined in /build-system/tsconfig.json.
  */
-function checkBuildSystem() {
+async function checkBuildSystem() {
+  await updateBuildSystemSubpackages();
   log('Checking types in', cyan('build-system') + '...');
   execOrThrow(
     'npx -p typescript tsc --project ./build-system/tsconfig.json',
