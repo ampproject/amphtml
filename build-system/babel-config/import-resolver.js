c856e14b64896dedbea0e8abd0b17474e51123aa
Ryan Cebulko <ryan@cebulko.com>
2021-05-26 15:21:16 -0400

    Add aliases for top directories

--- build-system/babel-config/import-resolver.js
+++ build-system/babel-config/import-resolver.js
@@ -62,14 +62,6 @@ function getImportResolver() {
   return {
     'root': ['.'],
     'alias': readJsconfigPaths(),
-    // Below include paths slated to be updated. Leaving them commented for
-    // now so that each one can be added and its imports updated.
-    // '#polyfills': './src/polyfills',
-    // '#3p': './3p',
-    // '#ads': './ads',
-    // '#context': './src/context',
-    // '#preact': './src/preact',
-    // '#service': './src/service',
   };
 }
 
