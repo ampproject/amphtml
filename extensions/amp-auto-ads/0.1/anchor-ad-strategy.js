58d9b193a4955f9cd7d073a120660bb7d4c6970d
Pablo <rocha.pabloricardo@gmail.com>
2021-05-27 14:21:49 -0300

    🐛 Bug fix allowing set height and fixing Ad position (#34092)
    * allowing add data-attributes
    
    * using dataset for shorter code
    
    * fixing error use of style to style elements is forbidden
    
    * fixing default attribute values
    
    * removing sizes
    
    * allowing use min height
    
    * fixing banner alignment
    
    * fixing test
    
    * fixing prettier validation
    
    * fixing prettier validation

--- extensions/amp-auto-ads/0.1/anchor-ad-strategy.js
+++ extensions/amp-auto-ads/0.1/anchor-ad-strategy.js
@@ -87,14 +87,15 @@ export class AnchorAdStrategy {
    * @private
    */
   placeStickyAd_() {
+    const baseAttributes = this.baseAttributes_;
     const viewportWidth = Services.viewportForDoc(this.ampdoc).getWidth();
     const attributes = /** @type {!JsonObject} */ (
       Object.assign(
         dict(),
-        this.baseAttributes_,
+        baseAttributes,
         dict({
           'width': String(viewportWidth),
-          'height': '100',
+          'height': baseAttributes.height || '100',
         })
       )
     );
