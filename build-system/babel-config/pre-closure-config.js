9e0d2bf968500523ac0fd25a1bb82a933dcb5275
Alan Orozco <alan@orozco.xyz>
2021-05-27 14:01:13 -0700

    🏗 Add Babel plugin to optionally use imported helpers

--- build-system/babel-config/pre-closure-config.js
+++ build-system/babel-config/pre-closure-config.js
@@ -41,6 +41,7 @@ function getPreClosureConfig() {
   const preClosurePlugins = [
     'optimize-objstr',
     argv.coverage ? 'babel-plugin-istanbul' : null,
+    './build-system/babel-plugins/babel-plugin-imported-helpers',
     './build-system/babel-plugins/babel-plugin-transform-fix-leading-comments',
     './build-system/babel-plugins/babel-plugin-transform-promise-resolve',
     '@babel/plugin-transform-react-constant-elements',
