0c326e4f7d87782ae88fe184d2a125dc9ea4c262
Renee Woo <renee@jixie.io>
2021-05-28 16:29:53 +0800

    changed to do a universal ad

--- ads/vendors/jixie.js
+++ ads/vendors/jixie.js
@@ -15,14 +15,14 @@
  */
 
  import {loadScript, validateData} from '../../3p/3p';
-
+/* 
  export function jixie(global, data) {
      console.log("!!!! CALLED jixie");
      //just this copied from Teads TEMPORARILY just so you can SEE something show up
      //later we use what is in the REAL_jixie ...
      //and you comment out this function!!
 
-    /*eslint "google-camelcase/google-camelcase": 0*/
+    //eslint "google-camelcase/google-camelcase": 0
     global._teads_amp = {
       allowed_data: ['pid', 'tag'],
       mandatory_data: ['pid'],
@@ -53,17 +53,41 @@
       );
     }
   }
-/*
- export function REAL_jixie(global, data, scriptLoader = loadScript) {
-   validateData(data, ['unit']);
- 
-   global.jixie3p = global.jixie3p || {};
-   global.jixie3p.params = {
-     unit: data.unit
-   };
+  */
+      
+							   
+ export function jixie(global, data, scriptLoader = loadScript) {
+   //validateData(data, ['unit']);
+   const d = global.document.createElement('div');
+   d.setAttribute('id', 'jxOutstreamContainer');
+   global.document.getElementById('c').appendChild(d);
+   
+   var p = {
+    responsive: 1, 
+    maxwidth: 640, 
+    unit: "62dfd0d28588b4a2ed791b90dda06fce",
+    container: "jxOutstreamContainer", // indicates where to create the iFrame, otherwise will be created as a child of the parent node
+    creativeid: 613,
+  };
+  function jxdefer(p) {
+    if (global.jxuniversal) {
+          global.jxuniversal.init(p);
+    } else {
+          setTimeout(function() { jxdefer(p) }, 100);
+    }
+  }
+  jxdefer(p);
+   //jixie3p = global.jixie3p || {};
+   //global.jixie3p.params = {
+     //unit: data.unit
+   //};
    //there is a loadScript and there is a writeScript
    //please check what the difference is. I think to do with sychronouse or asynchronous loading
-   scriptLoader(global, 'https://jixie-creative-debug.s3-ap-southeast-1.amazonaws.com/ferytests/amp.js');
+   //
+   loadScript(
+    global,
+    'https://scripts.jixie.io/jxfriendly.1.3.min.js'
+  );
  }
- */
+ 
  
