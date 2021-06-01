19ad93e3d6c1452a0ceb934401fa7ac60ef2d405
Raghu Simha <rsimha@amp.dev>
2021-06-01 16:52:29 -0400

    🏗 Shorten copyright comments in esm and non-esm minified output (#34638)

--- third_party/webcomponentsjs/ShadowCSS.js
+++ third_party/webcomponentsjs/ShadowCSS.js
@@ -3,7 +3,8 @@
 * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
 * Use of this source code is governed by a BSD-style
 * license that can be found in the LICENSE file or at
-* https://developers.google.com/open-source/licenses/bsd */
+* https://developers.google.com/open-source/licenses/bsd
+*/
 
 /*
   This is a limited shim for ShadowDOM css styling.
