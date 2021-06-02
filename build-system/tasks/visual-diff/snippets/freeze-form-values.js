9f7344a6066f15641de769ce6edf60b1a1c3fc6b
Raghu Simha <rsimha@amp.dev>
2021-06-02 18:49:20 -0400

    🏗 Enable type-checking for `build-system/tasks/visual-diff` (#34653)

--- build-system/tasks/visual-diff/snippets/freeze-form-values.js
+++ build-system/tasks/visual-diff/snippets/freeze-form-values.js
@@ -1,14 +1,31 @@
-// This file is executed via Puppeteer's page.evaluate on a document to copy the
-// values of forms into their attributes, so that they will be passed in the
-// snapshots to Percy.
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-for (const form of document.forms) {
-  for (const formElement of form.elements) {
-    switch (formElement.tagName) {
+/**
+ * @fileoverview This file is executed via Puppeteer's page.evaluate on a
+ * document to copy the values of forms into their attributes, so that they will
+ * be passed in the snapshots to Percy.
+ */
+
+for (const form of Array.from(document.forms)) {
+  for (const element of Array.from(form.elements)) {
+    switch (element.tagName) {
       case 'INPUT':
-        // Need to update this? Look at
-        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement#Properties
-        // for inspiration.
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
+        const formElement = /** @type {!HTMLInputElement} */ (element);
         switch (formElement.type) {
           case 'file':
           case 'hidden':
@@ -18,9 +35,9 @@ for (const form of document.forms) {
           case 'checkbox':
           case 'radio':
             if (formElement.checked) {
-                formElement.setAttribute('checked', '');
+              formElement.setAttribute('checked', '');
             } else {
-                formElement.removeAttribute('checked');
+              formElement.removeAttribute('checked');
             }
             break;
 
@@ -30,7 +47,9 @@ for (const form of document.forms) {
         break;
 
       case 'SELECT':
-        for (const optionElement of formElement.options) {
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptionElement
+        const selectElement = /** @type {!HTMLSelectElement} */ (element);
+        for (const optionElement of Array.from(selectElement.options)) {
           if (optionElement.selected) {
             optionElement.setAttribute('selected', '');
           } else {
@@ -40,7 +59,9 @@ for (const form of document.forms) {
         break;
 
       case 'TEXTAREA':
-        formElement.textContent = formElement.value;
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLTextAreaElement
+        const textElement = /** @type {!HTMLTextAreaElement} */ (element);
+        textElement.textContent = textElement.value;
         break;
     }
   }
