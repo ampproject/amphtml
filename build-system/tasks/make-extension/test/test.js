33e9fc7328069b10f9d961a294ef48164ee81384
Alan Orozco <alanorozco@users.noreply.github.com>
2021-06-04 12:39:57 -0700

    🏗 Extension generator inserts in order: `name`, `version`, `latestVersion` (#34713)

--- build-system/tasks/make-extension/test/test.js
+++ build-system/tasks/make-extension/test/test.js
@@ -219,14 +219,15 @@ test('insertExtensionBundlesConfig inserts new entry', (t) =>
 
       await insertExtensionBundlesConfig(
         {
-          name: 'a',
           version: 'x',
+          name: 'a',
           options: {hasCss: true},
         },
         destination
       );
 
-      t.deepEqual(await readJson(destination), [
+      const items = await readJson(destination);
+      t.deepEqual(items, [
         // inserted in lexicographical order by name:
         {
           name: '_',
@@ -240,6 +241,14 @@ test('insertExtensionBundlesConfig inserts new entry', (t) =>
         {
           name: 'z',
         },
+      ]);
+
+      // expected order of keys
+      t.deepEqual(Object.keys(items[1]), [
+        'name',
+        'version',
+        'latestVersion',
+        'options',
       ]);
     },
     {extension: 'json'}
