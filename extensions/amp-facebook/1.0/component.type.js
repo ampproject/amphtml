3390233afc7e5ebde9f0b0d529845a1970243ce6
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2021-06-07 11:36:23 -0400

    ✨ Bento amp-facebook-like (#34715)
    * Add amp-facebook-like
    
    * latestVersion precedes options
    
    * Update Storybook
    
    * Update dep-check-config

--- extensions/amp-facebook/1.0/component.type.js
+++ extensions/amp-facebook/1.0/component.type.js
@@ -21,17 +21,23 @@ var FacebookDef = {};
 
 /**
  * @typedef {{
+ *   action: (string|undefined),
  *   colorScheme: (string|undefined),
  *   hideCta: (string|undefined),
  *   hideCover: (string|undefined),
  *   href: (string|undefined),
  *   loading: (string|undefined),
+ *   kdSite: (boolean|undefined),
  *   locale: (string|undefined),
+ *   layout: (string|undefined),
  *   numPosts: (number|undefined),
  *   orderBy: (string|undefined),
  *   onReadyState: (function(string, *=)|undefined),
+ *   refLabel: (string|undefined),
  *   requestResize: (function(number):*|undefined),
+ *   share: (boolean|undefined),
  *   showFacepile: (string|undefined),
+ *   size: (string|undefined),
  *   smallHeader: (string|undefined),
  *   tabs: (string|undefined),
  *   title: (string|undefined),
