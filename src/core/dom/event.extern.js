811349376fc6a0e98a6f08a2758cdba275e80dd4
Ryan Cebulko <ryan@cebulko.com>
2021-06-07 15:25:25 -0400

    Move dom to src/core
    Fix up types and modernize some syntax

--- src/core/dom/event.extern.js
+++ src/core/dom/event.extern.js
@@ -14,4 +14,10 @@
  * limitations under the License.
  */
 
-export * from './core/dom';
+/**
+ * @fileoverview Externs for properties that might be seen/added to Events.
+ * @externs
+ */
+
+/** @type {undefined|!Object} */
+Event.prototype.data;
