6fd27b3159c2b92afbd50749c0ef9bcf465c7e85
Jake Fried <samouri@users.noreply.github.com>
2021-06-09 18:34:42 -0400

    ryan nit

--- server-render/index.ts
+++ server-render/index.ts
@@ -1,12 +1,1 @@
-import {parse, print} from './ast';
-import {renderAmpAst} from './render';
-
-/*
- * Renders all of the eligible AMP Components within an AMP Document
- */
-export function renderAmpHtml(html: string): string {
-  const ast = parse(html);
-  return print(renderAmpAst(ast));
-}
-
-export {renderAmpAst};
+export * from './render';
--- server-render/render.ts
+++ server-render/render.ts
@@ -1,4 +1,4 @@
-import {TreeProto} from './ast';
+import {parse, TreeProto, print} from './ast';
 
 /*
  * Returns a new AST created by walking the entire tree and rendering each
@@ -6,4 +6,12 @@ import {TreeProto} from './ast';
  */
 export function renderAmpAst(ast: TreeProto): TreeProto {
   throw new Error('TODO');
+}
+
+/*
+ * Renders all of the eligible AMP Components within an AMP Document
+ */
+export function renderAmpHtml(html: string): string {
+  const ast = parse(html);
+  return print(renderAmpAst(ast));
 }
