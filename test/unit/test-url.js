334f3f4a2bda673c48cff480df25625935ce2385
Ryan Cebulko <ryan@cebulko.com>
2021-06-11 15:58:34 -0400

    Remove test that has never passed in ESM

--- test/unit/test-url.js
+++ test/unit/test-url.js
@@ -220,10 +220,6 @@ describes.sandboxed('parseUrlDeprecated', {}, () => {
     );
   });
 
-  it('should parse origin data:12345', () => {
-    expect(parseUrlDeprecated('data:12345').origin).to.equal('data:12345');
-  });
-
   it('should parse relative', () => {
     expect(parseUrlDeprecated('chilaquiles/rojos')).to.include({
       pathname: '/chilaquiles/rojos',
