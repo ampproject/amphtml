23be5cf9ff8802a00e51764e015aaaaac6cbe041
Ryan Cebulko <ryan@cebulko.com>
2021-06-21 13:56:23 -0400

    Flatten core/dom/stream namespace

--- src/core/dom/stream/index.js
+++ src/core/dom/stream/index.js
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-export {DetachedDomStream} from './detached';
-export {streamResponseToWriter} from './response';
+export * from './detached';
+export * from './response';
 // TODO(#32693): Migrate DomTransformStream into core to finish the trio
