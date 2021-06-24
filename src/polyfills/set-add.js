ed4ffe7e9c3fa6461bbca14af32925b80e8792c6
Alan Orozco <alan@orozco.xyz>
2021-06-24 11:25:47 -0700

    conformance/types

--- src/polyfills/set-add.js
+++ src/polyfills/set-add.js
@@ -20,27 +20,29 @@ import {toArray} from '#core/types/array';
  * @param {!Window} win
  */
 export function install(win) {
-  const {Set} = win;
+  const {Set: SetConstructor} = win;
   const s = new Set([1]);
   // Add suppport for `new Set(iterable)`. IE11 lacks it.
   if (s.size < 1) {
-    win.Set = function (iterable) {
-      const set = new Set();
-      if (iterable) {
-        const asArray = toArray(iterable);
-        for (let i = 0; i < asArray.length; i++) {
-          set.add(asArray[i]);
+    win.Set = /** @type {typeof Set} */ (
+      function (iterable) {
+        const set = new SetConstructor();
+        if (iterable) {
+          const asArray = toArray(iterable);
+          for (let i = 0; i < asArray.length; i++) {
+            set.add(asArray[i]);
+          }
         }
+        return set;
       }
-      return set;
-    };
+    );
   }
   // Forces the return value from Set.prototype.add to always be the set
   // instance. IE11 returns undefined.
   if (s.add(0) !== s) {
     const {add} = s;
 
-    win.Object.defineProperty(Set.prototype, 'add', {
+    win.Object.defineProperty(SetConstructor.prototype, 'add', {
       enumerable: false,
       configurable: true,
       writable: true,
