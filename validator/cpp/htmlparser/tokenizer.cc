4eb375b02e0d0582dbbd5d192fc4a866753971d9
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2021-06-25 12:24:39 -0700

    Sync for validator/cpp/{engine,htmlparser} (#35044)
    * - Add support for recording node's start/end offset in original html src. This in addition to line/col already supported in htmlparser.
    - Add support for recording number of words (num_terms) in a text node.
    
    PiperOrigin-RevId: 378533243
    
    * - Add support for recording node's start/end offset in original html src. This in addition to line/col already supported in htmlparser.
    - Add support for recording number of words (num_terms) in a text node.
    
    PiperOrigin-RevId: 378533243
    
    Co-authored-by: Amaltas Bohra <amaltas@google.com>

--- validator/cpp/htmlparser/tokenizer.cc
+++ validator/cpp/htmlparser/tokenizer.cc
@@ -659,6 +659,7 @@ void Tokenizer::ReadTag(bool save_attr, bool template_mode) {
     }
     SkipWhiteSpace();
   }
+  position_end_ = raw_.end;
 }
 
 void Tokenizer::ReadTagName() {
@@ -843,9 +844,11 @@ void Tokenizer::ReadTagAttributeValue() {
 }
 
 TokenType Tokenizer::Next(bool template_mode) {
+  position_ = raw_.end;
   raw_.start = raw_.end;
   data_.start = raw_.end;
   data_.end = raw_.end;
+  num_terms_ = 0;
   is_token_manufactured_ = false;
 
   if (eof_) {
@@ -869,6 +872,7 @@ TokenType Tokenizer::Next(bool template_mode) {
     if (data_.end > data_.start) {
       token_type_ = TokenType::TEXT_TOKEN;
       convert_null_ = true;
+      position_end_ = data_.end;
       return token_type_;
     }
   }
@@ -916,6 +920,7 @@ TokenType Tokenizer::Next(bool template_mode) {
       // We know there is no \n so no line adjustment needed.
       current_line_col_.second -= 2;
       token_type_ = TokenType::TEXT_TOKEN;
+      position_end_ = data_.end;
       return token_type_;
     }
 
@@ -1044,7 +1049,7 @@ std::optional<std::tuple<Attribute, bool>> Tokenizer::TagAttr() {
             {.name_space = "",
              .key = std::move(key),
              .value = std::move(val),
-             .position_in_html_src = std::get<LineCol>(attr)},
+             .line_col_in_html_src = std::get<LineCol>(attr)},
             n_attributes_returned_ < attributes_.size());
       }
       default:
@@ -1075,6 +1080,7 @@ Token Tokenizer::token() {
         }
       }
       token_line_col_ = {line_number, column_number};
+      position_end_ = data_.end;
       break;
     }
     case TokenType::COMMENT_TOKEN:
@@ -1083,6 +1089,7 @@ Token Tokenizer::token() {
       t.is_manufactured = is_token_manufactured_;
       token_line_col_ = {current_line_col_.first,
                          current_line_col_.second - t.data.size()};
+      position_end_ = data_.end;
       break;
     case TokenType::START_TAG_TOKEN:
     case TokenType::SELF_CLOSING_TAG_TOKEN:
@@ -1116,7 +1123,8 @@ Token Tokenizer::token() {
       break;
   }
 
-  t.position_in_html_src = token_line_col_;
+  t.line_col_in_html_src = token_line_col_;
+  t.offsets_in_html_src = std::make_pair(position_, position_end_);
   return t;
 }
 
