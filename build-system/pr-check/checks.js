bbc258ae2d09f34613c80311fdd4bee9c5f70d7c
Caroline Liu <carolineliu@google.com>
2021-06-25 14:20:49 -0400

    Test amp make-extension with foo instead of t

--- build-system/pr-check/checks.js
+++ build-system/pr-check/checks.js
@@ -42,8 +42,8 @@ function pushBuildWorkflow() {
   timedExecOrDie('amp check-exact-versions');
   timedExecOrDie('amp check-renovate-config');
   timedExecOrDie('amp server-tests');
-  timedExecOrDie('amp make-extension --name=t --test --cleanup');
-  timedExecOrDie('amp make-extension --name=t --test --cleanup --bento');
+  timedExecOrDie('amp make-extension --name=foo --test --cleanup');
+  timedExecOrDie('amp make-extension --name=foo --test --cleanup --bento');
   timedExecOrDie('amp dep-check');
   timedExecOrDie('amp check-types');
   timedExecOrDie('amp check-sourcemaps');
@@ -121,8 +121,8 @@ async function prBuildWorkflow() {
   }
 
   if (buildTargetsInclude(Targets.AVA, Targets.RUNTIME)) {
-    timedExecOrDie('amp make-extension --name=t --test --cleanup');
-    timedExecOrDie('amp make-extension --name=t --test --cleanup --bento');
+    timedExecOrDie('amp make-extension --name=foo --test --cleanup');
+    timedExecOrDie('amp make-extension --name=foo --test --cleanup --bento');
   }
 
   if (buildTargetsInclude(Targets.RUNTIME)) {
