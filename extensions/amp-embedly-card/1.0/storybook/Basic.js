63dc3060bf6b891e92200a88c59b0faa9426317b
AnuragVasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-06-30 00:45:03 +0530

    ✨ Bento `amp-embedly-card` (#35034)
    * Initial Commit
    
    * Test Commit - With demo storybook
    
    * ✨ Main component, Storybook, Test Cases
    
    * ✨ Card and Key Component, Storybook, Test Cases
    
    * 📖 YAML updated with bento and experimental attribute
    
    * 🏗 'bundles.config.extension.json' updated with npm and bento wrapper
    
    * 🏗 Fixed error reported by Lint
    
    * 🏗 Prettify, ♻️ Preact Storybook port update
    
    * 🚮 Removed 'loadable', ♻️ Updated test-case with problem description
    
    * 🐛 Fix for "renders responsively" test case
    
    The responsive class name is put on the element, not the inner iframe.
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ♻️ Fix for test case - 'throws when data-url is not given'
    
    * ♻️ Fix for test case - 'throws when data-url is not given'
    
    * 🐛 Fixs for BaseElement and AMP Component
    
    * 🐛 Renamed 'isLayout' to 'isLayoutSupported' in preact
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ♻️ 'forceChangeHeight' updated to 'attemptChangeHeight'
    
    * ♻️ Improved checking for 'url' prop
    
    We also want to exclude `null` and `''`.
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ♻️ Updated warning message for 'url' prop
    
    * ♻️ Removed DOM APIs in Preact Layer with EmbedlyContext
    
    * ♻️ Enable single experiments for all components - 'bento-embedly-card'
    
    Let's have this use the same experiment as the `amp-embedly-card` component for smoother devx - should be safe to assume that enabling one experiment can imply the api key feature for this component.
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * 🚮 `ProxyIframeEmbed` and `IframeEmbed` don't propagate children props anyway
    
    * ♻️ Simplified check condition for `url` prop
    
    Same as:
    ```
    if (!url) {
      return false;
    }
    return true;
    ```
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ⏪ Removed `requires_usage` and `deprecated_allow_duplicates`
    
    Should not be added if not already present, since these are looser validations than are necessary.
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ♻️ Updated validator rules
    
    We differentiate versions when adding validator rules with `satisfies`/`excludes` when the rules differ between versions. Since the two versions have `requires_usage` and `deprecated_allow_duplicates` in common (omitted values), their set of validation rules is identical. Therefore we can just do this (effectively the only modification in this file is to insert `version: "1.0"` between `"0.1"` and `"latest"`.
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * 🚮 Removed `hello` child from `amp-embedly-key` with `layout="no display"`
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>
    
    * ✅ Added test to check iframe `height` and `width` with `layout=responsive`
    
    * 📖 Added storybook example for Embedly API Key
    
    * ♻️ Updated test to check `iframe` dimensions with parent element
    
    * 🏗 Updates as per lint error report fro CI
    
    * ♻️ Fixed assert message for experiment
    
    * ♻️ Grouped valid examples together at the top
    
    Co-authored-by: Caroline Liu <10456171+caroqliu@users.noreply.github.com>

---
+++ extensions/amp-embedly-card/1.0/storybook/Basic.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Preact from '#preact';
+import {EmbedlyCard} from '../component';
+import {EmbedlyContext} from '../embedly-context';
+import {text, withKnobs} from '@storybook/addon-knobs';
+
+export default {
+  title: 'EmbedlyCard',
+  component: EmbedlyCard,
+  decorators: [withKnobs],
+};
+
+export const _default = () => {
+  return (
+    <EmbedlyCard
+      bootstrap="http://localhost:9001/dist.3p/current/vendor/embedly.max.js"
+      src="http://ads.localhost:9001/dist.3p/current/frame.max.html"
+      url="https://www.youtube.com/watch?v=lBTCB7yLs8Y"
+      title="Embedly Card"
+      style={{width: '400px', height: '400px'}}
+    />
+  );
+};
+
+export const WithAPIKey = () => {
+  const apiKey = text('Embedly API Key', 'valid-api-key');
+
+  return (
+    <EmbedlyContext.Provider value={apiKey}>
+      <EmbedlyCard
+        bootstrap="http://localhost:9001/dist.3p/current/vendor/embedly.max.js"
+        src="http://ads.localhost:9001/dist.3p/current/frame.max.html"
+        url="https://www.youtube.com/watch?v=lBTCB7yLs8Y"
+        title="Embedly Card"
+        style={{width: '400px', height: '400px'}}
+      />
+    </EmbedlyContext.Provider>
+  );
+};
