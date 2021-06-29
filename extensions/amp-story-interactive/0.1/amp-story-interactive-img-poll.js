cf83aad7af066f578fdb0de7d35de5d063108ad0
Brandon Suen <brandonbsuen@google.com>
2021-06-29 12:51:28 -0700

    Add TODO

--- extensions/amp-story-interactive/0.1/amp-story-interactive-img-poll.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-img-poll.js
@@ -98,6 +98,7 @@ export class AmpStoryInteractiveImgPoll extends AmpStoryInteractive {
     convertedOption.optionIndex_ = option['optionIndex'];
 
     // Extract and structure the option information
+    // TODO: Rewrite image URL (https://github.com/ampproject/amphtml/pull/35043#discussion_r660874389)
     setStyle(
       convertedOption.querySelector(
         '.i-amphtml-story-interactive-img-option-img'
