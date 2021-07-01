3c455dd436393568b502461ca4b70e564e537ec9
Alan Orozco <alan@orozco.xyz>
2021-07-01 08:28:54 -0700

    copyright/doc

--- src/polyfills/custom-elements-entrypoint.js
+++ src/polyfills/custom-elements-entrypoint.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * @fileoverview
+ * Entry point for `custom-elements-polyfill.js`, used on Bento documents.
  */
 
 import {install} from './custom-elements';
