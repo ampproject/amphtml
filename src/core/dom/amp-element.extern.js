2efbf8f57f8471fd41ee29fbd6694f510ae9402a
Ryan Cebulko <ryan@cebulko.com>
2021-07-04 14:51:25 -0400

    Define AmpElement extern and integrate PausableInterface

--- src/core/dom/amp-element.extern.js
+++ src/core/dom/amp-element.extern.js
@@ -15,10 +15,27 @@
  */
 
 /**
+ * @fileoverview Provides AmpElement interface for typechecking. Includes
+ * smaller interfaces that modules can type-check against to keep expected
+ * API surfaces narrow, so it's clear what properties and methods are
+ * expected/required.
+ *
+ * @externs
+ */
+
+/**
  * An interface for elements with pause functionality.
  * @interface
  */
-export class PausableInterface {
+class PausableInterface {
   /** @function */
   pause() {}
 }
+
+/**
+ * Just an element, but used with AMP custom elements..
+ * @constructor
+ * @extends {HTMLElement}
+ * @implements {PausableInterface}
+ */
+let AmpElement = function () {};
