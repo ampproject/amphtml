80e72913404fb17bc1a3efdfe043723aed671870
olivererdmann <o.erdmann@finaldream.de>
2021-07-05 07:29:34 +0200

    [amp-native-player]: initial commit

---
+++ extensions/amp-native-player/0.1/test/test-amp-native-player.js
@@ -0,0 +1,246 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../amp-brid-player';
+import {Services} from '#service';
+import {VideoEvents} from '../../../../src/video-interface';
+import {listenOncePromise} from '../../../../src/event-helper';
+
+describes.realWin(
+  'amp-brid-player',
+  {
+    amp: {
+      extensions: ['amp-brid-player'],
+    },
+  },
+  (env) => {
+    let win, doc;
+    let timer;
+
+    beforeEach(() => {
+      win = env.win;
+      doc = win.document;
+      timer = Services.timerFor(win);
+    });
+
+    function getBridPlayer(attributes, opt_responsive) {
+      const bc = doc.createElement('amp-brid-player');
+
+      for (const key in attributes) {
+        bc.setAttribute(key, attributes[key]);
+      }
+      bc.setAttribute('width', '640');
+      bc.setAttribute('height', '360');
+      if (opt_responsive) {
+        bc.setAttribute('layout', 'responsive');
+      }
+
+      // see yt test implementation
+      timer
+        .promise(50)
+        .then(() => bc.getImpl())
+        .then((impl) => {
+          const bridTimerIframe = bc.querySelector('iframe');
+
+          impl.handleBridMessage_({
+            origin: 'https://services.brid.tv',
+            source: bridTimerIframe.contentWindow,
+            data: 'Brid|0|trigger|ready',
+          });
+        });
+      doc.body.appendChild(bc);
+      return bc
+        .buildInternal()
+        .then(() => {
+          bc.layoutCallback();
+        })
+        .then(() => bc);
+    }
+
+    it('renders', () => {
+      return getBridPlayer({
+        'data-partner': '264',
+        'data-player': '4144',
+        'data-video': '13663',
+      }).then((bc) => {
+        const iframe = bc.querySelector('iframe');
+        expect(iframe).to.not.be.null;
+        expect(iframe.tagName).to.equal('IFRAME');
+        expect(iframe.src).to.equal(
+          'https://services.brid.tv/services/iframe/video/13663/264/4144/0/1/?amp=1'
+        );
+      });
+    });
+
+    it('renders responsively', () => {
+      return getBridPlayer(
+        {
+          'data-partner': '1177',
+          'data-player': '979',
+          'data-video': '5204',
+        },
+        true
+      ).then((bc) => {
+        const iframe = bc.querySelector('iframe');
+        expect(iframe).to.not.be.null;
+        expect(iframe.className).to.match(/i-amphtml-fill-content/);
+      });
+    });
+
+    it('requires data-partner', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-player': '4144',
+          'data-video': '13663',
+        }).should.eventually.be.rejectedWith(
+          /The data-partner attribute is required for/
+        );
+      });
+    });
+
+    it('requires data-player', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-partner': '264',
+          'data-video': '13663',
+        }).should.eventually.be.rejectedWith(
+          /The data-player attribute is required for/
+        );
+      });
+    });
+
+    it('requires data-partner for playlists', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-player': '4144',
+          'data-playlist': '13663',
+        }).should.eventually.be.rejectedWith(
+          /The data-partner attribute is required for/
+        );
+      });
+    });
+
+    it('requires data-player for playlists', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-partner': '264',
+          'data-playlist': '13663',
+        }).should.eventually.be.rejectedWith(
+          /The data-player attribute is required for/
+        );
+      });
+    });
+
+    it('requires data-partner for carousels', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-player': '4144',
+          'data-carousel': '459',
+        }).should.eventually.be.rejectedWith(
+          /The data-partner attribute is required for/
+        );
+      });
+    });
+
+    it('requires data-player for carousels', () => {
+      return allowConsoleError(() => {
+        return getBridPlayer({
+          'data-partner': '264',
+          'data-carousel': '459',
+        }).should.eventually.be.rejectedWith(
+          /The data-player attribute is required for/
+        );
+      });
+    });
+
+    it('should forward events from brid-player to the amp element', async () => {
+      const bc = await getBridPlayer(
+        {
+          'data-partner': '1177',
+          'data-player': '979',
+          'data-video': '5204',
+        },
+        true
+      );
+      const impl = await bc.getImpl();
+
+      const iframe = bc.querySelector('iframe');
+      return Promise.resolve()
+        .then(() => {
+          const p = listenOncePromise(bc, VideoEvents.PLAYING);
+          sendFakeMessage(impl, iframe, 'trigger|play');
+          return p;
+        })
+        .then(() => {
+          const p = listenOncePromise(bc, VideoEvents.MUTED);
+          sendFakeMessage(impl, iframe, 'volume|0');
+          return p;
+        })
+        .then(() => {
+          const p = listenOncePromise(bc, VideoEvents.PAUSE);
+          sendFakeMessage(impl, iframe, 'trigger|pause');
+          return p;
+        })
+        .then(() => {
+          const p = listenOncePromise(bc, VideoEvents.UNMUTED);
+          sendFakeMessage(impl, iframe, 'volume|1');
+          return p;
+        });
+    });
+
+    function sendFakeMessage(impl, iframe, command) {
+      impl.handleBridMessage_({
+        origin: 'https://services.brid.tv',
+        source: iframe.contentWindow,
+        data: 'Brid|0|' + command,
+      });
+    }
+
+    describe('createPlaceholderCallback', () => {
+      it('should create a placeholder image', () => {
+        return getBridPlayer({
+          'data-partner': '264',
+          'data-player': '979',
+          'data-video': '13663',
+        }).then((brid) => {
+          const img = brid.querySelector('img');
+          expect(img).to.not.be.null;
+          expect(img.getAttribute('src')).to.equal(
+            'https://cdn.brid.tv/live/partners/264/snapshot/13663.jpg'
+          );
+          expect(img).to.have.class('i-amphtml-fill-content');
+          expect(img).to.have.attribute('placeholder');
+          expect(img.getAttribute('alt')).to.equal('Loading video');
+          expect(img.getAttribute('referrerpolicy')).to.equal('origin');
+        });
+      });
+      it('should propagate aria label for placeholder image', () => {
+        return getBridPlayer({
+          'data-partner': '264',
+          'data-player': '979',
+          'data-video': '13663',
+          'aria-label': 'great video',
+        }).then((brid) => {
+          const img = brid.querySelector('img');
+          expect(img).to.not.be.null;
+          expect(img.getAttribute('alt')).to.equal(
+            'Loading video - great video'
+          );
+        });
+      });
+    });
+  }
+);
---
+++ extensions/amp-native-player/amp-native-player.md
@@ -0,0 +1,126 @@
+---
+$category@: media
+formats:
+  - websites
+teaser:
+  text: Displays the Native-Player by target-video.com
+---
+
+<!---
+Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# amp-native-player
+
+An `amp-native-player` displays the Native Player by [Target Video](https://www.target-video.com/).
+
+The `width` and `height` attributes determine the aspect ratio of the player embedded in responsive layouts.
+
+```html
+<amp-native-player
+  data-partner="264"
+  data-player="4144"
+  data-video="13663"
+  layout="responsive"
+  width="480"
+  height="270"
+>
+</amp-native-player>
+```
+
+### `autoplay`
+
+If this attribute is present, and the browser supports autoplay:
+
+-   the video is automatically muted before autoplay starts
+-   when the video is scrolled out of view, the video is paused
+-   when the video is scrolled into view, the video resumes playback
+-   when the user taps the video, the video is unmuted
+-   if the user has interacted with the video (e.g., mutes/unmutes,
+    pauses/resumes, etc.), and the video is scrolled in or out of view, the
+    state of the video remains as how the user left it. For example, if the user
+    pauses the video, then scrolls the video out of view and returns to the
+    video, the video is still paused.
+
+### `data-partner`
+
+The Target Video partner ID.
+
+### `data-player`
+
+The Target Video player ID. Specific to every partner.
+
+### `data-video`
+
+The Target Video video ID. Embed code must either have `video`, `playlist`, `carousel`
+or `outstream` attribute.
+
+### `data-playlist`
+
+The Target Video playlist ID or custom string value for dynamic playlists. Embed code
+must either have `video`, `playlist`, `carousel` or `outstream` attribute.
+
+### `data-carousel`
+
+The Target Video carousel ID. Embed code must either have `video`, `playlist`, `carousel`
+or `outstream` attribute.
+
+### `data-outstream`
+
+The Target Video outstream unit ID. Embed code must either have `video`, `playlist`, `carousel`
+or `outstream` attribute.
+
+### `data-dynamic`
+
+Parameter used to specify type of dynamic playlist, e.g. latest, channel, tag.
+
+### `dock`
+
+**Requires `amp-video-docking` extension.** If this attribute is present and the
+video is playing manually, the video will be "minimized" and fixed to a corner
+or an element when the user scrolls out of the video component's visual area.
+
+For more details, see [documentation on the docking extension itself](https://amp.dev/documentation/components/amp-video-docking).
+
+### Common attributes
+
+This element includes [common attributes](https://amp.dev/documentation/guides-and-tutorials/learn/common_attributes)
+extended to AMP components.
+
+## Actions
+
+### `play`
+
+Plays the video.
+
+### `pause`
+
+Pauses the video.
+
+### `mute`
+
+Mutes the video.
+
+### `unmute`
+
+Unmutes the video.
+
+### `fullscreenenter`
+
+Takes the video to fullscreen.
+
+## Validation
+
+See [amp-native-player rules](https://github.com/ampproject/amphtml/blob/main/extensions/amp-native-player/validator-amp-native-player.protoascii) in the AMP validator specification.
