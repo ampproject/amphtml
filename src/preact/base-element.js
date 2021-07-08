c927554350f9719a1f984943e70f225a4ae6642f
Caroline Liu <carolineliu@google.com>
2021-07-08 13:12:11 -0400

    return super.attemptChangeHeight()

--- src/preact/base-element.js
+++ src/preact/base-element.js
@@ -384,7 +384,7 @@ export class PreactBaseElement extends AMP.BaseElement {
 
   /** @override */
   attemptChangeHeight(newHeight) {
-    super.attemptChangeHeight(newHeight).catch(() => {
+    return super.attemptChangeHeight(newHeight).catch(() => {
       if (this.getOverflowElement && !this.getOverflowElement()) {
         console./* OK */ warn(
           '[overflow] element not found. Provide one to enable resizing to full contents.',
