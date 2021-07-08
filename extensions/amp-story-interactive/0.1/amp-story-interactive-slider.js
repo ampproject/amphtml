92a56a9e5af78786789127aa207eca6af7833ee9
syydneyyhardister <shardister3@gmail.com>
2021-07-08 10:40:50 -0700

    post state and clean up

--- extensions/amp-story-interactive/0.1/amp-story-interactive-slider.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-slider.js
@@ -21,6 +21,7 @@ import {
 import {CSS} from '../../../build/amp-story-interactive-slider-0.1.css';
 import {htmlFor} from '#core/dom/static-template';
 import {scopedQuerySelector, scopedQuerySelectorAll} from '#core/dom/query';
+import { setImportantStyles } from '#core/dom/style';
 
 /**
  * Generates the template for the slider.
@@ -40,24 +41,20 @@ const buildSliderTemplate = (element) => {
       <div class="i-amphtml-story-interactive-prompt-container"></div>
       <div class="i-amphtml-story-interactive-slider-input-container">
         <div class="i-amphtml-story-interactive-slider-field">
-          <div class="i-amphtml-story-interactive-slidervalue">
-            0
+          <div class="i-amphtml-story-interactive-slidervalue">0</div>
+          <div class="i-amphtml-story-interactive-slider-input-size">
+            <input
+              class="i-amphtml-story-interactive-slider-input"
+              type="range"
+              min="0"
+              max="100"
+              value="0"
+            />
+            <div class="i-amphtml-story-interactive-slider-sliderValue span">
+              <span>50</span>
+            </div>
           </div>
-        <div class="i-amphtml-story-interactive-slider-input-size">
-          <input
-            class="i-amphtml-story-interactive-slider-input"
-            type="range"
-            min="0"
-            max="100"
-            value="0"
-          />
-          <div class="i-amphtml-story-interactive-slider-sliderValue span">
-          <span>50</span>
-        </div>
-          </div>
-          <div class="i-amphtml-story-interactive-slidervalue">
-            100
-          </div>
+          <div class="i-amphtml-story-interactive-slidervalue">100</div>
         </div>
       </div>
     </div>
@@ -77,28 +74,47 @@ export class AmpStoryInteractiveSlider extends AmpStoryInteractive {
   }
 
   /** @override */
+  layoutCallback() {
+    this.initializeListeners_();
+  }
+
+  /** @override */
+  initializeListeners_(){
+    console.log("running");
+    super.initializeListeners_();
+    const sliderValue = scopedQuerySelector(this.rootEl_, 'span');
+    const inputSlider = scopedQuerySelector(this.rootEl_, 'input');
+
+    this.rootEl_.addEventListener('input', () => {
+      let value = inputSlider.value;
+      sliderValue.textContent = value;
+      sliderValue.classList.add('show');
+      sliderValue.classList.remove('answered');
+      this.updateSlider(value);
+
+    });
+    this.rootEl_.addEventListener('mouseup', () => {
+      inputSlider.setAttribute('disabled', "");
+      sliderValue.classList.remove('show');
+      sliderValue.classList.add('answered');
+    });
+    this.rootEl_.addEventListener('touchend', () => {
+      inputSlider.setAttribute('disabled', "");
+      sliderValue.classList.remove('show');
+      sliderValue.classList.add('answered');
+    });
+  }
+
+  updateSlider(val) {
+    console.log(val);
+      setImportantStyles(this.rootEl_, {"--percentage": val + "%"});
+  }
+
+  /** @override */
   buildComponent() {
     this.rootEl_ = buildSliderTemplate(this.element);
     this.attachPrompt_(this.rootEl_);
-    const sliderValue = scopedQuerySelector(this.rootEl_, 'span');
-    const inputSlider = scopedQuerySelector(this.rootEl_, 'input');
-    const Slider = scopedQuerySelector(this.rootEl_, 'input');
-
-    this.rootEl_.addEventListener('input', () => {
-      console.log('running');
-      let value = inputSlider.value;
-      sliderValue.textContent = value;
-      console.log(sliderValue);
-      sliderValue.style.left = value + '%';
-      sliderValue.classList.add('show');
-      
-    });
-    function updateSlider(val) {
-        Slider.style.setProperty("--percentage", val + "%");
-    }
-    Slider.addEventListener("input", () => {
-        updateSlider(Slider.value);
-    });
     return this.rootEl_;
+  
   }
 }
