f11f3a895626837e5151a3b8b86ca702d5f9c454
Jake Fried <4656974+samouri@users.noreply.github.com>
2021-07-15 13:34:40 -0400

    forgot typedefs

---
+++ src/core/window/window.d.ts
@@ -0,0 +1,38 @@
+interface Window {
+    Image: any,
+    XMLHttpRequest: any,
+/**
+ * Never exists; used as part of post-compilation checks to verify DCE.
+ */
+__AMP_ASSERTION_CHECK: undefined,
+
+/**
+ * Global error reporting handler; only present in AMP pages.
+ */
+__AMP_REPORT_ERROR: undefined | ((this:Window, e:Error , element?: Element) => void),
+
+/**
+ * Global property set by test some harnesses to signal a testing environment.
+ */
+__AMP_TEST: undefined|boolean,
+
+/**
+ * Counter for the DomBaseWeakRef polyfill.
+ */
+__AMP_WEAKREF_ID: undefined|number,
+
+/**
+ * AMP Runtime settings, configuration, and environment/build constants.
+ */
+AMP_CONFIG: AmpConfigDef | undefined
+
+msCrypto: typeof window.crypto | undefined
+
+}
+
+
+// TODO: move these elsewhere
+declare var IS_ESM: boolean;
+declare var IS_MINIFIED: boolean;
+declare var IS_FORTESTING: boolean;
+ 
