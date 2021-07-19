868f0cf7c69db8bda15ea1f529fe3bb2b50730ce
Brandon Suen <brandonbsuen@google.com>
2021-07-19 13:32:04 -0700

    🐛 [Story interactive] Post-Selection Fill Bug Fix for Image Quizzes and Polls (#35274)
    * Start converting scale percentages to numbers
    
    * Fix scaling
    
    * Fix overflow bug
    
    * Update z-index MD file
    
    * Fix Safari shift

--- extensions/amp-story-interactive/0.1/amp-story-interactive-img-quiz.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-img-quiz.js
@@ -172,7 +172,7 @@ export class AmpStoryInteractiveImgQuiz extends AmpStoryInteractive {
       el.querySelector(
         '.i-amphtml-story-interactive-img-option-percentage-text'
       ).textContent = `${percentages[index]}%`;
-      setImportantStyles(el, {'--option-percentage': `${percentages[index]}%`});
+      setImportantStyles(el, {'--option-percentage': percentages[index] / 100});
     });
   }
 
