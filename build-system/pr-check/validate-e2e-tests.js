82c3d3c75f2384944cf4ffc391fb2485b6fce11a
Riley Jones <rileyjones@google.com>
2021-07-21 11:40:59 -0700

    add validator ci task

---
+++ build-system/pr-check/validate-e2e-tests.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const {
+  skipDependentJobs,
+  timedExecOrDie,
+  timedExecOrThrow,
+} = require('./utils');
+const {runCiJob} = require('./ci-job');
+const {Targets, buildTargetsInclude} = require('./build-targets');
+
+const jobName = 'validate-e2e-tests.js';
+
+/**
+ * Steps to run during push builds.
+ */
+function pushBuildWorkflow() {
+  try {
+    timedExecOrThrow(
+      'amp e2e --nobuild --headless --compiled --report --validate',
+      'End-to-end test validation failed!'
+    );
+  } catch (e) {
+    if (e.status) {
+      process.exitCode = e.status;
+    }
+  } finally {
+    timedExecOrDie('amp test-report-upload');
+  }
+}
+
+/**
+ * Steps to run during PR builds.
+ */
+function prBuildWorkflow() {
+  if (buildTargetsInclude(Targets.E2E_TEST)) {
+    timedExecOrDie('amp e2e --nobuild --headless --compiled --validate');
+  } else {
+    skipDependentJobs(
+      jobName,
+      'this PR does not affect the runtime or end-to-end tests'
+    );
+  }
+}
+runCiJob(jobName, pushBuildWorkflow, prBuildWorkflow);
