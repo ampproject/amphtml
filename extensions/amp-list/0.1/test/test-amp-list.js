9e7a19517a8a8ae90812595648c02a200113350e
Daniel Rozenberg <rodaniel@amp.dev>
2021-07-22 14:24:33 -0400

    Skip flaky amp-list tests

--- extensions/amp-list/0.1/test/test-amp-list.js
+++ extensions/amp-list/0.1/test/test-amp-list.js
@@ -328,7 +328,7 @@ describes.repeated(
                   }
                 });
 
-                it('should unlock height for layout=container with successful attemptChangeHeight', () => {
+                it.skip('should unlock height for layout=container with successful attemptChangeHeight', () => {
                   expect(list.isLayoutSupported('container')).to.be.true;
                   expect(list.enableManagedResizing_).to.be.true;
                   expectFetch(DEFAULT_FETCHED_DATA, [itemElement]);
@@ -360,7 +360,7 @@ describes.repeated(
                   return list.layoutCallback();
                 });
 
-                it('should not unlock height for layout=container for null return', () => {
+                it.skip('should not unlock height for layout=container for null return', () => {
                   expect(list.isLayoutSupported('container')).to.be.true;
                   expect(list.enableManagedResizing_).to.be.true;
                   expectFetch(DEFAULT_FETCHED_DATA, [itemElement]);
@@ -379,7 +379,7 @@ describes.repeated(
             );
           });
 
-          it('should attemptChangeHeight rendered contents', () => {
+          it.skip('should attemptChangeHeight rendered contents', () => {
             const itemElement = doc.createElement('div');
             itemElement.style.height = '1337px';
 
@@ -762,7 +762,7 @@ describes.repeated(
               expect(list.container_.contains(newImg)).to.be.true;
             });
 
-            it('should attemptChangeHeight initial content', async () => {
+            it.skip('should attemptChangeHeight initial content', async () => {
               const initialContent = doc.createElement('div');
               initialContent.setAttribute('role', 'list');
               initialContent.setAttribute('style', 'height: 123px');
