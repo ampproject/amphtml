b99932b902e95427c318d0db8a2d5c30eb04ea51
Jake Fried <samouri@users.noreply.github.com>
2021-07-22 14:13:46 -0400

    build-constants/mode: rename from isForTesting --> isForDistribution (#35336)
    * build-constants/mode: rename from isForTesting --> isForDistribution
    
    * lint fix
    
    * Fix preClosureConfig
    
    * slight cleanup

--- build-system/babel-config/pre-closure-config.js
+++ build-system/babel-config/pre-closure-config.js
@@ -26,9 +26,7 @@ const {getReplacePlugin} = require('./helpers');
  */
 function getPreClosureConfig() {
   const isCheckTypes = argv._.includes('check-types');
-  const testTasks = ['e2e', 'integration', 'visual-diff'];
-  const isTestTask = testTasks.some((task) => argv._.includes(task));
-  const isFortesting = argv.fortesting || isTestTask;
+  const isProd = argv._.includes('dist') && !argv.fortesting;
 
   const reactJsxPlugin = [
     '@babel/plugin-transform-react-jsx',
@@ -73,15 +71,12 @@ function getPreClosureConfig() {
     // argv.esm
     //? './build-system/babel-plugins/babel-plugin-transform-function-declarations'
     //: null,
-    !isCheckTypes
-      ? './build-system/babel-plugins/babel-plugin-transform-json-configuration'
-      : null,
-    !(isFortesting || isCheckTypes)
-      ? [
-          './build-system/babel-plugins/babel-plugin-amp-mode-transformer',
-          {isEsmBuild: !!argv.esm},
-        ]
-      : null,
+    !isCheckTypes &&
+      './build-system/babel-plugins/babel-plugin-transform-json-configuration',
+    isProd && [
+      './build-system/babel-plugins/babel-plugin-amp-mode-transformer',
+      {isEsmBuild: !!argv.esm},
+    ],
   ].filter(Boolean);
   const presetEnv = [
     '@babel/preset-env',
