9a8b6e9ec6b39d0f401493d0ce4cff318c5cca41
Sydney Hardister <41082236+syydneyyhardister@users.noreply.github.com>
2021-07-23 07:11:25 -0700

    [Story interactive] Disable overlay on slider when dragging on mobile (#35358)
    * setting up slider
    
    * removing spaces
    
    * added slider.html to test.configs
    
    * changed dates to 2021
    
    * example story
    
    * formatting
    
    * css
    
    * slider
    
    * minor changes
    
    * resolve merge
    
    * formatting & styling
    
    * formatting and removing unnecessary code
    
    * centering and pointer
    
    * minor formatting changes
    
    * addressed pr comments
    
    * removing unnecessary code in css
    
    * adding bubble
    
    * adding gradient and working on bubble
    
    * updating compared to previous pr
    
    * working on bubble
    
    * bubble
    
    * post state and clean up
    
    * formatting and testing
    
    * first pr changes and fixing bubble
    
    * removing whitespace from css
    
    * css index
    
    * changes from review
    
    * changed line height in bubble
    
    * circle check
    
    * css and testing
    
    * tests and bubble
    
    * css white space
    
    * removing sides
    
    * change bubble and slider math
    
    * reverting code
    
    * overall beginning
    
    * tests and review
    
    * whitespaces
    
    * css index
    
    * reviews: minor css changes
    
    * beginning from temp file
    
    * testing and  basic  functionaltiy
    
    * bubble sizing
    
    * 1st reviews, content class
    
    * fixing bubble and gradient
    
    * linting
    
    * break on variable changes
    
    * fixing variables and adding review comments
    
    * whitespaces
    
    * disable drag education
    
    * remove console log
    
    * removing brackets

--- extensions/amp-story-interactive/0.1/amp-story-interactive-slider.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-slider.js
@@ -109,6 +109,12 @@ export class AmpStoryInteractiveSlider extends AmpStoryInteractive {
     this.inputEl_.addEventListener('change', () => {
       this.onRelease_();
     });
+
+    this.inputEl_.addEventListener(
+      'touchmove',
+      (event) => event.stopPropagation(),
+      true
+    );
   }
 
   /**
