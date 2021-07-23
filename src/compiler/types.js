0f40636a872de43f055792f063ade03d4d0acf46
Jake Fried <samouri@users.noreply.github.com>
2021-07-23 12:16:45 -0400

    Increase text further

--- src/compiler/types.js
+++ src/compiler/types.js
@@ -22,5 +22,8 @@
  * - It must not perform any side effects, such as adding event listeners or assigning instance variables.
  * - It must not perform any of the operations involved with layoutCallback (loading).
  *
+ * The return value is set to *, so that we may optionally return any DOM nodes
+ * created during a client-side render. These nodes are often needed for ivars.
+ *
  * @typedef {function(!Element):*} BuildDom
  */
