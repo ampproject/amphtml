0a86a97988a45d4da5de15e13b8c4d9e1cfdec73
Brandon Suen <brandonbsuen@google.com>
2021-07-26 14:15:45 -0700

    ✨ [Story interactive] Scaffolding for New Results Component (#35400)
    * Set up new CSS and JS files
    
    * Register component
    
    * Add css binary

--- extensions/amp-story-interactive/0.1/amp-story-interactive.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive.js
@@ -20,6 +20,7 @@ import {AmpStoryInteractiveImgQuiz} from './amp-story-interactive-img-quiz';
 import {AmpStoryInteractivePoll} from './amp-story-interactive-poll';
 import {AmpStoryInteractiveQuiz} from './amp-story-interactive-quiz';
 import {AmpStoryInteractiveResults} from './amp-story-interactive-results';
+import {AmpStoryInteractiveResultsDetailed} from './amp-story-interactive-results-detailed';
 import {AmpStoryInteractiveSlider} from './amp-story-interactive-slider';
 
 /**
@@ -56,5 +57,9 @@ AMP.extension('amp-story-interactive', '0.1', (AMP) => {
   AMP.registerElement(
     'amp-story-interactive-results',
     AmpStoryInteractiveResults
+  );
+  AMP.registerElement(
+    'amp-story-interactive-results-detailed',
+    AmpStoryInteractiveResultsDetailed
   );
 });
