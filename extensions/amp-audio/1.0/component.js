08d71d63dde7f61cf6dd06378bac59f5c4cff035
AnuragVasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-07-30 01:53:30 +0530

    🚮 Removed unused prop `children` from dependency

--- extensions/amp-audio/1.0/component.js
+++ extensions/amp-audio/1.0/component.js
@@ -125,7 +125,7 @@ export function AudioWithRef(
     return () => {
       unlistenPlaying();
     };
-  }, [audioPlaying, children, propagateAttributes]);
+  }, [audioPlaying, propagateAttributes]);
 
   /** Audio Component - API Functions */
   useImperativeHandle(
