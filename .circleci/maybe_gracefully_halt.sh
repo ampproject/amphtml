6b60ad289401ef0b633ccd3c430520802d037b01
Daniel Rozenberg <rodaniel@amp.dev>
2021-07-30 14:55:08 -0400

    Remove newline from log color definitions in CircleCI bash files

--- .circleci/maybe_gracefully_halt.sh
+++ .circleci/maybe_gracefully_halt.sh
@@ -20,8 +20,8 @@
 
 set -e
 
-GREEN() { echo -e "\n\033[0;32m$1\033[0m"; }
-YELLOW() { echo -e "\n\033[0;33m$1\033[0m"; }
+GREEN() { echo -e "\033[0;32m$1\033[0m"; }
+YELLOW() { echo -e "\033[0;33m$1\033[0m"; }
 
 if ls /tmp/restored-workspace/.CI_GRACEFULLY_HALT_* 1>/dev/null 2>&1; then
   echo $(GREEN "Gracefully halting this job.")
