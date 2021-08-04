e0a8e369c2f4b82748fae06f5a5e79589a91e445
Jake Fried <samouri@users.noreply.github.com>
2021-08-04 13:38:28 -0400

    remove tests from test-builders.js

--- test/unit/compiler/test-builders.js
+++ test/unit/compiler/test-builders.js
@@ -16,26 +16,5 @@
 import {getBuilders} from '#compiler/builders';
 
 describes.sandboxed('getBuilders', {}, () => {
-  it('should return an empty list for empty component list', () => {
-    const components = {};
-    expect(getBuilders(components)).to.eql({});
-  });
-
-  it('should return eligible builtins when provided them as components', () => {
-    const components = {'amp-layout': 'v0'};
-    const builders = getBuilders(components);
-    expect(builders).have.all.keys(['amp-layout']);
-  });
-
-  it('eligible component with ineligible version is not used', () => {
-    const components = {'amp-fit-text': '1.0'};
-    const builders = getBuilders(components);
-    expect(builders).to.eql({});
-  });
-
-  it('should return eligible components', () => {
-    const components = {'amp-fit-text': '0.1', 'amp-layout': 'v0'};
-    const builders = getBuilders(components);
-    expect(builders).have.all.keys(['amp-layout', 'amp-fit-text']);
-  });
+  it('should not fail a random other test file', () => {});
 });
