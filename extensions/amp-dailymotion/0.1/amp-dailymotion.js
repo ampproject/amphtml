3a475206a46b36a67d360f1cfc90041625702338
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-08-09 13:27:28 -0700

    [`bento-dailymotion`] Add validation for amp-dailymotion v1.0.

--- extensions/amp-dailymotion/0.1/amp-dailymotion.js
+++ extensions/amp-dailymotion/0.1/amp-dailymotion.js
@@ -162,7 +162,6 @@ class AmpDailymotion extends AMP.BaseElement {
    * @private
    */
   handleEvents_(event) {
-    console.log(event);
     if (!originMatches(event, this.iframe_, 'https://www.dailymotion.com')) {
       return;
     }
