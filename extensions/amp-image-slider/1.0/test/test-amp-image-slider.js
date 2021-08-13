a6ee470d890063d37321b3ca00ea194b6fd438dd
AnuragVasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-08-13 15:10:05 +0530

    ✨ Initial Commit

---
+++ extensions/amp-image-slider/1.0/test/test-amp-image-slider.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../amp-image-slider';
+import {htmlFor} from '#core/dom/static-template';
+import {toggleExperiment} from '#experiments';
+import {waitFor} from '#testing/test-helper';
+
+describes.realWin(
+  'amp-image-slider-v1.0',
+  {
+    amp: {
+      extensions: ['amp-image-slider:1.0'],
+    },
+  },
+  (env) => {
+    let win;
+    let doc;
+    let html;
+
+    beforeEach(async () => {
+      win = env.win;
+      doc = win.document;
+      html = htmlFor(doc);
+      toggleExperiment(win, 'bento-image-slider', true, true);
+    });
+
+    // DO NOT SUBMIT: This is example code only.
+    it('example test renders', async () => {
+      const element = html` <amp-image-slider></amp-image-slider> `;
+      doc.body.appendChild(element);
+      await waitFor(() => element.isConnected, 'element connected');
+      expect(element.parentNode).to.equal(doc.body);
+    });
+  }
+);
