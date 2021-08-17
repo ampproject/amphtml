068be70835cfe6d4ab2d047ee4c6ca2a0d77e3b6
Caroline Liu <carolineliu@google.com>
2021-08-17 15:54:31 -0400

    Create eslint rule

---
+++ build-system/eslint-rules/jsx-alternative-props.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// Enforces preferred DOM attributes inside JSX.
+//
+// Good
+// <div tabIndex="0" />
+// <path stroke-linecap />
+//
+// Bad
+// <div tabindex="0" />
+// <path strokeLinecap />
+
+const DOM_ATTRIBUTES = {
+  className: 'class',
+  tabindex: 'tabIndex',
+};
+
+/**
+ * This list derives all hyphenated attributes from
+ * https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute
+ */
+const SVG_ATTRIBUTES = {
+  accentHeight: 'accent-height',
+  alignmentBaseline: 'alignment-baseline',
+  arabicForm: 'arabic-form',
+  baselineShift: 'baseline-shift',
+  capHeight: 'cap-height',
+  clipPath: 'clip-path',
+  clipRule: 'clip-rule',
+  colorInterpolation: 'color-interpolation',
+  colorInterpolationFilters: 'color-interpolation-filters',
+  colorProfile: 'color-profile',
+  colorRendering: 'color-rendering',
+  dominantBaseline: 'dominant-baseline',
+  enableBackground: 'enable-background',
+  fillOpacity: 'fill-opacity',
+  fillRule: 'fill-rule',
+  floodColor: 'flood-color',
+  floodOpacity: 'flood-opacity',
+  fontFamily: 'font-family',
+  fontSize: 'font-size',
+  fontSizeAdjust: 'font-size-adjust',
+  fontStretch: 'font-stretch',
+  fontStyle: 'font-style',
+  fontVariant: 'font-variant',
+  fontWeight: 'font-weight',
+  glyphName: 'glyph-name',
+  glyphOrientationHorizontal: 'glyph-orientation-horizontal',
+  glyphOrientationVertical: 'glyph-orientation-vertical',
+  horizAdvX: 'horiz-adv-x',
+  horizOriginX: 'horiz-origin-x',
+  imageRendering: 'image-rendering',
+  letterSpacing: 'letter-spacing',
+  lightingColor: 'lighting-color',
+  markerEnd: 'marker-end',
+  markerMid: 'marker-mid',
+  markerStart: 'marker-start',
+  overlinePosition: 'overline-position',
+  overlineThickness: 'overline-thickness',
+  paintOrder: 'paint-order',
+  panose1: 'panose-1',
+  pointerEvents: 'pointer-events',
+  renderingIntent: 'rendering-intent',
+  shapeRendering: 'shape-rendering',
+  stopColor: 'stop-color',
+  stopOpacity: 'stop-opacity',
+  strikethroughPosition: 'strikethrough-position',
+  strikethroughThickness: 'strikethrough-thickness',
+  strokeDasharray: 'stroke-dasharray',
+  strokeDashoffset: 'stroke-dashoffset',
+  strokeLinecap: 'stroke-linecap',
+  strokeLinejoin: 'stroke-linejoin',
+  strokeMiterlimit: 'stroke-miterlimit',
+  strokeOpacity: 'stroke-opacity',
+  strokeWidth: 'stroke-width',
+  textAnchor: 'text-anchor',
+  textDecoration: 'text-decoration',
+  textRendering: 'text-rendering',
+  transformOrigin: 'transform-origin',
+  underlinePosition: 'underline-position',
+  underlineThickness: 'underline-thickness',
+  unicodeBidi: 'unicode-bidi',
+  unicodeRange: 'unicode-range',
+  unitsPerEm: 'units-per-em',
+  vAlphabetic: 'v-alphabetic',
+  vHanging: 'v-hanging',
+  vIdeographic: 'v-ideographic',
+  vMathematical: 'v-mathematical',
+  vectorEffect: 'vector-effect',
+  vertAdvY: 'vert-adv-y',
+  vertOriginX: 'vert-origin-x',
+  vertOriginY: 'vert-origin-y',
+  wordSpacing: 'word-spacing',
+  writingMode: 'writing-mode',
+  xHeight: 'x-height',
+};
+
+module.exports = {
+  meta: {
+    fixable: 'code',
+  },
+  create(context) {
+    return {
+      JSXAttribute(node) {
+        const alternative =
+          DOM_ATTRIBUTES[node.name.name] || SVG_ATTRIBUTES[node.name.name];
+        if (!alternative) {
+          return;
+        }
+        context.report({
+          node,
+          message:
+            'Prefer `' +
+            alternative +
+            '` to `' +
+            node.name +
+            '` when using JSX.',
+
+          fix(fixer) {
+            return fixer.replaceText(node.name, alternative);
+          },
+        });
+      },
+    };
+  },
+};
