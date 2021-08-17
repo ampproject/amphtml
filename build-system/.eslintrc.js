194d47d2105b6983a49aace8b93e54b31ef8f610
Jake Fried <samouri@users.noreply.github.com>
2021-08-16 22:13:49 -0400

    babel-plugins: remove simple-array-destructuring (#35695)

--- build-system/.eslintrc.js
+++ build-system/.eslintrc.js
@@ -36,7 +36,6 @@ module.exports = {
   },
   'rules': {
     'import/no-unresolved': 0,
-    'local/no-array-destructuring': 0,
     'local/no-bigint': 0,
     'local/no-dynamic-import': 0,
     'local/no-export-side-effect': 0,
