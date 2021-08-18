8018407f34f291f2ceeb93e989f64bb18ff83c6a
Dhruv Manek <dmanek@gmail.com>
2021-08-18 14:52:40 -0700

    add knob in storybook

--- extensions/amp-iframe/1.0/storybook/Basic.js
+++ extensions/amp-iframe/1.0/storybook/Basic.js
@@ -16,7 +16,7 @@
 
 import * as Preact from '#preact';
 import {Iframe} from '../component';
-import {withKnobs} from '@storybook/addon-knobs';
+import {text, withKnobs} from '@storybook/addon-knobs';
 
 export default {
   title: 'Iframe',
@@ -27,17 +27,17 @@ export default {
 export const _default = () => {
   return (
     <Iframe
-      style={{width: 800, height: 600}}
+      style={{width: 800, height: 600, borderWidth: 2}}
       src="https://www.wikipedia.org/"
       title="Wikipedia"
     ></Iframe>
   );
 };
 
-export const WithResizableIframe = () => {
+export const WithIntersectingIframe = () => {
   return (
     <Iframe
-      title="Resizable iframe example"
+      style={{borderWidth: 2}}
       width="100"
       height="100"
       sandbox="allow-scripts allow-same-origin"
@@ -49,4 +49,29 @@ export const WithResizableIframe = () => {
   );
 };
 
-WithResizableIframe.storyName = 'resizable amp-iframe';
+WithIntersectingIframe.storyName = 'Resizable iframe in viewport';
+
+export const WithResizableIframe = () => {
+  const sampleText =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, beatae repellat, eligendi tempora, cumque veniam voluptatibus amet cum aliquid aut aperiam officiis autem pariatur. Nemo cum maxime vitae. Consectetur, iure?';
+  const textAbove = text('text above', sampleText.repeat(10));
+  const textBelow = text('text below', sampleText.repeat(10));
+  return (
+    <div>
+      <h1>{textAbove}</h1>
+      <Iframe
+        style={{borderWidth: 2}}
+        width="100"
+        height="100"
+        sandbox="allow-scripts allow-same-origin"
+        resizable
+        src="/examples/bento/amp-iframe-resizing-example.html"
+      >
+        <div placeholder>Placeholder</div>
+      </Iframe>
+      <h1>{textBelow}</h1>
+    </div>
+  );
+};
+
+WithResizableIframe.storyName = 'Resizable iframe outside viewport';
