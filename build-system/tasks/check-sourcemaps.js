75f21e11ae190bbb28361c75d186439f3b9a5de5
Jake Fried <samouri@users.noreply.github.com>
2021-08-18 13:05:53 -0400

    clean up constants

--- build-system/tasks/check-sourcemaps.js
+++ build-system/tasks/check-sourcemaps.js
@@ -33,12 +33,6 @@ const v0MjsMap = 'dist/v0.mjs.map';
 const sourcemapUrlMatcher =
   'https://raw.githubusercontent.com/ampproject/amphtml/\\d{13}/';
 
-// Mapping related constants
-const expectedFirstLineFileMjs = 'src/core/mode/version.js'; // First file that is compiled into v0.mjs.
-const expectedFirstLineCodeMjs = 'function version() {'; // First line of code in that file.
-const expectedFirstLineFileJs = 'src/core/mode/prod.js'; // First file that is compiled into v0.js.
-const expectedFirstLineCodeJs = 'export function isProd() {'; // First line of code in that file.
-
 /**
  * Build runtime with sourcemaps if needed.
  */
@@ -147,24 +141,22 @@ function checkSourcemapMappings(sourcemapJson, map) {
     cyan('build-system/tasks/check-sourcemaps.js') +
     '.';
 
-  const expectedFirstLineFile = map.includes('mjs')
-    ? expectedFirstLineFileMjs
-    : expectedFirstLineFileJs;
-  const expectedFirstLineCode = map.includes('mjs')
-    ? expectedFirstLineCodeMjs
-    : expectedFirstLineCodeJs;
+  // Mapping related constants
+  const expectedFirstLine = map.includes('mjs')
+    ? {file: 'src/core/mode/version.js', code: 'function version() {'}
+    : {file: 'src/core/mode/prod.js', code: 'export function isProd() {'};
 
-  if (firstLineFile != expectedFirstLineFile) {
+  if (firstLineFile != expectedFirstLine.file) {
     log(red('ERROR:'), 'Found mapping for incorrect file.');
     log('Actual:', cyan(firstLineFile));
-    log('Expected:', cyan(expectedFirstLineFile));
+    log('Expected:', cyan(expectedFirstLine.file));
     log(helpMessage);
     throw new Error('Found mapping for incorrect file');
   }
-  if (firstLineCode != expectedFirstLineCode) {
+  if (firstLineCode != expectedFirstLine.code) {
     log(red('ERROR:'), 'Found mapping for incorrect code.');
     log('Actual:', cyan(firstLineCode));
-    log('Expected:', cyan(expectedFirstLineCode));
+    log('Expected:', cyan(expectedFirstLine.code));
     log(helpMessage);
     throw new Error('Found mapping for incorrect code');
   }
