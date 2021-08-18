6aad99f7e341e549fa759c59da790f8d720babf9
Esther Kim <44627152+estherkim@users.noreply.github.com>
2021-08-18 15:03:09 -0700

    🏗  Add release tagger github workflow (#35727)
    * workflow and script
    
    * uncomment
    
    * split label-prs script and add logging

---
+++ build-system/release-tagger/index.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview
+ * Entry file for ./github/workflows/release-tagger.yml
+ * Parameters
+ * 1. action (promote|rollback)
+ * 2. head (AMP version)
+ * 3. base (AMP version)
+ * 4. channel (beta|stable|lts)
+ */
+
+const [action, head, base, channel] = process.argv.slice(2);
+
+const {addLabels, removeLabels} = require('./label-pull-requests');
+const {log} = require('../common/logging');
+const {makeRelease} = require('./make-release');
+const {publishRelease, rollbackRelease} = require('./update-release');
+
+/**
+ * Promote actions
+ * @return {Promise<void>}
+ */
+async function _promote() {
+  try {
+    await publishRelease(head);
+    log('Published release', head);
+  } catch (e) {
+    await makeRelease(head, base, channel);
+    log('Created release', head);
+  }
+
+  await addLabels(head, base, channel);
+  log('Labeled PRs for release', head, 'and channel', channel);
+}
+
+/**
+ * Rollback actions
+ * @return {Promise<void>}
+ */
+async function _rollback() {
+  try {
+    await rollbackRelease(head);
+    log('Rolled back release', head);
+  } catch (e) {
+    log('Could not roll back release.', e);
+  }
+
+  await removeLabels(head, base, channel);
+  log('Removed labels from PRs for release', head, 'and channel', channel);
+}
+
+/**
+ * Main functions for the release tagger
+ * @return {Promise<void>}
+ */
+async function main() {
+  if (action == 'promote') {
+    log('Action: promote');
+    return await _promote();
+  }
+
+  if (action == 'rollback') {
+    log('Action: rollback');
+    return await _rollback();
+  }
+
+  // TODO(estherkim): add release tracker comment on prs
+}
+
+main();
