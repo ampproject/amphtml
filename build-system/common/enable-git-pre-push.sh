2383dfbb14d31aa021a2d0efd1649a3e25a1d1dc
Raghu Simha <rsimha@amp.dev>
2021-08-19 15:40:32 -0400

    🏗📖 Remove AMP copyright from infrastructure source files (#35744)

--- build-system/common/enable-git-pre-push.sh
+++ build-system/common/enable-git-pre-push.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 #
-# Copyright 2018 The AMP HTML Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the license.
-
 # This script adds a pre-push hook to .git/hooks/, which runs some basic tests
 # before running "git push".
 # To enable it, run this script: "./build-system/common/enable-git-pre-push.sh"
