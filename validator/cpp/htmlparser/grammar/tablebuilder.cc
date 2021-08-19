8a7270a5a82e5f011b1a72842eb589aa3e288329
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2021-08-19 09:37:42 -0700

    Sync for validator cpp engine and cpp htmlparser (#35737)
    * Remove usage of glog in favor of simpler logging mechanism. Also a few other minor refactoring cleanups.
    
    PiperOrigin-RevId: 387395387
    
    * Remove usage of glog in favor of simpler logging mechanism. Also a few other minor refactoring cleanups.
    
    PiperOrigin-RevId: 387405195
    
    * Remove notice from c++ validator
    
    PiperOrigin-RevId: 391628308
    
    * Remove notice from htmlparser
    
    PiperOrigin-RevId: 391630746
    
    Co-authored-by: Greg Grothaus <greggrothaus@google.com>

--- validator/cpp/htmlparser/grammar/tablebuilder.cc
+++ validator/cpp/htmlparser/grammar/tablebuilder.cc
@@ -1,19 +1,3 @@
-//
-// Copyright 2020 The AMP HTML Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the license.
-//
-
 #include "grammar/tablebuilder.h"
 
 #include <array>
@@ -180,9 +164,6 @@ bool TableBuilder::OutputHeaderFile(
 
   fd << "// -*- C++ -*-\n";
 
-  if (!header_options_.license_header.empty()) {
-    fd << header_options_.license_header << "\n";
-  }
   fd << "\n// AUTO GENERATED; DO NOT EDIT.\n";
   fd << "// To regenerate this file, see comments in bin/jsongrammargen\n\n";
   if (!header_options_.ifdef_guard.empty()) {
