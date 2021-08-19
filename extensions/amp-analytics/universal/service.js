aa8ed0a7e031033b1937e931a8611f8af0a24453
Alan Orozco <alan@orozco.xyz>
2021-08-18 19:11:59 -0700

    naming/handle .then

--- extensions/amp-analytics/universal/service.js
+++ extensions/amp-analytics/universal/service.js
@@ -13,22 +13,25 @@ import extensionsImpl from './extensions-impl';
  * @param {string} objectName
  * @return {!Object}
  */
-function passthrough(objectName) {
-  return new Proxy(
-    {},
-    {
-      get(target, name) {
-        try {
-          throw new Error(
-            `[universal] Attempted to use property ${objectName}(...).${name} on unimplemented service.`
-          );
-        } catch (e) {
-          console./*OK*/ warn(e.stack);
-        }
-        return () => {};
-      },
-    }
-  );
+function empty(objectName) {
+  return new Proxy(Object.create(null), {
+    get(target, name) {
+      // Special case to allow us to print access of methods of Services
+      // resolved from a promise, instead of merely erroring-out during an
+      // implicit `.then()`.
+      if (name === 'then') {
+        return undefined;
+      }
+      try {
+        throw new Error(
+          `[universal] Attempted to use property ${objectName}(...).${name} on unimplemented service.`
+        );
+      } catch (e) {
+        console./*OK*/ warn(e.stack);
+      }
+      return () => {};
+    },
+  });
 }
 
 const unavailable = once(() => Promise.resolve(null));
@@ -49,14 +52,14 @@ export const Services = {
   // Passthrough placeholders for services that aren't yet known to be required
   // for the supported codepaths.
   viewerPromiseForDoc: once(() =>
-    Promise.resolve(passthrough('viewerPromiseForDoc'))
+    Promise.resolve(empty('viewerPromiseForDoc'))
   ),
-  viewportForDoc: once(() => passthrough('viewportForDoc')),
-  formSubmitForDoc: once(() => passthrough('formSubmitForDoc')),
-  preconnectFor: once(() => passthrough('preconnectFor')),
+  viewportForDoc: once(() => empty('viewportForDoc')),
+  formSubmitForDoc: once(() => empty('formSubmitForDoc')),
+  preconnectFor: once(() => empty('preconnectFor')),
   // eslint-disable-next-line local/no-forbidden-terms
-  storageForDoc: () => once(() => passthrough('storageForDoc')),
-  xhrFor: () => once(() => passthrough('xhrFor')),
+  storageForDoc: () => once(() => empty('storageForDoc')),
+  xhrFor: () => once(() => empty('xhrFor')),
 
   // These can be null since they're optional.
   // For now, we assume that their interoperability is unavailable.
