9f3ecd41fc0389cb89d0b9e1d8b4d87e2258ceb0
honeybadgerdontcare <honeybadgerdontcare@users.noreply.github.com>
2021-08-19 09:37:42 -0700

    Sync for validator cpp engine and cpp htmlparser (#35737)
    * Remove usage of glog in favor of simpler logging mechanism. Also a few other minor refactoring cleanups.
    
    PiperOrigin-RevId: 387395387
    
    * Remove usage of glog in favor of simpler logging mechanism. Also a few other minor refactoring cleanups.
    
    PiperOrigin-RevId: 387405195
    
    * Remove notice from c++ validator
    
    PiperOrigin-RevId: 391628308
    
    * Remove notice from htmlparser
    
    PiperOrigin-RevId: 391630746
    
    Co-authored-by: Greg Grothaus <greggrothaus@google.com>

--- validator/cpp/htmlparser/bin/statetablegen.cc
+++ validator/cpp/htmlparser/bin/statetablegen.cc
@@ -1,19 +1,3 @@
-//
-// Copyright 2020 The AMP HTML Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the license.
-//
-
 // To regenerate states.h file, run:
 //
 // blaze clean
@@ -29,22 +13,6 @@
 #include "grammar/tablebuilder.h"
 
 ABSL_FLAG(std::string, input_grammar, "", "Options: url, json");
-
-constexpr std::string_view kLicenseHeader = R"LICENSETXT(//
-// Copyright 2020 The AMP HTML Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the license.
-//)LICENSETXT";
 
 
 int main(int argc, char** argv) {
@@ -65,12 +33,10 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  htmlparser::grammar::TableBuilder builder(
-      input_grammar,
-      {.output_file_path = output_file,
-       .license_header = kLicenseHeader.data(),
-       .ifdef_guard = ifdef_guard,
-       .cpp_namespace = cpp_namespace});
+  htmlparser::grammar::TableBuilder builder(input_grammar,
+                                            {.output_file_path = output_file,
+                                             .ifdef_guard = ifdef_guard,
+                                             .cpp_namespace = cpp_namespace});
 
   if (!builder.ParseRulesAndGenerateTable()) {
     std::cerr << "Table generation failed.\n";
