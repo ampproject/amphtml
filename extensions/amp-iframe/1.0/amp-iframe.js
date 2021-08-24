b69bd53534800a5ea71d095548d02c5a0a8c958f
dmanek <506183+dmanek@users.noreply.github.com>
2021-08-24 09:12:59 -0700

    [amp-iframe] resize iframe using IntersectionObserver (#35566)

--- extensions/amp-iframe/1.0/amp-iframe.js
+++ extensions/amp-iframe/1.0/amp-iframe.js
@@ -6,6 +6,10 @@ import {measureIntersection} from '#core/dom/layout/intersection';
 
 /** @const {string} */
 const TAG = 'amp-iframe';
+/** @const {number} */
+const MINIMUM_DISTANCE_FROM_TOP_PX = 600;
+/** @const {number} */
+const MINIMUM_VIEWPORT_PROPORTION = 0.75;
 
 class AmpIframe extends BaseElement {
   /** @override */
@@ -18,34 +22,78 @@ class AmpIframe extends BaseElement {
     return super.isLayoutSupported(layout);
   }
 
+  /**
+   * Callback for onload event of iframe. Checks if the component has a placeholder
+   * element and hides it if it does. Also checks the position of the iframe on the
+   * document.
+   */
+  handleOnLoad_() {
+    if (this.getPlaceholder()) {
+      this.togglePlaceholder(false);
+      return;
+    }
+    // TODO(dmanek): Extract this to a common function & share
+    // between 0.1 and 1.0 versions.
+    measureIntersection(this.element).then((intersectionEntry) => {
+      const {top} = intersectionEntry.boundingClientRect;
+      const viewportHeight = intersectionEntry.rootBounds.height;
+      const minTop = Math.min(
+        MINIMUM_DISTANCE_FROM_TOP_PX,
+        viewportHeight * MINIMUM_VIEWPORT_PROPORTION
+      );
+      userAssert(
+        top >= minTop,
+        '<amp-iframe> elements must be positioned outside the first 75% ' +
+          'of the viewport or 600px from the top (whichever is smaller): %s ' +
+          ' Current position: %s. Min: %s' +
+          "Positioning rules don't apply for iframes that use `placeholder`." +
+          'See https://github.com/ampproject/amphtml/blob/main/extensions/' +
+          'amp-iframe/amp-iframe.md#iframe-with-placeholder for details.',
+        this.element,
+        top,
+        minTop
+      );
+    });
+  }
+
+  /**
+   * Updates the element's dimensions to accommodate the iframe's
+   * requested dimensions.
+   * @param {number} height
+   * @param {number} width
+   * @return {!Promise}
+   * @private
+   */
+  updateSize_(height, width) {
+    if (height < 100) {
+      this.user().error(
+        TAG,
+        'Ignoring embed-size request because the resize height is less ' +
+          'than 100px. If you are using amp-iframe to display ads, consider ' +
+          'using amp-ad instead.',
+        this.element
+      );
+      return Promise.reject('Resize height is < 100px');
+    }
+
+    return this.attemptChangeSize(height, width).catch(() => {
+      if (!this.getOverflowElement?.()) {
+        console./* OK */ warn(
+          '[overflow] element not found. Provide one to enable resizing.',
+          this.element
+        );
+      }
+    });
+  }
+
   /** @override */
   init() {
     return dict({
-      'onLoadCallback': () => {
-        const hasPlaceholder = Boolean(this.getPlaceholder());
-        if (hasPlaceholder) {
-          this.togglePlaceholder(false);
-          return;
-        }
-        // TODO(dmanek): Extract this to a common function & share
-        // between 0.1 and 1.0 versions.
-        measureIntersection(this.element).then((intersectionEntry) => {
-          const {top} = intersectionEntry.boundingClientRect;
-          const viewportHeight = intersectionEntry.rootBounds.height;
-          const minTop = Math.min(600, viewportHeight * 0.75);
-          userAssert(
-            top >= minTop,
-            '<amp-iframe> elements must be positioned outside the first 75% ' +
-              'of the viewport or 600px from the top (whichever is smaller): %s ' +
-              ' Current position %s. Min: %s' +
-              "Positioning rules don't apply for iframes that use `placeholder`." +
-              'See https://github.com/ampproject/amphtml/blob/main/extensions/' +
-              'amp-iframe/amp-iframe.md#iframe-with-placeholder for details.',
-            this.element,
-            top,
-            minTop
-          );
-        });
+      'onLoad': () => {
+        this.handleOnLoad_();
+      },
+      'requestResize': (height, width) => {
+        return this.updateSize_(height, width);
       },
     });
   }
--- extensions/amp-iframe/1.0/component.js
+++ extensions/amp-iframe/1.0/component.js
@@ -1,6 +1,10 @@
 import * as Preact from '#preact';
+import {useCallback, useEffect, useRef} from '#preact';
+import {MessageType} from '#preact/component/3p-frame';
+import {toWin} from '#core/window';
 
 const NOOP = () => {};
+const FULL_HEIGHT = '100%';
 
 /**
  * @param {!IframeDef.Props} props
@@ -10,15 +14,100 @@ export function Iframe({
   allowFullScreen,
   allowPaymentRequest,
   allowTransparency,
-  onLoadCallback = NOOP,
+  onLoad = NOOP,
   referrerPolicy,
+  requestResize,
   sandbox,
   src,
   srcdoc,
   ...rest
 }) {
+  const iframeRef = useRef();
+  const dataRef = useRef(null);
+  const isIntersectingRef = useRef(null);
+
+  const attemptResize = useCallback(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) {
+      return;
+    }
+    let height = Number(dataRef.current.height);
+    let width = Number(dataRef.current.width);
+    if (!height && !width) {
+      console./*OK*/ error(
+        'Ignoring resize request because width and height value is invalid'
+      );
+      return;
+    }
+    // TODO(dmanek): Calculate width and height of the container to include padding.
+    if (!height) {
+      height = iframe./*OK*/ offsetHeight;
+    }
+    if (!width) {
+      width = iframe./*OK*/ offsetWidth;
+    }
+    if (requestResize) {
+      // Currently `requestResize` is called twice:
+      // 1. when post message is received in viewport
+      // 2. when exiting viewport
+      // This could be optimized by reducing to one call by assessing when to call.
+      requestResize(height, width).then(() => {
+        iframe.height = FULL_HEIGHT;
+        iframe.width = FULL_HEIGHT;
+      });
+    } else if (isIntersectingRef.current === false) {
+      // attemptResize can be called before the IntersectionObserver starts observing
+      // the component if an event is fired immediately. Therefore we check
+      // isIntersectingRef has changed via isIntersectingRef.current === false.
+      if (width) {
+        iframe.width = width;
+      }
+      if (height) {
+        iframe.height = height;
+      }
+    }
+  }, [requestResize]);
+
+  const handlePostMessage = useCallback(
+    (event) => {
+      if (event.data?.type !== MessageType.EMBED_SIZE) {
+        return;
+      }
+      dataRef.current = event.data;
+      attemptResize();
+    },
+    [attemptResize]
+  );
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) {
+      return;
+    }
+    const win = iframe && toWin(iframe.ownerDocument.defaultView);
+    if (!win) {
+      return;
+    }
+    const io = new win.IntersectionObserver((entries) => {
+      const last = entries[entries.length - 1];
+      isIntersectingRef.current = last.isIntersecting;
+      if (last.isIntersecting || !dataRef.current || !win) {
+        return;
+      }
+      attemptResize();
+    });
+    io.observe(iframe);
+    win.addEventListener('message', handlePostMessage);
+
+    return () => {
+      io.unobserve(iframe);
+      win.removeEventListener('message', handlePostMessage);
+    };
+  }, [attemptResize, handlePostMessage]);
+
   return (
     <iframe
+      ref={iframeRef}
       src={src}
       srcdoc={srcdoc}
       sandbox={sandbox}
@@ -26,7 +115,8 @@ export function Iframe({
       allowpaymentrequest={allowPaymentRequest}
       allowtransparency={allowTransparency}
       referrerpolicy={referrerPolicy}
-      onload={onLoadCallback}
+      onload={onLoad}
+      frameBorder="0"
       {...rest}
     ></iframe>
   );
