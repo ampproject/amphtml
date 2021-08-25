8059a5825b410ff7634775bc753702d79d7de23d
AnuragVasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-08-25 16:04:11 +0530

    🚮 Removed unnecessary comments

--- extensions/amp-audio/1.0/test/test-amp-audio.js
+++ extensions/amp-audio/1.0/test/test-amp-audio.js
@@ -33,7 +33,6 @@ describes.realWin(
   (env) => {
     let win;
     let doc;
-    // let html;
     let element;
     let ampAudio;
 
@@ -214,15 +213,6 @@ describes.realWin(
       expect(audio.childNodes[0].getAttribute('src')).to.equal('audio.mp3');
       expect(audio.childNodes[1].tagName).to.equal('SOURCE');
       expect(audio.childNodes[1].getAttribute('src')).to.equal('audio.ogg');
-
-      /**
-       * This will not be defined once we have sources prop defined in base-element.js
-       * Instead assert expect(audio.childNodes).to.have.lengthOf(2) since the third
-       * child is disregarded due to being not a <source> tag.
-       *
-       *   expect(audio.childNodes[2].nodeType).to.equal(Node.TEXT_NODE);
-       *   expect(audio.childNodes[2].textContent).to.equal('Unsupported.');
-       */
       expect(audio.childNodes).to.have.lengthOf(2);
     });
 
@@ -323,12 +313,5 @@ describes.realWin(
         impl.executeAction({method: 'isPlaying', satisfiesTrust: () => true})
       ).to.be.false;
     });
-
-    /**
-     * Following TestCase is no longer needed:
-     * 'should not play/pause when `amp-audio` is a direct descendant of `amp-story`'
-     *
-     * Refer: https://github.com/ampproject/amphtml/pull/35643
-     */
   }
 );
