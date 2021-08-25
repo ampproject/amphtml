cd9856084fcc7c0a273a6c3c13b6426703788495
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-08-25 17:36:26 -0500

    🚮 Remove IE Support via Polyfills and Conditional Statements (#35317)
    * Since we are not using the data, lets remove it
    
    * Remove all isIE checks and polyfills only used by IE
    
    * Remove URL changes
    
    * Integration tests were looking for IE specifics
    
    * remove transparent img, and ie tests
    
    * Skip all tests on IE, this functionality will be removed by the infra team following success
    
    * try using user agent directy in test configuration
    
    * Fix
    
    * Feedback on describe tests not needing configuration now
    
    * Remove unused files again
    
    * Fix build before main is repaired

--- test/integration/test-shadow-dom-element.js
+++ test/integration/test-shadow-dom-element.js
@@ -6,7 +6,6 @@ import {createFixtureIframe} from '#testing/iframe';
 
 describes.sandboxed
   .configure()
-  .enableIe()
   .run('Render a shadow-dom based element', {}, () => {
     let fixture;
 
