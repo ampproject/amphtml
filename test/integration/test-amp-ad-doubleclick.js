d87e3929dbbcd209b75f5a0915cf5fd21dad29a0
Raghu Simha <rsimha@amp.dev>
2021-08-26 11:31:43 -0400

    🏗🚮 Remove all IE11 infrastructure code paths (#35341)

--- test/integration/test-amp-ad-doubleclick.js
+++ test/integration/test-amp-ad-doubleclick.js
@@ -1,6 +1,6 @@
 import {createFixtureIframe, poll, pollForLayout} from '#testing/iframe';
 
-describes.sandboxed.configure().run('Rendering of one ad', {}, () => {
+describes.sandboxed('Rendering of one ad', {}, () => {
   let fixture;
   let beforeHref;
 
