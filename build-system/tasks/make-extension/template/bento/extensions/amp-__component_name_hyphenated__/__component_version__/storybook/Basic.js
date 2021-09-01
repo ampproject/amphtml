27cc3f9951829b4224c29093818952fec8a56e38
Alan Orozco <alan@orozco.xyz>
2021-09-01 11:15:45 -0700

    remove from generator

--- build-system/tasks/make-extension/template/bento/extensions/amp-__component_name_hyphenated__/__component_version__/storybook/Basic.js
+++ build-system/tasks/make-extension/template/bento/extensions/amp-__component_name_hyphenated__/__component_version__/storybook/Basic.js
@@ -1,19 +1,20 @@
 import * as Preact from '#preact';
 import {__component_name_pascalcase__} from '../component'
-import {withKnobs} from '@storybook/addon-knobs';
 
 export default {
   title: '__component_name_pascalcase__',
   component: __component_name_pascalcase__,
-  decorators: [withKnobs],
+  args: {
+    'exampleProperty': 'example string property argument'
+  }
 };
 
-export const _default = () => {
-  // __do_not_submit__: This is example code only.
+// __do_not_submit__: This is example code only.
+export const _default = (args) => {
   return (
     <__component_name_pascalcase__
       style={{width: 300, height: 200}}
-      example-property="example string property value"
+      {...args}
     >
       This text is inside.
     </__component_name_pascalcase__>
