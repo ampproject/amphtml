ae6fce7fb1f1632ce12a9e794bcaa6649d6354f3
rebeccanthomas <64608436+rebeccanthomas@users.noreply.github.com>
2021-08-31 17:15:47 -0700

    [`bento-reddit`] Add Storybook Basic.js for bento-reddit.

--- extensions/amp-reddit/1.0/storybook/Basic.js
+++ extensions/amp-reddit/1.0/storybook/Basic.js
@@ -28,12 +28,24 @@ export const _default = () => {
   return (
     <Reddit
       style={{width: 300, height: 200}}
+<<<<<<< Updated upstream
       src=
           'https://www.reddit.com/r/me_irl/comments/52rmir/me_irl/?ref=share&amp;ref_source=embed'
     ></Reddit>
   );
 };
       // options={{"src":"https://www.reddit.com/r/me_irl/comments/52rmir/me_irl/?ref=share&amp;ref_source=embed"}}
+=======
+      bootstrap="./vendor/reddit.max.js"
+      src="http://ads.localhost:8000/dist.3p/current/frame.max.html"
+      options={{
+        'href':
+          'https://www.reddit.com/r/me_irl/comments/52rmir/me_irl/?ref=share&amp;ref_source=embed',
+      }}
+    ></Reddit>
+  );
+};
+>>>>>>> Stashed changes
 
 // <Reddit
 //   style={{width: 300, height: 200}}
