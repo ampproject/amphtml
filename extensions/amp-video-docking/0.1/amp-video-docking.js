01481346ccef6009c16453300eefcb594767b209
Tobias von Klipstein <tobias@klpstn.com>
2021-09-02 17:55:38 +0200

    🐛 [amp-video-docking] Fix issue hiding docked player during scroll on mobile (#35839)
    Test-Case:
    
    - Start via `amp --extensions=amp-video-iframe,amp-video-docking`
    - Open http://localhost:8000/examples/article-with-docked-video.html with the iOS simulator
    - Play first video in article
    - Scroll down so that the player is docked
    - Now scroll further down and then quickly up so that the URL bar is shown again
    - Result => dock gets hidden
    
    This pull request fixes the problem by only checking whether the width changed on resize to decide whether the player should be undocked.
    
    Also see this comment, which also explains the bug: https://github.com/ampproject/amphtml/issues/27825#issuecomment-656565478
    
    cc @alanorozco

--- extensions/amp-video-docking/0.1/amp-video-docking.js
+++ extensions/amp-video-docking/0.1/amp-video-docking.js
@@ -128,6 +128,7 @@ export const DockTargetType = {
  *   video: !VideoOrBaseElementDef,
  *   target: !DockTargetDef,
  *   step: number,
+ *   viewportRect: !RectDef,
  * }}
  */
 let DockedDef;
@@ -435,6 +436,15 @@ export class VideoDocking {
 
   /** @private */
   onViewportResize_() {
+    if (
+      this.viewportRect_.width === this.currentlyDocked_?.viewportRect.width
+    ) {
+      // Ignore resize events that occur when only the height changes.
+      // This works around issues where the browser may hide the location bar,
+      // or a virtual keyboard; causing a height-only resize that would
+      // otherwise undock the video.
+      return;
+    }
     this.observed_.forEach((video) => this.updateOnResize_(video));
   }
 
@@ -1154,7 +1164,8 @@ export class VideoDocking {
    */
   setCurrentlyDocked_(video, target, step) {
     const previouslyDocked = this.currentlyDocked_;
-    this.currentlyDocked_ = {video, target, step};
+    const viewportRect = {...this.viewportRect_};
+    this.currentlyDocked_ = {video, target, step, viewportRect};
     if (
       previouslyDocked &&
       previouslyDocked.video == video &&
