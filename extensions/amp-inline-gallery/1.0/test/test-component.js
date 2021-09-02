60d3d18838c4ac67ddc087304b69a6e8428b97fc
Dhruv Manek <dmanek@gmail.com>
2021-09-02 14:26:01 -0700

    fix failingwq unit tests

--- extensions/amp-inline-gallery/1.0/test/test-component.js
+++ extensions/amp-inline-gallery/1.0/test/test-component.js
@@ -1,5 +1,5 @@
 import * as Preact from '#preact';
-import {BaseCarousel} from '../../../amp-base-carousel/1.0/component';
+import {BentoBaseCarousel} from '../../../amp-base-carousel/1.0/component';
 import {InlineGallery} from '../component';
 import {Pagination} from '../pagination';
 import {Thumbnails} from '../thumbnails';
@@ -7,19 +7,19 @@ import {mount} from 'enzyme';
 
 describes.sandboxed('InlineGallery preact component', {}, () => {
   describe('Pagination component', () => {
-    it('should render BaseCarousel and Pagination', () => {
+    it('should render BentoBaseCarousel and Pagination', () => {
       const jsx = (
         <InlineGallery>
-          <BaseCarousel>
+          <BentoBaseCarousel>
             <div>slide 1</div>
             <div>slide 2</div>
             <div>slide 3</div>
-          </BaseCarousel>
+          </BentoBaseCarousel>
           <Pagination />
         </InlineGallery>
       );
       const wrapper = mount(jsx);
-      const carousel = wrapper.find('BaseCarousel');
+      const carousel = wrapper.find('BentoBaseCarousel');
       expect(carousel).to.have.lengthOf(1);
       const slides = carousel.find('[data-slide]');
       expect(slides).to.have.lengthOf(3);
@@ -30,27 +30,27 @@ describes.sandboxed('InlineGallery preact component', {}, () => {
   });
 
   describe('Thumbnail component', () => {
-    it('should render BaseCarousel and Thumbnails', () => {
+    it('should render BentoBaseCarousel and Thumbnails', () => {
       const jsx = (
         <InlineGallery>
-          <BaseCarousel>
+          <BentoBaseCarousel>
             <div>slide 1</div>
             <div>slide 2</div>
             <div>slide 3</div>
-          </BaseCarousel>
+          </BentoBaseCarousel>
           <Thumbnails />
         </InlineGallery>
       );
       const wrapper = mount(jsx);
 
-      const carousels = wrapper.find('BaseCarousel');
+      const carousels = wrapper.find('BentoBaseCarousel');
       expect(carousels).to.have.lengthOf(2);
       const slides = carousels.first().find('[data-slide]');
       expect(slides).to.have.lengthOf(3);
 
       const thumbnails = wrapper.find('Thumbnails');
       expect(thumbnails).to.have.lengthOf(1);
-      const generatedCarousel = thumbnails.find('BaseCarousel');
+      const generatedCarousel = thumbnails.find('BentoBaseCarousel');
       expect(generatedCarousel).to.have.lengthOf(1);
       expect(generatedCarousel.prop('loop')).to.be.false;
       expect(generatedCarousel.prop('snapAlign')).to.equal('start');
@@ -68,24 +68,24 @@ describes.sandboxed('InlineGallery preact component', {}, () => {
     it('should respect thumbnailSrc', () => {
       const jsx = (
         <InlineGallery>
-          <BaseCarousel>
+          <BentoBaseCarousel>
             <div thumbnailSrc="slide1.jpg">slide 1</div>
             <div thumbnailSrc="slide2.jpg">slide 2</div>
             <div thumbnailSrc="slide3.jpg">slide 3</div>
-          </BaseCarousel>
+          </BentoBaseCarousel>
           <Thumbnails />
         </InlineGallery>
       );
       const wrapper = mount(jsx);
 
-      const carousels = wrapper.find('BaseCarousel');
+      const carousels = wrapper.find('BentoBaseCarousel');
       expect(carousels).to.have.lengthOf(2);
       const slides = carousels.first().find('[data-slide]');
       expect(slides).to.have.lengthOf(3);
 
       const thumbnails = wrapper.find('Thumbnails');
       expect(thumbnails).to.have.lengthOf(1);
-      const generatedCarousel = thumbnails.find('BaseCarousel');
+      const generatedCarousel = thumbnails.find('BentoBaseCarousel');
       expect(generatedCarousel).to.have.lengthOf(1);
       expect(generatedCarousel.prop('loop')).to.be.false;
       expect(generatedCarousel.prop('snapAlign')).to.equal('start');
@@ -109,24 +109,24 @@ describes.sandboxed('InlineGallery preact component', {}, () => {
     it('should respect looping with slide alignment', () => {
       const jsx = (
         <InlineGallery>
-          <BaseCarousel>
+          <BentoBaseCarousel>
             <div>slide 1</div>
             <div>slide 2</div>
             <div>slide 3</div>
-          </BaseCarousel>
+          </BentoBaseCarousel>
           <Thumbnails loop />
         </InlineGallery>
       );
       const wrapper = mount(jsx);
 
-      const carousels = wrapper.find('BaseCarousel');
+      const carousels = wrapper.find('BentoBaseCarousel');
       expect(carousels).to.have.lengthOf(2);
       const slides = carousels.first().find('[data-slide]');
       expect(slides).to.have.lengthOf(3);
 
       const thumbnails = wrapper.find('Thumbnails');
       expect(thumbnails).to.have.lengthOf(1);
-      const generatedCarousel = thumbnails.find('BaseCarousel');
+      const generatedCarousel = thumbnails.find('BentoBaseCarousel');
       expect(generatedCarousel).to.have.lengthOf(1);
       expect(generatedCarousel.prop('loop')).to.be.true;
       expect(generatedCarousel.prop('snapAlign')).to.equal('center');
