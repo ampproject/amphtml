1aaf5c3c52f5d0066c00434c0b7bb444dc0c5092
Jake Fried <samouri@users.noreply.github.com>
2021-09-09 15:53:22 -0400

    forbidden terms

--- build-system/test-configs/forbidden-terms.js
+++ build-system/test-configs/forbidden-terms.js
@@ -1083,7 +1083,6 @@ const forbiddenTermsSrcInclusive = {
       'extensions/amp-ad-network-adsense-impl/0.1/amp-ad-network-adsense-impl.js',
       'extensions/amp-ad-network-doubleclick-impl/0.1/amp-ad-network-doubleclick-impl.js',
       'extensions/amp-ad/0.1/amp-ad-3p-impl.js',
-      'extensions/amp-iframe/0.1/amp-iframe.js',
       'src/custom-element.js',
       'src/utils/intersection-observer-3p-host.js',
     ],
