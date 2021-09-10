8ddf72423162133b2318d953adaeff07b4e87142
dmanek <506183+dmanek@users.noreply.github.com>
2021-09-09 22:44:39 -0700

    Custom hook using IntersectionObserver (#35835)

---
+++ src/preact/component/intersection-observer-resize.js
@@ -0,0 +1,32 @@
+import {
+  observeIntersections,
+  unobserveWithSharedInOb,
+} from '#core/dom/layout/viewport-observer';
+
+import {useCallback, useRef} from '#preact';
+
+/**
+ * Uses a shared IntersectionObserver per window instance to observe the given `ref`.
+ *
+ * @param {function(IntersectionObserverEntry)} callback
+ * @return {function(Element)}
+ */
+export function useIntersectionObserver(callback) {
+  const nodeRef = useRef(null);
+  const refCb = useCallback(
+    (node) => {
+      const prevNode = nodeRef.current;
+      nodeRef.current = node;
+      if (prevNode) {
+        unobserveWithSharedInOb(prevNode);
+      }
+      if (!node) {
+        return;
+      }
+      observeIntersections(node, callback);
+    },
+    [callback]
+  );
+
+  return refCb;
+}
