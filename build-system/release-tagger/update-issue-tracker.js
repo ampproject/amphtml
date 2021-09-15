d69b2f957f692e0eb158597dfb08ac96c8533b16
esth <esth@google.com>
2021-09-15 15:59:16 -0400

    convert time eastern

--- build-system/release-tagger/update-issue-tracker.js
+++ build-system/release-tagger/update-issue-tracker.js
@@ -102,6 +102,9 @@ class IssueTracker {
  * @return {Promise<void>}
  */
 async function createOrUpdateTracker(head, base, channel, time) {
+  const timeET = new Date(time).toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+  });
   const isCherrypick = Number(head) - Number(base) < 1000;
   const issue = isCherrypick
     ? await getIssue(`Release ${base}`)
@@ -110,7 +113,7 @@ async function createOrUpdateTracker(head, base, channel, time) {
   // create new tracker
   if (!issue) {
     const tracker = new IssueTracker(head, base);
-    tracker.checkTask(channel, time);
+    tracker.checkTask(channel, timeET);
     const {footer, header, label, main, title} = tracker;
     const body = `${header}\n\n${main}\n\n${footer}`;
     return await createIssue(body, label, title);
@@ -121,7 +124,7 @@ async function createOrUpdateTracker(head, base, channel, time) {
   if (isCherrypick) {
     tracker.addCherrypickTasks(channel);
   }
-  tracker.checkTask(channel, time);
+  tracker.checkTask(channel, timeET);
   const {footer, header, main, number, title} = tracker;
   const body = `${header}\n\n${main}\n\n${footer}`;
   await updateIssue(body, number, title);
