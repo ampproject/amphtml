6f47a2e84f10efd11f5e883546eefcd803273d64
Jake Fried <samouri@users.noreply.github.com>
2021-09-22 15:21:46 -0400

    babel-plugin: rename all amp privates with sentinel suffix

---
+++ build-system/babel-plugins/babel-plugin-transform-rename-privates/index.js
@@ -0,0 +1,34 @@
+/**
+ * Renames all private variables within AMP to end with AMP_PRIVATE_ for safe
+ * identification later w/ terser. At that point it may be combined with files from 3p or node_modules,
+ * where we cannot guarantee that the `_` suffix means private.
+ *
+ * @interface {babel.PluginPass}
+ * @param {babel} babel
+ * @return {babel.PluginObj}
+ */
+module.exports = function (babel) {
+  return {
+    visitor: {
+      Identifier(path, state) {
+        const filename = state.file.opts.filenameRelative;
+        if (!filename) {
+          throw new Error('Cannot use plugin without providing a filename');
+        }
+
+        const isAmpSrcCode =
+          filename.startsWith('src/') || filename.startsWith('extensions/');
+        if (!isAmpSrcCode) {
+          return;
+        }
+
+        // AMP Privates are marked via trailing suffix.
+        if (!path.node.name.endsWith('_')) {
+          return;
+        }
+
+        path.node.name += `AMP_PRIVATE_`;
+      },
+    },
+  };
+};
---
+++ build-system/babel-plugins/babel-plugin-transform-rename-privates/test/fixtures/transform/rename-amp-privates/output.mjs
@@ -0,0 +1,10 @@
+class Foo {
+  constructor() {
+    /**
+     * @type {string}
+     * @private
+     */
+    this.hello_AMP_PRIVATE_ = 'world';
+  }
+
+}
