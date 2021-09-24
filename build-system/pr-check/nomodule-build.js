5923b61b34e254d137b7a5ce9d97acaad608cdb7
Daniel Rozenberg <rodaniel@amp.dev>
2021-09-24 12:26:52 -0400

    DO NOT MERGE attempting to recreate an `amp dist` crash

--- build-system/pr-check/nomodule-build.js
+++ build-system/pr-check/nomodule-build.js
@@ -6,7 +6,6 @@
 
 const {
   abortTimedJob,
-  skipDependentJobs,
   startTimer,
   storeNomoduleBuildToWorkspace,
   timedExecOrDie,
@@ -16,7 +15,6 @@ const {log} = require('../common/logging');
 const {red, yellow} = require('kleur/colors');
 const {runCiJob} = require('./ci-job');
 const {signalPrDeployUpload} = require('../tasks/pr-deploy-bot-utils');
-const {Targets, buildTargetsInclude} = require('./build-targets');
 
 const jobName = 'nomodule-build.js';
 
@@ -34,31 +32,14 @@ function pushBuildWorkflow() {
  */
 async function prBuildWorkflow() {
   const startTime = startTimer(jobName);
-  if (
-    buildTargetsInclude(
-      Targets.RUNTIME,
-      Targets.INTEGRATION_TEST,
-      Targets.E2E_TEST,
-      Targets.VISUAL_DIFF
-    )
-  ) {
-    const process = timedExecWithError('amp dist --fortesting');
-    if (process.status !== 0) {
-      const message = process?.error
-        ? process.error.message
-        : 'Unknown error, check logs';
-      log(red('ERROR'), yellow(message));
-      await signalPrDeployUpload('errored');
-      return abortTimedJob(jobName, startTime);
-    }
-    storeNomoduleBuildToWorkspace();
-  } else {
-    await signalPrDeployUpload('skipped');
-    timedExecOrDie('amp visual-diff --empty');
-    skipDependentJobs(
-      jobName,
-      'this PR does not affect the runtime, integration tests, end-to-end tests, or visual diff tests'
-    );
+  const process = timedExecWithError('amp dist --fortesting');
+  if (process.status !== 0) {
+    const message = process?.error
+      ? process.error.message
+      : 'Unknown error, check logs';
+    log(red('ERROR'), yellow(message));
+    await signalPrDeployUpload('errored');
+    return abortTimedJob(jobName, startTime);
   }
 }
 
