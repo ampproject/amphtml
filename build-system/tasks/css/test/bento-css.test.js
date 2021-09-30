6bbd519d44ade6419727dd922280e2cdca5578d2
Alan Orozco <alan@orozco.xyz>
2021-09-30 09:32:26 -0700

    test

---
+++ build-system/tasks/css/test/bento-css.test.js
@@ -0,0 +1,67 @@
+const test = require('ava');
+const {renameSelectorsToBentoTagNames} = require('../bento-css');
+
+const cases = {
+  'simple': [
+    `amp-foo {
+      background: red;
+    }`,
+    `bento-foo {
+      background: red;
+    }`,
+  ],
+  'multiple': [
+    `amp-foo {
+      background: red;
+    }
+    amp-bar {
+      color: blue;
+    }
+    div {
+      transform: translate(100%, 0);
+    }`,
+    `bento-foo {
+      background: red;
+    }
+    bento-bar {
+      color: blue;
+    }
+    div {
+      transform: translate(100%, 0);
+    }`,
+  ],
+  'mixed selector': [
+    `amp-foo > div .amp-bar:not(amp-whatever) {
+      background: red;
+    }`,
+    `bento-foo > div .amp-bar:not(bento-whatever) {
+      background: red;
+    }`,
+  ],
+  'not a rule': [
+    `@keyframes amp-x {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }`,
+    `@keyframes amp-x {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }`,
+  ],
+};
+
+for (const name of Object.keys(cases)) {
+  test(name, async (t) => {
+    const [input, expected] = cases[name];
+    const output = await renameSelectorsToBentoTagNames(input, 'foo.css');
+    t.is(output, expected);
+  });
+}
