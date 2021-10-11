5873ceeb8c08d673de008d064ff2c608e14be8f8
Chris Almeida-Symons <chris.almeida-symons@dianomi.com>
2021-10-11 11:31:25 +0100

    Updates from lint error in CircleCI

--- extensions/amp-ad-network-dianomi-impl/0.1/amp-ad-network-dianomi-impl.js
+++ extensions/amp-ad-network-dianomi-impl/0.1/amp-ad-network-dianomi-impl.js
@@ -1,8 +1,9 @@
 import {CONSENT_POLICY_STATE} from '#core/constants/consent-state';
 
+import {userAssert} from '#utils/log';
+
 import {addParamsToUrl} from 'src/url';
 
-import {userAssert} from '#utils/log';
 import {AmpA4A} from '../../amp-a4a/0.1/amp-a4a';
 
 const REQUEST_PARAM_ID = new RegExp(/^.[0-9]*$/);
