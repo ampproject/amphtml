56a6200c74ff215ac77b92c1ae84a611f7b518a4
Alan Orozco <alan@orozco.xyz>
2021-10-13 15:33:12 -0700

    test

--- build-system/tasks/css/test/bento-css.test.js
+++ build-system/tasks/css/test/bento-css.test.js
@@ -1,89 +1,101 @@
 const test = require('ava');
 const {renameSelectorsToBentoTagNames} = require('../bento-css');
 
-const cases = {
-  'simple': [
-    `amp-foo {
-      background: red;
-    }`,
-    `bento-foo {
-      background: red;
-    }`,
-  ],
-  'nested': [
-    `@media screen {
-      amp-foo {
-        background: red;
-      }
-    }`,
-    `@media screen {
-      bento-foo {
-        background: red;
-      }
-    }`,
-  ],
-  'multiple': [
-    `amp-foo {
-      background: red;
-    }
-    amp-bar {
-      color: blue;
-    }
-    div {
-      transform: translate(100%, 0);
-    }
-    has-amp-but-does-not-start-with-amp- {
-      background: yellow;
-    }`,
-    `bento-foo {
-      background: red;
-    }
-    bento-bar {
-      color: blue;
-    }
-    div {
-      transform: translate(100%, 0);
-    }
-    has-amp-but-does-not-start-with-amp- {
-      background: yellow;
-    }`,
-  ],
-  'mixed selector': [
-    `amp-foo > div .amp-bar:not(amp-whatever) {
-      background: red;
-    }`,
-    `bento-foo > div .amp-bar:not(bento-whatever) {
-      background: red;
-    }`,
-  ],
-  'ignores @rule names': [
-    `@keyframes amp-x {
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    }`,
-    `@keyframes amp-x {
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    }`,
-  ],
-  'preserves minification': [
-    `amp-foo>div:not(.amp-classname){background:red}amp-whatever{color:blue}`,
-    `bento-foo>div:not(.amp-classname){background:red}bento-whatever{color:blue}`,
-  ],
-};
-
-for (const name of Object.keys(cases)) {
-  test(`renames amp-* selectors to bento-* (${name})`, async (t) => {
-    const [input, expected] = cases[name];
-    const output = await renameSelectorsToBentoTagNames(input);
-    t.is(output, expected);
-  });
+async function renameSelectorsToBentoTagNamesExpected(t, input, expected) {
+  const output = await renameSelectorsToBentoTagNames(input);
+  t.is(output, expected);
 }
+
+test(
+  'renames amp-* selectors to bento-* (simple)',
+  renameSelectorsToBentoTagNamesExpected,
+  `amp-foo {
+    background: red;
+  }`,
+  `bento-foo {
+    background: red;
+  }`
+);
+
+test(
+  'renames amp-* selectors to bento-* (nested)',
+  renameSelectorsToBentoTagNamesExpected,
+  `@media screen {
+    amp-foo {
+      background: red;
+    }
+  }`,
+  `@media screen {
+    bento-foo {
+      background: red;
+    }
+  }`
+);
+
+test(
+  'renames amp-* selectors to bento-* (multiple)',
+  renameSelectorsToBentoTagNamesExpected,
+  `amp-foo {
+    background: red;
+  }
+  amp-bar {
+    color: blue;
+  }
+  div {
+    transform: translate(100%, 0);
+  }
+  has-amp-but-does-not-start-with-amp- {
+    background: yellow;
+  }`,
+  `bento-foo {
+    background: red;
+  }
+  bento-bar {
+    color: blue;
+  }
+  div {
+    transform: translate(100%, 0);
+  }
+  has-amp-but-does-not-start-with-amp- {
+    background: yellow;
+  }`
+);
+
+test(
+  'renames amp-* selectors to bento-* (mixed selector)',
+  renameSelectorsToBentoTagNamesExpected,
+  `amp-foo > div .amp-bar:not(amp-whatever) {
+    background: red;
+  }`,
+  `bento-foo > div .amp-bar:not(bento-whatever) {
+    background: red;
+  }`
+);
+
+test(
+  'renames amp-* selectors to bento-* (ignores @rule names)',
+  renameSelectorsToBentoTagNamesExpected,
+  `@keyframes amp-x {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }`,
+  `@keyframes amp-x {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }`
+);
+
+test(
+  'renames amp-* selectors to bento-* (preserves minification)',
+  renameSelectorsToBentoTagNamesExpected,
+  `amp-foo>div:not(.amp-classname){background:red}amp-whatever{color:blue}`,
+  `bento-foo>div:not(.amp-classname){background:red}bento-whatever{color:blue}`
+);
