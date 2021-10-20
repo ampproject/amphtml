696f5d895620122a5c1b82b8d4bc4826b0d101f8
Alan Orozco <alan@orozco.xyz>
2021-10-20 15:12:37 -0700

    test name

--- test/e2e/test-bento.js
+++ test/e2e/test-bento.js
@@ -5,7 +5,7 @@ describes.endtoend(
     fixture: 'bento/minimal.html',
   },
   async function (env) {
-    it('attaches shadow root without v0.js', async () => {
+    it('attaches shadow root', async () => {
       // Set timeout lower than default in order to fail early, since the root
       // is attached as soon as possible.
       // (Feel free to increase if flaky.)
