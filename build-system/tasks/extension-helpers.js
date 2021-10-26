c2b85358497a629e4f44aad73033f7229a0c53ab
Alan Orozco <alan@orozco.xyz>
2021-10-25 19:30:18 -0700

    ajdkljasdl

--- build-system/tasks/extension-helpers.js
+++ build-system/tasks/extension-helpers.js
@@ -357,7 +357,7 @@ async function buildExtensions(options) {
       options.compileOnlyCss ||
       extensionsToBuild.includes(extensions[extension].name)
     ) {
-      console./*OK*/ log('started', extension[extension].name);
+      console./*OK*/ log('started', extensions[extension].name);
       results.push(doBuildExtension(extensions, extension, options));
     }
   }
