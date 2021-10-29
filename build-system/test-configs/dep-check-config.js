55fa3e004e2aeece5faf9a68717228077c215a13
Jonathan <jshamble@users.noreply.github.com>
2021-10-28 22:45:38 -0700

    ✨amp-story-shopping scaffold : contains shopping config, attachment, and tag. (#36462)
    * stories code lab work so far
    
    * updated shopping comonent to add vlidation and unit tests
    
    * updated stories codelab unit tests to work
    
    * finished the codelab
    
    * Added boiler plate with working linked css files and working test cases for amp story shopping tag, attachment, and config components
    
    * cleaned up a few files
    
    * added css file to make circleci happy
    
    * fixed formatting of owners file
    
    * added validator tests
    
    * Updated validation test
    
    * added descendat tags
    
    * added layout changes
    
    * added validtor tags for amp story shopping subcomponenets to story protoascii file
    
    * Added subcomponent elements to valdiation tests
    
    * fixed formatting
    
    * added mandatory ancestors
    
    * Added amp story page
    
    * added auto width for tag element
    
    * added tag and config to amp-story-protoascii
    
    * added child tags to correct location
    
    * fixed layout issues
    
    * updated validation tests
    
    * added mroe validation checks
    
    * Added increased test coverage for layouts
    
    * fixed unit test with correct alyout
    
    * added increased code coverage
    
    * added increased test coverage
    
    * fixed code coverage title bug with amp shopping tag unit test (needed an extra dash between the version number)
    
    * cleanup up unit tests
    
    * added shopping tag stuff
    
    * added requires usage method
    
    * cleaned up config files and test files
    
    * reconfigured protoascii
    
    * removed unnescessary config files
    
    * updated ownders files
    
    * fixed layout attribute so that publisher don't need ot specify
    
    * added fixed to css files
    
    * updated owners file
    
    * removed hasCSs line
    
    * removed css files
    
    * updated owners file
    
    * removed validation test stuff
    
    * removed css files
    
    * added needed css file
    
    * added needed css file
    
    * Added shopping attachment dependency to page attachment
    
    * fixed typo
    
    * fixed unit tests for code coverage
    
    * clean build
    
    * optimizated unit tests
    
    * fixed shopping tag file
    
    * fixed fixed height
    
    * added super call to buildcallBack
    
    * removed unesc. import
    
    * changed css file to support multiple imports
    
    * updated suint test
    
    * fixed validation file formatting
    
    * cleanup up validation files
    
    * removed newline
    
    * removed newline
    
    * removed newline

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -263,6 +263,9 @@ exports.rules = [
       'extensions/amp-story-education/0.1/amp-story-education.js->extensions/amp-story/1.0/utils.js',
       'extensions/amp-story-education/0.1/amp-story-education.js->extensions/amp-story/1.0/amp-story-localization-service.js',
 
+      // Story Shopping
+      'extensions/amp-story-shopping/0.1/amp-story-shopping-attachment.js->extensions/amp-story/1.0/amp-story-page-attachment.js',
+
       // Interactive components that depend on story functionality.
       'extensions/amp-story-interactive/0.1/amp-story-interactive-abstract.js->extensions/amp-story/1.0/amp-story-store-service.js',
       'extensions/amp-story-interactive/0.1/amp-story-interactive-abstract.js->extensions/amp-story/1.0/story-analytics.js',
