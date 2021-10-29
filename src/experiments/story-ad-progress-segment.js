674e9819559af5db4d5678be9002ea5df9c3d9f3
Caleb Cordry <ccordry@google.com>
2021-10-29 15:54:56 -0700

    update ids

--- src/experiments/story-ad-progress-segment.js
+++ src/experiments/story-ad-progress-segment.js
@@ -1,12 +1,12 @@
 /** @const */
 export const StoryAdSegmentExp = {
   ID: 'story-ad-progress-segment',
-  CONTROL: '31063366', // No ad advance, no page advance, no new cta.
-  NO_ADVANCE_BOTH: '31063367', // No ad advance, no page advance, new cta.
-  NO_ADVANCE_AD: '31063367', // No ad advance, page=10s, new cta.
-  TEN_SECONDS: '31063368', // Advance ad with new cta for the rest, page=10s.
-  TWELVE_SECONDS: '31063369',
-  FOURTEEN_SECONDS: '31063370',
+  CONTROL: '31063379', // No ad advance, no page advance, no new cta.
+  NO_ADVANCE_BOTH: '31063380', // No ad advance, no page advance, new cta.
+  NO_ADVANCE_AD: '31063381', // No ad advance, page=10s, new cta.
+  TEN_SECONDS: '31063382', // Advance ad with new cta for the rest, page=10s.
+  TWELVE_SECONDS: '31063383',
+  FOURTEEN_SECONDS: '31063384',
 };
 
 /** @const */
