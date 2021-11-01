ad19908d594c1cfd9ee45416bafa8e130fc8592a
Alan Orozco <alan@orozco.xyz>
2021-11-01 08:08:31 -0700

    🏗 Remove `freeze` optiion from babel-plugin-transform-json-import

--- build-system/babel-plugins/babel-plugin-transform-json-import/test/fixtures/transform-assertions/transform-minify-locales/output.mjs
+++ build-system/babel-plugins/babel-plugin-transform-json-import/test/fixtures/transform-assertions/transform-minify-locales/output.mjs
@@ -1,10 +1,4 @@
 // options.js is left as is
 // lang.json is minified because the path includes /_locales/
-const options = JSON.parse("{\"plugins\":[\"../../../..\"],\"sourceType\":\"module\"}", function (key, val) {
-  if (typeof val === 'object') Object.freeze(val);
-  return val;
-});
-const lang = JSON.parse("{\"2\":\"foo\",\"15\":\"bar\"}", function (key, val) {
-  if (typeof val === 'object') Object.freeze(val);
-  return val;
-});
+const options = JSON.parse("{\"plugins\":[\"../../../..\"],\"sourceType\":\"module\"}");
+const lang = JSON.parse("{\"2\":\"foo\",\"15\":\"bar\"}");
