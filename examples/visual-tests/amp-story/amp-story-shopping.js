75192c737d0cff4cbd41792eb006da14845ac4e0
processprocess <philip.hunter.bell@gmail.com>
2021-11-02 16:18:30 -0400

    Add visual test

---
+++ examples/visual-tests/amp-story/amp-story-shopping.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {
+  verifySelectorsVisible,
+} = require('../../../build-system/tasks/visual-diff/helpers');
+
+module.exports = {
+  'default shopping CTA UI should display': async (page, name) => {
+    const pageID = 'default';
+    const url = await page.url();
+    await page.goto(`${url}#page=${pageID}`);
+    await page.waitForSelector(
+      `amp-story-page#${pageID}[active][distance="0"]`
+    );
+    await verifySelectorsVisible(page, name, [
+      '.i-amphtml-story-page-open-attachment[active]',
+    ]);
+  },
+
+  'dark theme - shopping CTA UI should display': async (page, name) => {
+    const pageID = 'dark-theme';
+    const url = await page.url();
+    await page.goto(`${url}#page=${pageID}`);
+    await page.waitForSelector(
+      `amp-story-page#${pageID}[active][distance="0"]`
+    );
+    await verifySelectorsVisible(page, name, [
+      '.i-amphtml-story-page-open-attachment[active]',
+    ]);
+  },
+};
