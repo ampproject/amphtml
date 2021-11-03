43674a19ab2cb596d4de119ab8c858db6a55863e
Deepak Lalwani <deepak.lalwani81@gmail.com>
2021-11-03 13:09:14 +0530

    Nit: Fix typo and linitng in component.jss

--- extensions/amp-twitter/1.0/component.jss.js
+++ extensions/amp-twitter/1.0/component.jss.js
@@ -7,15 +7,15 @@ const loaderWrapper = {
 };
 
 const loader = {
-  border: '1px solid #f3f3f3' /* Light grey */,
-  'border-top': '2px solid #a3a3a3' /* Blue */,
-  'border-radius': '50%',
-  width: '48px',
-  height: '48px',
   animation: '$spin 2s linear infinite',
-  position: 'absolute',
+  border: '1px solid #f3f3f3' /* Light grey */,
+  'border-radius': '50%',
+  'border-top': '2px solid #a3a3a3' /* Dark grey */,
+  height: '48px',
   left: 'calc(50% - 24px)',
+  position: 'absolute',
   top: 'calc(50% - 24px)',
+  width: '48px',
 };
 
 const JSS = {
