a8baaeacd4c9c2282962fa25094dec5fce89e468
Alan Orozco <alanorozco@users.noreply.github.com>
2021-11-09 13:00:46 -0800

    🏗🚀 Speed up `amp get-zindex` ~4x (#36836)
    These changes speed up the `amp get-zindex` task by ~4x.
    
    1. Use `babel` for simple AST parsing instead of `jscodeshift`. 
    1. Shortcut by checking pattern inline instead of grepping early. 
    
    Additionally, we uninstall `jscodeshift` as a global dependency. The only task remaining that uses it is `sweep-experiments`, which does not run on CI. It uses `npx` to temporarily install on execution instead.

--- build-system/tasks/get-zindex/index.js
+++ build-system/tasks/get-zindex/index.js
@@ -5,11 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const Postcss = require('postcss');
 const prettier = require('prettier');
-const {
-  getJscodeshiftReport,
-  jscodeshiftAsync,
-} = require('../../test-configs/jscodeshift');
-const {getStdout} = require('../../common/process');
+const {getZindexJs} = require('./js');
 const {gray, magenta} = require('kleur/colors');
 const {logLocalDev, logOnSameLineLocalDev} = require('../../common/logging');
 const {writeDiffOrFail} = require('../../common/diff');
@@ -120,65 +116,25 @@ async function getZindexSelectors(glob, cwd = '.') {
  * @param {string=} cwd
  * @return {!Promise<Object>}
  */
-function getZindexChainsInJs(glob, cwd = '.') {
-  return new Promise((resolve) => {
-    const files = fastGlob
-      .sync(glob, {cwd})
-      .map((file) => path.join(cwd, file));
+async function getZindexChainsInJs(glob, cwd = '.') {
+  const files = (await fastGlob(glob, {cwd})).map((file) =>
+    path.join(cwd, file)
+  );
 
-    const filesIncludingString = getStdout(
-      ['grep -irl "z-*index"', ...files].join(' ')
-    )
-      .trim()
-      .split('\n');
-
-    const result = {};
-
-    let resultCountInverse = filesIncludingString.length;
-
-    if (resultCountInverse === 0) {
-      // We don't expect this fileset to be empty since it's unlikely that we
-      // never change the z-index from JS, but we add this just in case to
-      // prevent hanging infinitely.
-      resolve(result);
-      return;
-    }
-
-    const process = jscodeshiftAsync([
-      '--dry',
-      '--no-babel',
-      `--transform=${__dirname}/jscodeshift/collect-zindex.js`,
-      ...filesIncludingString,
-    ]);
-    const {stderr, stdout} = process;
-
-    stderr.on('data', (data) => {
-      throw new Error(data.toString());
-    });
-
-    stdout.on('data', (data) => {
-      const reportLine = getJscodeshiftReport(data.toString());
-
-      if (!reportLine) {
-        return;
-      }
-
-      const [filename, report] = reportLine;
-      const relative = path.relative(cwd, filename);
-
+  const resultEntries = await Promise.all(
+    files.map(async (filename) => {
       logChecking(filename);
+      const relative = path.relative(cwd, filename);
+      const found = await getZindexJs(filename);
+      return [relative, found];
+    })
+  );
 
-      try {
-        const reportParsed = JSON.parse(report);
-        if (reportParsed.length) {
-          result[relative] = reportParsed.sort(sortedByEntryKey);
-        }
-        if (--resultCountInverse === 0) {
-          resolve(result);
-        }
-      } catch (_) {}
-    });
-  });
+  return Object.fromEntries(
+    resultEntries
+      // eslint-disable-next-line local/no-deep-destructuring
+      .filter(([, {length}]) => length)
+  );
 }
 
 /**
