9fcaaf451ed684e1a301d1399943977241b2e133
Erwin Mombay <erwinm@google.com>
2021-11-09 13:37:28 -0800

    fix tests

--- test/unit/test-style-installer.js
+++ test/unit/test-style-installer.js
@@ -241,7 +241,7 @@ describes.sandboxed('Styles', {}, () => {
           head.appendChild(serverEl);
           const promise = installStylesAsPromise('other{}', false, 'amp-ext1');
           return promise.then((styleEl) => {
-            expect(head.__AMP_CSS_SM['amp-ext1']).to.not.exist;
+            expect(head.__AMP_CSS_SM['amp-runtime']).to.not.exist;
             expect(styleEl).to.equal(serverEl);
             expect(styleEl.textContent).to.equal('other{}');
             expect(
@@ -261,7 +261,8 @@ describes.sandboxed('Styles', {}, () => {
           head.appendChild(serverEl);
           const promise = installStylesAsPromise('other{}', false, 'amp-story');
           return promise.then((styleEl) => {
-            expect(head.__AMP_CSS_SM['amp-story']).to.not.exist;
+            expect(head.__AMP_CSS_SM['amp-runtime']).to.not.exist;
+            expect(head.__AMP_CSS_SM['amp-extension=amp-story']).to.equal(serverEl);
             expect(styleEl).to.equal(serverEl);
             expect(
               head.querySelectorAll('link[amp-extension=amp-story]')
