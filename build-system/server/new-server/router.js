459a104cbd74a0f1c3e2daba1f47644fa2c9ce7e
Alan Orozco <alan@orozco.xyz>
2021-11-09 15:52:34 -0800

    🏗 Serve Markdown files

--- build-system/server/new-server/router.js
+++ build-system/server/new-server/router.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
+const {
+  // @ts-ignore
+  renderMarkdown,
+  // @ts-ignore
+  renderMarkdownSnippet,
+} = require('./transforms/dist/markdown');
 // @ts-ignore
 const {transform} = require('./transforms/dist/transform');
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {string} content
+ */
+function serveHtml(req, res, content) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  if (req.query.__amp_source_origin) {
+    res.setHeader('Access-Control-Allow-Origin', req.query.__amp_source_origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  }
+  res.end(content);
+}
 
 router.get('/examples/*.html', async (req, res) => {
   let transformedHTML;
@@ -13,12 +33,19 @@ router.get('/examples/*.html', async (req, res) => {
         `pipeline.\n${e.message}`
     );
   }
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  if (req.query.__amp_source_origin) {
-    res.setHeader('Access-Control-Allow-Origin', req.query.__amp_source_origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
-  res.end(transformedHTML);
+  serveHtml(req, res, transformedHTML);
+});
+
+router.get('/*.md', async (req, res) => {
+  const filename = req.path;
+  const html = await renderMarkdown(filename, process.cwd());
+  serveHtml(req, res, html);
+});
+
+router.get('/*.md.html', async (req, res) => {
+  const filename = req.path.replace(/.html$/, '');
+  const html = await renderMarkdownSnippet(filename, process.cwd());
+  serveHtml(req, res, html);
 });
 
 module.exports = router;
---
+++ build-system/server/new-server/transforms/markdown.ts
@@ -0,0 +1,89 @@
+// @ts-ignore
+import dedent from 'dedent';
+import {readFile} from 'fs/promises';
+import {marked, Lexer, Parser} from 'marked';
+import {basename} from 'path';
+
+async function lexFile(filename: string) {
+  const markdown = await readFile(filename, 'utf8');
+  return Lexer.lex(markdown);
+}
+
+export function getHtmlSnippetFromMarkdown(
+  tokens: marked.Token[]
+): marked.Tokens.Code | null {
+  for (const token of tokens) {
+    if (
+      token.type === 'code' &&
+      token.lang === 'html' &&
+      token.text.includes('<html>')
+    ) {
+      return token;
+    }
+  }
+  return null;
+}
+
+export async function renderMarkdown(filename: string, cwd = '.') {
+  const tokens = await lexFile(`${cwd}/${filename}`);
+  const snippet = getHtmlSnippetFromMarkdown(tokens);
+  if (snippet) {
+    const link = `[**▶️ Run this snippet**](${filename}.html)`;
+    const index = tokens.indexOf(snippet);
+    tokens.splice(index, 0, ...Lexer.lex(link));
+  }
+  const body = Parser.parse(tokens);
+  return wrapTextBody(body, basename(filename));
+}
+
+export async function renderMarkdownSnippet(filename: string, cwd = '.') {
+  const tokens = await lexFile(`${cwd}/${filename}`);
+  const snippet = getHtmlSnippetFromMarkdown(tokens);
+  if (snippet) {
+    return snippet.text;
+  }
+  const body = dedent(/* HTML */ `
+    <p>
+      To add a runnable snippet, make sure that
+      <code><a href="${filename}">${filename}</a></code> contains:
+    </p>
+    <pre><code>${dedent(`
+      &lt;html&gt;
+        ...
+      &lt;/html&gt;
+    `)}</code></pre>
+  `);
+  return wrapTextBody(body, basename(filename));
+}
+
+export function wrapTextBody(body: string, title: string): string {
+  return dedent(/* HTML */ `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${title}</title>
+        <style>
+          body {
+            font-family: sans-serif;
+            line-height: 1.4;
+            margin: 0;
+            padding: 10px 20px;
+            font-size: 16px;
+          }
+          pre {
+            color: #444;
+            overflow: auto;
+            margin: 20px -20px;
+            padding: 0 20px;
+          }
+          a {
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body>
+        ${body}
+      </body>
+    </html>
+  `);
+}
