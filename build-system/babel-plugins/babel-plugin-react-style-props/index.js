4b32cefe4bd761d27754db5c9bac76cad6a1b199
Alan Orozco <alanorozco@users.noreply.github.com>
2021-11-11 07:44:47 -0800

    🏗♻️ Annotate use of Preact-style prop names (#36877)
    Partial for #35678
    
    For safety of prop name replacement, and consistent with either Preact or React style, replace e.g. `propName('class')` when destructuring props.
    
    - Added passthrough function `propName()`.
    - Updated Babel plugin to replace function uses.
    - Updated eslint rule so that it generates `[propName('class')]: className`, and verifies uses of `propName()`.

--- build-system/babel-plugins/babel-plugin-react-style-props/index.js
+++ build-system/babel-plugins/babel-plugin-react-style-props/index.js
@@ -3,17 +3,40 @@
  * Transforms Preact-style props ("class") into React-style ("className")
  */
 
+const propNameFn = 'propName';
+
 module.exports = function (babel) {
   const {types: t} = babel;
+
+  /**
+   * @param {string} name
+   * @return {?string}
+   */
+  function getReactStyle(name) {
+    // TODO(wg-bento): This mapping is incomplete.
+    if (name === 'class') {
+      return 'className';
+    }
+    return name;
+  }
 
   return {
     name: 'react-style-props',
     visitor: {
       JSXAttribute(path) {
-        // TODO(wg-bento): This mapping is incomplete.
-        if (t.isJSXIdentifier(path.node.name, {name: 'class'})) {
-          path.node.name.name = 'className';
+        const reactStyle = getReactStyle(path.node.name.name);
+        path.node.name.name = reactStyle;
+      },
+      CallExpression(path) {
+        if (!t.isIdentifier(path.node.callee, {name: propNameFn})) {
+          return;
         }
+        const arg = path.get('arguments.0');
+        if (!arg.isStringLiteral()) {
+          throw arg.buildCodeFrameError('Should be string literal');
+        }
+        const reactStyle = getReactStyle(arg.node.value);
+        path.replaceWith(t.stringLiteral(reactStyle));
       },
     },
   };
