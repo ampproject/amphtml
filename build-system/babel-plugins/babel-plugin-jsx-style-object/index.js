c065362be7e01652521ed1b37e38a32b7cfe49cf
Alan Orozco <alan@orozco.xyz>
2021-11-10 18:30:48 -0800

    full non-dim

--- build-system/babel-plugins/babel-plugin-jsx-style-object/index.js
+++ build-system/babel-plugins/babel-plugin-jsx-style-object/index.js
@@ -12,33 +12,54 @@ const baseModule = 'core/dom/jsx';
 const helperModule = 'core/dom/jsx-style-property-string';
 const helperFn = 'jsxStylePropertyString';
 
+// All values from here, converted to dash-case:
+// https://github.com/facebook/react/blob/a7c5726/packages/react-dom/src/shared/CSSProperty.js
 const nonDimensional = new Set([
   'animation-iteration-count',
+  'aspect-ratio',
+  'border-image-outset',
+  'border-image-slice',
+  'border-image-width',
   'box-flex',
   'box-flex-group',
   'box-ordinal-group',
   'column-count',
-  'fill-opacity',
+  'columns',
   'flex',
   'flex-grow',
   'flex-positive',
   'flex-shrink',
   'flex-negative',
   'flex-order',
+  'grid-area',
+  'grid-row',
+  'grid-row-end',
+  'grid-row-span',
+  'grid-row-start',
+  'grid-column',
+  'grid-column-end',
+  'grid-column-span',
+  'grid-column-start',
   'font-weight',
   'line-clamp',
   'line-height',
   'opacity',
   'order',
   'orphans',
-  'stop-opacity',
-  'stroke-dashoffset',
-  'stroke-opacity',
-  'stroke-width',
   'tab-size',
   'widows',
   'z-index',
   'zoom',
+
+  // SVG-related properties
+  'fill-opacity',
+  'flood-opacity',
+  'stop-opacity',
+  'stroke-dasharray',
+  'stroke-dashoffset',
+  'stroke-miterlimit',
+  'stroke-opacity',
+  'stroke-width',
 ]);
 
 module.exports = function (babel) {
