be3d84ddc343f92945ebd89f4c00e9f9b88f36f2
processprocess <philip.hunter.bell@gmail.com>
2021-11-18 15:17:12 -0500

    Non-working test

--- extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-attachment.js
+++ extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-attachment.js
@@ -1,4 +1,5 @@
 import '../amp-story-shopping';
+import '../../../amp-story/1.0/amp-story-page-attachment';
 
 describes.realWin(
   'amp-story-shopping-attachment-v0.1',
@@ -24,12 +25,23 @@ describes.realWin(
       element = win.document.createElement('amp-story-shopping-attachment');
       pageEl.appendChild(element);
       win.document.body.appendChild(pageEl);
-
       shoppingAttachment = await element.getImpl();
     }
 
     it('should build shopping attachment component', () => {
       expect(() => shoppingAttachment.buildCallback()).to.not.throw();
+    });
+
+    it('should call open on child attachment impl', async () => {
+      const attachmentChildEl = element.querySelector(
+        'amp-story-page-attachment'
+      );
+      const attachmentChildImpl = await attachmentChildEl.getImpl();
+      await attachmentChildImpl.buildCallback();
+      shoppingAttachment.open();
+      expect(attachmentChildEl).to.have.class(
+        'i-amphtml-story-draggable-drawer-open'
+      );
     });
   }
 );
