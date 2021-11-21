360c0c585d035b47c7f205bdfc3eddddce935d73
Alan Orozco <alan@orozco.xyz>
2021-11-20 18:26:36 -0800

    🚀 amp-story: optimize SVG size

--- extensions/amp-story/1.0/amp-story-page-attachment.js
+++ extensions/amp-story/1.0/amp-story-page-attachment.js
@@ -217,7 +217,7 @@ export class AmpStoryPageAttachment extends DraggableDrawer {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
         >
-          <path d="M38 38H10V10h14V6H10c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4V24h-4v14zM28 6v4h7.17L15.51 29.66l2.83 2.83L38 12.83V20h4V6H28z"></path>
+          <path d="M38 38H10V10h14V6H10a4 4 0 0 0-4 4v28a4 4 0 0 0 4 4h28a4 4 0 0 0 4-4V24h-4v14zM28 6v4h7.2L15.5 29.7l2.8 2.8L38 12.8V20h4V6H28z" />
         </svg>
       </a>
     );
