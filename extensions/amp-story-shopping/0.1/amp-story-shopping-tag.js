6f43e35358e920e8857dca3e742e967ccb6ea2ba
processprocess <philip.hunter.bell@gmail.com>
2021-11-22 12:19:45 -0500

    Shopping tag.

--- extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
+++ extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
@@ -1,4 +1,7 @@
+import * as Preact from '#core/dom/jsx';
 import {Layout_Enum} from '#core/dom/layout';
+import {createShadowRootWithStyle} from '../../amp-story/1.0/utils';
+import {CSS as shoppingTagCSS} from '../../../build/amp-story-shopping-tag-0.1.css';
 
 import {Services} from '#service';
 
@@ -8,6 +11,34 @@ import {
   ShoppingConfigDataDef,
   StateProperty,
 } from '../../amp-story/1.0/amp-story-store-service';
+
+/** @const {Array<Object>} fontFaces with urls from https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&amp;display=swap */
+const fontsToLoad = [
+  {
+    family: 'Poppins',
+    weight: '400',
+    style: 'normal',
+    src: "url(https://fonts.gstatic.com/s/poppins/v9/pxiEyp8kv8JHgFVrJJfecnFHGPc.woff2) format('woff2')",
+  },
+  {
+    family: 'Poppins',
+    weight: '700',
+    style: 'normal',
+    src: "url(https://fonts.gstatic.com/s/poppins/v9/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2) format('woff2')",
+  },
+];
+
+const ShoppingTagTemplate = ({tagData}) => (
+  <div class="amp-story-shopping-tag-inner">
+    <span class="amp-story-shopping-tag-dot"></span>
+    <span class="amp-story-shopping-tag-pill">
+      <span class="amp-story-shopping-tag-pill-image"></span>
+      <span class="amp-story-shopping-tag-pill-text">
+        {tagData['product-price']}
+      </span>
+    </span>
+  </div>
+);
 
 /**
  * @typedef {{
@@ -26,11 +57,10 @@ export class AmpStoryShoppingTag extends AMP.BaseElement {
 
   /** @override */
   buildCallback() {
+    this.loadFonts_();
+    this.element.setAttribute('role', 'button');
     return Services.storyStoreServiceForOrNull(this.win).then(
-      (storeService) => {
-        devAssert(storeService, 'Could not retrieve AmpStoryStoreService');
-        this.storeService_ = storeService;
-      }
+      (storeService) => (this.storeService_ = storeService)
     );
   }
 
@@ -58,7 +88,23 @@ export class AmpStoryShoppingTag extends AMP.BaseElement {
       return;
     }
     this.mutateElement(() => {
+      createShadowRootWithStyle(
+        this.element,
+        <ShoppingTagTemplate tagData={tagData} />,
+        shoppingTagCSS
+      );
       this.element.textContent = tagData['product-title'];
     });
+  }
+
+  /** @private */
+  loadFonts_() {
+    if (this.win.document.fonts && FontFace) {
+      fontsToLoad.forEach(({family, src, weight, style}) =>
+        new FontFace(family, src, {weight, style})
+          .load()
+          .then((font) => this.win.document.fonts.add(font))
+      );
+    }
   }
 }
