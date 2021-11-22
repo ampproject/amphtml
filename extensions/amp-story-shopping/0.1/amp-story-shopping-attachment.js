eb3c18b01b721b07fa03c3ceab8c83d471dbb633
processprocess <philip.hunter.bell@gmail.com>
2021-11-22 09:45:45 -0500

    Update reference to impl.

--- extensions/amp-story-shopping/0.1/amp-story-shopping-attachment.js
+++ extensions/amp-story-shopping/0.1/amp-story-shopping-attachment.js
@@ -1,39 +1,35 @@
 import * as Preact from '#core/dom/jsx';
 import {Layout_Enum} from '#core/dom/layout';
-import {once} from '#core/types/function';
 
 export class AmpStoryShoppingAttachment extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
     super(element);
 
-    /** @private @return {extensions/amp-story/1.0/amp-story-page-attachment.js.AmpStoryPageAttachment} */
-    this.attachmentEl_ = null;
-
     /**
      * Caches a reference to the attachmnet's impl.
      * @private @return {extensions/amp-story/1.0/amp-story-page-attachment.js.AmpStoryPageAttachment}
      * */
-    this.getAttachmentImpl_ = once(() =>
-      customElements
-        .whenDefined('amp-story-page-attachment')
-        .then(() => this.attachmentEl_.getImpl())
-    );
+    this.attachmentImpl_ = null;
   }
 
   /** @override */
   buildCallback() {
-    this.attachmentEl_ = (
+    const attachmentEl = (
       <amp-story-page-attachment layout="nodisplay"></amp-story-page-attachment>
     );
-    this.element.appendChild(this.attachmentEl_);
+    this.element.appendChild(attachmentEl);
+    return customElements
+      .whenDefined('amp-story-page-attachment')
+      .then(() => attachmentEl.getImpl())
+      .then((impl) => (this.attachmentImpl_ = impl));
   }
 
   /**
    * @param {boolean=} shouldAnimate
    */
   open(shouldAnimate = true) {
-    this.getAttachmentImpl_().then((impl) => impl.open(shouldAnimate));
+    this.attachmentImpl_.open(shouldAnimate);
   }
 
   /** @override */
