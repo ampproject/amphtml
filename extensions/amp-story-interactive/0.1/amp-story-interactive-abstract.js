4fa6498f0d8b13d492427e1bd32573cc64d62fad
Alan Orozco <alan@orozco.xyz>
2021-11-24 10:32:28 -0800

    🚀♻️ Story performance: Interactive host styles into extension

--- extensions/amp-story-interactive/0.1/amp-story-interactive-abstract.js
+++ extensions/amp-story-interactive/0.1/amp-story-interactive-abstract.js
@@ -8,7 +8,8 @@ import {
   StateProperty,
 } from '../../amp-story/1.0/amp-story-store-service';
 import {AnalyticsVariable} from '../../amp-story/1.0/variable-service';
-import {CSS} from '../../../build/amp-story-interactive-0.1.css';
+import {CSS as hostCss} from '../../../build/amp-story-interactive-host-0.1.css';
+import {CSS as shadowCss} from '../../../build/amp-story-interactive-shadow-0.1.css';
 import {Services} from '#service';
 import {
   addParamsToUrl,
@@ -32,6 +33,7 @@ import {dict} from '#core/types/object';
 import {emojiConfetti} from './interactive-confetti';
 import {toArray} from '#core/types/array';
 import {isExperimentOn} from '#experiments/';
+import {escapeCssSelectorIdent} from '#core/dom/css-selectors';
 
 /** @const {string} */
 const TAG = 'amp-story-interactive';
@@ -101,6 +103,44 @@ const fontsToLoad = [
     src: "url(https://fonts.gstatic.com/s/poppins/v9/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2) format('woff2')",
   },
 ];
+
+/**
+ * Installs the host CSS.
+ *
+ * We define this function instead of using runtime style install solutions for
+ * different reasons:
+ *
+ * 1. We don't do it as part of `registerElement` since components with
+ *    different names extend from the same abstract AmpStoryInteractive. As such,
+ *    we'd unnnecessarily install the same styles for every inheriting component
+ *    present on the page.
+ *
+ * 2. We don't use `installStylesForDoc()` since it's unnecessarily large. We
+ *    don't want to bundle it as part of this extension.
+ * @param {string} id
+ * @param {!Element|!ShadowRoot} cssRoot
+ * @param {!Element} sibling
+ * @param {string} cssText
+ */
+function installHostCssOnce(id, cssRoot, sibling, cssText) {
+  if (cssRoot.querySelector(`#${escapeCssSelectorIdent(id)}`)) {
+    return;
+  }
+  const docOrShadowRoot = cssRoot.ownerDocument || cssRoot;
+  const style = docOrShadowRoot.createElement('style');
+  style.id = id;
+  style./*OK*/ textContent = cssText;
+  if (docOrShadowRoot !== cssRoot) {
+    // If in head, add before `<style amp-custom>` if present.
+    // If in a shadow root, preserve the provided insertBefore sibling.
+    sibling = cssRoot.querySelector('style[amp-custom]');
+  }
+  if (sibling) {
+    sibling.parentNode.insertBefore(style, sibling);
+  } else {
+    cssRoot.appendChild(style);
+  }
+}
 
 /**
  * Interactive abstract class with shared functionality for interactive components.
@@ -185,6 +225,13 @@ export class AmpStoryInteractive extends AMP.BaseElement {
 
     /** @protected {?../../amp-story/1.0/variable-service.AmpStoryVariableService} */
     this.variableService_ = null;
+
+    installHostCssOnce(
+      'i-amphtml-style-story-interactive',
+      this.getAmpDoc().getHeadNode(),
+      this.element,
+      hostCss
+    );
   }
 
   /**
@@ -278,7 +325,7 @@ export class AmpStoryInteractive extends AMP.BaseElement {
       createShadowRootWithStyle(
         this.element,
         dev().assertElement(this.rootEl_),
-        CSS + concreteCSS
+        shadowCss + concreteCSS
       );
       return Promise.resolve();
     });
