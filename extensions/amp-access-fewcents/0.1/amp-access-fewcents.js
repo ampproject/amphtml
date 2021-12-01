45ed69847cc792f68abce3c0416ae819f7a5ccf7
Mayank Sahu <mayank.sahu@joshsoftware.com>
2021-12-01 15:18:32 +0530

    [amp-access-fewcents] Registering the extension and extending amp-access

--- extensions/amp-access-fewcents/0.1/amp-access-fewcents.js
+++ extensions/amp-access-fewcents/0.1/amp-access-fewcents.js
@@ -1,35 +1,43 @@
-import {CSS} from '../../../build/amp-access-fewcents-0.1.css';
-import {Layout_Enum, applyFillContent} from '#core/dom/layout';
+/**
+ * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Services} from '#service';
+
+import {AmpAccessFewcents} from './fewcents-impl';
 
 const TAG = 'amp-access-fewcents';
 
-export class AmpAccessFewcents extends AMP.BaseElement {
-  /** @param {!AmpElement} element */
-  constructor(element) {
-    // DO NOT SUBMIT: This is example code only.
-    super(element);
-
-    /** @private {string} */
-    this.myText_ = 'hello world';
-
-    /** @private {?Element} */
-    this.container_ = null;
-  }
-
-  /** @override */
-  buildCallback() {
-    this.container_ = this.element.ownerDocument.createElement('div');
-    this.container_.textContent = this.myText_;
-    this.element.appendChild(this.container_);
-    applyFillContent(this.container_, /* replacedContent */ true);
-  }
-
-  /** @override */
-  isLayoutSupported(layout) {
-    return layout == Layout_Enum.RESPONSIVE;
-  }
-}
-
-AMP.extension(TAG, '0.1', (AMP) => {
-  AMP.registerElement(TAG, AmpAccessFewcents, CSS);
+AMP.extension(TAG, '0.1', function (AMP) {
+  AMP.registerServiceForDoc(
+    'fewcents',
+    /**
+     * @param {!../../../src/service/ampdoc-impl.AmpDoc} ampdoc
+     * @return {*} return type
+     */
+    function (ampdoc) {
+      const element = ampdoc.getHeadNode();
+      return Services.accessServiceForDoc(element).then((accessService) => {
+        const source = accessService.getVendorSource('fewcents');
+        const vendor = new AmpAccessFewcents(accessService, source);
+        const adapter = /** @type {
+             !../../amp-access/0.1/amp-access-vendor.AccessVendorAdapter
+           } */ (source.getAdapter());
+        adapter.registerVendor(vendor);
+        return vendor;
+      });
+    }
+  );
 });
