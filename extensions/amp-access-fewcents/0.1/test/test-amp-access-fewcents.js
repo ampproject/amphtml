493b7df3f8fb5021ad6fbc3bc1b6a1e3e4c5e267
Mayank Sahu <mayank.sahu@joshsoftware.com>
2021-12-02 16:45:24 +0530

    [amp-access-fewcents] Removed the default testcases

--- extensions/amp-access-fewcents/0.1/test/test-amp-access-fewcents.js
+++ extensions/amp-access-fewcents/0.1/test/test-amp-access-fewcents.js
@@ -1,5 +1,4 @@
 import '../amp-access-fewcents';
-import {htmlFor} from '#core/dom/static-template';
 
 describes.realWin(
   'amp-access-fewcents-v0.1',
@@ -9,25 +8,5 @@ describes.realWin(
       extensions: ['amp-access-fewcents:0.1'],
     },
   },
-  (env) => {
-    let win;
-    let doc;
-    let html;
-
-    beforeEach(() => {
-      win = env.win;
-      doc = win.document;
-      html = htmlFor(doc);
-    });
-
-    it('should contain "hello world" when built', async () => {
-      const element = html`
-        <amp-access-fewcents width="100" height="10" layout="responsive">
-        </amp-access-fewcents>
-      `;
-      doc.body.appendChild(element);
-      await element.whenBuilt();
-      expect(element.querySelector('div').textContent).to.equal('hello world');
-    });
-  }
+  () => {}
 );
