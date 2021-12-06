188c131812fa708aa2f9749eaad3d253cee3fc8c
Erwin Mombay <erwinm@google.com>
2021-12-05 22:53:02 -0800

    apply recs

--- build-system/server/new-server/router.js
+++ build-system/server/new-server/router.js
@@ -2,18 +2,12 @@ const router = require('express').Router();
 // @ts-ignore
 const {transform} = require('./transforms/dist/transform');
 
-// There are some pages that we do not want transformed explicitly.
-const exemptPages = [
-  // We do not transform amp-story-unsupported-browser-layer as it tests
-  // a page that explicitly wants to load the js version and not the mjs as it
-  // executes a code block that is guarded by `isEsm()` which is dead code
-  // eliminated in the mjs version.
-  '/examples/visual-tests/amp-story/amp-story-unsupported-browser-layer.html',
-];
-
 router.get('/examples/*.html', async (req, res, next) => {
   let transformedHTML;
-  if (exemptPages.includes(req.path)) {
+  // nomodule.html will force an opt-out of the transformation. This can
+  // be useful if we want to explicitly test the nomodule version of the
+  // code.
+  if (/\.nomodule\.html$/.test(req.path)) {
     return next();
   }
   const filePath = `${process.cwd()}${req.path}`;
