e8dc3337b2aba42d9dc700a69ec64597689baee1
Erwin Mombay <erwinm@google.com>
2021-12-06 10:29:42 -0800

    lint fix

--- build-system/server/new-server/router.js
+++ build-system/server/new-server/router.js
@@ -8,7 +8,8 @@ router.get('/examples/*.html', async (req, res, next) => {
   // ?transform=0 or ?transform=false will force an opt-out of the
   // transformation. This can be useful if we want to explicitly test the
   // nomodule version of the code.
-  const skipTransform = 'transform' in req.query &&
+  const skipTransform =
+    'transform' in req.query &&
     (req.query.transform === '0' || req.query.transform === 'false');
   if (skipTransform) {
     return next();
