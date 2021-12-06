d986e96d93375d368eafa44e59f343b1e45d48db
Erwin Mombay <erwinm@google.com>
2021-12-06 13:15:38 -0800

    Revert "add --esm and --minified options for visual-diff task (#37115)"
    This reverts commit 8814a3fc3a3f384febe00f10d79134243695df6b.

--- build-system/server/app.js
+++ build-system/server/app.js
@@ -73,7 +73,10 @@ app.use(bodyParser.text());
 // Middleware is executed in order, so this must be at the top.
 // TODO(#24333): Migrate all server URL handlers to new-server/router and
 // deprecate app.js.
-app.use(require('./new-server/router'));
+// TODO(erwinmombay, #32865): Make visual diff tests use the new server
+if (!argv._.includes('visual-diff')) {
+  app.use(require('./new-server/router'));
+}
 
 app.use(require('./routes/a4a-envelopes'));
 app.use('/amp4test', require('./amp4test').app);
