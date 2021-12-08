e4a757a838bda598f75195045efedde72886352c
Dhruv Manek <dmanek@gmail.com>
2021-12-08 12:00:16 -0800

    add template support in base-element.js

--- extensions/amp-date-display/1.0/base-element.js
+++ extensions/amp-date-display/1.0/base-element.js
@@ -1,11 +1,33 @@
 import {parseDateAttrs as parseDateAttrsBase} from '#core/dom/parse-date-attributes';
+import {dict} from '#core/types/object';
 
 import {PreactBaseElement} from '#preact/base-element';
 import {createParseAttrsWithPrefix} from '#preact/parse-props';
 
 import {BentoDateDisplay} from './component';
 
-export class BaseElement extends PreactBaseElement {}
+export class BaseElement extends PreactBaseElement {
+  /** @override */
+  checkPropsPostMutations() {
+    const template = this.element.hasAttribute('template')
+      ? this.element.ownerDocument.getElementById(
+          this.element.getAttribute('template')
+        )
+      : this.element.querySelector('template');
+    this.mutateProps(
+      dict({
+        'render': (data) => {
+          let html = template.content.firstElementChild./*REVIEW*/ outerHTML;
+          for (const [key, value] of Object.entries(data)) {
+            html = html.replaceAll('${' + key + '}', value);
+          }
+          console.log(template.content, data, html);
+          return dict({'__html': html});
+        },
+      })
+    );
+  }
+}
 
 /** @override */
 BaseElement['Component'] = BentoDateDisplay;
