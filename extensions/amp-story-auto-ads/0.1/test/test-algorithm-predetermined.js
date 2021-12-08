0d3ba00e35a0b7932dc56576e80223bc0a1d4db2
Matias Szylkowski <mszylkowski@google.com>
2021-12-08 11:56:31 -0500

    Fixed more tests

--- extensions/amp-story-auto-ads/0.1/test/test-algorithm-predetermined.js
+++ extensions/amp-story-auto-ads/0.1/test/test-algorithm-predetermined.js
@@ -19,6 +19,7 @@ describes.realWin('PredeterminedPositionAlgorithm', {amp: true}, (env) => {
   beforeEach(() => {
     storeService = getStoreService(env.win);
     const storyElement = env.win.document.createElement('amp-story');
+    env.win.document.body.append(storyElement);
     const ampStory = new AmpStory(storyElement);
     env.sandbox.stub(ampStory, 'getAmpDoc').returns(env.ampdoc);
     pageManager = new StoryAdPageManager(ampStory, {} /* config */);
