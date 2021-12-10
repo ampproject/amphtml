be858557803131139bcfffa7cfabb65ee9324d49
Keshav Varadachari <keshavvi@google.com>
2021-12-10 14:19:15 -0500

    refactor amp/bento-fit-text

--- extensions/amp-fit-text/1.0/amp-fit-text.js
+++ extensions/amp-fit-text/1.0/amp-fit-text.js
@@ -1,12 +1,20 @@
-import {BaseElement} from './base-element';
 import {CSS} from '../../../build/amp-fit-text-1.0.css';
 import {isExperimentOn} from '#experiments';
 import {userAssert} from '#utils/log';
 
+import {
+  Component,
+  layoutSizeDefined,
+  props,
+  shadowCss,
+  usesShadowDom,
+} from './element';
+import {AmpPreactBaseElement} from '#preact/amp-base-element';
+
 /** @const {string} */
 const TAG = 'amp-fit-text';
 
-class AmpFitText extends BaseElement {
+class AmpFitText extends AmpPreactBaseElement {
   /** @override */
   isLayoutSupported(layout) {
     userAssert(
@@ -17,6 +25,21 @@ class AmpFitText extends BaseElement {
     return super.isLayoutSupported(layout);
   }
 }
+
+/** @override */
+AmpFitText['Component'] = Component;
+
+/** @override */
+AmpFitText['props'] = props;
+
+/** @override */
+AmpFitText['layoutSizeDefined'] = layoutSizeDefined;
+
+/** @override */
+AmpFitText['usesShadowDom'] = usesShadowDom;
+
+/** @override */
+AmpFitText['shadowCss'] = shadowCss;
 
 AMP.extension(TAG, '1.0', (AMP) => {
   AMP.registerElement(TAG, AmpFitText, CSS);
