8c2d5957cd85234f7ba39f80cd2bad2202990f31
Keshav Varadachari <keshavvi@google.com>
2021-12-10 14:19:15 -0500

    refactor amp/bento-instagram

--- extensions/amp-instagram/1.0/amp-instagram.js
+++ extensions/amp-instagram/1.0/amp-instagram.js
@@ -2,16 +2,25 @@ import {dict} from '#core/types/object';
 
 import {isExperimentOn} from '#experiments';
 
+import {AmpPreactBaseElement} from '#preact/amp-base-element';
+
 import {userAssert} from '#utils/log';
 
-import {BaseElement} from './base-element';
+import {
+  Component,
+  layoutSizeDefined,
+  loadable,
+  props,
+  unloadOnPause,
+  usesShadowDom,
+} from './element';
 
 import {CSS} from '../../../build/amp-instagram-1.0.css';
 
 /** @const {string} */
 const TAG = 'amp-instagram';
 
-class AmpInstagram extends BaseElement {
+class AmpInstagram extends AmpPreactBaseElement {
   /** @override */
   isLayoutSupported(layout) {
     userAssert(
@@ -29,6 +38,24 @@ class AmpInstagram extends BaseElement {
     });
   }
 }
+
+/** @override */
+AmpInstagram['Component'] = Component;
+
+/** @override */
+AmpInstagram['loadable'] = loadable;
+
+/** @override */
+AmpInstagram['unloadOnPause'] = unloadOnPause;
+
+/** @override */
+AmpInstagram['props'] = props;
+
+/** @override */
+AmpInstagram['usesShadowDom'] = usesShadowDom;
+
+/** @override */
+AmpInstagram['layoutSizeDefined'] = layoutSizeDefined;
 
 AMP.extension(TAG, '1.0', (AMP) => {
   AMP.registerElement(TAG, AmpInstagram, CSS);
---
+++ extensions/amp-instagram/1.0/element.js
@@ -0,0 +1,15 @@
+export {BentoInstagram as Component} from './component';
+
+export const loadable = true;
+
+export const unloadOnPause = true;
+
+export const props = {
+  'captioned': {attr: 'data-captioned'},
+  'shortcode': {attr: 'data-shortcode'},
+  'title': {attr: 'title'},
+};
+
+export const usesShadowDom = true;
+
+export const layoutSizeDefined = true;
