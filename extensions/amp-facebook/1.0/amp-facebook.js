5e6e3af0dd87248ac1df12b7ce5d1fa202bd3af2
Keshav Varadachari <keshavvi@google.com>
2021-12-10 14:19:15 -0500

    refactor amp/bento-facebook

--- extensions/amp-facebook/1.0/amp-facebook.js
+++ extensions/amp-facebook/1.0/amp-facebook.js
@@ -1,15 +1,21 @@
-import {
-  BaseElement,
-  CommentsBaseElement,
-  LikeBaseElement,
-  PageBaseElement,
-} from './base-element';
 import {createLoaderLogo} from '../0.1/facebook-loader';
 import {dashToUnderline} from '#core/types/string';
 import {dict} from '#core/types/object';
-import {getBootstrapBaseUrl, getBootstrapUrl} from '../../../src/3p-frame';
 import {isExperimentOn} from '#experiments';
 import {userAssert} from '#utils/log';
+
+import {
+  Component,
+  commentsStaticProps,
+  layoutSizeDefined,
+  likeStaticProps,
+  loadable,
+  pageStaticProps,
+  props,
+  usesShadowDom,
+} from './element';
+import {AmpPreactBaseElement} from '#preact/amp-base-element';
+import {getBootstrapBaseUrl, getBootstrapUrl} from '../../../src/3p-frame';
 
 /** @const {string} */
 const TAG = 'amp-facebook';
@@ -18,62 +24,77 @@ const LIKE_TAG = 'amp-facebook-like';
 const PAGE_TAG = 'amp-facebook-page';
 const TYPE = 'facebook';
 
-/**
- * Mixin to implement base amp functionality for all facebook components
- * @param {*} clazz1
- * @return {*} mixin
- */
-function AmpFacebookMixin(clazz1) {
-  return class extends clazz1 {
-    /** @override @nocollapse */
-    static createLoaderLogoCallback(element) {
-      return createLoaderLogo(element);
-    }
+class AmpFacebook extends AmpPreactBaseElement {
+  /** @override @nocollapse */
+  static createLoaderLogoCallback(element) {
+    return createLoaderLogo(element);
+  }
 
-    /** @override @nocollapse */
-    static getPreconnects(element) {
-      const ampdoc = element.getAmpDoc();
-      const {win} = ampdoc;
-      const locale = element.hasAttribute('data-locale')
-        ? element.getAttribute('data-locale')
-        : dashToUnderline(window.navigator.language);
-      return [
-        // Base URL for 3p bootstrap iframes
-        getBootstrapBaseUrl(win, ampdoc),
-        // Script URL for iframe
-        getBootstrapUrl(TYPE),
-        'https://facebook.com',
-        // This domain serves the actual tweets as JSONP.
-        'https://connect.facebook.net/' + locale + '/sdk.js',
-      ];
-    }
+  /** @override @nocollapse */
+  static getPreconnects(element) {
+    const ampdoc = element.getAmpDoc();
+    const {win} = ampdoc;
+    const locale = element.hasAttribute('data-locale')
+      ? element.getAttribute('data-locale')
+      : dashToUnderline(window.navigator.language);
+    return [
+      // Base URL for 3p bootstrap iframes
+      getBootstrapBaseUrl(win, ampdoc),
+      // Script URL for iframe
+      getBootstrapUrl(TYPE),
+      'https://facebook.com',
+      // This domain serves the actual tweets as JSONP.
+      'https://connect.facebook.net/' + locale + '/sdk.js',
+    ];
+  }
 
-    /** @override */
-    init() {
-      return dict({
-        'requestResize': (height) => this.attemptChangeHeight(height),
-      });
-    }
+  /** @override */
+  init() {
+    return dict({
+      'requestResize': (height) => this.attemptChangeHeight(height),
+    });
+  }
 
-    /** @override */
-    isLayoutSupported(layout) {
-      userAssert(
-        isExperimentOn(this.win, 'bento') ||
-          isExperimentOn(this.win, 'bento-facebook'),
-        'expected global "bento" or specific "bento-facebook" experiment to be enabled'
-      );
-      return super.isLayoutSupported(layout);
-    }
-  };
+  /** @override */
+  isLayoutSupported(layout) {
+    userAssert(
+      isExperimentOn(this.win, 'bento') ||
+        isExperimentOn(this.win, 'bento-facebook'),
+      'expected global "bento" or specific "bento-facebook" experiment to be enabled'
+    );
+    return super.isLayoutSupported(layout);
+  }
 }
 
-class AmpFacebook extends AmpFacebookMixin(BaseElement) {}
+/** @override */
+AmpFacebook['Component'] = Component;
 
-class AmpFacebookComments extends AmpFacebookMixin(CommentsBaseElement) {}
+/** @override */
+AmpFacebook['loadable'] = loadable;
 
-class AmpFacebookLike extends AmpFacebookMixin(LikeBaseElement) {}
+/** @override */
+AmpFacebook['props'] = props;
 
-class AmpFacebookPage extends AmpFacebookMixin(PageBaseElement) {}
+/** @override */
+AmpFacebook['layoutSizeDefined'] = layoutSizeDefined;
+
+/** @override */
+AmpFacebook['usesShadowDom'] = usesShadowDom;
+
+class AmpFacebookComments extends AmpFacebook {}
+
+/** @override */
+AmpFacebookComments['staticProps'] = commentsStaticProps;
+
+class AmpFacebookLike extends AmpFacebook {}
+
+/** @override */
+AmpFacebookLike['staticProps'] = likeStaticProps;
+
+class AmpFacebookPage extends AmpFacebook {}
+
+/** @override */
+AmpFacebookPage['staticProps'] = pageStaticProps;
 
 AMP.extension(TAG, '1.0', (AMP) => {
   AMP.registerElement(TAG, AmpFacebook);
--- extensions/amp-facebook/1.0/base-element.js
+++ extensions/amp-facebook/1.0/base-element.js
@@ -1,7 +1,14 @@
-import {userAssert} from '#core/assert';
-import {dashToUnderline} from '#core/types/string';
 import {PreactBaseElement} from '#preact/base-element';
-import {BentoFacebook} from './component';
+import {
+  Component,
+  commentsStaticProps,
+  layoutSizeDefined,
+  likeStaticProps,
+  loadable,
+  pageStaticProps,
+  props,
+  usesShadowDom,
+} from './element';
 
 /** @const {string} */
 export const TAG = 'bento-facebook';
@@ -12,82 +19,28 @@ export const PAGE_TAG = 'bento-facebook-page';
 export class BaseElement extends PreactBaseElement {}
 
 /** @override */
-BaseElement['Component'] = BentoFacebook;
+BaseElement['Component'] = Component;
 
 /** @override */
-BaseElement['loadable'] = true;
+BaseElement['loadable'] = loadable;
 
 /** @override */
-BaseElement['props'] = {
-  // common attributes
-  'title': {attr: 'title'}, // Needed for Preact component
-  'href': {attr: 'data-href'},
-  'locale': {
-    attr: 'data-locale',
-    default: dashToUnderline(window.navigator.language),
-  },
-  // TODO(wg-components): Current behavior defaults to loading="auto".
-  // Refactor to make loading="lazy" as the default.
-  'loading': {attr: 'data-loading'},
-  // amp-facebook
-  'allowFullScreen': {attr: 'data-allowfullscreen'},
-  'embedAs': {attrs: ['data-embed-as'], parseAttrs: parseEmbed},
-  'includeCommentParent': {
-    attr: 'data-include-comment-parent',
-    type: 'boolean',
-    default: false,
-  },
-  'showText': {attr: 'data-show-text'},
-  // -comments
-  'numPosts': {attr: 'data-numposts'},
-  'orderBy': {attr: 'data-order-by'},
-  // -comments & -like
-  'colorscheme': {attr: 'data-colorscheme'},
-  // -like
-  'action': {attr: 'data-action'},
-  'kdSite': {attr: 'data-kd_site'},
-  'layout': {attr: 'data-layout'},
-  'refLabel': {attr: 'data-ref'},
-  'share': {attr: 'data-share'},
-  'size': {attr: 'data-size'},
-  // -page
-  'hideCover': {attr: 'data-hide-cover'},
-  'hideCta': {attr: 'data-hide-cta'},
-  'showFacepile': {attr: 'data-show-facepile'},
-  'smallHeader': {attr: 'data-small-header'},
-  'tabs': {attr: 'data-tabs'},
-};
-
-/**
- * Checks for valid data-embed-as attribute when given.
- * @param {!Element} element
- * @return {string}
- */
-function parseEmbed(element) {
-  const embedAs = element.getAttribute('data-embed-as');
-  userAssert(
-    !embedAs ||
-      ['post', 'video', 'comment', 'comments', 'like', 'page'].indexOf(
-        embedAs
-      ) !== -1,
-    'Attribute data-embed-as for <amp-facebook> value is wrong, should be' +
-      ' "post", "video", "comment", "comments", "like", or "page", but was: %s',
-    embedAs
-  );
-  return embedAs;
-}
+BaseElement['props'] = props;
 
 /** @override */
-BaseElement['layoutSizeDefined'] = true;
+BaseElement['layoutSizeDefined'] = layoutSizeDefined;
 
 /** @override */
-BaseElement['usesShadowDom'] = true;
+BaseElement['usesShadowDom'] = usesShadowDom;
 
 export class CommentsBaseElement extends BaseElement {}
-CommentsBaseElement['staticProps'] = {'embedAs': 'comments'};
+/** @override */
+CommentsBaseElement['staticProps'] = commentsStaticProps;
 
 export class LikeBaseElement extends BaseElement {}
-LikeBaseElement['staticProps'] = {'embedAs': 'like'};
+/** @override */
+LikeBaseElement['staticProps'] = likeStaticProps;
 
 export class PageBaseElement extends BaseElement {}
-PageBaseElement['staticProps'] = {'embedAs': 'page'};
+/** @override */
+PageBaseElement['staticProps'] = pageStaticProps;
