a75b0c993a1f9cfc6226944b8695ca4f128f96a4
Jake Fried <samouri@users.noreply.github.com>
2021-12-13 16:19:31 -0500

    Update src/core/dom/dom-globals.d.ts
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>

--- src/core/dom/dom-globals.d.ts
+++ src/core/dom/dom-globals.d.ts
@@ -3,11 +3,11 @@ export {};
 declare global {
   interface HTMLElement {
     // Used by modal.js to keep track of the saved tab on an element.
-    '__AMP_MODAL_SAVED_TAB_INDEX'?: string | null;
+    __AMP_MODAL_SAVED_TAB_INDEX?: string | null;
 
     // Used by extensions/amp-form/0.1 and src/core/form.js.
-    // TODO: move to extensions/amp-form, as it is the only consumer.
-    '__AMP_FORM': AmpForm;
+    // TODO(wg-performance): move to extensions/amp-form, as it is the only consumer.
+    __AMP_FORM: AmpForm;
   }
 
   interface AmpForm {}
