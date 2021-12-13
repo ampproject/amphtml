d38ba551e86d10cfc614389298b7056b666ed802
Jake Fried <samouri@users.noreply.github.com>
2021-12-13 10:51:21 -0500

    address kbax nits.

--- extensions/amp-carousel/0.2/amp-carousel.js
+++ extensions/amp-carousel/0.2/amp-carousel.js
@@ -214,15 +214,17 @@ class AmpCarousel extends AMP.BaseElement {
    * @param {*} onInteraction
    */
   setupButtonInteraction(button, onInteraction) {
-    button.onkeydown = (event) => {
-      if (event.key == Keys_Enum.ENTER || event.key == Keys_Enum.SPACE) {
-        if (!event.defaultPrevented) {
-          event.preventDefault();
-          onInteraction();
-        }
+    button.addEventListener('click', onInteraction);
+    button.addEventListener('keydown', (event) => {
+      if (event.defaultPrevented) {
+        return;
       }
-    };
-    button.onclick = onInteraction;
+
+      if (event.key == Keys_Enum.ENTER || event.key == Keys_Enum.SPACE) {
+        event.preventDefault();
+        onInteraction();
+      }
+    });
   }
 
   /** @override */
