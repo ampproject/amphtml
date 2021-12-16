70bcdd95a8f8b7138c1ec296a4fb43b07c302723
Jake Fried <samouri@users.noreply.github.com>
2021-12-16 17:00:03 -0500

    turn eslint off for a small subsection

--- src/preact/index.js
+++ src/preact/index.js
@@ -8,14 +8,18 @@
  */
 
 // Importing directly from preact/dom is a hack to allow us to mimic the structure of react.
+
+/* eslint-disable */
 // @ts-ignore
-import * as preactDOM from 'preact/dom'
+import * as preactDOM from 'preact/dom';
 
 /** @type {typeof import('preact').hydrate} */
 export const hydrate = preactDOM.hydrate;
 
 /** @type {typeof import('preact').hydrate} */
 export const render = preactDOM.render;
+
+/* eslint-enable */
 
 export {createElement, cloneElement, createRef, createContext} from 'preact';
 export {
