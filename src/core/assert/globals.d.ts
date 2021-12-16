84662bc9a454d6fff2c9ce413a9d6650e715869e
Ryan Cebulko <ryan@cebulko.com>
2021-12-16 12:39:54 -0500

    Merge assert globals.d.ts

--- src/core/assert/globals.d.ts
+++ src/core/assert/globals.d.ts
@@ -8,4 +8,9 @@ declare global {
     // Global error reporting handler; only present in AMP pages.
     __AMP_REPORT_ERROR?: (this: Window, err: Error, opt_el?: Element) => void;
   }
+
+  interface Error {
+    // Allows error handlers to display errors with console-interactive values.
+    messageArray?: any[];
+  }
 }
