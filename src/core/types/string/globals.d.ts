eba7b5a92a7c94aaadd92f5683e322e574eb079d
Ryan Cebulko <ryan@cebulko.com>
2021-12-16 14:12:45 -0500

    Consolidate #core globals

--- src/core/types/string/globals.d.ts
+++ src/core/types/string/globals.d.ts
@@ -4,4 +4,9 @@ declare global {
   interface Window {
     msCrypto?: Crypto;
   }
+
+  interface Location {
+    // Set by a viewer when it removes the fragment.
+    originalHash?: string;
+  }
 }
