6654c1a414fe335cb30ba80ee37be8e3c2873424
Matias Szylkowski <mszylkowski@google.com>
2021-12-20 12:24:35 -0500

    🚀 [Story performance] Move building share menu to share-menu.js (#37209)
    * Move building share menu to share-menu.js
    
    * Reorganized share-menu.js
    
    * Working share menu with proper split
    
    * Fixed dep-check, streamlined build
    
    * Fixed styling
    
    * Cleaned up
    
    * Updated comment
    
    * CanonicalURL is unnecessary in constructor
    
    * Fixed tests
    
    * Removed outdated amp-story links
    
    * Fixed lint errors

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -266,11 +266,11 @@ exports.rules = [
 
       // Story share menu
       'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/utils.js',
-      'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/amp-story-share.js',
       'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/amp-story-store-service.js',
-      'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/story-analytics.js',
-      'extensions/amp-story/1.0/amp-story.js->extensions/amp-story-share-menu/0.1/amp-story-share-menu.js',
       'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/amp-story-localization-service.js',
+      'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/request-utils.js',
+      'extensions/amp-story-share-menu/0.1/amp-story-share-menu.js->extensions/amp-story/1.0/toast.js',
+      'extensions/amp-story/1.0/amp-story-share.js->extensions/amp-story-share-menu/0.1/amp-story-share-menu.js',
 
       // Story Shopping
       'extensions/amp-story-shopping/0.1/amp-story-shopping-config.js->extensions/amp-story/1.0/amp-story-store-service.js',
