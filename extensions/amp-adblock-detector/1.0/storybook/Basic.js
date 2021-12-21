554c5cfe7d8f4e624fa57d6a14f03d5e616cdf7d
Anurag Vasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-12-21 20:32:19 +0530

    ♻️ Update Preact Example

--- extensions/amp-adblock-detector/1.0/storybook/Basic.js
+++ extensions/amp-adblock-detector/1.0/storybook/Basic.js
@@ -8,6 +8,20 @@ export default {
 };
 
 export const _default = (args) => {
+  /**
+   * TODO: Update Preact example when AMP Storybook Enviroment will be enabled (@anuragvasanwala)
+   *
+   * To render `amp-ad:0.1` we need:
+   * import {withAmp} from '@ampproject/storybook-addon';
+   *
+   * At this moment AMP Storybook Enviroment is disabled,
+   * so `amp-ad` will not be rendered and show `amp-ad`
+   * fallback div.
+   *
+   * Refer this PR for more information:
+   * https://github.com/ampproject/amphtml/pull/36780
+   */
+
   return (
     <BentoAdblockDetector
       style={{width: 120, height: 600}}
@@ -20,13 +34,19 @@ export const _default = (args) => {
           data-slot="/21730346048/test-skyscraper"
           {...props}
         >
-          <div fallback>
-            This is a fallback -- do similar as image-slider for img tag slot
-          </div>
+          <div fallback>This is AMP-Ad fallback.</div>
         </amp-ad>
       )}
       fallbackDiv={(props) => (
-        <div status="blocked" {...props}>
+        <div
+          style={{
+            border: '2px solid red',
+            borderRadius: 10,
+            margin: 5,
+            padding: 5,
+          }}
+          {...props}
+        >
           <h2>Ad Blocker Detected</h2>
           <p>Please allow ads to run on this page.</p>
         </div>
