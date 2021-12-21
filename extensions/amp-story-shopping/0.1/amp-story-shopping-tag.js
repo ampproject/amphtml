1fd678912e9195bfb87bdc50bdb3eaf72e0868a8
Jonathan Shamblen <jshamble@google.com>
2021-12-20 16:02:17 -0800

    added back in currency

--- extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
+++ extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
@@ -43,7 +43,11 @@ const renderShoppingTagTemplate = (tagData) => (
             {tagData['product-tag-text']}
           </span>
         )) ||
-          tagData['product-price']}
+          new Intl.NumberFormat('en-EN', {
+            style: 'currency',
+            currency: 'USD',
+            maximumFractionDigits: 0,
+          }).format(tagData['product-price'])}
       </span>
     </span>
   </div>
