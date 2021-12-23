3d06e61b6949affa49fa2c89bd550777f0fc8c3a
Matias Szylkowski <mszylkowski@google.com>
2021-12-23 16:45:05 -0500

    🚀  [Story attachment] Move page-attachment to separate extension (#37253)
    * Started moving page attachment
    
    * Create bundle
    
    * Fixed tests
    
    * Fixed import
    
    * Not use service getters to avoid bundling
    
    * Fix services
    
    * Fix shopping unit test
    
    * Removed dependency on localizationservice
    
    * Fixed localization
    
    * Fixed tests
    
    * Sync service fetchers
    
    * Made services sync
    
    * Fixed tests for sync services
    
    * Remove unused import in test
    
    * AnalyticsService to sync
    
    * Import devAssert
    
    * Remove param to buildInline
    
    * Undo describes.js
    
    * Fix tests
    
    * Remove unused import
    
    * Debundle store with forms
    
    * Install extension without check

--- extensions/amp-story-page-attachment/0.1/test/test-amp-story-page-attachment.js
+++ extensions/amp-story-page-attachment/0.1/test/test-amp-story-page-attachment.js
@@ -1,4 +1,9 @@
 import {AmpDocSingle} from '#service/ampdoc-impl';
+import {LocalizationService} from '#service/localization';
+
+import {AmpStoryStoreService} from 'extensions/amp-story/1.0/amp-story-store-service';
+import {registerServiceBuilder} from 'src/service-helpers';
+
 import {AmpStoryPageAttachment} from '../amp-story-page-attachment';
 
 describes.realWin('amp-story-page-attachment', {amp: true}, (env) => {
@@ -14,6 +19,16 @@ describes.realWin('amp-story-page-attachment', {amp: true}, (env) => {
     const storyEl = win.document.createElement('amp-story');
     storyEl.getAmpDoc = () => new AmpDocSingle(win);
     win.document.body.appendChild(storyEl);
+
+    const localizationService = new LocalizationService(win.document.body);
+    registerServiceBuilder(win, 'localization', function () {
+      return localizationService;
+    });
+
+    const storeService = new AmpStoryStoreService(win);
+    registerServiceBuilder(win, 'story-store', function () {
+      return storeService;
+    });
 
     // Set up the attachment element for inline attachment testing.
     attachmentEl = win.document.createElement('amp-story-page-attachment');
@@ -35,12 +50,12 @@ describes.realWin('amp-story-page-attachment', {amp: true}, (env) => {
   });
 
   it('should build an attachment', async () => {
-    attachment.buildCallback();
+    await attachment.buildCallback();
     return attachment.layoutCallback();
   });
 
   it('should build an outlink', async () => {
-    outlink.buildCallback();
+    await outlink.buildCallback();
     return outlink.layoutCallback();
   });
 
@@ -48,7 +63,7 @@ describes.realWin('amp-story-page-attachment', {amp: true}, (env) => {
     const anchorEl = outlinkEl.querySelector('amp-story-page-outlink a');
     anchorEl.setAttribute('target', '_blank');
 
-    outlink.buildCallback();
+    await outlink.buildCallback();
     await outlink.layoutCallback();
 
     expect(anchorEl.getAttribute('target')).to.eql('_top');
