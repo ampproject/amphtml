bf44bbbd58442b56f5c15620fdaf4faef7a4cc89
Anurag Vasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-12-28 14:56:03 +0530

    ♻️ Update example with multiple Ads

--- extensions/amp-adblock-detector/1.0/storybook/Basic.amp.js
+++ extensions/amp-adblock-detector/1.0/storybook/Basic.amp.js
@@ -16,24 +16,82 @@ export default {
 
 export const _default = (args) => {
   return (
-    <amp-adblock-detector layout="fixed" width="120" height="600" {...args}>
-      <amp-ad
+    <>
+      <amp-adblock-detector
+        layout="fixed"
         width="120"
         height="600"
-        type="doubleclick"
-        data-slot="/21730346048/test-skyscraper"
+        style="margin: 10px"
+        {...args}
       >
-        <div fallback>
-          <p>Error while loading Ad</p>
+        <amp-ad
+          width="120"
+          height="600"
+          type="doubleclick"
+          data-slot="/21730346048/test-skyscraper"
+        >
+          <div fallback>
+            <p>Error while loading Ad</p>
+          </div>
+        </amp-ad>
+        <div
+          status="blocked"
+          style="border: 2px solid red; border-radius: 10px; padding: 5px;"
+        >
+          <h2>Ad Blocker Detected</h2>
+          <p>Please allow ads to run on this page.</p>
         </div>
-      </amp-ad>
-      <div
-        status="blocked"
-        style="border: 2px solid red; border-radius: 10px; margin: 5px; padding: 5px;"
+      </amp-adblock-detector>
+      <amp-adblock-detector
+        layout="fixed"
+        width="120"
+        height="600"
+        style="margin: 10px"
+        {...args}
       >
-        <h2>Ad Blocker Detected</h2>
-        <p>Please allow ads to run on this page.</p>
-      </div>
-    </amp-adblock-detector>
+        <amp-ad
+          width="120"
+          height="600"
+          type="doubleclick"
+          data-slot="/21730346048/test-skyscraper"
+        >
+          <div fallback>
+            <p>Error while loading Ad</p>
+          </div>
+        </amp-ad>
+        <div
+          status="blocked"
+          style="border: 2px solid red; border-radius: 10px; padding: 5px;"
+        >
+          <h2>Ad Blocker Detected</h2>
+          <p>Please allow ads to run on this page.</p>
+        </div>
+      </amp-adblock-detector>
+      <amp-adblock-detector
+        layout="fixed"
+        width="120"
+        height="600"
+        style="margin: 10px"
+        {...args}
+      >
+        <amp-ad
+          width="120"
+          height="600"
+          type="doubleclick"
+          data-slot="/21730346048/test-skyscraper"
+        >
+          <div fallback>
+            <p>Error while loading Ad</p>
+          </div>
+        </amp-ad>
+        <div
+          status="blocked"
+          style="border: 2px solid red; border-radius: 10px; padding: 5px;"
+        >
+          <h2>Ad Blocker Detected</h2>
+          <p>Please allow ads to run on this page.</p>
+        </div>
+      </amp-adblock-detector>
+    </>
   );
 };
