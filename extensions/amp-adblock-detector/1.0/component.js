9d49243d64b9c73bd9d7c152782b7c43aa32c097
Anurag Vasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2021-12-28 14:52:26 +0530

    ♻️ Fix to avoide multiple request to server
    `window` (say global) variable is used to set status of the extension.
    
    Loading of the first most extension initialize parameters and then try to fetch from the server to check AdBlocker.
    
    Subsequent extension only add their functionCallback to `window` parameter.
    
    When first most extension detects blocker, it shows it's own fallback and then call functionCallback of the subsequent extensions.
    
    This save time and resources by sending only one request to the server and based on the responce, all extension on the page shows fallback when AdBlocker is present.

--- extensions/amp-adblock-detector/1.0/component.js
+++ extensions/amp-adblock-detector/1.0/component.js
@@ -1,5 +1,5 @@
 import * as Preact from '#preact';
-import {useEffect, useRef, useState} from '#preact';
+import {useCallback, useEffect, useRef, useState} from '#preact';
 import {forwardRef} from '#preact/compat';
 import {ContainWrapper} from '#preact/component';
 
@@ -32,18 +32,56 @@ export function BentoAdblockDetectorWithRef(
   const fallbackDivRef = useRef(null);
   const containerWrapperRef = useRef(ref);
 
+  /** Callbacks */
+  const blockerDetectedCallback = useCallback(() => {
+    /** AdBlocker won't allow to fetch from `url`, show `fallbackDiv` */
+    setIsBlockerDetected(true);
+  }, []);
+
   useEffect(() => {
+    /** Check for `adBlockExtension` in window */
+    if (!window.adBlockExtension) {
+      /** Initialize adBlockExtension */
+      window.adBlockExtension = {};
+
+      /** Set loading state */
+      window.adBlockExtension.isLoading = true;
+    } else if (window.adBlockExtension.isLoading) {
+      /** If `adBlockExtension` exists, push this extension's `blockerDetectedCallback()` */
+      window.adBlockExtension.fallbackFunctions =
+        window.adBlockExtension.fallbackFunctions || [];
+      window.adBlockExtension.fallbackFunctions.push(() =>
+        blockerDetectedCallback()
+      );
+
+      /** Return to avoide multiple request to `https://doubleclick.net` */
+      return;
+    }
+
     /** Try to fetch `https://doubleclick.net` */
     const url = 'https://doubleclick.net';
     fetch(url, {
       method: 'HEAD',
       mode: 'no-cors',
       cache: 'no-store',
-    }).catch(() => {
-      /** AdBlocker won't allow to fetch from `url`, show `fallbackDiv` */
-      setIsBlockerDetected(true);
-    });
-  }, []);
+    })
+      .catch(() => {
+        /** AdBlocker won't allow to fetch from `url`, show `fallbackDiv` for the first extension */
+        setIsBlockerDetected(true);
+
+        /** show `fallbackDiv` for the rest of the extensions */
+        window.adBlockExtension.fallbackFunctions.forEach(
+          (fallbackFunction) => {
+            fallbackFunction();
+          }
+        );
+      })
+      .finally(() => {
+        /** Cleanup resources */
+        window.adBlockExtension.isLoading = false;
+        window.adBlockExtension.fallbackFunctions = [];
+      });
+  }, [blockerDetectedCallback]);
 
   return (
     <ContainWrapper layout size paint {...rest} ref={containerWrapperRef}>
