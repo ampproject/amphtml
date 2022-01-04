ca432cfdcf26215e9b0f4ff1e39aa7ab8e7bdf09
Matias Szylkowski <mszylkowski@google.com>
2022-01-04 12:02:07 -0500

    🚀 [Story performance] Remove logging and development UI from the amp-story bundle (#37279)
    * Added tasts
    
    * Undo
    
    * Removed logging and dev-ui
    
    * Zindex
    
    * Removed more code in system layer
    
    * Removed developer stuff on amp-story

--- extensions/amp-story/1.0/amp-story-page.js
+++ extensions/amp-story/1.0/amp-story-page.js
@@ -43,9 +43,7 @@ import {dev} from '#utils/log';
 import {dict} from '#core/types/object';
 import {getFriendlyIframeEmbedOptional} from '../../../src/iframe-helper';
 import {localize} from './amp-story-localization-service';
-import {getLogEntries} from './logging';
 import {getMediaPerformanceMetricsService} from './media-performance-metrics-service';
-import {getMode} from '../../../src/mode';
 import {isExperimentOn} from '#experiments';
 import {isPrerenderActivePage} from './prerender-active-page';
 import {listen, listenOnce} from '#utils/event-helper';
@@ -517,8 +515,6 @@ export class AmpStoryPage extends AMP.BaseElement {
       this.checkPageHasElementWithPlayback_();
       this.findAndPrepareEmbeddedComponents_();
     }
-
-    this.reportDevModeErrors_();
   }
 
   /** @override */
@@ -1352,26 +1348,6 @@ export class AmpStoryPage extends AMP.BaseElement {
       Action.TOGGLE_PAGE_HAS_ELEMENT_WITH_PLAYBACK,
       pageHasElementWithPlayback
     );
-  }
-
-  /**
-   * @private
-   */
-  reportDevModeErrors_() {
-    if (!getMode().development) {
-      return;
-    }
-
-    getLogEntries(this.element).then((logEntries) => {
-      dispatch(
-        this.win,
-        this.element,
-        EventType.DEV_LOG_ENTRIES_AVAILABLE,
-        // ? is OK because all consumers are internal.
-        /** @type {?} */ (logEntries),
-        {bubbles: true}
-      );
-    });
   }
 
   /**
