09cb7c2c04a007089967ccf35accd80cfaff0c83
Jonathan <jshamble@users.noreply.github.com>
2022-01-05 14:42:16 -0800

    ✨ amp-story-shopping Updated currency with product price and correct Localized currency (#37249)
    * updated PR with localization service that fetches the correct lanaguage ISO code, updated the visual diff test
    
    * fixed a few typos
    
    * added dependency
    
    * added ecimal exmaples
    
    * added localization langauge code
    
    * added in i18n visual diff test, changen strings to numbers
    
    * deleted duplicate test
    
    * deleted duplicate test
    
    * removed shopping tag span - will meo it to another PR when ready
    
    * added correct types for parameters
    
    * udpated price in visual diff test
    
    * udtaed unit test
    
    * updated product price
    
    * added localization service stub to unit tests
    
    * added localization service variable
    
    * removed unused allowlist entry
    
    * created a class method, followed pattern
    
    * added back code
    
    * added back code
    
    * added onclick fix
    
    * removed tag data from onclcik method
    
    * removed duplicate page id

--- extensions/amp-story/1.0/amp-story-store-service.js
+++ extensions/amp-story/1.0/amp-story-store-service.js
@@ -72,7 +72,8 @@ export let InteractiveReactData;
  * @typedef {{
  *   product-tag-id: string,
  *   product-title: string,
- *   product-price: string,
+ *   product-price: number,
+ *   product-price-currency: string,
  *   product-icon: string,
  *   product-tag-text: ?string,
  * }}
