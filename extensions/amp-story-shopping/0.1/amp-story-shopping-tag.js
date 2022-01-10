05a997ca887e26f8267767daa2e138c77e4d5e45
processprocess <philip.hunter.bell@gmail.com>
2022-01-10 12:55:40 -0600

    revert test commit

--- extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
+++ extensions/amp-story-shopping/0.1/amp-story-shopping-tag.js
@@ -180,7 +180,6 @@ export class AmpStoryShoppingTag extends AMP.BaseElement {
    * @private
    */
   createAndAppendInnerShoppingTagEl_(shoppingData) {
-    //test
     this.tagData_ = shoppingData[this.element.getAttribute('data-tag-id')];
     if (this.hasAppendedInnerShoppingTagEl_ || !this.tagData_) {
       return;
