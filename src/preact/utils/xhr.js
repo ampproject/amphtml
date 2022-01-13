a18ddf9fdd73b3019f65adbb2b6eff7b451ba70a
scottrippey <scott.william.rippey@gmail.com>
2022-01-13 13:25:05 -0700

    feature(bento-list): fixed `fetchJson` signature

--- src/preact/utils/xhr.js
+++ src/preact/utils/xhr.js
@@ -1,10 +1,10 @@
 export const xhrUtils = {
   /**
-   * Simply calls `fetch(url).then(r => r.json())`
+   * Simple wrapper around `fetch`
    * @param {RequestInfo} url
    * @return {Promise<Response>}
    */
   fetchJson(url) {
-    return self.fetch(url).then((r) => r.json());
+    return self.fetch(url);
   },
 };
