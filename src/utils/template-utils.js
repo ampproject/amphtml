9f58916544ad1c51f9e10b820a6cfe735a12e2de
Dhruv Manek <dmanek@gmail.com>
2022-01-20 15:03:14 -0800

    POC

---
+++ src/utils/template-utils.js
@@ -0,0 +1,78 @@
+/**
+ * @fileoverview Utils for supporting <template> element in for Bento web components like bento-date-display.
+ */
+
+/**
+ * @param {string} str string with backticks
+ * @return {string} string with escaped backticks for use in template strings
+ */
+function escapeBackticks(str) {
+  return str.replaceAll('`', '\\`');
+}
+
+/**
+ * @param {Object} data
+ * @return {string}
+ */
+function getVariablesAsString(data) {
+  let variables = '';
+  for (const [key, value] of Object.entries(data)) {
+    variables += `const ${key} = '${value}';`;
+  }
+  return variables;
+}
+
+/**
+ * @param {HTMLTemplateElement} template
+ * @return {string}
+ */
+function getTemplateContentsAsString(template) {
+  let templateStr = '';
+  // `template.content` returns a Document Fragment, so iterate over it's
+  // children to return as a string.
+  for (let i = 0, count = template.content.childElementCount; i < count; i++) {
+    templateStr += template.content.children[i]./*OK*/ outerHTML;
+  }
+  return templateStr;
+}
+
+/**
+ * @param {HTMLElement} element
+ * @return {HTMLTemplateElement|null}
+ */
+export function getTemplateElement(element) {
+  return element.hasAttribute('template')
+    ? element.ownerDocument.getElementById(element.getAttribute('template'))
+    : element.querySelector('template');
+}
+
+/**
+ *
+ * @param {Object} data
+ * @param {HTMLTemplateElement} template
+ * @return {Function}
+ */
+export function getTemplateFunction(data, template) {
+  const variables = getVariablesAsString(data);
+  const templateContents = getTemplateContentsAsString(template);
+  return new Function(
+    `${variables} return \`${escapeBackticks(templateContents)}\`;`
+  );
+}
+
+const regex = /\{\w*}/g;
+
+/**
+ * @param {HTMLTemplateElement} template
+ * @return {!Object}
+ */
+export function makeTemplateObject(template) {
+  const contents = getTemplateContentsAsString(template)
+    .replaceAll(regex, '')
+    .split('$');
+
+  return Object.defineProperty(contents, 'raw', {
+    value: contents.slice(0),
+  });
+  // render(html(obj, data), document.getElementById('app'));
+}
