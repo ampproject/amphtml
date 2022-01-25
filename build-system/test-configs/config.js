e98ae97d192397cd13c7ad919f655c7fb21999b2
Ya-Chen Hsieh <1697814+ychsieh@users.noreply.github.com>
2022-01-25 16:55:57 -0500

    Add a todo to remove allowlist once the validator rules are in.

--- build-system/test-configs/config.js
+++ build-system/test-configs/config.js
@@ -254,6 +254,7 @@ const htmlFixtureGlobs = [
   '!examples/amp-script/vue-todomvc.amp.html',
   '!examples/amp-skimlinks.html',
   '!examples/amp-smartlinks.html',
+  // TODO(#37285): remove after the new validator rules are pushed with the new npm.
   '!examples/amp-story-subscription/amp-story-subscription.html',
   '!examples/amp-subscriptions-google/amp-subscriptions-iframe.provider.html',
   '!examples/amp-subscriptions-google/amp-subscriptions-metering-laa.amp.html',
