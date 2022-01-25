4f03c3ba4a3c50032e2537cf846230724be33cf6
Jonathan Shamblen <jshamble@google.com>
2022-01-25 13:29:58 -0800

    added in proper valdiation checks using reduce funcitons

--- extensions/amp-story-shopping/0.1/amp-story-shopping-config.js
+++ extensions/amp-story-shopping/0.1/amp-story-shopping-config.js
@@ -4,6 +4,7 @@ import {Services} from '#service';
 
 import {getElementConfig} from 'extensions/amp-story/1.0/request-utils';
 
+import {assertHttpsUrl} from '../../../src/url';
 import {
   Action,
   ShoppingConfigDataDef,
@@ -16,28 +17,70 @@ import {
  */
 let ShoppingConfigDef;
 
-const attributeValidators = {
-  'required': new Set([
-    'productTagId',
-    'brandLabel',
-    'productTitle',
-    'productPrice',
-    'productImages',
-    'productDetails',
-    'productPriceCurrency',
-    'reviewsPage',
-  ]),
-  'optional': new Set([
-    'productColor',
-    'productSize',
-    'productIcon',
-    'productTagText',
-    'reviewsData',
-    'ctaText',
-    'shippingText',
-  ]),
-};
+/**
+ * Validates string length of shopping config attributes
+ * @param {?string} field
+ * @param {?string} value
+ * @return {!Array<string>}
+ * @private
+ */
+function validateRequired(field, value) {
+  if (value == null) {
+    return [`${field} is required.`];
+  }
+}
 
+/**
+ * Validates string length of shopping config attributes
+ * @param {?string} field
+ * @param {?string} str
+ * @param {?number} maxLen
+ * @return {!Array<string>}
+ * @private
+ */
+function validateStringLength(field, str, maxLen = 100) {
+  if (str != null && str.length > maxLen) {
+    return [
+      'Length of ' +
+        field +
+        ' exceeds max length: ' +
+        str.length +
+        ' > ' +
+        maxLen,
+    ];
+  }
+}
+
+/**
+ * Validates number in shopping config attributes
+ * @param {?string} field
+ * @param {?number} number
+ * @private
+ * @return {!Array<string>}
+ */
+function validateNumber(field, number) {
+  if (isNaN(number)) {
+    return ['Value for field ' + field + ' is not a number '];
+  }
+}
+
+/**
+ * Validates url of shopping config attributes
+ * @param {?string} field
+ * @param {?string} url
+ * @private
+ * @return {!Array<string>}
+ */
+function validateURL(field, url) {
+  const urls = Array.isArray(url) ? url : [url];
+  return urls.map((url) => {
+    try {
+      assertHttpsUrl(url, 'amp-story-shopping-config ' + field);
+    } catch (e) {
+      return e.message;
+    }
+  });
+}
 export class AmpStoryShoppingConfig extends AMP.BaseElement {
   /** @param {!AmpElement} element */
   constructor(element) {
@@ -45,96 +88,50 @@ export class AmpStoryShoppingConfig extends AMP.BaseElement {
     /** @private @const {?../../amp-story/1.0/amp-story-store-service.AmpStoryStoreService} */
     this.storeService_ = null;
 
-    const validateStringLength = [this.validateStringLength_];
-    const validateNumber = [this.validateNumber_];
-    const validateURL = [this.validateURL_];
-
     /** @private @const {!Object<Array<Function>>} */
     this.productValidationConfig_ = {
       /* Required Attrs */
-      'productTagId': validateStringLength, // Required. String. Keys to amp-story-shopping-tag nodes.
-      'brandLabel': validateStringLength, // Required. String.
-      'productTitle': validateStringLength, // Required. String.
-      'productPrice': validateNumber, // Required. Number.
-      'productImages': validateURL, // Required. String or array of strings.
-      'productDetails': validateStringLength, // Required. String.
-      'reviewsPage': validateURL, // Required if reviews-data. Links to a page where reviews can be read.
-      'productPriceCurrency': validateStringLength, // Required. String. ISO 4217 currency code used to display the correct currency symbol.
+      'productTagId': [validateRequired, validateStringLength], // Required. String. Keys to amp-story-shopping-tag nodes.
+      'brandLabel': [validateRequired, validateStringLength], // Required. String.
+      'productTitle': [validateRequired, validateStringLength], // Required. String.
+      'productPrice': [validateRequired, validateNumber], // Required. Number.
+      'productImages': [validateRequired, validateURL], // Required. String or array of strings.
+      'productDetails': [validateRequired, validateStringLength], // Required. String.
+      'reviewsPage': [validateRequired, validateURL], // Required if reviews-data. Links to a page where reviews can be read.
+      'productPriceCurrency': [validateRequired, validateStringLength], // Required. String. ISO 4217 currency code used to display the correct currency symbol.
       /* Optional Attrs */
-      'productColor': validateStringLength, // Optional. String.
-      'productSize': validateStringLength, // Optional. String.
-      'productIcon': validateURL, // Optional. Links to an image. Defaults to a shopping bag icon.
-      'productTagText': validateStringLength, // Optional. String.
-      'reviewsData': validateURL, // Optional. Links to review data.
-      'ctaText': validateNumber, // Optional. Number. Defaults to “Buy now”. Keys to a CTA text option for i18n.
-      'shippingText': validateNumber, // Optional. Number. Keys to a shipping text option for i18n. Shipping text will not be rendered if the string is omitted.
+      'productColor': [validateRequired, validateStringLength], // Optional. String.
+      'productSize': [validateStringLength], // Optional. String.
+      'productIcon': [validateURL], // Optional. Links to an image. Defaults to a shopping bag icon.
+      'productTagText': [validateStringLength], // Optional. String.
+      'reviewsData': [validateURL], // Optional. Links to review data.
+      'ctaText': [validateNumber], // Optional. Number. Defaults to “Buy now”. Keys to a CTA text option for i18n.
+      'shippingText': [validateNumber], // Optional. Number. Keys to a shipping text option for i18n. Shipping text will not be rendered if the string is omitted.
     };
   }
-
-  /**
-   * Validates shopping config Attributes
-   * @param {!ShoppingConfigDataDef} prouductAttrs
-   * @param {?string} attribValidatorType
-   * @private
-   */
-  validateAttributes_(prouductAttrs, attribValidatorType = 'required') {
-    for (const attrib of attributeValidators[attribValidatorType]) {
-      if (!prouductAttrs[attrib]) {
-        console.log('missing ' + attribValidatorType + ' attribute ' + attrib);
-      }
-    }
-  }
-
-  /**
-   * Validates string length of shopping config attributes
-   * @param {string} str
-   * @param {?number} maxLen
-   * @private
-   */
-  validateStringLength_(str, maxLen = 100) {
-    if (str.length > maxLen) {
-      console.log(
-        str + ' length exceeds max length: ' + str.length + ' > ' + maxLen
-      );
-    }
-  }
-
-  /**
-   * Validates number in shopping config attributes
-   * @param {number} number
-   * @private
-   */
-  validateNumber_(number) {
-    if (isNaN(number)) {
-      console.log(number + ' is not a number ');
-    }
-  }
-
-  /**
-   * Validates url of shopping config attributes
-   * @param {string} url
-   * @private
-   */
-  validateURL_(url) {
-    //TODO (need to check if string or araay of strings (use typeof?) for 'productImages' )
-  }
-
   /**
    * Validates shopping config.
    * @param {!ShoppingConfigDataDef} shoppingConfig
    * @private
    */
   validateConfig_(shoppingConfig) {
-    for (const attr in shoppingConfig) {
-      if (attr in this.productValidationConfig_) {
-        /* Valid Attribute, run the function array check here */
-        for (const validationFunction of this.productValidationConfig_[attr]) {
-          validationFunction(shoppingConfig[attr]);
-        }
-      } else {
-        console.log('invalid product attribute: ' + attr);
-      }
-      this.validateAttributes_(shoppingConfig);
+    const errors = Object.keys(this.productValidationConfig_)
+      .reduce((errors, k) => {
+        const value = shoppingConfig[k];
+        const fns = this.productValidationConfig_[k];
+        errors.push(
+          ...fns.reduce((errors, fn) => {
+            const localErrors = fn(k, value) || [];
+            errors.push(...localErrors);
+            return errors;
+          }, [])
+        );
+        return errors;
+      }, [])
+      .filter(Boolean);
+
+    for (const error of errors) {
+      this.user().warn(error);
     }
   }
 
