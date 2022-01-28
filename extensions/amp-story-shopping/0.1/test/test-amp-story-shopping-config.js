dab5ae0fc458f27efb7cd955ec16efe39b08372d
Alan Orozco <alan@orozco.xyz>
2022-01-28 12:36:25 -0800

    test

--- extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-config.js
+++ extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-config.js
@@ -2,7 +2,7 @@ import * as Preact from '#core/dom/jsx';
 
 import {Services} from '#service';
 
-import * as defaultConfig from '../../../../examples/amp-story/shopping/remote.json';
+import * as remoteConfig from '../../../../examples/amp-story/shopping/remote.json';
 import {Action} from '../../../amp-story/1.0/amp-story-store-service';
 import {
   getShoppingConfig,
@@ -20,6 +20,21 @@ describes.realWin(
   (env) => {
     let pageElement;
 
+    const defaultInlineConfig = {
+      items: [
+        {
+          'product-tag-id': 'city-pop',
+          'product-title': 'Plastic Love',
+          'product-price': 19,
+          'product-price-currency': 'JPY',
+        },
+      ],
+    };
+
+    const keyedDefaultInlineConfig = {
+      'city-pop': defaultInlineConfig.items[0],
+    };
+
     beforeEach(async () => {
       pageElement = <amp-story-page id="page1"></amp-story-page>;
       env.win.document.body.appendChild(pageElement);
@@ -27,7 +42,7 @@ describes.realWin(
 
     async function createAmpStoryShoppingConfig(
       src = null,
-      config = defaultConfig
+      config = defaultInlineConfig
     ) {
       pageElement.appendChild(
         <amp-story-shopping-config layout="nodisplay" src={src}>
@@ -47,48 +62,48 @@ describes.realWin(
     });
 
     it('does use inline config', async () => {
-      const exampleURL = 'foo.example';
-
-      const result = await createAmpStoryShoppingConfig(exampleURL);
-
-      const expectedRemoteResult = JSON.parse(
-        '{"art":{"productId": "art","productTitle": "Abstract Art","productBrand": "V. Artsy","productPrice": 1200.0,"productPriceCurrency": "JPY","productImages": ["https://source.unsplash.com/BdVQU-NDtA8/500x500"]}}'
-      );
-
-      expect(result).to.deep.eql(expectedRemoteResult);
+      const result = await createAmpStoryShoppingConfig();
+      expect(result).to.deep.eql(keyedDefaultInlineConfig);
     });
 
     it('does use remote config when src attribute is provided', async () => {
-      const exampleURL = 'foo.example';
-
-      const result = await createAmpStoryShoppingConfig(exampleURL);
-
-      const expectedRemoteResult = JSON.parse(
-        '{"city-pop":{"product-tag-id":"city-pop","product-title":"Plastic Love","product-price": 19, "product-price-currency": "JPY"}}'
-      );
-
+      const remoteUrl = 'https://foo.example';
+      const expectedRemoteResult =
+        // matches remote.json
+        {
+          'art': {
+            'product-tag-id': 'art',
+            'product-title': 'Abstract Art',
+            'product-brand': 'V. Artsy',
+            'product-price': 1200.0,
+            'product-price-currency': 'JPY',
+            'product-images': [
+              'https://source.unsplash.com/BdVQU-NDtA8/500x500',
+            ],
+          },
+        };
+      env.sandbox.stub(Services, 'xhrFor').returns({
+        fetchJson(url) {
+          if (url === remoteUrl) {
+            return Promise.resolve({
+              ok: true,
+              json: () => remoteConfig,
+            });
+          }
+        },
+      });
+      const result = await createAmpStoryShoppingConfig(remoteUrl);
       expect(result).to.deep.eql(expectedRemoteResult);
     });
 
     it('does use inline config when remote src is invalid', async () => {
-      const exampleURL = 'invalidRemoteURL';
-      const inlineConfig = {items: [{'product-tag-id': 'foo'}]};
-
-      const result = await createAmpStoryShoppingConfig(
-        exampleURL,
-        inlineConfig
-      );
-
-      expect(result).to.deep.eql({foo: {'product-tag-id': 'foo'}});
-    });
-
-    it('errors with invalid remote config URL', async () => {
-      const exampleURL = 'invalidRemoteURL';
-      expectAsyncConsoleError(async () => {
-        expect(() => createAmpStoryShoppingConfig(exampleURL)).to.throw(
-          /error determining if remote config is valid json: bad url or bad json/
-        );
+      env.sandbox.stub(Services, 'xhrFor').returns({
+        fetchJson() {
+          throw new Error();
+        },
       });
+      const result = await createAmpStoryShoppingConfig('invalidRemoteUrl');
+      expect(result).to.deep.eql(keyedDefaultInlineConfig);
     });
 
     describe('storeShoppingConfig', () => {
