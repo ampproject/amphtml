62677801a3b8bf582d3cb7d15f1faf2ae8ed3f74
Philip Bell <philip.hunter.bell@gmail.com>
2022-01-28 10:32:46 -0600

    [amp story shopping] Product Listing Page (PLP) (#37318)
    * plp
    
    * comment
    
    * Styling
    
    * Number formatting function.
    
    * White space
    
    * Rename enum
    
    * abstract load fonts into amp-story
    
    * Fix conflict
    
    * revert
    
    * format number
    
    * revert
    
    * Abstract shared functions from shopping tag
    
    * Revert "Abstract shared functions from shopping tag"
    
    This reverts commit 68518455c7a516a80b2c5b9a2a3608a7f6a848c7.
    
    * tag
    
    * test commit
    
    * revert test commit
    
    * lint
    
    * Update dep check
    
    * Example update
    
    * Update variable names.
    
    * Set template function
    
    * Update visual tests.
    
    * unit test
    
    * lint
    
    * Update config test with new entries.
    
    * formatI18nNumber test
    
    * lint fix
    
    * Attachment content unit test.
    
    * Types, comments and structure
    
    * comment
    
    * indention in example template.
    
    * Template variables.
    
    * lint fix
    
    * update test
    
    * template names
    
    * Add depends on story service.
    
    * add dep
    
    * Update templates.
    
    * CTA label
    
    * CSS prefix. MutateElement.
    
    * Update type
    
    * Attachment state listener for populating PLP.
    
    * Test for invalid currency
    
    * Update template and images to royalty free.
    
    * background image on cards.
    
    * Update images and template
    
    * Prevent experience from building if no tags are on page.
    
    * Update template.
    
    * Update string
    
    * whitespace
    
    * Text adjustments
    
    * Revert "Text adjustments"
    
    This reverts commit 91180ded3bb0966fd21030bef49816e49a0818ee.
    
    * text adjustments
    
    * rebase
    
    * comment.
    
    * try catch to gracefully fallback to a number.
    
    * space
    
    * Link directly to images.
    
    * call open on amp-story-page-attachment
    
    * spaces
    
    * lint
    
    * price fallback
    
    * update tests
    
    * Update price fallback
    
    * newline
    
    * clean up tests

--- examples/visual-tests/amp-story/amp-story-shopping.js
+++ examples/visual-tests/amp-story/amp-story-shopping.js
@@ -16,4 +16,19 @@ module.exports = {
       '.i-amphtml-story-page-open-attachment[active]',
     ]);
   },
+  'displays shopping PLP (product list page)': async (page, name) => {
+    const pageID = 'inline-light-theme';
+    const url = await page.url();
+    await page.goto(`${url}#page=${pageID}`);
+    await page.waitForSelector(
+      `amp-story-page#${pageID}[active][distance="0"]`
+    );
+    await page.tap(
+      `amp-story-page#${pageID} .i-amphtml-story-inline-page-attachment-chip`
+    );
+    await page.waitForSelector('.i-amphtml-story-draggable-drawer-open');
+    await verifySelectorsVisible(page, name, [
+      '.i-amphtml-story-draggable-drawer-open',
+    ]);
+  },
 };
