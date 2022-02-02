689ffdf87323caf142012fc29ae3fcdcfcea76f8
Jonathan Shamblen <jshamble@google.com>
2022-02-02 12:12:01 -0800

    changed test string

--- extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-config.js
+++ extensions/amp-story-shopping/0.1/test/test-amp-story-shopping-config.js
@@ -118,7 +118,7 @@ describes.realWin(
           {
             'productId': 'city-pop',
             'productTitle':
-              'Mariya Takeuchi (竹内 まりや, Takeuchi Mariya, born 20 March 1955) is a Japanese singer and songwriter. Regarded as an influential figure in the city pop genre, she is one of the best-selling music artists in Japan, having sold over 16 million records, and has received several accolades. Takeuchi was born in Taisha, Hikawa district, now the city of Izumo, Shimane, and attended Keio University. She made her singing debut after signing with the RCA record label in 1978, with whom she released her debut album Beginning, which peaked at No. 17 on Oricon Charts. She then released four albums between 1979 and 1981, all of which obtained commercial success, including the 1980 album Love Songs, which became her first work to peak at No. 1 on Oricon Charts. Takeuchi then announced she would go on a temporary hiatus in 1981, terminating her contract with RCA records. Three years later, Takeuchi and her husband Tatsuro Yamashita signed with Moon Records, and she made her comeback with her sixth studio album Variety in 1984, which was released internationally and shot her to mainstream success, and peaked at No. 1 on Oricon Charts. The track "Plastic Love", which was released in 1985 as a single, became a surprise hit outside of Japan in 2017 after a YouTube upload of the song went viral. The song has since attained a cult following and is seen as the staple in a revival of interest in city pop in the late 2010s.',
+              'Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, Very, long string',
             /* Very, Very, Very long string value - will not pass string length test */
             'productPrice': 19,
             'productPriceCurrency': 'JPY',
@@ -128,7 +128,7 @@ describes.realWin(
             ],
             'reviewsPage': 'https://store.google.com/jp/?hl=ja',
             'brandLabel': 'Google',
-            'productDetails': 'Vinyl for the hit J-Pop song Plastic Love.',
+            'productDetails': 'Vinyl for a hit J-Pop song.',
           },
         ],
       };
