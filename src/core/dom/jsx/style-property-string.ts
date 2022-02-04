1cb2ed7a88fc0fe0c5e7ec8106d34e6baedbd733
Alan Orozco <alan@orozco.xyz>
2022-02-04 12:48:43 -0800

    review

--- src/core/dom/jsx/style-property-string.ts
+++ src/core/dom/jsx/style-property-string.ts
@@ -5,12 +5,12 @@
 export function jsxStylePropertyString(
   property: string,
   value: any,
-  opt_isDimensional: boolean | undefined
+  isDimensional?: boolean
 ) {
   if (value == null || value === '') {
     return '';
   }
   const withUnit =
-    opt_isDimensional && typeof value === 'number' ? `${value}px` : value;
+    isDimensional && typeof value === 'number' ? `${value}px` : value;
   return `${property}:${withUnit};`;
 }
