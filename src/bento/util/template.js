4bb3f8d587f22429224752ced0ae0724965b762b
Dhruv Manek <dmanek@gmail.com>
2022-02-08 12:36:26 -0800

    fix failed check

--- src/bento/util/template.js
+++ src/bento/util/template.js
@@ -1,11 +1,15 @@
+import {escapeCssSelectorIdent} from '#core/dom/css-selectors';
+
 /**
  * @param {HTMLElement} element
  * @return {HTMLTemplateElement|null}
  */
 export function getTemplate(element) {
   if (element.hasAttribute('template')) {
-    const id = element.getAttribute('template')
-    return element.ownerDocument.querySelector(`template#${id}`);
+    const id = element.getAttribute('template');
+    return Boolean(id) ? element.ownerDocument.querySelector(
+      `template#${escapeCssSelectorIdent(id)}`
+    ) : null;
   }
   return element.querySelector('template');
 }
