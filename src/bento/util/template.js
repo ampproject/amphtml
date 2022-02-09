03d43198c84de4f17ac01e0dc69689a759f3c7fb
Dhruv Manek <506183+dmanek@users.noreply.github.com>
2022-02-09 11:34:15 -0800

    Update src/bento/util/template.js
    Co-authored-by: Scott Rippey <scottrippey@users.noreply.github.com>

--- src/bento/util/template.js
+++ src/bento/util/template.js
@@ -4,9 +4,9 @@ import {escapeCssSelectorIdent} from '#core/dom/css-selectors';
  * @param {HTMLElement} element
  * @return {HTMLTemplateElement|null}
  */
-export function getTemplate(element) {
-  if (element.hasAttribute('template')) {
-    const id = element.getAttribute('template');
+export function getTemplate(element, attribute = 'template') {
+  if (element.hasAttribute(attribute)) {
+    const id = element.getAttribute(attribute);
     return Boolean(id)
       ? element.ownerDocument.querySelector(
           `template#${escapeCssSelectorIdent(id)}`
