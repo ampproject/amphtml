75b8ad478dfe45298d23d30bb734c983f5dccae7
Caleb Cordry <ccordry@google.com>
2022-02-17 15:46:59 -0800

    oops

--- extensions/amp-story-auto-ads/0.1/story-ad-ui.js
+++ extensions/amp-story-auto-ads/0.1/story-ad-ui.js
@@ -309,7 +309,7 @@ export function createCta(doc, buttonFitter, container, uiMetadata) {
       return null;
     }
 
-    if (!isExperimentOn(doc.defaultView, 'story-ad-auto-advance')) {
+    if (isExperimentOn(doc.defaultView, 'story-ad-auto-advance')) {
       return createPageOutlink_(doc, uiMetadata, container);
     } else {
       return createCtaLayer_(a, doc, container);
