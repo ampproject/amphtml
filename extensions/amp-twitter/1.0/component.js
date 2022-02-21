9525cf3c0991cfc08c9e395ab14ebc280ff86a4e
Deepak Lalwani <deepak.lalwani81@gmail.com>
2022-02-21 17:56:23 +0530

    Add a workaround to resize amp-twitter component on build

--- extensions/amp-twitter/1.0/component.js
+++ extensions/amp-twitter/1.0/component.js
@@ -1,7 +1,8 @@
 import {MessageType_Enum, deserializeMessage} from '#core/3p-frame-messaging';
+import {setStyles} from '#core/dom/style';
 
 import * as Preact from '#preact';
-import {useCallback, useMemo, useState} from '#preact';
+import {useCallback, useEffect, useMemo, useRef, useState} from '#preact';
 import {forwardRef} from '#preact/compat';
 import {useValueRef} from '#preact/component';
 import {ProxyIframeEmbed} from '#preact/component/3p-frame';
@@ -40,6 +41,7 @@ function BentoTwitterWithRef(
   const [height, setHeight] = useState(null);
   const onLoadRef = useValueRef(onLoad);
   const onErrorRef = useValueRef(onError);
+  const iframeRef = useRef(ref);
 
   const messageHandler = useCallback(
     (event) => {
@@ -48,6 +50,17 @@ function BentoTwitterWithRef(
         const height = data['height'];
         if (requestResize) {
           requestResize(height);
+
+          // Remove the position style from embed after the resize is complete.
+          setStyles(iframeRef.current.node.getRootNode().host, {
+            position: '',
+            opacity: '',
+            top: '',
+            bottom: '',
+            left: '',
+            right: '',
+            pointerEvents: '',
+          });
           setHeight(FULL_HEIGHT);
         } else {
           setHeight(height);
@@ -87,10 +100,25 @@ function BentoTwitterWithRef(
     ]
   );
 
+  useEffect(() => {
+    // This style is added as part of a workaround to fix the component resizing issue because
+    // the resizing happens only when the embed comes into viewport and most of the time the
+    // attemptChangeHeight request is gets rejected.
+    setStyles(iframeRef.current.node.getRootNode().host, {
+      position: 'fixed',
+      opacity: '0',
+      top: '0',
+      bottom: '0',
+      left: '0',
+      right: '0',
+      pointerEvents: 'none',
+    });
+  }, [iframeRef]);
+
   return (
     <ProxyIframeEmbed
       allowfullscreen
-      ref={ref}
+      ref={iframeRef}
       title={title}
       {...rest}
       // non-overridable props
