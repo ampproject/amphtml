4ee2ba8e13f63c64c4d5127813591b387da136ca
setdream <setdream@yandex.ru>
2022-02-24 19:29:10 +0200

    Add subdivision support to amp-geo, codefixes

--- extensions/amp-geo/0.1/amp-geo.js
+++ extensions/amp-geo/0.1/amp-geo.js
@@ -444,7 +444,7 @@ export class AmpGeo extends AMP.BaseElement {
 
         // Otherwise we add the country to the list
         if (
-          country == VALUE_UNKNOWN ||
+          country === VALUE_UNKNOWN ||
           /^[a-zA-Z]{2}(?:-[0-9a-zA-Z]{1,3})?$/.test(country)
         ) {
           countries.push(country);
