2bbc0a6ef1d9b6f6c9ed2aa6649cea67ef57f811
scottrippey <scott.william.rippey@gmail.com>
2022-03-03 13:52:31 -0700

    feature(bento-pan-zoom): added more story types

--- extensions/amp-pan-zoom/1.0/storybook/Basic.js
+++ extensions/amp-pan-zoom/1.0/storybook/Basic.js
@@ -10,41 +10,38 @@ export default {
   },
 };
 
+const ipsum = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae dolores dolorum ducimus ex explicabo iusto magni maxime nam natus necessitatibus odio, odit porro quidem saepe sed sunt, temporibus veritatis. Aspernatur beatae eveniet expedita facilis iste numquam rerum! Explicabo ipsum nisi recusandae. Et in necessitatibus nisi nulla, officia placeat quas tenetur. Ab atque autem blanditiis ea eos explicabo fuga illum iste neque, non pariatur quae quos sed sit tenetur, vel velit. Ab dolorum harum nulla officiis, sunt suscipit tenetur? Animi aperiam atque eos nostrum voluptatibus. Atque dolorum ea, earum eligendi ipsum nesciunt pariatur placeat, quo quod similique sit sunt vitae.`;
+const img = `https://lh3.googleusercontent.com/pSECrJ82R7-AqeBCOEPGPM9iG9OEIQ_QXcbubWIOdkY=w400-h300-no`;
 export const _default = (args) => {
   return (
     <BentoPanZoom {...args}>
-      <span>This text is inside.</span>
-      This text is inside.
+      <div style={{border: '1px solid black'}}>{ipsum}</div>
+    </BentoPanZoom>
+  );
+};
+
+export const AutomaticSize = (args) => {
+  return (
+    <BentoPanZoom {...args} style={{}}>
+      <div style={{border: '1px solid black'}}>{ipsum}</div>
     </BentoPanZoom>
   );
 };
 
 export const WithImage = (args) => (
   <BentoPanZoom {...args}>
-    <img
-      height="225"
-      width="400"
-      alt="Destiny artwork"
-      src="https://lh3.googleusercontent.com/pSECrJ82R7-AqeBCOEPGPM9iG9OEIQ_QXcbubWIOdkY=w400-h300-no"
-    />
+    <img height="225" width="400" alt="Destiny artwork" src={img} />
+  </BentoPanZoom>
+);
+
+export const SmallImage = (args) => (
+  <BentoPanZoom {...args}>
+    <img height="88" width="160" alt="Destiny artwork" src={img} />
   </BentoPanZoom>
 );
 
 export const InitialScale = (args) => (
   <BentoPanZoom {...args} initialScale={3} initialX={-400} initialY={-75}>
-    <img
-      height="225"
-      width="400"
-      alt="Destiny artwork"
-      src="https://lh3.googleusercontent.com/pSECrJ82R7-AqeBCOEPGPM9iG9OEIQ_QXcbubWIOdkY=w400-h300-no"
-    />
+    <img height="225" width="400" alt="Destiny artwork" src={img} />
   </BentoPanZoom>
 );
-
-// export const _default = (args) => {
-//   return (
-//     <BentoPanZoom style={{width: 300, height: 200}} {...args}>
-//       This text is inside.
-//     </BentoPanZoom>
-//   );
-// };
