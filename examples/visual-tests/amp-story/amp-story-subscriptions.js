9bce07c0b6f3b48c13d05171bf17ccc78b066d2e
Ya-Chen Hsieh <1697814+ychsieh@users.noreply.github.com>
2022-03-09 15:21:29 -0500

    visual test skeleton

---
+++ examples/visual-tests/amp-story/amp-story-subscriptions.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const {
+  verifySelectorsVisible,
+} = require('../../../build-system/tasks/visual-diff/verifiers');
+
+module.exports = {
+  'paywall UI should display': async (page, name) => {
+    // const url = await page.url();
+    // await page.goto(`${url}`);
+    // // await page.waitForSelector(
+    // //   `amp-story-page#${pageID}[active][distance="0"]`
+    // // );
+    // await verifySelectorsVisible(page, name, [
+    //   '.i-amphtml-story-page-open-attachment[active]',
+    // ]);
+  },
+};
