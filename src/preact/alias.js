fe56299f00d5547d9b96d842bbc61a6d341e5e8b
scottrippey <scott.william.rippey@gmail.com>
2022-03-10 13:16:43 -0700

    feature(bento-pan-zoom): cherry-picked build config for 3rd party react libraries

---
+++ src/preact/alias.js
@@ -0,0 +1,41 @@
+import {
+  createContext,
+  createElement,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from '.';
+import {forwardRef} from './compat';
+
+export {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+  forwardRef,
+  createElement,
+};
+
+export default {
+  useState,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+  useContext,
+  useCallback,
+  createContext,
+  useImperativeHandle,
+  useMemo,
+  forwardRef,
+  createElement,
+};
