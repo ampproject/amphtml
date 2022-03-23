1e4481f17578afd2e16bba7da062b2b761dbe957
Alan Orozco <alan@orozco.xyz>
2022-03-22 18:11:16 -0700

    catch

--- src/core/json-schema.ts
+++ src/core/json-schema.ts
@@ -2,7 +2,7 @@ export function isValidCurrencyCode(data: any) {
   try {
     Intl.NumberFormat('en-EN', {currency: data}).format(0);
     return true;
-  } catch (_) {
+  } catch {
     return false;
   }
 }
