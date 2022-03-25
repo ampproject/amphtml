ebb6504863214df7985dbfdefd620ee372d3bb7a
scottrippey <scott.william.rippey@gmail.com>
2022-03-25 10:53:35 -0600

    feature(bento-imgur): updated bento attribute mappings

--- src/bento/components/bento-imgur/1.0/base-element.js
+++ src/bento/components/bento-imgur/1.0/base-element.js
@@ -1,6 +1,6 @@
-import {CSS as COMPONENT_CSS} from './component.jss';
-import {BentoImgur} from './component';
 import {PreactBaseElement} from '#preact/base-element';
+
+import {BentoImgur} from './component';
 
 export class BaseElement extends PreactBaseElement {}
 
@@ -9,9 +9,7 @@ BaseElement['Component'] = BentoImgur;
 
 /** @override */
 BaseElement['props'] = {
-  'children': {passthrough: true},
-  // 'children': {passthroughNonEmpty: true},
-  // 'children': {selector: '...'},
+  'imgurId': {attr: 'data-imgur-id'},
 };
 
 /** @override */
@@ -19,10 +17,3 @@ BaseElement['layoutSizeDefined'] = true;
 
 /** @override */
 BaseElement['usesShadowDom'] = true;
-
-// DO NOT SUBMIT: If BaseElement['shadowCss']  is set to `null`, remove the
-// following declaration.
-// Otherwise, keep it when defined to an actual value like `COMPONENT_CSS`.
-// Once addressed, remove this set of comments.
-/** @override */
-BaseElement['shadowCss'] = COMPONENT_CSS;
