e9320ffebe262a787ac8395c91fd60f321715081
scottrippey <scott.william.rippey@gmail.com>
2022-03-25 10:03:57 -0600

    feature(bento-imgur): added unit test for successful loading

--- src/bento/components/bento-imgur/1.0/test/test-component.js
+++ src/bento/components/bento-imgur/1.0/test/test-component.js
@@ -1,14 +1,58 @@
-import * as Preact from '#preact';
-import {BentoImgur} from '../component';
 import {mount} from 'enzyme';
 
-describes.sandboxed('BentoImgur preact component v1.0', {}, (env) => {
-  // DO NOT SUBMIT: This is example code only.
-  it('should render', () => {
-    const wrapper = mount(<BentoImgur testProp={true} />);
+import {BentoImgur} from '#bento/components/bento-imgur/1.0/component';
 
-    const component = wrapper.find(BentoImgur.name);
-    expect(component).to.have.lengthOf(1);
-    expect(component.prop('testProp')).to.be.true;
+import * as Preact from '#preact';
+
+describes.sandboxed('BentoImgur preact component v1.0', {}, (env) => {
+  it('should render a single post', () => {
+    const wrapper = mount(<BentoImgur imgurId="TEST" />);
+
+    const iframe = wrapper.find('iframe');
+    expect(iframe.prop('src')).to.include(`https://imgur.com/TEST/embed`);
+  });
+  it('should render an album/gallery', () => {
+    const wrapper = mount(<BentoImgur imgurId="a/TEST" />);
+
+    const iframe = wrapper.find('iframe');
+    expect(iframe.prop('src')).to.include(`https://imgur.com/a/TEST/embed`);
+  });
+  it('should escape ids if needed', () => {
+    const wrapper = mount(<BentoImgur imgurId="a/TEST$%&TEST" />);
+
+    const iframe = wrapper.find('iframe');
+    expect(iframe.prop('src')).to.include(
+      `https://imgur.com/a/TEST%24%25%26TEST/embed`
+    );
+  });
+  it('should resize', () => {
+    const wrapper = mount(<BentoImgur imgurId="TEST" />);
+
+    const iframe = wrapper.find('iframe').getDOMNode();
+
+    dispatchIframeMessage({
+      iframe,
+      data: {
+        message: 'resize_imgur',
+        height: 999,
+      },
+    });
+    wrapper.update();
+
+    expect(findContainer(wrapper).prop('style').height).to.equal(999);
   });
 });
+
+function findContainer(wrapper) {
+  return wrapper.find('div').first();
+}
+
+function dispatchIframeMessage({data, iframe}) {
+  const mockEvent = new CustomEvent('message');
+
+  mockEvent.source = iframe.contentWindow;
+  mockEvent.origin = new URL(iframe.src).origin;
+  mockEvent.data = JSON.stringify(data);
+
+  window.dispatchEvent(mockEvent);
+}
