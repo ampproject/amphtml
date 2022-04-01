012af157ef3766845c6952c05a79ae02fc6d3b34
Ya-Chen Hsieh <1697814+ychsieh@users.noreply.github.com>
2022-04-01 16:49:53 -0400

    lint

--- examples/visual-tests/amp-story/amp-story-subscriptions.js
+++ examples/visual-tests/amp-story/amp-story-subscriptions.js
@@ -5,26 +5,26 @@ const {
 } = require('../../../build-system/tasks/visual-diff/verifiers');
 
 module.exports = {
-  'paywall UI should display after navigating to the paywall page': async (page, name) => {
+  'paywall UI should display after navigating to the paywall page': async (
+    page,
+    name
+  ) => {
     const pageID = 'page-2';
     const url = await page.url();
     await page.goto(`${url}#page=${pageID}`);
-    await page.waitForSelector(
-      `amp-subscriptions-dialog:not([hidden])`
-    );
+    await page.waitForSelector(`amp-subscriptions-dialog:not([hidden])`);
     await verifySelectorsVisible(page, name, [
       '.i-amphtml-story-subscriptions-visible',
     ]);
   },
-  'paywall UI should display after deep linking to the locked page after paywall page': async (page, name) => {
-    const pageID = 'page-3';
-    const url = await page.url();
-    await page.goto(`${url}#page=${pageID}`);
-    await page.waitForSelector(
-      `amp-subscriptions-dialog:not([hidden])`
-    );
-    await verifySelectorsVisible(page, name, [
-      '.i-amphtml-story-subscriptions-visible',
-    ]);
-  },
+  'paywall UI should display after deep linking to the locked page after paywall page':
+    async (page, name) => {
+      const pageID = 'page-3';
+      const url = await page.url();
+      await page.goto(`${url}#page=${pageID}`);
+      await page.waitForSelector(`amp-subscriptions-dialog:not([hidden])`);
+      await verifySelectorsVisible(page, name, [
+        '.i-amphtml-story-subscriptions-visible',
+      ]);
+    },
 };
