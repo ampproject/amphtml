27e6bb0507506e37a7a56fbe1482e53da6867efd
Ya-Chen Hsieh <1697814+ychsieh@users.noreply.github.com>
2022-04-01 18:21:49 -0400

    add dependency

--- build-system/test-configs/dep-check-config.js
+++ build-system/test-configs/dep-check-config.js
@@ -260,6 +260,7 @@ exports.rules = [
 
       // AMP Story Subscriptions.
       'extensions/amp-story-subscriptions/0.1/amp-story-subscriptions.js->extensions/amp-story/1.0/amp-story-store-service.js',
+      'extensions/amp-story-subscriptions/0.1/amp-story-subscriptions.js->extensions/amp-story/1.0/utils.js',
 
       // Story localization.
       'extensions/amp-story-360/0.1/amp-story-360.js->extensions/amp-story/1.0/amp-story-localization-service.js',
