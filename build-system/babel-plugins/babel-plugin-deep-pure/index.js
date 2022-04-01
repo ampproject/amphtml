4429ba3f5a9d784d701621bab2ee381737b6bb53
Alan Orozco <alan@orozco.xyz>
2022-04-01 13:15:48 -0700

    transform

--- build-system/babel-plugins/babel-plugin-deep-pure/index.js
+++ build-system/babel-plugins/babel-plugin-deep-pure/index.js
@@ -1,22 +1,44 @@
+const pureFnName = 'pure';
+
 /** @return {import('@babel/core').PluginObj} */
 module.exports = function () {
   /** @param {import('@babel/core').NodePath} path */
   function setIsPure(path) {
     path.addComment('leading', ' #__PURE__ ');
   }
+
+  /**
+   * @param {import('@babel/core').NodePath<import('@babel/types').CallExpression>} path
+   * @return {boolean}
+   */
+  function replacePureFnCallExpression(path) {
+    if (
+      path.node.arguments.length !== 1 ||
+      !path.get('callee').isIdentifier({name: pureFnName})
+    ) {
+      return false;
+    }
+    path.replaceWith(path.node.arguments[0]);
+    return true;
+  }
+
   return {
     name: 'deep-pure',
     visitor: {
       CallExpression(path) {
-        if (
-          path.node.arguments.length === 1 &&
-          path.get('callee').isIdentifier({name: 'pure'})
-        ) {
-          path.replaceWith(path.node.arguments[0]);
+        if (replacePureFnCallExpression(path)) {
           path.traverse({
             NewExpression: setIsPure,
-            MemberExpression: setIsPure,
-            CallExpression: setIsPure,
+            CallExpression(path) {
+              if (!replacePureFnCallExpression(path)) {
+                setIsPure(path);
+              }
+            },
+            MemberExpression(path) {
+              throw path.buildCodeFrameError(
+                `${pureFnName}() expressions cannot contain member expressions`
+              );
+            },
           });
         }
       },
