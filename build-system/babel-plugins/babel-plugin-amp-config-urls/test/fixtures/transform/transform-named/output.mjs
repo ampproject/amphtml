a0456084f4de7d82225611e96be089c974d0ad4a
Alan Orozco <alan@orozco.xyz>
2022-04-05 16:57:38 -0700

    test

---
+++ build-system/babel-plugins/babel-plugin-amp-config-urls/test/fixtures/transform/transform-named/output.mjs
@@ -0,0 +1,5 @@
+import { ampConfigUrlsDoNotImportMeUseConfigUrlsInstead as _ampConfigUrlsDoNotImportMeUseConfigUrlsInstead } from "#core/amp-config-urls";
+
+const bar = /* #__PURE__ */_ampConfigUrlsDoNotImportMeUseConfigUrlsInstead("foo");
+
+const named = /* #__PURE__ */_ampConfigUrlsDoNotImportMeUseConfigUrlsInstead("named");
