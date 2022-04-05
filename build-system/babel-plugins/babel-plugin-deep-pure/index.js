7bc7fc771bd6b94af5c1eaad47db44ee3e256721
Alan Orozco <alan@orozco.xyz>
2022-04-05 11:06:58 -0700

    transform-relative-path

--- build-system/babel-plugins/babel-plugin-deep-pure/index.js
+++ build-system/babel-plugins/babel-plugin-deep-pure/index.js
@@ -1,5 +1,8 @@
+const {dirname, relative} = require('path');
+
 const pureFnName = 'pure';
-const pureFnImportSource = '#core/types/pure';
+const pureFnImportSourceAliased = '#core/types/pure';
+const pureFnImportSourceDirect = 'src/core/types/pure';
 
 /**
  * Deeply adds #__PURE__ comments to an expression passed to a function named
@@ -7,15 +10,33 @@ const pureFnImportSource = '#core/types/pure';
  * @return {import('@babel/core').PluginObj}
  */
 module.exports = function () {
+  let filename;
+  let relativePureFnImportSource;
+
   /**
    * @param {import('@babel/core').NodePath<import('@babel/types').CallExpression>} path
    * @return {boolean}
    */
   function isPureFnCallExpression(path) {
-    return (
-      path.node.arguments.length === 1 &&
-      path.get('callee').referencesImport(pureFnImportSource, pureFnName)
-    );
+    if (path.node.arguments.length !== 1) {
+      return false;
+    }
+    const callee = path.get('callee');
+    if (callee.referencesImport(pureFnImportSourceAliased, pureFnName)) {
+      return true;
+    }
+    if (!relativePureFnImportSource) {
+      if (!filename) {
+        throw new Error(
+          'babel-plugin-deep-pure must be called with a filename'
+        );
+      }
+      relativePureFnImportSource = relative(
+        dirname(filename),
+        `${process.cwd()}/${pureFnImportSourceDirect}`
+      );
+    }
+    return callee.referencesImport(relativePureFnImportSource, pureFnName);
   }
 
   /** @param {import('@babel/core').NodePath} path */
@@ -31,6 +52,11 @@ module.exports = function () {
   return {
     name: 'deep-pure',
     visitor: {
+      Program: {
+        enter() {
+          filename = this.file.opts.filename;
+        },
+      },
       CallExpression(path) {
         if (isPureFnCallExpression(path)) {
           path.traverse({
