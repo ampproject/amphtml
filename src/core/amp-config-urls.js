81923a94304583715491e8c930435b39ac7c7fd9
Alan Orozco <alan@orozco.xyz>
2022-04-06 10:28:37 -0700

    oops

--- src/core/amp-config-urls.js
+++ src/core/amp-config-urls.js
@@ -2,10 +2,9 @@
  * DO NOT USE THIS FUNCTION DIRECTLY.
  * It's meant only to be used by babel plugin output.
  * You should import `src/config/urls` instead.
- * @param {string=} k
  * @return {{[string: string]: string|RegExp}|string|RegExp}
  */
-export const ampConfigUrlsDoNotImportMeUseConfigUrlsInstead = (k) => {
+export const ampConfigUrlsDoNotImportMeUseConfigUrlsInstead = () => {
   // @ts-ignore
-  return self.AMP.config;
+  return self.AMP.config.urls;
 };
