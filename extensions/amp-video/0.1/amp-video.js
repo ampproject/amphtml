26dce88dd32a15b574a7e67e819c5a914613186b
Corey Masanto <masanto@google.com>
2022-04-20 15:00:27 -0400

    Lint fix

--- extensions/amp-video/0.1/amp-video.js
+++ extensions/amp-video/0.1/amp-video.js
@@ -397,21 +397,23 @@ export class AmpVideo extends AMP.BaseElement {
         this.video_.setAttribute('preload', 'auto');
       }
 
-      pendingOriginPromise = this.getAmpDoc().whenFirstPreviewedOrVisible().then(() => {
-        this.propagateLayoutChildren_();
-        // We need to yield to the event queue before listing for loadPromise
-        // because this element may still be in error state from the pre-render
-        // load.
-        return Services.timerFor(this.win)
-          .promise(1)
-          .then(() => {
-            // Don't wait for the source to load if media pool is taking over.
-            if (this.isManagedByPool_()) {
-              return;
-            }
-            return this.loadPromise(this.video_);
-          });
-      });
+      pendingOriginPromise = this.getAmpDoc()
+        .whenFirstPreviewedOrVisible()
+        .then(() => {
+          this.propagateLayoutChildren_();
+          // We need to yield to the event queue before listing for loadPromise
+          // because this element may still be in error state from the pre-render
+          // load.
+          return Services.timerFor(this.win)
+            .promise(1)
+            .then(() => {
+              // Don't wait for the source to load if media pool is taking over.
+              if (this.isManagedByPool_()) {
+                return;
+              }
+              return this.loadPromise(this.video_);
+            });
+        });
     } else {
       this.propagateLayoutChildren_();
     }
