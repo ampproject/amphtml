7e6ba4c9e3e4722c022e0bff0eeed179fd4c3610
adenzanetwork <dev@adenza.network>
2022-04-22 22:46:32 +0700

    Fix eslint errors

--- ads/vendors/adenza.js
+++ ads/vendors/adenza.js
@@ -7,7 +7,10 @@ import {validateData, writeScript} from '#3p/3p';
 export function adenza(global, data) {
   validateData(data, ['blockId']);
 
-  const url = 'https://adenza.network/network/data/teasers/' + encodeURIComponent(data['blockId']) + '/script?async=1&div=c';
+  const url =
+    'https://adenza.network/network/data/teasers/' +
+    encodeURIComponent(data['blockId']) +
+    '/script?async=1&div=c';
 
   const mainBlock = global.document.getElementById('c');
   const insertionBlock = global.document.createElement('div');
@@ -16,10 +19,13 @@ export function adenza(global, data) {
 
   window.context.observeIntersection(function (changes) {
     changes.forEach(function () {
-      const findIframe = global.document.getElementById('c').querySelector('iframe');
-      if(findIframe){
-        const height = findIframe?.style?.height;
-        if(height){
+      const findIframe = global.document
+        .getElementById('c')
+        .querySelector('iframe');
+      if (findIframe) {
+        const styleString = findIframe.getAttribute('style');
+        const height = getStyleAdenza(styleString, 'height');
+        if (height) {
           window.context.requestResize(undefined, height);
         }
       }
@@ -27,13 +33,33 @@ export function adenza(global, data) {
   });
 
   writeScript(
-  global,
-  url,
-  () => {
-    global.context.renderStart();
-  },
-  () => {
-    global.context.noContentAvailable();
-  }
+    global,
+    url,
+    () => {
+      global.context.renderStart();
+    },
+    () => {
+      global.context.noContentAvailable();
+    }
   );
+}
+
+/**
+ * @param {string} style
+ * @param {string} prop
+ * @return {string}
+ */
+function getStyleAdenza(style, prop) {
+  const styleArr = style.split(';');
+  const res = styleArr.filter(function (value) {
+    return value.includes(prop);
+  });
+  if (res) {
+    return res
+      .join('')
+      .replace(prop + ':', '')
+      .replace(/\s+/g, '');
+  } else {
+    return '480px';
+  }
 }
