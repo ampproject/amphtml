f413856225a70f8366ec8f1806249258737518e0
adenzanetwork <dev@adenza.network>
2022-04-22 14:05:54 +0700

    Fix eslint errors try 1

--- ads/vendors/adenza.js
+++ ads/vendors/adenza.js
@@ -7,36 +7,33 @@ import {validateData, writeScript} from '#3p/3p';
 export function adenza(global, data) {
   validateData(data, ['blockId']);
 
-  const url =
-    'https://adenza.network/network/data/teasers/' +
-    encodeURIComponent(data['blockId']) +
-    '/script?async=1&div=c';
+  const url = 'https://adenza.network/network/data/teasers/' + encodeURIComponent(data['blockId']) + '/script?async=1&div=c';
 
-    const mainBlock = global.document.getElementById('c');
-    const insertionBlock = global.document.createElement('div');
-    insertionBlock.setAttribute('id', 'pw-net-' + data.blockId);
-    mainBlock.append(insertionBlock);
+  const mainBlock = global.document.getElementById('c');
+  const insertionBlock = global.document.createElement('div');
+  insertionBlock.setAttribute('id', 'pw-net-' + data.blockId);
+  mainBlock.append(insertionBlock);
 
-    window.context.observeIntersection(function (changes) {
-      changes.forEach(function (c) {
-        const findIframe = global.document.getElementById('c').querySelector('iframe');
-        if(findIframe){
-          const height = findIframe?.style?.height;
-          if(height){
-            window.context.requestResize(undefined, height);
-          }
+  window.context.observeIntersection(function (changes) {
+    changes.forEach(function () {
+      const findIframe = global.document.getElementById('c').querySelector('iframe');
+      if(findIframe){
+        const height = findIframe?.style?.height;
+        if(height){
+          window.context.requestResize(undefined, height);
         }
-      });
+      }
     });
+  });
 
-    writeScript(
-    global,
-    url,
-    () => {
-      global.context.renderStart();
-    },
-    () => {
-      global.context.noContentAvailable();
-    }
+  writeScript(
+  global,
+  url,
+  () => {
+    global.context.renderStart();
+  },
+  () => {
+    global.context.noContentAvailable();
+  }
   );
 }
