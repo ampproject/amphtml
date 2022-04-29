6e22103bf3d1787c5c8dfe9c3d88d9b8f58f2501
jioads <jioads.sdk@ril.com>
2022-04-29 21:31:16 +0530

    adding jioads network

--- ads/vendors/jioads.js
+++ ads/vendors/jioads.js
@@ -18,23 +18,25 @@ export function jioads(global, data) {
   }
   writeScript(
     global,
-    'https://mercury.akamaized.net/jioads/websdk/amp/jioAds.js?rnd='+Math.random()
+    'https://mercury.akamaized.net/jioads/websdk/amp/jioAds.js?rnd=' +
+    Math.random()
   );
   loadScript(
     global,
-    'https://mercury.akamaized.net/jioads/websdk/amp/ampwrapper.js?rnd='+Math.random(),
+    'https://mercury.akamaized.net/jioads/websdk/amp/ampwrapper.js?rnd=' +
+    Math.random(),
     () => {
       let refresh = '';
       let adMetaData = '';
       let video = '';
       let videocontainer = '';
-      if (data.hasOwnProperty('refreshRate')) {
+      if (data.hasOwn('refreshRate')) {
         refresh = ` data-refresh-rate="${data['refreshRate']}"`;
       }
-      if (data.hasOwnProperty('adMetaData')) {
+      if (data.hasOwn('adMetaData')) {
         adMetaData = ` data-adMetaData="${data['adMetaData']}"`;
       }
-      if (data.hasOwnProperty('videoAd') && data['videoAd'] == '1') {
+      if (data.hasOwn('videoAd') && data['videoAd'] == '1') {
         video = `<div id="instreamContainer" style="width:100%;"></div>`;
         videocontainer = ` data-container-id="instreamContainer"`;
       }
@@ -42,28 +44,30 @@ export function jioads(global, data) {
       const container = document.createElement('div');
       container.classList.add('jads-flex-center', 'jads-f-align-center');
       container.setAttribute('id', 'jads_amp_ad');
-      container.style.width = `${global.context.initialIntersection.boundingClientRect.width}px`;
-      container.style.height = `${global.context.initialIntersection.boundingClientRect.height}px`; 
-      html = `${video} <ins id="${global.context.container}" data-adspot-key="${data['adspot']}" data-source="${data['pkgName']}" ${refresh} ${adMetaData} ${videocontainer}></ins>`;
+      container.setStyle('width',
+            `${global.context.initialIntersection.boundingClientRect.width}px`);
+      container.setStyle('height',
+            `${global.context.initialIntersection.boundingClientRect.height}px`);
+      html = `${video} <ins id="${global.context.container}"
+       data-adspot-key="${data['adspot']}"
+       data-source="${data['pkgName']}"
+       ${refresh} ${adMetaData} ${videocontainer}></ins>`;
       container.innerHTML = html;
       global.document.getElementById('c').appendChild(container);
     },
     () => {
-      console.log('fall back from wrapper');
       global.context.noContentAvailable();
     }
   );
-
-// addon observation on entering/leaving the view when scroll vertically and horizontally
-window.context.observeIntersection(function (newrequest) {
-    /** @type {!Array} */ 
-    (newrequest).forEach(function (data) {
-        if (data.intersectionRatio >= 0.5) {
-            global.context.initialViewAbility = true;
-        } else {
-            global.context.initialViewAbility = false;
-        }
-    });
-});
-
+  // addon observation on entering/leaving the view when scroll vertically and horizontally
+  window.context.observeIntersection(function (newrequest) {
+      /** @type {!Array} */
+      (newrequest).forEach(function (data) {
+          if (data.intersectionRatio >= 0.5) {
+              global.context.initialViewAbility = true;
+          } else {
+              global.context.initialViewAbility = false;
+          }
+      });
+  });
 }
