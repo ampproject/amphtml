99ad4f07456c22ed6fd0ff83a79774a897b74f5d
scottrippey <scott.william.rippey@gmail.com>
2022-05-06 15:09:27 -0600

    feature(bento-mega-menu): implemented a shim-based approach to bento-mode, similar to accordion

--- src/bento/components/bento-mega-menu/1.0/base-element.js
+++ src/bento/components/bento-mega-menu/1.0/base-element.js
@@ -3,6 +3,7 @@ import {childElementsByTag} from '#core/dom/query';
 import {toArray} from '#core/types/array';
 
 import * as Preact from '#preact';
+import {useLayoutEffect} from '#preact';
 import {PreactBaseElement} from '#preact/base-element';
 
 import {BentoMegaMenu} from './component';
@@ -13,7 +14,7 @@ export class BaseElement extends PreactBaseElement {
   init() {
     const {element} = this;
     const mu = new MutationObserver(() => {
-      this.mutateProps({children: mapChildrenFromDom(element, mu)});
+      // this.mutateProps({children: mapChildrenFromDom(element, mu)});
     });
     mu.observe(element, {
       attributeFilter: ['expanded', 'id'],
@@ -30,10 +31,13 @@ function mapChildrenFromDom(element, mu) {
   const children = sections.map((section) => {
     const {firstElementChild: title, lastElementChild: contents} = section;
 
+    const TitleShim = getTitleShim(title);
+    const ContentShim = getContentShim(contents);
+
     const item = (
       <BentoMegaMenu.Item>
-        <BentoMegaMenu.Title>{title.innerText}</BentoMegaMenu.Title>
-        <BentoMegaMenu.Content>{contents.innerText}</BentoMegaMenu.Content>
+        <BentoMegaMenu.Title as={TitleShim} />
+        <BentoMegaMenu.Content as={ContentShim} />
       </BentoMegaMenu.Item>
     );
     return item;
@@ -41,20 +45,72 @@ function mapChildrenFromDom(element, mu) {
   return children;
 }
 
+function getTitleShim(element) {
+  return function TitleShim(props) {
+    useLayoutEffect(() => {
+      setAttributes(element, props);
+      return () => unsetAttributes(element, props);
+    }, [props]);
+
+    return null;
+  };
+}
+function getContentShim(element) {
+  return function ContentShim(props) {
+    useLayoutEffect(() => {
+      setAttributes(element, props);
+      return () => unsetAttributes(element, props);
+    }, [props]);
+
+    return null;
+  };
+}
+
 /** @override */
 BaseElement['Component'] = BentoMegaMenu;
 
 /** @override */
 BaseElement['props'] = {
-  'children': {passthrough: true},
+  // 'children': {passthrough: true},
 };
 
+// Prevent the Preact component from showing:
 /** @override */
-BaseElement['layoutSizeDefined'] = true;
+// BaseElement['detached'] = true;
 
-// TODO: Should shadowDom be disabled, like bento-accordion?
-/** @override */
-BaseElement['usesShadowDom'] = true;
+function setAttributes(element, props) {
+  updateAttributes(element, props, false);
+}
+function unsetAttributes(element, props) {
+  updateAttributes(element, props, true);
+}
+function updateAttributes(element, props, unset) {
+  Object.keys(props).forEach((prop) => {
+    const value = props[prop];
+    if (value === undefined || value === false) {
+      return;
+    }
 
-/** @override */
-BaseElement['shadowCss'] = COMPONENT_CSS;
+    if (prop === 'onClick') {
+      if (!unset) {
+        console.log('addEventListener', value);
+        element.addEventListener('click', value);
+      } else {
+        element.removeEventListener('click', value);
+      }
+    } else if (prop === 'class' || prop === 'className') {
+      const classes = value.split(' ');
+      if (!unset) {
+        element.classList.add(...classes);
+      } else {
+        element.classList.remove(...classes);
+      }
+    } else {
+      if (!unset) {
+        element.setAttribute(prop, value);
+      } else {
+        element.removeAttribute(prop);
+      }
+    }
+  });
+}
