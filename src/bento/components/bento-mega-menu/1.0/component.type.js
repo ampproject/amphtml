9e064b7bcb4df758738ffae47434b01a217683ed
scottrippey <scott.william.rippey@gmail.com>
2022-05-13 15:56:54 -0600

    feature(bento-mega-menu): removed dead code

--- src/bento/components/bento-mega-menu/1.0/component.type.js
+++ src/bento/components/bento-mega-menu/1.0/component.type.js
@@ -5,13 +5,9 @@ var BentoMegaMenuDef = {};
 
 /**
  * @typedef {{
- *   exampleProperty: (string|undefined), (DO NOT SUBMIT)
  * }}
  */
 BentoMegaMenuDef.Props;
 
 /** @interface */
-BentoMegaMenuDef.BentoMegaMenuApi = class {
-  /** Example: API method to toggle the component */
-  exampleToggle() {} // DO NOT SUBMIT
-};
+BentoMegaMenuDef.BentoMegaMenuApi = class {};
