6947e0041696a60d92c7d5bec657461a24baa1dc
scottrippey <scott.william.rippey@gmail.com>
2022-05-20 13:26:37 -0600

    feature(bento-mega-menu): added simpler implementation of `objectsEqualDeep`

--- src/preact/hooks/useDeepValue.ts
+++ src/preact/hooks/useDeepValue.ts
@@ -1,3 +1,5 @@
+import {objectsEqualDeep} from '#core/types/object';
+
 import {useRef} from '#preact';
 
 export type Comparer<T> = (a: T, b: T) => boolean;
@@ -8,61 +10,15 @@ export type Comparer<T> = (a: T, b: T) => boolean;
  * Especially useful for dependency arrays.
  *
  * @param value - Any object, array, or value, which will be deep-compared
- * @param compare - Should return `true` if the values are the same
+ * @param [compare] - Should return `true` if the values are the same
  */
 export function useDeepValue<TObj>(
   value: TObj,
-  compare: Comparer<TObj> = fastDeepEqual
+  compare: Comparer<TObj> = objectsEqualDeep
 ): TObj {
   const previousValue = useRef(value);
   if (!compare(previousValue.current, value)) {
     previousValue.current = value;
   }
   return previousValue.current;
-}
-
-// Modified from https://github.com/epoberezkin/fast-deep-equal/blob/master/src/index.jst
-function fastDeepEqual(a: any, b: any) {
-  if (a === b) {
-    return true;
-  }
-
-  if (a && b && typeof a == 'object' && typeof b == 'object') {
-    // Deep-compare arrays:
-    if (Array.isArray(a)) {
-      const {length} = a;
-      if (length != b.length) {
-        return false;
-      }
-      for (let i = length; i-- !== 0; ) {
-        if (!fastDeepEqual(a[i], b[i])) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    // Deep-compare objects:
-    const keys = Object.keys(a);
-    const {length} = keys;
-    if (length !== Object.keys(b).length) {
-      return false;
-    }
-    for (let i = length; i-- !== 0; ) {
-      if (!Object.prototype.hasOwnProperty.call(b, keys[i])) {
-        return false;
-      }
-    }
-    for (let i = length; i-- !== 0; ) {
-      const key = keys[i];
-      if (!fastDeepEqual(a[key], b[key])) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  // true if both NaN, false otherwise
-  return a !== a && b !== b; // eslint-disable-line no-self-compare
 }
