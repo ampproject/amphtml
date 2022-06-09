229f749cabad76c4e16c09de1227f08bf30fd0b2
Matias Szylkowski <mszylkowski@google.com>
2022-06-09 18:07:40 -0400

    🔥 [Story videocache] Apply hasAudio flag behind experiment (#38285)
    * Apply hasAudio flag behind experiment
    
    * Added spec onto experiment config
    
    * Fixed unit tests
    
    (cherry picked from commit ed74a4640ab1e5d3a1c2d4563ca8aa35f73285ce)

--- tools/experiments/experiments-config.js
+++ tools/experiments/experiments-config.js
@@ -183,4 +183,9 @@ export const EXPERIMENTS = [
     name: 'Load inactive pages outside the viewport to prevent them from counting towards LCP when invisible',
     spec: 'https://github.com/ampproject/amphtml/pull/35323',
   },
+  {
+    id: 'story-video-cache-apply-audio',
+    name: 'Apply the hasAudio flag from cached videos on stories',
+    spec: 'https://github.com/ampproject/amphtml/pull/38285',
+  },
 ];
