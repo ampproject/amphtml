b82b832cae23ff854449af280f1b02f4dfeb99f4
Maximilian Haupt <max@main.dev>
2022-08-02 20:08:01 +0200

    Add CANONICAL_URL to allowed list of variables (#38207)

--- extensions/amp-consent/0.1/consent-config.js
+++ extensions/amp-consent/0.1/consent-config.js
@@ -25,6 +25,7 @@ const ALLOWED_DEPR_CONSENTINSTANCE_ATTRS = {
 
 /** @const @type {!Object<string, boolean>} */
 const CONSENT_VARS_ALLOWED_LIST = {
+  'CANONICAL_URL': true,
   'PAGE_VIEW_ID': true,
   'PAGE_VIEW_ID_64': true,
   'SOURCE_URL': true,
