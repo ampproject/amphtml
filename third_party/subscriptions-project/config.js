940a644a8b6bc59dcdf17040032b5a37d50aead4
kristenwang23@gmail.com <chuyangwang@google.com>
2022-08-10 21:10:45 +0000

    SwG Release 0.1.22.229

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 0.1.22.228 */
+/** Version: 0.1.22.229 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
