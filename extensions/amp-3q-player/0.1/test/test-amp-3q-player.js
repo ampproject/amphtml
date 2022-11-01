175c8e97084a6ebde410a04d3f706f1fc08cf663
nikola <nikola.njavro@3q.video>
2022-11-01 17:32:26 +0100

    q

--- extensions/amp-3q-player/0.1/test/test-amp-3q-player.js
+++ extensions/amp-3q-player/0.1/test/test-amp-3q-player.js
@@ -110,7 +110,7 @@ describes.realWin(
       it('requires data-id', () => {
         return allowConsoleError(() => {
           return get3QElement('').should.eventually.be.rejectedWith(
-            'Data-id attribute is required for <amp-3q-player>'
+            'Data-player attribute is required for <amp-3q-player>'
           );
         });
       });
@@ -118,7 +118,7 @@ describes.realWin(
       it('requires data-player', () => {
         return allowConsoleError(() => {
           return get3QElement('').should.eventually.be.rejectedWith(
-            'Data-player attribute is required for <amp-3q-player>'
+            'Data-id attribute is required for <amp-3q-player>'
           );
         });
       });
