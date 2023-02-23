4b1c8698efa2107d1f0492b302f128f15953731b
Amaltas <amaltas@amaltas.org>
2023-02-23 10:09:23 -0800

    add check for openjs domain in validator.js

--- validator/js/engine/validator.js
+++ validator/js/engine/validator.js
@@ -4799,7 +4799,7 @@ function validateClassAttr(attr, tagSpec, context, result) {
  * @param {!generated.ValidationResult} result
  */
 function validateAmpScriptSrcAttr(tag, attrValue, tagSpec, context, result) {
-  if (!tag.isAmpDomain()) {
+  if (!(tag.isAmpDomain() || tag.isOpenjsDomain)) {
     context.addError(
         generated.ValidationError.Code.DISALLOWED_AMP_DOMAIN,
         context.getLineCol(), /* params */[], /* spec_url*/ '', result);
