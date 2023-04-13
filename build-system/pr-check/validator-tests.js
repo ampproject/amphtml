aa18782cdfd657e3eb41db3a459d44066ea6f1b9
erwin mombay <erwinm@google.com>
2023-04-13 13:52:31 -0700

    disable amp validator tests (#38751)
    We now build the C++ validator into wasm and no longer need the JS
    validator and its tests.

--- build-system/pr-check/validator-tests.js
+++ build-system/pr-check/validator-tests.js
@@ -14,8 +14,9 @@ const jobName = 'validator-tests.js';
  * Steps to run during push builds.
  */
 function pushBuildWorkflow() {
-  timedExecOrDie('amp validator-webui');
-  timedExecOrDie('amp validator');
+  // NOTE: We've removed the amp validator calls as the JS validator and its
+  // tests are no longer needed since we now create a WASM build off of the
+  // the C++ code.
   // TODO(#38610): fix for bazel 6.0 or use older version
   // timedExecOrDie('amp validator-cpp');
   timedExecOrDie('amp validate-html-fixtures');
@@ -39,25 +40,6 @@ function prBuildWorkflow() {
     );
     return;
   }
-
-  if (buildTargetsInclude(Targets.VALIDATOR_WEBUI)) {
-    timedExecOrDie('amp validator-webui');
-  }
-
-  if (
-    buildTargetsInclude(
-      Targets.HTML_FIXTURES,
-      Targets.RUNTIME,
-      Targets.VALIDATOR
-    )
-  ) {
-    timedExecOrDie('amp validator');
-  }
-
-  // TODO(#38610): fix for bazel 6.0 or use older version
-  // if (buildTargetsInclude(Targets.VALIDATOR)) {
-  //   timedExecOrDie('amp validator-cpp');
-  // }
 
   if (buildTargetsInclude(Targets.HTML_FIXTURES)) {
     timedExecOrDie('amp validate-html-fixtures');
