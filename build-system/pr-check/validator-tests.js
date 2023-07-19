d655495b54f65ecd16f1780158e15bf6e58eebfb
Amaltas <amaltas@google.com>
2023-07-18 22:13:52 -0700

    Update validator-tests.js

--- build-system/pr-check/validator-tests.js
+++ build-system/pr-check/validator-tests.js
@@ -44,11 +44,6 @@ function prBuildWorkflow() {
   if (buildTargetsInclude(Targets.HTML_FIXTURES)) {
     timedExecOrDie('amp validate-html-fixtures');
   }
-
-  if (buildTargetsInclude(Targets.VALIDATOR)) {
-    // Trigger c++ validator early.
-    timedExecOrDie('amp validator-cpp');
-  }
 }
 
 runCiJob(jobName, pushBuildWorkflow, prBuildWorkflow);
