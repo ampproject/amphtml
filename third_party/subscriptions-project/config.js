5a63e4bb12b8834fd49c88f113eee13c05e5a450
Yeongjin Oh <oyj9109@gmail.com>
2023-11-09 20:12:19 +0000

    SwG Release 1.0.46

--- third_party/subscriptions-project/config.js
+++ third_party/subscriptions-project/config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Version: 216488bf */
+/** Version: 1.0.46 */
 /**
  * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
  *
