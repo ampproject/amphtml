ed7bc2b5532900318654b0bc01ad60f874be283d
Fatih Kaya <fatih.kaya.84@gmail.com>
2024-01-15 18:53:19 +0300

    fix

--- ads/vendors/pixad.js
+++ ads/vendors/pixad.js
@@ -5,25 +5,25 @@ import {validateData, writeScript} from '#3p/3p';
  * @param {!Object} data
  */
 export function pixad(global, data) {
-  validateData(data, ['adNetwork','adPublisher','adTypeId']);
+  validateData(data, ['adNetwork', 'adPublisher', 'adTypeId']);
   global._pixad = global._pixad || {
     publisher: data['adNetwork'],
     adNetwork: data['adPublisher'],
     adTypeId: data['adTypeId'],
     host: `static.cdn.pixad.com.tr`,
-    prefix: `px`
+    prefix: `px`,
   };
 
-  if(data['adNetwork'].indexOf("adm-pub") != -1) {
+  if (global._pixad.publisher.indexOf('adm-pub') != -1) {
     global._pixad.host = `static.cdn.admatic.com.tr`;
     global._pixad.prefix = `adm`;
-  };
+  }
 
   const ins = global.document.createElement('ins');
   ins.setAttribute('data-publisher', global._pixad.publisher);
-  if(global._pixad.adTypeId == "standard") {
+  if (global._pixad.adTypeId == 'standard') {
     ins.setAttribute('data-ad-size', `[[${data.width},${data.height}]]`);
-  };
+  }
   ins.setAttribute('data-ad-network', global._pixad.adNetwork);
   ins.setAttribute('data-ad-type-id', global._pixad.adTypeId);
   ins.setAttribute('class', `${global._pixad.prefix}-ads-area`);
@@ -34,5 +34,4 @@ export function pixad(global, data) {
   );
 
   writeScript(global, `https://${global._pixad.host}/showad/showad.min.js`);
-
 }
