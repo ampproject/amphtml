4bf95bd15fdfc6ce70caa02e69ffedff3e7c907f
Daniel Rozenberg <rodaniel@amp.dev>
2024-02-26 14:12:09 -0500

    Nicer format for test names when reporting to CircleCI

--- build-system/tasks/runtime-test/runtime-test-base.js
+++ build-system/tasks/runtime-test/runtime-test-base.js
@@ -133,6 +133,15 @@ class RuntimeTestConfig {
       this.junitReporter = {
         outputFile: `result-reports/${this.testType}.xml`,
         useBrowserName: false,
+        nameFormatter(_, result) {
+          return result.description.trim();
+        },
+        classNameFormatter(_, result) {
+          return result.suite
+            .map((s) => s.trim())
+            .filter(Boolean)
+            .join(' » ');
+        },
       };
     }
 
