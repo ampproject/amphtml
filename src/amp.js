b17abbf53f8adfe8aac332635eae0d5ac5d37073
Daniel Rozenberg <rodaniel@amp.dev>
2024-05-07 16:39:37 -0400

    Test artificial bundle-size increase

--- src/amp.js
+++ src/amp.js
@@ -146,7 +146,7 @@ startupChunk(self.document, function initial() {
 if (self.console) {
   (console.info || console.log).call(
     console,
-    `Powered by AMP ⚡ HTML – Version ${mode.version()}`,
+    `Powered by AMP ⚡ HTML – Version ${mode.version()} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
     self.location.href
   );
 }
