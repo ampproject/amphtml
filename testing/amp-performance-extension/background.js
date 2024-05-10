9a123184adc68fac038f8554dd8944014af9deba
danielrozenberg-test <45243161+danielrozenberg-test@users.noreply.github.com>
2024-05-10 12:28:51 -0400

    DO NOT MERGE testing infra changes

--- testing/amp-performance-extension/background.js
+++ testing/amp-performance-extension/background.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
-chrome.browserAction.setBadgeText({
   text: 'ON',
 });
-chrome.browserAction.setBadgeBackgroundColor({
   color: '#15a341',
 });
 /* eslint-enable no-undef */
