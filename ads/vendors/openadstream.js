4b5db06f6f963c2975c1fd85c4f4611f54f9c0c5
HJK51 <70740626+HJK51@users.noreply.github.com>
2024-07-18 13:58:37 +0900

    Prettier test

--- ads/vendors/openadstream.js
+++ ads/vendors/openadstream.js
@@ -1,24 +1,24 @@
-import { validateData, writeScript } from "#3p/3p";
+import {validateData, writeScript } from '#3p/3p';
 
 /**
  * @param {!Window} global
  * @param {!Object} data
  */
 export function openadstream(global, data) {
-  validateData(data, ["adhost", "urlpath", "sitepage", "pos"], ["query"]);
+  validateData(data, ['adhost', 'urlpath', 'sitepage', 'pos'], ['query']);
 
   let url =
-    "https://" +
+    'https://' +
     encodeURIComponent(data.adhost) +
-    "/" +
+    '/' +
     data.urlpath +
-    "/" +
+    '/' +
     data.sitepage +
-    "@" +
+    '@' +
     data.pos;
 
   if (data.query) {
-    url = url + "?" + data.query;
+    url = url + '?' + data.query;
   }
   writeScript(global, url);
 }
