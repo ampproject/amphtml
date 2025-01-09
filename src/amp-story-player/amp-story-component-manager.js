63fd3d0342ab842b0dfb5483702efb9c9714962e
Hopeberg <anam@itu.dk>
2025-01-09 17:48:56 +0100

    my branch

--- src/amp-story-player/amp-story-component-manager.js
+++ src/amp-story-player/amp-story-component-manager.js
@@ -4,6 +4,10 @@ import {AmpStoryEntryPoint} from './amp-story-entry-point/amp-story-entry-point-
 import {AmpStoryPlayer} from './amp-story-player-impl';
 import {AmpStoryPlayerViewportObserver} from './amp-story-player-viewport-observer';
 
+/**
+   * Mybranch
+   */
+
 export class AmpStoryComponentManager {
   /**
    * @param {!Window} win
